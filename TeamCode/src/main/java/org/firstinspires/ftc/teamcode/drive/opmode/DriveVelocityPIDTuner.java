0fa1efcadc9937336bec9b3d60fdd53c771bad56
Ryan Brott <rcbrott@gmail.com>
2019-06-16 12:42:58 -0700

    One track width tuner to rule them all

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriveVelocityPIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/DriveVelocityPIDTuner.java
@@ -15,6 +15,7 @@ import com.acmerobotics.roadrunner.util.NanoClock;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveBase;
@@ -32,7 +33,7 @@ import java.util.List;
  * successfully connected, start the program, and your robot will begin moving forward and backward
  * according to a motion profile. Your job is to graph the velocity errors over time and adjust the
  * PID coefficients (it's highly suggested to leave F at its default value) like any normal PID
- * controller. Once you've found a satisfactory set of gains, add them to your drive class init.
+ * controller. Once you've found a satisfactory set of gains, add them to your drive class ctor.
  */
 @Config
 @Autonomous(group = "drive")
@@ -45,7 +46,10 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
      */
     public static boolean USE_THEORETICAL_KV = true;
 
+    private static final String PID_VAR_NAME = "VELO_PID";
+
     private FtcDashboard dashboard = FtcDashboard.getInstance();
+    private String catName;
     private CustomVariable catVar;
 
     private SampleMecanumDriveBase drive;
@@ -54,17 +58,22 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
         MotionState start = new MotionState(movingForward ? 0 : DISTANCE, 0, 0, 0);
         MotionState goal = new MotionState(movingForward ? DISTANCE : 0, 0, 0, 0);
         return MotionProfileGenerator.generateSimpleMotionProfile(start, goal,
-                DriveConstants.BASE_CONSTRAINTS.maxVel, DriveConstants.BASE_CONSTRAINTS.maxAccel);
+                DriveConstants.BASE_CONSTRAINTS.maxVel,
+                DriveConstants.BASE_CONSTRAINTS.maxAccel,
+                DriveConstants.BASE_CONSTRAINTS.maxJerk);
     }
 
     private void addPidVariable() {
-        String catName = getClass().getSimpleName();
+        catName = getClass().getSimpleName();
         catVar = (CustomVariable) dashboard.getConfigRoot().getVariable(catName);
         if (catVar == null) {
             // this should never happen...
             catVar = new CustomVariable();
             dashboard.getConfigRoot().putVariable(catName, catVar);
+
+            RobotLog.w("Unable to find top-level category %s", catName);
         }
+
         CustomVariable pidVar = new CustomVariable();
         pidVar.putVariable("kP", new BasicVariable<>(new ValueProvider<Double>() {
             @Override
@@ -105,12 +114,17 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
                         new PIDCoefficients(coeffs.kP, coeffs.kI, value));
             }
         }));
-        catVar.putVariable("VELO_PID", pidVar);
+
+        catVar.putVariable(PID_VAR_NAME, pidVar);
         dashboard.updateConfig();
     }
 
     private void removePidVariable() {
-        catVar.removeVariable("VELO_PID");
+        if (catVar.size() > 1) {
+            catVar.removeVariable(PID_VAR_NAME);
+        } else {
+            dashboard.getConfigRoot().removeVariable(catName);
+        }
         dashboard.updateConfig();
     }
 
@@ -134,7 +148,7 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
 
         boolean movingForwards = true;
         MotionProfile activeProfile = generateProfile(true);
-        double profileStartTimestamp = clock.seconds();
+        double profileStart = clock.seconds();
 
         List<Double> lastWheelPositions = null;
         double lastTimestamp = 0;
@@ -144,15 +158,17 @@ public class DriveVelocityPIDTuner extends LinearOpMode {
 
         while (!isStopRequested()) {
             // calculate and set the motor power
-            double profileTime = clock.seconds() - profileStartTimestamp;
+            double profileTime = clock.seconds() - profileStart;
             double dt = profileTime - lastTimestamp;
             lastTimestamp = profileTime;
+
             if (profileTime > activeProfile.duration()) {
                 // generate a new profile
                 movingForwards = !movingForwards;
                 activeProfile = generateProfile(movingForwards);
-                profileStartTimestamp = clock.seconds();
+                profileStart = clock.seconds();
             }
+
             MotionState motionState = activeProfile.get(profileTime);
             double targetPower = kV * motionState.getV();
             drive.setDrivePower(new Pose2d(targetPower, 0, 0));
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TrackWidthTuner.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.util.Angle;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.MovingStatistics;
+
+import org.firstinspires.ftc.robotcore.internal.system.Misc;
+import org.firstinspires.ftc.teamcode.drive.DriveConstants;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveBase;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDriveREV;
+/*
+ * This routine determines the effective track width. The procedure works by executing a point turn
+ * with a given angle and measuring the difference between that angle and the actual angle (as
+ * indicated by an external IMU/gyro, track wheels, or some other localizer). The quotient
+ * given angle / actual angle gives a multiplicative adjustment to the estimated track width
+ * (effective track width = estimated track width * given angle / actual angle). The routine repeats
+ * this procedure a few times and averages the values for additional accuracy. Note: a relatively
+ * accurate track width estimate is important or else the angular constraints will be thrown off.
+ */
+@Config
+@Autonomous(group = "drive")
+public class TrackWidthTuner extends LinearOpMode {
+    public static double ANGLE = Math.toRadians(180);
+    public static int NUM_TRIALS = 5;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDriveBase drive = new SampleMecanumDriveREV(hardwareMap);
+        // TODO: if you haven't already, set the localizer to something that doesn't depend on
+        // drive encoders for computing the heading
+
+        telemetry.log().add("Press play to begin the track width tuner routine");
+        telemetry.log().add("Make sure your robot has enough clearance to turn smoothly");
+        telemetry.update();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        telemetry.log().clear();
+        telemetry.log().add("Running...");
+        telemetry.update();
+
+        MovingStatistics trackWidthStats = new MovingStatistics(NUM_TRIALS);
+        for (int i = 0; i < NUM_TRIALS; i++) {
+            drive.setPoseEstimate(new Pose2d());
+
+            // it is important to handle heading wraparounds
+            double headingAccumulator = 0;
+            double lastHeading = 0;
+
+            drive.turn(ANGLE);
+
+            while (!isStopRequested() && drive.isBusy()) {
+                double heading = drive.getPoseEstimate().getHeading();
+                headingAccumulator += Angle.norm(heading - lastHeading);
+                lastHeading = heading;
+
+                drive.update();
+            }
+
+            double trackWidth = DriveConstants.TRACK_WIDTH * ANGLE / headingAccumulator;
+            trackWidthStats.add(trackWidth);
+
+            sleep(1000);
+        }
+
+        telemetry.log().clear();
+        telemetry.log().add("Tuning complete");
+        telemetry.log().add(Misc.formatInvariant("Effective track width = %.2f (SE = %.3f)",
+                trackWidthStats.getMean(),
+                trackWidthStats.getStandardDeviation() / Math.sqrt(NUM_TRIALS)));
+        telemetry.update();
+
+        while (!isStopRequested()) {
+            idle();
+        }
+    }
+}
