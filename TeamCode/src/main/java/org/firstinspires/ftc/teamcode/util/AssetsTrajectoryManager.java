963a396cfe79af2890599429a8d6e2776fb229e3
Ryan Brott <rcbrott@gmail.com>
2020-02-24 14:03:54 -0500

    Remove generated trajectory loader

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AssetsTrajectoryManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/AssetsTrajectoryManager.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.util;
 
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.acmerobotics.roadrunner.trajectory.config.TrajectoryConfig;
 import com.acmerobotics.roadrunner.trajectory.config.TrajectoryConfigManager;
 
@@ -12,21 +11,12 @@ import java.io.InputStream;
 /**
  * Set of utilities for loading trajectories from assets (the plugin save location).
  */
-// TODO: retrofit once the new trajectory file structure has settled
-public class AssetsTrajectoryLoader {
+public class AssetsTrajectoryManager {
     /**
      * Loads a trajectory config with the given name.
      */
     public static TrajectoryConfig loadConfig(String name) throws IOException {
         InputStream inputStream = AppUtil.getDefContext().getAssets().open("trajectory/" + name + ".yaml");
         return TrajectoryConfigManager.loadConfig(inputStream);
-    }
-
-    /**
-     * Loads a trajectory with the given name.
-     * @see #loadConfig(String)
-     */
-    public static Trajectory load(String name) throws IOException {
-        return loadConfig(name).toTrajectory(null);
     }
 }
