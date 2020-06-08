27239cc5d0802e7ddad0899cf03d6168a9da8d6d
Zack Horton <zackh6789@gmail.com>
2020-06-08 00:28:57 -0400

    Revert "a bit of work ive done"
    This reverts commit 7f39eb6439524d4b5aded6e8cc9a6c8b8928a4ba.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Trajectories/Trajectory.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Trajectories;
-
-public interface Trajectory {
-
-    public double getVal(double time);
-    public double getVelocity(double time);
-    public double getAcceleration(double time);
-
-}
