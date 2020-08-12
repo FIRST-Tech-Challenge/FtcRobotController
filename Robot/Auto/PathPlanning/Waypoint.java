7f304ef4e53edfc9fcee981a67006cb28778c25f
Albert Cai <albertcai101@alberts-mbp.lan>
2020-08-11 20:25:16 -0700

    actionable in waypoint

--- Robot/Auto/PathPlanning/Waypoint.java
+++ Robot/Auto/PathPlanning/Waypoint.java
@@ -6,10 +6,12 @@ public class Waypoint extends Point {
 
     public Waypoint(double x, double y){
         super(x,y);
+        actionable = false;
     }
 
     public Waypoint(double x, double y, Actions action) {
         super(x,y);
+        actionable = true;
         this.action = action;
     }
 }
