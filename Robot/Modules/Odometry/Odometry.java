bb86c01055e31dd426d7fa724071eb4b62e19e92
Albert Cai <albertcai101@alberts-mbp.lan>
2020-08-15 20:16:49 -0700

    untested circular odo

--- Robot/Modules/Odometry/Odometry.java
+++ Robot/Modules/Odometry/Odometry.java
@@ -11,22 +11,17 @@ import org.firstinspires.ftc.teamcode.rework.Robot.Robot;
  * TeleOp or Autonomous.
  */
 public class Odometry implements Module {
-    RobotPosition robotPosition;
 
+    RobotPosition robotPosition;
     Robot robot;
 
     public DcMotor yLeft;
     public DcMotor yRight;
     public DcMotor mecanum;
 
-    // Constants (easily modified for changing hardware)
-    private static final int LEFT_POD_ENCODER_PORT = 0;
-    private static final int RIGHT_POD_ENCODER_PORT = 1;
-    private static final int MECANUM_POD_ENCODER_PORT = 2;
-
-    private double moveScaleFactor = 0.0007284406721;
-    private double turnScaleFactor = 0.00005282291078;
-    private double strafePredictionFactor = 4.5;
+    private final double INCHES_PER_ENCODER_TICK = 0.0007284406721;
+    private final double LR_ENCODER_DIST_FROM_CENTER = 6.89512052;
+    private final double M_ENCODER_DIST_FROM_CENTER = 4.5;
 
     public Odometry(Robot robot) {
         this.robot = robot; // Odometry needs robot in order to be able to get data from robot
@@ -36,7 +31,6 @@ public class Odometry implements Module {
     }
 
     public void init() {
-
         yLeft = robot.getDcMotor("yLeft");
         yRight = robot.getDcMotor("yRight");
         mecanum = robot.getDcMotor("mecanum");
@@ -73,66 +67,74 @@ public class Odometry implements Module {
         double rightPodDelta = rightPodNewPosition - rightPodOldPosition;
         double mecanumPodDelta = mecanumPodNewPosition - mecanumPodOldPosition;
 
-        smallAngleOdometry(leftPodDelta, rightPodDelta, mecanumPodDelta);
+        calculateNewPosition(leftPodDelta, rightPodDelta, mecanumPodDelta);
 
         leftPodOldPosition = leftPodNewPosition;
         rightPodOldPosition = rightPodNewPosition;
         mecanumPodOldPosition = mecanumPodNewPosition;
     }
 
-    public void smallAngleOdometry(double dLeftPod, double dRightPod, double dMecanumPod) {
-        double dLeftPodInches = dLeftPod * moveScaleFactor;
-        double dRightPodInches = dRightPod * moveScaleFactor;
-        double dMecanumPodInches = dMecanumPod * moveScaleFactor;
+    public void calculateNewPosition(double dLeftPod, double dRightPod, double dMecanumPod) {
+        // convert all inputs to inches
+        double dLeftPodInches = dLeftPod * INCHES_PER_ENCODER_TICK;
+        double dRightPodInches = dRightPod * INCHES_PER_ENCODER_TICK;
+        double dMecanumPodInches = dMecanumPod * INCHES_PER_ENCODER_TICK;
 
-        robotPosition.setHeading(((dLeftPod + leftPodOldPosition) - (dRightPod + rightPodOldPosition)) * turnScaleFactor);
+        // so its easier to type
+        double L = dLeftPodInches;
+        double R = dRightPodInches;
+        double M = dMecanumPodInches;
+        double P = LR_ENCODER_DIST_FROM_CENTER;
+        double Q = M_ENCODER_DIST_FROM_CENTER;
 
-        double dAngle = (dLeftPodInches - dRightPodInches) * turnScaleFactor;
-        double dRobotX = (dLeftPodInches + dRightPodInches) * .5;
-        double dRobotY = dMecanumPodInches + strafePredictionFactor * dAngle;
+        // find robot relative deltas
+        double dTheta = (L - R) / (2 * P);
+        double dRobotX = M * sinXOverX(dTheta) + Q * Math.sin(dTheta) - L * cosXMinusOneOverX(dTheta) + P * (Math.cos(dTheta) - 1);
+        double dRobotY = L * sinXOverX(dTheta) - P * Math.sin(dTheta) + M * cosXMinusOneOverX(dTheta) + Q * (Math.cos(dTheta) - 1);
 
-        robotPosition.getLocation().x += dRobotX * Math.cos(robotPosition.getHeading() + dAngle * 0.5) - dRobotY * Math.sin(robotPosition.getHeading() + dAngle * 0.5);
-        robotPosition.getLocation().y += dRobotX * Math.sin(robotPosition.getHeading() + dAngle * 0.5) + dRobotY * Math.cos(robotPosition.getHeading() + dAngle * 0.5);
+        // find global deltas by rotating robot relative deltas by the old heading, then add the delta to the sum
+        robotPosition.getLocation().x += dRobotX * Math.cos(robotPosition.getHeading()) + dRobotY * Math.sin(robotPosition.getHeading());
+        robotPosition.getLocation().y += dRobotY * Math.cos(robotPosition.getHeading()) - dRobotX * Math.sin(robotPosition.getHeading());
+
+        // update heading
+        robotPosition.setHeading(((dLeftPod + leftPodOldPosition) - (dRightPod + rightPodOldPosition)) * INCHES_PER_ENCODER_TICK / (2 * P));
     }
 
-    public void circularOdometry(double dLeftPod, double dRightPod, double dMecanumPod) {
-        double dLeftPodInches = dLeftPod * moveScaleFactor;
-        double dRightPodInches = dRightPod * moveScaleFactor;
-        double dMecanumPodInches = dMecanumPod * moveScaleFactor;
-
-        double dTheta = (dLeftPodInches - dRightPodInches) * turnScaleFactor;
-
-        // Default changes in x' and y' directions assume no change in robot's angle
-        double dYPrime = dMecanumPodInches;
-        double dXPrime = dRightPodInches;
-
-        // Calculate midAngle, the angle used to convert from x'y' coordinate system to global (x,y) system
-        double midAngle = robotPosition.getHeading() + dTheta * .5;
-
-        // Update the global angle of the robot
-//        worldAngle += dTheta;
-        robotPosition.setHeading((((dLeftPod + leftPodOldPosition) * moveScaleFactor) - ((dRightPod + rightPodOldPosition) * moveScaleFactor)) * turnScaleFactor);
-
-        if (dTheta != 0.0) { // if robot turned
-            // Calculate the trigonometry portion of the positions
-            double curveFactor = circularOdometrySinXOverX(dTheta / 2);
-
-            dXPrime = (dLeftPodInches + dRightPodInches) * .5 * curveFactor;
-            dYPrime = dMecanumPodInches * curveFactor + .25 * 2 * Math.sin(dTheta / 2);
-        }
-
-        // Update world x and y positions of the robot by converting from robot's x'y' coordinate
-        // system to the global xy coordinate system
-        robotPosition.getLocation().x += dXPrime * Math.cos(midAngle) - dYPrime * Math.sin(midAngle);
-        robotPosition.getLocation().y += dXPrime * Math.sin(midAngle) + dYPrime * Math.cos(midAngle);
-    }
-
-    private double circularOdometrySinXOverX(double x) {
-        if (Math.abs(x) < .00005) { // If the ratio is close enough to the limit, make it the limit
-            return 1;
+    /*
+    taylor series expansion to make stuff COOL
+     */
+    private double sinXOverX(double x){
+        if (Math.abs(x) < 2){
+            double retVal = 0;
+            double top = 1;
+            double bottom = 1;
+            for (int i = 0; i < 9; i++){
+                retVal += top/bottom;
+                top *= -x*x;
+                bottom *= (2 * i + 2)*(2 * i + 3);
+            }
+            return retVal;
         } else {
-            return Math.sin(x) / x;
+            return Math.sin(x)/x;
         }
     }
 
+    /*
+    taylor series expansion to make stuff COOL
+     */
+    private double cosXMinusOneOverX(double x){
+        if (Math.abs(x) < 2){
+            double retVal = 0;
+            double top = -x;
+            double bottom = 2;
+            for (int i = 0; i < 9; i++){
+                retVal += top/bottom;
+                top *= -x*x;
+                bottom *= (2 * i + 3)*(2 * i + 4);
+            }
+            return retVal;
+        } else {
+            return (Math.cos(x)-1)/x;
+        }
+    }
 }
