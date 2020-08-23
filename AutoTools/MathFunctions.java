b5b77ccc52065bcc805583ca4b79f63f70486f2b
e3l <e3lcool@gmail.com>
2020-08-23 10:39:59 -0700

    Added comments to Math Functions, will add test functions later after double-checking line-circle intersection

--- AutoTools/MathFunctions.java
+++ AutoTools/MathFunctions.java
@@ -29,37 +29,99 @@ public class MathFunctions {
        }
     }
 
+    /**
+     * Calculate the distance between a given point and a given line, represented by two points.
+     *
+     * @param point The point.
+     * @param linePoint1 A point on the line.
+     * @param linePoint2 Another, different, point on the line.
+     * @return The distance between the point and the line determined by the two linePoints.
+     */
     public static double linePointDistance(Point point, Point linePoint1, Point linePoint2) {
-        double top = Math.abs((linePoint2.y - linePoint1.y) * point.x - (linePoint2.x - linePoint1.x) * point.y + linePoint2.x * linePoint1.y - linePoint2.y * linePoint1.x);
+        /* From ax + by + c = 0, the equation for the distance between a point (x0, y0) and that
+            line is ((a * x0) + (b * y0) + c)/sqrt(a^2 + b^2). This can be derived geometrically
+            or with some algebra.
+
+            The general standard form (ax + by + c = 0) equation of a line with points (x1,y1) and
+            (x2,y2) is (y2 - y1)x - (x2-x1)y + x2y1 - y2x1 = 0 (derived by plugging our points into
+            slope-intercept and rearranging). So, in our case, a = (y2 - y1), b = -(x2 - x1),
+            and c = x2y1 - y2x1.
+
+            We use these a, b, and c in our distance equation from the first paragraph to get the
+            lines below.
+         */
+
+        double top = Math.abs((linePoint2.y - linePoint1.y) * point.x // a
+                - (linePoint2.x - linePoint1.x) * point.y // b
+                + linePoint2.x * linePoint1.y - linePoint2.y * linePoint1.x); // c
+
         double bottom = Math.hypot(linePoint2.y - linePoint1.y, linePoint2.x - linePoint1.x);
 
         return top / bottom;
     }
 
+    /**
+     * Given a point and a line (represented by 2 points), this method finds the point on the line
+     * closest to the given point.
+     *
+     * @param point The point for which to find the closest point lying on the given line.
+     * @param linePoint1 A point on the line.
+     * @param linePoint2 A different point on the line.
+     * @return The point closest to the given point that lies on the line specified.
+     */
     public static Point closestPointOnLineToPoint(Point point, Point linePoint1, Point linePoint2) {
+        double lineSlope;
 
-        double m;
-
-        if (linePoint2.x == linePoint1.x) {
+        if (linePoint2.x == linePoint1.x) { // If line is vertical
             return new Point(linePoint1.x, point.y);
-        } else if (linePoint2.y == linePoint1.y) {
+        } else if (linePoint2.y == linePoint1.y) { // If line is horizontal
             return new Point(point.x, linePoint1.y);
-        } else {
-            m = (linePoint2.y - linePoint1.y) / (linePoint2.x - linePoint1.x);
+        } else { // Oblique line
+            lineSlope = (linePoint2.y - linePoint1.y) / (linePoint2.x - linePoint1.x);
         }
 
-        return twoLineIntersectionPoint(linePoint1, m, point, -1 / m);
+        /* The closest point to the given point on the given line will be the intersection between
+            the given line and the line perpendicular to that line that passes through the point.
+         */
+        return twoLineIntersectionPoint(linePoint1, lineSlope, point, -1 / lineSlope);
     }
 
-    public static Point twoLineIntersectionPoint(Point firstLinePoint, double firstLineM, Point secondLinePoint, double secondLineM) {
-        double x = (firstLineM * firstLinePoint.x - firstLinePoint.y - secondLineM * secondLinePoint.x + secondLinePoint.y) / (firstLineM - secondLineM);
-        double y = firstLineM * (x - firstLinePoint.x) + firstLinePoint.y;
+    /**
+     * Returns the intersection between two lines.
+     *
+     * @param firstLinePoint A point on the first line.
+     * @param firstLineSlope The slope of the first line.
+     * @param secondLinePoint A point on the second line.
+     * @param secondLineSlope The slope of the second line.
+     * @return The point representing the intersection of the two lines.
+     */
+    public static Point twoLineIntersectionPoint(Point firstLinePoint, double firstLineSlope, Point secondLinePoint, double secondLineSlope) {
+        /* The generalized equation for the intersection of two lines given by y = mx + b is
+            x = (b2 - b1) / (m1 - m2), where b2 is the b term for the second line,
+            and y can be solved using either line's equation.
+
+            For each line, b = y - mx, where (x, y) is a point on the line. We can substitute this
+            into the generalized intersection equation to get the lines below.
+         */
+
+        double x = ((secondLinePoint.y - (secondLineSlope * secondLinePoint.x)) - (firstLinePoint.y - (firstLineSlope * firstLinePoint.x))) // numerator
+                / (firstLineSlope - secondLineSlope); // denominator
+        double y = (firstLineSlope * (x)) + (firstLinePoint.y - (firstLineSlope * firstLinePoint.x)); // Using y = mx + b
 
         return new Point(x, y);
     }
 
+    /**
+     * Returns all the intersections between a line and a circle.
+     *
+     * @param circleCenter The center of the circle.
+     * @param radius The radius of the circle.
+     * @param linePoint1 A point on the line.
+     * @param linePoint2 A different point on the line.
+     * @return An ArrayList of points representing the intersection between the circle and the line.
+     */
     public static ArrayList<Point> lineCircleIntersection(Point circleCenter, double radius, Point linePoint1, Point linePoint2) {
-        //make sure the points don't exactly line up so the slopes work
+        // make sure the points don't exactly line up so the slopes work
         if (Math.abs(linePoint1.y - linePoint2.y) < 0.003) {
             linePoint1.y = linePoint2.y + 0.003;
         }
@@ -67,24 +129,23 @@ public class MathFunctions {
             linePoint1.x = linePoint2.x + 0.003;
         }
 
-        //calculate the slope of the line
-        double m1 = (linePoint2.y - linePoint1.y) / (linePoint2.x - linePoint1.x);
+        // calculate the slope of the line
+        double lineSlope = (linePoint2.y - linePoint1.y) / (linePoint2.x - linePoint1.x);
 
-        double quadraticA = 1.0 + pow(m1, 2);
+        double quadraticA = 1.0 + pow(lineSlope, 2);
 
         //shift one of the line's points so it is relative to the circle
         double x1 = linePoint1.x - circleCenter.x;
         double y1 = linePoint1.y - circleCenter.y;
 
-        double quadraticB = (2.0 * m1 * y1) - (2.0 * pow(m1, 2) * x1);
-        double quadraticC = ((pow(m1, 2) * pow(x1, 2)) - (2.0 * y1 * m1 * x1) + pow(y1, 2) - pow(radius, 2));
-
+        double quadraticB = (2.0 * lineSlope * y1) - (2.0 * pow(lineSlope, 2) * x1);
+        double quadraticC = ((pow(lineSlope, 2) * pow(x1, 2)) - (2.0 * y1 * lineSlope * x1) + pow(y1, 2) - pow(radius, 2));
 
         ArrayList<Point> allPoints = new ArrayList<>();
         try {
             // solve roots
             double xRoot1 = (-quadraticB + Math.sqrt(pow(quadraticB, 2) - (4.0 * quadraticA * quadraticC))) / (2.0 * quadraticA);
-            double yRoot1 = m1 * (xRoot1 - x1) + y1;
+            double yRoot1 = lineSlope * (xRoot1 - x1) + y1;
 
             //add back in translations
             xRoot1 += circleCenter.x;
@@ -100,7 +161,7 @@ public class MathFunctions {
 
             //do the same for the other root
             double xRoot2 = (-quadraticB - Math.sqrt(pow(quadraticB, 2) - (4.0 * quadraticA * quadraticC))) / (2.0 * quadraticA);
-            double yRoot2 = m1 * (xRoot2 - x1) + y1;
+            double yRoot2 = lineSlope * (xRoot2 - x1) + y1;
 
             xRoot2 += circleCenter.x;
             yRoot2 += circleCenter.y;
@@ -108,10 +169,10 @@ public class MathFunctions {
             if (xRoot2 > minX && xRoot2 < maxX) {
                 allPoints.add(new Point(xRoot2, yRoot2));
             }
-
         } catch (Exception e) {
             //if there are no roots
         }
+
         return allPoints;
     }
 }
