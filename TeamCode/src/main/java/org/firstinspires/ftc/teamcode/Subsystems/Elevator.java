d3f0c90e2d6935373afb42289cc7331ae0d6cdf1
David Atkinon <david.atkinson@netzero.com>
2020-09-24 22:46:31 -0500

    moved from Ultimate Goal repo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Elevator.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Elevator {
+
+    //define hardware
+    public DcMotor elevator = null;
+    private DigitalChannel switch1 = null;
+    private DigitalChannel switch2 = null;
+
+    //constants
+    public DcMotor elevator = null;
+
+    private static final double ElevatorSpeedfast=0.5
+    private static final double Elevatorspeedslow=0.25
+
+    public void init(HardwareMap hwMap){
+elevator=hwMap.get(DcMotor.class, )
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Shooter {
+    // Define hardware objects
+    public DcMotor shooterleft=null;
+    public DcMotor shooterright=null;
+    public Servo moveforward=null;
+    public Servo moveback=null;
+
+
+    //Constants
+    private static final double ShooterSpeedfastleft=.6;
+    private static final double ShooterSpeedfastright=.8;
+    private static final double shooterSpeedslowleft=.55;
+    private static final double shooterSpeedslowright=.75;
+    private static final double servoforwardreturn=0;
+    private static final double servoforwardlaunch=1;
+    private static final double servobackreturn=0;
+    private static final double servobacklaunch=0;
+
+
+    public void init(HardwareMap hwMap)  {
+        shooterleft=hwMap.get(DcMotor.class,"LeftShooter");
+        shooterright=hwMap.get(DcMotor.class,"RightShooter");
+        moveforward=hwMap.get(Servo.class,"ForwardMove");
+        moveback=hwMap.get(Servo.class,"BackMove");
+
+        shooterleft.setDirection(DcMotor.Direction.FORWARD);
+    }
+}
