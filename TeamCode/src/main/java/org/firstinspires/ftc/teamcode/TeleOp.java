d91cb4ac927da2788ddd2030449f76195b789f03
Jack Fetkovich <johnfetkovich1014@gmail.com>
2020-09-24 20:53:04 -0400

    Initial Setup and Create TeleOp File

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+class TeleOp extends OpMode {
+
+    public void init(){
+
+    }
+
+    public void loop(){
+
+    }
+
+
+}
