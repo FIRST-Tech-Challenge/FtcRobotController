5c75dcb7163159b603d2c0545eec6fb74cf41e02
lverno321 <lverno321@gmail.com>
2020-09-27 17:47:38 -0400

    build status

--- upload.py
+++ upload.py
@@ -1,7 +1,10 @@
+from os import stat
 import requests
+import base64
 import os
 import websockets
 import asyncio
+import json
 
 CONTROL_HUB_URL = "http://localhost:8080"
 CONTROL_HUB_WSURL = "ws://localhost:8081"
@@ -12,16 +15,27 @@ SRC_DIRECTORY = "C:\\Users\\lvern\\Documents\\GitHub\\UltimateGoal\\TeamCode\\sr
 def uploadJavaFile(path: str):
     if path.endswith(".java"):
         files = {'file': (os.path.basename(path), open(path, 'rb'), 'application/octet-stream')}
-        r = requests.post(CONTROL_HUB_UPLOAD_URL, files=files)
-        print (r.text)
+        return requests.post(CONTROL_HUB_UPLOAD_URL, files=files)
+        
 
 async def build():
     async with websockets.connect(CONTROL_HUB_WSURL) as ws:
+        await ws.send('{"namespace":"system","type":"subscribeToNamespace","encodedPayload":"T05CT1RKQVZB"}')
         await ws.send('{"namespace":"ONBOTJAVA","type":"build:launch","encodedPayload":""}')
+        status = ""
+        while status != "SUCCESSFUL" and status != "FAILED":
+            msg = await ws.recv()
+            j = json.loads(msg)
+            payload = base64.b64decode(j["encodedPayload"]).decode()
+            j_payload = json.loads(payload)
+            status = j_payload['status']
+            print (f"Build Status: {status}")
 
 for path, subdirs, files in os.walk(SRC_DIRECTORY):
     for name in files:
         file_path = os.path.join(path, name)
-        uploadJavaFile(file_path)
+        r = uploadJavaFile(file_path)
+        if (r is not None and r.status_code == 200):
+            print (f"Uploaded: {r.text}")
 
 asyncio.get_event_loop().run_until_complete(build())
