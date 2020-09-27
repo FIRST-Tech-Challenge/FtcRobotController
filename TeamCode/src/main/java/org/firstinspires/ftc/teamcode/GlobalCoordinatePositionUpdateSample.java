62869acb23116dd79077a3645b616bc25557de70
Adrian Chan <adrian27513@gmail.com>
2020-09-27 16:55:26 -0400

    Testing Webhook

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalCoordinatePositionUpdateSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GlobalCoordinatePositionUpdateSample.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.OdometryGlobalCoordinatePosition;
 @TeleOp(name = "Global Coordinate Position Test", group = "Calibration")
 public class GlobalCoordinatePositionUpdateSample extends LinearOpMode {
 
-    //test Github webhook
+    //test Discord webhook
     //Odometry encoder wheels
     DcMotor verticalRight, verticalLeft, horizontal;
 
