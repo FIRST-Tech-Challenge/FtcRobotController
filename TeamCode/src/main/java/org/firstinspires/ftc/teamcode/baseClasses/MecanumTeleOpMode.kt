c5823305f9f7c9d49f08414f6659cec5a55565c1
Jakub Charvat <jakcharvat@gmail.com>
2020-09-30 23:24:35 +0200

    Update MecanumTeleOpMode.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseClasses/MecanumTeleOpMode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/baseClasses/MecanumTeleOpMode.kt
@@ -6,8 +6,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation
 import kotlin.math.cos
 import kotlin.math.sin
 
-/// Credits for the mecanum drive portion of this class go to FTC11848
-/// https://www.reddit.com/r/FTC/comments/9ou0ib/mecanum_drive_code/e7wrp9f?utm_source=share&utm_medium=web2x&context=3
 
 abstract class MecanumTeleOpMode: BaseLinearOpMode() {
     /**
