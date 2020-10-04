0479b96b0ed22799b4c7aa18deea2c326375cbd0
kashvi <pundirkashvi@gmail.com>
2020-10-04 15:13:51 -0700

    added sample MasterOpMode and TeleOp class.

---
+++ Team417_2020/src/main/java/org/firstinspires/ftc/team417_2020/KashviMasterOpMode.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.team417_2020;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+abstract public class KashviMasterOpMode extends LinearOpMode {
+
+    public DcMotor motorFL = null;
+    public DcMotor motorFR = null;
+    public DcMotor motorBL = null;
+    public DcMotor motorBR = null;
+
+    protected void initializeHardware () {
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+        motorFR = hardwareMap.dcMotor.get("motorFR");
+        motorBL = hardwareMap.dcMotor.get("motorBL");
+        motorBR = hardwareMap.dcMotor.get("motorBR");
+
+        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorFL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motorFL.setDirection(DcMotor.Direction.REVERSE);
+        motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorFR.setDirection(DcMotor.Direction.FORWARD);
+        motorBR.setDirection(DcMotor.Direction.FORWARD);
+
+        motorFL.setPower(0);
+        motorFR.setPower(0);
+        motorBL.setPower(0);
+        motorBR.setPower(0);
+
+
+
+    }
+
+
+    public void moveMecanum(double angle, double power, double turningPower) {
+        double x = power * Math.cos(angle);
+        double y = power * Math.sin(angle);
+
+
+        double motorFLPower = x + y - turningPower;
+        double motorFRPower = x - y - turningPower;
+        double motorBLPower = -x + y - turningPower;
+        double motorBRPower = -x -y - turningPower;
+
+        motorFL.setPower(motorFLPower);
+        motorFR.setPower(motorFRPower);
+        motorBL.setPower(motorBLPower);
+        motorBR.setPower(motorBRPower);
+
+    }
+}
---
+++ Team417_2020/src/main/java/org/firstinspires/ftc/team417_2020/KashviTestTeleOp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.team417_2020;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "KashviTestTeleOp")
+
+
+public class KashviTestTeleOp extends KashviMasterOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initializeHardware();
+
+        telemetry.addData ("init", "done");
+
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()) {
+
+            double turningPower = gamepad1.left_stick_x;
+            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x);
+            double power = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+
+            moveMecanum(angle, power, turningPower);
+
+
+        }
+    }
+}
