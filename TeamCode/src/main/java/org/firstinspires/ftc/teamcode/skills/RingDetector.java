ceadc4d4faea69fcee94dc173590db352f16aea4
PriyaSoneji <priyas.fcs@gmail.com>
2020-10-07 17:33:17 -0400

    New Models

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skills/RingDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skills/RingDetector.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.skills;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -17,7 +16,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -65,12 +63,19 @@ public class RingDetector {
             if (tfod != null) {
                 // getUpdatedRecognitions() will return null if no new information is available since
                 // the last time that call was made.
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                List<Recognition> updatedRecognitions = tfod.getRecognitions();
                 if (updatedRecognitions != null) {
                     telemetry.addData("# Object Detected", updatedRecognitions.size());
-
                     // step through the list of recognitions and display boundary info.
+                    int i = 0;
                     for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+
+                        // calculate zone
                         if ((recognition.getLabel().contentEquals(LABEL_B))) {
                             targetZone = "B";
                         } else if (recognition.getLabel().contentEquals(LABEL_C)) {
@@ -78,11 +83,8 @@ public class RingDetector {
                         } else {
                             targetZone = "A";
                         }
-
                         found = true;
                         telemetry.addData("Found", found);
-                        break;
-
                     }
                     telemetry.update();
                 }
@@ -116,7 +118,7 @@ public class RingDetector {
         // with FIRST model
 //        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_C, LABEL_B);
         // with custom model
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_A, LABEL_B, LABEL_C);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_A, LABEL_B);
     }
 
     protected void activateTfod(){
