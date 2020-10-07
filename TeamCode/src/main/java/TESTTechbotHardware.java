e7f80c8f7f5ed725d494915547266d3d53f81700
terrifictechbots <terrifictechbots@gmail.com>
2020-10-07 15:06:43 -0700

    copied color sensor code & techbothardware

---
+++ TeamCode/src/main/java/TESTTechbotHardware.java
@@ -0,0 +1,150 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ */
+public class TESTTechbotHardware
+{
+    /* Public OpMode members. */
+    public DcMotor leftDrive   = null;
+    public DcMotor rightDrive  = null;
+    public DcMotor leftBackDrive = null;
+    public DcMotor rightBackDrive = null;
+
+
+    //public DcMotor arm = null;
+    public DcMotor lift = null;
+    public Servo servoHand = null;
+    public Servo servoWrist = null;
+
+    public static final double MID_SERVO       =  2.00 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public TESTTechbotHardware(){
+
+    }
+
+    public void slide (double slideDrive) {
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        leftDrive.setPower(slideDrive);
+        rightDrive.setPower(slideDrive);
+        leftBackDrive.setPower(slideDrive);
+        rightBackDrive.setPower(slideDrive);
+
+    }
+
+    public void drive (double driveDrive) {
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        leftDrive.setPower(driveDrive);
+        rightDrive.setPower(driveDrive);
+        leftBackDrive.setPower(driveDrive);
+        rightBackDrive.setPower(driveDrive);
+
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        //Issues getting the techbot to run. Keeps looking for left front and back and right front and back motors
+        // Define and Initialize Motors
+        leftDrive  = hwMap.get(DcMotor.class, "Left front motor");
+        rightDrive = hwMap.get(DcMotor.class, "Right front motor");
+        leftBackDrive = hwMap.get(DcMotor.class, "Left back motor");
+        rightBackDrive = hwMap.get(DcMotor.class, "Right back motor");
+        //arm    = hwMap.get(DcMotor.class, "arm");
+        servoHand = hwMap.get(Servo.class, "hand");
+        //lift = hwMap.get(DcMotor.class, "lift");
+
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        //arm.setDirection(DcMotor.Direction.FORWARD);
+        //lift.setDirection(DcMotor.Direction.FORWARD);
+
+        // Set all motors to zero power
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        //arm.setPower(0);
+        //lift.setPower(0);
+
+        /*
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+*/
+        // Define and initialize ALL installed servos.
+        servoHand  = hwMap.get(Servo.class, "hand");
+        /*servoWrist = hwMap.get(Servo.class, "wrist");
+        servoHand.setPosition(MID_SERVO);
+        servoWrist.setPosition(MID_SERVO);*/
+    }
+ }
+
