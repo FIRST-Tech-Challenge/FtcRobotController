64b4a99868fffd152fa015068e82fc7ec0c812c5
Havisha Chiliveri <havishach@icloud.com>
2020-10-06 20:15:36 -0500

    initial commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -1,5 +1,20 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Intake {
+    public DcMotor intake_motor = null;
+
+
+    //contstants
+    public static final double intake_speed = .5
+
+    public void init(HardwareMap hwMap)  {}
     //
 }
+
+
+
+
+
