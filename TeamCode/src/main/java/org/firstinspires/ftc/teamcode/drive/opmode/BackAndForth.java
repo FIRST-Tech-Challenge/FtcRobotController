52e2ceac74afcc08fbf5b9542bb30610236dedb1
Noah <noahbresler2002@gmail.com>
2020-10-08 20:50:14 -0500

    Add BackAndForth follower tuning op mode (#96)
    * Add a BackAndForth tuning opmode
    
    * Add instructions to BackAndForth.java
    
    * Add further instructions in FollowerPIDTuner.java
    
    * Fix changes requested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForth.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BackAndForth.java
@@ -10,42 +10,43 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 /*
  * Op mode for preliminary tuning of the follower PID coefficients (located in the drive base
- * classes). The robot drives in a DISTANCE-by-DISTANCE square indefinitely. Utilization of the
+ * classes). The robot drives back and forth in a straight line indefinitely. Utilization of the
  * dashboard is recommended for this tuning routine. To access the dashboard, connect your computer
  * to the RC's WiFi network. In your browser, navigate to https://192.168.49.1:8080/dash if you're
  * using the RC phone or https://192.168.43.1:8080/dash if you are using the Control Hub. Once
- * you've successfully connected, start the program, and your robot will begin driving in a square.
- * You should observe the target position (green) and your pose estimate (blue) and adjust your
- * follower PID coefficients such that you follow the target position as accurately as possible.
+ * you've successfully connected, start the program, and your robot will begin moving forward and
+ * backward. You should observe the target position (green) and your pose estimate (blue) and adjust
+ * your follower PID coefficients such that you follow the target position as accurately as possible.
  * If you are using SampleMecanumDrive, you should be tuning TRANSLATIONAL_PID and HEADING_PID.
  * If you are using SampleTankDrive, you should be tuning AXIAL_PID, CROSS_TRACK_PID, and HEADING_PID.
  * These coefficients can be tuned live in dashboard.
+ *
+ * This opmode is designed as a convenient, coarse tuning for the follower PID coefficients. It
+ * is recommended that you use the FollowerPIDTuner opmode for further fine tuning.
  */
 @Config
 @Autonomous(group = "drive")
-public class FollowerPIDTuner extends LinearOpMode {
-    public static double DISTANCE = 48; // in
+public class BackAndForth extends LinearOpMode {
+
+    public static double DISTANCE = 50;
 
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d(-DISTANCE / 2, -DISTANCE / 2, 0);
+        Trajectory trajectoryForward = drive.trajectoryBuilder(new Pose2d())
+                .forward(DISTANCE)
+                .build();
 
-        drive.setPoseEstimate(startPose);
+        Trajectory trajectoryBackward = drive.trajectoryBuilder(trajectoryForward.end())
+                .back(DISTANCE)
+                .build();
 
         waitForStart();
 
-        if (isStopRequested()) return;
-
-        while (!isStopRequested()) {
-            Trajectory traj = drive.trajectoryBuilder(startPose)
-                    .forward(DISTANCE)
-                    .build();
-            drive.followTrajectory(traj);
-            drive.turn(Math.toRadians(90));
-
-            startPose = traj.end().plus(new Pose2d(0, 0, Math.toRadians(90)));
+        while (opModeIsActive() && !isStopRequested()) {
+            drive.followTrajectory(trajectoryForward);
+            drive.followTrajectory(trajectoryBackward);
         }
     }
 }
