eb60ed306630647cbe58beea53e7fd23ed286309
shrmad <65050031+shrmad@users.noreply.github.com>
2020-10-11 16:20:27 -0700

    My own teleConfigShrinidhi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigShrinidhi.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+public class teleConfigShrinidhi implements teleOpInterface {
+    HardwareMapV2 robot;
+    Drivetrain drivetrain;
+    public void a() {
+        
+    }
+
+    public void b() {
+
+    }
+
+    public void x() {
+
+    }
+
+    public void y() {
+
+    }
+
+    public void dd() {
+
+    }
+
+    public void dp() {
+
+    }
+
+    public void dl() {
+
+    }
+
+    public void dr() {
+
+    }
+
+    public void rb() {
+
+    }
+
+    public void rt() {
+
+    }
+
+    public void lb() {
+
+    }
+
+    public void lt() {
+
+    }
+
+    public void rjoy(float x, float y) {
+
+    }
+
+    public void ljoy(float x, float y) {
+
+    }
+
+    public void rjoyb() {
+
+    }
+
+    public void ljoyb() {
+
+    }
+
+    public void custom1() {
+
+    }
+}
