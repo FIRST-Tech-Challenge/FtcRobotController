1ad6d067eacc27db48ab32cb951ed375490872a3
Abhi Divvela <saidivvela2005@gmail.com>
2020-10-12 16:54:01 -0400

    test commit to create a pull request for learning purposes

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOpMode_Iterative.java
@@ -47,7 +47,7 @@ import com.qualcomm.robotcore.util.Range;
  * It includes all the skeletal structure that all iterative OpModes contain.
  *
  * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
 @TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
