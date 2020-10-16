51afb19f0491158fc299701f8f010e91a39bcea3
rsmrohit <32987551+rsmrohit@users.noreply.github.com>
2020-10-15 22:47:26 -0700

    finished all known methods for my config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigRohit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigRohit.java
@@ -9,9 +9,12 @@ public class teleConfigRohit implements teleOpInterface {
         INCREMENT, POSITION
     }
     static mode tiltmode = mode.POSITION;
+    static double inttakePower = 1.0;
+    static double outtakePower = 1.0;
+    static double conveyorPower = 1.0;
 
     public void a() {
-
+        conveyorPower-=0.1;
     }
 
     public void b() {
@@ -19,7 +22,7 @@ public class teleConfigRohit implements teleOpInterface {
     }
 
     public void x() {
-
+        conveyorPower+=0.1;
     }
 
     public void y() {
@@ -43,19 +46,19 @@ public class teleConfigRohit implements teleOpInterface {
     }
 
     public void rb() {
-
+        drivetrain.outtakeAll(conveyorPower, outtakePower);
     }
 
     public void rt() {
-
+        drivetrain.outtakeAll(-conveyorPower, -outtakePower);
     }
 
     public void lb() {
-
+        robot.intake.setPower(inttakePower);
     }
 
     public void lt() {
-
+        robot.intake.setPower(-inttakePower);
     }
 
     public void rjoy(float x, float y) {
@@ -74,7 +77,11 @@ public class teleConfigRohit implements teleOpInterface {
     }
 
     public void ljoy(float x, float y) {
-
+        if (Math.abs(y)>0.75){ wait = true; up = y > 0; }
+        if (y==0 && wait) {
+            inttakePower = (up && inttakePower!=1.0) ? inttakePower*2 : (!up && inttakePower!=0.125) ? inttakePower/2 : 1.0;
+        }
+        wait = false;
     }
 
     public void rjoyb() {
@@ -82,7 +89,7 @@ public class teleConfigRohit implements teleOpInterface {
     }
 
     public void ljoyb() {
-
+        inttakePower = 1.0;
     }
 
     public void custom1() {
