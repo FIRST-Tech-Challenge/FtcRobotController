ab439403df89eaa9b6723b7ce1306cc8c79da1f0
shuhul <shuhulmujoo@gmail.com>
2020-10-15 19:47:34 -0700

    Started New Code

--- TeamCode/src/main/java/global/TerraBot.java
+++ TeamCode/src/main/java/global/TerraBot.java
@@ -3,6 +3,9 @@ package global;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.concurrent.ForkJoinPool;
 
 
 public class TerraBot {
@@ -14,6 +17,19 @@ public class TerraBot {
     public DcMotor r2;
 
     public DcMotor in;
+    public DcMotor outr;
+    public DcMotor outl;
+    public DcMotor arm;
+
+    public Servo slr;
+    public Servo sll;
+    public Servo ssr;
+    public Servo ssl;
+    public Servo st;
+    public Servo sgr;
+    public Servo sgl;
+
+    public boolean intaking = false;
 
 
     public void init(HardwareMap hwMap) {
@@ -22,32 +38,74 @@ public class TerraBot {
         l2 = hwMap.get(DcMotor.class, "l2");
         r1 = hwMap.get(DcMotor.class, "r1");
         r2 = hwMap.get(DcMotor.class, "r2");
-
         in = hwMap.get(DcMotor.class, "in");
+        outr = hwMap.get(DcMotor.class, "outr");
+        outl = hwMap.get(DcMotor.class, "outl");
+        arm = hwMap.get(DcMotor.class, "arm");
+
+        slr = hwMap.get(Servo.class, "slr");
+        sll = hwMap.get(Servo.class, "sll");
+        ssr = hwMap.get(Servo.class, "ssr");
+        ssl = hwMap.get(Servo.class, "ssl");
+        st = hwMap.get(Servo.class, "st");
+        sgr = hwMap.get(Servo.class, "sgr");
+        sgl = hwMap.get(Servo.class, "sgl");
+
 
         l1.setPower(0);
         l2.setPower(0);
         r1.setPower(0);
         r2.setPower(0);
         in.setPower(0);
+        outr.setPower(0);
+        outl.setPower(0);
+        arm.setPower(0);
+
+
 
         l1.setDirection(DcMotorSimple.Direction.REVERSE);
         l2.setDirection(DcMotorSimple.Direction.FORWARD);
         r1.setDirection(DcMotorSimple.Direction.FORWARD);
         r2.setDirection(DcMotorSimple.Direction.REVERSE);
-        in.setDirection(DcMotorSimple.Direction.FORWARD);
+        in.setDirection(DcMotorSimple.Direction.REVERSE);
+        outl.setDirection(DcMotorSimple.Direction.FORWARD);
+        outr.setDirection(DcMotorSimple.Direction.REVERSE);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        slr.setDirection(Servo.Direction.FORWARD);
+        sll.setDirection(Servo.Direction.FORWARD);
+        ssr.setDirection(Servo.Direction.FORWARD);
+        ssl.setDirection(Servo.Direction.FORWARD);
+        st.setDirection(Servo.Direction.FORWARD);
+        sgr.setDirection(Servo.Direction.FORWARD);
+        sgl.setDirection(Servo.Direction.REVERSE);
+
+
+        slr.setPosition(0);
+        sll.setPosition(0);
+        ssr.setPosition(0);
+        ssl.setPosition(0);
+        st.setPosition(0.8);
+        sgr.setPosition(0.3);
+        sgl.setPosition(0.7);
 
         l1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         l2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         r1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         r2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        in.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        in.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        outr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        outl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         l1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         l2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         r1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         r2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         in.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        outr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        outl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        arm.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
     }
 
@@ -61,6 +119,35 @@ public class TerraBot {
     public void intake(double p){
         in.setPower(p);
     }
+
+    public void outtake(double p){
+        outr.setPower(p);
+        outl.setPower(p);
+    }
+
+    public void moveArm(double p){
+        arm.setPower(p);
+    }
+
+    public void lift(double pos){
+        slr.setPosition(pos);
+        sll.setPosition(pos);
+    }
+
+    public void shoot(double pos){
+        ssr.setPosition(pos);
+        ssl.setPosition(pos);
+    }
+
+    public void turnArm(double pos){
+        st.setPosition(pos);
+    }
+
+    public void grab(double pos){
+        sgr.setPosition(pos);
+        sgl.setPosition(pos);
+    }
+
 
 
 }
--- TeamCode/src/main/java/teleop/TerraOp.java
+++ TeamCode/src/main/java/teleop/TerraOp.java
@@ -23,19 +23,51 @@ public class TerraOp extends OpMode {
 
     @Override
     public void loop() {
-        if(gamepad1.right_bumper) {
-            bot.intake(-1);
+        if(gamepad1.right_bumper){
+            bot.intaking = true;
+            bot.intake(1);
         }else if(gamepad1.left_bumper){
+            bot.intaking = false;
+            bot.intake(-1);
+        }else if(bot.intaking){
             bot.intake(1);
         }else{
             bot.intake(0);
         }
+
+        if(gamepad2.right_bumper){
+            bot.lift(0);
+        }else if(gamepad2.left_bumper){
+            bot.lift(1);
+        }
+
+        if(gamepad2.right_trigger > 0){
+            bot.lift(1);
+        }else if(gamepad2.left_trigger > 0){
+            bot.lift(0);
+        }
+
+        if(gamepad2.dpad_up){
+            bot.turnArm(0.4);
+        }else if(gamepad2.dpad_down){
+            bot.turnArm(0.8);
+        }
+
+        if (gamepad2.dpad_right){
+            bot.grab(0.3);
+        }else if(gamepad2.dpad_left){
+            bot.grab(0.7);
+        }
+
+        bot.outtake(gamepad2.right_stick_y);
+        bot.moveArm(gamepad2.left_stick_y);
 
         double forward = -gamepad1.right_stick_y;
         double strafe = gamepad1.right_stick_x;
         double turn = -gamepad1.left_stick_x;
 
         bot.move(forward, strafe, turn);
+
 
     }
 }
