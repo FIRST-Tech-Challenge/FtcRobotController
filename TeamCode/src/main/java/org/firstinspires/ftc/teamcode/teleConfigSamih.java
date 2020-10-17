15cd1526afca50f7cf20d8d4b4bf3c318ee4fae1
samihsq <samih.qureshi@gmail.com>
2020-10-16 20:52:35 -0700

    added my gamepad config v1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigSamih.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigSamih.java
@@ -3,20 +3,33 @@ package org.firstinspires.ftc.teamcode;
 public class teleConfigSamih implements teleOpInterface {
     HardwareMapV2 robot;
     Drivetrain drivetrain;
+    double angle = 0;
+    double outlimiter = 1f;
+
+    public void loop() {
+        outlimiter = 1;
+    }
+
+    public void updateTelemetryDM() {
+
+    }
 
     public void a() {
-
+        robot.intake.setPower((robot.intake.getPower() >= 0.1) ? 0 : 1);
     }
 
     public void b() {
-
+        drivetrain.outtakeAll((robot.conveyor.getPower() >= 0.1) ? 0 : 1 * outlimiter, (robot.outtake.getPower() >= 0.1) ? 0 : 1 * outlimiter);
     }
 
     public void x() {
-
+        robot.intake.setPower(0);
+        drivetrain.outtakeAll(0,0);
     }
 
     public void y() {
+        angle = (angle >= 1) ? 0 : angle + 0.5;
+        drivetrain.tilt(angle);
 
     }
 
@@ -37,7 +50,7 @@ public class teleConfigSamih implements teleOpInterface {
     }
 
     public void rb() {
-
+        outlimiter = 0.5;
     }
 
     public void rt() {
@@ -45,7 +58,7 @@ public class teleConfigSamih implements teleOpInterface {
     }
 
     public void lb() {
-
+        robot.intake.setPower(-0.7);
     }
 
     public void lt() {
