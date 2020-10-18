55cf9a7bfc01ee7cbd9c312f4e142e8d098d5784
David Atkinon <david.atkinson@netzero.com>
2020-10-17 22:13:49 -0500

    cleaned up comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Drivetrain.java
@@ -10,14 +10,13 @@ public class Drivetrain  {
         // Define hardware objects
         public DcMotor leftFront = null;
         public DcMotor rightFront = null;
-        public BNO055IMU imu;
+        public BNO055IMU imu = null;
 
         // List constants
-
-        public static final double COUNTS_PER_MOTOR_REV = 28;         // HD Hex encoder counts at the motor (28)
+        public static final double COUNTS_PER_DRIVE_MOTOR_REV = 28;         // HD Hex encoder counts at the motor (28)
         public static final double DRIVE_GEAR_REDUCTION = 18.9;       // 4:1 and 5:1 Planetary "20:1" nominal reduction
         public static final double WHEEL_DIAMETER_INCHES = 3.54;      // 90mm wheels. For figuring circumference its a 90 millimeter wheel
-        public static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+        public static final double COUNTS_PER_INCH = (COUNTS_PER_DRIVE_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                 (WHEEL_DIAMETER_INCHES * 3.1415);
         public static final double DRIVE_SPEED = 1;
         private static final double TURN_SPEED = 0.5;
@@ -25,18 +24,19 @@ public class Drivetrain  {
         private ElapsedTime runtime = new ElapsedTime();
 
         // Contructor for Drivetrain
-        // Passing boolean to automatically config encoders for correct part of match.
+        // Passing boolean to automatically config encoders for auto or teleop.
         public Drivetrain(boolean inTeleOp) {
 
-
         }
+
         // initialize drivetrain components, assign names for driver station config, set directions
         // and encoders if needed.
         public void init(HardwareMap hwMap) {
 
             // initialize the imu first.
-            // Note this in NOT IMU calibration...just initialization.
+            // Note this in NOT IMU calibration.
             imu = hwMap.get(BNO055IMU.class, "imu");
+
             // initialize al the drive motors
             leftFront = hwMap.get(DcMotor.class, "Left_front");
             rightFront = hwMap.get(DcMotor.class, "Right_front");
@@ -53,6 +53,8 @@ public class Drivetrain  {
             if (!inTeleOp) {
                 leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
                 rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                // FLOAT allows motors to coast when count is reached. Change to BRAKE
+                // if necessary
                 leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
                 rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
 
