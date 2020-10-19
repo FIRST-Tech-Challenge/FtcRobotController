630290eed78a5e2aeae2d093b690cbf3428ce318
rsmrohit <32987551+rsmrohit@users.noreply.github.com>
2020-10-18 19:07:03 -0700

    trying second map

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleConfigRohit2.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+public class teleConfigRohit2 implements teleOpInterface {
+    public void a() {
+
+    }
+
+    public void b() {
+
+    }
+
+    public void x() {
+
+    }
+
+    public void y() {
+
+    }
+
+    public void dd() {
+
+    }
+
+    public void dp() {
+
+    }
+
+    public void dl() {
+
+    }
+
+    public void dr() {
+
+    }
+
+    public void rb() {
+
+    }
+
+    public void rt(float pressure) {
+
+    }
+
+    public void lb() {
+
+    }
+
+    public void lt(float pressure) {
+
+    }
+
+    public void rjoy(float x, float y) {
+
+    }
+
+    public void ljoy(float x, float y) {
+
+    }
+
+    public void rjoyb() {
+
+    }
+
+    public void ljoyb() {
+
+    }
+
+    public void custom1() {
+
+    }
+
+    public void updateTelemetryDM() {
+
+    }
+
+    public String getName() {
+        return "teleConfigRohit2";
+    }
+}
