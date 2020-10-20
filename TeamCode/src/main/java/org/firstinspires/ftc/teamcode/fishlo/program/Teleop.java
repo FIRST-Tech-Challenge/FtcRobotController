3cfad06fffa5b0a39f7bc3d9a9eea8daf2cacba6
Keshav Varadarajan <kvthevk@gmail.com>
2020-10-20 17:24:31 -0400

    Change the name of the class so that the annotation works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/program/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/program/Teleop.java
@@ -1,13 +1,15 @@
 package org.firstinspires.ftc.teamcode.fishlo.program;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 import org.firstinspires.ftc.teamcode.fishlo.robot.Fishlo;
 import org.firstinspires.ftc.teamcode.opMode.DriverControlledProgram;
-import org.firstinspires.ftc.teamcode.opMode.annotation.Program;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
 
-@Program(name = "Teleop", enabled = true)
-public class TeleOp extends DriverControlledProgram {
+
+@TeleOp()
+public class Teleop extends DriverControlledProgram {
     private Fishlo fishlo;
 
     @Override
