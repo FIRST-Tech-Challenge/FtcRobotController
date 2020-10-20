55ba31ea2207eb08499c0171b9cfeaa5d44ab35e
David Atkinon <david.atkinson@netzero.com>
2020-10-19 23:03:34 -0500

    added center cal method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Stacker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Stacker.java
@@ -10,16 +10,17 @@ public class Stacker {
     public Servo rightFliper = null;
 
     // Constants
-    private static final double leftUp = .5;
-    private static final double leftBack = .6;
-    private static final double rightUp = .5;
-    private static final double rightBack = .6;
+    private static final double leftUp = 0.75; // .75 a little shy but ok due to hitting bolt
+    private static final double leftBack = .4; //good at 0.4;
+    private static final double rightUp = (1-leftUp);
+    private static final double rightBack = (1-leftBack);
+    private static final double flippercenter = 0.5;
 
     public void init(HardwareMap hwMap) {
         leftFliper = hwMap.get(Servo.class, "Left_Flipper");
         rightFliper = hwMap.get(Servo.class, "Right_Flipper");
     }
-
+        //x button
     public void loadShooter() {
         leftFliper.setPosition(leftUp);
         rightFliper.setPosition(rightUp);
@@ -27,5 +28,9 @@ public class Stacker {
     public void resetShooter() {
         leftFliper.setPosition(leftBack);
         rightFliper.setPosition(rightBack);
+    }
+    public void flipperCalibrateinCenter() {
+        leftFliper.setPosition(flippercenter);
+        rightFliper.setPosition(flippercenter);
     }
 }
