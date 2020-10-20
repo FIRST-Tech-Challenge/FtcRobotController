e49fabde2c882d8fb01d10f2df04a95d61348238
Keshav Varadarajan <kvthevk@gmail.com>
2020-10-20 17:18:27 -0400

    Add the program annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/program/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/program/TeleOp.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode.fishlo.program;
 
 import org.firstinspires.ftc.teamcode.fishlo.robot.Fishlo;
 import org.firstinspires.ftc.teamcode.opMode.DriverControlledProgram;
+import org.firstinspires.ftc.teamcode.opMode.annotation.Program;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 
 
+@Program(name = "Teleop", enabled = true)
 public class TeleOp extends DriverControlledProgram {
     private Fishlo fishlo;
 
