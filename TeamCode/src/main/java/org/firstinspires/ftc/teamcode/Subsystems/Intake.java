3bd1be68f478797c11ec33c0172d3c115a7048d7
Krishna Unnam <krichaitu.unnam6@gmail.com>
2020-10-21 20:10:48 -0500

    Added code to Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Intake.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -8,11 +8,28 @@ public class Intake {
 
 
     //contstants
-    public static final double intake_speed = .5
+    public static final double intake_speed = .5;
+    public static final double Intakeoff = 0;
+    public static final double Intakeon = 0.9;
+    public void init(HardwareMap hwMap){
+        intake_motor =hwMap.get(DcMotor.class,"Elevator");
+        intake_motor.setDirection(DcMotor.Direction.REVERSE);
+        intake_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
 
-    public void init(HardwareMap hwMap)  {}
-    //
+    public void Intakeoff() {
+        intake_motor.setPower(Intakeoff);
+
+    }
+    public void Intakeon() {
+        intake_motor.setPower(Intakeon);
+
+    }
+    public void Elevatorbackup() {
+        intake_motor.setPower(-Intakeon);
+    }
 }
+
 
 
 
