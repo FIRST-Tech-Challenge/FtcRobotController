aa6a54bf855132c4f18cb40083f909beaf6c7dda
David Atkinon <david.atkinson@netzero.com>
2020-10-27 00:05:51 -0500

    new auto test classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/BasicAutonomous_Simple.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/BasicAutonomous_Simple.java
@@ -16,13 +16,13 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.teamcode.Enums.WobbleTargetZone;
-//import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
-import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain_v3;
 
 import java.util.List;
 
-@Autonomous(name="Basic Autonomous for Test", group="Test")
+//import org.firstinspires.ftc.teamcode.Subsystems.Drivetrain;
+
+@Autonomous(name="Basic AutonomousSimple", group="Test")
 
 //////////////////////////////////////////////////////////
     // EXTEND this class to create multiple drive paths.
@@ -31,7 +31,7 @@ import java.util.List;
 
 
 
-public class BasicAutonomous extends LinearOpMode {
+public class BasicAutonomous_Simple extends LinearOpMode {
     /* Declare OpMode members. */
     public Drivetrain_v3 drivetrain  = new Drivetrain_v3(false);   // Use subsystem Drivetrain
     public Orientation         lastAngles  = new Orientation();
@@ -57,47 +57,16 @@ public class BasicAutonomous extends LinearOpMode {
     public double                  lasterror;
     public  double                  totalError;
 
-    //// Vuforia Content
-    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
-    private static final String LABEL_FIRST_ELEMENT = "Quad";
-    private static final String LABEL_SECOND_ELEMENT = "Single";
-    private String StackSize = "None";
-    WobbleTargetZone Square = WobbleTargetZone.RED_A; // Default
-    private static double tfSenseTime = 3;
 
-    private static final String VUFORIA_KEY =
-            "AQXVmfz/////AAABmXaLleqhDEfavwYMzTtToIEdemv1X+0FZP6tlJRbxB40Cu6uDRNRyMR8yfBOmNoCPxVsl1mBgl7GKQppEQbdNI4tZLCARFsacECZkqph4VD5nho2qFN/DmvLA0e1xwz1oHBOYOyYzc14tKxatkLD0yFP7/3/s/XobsQ+3gknx1UIZO7YXHxGwSDgoU96VAhGGx+00A2wMn2UY6SGPl+oYgsE0avmlG4A4gOsc+lck55eAKZ2PwH7DyxYAtbRf5i4Hb12s7ypFoBxfyS400tDSNOUBg393Njakzcr4YqL6PYe760ZKmu78+8X4xTAYSrqFJQHaCiHt8HcTVLNl2fPQxh0wBmLvQJ/mvVfG495ER1A";
+    
 
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
-    private VuforiaLocalizer vuforia;
 
-    /**
-     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
-     * Detection engine.
-     */
-    private TFObjectDetector tfod;
 
     @Override
     public void runOpMode() {
 
-        initVuforia();
-        initTfod();
-        if (tfod != null) {
-            tfod.activate();
 
-            // The TensorFlow software will scale the input images from the camera to a lower resolution.
-            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
-            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-            // should be set to the value of the images used to create the TensorFlow Object Detection model
-            // (typically 1.78 or 16/9).
 
-            // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
-            tfod.setZoom(2.5, 1.78);
-        }
 
         drivetrain.init(hardwareMap, telemetry);
 
@@ -121,48 +90,12 @@ public class BasicAutonomous extends LinearOpMode {
         telemetry.addData("Mode", "waiting for start");
         telemetry.addData("imu calib status", drivetrain.imu.getCalibrationStatus().toString());
         /** Wait for the game to begin */
-        telemetry.addData("Square", Square);
 
         telemetry.update();
 
         /////////////////////////////////////////////////////////////////////////////////////////////
         waitForStart();
-        tfTime.reset();
-        while (tfTime.time() < tfSenseTime) {
-            if (tfod != null) {
-                // getUpdatedRecognitions() will return null if no new information is available since
-                // the last time that call was made.
-                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                if (updatedRecognitions != null) {
-                    telemetry.addData("# Object Detected", updatedRecognitions.size());
-                    // step through the list of recognitions and display boundary info.
-                    int i = 0;
-                    for (Recognition recognition : updatedRecognitions) {
-                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
-                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
-                                recognition.getLeft(), recognition.getTop());
-                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
-                                recognition.getRight(), recognition.getBottom());
-                        ///
-                        StackSize = recognition.getLabel();
-                        //telemetry.addData("Target", Target);
-                        if (StackSize == "Quad") {
-                            Square = WobbleTargetZone.RED_C;
-                            telemetry.addData("Square", Square);
-                        } else if (StackSize == "Single") {
-                            Square = WobbleTargetZone.RED_B;
-                            telemetry.addData("Square", Square);
 
-                        }
-
-                    }
-                    telemetry.update();
-                }
-            }
-            if (tfod != null) {
-                tfod.shutdown();
-            }
-        }
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         // Put a hold after each turn
@@ -427,30 +360,10 @@ public class BasicAutonomous extends LinearOpMode {
 
     }
 
-    private void initVuforia() {
-        /*
-         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
-         */
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
-
-        //  Instantiate the Vuforia engine
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
-    }
 
     /**
      * Initialize the TensorFlow Object Detection engine.
      */
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minResultConfidence = 0.8f;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
-    }
+
 }
