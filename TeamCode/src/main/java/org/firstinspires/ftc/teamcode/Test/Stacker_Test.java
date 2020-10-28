63ebf71506b7f6fd00fc8987e6482cad30d762bd
David Atkinon <david.atkinson@netzero.com>
2020-10-28 17:00:31 -0500

    updated tuning values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Stacker_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/Stacker_Test.java
@@ -29,16 +29,10 @@
 
 package org.firstinspires.ftc.teamcode.Test;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.Subsystems.Shooter;
-import org.firstinspires.ftc.teamcode.Subsystems.Stacker;
 import org.firstinspires.ftc.teamcode.Subsystems.Wobblegoal;
 
 
