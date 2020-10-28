3b09286d494e05c8533c173d56dce63ac97536b7
Nathan_Bosse <MortimerQuinn@gmail.com>
2020-10-28 15:06:56 -0700

    blurb

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Proto.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="Pushbot: Auto Drive By Encoder", group="Pushbot")
+@Disabled
+public class Autonomous_Proto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        
+    }
+}
