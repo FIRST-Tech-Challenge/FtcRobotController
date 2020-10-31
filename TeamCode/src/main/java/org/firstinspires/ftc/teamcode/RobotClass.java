ab40e7fc94d8a4ad272a479886a94fe457bb5b24
Karine Josien <kjosien1@gmail.com>
2020-10-31 09:44:56 -0600

    add robot class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotClass.java
@@ -0,0 +1,152 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class RobotClass {
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private double ticks = 537;
+
+
+    public RobotClass(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft" );
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight" );
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft" );
+        backRight = hardwareMap.get(DcMotor.class, "backRight" );
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void forward (double speed, double rotations){
+        int leftCurrent = frontLeft.getCurrentPosition();
+        int rightCurrent =frontRight.getCurrentPosition();
+        int backLeftCurrent = backLeft.getCurrentPosition();
+        int backRightCurrent = backRight.getCurrentPosition();
+
+        double toPositionLeft = leftCurrent + rotations*ticks;
+        double toPositionRight = rightCurrent + rotations*ticks;
+        double toPositionbackLeft = backLeftCurrent + rotations*ticks;
+        double toPositionbackRight = backRightCurrent + rotations*ticks;
+
+        frontLeft.setTargetPosition((int)toPositionLeft);
+        frontRight.setTargetPosition((int)toPositionRight);
+        backLeft.setTargetPosition((int)toPositionbackLeft);
+        backRight.setTargetPosition((int)toPositionbackRight);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(Math.abs(speed));
+        frontRight.setPower(Math.abs(speed));
+        backLeft.setPower(Math.abs(speed));
+        backRight.setPower(Math.abs(speed));
+    }
+    public void backwards (double speed, double rotations) {
+        int leftCurrent = frontLeft.getCurrentPosition();
+        int rightCurrent = frontRight.getCurrentPosition();
+        int backLeftCurrent = backLeft.getCurrentPosition();
+        int backRightCurrent = backRight.getCurrentPosition();
+
+        double toPositionLeft = leftCurrent - rotations * ticks;
+        double toPositionRight = rightCurrent - rotations * ticks;
+        double toPositionbackLeft = backLeftCurrent - rotations * ticks;
+        double toPositionbackRight = backRightCurrent - rotations * ticks;
+
+        frontLeft.setTargetPosition((int) toPositionLeft);
+        frontRight.setTargetPosition((int) toPositionRight);
+        backLeft.setTargetPosition((int) toPositionbackLeft);
+        backRight.setTargetPosition((int) toPositionbackRight);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+    }
+
+        public void turnright (double speed, double angle) {
+        double anglemult = 1.5;
+
+            int leftCurrent = frontLeft.getCurrentPosition();
+            int rightCurrent = frontRight.getCurrentPosition();
+            int backLeftCurrent = backLeft.getCurrentPosition();
+            int backRightCurrent = backRight.getCurrentPosition();
+
+            double toPositionLeft = leftCurrent + anglemult  * angle;
+            double toPositionRight = rightCurrent - anglemult * angle;
+            double toPositionbackLeft = backLeftCurrent + anglemult * angle;
+            double toPositionbackRight = backRightCurrent - anglemult * angle;
+
+            frontLeft.setTargetPosition((int) toPositionLeft);
+            frontRight.setTargetPosition((int) toPositionRight);
+            backLeft.setTargetPosition((int) toPositionbackLeft);
+            backRight.setTargetPosition((int) toPositionbackRight);
+
+            frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontLeft.setPower(speed);
+            frontRight.setPower(speed);
+            backLeft.setPower(speed);
+            backRight.setPower(speed);
+    }
+    public void turnleft (double speed, double angle) {
+        double anglemult = 1.5;
+
+        int leftCurrent = frontLeft.getCurrentPosition();
+        int rightCurrent = frontRight.getCurrentPosition();
+        int backLeftCurrent = backLeft.getCurrentPosition();
+        int backRightCurrent = backRight.getCurrentPosition();
+
+        double toPositionLeft = leftCurrent - anglemult * angle;
+        double toPositionRight = rightCurrent + anglemult * angle;
+        double toPositionbackLeft = backLeftCurrent - anglemult * angle;
+        double toPositionbackRight = backRightCurrent + anglemult * angle;
+
+        frontLeft.setTargetPosition((int) toPositionLeft);
+        frontRight.setTargetPosition((int) toPositionRight);
+        backLeft.setTargetPosition((int) toPositionbackLeft);
+        backRight.setTargetPosition((int) toPositionbackRight);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        backRight.setPower(speed);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
