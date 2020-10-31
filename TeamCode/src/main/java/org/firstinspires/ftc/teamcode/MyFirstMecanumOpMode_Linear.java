7682c65df11f40677dbbd3d3a555c938c61c1745
Ollie <ollied370@gmail.com>
2020-10-31 09:36:15 -0600

    Added first Teleop try.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstMecanumOpMode_Linear.java
@@ -0,0 +1,81 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.DigitalChannel;
+//import com.qualcomm.robotcore.hardware.DistanceSensor;
+//import com.qualcomm.robotcore.hardware.Gyroscope;
+//import com.qualcomm.robotcore.hardware.Servo;
+
+
+@TeleOp (name="Mecanum Test?")
+public class MyFirstMecanumOpMode_Linear extends LinearOpMode {
+    //    private Gyroscope imu;
+//    private DcMotor motorTest;
+//    private DigitalChannel digitalTouch;
+//    private DistanceSensor sensorColorRange;
+//    private Servo servoTest;
+    DcMotor leftFrontMotor = null;
+    DcMotor rightFrontMotor = null;
+    DcMotor leftRearMotor = null;
+    DcMotor rightRearMotor = null;
+    @Override
+    public void runOpMode() {
+//        imu = hardwareMap.get(Gyroscope.class, "imu");
+//        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+//        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+//        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+//        servoTest = hardwareMap.get(Servo.class, "servoTest");
+
+        leftFrontMotor = hardwareMap.dcMotor.get("motorFrontLeft");
+        rightFrontMotor = hardwareMap.dcMotor.get("motorFrontRight");
+        leftRearMotor = hardwareMap.dcMotor.get("motorBackLeft");
+        rightRearMotor = hardwareMap.dcMotor.get("motorBackRight");
+
+        leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
+        leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.3;
+            double rx = gamepad1.right_stick_x;
+
+            double leftFrontPower = y + x + rx;
+            double leftRearPower = y - x + rx;
+            double rightFrontPower = y - x - rx;
+            double rightRearPower = y + x - rx;
+
+            if (Math.abs(leftFrontPower) > 1 || Math.abs(leftRearPower) > 1 || Math.abs(rightFrontPower) > 1 || Math.abs(rightRearPower) > 1) {
+
+                double max;
+                max = Math.max(leftFrontPower, leftRearPower);
+                max = Math.max(max, rightFrontPower);
+                max = Math.max(max, rightRearPower);
+
+                leftFrontPower /= max;
+                leftRearPower /= max;
+                rightFrontPower /= max;
+                rightRearPower /= max;
+            }
+
+            leftFrontMotor.setPower(leftFrontPower);
+            leftRearMotor.setPower(leftRearPower);
+            rightFrontMotor.setPower(rightFrontPower);
+            rightRearMotor.setPower(rightRearPower);
+
+
+        }
+    }
+}
