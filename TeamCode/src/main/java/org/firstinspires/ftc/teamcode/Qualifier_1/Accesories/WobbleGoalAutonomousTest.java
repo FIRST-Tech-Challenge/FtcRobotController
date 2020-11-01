048199bee7ef17bbc4a242510e1e059200a62ed3
Siddharth Arutla <sidarutla@gmail.com>
2020-11-01 11:48:00 -0800

    Fixed import error for wobble goal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Accesories/WobbleGoalAutonomousTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Accesories/WobbleGoalAutonomousTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Qualifier_1.Accesories;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.Qualifier_1.Components.Accesories.WobbleGoal;
 
 @Autonomous (name = "WobbleGoalAutonomous")
 public class WobbleGoalAutonomousTest extends LinearOpMode {
