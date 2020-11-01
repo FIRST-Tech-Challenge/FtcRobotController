a4905a2a5f3dfc9dd415d3ba1e140e8195f124ba
Siddharth Arutla <sidarutla@gmail.com>
2020-10-31 19:18:30 -0700

    Wobble Goal programming (not finished yet)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Accesories/WobbleGoal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Accesories/WobbleGoal.java
@@ -3,28 +3,56 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robot;
 
-@Autonomous(name = "wobbleGoal")
-public class WobbleGoal extends LinearOpMode {
 
-    DcMotorEx wobbleGoalMotor;
-    Robot robot = new Robot();
+public class WobbleGoal {
+
+    DcMotor wobbleGoalMotor;
+
+
+    private Robot robot = new Robot();
     private ElapsedTime runtime = new ElapsedTime();
 
-    @Override
-    public void runOpMode() {
+    public WobbleGoal(DcMotor inputWobbleGoalMotor){
 
-
-        wobbleGoalMotor = (DcMotorEx) this.hardwareMap.dcMotor.get("wobbleGoalMotor");
-        waitForStart();
-
-        wobbleGoalMotor.setPower(0.1);
+        wobbleGoalMotor = inputWobbleGoalMotor;
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        wobbleGoalMotor.setPower(0.5);
     }
 
+    public void position0() {
+
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wobbleGoalMotor.setTargetPosition(0);
+
+    }
+
+    public void position1() {
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wobbleGoalMotor.setTargetPosition(1200);
+    }
+
+    public void position2() {
+
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wobbleGoalMotor.setTargetPosition(700);
+
+
+    }
+
+    public void position3() {
+
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        wobbleGoalMotor.setTargetPosition(1000);
+
+
+    }
 }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Accesories/WobbleGoalAutonomousTest.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.Qualifier_1.Accesories;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Autonomous (name = "WobbleGoalAutonomous")
+public class WobbleGoalAutonomousTest extends LinearOpMode {
+
+    public void runOpMode(){
+
+        DcMotorEx motor = (DcMotorEx) hardwareMap.dcMotor.get ("wobbleGoalMotor");
+        WobbleGoal wobbleGoal = new WobbleGoal(motor);
+
+
+        wobbleGoal.position1();
+
+    }
+}
