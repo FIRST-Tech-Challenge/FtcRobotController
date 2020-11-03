2173c1bd286409bd200671281543b5cf376f4391
rsmrohit <32987551+rsmrohit@users.noreply.github.com>
2020-11-02 21:55:06 -0800

    Checkpoint to save work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testAuto.java
@@ -8,6 +8,8 @@ public class testAuto extends LinearOpMode {
     HardwareMapV2 robot;
     Drivetrain drivetrain;
     OdometryGlobalCoordinatePosition globalCoordinatePosition;
+    double powerA, powerB;
+    double motorX, motorY = 1.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,15 +23,37 @@ public class testAuto extends LinearOpMode {
         Thread positionThread = new Thread(globalCoordinatePosition);
         positionThread.start();
 
-
+        driveTo(40, 40, 50);
 
     }
 
     public void driveTo(double x, double y, double theta) {
         while (opModeIsActive() && withinpos(globalCoordinatePosition.globalPositionX, x, globalCoordinatePosition.globalPositionY, y, 50) && withinpos(globalCoordinatePosition.robotOrientationDegrees, theta, 5)) {
-            
+            findPower(x, y);
+            drivetrain.setMotorPowers(powerB, powerA, powerA, powerB); //doesnt integrate turning
         }
     }
+
+    public void findPower(double x, double y) {
+        double slope = y/x;
+        motorX = Math.cos(Math.toRadians(globalCoordinatePosition.changeinrobotorientationDegrees-45));//PROBLEM IN CODE
+        motorY = Math.sin(Math.toRadians(globalCoordinatePosition.changeinrobotorientationDegrees-45));//PROBLEM IN CODE
+        powerA = ((motorX/1) + (motorY/slope))/2; //(1/1) condensed to 1
+        powerB = ((-motorX/1) + (motorY/slope))/2; //(-1/1) condensed to -1
+        if (Math.abs(powerA) > Math.abs(powerB)) {
+            powerB = 1/(powerA/powerB);
+            powerA = 1;
+        } else {
+            powerA = 1/(powerB/powerA);
+            powerB = 1;
+        }
+        if (x < 0){
+            powerA = -powerA;
+        } else {
+            powerB = -powerB;
+        }
+    }
+
 
     public boolean withinpos(double currpos, double wantpos, double within){
         return currpos > wantpos-within && currpos < wantpos + within;
