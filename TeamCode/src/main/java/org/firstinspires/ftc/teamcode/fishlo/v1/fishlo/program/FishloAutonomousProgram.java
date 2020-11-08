5dd938d57176e6f263ff7e5ed9b5172c3ea73c68
Saranga Rajagopalan <sarnga.raj@gmail.com>
2020-11-08 14:25:27 -0500

    Auto Wobble Goal program for right (Need to add turning)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/program/FishloAutonomousProgram.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.program;
+
+import org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot.*;
+import org.firstinspires.ftc.teamcode.opMode.AutonomousProgram;
+import org.firstinspires.ftc.teamcode.robot.Robot;
+
+public class FishloAutonomousProgram extends AutonomousProgram {
+    protected Claw claw;
+    protected Drive drive;
+    protected Gyro gyro;
+    protected Lift lift;
+    protected Vision vision;
+
+    @Override
+    protected Robot buildRobot() {
+        Fishlo fishlo = new Fishlo(this);
+
+        drive = (Drive) fishlo.getSubSystem("Drive");
+        claw = (Claw) fishlo.getSubSystem("Claw");
+        gyro = (Gyro) fishlo.getSubSystem("Gyro");
+        lift = (Lift) fishlo.getSubSystem("Lift");
+        vision = (Vision) fishlo.getSubSystem("Vision");
+
+        return fishlo;
+    }
+
+    @Override
+    public void main() {}
+}
