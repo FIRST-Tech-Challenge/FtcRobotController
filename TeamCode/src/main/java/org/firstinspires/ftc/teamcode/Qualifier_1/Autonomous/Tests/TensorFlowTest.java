df23e1fb45b1ef5da865f1b6ed6d588775ab3557
awesomeaamod <awesome.aamod@gmail.com>
2020-11-08 17:43:21 -0800

    Fixed TensorFlow errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Autonomous/Tests/TensorFlowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Autonomous/Tests/TensorFlowTest.java
@@ -9,19 +9,28 @@ import org.firstinspires.ftc.teamcode.Qualifier_1.Robot;
 import org.firstinspires.ftc.teamcode.Qualifier_1.Components.ObjectDetection.TensorFlow;
 
 @Autonomous
-//@Disabled
+@Disabled
 public class TensorFlowTest extends LinearOpMode {
     Robot robot = new Robot();
+    TensorFlow tensorFlow = new TensorFlow(this);
     private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
-        robot.initVuforia();
-        robot.initTfod();
+        robot.initTensorFlow();
+        robot.runTensorFlow();
         robot.initChassis(this);
+        robot.stopTensorFlow();
 
         waitForStart();
 
-
+        robot.runTensorFlow();
+        if (tensorFlow.getNumberOfRings() == 4) {
+            robot.moveForward(10,0.5);
+        } else if (tensorFlow.getNumberOfRings() == 1) {
+            robot.moveLeft(10,0.5);
+        } else {
+            robot.moveRight(10, 0.5);
+        }
     }
 }
