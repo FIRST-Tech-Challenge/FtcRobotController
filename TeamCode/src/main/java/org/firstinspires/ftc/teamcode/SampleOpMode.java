ba64e2b9d753310fdcbb32aabe74aa02a7be59cd
asarad39 <55108550+asarad39@users.noreply.github.com>
2020-11-15 13:39:46 -0500

    Changed testing teleop to test the hopper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleOpMode.java
@@ -12,10 +12,11 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class SampleOpMode extends LinearOpMode {
 
     private DcMotor testMotor;
-    private Servo testServo;
-    private double max = 0.00; // Maximum rotational position
-    private double min = 1.00; // Minimum rotational position
-    private double currentPos = 0.00;
+    private DcMotor testMotor2;
+//    private Servo testServo;
+//    private double max = 0.00; // Maximum rotational position
+//    private double min = 1.00; // Minimum rotational position
+//    private double currentPos = 0.00;
 
     @Override
     public void runOpMode() {
@@ -24,7 +25,8 @@ public class SampleOpMode extends LinearOpMode {
         //these names all need to match the names in the config
 
         testMotor = hardwareMap.get(DcMotor.class, "testmotor");
-        testServo = hardwareMap.get(Servo.class, "testservo");
+        testMotor2 = hardwareMap.get(DcMotor.class, "testmotor2");
+//        testServo = hardwareMap.get(Servo.class, "testservo");
 
 
         waitForStart();
@@ -33,31 +35,37 @@ public class SampleOpMode extends LinearOpMode {
             //motor..........left stick up and down
             //servo..........right stick up
 
-            // gamepad 1
-            testMotor.setPower( gamepad1.left_stick_y );
-
+//            // gamepad 1
+//            testMotor.setPower( gamepad1.left_stick_y );
+            if(gamepad2.right_bumper) {
+                testMotor.setPower(1);
+                testMotor2.setPower(.35);
+            } else {
+                testMotor.setPower(0);
+                testMotor2.setPower(0);
+            }
             /*
             when stick is not at zero, it moves the servo. pressing A will set the current pos
             to the new max and pressing B will set the min. when stick is at zero, then pressing A
             or B will set the servo to the max or min position
             */
 
-            if (gamepad1.right_stick_y != 0) { //if stick is being moved
-                currentPos = Math.abs(gamepad1.right_stick_y);
-                if (gamepad1.a) { max = currentPos; }
-                if (gamepad1.b) { min = currentPos; }
-            } else { //if stick is untouched
-                if (gamepad1.a) { currentPos = max; }
-                if (gamepad1.b) { currentPos = min; }
-            }
-
-            testServo.setPosition(currentPos);
+//            if (gamepad1.right_stick_y != 0) { //if stick is being moved
+//                currentPos = Math.abs(gamepad1.right_stick_y);
+//                if (gamepad1.a) { max = currentPos; }
+//                if (gamepad1.b) { min = currentPos; }
+//            } else { //if stick is untouched
+//                if (gamepad1.a) { currentPos = max; }
+//                if (gamepad1.b) { currentPos = min; }
+//            }
+//
+//            testServo.setPosition(currentPos);
 
             telemetry.addData("Motor Power", gamepad1.left_stick_y);
             telemetry.addData("Right Stick Pos", gamepad1.right_stick_y);
-            telemetry.addData("Servo Position", currentPos);
-            telemetry.addData("Servo Max", max);
-            telemetry.addData("Servo Min", min);
+//            telemetry.addData("Servo Position", currentPos);
+//            telemetry.addData("Servo Max", max);
+//            telemetry.addData("Servo Min", min);
             telemetry.update();
 
         }
