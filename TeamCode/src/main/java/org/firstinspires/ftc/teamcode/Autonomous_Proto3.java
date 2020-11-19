6d3fd7a6276b8ce000fdc260f2449c3d3fedd8c1
Nathan_Bosse <MortimerQuinn@gmail.com>
2020-11-18 17:12:02 -0800

    MORE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Proto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Proto3.java
@@ -51,9 +51,9 @@ public class Autonomous_Proto3 extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout      \\\
+        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout    |||SEE FUNCTION AT BOTTOM
+        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout      ///
 
         robot.leftClaw.setPosition(1.0);            // S4: Stop and close the claw.
         robot.rightClaw.setPosition(0.0);
