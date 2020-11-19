51f81c452844a2eb6ec70b1c5585f8ba3839f5eb
MenachemLotwin <Menachemlotwin@gmail.com>
2020-11-18 20:09:06 -0500

    Set up motor direction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Loader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Loader.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Loader {
     private DcMotor motor;
     public Loader(DcMotor motor) {
         this.motor = motor;
+        motor.setDirection(DcMotorSimple.Direction.FORWARD);
+        motor.setPower(0.0);
     }
     public void on() {
         motor.setPower(1.0);
