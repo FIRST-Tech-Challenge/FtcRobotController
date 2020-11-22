882b151a6750fd6ffbe3e75f79601ff387181231
Saranga Rajagopalan <sarnga.raj@gmail.com>
2020-11-22 16:47:07 -0500

    Forgot to stage :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCVExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCVExample.java
@@ -21,6 +21,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -36,7 +37,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-@TeleOp
+@Disabled
 public class EasyOpenCVExample extends LinearOpMode
 {
     OpenCvCamera webcam;
