c359202e8a82f01157cbd7bf87fbadf0651294de
Saranga Rajagopalan <sarnga.raj@gmail.com>
2020-11-22 16:43:15 -0500

    Added Intake and Shooter subsystems (Also cleaned up imports for Drive.java)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/fishlo/v1/fishlo/robot/Drive.java
@@ -1,17 +1,9 @@
 package org.firstinspires.ftc.teamcode.fishlo.v1.fishlo.robot;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
-import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.teamcode.robot.Robot;
 import org.firstinspires.ftc.teamcode.robot.SubSystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Drive extends SubSystem {
 
