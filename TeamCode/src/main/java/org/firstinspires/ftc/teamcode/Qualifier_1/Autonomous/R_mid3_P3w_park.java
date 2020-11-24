952b4cca4c09540008478552be5df2028f8f48d8
Sai Monish Tunguturu <tsaimonish@gmail.com>
2020-11-23 21:56:03 -0800

    Started

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Autonomous/R_mid3_P3w_park.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Qualifier_1/Autonomous/R_mid3_P3w_park.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.Qualifier_1.Components.BasicChassis;
 import org.firstinspires.ftc.teamcode.Qualifier_1.Robot;
 
-@Autonomous(name = "R_mid3_P2w_park")
-public class R_mid3_P2w_park extends LinearOpMode {
+@Autonomous(name = "R_mid3_P3w_park")
+public class R_mid3_P3w_park extends LinearOpMode {
 
     @Override
     public void runOpMode() {
@@ -18,11 +18,12 @@ public class R_mid3_P2w_park extends LinearOpMode {
 
         waitForStart();
         robot.moveWobbleGoalServo(false);
-        robot.moveBackward(59, 0.5);
+        robot.moveBackward(60, 0.5);
+        telemetry.addData("Moving Backwards", 60);
+        telemetry.update();
         robot.shootHighGoal(3);
-        robot.moveBackward(20, 0.5);
-        sleep(1500);
-        robot.moveWobbleGoalServo(true);
-        robot.moveForward(10, 0.5);
+        telemetry.addData("Shooting Rings", 3);
+        telemetry.update();
+        robot.moveLeft(24, 0.5);
     }
 }
