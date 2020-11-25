97bf88fd0bfe85daab669f67c45e71a1d1f40623
David Atkinon <david.atkinson@netzero.com>
2020-11-24 22:28:53 -0600

    removed RAMS file and adjusted shooter speed down a bit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Shooter.java
@@ -7,16 +7,16 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Shooter {
     // Define hardware objects
-    public DcMotor shooterleft=null;
-    public DcMotor shooterright=null;
-    public Servo leftFlipper=null;
-    public Servo rightFlipper=null;
-    public Servo stacker=null;
+    public DcMotor  shooterleft=null;
+    public DcMotor  shooterright=null;
+    public Servo    leftFlipper=null;
+    public Servo    rightFlipper=null;
+    public Servo    stacker=null;
 
     //
     //Constants for shooter motors
-    private static final double ShooterSpeedfastleft=.47;
-    private static final double ShooterSpeedfastright=.53;
+    private static final double ShooterSpeedfastleft=.46; // reset after meet 1 to improve consistiencey
+    private static final double ShooterSpeedfastright=.52;
     //We are changing the speeds to see which speeds make the first ring in
     private static final double shooterSpeedslowleft=.4;
     private static final double shooterSpeedslowright=.50;
@@ -29,18 +29,18 @@ public class Shooter {
     private static final double flippercenter = 0.5;
     private static final double stackerReload = 0.43; // 0.42 causes rings to slide forward too much
     private static final double stackerShoot = 0.54; // 0.55 is a bit high
-    private static final double stackerShootAutoOnly = 0.54; // slightly different in Auto so Teleop stays the same
+    //private static final double stackerShootAutoOnly = 0.54; // slightly different in Auto so Teleop stays the same
     private static final double stacketMidLoad = .43; // tips stacker back so it loads better
 
 
 
 
     public void init(HardwareMap hwMap)  {
-        shooterleft=hwMap.get(DcMotor.class,"LeftShooter");
-        shooterright=hwMap.get(DcMotor.class,"RightShooter");
-        leftFlipper = hwMap.get(Servo.class, "Left_Flipper");
-        rightFlipper = hwMap.get(Servo.class, "Right_Flipper");
-        stacker =  hwMap.get(Servo.class, "Stacker");
+        shooterleft     = hwMap.get(DcMotor.class,"LeftShooter");
+        shooterright    = hwMap.get(DcMotor.class,"RightShooter");
+        leftFlipper     = hwMap.get(Servo.class, "Left_Flipper");
+        rightFlipper    = hwMap.get(Servo.class, "Right_Flipper");
+        stacker         = hwMap.get(Servo.class, "Stacker");
 
         shooterleft.setDirection(DcMotor.Direction.REVERSE);
         shooterright.setDirection(DcMotor.Direction.FORWARD);
@@ -64,19 +64,18 @@ public class Shooter {
     public void shooterOff() {
             shooterleft.setPower(0);
             shooterright.setPower(0);
+    }
 
-        }
     public void stackerMoveToShoot() {
         stacker.setPosition(stackerShoot);
-
     }
 
     public void stackerMoveToShootInAutoOnly() {
-        stacker.setPosition(stackerShootAutoOnly);
-
+        stacker.setPosition(stackerShoot);
     }
 
     public void stackerMoveToReload() {
+
         stacker.setPosition(stackerReload);
     }
 
@@ -122,7 +121,7 @@ public class Shooter {
         shooterOff(); // turn off shooters
 
         }
-
+    // not used
     public void jamClear() {
         shooterleft.setPower(jamClear);
         shooterright.setPower(jamClear);
