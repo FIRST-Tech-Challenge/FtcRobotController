2a0a6f2d6306ce0c044977203f60a98dd7ad34bd
asarad39 <55108550+asarad39@users.noreply.github.com>
2020-11-30 14:43:34 -0500

    Added servo test file that allowed us to  move claw and gearbox servos at the same time!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Servo Test", group="Linear Opmode")
+
+public class ServoTest extends LinearOpMode {
+
+    private TwoPosServo claw; //the file this used to be is still called Foundation btw
+    private TwoPosServo gear;
+    private boolean clawButtonIsDown = false; // controls the claw servo button press
+    private boolean gearboxButtonIsDown = false; // controls the gearbox servo button press
+
+    @Override
+    public void runOpMode() {
+
+        claw = new TwoPosServo(
+                hardwareMap.get(Servo.class, "claw"),
+                0.5, 1.0);
+        gear = new TwoPosServo(
+                hardwareMap.get(Servo.class, "gearbox"),
+                0.5, 1.0);
+
+
+        waitForStart();
+        while (opModeIsActive()) {
+
+            if (gamepad1.y && !clawButtonIsDown) {
+                clawButtonIsDown = true;
+                gear.nextPos();
+                claw.nextPos();
+            } else if (!gamepad1.y) {
+                clawButtonIsDown = false;
+            }
+
+            telemetry.addData("Motor Power", gamepad1.left_stick_y);
+            telemetry.addData("Right Stick Pos", gamepad1.right_stick_y);
+//            telemetry.addData("Servo Position", currentPos);
+//            telemetry.addData("Servo Max", max);
+//            telemetry.addData("Servo Min", min);
+            telemetry.update();
+
+        }
+    }
+}
