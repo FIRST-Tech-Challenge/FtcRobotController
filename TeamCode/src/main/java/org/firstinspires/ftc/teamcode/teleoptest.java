bd61bc6c7918685736e5cb14bf69e04b039281a0
samihsq <samih.qureshi@gmail.com>
2020-12-05 11:24:50 -0800

    Create teleoptest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleoptest.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Ultimate Goal Teleop", group="UltimateGoal")
+public class teleoptest extends OpMode {
+
+    // declaring variables
+    MecanumDriveTrain vroom;
+
+    /* Declare OpMode members. */
+    HardwareMapV2 robot; // use the class created to define a RoverRuckus's hardware
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
