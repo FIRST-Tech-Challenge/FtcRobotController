12d7f9025e5b69eef6d52815253750eb909957f3
Mark Matthews <markdmatthews@users.noreply.github.com>
2020-12-07 16:35:39 -0600

    GRIP experiments

---
+++ TeamCode/src/main/java/com/hfrobots/tnt/season2021/VisionTest.java
@@ -0,0 +1,35 @@
+/*
+ Copyright (c) 2020 HF Robotics (http://www.hfrobots.com)
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+package com.hfrobots.tnt.season2021;
+
+import com.ftc9929.corelib.control.NinjaGamePad;
+import com.hfrobots.tnt.corelib.vision.OpenCvTestTeleop;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@TeleOp(name = "UG-Vision")
+public class VisionTest extends OpenCvTestTeleop {
+    @Override
+    protected OpenCvPipeline getPipeline() {
+        return StarterStackDetectorPipeline.builder().telemetry(telemetry).build();
+    }
+}
