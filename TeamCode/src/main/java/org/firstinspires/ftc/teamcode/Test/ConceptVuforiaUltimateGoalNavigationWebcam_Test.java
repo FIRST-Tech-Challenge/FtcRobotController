cb8a65c815114a43a93b5fd0528ec8afe629df5e
David Atkinon <david.atkinson@netzero.com>
2020-12-09 23:25:19 -0600

    changed camera location o robot. Did not seem to work on webcam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ConceptVuforiaUltimateGoalNavigationWebcam_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/ConceptVuforiaUltimateGoalNavigationWebcam_Test.java
@@ -244,9 +244,9 @@ public class ConceptVuforiaUltimateGoalNavigationWebcam_Test extends LinearOpMod
 
         // Next, translate the camera lens to where it is on the robot.
         // In this example, it is centered (left to right), but forward of the middle of the robot, and above ground level.
-        final float CAMERA_FORWARD_DISPLACEMENT  = 4.0f * mmPerInch;   // eg: Camera is 4 Inches in front of robot-center
-        final float CAMERA_VERTICAL_DISPLACEMENT = 8.0f * mmPerInch;   // eg: Camera is 8 Inches above ground
-        final float CAMERA_LEFT_DISPLACEMENT     = 0;     // eg: Camera is ON the robot's center line
+        final float CAMERA_FORWARD_DISPLACEMENT  = 2.5f * mmPerInch;   // eg: Camera is 4 Inches in front of robot-center
+        final float CAMERA_VERTICAL_DISPLACEMENT = 0.5f * mmPerInch;   // eg: Camera is 8 Inches above ground
+        final float CAMERA_LEFT_DISPLACEMENT     = 0.5f * mmPerInch;     // eg: Camera is ON the robot's center line
 
         OpenGLMatrix robotFromCamera = OpenGLMatrix
                     .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
