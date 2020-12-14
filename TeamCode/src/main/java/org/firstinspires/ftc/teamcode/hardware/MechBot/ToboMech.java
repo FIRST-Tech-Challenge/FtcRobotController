3148f2f0fde335d9a381075309f17ab523069e7a
praveer35 <51690132+praveer35@users.noreply.github.com>
2020-12-13 16:54:24 -0800

    deliversecondwobblegoal zoneb

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
@@ -1245,7 +1245,7 @@ public class ToboMech extends Logger<ToboMech> implements Robot2 {
                 // chassis.driveTo(.8, side(30), 40, 0, false, 2);
                 chassis.driveTo(0.9, side(20), 165, -5, false, 5);
             } else if (tZone == TargetZone.ZONE_B) {//1
-                chassis.driveTo(auto_chassis_power, side(70), 225, 0, false, 5);
+                chassis.driveTo(auto_chassis_power, side(70), 230, 0, false, 5);
 
             } else if (tZone == TargetZone.ZONE_C) {//4
                 chassis.driveTo(.8, side(50), 60, 0, false, 5);
