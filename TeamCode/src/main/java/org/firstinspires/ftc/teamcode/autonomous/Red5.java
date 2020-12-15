3f8aa88694e3bb641f4af20d17b4a2dbf2267107
PriyaSoneji <priyas.fcs@gmail.com>
2020-12-15 18:47:00 -0500

    added Red-5

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red5.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Red-5", group ="playback")
+public class Red5 extends AutoReplayBase {
+
+}
