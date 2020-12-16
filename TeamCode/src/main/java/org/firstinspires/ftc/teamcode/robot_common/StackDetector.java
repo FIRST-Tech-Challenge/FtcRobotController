494abfd3a04c401109b31013d332f137ad8afff9
anayamdesai <adesai@darlingtonschool.org>
2020-12-16 09:39:38 -0500

    Create StackDetector.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_common/StackDetector.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.robot_common;
+
+
+import org.darbots.darbotsftclib.main.java.org.openftc.easyopencv.OpenCvPipeline;
+import org.darbots.darbotsftclib.main.java.org.openftc.easyopencv.Mat;
+
+public class StackDetector extends OpenCvPipeline {
+    private Mat workingMatrix = new Mat();
+    @Override
+    public final Mat processFrame(Mat input) {
+        input.copyTo(workingMatrix);
+        if (workingMatrix.empty()) {
+            return input;
+        }
+
+        return input;
+
+    }
+
+}
