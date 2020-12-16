c033a43ee22afed46efe0449345db06c3508a9a6
anayamdesai <adesai@darlingtonschool.org>
2020-12-16 09:39:27 -0500

    Create TeamCodeDetector.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCodeDetector.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.openftc.easyopencv.OpenCvPipeline;
+public class TeamCodeDetector extends OpenCvPipeli {
+    public TeamCodeDetector (){
+
+    }
+    @Override
+    public final Mat processFrame(Mat input) {
+
+    }
+
+}
