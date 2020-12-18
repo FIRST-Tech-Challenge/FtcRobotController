565a52a4382f7461fa4aea9256ec83eedaad2144
Mason Wu <yutong.w@outlook.com>
2020-12-19 00:09:06 +0800

    Fix errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_common/Robot4100Common_Template.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_common/Robot4100Common_Template.java
@@ -1,26 +1,27 @@
+/*
+MIT License
+Copyright (c) 2018 DarBots Collaborators
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+To use this teamcode,
+Refractor Robot4100Common_Template to Robot4100Common, and fill in the following information.
+ */
 package org.firstinspires.ftc.teamcode.robot_common;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+public class Robot4100Common_Template {
+    public static final String VUFORIA_LICENSE = ""; //Paste your Vuforia License Here
 
-//import org.openftc.easyopencv.OpenCvCameraFactory;
-//import org.openftc.easyopencv.OpenCvInternalCamera;
-//import org.darbots.darbotsftclib.main.java.*;
-import org.darbots.darbotsftclib.main.java.org.openftc.easyopencv.OpenCvCamera;
-import org.darbots.darbotsftclib.main.java.org.openftc.easyopencv.OpenCvInternalCamera;
-import org.darbots.darbotsftclib.main.java.org.openftc.easyopencv.OpenCvCameraFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-
-@Autonomous(name= "TeamCode Autonomous")
-public class Robot4100Common_Template extends LinearOpMode {
-    private OpenCvCamera phoneCam;
-
-    @Override
-    public void runOpMode (){
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        WebcamName webcamname = hardwareMap.get(WebcamName.class, "Webcam 1");
-        phoneCam = OpenCvCameraFactory.getInstance().createWebcam(webcamname, cameraMonitorViewId);
-        phoneCam.openCameraDevice();
-    }
-}
-
+} 
