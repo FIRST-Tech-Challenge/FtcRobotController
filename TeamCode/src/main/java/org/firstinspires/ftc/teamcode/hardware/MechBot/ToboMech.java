113141ada3537c3fa44d6c8a4bc47169edafa18c
sasha <fefel05714@loswego.k12.or.us>
2021-01-03 15:15:59 -0800

    increase speed for zone C - no rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
@@ -1199,7 +1199,7 @@ public class ToboMech extends Logger<ToboMech> implements Robot2 {
             } else if (tZone == TargetZone.ZONE_B) {//1
                 chassis.driveTo(auto_chassis_power, 70, 240, 0, true, 4);
             } else if (tZone == TargetZone.ZONE_C) {//4
-                chassis.driveTo(auto_chassis_power+0.1, 10, 300, -40, true, 6);
+                chassis.driveTo(auto_chassis_power+0.1, 10, 300, 0, false, 6); // no rotation to make it faster
             } else {
                 return;
             }
@@ -1362,7 +1362,7 @@ public class ToboMech extends Logger<ToboMech> implements Robot2 {
             if(tZone == TargetZone.ZONE_B){
                 chassis.driveTo(auto_chassis_power, side(50), 170, 0, true,  5);
             }
-            chassis.driveTo(auto_chassis_power, side(70), 40, 0, true,  5);
+            chassis.driveTo(.99, side(70), 40, 0, true,  5);
 
         if(startPos == StartPosition.OUT){
             if (tZone == TargetZone.ZONE_C || tZone == TargetZone.ZONE_B) {
@@ -1429,7 +1429,11 @@ public class ToboMech extends Logger<ToboMech> implements Robot2 {
         if (tZone==TargetZone.ZONE_A){
             chassis.driveTo(auto_chassis_power+0.2, side(110), 165, 0, false, 2);
         }
-        chassis.driveTo(1.0, Math.max(90, Math.min(chassis.odo_x_pos_cm(), 170)), 210, chassis.getCurHeading(), false,  2);
+        if (tZone==TargetZone.ZONE_C){
+            chassis.driveTo(1.0, Math.max(90, Math.min(chassis.odo_x_pos_cm(), 170)), 225, chassis.getCurHeading(), false,  2);
+        } else{
+            chassis.driveTo(1.0, Math.max(90, Math.min(chassis.odo_x_pos_cm(), 170)), 210, chassis.getCurHeading(), false,  2);
+        }
     }
     public double side( double x){
         if (side == ProgramType.AUTO_RED){
