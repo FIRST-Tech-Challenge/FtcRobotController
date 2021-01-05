9ad88fdd1cd24320d6576369dc896a925aba1911
awesomeaamod <awesome.aamod@gmail.com>
2021-01-04 17:17:37 -0800

    Worked on some TODO items

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Navigations/VuforiaWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Navigations/VuforiaWebcam.java
@@ -71,11 +71,6 @@ public class VuforiaWebcam extends Thread {
         final float quarterField = 36 * mmPerInch;
         final float mmTargetHeight = 5.75f * mmPerInch;
 
-        //TODO: Aamod, unused variable.
-        // Use back camera
-        final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
-
-
         WebcamName webcamName = op.hardwareMap.get(WebcamName.class, "WebcamSide");
 
         // Show camera view on RC phone screen
@@ -196,41 +191,5 @@ public class VuforiaWebcam extends Thread {
 
     public String getVuforiaTrackable() {
         return trackable;
-    }
-
-
-    //TODO: Aamod, hmm.. I don't think this is the right way of running a thread. We should rely on the run method.
-    public void runVuforia() {
-        while (!isInterrupted()) {
-            targetVisible = false;
-            // Look for Trackable, Update Robot Location if Possible
-            for (VuforiaTrackable trackable : allTrackables) {
-                if (((VuforiaTrackableDefaultListener) trackable.getListener()).isVisible()) {
-                    op.telemetry.addData("Visible Target ", trackable.getName());
-                    targetVisible = true;
-
-                    OpenGLMatrix robotLocationTransform = ((VuforiaTrackableDefaultListener) trackable.getListener()).getUpdatedRobotLocation();
-                    if (robotLocationTransform != null) {
-                        lastLocation = robotLocationTransform;
-                    }
-                    break;
-                }
-            }
-
-            // Return Location Data (Last Known Location)
-            if (targetVisible) {
-                VectorF translation = lastLocation.getTranslation();
-                op.telemetry.addData("Pos (in)", "{X, Y, Z} = %.1f, %.1f, %.1f",
-                        translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, translation.get(2) / mmPerInch);
-                Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                xpos = translation.get(0) / mmPerInch;
-                ypos = translation.get(1) / mmPerInch;
-                angle = rotation.thirdAngle;
-            }
-            else {
-                op.telemetry.addData("Visible Target", "none");
-            }
-            op.telemetry.update();
-        }
     }
 }
