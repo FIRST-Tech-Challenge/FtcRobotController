a67c94091ea260dbfd120ea94429f491afeeb59b
Ollie <ollied370@gmail.com>
2021-01-06 17:55:39 -0700

    Adding wobble goal controls to autonomous.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstMecanumOpMode_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstMecanumOpMode_Linear.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 //import com.qualcomm.robotcore.hardware.DcMotorSimple;
 //import com.qualcomm.robotcore.hardware.DigitalChannel;
 //import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -21,6 +22,12 @@ public class MyFirstMecanumOpMode_Linear extends LinearOpMode {
     DcMotor rightFrontMotor = null;
     DcMotor leftRearMotor = null;
     DcMotor rightRearMotor = null;
+    DcMotor wobbleGoalExtendMotor = null;
+    DcMotor wobbleGoalRaiseMotor = null;
+    Servo wobbleGoalGrippyThing = null;
+
+
+
     @Override
     public void runOpMode() {
 //        imu = hardwareMap.get(Gyroscope.class, "imu");
@@ -34,11 +41,17 @@ public class MyFirstMecanumOpMode_Linear extends LinearOpMode {
         rightFrontMotor = hardwareMap.dcMotor.get("frontRight");
         leftRearMotor = hardwareMap.dcMotor.get("backLeft");
         rightRearMotor = hardwareMap.dcMotor.get("backRight");
+        wobbleGoalExtendMotor = hardwareMap.dcMotor.get("");
+        wobbleGoalRaiseMotor = hardwareMap.dcMotor.get("");
+        wobbleGoalGrippyThing = hardwareMap.servo.get("");
 
         leftFrontMotor.setDirection(DcMotor.Direction.REVERSE);
         rightFrontMotor.setDirection(DcMotor.Direction.FORWARD);
         leftRearMotor.setDirection(DcMotor.Direction.REVERSE);
         rightRearMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        boolean yPressed = false;
+
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -76,7 +89,43 @@ public class MyFirstMecanumOpMode_Linear extends LinearOpMode {
             rightFrontMotor.setPower(rightFrontPower);
             rightRearMotor.setPower(rightRearPower);
 
+            if (gamepad2.left_trigger >= .87) {
+                wobbleGoalRaiseMotor.setPower(.1);
+            } else {
+                wobbleGoalRaiseMotor.setPower(0);
+            }
 
+            if (gamepad2.left_bumper == true) {
+                wobbleGoalRaiseMotor.setPower(-.3);
+            } else {
+                wobbleGoalRaiseMotor.setPower(0);
+            }
+
+            if (gamepad2.right_trigger >= .87) {
+                wobbleGoalExtendMotor.setPower(.2);
+            } else {
+                wobbleGoalExtendMotor.setPower(0);
+            }
+
+            if (gamepad2.right_bumper == true) {
+                wobbleGoalExtendMotor.setPower(-.2);
+            } else {
+                wobbleGoalExtendMotor.setPower(0);
+            }
+
+
+            if (gamepad2.y) {
+                if (!yPressed) {
+                    wobbleGoalGrippyThing.setPosition(.4);
+                    yPressed = true;
+                }
+            } else {
+                if (yPressed) {
+                    wobbleGoalGrippyThing.setPosition(.7);
+                    yPressed = false;
+                }
+
+            }
         }
     }
 }
