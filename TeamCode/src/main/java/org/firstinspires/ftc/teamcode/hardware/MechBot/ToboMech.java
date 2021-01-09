4bb0b3ff6c0825ef54d93d50cc4b3e48233f5e7f
praveer35 <51690132+praveer35@users.noreply.github.com>
2021-01-09 14:58:18 -0800

    changed x-pos of first wobble goal delivery from 13 to 3

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MechBot/ToboMech.java
@@ -1418,7 +1418,7 @@ public class ToboMech extends Logger<ToboMech> implements Robot2 {
                 autoIntakeRings(3);
                 chassis.driveTo(1.0, side(90), 140, 0, false, 5);
                 autoShootHighGoal(3);
-                chassis.driveTo(1.0, side(13), 280, 0, false, 5);
+                chassis.driveTo(1.0, side(3), 280, 0, false, 5);
             } else {
                 return;
             }
