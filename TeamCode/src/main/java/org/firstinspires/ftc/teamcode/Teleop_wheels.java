eeef066995abbcb52f7e4adeb5978914d0433987
Nathan_Bosse <MortimerQuinn@gmail.com>
2021-01-09 10:50:09 -0800

    a

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop_wheels.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 import static java.lang.StrictMath.abs;
 
 
