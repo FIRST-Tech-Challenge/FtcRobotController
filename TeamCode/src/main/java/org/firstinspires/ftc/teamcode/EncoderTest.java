df7bd4cd427347079ed57e780adc3151898da6bd
asarad39 <55108550+asarad39@users.noreply.github.com>
2021-01-11 14:47:51 -0500

    Commented out unecessary code
    The program only prints the encoder value of one motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -41,49 +41,35 @@ public class EncoderTest extends LinearOpMode {
 //                lift.rest();
 //            }
 
-            if (gamepad1.right_trigger > 0) {
-                lift.up(Math.abs(gamepad1.right_trigger));
-            } else if (gamepad1.left_trigger > 0){
-                lift.down(Math.abs(gamepad1.left_trigger));
-            } else {
-                lift.rest();
-            }
+//             if (gamepad1.right_trigger > 0) {
+//                 lift.up(Math.abs(gamepad1.right_trigger));
+//             } else if (gamepad1.left_trigger > 0){
+//                 lift.down(Math.abs(gamepad1.left_trigger));
+//             } else {
+//                 lift.rest();
+//             }
 
-            d.setPower(
-                 gamepad1.left_stick_y,
-                 gamepad1.left_stick_x,
-                 gamepad1.right_stick_x,
-                 gamepad1.right_trigger
-              );
-
-            if (gamepad1.y && !clawButtonIsDown) {
-                clawButtonIsDown = true;
-                claw.nextPos();
-            } else if (!gamepad1.y) {
-                clawButtonIsDown = false;
-            }
-
-            if (gamepad1.x && !gearboxButtonIsDown) {
-                gearboxButtonIsDown = true;
-                gear.nextPos();
-            } else if (!gamepad1.x) {
-                gearboxButtonIsDown = false;
-            }
+//             d.setPower(
+//                  gamepad1.left_stick_y,
+//                  gamepad1.left_stick_x,
+//                  gamepad1.right_stick_x,
+// //                  gamepad1.right_trigger
+//               );
 
 
-            telemetry.addData("Status", "Run Time: ");
-            telemetry.addData("Motor Power", gamepad1.left_stick_y);
-            telemetry.addData("Right Stick Pos", gamepad1.right_stick_y);
-            telemetry.addData("Ly", gamepad1.left_stick_y);
-            telemetry.addData("Lx", gamepad1.left_stick_x);
-            telemetry.addData("Rx", gamepad1.right_stick_x);
+//             telemetry.addData("Status", "Run Time: ");
+//             telemetry.addData("Motor Power", gamepad1.left_stick_y);
+//             telemetry.addData("Right Stick Pos", gamepad1.right_stick_y);
+//             telemetry.addData("Ly", gamepad1.left_stick_y);
+//             telemetry.addData("Lx", gamepad1.left_stick_x);
+//             telemetry.addData("Rx", gamepad1.right_stick_x);
             telemetry.addData("Clicks: ", d.getClickslf());
-            telemetry.addData("lf", d.getPowerlf());
-            telemetry.addData("lb", d.getPowerlb());
-            telemetry.addData("rf", d.getPowerrf());
-            telemetry.addData("rb", d.getPowerrb());
-            telemetry.addData("Lift", lift.getClicks());
-            telemetry.addData("Dist sensor!", sensors.getDistanceFront());
+//             telemetry.addData("lf", d.getPowerlf());
+//             telemetry.addData("lb", d.getPowerlb());
+//             telemetry.addData("rf", d.getPowerrf());
+//             telemetry.addData("rb", d.getPowerrb());
+//             telemetry.addData("Lift", lift.getClicks());
+//             telemetry.addData("Dist sensor!", sensors.getDistanceFront());
             telemetry.update();
 
         }
