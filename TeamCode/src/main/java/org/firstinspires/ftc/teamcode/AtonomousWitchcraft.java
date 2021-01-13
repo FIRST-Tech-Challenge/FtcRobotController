83222c1b4aa392deab6427a06e0f95c067956058
Karine Josien <kjosien1@gmail.com>
2021-01-13 16:12:32 -0700

    teleop work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AtonomousWitchcraft.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous(name="AutoW")
+public class AtonomousWitchcraft extends LinearOpMode {
+
+    DcMotor wobbleGoalExtendMotor = null;
+    DcMotor wobbleGoalRaiseMotor = null;
+    Servo wobbleGoalGrippyThing = null;
+    RobotClass robotClass;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        wobbleGoalExtendMotor = hardwareMap.dcMotor.get("wobbleExtendo");
+        wobbleGoalRaiseMotor = hardwareMap.dcMotor.get("wobbleLift");
+        wobbleGoalGrippyThing = hardwareMap.servo.get("wobbleGrip");
+        robotClass= new RobotClass(hardwareMap, telemetry, this);
+
+        wobbleGoalGrippyThing.setPosition(0.2);
+
+
+        waitForStart();
+
+    }
+}
