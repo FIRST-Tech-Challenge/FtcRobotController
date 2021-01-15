effc74a74bab73653a4891de0d1fee23f3275700
Mason Wu <yutong.w@outlook.com>
2021-01-16 00:59:29 +0800

    add object detection support

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mason_wu/Chassis_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mason_wu/Chassis_Auto.java
@@ -9,19 +9,25 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.ReadWriteFile;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.teamcode.robot_common.Robot4100Common;
 
 import java.io.File;
+import java.util.List;
 import java.util.Locale;
 
 @Autonomous(name="Chassis Autonomous", group="4100")
-public class Chassis_Auto extends LinearOpMode
-{
+public class Chassis_Auto extends LinearOpMode {
     // Declare OpMode members.
     private DcMotor Arm = null;
     private Servo Hand = null;
@@ -37,21 +43,29 @@ public class Chassis_Auto extends LinearOpMode
     final double HAND_CLOSE_POSITION = 0.8;
     final double HAND_OPEN_POSITION = 0.0;
 
+    //Vuforia setup for vision
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+    private static final String VUFORIA_KEY =
+            Robot4100Common.VUFORIA_LICENSE;
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
     @Override
-    public void runOpMode() throws InterruptedException
-    {
+    public void runOpMode() throws InterruptedException {
 
         // Initialize the hardware variables.
         Hand = hardwareMap.get(Servo.class, "hand");
         Arm = hardwareMap.get(DcMotor.class, "arm");
 
         Intake = hardwareMap.get(DcMotor.class, "intake");
-        Spanker = hardwareMap.get(Servo.class,"spanker");
+        Spanker = hardwareMap.get(Servo.class, "spanker");
         Shooter = hardwareMap.get(DcMotor.class, "shooter");
 
-        LF  = hardwareMap.get(DcMotor.class, "LF");
+        LF = hardwareMap.get(DcMotor.class, "LF");
         RF = hardwareMap.get(DcMotor.class, "RF");
-        LB  = hardwareMap.get(DcMotor.class, "LB");
+        LB = hardwareMap.get(DcMotor.class, "LB");
         RB = hardwareMap.get(DcMotor.class, "RB");
 
         LF.setDirection(DcMotor.Direction.FORWARD);
@@ -72,18 +86,26 @@ public class Chassis_Auto extends LinearOpMode
         telemetry.addData("Status", "Initialized");
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.mode                 = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.mode = BNO055IMU.SensorMode.IMU;
         parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
+        parameters.loggingEnabled = true;
 
         imu = hardwareMap.get(BNO055IMU.class, "imu");
 
         imu.initialize(parameters);
         telemetry.addData("Gyro Calibration Status", imu.getCalibrationStatus().toString());
 
-        // composeTelemetry();
+        //Initializing vision
+        initVuforia();
+        initTfod();
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.update();
 
         waitForStart();
 
@@ -97,64 +119,77 @@ public class Chassis_Auto extends LinearOpMode
                                   rotateToAngle (double targetAngle, double margin, double power)
              */
 
-            // code below drives and positions for shooting
-            driveStraight(true,1.0,-0.6,1120);
-            stopMotion(100);
-            rotateToAngle(90.0,1.0,0.2);
-            stopMotion(100);
-            driveStraight(true,1.0,-0.6,420);
-            stopMotion(100);
-            rotateToAngle(0.0,1.0,0.2);
-            stopMotion(100);
-            // need code for shooting here
-            Shooter.setPower(1.0);
-            sleep(1000);
-            for (int i = 0; i < 3; i++) {
-                Spanker.setPosition(0.55);
-                sleep(500);
-                Spanker.setPosition(0.85);
-                sleep(500);
+            //code below is for SINGLE ring scenario
+            if(objectDetection().equals("Single")) {
+                // code below drives and positions for shooting
+                driveStraight(true, 1.0, -0.6, 1120);
+                stopMotion(100);
+                rotateToAngle(90.0, 1.0, 0.2);
+                stopMotion(100);
+                driveStraight(true, 1.0, -0.6, 420);
+                stopMotion(100);
+                rotateToAngle(0.0, 1.0, 0.2);
+                stopMotion(100);
+                // need code for shooting here
+                Shooter.setPower(1.0);
+                sleep(1000);
+                for (int i = 0; i < 3; i++) {
+                    Spanker.setPosition(0.55);
+                    sleep(500);
+                    Spanker.setPosition(0.85);
+                    sleep(500);
+                }
+                Shooter.setPower(0.0);
+                // code below would drive forward to drop wobble
+                driveStraight(true, 1.0, -0.6, 475);
+                stopMotion(100);
+                // drops wobble here
+                armMotion(false, 0.8, 300);
+                sleep(250);
+                handMotion(false);
+                sleep(250);
+                armMotion(true, 0.8, 400);
+                sleep(250);
+                handMotion(true);
+                sleep(250);
+                driveStraight(true, 1.0, 0.6, 250);
             }
-            Shooter.setPower(0.0);
-            // code below would drive forward to drop wobble
-            driveStraight(true,1.0,-0.6,475);
-            stopMotion(100);
-            // drops wobble here
-            armMotion(false, 0.8, 300);
-            sleep(250);
-            handMotion(false);
-            sleep(250);
-            armMotion(true, 0.8, 400);
-            sleep(250);
-            handMotion(true);
-            sleep(250);
-            driveStraight(true,1.0,0.6,250);
+
+            //code below is for QUAD rings scenario
+            else if(objectDetection().equals("Quad")) {
+
+            }
+
+            //code below is for ZERO ring scenario
+            else {
+
+            }
         }
     }
 
-    void stopMotion(){
+    void stopMotion() {
         LF.setPower(0);
         RF.setPower(0);
         LB.setPower(0);
         RB.setPower(0);
     }
 
-    void stopMotion(long timeInterval){
+    void stopMotion(long timeInterval) {
         stopMotion();
         sleep(timeInterval);
     }
 
-    double normalizeAngle(double angle){
+    double normalizeAngle(double angle) {
         double tempDeg = angle % 360;
-        if(tempDeg >= 180){
+        if (tempDeg >= 180) {
             tempDeg -= 360;
-        }else if(tempDeg < -180){
+        } else if (tempDeg < -180) {
             tempDeg += 360;
         }
         return tempDeg;
     }
 
-    double aquireHeading(){
+    double aquireHeading() {
         Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
         double heading = angles.firstAngle;
         double tempHead = normalizeAngle(heading);
@@ -164,16 +199,16 @@ public class Chassis_Auto extends LinearOpMode
         return tempHead;
     }
 
-    void displayVoltageEncoderValue(){
+    void displayVoltageEncoderValue() {
         try {
             double volt = hardwareMap.voltageSensor.get("Motor Controller 1").getVoltage();
             telemetry.addData("Motor Voltage: ", volt);
             telemetry.update();
-        } catch(Exception e){
+        } catch (Exception e) {
             telemetry.addLine("Unable to find voltage value");
             telemetry.update();
         }
-        try{
+        try {
             double LFEnc = LF.getCurrentPosition();
             double RFEnc = RF.getCurrentPosition();
             double LBEnc = LB.getCurrentPosition();
@@ -182,19 +217,19 @@ public class Chassis_Auto extends LinearOpMode
             telemetry.addData("RF Encoder Value: ", RFEnc);
             telemetry.addData("LB Encoder Value: ", LBEnc);
             telemetry.addData("RB Encoder Value: ", RBEnc);
-            telemetry.addData("Average Encoder Value: ", (LFEnc + LBEnc + RFEnc + RBEnc)/4.0);
+            telemetry.addData("Average Encoder Value: ", (LFEnc + LBEnc + RFEnc + RBEnc) / 4.0);
             telemetry.update();
-        } catch(Exception e){
+        } catch (Exception e) {
             telemetry.addLine("Unable to find encoder value");
             telemetry.update();
         }
     }
 
-    void driveStraight (boolean isForward, double margin, double power, double timeInterval) throws InterruptedException{
+    void driveStraight(boolean isForward, double margin, double power, double timeInterval) throws InterruptedException {
         ElapsedTime driveTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         final double currentAngle = aquireHeading();
         int straightFactor = -1;
-        if(isForward){
+        if (isForward) {
             straightFactor = 1;
         }
         double targetAngle = currentAngle;
@@ -202,13 +237,13 @@ public class Chassis_Auto extends LinearOpMode
         double LB_power;
         double RF_power;
         double RB_power;
-        while(driveTime.milliseconds() < timeInterval) {
+        while (driveTime.milliseconds() < timeInterval) {
             double tempAngle = aquireHeading();
             LF_power = straightFactor * power;
             LB_power = straightFactor * power;
             RF_power = straightFactor * power;
             RB_power = straightFactor * power;
-            if (tempAngle < normalizeAngle(targetAngle -1 * margin)) {
+            if (tempAngle < normalizeAngle(targetAngle - 1 * margin)) {
                 RF_power += 0.1;
                 RB_power += 0.1;
                 LF_power -= 0.1;
@@ -238,11 +273,11 @@ public class Chassis_Auto extends LinearOpMode
         displayVoltageEncoderValue();
     }
 
-    void drivePerpendicularly(boolean isLeft, double margin, double power, double timeInterval){
+    void drivePerpendicularly(boolean isLeft, double margin, double power, double timeInterval) {
         ElapsedTime driveTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         final double currentAngle = aquireHeading();
         int perpendicularFactor = -1;
-        if(isLeft){
+        if (isLeft) {
             perpendicularFactor = 1;
         }
         double targetAngle = normalizeAngle(currentAngle + 90 * perpendicularFactor);
@@ -250,7 +285,7 @@ public class Chassis_Auto extends LinearOpMode
         double LB_power;
         double RF_power;
         double RB_power;
-        while(driveTime.milliseconds() < timeInterval) {
+        while (driveTime.milliseconds() < timeInterval) {
             double tempAngle = aquireHeading();
             LF_power = -1 * perpendicularFactor * power;
             LB_power = perpendicularFactor * power;
@@ -287,9 +322,9 @@ public class Chassis_Auto extends LinearOpMode
 
 
     //make a turn that based on the current heading in a certain direction and angle
-    void rotateAtAngle (boolean isClockwise, double degree, double margin, double power){
+    void rotateAtAngle(boolean isClockwise, double degree, double margin, double power) {
         int angleFactor = -1;
-        if(!isClockwise){
+        if (!isClockwise) {
             angleFactor = 1;
         }
         final double currentAngle = aquireHeading();
@@ -300,25 +335,22 @@ public class Chassis_Auto extends LinearOpMode
     }
 
     //make a turn TO a certain angle
-    void rotateToAngle (double targetAngle, double margin, double power){
+    void rotateToAngle(double targetAngle, double margin, double power) {
         int angleFactor = 0;
         final double currentAngle = aquireHeading();
-        if(currentAngle - targetAngle > 0){
-            if(currentAngle - targetAngle < 180){
+        if (currentAngle - targetAngle > 0) {
+            if (currentAngle - targetAngle < 180) {
                 //cw
                 angleFactor = -1;
-            }
-            else {
+            } else {
                 //ccw
                 angleFactor = 1;
             }
-        }
-        else{
-            if(targetAngle - currentAngle < 180){
+        } else {
+            if (targetAngle - currentAngle < 180) {
                 //ccw
                 angleFactor = 1;
-            }
-            else {
+            } else {
                 //cw
                 angleFactor = -1;
             }
@@ -328,7 +360,7 @@ public class Chassis_Auto extends LinearOpMode
         double RF_power;
         double RB_power;
         double tempAngle = currentAngle;
-        while (!((tempAngle < targetAngle + margin)&&(tempAngle > targetAngle - margin))){
+        while (!((tempAngle < targetAngle + margin) && (tempAngle > targetAngle - margin))) {
             tempAngle = aquireHeading();
             RF_power = angleFactor * power;
             RB_power = angleFactor * power;
@@ -351,32 +383,87 @@ public class Chassis_Auto extends LinearOpMode
         stopMotion();
         displayVoltageEncoderValue();
     }
-    boolean armMotion(boolean moveUp, double power, double timeInterval){
+
+    boolean armMotion(boolean moveUp, double power, double timeInterval) {
         ElapsedTime armTime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
         int upFactor = -1;
-        if(!moveUp){
+        if (!moveUp) {
             upFactor = 1;
         }
-        while(armTime.milliseconds() <= timeInterval) {
-            if(armTime.milliseconds()<= timeInterval-300) {
+        while (armTime.milliseconds() <= timeInterval) {
+            if (armTime.milliseconds() <= timeInterval - 300) {
                 power -= upFactor * 0.02;
             }
-            Arm.setPower(Math.abs(power)*(upFactor));
+            Arm.setPower(Math.abs(power) * (upFactor));
             sleep(20);
         }
         Arm.setPower(0);
         return true;
     }
 
-    boolean handMotion(boolean close){
-        if(close) {
+    boolean handMotion(boolean close) {
+        if (close) {
             Hand.setPosition(HAND_CLOSE_POSITION);
-        }
-        else{
+        } else {
             Hand.setPosition(HAND_OPEN_POSITION);
         }
         telemetry.addData("Hand Position:", Hand.getPosition());
         telemetry.update();
         return true;
     }
+
+    //Vision
+    private void initVuforia() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         */
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
+    }
+
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+
+    private String objectDetection() {
+        //Single/Quad
+        if (tfod != null) {
+            // getUpdatedRecognitions() will return null if no new information is available since
+            // the last time that call was made.
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                // step through the list of recognitions and display boundary info.
+                int i = 0;
+                String label = null;
+                for (Recognition recognition : updatedRecognitions) {
+                    label = recognition.getLabel();
+                    telemetry.addData(String.format("label (%d)", i), label);
+                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+                }
+                telemetry.update();
+                return label;
+            }
+        }
+        return null;
+    }
+
 }
