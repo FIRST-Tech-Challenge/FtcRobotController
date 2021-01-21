77101240c978ab1b50dc11cfcedc65715cdc0c55
Nathan_Bosse <MortimerQuinn@gmail.com>
2021-01-20 16:57:27 -0800

    aa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Proto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_Proto.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -9,94 +10,137 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import java.util.List;
-import org.firstinspires.ftc.robotcore.external.ClassFactory;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import java.util.Locale;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
 @Autonomous(name="Autonomous_Proto", group="Autonomous")
-//@Disabled
+@Disabled
 public class Autonomous_Proto extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor lf;
     private DcMotor rf;
     private DcMotor lb;
     private DcMotor rb;
-    private DcMotorEx vt;
-    private Servo atmt;
-    private DcMotor liftL;
-    private DcMotor liftR;
+    private CRServo collector;
+    private Servo pushover;         //empties hopper
+    private DcMotor spindoctor;     //spins wheels
+
+    private ColorSensor CS;         //lower sensor
+    private DistanceSensor DS;      //upper sensor
+    double distance;
+    double color;
+    float hsvValues[] = {0F, 0F, 0F};
+    final float values[] = hsvValues;
+    final double SCALE_FACTOR = 255;
+    int relativeLayoutId = hardwareMap.appContext.getResources().getIdentifier("RelativeLayout", "id", hardwareMap.appContext.getPackageName());
+    final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(relativeLayoutId);
+
+
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Ready to run");
         telemetry.update();
 
         lf  = hardwareMap.get(DcMotor.class, "lf");
-        lf  = hardwareMap.get(DcMotor.class, "LF");
         rf = hardwareMap.get(DcMotor.class, "rf");
         lb  = hardwareMap.get(DcMotor.class, "lb");
         rb = hardwareMap.get(DcMotor.class, "rb");
-        vt = hardwareMap.get(DcMotorEx.class, "vt");
-        liftL = hardwareMap.get(DcMotorEx.class, "liftl");
-        liftR = hardwareMap.get(DcMotorEx.class, "liftr");
-        atmt = hardwareMap.get(Servo.class, "atmt");
+        collector = hardwareMap.get(CRServo.class, "collector");
+        spindoctor = hardwareMap.get(DcMotorEx.class, "spindoctor");
+        pushover = hardwareMap.get(Servo.class, "pushover");
+        CS = hardwareMap.get(ColorSensor.class, "cs");
+        DS = hardwareMap.get(DistanceSensor.class, "ds");
+
         lf.setDirection(DcMotor.Direction.FORWARD);
         rf.setDirection(DcMotor.Direction.REVERSE);
         lb.setDirection(DcMotor.Direction.FORWARD);
         rb.setDirection(DcMotor.Direction.FORWARD);
-        vt.setDirection(DcMotor.Direction.FORWARD);
-        liftL.setDirection(DcMotor.Direction.FORWARD);
-        liftR.setDirection(DcMotor.Direction.REVERSE);
-        //initVuforia();
-        /*if (ClassFactory.getInstance().canCreateTFObjectDetector()) {
-            initTfod();
-        } else {
-            telemetry.addData("Sorry!", "This device is not compatible with TFOD");
-        }
-        if (tfod != null) {
-            tfod.activate();
-        }*/
+        collector.setDirection(CRServo.Direction.FORWARD);
+        spindoctor.setDirection(DcMotor.Direction.FORWARD);
+
         waitForStart();
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 30.0)) {
 
-            liftL.setPower(0);
-            liftR.setPower(0);
-            lf.setPower(0.5);
-            rf.setPower(0.5);
-            lb.setPower(0.5);
-            rb.setPower(0.5);
-            vt.setPower(0);
-            sleep(1350);
+            if (color >= 0 && distance <= 0){
+
+
+                // Autonomous A: four rings
+                spindoctor.setPower(0);
+                collector.setPower(0);
+                lf.setPower(0);
+                rf.setPower(0);
+                lb.setPower(0);
+                rb.setPower(0);
+                pushover.setPosition(0);
+                sleep(1000);
+
+            }else {
+
+                // check for Autonomous B
+                spindoctor.setPower(0);
+                collector.setPower(0);
+                lf.setPower(0);
+                rf.setPower(0);
+                lb.setPower(0);
+                rb.setPower(0);
+                pushover.setPosition(0);
+                sleep(1000);
+
+                if (color >= 0 && distance > 0){
+
+                    // Autonomous B: one ring
+                    spindoctor.setPower(0);
+                    collector.setPower(0);
+                    lf.setPower(0);
+                    rf.setPower(0);
+                    lb.setPower(0);
+                    rb.setPower(0);
+                    pushover.setPosition(0);
+                    sleep(1000);
+
+                }else {
+
+                    //Autonomous C: no rings
+                    spindoctor.setPower(0);
+                    collector.setPower(0);
+                    lf.setPower(0);
+                    rf.setPower(0);
+                    lb.setPower(0);
+                    rb.setPower(0);
+                    pushover.setPosition(0);
+                    sleep(1000);
 
                 }
             }
-    /*private void initVuforia() {
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
-        parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
-        vuforia = ClassFactory.getInstance().createVuforia(parameters);
-    }
-    private void initTfod() {
-        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
-        tfodParameters.minimumConfidence = 0.8;
-        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
-        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
-    }
-    private boolean checkBlock() {
-        if (tfod != null) {
-            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-            if (updatedRecognitions != null) {
-                for (Recognition recognition : updatedRecognitions) {
-                    if (recognition.getLabel() == "Skystone") {
-                        return true;
-                    }
+            //function  VVV
+            /*
+            Color.RGBToHSV((int) (sensorColor.red() * SCALE_FACTOR),
+                    (int) (sensorColor.green() * SCALE_FACTOR),
+                    (int) (sensorColor.blue() * SCALE_FACTOR),
+                    hsvValues);
+            telemetry.addData("Distance (cm)",
+            String.format(Locale.US, "%.02f", sensorDistance.getDistance(DistanceUnit.CM)));
+            telemetry.addData("Alpha", sensorColor.alpha());
+            telemetry.addData("Red  ", sensorColor.red());
+            telemetry.addData("Green", sensorColor.green());
+            telemetry.addData("Blue ", sensorColor.blue());
+            telemetry.addData("Hue", hsvValues[0]);
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
                 }
-            }
+            });
+
+            telemetry.update();
+             */
+
         }
-        return false;
-    }*/
+    }
 }
