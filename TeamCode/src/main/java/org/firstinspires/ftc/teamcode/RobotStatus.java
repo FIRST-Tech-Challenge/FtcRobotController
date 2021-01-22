f753b8af598f564325d155170e7eb35fea58f530
jstapleton463 <42473264+jstapleton463@users.noreply.github.com>
2021-01-22 15:48:05 -0500

    Update RobotStatus.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStatus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotStatus.java
@@ -1,1 +1,27 @@
-
+public class RobotStatus {
+  
+  float x;
+  float y;
+  float heading;
+  
+  public static RobotStatus(float x, float y, float heading) { // add args as needed
+    this.x = x;
+    this.y = y;
+    this.heading = heading;
+  }
+  
+  public float getX() {
+    return this.x;
+  }
+  
+  public float getY() {
+    return this.y;
+  }
+  
+  public float getHeading() {
+    return this.heading;
+  }
+  
+  // add methods as needed
+  
+}
