791435b4cb38d27e3106ca1e4f8002235a1eac40
Sai Divyesh Tunguturu <spartansai05@gmail.com>
2021-01-25 20:25:46 -0800

    I updated the test program. I added the shooter and shooter-servo. I also added the wobble goal arm. However, whenever I try running the wobbleGoalServoClaw, it does not move at all. I will add the WobbleGoalServo once it is connected.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/WalrusTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/WalrusTest.java
@@ -7,7 +7,8 @@
  * @status finished
  */
 
-//TODO Sai: add other motors and servos once they are connected
+//TODO Sai: fix the wobbleGoalServoClaw (it is not moving right now)
+//TODO Sai: add the wobbleGoalServo once it is added onto the robot
 
 package org.firstinspires.ftc.teamcode.Autonomous.Tests;
 
@@ -15,6 +16,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -31,6 +33,10 @@ public class WalrusTest extends LinearOpMode {
         DcMotorEx motorRightBack;
         DcMotorEx intakeMotor;
         DcMotorEx shooterMotor;
+        Servo shooter_Servo;
+        DcMotorEx wobbleGoalMotor;
+        Servo wobbleGoalServo;
+        Servo wobbleGoalServoClaw;
 
 
         motorLeftFront = (DcMotorEx) hardwareMap.dcMotor.get("motorLeftFront");
@@ -39,6 +45,10 @@ public class WalrusTest extends LinearOpMode {
         motorRightBack = (DcMotorEx) hardwareMap.dcMotor.get("motorRightBack");
         intakeMotor = (DcMotorEx) hardwareMap.dcMotor.get("IntakeMotor");
         shooterMotor = (DcMotorEx) hardwareMap.dcMotor.get("ShooterMotor");
+        wobbleGoalMotor = (DcMotorEx) hardwareMap.dcMotor.get("wobbleGoalMotor");
+        shooter_Servo = (Servo) hardwareMap.servo.get("ShooterServo");
+        wobbleGoalServo = (Servo) hardwareMap.servo.get("WobbleGoalServo");
+        wobbleGoalServoClaw = (Servo) hardwareMap.servo.get("wobbleGoalServoClaw");
 
 
         // All Motors
@@ -48,6 +58,12 @@ public class WalrusTest extends LinearOpMode {
         motorRightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         shooterMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        wobbleGoalMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Servos
+        shooter_Servo.setPosition(-0.5);
+        wobbleGoalServo.setPosition(0);
+        wobbleGoalServoClaw.setPosition(0);
 
         // To match default
         motorLeftFront.setDirection(DcMotor.Direction.REVERSE);
@@ -56,14 +72,16 @@ public class WalrusTest extends LinearOpMode {
         motorRightBack.setDirection(DcMotor.Direction.FORWARD);
         intakeMotor.setDirection(DcMotor.Direction.FORWARD);
         shooterMotor.setDirection(DcMotor.Direction.FORWARD);
+        wobbleGoalMotor.setDirection(DcMotorSimple.Direction.FORWARD);
 
-        // reset encoder count kept by the motors.
+        // make the motors run on only power
         motorLeftBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorLeftFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorRightFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motorRightBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         shooterMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        wobbleGoalMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         ElapsedTime runtime = new ElapsedTime();
         //Robot robot = new Robot(this);
@@ -134,18 +152,61 @@ public class WalrusTest extends LinearOpMode {
         sleep(1000);
         intakeMotor.setPower(0);
 
-        //Tests shooter motor but the motor has not been connected, so it is commented
-//        shooterMotor.setPower(0.8);
-//        telemetry.addData("Moving shooterMotor forwards", 0.8);
-//        telemetry.update();
-//        sleep(700);
-//        shooterMotor.setPower(0);
+        //Tests shooter motor
+        shooterMotor.setPower(0.8);
+        telemetry.addData("Moving shooterMotor forwards", 0.8);
+        telemetry.update();
+        sleep(700);
+        shooterMotor.setPower(0);
+        sleep(1000);
+        shooterMotor.setPower(-0.8);
+        telemetry.addData("Moving shooterMotor backwards", -0.8);
+        telemetry.update();
+        sleep(700);
+        shooterMotor.setPower(0);
+        sleep(1000);
+
+        //Tests Shooter Servo
+        shooter_Servo.setPosition(0.5);
+        telemetry.addData("Moving Shooter Servo", 0.5);
+        telemetry.update();
+        sleep(1000);
+        shooter_Servo.setPosition(0);
+        sleep(2000);
+        shooter_Servo.setPosition(-0.5);
+        telemetry.addData("Moving Shooter Servo", -0.5);
+        telemetry.update();
+        sleep(1000);
+        shooter_Servo.setPosition(0);
+
+        //Moving wobbleGoalMotor
+        wobbleGoalMotor.setPower(0.25);
+        telemetry.addData("Moving wobbleGoalMotor", 0.25);
+        telemetry.update();
+        sleep(750);
+        wobbleGoalMotor.setPower(0);
+        sleep(1000);
+        wobbleGoalMotor.setPower(-0.3);
+        telemetry.addData("Moving wobbleGoalMotor", -0.25);
+        telemetry.update();
+        sleep(750);
+        wobbleGoalMotor.setPower(0);
+        sleep(1500);
+
+        //Wobble Goal Servo Claw
+        wobbleGoalServoClaw.setPosition(1);
+        telemetry.addData("Moving wobbleGoalServoClaw", 1);
+        telemetry.update();
+        sleep(1000);
+        wobbleGoalServoClaw.setPosition(0);
+        telemetry.addData("Moving wobbleGoalServoClaw", 0);
+        telemetry.update();
+        sleep(1500);
+
+        //Wobble Goal Servo
+//        wobbleGoalServo.setPosition(1);
 //        sleep(1000);
-//        shooterMotor.setPower(-0.8);
-//        telemetry.addData("Moving shooterMotor backwards", -0.8);
-//        telemetry.update();
-//        sleep(700);
-//        shooterMotor.setPower(0);
-//        sleep(1000);
+//        wobbleGoalServo.setPosition(0);
+//        sleep(2000);
     }
 }
