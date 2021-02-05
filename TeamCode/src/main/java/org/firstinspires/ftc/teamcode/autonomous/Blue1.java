c25cd477e36000e35827ee4250cd0ca0cf3a553b
PriyaSoneji <priyas.fcs@gmail.com>
2021-02-05 17:47:57 -0500

    blank blue autos

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Blue1.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Blue-1-3Peg", group ="playback")
+public class Blue1 extends AutoReplayBase {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Blue2.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Blue-2-RegFull", group ="playback")
+public class Blue2 extends AutoReplayBase {
+
+}
