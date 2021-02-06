8ecae00d39d18b70bddcbc741648ad774b5b07da
FTC-9929 <tnt9929@hfrobots.com>
2021-02-06 12:03:36 -0600

    Enabling the blinkinled sample

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SampleRevBlinkinLedDriver.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SampleRevBlinkinLedDriver.java
@@ -49,8 +49,7 @@ import java.util.concurrent.TimeUnit;
  *
  * Displays the first pattern upon init.
  */
-@TeleOp(name="BlinkinExample")
-@Disabled
+@TeleOp(name="BlinkinExample", group="Utilities")
 public class SampleRevBlinkinLedDriver extends OpMode {
 
     /*
