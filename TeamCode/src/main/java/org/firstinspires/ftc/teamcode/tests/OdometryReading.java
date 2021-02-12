d89f1887d0ac923c1a37a3a81cfdd12953351da7
Mason Wu <yutong.w@outlook.com>
2021-02-12 22:31:52 +0800

    Update OdometryReading.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/OdometryReading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/OdometryReading.java
@@ -2,13 +2,14 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 
-@Autonomous(name="Odometry Reading Test", group="tests")
+@TeleOp(name="Odometry Reading Test", group="tests")
 public class OdometryReading extends LinearOpMode{
 
     //for two odometry wheels setup
