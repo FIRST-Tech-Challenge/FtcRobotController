e15d7a51ffe980a028a2c2582399b6229264bafb
rodoggyro <47298960+rodoggyro@users.noreply.github.com>
2021-02-13 11:24:36 -0500

    Adds robot movement code, and shooting motor code.
    This now adds code to move the robot and shoot disks at a consistant height.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -1,72 +1,108 @@
-//backRight and frontLeft are reversed
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
-public class TestOpMode extends LinearOpMode{
+public class TestOpMode extends LinearOpMode {
+
+    private Gyroscope imu;
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private DcMotor backLeft;
-    private DcMotor backRight;
-    /*private DcMotor rampLeft;
-    private DcMotor rampRight;*/
+    private DcMotor BackLeft;
+    private DcMotor BackRight;
+    //private DcMotor loading;
+    //private DcMotor shoot;
 
-    public void runOpMode(){
+
+
+    @Override
+    public void runOpMode() {
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        BackLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        BackRight = hardwareMap.get(DcMotor.class, "backRight");
+        //loading = hardwareMap.get(DcMotor.class, "load");
+        //shoot = hardwareMap.get(DcMotor.class, "shoot");
 
-
-
-
-        /*rampLeft = hardwareMap.get(DcMotor.class, "rampLeft");
-        rampRight = hardwareMap.get(DcMotor.class, "rampRight");*/
-
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        // run until the end of the match (driver presses STOP)
         double throttle = 0;
-        double turn = 0;
+        double TurnL = 0;
+        double TurnR = 0;
+        boolean leftStrafe;
+        boolean rightStrafe;
+        boolean loader;
+        boolean shooter;
 
-        telemetry.addData("throttle", throttle);
-
-
-        backRight.setPower(0);
-        backLeft.setPower(0);
+        BackRight.setPower(0);
+        BackLeft.setPower(0);
         frontRight.setPower(0);
         frontLeft.setPower(0);
+        //loading.setPower(0);
+        sleep(1000);
 
-        //rampLeft.setPower(0);
-        //rampRight.setPower(0);
+        while (opModeIsActive()) {
+            throttle = this.gamepad1.left_stick_x;
+            TurnL = this.gamepad1.right_stick_y;
+            TurnR = -this.gamepad1.right_stick_y;
+            leftStrafe = this.gamepad1.left_bumper;
+            rightStrafe = this.gamepad1.right_bumper;
+            loader = this.gamepad1.a;
+            shooter = this.gamepad1.b;
 
-        while (opModeIsActive()){
-            throttle = this.gamepad1.left_stick_y;
-            turn = this.gamepad1.right_stick_x;
+            /*if (shooter) {
+                shoot.setPower(-1);
+            }else{
+                shoot.setPower(0);
+            }*/
 
+            /*if (loader) {
+                loading.setPower(.75);
+                sleep(1000);
+                loading.setPower(1);
+            }else {
+                loading.setPower(0);
+            }*/
 
+            BackLeft.setPower(throttle);
+            BackRight.setPower(.75*throttle);
+            frontLeft.setPower(.75*throttle);
+            frontRight.setPower(.75*throttle);
 
-            /*rampLeft.setPower(throttle);
-            rampRight.setPower(throttle);*/
+            frontLeft.setPower(TurnL);
+            frontRight.setPower(TurnR);
+            BackRight.setPower(-TurnR);
+            BackLeft.setPower(-TurnL);
 
-            frontLeft.setPower(throttle);
-            frontRight.setPower(-throttle);
-            backLeft.setPower(throttle);
-            backRight.setPower(-throttle);
+//            armdrive.setPower(arm/2);
 
-            frontLeft.setPower(-turn);
-            frontRight.setPower(turn);
-            backLeft.setPower(-turn);
-            backRight.setPower(turn);
+            if (leftStrafe){
+                frontLeft.setPower(.5);
+                frontRight.setPower(.5);
+                BackLeft.setPower(-1);
+                BackRight.setPower(-1);
+            }
+
+            if (rightStrafe) {
+                frontLeft.setPower(-.5);
+                frontRight.setPower(-.5);
+                BackLeft.setPower(1);
+                BackRight.setPower(1);
+            }
+
+            telemetry.addData("Target Power", throttle);
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
         }
     }
 }
