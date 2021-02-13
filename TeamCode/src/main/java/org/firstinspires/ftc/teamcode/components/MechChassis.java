babf9fc39af6495af303edc3759b223095618aec
sasha <fefel05714@loswego.k12.or.us>
2021-02-12 21:17:09 -0800

    make voltage correction stronger

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MechChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/MechChassis.java
@@ -184,7 +184,7 @@ public class MechChassis extends Logger<MechChassis> implements Configurable {
         if (volt<=13.0) {
             auto_power_scale_by_voltage = 1.0;
         } else {
-            auto_power_scale_by_voltage = 13.0/Math.pow(volt, 1.1);
+            auto_power_scale_by_voltage = 13.0/Math.pow(volt, 1.05);
         }
     }
     public void switchAutoMode() {
