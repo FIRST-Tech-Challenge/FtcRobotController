2547a2484323eb6bf7190da275ba3f977d50aa59
Ollie <ollied370@gmail.com>
2021-02-22 20:48:52 -0700

    A certain Oliver forgot to commit right after he made changes, and therefore forgot what they were. Whoops.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OuterRedAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OuterRedAutonomous.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -9,23 +8,21 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import java.util.Timer;
-
-@Autonomous(name = "OuterBlueAutonomous")
-public class OuterBlueAutonomous extends LinearOpMode{
+@Autonomous(name = "OuterRedAutonomous")
+public class OuterRedAutonomous extends LinearOpMode {
 
     RobotClass robot;
 
-
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
 
         robot= new RobotClass(hardwareMap, telemetry, this);
 
-        robot.wobbleGoalGrippyThingGrab();
         waitForStart();
 
-        robot.forward(.8,-2);
+        robot.strafeLeft(.5,.2);
+        robot.forward(.8,-2.2);
+        robot.pivotLeft(.5,27);
         robot.shooterEngage(4000);
         robot.pause(500);
         robot.shooterServo1(.8);
@@ -33,24 +30,10 @@ public class OuterBlueAutonomous extends LinearOpMode{
         robot.intakeServoEngage(.9);
         robot.pause(4500);
         robot.shooterStop();
-        robot.shooterServo1Stop();
-        robot.shooterServo2Stop();
-        robot.intakeServoStop();
-        robot.forward(.8,-1);
-        robot.strafeLeft(.6,2.1);
-        robot.forward(.6,-1.5);
+        robot.pivotRight(.5, 27);
+        robot.forward(.8,-1.8);
+        robot.strafeRight(.6,2.1);
+        robot.forward(.6,-1.2);
 
-
-
-
-
-//        robot.forward(.8,-6);
-//        robot.pivotRight(.4,90);
-//        robot.pivotRight(.4,90);
-//        robot.moveWobbleGoalArm(.3,.5);
-//        robot.wobbleGoalGrippyThingRelease();
-//
-        // I loathe you all.
-        //Ég óska ​​þess að þessi heimur endi í eldi.
     }
 }
