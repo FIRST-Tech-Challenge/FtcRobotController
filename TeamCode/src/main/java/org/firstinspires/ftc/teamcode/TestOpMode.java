7768909bf1abdd8fe5f418000bc7438c3d132c0a
rodoggyro <47298960+rodoggyro@users.noreply.github.com>
2021-02-26 14:34:20 -0500

    Adds robot movement code, shooting loader code, and shooting motor code.
    This now adds code to move the robot and shoot disks at a constant height. This also has code to load the shooting mechanism.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOpMode.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 
@@ -15,8 +16,10 @@ public class TestOpMode extends LinearOpMode {
     private DcMotor frontRight;
     private DcMotor BackLeft;
     private DcMotor BackRight;
-    //private DcMotor loading;
-    //private DcMotor shoot;
+    private DcMotor loading;
+    private DcMotor shoot;
+    private DcMotor ramp;
+    private DcMotor belt;
 
 
 
@@ -26,8 +29,16 @@ public class TestOpMode extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         BackLeft = hardwareMap.get(DcMotor.class, "backLeft");
         BackRight = hardwareMap.get(DcMotor.class, "backRight");
-        //loading = hardwareMap.get(DcMotor.class, "load");
-        //shoot = hardwareMap.get(DcMotor.class, "shoot");
+        loading = hardwareMap.get(DcMotor.class, "load");
+        shoot = hardwareMap.get(DcMotor.class, "shoot");
+        ramp = hardwareMap.get(DcMotor.class, "ramp");
+        belt = hardwareMap.get(DcMotor.class, "belt");
+
+
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        BackLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        BackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -35,13 +46,15 @@ public class TestOpMode extends LinearOpMode {
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-        double throttle = 0;
-        double TurnL = 0;
-        double TurnR = 0;
+        double turn = 0;
+        double throttleL = 0;
+        double throttleR = 0;
         boolean leftStrafe;
         boolean rightStrafe;
         boolean loader;
         boolean shooter;
+        boolean AngleOfAttackUp;
+        boolean AngleOfAttackDown;
 
         BackRight.setPower(0);
         BackLeft.setPower(0);
@@ -51,39 +64,43 @@ public class TestOpMode extends LinearOpMode {
         sleep(1000);
 
         while (opModeIsActive()) {
-            throttle = this.gamepad1.left_stick_x;
-            TurnL = this.gamepad1.right_stick_y;
-            TurnR = -this.gamepad1.right_stick_y;
+            turn = this.gamepad1.left_stick_x;
+            throttleL = this.gamepad1.right_stick_y;
+            throttleR = -this.gamepad1.right_stick_y;
             leftStrafe = this.gamepad1.left_bumper;
             rightStrafe = this.gamepad1.right_bumper;
             loader = this.gamepad1.a;
             shooter = this.gamepad1.b;
+            AngleOfAttackUp = this.gamepad1.dpad_up;
+            AngleOfAttackDown = this.gamepad1.dpad_down;
 
-            /*if (shooter) {
-                shoot.setPower(-1);
+            if (shooter) {
+                shoot.setPower(1);
             }else{
                 shoot.setPower(0);
-            }*/
+            }
 
-            /*if (loader) {
+            if (AngleOfAttackUp){
+                ramp.setPower(.5);
+            }else if (AngleOfAttackDown){
+                ramp.setPower(-.25);
+            }else{
+                ramp.setPower(0);
+            }
+
+            if (loader) {
                 loading.setPower(.75);
                 sleep(1000);
                 loading.setPower(1);
             }else {
                 loading.setPower(0);
-            }*/
+            }
 
-            BackLeft.setPower(throttle);
-            BackRight.setPower(.75*throttle);
-            frontLeft.setPower(.75*throttle);
-            frontRight.setPower(.75*throttle);
+            frontLeft.setPower(throttleL);
+            frontRight.setPower(throttleR);
+            BackRight.setPower(throttleR);
+            BackLeft.setPower(throttleL);
 
-            frontLeft.setPower(TurnL);
-            frontRight.setPower(TurnR);
-            BackRight.setPower(-TurnR);
-            BackLeft.setPower(-TurnL);
-
-//            armdrive.setPower(arm/2);
 
             if (leftStrafe){
                 frontLeft.setPower(.5);
@@ -99,7 +116,6 @@ public class TestOpMode extends LinearOpMode {
                 BackRight.setPower(1);
             }
 
-            telemetry.addData("Target Power", throttle);
             telemetry.addData("Status", "Running");
             telemetry.update();
 
