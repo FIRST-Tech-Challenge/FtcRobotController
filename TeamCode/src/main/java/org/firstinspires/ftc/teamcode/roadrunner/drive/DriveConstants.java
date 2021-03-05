551cec12f2a4cc947e38fa2af9108549f5400364
Mason Wu <yutong.w@outlook.com>
2021-03-05 19:52:53 +0800

    Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -33,8 +33,8 @@ public class DriveConstants {
      */
     public static final boolean RUN_USING_ENCODER = true;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            16.88);
-    //f=getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
+            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -65,7 +65,7 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 92.375;
+    public static double MAX_VEL = 92.375; //You can adjust the max velocity value here
     public static double MAX_ACCEL = 30;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
