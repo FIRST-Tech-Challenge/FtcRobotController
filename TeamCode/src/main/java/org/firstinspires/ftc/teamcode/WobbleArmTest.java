ee112a6ae074249cb95fe29e0dde29f3add3af7a
Dodesimo <devammondal06@gmail.com>
2021-03-07 00:05:50 -0500

    Wobble Arm open-up initialized.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleArmTest.java
@@ -1,46 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robot.FlyWheel;
-import org.firstinspires.ftc.robot_utilities.GamePadController;
-import org.firstinspires.ftc.robot.Hitter;
 
 @Config
 class WobbleArmServoVals {
     public static double position = 0;
 }
 
-@TeleOp (name = "WobbleArmServoTest")
-public class WobbleArmTest extends LinearOpMode{
+@TeleOp(name = "WobbleArmServoTest")
+public class WobbleArmTest extends OpMode {
 
-    private Servo wobbleArmServo;
     double servoPosition = 0.0;
+    private Servo wobbleArmServo;
 
     @Override
-    public void runOpMode() throws InterruptedException {
-
+    public void init() {
         wobbleArmServo = hardwareMap.servo.get("wobbleArmServo");
-        wobbleArmServo.setPosition(servoPosition);
-
-        waitForStart();
-
-        servoPosition = 0.5;
-        wobbleArmServo.setPosition(servoPosition);
-        sleep(2000);
-
-        servoPosition = 1.0;
-        wobbleArmServo.setPosition(servoPosition);
-        sleep(2000);
-
 
     }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.a) {
+
+            servoPosition = 1.0;
+
+        } else {
+
+            servoPosition = 0.0;
+        }
+
+        wobbleArmServo.setPosition(servoPosition);
+    }
 }
+
+
 
