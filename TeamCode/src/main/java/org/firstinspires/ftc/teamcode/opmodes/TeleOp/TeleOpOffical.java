7fc3d5e1b12fa9091d59a2b2fa0f3d354caac707
matthewself12 <matthewself12@users.noreply.github.com>
2021-03-10 13:24:46 -0500

    Update TeleOpOffical.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp/TeleOpOffical.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp/TeleOpOffical.java
@@ -32,32 +32,40 @@ public class TeleOpOffical extends UpliftTele {
     public void initHardware() {
 
         robot = new UpliftRobot(this);
-        odom = robot.odometry;
-        drive = new DriveCommands(this, robot, robot.driveSub);
-        transfer = new TransferCommands(this, robot, robot.transferSub);
-        flicker = new FlickerCommands(this, robot, robot.flickerSub);
-        intake = new IntakeCommands(this, robot, robot.intakeSub);
-        shooter = new ShooterCommands(this, robot, robot.shooterSub);
-        intake = new IntakeCommands(this, robot, robot.intakeSub);
-        wobble = new WobbleCommands(this, robot, robot.wobbleSub);
+        if(robot.driveInitialized) {
+            odom = robot.odometry;
+            drive = new DriveCommands(this, robot, robot.driveSub);
+            drive.enable();
+        }
+        if(robot.transferInitialized) {
+            transfer = new TransferCommands(this, robot, robot.transferSub);
+            transfer.enable();
+        }
+        if(robot.flickerInitialized) {
+            flicker = new FlickerCommands(this, robot, robot.flickerSub);
+            flicker.enable();
+        }
+        if(robot.intakeInitialized) {
+            intake = new IntakeCommands(this, robot, robot.intakeSub);
+            intake.enable();
+        }
+        if(robot.shooterInitialized) {
+            shooter = new ShooterCommands(this, robot, robot.shooterSub);
+            shooter.enable();
+        }
+        if(robot.wobbleInitialized) {
+            wobble = new WobbleCommands(this, robot, robot.wobbleSub);
+            wobble.enable();
+        }
 
     }
 
     @Override
     public void initAction() {
 
-        drive.enable();
-        transfer.enable();
-        flicker.enable();
-        intake.enable();
-        shooter.enable();
-        wobble.enable();
-
         robot.readPositionFiles();
-        wobble.wobble.openWobble();
 
-        robot.shooter1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(5, 0, 0, 25));
-        robot.shooter2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(5, 0, 0, 25));
+        shooter.shooter.setShooterPIDF(5, 0, 0, 25);
 
     }
 
