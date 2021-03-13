b15be480d65ae1ead25ff55a31302c87f1577b74
awesomeaamod <awesome.aamod@gmail.com>
2021-03-12 16:15:03 -0800

    worked on vuforia

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/VuforiaTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/VuforiaTest.java
@@ -37,43 +37,16 @@ public class VuforiaTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             if(Math.sqrt(Math.pow(VuforiaWebcam.getVuforiaX(), 2) + Math.pow(VuforiaWebcam.getVuforiaY(), 2))>=24.5 && VuforiaWebcam.isTargetVisible()==true){
-                telemetry.addData("goodDistance", Math.hypot(VuforiaWebcam.getVuforiaX(),VuforiaWebcam.getVuforiaY()));
+                telemetry.addData("ok dist", Math.hypot(VuforiaWebcam.getVuforiaX(),VuforiaWebcam.getVuforiaY()));
+                telemetry.update();
             }
 
             if(Math.sqrt(Math.pow(VuforiaWebcam.getVuforiaX(), 2) + Math.pow(VuforiaWebcam.getVuforiaY(), 2))<24.5 && VuforiaWebcam.isTargetVisible()==true){
                 telemetry.addData("Too far", Math.hypot(VuforiaWebcam.getVuforiaX(),VuforiaWebcam.getVuforiaY()));
+                telemetry.update();
             }
 
-            //Webcam
-
-//            while (opModeIsActive()) {
-//
-//                //Webcam
-//
-//                double x = vuforiaWebcam.getVuforiaX();
-//                double y = vuforiaWebcam.getVuforiaY();
-//                double angle = vuforiaWebcam.getVuforiaAngle();
-//
-//                telemetry.addData("Back X", x);
-//                telemetry.addData("Back Y", y);
-//                telemetry.addData("Back Angle", "%.2f, %.2f", angle, angle + 90);
-//                telemetry.addData("Back Target", vuforiaWebcam.getVuforiaTrackable());
-//                telemetry.update();
-//
-//                //Back
-//                telemetry.addData("Back X", vuforiaB.getVuforiaX());
-//                telemetry.addData("Back Y", vuforiaB.getVuforiaY());
-//                telemetry.addData("Back Angle", vuforiaB.getVuforiaAngle());
-//                telemetry.addData("Back Target", vuforiaB.getVuforiaTrackable());
-//                sleep(500);
-//                //Front
-//                telemetry.addData("Front X", vuforiaF.getVuforiaX());
-//                telemetry.addData("Front Y", vuforiaF.getVuforiaY());
-//                telemetry.addData("Front Angle", vuforiaF.getVuforiaAngle());
-//                telemetry.addData("Front Target", vuforiaF.getVuforiaTrackable());
-//                telemetry.update();
-//            }
-//            vuforiaWebcam.interrupt();
         }
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Navigations/VuforiaWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Navigations/VuforiaWebcam.java
@@ -35,6 +35,7 @@ import org.firstinspires.ftc.teamcode.key;
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesReference.EXTRINSIC;
+import static org.firstinspires.ftc.teamcode.Components.OdometryChassis.*;
 
 public class VuforiaWebcam extends Thread {
     private OpMode op;
@@ -156,19 +157,23 @@ public class VuforiaWebcam extends Thread {
             if (targetVisible) {
                 //setInVuforia(true);
                 VectorF translation = lastLocation.getTranslation();
-                op.telemetry.addData("Pos (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f, %.1f, %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, OdometryChassis.getCurrentAngle(), OdometryChassis.getXpos(), OdometryChassis.getYpos());
+//                op.telemetry.addData("Pos (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch);
+//                op.telemetry.addData("Pos (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f, %.1f, %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, getCurrentAngle(), getXpos(), getYpos());
                 Orientation rotation = Orientation.getOrientation(lastLocation, EXTRINSIC, XYZ, DEGREES);
-                if(Math.sqrt(Math.pow(VuforiaWebcam.getVuforiaX(), 2) + Math.pow(VuforiaWebcam.getVuforiaY(), 2))<=46.5 && VuforiaWebcam.isTargetVisible()==true) {
+                if(Math.sqrt(Math.pow(VuforiaWebcam.getVuforiaX(), 2) + Math.pow(VuforiaWebcam.getVuforiaY(), 2))>=24.5 && VuforiaWebcam.isTargetVisible()==true) {
+//                    setXposition(translation.get(0) / mmPerInch);
+//                    setYposition((translation.get(1) / mmPerInch));
+//                    op.telemetry.addData("PosIf (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f, %.1f, %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, getAngle(), getXposition(), getYposition());
                     OdometryChassis.setXpos(translation.get(0) / mmPerInch);
                     OdometryChassis.setYpos((translation.get(1) / mmPerInch));
-                    op.telemetry.addData("PosIf (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f, %.1f, %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, OdometryChassis.getCurrentAngle(), OdometryChassis.getXpos(), OdometryChassis.getYpos());
+//                    op.telemetry.addData("PosIf (in)", "{X, Y, Angle, getX, getY} = %.1f, %.1f, %.1f, %.1f, %.1f",translation.get(0) / mmPerInch, translation.get(1) / mmPerInch, getCurrentAngle(), getXpos(), getYpos());
 
 //                    op.telemetry.addData("OVERWRITING...", null);
 //                    op.telemetry.update();
                 }
-//                xpos = translation.get(0) / mmPerInch;
-//                ypos = translation.get(1) / mmPerInch;
-//                angle = rotation.thirdAngle;
+                xpos = translation.get(0) / mmPerInch;
+                ypos = translation.get(1) / mmPerInch;
+
                 //setInVuforia(false);
                 //op.telemetry.addData("Rot (deg)", "{Roll, Pitch, Heading} = %.0f, %.0f, %.0f", rotation.firstAngle, rotation.secondAngle, rotation.thirdAngle);
             }
