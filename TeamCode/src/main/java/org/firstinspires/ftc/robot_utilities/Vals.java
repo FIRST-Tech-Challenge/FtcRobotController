aa32060b350b727a6f071b31e5dbe14a4af3803d
Ishan Arya <ishan.arya130@gmail.com>
2021-03-17 18:27:12 -0400

    added wobble arm stuff and position tracking

--- TeamCode/src/main/java/org/firstinspires/ftc/robot_utilities/Vals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/robot_utilities/Vals.java
@@ -4,18 +4,18 @@ import com.acmerobotics.dashboard.config.Config;
 
 @Config
 public class Vals {
-    public static double flywheel_kp = 0.053; //40
-    public static double flywheel_ki = 0.1;
+    public static double flywheel_kp = 0.06; //40
+    public static double flywheel_ki = 0.3;
     public static double flywheel_kd = 0.007;
     public static double flywheel_ks = 0;
     public static double flywheel_kv = 0.03;
     public static double flywheel_ff = 0.00827;
     public static double flywheel_tolerance = 1;
-    public static double flywheel_speed = 1125;
+    public static double flywheel_speed = 1200;
     public static double flywheel_direction = -1;
-    public static int flywheel_ready_ticks = 13;
+    public static int flywheel_ready_ticks = 5;
     public static int flywheel_ready_min_speed = 1110;
-    public static int flywheel_ready_max_speed = 1130;
+    public static int flywheel_ready_max_speed = 1200;
     public static double flywheel_filtered_speed = 0;
     public static int flywheel_max_achievable_ticks = 1360;
 
@@ -29,5 +29,9 @@ public class Vals {
 
     public static double hitter_start = 0.4;
     public static double hitter_end = .7;
+
+    public static double wobble_arm_speed = 0.8;
+    public static double wobble_hand_open = 1.0;
+    public static double wobble_hand_close = 0;
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EchoOp.java
@@ -2,35 +2,55 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robot.DriveTrain;
 import org.firstinspires.ftc.robot.FlyWheel;
 import org.firstinspires.ftc.robot_utilities.GamePadController;
 import org.firstinspires.ftc.robot.Hitter;
+import org.firstinspires.ftc.robot_utilities.RotationController;
 import org.firstinspires.ftc.robot_utilities.Vals;
 
 @TeleOp(name = "EchoOp")
 public class EchoOp extends OpMode {
+    FtcDashboard dashboard;
     private GamePadController gamepad;
     private DriveTrain driveTrain;
     private FlyWheel flywheel;
     private Hitter hitter;
     private Motor intake1, intake2;
+    private Motor wobbleArm;
+    private Servo wobbleHand;
+    private DifferentialDriveOdometry odometry;
+    RotationController rotationController;
 
     private double intakeSpeed = 0;
+    private double wobbleHandPos = Vals.wobble_hand_close;
+    private double wobbleArmVelocity = 0;
     private boolean flywheelOn = false;
 
     @Override
     public void init() {
-        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
         gamepad = new GamePadController(gamepad1);
 
         driveTrain = new DriveTrain(new Motor(hardwareMap, "dl"),
                                     new Motor(hardwareMap, "dr"));
+        driveTrain.resetEncoders();
+
+        rotationController = new RotationController(hardwareMap.get(BNO055IMU.class, "imu"));
+
+        odometry = new DifferentialDriveOdometry(new Rotation2d(rotationController.getAngleRadians()), new Pose2d(45,  0, new Rotation2d(0)));
 
         intake1 = new Motor(hardwareMap, "in1");
         intake2 = new Motor(hardwareMap, "in2");
@@ -38,6 +58,9 @@ public class EchoOp extends OpMode {
         intake2.setRunMode(Motor.RunMode.VelocityControl);
         intake1.setVeloCoefficients(0.05, 0, 0);
         intake2.setVeloCoefficients(0.05, 0, 0);
+
+        wobbleHand = hardwareMap.servo.get("wobbleArmServo");
+        wobbleArm = new Motor(hardwareMap, "wobbleArmMotor");
 
         flywheel = new FlyWheel(new Motor(hardwareMap, "fw", Motor.GoBILDA.BARE), telemetry);
 
@@ -47,6 +70,9 @@ public class EchoOp extends OpMode {
     @Override
     public void loop() {
         gamepad.update();
+        double leftDistanceInch = driveTrain.driveLeft.getDistance() / Vals.TICKS_PER_INCH_MOVEMENT;
+        double rightDistanceInch = driveTrain.driveRight.getDistance() / Vals.TICKS_PER_INCH_MOVEMENT;
+        odometry.update(new Rotation2d(rotationController.getAngleRadians()), leftDistanceInch, rightDistanceInch);
 
 
         double leftSpeed = gamepad1.left_stick_y;
@@ -58,6 +84,21 @@ public class EchoOp extends OpMode {
         } else {
             intakeSpeed = 0;
         }
+
+        if(gamepad1.a) {
+            wobbleHandPos = Vals.wobble_hand_open;
+        } else {
+            wobbleHandPos = Vals.wobble_hand_close;
+        }
+
+        if(gamepad1.dpad_up) {
+            wobbleArmVelocity = Vals.wobble_arm_speed;
+        } else if(gamepad1.dpad_down) {
+            wobbleArmVelocity = -Vals.wobble_arm_speed;
+        } else {
+            wobbleArmVelocity = 0;
+        }
+
 
         if(gamepad.isYRelease()) {
             flywheel.flipDirection();
@@ -96,6 +137,9 @@ public class EchoOp extends OpMode {
         intake1.set(intakeSpeed);
         intake2.set(intakeSpeed);
 
+        wobbleHand.setPosition(wobbleHandPos);
+        wobbleArm.set(wobbleArmVelocity);
+
 
         telemetry.addData("Flywheel Speed", flywheel.flywheel.get());
         telemetry.addData("Flywheel Velocity", flywheel.flywheel.getCorrectedVelocity());
@@ -106,6 +150,12 @@ public class EchoOp extends OpMode {
         telemetry.addData("Right Speed", rightSpeed);
         telemetry.addData("Intake Speed", intakeSpeed);
         telemetry.addData("Flywheel Ready State", isReady);
+
+        Pose2d pose = odometry.getPoseMeters();
+        TelemetryPacket packet = new TelemetryPacket();
+        packet.fieldOverlay()
+                .setFill("blue")
+                .fillRect(pose.getX(), pose.getY(), 40, 40);
 
 
     }
