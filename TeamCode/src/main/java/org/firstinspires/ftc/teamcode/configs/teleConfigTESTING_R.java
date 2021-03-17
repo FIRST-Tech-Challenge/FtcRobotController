95b95fd2d026f60c725fcf0c113b57d9a521303f
rsmrohit <32987551+rsmrohit@users.noreply.github.com>
2021-03-16 19:56:28 -0700

    Added configs for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configs/teleConfigTESTING_R.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/configs/teleConfigTESTING_R.java
@@ -1,4 +1,83 @@
 package org.firstinspires.ftc.teamcode.configs;
 
-public class teleConfigTESTING_R {
+public class teleConfigTESTING_R implements teleOpInterface{
+    public void a(boolean pressed) {
+        
+    }
+
+    public void b(boolean pressed) {
+
+    }
+
+    public void x(boolean pressed) {
+
+    }
+
+    public void y(boolean pressed) {
+
+    }
+
+    public void dd(boolean pressed) {
+
+    }
+
+    public void dp(boolean pressed) {
+
+    }
+
+    public void dl(boolean pressed) {
+
+    }
+
+    public void dr(boolean pressed) {
+
+    }
+
+    public void rb(boolean pressed) {
+
+    }
+
+    public void rt(float pressure) {
+
+    }
+
+    public void lb(boolean pressed) {
+
+    }
+
+    public void lt(float pressure) {
+
+    }
+
+    public void rjoy(float x, float y) {
+
+    }
+
+    public void ljoy(float x, float y) {
+
+    }
+
+    public void rjoyb(boolean pressed) {
+
+    }
+
+    public void ljoyb(boolean pressed) {
+
+    }
+
+    public void custom1() {
+
+    }
+
+    public void updateTelemetryDM() {
+
+    }
+
+    public void loop() {
+
+    }
+
+    public String getName() {
+        return null;
+    }
 }
