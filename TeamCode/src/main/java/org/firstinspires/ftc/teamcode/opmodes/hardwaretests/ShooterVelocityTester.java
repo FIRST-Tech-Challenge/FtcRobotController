3ecaa04689eb07f418a01ee2739f0c0dc4f3eb8b
matthewself12 <matthewself12@users.noreply.github.com>
2021-03-19 23:30:24 -0400

    Update ShooterVelocityTester.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/hardwaretests/ShooterVelocityTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/hardwaretests/ShooterVelocityTester.java
@@ -20,7 +20,6 @@ public class ShooterVelocityTester extends UpliftAuto {
     UpliftRobot robot;
     VelocityData velocityData;
     FtcDashboard ftcDashboard;
-    Telemetry dashTelem;
 
     @Override
     public void initHardware() {
@@ -31,34 +30,19 @@ public class ShooterVelocityTester extends UpliftAuto {
     @Override
     public void initAction() {
         ftcDashboard = FtcDashboard.getInstance();
-        dashTelem = ftcDashboard.getTelemetry();
-
     }
 
     @Override
     public void body() throws InterruptedException {
         waitForStart();
-
-        Log.i("PID", robot.shooter1.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER) + "");
-
-
-//        robot.shooter1.setVelocity(robot.powerShotVelocity);
-//        robot.shooter2.setVelocity(robot.powerShotVelocity);
-
-        double initialTime = System.currentTimeMillis();
-        double timeElapsed = 0;
+        
         while(opModeIsActive()) {
             robot.shooter1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(DashboardConstants.Kp, DashboardConstants.Ki, DashboardConstants.Kd, DashboardConstants.Kf));
             robot.shooter2.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(DashboardConstants.Kp, DashboardConstants.Ki, DashboardConstants.Kd, DashboardConstants.Kf));
             robot.shooter1.setVelocity(DashboardConstants.targetVel);
             robot.shooter2.setVelocity(DashboardConstants.targetVel);
-//            robot.shooter1.setPower(1);
-//            robot.shooter2.setPower(1);
             Log.d("Shooter1 Velocity", robot.shooter1Vel + "");
             Log.d("Shooter2 Velocity", robot.shooter2Vel + "");
-            Log.d("Time Elapsed", timeElapsed + "");
-            Utils.sleep(50);
-            timeElapsed = System.currentTimeMillis() - initialTime;
         }
 
         robot.shooter1.setPower(0);
