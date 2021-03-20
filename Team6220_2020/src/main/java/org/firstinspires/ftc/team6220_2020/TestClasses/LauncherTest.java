150940ce2b958920981d19d9b513c054a5cb5407
satvik <satvik.kabbur@gmail.com>
2021-03-19 19:30:22 -0700

    Added LauncherTest class to test the ring launcher.

---
+++ Team6220_2020/src/main/java/org/firstinspires/ftc/team6220_2020/TestClasses/LauncherTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.team6220_2020.TestClasses;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "LauncherTest", group = "TeleOp")
+public class LauncherTest extends LinearOpMode{
+
+    // Motors
+    DcMotor motorLauncher;
+
+    @Override
+    public void runOpMode() {
+
+        // Initialize
+        motorLauncher = hardwareMap.dcMotor.get("motorLauncher");
+        motorLauncher.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // If/Else statement to detect whether or notthe
+            if (gamepad1.a) {
+                motorLauncher.setPower(1.0);
+            }
+
+            else {
+                motorLauncher.setPower(0.0);
+            }
+
+            //Telemetry
+            telemetry.addData("Launcher Motor Power: ", motorLauncher.getPower());
+            telemetry.update();
+
+            idle();
+        }
+    }
+}
