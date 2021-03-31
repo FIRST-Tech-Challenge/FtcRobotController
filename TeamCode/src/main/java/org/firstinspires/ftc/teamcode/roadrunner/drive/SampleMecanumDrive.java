300faef2ab82b005ea2e09b8d88a69b259bf0d8e
Mason Wu <yutong.w@outlook.com>
2021-03-31 23:11:12 +0800

    Update SampleMecanumDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/SampleMecanumDrive.java
@@ -63,7 +63,7 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(3, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 2;
+    public static double LATERAL_MULTIPLIER = 30;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
