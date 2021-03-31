2b8920268b126db2fef516e2570f9eb1de7a8ac3
Mason Wu <yutong.w@outlook.com>
2021-03-31 23:05:57 +0800

    Update StrafeTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/StrafeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/opmode/StrafeTest.java
@@ -14,7 +14,7 @@ import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 @Config
 @Autonomous(group = "drive")
 public class StrafeTest extends LinearOpMode {
-    public static double DISTANCE = 60; // in
+    public static double DISTANCE = 15; // in
 
     @Override
     public void runOpMode() throws InterruptedException {
