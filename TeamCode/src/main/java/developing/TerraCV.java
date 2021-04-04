9349dccfc90d0d21b48b50b106a78c3e36f34bee
shuhul <shuhulmujoo@gmail.com>
2021-04-03 18:57:26 -0700

    Changed Orange Constant (AR)

--- TeamCode/src/main/java/developing/TerraCV.java
+++ TeamCode/src/main/java/developing/TerraCV.java
@@ -13,15 +13,22 @@ import java.util.ArrayList;
 public class TerraCV extends OpenCvPipeline
 {
 
+    public static double ORANGE_MIN = 80;
+    public static double ORANGE_MAX = 110;
+
     public Mat hsvMat = new Mat();
     public Mat thresholdMat = new Mat();
     public Mat contoursOnFrameMat = new Mat();
     public ArrayList<MatOfPoint> contoursList = new ArrayList<>();
     public int numContoursFound;
 
-    public Scalar ringColor =  new Scalar(30,86,82);
-    Scalar lower =  new Scalar(20,64,64);
-    Scalar upper =  new Scalar(40,100,100);
+    private Mat yCrCb = new Mat();
+    private Mat cb = new Mat();
+    private Mat processed = new Mat();
+//
+//    public Scalar ringColor =  new Scalar(30,86,82);
+//    Scalar lower =  new Scalar(20,100,100);
+//    Scalar upper =  new Scalar(40,255,255);
 
 
     @Override
@@ -29,21 +36,31 @@ public class TerraCV extends OpenCvPipeline
     {
         contoursList.clear();
 
+        Imgproc.cvtColor(input, yCrCb, Imgproc.COLOR_RGB2YCrCb);
 
-        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+        Core.extractChannel(yCrCb, cb, 2);
+
+
+        Core.inRange(cb, new Scalar(ORANGE_MIN), new Scalar(ORANGE_MIN), processed);
+
+        Imgproc.morphologyEx(processed, processed, Imgproc.MORPH_CLOSE, new Mat());
+
 //
-//        Core.extractChannel(hsvMat, hsvMat, 0);
+//
+//        Imgproc.cvtColor(input, hsvMat, Imgproc.COLOR_RGB2HSV);
+////
+////        Core.extractChannel(hsvMat, hsvMat, 0);
+//
+//
+//        Core.inRange(hsvMat, lower, upper, thresholdMat);
+////        Imgproc.threshold(hsvMat, thresholdMat, 102, 100, Imgproc.THRESH_BINARY_INV);
+//        Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+//        numContoursFound = contoursList.size();
+//        input.copyTo(contoursOnFrameMat);
+//        Imgproc.drawContours(contoursOnFrameMat, contoursList, -1, new Scalar(0, 0, 255), 3, 8);
 
 
-        Core.inRange(hsvMat, lower, upper, thresholdMat);
-//        Imgproc.threshold(hsvMat, thresholdMat, 102, 100, Imgproc.THRESH_BINARY_INV);
-        Imgproc.findContours(thresholdMat, contoursList, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        numContoursFound = contoursList.size();
-        input.copyTo(contoursOnFrameMat);
-        Imgproc.drawContours(contoursOnFrameMat, contoursList, -1, new Scalar(0, 0, 255), 3, 8);
-
-
-        return contoursOnFrameMat;
+        return processed;
     }
 
     @Override
