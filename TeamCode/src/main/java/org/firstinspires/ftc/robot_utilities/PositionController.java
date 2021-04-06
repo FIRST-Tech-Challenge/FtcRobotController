a8bcba5c7314da69c4d0af88323c57644e04584d
Ishan Arya <ishan.arya130@gmail.com>
2021-04-06 00:23:38 -0400

    rotational speed finder

--- TeamCode/src/main/java/org/firstinspires/ftc/robot_utilities/PositionController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/robot_utilities/PositionController.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.arcrobotics.ftclib.geometry.Pose2d;
 import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
 
 public class PositionController {
@@ -11,16 +12,50 @@ public class PositionController {
     public DifferentialDriveOdometry odometry;
 
     private Pose2d startPose;
+    private Pose2d m_poseError = new Pose2d();
+    private Pose2d m_poseTolerance = new Pose2d();
     private PIDController pidDrive;
     private RotationController rotationController;
 
-    public PositionController(Pose2d currentPose, RotationController rotationController) {
+    double b;
+    double zeta;
+
+    public PositionController(Pose2d currentPose,
+                              RotationController rotationController,
+                              double b,
+                              double zeta) {
         pidDrive = new PIDController(Vals.drive_kp, Vals.drive_ki, Vals.drive_kd);
         pidDrive.setTolerance(Vals.drive_tolerance);
+
+        this.b = b;
+        this.zeta = zeta;
 
         this.rotationController = rotationController;
         this.startPose = currentPose;
         odometry = new DifferentialDriveOdometry(new Rotation2d(rotationController.getAngleRadians()), currentPose);
+    }
+
+    public PositionController(Pose2d currentPose, RotationController rotationController) {
+        this(currentPose, rotationController, 2.0, 0.7);
+    }
+
+    /**
+     * Sets the pose error which is considered tolerable for use with atReference().
+     * @param poseTolerance Pose error which is tolerable.
+     */
+    public void setTolerance(Pose2d poseTolerance) {
+        this.m_poseTolerance = poseTolerance;
+    }
+
+    /** Returns true if the pose error is within tolerance of the reference. */
+    public boolean atReference() {
+        final Translation2d eTranslate = m_poseError.getTranslation();
+        final Rotation2d eRotate = m_poseError.getRotation();
+        final Translation2d tolTranslate = m_poseTolerance.getTranslation();
+        final Rotation2d tolRotate = m_poseTolerance.getRotation();
+        return Math.abs(eTranslate.getX()) < tolTranslate.getX()
+                && Math.abs(eTranslate.getY()) < tolTranslate.getY()
+                && Math.abs(eRotate.getRadians()) < tolRotate.getRadians();
     }
 
     /**
@@ -61,39 +96,71 @@ public class PositionController {
         return Math.toDegrees(Math.atan2(dx, dy));
     }
 
+    /**
+     * Returns sin(x) / x.
+     *
+     * @param x Value of which to take sinc(x).
+     */
+    @SuppressWarnings("ParameterName")
+    private static double sinc(double x) {
+        if (Math.abs(x) < 1e-9) {
+            return 1.0 - 1.0 / 6.0 * x * x;
+        } else {
+            return Math.sin(x) / x;
+        }
+    }
+
     public void reset() {
         pidDrive.reset();
         startPose = odometry.getPoseMeters();
         rotationController.resetAngle();
     }
 
-    public double[] goto_pose(Pose2d targetPose, TelemetryPacket packet) {
-        double targetDistance = getDistance(targetPose, startPose);
-        double currentDistance = getDistance(odometry.getPoseMeters(), startPose);
-        double rotationToPoint = getRotationToPoint(targetPose, startPose);
+    public double[] goto_pose(Pose2d targetPose,
+                              double linearVelocityRefMeters,
+                              double angularVelocityRefRadiansPerSecond,
+                              TelemetryPacket packet) {
 
-        double leftSpeed = 0;
-        double rightSpeed = 0;
+        Pose2d currentPose = odometry.getPoseMeters();
 
-        double power = 0;
-        double power2 = 0;
+        m_poseError = targetPose.relativeTo(currentPose);
 
-        power = rotationController.rotate(rotationToPoint);
-        power2 = pidDrive.calculate(currentDistance, targetDistance);
+        final double eX = m_poseError.getX();
+        final double eY = m_poseError.getY();
+        final double eTheta = m_poseError.getRotation().getRadians();
+        final double vRef = linearVelocityRefMeters;
+        final double omegaRef = angularVelocityRefRadiansPerSecond;
 
-        leftSpeed += power;
-        rightSpeed -= power;
+        double k = 2.0 * zeta * Math.sqrt(Math.pow(omegaRef, 2) + b * Math.pow(vRef, 2));
 
-        leftSpeed -= power2;
-        rightSpeed -= power2;
+        double linearVelocity = vRef * m_poseError.getRotation().getCos() + k * eX;
+        double angularVelocity = omegaRef + k * eTheta + b * vRef * sinc(eTheta) * eY;
+//        double targetDistance = getDistance(targetPose, startPose);
+//        double currentDistance = getDistance(odometry.getPoseMeters(), startPose);
+//        double rotationToPoint = getRotationToPoint(targetPose, startPose);
+//
+//        double leftSpeed = 0;
+//        double rightSpeed = 0;
+//
+//        double power = 0;
+//        double power2 = 0;
+//
+//        power = rotationController.rotate(rotationToPoint);
+//        power2 = pidDrive.calculate(currentDistance, targetDistance);
+//
+//        leftSpeed += power;
+//        rightSpeed -= power;
+//
+//        leftSpeed -= power2;
+//        rightSpeed -= power2;
 
-        packet.put("Rotation to point", rotationToPoint);
-        packet.put("Target Distance", targetDistance);
-        packet.put("Current Distance", currentDistance);
-        packet.put("Position Power", power2);
-        packet.put("Rotation Power", power);
+//        packet.put("Rotation to point", rotationToPoint);
+//        packet.put("Target Distance", targetDistance);
+//        packet.put("Current Distance", currentDistance);
+//        packet.put("Position Power", power2);
+//        packet.put("Rotation Power", power);
 
-        return new double[]{leftSpeed, rightSpeed};
+        return new double[]{1, 1};
     }
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -2,31 +2,49 @@ package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robot.DriveTrain;
 
 @TeleOp(name = "MotorTest")
 public class MotorTest extends OpMode {
     private DcMotor motor;
+    private DriveTrain driveTrain;
+    private ElapsedTime elapsedTime;
+    boolean reset;
 
     @Override
     public void init() {
+        driveTrain = new DriveTrain(new Motor(hardwareMap, "dl"),
+                new Motor(hardwareMap, "dr"),
+                Motor.RunMode.RawPower);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
-        motor = hardwareMap.dcMotor.get("fw");
+//        motor = hardwareMap.dcMotor.get("fw");
+        elapsedTime = new ElapsedTime();
+        reset = true;
     }
 
     @Override
     public void loop() {
         if (gamepad1.a) {
-            motor.setPower(1);
+            if(reset) {
+                reset = false;
+                elapsedTime.reset();
+            }
+            driveTrain.setSpeed(1, -1);
         } else {
-            motor.setPower(0);
+            reset = true;
+            driveTrain.setSpeed(0, 0);
         }
 
-        telemetry.addData("Motor Ticks", ((DcMotorEx)(motor)).getVelocity());
-        telemetry.addData("Motor Power", motor.getPower());
+        telemetry.addData("Right Speed", driveTrain.driveRight.getCorrectedVelocity());
+        telemetry.addData("Right Power", driveTrain.driveRight.get());
+        telemetry.addData("Elapsed Time", elapsedTime.seconds());
     }
 
 }
