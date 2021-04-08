8cb9045b2e99e9ed7eb47955a79e6432c2412e9a
rodoggyro <47298960+rodoggyro@users.noreply.github.com>
2021-04-08 19:35:54 -0400

    Added to autonomous and teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeV1.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 
@@ -17,10 +18,14 @@ public class OpModeV1 extends LinearOpMode {
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
+
     private DcMotor shooter;
     private DcMotor belt;
     private DcMotor intake;
+
     private DcMotor arm;
+    private Servo clamp;
+
 
     //code to play once the OpMode is active
     public void runOpMode() {
@@ -34,7 +39,9 @@ public class OpModeV1 extends LinearOpMode {
         shooter = hardwareMap.get(DcMotor.class, "buzz");
         belt = hardwareMap.get(DcMotor.class, "belt");
         intake = hardwareMap.get(DcMotor.class, "intake");
+
         arm = hardwareMap.get(DcMotor.class, "arm");
+        clamp = hardwareMap.get(Servo.class, "clamp");
 
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -45,62 +52,96 @@ public class OpModeV1 extends LinearOpMode {
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
         belt.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        clamp.setDirection(Servo.Direction.REVERSE);
+
         waitForStart();
 
         // Variable initialization
         double throttle;
         double pivot;
+        double armDrive;
+        boolean strafeRight;
+        boolean strafeLeft;
+
         double beltPower;
+        double shooterPower;
         boolean powerA;
-        boolean armUp;
-        boolean armDown;
         boolean powerB;
         boolean powerX;
         boolean powerY;
         boolean intakePower;
-        double shooterPower;
-        boolean strafeRight;
-        boolean strafeLeft;
 
-        while (opModeIsActive()){
+        boolean armUp;
+        boolean armDown;
+        boolean clampIn;
+        boolean clampOut;
+
+
+        while (opModeIsActive()) {
             // Read in values from controller
             throttle = gamepad1.right_stick_y;
             pivot = gamepad1.left_stick_x;
-            beltPower = gamepad2.left_stick_y;
-            shooterPower = gamepad2.right_trigger;
             strafeRight = gamepad1.right_bumper;
             strafeLeft = gamepad1.left_bumper;
+            armDrive = gamepad1.right_trigger;
+
+
+            beltPower = gamepad2.left_stick_y;
+            shooterPower = gamepad2.right_trigger;
             powerA = gamepad2.a;
             powerB = gamepad2.b;
             powerX = gamepad2.x;
             powerY = gamepad2.y;
             intakePower = gamepad2.left_bumper;
+
             armUp = gamepad1.dpad_up;
             armDown = gamepad1.dpad_down;
 
-
-
             // Driving
-            if (strafeRight){
-                frontLeft.setPower(-0.5);
-                backLeft.setPower(0.5);
-                frontRight.setPower(0.5);
-                backRight.setPower(-0.5);
-            }else if (strafeLeft){
-                frontLeft.setPower(0.5);
-                backLeft.setPower(-0.5);
-                frontRight.setPower(-0.5);
-                backRight.setPower(0.5);
-            }else{
-                frontLeft.setPower(throttle);
-                frontRight.setPower(throttle);
-                backLeft.setPower(throttle);
-                backRight.setPower(throttle);
+            if (armDrive <= 0.25) {
+                if (strafeRight) {
+                    frontLeft.setPower(-0.5);
+                    backLeft.setPower(0.5);
+                    frontRight.setPower(0.5);
+                    backRight.setPower(-0.5);
+                } else if (strafeLeft) {
+                    frontLeft.setPower(0.5);
+                    backLeft.setPower(-0.5);
+                    frontRight.setPower(-0.5);
+                    backRight.setPower(0.5);
+                } else {
+                    frontLeft.setPower(throttle);
+                    frontRight.setPower(throttle);
+                    backLeft.setPower(throttle);
+                    backRight.setPower(throttle);
 
-                frontLeft.setPower(-pivot);
-                frontRight.setPower(pivot);
-                backLeft.setPower(-pivot);
-                backRight.setPower(pivot);
+                    frontLeft.setPower(-pivot);
+                    frontRight.setPower(pivot);
+                    backLeft.setPower(-pivot);
+                    backRight.setPower(pivot);
+                }
+            }else if (armDrive > 0.25){
+                if (strafeRight) {
+                    frontLeft.setPower(0.5);
+                    backLeft.setPower(-0.5);
+                    frontRight.setPower(-0.5);
+                    backRight.setPower(0.5);
+                } else if (strafeLeft) {
+                    frontLeft.setPower(-0.5);
+                    backLeft.setPower(0.5);
+                    frontRight.setPower(0.5);
+                    backRight.setPower(-0.5);
+                } else {
+                    frontLeft.setPower(-throttle);
+                    frontRight.setPower(-throttle);
+                    backLeft.setPower(-throttle);
+                    backRight.setPower(-throttle);
+
+                    frontLeft.setPower(pivot);
+                    frontRight.setPower(-pivot);
+                    backLeft.setPower(pivot);
+                    backRight.setPower(-pivot);
+                }
             }
 
             if (shooterPower > 0){
@@ -116,21 +157,11 @@ public class OpModeV1 extends LinearOpMode {
             }else{
                 shooter.setPower(0);
             }
-
             if (intakePower){
                 intake.setPower(.5);
             }else{
                 intake.setPower(0);
             }
-
-            if (armUp){
-                arm.setPower(0.25);
-            }else if (armDown){
-                arm.setPower(-0.25);
-            }else{
-                arm.setPower(0);
-            }
-
             if (beltPower > 0){
                 belt.setPower(0.5);
             }else if(beltPower<0){
@@ -138,6 +169,17 @@ public class OpModeV1 extends LinearOpMode {
             }else{
                 belt.setPower(0);
             }
+
+            if (armUp){
+                arm.setPower(0.5);
+            }else if (armDown){
+                arm.setPower(-0.25);
+            }else{
+                arm.setPower(0);
+            }
+
+            telemetry.addData("Arm Front", armDrive);
+            telemetry.update();
         }
     }
 }
