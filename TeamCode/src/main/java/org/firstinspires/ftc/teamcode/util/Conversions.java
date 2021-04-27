e6594707ec9764437e4064d1168394796a99f364
IronReignRobotics <ironreignrobotics@gmail.com>
2021-04-26 22:47:26 -0500

    Update Conversions.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Conversions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Conversions.java
@@ -74,20 +74,6 @@ public class Conversions {
         return (between(value, leftAngle, rightAngle));
     }
 
-    public static boolean between360NonDirectional(double value, double leftAngle, double rightAngle){
-        if(wrap360(value) == wrap360(leftAngle) || wrap360(value) == wrap360(rightAngle)){
-            return true;
-        }
-
-        double shift = -diffAngle2(0,leftAngle);
-
-        leftAngle = wrap360(leftAngle, shift);
-        rightAngle = wrap360(rightAngle, shift);
-        value = wrap360(value, shift);
-
-        return (between(value, leftAngle, rightAngle));
-    }
-
     public static double wrapAngle(double angle){
         return (angle) % 360;
     }
