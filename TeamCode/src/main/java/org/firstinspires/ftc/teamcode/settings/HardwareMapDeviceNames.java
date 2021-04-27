7c9b954c09976c3e2dde05c56ee0477b84448cf2
Joshua Miller <22jmiller@xbhs.net>
2021-04-27 11:37:25 -0400

    Resetting master back to original

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/settings/HardwareMapDeviceNames.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.settings;
-
-public class HardwareMapDeviceNames {
-    public static final String LEFT_DRIVE = "left_drive";
-    public static final String RIGHT_DRIVE = "right_drive";
-
-}
