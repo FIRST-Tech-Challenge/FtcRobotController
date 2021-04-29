452117855f629264ca177e29e1497202c84d43a3
Josh M <22jmiller@xbhs.net>
2021-04-28 22:09:38 -0400

    Able to drive with tank and arcade mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Drive.java
@@ -3,8 +3,10 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
-import org.firstinspires.ftc.teamcode.settings.HardwareMapDeviceNames;
+import org.firstinspires.ftc.teamcode.settings.DeviceNames;
 
 import java.util.ArrayList;
 
@@ -17,21 +19,68 @@ public class Drive extends OpMode {
     public void init() {
         // Left Drive
         leftDriveMotors = new ArrayList<DcMotor>();
-        for (String name : HardwareMapDeviceNames.LEFT_DRIVE) {
-            leftDriveMotors.add(hardwareMap.get(DcMotor.class, name));
+        for (String name : DeviceNames.LEFT_DRIVE) {
+            DcMotor motor = hardwareMap.get(DcMotor.class, name);
+            motor.setDirection(DcMotorSimple.Direction.REVERSE);
+            leftDriveMotors.add(motor);
         }
 
         // Right Drive
         rightDriveMotors = new ArrayList<DcMotor>();
-        for (String name : HardwareMapDeviceNames.RIGHT_DRIVE) {
-            rightDriveMotors.add(hardwareMap.get(DcMotor.class, name));
+        for (String name : DeviceNames.RIGHT_DRIVE) {
+            DcMotor motor = hardwareMap.get(DcMotor.class, name);
+            rightDriveMotors.add(motor);
         }
     }
 
+    // Left is -
+    // Up is +
+    public double[] getPowerOneWay(Gamepad gamepad) {
+        double x = gamepad.left_stick_x;
+        double y = -gamepad.left_stick_y;
+        double totalPower = Math.sqrt(Math.pow(x,2) + Math.pow(y,2)); // Distance Formula
+
+        double lPower = 0;
+        double rPower = 0;
+        if (y >= 0) { // Going Forwards
+            // y = mx + b
+            rPower = x + totalPower;
+            lPower = -x + totalPower;
+
+            // Cap to 1
+            lPower = Math.min(lPower,1);
+            rPower = Math.min(rPower,1);
+        } else {
+            totalPower *= -1;
+            // y = mx + b
+            rPower = x + totalPower;
+            lPower = -x + totalPower;
+
+            // Cap to -1
+            //lPower = Math.max(lPower,-1);
+            //rPower = Math.max(rPower,-1);
+        }
+
+
+        System.out.println(totalPower);
+        //double lPower = gamepad.left_stick_y;
+        //double rPower = gamepad.right_stick_y;
+        return new double[] {lPower, rPower};
+    }
+
+    public double[] getPowerTank(Gamepad gamepad) {
+        double lPower = gamepad.left_stick_y;
+        double rPower = gamepad.right_stick_y;
+        return new double[] {lPower, rPower};
+    }
+
+
+
     @Override
     public void loop() {
-        double lPower = gamepad1.left_stick_y;
-        double rPower = gamepad1.right_stick_y;
+        double[] array = getPowerOneWay(gamepad1);
+        double lPower = array[0];
+        double rPower = array[1];
         for (DcMotor leftMotor : leftDriveMotors) {
             leftMotor.setPower(lPower);
         }
@@ -41,5 +90,7 @@ public class Drive extends OpMode {
         }
 
         telemetry.addData("Motors ", lPower + "l | " + rPower + "r");
+
+        telemetry.addData("Controller", gamepad1.left_stick_x + "x | " + gamepad1.left_stick_y);
     }
 }
