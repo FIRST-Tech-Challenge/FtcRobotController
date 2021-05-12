65e355084e50f705558b2ec7dc6763085707f29a
rishabh-tole <68649037+rishabhtole@gmail.com>
2021-05-12 13:52:01 -0400

    personalized and optimized

--- camera/goal_detector.py
+++ camera/goal_detector.py
@@ -40,69 +40,61 @@ time.sleep(2.0)
 # keep looping
 while True:
 	# grab the current frame
-	frame = vs.read()
-	# handle the frame from VideoCapture or VideoStream
-	frame = frame[1] if args.get("video", False) else frame
+        frame = vs.read()
+        # handle the frame from VideoCapture or VideoStream
+        frame = frame[1] if args.get("video", False) else frame
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
-	if frame is None:
-		break
+        if frame is None:
+            break
 	# resize the frame, blur it, and convert it to the HSV
 	# color space
-	frame = imutils.resize(frame, width=600)
-	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+        frame = imutils.resize(frame, width=600, height=600)
+        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 	# construct a mask for the color "green", then perform
 	# a series of dilations and erosions to remove any small
 	# blobs left in the mask
-	mask = cv2.inRange(hsv, Lower, Upper)
-	mask = cv2.erode(mask, None, iterations=2)
-	mask = cv2.dilate(mask, None, iterations=2)
-  # find contours in the mask and initialize the current
+        mask = cv2.inRange(hsv, Lower, Upper)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        # find contours in the mask and initialize the current
 	# (x, y) center of the ball
-	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = imutils.grab_contours(cnts)
-	center = None
-	# only proceed if at least one contour was found
-	if len(cnts) > 0:
-		# find the largest contour in the mask, then use
-		# it to compute the minimum enclosing circle and
-		# centroid
-	    c = max(cnts, key=cv2.contourArea)
-	    ((x, y), radius) = cv2.minEnclosingCircle(c)
-	    M = cv2.moments(c)
-	    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-		# only proceed if the radius meets a minimum size
-	    if radius > 10:
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
+        center = None
+        # only proceed if at least one contour was found
+        if len(cnts) > 0:
+
+            # find the largest contour in the mask, then use
+            # it to compute the minimum enclosing circle and
+            # centroid
+            c = max(cnts, key=cv2.contourArea)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+	    # only proceed if the radius meets a minimum size
+            if radius > 10:
                 print(x,y)
-		    # draw the circle and centroid on the frame,
-		    # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                # draw the circle and centroid on the frame,
+                # then update the list of tracked points
+                start_x = (int(x),0)
+                end_x = (int(x),600)
+                start_y = (0,int(y))
+                end_y = (600,int(y))
+                cv2.line(frame, start_x, end_x, (255,0,0), 2)
+                cv2.line(frame, start_y, end_y, (255,0,0), 2)
 	# update the points queue
-	pts.appendleft(center)
-  # loop over the set of tracked points
-	for i in range(1, len(pts)):
-		# if either of the tracked points are None, ignore
-		# them
-	    if pts[i - 1] is None or pts[i] is None:
-                continue
-		# otherwise, compute the thickness of the line and
-		# draw the connecting lines
-	    thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-	    cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-	# show the frame to our screen
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+        pts.appendleft(center)
+	# # show the frame to our screen
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
 	# if the 'q' key is pressed, stop the loop
-	if key == ord("q"):
-		break
+        if key == ord("q"):
+            break
 # if we are not using a video file, stop the camera video stream
 if not args.get("video", False):
-	vs.stop()
+        vs.stop()
 # otherwise, release the camera
 else:
-	vs.release()
+        vs.release()
 # close all windows
 cv2.destroyAllWindows()
