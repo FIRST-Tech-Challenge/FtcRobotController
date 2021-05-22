7cac2a426b034b8b2449d69e2f732d8a8a215248
Dodesimo <devammondal06@gmail.com>
2021-05-22 11:19:13 -0400

    Vision Test Movements for 0 rings and 1 ring

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VisionTest.java
@@ -1,21 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
+import org.firstinspires.ftc.robot.FlyWheel;
+import org.firstinspires.ftc.robot.Hitter;
+import org.firstinspires.ftc.robot_utilities.Vals;
 import org.firstinspires.ftc.robot_utilities.VisionController;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous(name = "VisionTest")
 public class VisionTest extends LinearOpMode {
@@ -25,6 +26,53 @@ public class VisionTest extends LinearOpMode {
     OpenCvInternalCamera phoneCam;
     ElapsedTime elapsedTime;
 
+    private Motor driveLeft, driveRight;
+    private FlyWheel flywheel;
+    private Hitter hitter;
+    private Motor intake1, intake2;
+    private BNO055IMU imu;
+
+    private Orientation lastAngles;
+    private double currentHeading = 0;
+    private PIDController pidRotate;
+
+    private double driveSpeed = 0.4;
+    double rotatePower = 0;
+
+    public void initRobot() {
+
+        elapsedTime = new ElapsedTime();
+
+        driveLeft = new Motor(hardwareMap, "dl");
+        driveRight = new Motor(hardwareMap, "dr");
+        driveRight.setInverted(true);
+        driveLeft.setRunMode(Motor.RunMode.RawPower);
+        driveRight.setRunMode(Motor.RunMode.RawPower);
+
+        intake1 = new Motor(hardwareMap, "in1");
+        intake2 = new Motor(hardwareMap, "in2");
+        intake1.setRunMode(Motor.RunMode.VelocityControl);
+        intake2.setRunMode(Motor.RunMode.VelocityControl);
+        intake1.setVeloCoefficients(0.05, 0, 0);
+        intake2.setVeloCoefficients(0.05, 0, 0);
+
+        flywheel = new FlyWheel(new Motor(hardwareMap, "fw", Motor.GoBILDA.BARE), telemetry);
+        hitter = new Hitter(hardwareMap.servo.get("sv"));
+
+        pidRotate = new PIDController(Vals.rotate_kp, Vals.rotate_ki, Vals.rotate_kd);
+        pidRotate.setTolerance(Vals.rotate_tolerance);
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        resetAngle();
+    }
+
     @Override
     public void runOpMode() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -32,9 +80,10 @@ public class VisionTest extends LinearOpMode {
         elapsedTime = new ElapsedTime();
         visionController = new VisionController(phoneCam);
 
+        initRobot();
         waitForStart();
         elapsedTime.reset();
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
             telemetry.addData("Position", visionController.getRingPosition());
 //            telemetry.addData("Analysis", visionController.getAnalysis());
             telemetry.addData("Height", visionController.getHeight());
@@ -43,6 +92,94 @@ public class VisionTest extends LinearOpMode {
             sleep(50);
         }
 
+
+        if (visionController.getHeight().equals("ZERO")) {
+
+            while (elapsedTime.seconds() < 2) {
+
+                rotatePower = rotate(0);
+                double leftPower = -rotatePower;
+                double rightPower = rotatePower;
+
+                leftPower += driveSpeed;
+                rightPower += driveSpeed;
+
+
+                driveLeft.set(leftPower);
+                driveRight.set(rightPower);
+
+            }
+
+            while (elapsedTime.seconds() < 2.18) {
+
+                driveLeft.set(0);
+                driveRight.set(0);
+
+            }
+        }
+
+        if (visionController.getHeight().equals("ONE")) {
+
+            while (elapsedTime.seconds() < 1.5) {
+
+                rotatePower = rotate(0);
+                double leftPower = -rotatePower;
+                double rightPower = rotatePower;
+
+                leftPower += driveSpeed;
+                rightPower += driveSpeed;
+
+
+                driveLeft.set(leftPower);
+                driveRight.set(rightPower);
+
+            }
+
+            while (elapsedTime.seconds() < 1.68) {
+
+                rotatePower = rotate(50);
+                driveLeft.set(0);
+                driveRight.set(0);
+
+            }
+        }
+
+        if (visionController.getHeight().equals("FOUR")) {
+
+
+        }
+
+
+    }
+
+    private void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        currentHeading = 0;
+        pidRotate.reset();
+    }
+
+    private double updateHeading() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+
+        if (deltaAngle < -180) {
+            deltaAngle += 360;
+        } else if (deltaAngle > 180) {
+            deltaAngle -= 360;
+        }
+
+        currentHeading += deltaAngle;
+        lastAngles = angles;
+
+        return currentHeading;
+    }
+
+    private double rotate(double degrees) {
+        if (Math.abs(degrees) > 359) degrees = Math.copySign(359, degrees);
+
+        double power = pidRotate.calculate(updateHeading(), degrees);
+        return power;
 
     }
 
