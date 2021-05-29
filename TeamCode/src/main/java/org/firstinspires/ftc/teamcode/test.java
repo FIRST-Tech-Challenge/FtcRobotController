4c39f4f6a714f2c0ec40c2385d24fca5ee55343f
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-05-29 09:36:02 -0500

    Update test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-public class test {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class test extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
