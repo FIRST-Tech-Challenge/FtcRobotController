7e1add24209f666ed61258c022b5a4b83d40d113
Ely31 <78882466+Ely31@users.noreply.github.com>
2021-05-30 20:00:09 -0500

    Create gm0_mecanum.java
    eee

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/gm0_mecanum.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class gm0_mecanum extends OpMode {
+    @Override
+    public void init() {
+        DcMotor FL = hardwareMap.dcMotor.get("FL");
+        DcMotor BL = hardwareMap.dcMotor.get("BL");
+        DcMotor FR = hardwareMap.dcMotor.get("FR");
+        DcMotor BR = hardwareMap.dcMotor.get("BR");
+
+        // Reverse the right side motors
+        // Reverse left motors if you are using NeveRests
+        FR.setDirection(DcMotorSimple.Direction.REVERSE);
+        BR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
+
+        double frontLeftPower = y + x + rx;
+        double backLeftPower = y - x + rx;
+        double frontRightPower = y - x - rx;
+        double backRightPower = y + x - rx;
+
+        // Put powers in the range of -1 to 1 only if they aren't already
+        // Not checking would cause us to always drive at full speed
+        if (Math.abs(frontLeftPower) > 1 || Math.abs(backLeftPower) > 1 ||
+                Math.abs(frontRightPower) > 1 || Math.abs(backRightPower) > 1) {
+            // Find the largest power
+            double max = 0;
+            max = Math.max(Math.abs(frontLeftPower), Math.abs(backLeftPower));
+            max = Math.max(Math.abs(frontRightPower), max);
+            max = Math.max(Math.abs(backRightPower), max);
+
+            // Divide everything by max (it's positive so we don't need to worry
+            // about signs)
+            frontLeftPower /= max;
+            backLeftPower /= max;
+            frontRightPower /= max;
+            backRightPower /= max;
+        }
+
+        FL.setPower(frontLeftPower);
+        motorBackLeft.setPower(backLeftPower);
+        motorFrontRight.setPower(frontRightPower);
+        motorBackRight.setPower(backRightPower);
+    }
+}
