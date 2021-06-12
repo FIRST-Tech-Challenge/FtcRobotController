bfd76695623a477bad49b3e5d59bdfc3d890ac8e
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2021-06-12 15:52:08 -0700

    feeling cute, might try to make a multithreaded PID later, dunno

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDThread.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+public class PIDThread implements Runnable {
+    //needs to be given a complete PID Controller,
+
+    @Override
+    public void run() {
+
+    }
+
+
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PIDTuner.java
@@ -14,7 +14,7 @@ public class PIDTuner extends LinearOpMode {
     /*declare OpMode members, initialize some classes*/
     Hardware robot          = new Hardware();
     ElapsedTime runtime     = new ElapsedTime();
-    PIDController pid       = new PIDController(15,0,0);
+    PIDController pid       = new PIDController(0,0,0);
 
 
 
@@ -27,24 +27,36 @@ public class PIDTuner extends LinearOpMode {
         double[] strafePidResults = new double[3];
         double[] rotatePidResults = new double[3];
 
-        double Ku = 15;
+        double Ku = .003;
         double t1=0,t2=0; // times
         int c1=0, c2=0;    //previous encoder count, and current count
         double d1=0,d2=0;  //previous deriv of c, and current deriv of c
         double m1=0,m2=0; //the time, in seconds, of the most recent max, and the max before that
 
+        double scaleFactor = .001;
+
         /* Initialize the hardware variables.
          * The init() method of the hardware class does all the work here
          */
         robot.init(hardwareMap);
-        pid.setSetpoint(0);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
         runtime.reset();
 
+        //PID config
+        pid.reset();
+        //pid.setOutputRange(-1,1);
+        pid.setTolerance(1);
+        pid.setSetpoint(100);
+        pid.enable();
+
+
         //button locks
-        boolean upCurPressed, upPrevPressed = false, downCurPressed, downPrevPressed = false;
+        boolean upCurPressed, upPrevPressed = false,
+                downCurPressed, downPrevPressed = false,
+                leftCurPressed, leftPrevPressed = false,
+                rightCurPressed, rightPrevPressed = false;
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -70,6 +82,27 @@ public class PIDTuner extends LinearOpMode {
             Ki = Kp / Ti
             Kd = Kp * Td
 
+            Ku seems to be 0.00767
+            Tu seems to be 0.098 or 0.0 sooo
+                                    Kp      Ti      Td
+            PID                 |.004602| 0.048 | 0.012
+            PI                  |.003452| 0.08  |
+            P                   |.003835|       |
+            PD                  |.006136|       | 0.012
+            some overshoot      |.002557| 0.048 | 0.032
+            no overshoot        |.001534| 0.048 | 0.032
+            pessen integration  |.005369| 0.0384| 0.0144
+
+                                    Kp      Ki      Kd
+            PID                 |.004602|.095875| 0.000055
+            PI                  |.003452|0.04315|
+            P                   |.003835|       |
+            PD                  |.006136|       | 0.000074
+            some overshoot      |.002557|.053271| 0.000082
+            no overshoot        |.001534|.031958| 0.000049
+            pessen integration  |.005369|.139818| 0.000077
+
+            standard PID, pessen integration seem especially promising
              */
 
             // do the ziegler-nichols method for the strafing PID
@@ -77,19 +110,30 @@ public class PIDTuner extends LinearOpMode {
             //allow K to be adjusted with buttons
             upCurPressed = gamepad1.dpad_up;
             if (upCurPressed && !upPrevPressed ){
-                Ku += 1;
+                Ku += scaleFactor;
             }
             upPrevPressed=upCurPressed;
 
             downCurPressed = gamepad1.dpad_down;
             if (downCurPressed && !downPrevPressed){
-                Ku -= 1;
+                Ku -= scaleFactor;
             }
             downPrevPressed=downCurPressed;
 
+            leftCurPressed = gamepad1.dpad_left;
+            if (leftCurPressed && !leftPrevPressed){
+                scaleFactor /= 10;
+            }
+            leftPrevPressed=leftCurPressed;
+
+            rightCurPressed = gamepad1.dpad_right;
+            if (rightCurPressed && !rightPrevPressed){
+                scaleFactor *= 10;
+            }
+            rightPrevPressed=rightCurPressed;
+
             //update pid coefficients
             pid.setPID(Ku,0,0);
-
 
             //find the change in encoder ticks (derivative), use that to find the time difference between max's
             t1 = t2;
@@ -97,18 +141,27 @@ public class PIDTuner extends LinearOpMode {
             double dT = t2 - t1; //change in time
 
             c1 = c2;
-            c2 = robot.driveBackRight.getCurrentPosition();
+            c2 = robot.driveFrontRight.getCurrentPosition();
             double dC = c2 - c1; //change in count
 
             d1 = d2;
             d2 = dC/dT; //change in encoder count with respect to time
 
-            if (d2 == 0 && c2 != 0) {
+            if ((d2 == 0 && c2 != 0) || d2-d1 == 0) {
                 m1 = m2;
                 m2 = t2;
             }
 
+            //run PID and power front right motor accordingly
+            double power;
+            if (gamepad1.a) pid.setSetpoint(1000);
+            else pid.setSetpoint(100);
+            power = pid.performPID(robot.driveFrontRight.getCurrentPosition());
+            robot.driveFrontRight.setPower(power);
+
             //telemetry
+            telemetry.addData("current PID output: ", power);
+            telemetry.addData("scale factor: ", scaleFactor);
             telemetry.addLine("---====data====---");
             telemetry.addData("time: ", t2);
             telemetry.addData("dT: ", dT);
@@ -118,10 +171,7 @@ public class PIDTuner extends LinearOpMode {
             telemetry.addData("Ku = ", Ku);
             telemetry.addData("Tu = ", m2-m1);
 
-            telemetry.update();
-
-            //run PID and power front right motor accordingly
-            robot.driveFrontRight.setPower(pid.performPID(c2));
+            if (!gamepad1.y) telemetry.update();
 
         }
 
