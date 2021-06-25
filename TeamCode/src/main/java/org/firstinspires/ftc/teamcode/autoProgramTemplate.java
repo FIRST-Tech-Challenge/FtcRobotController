ba65390c6d49bce24c2020bfd78f2b3c6f3e9bf0
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2021-06-25 11:27:45 -0700

    finished autonomous and teleop program templates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoProgramTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoProgramTemplate.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode;
 
-public class autoProgramTemplate {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+@Autonomous(name="Auto Name goes here", group="opmode group goes here" )
+@Disabled //this line disables the autonomous from appearing on the driver station, remove it for your code
+public class autoProgramTemplate extends LinearOpMode{
+    /*declare OpMode members, initialize some classes*/
+    Hardware robot          = new Hardware();
+    ElapsedTime runtime     = new ElapsedTime();
+
+    //this is the control loop, basically the equivalent of a main function almost
+    @Override
+    public void runOpMode()
+    {
+        ////////////before driver presses play////////////
+        //Variables
+
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        ////////////after driver presses play////////////
+
+        //autonomous routine goes here step by step
+
+
+        ////////////after driver presses stop////////////
+    }
+
+    ////////////other methods and whatnot below here////////////
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpProgramTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOpProgramTemplate.java
@@ -1,4 +1,49 @@
 package org.firstinspires.ftc.teamcode;
 
-public class teleOpProgramTemplate {
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="TeleOp Name goes here", group="opmode group goes here" )
+@Disabled //this line disables the teleop from appearing on the driver station, remove it for your code
+public class teleOpProgramTemplate extends LinearOpMode {
+    /*declare OpMode members, initialize some classes*/
+    Hardware robot          = new Hardware();
+    ElapsedTime runtime     = new ElapsedTime();
+
+    //this is the control loop, basically the equivalent of a main function almost
+    @Override
+    public void runOpMode()
+    {
+        ////////////before driver presses play////////////
+        //Variables
+
+
+        /* Initialize the hardware variables.
+         * The init() method of the hardware class does all the work here
+         */
+        robot.init(hardwareMap);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        ////////////after driver presses play////////////
+        //maybe some other set up stuff depending on how we want to do this
+
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            //code goes here
+
+        }
+
+        ////////////after driver presses stop////////////
+    }
+
+    ////////////other methods and whatnot below here////////////
+
+
 }
