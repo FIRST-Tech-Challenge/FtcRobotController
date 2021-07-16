80a4ef068d7fca0380b0caa6c4d03aa9b66e5c76
Nick-Fanelli <nickfanelli5@gmail.com>
2021-07-16 13:45:11 -0400

    feat: require wheel specs

--- TeamCode/src/main/java/com/bravenatorsrobotics/core/RobotSpecifications.java
+++ TeamCode/src/main/java/com/bravenatorsrobotics/core/RobotSpecifications.java
@@ -8,13 +8,22 @@ public class RobotSpecifications {
     public final String[] robotMotors;
     public final Class<? extends AbstractDrive> driveType;
 
+    public final double ticksPerMotorRev;
+    public final double driveGearReduction;
+    public final double wheelDiameterInches;
+
     public boolean useVelocity = false;
     public int maxVelocity; // Run the max velocity test to figure out
 
     public DcMotor.ZeroPowerBehavior zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE;
 
-    public RobotSpecifications(String[] robotMotors, Class<? extends AbstractDrive> driveType) {
+    public RobotSpecifications(String[] robotMotors, Class<? extends AbstractDrive> driveType,
+                               double ticksPerMotorRev, double driveGearReduction, double wheelDiameterInches) {
         this.robotMotors = robotMotors;
         this.driveType = driveType;
+
+        this.ticksPerMotorRev = ticksPerMotorRev;
+        this.driveGearReduction = driveGearReduction;
+        this.wheelDiameterInches = wheelDiameterInches;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Specifications.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Specifications.java
@@ -6,7 +6,8 @@ import com.bravenatorsrobotics.drive.FourWheelDrive;
 public class Specifications extends RobotSpecifications {
 
     public Specifications() {
-        super(new String[] { "!fl", "fr", "!bl", "br" }, FourWheelDrive.class);
+        super(new String[] { "!fl", "fr", "!bl", "br" }, FourWheelDrive.class,
+                1120, 1, 3.78);
 
         this.useVelocity = true;
         this.maxVelocity = 3120;
