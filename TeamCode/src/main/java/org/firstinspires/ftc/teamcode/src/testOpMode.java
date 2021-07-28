57dea4aaac5a8a5a7c5087b05c5605ed5f40404f
willm <willmoiser@gmail.com>
2021-07-27 20:49:35 -0500

    Removed Unused Import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/testOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/testOpMode.java
@@ -29,7 +29,6 @@
 
 package org.firstinspires.ftc.teamcode.src;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
