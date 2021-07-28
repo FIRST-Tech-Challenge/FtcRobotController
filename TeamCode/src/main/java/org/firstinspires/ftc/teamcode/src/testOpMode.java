7d41a52d5dacf86701bfa9e7cd6b20414b613039
willm <willmoiser@gmail.com>
2021-07-27 20:47:51 -0500

    Added A Default OpMode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/testOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/testOpMode.java
@@ -37,18 +37,6 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
-/**
- * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
- * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
- * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
- * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
- */
 
 @TeleOp(name="OpMode Test", group="Linear Opmode")
 public class testOpMode extends LinearOpMode {
