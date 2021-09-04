7a79fa94a07a445768046a3ada0df5c73c1f28a5
Eric Hou <hyweric@gmail.com>
2021-09-03 19:59:30 -0700

    changed variable of int

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/WarrenGitTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/WarrenGitTest.java
@@ -2,15 +2,12 @@ package org.firstinspires.ftc.teamcode.Autonomous.Tests;
 
 public class WarrenGitTest {
 
-    int apple_cost=0;
+    int apple_cost=3;
 
     public WarrenGitTest(){
         apple_cost=6;
     }
-    public EricTest(){
-        apple_cost= 7;
-        System.out.println("Working nani");
-    }
+
     int victorMasThisManyApple(){
         return 11;
     }
