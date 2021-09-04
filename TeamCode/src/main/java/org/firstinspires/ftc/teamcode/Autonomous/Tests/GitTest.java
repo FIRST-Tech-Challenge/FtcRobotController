61df7afee49f30098d4ba844755db04f5428d93a
Victor Ma <victormma@gmail.com>
2021-09-03 19:26:36 -0700

    Revert "speed mode works on turning but its not perfected yet"
    This reverts commit a239663d

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Tests/GitTest.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.Autonomous.Tests;
+
+public class GitTest {
+
+    int apple_cost = 0;
+
+    public GitTest () {
+        apple_cost = 6;
+    }
+
+    int victorHasThisManyApple () {
+        return 10;
+    }
+
+}
