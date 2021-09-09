e77bae756c6d5c919cb77100851381129bb442d6
PilotSamBevans <PilotSamBevans@gmail.com>
2021-09-09 08:33:48 -0500

    Add hardware devices

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/DeviceMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/DeviceMap.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.RobotData;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class DeviceMap {
+        //Listing hardware devices
+    public DcMotor leftFront = null;
+    public DcMotor rightFront = null;
+    public  DcMotor leftRear = null;
+    public  DcMotor rightRear = null;
+    public  Servo   fontClaw  = null;
 }
