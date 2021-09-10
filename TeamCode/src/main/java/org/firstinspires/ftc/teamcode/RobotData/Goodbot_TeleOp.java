9182ad76ecf1052f549d2655019c18a68485138f
Sam Bevans <flyboysam21@gmail.com>
2021-09-09 22:59:17 -0500

    Working on TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.RobotData;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
+@TeleOp
 public class Goodbot_TeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
