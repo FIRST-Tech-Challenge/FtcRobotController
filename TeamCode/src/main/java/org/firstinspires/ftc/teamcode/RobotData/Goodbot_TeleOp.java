d4ae7477ee3c688a3377f49864ba37d39cacd320
PilotSamBevans <PilotSamBevans@gmail.com>
2021-09-10 16:12:03 -0500

    Working on teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
@@ -2,15 +2,25 @@ package org.firstinspires.ftc.teamcode.RobotData;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-@TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp(name = "Good Bot TeleOp ", group = "Opmode")
 public class Goodbot_TeleOp extends LinearOpMode {
+
+    private DcMotor leftFront;
+    private DcMotor leftRight;
+    private DcMotor rightFront;
+    private DcMotor rightRear;
     @Override
-    public void runOpMode() throws InterruptedException {
-        waitForStart();
-        while (opModeIsActive()) {
+    public void runOpMode() throws InterruptedException
+    {
+            waitForStart();
 
+            while(opModeIsActive())
+            {
+                idle();
+            }
 
-            idle();
-        }
     }
 }
