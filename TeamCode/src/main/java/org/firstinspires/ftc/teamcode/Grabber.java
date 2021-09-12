3f89fb27a46f18f52c8848fa7ed8b0559e7beb26
Ethan Sanders <ethanmrcod@gmail.com>
2021-09-12 18:51:26 -0500

    this will be deleted!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
@@ -1,26 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Servo;
 public class Grabber {
-        public Servo GrabberLeft;
-        public Servo GrabberRight;
-        private boolean GrabberClosed = false;
-        private boolean Waiting = false;
-        public void Open() {
-            GrabberLeft.setPosition(0.49);
-            GrabberRight.setPosition(0.405);
-            GrabberClosed = false;
-        }
-        public void Close() {
-            GrabberLeft.setPosition(0.11);
-            GrabberRight.setPosition(.776);
-            GrabberClosed = true;
-        }
-        public void Toggle(boolean rbump) {
-            if (rbump && !Waiting) { Waiting = true; }
-            else if (Waiting && !rbump)  {
-                Waiting = false;
-                if (GrabberClosed) { this.Open(); }
-                else { this.Close();}
-            }
+    public Servo GrabberLeft;
+    public Servo GrabberRight;
+    private boolean GrabberClosed = false;
+    private boolean Waiting = false;
+    public void Open() {
+        GrabberLeft.setPosition(0.49);
+        GrabberRight.setPosition(0.405);
+        GrabberClosed = false;
+    }
+    public void Close() {
+        GrabberLeft.setPosition(0.11);
+        GrabberRight.setPosition(.776);
+        GrabberClosed = true;
+    }
+    public void Toggle(boolean rbump) {
+        if (rbump && !Waiting) { Waiting = true; }
+        else if (Waiting && !rbump)  {
+            Waiting = false;
+            if (GrabberClosed) { this.Open(); }
+            else { this.Close();}
         }
     }
+}
