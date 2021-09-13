93ee083e212eed55f61734904d9d80d0f2de81e7
Sam Bevans <flyboysam21@gmail.com>
2021-09-13 15:27:03 -0500

    Working on TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/Goodbot_TeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.RobotData;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name = "Good Bot TeleOp ", group = "Opmode")
@@ -14,7 +15,21 @@ public class Goodbot_TeleOp extends LinearOpMode {
     private DcMotor rightRear;
     @Override
     public void runOpMode() throws InterruptedException
-    {
+    {                   // Naming the motors in configuration
+            leftFront = hardwareMap.dcMotor.get("leftFront");
+            leftRight = hardwareMap.dcMotor.get("leftRight");
+            rightFront = hardwareMap.dcMotor.get("rightFront");
+            rightRear = hardwareMap.dcMotor.get("rightRear");
+
+                    //Setting the direction of the motors
+            leftFront.setDirection(DcMotor.Direction.FORWARD);
+            leftRight.setDirection(DcMotor.Direction.FORWARD);
+            rightFront.setDirection(DcMotor.Direction.FORWARD);
+            rightRear.setDirection(DcMotor.Direction.FORWARD);
+
+
+
+
             waitForStart();
 
             while(opModeIsActive())
