bc34c64b510f93220d3b8c699394de1a9f9053f5
FourInchKnife <riley.j.mclain@gmail.com>
2021-09-14 02:04:24 -0400

    Changed a javadoc to a regular comment

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcOpModeRegister.java
@@ -63,7 +63,7 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
     public void register(OpModeManager manager) {
 
-        /**
+        /*
          * Any manual OpMode class registrations should go here.
          */
     }
