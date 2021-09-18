b89803ffe58ae915150328fde3a9960bbcf4bbe5
Sam Bevans <flyboysam21@gmail.com>
2021-09-17 23:10:44 -0500

    no beta

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/DeviceMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotData/DeviceMap.java
@@ -23,7 +23,7 @@ public class DeviceMap {
     }
     {
                         // Naming hardware Devices
-                            //test
+
         leftFront = hwMap.get(DcMotor.class, "leftFront");
         leftRear = hwMap.get(DcMotor.class, "leftRear");
         rightFront = hwMap.get(DcMotor.class, "rightFront");
