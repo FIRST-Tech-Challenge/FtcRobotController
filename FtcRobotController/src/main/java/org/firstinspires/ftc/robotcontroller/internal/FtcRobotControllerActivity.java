2f144d5d89b93a470f0f66014dfc0b78e4314d2a
amjadj <amjad.jabs@gmail.com>
2021-09-23 00:04:46 -0500

    Installed Roadrunner0.5.3 and FtcDashboard0.4.2 from AcmeRobotics
    Steps followed to install RoadRunner in FrozenFrenzyPlatform branch from FTCRobotController Master V7.0 on 20210922
    
    Installation instructions at https://acme-robotics.gitbook.io/road-runner/
        Open build.dependencies.gradle and add maven { url = 'https://maven.brott.dev/' } to the end of the repositories block.
        Open TeamCode/build.gradle and add implementation 'com.acmerobotics.roadrunner:core:0.5.3' to the end of the dependencies block.
        Sync the project (Android Studio should prompt you to do this).
    
    Further installation instruction from Method 2: Step 4 - 8 in https://learnroadrunner.com/installing.html#method-2-installing-rr-on-your-project
    	Update build.gradle for Teachcode
    	Download files from https://github.com/acmerobotics/road-runner-quickstart/tree/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode to TeamCode using drag from explorer and drop on Android Studio
    
    Follow instructions for FtcDashboard installation from https://acmerobotics.github.io/ftc-dashboard/gettingstarted
    
    GUI
        Road Runner includes a simple GUI for generating trajectories from pose waypoints and constraints. You can download the latest version from the Releases tab (or build it with ./gradlew shadowJar).
        Download Release Assets from https://github.com/acmerobotics/road-runner/releases/tag/v0.5.3 - road-runner-gui-0.5.3.jar
        Install Java on laptop from https://www.java.com/en/download/
        Run road-runner-gui-0.5.3.jar (using Jre) - select Project Github/FtcRobotController on laptop drive, to open up UI
    
    Plugin
        Road Runner also includes a simple IDEA/Android Studio plugin based upon the GUI. Here are some instructions for building and loading the plugin:
        Download the latest plugin zip from the Release assets or build it with ./gradlew buildPlugin.
        Download Release Assets from https://github.com/acmerobotics/road-runner/releases/tag/v0.5.3 - road-runner-plugin-0.5.3.zip
        In Android Studio, navigate to Settings > Plugins.
        Click the gear icon and select Install Plugin from Disk....
        Select the zip archive from your downloads or plugin/build/distributions.
        Restart Android Studio to activate plugin changes.
        Click on "Road Runner" on the right side of the editor and the tool window should appear.

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -61,6 +61,7 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupMenu;
 import android.widget.TextView;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.google.blocks.ftcrobotcontroller.ProgrammingWebHandlers;
 import com.google.blocks.ftcrobotcontroller.runtime.BlocksOpMode;
 import com.qualcomm.ftccommon.ClassManagerFactory;
@@ -333,6 +334,7 @@ public class FtcRobotControllerActivity extends Activity
         popupMenu.inflate(R.menu.ftc_robot_controller);
         AnnotatedHooksClassFilter.getInstance().callOnCreateMenuMethods(
             FtcRobotControllerActivity.this, popupMenu.getMenu());
+        FtcDashboard.populateMenu(popupMenu.getMenu());
         popupMenu.show();
       }
     });
@@ -410,6 +412,8 @@ public class FtcRobotControllerActivity extends Activity
     checkPreferredChannel();
 
     AnnotatedHooksClassFilter.getInstance().callOnCreateMethods(this);
+
+    FtcDashboard.start();
   }
 
   protected UpdateUI createUpdateUI() {
@@ -483,6 +487,8 @@ public class FtcRobotControllerActivity extends Activity
     RobotLog.cancelWriteLogcatToDisk();
 
     AnnotatedHooksClassFilter.getInstance().callOnDestroyMethods(this);
+
+    FtcDashboard.stop();
   }
 
   protected void bindToService() {
@@ -539,6 +545,7 @@ public class FtcRobotControllerActivity extends Activity
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.ftc_robot_controller, menu);
     AnnotatedHooksClassFilter.getInstance().callOnCreateMenuMethods(this, menu);
+    FtcDashboard.populateMenu(menu);
     return true;
   }
 
@@ -707,6 +714,8 @@ public class FtcRobotControllerActivity extends Activity
 
     AnnotatedHooksClassFilter.getInstance().callWebHandlerRegistrarMethods(this,
         service.getWebServer().getWebHandlerManager());
+
+    FtcDashboard.attachWebServer(service.getWebServer());
   }
 
   private void updateUIAndRequestRobotSetup() {
@@ -753,6 +762,8 @@ public class FtcRobotControllerActivity extends Activity
     AndroidBoard.showErrorIfUnknownControlHub();
 
     AnnotatedHooksClassFilter.getInstance().callOnCreateEventLoopMethods(this, eventLoop);
+
+    FtcDashboard.attachEventLoop(eventLoop);
   }
 
   protected OpModeRegister createOpModeRegister() {
