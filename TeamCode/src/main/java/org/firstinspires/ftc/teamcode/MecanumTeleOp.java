aed987d6d01a5cfb5f8cd179369952365f321213
HP Rev Laptop <ftc14169alpha@gmail.com>
2021-09-27 23:14:18 -0400

    Corrected mecanum spelling in Teleop name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name="MecunumTeleOp", group="test")
+@TeleOp(name="MecanumTeleOp", group="test")
 public class MecanumTeleOp extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
