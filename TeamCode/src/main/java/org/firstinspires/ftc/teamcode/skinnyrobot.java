82a5d60bac5019613f3378b91fb7ce0720db63dc
Jolie <jolie23fish@gmail.com>
2021-10-02 16:58:48 -0400

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skinnyrobot.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "skinny robot")
+public class skinnyrobot extends LinearOpMode {
+    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        motorFrontRight = hardwareMap.dcMotor.get("FR");
+        motorFrontLeft = hardwareMap.dcMotor.get("FL");
+        motorBackLeft = hardwareMap.dcMotor.get("BL");
+        motorBackRight = hardwareMap.dcMotor.get("BR");
+
+        //reverse the needed motors
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        double powerMod = 1.0;
+
+        waitForStart();
+
+        while(opModeIsActive()){
+
+            if(gamepad1.right_bumper){
+                powerMod = 0.5;
+            }else{
+                powerMod = 1.0;
+            }
+
+            double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + (Math.PI/4);
+            double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
+            double rotation = gamepad1.left_stick_x;
+
+            double powerOne = r*Math.sin(angle);
+            double powerTwo = r*Math.cos(angle);
+
+            motorFrontLeft.setPower((powerOne - (rotation))*powerMod);
+            motorFrontRight.setPower((powerTwo + (rotation))*powerMod);
+            motorBackLeft.setPower((powerTwo - (rotation))*powerMod);
+            motorBackRight.setPower((powerOne + (rotation))*powerMod);
+        }
+    }
+}
