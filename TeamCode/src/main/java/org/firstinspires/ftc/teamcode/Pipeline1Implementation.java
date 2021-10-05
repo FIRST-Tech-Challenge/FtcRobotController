2066c3fa06f79b009b86bfd0f544bf54b0e2e311
Ethan Sanders <ethanmrcod@gmail.com>
2021-10-04 19:39:49 -0500

    very basic ring stack implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pipeline1Implementation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Pipeline1Implementation.java
@@ -2,11 +2,19 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.cameratests.pipeline1Detector;
-import com.arcrobotics.ftclib.vision.UGRectDetector;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
 public class Pipeline1Implementation extends LinearOpMode {
     pipeline1Detector pipeline1Detector;
     @Override
-    public void runOpMode(){
+    public void runOpMode() {
         pipeline1Detector = new pipeline1Detector(hardwareMap, "webcam");
+        pipeline1Detector.init();
+        waitForStart();
+        while (!isStopRequested() && opModeIsActive()) {
+            pipeline1Detector.Stack stack = pipeline1Detector.getStack();
+            telemetry.addData("Rings", stack);
+            telemetry.update();
+        }
     }
 }
