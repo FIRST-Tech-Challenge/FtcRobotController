48bd4f6a012dafe6c1c5f28e6fa5a7885aa62d03
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2021-10-06 17:47:09 -0600

    Reorganized last years code into a folder and added @Disabled. Kept specific programs that could be helpful.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/2020-21 FTC Year/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/2020-21 FTC Year/ConceptTensorFlowObjectDetection.java
@@ -52,7 +52,7 @@ import java.util.List;
  * is explained below.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-//@Disabled
+@Disabled
 public class ConceptTensorFlowObjectDetection extends LinearOpMode {
     private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
     private static final String LABEL_FIRST_ELEMENT = "Quad";
