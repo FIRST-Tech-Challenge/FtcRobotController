2d089350221b3d4a84ba32e19731e416302e74c7
Charlie Garfield <charlie.garfield@hotmail.com>
2021-10-07 09:22:47 -0400

    Fixed weird problem somehow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecanumOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecanumOpMode.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.chassis.Chassis;
 import org.firstinspires.ftc.teamcode.chassis.MecanumChassis;
 
-@TeleOp(name = "Mecanum OpMode", group = "Remote")
+@TeleOp(name = "Mecanum OpMode \uD83D\uDE0A", group = "Remote")
 public class MecanumOpMode extends BasicOpMode{
     @Override
     Chassis getChassis() {
