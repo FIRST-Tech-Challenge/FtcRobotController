ad3bbfb1abafe49f612f4f649cb50fa006dee990
Priya Govindarajan <priya.govindarajan@teradata.com>
2021-10-09 15:29:46 -0700

    DetectMarker.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/DetectMarker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/DetectMarker.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode.vision;
 
-public class DetectMarker {
+import org.firstinspires.ftc.robotcore.external.Telemetry
+import org.opencv.core.Mat;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class DetectMarker extends OpenCvPipeline {
+    Telemetry telemetry;
+    public DetectMarker(Telemetry t) {
+        telemetry = t;
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
 }
