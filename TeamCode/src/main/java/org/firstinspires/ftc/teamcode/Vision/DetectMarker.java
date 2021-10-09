df3b3926a5f98668a16a184350c7ab09663a51b8
Priya Govindarajan <priya.govindarajan@teradata.com>
2021-10-09 15:33:49 -0700

    fixed error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
@@ -2,16 +2,20 @@ package org.firstinspires.ftc.teamcode.Vision;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 public class DetectMarker extends OpenCvPipeline {
     Telemetry telemetry;
+    Mat mat = new Mat();
     public DetectMarker(Telemetry t) {
         telemetry = t;
     }
 
     @Override
     public Mat processFrame(Mat input) {
+        Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
         return null;
     }
 }
