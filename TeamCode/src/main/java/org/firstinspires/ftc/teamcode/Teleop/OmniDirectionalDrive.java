1f0b8267e65208917aae9aa3cf5f4fa40e76927e
Priya Govindarajan <priya.govindarajan@teradata.com>
2021-10-09 14:50:12 -0700

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OmniDirectionalDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/OmniDirectionalDrive.java
@@ -5,11 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
-import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+// import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+// import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+// import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
-import org.firstinspires.ftc.teamcode.SubSystems.Robot;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
 
 import java.io.IOException;
 
@@ -17,8 +17,8 @@ import java.io.IOException;
  * Created by Elijah Rowe (Skystone 2020)
  */
 
-@TeleOp(name="OmniDirectionalDrive2", group="Assisted Driving")
-public class OmniDirectionalDrive2 extends LinearOpMode {
+@TeleOp(name="OmniDirectionalDrive", group="Assisted Driving")
+public class OmniDirectionalDrive extends LinearOpMode {
     private Robot robot;
     private BNO055IMU imu;
     Orientation lastAngles = new Orientation();
