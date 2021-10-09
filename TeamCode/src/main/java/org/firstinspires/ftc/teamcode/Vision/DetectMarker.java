f63481a652dec2143eb98d1db5812cb8b752e46b
Priya Govindarajan <priya.govindarajan@teradata.com>
2021-10-09 15:50:33 -0700

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
@@ -47,7 +47,54 @@ public class DetectMarker extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
         Scalar lowHSV = new Scalar(23, 50, 70);
-        Scalar highHSV = new Scalar(23, 50, 70);
-        return null;
+        Scalar highHSV = new Scalar(32, 255, 255);
+
+        Core.inRange(mat, lowHSV, highHSV, mat);
+
+        Mat left = mat.submat(LEFT_RECT);
+        Mat middle = mat.submat(MIDDLE_RECT);
+        Mat right = mat.submat(RIGHT_RECT);
+
+        double leftValue = Core.sumElems(left).val[0] / LEFT_RECT.area() / 255;
+        double middleValue = Core.sumElems(left).val[0] / MIDDLE_RECT.area() / 255;
+        double rightValue = Core.sumElems(right).val[0] / RIGHT_RECT.area() / 255;
+
+        left.release();
+        right.release();
+
+        telemetry.addData("Left raw value", (int) Core.sumElems(left).val[0]);
+        telemetry.addData("Middle raw value", (int) Core.sumElems(left).val[0]);
+        telemetry.addData("Right raw value", (int) Core.sumElems(right).val[0]);
+        telemetry.addData("Left percentage", Math.round(leftValue * 100) + "%");
+        telemetry.addData("Middle percentage", Math.round(leftValue * 100) + "%");
+        telemetry.addData("Right percentage", Math.round(rightValue * 100) + "%");
+
+        boolean markerLeft = leftValue > PERCENT_COLOR_THRESHOLD;
+        boolean markerMiddle = middleValue > PERCENT_COLOR_THRESHOLD;
+        boolean markerRight = rightValue > PERCENT_COLOR_THRESHOLD;
+
+        if (stoneLeft && stoneRight) {
+            location = Location.NOT_FOUND;
+            telemetry.addData("Skystone Location", "not found");
+        }
+        else if (stoneLeft) {
+            location = Location.RIGHT;
+            telemetry.addData("Skystone Location", "right");
+        }
+        else {
+            location = Location.LEFT;
+            telemetry.addData("Skystone Location", "left");
+        }
+        telemetry.update();
+
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_GRAY2RGB);
+
+        Scalar colorStone = new Scalar(255, 0, 0);
+        Scalar colorSkystone = new Scalar(0, 255, 0);
+
+        Imgproc.rectangle(mat, LEFT_ROI, location == Location.LEFT? colorSkystone:colorStone);
+        Imgproc.rectangle(mat, RIGHT_ROI, location == Location.RIGHT? colorSkystone:colorStone);
+
+        return mat;
     }
 }
