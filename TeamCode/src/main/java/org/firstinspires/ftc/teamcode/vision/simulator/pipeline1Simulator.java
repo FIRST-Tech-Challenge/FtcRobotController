20772f1fc29bae24809620519cc74aaa52cf5bd9
Ethan Sanders <ethanmrcod@gmail.com>
2021-10-10 18:31:02 -0500

    computer vision that hopefully actually works!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/simulator/pipeline1Simulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/simulator/pipeline1Simulator.java
@@ -14,22 +14,27 @@ public class pipeline1Simulator extends OpenCvPipeline {
     //We declare the mats ontop so we can reuse them later to avoid memory leaks
     private Mat matYCrCb = new Mat();
     private Mat matCbBottom = new Mat();
+    private Mat matCbMiddle = new Mat();
     private Mat matCbTop = new Mat();
     private Mat topBlock = new Mat();
+    private Mat middleBlock = new Mat();
     private Mat bottomBlock = new Mat();
 
     //Where the average CB value of the rectangles are stored
-    private double topAverage;
-    private double bottomAverage;
+    private double topAverage = 0;
+    private double middleAverage = 0;
+    private double bottomAverage = 0;
 
     //The max difference allowed inside the rectangles
     private int threshold = 15;
 
     //The position related to the screen
     private double topRectWidthPercentage = 0.25;
-    private double topRectHeightPercentage = 0.25;
+    private double topRectHeightPercentage = 0.50;
+    private double middleRectWidthPercentage = 0.50;
+    private double middleRectHeightPercentage = 0.50;
     private double bottomRectWidthPercentage = 0.75;
-    private double bottomRectHeightPercentage = 0.75;
+    private double bottomRectHeightPercentage = 0.50;
 
     //The width and height of the rectangles in terms of pixels
     private int rectangleWidth = 10;
@@ -52,17 +57,25 @@ public class pipeline1Simulator extends OpenCvPipeline {
         Imgproc.cvtColor(input, matYCrCb, Imgproc.COLOR_RGB2YCrCb);
         telemetry.addData("Telemetry","Telemetry");
         telemetry.addData("topBoxAverage",topAverage);
+        telemetry.addData("topBoxAverage",middleAverage);
         telemetry.addData("bottomBoxAverage",bottomAverage);
         // USE Z-SCORE
         telemetry.update();
         //The points needed for the rectangles are calculated here
+        Scalar red = new Scalar(255,0,0);
+        Scalar yellow = new Scalar(255,255,0);
         Rect topRect = new Rect(
                 (int) (matYCrCb.width() * topRectWidthPercentage),
                 (int) (matYCrCb.height() * topRectHeightPercentage),
                 rectangleWidth,
                 rectangleHeight
         );
-
+        Rect middleRect = new Rect(
+                (int) (matYCrCb.width() * middleRectWidthPercentage),
+                (int) (matYCrCb.height() * middleRectHeightPercentage),
+                rectangleWidth,
+                rectangleHeight
+        );
         Rect bottomRect = new Rect(
                 (int) (matYCrCb.width() * bottomRectWidthPercentage),
                 (int) (matYCrCb.height() * bottomRectHeightPercentage),
@@ -71,24 +84,57 @@ public class pipeline1Simulator extends OpenCvPipeline {
         );
 
         //The rectangle is drawn into the mat
-        drawRectOnToMat(input, topRect, new Scalar(255, 0, 0));
-        drawRectOnToMat(input, bottomRect, new Scalar(100, 255, 0));
+        switch (mostDifferent(topAverage,middleAverage,bottomAverage)) {
+            case 1:
+                drawRectOnToMat(input, topRect, yellow);
+                drawRectOnToMat(input, middleRect, red);
+                drawRectOnToMat(input, bottomRect, red);
+                break;
+            case 2:
+                drawRectOnToMat(input, topRect, red);
+                drawRectOnToMat(input, middleRect, yellow);
+                drawRectOnToMat(input, bottomRect, red);
+                break;
+            case 3:
+                drawRectOnToMat(input, topRect, red);
+                drawRectOnToMat(input, middleRect, red);
+                drawRectOnToMat(input, bottomRect, yellow);
+                break;
+        }
 
         //We crop the image so it is only everything inside the rectangles and find the cb value inside of them
         topBlock = matYCrCb.submat(topRect);
+        middleBlock = matYCrCb.submat(middleRect);
         bottomBlock = matYCrCb.submat(bottomRect);
-        Core.extractChannel(bottomBlock, matCbBottom, 0);
         Core.extractChannel(topBlock, matCbTop, 0);
+        Core.extractChannel(middleBlock,matCbMiddle,0);
+        Core.extractChannel(bottomBlock, matCbBottom, 0);
 
         //We take the average
-        Scalar bottomMean = Core.mean(matCbBottom);
         Scalar topMean = Core.mean(matCbTop);
+        Scalar middleMean = Core.mean(matCbMiddle);
+        Scalar bottomMean = Core.mean(matCbBottom);
 
-        bottomAverage = bottomMean.val[0];
         topAverage = topMean.val[0];
+        middleAverage = middleMean.val[0];
+        bottomAverage = bottomMean.val[0];
+
 
         //return the mat to be shown onto the screen
         return input;
+    }
+
+    private int mostDifferent(double box1, double box2, double box3) {
+        double boxMean = (box1+box2+box3)/3;
+        double diffBox1 = Math.abs(boxMean - box1);
+        double diffBox2 = Math.abs(boxMean - box2);
+        double diffBox3 = Math.abs(boxMean - box3);
+        ArrayList<Double> list = new ArrayList<>();
+        list.add(diffBox1);
+        list.add(diffBox2);
+        list.add(diffBox3);
+        double boxNum = Collections.max(list);
+        return list.indexOf(boxNum) + 1;
     }
 
     /**
@@ -98,18 +144,6 @@ public class pipeline1Simulator extends OpenCvPipeline {
      * @param rect  The rectangle
      * @param color The color the rectangle will be
      */
-    private void mostDifferent(double box1, double box2, double box3) {
-        private double boxMean = (box1+box2+box3)/3;
-        private double diffBox1 = Math.abs(boxMean - box1);
-        private double diffBox2 = Math.abs(boxMean - box2);
-        private double diffBox3 = Math.abs(boxMean - box3);
-        ArrayList<Double> set = new ArrayList<>();
-        set.add(diffBox1);
-        set.add(diffBox2);
-        set.add(diffBox3);
-        private double boxNum = Collections.max(set);
-        return set.indexOf(boxNum) + 1;
-    }
     private void drawRectOnToMat(Mat mat, Rect rect, Scalar color) {
         Imgproc.rectangle(mat, rect, color, 1);
     }
@@ -117,6 +151,8 @@ public class pipeline1Simulator extends OpenCvPipeline {
     public double getTopAverage() {
         return topAverage;
     }
+
+    public double getMiddleAverage() { return middleAverage; }
 
     public double getBottomAverage() {
         return bottomAverage;
