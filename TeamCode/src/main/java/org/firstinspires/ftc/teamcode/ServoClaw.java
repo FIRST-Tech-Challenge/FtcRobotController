f2a2d9e310ff209b3b876e37e0e71ee44f8c6a09
Jack Turula <jackturula@Jacks-MacBook-Air-2.local>
2021-10-11 16:26:26 -0500

    updated class name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoClaw.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Servo;
 
 //class for operating claw
-public class Enumclaw {
+public class ServoClaw {
 
 //var declarations
     private Servo claw;
@@ -10,14 +10,14 @@ public class Enumclaw {
     private float maxturn;
 
     //constructor accepts min and max turn params
-    public Enumclaw(Servo claw, float minturn1, float maxturn1) {
+    public ServoClaw(Servo claw, float minturn1, float maxturn1) {
         this.minturn = minturn1;
         this.maxturn = maxturn1;
     }
     //turns servo in range of min and max; percentage is used for pos
     public void turninrange(float pos) {
 
-        //corrects incorrect nums
+        //corrects out of domain pos values
         if(pos>1){
             pos=1;
         }
