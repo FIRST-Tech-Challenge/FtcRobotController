20351984a1d95223e70ac6a39f92140b466dbfdb
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-10-10 19:48:27 -0500

    more attachment work (incomplete)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/hardware/attachment/api/Attachment.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/hardware/attachment/api/Attachment.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.core.hardware.attachment.api;
 
 public interface Attachment {
+    void moveOnThisThread(double position, double speed);
+
     void move(double position, double speed);
 
     void move(double position);
