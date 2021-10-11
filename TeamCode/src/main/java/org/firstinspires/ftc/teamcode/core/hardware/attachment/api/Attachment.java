8ee273b4f6425ed00689cad45f9d4c4457494b60
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-10-11 18:33:48 -0500

    not sure about attachments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/hardware/attachment/api/Attachment.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.core.hardware.attachment.api;
-
-public interface Attachment {
-    void moveOnThisThread(double position, double speed);
-
-    void move(double position, double speed);
-
-    void move(double position);
-}
