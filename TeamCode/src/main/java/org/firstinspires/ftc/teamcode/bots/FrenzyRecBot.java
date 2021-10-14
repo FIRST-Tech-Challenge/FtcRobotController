c755dad00d819832fe9583bb8d4e333cff4db283
Rudra Singh <rudra0502@gmail.com>
2021-10-13 20:06:41 -0400

    added recog opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/FrenzyRecBot.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.bots;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.CVRec.CVFrenzyPipeline;
+
+public class FrenzyRecBot extends CVFrenzyPipeline {
+
+    /* Constructor */
+    public FrenzyRecBot() {
+
+    }
+
+    @Override
+    public void init(LinearOpMode owner, HardwareMap ahwMap, Telemetry telemetry) throws Exception {
+        super.init(owner, ahwMap, telemetry);
+    }
+
+}
