d0c53723c4e3b7c1373c5490c2a328c37d478190
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-10-14 19:50:35 -0500

    more threading aaaaaaaaaaa

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EventTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EventTest.java
@@ -8,9 +8,6 @@ import org.firstinspires.ftc.teamcode.core.event.EventThread;
 import org.firstinspires.ftc.teamcode.core.movement.api.StrafingMovement;
 import org.firstinspires.ftc.teamcode.core.movement.impl.StrafedMovementImpl;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @Autonomous
 public class EventTest extends LinearOpMode {
     public EventThread eventThread = new EventThread();
@@ -18,24 +15,26 @@ public class EventTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         int runTime = 1000;
-        double power = 0.3;
-        int restTime = 1000;
+//        double power = 0.3;
+//        int restTime = 1000;
 
+        eventThread.start();
         waitForStart();
 
         Object object = new Object();
-        StrafingMovement strafedMovement = new StrafedMovementImpl(hardwareMap);
-        telemetry.addLine("Driving forward for " + runTime / 1000 + " second(s).");
+//        StrafingMovement strafedMovement = new StrafedMovementImpl(hardwareMap);
+        telemetry.addLine("Doing nothing for " + runTime / 1000 + " second(s).");
         telemetry.update();
-        strafedMovement.drive(power);
+//        strafedMovement.drive(power);
 
+        //            strafedMovement.stop();
         eventThread.addEvent(Event.createEventWithMilis(() -> {
-            strafedMovement.stop();
+            telemetry.addLine("done!");
             object.notifyAll();
         }, runTime));
 
-        object.wait();
-
-
+        synchronized (object) {
+            object.wait();
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/event/EventThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/event/EventThread.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.core.event;
 
 import java.util.Iterator;
 import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Thread to handle {@link Event events} as requests. Watch out, if it is overloaded with requests
@@ -9,62 +11,79 @@ import java.util.TreeSet;
  */
 public class EventThread extends Thread {
     private final TreeSet<Event> queue = new TreeSet<>();
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Object waitObject = new Object();
 
     public void run() {
+        lock.lock();
         while (!this.isInterrupted()) {
             boolean queueEmpty;
-            synchronized (queue) {
-                queueEmpty = queue.isEmpty();
-            }
+            long fullNanos;
+            long ms;
+            int ns;
+
+            queueEmpty = queue.isEmpty();
 
             if (!queueEmpty) {
+                lock.unlock();
                 try {
-                    queue.wait();
+                    synchronized (waitObject) {
+                        waitObject.wait();
+                    }
                 } catch (InterruptedException e) {
                     break;
                 }
+                lock.lock();
             }
 
             long currentTime = Event.time.nanoseconds();
             long waitUntil = 0;
 
-            synchronized (queue) {
-                Iterator<Event> iterator = queue.iterator();
+            Iterator<Event> iterator = queue.iterator();
 
-                while (iterator.hasNext()) {
-                    Event event = iterator.next();
-
-                    if (event.runTime <= currentTime) {
-                        iterator.remove();
-                        // run the event
-                        event.listener.run();
-                    } else {
-                        waitUntil = event.runTime;
-                    }
+            while (iterator.hasNext()) {
+                Event event = iterator.next();
+                if (event.runTime <= currentTime) {
+                    iterator.remove();
+                    // run the event
+                    event.listener.run();
+                } else {
+                    waitUntil = event.runTime;
                 }
             }
 
-            try {
-                long fullNanos = waitUntil - currentTime;
-                long ms = fullNanos / 1000000;
-                int ns = (int) (fullNanos % 1000000);
+            fullNanos = waitUntil - currentTime;
+            ms = fullNanos / 1000000;
+            ns = (int) (fullNanos % 1000000);
 
+            lock.unlock();
+            try {
                 queue.wait(ms, ns);
             } catch (InterruptedException e) {
                 break;
             }
+            lock.lock();
         }
     }
 
     /**
-     * Adds an event to the queue
+     * Adds an event to the queue.
      * @param event The event you want to add.
+     * @return Whether the event was successfully added.
      */
-    public void addEvent(Event event) {
-        synchronized (queue) {
-            queue.add(event);
-            queue.notifyAll();
+    public boolean addEvent(Event event) {
+        try {
+            if (lock.tryLock(10, TimeUnit.SECONDS)) {
+                queue.add(event);
+                waitObject.notifyAll();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+        return false;
     }
 
 }
