2a28133e7e10df15c4392cf1288def5883a82a76
albonec <albonec@ohs.stanford.edu>
2021-10-16 16:34:10 -0700

    added strafe functionality to MecanumDriveTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MecanumDriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MecanumDriveTest.java
@@ -5,16 +5,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(name="MecanumDriveTest")
 public class MecanumDriveTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("fl");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("bl");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("fr");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("br");
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
@@ -26,23 +26,21 @@ public class MecanumDriveTest extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double y = gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
+            double r = Math.hypot(x, y);
+            double robotAngle = Math.atan2(y, x) - Math.PI / 4;
+            double lrPower = r * Math.sin(robotAngle) + rx;
+            double lfPower = r * Math.cos(robotAngle) + rx;
+            double rrPower = r * Math.cos(robotAngle) - rx;
+            double rfPower = r * Math.sin(robotAngle) - rx;
 
-            motorFrontLeft.setPower(frontLeftPower);
-            motorBackLeft.setPower(backLeftPower);
-            motorFrontRight.setPower(frontRightPower);
-            motorBackRight.setPower(backRightPower);
+            motorFrontLeft.setPower(lfPower);
+            motorBackLeft.setPower(lrPower);
+            motorFrontRight.setPower(rfPower);
+            motorBackRight.setPower(rrPower);
         }
     }
 }
