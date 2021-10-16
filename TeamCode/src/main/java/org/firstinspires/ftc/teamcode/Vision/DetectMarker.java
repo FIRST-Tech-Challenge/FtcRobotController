308a4d8e4c76980b79968224edfa89b19c959511
albonec <albonec@ohs.stanford.edu>
2021-10-15 17:13:10 -0700

    idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/DetectMarker.java
@@ -33,7 +33,7 @@ class DetectionPipeline extends OpenCvPipeline {
 
     public enum AllianceColor { // TODO: See if this works as a solution for allianceColor standardization
         RED, // Inserting it here because its the only place allianceColor seems to be referenced at.
-        BLUE // undefined is not needed
+        BLUE // undefined is not needed.
     }
 
     private AllianceColor allianceColor;
