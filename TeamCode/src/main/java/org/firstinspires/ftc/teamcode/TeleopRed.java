72163e1feb6f8a9b47d33d28ed903bd0d2e7fec0
DraskiTheDragon <deanbras@gmail.com>
2021-10-16 10:59:31 -0600

    BlackSwanTeleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopRed.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="TeleopRed")
 
