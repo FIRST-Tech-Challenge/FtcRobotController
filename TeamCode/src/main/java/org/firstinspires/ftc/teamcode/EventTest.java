6505ce3a02973ee575f28c7d18c68aacf1e24017
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-10-17 19:44:27 -0500

    IM SO CLOSE AAAAAAAAA

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EventTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EventTest.java
@@ -8,9 +8,19 @@ import org.firstinspires.ftc.teamcode.core.event.EventThread;
 import org.firstinspires.ftc.teamcode.core.movement.api.StrafingMovement;
 import org.firstinspires.ftc.teamcode.core.movement.impl.StrafedMovementImpl;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 @Autonomous
 public class EventTest extends LinearOpMode {
     public EventThread eventThread = new EventThread();
+
+    public EventTest() {
+        this.msStuckDetectStart = Integer.MAX_VALUE;
+        this.msStuckDetectInit = Integer.MAX_VALUE;
+        this.msStuckDetectInitLoop = Integer.MAX_VALUE;
+        this.msStuckDetectLoop = Integer.MAX_VALUE;
+        this.msStuckDetectStop = Integer.MAX_VALUE;
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -21,20 +31,22 @@ public class EventTest extends LinearOpMode {
         eventThread.start();
         waitForStart();
 
-        Object object = new Object();
 //        StrafingMovement strafedMovement = new StrafedMovementImpl(hardwareMap);
         telemetry.addLine("Doing nothing for " + runTime / 1000 + " second(s).");
         telemetry.update();
 //        strafedMovement.drive(power);
 
+        AtomicBoolean atomicBoolean = new AtomicBoolean();
+
         //            strafedMovement.stop();
         eventThread.addEvent(Event.createEventWithMilis(() -> {
             telemetry.addLine("done!");
-            object.notifyAll();
+            telemetry.update();
+            atomicBoolean.set(true);
         }, runTime));
 
-        synchronized (object) {
-            object.wait();
+        while (true) {
+            if(atomicBoolean.get()) break;
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/event/EventThread.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/event/EventThread.java
@@ -1,41 +1,33 @@
 package org.firstinspires.ftc.teamcode.core.event;
 
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Thread to handle {@link Event events} as requests. Watch out, if it is overloaded with requests
  * and its not able to fulfill them faster than they are sent, it will not finish the queue.
  */
 public class EventThread extends Thread {
-    private final TreeSet<Event> queue = new TreeSet<>();
-    private final ReentrantLock lock = new ReentrantLock();
+    private final SortedSet<Event> queue = Collections.synchronizedSortedSet(new TreeSet<>());
     private final Object waitObject = new Object();
 
     public void run() {
-        lock.lock();
         while (!this.isInterrupted()) {
-            boolean queueEmpty;
             long fullNanos;
             long ms;
             int ns;
 
-            queueEmpty = queue.isEmpty();
-
-            if (!queueEmpty) {
-                lock.unlock();
-                try {
-                    synchronized (waitObject) {
+            synchronized (waitObject) {
+                while (!queue.isEmpty()) {
+                    try {
                         waitObject.wait();
+                    } catch (InterruptedException e) {
+                        break;
                     }
-                } catch (InterruptedException e) {
-                    break;
                 }
-                lock.lock();
             }
-
             long currentTime = Event.time.nanoseconds();
             long waitUntil = 0;
 
@@ -47,43 +39,36 @@ public class EventThread extends Thread {
                     iterator.remove();
                     // run the event
                     event.listener.run();
-                } else {
+                } else if (waitUntil > event.runTime){
                     waitUntil = event.runTime;
                 }
             }
 
-            fullNanos = waitUntil - currentTime;
-            ms = fullNanos / 1000000;
-            ns = (int) (fullNanos % 1000000);
+            if(waitUntil != 0) {
+                fullNanos = waitUntil - currentTime;
+                ms = fullNanos / 1000000;
+                ns = (int) (fullNanos % 1000000);
 
-            lock.unlock();
-            try {
-                queue.wait(ms, ns);
-            } catch (InterruptedException e) {
-                break;
+                synchronized (waitObject) {
+                    try {
+                        waitObject.wait(ms, ns);
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
             }
-            lock.lock();
         }
     }
 
     /**
      * Adds an event to the queue.
      * @param event The event you want to add.
-     * @return Whether the event was successfully added.
      */
-    public boolean addEvent(Event event) {
-        try {
-            if (lock.tryLock(10, TimeUnit.SECONDS)) {
-                queue.add(event);
-                waitObject.notifyAll();
-                return true;
-            } else {
-                return false;
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    public void addEvent(Event event) {
+        synchronized (waitObject) {
+            queue.add(event);
+            waitObject.notify();
         }
-        return false;
     }
 
 }
