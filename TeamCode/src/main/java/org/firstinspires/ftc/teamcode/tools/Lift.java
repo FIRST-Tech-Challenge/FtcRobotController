0408f9016be6db58f7796bdbc1c4bcdf7ad134c1
Ethan Sanders <ethanmrcod@gmail.com>
2021-10-18 18:54:58 -0500

    even even even even better lift code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/Lift.java
@@ -5,7 +5,7 @@ import androidx.annotation.NonNull;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-
+import com.qualcomm.robotcore.hardware.Servo;
 public class Lift {
 
     /**
@@ -15,15 +15,19 @@ public class Lift {
      */
     public Lift(@NonNull HardwareMap map, Telemetry telemetry, GamepadEx toolGamepad) {
         this.liftMotor = map.get(DcMotor.class,"liftMotor");
+        this.armServo = map.get(Servo.class,"armServo");
         this.encoderOffset = liftMotor.getCurrentPosition() * -1;
         this.bottomSensor = map.get(DigitalChannel.class,"bottomSensor");
+        this.bottomSensor.setMode(DigitalChannel.Mode.INPUT);
         this.topSensor = map.get(DigitalChannel.class,"topSensor");
+        this.topSensor.setMode(DigitalChannel.Mode.INPUT);
         this.telemetry = telemetry;
         this.stick = toolGamepad;
     }
 
     private final Telemetry telemetry;
     private final DcMotor liftMotor;
+    private final Servo armServo;
     private final DigitalChannel bottomSensor;
     private final DigitalChannel topSensor;
     private final GamepadEx stick;
@@ -38,6 +42,7 @@ public class Lift {
         telemetry.addData("bottomSensor",bottomSensor.getState());
         telemetry.addData("topSensor", topSensor.getState());
         telemetry.addData("motor encoder",curPos);
+        telemetry.addData("armServo",armServo.getPosition());
         if ((stickValue >= 0.05 && !topSensor.getState()) || (stickValue <= -0.05 && !bottomSensor.getState())) {
             liftMotor.setPower(stickValue);
         } else if (bottomSensor.getState()) {
