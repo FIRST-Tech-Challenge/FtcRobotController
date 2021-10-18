74462ce7fc8152583df2c68e15f66e8587850e64
Ethan Sanders <ethanmrcod@gmail.com>
2021-10-17 19:32:05 -0500

    fixed the logic and fixed a year old typo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/Lift.java
@@ -3,37 +3,10 @@ import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import androidx.annotation.NonNull;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.gamepad.TriggerReader;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Lift {
-    private final Telemetry telemetry;
-    private final DcMotor liftMotor;
-    private final DigitalChannel bottomSensor;
-    private final DigitalChannel topSensor;
-    private final GamepadEx stick;
-    private final double  afloatValue = 0.05;
-    public void update() {
-        telemetry.addData("left stick",stick.getLeftY());
-        telemetry.addData("lift motor power", liftMotor.getPower());
-        telemetry.addData("bottomSensor",bottomSensor.getState());
-        telemetry.addData("topSensor", topSensor.getState());
-        if (!topSensor.getState()) {
-            double stickValue = stick.getLeftY();
-            if (stickValue >= 0.05 || stickValue <= -0.05) {
-                liftMotor.setPower(stickValue);
-            } else if (bottomSensor.getState()) {
-                liftMotor.setPower(0);
-            } else {
-                liftMotor.setPower(afloatValue);
-            }
-        } else {
-            liftMotor.setPower(afloatValue);
-        }
-    }
 
     /**
      *
@@ -47,5 +20,28 @@ public class Lift {
         this.topSensor = map.get(DigitalChannel.class,"topSensor");
         this.telemetry = telemetry;
         this.stick = toolGamepad;
+    }
+
+    private final Telemetry telemetry;
+    private final DcMotor liftMotor;
+    private final DigitalChannel bottomSensor;
+    private final DigitalChannel topSensor;
+    private final GamepadEx stick;
+
+    public void update() {
+        final double afloatValue = 0.05;
+        final double stickValue = stick.getLeftY();
+        telemetry.addData("left stick",stickValue);
+        telemetry.addData("lift motor power", liftMotor.getPower());
+        telemetry.addData("bottomSensor",bottomSensor.getState());
+        telemetry.addData("topSensor", topSensor.getState());
+        telemetry.addData("motor encoder",liftMotor.getCurrentPosition());
+        if ((stickValue >= 0.05 && !topSensor.getState()) || stickValue <= 0.05 && !bottomSensor.getState()) {
+            liftMotor.setPower(stickValue);
+        } else if (bottomSensor.getState()) {
+            liftMotor.setPower(0);
+        } else {
+            liftMotor.setPower(afloatValue);
+        }
     }
 }
