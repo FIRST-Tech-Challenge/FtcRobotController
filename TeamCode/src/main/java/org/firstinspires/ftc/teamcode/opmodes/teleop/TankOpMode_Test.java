8e5f270ff652e4176f60ceb6221149e3303fcc0b
AndroidUser4438 <michael@lachut.com>
2021-10-19 14:54:17 -0400

    óno
    Signed-off-by: AndroidUser4438 <michael@lachut.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TankOpMode_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TankOpMode_Test.java
@@ -9,7 +9,7 @@ import org.firstinspires.ftc.teamcode.utils.RobotWithSpinner;
 import org.firstinspires.ftc.teamcode.utils.Tank;
 import org.firstinspires.ftc.teamcode.utils.TankRobot;
 
-@TeleOp(name="Basic OpMode with spinner attachment", group="Special Hardware")
+@TeleOp(name="Basic OpMode with Tank Robot", group="Special Hardware")
 public class TankOpMode_Test extends LinearOpMode {
 
     // Declare OpMode members.
