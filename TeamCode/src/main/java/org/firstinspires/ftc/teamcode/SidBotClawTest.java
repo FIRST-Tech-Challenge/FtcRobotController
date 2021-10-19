161ce20feeadd25066ed40322d01c89107da0a3d
Charlie Gray <charliegray@Charlies-MacBook-Pro.local>
2021-10-18 19:04:01 -0500

    claw+arm functionality sidbot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SidBotClawTest.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="SidBot ClawTest", group="tests"))
+public class SidBot extends OpMode {
+}
