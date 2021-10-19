5acf4e1e41c6f675c577394269c0c47984df445f
Ashwin <arihant2math@gmail.com>
2021-10-18 19:35:36 -0700

    refractored control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/control/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/control/Control.java
@@ -1,8 +1,10 @@
-package org.firstinspires.ftc.teamcode.Subsystems;
+package org.firstinspires.ftc.teamcode.Subsystems.control;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Subsystems.MinorSubsystem;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
 
 public class Control extends MinorSubsystem {
 
