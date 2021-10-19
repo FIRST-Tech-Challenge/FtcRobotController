9af47422bb52ff74b3b1904b2c26f827729546e5
Dwalker1000 <25walkerd@gmail.com>
2021-10-19 16:53:15 -0500

    yeeeeeeet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuckMyLifeOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FuckMyLifeOp.java
@@ -16,21 +16,6 @@ public class FuckMyLifeOp extends OpMode {
 
     @Override
     public void loop() {
-        //left m0,m2
-        //right m1,m3
-        double drive = gamepad1.left_stick_y*.5;
-        double turn = gamepad1.left_stick_x*.5;
-        double strafe = gamepad1.right_stick_y*.5;
-//        double MIN_ANGLE = 0;
-//        double MAX_ANGLE = 180;
-//        double degreeRange = robot.servo.getAngle();
-//        double Position = 69.420;
-//
-//
-//        robot.servo.setRange(MIN_ANGLE, MAX_ANGLE);
-//
-//        robot.servo.setPosition(Position);
-
-        robot.mecanum.driveRobotCentric(strafe, drive, turn);
+        robot.mecanum.driveRobotCentric(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
     }
 }
