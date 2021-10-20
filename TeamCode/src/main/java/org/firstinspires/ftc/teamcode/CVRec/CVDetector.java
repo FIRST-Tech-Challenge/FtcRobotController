a44c683551c4e02cf1d6a5b74a64d883b87c10b2
NeeravS <neerav.fsaps@gmail.com>
2021-10-20 19:24:13 -0400

    Merge Detector to master

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVRec/CVDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CVRec/CVDetector.java
@@ -98,6 +98,7 @@ public class CVDetector {
                 public void onClose() {
                     Log.d(TAG, "CV Detector camera closed");
                 }
+
             });
         }
     }
