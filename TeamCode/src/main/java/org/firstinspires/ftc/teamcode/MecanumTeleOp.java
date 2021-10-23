74afe53f6e8e1a1186a1fe4a6ce9046978cdaf70
Colby <cms0679@outlook.com>
2021-10-23 11:26:41 -0400

    Cleaned up imports

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name="MecanumTeleOp", group="test")
 public class MecanumTeleOp extends LinearOpMode {
