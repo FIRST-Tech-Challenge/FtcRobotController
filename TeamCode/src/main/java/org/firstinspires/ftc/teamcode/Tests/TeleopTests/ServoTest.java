22dcb80424fda5517772e6f4ffff92b7fe5af170
albonec <70042211+albonec@users.noreply.github.com>
2021-10-23 19:38:33 -0700

    minor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TeleopTests/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TeleopTests/ServoTest.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
+/** made by Alessandro Bonecchi on 10.23.2021 **/
+
 @TeleOp(name = "Servo Test")
 @Disabled
 public class ServoTest extends LinearOpMode {
