82de1226f7211f946390fa010487794d785f829b
Megiddo Lions <megiddo.lions@gmail.com>
2021-10-24 17:31:39 +0300

    dosfsfs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive.java
@@ -15,24 +15,24 @@ public class TestDrive extends OpMode {
 
     @Override
     public void init() {
-        frontLeft = hardwareMap.dcMotor.get("TopLeft");
-        frontRight = hardwareMap.dcMotor.get("TopRight");
-        rearLeft = hardwareMap.dcMotor.get("BackLeft");
-        rearRight = hardwareMap.dcMotor.get("BackRight");
+        frontLeft = hardwareMap.dcMotor.get("FrontLeft");
+        frontRight = hardwareMap.dcMotor.get("FrontRight");
+        rearLeft = hardwareMap.dcMotor.get("RearLeft");
+        rearRight = hardwareMap.dcMotor.get("RearRight");
 
 
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
     }
 
     @Override
     public void loop() {
-        frontLeft.setPower(-gamepad1.left_stick_y / 3);
-        rearLeft.setPower(-gamepad1.left_stick_y / 3);
+        frontLeft.setPower(-gamepad1.left_stick_y);
+        rearLeft.setPower(-gamepad1.left_stick_y);
 
-        frontRight.setPower(-gamepad1.right_stick_y /3);
-        rearRight.setPower(-gamepad1.right_stick_y / 3);
+        frontRight.setPower(-gamepad1.right_stick_y);
+        rearRight.setPower(-gamepad1.right_stick_y);
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/YoavoushAndMaayanoushsAmazingWebcamAdventure3.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.openftc.easyopencv.OpenCvCamera;
+
+public class YoavoushAndMaayanoushsAmazingWebcamAdventure3 extends OpMode {
+
+public final OpenCvCamera YoavCanSeeeee;
+
+    public YoavoushAndMaayanoushsAmazingWebcamAdventure3(OpenCvCamera yoavCanSeeeee) {
+        YoavCanSeeeee = yoavCanSeeeee;
+    }
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
