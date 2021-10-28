1eafba8200e9061d67462bf176f982f99bf73986
devanchoksi <devan.choksi@students.tka.org>
2021-10-27 17:29:58 -0700

    started code for outtake

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/Outtake.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/Outtake.java
@@ -7,10 +7,21 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Outtake {
     public Outtake (HardwareMap hardwareMap) {
-        motor1 = hardwareMap.dcMotor.get("motorOuttake")
-
-
+        motor1 = hardwareMap.dcMotor.get("motorOuttake");
+        servo1 = hardwareMap.servo.get("servoOuttake");
+        motor1.setPower(0.3);
     }
+
+
+    public void FlapOpen() {
+        servo1.setPosition(0.2);
+    }
+
+    public void FlapClosed() {
+        servo1.setPosition(0.0);
+    }
+
+
 
     private DcMotor motor1;
     private Servo servo1;
---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/TestOuttake.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode.Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TestOuttake extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        boolean FlapOpenOrClosed = true;
+        Outtake outtake = new Outtake (hardwareMap);
+
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                if (FlapOpenOrClosed) {
+
+
+                }
+
+                else {
+
+
+                }
+
+            }
+        }
+    }
+}
