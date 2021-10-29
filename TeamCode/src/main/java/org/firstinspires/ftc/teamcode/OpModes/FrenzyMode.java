877afa0297e0794f1374378ef2268a183ef2112b
NeeravS <neerav.fsaps@gmail.com>
2021-10-28 22:30:12 -0400

    Changed values for recog zones in CVFrenzyPipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/FrenzyMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/FrenzyMode.java
@@ -177,17 +177,14 @@ public class FrenzyMode extends LinearOpMode {
     protected void handleLift() {
         if (isButtonPressable()) {
             double liftVal = gamepad2.left_stick_y;
-            if (liftVal < -0.5) {
-                startGamepadLockout();
-                robot.liftToUpper();
-            } else if (liftVal > 0.5) {
+            if (liftVal > 0.5) {
                 startGamepadLockout();
                 robot.liftToLower();
+            } else if (liftVal < -0.5) {
+                startGamepadLockout();
+                robot.liftToUpper();
             }
         }
-
-//        double liftVal = gamepad2.left_stick_y;
-//        robot.activateLift(liftVal);
     }
 
     protected void handleDropper() {
