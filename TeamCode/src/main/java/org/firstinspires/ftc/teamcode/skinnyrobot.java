893dcfce82e41b3203dfba4408d2cc49d7c65940
Joseph <josephwan.nc@gmail.com>
2021-10-30 18:09:00 -0400

    Changed the opMode to include Duck Servo and Intake Motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skinnyrobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skinnyrobot.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "skinny robot")
 public class skinnyrobot extends LinearOpMode {
-    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight, intake;
+    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight, motorIntake;
+    private CRServo servoDuck;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -14,10 +16,12 @@ public class skinnyrobot extends LinearOpMode {
         motorFrontLeft = hardwareMap.dcMotor.get("FL");
         motorBackLeft = hardwareMap.dcMotor.get("BL");
         motorBackRight = hardwareMap.dcMotor.get("BR");
+        motorIntake = hardwareMap.dcMotor.get("intake");
+        servoDuck = hardwareMap.crservo.get("duck");
 
         //reverse the needed motors
-        //motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
-        //motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontLeft.setDirection(DcMotor.Direction.REVERSE);
 
         motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -36,6 +40,15 @@ public class skinnyrobot extends LinearOpMode {
                 powerMod = 1.0;
             }
 
+            motorIntake.setPower(gamepad1.left_trigger);
+            motorIntake.setPower(-gamepad1.right_trigger);
+
+            if (gamepad1.left_bumper) {
+                servoDuck.setPower(powerMod);
+            }
+            else {
+                servoDuck.setPower(0);
+            }
 
             double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + (Math.PI/4);
             double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
@@ -44,10 +57,10 @@ public class skinnyrobot extends LinearOpMode {
             double powerOne = r*Math.cos(angle);
             double powerTwo = r*Math.sin(angle);
 
-            motorFrontLeft.setPower((powerOne + (rotation))*powerMod);
-            motorFrontRight.setPower((powerTwo + (rotation))*powerMod);
-            motorBackLeft.setPower((powerTwo - (rotation))*powerMod);
-            motorBackRight.setPower((powerOne - (rotation))*powerMod);
+            motorFrontLeft.setPower((powerOne - (rotation))*powerMod);
+            motorFrontRight.setPower((powerTwo - (rotation))*powerMod);
+            motorBackLeft.setPower((powerTwo + (rotation))*powerMod);
+            motorBackRight.setPower((powerOne + (rotation))*powerMod);
         }
     }
 }
