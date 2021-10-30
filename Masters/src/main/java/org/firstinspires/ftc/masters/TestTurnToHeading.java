9f695fbd51527871666360bddb6a3b4e1471202a
BlockBeasts <kjosien1@gmail.com>
2021-10-30 09:26:36 -0600

    BlackSwanAtutonomous

--- Masters/src/main/java/org/firstinspires/ftc/masters/TestTurnToHeading.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/TestTurnToHeading.java
@@ -25,16 +25,16 @@ public class TestTurnToHeading extends LinearOpMode {
 
         robot.setSpeedForTurnLeft(0.5);
 
-        telemetry.addData("Moving Forward","");
-        telemetry.update();
-        robot.forward(.1,.1);
-        robot.stopMotors();
+//        telemetry.addData("Moving Forward","");
+//        telemetry.update();
+//        robot.forward(.1,.1);
+//        robot.stopMotors();
 
         telemetry.addData("Turning to: ", 90);
         telemetry.addData("Current Heading: ", robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
         telemetry.addData("Integrated Heading: ", robot.getIntegratedHeading());
         robot.motorTelemetry();
-        telemetry.update();
+//        telemetry.update();
         robot.turnToHeading(.5,90,2);
         robot.pauseButInSecondsForThePlebeians(1);
 
@@ -42,7 +42,7 @@ public class TestTurnToHeading extends LinearOpMode {
         telemetry.addData("Current Heading: ", robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
         telemetry.addData("Integrated Heading: ", robot.getIntegratedHeading());
         robot.motorTelemetry();
-        telemetry.update();
+//        telemetry.update();
         robot.turnToHeading(.5,180,1);
         robot.pauseButInSecondsForThePlebeians(1);
 
@@ -50,7 +50,7 @@ public class TestTurnToHeading extends LinearOpMode {
         telemetry.addData("Current Heading: ", robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
         telemetry.addData("Integrated Heading: ", robot.getIntegratedHeading());
         robot.motorTelemetry();
-        telemetry.update();
+//        telemetry.update();
         robot.turnToHeading(.5,90,3);
         robot.pauseButInSecondsForThePlebeians(1);
 
@@ -58,7 +58,7 @@ public class TestTurnToHeading extends LinearOpMode {
         telemetry.addData("Current Heading: ", robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle);
         telemetry.addData("Integrated Heading: ", robot.getIntegratedHeading());
         robot.motorTelemetry();
-        telemetry.update();
+//        telemetry.update();
         robot.turnToHeading(.5,270,4);
         robot.pauseButInSecondsForThePlebeians(10);
     }
