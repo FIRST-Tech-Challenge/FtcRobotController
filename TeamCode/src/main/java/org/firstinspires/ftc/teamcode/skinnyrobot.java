2219d446672523fc80c22c4edc710e630c6c4907
Jolie <jolie23fish@gmail.com>
2021-10-30 17:49:31 -0400

    For other laptop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skinnyrobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/skinnyrobot.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "skinny robot")
 public class skinnyrobot extends LinearOpMode {
-    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight;
+    private DcMotor motorFrontRight, motorFrontLeft, motorBackLeft, motorBackRight, intake;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -36,6 +36,7 @@ public class skinnyrobot extends LinearOpMode {
                 powerMod = 1.0;
             }
 
+
             double angle = Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x) + (Math.PI/4);
             double r = Math.hypot(gamepad1.right_stick_x, gamepad1.right_stick_y);
             double rotation = gamepad1.left_stick_x;
@@ -43,10 +44,10 @@ public class skinnyrobot extends LinearOpMode {
             double powerOne = r*Math.cos(angle);
             double powerTwo = r*Math.sin(angle);
 
-            motorFrontLeft.setPower((powerOne - (rotation))*powerMod);
-            motorFrontRight.setPower((powerTwo - (rotation))*powerMod);
-            motorBackLeft.setPower((powerTwo + (rotation))*powerMod);
-            motorBackRight.setPower((powerOne + (rotation))*powerMod);
+            motorFrontLeft.setPower((powerOne + (rotation))*powerMod);
+            motorFrontRight.setPower((powerTwo + (rotation))*powerMod);
+            motorBackLeft.setPower((powerTwo - (rotation))*powerMod);
+            motorBackRight.setPower((powerOne - (rotation))*powerMod);
         }
     }
 }
