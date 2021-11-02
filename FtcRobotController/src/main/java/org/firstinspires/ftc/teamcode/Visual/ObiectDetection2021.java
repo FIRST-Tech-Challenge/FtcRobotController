484abaad3a253b38f17f411da752cf6de650975d
Ken Mori <ken.mori@students.tka.org>
2021-11-02 16:51:49 -0700

    I am sped boy

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Visual/ObiectDetection2021.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Visual/ObiectDetection2021.java
@@ -1,4 +1,4 @@
-/* Copyright (c) 2019 FIRST. All rights reserved.
+package org.firstinspires.ftc.teamcode.Visual;/* Copyright (c) 2019 FIRST. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted (subject to the limitations in the disclaimer below) provided that
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
