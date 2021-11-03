be81ae7b795b31ee1cb5be2782960dde8f98d011
Thomas Ricci <git@thomasricci.dev>
2021-11-03 10:28:52 -0400

    Add HW to mech

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Todo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Todo.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Todo {
+    // TODO: add options to toggle peripherals for managers so we can have the logic for all HW attachments in manager without commenting out stuff
     // TODO: references = tutorials
     // TODO: reports, other readme things, more in the docs homepage and stuff
     // TODO: back btn docs
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/utils/teleopmanagers/MechanumTeleOpManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/competition/utils/teleopmanagers/MechanumTeleOpManager.java
@@ -12,8 +12,8 @@ import org.firstinspires.ftc.teamcode.competition.utils.Motor;
 
 public class MechanumTeleOpManager extends TeleOpManager {
 
-//    private final Motor
     private final Mechanum MECHANUM;
+    private final Motor SPINNER, LIFT;
 
     public MechanumTeleOpManager(Telemetry telemetry, HardwareMap hardwareMap, Gamepad gamepad1, Gamepad gamepad2, GamepadFunctions function1, GamepadFunctions function2) {
         super(gamepad1, function1, gamepad2, function2);
@@ -22,11 +22,14 @@ public class MechanumTeleOpManager extends TeleOpManager {
         Motor lt = new Motor(telemetry, hardwareMap, hardwareMap.appContext.getString(R.string.DRIVETRAIN_LEFT_DRIVE_1), DcMotorSimple.Direction.FORWARD);
         Motor lb = new Motor(telemetry, hardwareMap, hardwareMap.appContext.getString(R.string.DRIVETRAIN_LEFT_DRIVE_2), DcMotorSimple.Direction.FORWARD);
         MECHANUM = new Mechanum(telemetry, rt, rb, lt, lb);
+        SPINNER = new Motor(telemetry, hardwareMap, hardwareMap.appContext.getString(R.string.HW_SPINNER), DcMotorSimple.Direction.FORWARD);
+        LIFT = new Motor(telemetry, hardwareMap, hardwareMap.appContext.getString(R.string.HW_LIFT), DcMotorSimple.Direction.FORWARD);
     }
 
     @Override
     public void main() {
         if(getGamepad1Functions().hasF1()) {
+            // do the math
             double y = -getGamepad1().left_stick_y;
             double x = getGamepad1().left_stick_x * 1.1;
             double rx = getGamepad1().right_stick_x;
@@ -35,8 +38,10 @@ public class MechanumTeleOpManager extends TeleOpManager {
             double rightBottomPower = (y + x - rx) / denominator;
             double leftTopPower = (y + x + rx) / denominator;
             double leftBottomPower = (y - x + rx) / denominator;
+            // normal drive code
             MECHANUM.driveWithEncoder((int) Range.clip(rightTopPower * 100, -100, 100), (int) Range.clip(rightBottomPower * 100, -100, 100), (int) Range.clip(leftTopPower * 100, -100, 100), (int) Range.clip(leftBottomPower * 100, -100, 100));
         }else if(getGamepad2Functions().hasF1()) {
+            // do the math
             double y = -getGamepad2().left_stick_y;
             double x = getGamepad2().left_stick_x * 1.1;
             double rx = getGamepad2().right_stick_x;
@@ -45,7 +50,21 @@ public class MechanumTeleOpManager extends TeleOpManager {
             double rightBottomPower = (y + x - rx) / denominator;
             double leftTopPower = (y + x + rx) / denominator;
             double leftBottomPower = (y - x + rx) / denominator;
+            // normal drive code
             MECHANUM.driveWithEncoder((int) Range.clip(rightTopPower * 100, -100, 100), (int) Range.clip(rightBottomPower * 100, -100, 100), (int) Range.clip(leftTopPower * 100, -100, 100), (int) Range.clip(leftBottomPower * 100, -100, 100));
+        }
+        if(getGamepad1Functions().hasF2()) {
+            SPINNER.driveWithEncoder((int) Range.clip((getGamepad1().left_trigger - getGamepad1().right_trigger) * 100, -100, 100));
+            int liftSpeed1 = getGamepad1().left_bumper ? 50 : 0;
+            int liftSpeed2 = getGamepad1().right_bumper ? -50 : 0;
+            int liftSpeed = liftSpeed1 - liftSpeed2;
+            LIFT.driveWithEncoder(liftSpeed);
+        }else if(getGamepad2Functions().hasF2()) {
+            int liftSpeed1 = getGamepad2().left_bumper ? 50 : 0;
+            int liftSpeed2 = getGamepad2().right_bumper ? -50 : 0;
+            int liftSpeed = liftSpeed1 - liftSpeed2;
+            LIFT.driveWithEncoder(liftSpeed);
+            SPINNER.driveWithEncoder((int) Range.clip((getGamepad2().left_trigger - getGamepad2().right_trigger) * 100, -100, 100));
         }
     }
 
