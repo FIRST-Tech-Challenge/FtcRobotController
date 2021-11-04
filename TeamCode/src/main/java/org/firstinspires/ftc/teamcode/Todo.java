4742d73d0db958557e22a9ea2a2670a39d200de6
Thomas Ricci <git@thomasricci.dev>
2021-11-04 14:48:22 -0400

    update todos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Todo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Todo.java
@@ -1,9 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 public class Todo {
-    // TODO: references = guides
-    // TODO: reports, other readme things, more in the docs homepage and stuff
     // TODO: back btn docs
-    // TODO: maybe different navbar for other docs pages
     // TODO: my docs
 }
