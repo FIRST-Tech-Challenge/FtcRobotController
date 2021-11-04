ab4afd307a6cf64bb1e42afed1f574f0023b93a5
devanchoksi <devan.choksi@students.tka.org>
2021-11-03 17:23:53 -0700

    started code for intake

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/OnBotJavaPasted.java
@@ -0,0 +1,54 @@
+//INTAKE
+
+package org.firstinspires.ftc.teamcode.Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+    @TeleOp
+
+    public class OnBotJavaPasted extends LinearOpMode {
+        DcMotor motor2;
+        boolean previousInput = false;
+        boolean intakeToggle = false;
+
+        public void runOpMode() throws InterruptedException {
+
+            waitForStart();
+
+            while (opModeIsActive()) {
+
+                motor2 = hardwareMap.get(DcMotor.class, "motorIntake");
+
+                if (gamepad1.right_bumper == true) {
+
+                    motor2.setPower(0.3);
+
+                } else {
+                    motor2.setPower(0);
+                }
+
+
+                if (gamepad1.right_bumper == true && previousInput == false) {
+                    intakeToggle = !intakeToggle;
+                    previousInput = true;
+                }
+
+                previousInput = gamepad1.right_bumper;
+
+                if (intakeToggle == true) {
+                    motor2.setPower(0.3);
+                }
+                else {
+                    motor2.setPower(0);
+                }
+
+
+            }
+        }
+    }
+
+
+
+
