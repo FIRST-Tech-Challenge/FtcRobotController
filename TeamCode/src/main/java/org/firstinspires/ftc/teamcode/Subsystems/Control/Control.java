fc2a213de5b9610d551e28b3f057ca35e3d382d0
albonec <70042211+albonec@users.noreply.github.com>
2021-11-04 20:22:30 -0700

    removed unused import statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Control/Control.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Control;
 
-import com.qualcomm.robotcore.hardware.DcMotor;
+
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Subsystem.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.Util.QuickTelemetry;
 
 /**
