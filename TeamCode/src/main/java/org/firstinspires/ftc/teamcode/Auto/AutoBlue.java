9da0096581618e192d4c57275d802e89d07de80b
Rohan Satapathy <satapathyrohan13@gmail.com>
2021-11-06 16:09:53 -0500

    Add TODOs for auto period

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoBlue.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+
+/**
+ * Auto creates a robots and runs it in auto mode. This auto class is for when we are
+ * on the blue alliance.
+ *
+ * <p>Auto currently just initializes the Robot as Auto.runOpMode() is empty.</p>
+ *
+ * @see com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+ */
+
+//Tasks:
+// Deliver duck from carousel (10)
+//Deliver freight to hub (6)
+// - deliver freight to corresponding level of custom element (20)
+//Park in warehouse (10)
+@Autonomous(name = "Auto", group = "Concept")
+public class AutoBlue extends LinearOpMode {
+    ElapsedTime timer = new ElapsedTime();
+    Robot robot = new Robot(this, timer, true);
+
+
+    /** Override of runOpMode()
+     *
+     * <p>Please do not swallow the InterruptedException, as it is used in cases
+     * where the op mode needs to be terminated early.</p>
+     *
+     * @throws InterruptedException
+     *
+     * @see com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+     */
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        // TODO: Detect position for freight
+
+        // TODO: Move downwards and deliver duck
+
+        // TODO: Navigate to center of field and deliver freight
+
+        // TODO: Navigate to warehouse and park
+
+    }
+}
