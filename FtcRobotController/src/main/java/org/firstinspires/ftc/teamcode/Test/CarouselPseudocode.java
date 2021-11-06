02b5f555bfcd50c47ad50337eb500d753d66ee6c
devanchoksi <devan.choksi@students.tka.org>
2021-11-05 17:31:16 -0700

    started code for intake

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/CarouselPseudocode.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/CarouselPseudocode.java
@@ -1,52 +1,22 @@
 package org.firstinspires.ftc.teamcode.Test;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class CarouselPseudocode {
+    private DcMotor carouselTurningMotor;
+    private DigitalChannel touchSensor;
 
-    //no assist
-
-    Boolean prevInput = false;
-
-    Int ct = 0;
-    If(gamepad1.a == true && prevInput == false) {
-        ct++;
-        prevInput = true;
-    }
-    prevInput = gamepad1.dpad_up;
-    If (ct % 2 == 1 && ts1.getState == true) {
-        motor2.setPower(1);
-    } else {
-        motor2.setPower(0);
+    // Constructor
+    public CarouselPseudocode(HardwareMap hardwareMap) {
+        carouselTurningMotor = hardwareMap.dcMotor.get("motorCarousel");
+        touchSensor = hardwareMap.digitalChannel.get("CarouselTouchSensor");
     }
 
-//assist?
+    public void CarouselHandler() {
 
-    Boolean buffer = true;
-    Boolean safeA = false;
-
-	if (gamepad1.a == true){
-        safeA = false;
-
-        while(ts1.getState() == 0){
-            //Drive motors
-            motor1.setPower(.2);
-
-            if (gamepad1.a != true) {
-                safeA = true;
-
-            }
-        }
-
-        while (safeA == false || gamepad1.a == false) {
-            motor2.setPower(1);
-
-            if (gamepad1.a != true) {
-                safeA = true;
-            }
-        }
-
-        motor2.setPower(0);
 
     }
+
 }
