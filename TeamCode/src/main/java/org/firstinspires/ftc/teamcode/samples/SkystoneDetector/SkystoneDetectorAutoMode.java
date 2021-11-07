c8af6cf45a548239be1f4f58f9b82d18b0ae1421
Alex <hbostic@gmail.com>
2021-11-07 11:37:25 -0500

    updated gradle created samples for using opencv dnn to leverage an optimized tensorflow model created with teachable machine

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/SkystoneDetector/SkystoneDetectorAutoMode.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.samples.SkystoneDetector;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@Autonomous(name="Auto: SkyStone Detector")
+public class SkystoneDetectorAutoMode extends LinearOpMode {
+    // Handle hardware stuff...
+
+    int width = 320;
+    int height = 240;
+    // store as variable here so we can access the location
+    SkystoneDetector detector = new SkystoneDetector(width);
+    OpenCvCamera phoneCam;
+
+    @Override
+    public void runOpMode() {
+        // robot logic...
+
+        // https://github.com/OpenFTC/EasyOpenCV/blob/master/examples/src/main/java/org/openftc/easyopencv/examples/InternalCameraExample.java
+        // Initialize the back-facing camera
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        phoneCam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        // Connect to the camera
+        phoneCam.openCameraDevice();
+        // Use the SkystoneDetector pipeline
+        // processFrame() will be called to process the frame
+        phoneCam.setPipeline(detector);
+        // Remember to change the camera rotation
+        phoneCam.startStreaming(width, height, OpenCvCameraRotation.SIDEWAYS_LEFT);
+
+        //...
+
+        SkystoneDetector.SkystoneLocation location = detector.getLocation();
+        if (location != SkystoneDetector.SkystoneLocation.NONE) {
+            // Move to the left / right
+        } else {
+            // Grab the skystone
+        }
+
+        // more robot logic...
+    }
+}
