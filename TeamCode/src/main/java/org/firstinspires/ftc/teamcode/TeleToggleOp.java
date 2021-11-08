5ba899d270e5d18072ebd63bf8a48731519398f1
Saber <48610047+garagebot15146@users.noreply.github.com>
2021-11-07 17:38:03 -0800

    TeleOp Toggle
    We coded toggle methods for the A button on the controller through the use of if/else statements.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleToggleOp.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "TeleToggleOp", group = "Iterative Opmode")
+public class TeleToggleOp extends OpMode {
+    ElapsedTime runtime = new ElapsedTime();
+    DcMotor leftMotor = null;
+
+    @Override
+    public void init() {
+        telemetry.addData("Status", "meh");
+        leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        telemetry.addData("Status", "lmotorized");
+    }
+
+    @Override
+    public void init_loop() {
+
+    }
+
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    boolean lock = false;
+    boolean state = false;
+
+    @Override
+    public void loop() {
+        if (gamepad1.a && !lock) {
+            lock = true;
+            if (state) {
+                leftMotor.setPower(0);
+                state = false;
+
+            } else {
+                leftMotor.setPower(1);
+                state = true;
+
+            }
+            telemetry.addData("Status", "good");
+            telemetry.addData("Status", leftMotor.getPower());
+        } else if (!gamepad1.a && lock) {
+            lock = false;
+        }
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+}
