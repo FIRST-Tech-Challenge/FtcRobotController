a3b3610a3dc079b317bdf53ae7c180a0944fe5da
Colby <cms0679@outlook.com>
2021-11-08 19:50:09 -0500

    Added Left and Right IMU turning methods.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumAutonomous.java
@@ -1,19 +1,30 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @Autonomous(name="MecanumAutonomous", group="FreightFrenzy")
 public class MecanumAutonomous extends LinearOpMode {
     //Add an ElapsedTime for function runtime calculations.
     private ElapsedTime runtime = new ElapsedTime();
     //Import the robot's hardware map.
     FrenzyHardwareMap robot = new FrenzyHardwareMap();
+    // Declare IMU
+    BNO055IMU.Parameters IMU_Parameters;
+    float Yaw_Angle = 0;
     @Override
     public void runOpMode() {
         //Run during program's init (anything before waitForStart()).
         //Import the hardware map
         robot.init(hardwareMap, telemetry);
         telemetry.addData("Say", "Hello Driver");
+        // Initialize IMU
+        IMU_Parameters = new BNO055IMU.Parameters();
+        IMU_Parameters.mode = BNO055IMU.SensorMode.IMU;
+        robot.imu.initialize(IMU_Parameters);
         IMU_Calibrated();
         //Begin after program's start
         waitForStart();
@@ -24,21 +35,21 @@ public class MecanumAutonomous extends LinearOpMode {
         move(225, 40, 0.8, 5.0);
         move(135, 40, 0.8, 5.0);
         move(315, 40, 0.8, 5.0);
+        turnRight();
+        turnLeft();
     }
     //Drive forward/backward. Travel distance in CM.
     public void forward(double distanceInCM, double power, double timeoutS) {
         telemetry.addData("status","encoder reset");
         telemetry.update();
         robot.restartEncoders();
-        if(opModeIsActive())
-        {
+        if(opModeIsActive()) {
             telemetry.addData("status","getEncoderClicks");
             telemetry.update();
             //Set motor target, reset runtime for timeout, then set motor powers
             int target = (int) driveDistance(distanceInCM);
             boolean forward = true;
-            if(distanceInCM < 0)
-            {
+            if(distanceInCM < 0) {
                 forward = false;
             }
             double slowdown = 1;
@@ -47,8 +58,7 @@ public class MecanumAutonomous extends LinearOpMode {
             robot.setRunToPosition();
             robot.setPowers(power);
             //Loop while motors are moving towards target
-            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (robot.motorsBusy()))
-            {
+            while (opModeIsActive() && (runtime.seconds() < timeoutS) && (robot.motorsBusy())) {
                 if(forward && robot.motorFrontLeft.getCurrentPosition() > target*0.75 && robot.motorFrontRight.getCurrentPosition() > target*0.75)
                 slowdown = 0.25;
                 if(!forward && robot.motorFrontLeft.getCurrentPosition() < target*0.75 && robot.motorFrontRight.getCurrentPosition() < target*0.75)
@@ -142,6 +152,72 @@ public class MecanumAutonomous extends LinearOpMode {
     public double driveDistance(double distance) {
         double drive  = (robot.REV_ENCODER_CLICKS/ robot.REV_WHEEL_CIRC);
         return (int)Math.floor(drive * distance);
+    }
+    public void turnRight() {
+        Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+        //MOVE RIGHT
+        robot.motorFrontLeft.setPower(0.2);
+        robot.motorBackLeft.setPower(0.2);
+        robot.motorFrontRight.setPower(-0.2);
+        robot.motorBackRight.setPower(-0.2);
+        // Continue until robot yaws right by 90 degrees or stop is pressed on Driver Station.
+        sleep(1000);
+        //turns to the right; 90, 180, negative, -90, 0
+        while ( !(Yaw_Angle >= 85 || isStopRequested()) ) {
+            // Update Yaw-Angle variable with current yaw.
+            Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+            // Report yaw orientation to Driver Station.
+            telemetry.addData("Yaw value", Yaw_Angle);
+            telemetry.update();
+        }
+        // We're done. Turn off motors
+        robot.setPowers(0);
+        // Pause so final telemetry is displayed.
+        sleep(1000);
+    }
+    public void turnRight() {
+        Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+        //MOVE RIGHT
+        robot.motorFrontLeft.setPower(0.2);
+        robot.motorBackLeft.setPower(0.2);
+        robot.motorFrontRight.setPower(-0.2);
+        robot.motorBackRight.setPower(-0.2);
+        // Continue until robot yaws right by 90 degrees or stop is pressed on Driver Station.
+        sleep(1000);
+        //turns to the right; 90, 180, negative, -90, 0
+        while ( !(Yaw_Angle >= 85 || isStopRequested()) ) {
+            // Update Yaw-Angle variable with current yaw.
+            Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+            // Report yaw orientation to Driver Station.
+            telemetry.addData("Yaw value", Yaw_Angle);
+            telemetry.update();
+        }
+        // We're done. Turn off motors
+        robot.setPowers(0);
+        // Pause so final telemetry is displayed.
+        sleep(1000);
+    }
+    public void turnLeft() {
+        Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+        //MOVE LEFT
+        robot.motorFrontLeft.setPower(-0.2);
+        robot.motorBackLeft.setPower(-0.2);
+        robot.motorFrontRight.setPower(0.2);
+        robot.motorBackRight.setPower(0.2);
+        // Continue until robot yaws right by 90 degrees or stop is pressed on Driver Station.
+        sleep(1000);
+        //turns to the right; 90, 180, negative, -90, 0
+        while ( !(Yaw_Angle <= -85 || isStopRequested()) ) {
+            // Update Yaw-Angle variable with current yaw.
+            Yaw_Angle = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+            // Report yaw orientation to Driver Station.
+            telemetry.addData("Yaw value", Yaw_Angle);
+            telemetry.update();
+        }
+        // We're done. Turn off motors
+        robot.setPowers(0);
+        // Pause so final telemetry is displayed.
+        sleep(1000);
     }
     //Rturns telemetry for IMU Calibration.
     public void IMU_Calibrated() {
