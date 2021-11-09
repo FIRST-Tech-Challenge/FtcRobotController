fa80cb07827bc4f2bedff52d6aa87571cca97f86
Charlie Garfield <charlie.garfield@hotmail.com>
2021-11-09 16:25:52 -0500

    Added basic intake code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Intake.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Intake implements Mechanism {
+    DcMotor intakeMotor;
+    @Override
+    public void init(HardwareMap hardwareMap) {
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake");
+    }
+
+    @Override
+    public void run(Gamepad gamepad) {
+        if (gamepad.right_trigger > 0.5) {
+            intakeMotor.setPower(gamepad.right_trigger);
+        } else if (gamepad.left_trigger > 0.5) {
+            intakeMotor.setPower(-gamepad.right_trigger);
+        } else {
+            intakeMotor.setPower(0);
+        }
+    }
+}
