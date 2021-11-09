c416c2e37e81bc41948b8d3396c0df8a86c23c64
Charlie Garfield <charlie.garfield@hotmail.com>
2021-11-09 16:26:01 -0500

    Added basic lift code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Lift.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Lift implements Mechanism {
+    DcMotor liftMotor;
+    @Override
+    public void init(HardwareMap hardwareMap) {
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
+    }
+
+    @Override
+    public void run(Gamepad gamepad) {
+        liftMotor.setPower(gamepad.left_stick_y);
+    }
+}
