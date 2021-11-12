d58dc146e4219b0eefadfae0d93efb2464fb21c8
Charlie Garfield <charlie.garfield@hotmail.com>
2021-11-12 13:27:40 -0500

    Made lift code better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/Lift.java
@@ -1,18 +1,26 @@
 package org.firstinspires.ftc.teamcode.mechanism;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
 
 public class Lift implements Mechanism {
-    DcMotor liftMotor;
+    DcMotorEx liftMotor;
+    float targetPosition = 0;
     @Override
     public void init(HardwareMap hardwareMap) {
-        liftMotor = hardwareMap.dcMotor.get("lift");
+        liftMotor = hardwareMap.get(DcMotorEx.class, "lift");
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
     public void run(Gamepad gamepad) {
-        liftMotor.setPower(gamepad.left_stick_y);
+        targetPosition += gamepad.left_stick_y;
+        targetPosition = Range.clip(targetPosition,0,5000);
+        liftMotor.setTargetPosition((int)targetPosition);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setPower(.5);
     }
 }
