a8ef712e2f56b2ea5d1c6f565ff565b2a29a2653
Kaitlin <karos1774@gmail.com>
2021-11-11 19:31:29 -0500

    added CVFrenzyRecTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red1.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Autonomous(name="Red-1-Simple", group ="playback")
+@Autonomous(name="Red-1-Storage", group ="playback")
 //@Disabled
 public class Red1 extends AutoReplayBaseCam {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Red2.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Autonomous(name="Red-2-RegFull", group ="playback")
-@Disabled
-public class Red2 extends AutoReplayBase {
+@Autonomous(name="Red-2-Warehouse", group ="playback")
+//@Disabled
+public class Red2 extends AutoReplayBaseCam {
 
 }
