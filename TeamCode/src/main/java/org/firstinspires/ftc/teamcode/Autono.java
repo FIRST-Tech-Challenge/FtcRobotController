c088bee21b0231c6751938ab8bcc8318a2b99d19
Harry100089 <71835723+Harry100089@users.noreply.github.com>
2021-11-14 11:03:27 -0500

    progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autono.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autono.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Autonomous extends LinearOpMode {
+@Autonomous(name = "yxorauto")
+public class Autono extends LinearOpMode {
     MecanumChassis robot = new MecanumChassis();
 
     private ElapsedTime runtime = new ElapsedTime();
