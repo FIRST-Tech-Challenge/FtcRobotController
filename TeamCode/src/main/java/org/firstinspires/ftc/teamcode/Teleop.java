f9bd698b284904e8f9d45f3db399106eb54ae3d2
Nick-Fanelli <nickfanelli5@gmail.com>
2021-11-14 20:55:58 -0500

    feat: add all operator and driver teleop controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,13 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import android.graphics.Color;
-
 import com.bravenatorsrobotics.core.FtcGamePad;
 import com.bravenatorsrobotics.drive.MecanumDrive;
 import com.bravenatorsrobotics.operation.TeleopMode;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -18,33 +15,53 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @TeleOp(name="Teleop")
 public class Teleop extends TeleopMode<MecanumDrive> {
 
+    // TODO: Replace with tested values
+    private static final int LIFT_STAGE_1 = 100;
+    private static final int LIFT_STAGE_2 = 200;
+    private static final int LIFT_STAGE_3 = 300;
+
+    private static final double LIFT_POWER = 0.75;
+
     private static final double CUP_OBJECT_THRESHOLD_CM = 6.0; // CM
+    private static final double REDUCE_SPEED_MULTIPLIER = 0.25;
+
+    private Config config;
 
     private DcMotorEx lift;
     private DcMotorEx intake;
     private DcMotorEx turnTableSpinner;
 
+    // TODO: Check to see what servo is on bot and put it into the configuration
     private Servo cupServo;
-
     private TouchSensor liftTouchSensor;
-
     private RevColorSensorV3 cupDistanceSensor;
-    private ColorSensor cupColorSensor;
+
+    private boolean shouldOverrideSpeedReduction = false;
+    private boolean shouldFreeLiftWhenDone = false;
+
+    private double turnTablePower = 1;
 
     // Create TeleopMode with specified specifications
     public Teleop() { super(new Specifications()); }
 
     @Override
     public void OnInitialize() {
+        PrintControls();
+
+        config = new Config(hardwareMap.appContext);
+
+        // Reverse the turn table power if on red alliance
+        if(config.allianceColor == Config.AllianceColor.RED)
+            turnTablePower = -turnTablePower;
+
         lift = robot.GetMotor("lift", false);
-        turnTableSpinner = robot.GetMotor("turnTable", false);
         intake = robot.GetMotor("intake", false);
+        turnTableSpinner = robot.GetMotor("turnTable", false);
 
         cupServo = hardwareMap.servo.get("cupServo");
 
         liftTouchSensor = hardwareMap.touchSensor.get("liftTouchSensor");
 
-        cupColorSensor = hardwareMap.colorSensor.get("cupDistanceSensor");
         cupDistanceSensor = hardwareMap.get(RevColorSensorV3.class, "cupDistanceSensor");
     }
 
@@ -55,26 +72,16 @@ public class Teleop extends TeleopMode<MecanumDrive> {
 
     @Override
     public void OnUpdate() {
-//        float[] hsvValues = { 0.0f, 0.0f, 0.0f };
-//
-//        Color.RGBToHSV(
-//                (int) (cupColorSensor.red() * 255),
-//                (int) (cupColorSensor.green() * 255),
-//                (int) (cupColorSensor.blue() * 255),
-//                hsvValues
-//        );
-//
-//        telemetry.addData("HUE", hsvValues[0]);
-//        telemetry.addData("S", hsvValues[1]);
-//        telemetry.addData("V", hsvValues[2]);
-//        telemetry.addData("Is In Cup", IsObjectInCup());
-//        telemetry.update();
+        HandleGamePadDrive();
 
-        // Cube the input to scale the power
-        robot.drive.Drive(Math.pow(gamepad1.left_stick_y, 3), Math.pow(gamepad1.left_stick_x, 3), Math.pow(-gamepad1.right_stick_x, 3));
+        // Check to see if object is in cup. If so tilt it back.
 
-
-
+        // Free Lift
+        if(shouldFreeLiftWhenDone) {
+            if(!lift.isBusy()) {
+                lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+        }
     }
 
     @Override
@@ -82,43 +89,55 @@ public class Teleop extends TeleopMode<MecanumDrive> {
         robot.drive.Stop();
     }
 
+    private void HandleGamePadDrive() {
+        double v = Math.pow(gamepad1.left_stick_y, 3);
+        double h = Math.pow(gamepad1.left_stick_x, 3) + Math.pow(driverGamePad.getLeftTrigger(), 3)
+                - Math.pow(driverGamePad.getRightTrigger(), 3);
+        double r = Math.pow(gamepad1.right_stick_x, 3);
+
+        if(!liftTouchSensor.isPressed() && !shouldOverrideSpeedReduction) {
+            v *= REDUCE_SPEED_MULTIPLIER;
+            h *= REDUCE_SPEED_MULTIPLIER;
+            r *= REDUCE_SPEED_MULTIPLIER;
+        }
+
+        super.robot.drive.Drive(v, h, r);
+    }
+
     @Override
     protected void OnDriverGamePadChange(FtcGamePad gamePad, int button, boolean pressed) {
-
+        switch (button) {
+            case FtcGamePad.GAMEPAD_BACK:
+                if(pressed) shouldOverrideSpeedReduction = !shouldOverrideSpeedReduction;
+                break;
+        }
     }
 
     @Override
     protected void OnOperatorGamePadChange(FtcGamePad gamePad, int button, boolean pressed) {
         switch (button) {
 
-            case FtcGamePad.GAMEPAD_DPAD_UP:
-                if(pressed) { // Max encoder tick
-                    lift.setPower(1);
-                } else {
-                    lift.setPower(0);
-                }
-
-                break;
-
-            case FtcGamePad.GAMEPAD_DPAD_DOWN:
-                if(pressed && !liftTouchSensor.isPressed()) { // if we aren't on the physical switch
-                    lift.setPower(-1);
-                } else {
-                    lift.setPower(0);
-                }
-
-                break;
-
-            case FtcGamePad.GAMEPAD_A:
+            // Turntable Direction Override
+            case FtcGamePad.GAMEPAD_BACK:
                 if(pressed) {
-                    intake.setPower(-1);
-                } else {
-                    intake.setPower(0);
+                    turnTablePower = -turnTablePower;
                 }
 
                 break;
 
+            // Turntable Spinner
             case FtcGamePad.GAMEPAD_Y:
+
+                if(pressed) {
+                    turnTableSpinner.setPower(turnTablePower);
+                } else {
+                    turnTableSpinner.setPower(0);
+                }
+
+                break;
+
+            // Intake
+            case FtcGamePad.GAMEPAD_A:
                 if(pressed) {
                     intake.setPower(1);
                 } else {
@@ -127,13 +146,78 @@ public class Teleop extends TeleopMode<MecanumDrive> {
 
                 break;
 
+            // Toggle Cup
+            case FtcGamePad.GAMEPAD_B:
+                // TODO: Toggle the cup dumped or not
+                break;
 
+            // Automatic Lift Down
+            case FtcGamePad.GAMEPAD_DPAD_DOWN:
+                if(pressed) {
+                    lift.setTargetPosition(0);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(LIFT_POWER);
+                    shouldFreeLiftWhenDone = true;
+
+                    // TODO: Check for manual safety switch
+                }
+
+                break;
+
+            // Automatic Lift Position 1
+            case FtcGamePad.GAMEPAD_DPAD_LEFT:
+                if(pressed) {
+                    lift.setTargetPosition(LIFT_STAGE_1);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(LIFT_POWER);
+                    shouldFreeLiftWhenDone = true;
+                }
+
+                break;
+
+            // Automatic Lift Position 2
+            case FtcGamePad.GAMEPAD_DPAD_UP:
+                if(pressed) {
+                    lift.setTargetPosition(LIFT_STAGE_2);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(LIFT_POWER);
+                    shouldFreeLiftWhenDone = true;
+                }
+
+                break;
+
+            // Automatic Lift Position 3
+            case FtcGamePad.GAMEPAD_DPAD_RIGHT:
+                if(pressed) {
+                    lift.setTargetPosition(LIFT_STAGE_3);
+                    lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    lift.setPower(LIFT_POWER);
+                    shouldFreeLiftWhenDone = true;
+                }
+
+                break;
         }
     }
 
     private boolean IsObjectInCup() {
         double distance = cupDistanceSensor.getDistance(DistanceUnit.CM);
         return (distance < CUP_OBJECT_THRESHOLD_CM) || (cupDistanceSensor.getLightDetected() > 0.1);
+    }
+
+    private void PrintControls() {
+        telemetry.log().add("DRIVER CONTROLS");
+        telemetry.log().add("(BACK) Emergency Movement Override");
+        telemetry.addLine();
+
+        telemetry.log().add("OPERATOR CONTROLS");
+        telemetry.log().add("(BACK) Reverse Turn-Table Direction");
+        telemetry.log().add("(Y) Turn-Table");
+        telemetry.log().add("(A) Intake");
+        telemetry.log().add("(B) Toggle Cup Position");
+        telemetry.log().add("(D-Pad Down) Lift Down");
+        telemetry.log().add("(D-Pad Left) Lift First Position");
+        telemetry.log().add("(D-Pad Up) Lift Second Position");
+        telemetry.log().add("(D-Pad Right) Lift Third Position");
     }
 
 }
