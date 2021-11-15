c061d9b3def3e3a402cb2c17ef22865f5bc49c0f
Harry100089 <71835723+Harry100089@users.noreply.github.com>
2021-11-15 13:42:59 -0500

    progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autono.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autono.java
@@ -4,6 +4,14 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
 @Autonomous(name = "yxorauto")
 public class Autono extends LinearOpMode {
     MecanumChassis robot = new MecanumChassis();
@@ -13,12 +21,44 @@ public class Autono extends LinearOpMode {
     private PositionControl positionControl = null;
     private volatile double[] robotPos = new double[3];
     private volatile double[] targetPos = new double[3];
+    private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
+    private static final String[] LABELS = {
+            "Ball",
+            "Cube",
+            "Duck",
+            "Marker"
+    };
+    private static final String VUFORIA_KEY =
+            "AVc6Q6P/////AAABmR9qZSEpdkuvg3gY65VM2CBj0qyusYkXggAzzLzeUGiCn6T3w/CDUskaaDtCPphwreE" +
+                    "L2wGP/498dRPiEcO/T9e5YokJIGna6LI5dIPnbUNe9qZJWbT+ZELQPlHT4/r/ipryTVejft6Agg" +
+                    "3zKUvnz9AF1eVFiP/pTmFvijCv2wFyaQD.cBxFmSvaq1YqA2zG0/W79j2wXzr26pJAN6yKZcjspx" +
+                    "PYcQm2SLTEuGA/IeNKqV8VTtnLas6OXmEARgF4M0xgM7pgTTs2hzo92q+AacWN6aD95XDmCFFn2" +
+                    "fS13tOJRwxWAg/Ju3B5ctPVo+wUFJJ4uWWf5xpc8ArvzzAD+niHQxOvHxa0xi+tbvQOJ12MQ";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+
 
     int[] armPos = {1,2,3};
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
+
+        initVuforia();
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+
+            // The TensorFlow software will scale the input images from the camera to a lower resolution.
+            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
+            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
+            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
+            // should be set to the value of the images used to create the TensorFlow Object Detection model
+            // (typically 16/9).
+            tfod.setZoom(2.5, 16.0/9.0);
+        }
 
         this.positionEstimation = new PositionEstimation(robot);
         this.positionControl = new PositionControl(robot, this.positionEstimation);
@@ -50,15 +90,31 @@ public class Autono extends LinearOpMode {
 
         waitForStart();
 
-        int DuckPos;
+        int DuckPos = 3;
         // TensorFlow find duck
+        if (tfod != null) {
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                int i = 0;
+                for (Recognition recognition : updatedRecognitions) {
+                    telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                    telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                            recognition.getLeft(), recognition.getTop());
+                    telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                            recognition.getRight(), recognition.getBottom());
+                    i++;
+                }
+                telemetry.update();
+            }
+        }
 
-        goToWayPoint(0,1,0,0.7,0,0.01,2);
+        goToWayPoint(0,1,0,0.7,0,0,0);
 
     }
     private void goToWayPoint(double x, double y, double angle, double vel, double vw, double disRes, double angleRes) throws InterruptedException {
-        targetPos[0] = x;//1.5;  //x
-        targetPos[1] = y;//-0.6;   //y
+        targetPos[0] = y;//1.5;  //x
+        targetPos[1] = x;//-0.6;   //y
         targetPos[2] = angle * Math.PI / 180; // Math.PI /2;   //heading, radian
         this.positionControl.goToTargetPosition(targetPos, vel,vw * Math.PI / 180, disRes,angleRes);
         while(!this.positionControl.checkTaskDone()){
@@ -75,5 +131,25 @@ public class Autono extends LinearOpMode {
         }
         Thread.sleep(200);
 
+    }
+    private void initVuforia() {
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfodParameters.isModelTensorFlow2 = true;
+        tfodParameters.inputSize = 320;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABELS);
     }
 }
