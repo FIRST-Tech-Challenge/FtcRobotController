15be4b7d8cc5a1c6c281551c8e9e4609a391c0d2
willm <willmoiser@gmail.com>
2021-11-17 18:07:15 -0600

    Added a template for Teleop called TeleopTemplate. Refactored the code so that the JavaDriveProgram class now inherits from TeleopTemplate.
    Signed-off-by: willm <willmoiser@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/DrivePrograms/JavaDriveProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/DrivePrograms/JavaDriveProgram.java
@@ -1,83 +1,24 @@
 package org.firstinspires.ftc.teamcode.src.DrivePrograms;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.src.robotAttachments.DriveTrains.TeleopDriveTrain;
-import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.CarouselSpinner;
-import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.ContinuousIntake;
-import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.LinearSlide;
-import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.OdometryPodServos;
+import org.firstinspires.ftc.teamcode.src.Utills.TeleopTemplate;
 
 
 @TeleOp(name = "2022 Drive Program")
-public class JavaDriveProgram extends LinearOpMode {
-
-    private TeleopDriveTrain driveTrain;
-    private CarouselSpinner spinner;
-    private OdometryPodServos pod;
-    private LinearSlide slide;
-    private ContinuousIntake intake;
-
+public class JavaDriveProgram extends TeleopTemplate {
 
     public void runOpMode() throws InterruptedException {
 
-
-        boolean xDepressed = true;
-        boolean yDepressed = true;
-        boolean aDepressed = true;
-        boolean bDepressed = true;
-
-        driveTrain = new TeleopDriveTrain(hardwareMap, "front_right", "front_left", "back_right", "back_left");
-
-        spinner = new CarouselSpinner(hardwareMap, "duck_spinner");
-
-        pod = new OdometryPodServos(hardwareMap, "right_odometry_servo", "left_odometry_servo", "horizontal_odometry_servo");
-        pod.raise();
-
-        slide = new LinearSlide(hardwareMap, "slide_motor");
-
-        intake = new ContinuousIntake(hardwareMap, "intake_motor");
-
-
-        telemetry.addData("Initialization Status", "Initialized");
-        telemetry.update();
-
+        this.initAll();
         waitForStart();
+
         while (opModeIsActive() && !isStopRequested()) {
             driveTrain.setPowerFromGamepad(gamepad1);
 
             //Handles Linear Slide Control
             slide.setMotorPower(0.75 * gamepad2.left_stick_y);
             intake.setMotorPower(gamepad2.right_trigger - gamepad2.left_trigger);
-
-            /*
-            //Gamepad 2 B Red Duck
-            {
-                if (!gamepad2.x) {
-                    bDepressed = true;
-                }
-                if (gamepad2.x && bDepressed) {
-                    driveTrain.stopAll();
-                    spinner.spinOffBlueDuck();
-                    bDepressed = false;
-                }
-            }
-
-            //Gamepad2 X BlueDuck
-            {
-                if (!gamepad2.b) {
-                    bDepressed = true;
-                }
-                if (gamepad2.b && bDepressed) {
-                    driveTrain.stopAll();
-                    spinner.spinOffRedDuck();
-                    bDepressed = false;
-                }
-            }8/
-
-             */
-
 
             if (gamepad2.x){
                 spinner.setPowerBlueDirection();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Utills/TeleopTemplate.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.src.Utills;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.src.robotAttachments.DriveTrains.TeleopDriveTrain;
+import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.CarouselSpinner;
+import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.ContinuousIntake;
+import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.LinearSlide;
+import org.firstinspires.ftc.teamcode.src.robotAttachments.Subsystems.OdometryPodServos;
+
+
+@Disabled
+public class TeleopTemplate extends LinearOpMode {
+
+    public TeleopDriveTrain driveTrain;
+    public CarouselSpinner spinner;
+    public OdometryPodServos pod;
+    public LinearSlide slide;
+    public ContinuousIntake intake;
+
+    public void initAll() {
+        driveTrain = new TeleopDriveTrain(hardwareMap, "front_right", "front_left", "back_right", "back_left");
+
+        spinner = new CarouselSpinner(hardwareMap, "duck_spinner");
+
+        pod = new OdometryPodServos(hardwareMap, "right_odometry_servo", "left_odometry_servo", "horizontal_odometry_servo");
+        pod.raise();
+
+        slide = new LinearSlide(hardwareMap, "slide_motor");
+
+        intake = new ContinuousIntake(hardwareMap, "intake_motor");
+
+
+        telemetry.addData("Initialization Status", "Initialized");
+        telemetry.update();
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
