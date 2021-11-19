fb49b1b564536ca9daddfe3f51e75ea492412035
William Kopans <wkopans123@gmail.com>
2021-11-19 17:36:03 -0500

    Worked on Auto and Improved Readibility

--- WheelerRobotics/src/main/java/org/wheelerschool/robotics/comp/chassis/Meccanum.java
+++ WheelerRobotics/src/main/java/org/wheelerschool/robotics/comp/chassis/Meccanum.java
@@ -1,38 +1,22 @@
-package org.wheelerschool.robotics.comp.auto;
-
-import static com.sun.tools.doclint.Entity.and;
-import static com.sun.tools.doclint.Entity.ge;
-import static com.sun.tools.doclint.Entity.pi;
-import static com.sun.tools.doclint.Entity.tau;
-import static java.lang.Math.floor;
-import static java.lang.Math.round;
+package org.wheelerschool.robotics.comp.chassis;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.sun.tools.javac.comp.Todo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous
-public class AutoNav5 extends LinearOpMode {
-
+public class Meccanum {
 
     private ElapsedTime runtime = new ElapsedTime();
 
     private Servo servo0;
     private DcMotor arm;
-
 
     private final double SERVO_FULLY_CLOSED = 0;
     private final double SERVO_FULLY_OPENED = 0;
@@ -40,10 +24,6 @@ public class AutoNav5 extends LinearOpMode {
     private final double HIGH_SPINNER_POWER = 0;
     private final double OPTIMAL_SPINNER_POWER = 0;
     private final double MOTOR_STOP = 0;
-
-    private final double NORMAL_SPEED = 0.5;
-
-
 
     private DcMotor spinner;
 
@@ -56,90 +36,11 @@ public class AutoNav5 extends LinearOpMode {
 
     private Orientation angles;
 
-
-
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        // internal IMU setup
-
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
-        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
-        parameters.loggingEnabled      = true;
-        parameters.loggingTag          = "IMU";
-        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
-
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu.initialize(parameters);
-        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
-
-        // Meccanum Motors Definition and setting prefs
-
-        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
-
-        // Reverse the left side motors and set behaviors to stop instead of coast
-        motorFrontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        //define arm and servo objects and also spinner
-        servo0 = hardwareMap.get(Servo.class, "servo-0");
-        arm = hardwareMap.get(DcMotor.class, "arm");
-        spinner = hardwareMap.get(DcMotor.class, "spinner");
-
-        //set prefs for arm and servo
-        servo0.setDirection(Servo.Direction.FORWARD);
-        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        spinner = hardwareMap.get(DcMotor.class, "spinner");
-        telemetry.addData("Motor Names & Numbers: ", hardwareMap.allDeviceMappings);
-
-        waitForStart();
-
-
-
-        runtime.reset();
-        while(opModeIsActive()){
-            angles   = getAngles();
-            telemetry.addData("Elapsed Time  ", floor(runtime.seconds() / 60) + ":" + runtime.seconds() % 60 + ":" + round(runtime.milliseconds() % 1000) );
-
-            // 685mm  0.5power 1sec
-
-            telemetry.update();
-            executeAutomaticSequence2();
-
-
-        }
-    }
-    private void executeAutomaticSequence2(){
-
-        // auto for near carousel
-
-        motorDriveBack(NORMAL_SPEED);
-        driveEncodedDistance();
-
-        spinnySpinForward(OPTIMAL_SPINNER_POWER);
-        spinRotations(1);
-
-        motorDriveRight(NORMAL_SPEED);
-        driveEncodedDistance();
-
-        motorDriveBack(NORMAL_SPEED);
-        driveEncodedDistance();
+    public void init(HardwareMap hardwareMap){
 
     }
+
+
     private void motorDrive(double motorFrontLeftPower, double motorBackLeftPower, double motorFrontRightPower, double motorBackRightPower){
         motorBackLeft.setPower(motorBackLeftPower);
         motorFrontLeft.setPower(motorFrontLeftPower);
@@ -225,5 +126,7 @@ public class AutoNav5 extends LinearOpMode {
         telemetry.addData("Motor Get Position Return: ", pos);
         telemetry.update();
     }
+
+
 
 }
