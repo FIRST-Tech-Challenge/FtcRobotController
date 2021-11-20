186c168fdd2bc09269f4447a018a1542e4dcda7f
DraskiTheDragon <deanbras@gmail.com>
2021-11-20 11:14:03 -0700

    BlackSwanAutonomous

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousRedCarousel.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.blackswan;
-
-public class AutonomousRedCarousel {
-//strafe left
-//move carousel
-//strafe right to first barcode
-//    move forward up next to it
-}
-
