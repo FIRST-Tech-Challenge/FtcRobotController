f7440828ade1af7344b9fb731b1c0c7ea4c4c798
DraskiTheDragon <deanbras@gmail.com>
2021-11-20 09:44:51 -0700

    BlackSwanAutonomous

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousMoveTest.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousMoveTest.java
@@ -12,11 +12,7 @@ public class AutonomousMoveTest extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap,telemetry,this);
         waitForStart();
-        robot.forward(1,.5);
-        robot.turnRight(90, .1);
         robot.left(1,.5);
         robot.right(1,.5);
-        robot.turnLeft(90,.1);
-        robot.backward(1,.5);
     }
 }
