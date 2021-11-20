853acbfec068e46734f5587ceda570125ef97661
devanchoksi <devan.choksi@students.tka.org>
2021-11-19 17:35:25 -0800

    started code for intake

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/CSENSOR.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@TeleOp
+public class CSENSOR extends LinearOpMode{
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Test/ColorSensor.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Test;
-
-public class ColorSensor {
-}
