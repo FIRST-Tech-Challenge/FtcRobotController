371aa0d04e69a17643a62595a6fd973794682258
Colby <cms0679@outlook.com>
2021-11-20 10:05:02 -0500

    something idk

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectShippingElement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectShippingElement.java
@@ -171,6 +171,52 @@ public class DetectShippingElement extends LinearOpMode
         }
     }
 
+    class SamplePipeline extends OpenCvPipeline {
+        boolean viewportPaused = false;
+
+        /*
+         * NOTE: if you wish to use additional Mat objects in your processing pipeline, it is
+         * highly recommended to declare them here as instance variables and re-use them for
+         * each invocation of processFrame(), rather than declaring them as new local variables
+         * each time through processFrame(). This removes the danger of causing a memory leak
+         * by forgetting to call mat.release(), and it also reduces memory pressure by not
+         * constantly allocating and freeing large chunks of memory.
+         */
+
+        @Override
+        public Mat processFrame(Mat input) {
+            /*
+             * IMPORTANT NOTE: the input Mat that is passed in as a parameter to this method
+             * will only dereference to the same image for the duration of this particular
+             * invocation of this method. That is, if for some reason you'd like to save a copy
+             * of this particular frame for later use, you will need to either clone it or copy
+             * it to another Mat.
+             */
+
+            /*
+             * Draw a simple box around the middle 1/2 of the entire frame
+             */
+            Imgproc.rectangle(
+                    input,
+                    new Point(
+                            input.cols() / 4,
+                            input.rows() / 4),
+                    new Point(
+                            input.cols() * (3f / 4f),
+                            input.rows() * (3f / 4f)),
+                    new Scalar(0, 255, 0), 4);
+
+            /**
+             * NOTE: to see how to get data from your pipeline to your OpMode as well as how
+             * to change which stage of the pipeline is rendered to the viewport when it is
+             * tapped, please see {@link PipelineStageSwitchingExample}
+             */
+
+
+            return input;
+        }
+    }
+
 
 
 
