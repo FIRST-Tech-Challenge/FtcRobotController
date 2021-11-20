8cff36d6c1931e4622344f1fb5fc9edf5331aa45
Luke B <solinexi@gmail.com>
2021-11-20 11:59:56 -0700

    changed controlls

---
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousRedWarehouse.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.blackswan;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutonomousRedWarehouse extends LinearOpMode {
+    Robot robot;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot = new Robot(hardwareMap,telemetry,this);
+        waitForStart();
+        robot.forward(.1,.5);
+    }
+}
