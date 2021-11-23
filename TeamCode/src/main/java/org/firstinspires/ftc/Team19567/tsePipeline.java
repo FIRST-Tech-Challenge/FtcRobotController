56c93cbbc188ebb8e765247f344207406741faf0
Ian <ianpengwu@gmail.com>
2021-11-22 18:57:11 -0600

    Ian changes

--- TeamCode/src/main/java/org/firstinspires/ftc/Team19567/tsePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/Team19567/tsePipeline.java
@@ -1,13 +1,18 @@
 package org.firstinspires.ftc.Team19567;
 
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 class tsePipeline extends OpenCvPipeline
 {
+
+    private Mat greyFrame = null;
     @Override
     public Mat processFrame(Mat input)
     {
-        return input;
+
+        Imgproc.cvtColor(input,greyFrame,Imgproc.COLOR_RGBA2GRAY);
+        return greyFrame;
     }
 }
