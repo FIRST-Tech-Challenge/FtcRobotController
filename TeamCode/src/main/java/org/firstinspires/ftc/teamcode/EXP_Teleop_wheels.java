ec5af01e4aa28480f1535237a7b52f9a9ac6f527
Gabe-Garcia05 <gaberuth2015@gmail.com>
2021-11-23 11:16:33 -0800

    Update EXP_Teleop_wheels.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EXP_Teleop_wheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EXP_Teleop_wheels.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import static java.lang.StrictMath.abs;
 
@@ -25,117 +26,80 @@ public class EXP_Teleop_wheels extends LinearOpMode {
     private DcMotor lb = null;
     private DcMotor rb = null;
 
-    private DcMotor SoN = null;
+    private DcMotor tower1 = null;
+    private DcMotor tower2 = null;
+
+    private Servo clawservo = null;
 
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        lf  = hardwareMap.get(DcMotor.class, "lf");
+        lf = hardwareMap.get(DcMotor.class, "lf");
         rf = hardwareMap.get(DcMotor.class, "rf");
-        lb  = hardwareMap.get(DcMotor.class, "lb");
+        lb = hardwareMap.get(DcMotor.class, "lb");
         rb = hardwareMap.get(DcMotor.class, "rb");
+        tower1 = hardwareMap.get(DcMotor.class, "tower1");
+        tower2 = hardwareMap.get(DcMotor.class, "tower2");
+        clawservo = hardwareMap.get(Servo.class,"clawservo");
 
-        SoN = hardwareMap.get(DcMotor.class, "SoN");
-
-        //left side needs to be reversed because the motors are upside down, forehead
         lf.setDirection(DcMotor.Direction.REVERSE);
         rf.setDirection(DcMotor.Direction.FORWARD);
         lb.setDirection(DcMotor.Direction.REVERSE);
         rb.setDirection(DcMotor.Direction.FORWARD);
-
-        SoN.setDirection(DcMotor.Direction.FORWARD);
-
-        float lfPower;
-        float rfPower;
-        float lbPower;
-        float rbPower;
-
-        float collectPwr;
-
-        float SFPwr;
-        float SSPwr;
-        //turnPwr: + clockwise, - counterclockwise
-        float turnPwr;
-
-        double deadzone = 0.1;
-        double basePwrMult = 0.3;
-        double highPwrMult = 0.6;
-        double SoNPwrMult = -1;
+        tower1.setDirection(DcMotor.Direction.FORWARD);
+        tower2.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
-        runtime.reset();
+
+        double lPower;
+        double rPower;
+        double towerPower;
+        double deadzone;
+        double towerPower2;
+
+        lPower = 0.0f;
+        rPower = 0.0f;
+        towerPower = 0.0f;
+        deadzone = 0.2f;
+        towerPower2 = 0.0f;
 
         while (opModeIsActive()) {
-            //reset so it doesn't stack
-            lfPower = 0.0f;
-            rfPower = 0.0f;
-            lbPower = 0.0f;
-            rbPower = 0.0f;
 
-            collectPwr = 0.0f;
 
-            //set directional energy according to gamepad1
 
-            SFPwr = gamepad1.left_stick_y;
-            SSPwr = gamepad1.left_stick_x;
-            turnPwr = gamepad1.right_stick_x;
+            lPower = gamepad1.left_stick_y;
+            rPower = gamepad1.right_stick_y;
+            towerPower = gamepad2.right_trigger;
+            towerPower2 = gamepad2.left_trigger;
 
-            //set the power vars according to directional energy
 
-            if(abs(SFPwr) > deadzone){
-                lfPower -= SFPwr;
-                rfPower -= SFPwr;
-                lbPower -= SFPwr;
-                rbPower -= SFPwr;
+            if (gamepad2.a) {
+                clawservo.setPosition(0.0);
+            }
+            if (gamepad2.b) {
+                clawservo.setPosition(.75);
             }
 
-            if(abs(SSPwr) > deadzone){
-                lfPower += SSPwr;
-                rfPower -= SSPwr;
-                lbPower -= SSPwr;
-                rbPower += SSPwr;
+            if (towerPower <= deadzone){
+                towerPower = 0.0f;
+            }
+            if (towerPower2 <= deadzone){
+                towerPower2 = 0.0f;
             }
 
-            if(abs(turnPwr) > deadzone){
-                lfPower -= turnPwr;
-                rfPower += turnPwr;
-                lbPower -= turnPwr;
-                rbPower += turnPwr;
-            }
-
-
-            //TODO put SoN on gamepad2
-            //sticking with gamepad1 for ease of testing at this point
-            collectPwr = gamepad1.right_trigger;
-
-
-            lfPower = Range.clip(lfPower, -1, 1);
-            rfPower = Range.clip(rfPower, -1, 1);
-            lbPower = Range.clip(lbPower, -1, 1);
-            rbPower = Range.clip(rbPower, -1, 1);
-
-            collectPwr = Range.clip(collectPwr, 0, 1);
-
-            if (gamepad1.right_bumper){
-                lf.setPower(lfPower * highPwrMult);
-                rf.setPower(rfPower * highPwrMult);
-                lb.setPower(lbPower * highPwrMult);
-                rb.setPower(rbPower * highPwrMult);
-            } else{
-                lf.setPower(lfPower * basePwrMult);
-                rf.setPower(rfPower * basePwrMult);
-                lb.setPower(lbPower * basePwrMult);
-                rb.setPower(rbPower * basePwrMult);
-            }
-
-            SoN.setPower(collectPwr * SoNPwrMult);
+            lf.setPower(lPower);
+            rf.setPower(rPower);
+            lb.setPower(lPower);
+            rb.setPower(rPower);
+            tower1.setPower(towerPower-towerPower2);
+            tower2.setPower(towerPower-towerPower2);
 
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "leftfront (%.2f), rightfront (%.2f), leftback (%.2f), rightback (%.2f), SoN (%.2f)", lfPower, rfPower, lbPower ,rbPower, collectPwr);
-            telemetry.addData("Power Multipliers", "normal (%.2f), sprint (%.2f), SoN (%.2f)", basePwrMult, highPwrMult, SoNPwrMult);
-            telemetry.addData("Direction", "fw (%.2f), side (%.2f), turn (%.2f)", SFPwr, SSPwr, turnPwr);
+            telemetry.addData("Motors", "leftfront (%.2f), rightfront (%.2f), leftback (%.2f), rightback (%.2f), SoN (%.2f)", lPower, rPower, lPower ,rPower, towerPower-towerPower2);
+            //telemetry.addData("Power Multipliers", "normal (%.2f), sprint (%.2f), SoN (%.2f)", basePwrMult, highPwrMult, SoNPwrMult);
+            //telemetry.addData("Direction", "fw (%.2f), side (%.2f), turn (%.2f)", SFPwr, SSPwr, turnPwr);
             telemetry.update();
         }
     }
