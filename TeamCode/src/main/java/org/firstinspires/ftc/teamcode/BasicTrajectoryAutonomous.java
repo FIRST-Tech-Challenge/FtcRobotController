661b3d160bc3f2cbb00bfacdf3e80184f824d566
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-11-22 19:50:55 -0600

    yes yes will do this sometime actually

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTrajectoryAutonomous.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class BasicTrajectoryAutonomous extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
