51fa6a1f3b4db505254d9561abee771ef7fcae88
ryan <tyryanosaur@gmail.com>
2021-11-23 13:58:38 -0600

    epic meepmeep installation

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,25 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
+
+public class MeepMeepTesting {
+    public static void main(String args[]) {
+        MeepMeep mm = new MeepMeep(800)
+                // Set field image
+                .setBackground(MeepMeep.Background.FIELD_ULTIMATE_GOAL_DARK)
+                // Set theme
+                .setTheme(new ColorSchemeRedDark())
+                // Background opacity from 0-1
+                .setBackgroundAlpha(1f)
+                // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .splineTo(new Vector2d(50,62),Math.toRadians(70)).build()
+                )
+                .start();
+    }
+}
