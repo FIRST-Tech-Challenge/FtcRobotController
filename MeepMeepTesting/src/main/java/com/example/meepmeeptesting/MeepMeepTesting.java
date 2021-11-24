5ce29e699d886e733c78289a82a04d8f434ea0ed
ryan <tyryanosaur@gmail.com>
2021-11-24 17:05:04 -0600

    OpenCV first pipeline completed by Ryan.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,16 +9,21 @@ public class MeepMeepTesting {
     public static void main(String args[]) {
         MeepMeep mm = new MeepMeep(800)
                 // Set field image
-                .setBackground(MeepMeep.Background.FIELD_ULTIMATE_GOAL_DARK)
+                .setBackground(MeepMeep.Background.FIELD_FREIGHT_FRENZY)
                 // Set theme
                 .setTheme(new ColorSchemeRedDark())
                 // Background opacity from 0-1
                 .setBackgroundAlpha(1f)
                 // Set constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15).setBotDimensions(12,15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .splineTo(new Vector2d(50,62),Math.toRadians(70)).build()
+                        drive.trajectorySequenceBuilder(new Pose2d(6, -63, Math.toRadians(90))).waitSeconds(5)
+                                .lineToSplineHeading(new Pose2d(7,-24,0))
+                                .waitSeconds(3)
+                                .lineTo(new Vector2d(12,-64)).strafeTo(new Vector2d(47,-64)).waitSeconds(3)
+                                .strafeTo(new Vector2d(15,-64)).lineToSplineHeading(new Pose2d(-11.5,-41,Math.toRadians(-90)))
+                                .waitSeconds(3).lineToSplineHeading(new Pose2d(12,-64,0)).strafeTo(new Vector2d(45,-64)).waitSeconds(3)
+                                .build()
                 )
                 .start();
     }
