5c95e63133bc47e07ca763f874b95408edd9e627
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2021-11-27 10:21:59 -0700

    Autonomous pathing progress.

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/TestLinearSlideReadings.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.masters;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class TestLinearSlideReadings extends LinearOpMode {
+    DcMotor linearSlideMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        linearSlideMotor = hardwareMap.dcMotor.get("");
+        int linearSlideOn = 0;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if(gamepad1.x && !(linearSlideOn == 1)) {
+                if(linearSlideMotor.getPower() != 0) linearSlideMotor.setPower(0);
+                else linearSlideMotor.setPower(.2);
+                linearSlideOn = 1;
+            } else if(!gamepad1.x) linearSlideOn = 0;
+
+            if(gamepad1.y && !(linearSlideOn == -1)) {
+                if(linearSlideMotor.getPower() != 0) linearSlideMotor.setPower(0);
+                else linearSlideMotor.setPower(-.2);
+                linearSlideOn = -1;
+            } else if(!gamepad1.y) linearSlideOn = 0;
+
+            telemetry.addData("Linear Slide Encoder Position: ", linearSlideMotor.getCurrentPosition());
+        }
+    }
+}
