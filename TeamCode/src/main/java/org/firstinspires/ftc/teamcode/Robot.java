70e6c6f7d7816d441adef97e287a078574f76a9a
8toAutomate <eighttoautomate@outlook.com>
2021-11-27 20:43:17 -0500

    started on the robot class, created simple functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.sun.tools.javac.code.Lint;
 
 public class Robot {
 
@@ -13,12 +14,18 @@ public class Robot {
     public DcMotor backLeftMotor = null;
     public DcMotor backRightMotor = null;
 
+    DcMotor[] driveMotors = {frontLeftMotor, frontRightMotor, backLeftMotor, backRightMotor};
+
+    public OpMode systemTools;
+
+
     //Define other variables
     HardwareMap hwMap;
 
     //Constructor Method
-    public Robot(HardwareMap hwMapIn) {
+    public Robot(HardwareMap hwMapIn, OpMode systemToolsIn) {
         hwMap = hwMapIn;
+        systemTools = systemToolsIn;
 
         // Define and Initialize Motors
         frontLeftMotor  = hwMap.get(DcMotor.class, "front_left_drive");
@@ -100,23 +107,124 @@ public class Robot {
     }
 
     public void resetDriveEncoders() {
-        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        for (DcMotor motor : driveMotors) {
+            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
     }
 
     public void startDriveEncoders() {
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        for (DcMotor motor : driveMotors) {
+            motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
     }
 
     public void startDriveEncodersTarget() {
-        frontLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        for (DcMotor motor : driveMotors) {
+            motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        }
+    }
+
+    public void RotateDEG(int degrees, double power, LinearOpMode linearOpMode, boolean TelemetryOn) {
+
+        final double conversionFactor = 8.46; // for outreach robot: 8.46, for FreightFrenzy robot: TBD
+
+        if (degrees < 0 && power > 0) power = -power;
+
+        int ticks = (int) Math.abs(Math.round(degrees * conversionFactor));
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Status", "Resetting Encoders");
+            systemTools.telemetry.update();
+        }
+
+        resetDriveEncoders();
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Path0", "Starting at %7d :%7d",
+                    frontLeftMotor.getCurrentPosition(),
+                    frontRightMotor.getCurrentPosition(),
+                    backLeftMotor.getCurrentPosition(),
+                    backRightMotor.getCurrentPosition());
+            systemTools.telemetry.update();
+        }
+
+        int FLtarget = frontLeftMotor.getCurrentPosition() + ticks;
+        int FRtarget = frontRightMotor.getCurrentPosition() - ticks;
+        int BLtarget = backLeftMotor.getCurrentPosition() + ticks;
+        int BRtarget = backRightMotor.getCurrentPosition() - ticks;
+
+        startDriveEncoders();
+
+        frontLeftMotor.setPower(power);
+        frontRightMotor.setPower(-power);
+        backRightMotor.setPower(-power);
+        backLeftMotor.setPower(power);
+
+        while (linearOpMode.opModeIsActive() &&
+              (Math.abs(frontLeftMotor.getCurrentPosition()) < ticks && Math.abs(frontRightMotor.getCurrentPosition()) < ticks && Math.abs(backLeftMotor.getCurrentPosition()) < ticks && Math.abs(backRightMotor.getCurrentPosition()) < ticks)) {
+        }
+
+        stopDriveMotors();
+
+        startDriveEncoders();
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Path", "Complete");
+            systemTools.telemetry.addData("counts", ticks);
+            systemTools.telemetry.update();
+        }
+
+    }
+
+    public void StrafeCM(int Centimeters, double Power, LinearOpMode linearOpMode, boolean TelemetryOn) {
+
+        final double conversionFactor = 8.46; // outreach robot: 8.46 FreightFrenzy robot: TBD
+        if (Centimeters < 0 && Power > 0) Power = -Power;
+
+        int ticks = (int) Math.abs(Math.round(Centimeters * conversionFactor));
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Status", "Resetting Encoders");
+            systemTools.telemetry.update();
+        }
+
+        resetDriveEncoders();
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Path0", "Starting at %7d :%7d",
+                    frontLeftMotor.getCurrentPosition(),
+                    frontRightMotor.getCurrentPosition(),
+                    backLeftMotor.getCurrentPosition(),
+                    backRightMotor.getCurrentPosition());
+            systemTools.telemetry.update();
+        }
+
+        int FLtarget = frontLeftMotor.getCurrentPosition() - ticks;
+        int FRtarget = frontRightMotor.getCurrentPosition() + ticks;
+        int BLtarget = backLeftMotor.getCurrentPosition() + ticks;
+        int BRtarget = backRightMotor.getCurrentPosition() - ticks;
+
+        startDriveEncoders();
+
+        frontLeftMotor.setPower(-Power);
+        frontRightMotor.setPower(Power);
+        backRightMotor.setPower(Power);
+        backLeftMotor.setPower(-Power);
+
+        while (linearOpMode.opModeIsActive() &&
+                (Math.abs(frontLeftMotor.getCurrentPosition()) < ticks && Math.abs(frontRightMotor.getCurrentPosition()) < ticks && Math.abs(backLeftMotor.getCurrentPosition()) < ticks && Math.abs(backRightMotor.getCurrentPosition()) < ticks)) {
+        }
+
+        stopDriveMotors();
+
+        startDriveEncoders();
+
+        if (TelemetryOn) {
+            systemTools.telemetry.addData("Path", "Complete");
+            systemTools.telemetry.addData("counts", ticks);
+            systemTools.telemetry.update();
+
+        }
+
     }
 }
