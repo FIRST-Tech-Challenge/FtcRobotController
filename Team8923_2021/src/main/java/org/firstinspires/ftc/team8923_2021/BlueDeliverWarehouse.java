a51ef91b9611329dc07287d742e939a48bd5428c
Sanj Saini <Sanj26Saini@gmail.com>
2021-11-28 15:24:34 -0800

    Added more deliver code

--- Team8923_2021/src/main/java/org/firstinspires/ftc/team8923_2021/BlueDeliverWarehouse.java
+++ Team8923_2021/src/main/java/org/firstinspires/ftc/team8923_2021/BlueDeliverWarehouse.java
@@ -1,4 +1,21 @@
 package org.firstinspires.ftc.team8923_2021;
 
-public class BlueDeliverWarehouse {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="BlueDeliverWarehouse")
+public class BlueDeliverWarehouse extends MasterAutonomous {
+    public void runOpMode() throws InterruptedException {
+        initAuto();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double referenceAngle = imu.getAngularOrientation().firstAngle;
+            moveForward(5.0, 10, 10);
+            imuPivot(referenceAngle, 90, 35, 0.015, 3.0);
+            imuPivot(referenceAngle, 90, 35, 0.015, 3.0);
+            moveForward(-2.0, 10, 10);
+            autoDeliver();
+            imuPivot(referenceAngle, -90, 35, 0.015, 3.0);
+        }
+    }
 }
