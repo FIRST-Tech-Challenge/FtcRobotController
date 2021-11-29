56a1ad41acc518a5d6bd91eaea68262fd850e743
Ethan Sanders <ethanmrcod@gmail.com>
2021-11-28 19:00:16 -0600

    cv implementation perhaps

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CVAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/CVAuto.java
@@ -3,14 +3,24 @@ package org.firstinspires.ftc.teamcode.opmodes.auto;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.core.robot.vision.robot.TseDetector;
+import org.firstinspires.ftc.teamcode.core.thread.EventThread;
+
 @Autonomous
 public class CVAuto extends LinearOpMode {
+    public EventThread eventThread = new EventThread(this::opModeIsActive);
     @Override
     public void runOpMode(){
+        TseDetector webcam = new TseDetector(eventThread, hardwareMap, "webcam");
+        Thread thread = new Thread(() -> {
+            telemetry.addData("webcamOutput", webcam.run());
+            telemetry.update();
+        });
+        thread.setPriority(4);
         waitForStart();
-        while (opModeIsActive()) {
-
-        }
+        thread.start();
+        //noinspection StatementWithEmptyBody
+        while (opModeIsActive()) { }
         requestOpModeStop();
     }
 }
