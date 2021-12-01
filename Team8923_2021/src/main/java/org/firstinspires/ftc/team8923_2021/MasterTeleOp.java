2c21e9f77a56d6bddd51a143619ed20560ea82ca
Daania Abbas <>
2021-11-30 20:09:48 -0800

    new teleop lift

--- Team8923_2021/src/main/java/org/firstinspires/ftc/team8923_2021/MasterTeleOp.java
+++ Team8923_2021/src/main/java/org/firstinspires/ftc/team8923_2021/MasterTeleOp.java
@@ -53,7 +53,6 @@ abstract public class MasterTeleOp extends MasterOpMode {
 
     public void deliver() throws InterruptedException {
         if(gamepad2.left_bumper) {
-            motorLift.getCurrentPosition();
             motorLift.setTargetPosition(10);
             wait(2000);
             //motorLift.setPower(0.1);
@@ -61,7 +60,6 @@ abstract public class MasterTeleOp extends MasterOpMode {
         } else if (gamepad2.right_bumper){
             servoGrabber.setPosition(0.0);
             wait(2000);
-            motorLift.getCurrentPosition();
             motorLift.setTargetPosition(0);
         } else {
             motorLift.setPower(0.0);
