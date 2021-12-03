d157ddbe7f0c56876a022f3294633d2db2782890
BlockBeasts <kjosien1@gmail.com>
2021-12-02 17:17:20 -0700

    add arm

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousRedWarehouse.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousRedWarehouse.java
@@ -1,6 +1,11 @@
-package org.firstinspires.ftc.blackswan;
+package org.firstinspires.ftc.blackSwan;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.blackSwan.Robot;
+
+@Autonomous
 
 public class AutonomousRedWarehouse extends LinearOpMode {
     Robot robot;
@@ -8,6 +13,12 @@ public class AutonomousRedWarehouse extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         robot = new Robot(hardwareMap,telemetry,this);
         waitForStart();
-        robot.forward(.1,.5);
+        robot.forward(1.5,.5);
+        robot.left(2,.5);
+        // this is where the dropping code will go
+        robot.back(.2,.5);
+        robot.turnRight(70,.5);
+        robot.forward(5,1);
+        robot.turnRight(30,.5);
     }
 }
