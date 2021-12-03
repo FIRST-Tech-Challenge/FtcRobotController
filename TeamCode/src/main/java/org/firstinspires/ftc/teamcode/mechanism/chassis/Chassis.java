406963dbeed414f505985b830c55c32c560b0f78
Charlie Garfield <charlie.garfield@hotmail.com>
2021-12-03 09:45:10 -0500

    Got rid of static from chassis motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/chassis/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/chassis/Chassis.java
@@ -7,10 +7,10 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.mechanism.Mechanism;
 
 public abstract class Chassis implements Mechanism {
-    public static DcMotor frontRight;
-    public static DcMotor frontLeft;
-    public static DcMotor backRight;
-    public static DcMotor backLeft;
+    public DcMotor frontRight;
+    public DcMotor frontLeft;
+    public DcMotor backRight;
+    public DcMotor backLeft;
     public void init(HardwareMap hardwareMap){
         frontRight = hardwareMap.get(DcMotor.class, "fr");
         frontLeft = hardwareMap.get(DcMotor.class, "fl");
