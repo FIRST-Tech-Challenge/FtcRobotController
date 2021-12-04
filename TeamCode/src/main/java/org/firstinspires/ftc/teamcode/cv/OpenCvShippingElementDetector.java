0ad1ec971e790958847495a2981719a77f6fde2a
Abigail Booth <chickensalt1238@gmail.com>
2021-12-04 12:11:02 -0500

    updated path to model
    updated path to model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/OpenCvShippingElementDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/OpenCvShippingElementDetector.java
@@ -67,7 +67,7 @@ public class OpenCvShippingElementDetector extends OpenCvPipeline {
         this.telemetry = telemetry;
 
         cvDNN = new Dnn();
-        net = cvDNN.readNetFromTensorflow("/sdcard/FIRST/EasyOpenCV/models/freight_frenzy_emma_optimized_graph.pb");
+        net = cvDNN.readNetFromTensorflow("/sdcard/FIRST/EasyOpenCV/models/freight_frenzy_tse_optimized_graph.pb");
         for(int i=0; i<classNames.length; i++)
             colors.add(randomColor());
 
