7b2c16047b16943265636ddd4a84a4f45a6c8726
hqlite <jonathanrhthong@gmail.com>
2021-12-03 16:31:16 -0800

    Intake Methods + Visual changes

--- FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Intake/IntakeMethods.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/teamcode/Intake/IntakeMethods.java
@@ -1,8 +1,45 @@
 
 package org.firstinspires.ftc.teamcode.Intake;
-import org.firstinspires.ftc.robotcore.internal.hardware.DcMotor;
 
-
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class IntakeMethods {
+    DcMotor intakeMotor;
+
+    //Toggle
+    Boolean prevInput;
+    Boolean itToggle;
+
+    public IntakeMethods(HardwareMap hardwareMap){
+        intakeMotor = hardwareMap.dcMotor.get("motorIntake");
+        prevInput = false;
+        itToggle = false;
+    }
+
+
+    public void intakeToggle(Boolean inp) {
+        if(inp == true && prevInput == false) {
+            itToggle = !itToggle;
+            prevInput = true;
+        }
+        prevInput = inp;
+        if (itToggle == true) {
+            intakeMotor.setPower(1);
+        } else {
+            intakeMotor.setPower(0);
+        }
+    }
+
+
+    public void intakeHold(Boolean inp) {
+        if (inp == true) {
+            intakeMotor.setPower(1);
+        } else {
+            intakeMotor.setPower(0);
+        }
+    }
+
 }
+
+
