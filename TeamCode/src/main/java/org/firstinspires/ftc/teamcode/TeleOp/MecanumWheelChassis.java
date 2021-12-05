c4a1eeb9998814dabb928ef1293e831d95e6bde9
Aadarsh Senthil <43424645+DarkXNinja@users.noreply.github.com>
2021-12-05 15:18:00 -0500

    Update MecanumWheelChassis.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumWheelChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MecanumWheelChassis.java
@@ -1,1 +1,108 @@
+/*
+Copyright 2021 FIRST Tech Challenge Team FTC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a PushBot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Remove a @Disabled the on the next line or two (if present) to add this opmode to the Driver Station OpMode list,
+ * or add a @Disabled annotation to prevent this OpMode from being added to the Driver Station
+ */
+@TeleOp
+
+public class MecanumWheelChassis extends LinearOpMode {
+    
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    
+    @Override
+    public void runOpMode() {
+        
+        initializeRobot();
+        
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            
+            if (gamepad1.right_bumper) {
+                // go right
+                frontRight.setPower(1);
+                frontLeft.setPower(-1);
+                backRight.setPower(-1);
+                backLeft.setPower(1);
+
+            } else if (gamepad1.left_bumper) {
+                // go left
+                frontLeft.setPower(-1);
+                frontRight.setPower(1);
+                backLeft.setPower(1);
+                backRight.setPower(-1);
+
+            } else {
+        
+                frontLeft.setPower(gamepad1.left_stick_y * 0.5);
+                backLeft.setPower(gamepad1.right_stick_y * 0.5);
+                frontRight.setPower(gamepad1.right_stick_y * 0.5);
+                backRight.setPower(gamepad1.left_stick_y * 0.5);
+
+            }
+ 
+            
+            
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+    
+    
+    private void initializeRobot() {
+        
+        frontLeft = hardwareMap.get(DcMotor.class,"FrontLeft");
+        frontRight = hardwareMap.get(DcMotor.class,"FrontRight");
+        backLeft = hardwareMap.get(DcMotor.class,"BackLeft");
+        backRight = hardwareMap.get(DcMotor.class,"BackRight");
+        
+       frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+       backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+       
+       
+        
+    }
+}
 
