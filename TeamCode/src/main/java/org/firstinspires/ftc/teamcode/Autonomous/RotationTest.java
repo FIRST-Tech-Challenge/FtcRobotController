65fa59c729c49442b36349406f6514e4c288c32a
Aadarsh Senthil <43424645+DarkXNinja@users.noreply.github.com>
2021-12-05 15:32:34 -0500

    Update RotationTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RotationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RotationTest.java
@@ -1,1 +1,159 @@
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+
+public class RotationTest extends LinearOpMode{
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    
+    @Override
+    public void runOpMode() {
+        
+        initializeRobot();
+        
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        if (opModeIsActive()) {
+            
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            /*
+            //forward
+            frontLeft.setPower(0.5);
+            backLeft.setPower(0.5);
+            frontRight.setPower(0.5);
+            backRight.setPower(0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            
+            sleep(5000);
+            
+            //backward
+            frontLeft.setPower(-0.5);
+            backLeft.setPower(-0.5);
+            frontRight.setPower(-0.5);
+            backRight.setPower(-0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+           
+            sleep(5000);
+            */
+            //strafe right
+            frontLeft.setPower(-0.5);
+            backLeft.setPower(0.5);
+            frontRight.setPower(0.5);
+            backRight.setPower(-0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+           
+            sleep(5000);
+            
+            //strafe left
+            frontLeft.setPower(0.5);
+            backLeft.setPower(-0.5);
+            frontRight.setPower(-0.5);
+            backRight.setPower(0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+           
+            sleep(5000);
+            
+            //forward right
+            frontLeft.setPower(0);
+            backLeft.setPower(0.5);
+            frontRight.setPower(0.5);
+            backRight.setPower(0);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            
+            sleep(5000);
+            
+            //forward left
+            frontLeft.setPower(0.5);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            
+            sleep(5000);
+            
+            //backward right
+            frontLeft.setPower(-0.5);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(-0.5);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            
+            sleep(5000);
+            
+            //backward left
+            frontLeft.setPower(0);
+            backLeft.setPower(-0.5);
+            frontRight.setPower(-0.5);
+            backRight.setPower(0);
+            sleep(1000);
+            
+            frontLeft.setPower(0);
+            backLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            
+        }
+    }
+    
+    
+    private void initializeRobot() {
+        
+        frontLeft = hardwareMap.get(DcMotor.class,"FrontLeft");
+        frontRight = hardwareMap.get(DcMotor.class,"FrontRight");
+        backLeft = hardwareMap.get(DcMotor.class,"BackLeft");
+        backRight = hardwareMap.get(DcMotor.class,"BackRight");
+        
+        frontLeft.setDirection(DcMotor.Direction.FORWARD) ;
+        frontRight.setDirection(DcMotor.Direction.REVERSE) ;
+        backLeft.setDirection(DcMotor.Direction.FORWARD) ;
+        backRight.setDirection(DcMotor.Direction.REVERSE) ;
+    }
+}
