9483f652cde5d538d1952c0286b9ae73d623e504
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-12-05 19:42:52 -0600

    brain going numb

---
+++ meepmeep/src/main/java/com/mrcod/meepmeep/MeepMeepHelper.java
@@ -0,0 +1,21 @@
+package com.mrcod.meepmeep;
+
+public class MeepMeepHelper {
+    /**
+     * Field is 12 by 12, coordinates are 140 by 140, which means that we can multiply feet by
+     * 140/12, which is equivalent to 35/3.
+     * @param feet size in feet
+     * @return size in coordinates
+     */
+    public static double feetToCoordinate(double feet) {
+        return feet*35/3;
+    }
+
+    /**
+     * Since changing feet to coords is multiplying by 3/35, and a foot is 12 inches, we can
+     * multiply inches by 35/3 * 1/12, which is equivalent to 1/140.
+     */
+    public static double inchesToCoordinate(double inches) {
+        return feetToCoordinate(inches/12);
+    }
+}
