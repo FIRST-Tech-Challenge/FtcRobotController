2c2fa209cd1426354722f6d723fb5b93255ee632
Alex <hbostic@gmail.com>
2021-12-07 13:39:18 -0500

    Implemented Subsystem for the blinkin LEDs using the FTCLib Object Model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/leds/blinkin/LEDSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/leds/blinkin/LEDSubsystem.java
@@ -1,2 +1,71 @@
-package org.firstinspires.ftc.teamcode.subsystems.leds.blinkin;public class LEDSubsystem {
+package org.firstinspires.ftc.teamcode.subsystems.leds.blinkin;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.internal.system.Deadline;
+import org.firstinspires.ftc.teamcode.opmodes.teleop.SampleRevBlinkinLedDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LEDSubsystem extends SubsystemBase {
+
+    private final RevBlinkinLedDriver blinkinLedDriver;
+    private DisplayKind displayKind;
+    private RevBlinkinLedDriver.BlinkinPattern pattern;
+
+
+    /*
+     * Change the pattern every 10 seconds in AUTO mode.
+     */
+    private int ledPeriod = 10;
+
+    private Deadline ledCycleDeadline;
+
+    protected enum DisplayKind {
+        MANUAL,
+        AUTO
+    }
+
+    public LEDSubsystem(final HardwareMap hwMap, final String deviceName){
+
+        blinkinLedDriver = hwMap.get(RevBlinkinLedDriver.class, deviceName);
+
+    }
+
+    public LEDSubsystem(final HardwareMap hwMap, final String deviceName, final int ledPeriod){
+
+        blinkinLedDriver = hwMap.get(RevBlinkinLedDriver.class, deviceName);
+        this.ledPeriod = ledPeriod;
+
+    }
+
+    public LEDSubsystem(final HardwareMap hwMap, final String deviceName, RevBlinkinLedDriver.BlinkinPattern p, final int ledPeriod){
+
+        blinkinLedDriver = hwMap.get(RevBlinkinLedDriver.class, deviceName);
+
+        setPattern(p);
+        setLedPeriod(ledPeriod);
+        setLedCycleDeadline();
+
+    }
+
+    public void setDisplayKind(DisplayKind dk){
+        displayKind = dk;
+    }
+
+
+    public void setPattern(RevBlinkinLedDriver.BlinkinPattern p){
+        pattern = p;
+        blinkinLedDriver.setPattern(pattern);
+    }
+
+    public void setLedPeriod(final int ledPeriod){
+        this.ledPeriod = ledPeriod;
+    }
+
+    public void setLedCycleDeadline(){
+        ledCycleDeadline = new Deadline(ledPeriod, TimeUnit.SECONDS);
+    }
 }
