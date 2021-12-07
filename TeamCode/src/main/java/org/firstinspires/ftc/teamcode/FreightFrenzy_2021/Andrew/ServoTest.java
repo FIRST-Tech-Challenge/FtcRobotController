f565e4d7751c30f72a1e2397cf6289c76b16254e
andrew200356 <59988150+andrew200356@users.noreply.github.com>
2021-12-07 17:45:52 -0500

    Update ServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy_2021/Andrew/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzy_2021/Andrew/ServoTest.java
@@ -8,24 +8,26 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-@TeleOp(name = "Servo Test", group = "Linear OpMode")
-@Disabled
+@TeleOp(name = "Servo Test", group = "Testing")
+
 
 public class ServoTest extends LinearOpMode{
-    private Servo Rotate = null;
+    private CRServo Rotate = null;
     private ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
     public void runOpMode() {
 
-        Rotate = hardwareMap.get(Servo.class, "Rotate");
-        Rotate.setDirection(Servo.Direction.FORWARD);
+        Rotate = hardwareMap.get(CRServo.class, "Rotate");
+        Rotate.setDirection(CRServo.Direction.FORWARD);
 
 
         telemetry.addData("Status", "Initialized");
@@ -47,28 +49,31 @@ public class ServoTest extends LinearOpMode{
 
             if (gamepad1.dpad_up) {
                 if (releasedDU1){
-
-                    while(Rotate.getPosition() <= 1.0 && gamepad1.dpad_left) {
-                        Rotate.setPosition(Rotate.getPosition()+0.02);
-                        sleep(40);
-                    }
+                    Rotate.setDirection(CRServo.Direction.FORWARD);
+                    Rotate.setPower(0.5);
                     releasedDU1 = false;
                 }
             } else if (!releasedDU1){
+                Rotate.setPower(0);
                 releasedDU1 = true;
             }
             if (gamepad1.dpad_down) {
                 if (releasedDD1){
-                    while(Rotate.getPosition() >= 0.03 && gamepad1.dpad_right) {
-                        Rotate.setPosition(Rotate.getPosition()-0.02);
-                        sleep(40);
-                    }
+                    Rotate.setDirection(CRServo.Direction.REVERSE);
+                    Rotate.setPower(0.5);
                     releasedDD1 = false;
+//                    while(Rotate.getPosition() >= 0.03 && gamepad1.dpad_right) {
+//                        Rotate.setPosition(Rotate.getPosition()-0.02);
+//                        sleep(40);
+//                    }
+//                    releasedDD1 = false;
                 }
             } else if (!releasedDD1){
+                Rotate.setPower(0);
                 releasedDD1 = true;
             }
-            telemetry.addData("Servo","Rotate (%.2f)", Rotate.getPosition());
+            telemetry.addData("Rotate speed","Rotate (%.2f)", Rotate.getPower());
+            telemetry.addData("Rotate direction","Rotate (%.2f)", Rotate.getDirection());
             telemetry.update();
 
         }
