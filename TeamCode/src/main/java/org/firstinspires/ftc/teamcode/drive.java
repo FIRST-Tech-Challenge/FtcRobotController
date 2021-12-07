831620808db98efc1bdad5123f9504d5b9d4d5ac
nicole <nicoleortuno1550@gmail.com>
2021-12-07 18:56:51 -0500

    two drive motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -95,6 +95,7 @@ public class drive extends LinearOpMode {
     TouchSensor touch;
     Servo servo;
 
+    //right trigger move one motor more depending on 1 or -1 values (Range.clip())
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
@@ -134,9 +135,10 @@ public class drive extends LinearOpMode {
             // POV Mode uses left stick to go forward, and right stick to turn.
             // - This uses basic math to combine motions and is easier to drive straight.
             double drive = -gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
-            leftPower = Range.clip(drive + turn, -1.0, 1.0);
-            rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            double turn1 = gamepad1.left_stick_x;
+            double turn2 = gamepad1.right_stick_x;
+            leftPower = 3*(drive + turn1);
+            rightPower = 3*(drive - turn1);
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
