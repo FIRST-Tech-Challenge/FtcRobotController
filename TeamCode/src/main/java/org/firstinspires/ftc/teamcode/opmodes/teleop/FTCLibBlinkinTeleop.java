9e6f41b932b2cd672ce11588f715291ae9564014
Alex <hbostic@gmail.com>
2021-12-07 13:40:29 -0500

    Example Teleop for FTCLib Object Model

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FTCLibBlinkinTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FTCLibBlinkinTeleop.java
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.commands.leds.blinkin.ShowAllianceColor;
+import org.firstinspires.ftc.teamcode.commands.leds.blinkin.ShowTeamColors;
+import org.firstinspires.ftc.teamcode.subsystems.leds.blinkin.LEDSubsystem;
 
 
 @TeleOp(name="Telop: FTCLib BlinkinExample", group="FTCLib")
-public class FTCLib_Blinkin_Teleop extends CommandOpMode {
+public class FTCLibBlinkinTeleop extends CommandOpMode {
+
+    private LEDSubsystem m_leds;
+    private ShowAllianceColor m_showAllianceColorCommand;
+    private ShowTeamColors m_showTeamColorsCommand;
 
     @Override
     public void initialize() {
+
+        m_leds = new LEDSubsystem(hardwareMap,"blinkin");
 
     }
 }
