dea75128ac58bfb99c614aeed5fd9d64364578ab
Alex <hbostic@gmail.com>
2021-12-08 01:13:28 -0500

    Creation of the Show Web Cam on FTC Dashboard command

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/webcam/StreamToDashboard.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/webcam/StreamToDashboard.java
@@ -1,86 +1,36 @@
 package org.firstinspires.ftc.teamcode.commands.webcam;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.arcrobotics.ftclib.command.CommandBase;
-import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.DetectShippingElement;
 import org.firstinspires.ftc.teamcode.cv.OpenCvShippingElementDetector;
-import org.firstinspires.ftc.teamcode.subsystems.leds.blinkin.LEDSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.webcam.WebCamSubsystem;
 
-public class DetectTSEPosition extends CommandBase {
+public class StreamToDashboard extends CommandBase {
 
     private final WebCamSubsystem m_webCamSubsytem;
-    private final OpenCvShippingElementDetector detector;
-    private Telemetry telemetry;
-    private Boolean gotPosition = false;
 
-    public DetectTSEPosition(WebCamSubsystem subsystem){
+    private final FtcDashboard dashboard;
+
+
+    public StreamToDashboard(WebCamSubsystem subsystem){
         m_webCamSubsytem = subsystem;
-        detector = (OpenCvShippingElementDetector) m_webCamSubsytem.getPipeline();
+        dashboard = null;
 
-        addRequirements(subsystem);
     }
 
-    public DetectTSEPosition(WebCamSubsystem subsystem, Telemetry telemetry){
+    public StreamToDashboard(WebCamSubsystem subsystem, FtcDashboard dashboard){
         m_webCamSubsytem = subsystem;
-        detector = (OpenCvShippingElementDetector) m_webCamSubsytem.getPipeline();
-        this.telemetry = telemetry;
+        this.dashboard = dashboard;
 
         addRequirements(subsystem);
     }
 
     @Override
     public void initialize(){
-        m_webCamSubsytem.openCameraDeviceAsync();
+        m_webCamSubsytem.StreamToDashboard(dashboard);
     }
 
-    @Override
-    public void execute(){
-        OpenCvShippingElementDetector.TSELocation location = detector.getLocation();
-
-        telemetry.addData("We have a", location);
-
-
-        if (location == OpenCvShippingElementDetector.TSELocation.P1_BLUE_LEFT) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-        else if (location == OpenCvShippingElementDetector.TSELocation.P1_BLUE_RIGHT) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-        else if (location == OpenCvShippingElementDetector.TSELocation.P1_BLUE_MIDDLE) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-        else if (location == OpenCvShippingElementDetector.TSELocation.P2_BLUE_RIGHT) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-        else if (location == OpenCvShippingElementDetector.TSELocation.P2_BLUE_LEFT) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-        else if (location == OpenCvShippingElementDetector.TSELocation.P2_BLUE_MIDDLE) {
-            // Do something with the plane
-            telemetry.addData("We have a 2", location);
-            gotPosition = true;
-        }
-
-
-        telemetry.update();
-    }
-
-    @Override
-    public boolean isFinished() {
-        return gotPosition;
-    }
 
 }
