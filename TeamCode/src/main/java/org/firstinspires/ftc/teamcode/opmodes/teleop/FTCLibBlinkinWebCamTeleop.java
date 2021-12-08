bdd571cbbb6de3c2b5f5376ef49709faa1c9bd9d
Alex <hbostic@gmail.com>
2021-12-08 01:15:16 -0500

    example teleop for cam and leds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FTCLibBlinkinWebCamTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/FTCLibBlinkinWebCamTeleop.java
@@ -1,28 +1,42 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.button.Button;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.DetectShippingElement;
 import org.firstinspires.ftc.teamcode.commands.leds.blinkin.ShowAllianceColor;
 import org.firstinspires.ftc.teamcode.commands.leds.blinkin.ShowTeamColors;
+import org.firstinspires.ftc.teamcode.commands.webcam.DetectTSEPosition;
+import org.firstinspires.ftc.teamcode.commands.webcam.StreamToDashboard;
+import org.firstinspires.ftc.teamcode.cv.OpenCvShippingElementDetector;
 import org.firstinspires.ftc.teamcode.subsystems.leds.blinkin.LEDSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.webcam.WebCamSubsystem;
 
 
-@TeleOp(name="Telop: FTCLib BlinkinExample", group="FTCLib")
-public class FTCLibBlinkinTeleop extends CommandOpMode {
+@TeleOp(name="Telop: FTCLib BlinkinWebCam Example", group="FTCLib")
+public class FTCLibBlinkinWebCamTeleop extends CommandOpMode {
 
     private LEDSubsystem m_leds;
+    private WebCamSubsystem m_webCam;
+
     private ShowAllianceColor m_showAllianceBlueColorCommand;
     private ShowAllianceColor m_showAllianceRedColorCommand;
     private ShowTeamColors m_showTeamColorsCommand;
 
+    private DetectTSEPosition m_detectTSEPosition;
+    private StreamToDashboard m_streamToDashboard;
+
     @Override
     public void initialize() {
+
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = dashboard.getTelemetry();
 
         //set up Game pad 1
         GamepadEx toolOp = new GamepadEx(gamepad1);
@@ -46,6 +60,18 @@ public class FTCLibBlinkinTeleop extends CommandOpMode {
         blueAlliance.whenPressed(m_showAllianceBlueColorCommand);
         redAlliance.whenPressed(m_showAllianceRedColorCommand);
         teamColors.whenPressed(m_showTeamColorsCommand);
+
+        //create webcam subsystem
+        m_webCam = new WebCamSubsystem(hardwareMap,"Webcam 1",new OpenCvShippingElementDetector(640,480,telemetry));
+
+        m_detectTSEPosition = new DetectTSEPosition(m_webCam, telemetry);
+
+        m_streamToDashboard = new StreamToDashboard(m_webCam,dashboard);
+
+        m_streamToDashboard.schedule();
+        m_detectTSEPosition.schedule();
+
+        CommandScheduler.getInstance().run();
 
 
     }
