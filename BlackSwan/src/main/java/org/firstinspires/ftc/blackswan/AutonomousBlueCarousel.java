2c36fa0a7fd88b41a0224ec5820748afe2e36a88
DraskiTheDragon <deanbras@gmail.com>
2021-12-09 19:07:33 -0700

    BlackSwanAutonomous

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarousel.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarousel.java
@@ -3,41 +3,42 @@ package org.firstinspires.ftc.blackswan;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.blackswan.Robot;
 @Autonomous
-public class AutonomousBlueCarousel  extends LinearOpMode {
+
+public class AutonomousBlueCarousel extends LinearOpMode {
+    Robot robot;
     @Override
     public void runOpMode() throws InterruptedException {
-        Robot robot;
-        robot = new Robot(hardwareMap, telemetry, this);
+        robot = new Robot(hardwareMap,telemetry,this);
         waitForStart();
-        robot.right(.5, .5);
-        robot.turnRight(45, .5);
-        robot.right(.5, .5);
-        robot.back(.47, .5);
-        robot.redCarousel(3000);
-        robot.forward(.47, .5);
-        robot.left(.5, .5);
-        robot.turnRight(25, .5);
+        robot.right(.5,.5);
+        robot.turnRight(45,.5);
+        robot.right(.5,.5);
+        robot.back(.47,.5);
+        robot.blueCarousel(3000);
+        robot.forward(.47,.5);
+        robot.left(.5,.5);
+        robot.turnRight(25,.5);
         robot.pause(10);
-        robot.forward(.5, .5);
+        robot.forward(.5,.5);
         robot.pause(10);
-        robot.left(.5, .5);
+        robot.left(.5,.5);
         robot.pause(10);
-        robot.forward(.35, .5);
+        robot.forward(.35,.5);
         robot.pause(10);
-        robot.turnRight(.15, .5);
+        robot.turnRight(.15,.5);
         robot.pause(100); //replace with color sensor check
-        robot.left(.75, .5);
+        robot.left(.75,.5);
         robot.pause(100); //replace with color sensor check
-        robot.back(.5, .5);
-        robot.turnLeft(45, .5);
-        robot.forward(.75, .5);
+        robot.left(1.75,.5);
+        robot.forward(.75,.5);
         robot.armThing(3);
-        robot.back(.75, .5);
-        robot.turnRight(45, .5);
-        robot.left(2.5, .75);
-        robot.forward(1.5, .5);
+        robot.back(.75,.5);
+        robot.right(3.75,.75);
+        robot.forward(1,.5);
+
+
+
 
     }
 }
