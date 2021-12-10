a738194f8f83bdebf3de19ad2c41d86d8ba2f1db
Karine Josien <kjosien1@gmail.com>
2021-12-10 14:42:55 -0700

    autonomous carousel

--- Masters/src/main/java/org/firstinspires/ftc/masters/JevilsRedCarousel.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/JevilsRedCarousel.java
@@ -31,42 +31,38 @@ public class JevilsRedCarousel extends LinearOpMode {
         if (freightLocation == EasyOpenCVIdentifyShippingElement.SkystoneDeterminationPipeline.FreightPosition.MIDDLE) {
 
         }
-        robot.forward(0.6, 1.5);
+        robot.forward(0.6, 1.6);
         robot.turnToHeadingSloppy(.6,-45,-20);
         robot.forward(.3,.8);
-<<<<<<< HEAD
 
-        //deposit shipping element
+//        if (freightLocation == EasyOpenCVIdentifyShippingElement.SkystoneDeterminationPipeline.FreightPosition.LEFT) {
+//            robot.dumpFreightBottom();
+//        } else if (freightLocation == EasyOpenCVIdentifyShippingElement.SkystoneDeterminationPipeline.FreightPosition.MIDDLE) {
+//            robot.dumpFreightMiddle();
+//        } else if (freightLocation == EasyOpenCVIdentifyShippingElement.SkystoneDeterminationPipeline.FreightPosition.RIGHT) {
+//            robot.dumpFreightTop();
+//        }
 
-
-//        robot.backwards(0.3,0.2);
-//        robot.strafeLeft(0.3,0.2);
-//        robot.turnToHeading(0.3,-125, 3);
-//        robot.forward(0.3,0.2);
-=======
-        robot.pauseButInSecondsForThePlebeians(.001);
-
-        //deposit shipping element
-        /*
-        robot.backwards(0.3,0.2);
-        robot.strafeLeft(0.3,0.2);
-        robot.turnToHeading(0.3,-125, 3);
-        robot.forward(0.3,0.2);
->>>>>>> origin/master
         robot.forward(.3,-2.6);
+        robot.strafeLeft(.4,.4);
+        robot.forward(.3,-.5);
+        robot.pivotLeftSloppy(.4,10);
+        robot.forward(.3, 3);
+        robot.turnToHeadingSloppy(.3,-90,3);
+        robot.parkRed();
+
+
 
         //Jevil turns carousel
         robot.jevilTurnCarousel(.5,2);
+//
+//        robot.forward(0.2,.4);
+//
+//        robot.turnToHeadingSloppy(.3,-90,-75);
+//        robot.strafeRight(.3,.8);
+//        robot.forward(.3,-1);
+//        robot.parkRed();
 
-        robot.turnToHeadingSloppy(.3,-90,-75);
-        robot.strafeRight(.3,.8);
-        robot.forward(.3,-0.8);
-
-        robot.turnToHeading(0.3, -90, 3);
-        robot.strafeRight(0.3,0.2);
-        robot.forward(0.3,0.2);
-        robot.parkRed();
-        */
     }
 }
 
