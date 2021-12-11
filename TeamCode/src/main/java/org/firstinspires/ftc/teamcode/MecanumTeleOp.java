468f57a34345fa14666cb318ec39522b90b4bfca
Abigail Booth <chickensalt1238@gmail.com>
2021-12-11 13:56:03 -0500

    Telop cleanup, added comments and grouped code for easier reading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -16,7 +16,7 @@ public class MecanumTeleOp extends LinearOpMode {
         //import the hardware map
         robot.init(hardwareMap, telemetry);
 
-        //set the slowdown
+        //set the drivetrain slowdown
         double slowdown = 1.0;
 
         //set the controller stick values
@@ -30,7 +30,6 @@ public class MecanumTeleOp extends LinearOpMode {
         int armSetPos = 0;
         int armMaxPos = 1200;
 
-
         //set arm levels
         int armLevel0 = 0;   // (a)
         int armLevel1 = 250; // (x)
@@ -43,12 +42,8 @@ public class MecanumTeleOp extends LinearOpMode {
         //carousel power
         double carouselPower = 0;
 
-
         //arm limit switch toggle to reset to 0
         boolean armLimitSwitchFlag = true;
-
-        //arm state
-        String armState = "down";
 
         //booleans for slow mode
         boolean bpressed = false;
@@ -59,7 +54,7 @@ public class MecanumTeleOp extends LinearOpMode {
          while (! isStarted()) {
              telemetry.addData("Arm Encoder", robot.motorArm.getCurrentPosition());
              telemetry.addData("Say GEX GEX GEX", "Hello Driver");
-             telemetry.addData("arm limit switch", robot.armLimitSwitch.isPressed());
+             telemetry.addData("Arm Limit Switch", robot.armLimitSwitch.isPressed());
              telemetry.update();
          }
 
@@ -68,64 +63,29 @@ public class MecanumTeleOp extends LinearOpMode {
 
         if (isStopRequested()) return;
 
+        // Set the Arm position and motor mode once before the while loop
         robot.motorArm.setTargetPosition(0);
         robot.motorArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         while (opModeIsActive()) {
-            //set joystick values
-            controller1lstickx = gamepad1.left_stick_x;
-            controller1lsticky = gamepad1.left_stick_y;
-            controller1rstickx = gamepad1.right_stick_x;
-            if(controller1lstickx < 0.3 && controller1lstickx > -0.3)
-                controller1lstickx = 0.0;
-            if(controller1lsticky < 0.3 && controller1lsticky > -0.3)
-                controller1lsticky = 0.0;
-            if(controller1rstickx < 0.3 && controller1rstickx > -0.3)
-                controller1rstickx = 0.0;
 
-            //set slowdown
-            if(gamepad1.b == true && bpressed == false){
-                if(slowdownflag){
-                    slowdown = 1;
-                    slowdownflag = false;
-                }
-                else if(slowdownflag == false){
-                    slowdown = 0.5;
-                    slowdownflag = true;
-                }
-            }
-            bpressed = gamepad1.b;
-            //Add slowdown
-            controller1lstickx = controller1lstickx * slowdown;
-            controller1lsticky = controller1lsticky * slowdown;
-            controller1rstickx = controller1rstickx * slowdown;
-
-            // Track Arm Current Pos
+            /* Arm, Gamepad 2
+             * Button A - Level 0, floor
+             * Button B - Level 1, bottom level
+             * Button C - Level 2, middle level
+             * Button D - Level 3, top level
+             * Rightstick Y - nudge up and down in small incremenets
+             */
             armCurrentPos = robot.motorArm.getCurrentPosition();
 
-            /* limit switch current state
-            if(robot.armLimitSwitch.isPressed()){
-                armState = "down";
-            }else{
-                armState = "up";
-            }
-            */
-
-
             if (robot.armLimitSwitch.isPressed() && armLimitSwitchFlag == false){
-
+                // Reset to 0 if the magnetic limit switch is pressed and our flag is set to false
                 robot.motorArm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                 robot.motorArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);  
                 armLimitSwitchFlag = true;
-
-                telemetry.addData("limitSwitch", robot.armLimitSwitch.isPressed());
-                telemetry.addData("limitSwitchFlag", armLimitSwitchFlag);
-
             }else {
-
-                // Arm Up and Arm Down in small increments
-                if (gamepad2.right_stick_y != 0.25) {
-                    //armSetPos += gamepad2.right_stick_y*10;
+                // Arm Up and Arm Down in small increments, >= 0.25 helps prevent issues with 0 value
+                if (gamepad2.right_stick_y >= 0.25) {
                     if (gamepad2.right_stick_y == -1 ){
                         armSetPos = armSetPos + 5;
                     } else if (gamepad2.right_stick_y == 1 ) {
@@ -142,17 +102,20 @@ public class MecanumTeleOp extends LinearOpMode {
                 }else if (gamepad2.b) {
                     armSetPos = armLevel3;
                 }
-                
+                // when arm is up set the limit switch flag to false to allow reset on next limit switch press
                 if( armCurrentPos > 20 && !robot.armLimitSwitch.isPressed()) {
-                    armState = "up";
                     armLimitSwitchFlag = false;
                 }
-                
-                telemetry.addData("limitSwitchFlag", armLimitSwitchFlag);
-                telemetry.addData("limitSwitch", robot.armLimitSwitch.isPressed());
             }
+            //set arm power
+            robot.motorArm.setTargetPosition(armSetPos);
+            robot.motorArm.setPower(0.5);
 
-            //intake
+
+            /* Intake, Gamepad 2
+             * Right trigger in at faster speed
+             * Left Trigger out at a slower speed
+             */
             if (gamepad2.right_trigger > 0.5){
                 intakePower = -0.75;
             }
@@ -165,7 +128,11 @@ public class MecanumTeleOp extends LinearOpMode {
             //set power intake
             robot.motorIntake.setPower(intakePower);
 
-            //carousel code
+
+            /* Carousel, Gamepad 2
+             * Right bumper
+             * Left Bumper
+             */
             if (gamepad2.right_bumper == true && !gamepad2.left_bumper) {
                 carouselPower = 0.5;
             }
@@ -175,11 +142,45 @@ public class MecanumTeleOp extends LinearOpMode {
             else{
                 carouselPower = 0;
             }
-
             //set power carousel
             robot.motorCarousel.setPower(carouselPower);
 
-            //drivetrain mecanum
+
+            /* Drivetrain Mecanum, Gamepad 1
+             * Leftstick controls forward/back and strafing
+             * Rightstick controls rotation
+             * B button press toggles slowdown, cuts speed in half
+             * Custom deadzone created to account for joystick drift
+             */
+            // Deadzone to correct drift
+            controller1lstickx = gamepad1.left_stick_x;
+            controller1lsticky = gamepad1.left_stick_y;
+            controller1rstickx = gamepad1.right_stick_x;
+            if(controller1lstickx < 0.3 && controller1lstickx > -0.3)
+                controller1lstickx = 0.0;
+            if(controller1lsticky < 0.3 && controller1lsticky > -0.3)
+                controller1lsticky = 0.0;
+            if(controller1rstickx < 0.3 && controller1rstickx > -0.3)
+                controller1rstickx = 0.0;
+
+            // set slowdown
+            if(gamepad1.b == true && bpressed == false){
+                if(slowdownflag){
+                    slowdown = 1;
+                    slowdownflag = false;
+                }
+                else if(slowdownflag == false){
+                    slowdown = 0.5;
+                    slowdownflag = true;
+                }
+            }
+            bpressed = gamepad1.b;
+
+            //Add slowdown
+            controller1lstickx = controller1lstickx * slowdown;
+            controller1lsticky = controller1lsticky * slowdown;
+            controller1rstickx = controller1rstickx * slowdown;
+
             double y = controller1lsticky;
             double x = -controller1lstickx * 1.1; // * 1.1 Counteract imperfect strafing
             double rx = -controller1rstickx;
@@ -197,18 +198,15 @@ public class MecanumTeleOp extends LinearOpMode {
             robot.motorFrontRight.setPower(frontRightPower);
             robot.motorBackRight.setPower(backRightPower);
 
-            //set arm power
-            robot.motorArm.setTargetPosition(armSetPos);
-            robot.motorArm.setPower(0.5);
-
             telemetry.addData("Left Stick X", controller1lstickx);
             telemetry.addData("Left Stick Y", controller1lsticky);
             telemetry.addData("Left Stick X input", gamepad1.left_stick_x);
             telemetry.addData("Left Stick Y input", gamepad1.left_stick_y);
             telemetry.addData("Left Stick Y", controller1lsticky);
-            telemetry.addData("Arm pos", armCurrentPos);
+            telemetry.addData("Arm Current pos", armCurrentPos);
             telemetry.addData("Arm Set pos", armSetPos);
-            telemetry.addData("Arm State", armState);
+            telemetry.addData("Arm LimitSwitch", robot.armLimitSwitch.isPressed());
+            telemetry.addData("Arm LimitSwitchFlag", armLimitSwitchFlag);
             telemetry.addData("Right Y Stick",gamepad1.right_stick_y);
             telemetry.addData("Left Y Stick",gamepad1.left_stick_y);
             telemetry.addData("Slowdown:",slowdownflag);
