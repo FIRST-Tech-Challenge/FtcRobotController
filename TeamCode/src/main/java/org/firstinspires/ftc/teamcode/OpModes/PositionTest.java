4865c7fabab414f45475a8a3ff6e14de38bb4a8f
NeeravS <neerav.fsaps@gmail.com>
2021-12-11 00:05:26 -0500

    Disabled new opmodes and changed some values for lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PositionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PositionTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.OpModes;
 
 import android.graphics.Point;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -13,7 +14,7 @@ import org.firstinspires.ftc.teamcode.bots.RobotDirection;
 import org.firstinspires.ftc.teamcode.odometry.IBaseOdometry;
 import org.firstinspires.ftc.teamcode.odometry.VSlamOdometry;
 
-//@Disabled
+@Disabled
 @TeleOp(name = "Color Test", group = "Robot15173")
 public class PositionTest extends LinearOpMode {
 
