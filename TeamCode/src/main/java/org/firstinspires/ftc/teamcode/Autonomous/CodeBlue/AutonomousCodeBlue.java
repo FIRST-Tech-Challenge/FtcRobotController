a05152c06a97735c922bc5586aa1932eb0dc4cc2
devanchoksi <devan.choksi@students.tka.org>
2021-12-10 17:10:15 -0800

    added servo code for intake code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CodeBlue/AutonomousCodeBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CodeBlue/AutonomousCodeBlue.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode.Autonomous.CodeBlue;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Carousel.Carousel;
 import org.firstinspires.ftc.teamcode.Intake.Intake;
+import org.firstinspires.ftc.teamcode.Outtake.Outtake;
 
 public class AutonomousCodeBlue extends LinearOpMode {
 
@@ -10,9 +12,24 @@ public class AutonomousCodeBlue extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         Intake AutonomousIntake = new Intake(hardwareMap);
+        Outtake AutonomousOuttake = new Outtake(hardwareMap);
+        Carousel AutonomousCarousel = new Carousel(hardwareMap);
 
 
 
+        waitForStart();
+
+        // TODO, step 1
+
+        //Step 2: DONE
+        AutonomousOuttake.FlapOpen();
+
+        //TODO, step 3
+
+        //Step 4: DONE
+        AutonomousCarousel.CarouselHandler();
+
+        //TODO, step 5/6
 
     }
 }
