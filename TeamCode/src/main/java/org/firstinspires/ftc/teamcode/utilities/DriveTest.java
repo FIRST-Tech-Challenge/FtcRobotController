eca31c41f09e4ebabe6ae2631ad5471c7e69fbd5
DESKTOP-AUURQ7F\BC4HSTEM <cms0679@outlook.com>
2021-12-11 08:15:37 -0800

    Code for testing individual wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilities/DriveTest.java
@@ -16,35 +16,71 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name="Max Velocity Test", group="Utilities")
-public class MaxVelocityTest extends LinearOpMode {
-    DcMotorEx motor;
-    double currentVelocity;
-    double maxVelocity = 0.0;
+import org.firstinspires.ftc.teamcode.FrenzyHardwareMap;
+
+@TeleOp(name="Drive Test", group="Utilities")
+public class DriveTest extends LinearOpMode {
+    FrenzyHardwareMap robot = new FrenzyHardwareMap();
 
     @Override
     public void runOpMode() {
-        motor = hardwareMap.get(DcMotorEx.class, "arm");
-        waitForStart();
+        robot.init(hardwareMap, telemetry);
 
-        motor.setPower(1.0);
-
-        while (opModeIsActive()) {
-            currentVelocity = motor.getVelocity();
-
-            if (currentVelocity > maxVelocity) {
-                maxVelocity = currentVelocity;
-            }
-
-            //intake in
-            // motor.setPower(gamepad2.left_trigger * 0.75);
-
-            //intake out
-            // motor.setPower(-gamepad2.right_trigger * 0.3);
-
-            telemetry.addData("current velocity", currentVelocity);
-            telemetry.addData("maximum velocity", maxVelocity);
+        //init loop
+        while (! isStarted()) {
+            telemetry.addData("Drive Test", "This test is best run on top of a box to test each wheel");
             telemetry.update();
         }
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+        //Code to test if the motors are functioning as intended. Leave commented out if not testing.
+
+
+       robot.motorFrontLeft.setPower(0.5);
+       telemetry.addData("motor", "front left");
+       telemetry.addData("encoder", robot.motorFrontLeft.getCurrentPosition());
+       telemetry.update();
+       sleep(5000);
+       robot.motorFrontLeft.setPower(0);
+
+       robot.motorFrontRight.setPower(0.5);
+       telemetry.addData("motor", "front right");
+       telemetry.addData("encoder", robot.motorFrontRight.getCurrentPosition());
+       telemetry.update();
+       sleep(5000);
+        robot.motorFrontRight.setPower(0);
+
+        robot.motorBackRight.setPower(0.5);
+        telemetry.addData("motor","back right");
+        telemetry.addData("encoder", robot.motorBackRight.getCurrentPosition());
+        telemetry.update();
+        sleep(5000);
+        robot.motorBackRight.setPower(0);
+
+        robot.motorBackLeft.setPower(0.5);
+        telemetry.addData("motor","back left");
+        telemetry.addData("encoder", robot.motorBackLeft.getCurrentPosition());
+        telemetry.update();
+        sleep(5000);
+        robot.motorBackLeft.setPower(0);
+
+        robot.motorFrontLeft.setPower(0.5);
+        robot.motorFrontRight.setPower(0.5);
+        telemetry.addData("motor","front Right and Left");
+        telemetry.update();
+        sleep(5000);
+        robot.motorFrontLeft.setPower(0);
+        robot.motorFrontRight.setPower(0);
+
+        robot.motorBackLeft.setPower(0.5);
+        robot.motorBackRight.setPower(0.5);
+        telemetry.addData("motor","back Right and Left");
+        telemetry.update();
+        sleep(5000);
+        robot.motorBackLeft.setPower(0);
+        robot.motorBackRight.setPower(0);
+
     }
 }
