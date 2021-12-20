e1f80d0658487f4e6235b248ca3519499482adad
Alex <hbostic@gmail.com>
2021-12-19 22:57:23 -0500

    Added PID value images for safe keeping and started a sample drive path using meep meep

---
+++ MeepMeepTesting/src/main/java/com/bc4h14169/meepmeeptesting/Sample1.java
@@ -0,0 +1,28 @@
+package com.bc4h14169.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class Sample1 {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(41.065033847087705, 41.065033847087705, Math.toRadians(180), Math.toRadians(180), 13.2435)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(-37, -72, Math.toRadians(90)))
+                                .splineTo(new Vector2d(-15,-40),Math.toRadians(90))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
