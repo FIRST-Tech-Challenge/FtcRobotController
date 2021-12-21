923271e00cad3e58d967059dc98328b586d3868d
NihalMahalingam <76792828+NihalMahalingam@users.noreply.github.com>
2021-12-20 19:47:32 -0600

    work on auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/AutoStorage.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.roadrunner.drive.RoadRunnerHelper.inchesToCoordinate;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+public class AutoStorage extends LinearOpMode {
+    public int multiplier = 1;
+    public int directionAdder = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(this.hardwareMap);
+
+        TrajectorySequenceBuilder builder = drive.trajectorySequenceBuilder(new Pose2d(-40, multiplier * 70 - inchesToCoordinate(9D),
+                Math.toRadians(90 + directionAdder)));
+
+        builder.waitSeconds(0.1);
+        // 9.35 seconds long
+        builder.lineTo(new Vector2d(-40, 55));
+        builder.splineToLinearHeading(new Pose2d(-20, 40, Math.toRadians(-110)),
+                Math.toRadians(-110));
+        builder.addDisplacementMarker(() -> {
+            // TODO LIFT UP
+        });
+        builder.waitSeconds(2);
+        builder.addDisplacementMarker(() -> {
+            // TODO LIFT DOWN
+        });
+        builder.lineTo(new Vector2d(-19, 45));
+        builder.lineToLinearHeading(new Pose2d(-59, 57.5, Math.toRadians(240)));
+        builder.addDisplacementMarker(() -> {
+            // TODO START CAROSUEL
+        });
+        builder.waitSeconds(3);
+        builder.addDisplacementMarker(() -> {
+            // TODO STOP CAROSUEL
+        });
+        builder.lineToLinearHeading(new Pose2d(-60, 35, Math.toRadians(90)));
+        builder.waitSeconds(42);
+        builder.addDisplacementMarker(() -> {
+            // TODO FIRE TAPE MEASURE
+        });
+        builder.waitSeconds(3);
+    }
+}
