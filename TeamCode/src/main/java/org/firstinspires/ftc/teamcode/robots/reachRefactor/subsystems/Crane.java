32b1261ab5fb401e2e5913e9bca4e3233916f4c1
Cooper Clem <ironreignrobotics@gmail.com>
2021-12-27 00:33:46 -0600

    Use target angles for crane joint control
    Make crane joints angular relative to a vertical home / neutral position
    
    Negative angles are counter clockwise when looking at the left side of the robot and the turret is neutral
    
    Untested and untuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/reachRefactor/subsystems/Crane.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/reachRefactor/subsystems/Crane.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.robots.reachRefactor.subsystems;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.ServoImplEx;
+import com.qualcomm.robotcore.util.Range;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,6 +33,23 @@ public class Crane implements Subsystem {
     public static int BUCKET_UP_POS = 900;
     public static int BUCKET_DOWN_POS = 1200;
 
+    public static int SHOULDER_HOME_PWM = 1500;
+    public static int ELBOW_HOME_PWM = 1500;
+    public static int WRIST_HOME_PWM = 1500;
+
+    public static double SHOULDER_PWM_PER_DEGREE = 750/90;
+    public static double ELBOW_PWM_PER_DEGREE = 750/90;
+    public static double WRIST_PWM_PER_DEGREE = 750/180;
+
+    public static double SHOULDER_DEG_MIN = -90; //negative angles are counter clockwise while looking at the left side of the robot
+    public static double ELBOW_DEG_MIN = -40;
+    public static double WRIST_DEG_MIN = -180;
+
+    public static double SHOULDER_DEG_MAX = 90;
+    public static double ELBOW_DEG_MAX = 140;
+    public static double WRIST_DEG_MAX = 180;
+
+
     public Crane(HardwareMap hardwareMap) {
         shoulderServo = hardwareMap.get(ServoImplEx.class, "firstLinkServo");
         elbowServo = hardwareMap.get(ServoImplEx.class, "secondLinkServo");
@@ -43,15 +61,15 @@ public class Crane implements Subsystem {
     }
 
     public enum Articulation {
-        INIT(2169, 1183, 1316, 0, 5),
+        INIT(0, 0, 0, 0, 5),
         MANUAL(0, 0, 0, 0, 0),
-        STARTING(2200,1600,1600,0, 5),
-        HOME(1700,1650,1600,0, 0),
-        LOWEST_TIER(0,0,0,0, 5),
-        MIDDLE_TIER(0,0,0,0, 5),
-        HIGH_TIER(1500, 1000,1650,0, 5),
-        CAP(1500, 900,1650,0, 5),
-        TRANSFER(1850,2000,1550,0, 5);
+        STARTING(-90,0,90,0, 5),
+        HOME(0,0,0,0, 0),
+        LOWEST_TIER(90,130,0,0, 5),
+        MIDDLE_TIER(70,130,0,0, 5),
+        HIGH_TIER(30, 130,0,0, 5),
+        CAP(30, 140,0,0, 5),
+        TRANSFER(-20,-40,-40,0, 5);
 
         public int shoulderPos, elbowPos, wristPos;
         public double turretAngle;
@@ -97,21 +115,37 @@ public class Crane implements Subsystem {
         return false;
     }
 
-    public boolean flipBucket(boolean down) {
-        wristTargetPos = down ? BUCKET_DOWN_POS : BUCKET_UP_POS;
-        return true;
-    }
-
     @Override
     public void update(){
         articulate(articulation);
 
-        shoulderServo.setPosition(servoNormalize(shoulderTargetPos));
-        elbowServo.setPosition(servoNormalize(elbowTargetPos));
-        wristServo.setPosition(servoNormalize(wristTargetPos));
+        shoulderServo.setPosition(ShoulderServoValue(shoulderTargetPos));
+        elbowServo.setPosition(ElbowServoValue(elbowTargetPos));
+        wristServo.setPosition(WristServoValue(wristTargetPos));
 
         turret.update();
     }
+
+    //take the supplied relative-to-home target value in degrees
+    //and convert to servo setting
+    private double ShoulderServoValue(double targetpos){
+        double newpos = Range.clip(targetpos,SHOULDER_DEG_MIN, SHOULDER_DEG_MAX);
+        newpos = newpos*SHOULDER_PWM_PER_DEGREE+SHOULDER_HOME_PWM;
+        return servoNormalize(newpos);
+    }
+
+    private double ElbowServoValue(double targetpos){
+        double newpos = Range.clip(targetpos,ELBOW_DEG_MIN, ELBOW_DEG_MAX);
+        newpos = newpos*ELBOW_PWM_PER_DEGREE+ELBOW_HOME_PWM;
+        return servoNormalize(newpos);
+    }
+
+    private double WristServoValue(double targetpos){
+        double newpos = Range.clip(targetpos,WRIST_DEG_MIN, WRIST_DEG_MAX);
+        newpos = newpos*WRIST_PWM_PER_DEGREE+WRIST_HOME_PWM;
+        return servoNormalize(newpos);
+    }
+
 
     @Override
     public void stop(){
