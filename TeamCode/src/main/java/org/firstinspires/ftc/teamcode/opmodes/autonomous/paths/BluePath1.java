282f22c745186e62ceaf961b50f413f8f5247aa9
DESKTOP-AUURQ7F\BC4HSTEM <cms0679@outlook.com>
2021-12-31 03:46:20 -0500

    Added Subsystems to BluePath for testing, intake is not stopping.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/paths/BluePath1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/paths/BluePath1.java
@@ -1,18 +1,26 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous.paths;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.commands.arm.SetArmLevel;
 import org.firstinspires.ftc.teamcode.commands.drive.roadrunner.TrajectoryFollowerCommand;
+import org.firstinspires.ftc.teamcode.commands.webcam.MockDetectTSEPosition;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.opmodes.createmechanism.CreateArm;
 import org.firstinspires.ftc.teamcode.opmodes.createmechanism.CreateCarousel;
+import org.firstinspires.ftc.teamcode.opmodes.createmechanism.CreateIntake;
+import org.firstinspires.ftc.teamcode.opmodes.createmechanism.CreateWebCam;
 import org.firstinspires.ftc.teamcode.subsystems.drive.roadrunner.MecanumDriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.webcam.WebCamSubsystem;
 
 public class BluePath1 {
 
@@ -24,7 +32,10 @@ public class BluePath1 {
     private TrajectoryFollowerCommand sample1Follower5;
     private TrajectoryFollowerCommand sample1Follower6;
 
-    SequentialCommandGroup carouselGroup;
+    private FtcDashboard dashboard;
+
+    private SequentialCommandGroup carouselGroupBlue1;
+    private SequentialCommandGroup intakeGroupBlue1;
 
     private Pose2d startPose;
     private final HardwareMap hwMap;
@@ -38,11 +49,38 @@ public class BluePath1 {
         drive.setPoseEstimate(startPose);
     }
 
+    public BluePath1(HardwareMap hwMap, FtcDashboard db, Telemetry telemetry){
+        this.hwMap = hwMap;
+        dashboard = db;
+        this.telemetry = telemetry;
+        drive = new MecanumDriveSubsystem(new SampleMecanumDrive(hwMap), false);
+        startPose = new Pose2d(-36, 60, Math.toRadians(270));
+        drive.setPoseEstimate(startPose);
+    }
+
     public void createPath(){
         CreateCarousel createCarousel = new CreateCarousel(hwMap,"carousel",telemetry);
+        CreateWebCam createWebCam = new CreateWebCam(hwMap, "Webcam 1", dashboard, telemetry);
+        CreateArm createArm = new CreateArm(hwMap, "arm", telemetry);
+
+        createArm.createAuto();
+
+        createWebCam.createAuto();
+        WebCamSubsystem webCamSubsystem = createWebCam.getWebCamSubsystem();
+
+        MockDetectTSEPosition mockDetectTSEPosition = createWebCam.getMockDetectTSEPositionCommand();
+        mockDetectTSEPosition.schedule();
+
         createCarousel.createAuto();
-        carouselGroup = new SequentialCommandGroup(createCarousel.getMoveCarouselToPosition(),
+        carouselGroupBlue1 = new SequentialCommandGroup(createCarousel.getMoveCarouselToPosition(),
                 new WaitUntilCommand(createCarousel.hasMaxEncoderCountSupplier()).andThen(createCarousel.getStopCarousel()));
+
+        CreateIntake createIntake = new CreateIntake(hwMap, "intake", telemetry);
+        createIntake.createAuto();
+
+        /* This triggers the command group error */
+        intakeGroupBlue1 = new SequentialCommandGroup(createIntake.getSeReleaser(), new WaitCommand(2000).andThen(createIntake.getStopIntake()));
+
 
         Trajectory traj1 = drive.trajectoryBuilder(startPose)
                 //.strafeTo(new Vector2d(-60, 60))
@@ -57,6 +95,8 @@ public class BluePath1 {
                 .strafeTo(new Vector2d(-60, 22))
                 .addDisplacementMarker(()->{
                     telemetry.addData("Path 2", "performing path 2 action");
+                    SetArmLevel setArmLevel = createArm.createSetArmLevel(webCamSubsystem.getLevel());
+                    setArmLevel.schedule();
                 }) //step 6
                 .build();
 
@@ -106,8 +146,9 @@ public class BluePath1 {
 
     public void execute(CommandOpMode commandOpMode){
         commandOpMode.schedule(new WaitUntilCommand(commandOpMode::isStarted).andThen(
-                carouselGroup
-                //sample1Follower1.andThen(carouselGroup,sample1Follower2, sample1Follower3, sample1Follower4)
+                intakeGroupBlue1
+                // carouselGroupBlue1
+                // sample1Follower1.andThen(carouselGroup,sample1Follower2,intakeGroup, sample1Follower3, sample1Follower4)
         ));
     }
 }
