17f02ea05f0200588576a6e4b3a4dd4a8097f7c4
Ethan Sanders <ethanmrcod@gmail.com>
2022-01-03 19:19:42 -0600

    grabber

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/headless/AutoGrabber.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.core.robot.tools.headless;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import androidx.annotation.NonNull;
+
+public class AutoGrabber {
+    private final Servo leftServo;
+    private final Servo rightServo;
+    private boolean open = false;
+    public AutoGrabber (@NonNull HardwareMap hardwareMap) {
+        leftServo = hardwareMap.get(Servo.class, "leftTse");
+        leftServo.resetDeviceConfigurationForOpMode();
+        rightServo = hardwareMap.get(Servo.class, "rightTse");
+        rightServo.resetDeviceConfigurationForOpMode();
+    }
+
+    public void close() {
+        leftServo.setPosition(0.42);
+        rightServo.setPosition(0.59);
+        if (open) {
+            open = false;
+        }
+    }
+    public void open() {
+        leftServo.setPosition(0);
+        rightServo.setPosition(1);
+        if (!open) {
+            open = true;
+        }
+    }
+    public void toggle() {
+        if (open) {
+            close();
+        } else {
+            open();
+        }
+    }
+}
