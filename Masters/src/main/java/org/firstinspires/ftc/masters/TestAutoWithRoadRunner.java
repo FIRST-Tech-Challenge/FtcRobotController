f59b47321ebd69c0fedc8c57324854d571229fab
Karine Josien <kjosien1@gmail.com>
2022-01-05 19:08:11 -0700

    masters auto

--- Masters/src/main/java/org/firstinspires/ftc/masters/TestAutoWithRoadRunner.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/TestAutoWithRoadRunner.java
@@ -12,14 +12,15 @@ import org.firstinspires.ftc.masters.trajectorySequence.TrajectorySequence;
 
 @Autonomous(name="test auto with roadRunner")
 public class TestAutoWithRoadRunner extends LinearOpMode {
-
+    RobotClass robot;
 
     @Override
     public void runOpMode() throws InterruptedException {
+      //  robot = new RobotClass(hardwareMap,telemetry,this);
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        Pose2d startPose = new Pose2d();
+        Pose2d startPose = new Pose2d(new Vector2d(-36, -63),Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
 
@@ -28,16 +29,43 @@ public class TestAutoWithRoadRunner extends LinearOpMode {
         if (isStopRequested()) return;
 
 
-            TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
-                   // .lineToConstantHeading(new Vector2d(24, 12), SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-.lineToLinearHeading(new Pose2d(new Vector2d(24, 12), Math.toRadians(45)))
-                    //.splineTo(new Vector2d(12, 12),0)
-                    //.splineTo(new Vector2d(12, 0), 0)
-                    .build();
-            drive.followTrajectorySequence(trajSeq);
+        TrajectorySequence trajSeq = drive.trajectorySequenceBuilder(startPose)
+             .lineToLinearHeading(new Pose2d(new Vector2d(-34, -39), Math.toRadians(45)))
+                .build();
+        drive.followTrajectorySequence(trajSeq);
 
-            drive.getLocalizer().getPoseEstimate();
+        drive.distanceSensorStuff();
+       // robot.distanceSensorStuff();
 
+             Pose2d position= drive.getLocalizer().getPoseEstimate();
+        TrajectorySequence trajSeq2 = drive.trajectorySequenceBuilder(position)
+                .lineToLinearHeading(new Pose2d(new Vector2d(-58.25, -58.25), Math.toRadians(90)))
+                .build();
+        drive.followTrajectorySequence(trajSeq2);
+
+        drive.jevilTurnCarousel(.5,5);
+       // robot.jevilTurnCarousel(.5,5);
+
+        Pose2d position2= drive.getLocalizer().getPoseEstimate();
+        TrajectorySequence trajSeq3 = drive.trajectorySequenceBuilder(position2)
+                .lineToLinearHeading(new Pose2d(new Vector2d(-52, -62), Math.toRadians(90)))
+                .build();
+        drive.followTrajectorySequence(trajSeq3);
+
+        Pose2d position3= drive.getLocalizer().getPoseEstimate();
+        TrajectorySequence trajSeq4 = drive.trajectorySequenceBuilder(position3)
+                .lineToLinearHeading(new Pose2d(new Vector2d(-34, -39), Math.toRadians(45)))
+                .build();
+        drive.followTrajectorySequence(trajSeq4);
+
+        drive.distanceSensorStuff(); 
+      //  robot.distanceSensorStuff();
+
+        Pose2d position4= drive.getLocalizer().getPoseEstimate();
+        TrajectorySequence trajSeq5 = drive.trajectorySequenceBuilder(position4)
+                .lineToLinearHeading(new Pose2d(new Vector2d(-61, -34), Math.toRadians(0)))
+                .build();
+        drive.followTrajectorySequence(trajSeq5);
     }
 
 
