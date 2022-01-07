e334800c5b2d5ffde755f7df942ec5fc01628b2c
Ethan Sanders <ethanmrcod@gmail.com>
2022-01-06 19:42:17 -0600

    lift!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/headless/AutoLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/headless/AutoLift.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.core.robot.tools.headless;
 
+import android.util.Pair;
+
 import com.arcrobotics.ftclib.gamepad.ButtonReader;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,6 +12,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.core.robot.tools.driveop.ControllerLift;
 import org.firstinspires.ftc.teamcode.core.thread.EventThread;
+import org.firstinspires.ftc.teamcode.core.thread.types.api.RunListenerIndefinitelyEvent;
 import org.firstinspires.ftc.teamcode.core.thread.types.impl.TimedEvent;
 
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -25,25 +28,38 @@ public class AutoLift {
     // MIDDLE motor 1850 arm 0.3 - 5 1/4 from back to rim
     // BOTTOM motor 1375 arm 0.25 - 5 1/4 from back to rim
 
-    protected enum Positions {
-        INTAKING,
-        SAFE,
-        TOP,
-        MIDDLE,
-        BOTTOM
+    @SuppressWarnings("unused")
+    public enum Positions {
+        INTAKING(0, 0.7D),
+        SAFE(1375, 0.7D),
+        TOP(2880, 0.3D),
+        MIDDLE(1850, 0.3D),
+        BOTTOM(1375, 0.25D);
+
+        public final int motorPos;
+        public final double armPos;
+
+        Positions(int motorPos, double armPos) {
+            this.motorPos = motorPos;
+            this.armPos = armPos;
+        }
+    }
+
+    private enum MovementStates { // switch this to a bool if you have time
+        NO_MOVEMENT,
+        MOTOR_MOVEMENT
     }
 
     private final DcMotor liftMotor;
     private final Servo armServo;
     // private final DigitalChannel bottomSensor;
-    private final DigitalChannel topSensor;
-    private double curPos = 0;
-    private boolean running = false; // currently moving down
-    private boolean first = true; // first time its reached bottom
     private final AtomicBoolean dumping = new AtomicBoolean(false);
     private final AtomicBoolean dumpingEventRunning = new AtomicBoolean(false);
     private final EventThread eventThread;
     private TimedEvent event;
+    private Positions position = Positions.INTAKING;
+    private Positions lastPosition = Positions.INTAKING;
+    private MovementStates state = MovementStates.NO_MOVEMENT;
     // fix this later
     /**
      * @param eventThread local eventThread instance
@@ -57,11 +73,32 @@ public class AutoLift {
         try { Thread.sleep(100); } catch (InterruptedException ignored) {}
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         armServo = map.get(Servo.class,"armServo");
-        topSensor = map.get(DigitalChannel.class,"topSensor");
-        topSensor.setMode(DigitalChannel.Mode.INPUT);
         this.eventThread = eventThread;
-
-
     }
-    // TODO make lift aaaaAAAAAAAAAA
+
+    public void goTo(@NonNull Positions positions) {
+        this.position = Positions.INTAKING;
+    }
+
+    public void update() {
+        if (position != lastPosition) state = MovementStates.NO_MOVEMENT;
+        switch (state) {
+            case NO_MOVEMENT:
+                if (position != lastPosition) {
+                    armServo.setPosition(position.armPos);
+                    Positions ogposition = position;
+                    eventThread.addEvent(new TimedEvent(() -> {
+                        if (ogposition == position) {
+                            state = MovementStates.MOTOR_MOVEMENT;
+                        }
+                    }, 800));
+                } else if (position == Positions.INTAKING) {
+                    armServo.setPosition(0.76D);
+                }
+                break;
+            case MOTOR_MOVEMENT:
+                liftMotor.setTargetPosition(position.motorPos);
+        }
+        lastPosition = position;
+    }
 }
