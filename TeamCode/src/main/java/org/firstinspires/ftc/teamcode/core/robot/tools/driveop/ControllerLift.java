327812ac26711fb1c273d419adda1d12427af0e0
Ethan Sanders <ethanmrcod@gmail.com>
2022-01-09 17:18:00 -0600

    controller lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/driveop/ControllerLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/robot/tools/driveop/ControllerLift.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.core.robot.tools.driveop;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.core.robot.tools.headless.AutoLift;
@@ -18,6 +20,8 @@ public class ControllerLift extends AutoLift {
     private final TriggerReader leftReader;
     private final TriggerReader rightReader;
     private final GamepadEx toolGamepad;
+    private final DigitalChannel topSensor;
+
     /**
      * @param eventThread local eventThread instance
      * @param map         local hardwareMap instance
@@ -27,6 +31,8 @@ public class ControllerLift extends AutoLift {
         this.toolGamepad = toolGamepad;
         leftReader = new TriggerReader(toolGamepad, GamepadKeys.Trigger.LEFT_TRIGGER);
         rightReader = new TriggerReader(toolGamepad, GamepadKeys.Trigger.RIGHT_TRIGGER);
+        topSensor = map.get(DigitalChannel.class,"topSensor");
+        topSensor.setMode(DigitalChannel.Mode.INPUT);
     }
 
     public void init() {
@@ -44,10 +50,17 @@ public class ControllerLift extends AutoLift {
     public void update() {
         super.update();
         if (position == Positions.SAFE && state == null) {
-            if (liftMotor.getCurrentPosition() >= 1375) { // PUT LIMTI SWITHC STUFF HERE
+            if (liftMotor.getMode() != DcMotor.RunMode.RUN_USING_ENCODER) {
+                liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+            if (liftMotor.getCurrentPosition() >= 1375 && !topSensor.getState()) { // PUT LIMTI SWITHC STUFF HERE
                 liftMotor.setPower(toolGamepad.getLeftY());
             } else {
                 liftMotor.setPower(0);
+            }
+        } else {
+            if (liftMotor.getMode() != DcMotor.RunMode.RUN_TO_POSITION) {
+                liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             }
         }
     }
