2567175009b4a0c36fe8772ee8ee0bb0c0c82e52
BlockBeasts <kjosien1@gmail.com>
2022-01-11 19:03:52 -0700

    led code

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/TeleopBlue.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/TeleopBlue.java
@@ -1,12 +1,24 @@
-package org.firstinspires.ftc.blackSwan;
+package org.firstinspires.ftc.blackswan;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.blackswan.TeleopRed;
 
 @TeleOp(name="TeleopBlue")
 
 public class TeleopBlue extends TeleopRed {
+
+    protected void turnDuck(DcMotor carousel){
+        if(gamepad2.right_bumper){
+            carousel.setPower(0.5 );
+        }else  if (gamepad2.left_bumper){
+            carousel.setPower(-0.5);
+        }
+        else {
+            carousel.setPower(0);
+        }
+    }
 
 }
