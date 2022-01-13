b614b19ccc7539d76b9f2cc25071ab0b53092440
Aadarsh Senthil <43424645+DarkXNinja@users.noreply.github.com>
2022-01-13 15:29:26 -0500

    Update FSMTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FSMTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FSMTeleOp.java
@@ -52,17 +52,106 @@ public class FSMTeleOp extends OpMode {
     
         LINEAR_SLIDE_START,
         LINEAR_SLIDE_EXTEND,
+        LINEAR_SLIDE_BUCKET,
+        LINEAR_SLIDE_RELEASE,
         LINEAR_SLIDE_RETRACT
+    
+    
+    }
+    
+    LinearSlideState linearSlideState = LinearSlideState.LINEAR_SLIDE_START;
+    
+    public void init() {
+    
+        frontLeft = hardwareMap.get(DcMotor.class,"FrontLeft");
+        frontRight = hardwareMap.get(DcMotor.class,"FrontRight");
+        backLeft = hardwareMap.get(DcMotor.class,"BackLeft");
+        backRight = hardwareMap.get(DcMotor.class,"BackRight");
+        
+        frontLeft.setDirection(DcMotor.Direction.FORWARD) ;
+        frontRight.setDirection(DcMotor.Direction.REVERSE) ;
+        backLeft.setDirection(DcMotor.Direction.FORWARD) ;
+        backRight.setDirection(DcMotor.Direction.REVERSE) ;
+        
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
+ 
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
+
+        bucket = hardwareMap.get(DcMotor.class,"Bucket");
+        bucketTurner = hardwareMap.get(DcMotor.class, "BucketTurner" );
+        linearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
+        carouselTurner = hardwareMap.get(DcMotor.class, "CarouselTurner" );
+
+
+        linearSlide.setDirection(DcMotor.Direction.FORWARD) ;
+        
+        
+        bucket.setDirection(DcMotor.Direction.FORWARD) ;
+ 
+
+        bucketTurner.setDirection(DcMotor.Direction.REVERSE) ;
+ 
+        
+        carouselTurner.setDirection(DcMotor.Direction.FORWARD) ;
+
+        
+        bucketTurner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    
+    
+    }
+        
+        
+
+    public void loop() {
+    
+        switch(linearSlideState) {
+            case LinearSlideState.LINEAR_SLIDE_START:
+                if (gamepad1.x) {
+                
+                    linearSlide.setPosition();
+                    linearSlideState = LinearSlideState.LINEAR_SLIDE_EXTEND;
+                
+                }
+                break;
+                
+            case LinearSlideState.LINEAR_SLIDE_EXTEND:
+                
+                if (Math.abs(linearSlide.getPosition() - LINEAR_SLIDE_HIGH) < 10) {
+                
+                    bucketTurner.setPosition(BUCKET_TURNER_HIGH);
+                    linearSlideState = LinearSlideState.LINEAR_SLIDE_BUCKET;
+
+                }
+                break;
+           
+            case LinearSlideState.LINEAR_SLIDE_BUCKET:
+                
+                if (Math.abs(bucketTurner.getPosition() - BUCKET_TURNER_HIGH) < 10) {
+                
+                    bucket.setPower(-1);
+                    
+                
+                
+                
+                }
+                    
+                    
+        
+        }
+ 
+    
     
     
     
     }
-      
-    public void   
-      
-        
-  
-    @Override
+    
+    /*
     public void runOpMode() throws InterruptedException {
         
         initializeRobot();
@@ -131,55 +220,12 @@ public class FSMTeleOp extends OpMode {
             
             telemetry.addData("Status", "Running");
             telemetry.update();
-            */
+            
           
           
         }
     }
     
+    */
     
-    private void initializeRobot() {
-        
-        frontLeft = hardwareMap.get(DcMotor.class,"FrontLeft");
-        frontRight = hardwareMap.get(DcMotor.class,"FrontRight");
-        backLeft = hardwareMap.get(DcMotor.class,"BackLeft");
-        backRight = hardwareMap.get(DcMotor.class,"BackRight");
-        
-        frontLeft.setDirection(DcMotor.Direction.FORWARD) ;
-        frontRight.setDirection(DcMotor.Direction.REVERSE) ;
-        backLeft.setDirection(DcMotor.Direction.FORWARD) ;
-        backRight.setDirection(DcMotor.Direction.REVERSE) ;
-        
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER) ;
- 
-        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
-        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
-        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
-        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER) ;
-
-        bucket = hardwareMap.get(DcMotor.class,"Bucket");
-        bucketTurner = hardwareMap.get(DcMotor.class, "BucketTurner" );
-        linearSlide = hardwareMap.get(DcMotor.class, "LinearSlide");
-        carouselTurner = hardwareMap.get(DcMotor.class, "CarouselTurner" );
-
-
-        linearSlide.setDirection(DcMotor.Direction.FORWARD) ;
-        
-        
-        bucket.setDirection(DcMotor.Direction.FORWARD) ;
- 
-
-        bucketTurner.setDirection(DcMotor.Direction.REVERSE) ;
- 
-        
-        carouselTurner.setDirection(DcMotor.Direction.FORWARD) ;
-
-        
-        bucketTurner.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        
-        
-    }
 }
