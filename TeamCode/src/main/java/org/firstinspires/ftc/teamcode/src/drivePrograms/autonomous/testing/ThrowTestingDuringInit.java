72a9d961f8dbd61054fbc592f22430f00f33ea9b
willm <willmoiser@gmail.com>
2022-01-17 14:06:33 -0600

    House Keeping
    Signed-off-by: willm <willmoiser@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/drivePrograms/autonomous/testing/ThrowTestingDuringInit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/drivePrograms/autonomous/testing/ThrowTestingDuringInit.java
@@ -1,15 +1,15 @@
-package org.firstinspires.ftc.teamcode.src.drivePrograms.teleop.testing;
+package org.firstinspires.ftc.teamcode.src.drivePrograms.autonomous.testing;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 import org.firstinspires.ftc.teamcode.src.utills.TeleOpTemplate;
 
-@Disabled
-@Autonomous(name = "throwTest")
 /**
  * A Autonomous to test that utility in {@link org.firstinspires.ftc.teamcode.src.utills.GenericOpModeTemplate} for extra info from stack traces works properly
  */
+@Disabled
+@Autonomous(name = "throwTest")
 public class ThrowTestingDuringInit extends TeleOpTemplate {
     public void opModeMain() throws InterruptedException {
         this.initAll();
