2d9fad50c7116bea0154f3c9da729b6400655517
BlockBeasts <kjosien1@gmail.com>
2022-01-20 17:45:55 -0700

    fix

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarouselNoSensor.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarouselNoSensor.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous
 
-public class AutonomousBlueCarouselNoSensor2 extends LinearOpMode {
+public class AutonomousBlueCarouselNoSensor extends LinearOpMode {
     Robot robot;
     int cubethingwithlevels = 0;
 
@@ -15,22 +15,22 @@ public class AutonomousBlueCarouselNoSensor2 extends LinearOpMode {
         waitForStart();
         robot.liftForMovement();
         // turns to rotate carousel and rotates carousel
-        robot.left(.5,.5);
-        robot.turnLeft(45,.5);
-        robot.left(.5,.5);
+        robot.right(.5,.5);
+        robot.turnRight(45,.5);
+        robot.right(.5,.5);
         robot.back(.47,.5);
         robot.redCarousel(4000);
         //check the code underneath, wrong places, too jerky
         robot.forward(.47,.5);
-        robot.right(.5,.5);
-        robot.turnLeft(30,.5);
-        robot.right(3,.5);
+        robot.left(.5,.5);
+        robot.turnRight(30,.5);
+        robot.left(3,.5);
         robot.armThing(3);
         robot.forward(.9,.5); //adjust to make it work
         robot.eject();
         robot.back(.8,.5); //match the forward
-        robot.turnLeft(.20,.5);
-        robot.left(4.25,.5);
+        robot.turnRight(.20,.5);
+        robot.right(4.25,.5);
         robot.forward(.8,.5); // adjust to make it work
 
     }
