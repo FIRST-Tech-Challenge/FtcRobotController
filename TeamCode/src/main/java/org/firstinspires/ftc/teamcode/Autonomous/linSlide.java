e5f95f1713ee6d1829e84ebfb9e61a5acf20ba1e
Andrewt53 <andrewt53@educbe.ca>
2022-01-21 18:07:05 -0700

    linear slide code. the file is made as its own OPmode because I can't figure out how to access hardwareMap without doing that. Copy out whichever parts of the code are needed. contains some unused code from a PID I tried making.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/linSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/linSlide.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
