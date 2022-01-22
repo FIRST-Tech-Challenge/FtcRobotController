ca569c601c227a6b4f5433a01c4a13461403c839
Andrewt53 <andrewt53@educbe.ca>
2022-01-21 18:25:11 -0700

    moved lin slide to teleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/linSlide.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonomous;
+package org.firstinspires.ftc.teamcode.TeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
