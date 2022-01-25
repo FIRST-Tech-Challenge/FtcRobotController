331ad2ceac7f825a8c6d3234c3f5d752b9d25d25
Alex <hbostic@gmail.com>
2022-01-24 23:03:17 -0500

    Start pipeline to use with FTCModel

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cv/sims/FTCMLOpenCvShippingElementDetectorSim.java
@@ -0,0 +1,344 @@
+package org.firstinspires.ftc.teamcode.cv.sims;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.dnn.Dnn;
+import org.opencv.dnn.Net;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class FTCMLOpenCvShippingElementDetectorSim extends OpenCvPipeline {
+
+
+
+    public enum TSELocation {
+        DUCKSIDE_BLUE_LEVEL_3,
+        DUCKSIDE_BLUE_LEVEL_2,
+        DUCKSIDE_BLUE_LEVEL_1,
+        NONE
+    }
+
+    TSELocation location;
+    Map<TSELocation, Integer> levels = new HashMap<>();
+
+
+    private int width = 320; // width of the image
+    private int height = 320;
+    public double inScaleFactor = 0.0278;
+
+    public double redMeanVal = 127.5;
+    public double greenMeanVal = 127.5;
+    public double blueMeanVal = 127.5;
+    public boolean cropDNN = true;
+    private Dnn cvDNN = null;
+    private Net net = null;
+    private Telemetry telemetry = null;
+    private Mat imageRGB = new Mat();
+
+    //used to store maximum confidence amoung samples taken.  Only overwrite the level if we get a higher confidence
+    private float maxConfidence = 0.0f;
+    private boolean absolutelySure = false;
+
+    public float CONF_THRESHOLD = 0.55f;
+
+
+
+    private final String[] classNamesDuckSideBlue = {"background",
+            "duckside_blue_level_3", "duckside_blue_level_2", "duckside_blue_level_1" };
+
+
+    private String[] classNames;
+
+    private final String modelPathDuckSideBlue = "/Users/alex/FtcRobotController/TeamCode/src/main/assets/tf_models/freight_frenzy_barcodes/duckside_blue/converted_keras_duckside_blue/optimized/freight_frenzy_barcodes_duckside_blue_graph.pb";
+    private final String modelPathDuckSideRed = "C:\\development\\BC4HStem\\FtcRobotController\\TeamCode\\src\\main\\assets\\tf_models\\freight_frenzy_barcodes\\duckside_red\\converted_keras_duckside_red\\optimized\\freight_frenzy_barcodes_duckside_red_graph.pb";
+    private final String modelPathWarehouseSideBlue = "C:\\development\\BC4HStem\\FtcRobotController\\TeamCode\\src\\main\\assets\\tf_models\\freight_frenzy_barcodes\\warehouseside_blue\\converted_keras_warehouseside_blue\\optimized\\freight_frenzy_barcodes_warehouseside_blue_graph.pb";
+    private final String modelPathWarehouseSideRed = "C:\\development\\BC4HStem\\FtcRobotController\\TeamCode\\src\\main\\assets\\tf_models\\freight_frenzy_barcodes\\warehouseside_blue\\converted_keras_warehouseside_blue\\optimized\\freight_frenzy_barcodes_warehouseside_blue_graph.pb";
+
+    private String modelPath = modelPathDuckSideBlue;
+    private String modelPathConfig = modelPathDuckSideBlue + "txt";
+
+    //JSONObject dnnConfig = new JSONObject();
+
+    private Mat blob = null;
+    private Mat detections = null;
+    private Mat row = null;
+
+    public FTCMLOpenCvShippingElementDetectorSim(Telemetry telemetry) {
+
+        this.telemetry = telemetry;
+
+        levels.put(TSELocation.NONE,0);
+
+        levels.put(TSELocation.DUCKSIDE_BLUE_LEVEL_1,1);
+        levels.put(TSELocation.DUCKSIDE_BLUE_LEVEL_2,2);
+        levels.put(TSELocation.DUCKSIDE_BLUE_LEVEL_3,3);
+
+        location = TSELocation.DUCKSIDE_BLUE_LEVEL_3;
+
+        classNames = classNamesDuckSideBlue;
+        modelPath = modelPathDuckSideBlue;
+        modelPathConfig = modelPathDuckSideBlue + "txt";
+        inScaleFactor = 0.0278;
+        redMeanVal = 5;
+        greenMeanVal = 191.3;
+        blueMeanVal = 226.7;
+        cropDNN = true;
+
+        /*modelPath = modelPathDuckSideRed;
+        modelPathConfig = modelPathDuckSideRed + "txt";
+        inScaleFactor = 0.02;
+        redMeanVal = 24.1;
+        greenMeanVal = 154.4;
+        blueMeanVal = 144.5;
+        cropDNN = true;*/
+
+        /*modelPath = modelPathWarehouseSideBlue;
+        modelPathConfig = modelPathWarehouseSideBlue + "txt";
+        inScaleFactor = 0.02;
+        redMeanVal = 218.2;
+        greenMeanVal = 154.4;
+        blueMeanVal = 144.5;
+        cropDNN = true;*/
+
+        /*modelPath = modelPathWarehouseSideRed;
+        modelPathConfig = modelPathWarehouseSideRed + "txt";
+        inScaleFactor = 0.019;
+        redMeanVal = 87.8;
+        greenMeanVal = 65.2;
+        blueMeanVal = 144.5;
+        cropDNN = true;*/
+
+        /*try {
+            JSONObject dsb = new JSONObject();
+            dsb.put("modelPath",modelPathDuckSideBlue);
+            dsb.put("modelPathConfig",modelPathDuckSideBlue + "txt");
+            dsb.put("inScaleFactor",0.0278);
+            dsb.put("redMeanVal",5);
+            dsb.put("greenMeanVal",191.3);
+            dsb.put("blueMeanVal",226.7);
+            dsb.put("crop",true);
+
+            JSONObject dsr = new JSONObject();
+            dsr.put("modelPath",modelPathDuckSideRed);
+            dsr.put("modelPathConfig",modelPathDuckSideRed + "txt");
+            dsr.put("inScaleFactor",0.02);
+            dsr.put("redMeanVal",24.1);
+            dsr.put("greenMeanVal",154.4);
+            dsr.put("blueMeanVal",144.5);
+            dsr.put("crop",true);
+
+            JSONObject wsb = new JSONObject();
+            wsb.put("modelPath",modelPathWarehouseSideBlue);
+            wsb.put("modelPathConfig",modelPathWarehouseSideBlue + "txt");
+            wsb.put("inScaleFactor",0.02);
+            wsb.put("redMeanVal",218.2);
+            wsb.put("greenMeanVal",154.4);
+            wsb.put("blueMeanVal",144.5);
+            wsb.put("crop",true);
+
+            JSONObject wsr = new JSONObject();
+            wsr.put("modelPath",modelPathWarehouseSideBlue);
+            wsr.put("modelPathConfig",modelPathWarehouseSideBlue + "txt");
+            wsr.put("inScaleFactor",0.02);
+            wsr.put("redMeanVal",158.7);
+            wsr.put("greenMeanVal",79.3);
+            wsr.put("blueMeanVal",97.8);
+            wsr.put("crop",true);
+
+            dnnConfig.put("dsbConfig",dsb);
+            dnnConfig.put("dsrConfig",dsr);
+            dnnConfig.put("wsb",wsb);
+            dnnConfig.put("wsr",wsr);
+        }
+        catch (JSONException ex){
+
+        }*/
+
+
+
+        /*JSONObject config = new JSONObject();
+        try{
+            config = dnnConfig.getJSONObject("dsb");
+        }
+        catch (JSONException ex){}
+
+
+        /*if(Alliance.getInstance().getAllianceTeam() == Alliance.AllianceTeam.BLUE &&
+                Side.getInstance().getPositionSide() == Side.PositionSide.DUCKSIDE){
+
+
+            try{
+                config = dnnConfig.getJSONObject("dsb");
+
+            }
+            catch (JSONException ex)
+            {
+
+            }
+
+        }
+        else if(Alliance.getInstance().getAllianceTeam() == Alliance.AllianceTeam.BLUE &&
+                Side.getInstance().getPositionSide() == Side.PositionSide.WAREHOUSESIDE)
+        {
+            try{
+                config = dnnConfig.getJSONObject("wsb");
+
+            }
+            catch (JSONException ex)
+            {
+
+            }
+        }
+
+        else if(Alliance.getInstance().getAllianceTeam() == Alliance.AllianceTeam.RED &&
+                Side.getInstance().getPositionSide() == Side.PositionSide.DUCKSIDE)
+        {
+            try{
+                config = dnnConfig.getJSONObject("dsr");
+
+            }
+            catch (JSONException ex)
+            {
+
+            }
+        }
+        else if(Alliance.getInstance().getAllianceTeam() == Alliance.AllianceTeam.RED &&
+                Side.getInstance().getPositionSide() == Side.PositionSide.WAREHOUSESIDE)
+        {
+            try{
+                config = dnnConfig.getJSONObject("wsr");
+
+            }
+            catch (JSONException ex)
+            {
+
+            }
+        }*/
+
+        /*try{
+            modelPath = config.getString("modelPath");
+            modelPathConfig = config.getString("modelPathConfig");
+            inScaleFactor = config.getDouble("inScaleFactor");
+            redMeanVal = config.getDouble("redMeanVal");
+            greenMeanVal = config.getDouble("greenMeanVal");
+            blueMeanVal = config.getDouble("blueMeanVal");
+            cropDNN = config.getBoolean("crop");
+        }
+        catch (JSONException ex){
+
+        }*/
+
+        cvDNN = new Dnn();
+        net = cvDNN.readNetFromTensorflow(modelPath);
+
+
+    }
+
+
+    @Override
+    public Mat processFrame(Mat inputFrame) {
+
+
+
+        Imgproc.cvtColor(inputFrame,imageRGB,Imgproc.COLOR_BGR2RGB);
+
+        blob = Dnn.blobFromImage(imageRGB, inScaleFactor,
+                new Size(width, height),
+                new Scalar(redMeanVal, greenMeanVal, blueMeanVal),
+                false, cropDNN);
+
+        net.setInput(blob);
+        detections = net.forward();
+
+
+        for (int i = 0; i < detections.rows(); ++i) {
+            row = detections.row(i);
+
+            Core.MinMaxLocResult mm = Core.minMaxLoc(row);
+
+            float confidence = (float) mm.maxVal;
+            Point classIdPoint = mm.maxLoc;
+
+            telemetry.addData("With confidence", confidence);
+            telemetry.update();
+
+            if (confidence >= CONF_THRESHOLD) {
+
+
+
+                int centerX = (int) (row.get(0, 0)[0] * imageRGB.cols());
+                int centerY = (int) (row.get(0, 1)[0] * imageRGB.rows());
+                int width = (int) (row.get(0, 2)[0] * imageRGB.cols());
+                int height = (int) (row.get(0, 3)[0] * imageRGB.rows());
+
+                //row.release();
+
+                int left = (int) (centerX - width * 0.5);
+                int top = (int) (centerY - height * 0.5);
+                int right = (int) (centerX + width * 0.5);
+                int bottom = (int) (centerY + height * 0.5);
+
+                Point left_top = new Point(left, top);
+                Point right_bottom = new Point(right, bottom);
+                Point label_left_top = new Point(left, top - 20);
+                DecimalFormat df = new DecimalFormat("#.##");
+
+                int class_id = (int) classIdPoint.x;
+                String className = classNames[class_id].toString();
+                String label =  className + ": " + df.format(confidence);
+
+                //telemetry.addData("This is a real new", label);
+                //telemetry.update();
+
+                switch (className)
+                {
+                    case "duckside_blue_level_1":
+                        location = TSELocation.DUCKSIDE_BLUE_LEVEL_1;
+                        break;
+
+                    case "duckside_blue_level_2":
+                        location = TSELocation.DUCKSIDE_BLUE_LEVEL_2;
+                        break;
+
+                    case "duckside_blue_level_3":
+                        location = TSELocation.DUCKSIDE_BLUE_LEVEL_3;
+                        break;
+
+                    default:
+                        location = TSELocation.NONE;
+                }
+
+                telemetry.addData("The location being set is", location);
+                telemetry.addData("With confidence", confidence);
+
+                telemetry.addData("This is a real location", getLocation());
+                telemetry.update();
+
+                Imgproc.rectangle(imageRGB, left_top, right_bottom, new Scalar(0,0,255), 3, 2);
+                Imgproc.putText(imageRGB, label, label_left_top, Imgproc.FONT_HERSHEY_SIMPLEX, .5, new Scalar(0, 0, 0), 4);
+                Imgproc.putText(imageRGB, label, label_left_top, Imgproc.FONT_HERSHEY_SIMPLEX, .5, new Scalar(255, 255, 255), 2);
+            }
+
+        }
+
+        return imageRGB;
+    }
+
+
+    public TSELocation getLocation() {
+        return location;
+    }
+    public int getTSELevel(){
+        //telemetry.addData("getTSELevel", location);
+        return levels.get(location);
+    }
+}
