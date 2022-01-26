6e40112f8c6acd92420a4e9dc5f29e57bcce5e1a
DragonDev07 <teowelton@gmail.com>
2022-01-25 21:15:20 -0700

    Added Methods and initialization to all auto files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BlueWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_BlueWarehouse.java
@@ -6,17 +6,85 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp
 public class Auto_BlueWarehouse extends LinearOpMode {
-    private ElapsedTime runtime = new ElapsedTime();
+    DcMotorEx frontL, frontR, backL, backR, duckWheel = null;
 
-    public static double encoderMethod(double distance){
-//      384.5(PPM) = ~50cm = ~20in
-//      7.9(PPM) = 1cm
-        double ppm = distance * 7.9;
-        return ppm;
+    public void drive(double directionInDegrees, double distanceInCm){
+//      384.5(PPR) = ~50cm = ~20in
+//      7.9(PPR) = 1cm
+//        4.27(PPR) = 1 Degree
+        final double oneCmInPPR = 7.9;
+        final double oneDegreeInPPR = 4.27;
+        final double velocity = 500;
+        double pprForward = distanceInCm * oneCmInPPR;
+        double pprTurn = directionInDegrees * oneDegreeInPPR;
+
+
+        if(directionInDegrees != 0) {
+            if (directionInDegrees < 0) {
+                frontL.setTargetPosition(-(int) pprTurn + frontL.getCurrentPosition());
+                frontR.setTargetPosition((int) pprTurn + frontR.getCurrentPosition());
+                backL.setTargetPosition(-(int) pprTurn + backL.getCurrentPosition());
+                backR.setTargetPosition((int) pprTurn + backR.getCurrentPosition());
+
+            } else if (directionInDegrees > 0) {
+                frontL.setTargetPosition((int) pprTurn + frontL.getCurrentPosition());
+                frontR.setTargetPosition(-(int) pprTurn + frontR.getCurrentPosition());
+                backL.setTargetPosition((int) pprTurn + backL.getCurrentPosition());
+                backR.setTargetPosition(-(int) pprTurn + backR.getCurrentPosition());
+            }
+
+            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontL.setVelocity(velocity);
+            frontR.setVelocity(velocity);
+            backR.setVelocity(velocity);
+            backL.setVelocity(velocity);
+
+            while (frontL.isBusy() || frontR.isBusy() || backL.isBusy() || backR.isBusy()) {
+                telemetry.addData("Status", "Waiting for Motors to Finish Turning");
+                telemetry.addData("Motors", "frontL Position: %d, %d", frontL.getCurrentPosition(), frontL.getTargetPosition());
+                telemetry.addData("Motors", "frontR Position: %d, %d", frontR.getCurrentPosition(), frontR.getTargetPosition());
+                telemetry.addData("Motors", "backL Position: %d, %d", backL.getCurrentPosition(), backL.getTargetPosition());
+                telemetry.addData("Motors", "backR Position: %d, %d", backR.getCurrentPosition(), backR.getTargetPosition());
+                telemetry.update();
+            }
+        }
+
+        if(distanceInCm != 0) {
+            frontL.setTargetPosition((int) pprForward + frontL.getCurrentPosition());
+            frontR.setTargetPosition((int) pprForward + frontR.getCurrentPosition());
+            backL.setTargetPosition((int) pprForward + backL.getCurrentPosition());
+            backR.setTargetPosition((int) pprForward + backR.getCurrentPosition());
+
+            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontL.setVelocity(velocity);
+            frontR.setVelocity(velocity);
+            backR.setVelocity(velocity);
+            backL.setVelocity(velocity);
+
+            while (frontL.isBusy() || frontR.isBusy() || backL.isBusy() || backR.isBusy()) {
+                telemetry.addData("Status", "Waiting for Motors to Finish Moving");
+                telemetry.addData("Motors", "frontL Position: %d, %d", frontL.getCurrentPosition(), frontL.getTargetPosition());
+                telemetry.addData("Motors", "frontR Position: %d, %d", frontR.getCurrentPosition(), frontR.getTargetPosition());
+                telemetry.addData("Motors", "backL Position: %d, %d", backL.getCurrentPosition(), backL.getTargetPosition());
+                telemetry.addData("Motors", "backR Position: %d, %d", backR.getCurrentPosition(), backR.getTargetPosition());
+                telemetry.update();
+            }
+        }
+    }
+
+    public void arm(double height, double extension){
+        // HEIGHT:
     }
 
     @Override
@@ -25,12 +93,12 @@ public class Auto_BlueWarehouse extends LinearOpMode {
         telemetry.update();
 
         DcMotor duckWheel = hardwareMap.get(DcMotor.class, "duckWheel");
-        DcMotorEx frontL  = hardwareMap.get(DcMotorEx.class, "leftFront");
-        DcMotorEx frontR = hardwareMap.get(DcMotorEx.class, "rightFront");
-        DcMotorEx backL  = hardwareMap.get(DcMotorEx.class, "leftRear");
-        DcMotorEx backR = hardwareMap.get(DcMotorEx.class, "rightRear");
-        DcMotorEx extender = hardwareMap.get(DcMotorEx.class, "extender");
-        DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
+        frontL  = hardwareMap.get(DcMotorEx.class, "leftFront");
+        frontR = hardwareMap.get(DcMotorEx.class, "rightFront");
+        backL  = hardwareMap.get(DcMotorEx.class, "leftRear");
+        backR = hardwareMap.get(DcMotorEx.class, "rightRear");
+//        DcMotorEx extender = hardwareMap.get(DcMotorEx.class, "extender");
+//        DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
 
         // Reset Encoder
         frontL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
@@ -38,51 +106,15 @@ public class Auto_BlueWarehouse extends LinearOpMode {
         backL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         backR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
 
-
-        /* Sets the motors to run using encoders. */
-
-        /* Most robots need the motor on one side to be reversed to drive forward.
-         * Reverse the motor that runs backwards when connected directly to the battery. */
         frontL.setDirection(DcMotorEx.Direction.FORWARD);
         backL.setDirection(DcMotorEx.Direction.FORWARD);
         frontR.setDirection(DcMotorEx.Direction.REVERSE);
         backR.setDirection(DcMotorEx.Direction.REVERSE);
 //      Full revolution 384.5(PPR) = ~50cm = ~20in
 
-        int power = 100;
-        double distance = 0.0;
-
 
         waitForStart();
-//      Update telemetry status to show that it is running
-        telemetry.addData("Status", "Running");
-        telemetry.update();
+        // TODO: Movements
 
-        frontL.setTargetPosition((int) Math.round(encoderMethod(distance)));
-        frontR.setTargetPosition((int) Math.round(encoderMethod(distance)));
-        backL.setTargetPosition((int) Math.round(encoderMethod(distance)));
-        backR.setTargetPosition((int) Math.round(encoderMethod(distance)));
-
-        frontL.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        frontR.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        backL.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-        backR.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
-
-        frontL.setVelocity(power);
-        frontR.setVelocity(power);
-        backL.setVelocity(power);
-        backR.setVelocity(power);
-
-
-
-        while (frontL.isBusy() || frontR.isBusy() || backL.isBusy() || backR.isBusy()) {
-            telemetry.addData("Status", "Waiting for Motors");
-            telemetry.addData("Motors", "frontL Position: %d", frontL.getCurrentPosition());
-            telemetry.addData("Motors", "frontR Position: %d", frontR.getCurrentPosition());
-            telemetry.addData("Motors", "backL Position: %d", backL.getCurrentPosition());
-            telemetry.addData("Motors", "backR Position: %d", backR.getCurrentPosition());
-            telemetry.update();
-        }
-//      Stop the Autonomous Mode after we finish parking
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedWarehouse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_RedWarehouse.java
@@ -1,4 +1,116 @@
 package org.firstinspires.ftc.teamcode;
 
-public class Auto_RedWarehouse {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Auto_RedWarehouse extends LinearOpMode {
+    DcMotorEx frontL, frontR, backL, backR, duckWheel = null;
+
+    public void drive(double directionInDegrees, double distanceInCm){
+//      384.5(PPR) = ~50cm = ~20in
+//      7.9(PPR) = 1cm
+//        4.27(PPR) = 1 Degree
+        final double oneCmInPPR = 7.9;
+        final double oneDegreeInPPR = 4.27;
+        final double velocity = 500;
+        double pprForward = distanceInCm * oneCmInPPR;
+        double pprTurn = directionInDegrees * oneDegreeInPPR;
+
+
+        if(directionInDegrees != 0) {
+            if (directionInDegrees < 0) {
+                frontL.setTargetPosition(-(int) pprTurn + frontL.getCurrentPosition());
+                frontR.setTargetPosition((int) pprTurn + frontR.getCurrentPosition());
+                backL.setTargetPosition(-(int) pprTurn + backL.getCurrentPosition());
+                backR.setTargetPosition((int) pprTurn + backR.getCurrentPosition());
+
+            } else if (directionInDegrees > 0) {
+                frontL.setTargetPosition((int) pprTurn + frontL.getCurrentPosition());
+                frontR.setTargetPosition(-(int) pprTurn + frontR.getCurrentPosition());
+                backL.setTargetPosition((int) pprTurn + backL.getCurrentPosition());
+                backR.setTargetPosition(-(int) pprTurn + backR.getCurrentPosition());
+            }
+
+            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontL.setVelocity(velocity);
+            frontR.setVelocity(velocity);
+            backR.setVelocity(velocity);
+            backL.setVelocity(velocity);
+
+            while (frontL.isBusy() || frontR.isBusy() || backL.isBusy() || backR.isBusy()) {
+                telemetry.addData("Status", "Waiting for Motors to Finish Turning");
+                telemetry.addData("Motors", "frontL Position: %d, %d", frontL.getCurrentPosition(), frontL.getTargetPosition());
+                telemetry.addData("Motors", "frontR Position: %d, %d", frontR.getCurrentPosition(), frontR.getTargetPosition());
+                telemetry.addData("Motors", "backL Position: %d, %d", backL.getCurrentPosition(), backL.getTargetPosition());
+                telemetry.addData("Motors", "backR Position: %d, %d", backR.getCurrentPosition(), backR.getTargetPosition());
+                telemetry.update();
+            }
+        }
+
+        if(distanceInCm != 0) {
+            frontL.setTargetPosition((int) pprForward + frontL.getCurrentPosition());
+            frontR.setTargetPosition((int) pprForward + frontR.getCurrentPosition());
+            backL.setTargetPosition((int) pprForward + backL.getCurrentPosition());
+            backR.setTargetPosition((int) pprForward + backR.getCurrentPosition());
+
+            frontL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            frontR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            backR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            frontL.setVelocity(velocity);
+            frontR.setVelocity(velocity);
+            backR.setVelocity(velocity);
+            backL.setVelocity(velocity);
+
+            while (frontL.isBusy() || frontR.isBusy() || backL.isBusy() || backR.isBusy()) {
+                telemetry.addData("Status", "Waiting for Motors to Finish Moving");
+                telemetry.addData("Motors", "frontL Position: %d, %d", frontL.getCurrentPosition(), frontL.getTargetPosition());
+                telemetry.addData("Motors", "frontR Position: %d, %d", frontR.getCurrentPosition(), frontR.getTargetPosition());
+                telemetry.addData("Motors", "backL Position: %d, %d", backL.getCurrentPosition(), backL.getTargetPosition());
+                telemetry.addData("Motors", "backR Position: %d, %d", backR.getCurrentPosition(), backR.getTargetPosition());
+                telemetry.update();
+            }
+        }
+    }
+
+    public void arm(double height, double extension){
+        // HEIGHT:
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        DcMotor duckWheel = hardwareMap.get(DcMotor.class, "duckWheel");
+        frontL  = hardwareMap.get(DcMotorEx.class, "leftFront");
+        frontR = hardwareMap.get(DcMotorEx.class, "rightFront");
+        backL  = hardwareMap.get(DcMotorEx.class, "leftRear");
+        backR = hardwareMap.get(DcMotorEx.class, "rightRear");
+//        DcMotorEx extender = hardwareMap.get(DcMotorEx.class, "extender");
+//        DcMotorEx arm = hardwareMap.get(DcMotorEx.class, "arm");
+
+        // Reset Encoder
+        frontL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        frontR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        backR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontL.setDirection(DcMotorEx.Direction.FORWARD);
+        backL.setDirection(DcMotorEx.Direction.FORWARD);
+        frontR.setDirection(DcMotorEx.Direction.REVERSE);
+        backR.setDirection(DcMotorEx.Direction.REVERSE);
+//      Full revolution 384.5(PPR) = ~50cm = ~20in
+
+
+        waitForStart();
+        // TODO: Movements
+
+    }
 }
