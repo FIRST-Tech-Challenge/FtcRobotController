cd78cf64dda95a338a0874d6952e85be74d8beae
celinelou <celine.lou@students.tka.org>
2022-01-27 16:27:37 -0800

    commit message i genuinely dont know what changed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel/CarouselTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Carousel/CarouselTest.java
@@ -1,10 +1,10 @@
-package org.firstinspires.ftc.teamcode.Carousel;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Jon DC")
+@TeleOp(name = "Carousel Test")
 public class CarouselTest extends LinearOpMode{
     DcMotor carouselTurningMotor;
 
@@ -13,17 +13,27 @@ public class CarouselTest extends LinearOpMode{
 
 
         waitForStart();
+        Carousel car = new Carousel(hardwareMap);
 
         while (opModeIsActive()) {
-
-            Carousel car = new Carousel(hardwareMap);
-
-            car.carouselBoolean(this.gamepad1.a, true);
-
+            car.toggleDirection(this.gamepad1.b);
+            car.toggleCarousel(this.gamepad1.a);
         }
-    }
 
+
+        //Hold
+        // car.carouselBoolean(this.gamepad1.a, true);
+
+
+        // car.carouselBoolean(this.gamepad1.b, false);
+
+    }
 }
+
+
+
+
+
 
 
 
