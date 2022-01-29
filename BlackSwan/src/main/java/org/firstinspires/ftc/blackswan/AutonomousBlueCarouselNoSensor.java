79177c787e6bbe428645db6dfe5a9217102bba17
DraskiTheDragon <deanbras@gmail.com>
2022-01-29 13:47:08 -0700

    TeleOP Fixes BlackSwan

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarouselNoSensor.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/AutonomousBlueCarouselNoSensor.java
@@ -16,27 +16,28 @@ public class AutonomousBlueCarouselNoSensor extends LinearOpMode {
         //setup for fieldcentric driving
         robot.forward(1,.5);
         robot.turnLeft(90,.5);
-        robot.right(1,.5);
+        robot.left(1.25,.5);
         //movelift
         robot.liftForMovement();
         // turns to rotate carousel and rotates carousel
-        robot.right(.5,.5);
+        //robot.left(.5,.5);
         robot.turnRight(45,.5);
         robot.right(.5,.5);
-        robot.back(.47,.5);
-        robot.blueCarousel(4000);
-        //check the code underneath, wrong places, too jerky
+        robot.back(.55,.5);
+        robot.redCarousel(4000);
         robot.forward(.47,.5);
         robot.left(.5,.5);
-        robot.turnRight(30,.5);
-        robot.left(3.4,.5);
+        robot.turnRight(40,.5);
+        //scoring stuff
+        robot.left(3,.5);
+        robot.forward(.5,.5); //adjust to make it work
         robot.armThing(3);
-        robot.forward(.95,.5); //adjust to make it work
+        robot.forward(0.4,0.5);
         robot.eject();
-        robot.back(.85,.5);
-        robot.turnRight(.20,.5);
-        robot.right(4.4,.5);
-        robot.forward(1.3,.5); // adjust to make it work
+        robot.back(.1,.5); //match the forward
+        robot.right(4.5,.5);
+        robot.armThing(0);
+        robot.forward(0.5,0.2);
 
     }
 }
