164740375e6bae2fbe67e471d326399beccf28a5
Ken Mori <ken.mori@students.tka.org>
2022-01-28 16:39:52 -0800

    Did some stuff regarding outtake intake and carousel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CodeBlue/AutonomousCodeBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/CodeBlue/AutonomousCodeBlue.java
@@ -1,38 +1,38 @@
-package org.firstinspires.ftc.teamcode.Autonomous.CodeBlue;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.teamcode.Carousel.Carousel;
-import org.firstinspires.ftc.teamcode.Chassis.Drivechain;
-import org.firstinspires.ftc.teamcode.Intake.Intake;
-import org.firstinspires.ftc.teamcode.Outtake.Outtake;
-
-public class AutonomousCodeBlue extends LinearOpMode {
-
-    @Override
-    public void runOpMode() throws InterruptedException {
-
-        Intake AutonomousIntake = new Intake(hardwareMap);
-        Outtake AutonomousOuttake = new Outtake(hardwareMap);
-        Carousel AutonomousCarousel = new Carousel(hardwareMap);
-        Drivechain AutonomousDC = new Drivechain(hardwareMap);
-
-        waitForStart();
-
-        // TODO, step 1
-
-        //Step 2: DONE
-//        AutonomousOuttake.FlapOpen();
-        AutonomousCarousel.CarouselAutonomous();
-        //TODO, step 3
-
-        //Step 4: DONE
-        //AutonomousCarousel.CarouselHandler();
-
-        AutonomousDC.moveRobot( 1000, -1000, 1000, -1000, 0.25, -0.25, 0.25, 0.25);
-
-        //TODO, step 5/6
-
-
-    }
-}
+//package org.firstinspires.ftc.teamcode.Autonomous.CodeBlue;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//
+//import org.firstinspires.ftc.teamcode.Carousel.Carousel;
+//import org.firstinspires.ftc.teamcode.Chassis.Drivechain;
+//import org.firstinspires.ftc.teamcode.Intake.Intake;
+//import org.firstinspires.ftc.teamcode.Outtake.Outtake;
+//
+//public class AutonomousCodeBlue extends LinearOpMode {
+//
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//
+//        Intake AutonomousIntake = new Intake(hardwareMap);
+//        Outtake AutonomousOuttake = new Outtake(hardwareMap);
+//        Carousel AutonomousCarousel = new Carousel(hardwareMap);
+//        Drivechain AutonomousDC = new Drivechain(hardwareMap);
+//
+//        waitForStart();
+//
+//        // TODO, step 1
+//
+//        //Step 2: DONE
+////        AutonomousOuttake.FlapOpen();
+//        AutonomousCarousel.CarouselAutonomous();
+//        //TODO, step 3
+//
+//        //Step 4: DONE
+//        //AutonomousCarousel.CarouselHandler();
+//
+//        AutonomousDC.moveRobot( 1000, -1000, 1000, -1000, 0.25, -0.25, 0.25, 0.25);
+//
+//        //TODO, step 5/6
+//
+//
+//    }
+//}
