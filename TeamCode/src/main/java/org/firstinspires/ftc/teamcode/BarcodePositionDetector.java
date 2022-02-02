bf8d3257db47282ce95499c93e8d0ecbfc952825
Sergio <lhsrobotics@lovingtonschools.net>
2022-02-02 15:53:27 -0700

    Tried Fixing BarcodePositionDetector

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarcodePositionDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BarcodePositionDetector.java
@@ -24,21 +24,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
-
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.opencv.core.Core;
+import org.openc.core.CORE;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
