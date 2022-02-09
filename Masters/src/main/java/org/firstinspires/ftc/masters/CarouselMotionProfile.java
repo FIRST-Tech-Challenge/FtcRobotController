ae9dd5fd8fe2694b2fd007c1be4bc53526bcf99a
BlockBeasts <kjosien1@gmail.com>
2022-02-08 22:18:00 -0700

    ducks

--- Masters/src/main/java/org/firstinspires/ftc/masters/CarouselMotionProfile.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/CarouselMotionProfile.java
@@ -1,39 +1,40 @@
 package org.firstinspires.ftc.masters;
 
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.roadrunner.control.PIDCoefficients;
-import com.acmerobotics.roadrunner.control.PIDFController;
-import com.acmerobotics.roadrunner.profile.MotionProfile;
-import com.acmerobotics.roadrunner.profile.MotionProfileBuilder;
-import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
-import com.acmerobotics.roadrunner.profile.MotionState;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
-@TeleOp(name = "carousel test")
+@Config
+@TeleOp(name = "carousel test quick duck")
 public class CarouselMotionProfile extends LinearOpMode {
 
-    public static int accelerate1;
-    public static int accelerate2;
-    public static int accelerate3;
+    public static int accelerate1 = 300;
+    public static int accelerate2= 200;
+    public static int accelerate3 = 600;
+    public static  int startVelocity= 650;
     public DcMotorEx carousel;
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        FtcDashboard dashboard = FtcDashboard.getInstance();
+
         telemetry = dashboard.getTelemetry();
 
         carousel = hardwareMap.get(DcMotorEx.class, "carouselMotor");
-        //carousel.setVelocityPIDFCoefficients(0,0,0,0);
+        carousel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        carousel.setVelocityPIDFCoefficients(10,0,0.01,14);
 
-        int encoderPos;
-        int goal = 1300;
-        double velocity;
+        int encoderPos=0;
+        int goal = 3500;
+
+        double velocity=0;
         boolean carouselOn = false;
+        boolean start = true;
 
 //        MotionProfile profile1 = MotionProfileGenerator.generateSimpleMotionProfile(
 //                new MotionState(0, 0, 0),
@@ -53,43 +54,64 @@ public class CarouselMotionProfile extends LinearOpMode {
 //                //.appendProfile(profile2);
 //        MotionProfile profile= motionProfileBuilder.build();
 
-//        ElapsedTime elapsedTime = new ElapsedTime();
+       ElapsedTime elapsedTime = new ElapsedTime();
 //        carousel.getController();
 //        PIDFController controller = new PIDFController(new PIDCoefficients(0, 0, 0));
         waitForStart();
+        double vel2Max=0;
+        double vel1Max=0;
 
         while (opModeIsActive()) {
-            if (carouselOn) {
-////                MotionState state = profile.get(elapsedTime.milliseconds() / 1000);
-//                telemetry.addData("x", state.getX());
-//                telemetry.addData("v", state.getV());
-                if (state.getX() < goal) {
-                    carousel.setVelocity(Math.max(800, state.getV() * 4));
-                } else {
-                    carouselOn = false;
-                    carousel.setVelocity(0);
-                }
-
-//                controller.setTargetPosition(state.getX());
-//                controller.setTargetVelocity(state.getV());
-//                controller.setTargetAcceleration(state.getA());
-
-
-//                if (state.getX()>=600){
-//                    carouselOn= false;
-//                    carousel.setVelocity(0);
-//                }
-            }
+            //telemetry.addData("carousel test", "1");
+            telemetry.addData("velocity", carousel.getVelocity());
+            telemetry.addData("encoder", carousel.getCurrentPosition());
+            telemetry.addData("time", elapsedTime.milliseconds());
+            telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
 
             if (gamepad1.a) {
                 carouselOn = true;
-                telemetry.addData("carousel on", "1");
+                if (start) {
+                    elapsedTime.reset();
+                    start= false;
+                }
+
             } else if (gamepad1.x) {
                 carouselOn = false;
                 carousel.setVelocity(0);
+
             }
 
-            telemetry.update();
+            if (carouselOn) {
+
+                encoderPos = carousel.getCurrentPosition();
+
+                if (encoderPos < 600) {
+                    velocity = Math.sqrt(2*accelerate1*encoderPos)+startVelocity;
+                    vel1Max = velocity;
+                    carousel.setVelocity(velocity);
+                    telemetry.update();
+                } else if (encoderPos >= 500 && encoderPos < 1900) {
+                    velocity = vel1Max + Math.sqrt(2 * accelerate2 * (encoderPos - 600));
+                    vel2Max = velocity;
+                    carousel.setVelocity(velocity);
+                    telemetry.update();
+                }
+                 else if (encoderPos >= 1900 && encoderPos < goal) {
+                    velocity = vel2Max+Math.sqrt(2*accelerate3*(encoderPos-1600));
+                    carousel.setVelocity(velocity);
+                    telemetry.update();
+                } else if (encoderPos >= goal) {
+                    carouselOn = false;
+                    carousel.setVelocity(0);
+                    carousel.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+                }
+
+            }
+
+
+
+
         }
 
     }
