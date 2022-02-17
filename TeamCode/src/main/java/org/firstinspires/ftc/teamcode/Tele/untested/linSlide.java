1e78b8e109a6463a04a390147319a0646886a17f
DiefenbakerChiefsFTC <dief.chiefs20802@gmail.com>
2022-02-17 16:21:49 -0700

    William's lin slide.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/linSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/linSlide.java
@@ -1,22 +1,57 @@
 package org.firstinspires.ftc.teamcode.Tele.untested;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
+@TeleOp
 public class linSlide {
 
-    static DcMotor LinSlideMotor = null;
+    static final int low = 000; //encoder values
+    static final int high = 2100;
+
+    public enum states{LOW, HIGH} //states the slide can be in
+    static states state = states.LOW;
+
+    static DcMotor LinSlideMotor = null; //decalres motor
 
     public static void setLSMotor(DcMotor LSMotor){
-        LinSlideMotor = LSMotor;
+        stateMachine(LSMotor);
+        if(LSMotor.getCurrentPosition() == low){
+            LSMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
     }
 
-    public static void stateMachine(){
-
+    public static void stateMachine(DcMotor LSM){
+        switch (state){
+            case LOW:
+                if(gamepad1.left_trigger == 1 && LSM.getCurrentPosition() != high){
+                    state = states.HIGH;
+                    LSM.setTargetPosition(high);
+                    while (LSM.getCurrentPosition() != high) {
+                        LSM.setPower(0.9);
+                        LSM.getCurrentPosition();
+                    }
+                    LSM.setPower(0.1);
+                }
+                break;
+            case HIGH:
+                if(gamepad1.right_trigger == 1 && LSM.getCurrentPosition() != low){
+                    state = states.LOW;
+                    LSM.setTargetPosition(low);
+                    while (LSM.getCurrentPosition() != low) {
+                        LSM.setPower(0.9);
+                        LSM.getCurrentPosition();
+                    }
+                    LSM.setPower(0);
+                }
+                break;
+            default:
+                break;
+        }
     }
 
-    public static void controllerInput(Gamepad gamepad1){
-
-    }
 
 }
