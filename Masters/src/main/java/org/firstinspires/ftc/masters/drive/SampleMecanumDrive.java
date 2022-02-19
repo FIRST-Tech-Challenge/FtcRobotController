d78bcae7fbf984854aca06072e9f1904f477787a
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2022-02-19 09:39:34 -0700

    Computer Vision happeninantionatings.

--- Masters/src/main/java/org/firstinspires/ftc/masters/drive/SampleMecanumDrive.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/drive/SampleMecanumDrive.java
@@ -587,6 +587,10 @@ public class SampleMecanumDrive extends MecanumDrive {
         return CV.duckPipeline.position;
     }
 
+    public MultipleCameraCV.WarehouseDeterminationPipeline.FreightPosition analyzeWarehouse(){
+        return CV.warehouseDeterminationPipeline.position;
+    }
+
 
 //    public TheAbsolutelyPositivelyWithoutAShadowOfADoubtFinalLastIterationOfFreightFrenzyCV.SkystoneDeterminationPipeline.HubPosition analyze_hub_blue() {
 //        return CV.pipeline.hub_position;
