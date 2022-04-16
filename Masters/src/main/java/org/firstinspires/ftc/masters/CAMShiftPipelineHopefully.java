1d8a994a41c5069c8d98cf6da5c8abe90de507dc
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2022-04-16 12:01:23 -0600

    Successful CV shenanigans. (contours work, CAMshift is up in the air.

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/CAMShiftPipelineHopefully.java
@@ -0,0 +1,228 @@
+package org.firstinspires.ftc.masters;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Hey, future Oliver, this is just your reminder that Greg (the intern)
+// arbitrarily changed everything from YCrCb to HSV without doing any of
+// the necessary conversions. Good luck.
+
+public class CAMShiftPipelineHopefully extends OpenCvPipeline {
+    Scalar HOT_PINK = new Scalar(196, 23, 112);
+
+    // Pink, the default color                         Y      Cr     Cb    (Do not change Y)
+//    public static Scalar scalarLowerYCrCb = new Scalar(0.0, 150.0, 120.0);
+//    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 255.0);
+
+    // Yellow, freight or ducks!
+    //public static Scalar scalarLowerYCrCb = new Scalar(0.0, 100.0, 0.0);
+    //public static Scalar scalarUpperYCrCb = new Scalar(255.0, 170.0, 120.0);
+
+    // Green                                             Y      Cr     Cb
+     public static Scalar scalarLowerHSV = new Scalar(  0.0, 0.0, 0.0);
+     public static Scalar scalarUpperHSV = new Scalar(255.0, 120.0, 120.0);
+
+    // Use this picture for you own color https://github.com/PinkToTheFuture/OpenCV_FreightFrenzy_2021-2022/blob/main/YCbCr.jpeg
+    // Note that the Cr and Cb values range between 0-255. this means that the origin of the coordinate system is (128,128)
+
+    // Volatile because accessed by OpMode without sync
+    public volatile boolean error = false;
+    public volatile Exception debug;
+
+    private double borderLeftX;     //fraction of pixels from the left side of the cam to skip
+    private double borderRightX;    //fraction of pixels from the right of the cam to skip
+    private double borderTopY;      //fraction of pixels from the top of the cam to skip
+    private double borderBottomY;   //fraction of pixels from the bottom of the cam to skip
+
+    private int CAMERA_WIDTH;
+    private int CAMERA_HEIGHT;
+
+    private int loopCounter = 0;
+    private int pLoopCounter = 0;
+
+
+    // hardcode the initial location of window
+    private Rect trackWindow = new Rect(150, 60, 63, 125);
+
+    private final Mat mat = new Mat();
+    private final Mat processed = new Mat();
+
+    private Rect maxRect = new Rect(600,1,1,1);
+
+    private double maxArea = 0;
+    private boolean first = false;
+
+    private final Object sync = new Object();
+
+    public CAMShiftPipelineHopefully(double borderLeftX, double borderRightX, double borderTopY, double borderBottomY) {
+        this.borderLeftX = borderLeftX;
+        this.borderRightX = borderRightX;
+        this.borderTopY = borderTopY;
+        this.borderBottomY = borderBottomY;
+    }
+    public void configureScalarLower(double h, double s, double v) {
+        scalarLowerHSV = new Scalar(h, s, v);
+    }
+    public void configureScalarUpper(double h, double s, double v) {
+        scalarUpperHSV = new Scalar(h, s, v);
+    }
+    public void configureScalarLower(int h, int s, int v) {
+        scalarLowerHSV = new Scalar(h, s, v);
+    }
+    public void configureScalarUpper(int h, int s, int v) {
+        scalarUpperHSV = new Scalar(h, s, v);
+    }
+    public void configureBorders(double borderLeftX, double borderRightX, double borderTopY, double borderBottomY) {
+        this.borderLeftX = borderLeftX;
+        this.borderRightX = borderRightX;
+        this.borderTopY = borderTopY;
+        this.borderBottomY = borderBottomY;
+    }
+
+    @Override
+    public Mat processFrame(Mat input) {
+        CAMERA_WIDTH = input.width();
+        CAMERA_HEIGHT = input.height();
+        try {
+            // Process Image
+            Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2HSV);
+            Core.inRange(mat, scalarLowerHSV, scalarUpperHSV, processed);
+            // Remove Noise
+            Imgproc.morphologyEx(processed, processed, Imgproc.MORPH_OPEN, new Mat());
+            Imgproc.morphologyEx(processed, processed, Imgproc.MORPH_CLOSE, new Mat());
+            // GaussianBlur
+            Imgproc.GaussianBlur(processed, processed, new Size(5.0, 15.0), 0.00);
+            // Find Contours
+            List<MatOfPoint> contours = new ArrayList<>();
+            Imgproc.findContours(processed, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            // Draw Contours
+            Imgproc.drawContours(input, contours, -1, new Scalar(255, 0, 0));
+
+            // Lock this up to prevent errors when outside threads access the max rect property.
+            synchronized (sync) {
+                // Loop Through Contours
+                for (MatOfPoint contour : contours) {
+                    Point[] contourArray = contour.toArray();
+
+                    // Bound Rectangle if Contour is Large Enough
+                    if (contourArray.length >= 15) {
+                        MatOfPoint2f areaPoints = new MatOfPoint2f(contourArray);
+                        Rect rect = Imgproc.boundingRect(areaPoints);
+
+                        if (                        rect.area() > maxArea
+                                && rect.x + (rect.width / 2.0)  > (borderLeftX * CAMERA_WIDTH)
+                                && rect.x + (rect.width / 2.0)  < CAMERA_WIDTH - (borderRightX * CAMERA_WIDTH)
+                                && rect.y + (rect.height / 2.0) > (borderTopY * CAMERA_HEIGHT)
+                                && rect.y + (rect.height / 2.0) < CAMERA_HEIGHT - (borderBottomY * CAMERA_HEIGHT)
+
+                                || loopCounter - pLoopCounter   > 6
+                                && rect.x + (rect.width / 2.0)  > (borderLeftX * CAMERA_WIDTH)
+                                && rect.x + (rect.width / 2.0)  < CAMERA_WIDTH - (borderRightX * CAMERA_WIDTH)
+                                && rect.y + (rect.height / 2.0) > (borderTopY * CAMERA_HEIGHT)
+                                && rect.y + (rect.height / 2.0) < CAMERA_HEIGHT - (borderBottomY * CAMERA_HEIGHT)
+                        ){
+                            maxArea = rect.area();
+                            maxRect = rect;
+                            pLoopCounter++;
+                            loopCounter = pLoopCounter;
+                            first = true;
+                        }
+                        else if(loopCounter - pLoopCounter > 10){
+                            maxArea = new Rect().area();
+                            maxRect = new Rect();
+                        }
+
+                        areaPoints.release();
+                    }
+                    contour.release();
+                }
+                if (contours.isEmpty()) {
+                    maxRect = new Rect(600,1,1,1);
+                }
+            }
+            // Draw Rectangles If Area Is At Least 500
+            if (first && maxRect.area() > 500) {
+                Imgproc.rectangle(input, maxRect, new Scalar(0, 255, 0), 2);
+            }
+            // Draw Borders
+            Imgproc.rectangle(input, new Rect(
+                    (int) (borderLeftX * CAMERA_WIDTH),
+                    (int) (borderTopY * CAMERA_HEIGHT),
+                    (int) (CAMERA_WIDTH - (borderRightX * CAMERA_WIDTH) - (borderLeftX * CAMERA_WIDTH)),
+                    (int) (CAMERA_HEIGHT - (borderBottomY * CAMERA_HEIGHT) - (borderTopY * CAMERA_HEIGHT))
+            ), HOT_PINK, 2);
+
+            // Display Data
+            Imgproc.putText(input, "Area: " + getRectArea() + " Midpoint: " + getRectMidpointXY().x + " , " + getRectMidpointXY().y, new Point(5, CAMERA_HEIGHT - 5), 0, 0.6, new Scalar(255, 255, 255), 2);
+
+            loopCounter++;
+        } catch (Exception e) {
+            debug = e;
+            error = true;
+        }
+        return input;
+    }
+    /*
+    Synchronize these operations as the user code could be incorrect otherwise, i.e a property is read
+    while the same rectangle is being processed in the pipeline, leading to some values being not
+    synced.
+     */
+
+    public int getRectHeight() {
+        synchronized (sync) {
+            return maxRect.height;
+        }
+    }
+    public int getRectWidth() {
+        synchronized (sync) {
+            return maxRect.width;
+        }
+    }
+    public int getRectX() {
+        synchronized (sync) {
+            return maxRect.x;
+        }
+    }
+    public int getRectY() {
+        synchronized (sync) {
+            return maxRect.y;
+        }
+    }
+    public double getRectMidpointX() {
+        synchronized (sync) {
+            return getRectX() + (getRectWidth() / 2.0);
+        }
+    }
+    public double getRectMidpointY() {
+        synchronized (sync) {
+            return getRectY() + (getRectHeight() / 2.0);
+        }
+    }
+    public Point getRectMidpointXY() {
+        synchronized (sync) {
+            return new Point(getRectMidpointX(), getRectMidpointY());
+        }
+    }
+    public double getAspectRatio() {
+        synchronized (sync) {
+            return getRectArea() / (CAMERA_HEIGHT * CAMERA_WIDTH);
+        }
+    }
+    public double getRectArea() {
+        synchronized (sync) {
+            return maxRect.area();
+        }
+    }
+}
