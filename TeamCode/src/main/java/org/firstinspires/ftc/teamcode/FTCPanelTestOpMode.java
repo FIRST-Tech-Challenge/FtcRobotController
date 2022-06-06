c7981c14ede2f48980fcc079743fffcdac1088fe
Cooper Clem <ironreignrobotics@gmail.com>
2022-06-06 16:49:48 -0500

    ftc panels
    not sure why this stuff was not checked in earlier

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCPanelTestOpMode.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+
+import androidx.annotation.ColorInt;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.util.FTCPanels;
+
+@TeleOp()
+// @Disabled
+public class FTCPanelTestOpMode extends OpMode {
+    public byte RedAlliance = 0x00;
+    public byte BlueAlliance = 0x01;
+
+    private FTCPanels panels;
+    private ElapsedTime elapsedTime = new ElapsedTime();
+    private int colorIndex = 0;
+    private @ColorInt int[] colors = new int[] { Color.rgb(148, 0, 211), Color.rgb(75, 0, 130), Color.rgb(0, 0, 255),
+            Color.rgb(0, 255, 0), Color.rgb(255, 0, 0), Color.rgb(255, 255, 0), Color.parseColor("purple"),
+            Color.parseColor("teal"), Color.parseColor("silver"), Color.rgb(0, 0, 0) };
+
+    // Code to run ONCE when the driver hits INIT
+    @Override
+    public void init() {
+        panels = hardwareMap.get(FTCPanels.class, "ftc_panels");
+        panels.setAlliance(RedAlliance);
+        panels.setBrightness(4);
+        panels.setColor(Color.parseColor("red"));
+        panels.setColor(5, Color.parseColor("blue"));
+
+    }
+
+    @Override
+    public void start() {
+        elapsedTime.reset();
+    }
+
+    // Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+    @Override
+    public void loop() {
+        if (elapsedTime.milliseconds() >= 500) {
+            if (colorIndex == colors.length) {
+                panels.setColors(colors);
+                colorIndex = 0;
+            } else {
+                panels.setColor(colors[colorIndex]);
+                colorIndex++;
+            }
+            elapsedTime.reset();
+        }
+    }
+
+    @Override
+    public void stop() {
+        panels.turnAllOff();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/FTCPanels.java
@@ -0,0 +1,190 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import android.graphics.Color;
+import androidx.annotation.ColorInt;
+
+import com.qualcomm.robotcore.hardware.I2cAddr;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchDevice;
+import com.qualcomm.robotcore.hardware.I2cDeviceSynchSimple;
+import com.qualcomm.robotcore.hardware.I2cWaitControl;
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
+
+@I2cDeviceType()
+@DeviceProperties(name = "FTCPanels", description = "Iron Reign FTC Panels", xmlTag = "FTC_PANELS")
+public class FTCPanels extends I2cDeviceSynchDevice<I2cDeviceSynchSimple> {
+
+    private enum Commands {
+        CHANGE_LED_LENGTH(0x70),
+        WRITE_SINGLE_LED_COLOR(0x71),
+        WRITE_ALL_LED_COLOR(0x72),
+        WRITE_RED_ARRAY(0x73),
+        WRITE_GREEN_ARRAY(0x74),
+        WRITE_BLUE_ARRAY(0x75),
+        WRITE_SINGLE_LED_BRIGHTNESS(0x76),
+        WRITE_ALL_LED_BRIGHTNESS(0x77),
+        WRITE_ALL_LED_OFF(0x78),
+        WRITE_SET_ALLIANCE(0x79);
+        int bVal;
+
+        Commands(int bVal) {
+            this.bVal = bVal;
+        }
+    }
+
+    /**
+     * Change the color of a specific LED
+     *
+     * @param position which LED to change (1 - 255)
+     * @param color    what color to set it to
+     */
+    public void setColor(int position, @ColorInt int color) {
+        byte[] data = new byte[4];
+        data[0] = (byte) position;
+        data[1] = (byte) Color.red(color);
+        data[2] = (byte) Color.green(color);
+        data[3] = (byte) Color.blue(color);
+        writeI2C(Commands.WRITE_SINGLE_LED_COLOR, data);
+    }
+
+    /**
+     * Change the color of all LEDs to a single color
+     *
+     * @param color what the color should be
+     */
+    public void setColor(@ColorInt int color) {
+        byte[] data = new byte[3];
+        data[0] = (byte) Color.red(color);
+        data[1] = (byte) Color.green(color);
+        data[2] = (byte) Color.blue(color);
+        writeI2C(Commands.WRITE_ALL_LED_COLOR, data);
+    }
+
+    /**
+     * Set the current Alliance
+     *
+     * @param alliance which Alliance to set - RED or BLUE
+     */
+    public void setAlliance(byte alliance) {
+        byte[] data = new byte[1];
+        data[0] = (byte) alliance;
+        writeI2C(Commands.WRITE_SET_ALLIANCE, data);
+    }
+    /**
+     * Send a segment of the LED array
+     *
+     * @param cmd    command to send
+     * @param array  the values (limited from 0..255)
+     * @param offset the starting value (LED only, array starts at 0)
+     * @param length the length to send
+     */
+    private void sendSegment(Commands cmd, int[] array, int offset, int length) {
+        byte[] data = new byte[length + 2];
+        data[0] = (byte) length;
+        data[1] = (byte) offset;
+
+        for (int i = 0; i < length; i++) {
+            data[2 + i] = (byte) array[i];
+        }
+        writeI2C(cmd, data);
+    }
+
+    /**
+     * Change the color of an LED color segment
+     *
+     * @param colors what the colors should be
+     * @param offset where in the array to start
+     * @param length length to send (limited to 12)
+     */
+    private void setLEDColorSegment(@ColorInt int[] colors, int offset, int length) {
+        int[] redArray = new int[length];
+        int[] greenArray = new int[length];
+        int[] blueArray = new int[length];
+
+        for (int i = 0; i < colors.length; i++) {
+            redArray[i] = Color.red(colors[i + offset]);
+            greenArray[i] = Color.green(colors[i + offset]);
+            blueArray[i] = Color.blue(colors[i + offset]);
+        }
+        sendSegment(Commands.WRITE_RED_ARRAY, redArray, offset, length);
+        sendSegment(Commands.WRITE_GREEN_ARRAY, greenArray, offset, length);
+        sendSegment(Commands.WRITE_BLUE_ARRAY, blueArray, offset, length);
+    }
+
+    /**
+     * Change the color of all LEDs using arrays
+     *
+     * @param colors array of colors to set lights to
+     */
+    public void setColors(@ColorInt int[] colors) {
+        int length = colors.length;
+
+        int numInLastSegment = length % 12;
+        int numSegments = length / 12;
+        for (int i = 0; i < numSegments; i++) {
+            setLEDColorSegment(colors, i * 12, 12);
+        }
+        setLEDColorSegment(colors, numSegments * 12, numInLastSegment);
+    }
+
+    /**
+     * Set the brightness of an individual LED
+     *
+     * @param number     which LED to change (1-255)
+     * @param brightness brightness level (0-31)
+     */
+    public void setBrightness(int number, int brightness) {
+        byte[] data = new byte[2];
+        data[0] = (byte) number;
+        data[1] = (byte) brightness;
+        writeI2C(Commands.WRITE_SINGLE_LED_BRIGHTNESS, data);
+    }
+
+    /**
+     * Set the brightness of all LEDs
+     *
+     * @param brightness brightness level (0-31)
+     */
+    public void setBrightness(int brightness) {
+        byte[] data = new byte[1];
+        data[0] = (byte) brightness;
+        writeI2C(Commands.WRITE_ALL_LED_BRIGHTNESS, data);
+    }
+
+    /**
+     * Turn all LEDS off...
+     */
+    public void turnAllOff() {
+        setColor(0);
+    }
+
+    private void writeI2C(Commands cmd, byte[] data) {
+        deviceClient.write(cmd.bVal, data, I2cWaitControl.WRITTEN);
+    }
+
+
+    @Override
+    public Manufacturer getManufacturer() {
+        return Manufacturer.Other;
+    }
+
+    @Override
+    protected synchronized boolean doInitialize() {
+        return true;
+    }
+
+    @Override
+    public String getDeviceName() {
+        return "FTC Panels";
+    }
+
+    private final static I2cAddr ADDRESS_I2C_DEFAULT = I2cAddr.create7bit(0x30);
+
+    public FTCPanels(I2cDeviceSynchSimple deviceClient) {
+        super(deviceClient, true);
+
+        this.deviceClient.setI2cAddress(ADDRESS_I2C_DEFAULT);
+        super.registerArmingStateCallback(false);
+    }
+
+}
