71741c305024fb4c743cd4f52e55d0bd8e46df1c
toptobes <kavinpg@gmail.com>
2022-07-28 13:50:15 -0500

    Added documentation for util classes

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/util/DataSupplier.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/util/DataSupplier.kt
@@ -1,3 +1,33 @@
 package org.firstinspires.ftc.teamcode.util
 
+/**
+ * An alias (a.k.a nickname) for a lamda (nameless function) that passes a Component as a parameter.
+ * Intended for telemetry functions where Component is the part (e.g. a
+ * [DcMotorEx][com.qualcomm.robotcore.hardware.DcMotorEx]) given to the lambda, and the output is
+ * the desired data to be logged to telemetry (e.g.
+ * [DcMotorEx.power][com.qualcomm.robotcore.hardware.DcMotorEx.getPower]).
+ *
+ * Essentially a macro; `DataSupplier<Component>` expands to `(Component) -> Any` in the actual code
+ *
+ * Example of usage; probably will not be used in your own code
+ * ```
+ * fun DriveMotors.logDriveMotorData(dataSupplier: DataSupplier<DcMotorEx>) {
+ *   telemetry.addData("Front-left motor:", dataSupplier(frontLeft))
+ *   telemetry.addData("Front-right motor:", dataSupplier(frontRight))
+ *   //...etc
+ * }
+ *
+ * fun main() {
+ *   val driveMotors = //...
+ *   logDriveMotorData(driveMotors) { it.power } // logs the drive motors' powers.
+ *   logDriveMotorData(driveMotors) { motor -> motor.velocity } // logs velocities.
+ *   // ^ either way works, though 'it' is more idiomatic for single param lambdas
+ * }
+ * ```
+ *
+ * __Honestly, just don't worry about this. I just added it to make some of the code cleaner and
+ * less repetitive__
+ *
+ * @param Component The part being passed into the data supplier function
+ */
 typealias DataSupplier<Component> = (Component) -> Any
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/util/LateinitVal.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/util/LateinitVal.kt
@@ -4,6 +4,36 @@ package org.firstinspires.ftc.teamcode.util
 
 import kotlin.reflect.KProperty
 
+/**
+ * A delegate class used to simulate a lateinit value; any variable that delegates to this may
+ * only be initialized once, but it may be initialized whenever it is desired.
+ *
+ * __NOTE:__ _This only works for class members,_ and is __Kotlin only__.
+ *
+ * Usage example:
+ * ```
+ * class IntegerThatCanOnlyBeSetOnce {
+ *   var int: Int by LateInitVal()
+ *   // or
+ *   var int by LateInitVal<Int>()
+ * }
+ *
+ * //...
+ *
+ * fun test1() {
+ *   val myInt = IntegerThatCanOnlyBeSetOnce()
+ *   myInt.int = 1 // this is fine
+ *   myInt.int = 2 // throws an exception; int has already been initialized
+ * }
+ *
+ * fun test2() {
+ *   val myInt = IntegerThatCanOnlyBeSetOnce()
+ *   println(myInt.int) // throws an exception; int is uninitialized
+ * }
+ * ```
+ *
+ * @param T The type of the value that is being delegated to.
+ */
 class LateInitVal<T> {
     private var value: Any? = Uninitialized
 
@@ -16,10 +46,14 @@ class LateInitVal<T> {
 
     operator fun setValue(thisRef: Any, property: KProperty<*>, _value: T) {
         if (value !is Uninitialized) {
-            throw IllegalStateException("Value is initialized")
+            throw IllegalStateException("Value is already initialized")
         }
         value = _value
     }
 }
 
+/**
+ * Object used to represent an uninitialized value. This is used to check if a [LateInitVal] is
+ * initialized or not.
+ */
 private object Uninitialized
