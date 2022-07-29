0f4bef5a457d41fe8dcfae25a3495c478015fc00
toptobes <kavinpg@gmail.com>
2022-07-28 20:35:59 -0500

    a

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/components/deadwheels/Deadwheel.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/components/deadwheels/Deadwheel.kt
@@ -1,7 +1,36 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package org.firstinspires.ftc.teamcode.components.deadwheels
 
 import com.qualcomm.robotcore.hardware.DcMotor
 
+/**
+ * Wrapper class around a Motor to provide dead-wheel functionality.
+ *
+ * Kotlin usage examples:
+ * ```
+ * fun main() {
+ *   val motor = initializedMotor("frontRight", hardwareMap)
+ *   val rightDeadwheel = Deadwheel(motor)
+ *   print(rightDeadwheel.ticks)
+ * }
+ * ```
+ *
+ * Java usage examples:
+ * ```
+ * public static void main(String... args) {
+ *   DcMotor motor = initializedMotor("frontRight", hardwareMap);
+ *   Deadwheel rightDeadwheel = new Deadwheel(motor);
+ *   System.out.print(rightDeadwheel.getTicks());
+ * }
+ * ```
+ *
+ * _Note: Both language examples produce the exact same objects_
+ *
+ * @property correspondingMotor The underlying motor that the deadwheel is connected to.
+ *
+ * @author KG
+ */
 class Deadwheel(
     private val correspondingMotor: DcMotor,
 ) {
@@ -11,6 +40,12 @@ class Deadwheel(
     var prevTicks = 0
         private set
 
+    val offset
+        get() = ticks - prevTicks
+
+    /**
+     * Snapshots the current value of `ticks`, should be called at the end of every `OpMode` loop.
+     */
     fun snapshotTicks() {
         prevTicks = correspondingMotor.currentPosition
     }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/components/deadwheels/Deadwheels.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/components/deadwheels/Deadwheels.kt
@@ -5,6 +5,39 @@ import org.firstinspires.ftc.teamcode.components.motors.DriveMotors
 import org.firstinspires.ftc.teamcode.util.DataSupplier
 import org.firstinspires.ftc.teamcode.util.LateInitVal
 
+/**
+ * The bot's drive motors logically grouped together.
+ *
+ * Kotlin usage example:
+ * ```
+ * fun main() {
+ *   val motors = initializedDriveMotors(hardwareMap)
+ *   val deadwheels = initializedDeadwheels(motors)
+ *   deadwheels.logData(telemetry) { it.ticks }
+ *   deadwheels.snapshotTicks()
+ * }
+ * ```
+ *
+ * Java usage example:
+ * ```
+ * public static void main(String... args) {
+ *   DriveMotors motors = initializedDriveMotors(hardwareMap);
+ *   Deadwheels deadwheels = DeadwheelsKt.initializedDeadwheels(motors);
+ *   deadwheels.logData(telemetry, Deadwheel::getTicks);
+ *   deadwheels.snapshotTicks();
+ * }
+ * ```
+ * _Note: Both language examples produce the exact same outputs_
+ *
+ * @property left The left deadwheel.
+ * @property right The right deadwheel.
+ * @property back The back deadwheel.
+ *
+ * @see [initializedDeadwheels]
+ * @see [DriveMotors]
+ *
+ * @author KG
+ */
 class Deadwheels {
     var left: Deadwheel by LateInitVal()
     var right: Deadwheel by LateInitVal()
@@ -16,6 +49,11 @@ class Deadwheels {
         telemetry.addData("Back wheel:", dataSupplier(back))
     }
 
+    /**
+     * Snapshots the current ticks of the deadwheels. Should be called at the end of every
+     * OpMode loop so that movement change can be tracked.
+     */
+    private object IgnoreMeIExistSoKDocsWillActuallyRenderTheDoc
     fun snapshotTicks() = this.run {
         left.snapshotTicks()
         right.snapshotTicks()
@@ -23,6 +61,25 @@ class Deadwheels {
     }
 }
 
+/**
+ * Initializes a [Deadwheels] object with the default configurations.
+ *
+ * Kotlin usage example:
+ * ```
+ * val Deadwheels = initializedDeadwheels(hardwareMap)
+ * ```
+ *
+ * Java usage example:
+ * ```
+ * Deadwheels deadwheels = DeadwheelsKt.initializedDeadwheels(hardwareMap)
+ * ```
+ *
+ * @param motors The [DriveMotors] object containing the corresponding motors
+ * @return A [Deadwheels] object with the motors initialized initialized.`
+ *
+ * @author KG
+ */
+private object IgnoreMeIExistSoKDocsWillActuallyRenderTheDoc
 fun initializedDeadwheels(motors: DriveMotors) = Deadwheels().apply {
     left = Deadwheel(motors.backRight)
     right = Deadwheel(motors.frontLeft)
