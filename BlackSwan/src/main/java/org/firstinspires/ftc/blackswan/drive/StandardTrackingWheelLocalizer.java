976c566783ac3ca10e65fe13c548c47e9f5383f9
BlockBeasts <kjosien1@gmail.com>
2022-07-29 22:47:04 -0600

    add roadrunner for black swan

---
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/drive/StandardTrackingWheelLocalizer.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.blackswan.drive;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.localization.ThreeTrackingWheelLocalizer;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.blackswan.util.Encoder;
+
+import java.util.Arrays;
+import java.util.List;
+
+/*
+ * Sample tracking wheel localizer implementation assuming the standard configuration:
+ *
+ *    /--------------\
+ *    |     ____     |
+ *    |     ----     |
+ *    | ||        || |
+ *    | ||        || |
+ *    |              |
+ *    |              |
+ *    \--------------/
+ *
+ */
+@Config
+public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
+  public static double TICKS_PER_REV = 8192;
+  public static double WHEEL_RADIUS = 0.69; // in
+  public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
+
+  public static double LATERAL_DISTANCE = 7.46; // in; distance between the left and right wheels
+  public static double FORWARD_OFFSET = 3.5; // in; offset of the lateral wheel
+
+  public static double X_MULTIPLIER =90/87.9;
+  public static double Y_MULTIPLIER =90/85;
+
+  private Encoder leftEncoder, rightEncoder, frontEncoder;
+
+  public StandardTrackingWheelLocalizer(HardwareMap hardwareMap) {
+    super(Arrays.asList(
+            new Pose2d(0, LATERAL_DISTANCE / 2, 0), // left
+            new Pose2d(0, -LATERAL_DISTANCE / 2, 0), // right
+            new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
+    ));
+
+    leftEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "backRight"));
+    rightEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "frontRight"));
+    frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "intake"));
+
+    leftEncoder.setDirection(Encoder.Direction.REVERSE);
+    rightEncoder.setDirection(Encoder.Direction.REVERSE);
+    frontEncoder.setDirection(Encoder.Direction.REVERSE);
+
+    // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+  }
+
+  public static double encoderTicksToInches(double ticks) {
+    return WHEEL_RADIUS * 2 * Math.PI * GEAR_RATIO * ticks / TICKS_PER_REV;
+  }
+
+  @NonNull
+  @Override
+  public List<Double> getWheelPositions() {
+    return Arrays.asList(
+            encoderTicksToInches(leftEncoder.getCurrentPosition())*X_MULTIPLIER,
+            encoderTicksToInches(rightEncoder.getCurrentPosition())*X_MULTIPLIER,
+            encoderTicksToInches(frontEncoder.getCurrentPosition())*Y_MULTIPLIER
+    );
+  }
+
+  @NonNull
+  @Override
+  public List<Double> getWheelVelocities() {
+    // TODO: If your encoder velocity can exceed 32767 counts / second (such as the REV Through Bore and other
+    //  competing magnetic encoders), change Encoder.getRawVelocity() to Encoder.getCorrectedVelocity() to enable a
+    //  compensation method
+
+    return Arrays.asList(
+            encoderTicksToInches(leftEncoder.getCorrectedVelocity())*X_MULTIPLIER,
+            encoderTicksToInches(rightEncoder.getCorrectedVelocity())*X_MULTIPLIER,
+            encoderTicksToInches(frontEncoder.getCorrectedVelocity())*Y_MULTIPLIER
+    );
+  }
+}
---
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/util/IMUstorage.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.blackswan.util;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class IMUstorage {
+    public static BNO055IMU imu;
+
+    private IMUstorage(HardwareMap hardwareMap){
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = null;//= new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+    }
+
+    static public BNO055IMU getImu(HardwareMap hardwareMap, Telemetry telemetry){
+        if (imu==null){
+            new IMUstorage(hardwareMap);
+
+        }
+
+        return imu;
+
+    }
+
+    static public BNO055IMU resetImu(HardwareMap hardwareMap){
+        new IMUstorage(hardwareMap);
+        return imu;
+    }
+
+}
