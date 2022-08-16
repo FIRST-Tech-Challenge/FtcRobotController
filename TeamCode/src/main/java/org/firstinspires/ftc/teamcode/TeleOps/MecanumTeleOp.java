e04d5f4c1980877253c77cf964ef1bfc2845c86b
MushiTea <94145946+MushiTea@users.noreply.github.com>
2022-08-16 00:34:14 -0500

    Switched back to Linear Opmode because apparently it's better idk atp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOps/MecanumTeleOp.java
@@ -5,9 +5,17 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "ChaoticMecanumTeleOp")
 public class MecanumTeleOp extends LinearOpMode {
+
+    // Declaring class members to be used in other methods
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotorEx motorFrontLeft, motorBackLeft, motorFrontRight, motorBackRight;
+    private Servo exampleThing1, exampleThing2;
+
     /**
      * Get the maximum absolute value from a static array of doubles
      * @param input the input array of double values
@@ -50,70 +58,71 @@ public class MecanumTeleOp extends LinearOpMode {
 
         waitForStart();
 
-        if (isStopRequested()) return;
-
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio, but only when
-            // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-
-            // Calculate the mecanum motor powers
-            double frontLeftPower = (y + x + 2 * rx) / denominator;
-            double backLeftPower = (y - x + 2 * rx) / denominator;
-            double frontRightPower = (y - x - 2 * rx) / denominator;
-            double backRightPower = (y + x - 2 * rx) / denominator;
-
-
-            // Cube the motor powers
-            frontLeftPower = Math.pow(frontLeftPower, 3);
-            frontRightPower = Math.pow(frontRightPower, 3);
-            backLeftPower = Math.pow(backLeftPower, 3);
-            backRightPower = Math.pow(backRightPower, 3);
-
-            // Calculate the maximum value of all the motor powers
-            // The argument here is just an array separated into different lines
-            double maxValue = getMax(new double[]{
-                    frontLeftPower,
-                    frontRightPower,
-                    backLeftPower,
-                    backRightPower
-            });
-
-
-            // Resize the motor power values
-            if (maxValue > 1) {
-                frontLeftPower /= maxValue;
-                frontRightPower /= maxValue;
-                backLeftPower /= maxValue;
-                backRightPower /= maxValue;
-            }
-
-            if (gamepad1.left_trigger > .5) {
-                motorFrontLeft.setPower(frontLeftPower * 0.35);
-                motorBackLeft.setPower(backLeftPower * 0.35);
-                motorFrontRight.setPower(frontRightPower * 0.35);
-                motorBackRight.setPower(backRightPower * 0.35);
-
-            } else {
-                motorFrontLeft.setPower(frontLeftPower);
-                motorBackLeft.setPower(backLeftPower);
-                motorFrontRight.setPower(frontRightPower);
-                motorBackRight.setPower(backRightPower);
-            }
+           drive();
 
 
 
 
+        }//end of "while(OpmodeIsActive)"
 
-            //end of "while(OpmodeIsActive)"
+    }// end of "runOpMode()"
+
+    public void drive(){
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
+
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+        // Calculate the mecanum motor powers
+        double frontLeftPower = (y + x + 2 * rx) / denominator;
+        double backLeftPower = (y - x + 2 * rx) / denominator;
+        double frontRightPower = (y - x - 2 * rx) / denominator;
+        double backRightPower = (y + x - 2 * rx) / denominator;
+
+
+        // Cube the motor powers
+        frontLeftPower = Math.pow(frontLeftPower, 3);
+        frontRightPower = Math.pow(frontRightPower, 3);
+        backLeftPower = Math.pow(backLeftPower, 3);
+        backRightPower = Math.pow(backRightPower, 3);
+
+        // Calculate the maximum value of all the motor powers
+        // The argument here is just an array separated into different lines
+        double maxValue = getMax(new double[]{
+                frontLeftPower,
+                frontRightPower,
+                backLeftPower,
+                backRightPower
+        });
+
+        // Resize the motor power values
+        if (maxValue > 1) {
+            frontLeftPower /= maxValue;
+            frontRightPower /= maxValue;
+            backLeftPower /= maxValue;
+            backRightPower /= maxValue;
         }
 
-      //end of "runOpMode()"
-    }
-    //end of class
-}
+        if (gamepad1.left_trigger > .5) {
+            motorFrontLeft.setPower(frontLeftPower * 0.3);
+            motorBackLeft.setPower(backLeftPower * 0.3);
+            motorFrontRight.setPower(frontRightPower * 0.3);
+            motorBackRight.setPower(backRightPower * 0.3);
+        }
+        else {
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+        }
+
+
+
+    } // end of drive()
+
+} // end of class
