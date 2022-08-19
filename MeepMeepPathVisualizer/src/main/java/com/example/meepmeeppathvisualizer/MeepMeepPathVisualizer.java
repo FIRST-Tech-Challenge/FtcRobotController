ecabe723b42c0ceda954732907f1f980d4dbc945
MushiTea <94145946+MushiTea@users.noreply.github.com>
2022-08-19 11:12:09 -0500

    Made an auto on meepmeep? I think. Stil haven't programmed an actual auto though, so we shall see ig.

--- MeepMeepPathVisualizer/src/main/java/com/example/meepmeeppathvisualizer/MeepMeepPathVisualizer.java
+++ MeepMeepPathVisualizer/src/main/java/com/example/meepmeeppathvisualizer/MeepMeepPathVisualizer.java
@@ -13,7 +13,7 @@ import java.util.Vector;
 public class MeepMeepPathVisualizer {
 
     public static void main(String[] args){
-        int numCycles = 10;
+        int numCycles = 3;
 
         MeepMeep MM = new MeepMeep(800,90);
         Pose2d startPose = new Pose2d(-12, -59, Math.toRadians(90)); // x, y, heading (angle in radians)
@@ -33,18 +33,29 @@ public class MeepMeepPathVisualizer {
                 .setColorScheme(new ColorSchemeRedDark())
 
                 // Dimensions of the bot
-                .setDimensions(13.5, 16)
+                .setDimensions(13, 16)
 
                 // () -> {} is the syntax for taking in a function, aka a lambda
+                // Assuming FRONT of the bot is the EXTAKE
                 .followTrajectorySequence(drive -> {
+                    int stepIncrement = 0;
                     TrajectorySequenceBuilder builder = drive.trajectorySequenceBuilder(startPose);
+                    builder.forward(15);
 
-                    for(int i = 0; i < numCycles; i++) {
-                        builder.forward(15);
-                        builder.waitSeconds(1.5);
-                        builder.lineToLinearHeading(new Pose2d(10,-63, Math.toRadians(0)));
-                        builder.forward(5);
+                    builder.waitSeconds(1.5);
+                    for (int i = 0; i < numCycles; i++) {
+                    builder.lineToLinearHeading(new Pose2d(10,-63.5, Math.toRadians(180)));
+                    builder.back(25 + stepIncrement);
+                    builder.waitSeconds(1);
+                    builder.forward(25 + stepIncrement);
+                    builder.setReversed(true);
+                    builder.lineToLinearHeading(new Pose2d(0,-42, Math.toRadians(125)));
+                    builder.waitSeconds(1.5);
+                    stepIncrement++;
                     }
+
+                    builder.setReversed(true);
+                    builder.splineToSplineHeading(new Pose2d(-59,-35), Math.toRadians(180));
 
                     return builder.build();
                 });
@@ -57,6 +68,6 @@ public class MeepMeepPathVisualizer {
                 .setBackgroundAlpha(1f)
                 .addEntity(bot)
                 .start();
-    }
+        }
 
     }
