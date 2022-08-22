4f02ba7d1f080e29172e3a4fbdcf0eca02f0dbc5
thort1 <44458087+thort1@users.noreply.github.com>
2022-08-22 12:42:43 -0700

    Constants tweaking.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DriveTest.java
@@ -18,35 +18,36 @@ import java.util.EnumMap;
 public class DriveTest extends OpMode {
 
     protected DriveSystem driveSystem;
-    private EnumMap<DriveSystem.MotorNames, DcMotor> driveMap;
+    private boolean turnComplete;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void init() {
         // Set up drive system
-        driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
+        EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
         for(DriveSystem.MotorNames name : DriveSystem.MotorNames.values()){
             driveMap.put(name, hardwareMap.get(DcMotor.class, name.toString()));
         }
         driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
         driveSystem.stopAndReset();
+        turnComplete = false;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void loop() {
         double maxPower = 0.3;
-        boolean driveComplete = false;
-        if (!driveComplete) {
-            driveComplete = driveSystem.driveToPosition(1000, DriveSystem.Direction.FORWARD, maxPower);
+        if (!turnComplete) {
+            //turnComplete = driveSystem.turn(45, maxPower);
+            turnComplete = driveSystem.turn(45, maxPower);
+            // Drive forward
+            // driveComplete = driveSystem.driveToPosition(1000, DriveSystem.Direction.FORWARD, maxPower);
+            // Strafe left
+            // driveSystem.driveToPosition(1000, DriveSystem.Direction.RIGHT, maxPower);
+            // Strafe right
+            // driveSystem.driveToPosition(1000, DriveSystem.Direction.RIGHT, maxPower);
+            // Drive backward
+            // driveSystem.driveToPosition(1000, DriveSystem.Direction.BACKWARD, maxPower);
         }
-        /*
-        // Strafe left
-        driveSystem.driveToPosition(1000, DriveSystem.Direction.LEFT, maxPower);
-        // Strafe right
-        driveSystem.driveToPosition(1000, DriveSystem.Direction.RIGHT, maxPower);
-        // Drive backward
-        driveSystem.driveToPosition(1000, DriveSystem.Direction.BACKWARD, maxPower);
-         */
     }
 }
