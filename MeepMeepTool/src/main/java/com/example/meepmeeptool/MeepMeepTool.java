431d00b9e4395b6dae466c5babd599bcc853f139
Jack Fetkovich <johnfetkovich1014@gmail.com>
2022-09-16 17:58:40 -0400

    add MeepMeep for path planning

---
+++ MeepMeepTool/src/main/java/com/example/meepmeeptool/MeepMeepTool.java
@@ -0,0 +1,34 @@
+package com.example.meepmeeptool;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTool {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(200, 200, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                .forward(50)
+                                .turn(Math.toRadians(180))
+                                .strafeLeft(50)
+                                .forward(15)
+                                .strafeTo(new Vector2d(25,65))
+                                .build()
+                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+
+}
