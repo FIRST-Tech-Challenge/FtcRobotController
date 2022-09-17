b393d4931908a77d4604fa63a52451ecf214b1b3
MostlyOperational18119 <ftc18119@gmail.com>
2022-09-17 12:37:25 -0700

    Update DriveMethods.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
@@ -1,5 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class DriveMethods {
-    
+    public void driveForDistance(double distanceMeters, double power) {
+        motor01.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor02.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor03.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor04.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        double distanceTraveled = 0;
+        int targetPos = (int) (distanceMeters * encoders * rotationsPerMeter);
+        motor01.setTargetPosition((targetPos));
+        motor02.setTargetPosition((targetPos));
+        motor03.setTargetPosition((targetPos));
+        motor04.setTargetPosition((targetPos));
+
+
+        motor01.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor02.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor03.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor04.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+        motor01.setPower(power);
+        motor02.setPower(power);
+        motor03.setPower(power);
+        motor04.setPower(power);
+        targetPos = motor01.getTargetPosition();
+        int currentPos = motor01.getCurrentPosition();
+        boolean hasNotReachedTarget = true;
+        while (hasNotReachedTarget) {
+            if(currentPos == targetPos){
+                hasNotReachedTarget = false;
+                motor01.setPower(0);
+                motor02.setPower(0);
+                motor03.setPower(0);
+                motor04.setPower(0);
+            }
+        }
+    }
 }
