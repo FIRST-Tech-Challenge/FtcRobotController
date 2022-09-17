1be5e0d96b1eb90adfb4a240da90d290b9009531
MostlyOperational18119 <ftc18119@gmail.com>
2022-09-17 12:42:26 -0700

    Update variables.java

--- TeamCode/src/main/java/variables.java
+++ TeamCode/src/main/java/variables.java
@@ -1,3 +1,10 @@
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class variables {
+
+    public DcMotor motor01;
+    public DcMotor motor02;
+    public DcMotor motor03;
+    public DcMotor motor04;
 
 }
