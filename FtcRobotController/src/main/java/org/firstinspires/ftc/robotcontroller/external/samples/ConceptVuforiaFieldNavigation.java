8a8578579574fc4023de454a90cd6b9890345dc2
BrendanFish <95725205+BrendanFish@users.noreply.github.com>
2022-09-17 14:03:06 -0400

    Localizer beginnings

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaFieldNavigation.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaFieldNavigation.java
@@ -75,7 +75,7 @@ import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocaliz
 
 @TeleOp(name="Vuforia Field Nav", group ="Concept")
 @Disabled
-public class ConceptVuforiaFieldNavigation extends LinearOpMode {
+public class  ConceptVuforiaFieldNavigation extends LinearOpMode {
 
     // IMPORTANT:  For Phone Camera, set 1) the camera source and 2) the orientation, based on how your phone is mounted:
     // 1) Camera Source.  Valid choices are:  BACK (behind screen) or FRONT (selfie side)
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Localizer.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Localizer {
+    public double y = 0;
+    public double x = 0;
+    public double angle = 0;
+
+    public void Localizer(){
+
+    }
+
+}
