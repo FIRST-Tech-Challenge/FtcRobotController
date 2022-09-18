7ef4766ef7f1cf5f419a2863fe9a7e709ca5b8e0
saa938 <83087832+saa938@users.noreply.github.com>
2022-09-18 10:23:09 -0700

    Update TurtleRobot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TurtleRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TurtleRobot.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode.robot;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
