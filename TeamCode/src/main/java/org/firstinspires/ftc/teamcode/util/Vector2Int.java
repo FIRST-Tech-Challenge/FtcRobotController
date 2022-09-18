0b08d3ac12195994e8af6116518d033bf55885c8
vman4d2 <vman4d2222@gmail.com>
2022-09-18 18:09:08 -0500

    Coordinate System Class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Vector2Int.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class Vector2Int {
+
+    public int x;
+    public int y;
+
+    public Vector2Int(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        return "(" + x + "," + y + ")";
+    }
+}
