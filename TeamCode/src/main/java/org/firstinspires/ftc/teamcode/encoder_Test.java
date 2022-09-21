051d8ff580578b4eb83fd1617a91c91a3f65fcb4
rodoggyro <Fiyrflash@gmail.com>
2022-09-21 19:00:30 -0400

    work in progress

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoder_Test.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+@TeleOp
+public class encoder_Test extends LinearOpMode {
+
+
+    private DcMotor crane;
+
+    public void runOpMode() {
+        crane = hardwareMap.get(DcMotor.class, "crane");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double throttle;
+            throttle = gamepad1.left_stick_y;
+
+            crane.setPower(throttle);
+            telemetry.addData("encoder value", crane.getCurrentPosition());
+            telemetry.update();
+        }
+    }
+}
+
+
