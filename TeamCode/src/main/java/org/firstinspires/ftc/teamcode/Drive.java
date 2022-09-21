474ae45f91658e96051150d47934d48b0cfd175a
Ritvik Gupta <86858242+R1tzG@users.noreply.github.com>
2022-09-21 13:54:21 -0400

    Create Drive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,32 @@
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+public class Drive {
+    DcMotor motorFrontLeft;
+    DcMotor motorBackLeft;
+    DcMotor motorFrontRight;
+    DcMotor motorBackRight;
+    public Drive(DcMotor getFrontLeft,
+                 DcMotor getBackLeft,
+                 DcMotor getFrontRight,
+                 DcMotor getBackRight) {
+        motorFrontLeft = getFrontLeft;
+        motorBackLeft = getBackLeft;
+        motorFrontRight = getFrontRight;
+        motorBackRight = getBackRight;
+    }
+
+    public void mecanum(double power, double strafe, double turn) {
+        double denominator = Math.max(Math.abs(power) + Math.abs(strafe) + Math.abs(turn), 1);
+        double frontLeftPower = (power + strafe + turn) / denominator;
+        double backLeftPower = (power - strafe + turn) / denominator;
+        double frontRightPower = (power - strafe - turn) / denominator;
+        double backRightPower = (power + strafe - turn) / denominator;
+
+        motorFrontLeft.setPower(frontLeftPower);
+        motorBackLeft.setPower(backLeftPower);
+        motorFrontRight.setPower(frontRightPower);
+        motorBackRight.setPower(backRightPower);
+    }
+}
