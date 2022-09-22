026ec813755356b545592adcde4e4800d2402993
cecilia512 <ceciliacskinner05@gmail.com>
2022-09-22 16:37:51 -0500

    byebye

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/BasicOmniOpMode_Linear.java
@@ -65,7 +65,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear Opmode")
 @Disabled
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+public class
+BasicOmniOpMode_Linear extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
