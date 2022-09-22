9d96256cdfbbfe39026c43371842851a60be8e88
Goldstormzy18 <91763425+Goldstormzy18@users.noreply.github.com>
2022-09-21 17:01:29 -0700

    sdfg

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -9,18 +9,54 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(300);
 
-        int scannerOutput = 0;
+        final int BLUE = 0;
+        final int RED = 1;
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+        final int TEAM1 = 0;
+        final int TEAM2 = 1;
+
+
+        int scannerOutput = 2;
+        RoadRunnerBotEntity myBot;
+
+
+
+
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, -61, Math.toRadians(90)))
-                                .forward(30)
+                switch(scannerOutput){
+                    case 1:
+                        myBot = new DefaultBotBuilder(meepMeep)
+                                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                                .followTrajectorySequence(drive ->
+                                        drive.trajectorySequenceBuilder(new Pose2d(calculateStartingX(), calculateStartingY(), calculateStartingRotation()))
+                                                .strafeRight(24)
+                                                .forward(50.6)
+                                                .strafeLeft(24)
+                                                .build()
+                                );
+                        break;
+                    case 2:
+                        myBot = new DefaultBotBuilder(meepMeep)
+                                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                                .followTrajectorySequence(drive ->
+                                        drive.trajectorySequenceBuilder(new Pose2d(calculateStartingX(), calculateStartingY(), calculateStartingRotation()))
+                                                .strafeLeft(24)
+                                                .forward(26.6)
+                                                .build()
+                                );
+                        break;
+                    default:
+                        myBot = new DefaultBotBuilder(meepMeep)
+                                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                                .followTrajectorySequence(drive ->
+                                        drive.trajectorySequenceBuilder(new Pose2d(calculateStartingX(), calculateStartingY(), calculateStartingRotation()))
+                                                .strafeRight(24)
+                                                .forward(26.6)
+                                                .build()
+                                );
+                        break;
+                }
 
-                                .build()
-
-                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
                 .setDarkMode(true)
@@ -28,4 +64,17 @@ public class MeepMeepTesting {
                 .addEntity(myBot)
                 .start();
     }
+
+    public static double calculateStartingX(){
+        return 62.6;
+    }
+    public static double calculateStartingY(){
+        return 36;
+    }
+    public static double calculateStartingRotation(){
+        return Math.toRadians(180);
+    }
+
+
+
 }
