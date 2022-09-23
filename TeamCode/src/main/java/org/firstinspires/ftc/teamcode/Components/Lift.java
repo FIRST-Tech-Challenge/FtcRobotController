685b80a334be5505b11b5cc4c209e1380c4bc75f
harry <harryberry71@gmail.com>
2022-09-22 22:18:20 -0700

    harry better version of empty functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Lift.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Components;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Lift {
+    private DcMotorEx liftMotor = null;
+    public Lift(){ //constructor
+        // hardware map
+    }
+    public enum liftStates{
+//        LIFT_HIGH_JUNCTION(0, "LIFT_HIGH_JUNCTION"),
+//        LIFT_MED_JUNCTION(0, "LIFT_MED_JUNCTION"),
+//        LIFT_LOW_JUNCTION(0, "LIFT_LOW_JUNCTION"),
+//        LIFT_GROUND_JUNCTION(0,"LIFT_GROUND_JUNCTION");
+//
+//        int goal;
+//        String name;
+//
+//        liftStates(int value, String name) {
+//            this.goal = value;
+//        }
+//
+//        public void setGoal(int goal) {
+//            this.goal = goal;
+//        }
+        //make enum for all the tick counts for ground low med high junctions, can set with setGoal(int goal);
+    }
+    public void liftToJunction(liftStates junctions){
+        //use rfmotor setPosition function to lift in accordance with the enum
+        // no conditions
+        // log when movement starts & when reach target position
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/clawExtension.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Components;
+
+public class clawExtension {
+    public clawExtension(){
+        // hardware mapping
+    }
+    public void extendClaw(){
+        //no input
+        //state of extendClaw has to be false(retracted state)
+        //set servo position
+        //set state of claw extended to true
+        //log when extension starts & when movement ends
+    }
+    public void retractClaw(){
+        //no input
+        //state of extendClaw has to be true(extended state)
+        //set servo position
+        //set state of claw extended to false
+        //log when extension starts & when movement ends
+    }
+}
