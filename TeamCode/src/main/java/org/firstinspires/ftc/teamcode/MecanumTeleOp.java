5dae8ebb0bd84fe92419a2bbab451ea5a48a1c26
david-man <86858242+R1tzG@users.noreply.github.com>
2022-09-24 13:31:19 -0400

    Finalized encoder test code and changed motor setup
    Co-Authored-By: david-man <86388290+david-man@users.noreply.github.com>
    Co-Authored-By: justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
    Co-Authored-By: Saaz Mahadkar <65580350+SaazM@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp.java
@@ -7,22 +7,39 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp
+@TeleOp(name="Drive")
 public class MecanumTeleOp extends LinearOpMode {
+    private final double inches_per_revolution = 60/25.4*Math.PI; //60 mm * (1 inches)/(25.4 mm) is the diameter of the wheel in inches, *pi for circumference
+    private final double ticks_per_revolution = 360*6.0; //6 ticks per degrees & 360 degrees per revolution
     @Override
     public void runOpMode() throws InterruptedException {
         // declare motors
         DcMotor motorFrontLeft = hardwareMap.dcMotor.get("frontLeft");
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD); //motor direction
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); //Braking behavior
+        motorFrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); //We don't want to use PID for the motors using the encoders
+
+
         DcMotor motorBackLeft = hardwareMap.dcMotor.get("backLeft");
+        motorBackLeft.setDirection(DcMotor.Direction.FORWARD);
+        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset encoder values
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
         DcMotor motorFrontRight = hardwareMap.dcMotor.get("frontRight");
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
         DcMotor motorBackRight = hardwareMap.dcMotor.get("backRight");
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-//        motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        motorBackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        // reverse right motors
-        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         Drive drive = new Drive(motorFrontLeft, motorBackLeft, motorFrontRight, motorBackRight);
         //Odometry odometry = new Odometry(leftEncoder, rightEncoder, perpendicularEncoder);
@@ -31,27 +48,20 @@ public class MecanumTeleOp extends LinearOpMode {
 
         if (isStopRequested()) return;
 
-
+        
         while (opModeIsActive()) {
             double power = gamepad1.left_stick_y/2; // Remember, this is reversed!
             double strafe = -gamepad1.left_stick_x * 1.1/2; // Counteract imperfect strafing
-            double turn = -gamepad1.right_stick_x/2;
-            
-//            double power = gamepad2.left_stick_y/3; // Remember, this is reversed!
-//            double strafe = -gamepad2.left_stick_x * 1.1/3; // Counteract imperfect strafing
-//            double turn = -gamepad2.right_stick_x/3;
-            if(gamepad1.right_trigger==1){
-                motorBackLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                motorBackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            }
+            double turn = gamepad1.right_stick_x/2;
 
             drive.mecanum(power, strafe, turn);
 
-            if(power > 0.1) {
+            if (power > 0.1) {
                 gamepad1.rumble(200);
             }
 
-            telemetry.addData("Back_Left Encoder: ", motorBackLeft.getCurrentPosition()/7.06858347058);
+            //7.06858347058
+            telemetry.addData("Back_Left Encoder: ", motorBackLeft.getCurrentPosition()/ticks_per_revolution*inches_per_revolution); //Converting encoder units to inches
             telemetry.addData("Power: ", power);
             telemetry.update();
         }
