274ed6a34b9e989286d2b5b592928d86713e333b
saa938 <83087832+saa938@users.noreply.github.com>
2022-09-25 11:42:43 -0700

    moved files
    Co-Authored-By: nikhilmantrala <61946870+nikhilmantrala@users.noreply.github.com>
    Co-Authored-By: saa938 <83087832+saa938@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/camera.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.opmodes.teleop;
+package org.firstinspires.ftc.teamcode.opmodes.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
