46da535c449d6cb89ec195a69222bf8512ff1b72
Fiyrflash <113477109+Fiyrflash@users.noreply.github.com>
2022-09-25 17:05:37 -0400

    added encoder_for1foot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous/encoder_for1foot.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.Autonmous;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@Autonomous
+public class encoder_for1foot extends LinearOpMode {
+
+    private DcMotor crane;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public void runOpMode() {
+        crane = hardwareMap.get(DcMotor.class, "crane");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        waitForStart();
+
+        if (opModeIsActive()) {
+
+            telemetry.addData("fR", frontRight.getCurrentPosition());
+            telemetry.addData("fL", frontLeft.getCurrentPosition());
+            telemetry.addData("BL", backLeft.getCurrentPosition());
+            telemetry.addData("BR", backRight.getCurrentPosition());
+            telemetry.update();
+
+
+            move(1,720);
+
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backRight.setPower(0);
+            backLeft.setPower(0);
+        }
+    }
+
+
+    public void crane(double power,int time){
+        crane.setPower(power);
+        sleep(time);
+        crane.setPower(0);
+    }
+
+
+    public void move(double power, int position){
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontRight.setTargetPosition(-position);
+        frontLeft.setTargetPosition(-position);
+        backRight.setTargetPosition(-position);
+        backLeft.setTargetPosition(-position);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
+
+        while (frontLeft.isBusy()){
+
+        }
+
+
+    }
+}
