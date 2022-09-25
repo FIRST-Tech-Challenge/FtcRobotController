799302a9aa123c453b3b205b886a4dbb88176622
Fiyrflash <113477109+Fiyrflash@users.noreply.github.com>
2022-09-25 16:58:44 -0400

    created autonmous folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous/encoder_auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonmous/encoder_auto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autonmous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
