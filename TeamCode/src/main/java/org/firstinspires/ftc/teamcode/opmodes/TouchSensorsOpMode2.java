9981d2d8e919890dd4ce590df05a65e0576d00d0
anmat24 <34289255+anmat24@users.noreply.github.com>
2022-09-29 16:59:01 -0500

    edited

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TouchSensorsOpMode2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TouchSensorsOpMode2.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard2;
 
