a35f5d75d736b423ba9c7a2375d3cdea2d8b1cae
rodoggyro <Fiyrflash@gmail.com>
2022-09-29 16:45:06 -0400

    fix folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoder_for1foot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/encoder_for1foot.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonmous;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
