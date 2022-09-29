6e690aab063e07eee6cf25725e4023192f3cc6fe
anmat24 <34289255+anmat24@users.noreply.github.com>
2022-09-29 16:53:00 -0500

    Touch Sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes.TouchSensorsOpMode2.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.mechanisms.ProgrammingBoard2;
+
+@TeleOp()
+public class TouchSensorOpMode2 extends OpMode {
+    ProgrammingBoard2 board = new ProgrammingBoard2();
+    @Override
+    public void init() {
+        board.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Touch pressed", board.isTouchSensorPressed());
+    }
+}
