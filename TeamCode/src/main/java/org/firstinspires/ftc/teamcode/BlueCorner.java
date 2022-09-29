0632248618b06e287a61fe8babbda9a97ebfe29a
stenzelkhai <stenzelkhai@gmail.com>
2022-09-28 19:03:48 -0700

    refactored some classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCorner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BlueCorner.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTrauma.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTrauma.java
@@ -1,3 +1,5 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
