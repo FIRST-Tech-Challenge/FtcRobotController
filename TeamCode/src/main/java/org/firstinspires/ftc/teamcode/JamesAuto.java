1ca268dd97cedb285ec39b710035bbf8a95f92f1
BetheBob1 <BetheBob082208@gmail.com>
2022-09-30 17:42:03 -0700

    made my own code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JamesAuto.java
@@ -0,0 +1,103 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.DriveMethods;
+import static org.firstinspires.ftc.teamcode.Variables.*;
+
+@Autonomous(name="JamesAuto", group="B")
+public class JamesAuto extends DriveMethods {
+    @Override
+
+    public void runOpMode() {
+
+        waitForStart();
+
+        driveForTime(10,1,Direction.FORWARD);
+        driveForTime(10,1,Direction.BACKWARD);
+        driveForTime(10,1,Direction.LEFT);
+        driveForTime(10,1,Direction.RIGHT);
+
+        initMotorsBlue();
+
+        while (opModeIsActive()) {
+
+        }
+
+    }
+    public enum Direction {
+        FORWARD,
+        BACKWARD,
+        ROTATE_LEFT,
+        ROTATE_RIGHT,
+        RIGHT,
+        LEFT,
+
+    }
+
+    public void driveDirection(Direction direction, double power){
+        switch (direction) {
+            case FORWARD:
+                motorFL.setPower(power);
+                motorBL.setPower(power);
+                motorFR.setPower(power);
+                motorBR.setPower(power);
+                break;
+            case BACKWARD:
+                motorFL.setPower(-power);
+                motorBL.setPower(-power);
+                motorFR.setPower(-power);
+                motorBR.setPower(-power);
+                break;
+            case RIGHT:
+                motorFL.setPower(power);
+                motorBL.setPower(-power);
+                motorFR.setPower(-power);
+                motorBR.setPower(power);
+                break;
+            case LEFT:
+                motorFL.setPower(-power);
+                motorBL.setPower(power);
+                motorFR.setPower(power);
+                motorBR.setPower(-power);
+                break;
+            case ROTATE_LEFT:
+                motorFL.setPower(-power);
+                motorBL.setPower(-power);
+                motorFR.setPower(power);
+                motorBR.setPower(power);
+                break;
+            case ROTATE_RIGHT:
+                motorFL.setPower(power);
+                motorBL.setPower(power);
+                motorFR.setPower(-power);
+                motorBR.setPower(-power);
+                break;
+
+        }
+
+
+    }
+
+    public void driveForTime(int seconds, double power, Direction direction){
+        //Fl is 0
+        //Bl is 1
+        //Br is 2
+        //Fr is 3
+        driveDirection(direction,power);
+
+        int mili = seconds*1000;
+        sleep(mili);
+
+        motorFL.setPower(0);
+        motorBL.setPower(0);
+        motorFR.setPower(0);
+        motorBR.setPower(0;
+    }
+}
