2016bddfca650752de5f53fc497ab263199ff7ca
William.Lu <3326007521@qq.com>
2022-10-01 12:14:59 -0500

    add HardwareMap to teamcode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
