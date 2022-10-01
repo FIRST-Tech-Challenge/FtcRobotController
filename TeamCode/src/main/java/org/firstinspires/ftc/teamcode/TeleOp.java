ec04d5dfa115e1c77cffb18f774df67f5f63fd06
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2022-10-01 13:09:48 -0400

    Completed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -3,25 +3,31 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.util.Localizer;
+import org.firstinspires.ftc.teamcode.util.MecanumDriveBase;
 
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="TeleOpStub", group="Basic drive")
 public class TeleOp extends OpMode
 
 {
-    private Localizer localizer = null;
+    private Localizer localizer        = null;
+    private MecanumDriveBase mecanumDriveBase = null;
     public void init() {
         telemetry.addData("Status", "Initialized");
         localizer = new Localizer(hardwareMap);
+        mecanumDriveBase = new MecanumDriveBase(hardwareMap);
         double max;
     }
     @Override
     public void loop() {
         localizer.displayTelemetry(telemetry);
         localizer.handleTracking();
+        mecanumDriveBase.gamepadController(gamepad1);
+        mecanumDriveBase.driveBaseTelemetry(telemetry);
         telemetry.update();
     }
 }
