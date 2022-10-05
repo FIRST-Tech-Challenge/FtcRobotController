9c7d28a38c0325bc6d68f19c8798b2b45edae7cf
Jesse Shieh <SJJCoding@gmail.com>
2022-10-04 18:38:36 -0700

    Made a tele-opmode

--- Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TeleOpCompetition.java
+++ Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TeleOpCompetition.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.team417_PowerPlay;
 
 public class TeleOpCompetition extends BaseTeleOp{
-
     @Override
     public void runOpMode() throws InterruptedException {
 
--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/TeleOpTest.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/TeleOpTest.java
@@ -3,6 +3,12 @@ package org.firstinspires.ftc.team6220_PowerPlay;
 public class TeleOpTest extends BaseTeleOp {
     @Override
     public void runOpMode() throws InterruptedException {
-
+        Initialize();
+        telemetry.addLine("Waiting for start :)");
+        telemetry.update();
+        waitForStart();
+        while(opModeIsActive()){
+            TeleOpDrive();
+        }
     }
 }
