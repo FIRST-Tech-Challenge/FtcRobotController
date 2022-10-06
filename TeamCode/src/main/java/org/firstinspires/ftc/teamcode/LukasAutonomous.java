f04349ef977f664ea61edc7bc011432ab596a685
MostlyOperational18119 <ftc18119@gmail.com>
2022-10-05 18:33:34 -0700

    Update LukasAutonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LukasAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LukasAutonomous.java
@@ -1,32 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import static org.firstinspires.ftc.teamcode.Variables.*;
+
+import android.graphics.drawable.GradientDrawable;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
 
 @Autonomous(name="LukasAuto", group = "A")
 
 public class LukasAutonomous extends DriveMethods {
 
 
+    boolean imuIsCalibrated = false;
+    double previousHeading = 0;
+    double integratedHeading = 0;
+    BNO055IMU imu;
+
     @Override
     public void runOpMode() {
 
-        initMotorsBlue();
 
         waitForStart();
 
-        driveForTime(5,1,Direction.FORWARD);
-        driveForTime(5,1,Direction.BACKWARD);
 
-
-
-        while(opModeIsActive()){
+        while (opModeIsActive()) {
 
         }
     }
 
 
-    public enum Direction{
+    public enum Direction {
         FORWARD,
         BACKWARD,
         ROTATE_LEFT,
@@ -35,8 +45,8 @@ public class LukasAutonomous extends DriveMethods {
         LEFT,
     }
 
-    public void driveDirection(Direction direction, double power){
-        switch(direction){
+    public void driveDirection(Direction direction, double power) {
+        switch (direction) {
             case FORWARD:
                 motorFL.setPower(power);
                 motorBL.setPower(power);
@@ -81,15 +91,14 @@ public class LukasAutonomous extends DriveMethods {
         }
 
 
-
     }
 
 
-    public void driveForTime (int seconds, double power, Direction direction){ // seconds: 10, power: 1, direction: FORWARD
+    public void driveForTime(int seconds, double power, Direction direction) { // seconds: 10, power: 1, direction: FORWARD
 
         driveDirection(direction, power);
 
-        sleep(seconds*1000);
+        sleep(seconds * 1000);
 
         motorFL.setPower(0);
         motorBL.setPower(0);
@@ -97,4 +106,60 @@ public class LukasAutonomous extends DriveMethods {
         motorBR.setPower(0);
 
     }
+
+
+    public void calibrateIMU() {
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        while(!isStopRequested() && !imu.isGyroCalibrated()){
+            sleep(50);
+            idle();
+        }
+
+        imuIsCalibrated = true;
+
+    }
+
+    public double getCurrentZ() {
+        if(imuIsCalibrated == false){
+            calibrateIMU();
+        }
+
+        Orientation currentAngle = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZXY, AngleUnit.DEGREES);
+        double currentZ = currentAngle.firstAngle;
+        return currentZ;
+
+    }
+
+    public double getCumulativeZ(){
+        double currentHeading = getCurrentZ();
+        double deltaHeading = currentHeading - previousHeading;
+
+        if(deltaHeading <= -180){
+            deltaHeading +=360;
+        }else if( deltaHeading >= 180){
+            deltaHeading -= 360;
+        }
+
+        integratedHeading += deltaHeading;
+        previousHeading = currentHeading;
+
+        return  integratedHeading;
+    }
 }
+
