1faed335a24a2c425b265629a17acf25f160544a
Jesse Shieh <SJJCoding@gmail.com>
2022-10-06 11:08:10 -0700

    More constants

---
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseAuto.java
@@ -0,0 +1,5 @@
+package org.firstinspires.ftc.team6220_PowerPlay;
+
+abstract public class BaseAuto extends BaseOpMode {
+    //Gaming goes here
+}
--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
@@ -2,4 +2,8 @@ package org.firstinspires.ftc.team6220_PowerPlay.ResourceClasses;
 
 public class Constants {
     public double WheelDiam = 3.73;
+    public double WheelCirc = 11.71;
+    public double TicksPerRev = 537.6;
+    public double inchesPerTick = 0.0217;
+    public double HoloAngleRotationsToInches = 3.73 / Math.sqrt(2);
 }
