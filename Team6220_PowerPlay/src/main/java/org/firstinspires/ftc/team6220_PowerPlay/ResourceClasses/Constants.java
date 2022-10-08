e4e33e1b691713a0116b4853f159155a019b1f75
Jesse Shieh <SJJCoding@gmail.com>
2022-10-07 19:42:51 -0700

    Derived constants from wheel diam

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
@@ -2,8 +2,8 @@ package org.firstinspires.ftc.team6220_PowerPlay.ResourceClasses;
 
 public class Constants {
     public double WheelDiam = 3.73;
-    public double WheelCirc = 11.71;
-    public double TicksPerRev = 537.6;
-    public double inchesPerTick = 0.0217;
-    public double HoloAngleRotationsToInches = 3.73 / Math.sqrt(2);
+    public double WheelCirc = WheelDiam * Math.PI;
+    public double TicksPerRev = 14.147 * 2 * 19;
+    public double inchesPerTick = WheelCirc / TicksPerRev;
+    public double HoloAngleRotationsToInches = WheelCirc / Math.sqrt(2);
 }
