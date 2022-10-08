67653ad959bc8e883c62efbe47005f17b6c371bf
Jesse Shieh <SJJCoding@gmail.com>
2022-10-07 19:34:14 -0700

    Removed changes to 417's opmode (not sure how it got there) and made more detailed comments in baseTeleOp

--- Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TeleOpCompetition.java
+++ Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TeleOpCompetition.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.team417_PowerPlay;
 
 public class TeleOpCompetition extends BaseTeleOp{
+
     @Override
     public void runOpMode() throws InterruptedException {
 
--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
@@ -7,14 +7,15 @@ import java.util.logging.Filter;
 
 abstract public class BaseTeleOp extends BaseOpMode {
     public Float[] GamePadInputAVG() {
-        //Initialization for all variables
+        //Initialization for all variables, the stack type is used here for easy adding and removing from the front and back of the list.
         Stack<Float> XAVGPos_LS = new Stack<Float>();
         Stack<Float> YAVGPos_LS = new Stack<Float>();
         Stack<Float> XAVGPos_RS = new Stack<Float>();
         Stack<Float> YAVGPos_RS = new Stack<Float>();
         Float[] AllStickAVGS = {null, null, null, null};
 
-        //Scuffed af
+        //This pushes the last recorded stick position if the stack size is below 5.
+        //It removes the last element and pushes the last recorded stick position is the stack size is above 5
         if (XAVGPos_LS.size() < 5) {
             XAVGPos_LS.push(gamepad1.left_stick_x);
             YAVGPos_LS.push(gamepad1.left_stick_y);
@@ -25,8 +26,12 @@ abstract public class BaseTeleOp extends BaseOpMode {
             YAVGPos_LS.pop();
             XAVGPos_RS.pop();
             YAVGPos_RS.pop();
+            XAVGPos_LS.push(gamepad1.left_stick_x);
+            YAVGPos_LS.push(gamepad1.left_stick_y);
+            XAVGPos_RS.push(gamepad1.right_stick_x);
+            YAVGPos_RS.push(gamepad1.right_stick_y);
         }
-        //Actual averaging, still kinda sus
+        //Loops through the stack and outputs a list of averaged x and y stick positions
         for(int iterator = 0; iterator < XAVGPos_LS.size(); iterator++){
             AllStickAVGS[0] = (XAVGPos_LS.get(iterator) + AllStickAVGS[0])/iterator;
             AllStickAVGS[1] = (YAVGPos_LS.get(iterator) + AllStickAVGS[1])/iterator;
@@ -39,7 +44,7 @@ abstract public class BaseTeleOp extends BaseOpMode {
     public void TeleOpDrive () {
         Float[] FilteredInput = GamePadInputAVG();
 
-        //Stick deadzones math:
+        //Stick deadzones:
         //FilteredInput[0] = Left stick X pos
         //FilteredInput[1] = Left stick Y pos
         //FilteredInput[2] = Right stick X pos
