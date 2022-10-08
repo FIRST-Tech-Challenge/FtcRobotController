2a6cf7ab41f9d57cfa69af6cbced1d1de600933d
kingKedSha <kedaar.s@gmail.com>
2022-10-08 17:00:12 -0400

    cleaned up auton code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton.java
@@ -1,127 +1,103 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 // PSEUDOCODE
 public class Auton {
 
-    private int parkingZone;
+    private final int parkingZone;
 
-    public Auton(int parkingZone) {
-
-        this.parkingZone = parkingZone;
+    @Override
+    public Auton(int pz) {
+        this.parkingZone = pz;
     }
 
-    public void runAutonRight1() {
+    public void runAutonRight() {
 
-        new TrajectoryBuilder(new Pose2d())
-            .forward(42)
-            .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(-45)))
-            .lineToLinearHeading(new Pose2d(0, 36, Math.toRadians(0)))
-            .forward(6)
-            .build()
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(0, 0, 0);
+        drive.setPoseEstimate(startPose);
 
-        placeFirstCone();
+        TrajectorySequence trajSeq = drive.TrajectorySequenceBuilder(startPose)
+                .forward(42)
+                .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(45)))
+                .addDisplacementMarker(() -> {
+                    //first cone placement
+                })
+                .lineToLinearHeading(new Pose2d(0, 36, Math.toRadians(0)))
+                .forward(6)
+                .build();
+
+        drive.followTrajectorySequence(trajSeq);
 
         if (parkingZone == 1) {
-            new TrajectoryBuilder(new Pose2d())
-                .strafeLeft(24)
-                .build
+            Trajectory park = drive.trajectoryBuilder(startPose)
+                    .strafeLeft(24)
+                    .build();
+            drive.followTrajectory(park);
         } else if (parkingZone == 3) {
-            new TrajectoryBuilder(new Pose2d())
-                .strafeRight(24)
-                .build
+            Trajectory park = drive.trajectoryBuilder(startPose)
+                    .strafeRight(24)
+                    .build();
+            drive.followTrajectory(park);
         }
     }
 
-    public void runAutonLeft1() {
-        new TrajectoryBuilder(new Pose2d())
-            .forward(42)
-            .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(-45)))
-            .lineToLinearHeading(new Pose2d(0, 36, Math.toRadians(0)))
-            .forward(6)
-            .build()
+    public void runAutonLeft() {
 
-        placeFirstCone();
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Pose2d startPose = new Pose2d(0, 0, 0);
+        drive.setPoseEstimate(startPose);
 
-        if (parkingZone == 1) {
-            new TrajectoryBuilder(new Pose2d())
-                .strafeRight(24)
-                .build()
-
-        } else if (parkingZone == 3) {
-            new TrajectoryBuilder(new Pose2d())
-                .strafeLeft(24)
-                .build()
-        }
-    }
-
-    public void runAutonRight2() {
-        new TrajectoryBuilder(new Pose2d())
-            .forward(42)
-            .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(45)))
-            .build()
-
-        placeFirstCone();
-
-        new TrajectoryBuilder(new Pose2d())
-            .backward(6)
-            .lineToLinearHeading(new Pose2d(24, 48, Math.toRadians(90)))
-            .build
-
-        pickUpSecondCone();
-
-        new TrajectoryBuilder(new Pose2d())
-            .lineToLinearHeading(new Pose2d(24, 42, Math.toRadians(0)))
-            .lineToLinearHeading(new Pose2d(24, 24, Math.toRadians(45)))
-            .build
-
-        placeSecondCone();
-
-        new TrajectoryBuilder(new Pose2d(24, 30, Math.toRadians(-90)))
-            .lineToLinearHeading(new)
-            .strafeLeft(18)
-            .build
-
-        if(parkingZone == 1) {
-            new TrajectoryBuilder(new Pose2d())
-                .strafeLeft(24)
-                .build
-        }
-    }
-
-    public void runAutonLeft2() {
-        new TrajectoryBuilder(new Pose2d())
+        TrajectorySequence trajSeq = drive.TrajectorySequenceBuilder(startPose)
                 .forward(42)
                 .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(-45)))
-                .build()
+                .addDisplacementMarker(() -> {
+                    //first cone placement
+                })
+                .lineToLinearHeading(new Pose2d(0, 36, Math.toRadians(0)))
+                .forward(6)
+                .build();
+
+        drive.followTrajectorySequence(trajSeq);
+
+        if (parkingZone == 1) {
+            Trajectory park = drive.trajectoryBuilder(startPose)
+                    .strafeLeft(24)
+                    .build();
+            drive.followTrajectory(park);
+        } else if (parkingZone == 3) {
+            Trajectory park = drive.trajectoryBuilder(startPose)
+                    .strafeRight(24)
+                    .build();
+            drive.followTrajectory(park);
+        }
+    }
+
+    public void runAutoasdfasdfnLeft() {
+        new TrajectoryBuilder(new Pose2d())
+            .forward(42)
+            .lineToLinearHeading(new Pose2d(0, 48, Math.toRadians(-45)))
+            .lineToLinearHeading(new Pose2d(0, 36, Math.toRadians(0)))
+            .forward(6)
+            .build();
 
         placeFirstCone();
 
-        new TrajectoryBuilder(new Pose2d())
-                .backward(6)
-                .lineToLinearHeading(new Pose2d(-24, 48, Math.toRadians(-90)))
-                .build
-
-        pickUpSecondCone();
-
-        new TrajectoryBuilder(new Pose2d())
-                .lineToLinearHeading(new Pose2d(-24, 42, Math.toRadians(0)))
-                .lineToLinearHeading(new Pose2d(-24, 24, Math.toRadians(45)))
-                .build
-
-        placeSecondCone();
-
-        new TrajectoryBuilder(new Pose2d(-24, 30, Math.toRadians(90)))
-                .lineToLinearHeading(new)
-                .strafeLeft(18)
-                .build
-
-        if(parkingZone == 3) {
+        if (parkingZone == 1) {
             new TrajectoryBuilder(new Pose2d())
-                    .strafeRight(24)
-                    .build
+                .strafeRight(24)
+                .build();
+
+        } else if (parkingZone == 3) {
+            new TrajectoryBuilder(new Pose2d())
+                .strafeLeft(24)
+                .build();
         }
     }
 
