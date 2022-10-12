5b52fbfe272cbd5a64ab955beee350b1a8fe6a1d
0zgeada <uzmanoa@gmail.com>
2022-10-12 18:15:08 -0400

    created turret class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Turret.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+public class Turret {
+    public Telemetry telemetry;
+    public DcMotorEx turretMotor;
+}
