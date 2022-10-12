1c0db239f750893329f6712c80127f52e526b200
Gavin <gmacgaming40@gmail.com>
2022-10-11 19:34:05 -0700

    Updated constant values to be correct.

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/ResourceClasses/Constants.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.team6220_PowerPlay.ResourceClasses;
 
 public class Constants {
-    public double WheelDiam = 3.73;
+    public double WheelDiam = 3.78; //96mm = 3.78in
     public double WheelCirc = WheelDiam * Math.PI;
-    public double TicksPerRev = 14.147 * 2 * 19;
+    public double TicksPerRev = 28 * 19.2;
     public double inchesPerTick = WheelCirc / TicksPerRev;
     public double HoloAngleIncherPerRot = WheelCirc / Math.sqrt(2);
 }
