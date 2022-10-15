ab67cf3294f6b1aeaa963fb2ba972074a3810260
MostlyOperational18119 <ftc18119@gmail.com>
2022-10-14 18:04:49 -0700

    Created first (working) meet 0 teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeetZeroTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MeetZeroTeleop.java
@@ -5,12 +5,15 @@ import static org.firstinspires.ftc.teamcode.Variables.motorBR;
 import static org.firstinspires.ftc.teamcode.Variables.motorFL;
 import static org.firstinspires.ftc.teamcode.Variables.motorFR;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp(name="Meet0Teleop", group = "A")
 public class MeetZeroTeleop extends LinearOpMode {
     @Override
     public void runOpMode() {
@@ -27,7 +30,7 @@ public class MeetZeroTeleop extends LinearOpMode {
         servoGrabberThing = hardwareMap.get(Servo.class, "grabber");
 
         motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
         motorFR.setDirection(DcMotorSimple.Direction.FORWARD);
         motorBR.setDirection(DcMotorSimple.Direction.FORWARD);
 
@@ -37,6 +40,8 @@ public class MeetZeroTeleop extends LinearOpMode {
         double leftX;
         double rightX;
         double speedDiv = 1;
+        double clampPosition = 0.76;
+        double releasePosition =0.66;
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
@@ -46,12 +51,29 @@ public class MeetZeroTeleop extends LinearOpMode {
             rightX = gamepad1.right_stick_x;
 
             motorLinearSlide.setPower(gamepad2.left_stick_y/6);
+            if(gamepad2.dpad_up){
+                clampPosition = clampPosition + 0.03;
+                sleep(200);
+            }
+            if(gamepad2.dpad_down){
+                clampPosition = clampPosition - 0.03;
+                sleep(200);
+            }
+            if(gamepad2.dpad_right){
+                releasePosition = releasePosition + 0.03;
+                sleep(200);
+            }
+            if(gamepad2.dpad_left){
+                releasePosition = releasePosition - 0.03;
+                sleep(200);
+            }
+
             if (gamepad2.x) {
-                servoGrabberThing.setPosition(.76);
+                servoGrabberThing.setPosition(clampPosition);
 
             }
             if (gamepad2.a) {
-                servoGrabberThing.setPosition(.66);
+                servoGrabberThing.setPosition(releasePosition);
             }
 
             if (!gamepad1.right_bumper) {
@@ -76,6 +98,8 @@ public class MeetZeroTeleop extends LinearOpMode {
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Motors", "left (%.2f), right (%.2f)");
+            telemetry.addLine("ClampPositoin: " + clampPosition);
+            telemetry.addLine("ReleasePosition: " + releasePosition);
             telemetry.update();
         }
     }
