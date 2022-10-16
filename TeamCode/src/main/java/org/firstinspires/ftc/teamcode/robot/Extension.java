256755e3ea55f2f29e97f67f0b6ce7a72fd1b4fb
0zgeada <uzmanoa@gmail.com>
2022-10-16 16:59:33 -0400

    added methods to extension class
    added a method for extending and another for retracting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Extension.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Extension.java
@@ -9,11 +9,12 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 public class Extension {
     private Telemetry telemetry;
-    public DcMotorEx extensionMotor;
+    public Servo extensionServo;
 
     static final double MM_TO_INCHES = 0.0393700787;
 
@@ -23,5 +24,36 @@ public class Extension {
     static final double MINIMUM_CLEARANCE_DISTANCE = 95.875 * MM_TO_INCHES;
 
     public final double EXTENSION_POSITION_RESET = 0;
-    //public final double EXTENSION_POSITION_MAXIMUM = inches of maximum extend
+    public final int MAXIMUM_EXTEND_VALUE = 2520;
+
+    public Extension(HardwareMap hwMap, Telemetry telemetry) {
+        this.telemetry = telemetry;
+        extensionServo = (Servo) hwMap.servo.get("Extension");
+    }
+
+    //speed is the value coming from controller
+    public void Extend(double speed){
+        double currentPosition;
+        currentPosition = extensionServo.getPosition();
+
+        //moving in increments of 250
+        double targetPosition = currentPosition + speed*250;
+        if (targetPosition > MAXIMUM_EXTEND_VALUE)
+            targetPosition = MAXIMUM_EXTEND_VALUE;
+
+        extensionServo.setPosition(targetPosition/MAXIMUM_EXTEND_VALUE);
+    }
+
+    //speed is the value coming from controller
+    public void Retract (double speed) {
+        double currentPosition;
+        currentPosition = extensionServo.getPosition();
+
+        //moving in increments of 250
+        double targetPosition = currentPosition - speed*250;
+        if (targetPosition < 0)
+            targetPosition = 0;
+        
+        extensionServo.setPosition(targetPosition/MAXIMUM_EXTEND_VALUE);
+    }
 }
