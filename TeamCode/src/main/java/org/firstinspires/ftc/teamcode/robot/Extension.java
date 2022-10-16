aedc1bffb0fe45bb6571adafe4c4f6105c259ff2
0zgeada <uzmanoa@gmail.com>
2022-10-16 14:02:38 -0400

    created Extension class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Extension.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+
+public class Extension {
+    private Telemetry telemetry;
+    public DcMotorEx extensionMotor;
+
+    static final double     MM_TO_INCHES = 0.0393700787;
+}
