4aaaf16e7b71e7d0f5e5edecd98ffae5bbe2beb4
sipandrei <pontos.serban@gmail.com>
2022-10-16 22:11:22 +0300

    traictorie test double cap

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -13,16 +14,17 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(35, -60, Math.toRadians(90)))
+                                .forward(10) //dupa vine scanare Signal
+                                .splineTo(new Vector2d(28,-7), Math.toRadians(135)) // lasat con
+                                .back(8)
+                                .turn(Math.toRadians(-135))
+                                .forward(25) //prindere con nou
+                                .back(12)
+                                .turn(Math.toRadians(-90)) //de aici merge pentru cap #2
+                                .forward(5)//lasare con nou
                                 .build()
+                                //dupa asta vine parcarea prin strafing
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
