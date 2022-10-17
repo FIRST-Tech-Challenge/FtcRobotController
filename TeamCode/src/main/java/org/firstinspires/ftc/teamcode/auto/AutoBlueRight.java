b6e8ed115259da05a0fc507843548bdf60c6e60c
SakinaK <skanji@bsd220.org>
2022-10-17 17:23:56 -0500

    Auto Opmodes for each side
    Trajectories need to be added for each opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/AutoBlueRight.java
@@ -0,0 +1,131 @@
+package org.firstinspires.ftc.teamcode.auto;
+import org.firstinspires.ftc.teamcode.vision.AprilTagDetectionPipeline;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import java.util.ArrayList;
+
+
+@Autonomous
+public class AutoBlueRight extends LinearOpMode {
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    //don't change
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    //our three tags
+    int left = 1;
+    int middle = 2;
+    int right = 3;
+
+    AprilTagDetection tagOfInterest = null;
+
+    @Override
+    public void runOpMode()
+    {
+        int webcam = hardwareMap.appContext.getResources().getIdentifier("Webcam1", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), webcam);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+
+
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLD();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == left || tag.id == right || tag.id == middle)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("tag is spotted:");
+
+                }
+            }
+
+            telemetry.update();
+            sleep(15);
+        }
+
+
+        /* Update the telemetry */
+        if(tagOfInterest != null)
+        {
+            telemetry.addLine(String.format("\nDetected tag ID=%d", tagOfInterest.id));
+            telemetry.update();
+        }
+        else
+        {
+            telemetry.addLine("No tag snapshot available");
+            telemetry.update();
+        }
+
+        //ADD TRAJECTORIES HERE
+        //https://firstinspiresst01.blob.core.windows.net/first-energize-ftc/game-manual-part-2-traditional.pdf
+        //page 46 above has the parking zones
+
+        if(tagOfInterest.id == left )
+        {
+            //insert trajectories for parking zone 1
+        }
+
+        else if(tagOfInterest.id == middle)
+        {
+            //insert trajectories for parking zone 2
+        }
+
+        else if(tagOfInterest.id == right)
+        {
+            //insert trajectories for parking zone 3
+        }
+
+
+    }
+
+
+}
