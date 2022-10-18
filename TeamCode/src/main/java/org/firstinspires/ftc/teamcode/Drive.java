7c29dff5bfa31fe304a86da09d59d29784571f43
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-10-18 14:24:08 -0700

    Added slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -14,10 +14,14 @@ public class Drive extends OpMode {
 
     @Override
     public void loop() {
-        warbotron.frontLeft.setPower(-gamepad1.left_stick_y);
-        warbotron.frontRight.setPower(-gamepad1.right_stick_y);
-        warbotron.backLeft.setPower(-gamepad1.left_stick_y);
-        warbotron.backRight.setPower(-gamepad1.right_stick_y);
+        double power = 1;
+
+        if(gamepad1.right_bumper) power = 0.5;
+
+        warbotron.frontLeft.setPower(-gamepad1.left_stick_y * power);
+        warbotron.frontRight.setPower(-gamepad1.right_stick_y * power);
+        warbotron.backLeft.setPower(-gamepad1.left_stick_y * power);
+        warbotron.backRight.setPower(-gamepad1.right_stick_y * power);
         warbotron.hammer1.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
         warbotron.hammer2.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
     }
