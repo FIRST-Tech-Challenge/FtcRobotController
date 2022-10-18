c8a9239f8cbb6e6ad58f8ffdbf6aa37169fa384e
Dragons 10523 <92609241+Dragons10523@users.noreply.github.com>
2022-10-18 14:31:45 -0700

    Reduce acceleration in slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -2,26 +2,37 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "Terminator Mode")
 public class Drive extends OpMode {
     Warbotron warbotron;
+
+    public final double SNEAK_ACCEL = 0.25;
 
     @Override
     public void init() {
         warbotron = new Warbotron(hardwareMap);
     }
 
+    double leftPower = 0;
+    double rightPower = 0;
+
+    ElapsedTime deltaTime = new ElapsedTime();
+
     @Override
     public void loop() {
-        double power = 1;
+        double leftTargetPower = -gamepad1.left_stick_y;
+        double rightTargetPower = -gamepad1.right_stick_y;
 
-        if(gamepad1.right_bumper) power = 0.5;
+        leftPower += (leftTargetPower - leftPower) * deltaTime.seconds() / SNEAK_ACCEL;
+        rightPower += (rightTargetPower - rightPower) * deltaTime.seconds() / SNEAK_ACCEL;
+        deltaTime.reset();
 
-        warbotron.frontLeft.setPower(-gamepad1.left_stick_y * power);
-        warbotron.frontRight.setPower(-gamepad1.right_stick_y * power);
-        warbotron.backLeft.setPower(-gamepad1.left_stick_y * power);
-        warbotron.backRight.setPower(-gamepad1.right_stick_y * power);
+        warbotron.frontLeft.setPower(-gamepad1.left_stick_y * leftPower);
+        warbotron.frontRight.setPower(-gamepad1.right_stick_y * rightPower);
+        warbotron.backLeft.setPower(-gamepad1.left_stick_y * leftPower);
+        warbotron.backRight.setPower(-gamepad1.right_stick_y * rightPower);
         warbotron.hammer1.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
         warbotron.hammer2.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
     }
