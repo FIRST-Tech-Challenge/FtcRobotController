dc9c633cc207a769dd82ae40237fa62659149063
Uttej <uttejkollu123@gmail.com>
2022-10-18 22:48:42 -0400

    mecanum class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Classes/MecanumClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Classes/MecanumClass.java
@@ -1,39 +1,112 @@
 package org.firstinspires.ftc.teamcode.Classes;
 
+import com.google.blocks.ftcrobotcontroller.runtime.CRServoAccess;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
 //uttej
 public class MecanumClass extends DrivetrainClass implements MotorInterface {
 
-    // all of these methods are extended from the abstract drivetrain class, this classi 
+    // all of these methods are extended from the abstract drivetrain class, this class
+
 
     // need four motors(parameters) and all initialization
-    void initialize() {
+
+    void initialize(String frontLeft, String frontRight, String backLeft, String backRight) {
+        frontLeft = hardwareMap.get(DcMotorEx.class,"FrontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class,"FrontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class,"BackLeft");
+        backRight = hardwareMap.get(DcMotorEx.class,"BackRight");
+
+        }
 
     }
 
     // time + speed are parameters for all the movement
-    void moveForward() {
+    void moveForward(time,speed) {
+        frontLeft.setPower(-speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(-speed);
+        backRight.setpower(speed);
+
+        sleep(time*1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
 
     }
 
-    void moveBackward() {
+    void moveBackward(time,speed) {
+        frontLeft.setPower(speed);
+        frontRight.setPower(-speed);
+        backLeft.setPower(speed);
+        frontRight.setpower(-speed);
+
+        sleep(time*1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
+    }
+
+    void moveRight(time,speed) {
+        frontLeft.setPower(speed);
+        frontRight.setPower(-speed);
+        backLeft.setPower(-speed);
+        frontRight.setpower(speed);
+
+        sleep(time*1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
 
     }
 
-    void moveRight() {
+    void moveLeft(time,speed) {
+        frontLeft.setPower(-speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        frontRight.setpower(-speed);
+
+        sleep(time*1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
+    }
+
+    void turnLeft(time,speed) {
+        frontLeft.setPower(speed);
+        frontRight.setPower(speed);
+        backLeft.setPower(speed);
+        frontRight.setpower(speed);
+
+        sleep(time*1000);
+
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
 
     }
 
-    void moveLeft() {
+    void turnRight(time,speed) {
+        frontLeft.setPower(-speed);
+        frontRight.setPower(-speed);
+        backLeft.setPower(-speed);
+        frontRight.setpower(-speed);
 
-    }
+        sleep(time*1000);
 
-    void turnLeft() {
-
-    }
-
-    void turnRight() {
-
-    }
+        frontLeft.setPower(0);
+        frontRight.setPower(0);
+        backLeft.setPower(0);
+        backRight.setpower(0);
 
 
 }
