a0c1f5964f869a9802e1007f7b3d1132d17d206c
Sanj Saini <Sanj26Saini@gmail.com>
2022-10-20 21:05:38 -0700

    Fixed auto drive methods adn parking.

--- Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseAutonomous.java
+++ Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseAutonomous.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.team8923_PowerPlay;
 
+import static org.firstinspires.ftc.team8923_PowerPlay.Constants.TICKS_PER_INCH;
+
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -22,61 +24,23 @@ public abstract class BaseAutonomous extends BaseOpMode {
     double robotAngle;
     double headingOffset = 0.0;
 
-    int newTargetFL;
-    int newTargetFR;
-    int newTargetBL;
-    int newTargetBR;
-
-    int errorFR;
-    int errorFL;
-    int errorBR;
-    int errorBL;
-
     double speedFL;
     double speedFR;
     double speedBL;
     double speedBR;
 
-    double Kmove = 1.0f/1200.0f;
-
-
-    int TOL = 100;
-
-    boolean isDoneSettingUp = false;
-
-    //Used to calculate distance traveled between loops
+    // Used to calculate distance traveled between loops
     int lastEncoderFL = 0;
     int lastEncoderFR = 0;
     int lastEncoderBL = 0;
     int lastEncoderBR = 0;
 
-    boolean autoReverseDrive = false;
-
-    Alliance alliance = Alliance.BLUE;
-    Destinations destination = Destinations.SQUAREA;
-
-    int rings;
-
-
-
-    int delays = 0;
-    int numOfSecondsDelay = 0;
-    int delayTime = 0;
-
-    double DRIVE_POWER_CONSTANT = 1.0/1000;
-    double TURN_POWER_CONSTANT = 1.0/65;
-
-    double MIN_DRIVE_POWER = 0.2;
-
-    enum Alliance{
-        BLUE,RED
+    // Variable for dectecion
+    enum Position {
+        ONE,
+        TWO,
+        THREE,
     }
-
-
-    enum Destinations{
-        SQUAREA, SQUAREB, SQUAREC
-    }
-
     public void initAuto() {
         telemetry.addData("Init State", "Init Started");
         telemetry.update();
@@ -86,10 +50,6 @@ public abstract class BaseAutonomous extends BaseOpMode {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
         telemetry.addData("Init State", "Init Finished");
-        telemetry.addData("Alliance", alliance.name());
-        telemetry.addData("Delay Time", delayTime);
-
-
 
         //Set last know encoder values
         lastEncoderFR = motorFR.getCurrentPosition();
@@ -103,99 +63,76 @@ public abstract class BaseAutonomous extends BaseOpMode {
         telemetry.clear();
         telemetry.update();
         telemetry.addLine("Initialized. Ready to start!");
-
-
-
-
     }
 
-    public void configureAutonomous(){
-        while(!isDoneSettingUp){
-            if(gamepad1.x){
-                alliance = Alliance.BLUE;
-            }else if(gamepad1.b){
-                alliance = Alliance.RED;
-            }
-            if(gamepad1.dpad_up){
-                delays++;
-            }else if(gamepad1.dpad_down){
-                delays--;
-            }
-            if(rings == 0){
-                destination = Destinations.SQUAREA;
-
-            }else if(rings == 1){
-                destination = Destinations.SQUAREB;
-            }else if(rings == 4){
-                destination = Destinations.SQUAREC;
-            }
-
-
-
-            if(gamepad1.start){
-                isDoneSettingUp = true;
-            }
-            // input information
-            telemetry.addLine("Alliance Blue/Red: X/B");
-            telemetry.addLine("Add a delay: D-Pad Up/Down");
-            telemetry.addLine("toggle objective: a park/park and foundation");
-            telemetry.addLine("After routine is complete and robot is on field, press Start");
-
-            telemetry.addLine();
-            telemetry.addData("alliance: ", alliance);
-            telemetry.addData("delays:", delays);
-            telemetry.addData("destination", destination);
-            telemetry.update();
-
-
-
-        }
+    public Position detectSignalSleeve() {
+        return Position.ONE;
     }
+    /**
+     * Moves the robot forward, backwards, and strafes right and left in inches.
+     * @param x strafes the robot right x inches. If moving left, pass in negative value.
+     * @param y moves the robot forward y inches. If moving backward, pass in negative value.
+     */
+    public void driveInches(double x, double y) {
+        double xTicks = x * TICKS_PER_INCH;
+        double yTicks = y * TICKS_PER_INCH;
 
-    public void moveAuto(double x, double y, double speed, double minSpeed, double timeout) throws InterruptedException
-    {
-        newTargetFL = motorFL.getCurrentPosition() + (int) Math.round(Constants.COUNTS_PER_MM * y) + (int) Math.round(Constants.COUNTS_PER_MM * x * 1.15);
-        newTargetFR = motorFR.getCurrentPosition() + (int) Math.round(Constants.COUNTS_PER_MM * y) - (int) Math.round(Constants.COUNTS_PER_MM * x * 1.15);
-        newTargetBL = motorBL.getCurrentPosition() + (int) Math.round(Constants.COUNTS_PER_MM * y) - (int) Math.round(Constants.COUNTS_PER_MM * x * 1.15);
-        newTargetBR = motorBR.getCurrentPosition() + (int) Math.round(Constants.COUNTS_PER_MM * y) + (int) Math.round(Constants.COUNTS_PER_MM * x * 1.15);
+        double targetFL = xTicks + yTicks;
+        double targetFR = yTicks - xTicks;
+        double targetBL = yTicks - xTicks;
+        double targetBR = yTicks + xTicks;
+
+        // Determine new target position, and pass to motor controller
+        targetFL += motorFL.getCurrentPosition();
+        targetFR += motorFR.getCurrentPosition();
+        targetBL += motorBL.getCurrentPosition();
+        targetBR += motorBR.getCurrentPosition();
+
+        motorFL.setTargetPosition((int) targetFL);
+        motorFR.setTargetPosition((int) targetFR);
+        motorBL.setTargetPosition((int) targetBL);
+        motorBR.setTargetPosition((int) targetBR);
+
+        // Turn On RUN_TO_POSITION
+        motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
         runtime.reset();
-        do
-        {
-            errorFL = newTargetFL - motorFL.getCurrentPosition();
-            speedFL = Math.abs(errorFL * Kmove);
-            speedFL = Range.clip(speedFL, minSpeed, speed);
-            speedFL = speedFL * Math.signum(errorFL);
-
-            errorFR = newTargetFR - motorFR.getCurrentPosition();
-            speedFR = Math.abs(errorFR * Kmove);
-            speedFR = Range.clip(speedFR, minSpeed, speed);
-            speedFR = speedFR * Math.signum(errorFR);
-
-            errorBL = newTargetBL - motorBL.getCurrentPosition();
-            speedBL = Math.abs(errorBL * Kmove);
-            speedBL = Range.clip(speedBL, minSpeed, speed);
-            speedBL = speedBL * Math.signum(errorBL);
-
-            errorBR = newTargetBR - motorBR.getCurrentPosition();
-            speedBR = Math.abs(errorBR * Kmove);
-            speedBR = Range.clip(speedBR, minSpeed, speed);
-            speedBR = speedBR * Math.signum(errorBR);
-
-            motorFL.setPower(speedFL);
-            motorFR.setPower(speedFR);
-            motorBL.setPower(speedBL);
-            motorBR.setPower(speedBR);
-            idle();
-        }
+        motorFL.setPower(0.5);
+        motorFR.setPower(0.5);
+        motorBL.setPower(0.5);
+        motorBR.setPower(0.5);
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
         while (opModeIsActive() &&
-                (runtime.seconds() < timeout) &&
-                (Math.abs(errorFL) > TOL || Math.abs(errorFR) > TOL || Math.abs(errorBL) > TOL || Math.abs(errorBR) > TOL));
+                (runtime.seconds() < 30) &&
+                (motorFL.isBusy() && motorFR.isBusy() && motorBL.isBusy() && motorBR.isBusy())) {
 
+            /*// Display it for the driver.
+            telemetry.addData("Running to",  " %7d :%7d", newLeftTarget,  newRightTarget);
+            telemetry.addData("Currently at",  " at %7d :%7d",
+                    leftDrive.getCurrentPosition(), rightDrive.getCurrentPosition());
+            telemetry.update();*/
+        }
+
+        // Stop all motion;
         stopDriving();
-    }
 
+        // Turn off RUN_TO_POSITION
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
     //using imu
-    public void imuPivot(double referenceAngle, double targetAngle, double maxSpeed, double kAngle, double timeout){
+    public void imuPivot(double referenceAngle, double targetAngle, double maxSpeed, double kAngle, double timeout) {
         runtime.reset();
         //counter-clockwise is positive
         double pivot;
@@ -204,15 +141,15 @@ public abstract class BaseAutonomous extends BaseOpMode {
 
         targetAngle = referenceAngle + targetAngle;
         targetAngle = adjustAngles(targetAngle);
-        do{
+        do {
             currentRobotAngle = imu.getAngularOrientation().firstAngle;
-            angleError =  currentRobotAngle - targetAngle;
+            angleError = currentRobotAngle - targetAngle;
             angleError = adjustAngles(angleError);
             pivot = angleError * kAngle;
 
-            if (pivot >= 0.0){
+            if (pivot >= 0.0) {
                 pivot = Range.clip(pivot, 0.15, maxSpeed);
-            }else{
+            } else {
                 pivot = Range.clip(pivot, -maxSpeed, -0.15);
             }
 
@@ -227,62 +164,18 @@ public abstract class BaseAutonomous extends BaseOpMode {
             motorBR.setPower(speedBR);
             idle();
         }
-        while((opModeIsActive() && (Math.abs(angleError) > 3.0)) && (runtime.seconds() < timeout));
+        while ((opModeIsActive() && (Math.abs(angleError) > 3.0)) && (runtime.seconds() < timeout));
         stopDriving();
-
-
-
-
     }
 
-    public void reverseImuPivot(double referenceAngle, double targetAngle, double maxSpeed, double kAngle, double timeout){
-        runtime.reset();
-        //counter-clockwise is positive
-        double pivot;
-        double currentRobotAngle;
-        double angleError;
-
-        targetAngle = referenceAngle + targetAngle;
-        targetAngle = adjustAngles(targetAngle);
-        do{
-            currentRobotAngle = imu.getAngularOrientation().firstAngle;
-            targetAngle = adjustAngles(targetAngle);
-            angleError = currentRobotAngle - targetAngle;
-            angleError = adjustAngles(angleError);
-            pivot = angleError * kAngle;
-
-            if(pivot >= 0.0){
-                pivot = Range.clip(pivot, 0.15, maxSpeed);
-            }else{
-                pivot = Range.clip(pivot, -maxSpeed, -0.15);
-            }
-
-            speedFL = -pivot;
-            speedFR = pivot;
-            speedBL = -pivot;
-            speedBR = pivot;
-
-            motorFL.setPower(speedFL);
-            motorFR.setPower(speedFR);
-            motorBL.setPower(speedBL);
-            motorBR.setPower(speedBR);
-            idle();
-        } while(opModeIsActive() && (Math.abs(angleError) > 3.0) && (runtime.seconds() < timeout));
-
-        stopDriving();
-
-    }
-
-    public void sendTelemetry(){
-
+    public void sendTelemetry() {
         //Informs drivers of robot location
         telemetry.addData("X", robotX);
         telemetry.addData("Y", robotY);
         telemetry.addData("Robot Angle", imu.getAngularOrientation().firstAngle);
     }
 
-    private void stopDriving(){
-
+    private void stopDriving() {
         motorFL.setPower(0.0);
         motorFR.setPower(0.0);
         motorBL.setPower(0.0);
@@ -291,63 +184,12 @@ public abstract class BaseAutonomous extends BaseOpMode {
     }
 
     //normalizing the angle to be between -180 to 180
-    private double adjustAngles(double angle){
-        while(angle > 180)
+    private double adjustAngles(double angle) {
+        while (angle > 180)
             angle -= 360;
-        while(angle < -180)
+        while (angle < -180)
             angle += 360;
         return angle;
 
     }
-
-    private double normalizeAngle(double rawAngle){
-        while(Math.abs(rawAngle) > 180){
-            rawAngle -= Math.signum(rawAngle) * 360;
-        }
-        return rawAngle;
-    }
-
-    private void updateRobotLocation(){
-        // Update robot angle
-        // subtraction here b/c imu returns a negative rotation when turned to the right
-        robotAngle = headingOffset - imu.getAngularOrientation().firstAngle;
-
-        // Calculate how far each motor has turned since last time
-        int deltaFL = motorFL.getCurrentPosition() - lastEncoderFL;
-        int deltaFR = motorFR.getCurrentPosition() - lastEncoderFR;
-        int deltaBL = motorBL.getCurrentPosition() - lastEncoderBL;
-        int deltaBR = motorBR.getCurrentPosition() - lastEncoderBR;
-
-        // Take average of encoder ticks to find translational x and y components. FR and BL are
-        // negative because of the direction at which they turn when going sideways
-        double deltaX = (deltaFL - deltaFR - deltaBL + deltaBR) / 4.0;
-        double deltaY = (deltaFL + deltaFR + deltaBL + deltaBR) / 4.0;
-
-        telemetry.addData("deltaX", deltaX);
-        telemetry.addData("deltaY", deltaY);
-
-        // Convert to mm
-        //TODO: maybe wrong proportions? something about 70/30 effectiveness maybe translation to MM is wrong
-        deltaX *= Constants.TICKS_PER_INCH;
-        deltaY *= Constants.TICKS_PER_INCH;
-
-        /*
-         * Delta x and y are intrinsic to the robot, so they need to be converted to extrinsic.
-         * Each intrinsic component has 2 extrinsic components, which are added to find the
-         * total extrinsic components of displacement. The extrinsic displacement components
-         * are then added to the previous position to set the new coordinates
-         */
-
-        robotX += deltaX * Math.sin(Math.toRadians(robotAngle)) + deltaY * Math.cos(Math.toRadians(robotAngle));
-        robotY += deltaX * -Math.cos(Math.toRadians(robotAngle)) + deltaY * Math.sin(Math.toRadians(robotAngle));
-
-        // Set last encoder values for next loop
-        lastEncoderFL = motorFL.getCurrentPosition();
-        lastEncoderFR = motorFR.getCurrentPosition();
-        lastEncoderBL = motorBL.getCurrentPosition();
-        lastEncoderBR = motorBR.getCurrentPosition();
-
-
-    }
-
 }
--- Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/Constants.java
+++ Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/Constants.java
@@ -4,14 +4,10 @@ public class Constants {
     // ratios
     static final double TICKS_PER_REVOLUTION = 537.6; // Neverest orbital 20, 7 pulse per revolution
     static final double GEAR_RATIO = 1.0;
-    static final double TICKS_PER_MOTOR_REVOLUTION = 560.0;
     static final double WHEEL_DIAMETER = 4.0; // inches
-    static final double MM_PER_REVOLUTION = Math.PI * WHEEL_DIAMETER;
-    static final double TICKS_PER_WHEEL_REVOLUTION = TICKS_PER_MOTOR_REVOLUTION / GEAR_RATIO;
     static final double TICKS_PER_INCH =  (TICKS_PER_REVOLUTION * GEAR_RATIO) / (WHEEL_DIAMETER * Math.PI);
     static final double INCHES_PER_REVOLUTION = Math.PI * WHEEL_DIAMETER;
     static final double INCHES_PER_TICK = INCHES_PER_REVOLUTION / TICKS_PER_REVOLUTION;
-    static final double COUNTS_PER_MM = TICKS_PER_WHEEL_REVOLUTION / MM_PER_REVOLUTION;
 
     // controls
     public static final double MINIMUM_JOYSTICK_POWER = 0.0;
