30fd6fff2ccf911a7fb984d3d6f965444a958299
Kieran <pwooks3@gmail.com>
2022-10-22 13:28:46 -0700

    Autonomous stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBluePlace.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBluePlace.java
@@ -14,20 +14,21 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-@Autonomous(name ="MOAuto", group = "A")
-public class RedRedPark extends DriveMethods {
+@Autonomous(name ="RedBluePlace", group = "A")
+public class RedBluePlace extends DriveMethods {
     public void runOpMode() {
         initMotorsSecondBot();
 
         waitForStart();
 
-        driveForDistance(1, Direction.FORWARD, 0.5,0);
+        driveForDistance(0.24, Direction.LEFT, 0.5, 0);
+        driveForDistance(0.3, Direction.FORWARD, 0.3, 0);
+        driveForDistance(0.3, Direction.BACKWARD, 0.5, 0);
+        driveForDistance(0.5, Direction.LEFT, 0.5, 0);
 
         while(opModeIsActive()) {
 
         }
-
-
     }
 
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRedPlace.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import static org.firstinspires.ftc.teamcode.Variables.*;
+
+import android.graphics.drawable.GradientDrawable;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+@Autonomous(name ="RedRedPlace", group = "A")
+public class RedRedPlace extends DriveMethods {
+    public void runOpMode() {
+        initMotorsSecondBot();
+
+        waitForStart();
+
+        driveForDistance(0.24, Direction.RIGHT, 0.5, 0);
+        driveForDistance(0.3, Direction.FORWARD, 0.3, 0);
+        driveForDistance(0.3, Direction.BACKWARD, 0.5, 0);
+        driveForDistance(1.4, Direction.LEFT, 0.5, 0);
+
+        while(opModeIsActive()) {
+
+        }
+    }
+
+
+
+}
