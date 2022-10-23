0b81e845e409bde16c4971bfba6933b5f962a02e
0zgeada <uzmanoa@gmail.com>
2022-10-23 12:13:23 -0400

    update driveconstants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -53,7 +53,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0149 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.0149;
     public static double kA = 0.0027;
     public static double kStatic = 0;
 
