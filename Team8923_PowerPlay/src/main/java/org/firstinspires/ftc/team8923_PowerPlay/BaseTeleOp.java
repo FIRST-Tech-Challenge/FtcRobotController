312d6b480cd8e186aa80eb2c40631f3dd07a63c5
Daania Abbas <>
2022-10-23 15:43:36 -0700

    edit auto

--- Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseTeleOp.java
+++ Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseTeleOp.java
@@ -36,6 +36,7 @@ abstract public class BaseTeleOp extends BaseOpMode {
      * if dpad up, linear slides go up
      * if dpad down, linear slides go down
      */
+    /*
     public void driveMechanism() {
         if (gamepad2.dpad_up) {
             motorSlideLeft.setPower(mechanismSpeed);
@@ -52,7 +53,7 @@ abstract public class BaseTeleOp extends BaseOpMode {
     /**
      * press the X button, claw opens
      * press the A button, claw closes
-     */
+
     public void driveClaw() {
         if (gamepad2.x) {
             servoClaw.setPosition(OPEN_CLAW);
@@ -60,6 +61,7 @@ abstract public class BaseTeleOp extends BaseOpMode {
             servoClaw.setPosition(CLOSED_CLAW);
         }
     }
+    */
 }
 
 
