96649ae5e15d78eaf0d3db7a0ec2f6540b1c7969
Jesse <SJJcoding@gmail.com>
2022-10-23 13:44:21 -0700

    TeleOpDrive method, probably working

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.team6220_PowerPlay;
 
 abstract public class BaseTeleOp extends BaseOpMode{
-
+    public void teleOpDrive(){
+        if(Math.abs(Math.atan2(gamepad1.left_stick_y, gamepad2.left_stick_x)) > 5){
+            driveRobot(gamepad1.left_stick_x, 0, gamepad1.right_stick_x);
+        }else if(Math.abs(Math.atan2(gamepad1.left_stick_x, gamepad2.left_stick_y)) > 5){
+            driveRobot(0, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }else{
+            driveRobot(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }
+    }
 };
