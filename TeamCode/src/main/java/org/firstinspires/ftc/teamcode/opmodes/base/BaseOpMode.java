924159d83b5684e3489d0095328b5282d60149bc
zerbash <dbasham@overlake.org>
2022-10-24 08:25:46 -0700

    Clean up base op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/base/BaseOpMode.java
@@ -1,16 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmodes.base;
 
-import android.util.Log;
-
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.components.DriveSystem;
-import org.firstinspires.ftc.teamcode.components.IMUSystem;
-import org.firstinspires.ftc.teamcode.components.PixyCam;
-import org.firstinspires.ftc.teamcode.components.Vuforia;
-import org.firstinspires.ftc.teamcode.components.Vuforia.CameraChoice;
+import org.firstinspires.ftc.teamcode.params.DriveParams;
 
 import java.util.EnumMap;
 
@@ -20,34 +15,24 @@ import java.util.EnumMap;
 public abstract class BaseOpMode extends OpMode {
 
     protected DriveSystem driveSystem;
-    protected Vuforia vuforia;
-    private boolean stopRequested;
-    protected PixyCam pixyCam;
 
-    /** Initialization */
+    @Override
     public void init(){
-        stopRequested = false;
-        // Timeouts to determine if stuck in loop
-        this.msStuckDetectInit     = 20000;
-        this.msStuckDetectInitLoop = 20000;
-        // Initialize motors
+        setDriveSystem();
+    }
+
+    private void setDriveSystem() {
+        // Instantiate Drive System motor map
         EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
         for(DriveSystem.MotorNames name : DriveSystem.MotorNames.values()){
             driveMap.put(name,hardwareMap.get(DcMotor.class, name.toString()));
         }
-        driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
 
+        // Instantiate IMU
+        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, DriveParams.IMU);
+
+        // Instantiate DriveSystem
+        driveSystem = new DriveSystem(driveMap, imu);
     }
 
-    /** Returns if a stop has been requested or if execution is
-     */
-    public final boolean isStopRequested() {
-        return this.stopRequested || Thread.currentThread().isInterrupted();
-    }
-
-    @Override
-    public void stop() {
-        stopRequested = true;
-        super.stop();
-    }
 }
