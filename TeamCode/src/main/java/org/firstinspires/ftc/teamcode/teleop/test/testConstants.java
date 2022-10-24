becd4978a9dc9e2b476a2e963105b148dd24a874
SanjiCharmeleon <sanjithc4@outlook.com>
2022-10-24 15:38:06 -0700

    Update testConstants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/testConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/test/testConstants.java
@@ -16,7 +16,7 @@ import org.firstinspires.ftc.teamcode.common.Constants;
 
 import org.firstinspires.ftc.teamcode.common.HardwareDrive;
 
-@TeleOp(name="Swerve Code", group="Drive")
+@TeleOp(name="test constants", group="Drive")
 //@Disabled
 public class testConstants extends OpMode{
     /* Declare OpMode members. */
@@ -94,10 +94,13 @@ public class testConstants extends OpMode{
         for(int i = 0; i < 4; i++){
             posData[i] = posSystem.getPositionArr()[i];
         }
-        telemetry.addData("Xpos", posData[0]);
-        telemetry.addData("Ypos", posData[1]);
+
         telemetry.addData("W", posData[2]);
         telemetry.addData("R", posData[3]);
+
+        telemetry.addData("State", state);
+        telemetry.addData("TopR", robot.topR.getCurrentPosition());
+        telemetry.addData("TopL", robot.topL.getCurrentPosition());
 
         telemetry.update();
     }
@@ -118,24 +121,24 @@ public class testConstants extends OpMode{
         if (x.getState() == Button.State.TAP) state = State.SAME_DIRECTION;
         else if (b.getState() == Button.State.TAP) state = State.SINGLE;
 
-        int distanceTopL = (int) (gamepad1.left_stick_y * 100 * beta);
-        int distanceBotL = (int) (-gamepad1.left_stick_y * 100 * beta);
+        int distanceTopR = (int) (gamepad1.left_stick_y * 100 * beta);
+        int distanceBotR = (int) (-gamepad1.left_stick_y * 100 * beta);
 
-        int rotationalTopL = (int) (gamepad1.left_stick_x * 100 * alpha);
-        int rotationalBotL = (int) (gamepad1.left_stick_x * 100 * alpha);
+        int rotationalTopR = (int) (gamepad1.left_stick_x * 100 * alpha);
+        int rotationalBotR = (int) (gamepad1.left_stick_x * 100 * alpha);
 
         switch (state){
             case SAME_DIRECTION:
-                robot.topL.setTargetPosition(robot.topL.getCurrentPosition() + distanceTopL + rotationalTopL);
-                robot.botL.setTargetPosition(robot.botL.getCurrentPosition() + distanceBotL + rotationalBotL);
+                robot.topR.setTargetPosition(robot.topR.getCurrentPosition() + distanceTopR + rotationalTopR);
+                robot.botR.setTargetPosition(robot.botR.getCurrentPosition() + distanceBotR + rotationalBotR);
 
-                robot.topL.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
-                robot.botL.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
+                robot.topR.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
+                robot.botR.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
                 break;
 
             case SINGLE:
-                robot.topL.setTargetPosition(robot.topL.getCurrentPosition() + distanceTopL + rotationalTopL);
-                robot.topL.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
+                robot.topR.setTargetPosition(robot.topR.getCurrentPosition() + distanceTopR + rotationalTopR);
+                robot.topR.setPower(gamepad1.left_stick_y * beta + gamepad1.left_stick_x * alpha);
                 break;
         }
 
