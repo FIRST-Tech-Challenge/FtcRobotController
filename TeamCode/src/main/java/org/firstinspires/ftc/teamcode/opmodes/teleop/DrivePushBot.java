e68365a65b8e008a413b716d038f205f0b8b3cc5
adityasengupta <adityasngpta@gmail.com>
2022-10-26 16:11:27 -0700

    Create Drive Skeleton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DrivePushBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/DrivePushBot.java
@@ -17,7 +17,7 @@ import java.util.EnumMap;
  * Drives a pushbot with teleop control.
  */
 @TeleOp(name = "Mecanum", group="TeleOp")
-public class DrivePushBot extends OpMode{
+public class DrivePushBot extends OpMode {
 
     private DriveSystem driveSystem;
 
@@ -28,7 +28,7 @@ public class DrivePushBot extends OpMode{
     public void init() {
         // Set up drive system
         EnumMap<DriveSystem.MotorNames, DcMotor> driveMap = new EnumMap<>(DriveSystem.MotorNames.class);
-        for(DriveSystem.MotorNames name : DriveSystem.MotorNames.values()){
+        for (DriveSystem.MotorNames name : DriveSystem.MotorNames.values()) {
             driveMap.put(name, hardwareMap.get(DcMotor.class, name.toString()));
         }
         driveSystem = new DriveSystem(driveMap, hardwareMap.get(BNO055IMU.class, "imu"));
@@ -39,11 +39,50 @@ public class DrivePushBot extends OpMode{
      * Left trigger engages slow-drive
      */
     @RequiresApi(api = Build.VERSION_CODES.N)
+    float x;
     public void loop() {
         float rx = (float) Math.pow(gamepad1.right_stick_x, 3);
         float lx = (float) Math.pow(gamepad1.left_stick_x, 3);
         float ly = (float) Math.pow(gamepad1.left_stick_y, 3);
         driveSystem.slowDrive(gamepad1.left_trigger > 0.3f);
         driveSystem.drive(rx, lx, ly);
+
+        boolean y = false;
+
+        x = gamepad1.right_trigger;
+        driveSystem.intake(x);
+
+        if (gamepad1.a) {
+            // Move Bar to Low Position
+        }
+
+        if (gamepad1.b) {
+            // Move Bar to Middle Position
+        }
+
+        if (gamepad1.y) {
+            // Move Bar to High Position
+        }
+
+        if (gamepad1.x) {
+            // Move Bar to Center Position
+        }
+
+
+        if (gamepad1.dpad_down) {
+            // Move Bar Down Based on Time Pressed
+        }
+
+        if (gamepad1.dpad_up) {
+            // Move Bar Up Based on Time Pressed
+        }
+
+        if ( y /*gamepad1.right_trigger*/) {
+            // Intake Rotate Out
+        }
+
+        if ( y /*gamepad1.right_trigger*/) {
+            // Intake Rotate In
+        }
     }
 }
