c5ec98ab4926b0782f6e13840970a4fae2216c78
CoolJosh40 <jsugino@lacanadaengineeringclub.org>
2022-10-26 21:12:49 -0700

    fixed hashmap storing mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/gps/TurnGPS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/gps/TurnGPS.java
@@ -1,7 +1,5 @@
 package org.firstinspires.ftc.teamcode.common.gps;
 
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.common.ConstantsPKG.Constants;
@@ -10,55 +8,56 @@ import org.firstinspires.ftc.teamcode.common.Kinematics.Kinematics;
 
 import java.util.HashMap;
 
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class to handle errors
-
 public class TurnGPS {
 
     Constants constants = new Constants();
+    private Kinematics.DriveType driveType;
 
     private double[] positionArr = new double[4];
-    private HashMap<DcMotorEx, Integer> motorClicksPose = new HashMap<>();
-    private HashMap<DcMotorEx, Integer> prevMotorClicks = new HashMap<>();
+    public HashMap<String, Integer> motorClicksPose = new HashMap<>();
+    public HashMap<String, Integer> prevMotorClicks = new HashMap<>();
 
     HardwareDrive robot;
-
-    private Kinematics.DriveType driveType;
 
     public double rotationalDegrees;
     public double translationalInches;
 
-    public TurnGPS(HardwareDrive robot, Kinematics.DriveType k){
+    public TurnGPS(HardwareDrive robot, Kinematics.DriveType k ){
         this.robot = robot;
-        for (int i = 0; i < 4; i++){
-            positionArr[i] = 0;
-        }
+        driveType = k;
 
-        updateHash();
+
+        motorClicksPose.put("topR", robot.topR.getCurrentPosition());
+        motorClicksPose.put("botR", robot.botR.getCurrentPosition());
+        motorClicksPose.put("topL", robot.topL.getCurrentPosition());
+        motorClicksPose.put("botL", robot.botL.getCurrentPosition());
+
+        prevMotorClicks.put("topR", motorClicksPose.get("topR"));
+        prevMotorClicks.put("botR", motorClicksPose.get("botR"));
+        prevMotorClicks.put("topL", motorClicksPose.get("topL"));
+        prevMotorClicks.put("botL", motorClicksPose.get("botL"));
     }
 
     public void calculatePos(){
+//        if (goodGap()) return;
+
         updateHash();
 
         //left
-        int topL = motorClicksPose.get(robot.topL) - prevMotorClicks.get(robot.topL); //change in top left
-        int botL = motorClicksPose.get(robot.botL) - prevMotorClicks.get(robot.botL); //change in bottom left
+        int topL = motorClicksPose.get("topL") - prevMotorClicks.get("topL"); //change in top left
+        int botL = motorClicksPose.get("botL") - prevMotorClicks.get("botL"); //change in bottom left
         double translateL = (topL - botL) / 2.0;
         double rotateL = topL - translateL;
         translateL *= constants.INCHES_PER_CLICK;
         rotateL *= constants.DEGREES_PER_CLICK;
 
         //right
-        int topR = motorClicksPose.get(robot.topR) - prevMotorClicks.get(robot.topR); //change in top right
-        int botR = motorClicksPose.get(robot.botR) - prevMotorClicks.get(robot.botR); //change in bottom right
+        int topR = motorClicksPose.get("topR") - prevMotorClicks.get("topR"); //change in top right
+        int botR = motorClicksPose.get("botR") - prevMotorClicks.get("botR"); //change in bottom right
         double translateR = (topR - botR) / 2.0;
         double rotateR = topR - translateR;
         translateR *= constants.INCHES_PER_CLICK;
         rotateR *= constants.DEGREES_PER_CLICK;
-
-        rotationalDegrees = (rotateL + rotateR) / 2;
-        translationalInches = (translateL + translateR) / 2;
-        double currentAngle = clamp(rotationalDegrees + positionArr[2]);
 
         if (driveType == Kinematics.DriveType.TURN){
             double radius = constants.DISTANCE_BETWEEN_MODULE_AND_CENTER;
@@ -69,11 +68,15 @@ public class TurnGPS {
             return;
         }
 
+        rotationalDegrees = (rotateL + rotateR) / 2;
+        translationalInches = (translateL + translateR) / 2;
+        double currentAngle = clamp(rotationalDegrees + positionArr[2]);
+        currentAngle = Math.toRadians(currentAngle);
+
         if (Math.abs(translationalInches) <= 0.3){
             update(translationalInches * Math.sin(currentAngle), translationalInches * Math.cos(currentAngle) , rotationalDegrees, 0);
         }
         else{
-            //note that while testing rotateGPS, ONLY wheel rotation should be changing.
             update(translationalInches * Math.cos(currentAngle), translationalInches * Math.sin(currentAngle), 0, rotationalDegrees);
         }
     }
@@ -102,6 +105,10 @@ public class TurnGPS {
         return (Math.abs(positionArr[1]) <= 0.3);
     }
 
+    public boolean dChange(){
+        return (xChange() || yChange());
+    }
+
     public boolean wChange(){
         return (Math.abs(positionArr[2]) <= 1);
     }
@@ -110,33 +117,43 @@ public class TurnGPS {
         return (Math.abs(positionArr[3]) <= 1);
     }
 
-    public void hardResetGPS(){
-        //Reset GPS
-        for (int i = 0; i < 4; i++){
-            positionArr[i] = 0;
-        }
-
-
-        robot.topL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.botL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.topR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.botR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        updateHash();
-    }
+//    public void hardResetGPS(){
+//        //Reset GPS
+//        for (int i = 0; i < 4; i++){
+//            positionArr[i] = 0;
+//        }
+//
+//        //Reset Motor Clicks
+//        for (DcMotorEx motor : robot.dtMotors){
+//            motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+//            motorClicksPose.put(motor, motor.getCurrentPosition());
+//            prevMotorClicks.put(motor, motor.getCurrentPosition());
+//        }
+//    }
 
     public void updateHash(){
-        prevMotorClicks.put(robot.topR, motorClicksPose.get(robot.topR));
-        prevMotorClicks.put(robot.botR, motorClicksPose.get(robot.botR));
-        prevMotorClicks.put(robot.topL, motorClicksPose.get(robot.topL));
-        prevMotorClicks.put(robot.botL, motorClicksPose.get(robot.botL));
+        prevMotorClicks.put("topR", motorClicksPose.get("topR"));
+        prevMotorClicks.put("botR", motorClicksPose.get("botR"));
+        prevMotorClicks.put("topL", motorClicksPose.get("topL"));
+        prevMotorClicks.put("botL", motorClicksPose.get("botL"));
 
-        motorClicksPose.put(robot.topR, robot.topR.getCurrentPosition());
-        motorClicksPose.put(robot.botR, robot.botR.getCurrentPosition());
-        motorClicksPose.put(robot.topL, robot.topL.getCurrentPosition());
-        motorClicksPose.put(robot.botL, robot.botL.getCurrentPosition());
+        motorClicksPose.put("topR", robot.topR.getCurrentPosition());
+        motorClicksPose.put("botR", robot.botR.getCurrentPosition());
+        motorClicksPose.put("topL", robot.topL.getCurrentPosition());
+        motorClicksPose.put("botL", robot.botL.getCurrentPosition());
     }
 
+//    private boolean goodGap(){
+//        if ( //will need to add the left side later
+//                Math.abs(robot.topR.getCurrentPosition() - prevMotorClicks.get(robot.topR)) <= constants.clickTOLERANCE && Math.abs(robot.botR.getCurrentPosition() - prevMotorClicks.get(robot.botR)) <= constants.clickTOLERANCE
+//        ) {
+//            goodGapw = false;
+//            return false;
+//        }
+//
+//        goodGapw = true;
+//        return true;
+//    }
 
     public double[] getPositionArr() {
         return positionArr;
@@ -166,7 +183,7 @@ public class TurnGPS {
         return driveType;
     }
 
-    public HashMap<DcMotorEx, Integer> getMotorClicksPose () {
+    public HashMap<String, Integer> getMotorClicksPose () {
         return motorClicksPose;
     }
 
