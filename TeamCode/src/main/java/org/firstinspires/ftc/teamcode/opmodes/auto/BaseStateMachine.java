c5fad84985c5df23376b134943360fc5f5a27535
zerbash <dbasham@overlake.org>
2022-10-27 12:33:55 -0700

    Clean up code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BaseStateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/BaseStateMachine.java
@@ -3,66 +3,47 @@ package org.firstinspires.ftc.teamcode.opmodes.auto;
 import android.util.Log;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
 import org.firstinspires.ftc.teamcode.components.DriveSystem;
 import org.firstinspires.ftc.teamcode.components.PixyCam;
 import org.firstinspires.ftc.teamcode.components.Vuforia;
-import org.firstinspires.ftc.teamcode.opmodes.auto.BaseAutonomous;
 
-import java.util.ArrayList;
-import java.util.List;
 @Autonomous(name = "BaseStateMachine", group = "Autonomous")
 public class BaseStateMachine extends BaseAutonomous {
-    // List of all states the robot could be in
-    Sleeve teamAsset;
-    private static final float mmPerInch = 25.4f;
-    private static final float mmTargetHeight = 6 * mmPerInch;          // the height of the center of the target image above the floor
-    private static final float halfField = 72 * mmPerInch;
-    private static final float oneAndHalfTile = 36 * mmPerInch;
-    private static double tile = 500.0;
-    private int parkStep = 0;
-    private int currentPos = 0;
-    private int junctionStep = 0;
-    private PixyCam pixycam;
 
+    // List of all states the robot could be in
     public enum State {
         IDENTIFY_TARGET,
-        DRIVE_TO_MEDIUM_JUNCTION,
-        POSITION_ROBOT_AT_JUNCTION,
-        PLACE_CONE,
+        DRIVE_TO_JUNCTION,
         PARK,
         ALIGN_WITH_POLE,
         END_STATE,
-        REVERSE_JUNCTION,
-        LOGGING,
+        REVERSE_JUNCTION
     }
 
     public enum Sleeve {
         DAVID,
         BRIAN,
-        TEAM,
+        TEAM
     }
+    
+    Sleeve teamAsset;
+    private int step = 0;
+    private int currentPos = 0;
+    private PixyCam pixycam;
 
     private final static String TAG = "BaseStateMachine";// Logging tag
     private State mCurrentState;                         // Current State Machine State.
-    private ElapsedTime mStateTime = new ElapsedTime();  // Time into current state
 
     /**
      * Initializes State Machine
      */
     public void init() {
         super.init();
-        this.msStuckDetectInit = 15000;
-        this.msStuckDetectInitLoop = 15000;
         // Starts state machine
         vuforia = new Vuforia(hardwareMap, Vuforia.CameraChoice.WEBCAM1);
         pixycam = hardwareMap.get(PixyCam.class, "pixy");
         newState(State.IDENTIFY_TARGET);
-
-
     }
 
     @Override
@@ -90,7 +71,7 @@ public class BaseStateMachine extends BaseAutonomous {
     public void loop() {
         // Update telemetry each time through loop
         telemetry.addData("State", mCurrentState);
-        telemetry.update();
+
         // Execute state machine
         switch (mCurrentState) {
             case IDENTIFY_TARGET:
@@ -107,11 +88,11 @@ public class BaseStateMachine extends BaseAutonomous {
                     telemetry.addData("signal sleeve?: ", vuforia.identifyTeamAsset());
 
                 } else {
-                    newState(State.DRIVE_TO_MEDIUM_JUNCTION);
+                    newState(State.DRIVE_TO_JUNCTION);
                 }
                 break;
-            case DRIVE_TO_MEDIUM_JUNCTION:
-                mediumJunction();
+            case DRIVE_TO_JUNCTION:
+                drive_to_junction();
                 break;
             case ALIGN_WITH_POLE:
                 align();
@@ -127,7 +108,7 @@ public class BaseStateMachine extends BaseAutonomous {
                 //"david" left two squares, "brain" center two, "7330" right two squares
 
         }
-
+        telemetry.update();
     }
 
     /**
@@ -136,18 +117,17 @@ public class BaseStateMachine extends BaseAutonomous {
      * @param newState state to change to
      */
     private void newState(State newState) {
-        // Restarts the state clock as well as the state
-        mStateTime.reset();
         mCurrentState = newState;
+        step = 0;
     }
 
     private void park() {
-        if (parkStep == 0) {
+        if (step == 0) {
             if (driveSystem.driveToPosition(440-currentPos, DriveSystem.Direction.BACKWARD, 0.3)) {
-                parkStep++;
+                step++;
             }
         }
-        if (parkStep == 1) {
+        if (step == 1) {
             if (teamAsset == Sleeve.BRIAN ||
                     (teamAsset == Sleeve.TEAM && driveSystem.driveToPosition(500, DriveSystem.Direction.LEFT, 0.3)) ||
                     (teamAsset == Sleeve.DAVID && driveSystem.driveToPosition(500, DriveSystem.Direction.RIGHT, 0.3))) {
@@ -156,13 +136,13 @@ public class BaseStateMachine extends BaseAutonomous {
         }
     }
 
-    private void mediumJunction() {
-        if (junctionStep == 0) {
+    private void drive_to_junction() {
+        if (step == 0) {
             if (driveSystem.driveToPosition(950 - currentPos, DriveSystem.Direction.BACKWARD, 0.4)) {
-                junctionStep++;
+                step++;
             }
         }
-        if (junctionStep == 1) {
+        if (step == 1) {
             if (driveSystem.turn(-45, 0.2)) {
                 newState(State.ALIGN_WITH_POLE);
             }
@@ -170,14 +150,14 @@ public class BaseStateMachine extends BaseAutonomous {
     }
 
     private void reverseJunction() {
-        if (junctionStep == 0) {
-            if (driveSystem.driveToPosition(450, DriveSystem.Direction.FORWARD, 0.4)) {
-                newState(State.PARK);
+        if (step == 0) {
+            if (driveSystem.turn(45, 0.2)) {
+                step--;
             }
         }
-        if (junctionStep == 1) {
-            if (driveSystem.turn(45, 0.2)) {
-                junctionStep--;
+        if (step == 1) {
+            if (driveSystem.driveToPosition(450, DriveSystem.Direction.FORWARD, 0.4)) {
+                newState(State.PARK);
             }
         }
     }
@@ -185,15 +165,11 @@ public class BaseStateMachine extends BaseAutonomous {
     private void align() {
         int offset = pixycam.offSetX();
         telemetry.addData("offset", offset);
-        telemetry.update();
         if (offset > 20) {
             driveSystem.turn(60, 0.5);
-        }
-
-        else if (offset < -20) {
+        } else if (offset < -20) {
             driveSystem.turn(-60, 0.5);
-        }
-        else {
+        } else {
             driveSystem.setMotorPower(0);
             newState(State.END_STATE);
         }
