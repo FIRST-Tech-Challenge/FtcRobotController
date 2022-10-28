f1fd7a11d7aba745330985edb0b627859ac0bb05
Brendan D <79545485+b-c-d@users.noreply.github.com>
2022-10-28 16:08:59 -0400

    Update LinearSlideClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Classes/LinearSlideClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Classes/LinearSlideClass.java
@@ -8,31 +8,42 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 //brendan
 public class LinearSlideClass {
 
-  public DcMotorEx leftDrive;
-  public DcMotorEx rightDrive;
-
+  public DcMotorEx Drive;
+  
+  //Intilizes the Linaer Slide
   public void initializeLinearSlide() {
 
-    leftDrive  = hardwareMap.get(DcMotorEx.class, "left_drive");
-    rightDrive = hardwareMap.get(DcMotorEx.class, "right_drive");
+    Drive  = hardwareMap.get(DcMotorEx.class, "drive");
 
-    leftDrive.setDirection(DcMotor.Direction.FORWARD);
-    rightDrive.setDirection(DcMotor.Direction.REVERSE);
-
+    Drive.setDirection(DcMotor.Direction.FORWARD);
   }
-
+  //Move linear Slides Up (Value has to be postive 0.0-1.0)
+  public void linearSlideUp() {
+  //Move linear slide all the way up with max power
+    Drive.setPower(1);
+  }
   public void linearSlideUp(double power) {
-
-    leftDrive.setPower(power);
-    rightDrive.setPower(power);
-
+  //Moves linear slide all the way up with set power.
+    Drive.setPower(power);
   }
-
+  public void linearSlideUp(double power, double distance) {
+  //Moves linear slide up set distance and speed.
+    Drive.setPower(power);
+  }
+  
+  
+  //Moves linear slide down (Value has to be postive 0.0-1.0)
+  public void linearSlideDown() {
+  //Move linear slide all the way down with max power
+    Drive.setPower(-1);   
+  }
   public void linearSlideDown(double power) {
-
-    leftDrive.setPower(-power);
-    rightDrive.setPower(-power);
-
+  //Moves linear slide all the way down with set power.
+    Drive.setPower(-power);
+  }
+    public void linearSlideDown(double power, double distance) {
+    //Moves linear slide down set distance and speed.
+      Drive.setPower(-power);
   }
 
 }
