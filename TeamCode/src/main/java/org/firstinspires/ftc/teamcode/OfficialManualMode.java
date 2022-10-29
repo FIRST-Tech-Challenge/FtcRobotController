98e926e2a2f3d169486408eb2f4c5946c9c42401
forshawny <foryuiop@hotmail.com>
2022-10-28 21:03:20 -0700

    specified motors and servos. updated control pads

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OfficialManualMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OfficialManualMode.java
@@ -10,35 +10,34 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name = "Official Manual Mode", group = "Match")
 public class OfficialManualMode extends LinearOpMode {
-    private DcMotor _fl, _fr, _rl, _rr, _arm, _turn;
-    private Servo _grab, _armassist;
+    private DcMotor _fl, _fr, _rl, _rr;
+    private Servo _grip, _platform, _elbow, _shoulder;
     private double _armPosition = 0, _leftTrigger = 0, _rightTrigger = 0;
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
         // Make sure your ID's match your configuration
-        _fl = hardwareMap.dcMotor.get("fl");
-        _fr = hardwareMap.dcMotor.get("fr");
-        _rl = hardwareMap.dcMotor.get("rl");
-        _rr = hardwareMap.dcMotor.get("rr");
+        _fl = hardwareMap.dcMotor.get("frontLeft");
+        _fr = hardwareMap.dcMotor.get("frontRight");
+        _rl = hardwareMap.dcMotor.get("rearLeft");
+        _rr = hardwareMap.dcMotor.get("rearRight");
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
         _fr.setDirection(DcMotorSimple.Direction.REVERSE);
         _rr.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        _arm = hardwareMap.get(DcMotor.class, "arm");
-        _turn = hardwareMap.get(DcMotor.class, "turn");
-        _grab = hardwareMap.get(Servo.class, "grab");
-        _armassist = hardwareMap.get(Servo.class, "armassist");
+        _platform = hardwareMap.get(Servo.class, "platform");
+        _grip = hardwareMap.get(Servo.class, "grip");
+        _elbow = hardwareMap.get(Servo.class, "elbow");
+        _shoulder = hardwareMap.get(Servo.class, "shoulder");
 
         waitForStart();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             controlArm();
-            controlTurn();
             controlWheels();
         }
     }
@@ -75,39 +74,25 @@ public class OfficialManualMode extends LinearOpMode {
 
 
 
-    private void controlTurn() {
-        if (gamepad1.left_bumper)
-            _turn.setPower(1.0);
-        else if (gamepad1.right_bumper)
-            _turn.setPower(-1.0);
-        else
-            _turn.setPower(0);
-    }
-
-    private void controlArm(){
-        if (gamepad2.left_stick_button || gamepad2.right_stick_button)
-            _grab.setPosition(0.2);
-        else
-            _grab.setPosition(0);
+        private void controlArm(){
 
         if (gamepad2.left_bumper)
-            _armPosition = 0;
+            _platform.setPosition(0.5);
         else if (gamepad2.right_bumper)
-            _armPosition = 1;
+            _platform.setPosition(0);
         else if (gamepad2.x)
-            _armPosition = 0.1;
+            _shoulder.setPosition(0.5);
         else if (gamepad2.a)
-            _armPosition = 0.2;
+            _shoulder.setPosition(0);
         else if (gamepad2.b)
-            _armPosition = 0.3;
+            _elbow.setPosition(0.5);
         else if (gamepad2.y)
-            _armPosition = 0.8  ;
-        else if (gamepad2.left_trigger > 0 && _leftTrigger == 0 && _armPosition < 1.0)
-            _armPosition = _armPosition + 0.05;
-        else if (gamepad2.right_trigger > 0 && _rightTrigger == 0 && _armPosition > 0)
-            _armPosition = _armPosition - 0.05;
+            _elbow.setPosition(0);
+        else if (gamepad2.left_stick_button)
+            _grip.setPosition(1);
+        else if (gamepad2.right_stick_button)
+            _grip.setPosition(0);
 
-        _armassist.setPosition(_armPosition);
 
         _leftTrigger = gamepad2.left_trigger;
         _rightTrigger = gamepad2.right_trigger;
