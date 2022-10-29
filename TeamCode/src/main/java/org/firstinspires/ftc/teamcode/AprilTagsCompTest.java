61a6766b75ee26672df692aa9b1be3836e6d405b
justanotherinternetguy <96833060+justanotherinternetguy@users.noreply.github.com>
2022-10-29 09:05:26 -0400

    Create AprilTagsCompTest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagsCompTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.apriltags.aprilTagsInit;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp
+public class AprilTagsCompTest extends LinearOpMode
+{
+    @Override
+    public void runOpMode()
+    {
+        aprilTagsInit init = new aprilTagsInit(hardwareMap,telemetry);
+        init.initialize();
+
+        while(!isStarted() && !isStopRequested())
+        {
+            init.search();
+            sleep(20);
+        }
+        waitForStart();
+        int finalID = init.stopAndSave();
+        telemetry.addLine(Integer.toString(finalID));
+        telemetry.update();
+    }
+}
