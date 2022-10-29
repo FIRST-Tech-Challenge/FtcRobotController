e2a1ab167dbd45e65d3cac134468d97df7ddacca
john0 <maybemedic2@gmail.com>
2022-10-29 10:29:06 -0500

    Added cone for custom signal sleeve recognition

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ConeImageProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ConeImageProcessor.java
@@ -14,41 +14,80 @@ import org.opencv.imgproc.Imgproc;
 public class ConeImageProcessor {
 
 	boolean debug = true;
+	//Blue Range for the blue cone
+	Scalar blueConeL = new Scalar(105, 60, 20 );
+	Scalar blueConeH = new Scalar(115, 255, 255 );
+
+	//Red Range for red cone
+	Scalar redConeL1 = new Scalar(0, 60, 20 );
+	Scalar redConeH1 = new Scalar(15, 255, 255 );
+	Scalar redConeL2 = new Scalar(170, 60, 20 );
+	Scalar redConeH2 = new Scalar(179, 255, 255 );
+
+	//Range for background, mat is grey
+	Scalar backgroundL = new Scalar(0, 0, 20 );
+	Scalar backgroundH = new Scalar(179, 27, 255 );
+
+    //Range for yellow pol
+	Scalar yellowPoleL = new Scalar(15, 60, 20 );
+	Scalar yellowPoleH = new Scalar(25, 255, 255 );
+
 
 	public static void main(String[] args) {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		Mat test01 = Imgcodecs.imread("D:\\eclipse-workspace\\Testing01.jpg");
+		Mat test01 = Imgcodecs.imread("C:\\FTC Code\\TestPIcs\\20221029_095209.jpg");
 
 		ConeImageProcessor proc = new ConeImageProcessor();
-		proc.processFrame2(test01);
+		proc.processFrame(test01);
 
 		// System.out.println("mat = " + test01.dump());
 
 	}
 
 
-	public Mat processFrame2 (Mat input) {
+	public Mat processFrame (Mat input) {
 		//Convert to HSV color space. 		
 		Mat hsvImg = new Mat();
 		hsvImg.create(input.size(), CvType.CV_8U);
 		Imgproc.cvtColor(input, hsvImg, Imgproc.COLOR_BGR2HSV);
 
-		//Find Blue color in range. 
-		Mat thresh = new Mat();		
-		Core.inRange(hsvImg, new Scalar(100, 100, 20 ), new Scalar(130,255,255), thresh);
-		
+		//Find Blue cone in range.
+		Mat blueConeTrd = new Mat();
+		Core.inRange(hsvImg, blueConeL, blueConeH, blueConeTrd);
+
+		Mat redConeTrd1 = new Mat();
+		Core.inRange(hsvImg, redConeL1, redConeH1, redConeTrd1);
+		Mat redConeTrd2 = new Mat();
+		Core.inRange(hsvImg, redConeL2, redConeH2, redConeTrd2);
+
+		Mat yellowPolTrd = new Mat ();
+		Core.inRange(hsvImg, yellowPoleL, yellowPoleH, yellowPolTrd);
+
+		Mat backroundTrd = new Mat();
+		Core.inRange(hsvImg, backgroundL, backgroundH, backroundTrd);
+
+
+		Mat tobeRemoveRev = new Mat();
+		Core.bitwise_or(blueConeTrd,redConeTrd1,tobeRemoveRev);
+		Core.bitwise_or(redConeTrd2,tobeRemoveRev,tobeRemoveRev);
+		Core.bitwise_or(yellowPolTrd,tobeRemoveRev,tobeRemoveRev);
+		Core.bitwise_or(backroundTrd,tobeRemoveRev,tobeRemoveRev);
+
+		Mat tobeRemove = new Mat();
+		Core.bitwise_not(tobeRemoveRev,tobeRemove);
+
 		///Apply mask to input image, and copy to new image. 
 		Mat result  = new Mat(input.size(), CvType.CV_8UC3, new Scalar(255, 255, 255));
-		input.copyTo(result, thresh);
+		input.copyTo(result, tobeRemove);
 		
 		List<Mat> hsvThreholdPlane = new ArrayList<>();
 		Core.split(result, hsvThreholdPlane);
 		
 		if (debug) {
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\thresh.jpg", thresh);
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\result.jpg", result);
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\foreGroundH.jpg", hsvThreholdPlane.get(0));
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\thresh.jpg", tobeRemoveRev);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\result.jpg", result);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\foreGroundH.jpg", hsvThreholdPlane.get(0));
 			System.out.println("Result = " + hsvThreholdPlane.get(0).dump());
 		}
 
@@ -62,7 +101,7 @@ public class ConeImageProcessor {
 	 * 
 	 * @param input
 	 * @return
-	 */
+
 	public Mat processFrame(Mat input) {
 		Mat hsvImg = new Mat();
 		hsvImg.create(input.size(), CvType.CV_8U);
@@ -101,18 +140,18 @@ public class ConeImageProcessor {
 		Core.split(foreground, hsvThreholdPlane);
 		
 		if (debug) {
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\H.jpg", hsvPlanes.get(0));
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\S.jpg", hsvPlanes.get(1));
-			//Imgcodecs.imwrite("D:\\eclipse-workspace\\V.jpg", hsvPlanes.get(2));
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\threshold.jpg", thresholdImg);
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\thresholdh.jpg", hThresholdImg);
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\thresholds.jpg", sThresholdImg);
-			Imgcodecs.imwrite("D:\\eclipse-workspace\\foreGround.jpg", foreground);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\H.jpg", hsvPlanes.get(0));
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\S.jpg", hsvPlanes.get(1));
+			//Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\V.jpg", hsvPlanes.get(2));
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\threshold.jpg", thresholdImg);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\thresholdh.jpg", hThresholdImg);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\thresholds.jpg", sThresholdImg);
+			Imgcodecs.imwrite("C:\\FTC Code\\TestPIcs\\foreGround.jpg", foreground);
 			System.out.println("Result = " + hsvThreholdPlane.get(0).dump());
 		}
 
 		return foreground;
 	}
 
-	
+	 */
 }
