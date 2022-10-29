0671c81e8ddbcf201d91f82bd2d9cce5b3d968a5
knguyen-hub <54388203+knguyen-hub@users.noreply.github.com>
2022-10-29 00:28:32 -0700

    Update Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/drive/DriveConstants.java
    Co-authored-by: Malavika Venkatesan <17712433+malavika1@users.noreply.github.com>

--- Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/drive/DriveConstants.java
+++ Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/drive/DriveConstants.java
@@ -58,7 +58,7 @@ public class DriveConstants {
     public static double kStatic = 0;
 
     /*
-     * These values are used to generate the trajectories for you robot. To ensure proper operation,
+     * These values are used to generate the trajectories for your robot. To ensure proper operation,
      * the constraints should never exceed ~80% of the robot's actual capabilities. While Road
      * Runner is designed to enable faster autonomous motion, it is a good idea for testing to start
      * small and gradually increase them later after everything is working. All distance units are
