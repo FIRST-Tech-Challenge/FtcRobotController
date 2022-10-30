f11d67f4d0cedaf1a914eae9fc339cea6e62b5f6
zerbash <dbasham@overlake.org>
2022-10-30 12:43:18 -0700

    add missing files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/ArmOpMode.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.components.ArmSystem.Intake.State.INTAKING;
+import static org.firstinspires.ftc.teamcode.components.ArmSystem.Intake.State.OUTTAKING;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.components.ArmSystem;
+
+@Autonomous (name = "arm", group = "Autonomous")
+public class ArmOpMode extends OpMode {
+
+    DcMotor motor1;
+    DcMotor motor2;
+
+    /** Initialization */
+    public void init() {
+        motor1 = hardwareMap.get(DcMotor.class,"arm1");
+        motor2 = hardwareMap.get(DcMotor.class,"arm2");
+        motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void loop(){
+
+        telemetry.addData("Motor1: ", motor1.getCurrentPosition());
+        telemetry.addData("Motor2: ", motor2.getCurrentPosition());
+        telemetry.update();
+
+    }
+
+}
+
