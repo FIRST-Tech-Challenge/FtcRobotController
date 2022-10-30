f1e0efa0be11d24d3d8cfcaec10eca75cac2e37e
panda1771 <85645315+panda1771@users.noreply.github.com>
2022-10-29 23:14:57 -0700

    stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/HardwareDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/HardwareDrive.java
@@ -130,7 +130,7 @@ public class HardwareDrive
         topR.setDirection(DcMotorEx.Direction.FORWARD);
 
         armBase.setDirection(DcMotorEx.Direction.REVERSE);
-        armTop.setDirection(DcMotor.Direction.FORWARD);
+        armTop.setDirection(DcMotor.Direction.REVERSE);
 
         // dtMotors[2].setDirection(DcMotorSimple.Direction.FORWARD);
         //dtMotors[3].setDirection(DcMotorSimple.Direction.FORWARD);
