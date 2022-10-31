2a0d026fe56514ab9967849879da7bd9ea7fb43b
ExternalYelu <eyal_winter@outlook.com>
2022-10-31 16:19:08 -0700

    Add specific Op Modes for team positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Blue_Left_OpMode.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.opmodes.auto.CompetitionAutonomous.TeamState.BLUE_LEFT;
+
+public class Blue_Left_OpMode extends CompetitionAutonomous{
+
+};
+    public void init() {super.init(BLUE_LEFT);}
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Blue_Right_OpMode.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.opmodes.auto;
+
+import static org.firstinspires.ftc.teamcode.opmodes.auto.CompetitionAutonomous.TeamState.BLUE_RIGHT;
+
+public class Blue_Right_OpMode extends CompetitionAutonomous{
+}
+
+    public void init() { super.init(BLUE_RIGHT); }
+}
+
+
+
+
