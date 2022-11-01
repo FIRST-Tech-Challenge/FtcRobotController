d348f0c269100a9a265c626a56db0d28bdf0622c
avalicherla <89961535+avalicherla@users.noreply.github.com>
2022-11-01 16:02:37 -0700

    Revert "rebase onto 7330"
    This reverts commit afb740fbdc52274706710ab8e4fb13d177822d01.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/ArmSystem.java
@@ -5,14 +5,26 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class ArmSystem {
+import org.firstinspires.ftc.teamcode.params.DriveParams;
 
-    private int currentPos;
-    private final DcMotor armLeft;
+public class ArmSystem {
+    public enum ArmLevel {
+        LOW,
+        MEDIUM,
+        HIGH,
+        FLOOR
+    }
+    //fill in constants
+    private final int POSITION_LOW = 500;
+    private final int POSITION_MEDIUM = 800;
+    private final int POSITION_HIGH = 1100;
+    private final int FLOORCONE = 0;
+    private final DcMotor armLeft; //arm left is motor1
     private final DcMotor armRight;
     private Intake intake;
 
-    public ArmSystem(DcMotor motor1, DcMotor motor2){
+
+    public ArmSystem(DcMotor motor1, DcMotor motor2, DcMotor intakeMotor, DigitalChannel beam){
         armLeft = motor1;
         armRight = motor2;
         initMotors();
