4ddd9c16a02d2137949f81442380a5ce373dcfa2
saa938 <83087832+saa938@users.noreply.github.com>
2022-11-01 18:37:05 -0700

    added turtlerobot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/TurtleRobotAuto.java
@@ -0,0 +1,71 @@
+package org.firstinspires.ftc.teamcode.robot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class TurtleRobotAuto {
+    /* Public OpMode members. */
+    public DcMotor rightfrontmotor = null;
+    public DcMotor rightbackmotor = null;
+    public DcMotor leftfrontmotor = null;
+    public DcMotor leftbackmotor = null;
+    public CRServo ClawMotor = null;
+    public CRServo ArmServo = null;
+    public DcMotor leftslidemotor = null;
+    public DcMotor rightslidemotor = null;
+    public ElapsedTime runtime = new ElapsedTime();
+    public LinearOpMode myOpMode = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap = null;
+    public ElapsedTime period = new ElapsedTime();
+
+    /* Constructor */
+    public TurtleRobotAuto (LinearOpMode opmode) { myOpMode = opmode;
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+        // Define and Initialize Motors
+        leftfrontmotor = hwMap.get(DcMotor.class, "leftfrontmotor");
+        leftbackmotor = hwMap.get(DcMotor.class, "leftbackmotor");
+        rightfrontmotor = hwMap.get(DcMotor.class, "rightfrontmotor");
+        rightbackmotor = hwMap.get(DcMotor.class, "rightbackmotor");
+        ClawMotor = hwMap.get(CRServo.class, "ClawMotor");
+        ArmServo = hwMap.get(CRServo.class, "ArmServo");
+        leftslidemotor = hwMap.get(DcMotor.class, "leftslidemotor");
+        rightslidemotor = hwMap.get(DcMotor.class, "rightslidemotor");
+
+        leftfrontmotor.setDirection(DcMotor.Direction.REVERSE); // Set to REVERSE if using AndyMark motors
+        leftbackmotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        rightfrontmotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightbackmotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftslidemotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightslidemotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        // Set all motors to zero power
+        leftfrontmotor.setPower(0);
+        leftbackmotor.setPower(0);
+        rightfrontmotor.setPower(0);
+        rightbackmotor.setPower(0);
+        //ClawMotor.setPower(0);
+        leftslidemotor.setPower(0);
+        rightslidemotor.setPower(0);
+
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        leftbackmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftfrontmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightfrontmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightbackmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+}
