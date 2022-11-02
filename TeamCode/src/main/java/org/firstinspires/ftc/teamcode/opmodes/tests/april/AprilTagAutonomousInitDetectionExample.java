8bd3dc2baf686a9530347e712e5c890111a6706d
saa938 <83087832+saa938@users.noreply.github.com>
2022-11-01 18:41:50 -0700

    moved code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/april/AprilTagAutonomousInitDetectionExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/april/AprilTagAutonomousInitDetectionExample.java
@@ -19,13 +19,12 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.april;
+package org.firstinspires.ftc.teamcode.opmodes.tests.april;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.teamcode.april.AprilTagDetectionPipeline;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/april/AprilTagDetectionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/april/AprilTagDetectionPipeline.java
@@ -19,7 +19,7 @@
  * SOFTWARE.
  */
 
-package org.firstinspires.ftc.teamcode.april;
+package org.firstinspires.ftc.teamcode.opmodes.tests.april;
 
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.CvType;
