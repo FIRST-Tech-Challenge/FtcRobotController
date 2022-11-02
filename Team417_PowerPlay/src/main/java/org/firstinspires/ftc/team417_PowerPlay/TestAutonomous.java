7587ed7e2022537dc7fff984d730ce014cab0132
Kaitlin Nguyen <cookykn@gmail.com>
2022-11-01 20:27:59 -0700

    Replaced numbers with constants in test auto

--- Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TestAutonomous.java
+++ Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/TestAutonomous.java
@@ -10,18 +10,24 @@ import org.firstinspires.ftc.team417_PowerPlay.drive.SampleMecanumDrive;
 
 @Autonomous (name="strafe test")
 public class TestAutonomous extends LinearOpMode {
+
+    // Coordinates for various tiles referenced on page 46 of:
+    // https://www.firstinspires.org/sites/default/files/uploads/resource_library/ftc/game-manual-part-2-traditional.pdf
+    // Robot's position starts at (0, 0) - all tile coordinates are in relation to this starting position
+    private static final double Y_POSITION_A = -30;
+    private static final double X_POSITION_C = 41;
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         Trajectory traject1 = drive.trajectoryBuilder(new Pose2d())
                 .splineToConstantHeading(new Vector2d(5, 0), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(5, -30), Math.toRadians(0))
-                .splineToConstantHeading(new Vector2d(41, -30), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(5, Y_POSITION_A), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(X_POSITION_C, Y_POSITION_A), Math.toRadians(0))
                 .build();
 
         Trajectory traject2 = drive.trajectoryBuilder(traject1.end(), Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(41, -28), Math.toRadians(0))
+                .splineToConstantHeading(new Vector2d(X_POSITION_C, Y_POSITION_A), Math.toRadians(0))
                 .build();
 
         waitForStart();
@@ -31,8 +37,5 @@ public class TestAutonomous extends LinearOpMode {
         drive.followTrajectory(traject1);
         drive.turn(Math.toRadians(90));
         drive.followTrajectory(traject2);
-        telemetry.addData("heading", drive.getLocalizer().getPoseEstimate());
-        telemetry.update();
-        sleep(10000);
     }
 }
