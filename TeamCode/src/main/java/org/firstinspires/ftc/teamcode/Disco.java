8471dd1178a59b7f227fa5afdb35a560f207bec7
Mr_Stoffer <49318416+mrstoffer@users.noreply.github.com>
2022-11-02 14:56:56 +0100

    remove comment

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Disco.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Disco.java
@@ -34,7 +34,6 @@ public class Disco extends LinearOpMode {
             double y = -gamepad1.left_stick_y; // Remember, this is reversed!
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
-            // i need to commit stuff
 
             double armPower = gamepad2.left_stick_y;
             if (gamepad2.dpad_up) {
