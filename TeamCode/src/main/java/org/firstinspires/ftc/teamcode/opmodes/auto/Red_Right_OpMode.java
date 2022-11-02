12fc1ab2af00630d705bd80ddadc108170dad948
Ved Anand <ved.anand@outlook.com>
2022-11-02 16:42:10 -0700

    quick typo change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Red_Right_OpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/Red_Right_OpMode.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode.opmodes.auto;
 import static org.firstinspires.ftc.teamcode.opmodes.auto.CompetitionAutonomous.TeamColor.RED;
 import static org.firstinspires.ftc.teamcode.opmodes.auto.CompetitionAutonomous.TeamSide.RIGHT;
 
-public class RedLeft_OpMode extends CompetitionAutonomous{
+public class Red_Right_OpMode extends CompetitionAutonomous{
 
     public void init() {
         super.init(RED, RIGHT);
