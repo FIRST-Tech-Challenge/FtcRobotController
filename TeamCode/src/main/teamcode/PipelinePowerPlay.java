18c4fdb0a66ebc123175afbe45010ad58cef06a3
OviedoRobotics <54600937+OviedoRobotics@users.noreply.github.com>
2022-11-03 14:06:17 -0400

    Update pipeline to check signal sleeve.

--- TeamCode/src/main/teamcode/PipelinePowerPlay.java
+++ TeamCode/src/main/teamcode/PipelinePowerPlay.java
@@ -40,119 +40,53 @@ class PipelinePowerPlay extends OpenCvPipeline
      * by forgetting to call mat.release(), and it also reduces memory pressure by not
      * constantly allocating and freeing large chunks of memory.
      */
-    private Mat YCrCb = new Mat();
-    private Mat Cb    = new Mat();
-    private Mat Cr    = new Mat();
-    private Mat subMat1;
-    private Mat subMat2;
-    private Mat subMat3;
+    private Mat RGB = new Mat();
+    private Mat channel[3];
+    private Mat r    = new Mat();
+    private Mat g    = new Mat();
+    private Mat b    = new Mat();
     private int max;
-    private int avg1;
-    private int avg2;
-    private int avg3;
+    public static int avgR;
+    public static int avgG;
+    public static int avgB;
     private Point marker = new Point();        // Team Element (populated once we find it!)
     private Point sub1PointA;
     private Point sub1PointB;
-    private Point sub2PointA;
-    private Point sub2PointB;
-    private Point sub3PointA;
-    private Point sub3PointB;
-
-    // Points for alignment
-    private Mat alignBlueMat1;
-    private Mat alignBlueMat2;
-    private Mat alignBlueMat3;
-    private int alignBlueAvg1;
-    private int alignBlueAvg2;
-    private int alignBlueAvg3;
-    private Mat alignRedMat1;
-    private Mat alignRedMat2;
-    private Mat alignRedMat3;
-    private int alignRedAvg1;
-    private int alignRedAvg2;
-    private int alignRedAvg3;
-
-    private Point alignment1RedPointA = new Point(45,212);
-    private Point alignment1RedPointB = new Point(55,222);
-    private Point alignment2RedPointA = new Point(160,212);
-    private Point alignment2RedPointB = new Point(170,222);
-    private Point alignment3RedPointA = new Point(273,212);
-    private Point alignment3RedPointB = new Point(285,222);
-    private final static double colorRedThreshold = 140.0;
-
-    private Point alignment1BluePointA = new Point(47,209);
-    private Point alignment1BluePointB = new Point(57,219);
-    private Point alignment2BluePointA = new Point(158,209);
-    private Point alignment2BluePointB = new Point(168,219);
-    private Point alignment3BluePointA = new Point(274,209);
-    private Point alignment3BluePointB = new Point(284,219);
-    private final static double colorBlueThreshold = 140.0;
 
     // Public statics to be used by opMode
-    public static int hubLevel;
-
-    public static int leftBlueAverage;
-    public static boolean alignedBlueLeft;
-    public static int centerBlueAverage;
-    public static boolean alignedBlueCenter;
-    public static int rightBlueAverage;
-    public static boolean alignedBlueRight;
-
-    public static int leftRedAverage;
-    public static boolean alignedRedLeft;
-    public static int centerRedAverage;
-    public static boolean alignedRedCenter;
-    public static int rightRedAverage;
-    public static boolean alignedRedRight;
+    public static int signalZone;
 
     public static Mat finalAutoImage = new Mat();
 
     private final boolean redAlliance;
-    private final boolean duckySide;
+    private final boolean terminalSide;
     private static String directory;
 
-    PipelinePowerPlay(boolean redAlliance, boolean duckySide) {
+    PipelinePowerPlay(boolean redAlliance, boolean terminalSide) {
         // Create a subdirectory based on DATE
         String dateString = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
         directory = Environment.getExternalStorageDirectory().getPath() + "//FIRST//Webcam//" + dateString;
         this.redAlliance = redAlliance;
-        this.duckySide = duckySide;
+        this.terminalSide = terminalSide;
         if(redAlliance) {
-            if(duckySide) {
-                directory += "/red_duck";
-                sub1PointA = new Point( 42,190); // 15x15 pixels on LEFT
+            if(terminalSide) {
+                directory += "/red_terminal";
+                sub1PointA = new Point( 42,190); // 15x15 pixels on signal sleeve
                 sub1PointB = new Point( 57,205);
-                sub2PointA = new Point(158,190); // 15x15 pixels on CENTER
-                sub2PointB = new Point(173,205);
-                sub3PointA = new Point(277,190); // 15x15 pixels on RIGHT
-                sub3PointB = new Point(292,205);
             } else {
-                directory += "/red_warehouse";
-                sub1PointA = new Point( 44,190); // 15x15 pixels on LEFT
+                directory += "/red_not_terminal";
+                sub1PointA = new Point( 44,190); // 15x15 pixels on signal sleeve
                 sub1PointB = new Point( 59,205);
-                sub2PointA = new Point(160,190); // 15x15 pixels on CENTER
-                sub2PointB = new Point(175,205);
-                sub3PointA = new Point(265,190); // 15x15 pixels on RIGHT (limited by barrier!)
-                sub3PointB = new Point(280,205);
             }
         } else {
-            if(duckySide) {
-                directory += "/blue_duck";
-                sub1PointA = new Point( 40,190); // 15x15 pixels on LEFT
+            if(terminalSide) {
+                directory += "/blue_terminal";
+                sub1PointA = new Point( 40,190); // 15x15 pixels on signal sleeve
                 sub1PointB = new Point( 55,205);
-                sub2PointA = new Point(155,190); // 15x15 pixels on CENTER
-                sub2PointB = new Point(170,205);
-                sub3PointA = new Point(272,190); // 15x15 pixels on RIGHT
-                sub3PointB = new Point(287,205);
-
             } else {
-                directory += "/blue_warehouse";
-                sub1PointA = new Point( 57,190); // 15x15 pixels on LEFT  (limited by barrier!)
+                directory += "/blue_not_terminal";
+                sub1PointA = new Point( 57,190); // 15x15 pixels on signal sleeve
                 sub1PointB = new Point( 72,205);
-                sub2PointA = new Point(155,190); // 15x15 pixels on CENTER
-                sub2PointB = new Point(170,205);
-                sub3PointA = new Point(272,190); // 15x15 pixels on RIGHT
-                sub3PointB = new Point(287,205);
             }
         }
 
@@ -190,103 +124,55 @@ class PipelinePowerPlay extends OpenCvPipeline
     @Override
     public Mat processFrame(Mat input)
     {
-        // Convert image frame from RGB to YCrCb
-        Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
-        // Extract the Cb channel from the image frame
-        Core.extractChannel(YCrCb, Cb, 2);
-        // Extract the Cr channel from the image frame
-        Core.extractChannel(YCrCb, Cr, 1);
-        // Pull data for the three sample zones from the Cb channel
-        subMat1 = Cb.submat(new Rect(sub1PointA,sub1PointB) );
-        subMat2 = Cb.submat(new Rect(sub2PointA,sub2PointB) );
-        subMat3 = Cb.submat(new Rect(sub3PointA,sub3PointB) );
-        alignBlueMat1 = Cb.submat(new Rect(alignment1BluePointA, alignment1BluePointB));
-        alignBlueMat2 = Cb.submat(new Rect(alignment2BluePointA, alignment2BluePointB));
-        alignBlueMat3 = Cb.submat(new Rect(alignment3BluePointA, alignment3BluePointB));
-        alignRedMat1 = Cr.submat(new Rect(alignment1RedPointA, alignment1RedPointB));
-        alignRedMat2 = Cr.submat(new Rect(alignment2RedPointA, alignment2RedPointB));
-        alignRedMat3 = Cr.submat(new Rect(alignment3RedPointA, alignment3RedPointB));
+        // Extract the RGB channels from the image frame
+        Core.split(input, channel);
+
+        // Pull RGB data for the sample zone from the RBG channels
+        b = channel[0].submat(new Rect(sub1PointA,sub1PointB) );
+        g = channel[1].submat(new Rect(sub1PointA,sub1PointB) );
+        r = channel[2].submat(new Rect(sub1PointA,sub1PointB) );
 
         // Average the three sample zones
-        avg1 = (int)Core.mean(subMat1).val[0];
-        avg2 = (int)Core.mean(subMat2).val[0];
-        avg3 = (int)Core.mean(subMat3).val[0];
-        alignBlueAvg1 = (int)Core.mean(alignBlueMat1).val[0];
-        alignBlueAvg2 = (int)Core.mean(alignBlueMat2).val[0];
-        alignBlueAvg3 = (int)Core.mean(alignBlueMat3).val[0];
-        alignRedAvg1 = (int)Core.mean(alignRedMat1).val[0];
-        alignRedAvg2 = (int)Core.mean(alignRedMat2).val[0];
-        alignRedAvg3 = (int)Core.mean(alignRedMat3).val[0];
+        avgR = (int)Core.mean(r).val[0];
+        avgB = (int)Core.mean(b).val[0];
+        avgG = (int)Core.mean(g).val[0];
 
-        // Draw alignment rectangles
-        Imgproc.rectangle(input, alignment1BluePointA, alignment1BluePointB, new Scalar(0, 0, 255), 1);
-        Imgproc.rectangle(input, alignment2BluePointA, alignment2BluePointB, new Scalar(0, 0, 255), 1);
-        Imgproc.rectangle(input, alignment3BluePointA, alignment3BluePointB, new Scalar(0, 0, 255), 1);
-        Imgproc.rectangle(input, alignment1RedPointA, alignment1RedPointB, new Scalar(255, 0, 0), 1);
-        Imgproc.rectangle(input, alignment2RedPointA, alignment2RedPointB, new Scalar(255, 0, 0), 1);
-        Imgproc.rectangle(input, alignment3RedPointA, alignment3RedPointB, new Scalar(255, 0, 0), 1);
-
-        // Draw rectangles around the sample zones
+        // Draw rectangles around the sample zone
         Imgproc.rectangle(input, sub1PointA, sub1PointB, new Scalar(0, 0, 255), 1);
-        Imgproc.rectangle(input, sub2PointA, sub2PointB, new Scalar(0, 0, 255), 1);
-        Imgproc.rectangle(input, sub3PointA, sub3PointB, new Scalar(0, 0, 255), 1);
-        // Determine which sample zone had the lowest contrast from blue (lightest color)
-        max = Math.min(avg1, Math.min(avg2, avg3));
+
+        // Determine which RBG channel had the highest value
+        max = Math.max(avgR, Math.max(avgB, avgG));
         // Draw a circle on the detected team shipping element
-        if(max == avg1) {
-            marker.x = (sub1PointA.x + sub1PointB.x) / 2;
-            marker.y = (sub1PointA.y + sub1PointB.y) / 2;
-            Imgproc.circle(input, marker, 5, new Scalar(225, 52, 235), -1);
-            hubLevel = 1;
-        } else if(max == avg2) {
-            marker.x = (sub2PointA.x + sub2PointB.x) / 2;
-            marker.y = (sub2PointA.y + sub2PointB.y) / 2;
-            Imgproc.circle(input, marker, 5, new Scalar(225, 52, 235), -1);
-            hubLevel = 2;
-        } else if(max == avg3) {
-            marker.x = (sub3PointA.x + sub3PointB.x) / 2;
-            marker.y = (sub3PointA.y + sub3PointB.y) / 2;
-            Imgproc.circle(input, marker, 5, new Scalar(225, 52, 235), -1);
-            hubLevel = 3;
+        marker.x = (sub1PointA.x + sub1PointB.x) / 2;
+        marker.y = (sub1PointA.y + sub1PointB.y) / 2;
+        if(max == avgR) {
+            Imgproc.circle(input, marker, 5, new Scalar(255, 0, 0), -1);
+            signalZone = 1;
+        } else if(max == avgB) {
+            Imgproc.circle(input, marker, 5, new Scalar(0, 0, 255), -1);
+            signalZone = 2;
+        } else if(max == avgG) {
+            Imgproc.circle(input, marker, 5, new Scalar(0, 255, 0), -1);
+            signalZone = 3;
         } else {
-            hubLevel = 3;
+            signalZone = 3;
         }
-
-        leftBlueAverage = alignBlueAvg1;
-        alignedBlueLeft = (alignBlueAvg1 >= colorBlueThreshold);
-        centerBlueAverage = alignBlueAvg2;
-        alignedBlueCenter = (alignBlueAvg2 >= colorBlueThreshold);
-        rightBlueAverage = alignBlueAvg3;
-        alignedBlueRight = (alignBlueAvg3 >= colorBlueThreshold);
-
-        leftRedAverage = alignRedAvg1;
-        alignedRedLeft = (alignRedAvg1 >= colorRedThreshold);
-        centerRedAverage = alignRedAvg2;
-        alignedRedCenter = (alignRedAvg2 >= colorRedThreshold);
-        rightRedAverage = alignRedAvg3;
-        alignedRedRight = (alignRedAvg3 >= colorRedThreshold);
 
         input.copyTo(finalAutoImage);
 
         // Free the allocated submat memory
-        subMat1.release();
-        subMat1 = null;
-        subMat2.release();
-        subMat2 = null;
-        subMat3.release();
-        subMat3 = null;
-        alignBlueMat1.release();
-        alignBlueMat1 = null;
-        alignBlueMat2.release();
-        alignBlueMat2 = null;
-        alignBlueMat3.release();
-        alignBlueMat3 = null;
-        alignRedMat1.release();
-        alignRedMat1 = null;
-        alignRedMat2.release();
-        alignRedMat2 = null;
-        alignRedMat3.release();
-        alignRedMat3 = null;
+        r.release();
+        r = null;
+        g.release();
+        g = null;
+        b.release();
+        b = null;
+        channel[0].release();
+        channel[0] = null;
+        channel[1].release();
+        channel[1] = null;
+        channel[2].release();
+        channel[2] = null;
 
         return input;
     }
