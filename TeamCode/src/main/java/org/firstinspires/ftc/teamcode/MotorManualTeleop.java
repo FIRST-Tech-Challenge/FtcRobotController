aca595fc89bbdc6adeac6cd5ac12e60f879fac08
TedArmenston <63930022+TedArmenston@users.noreply.github.com>
2022-11-05 12:20:47 -0700

    Create MotorManualTeleop.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorManualTeleop.java
@@ -0,0 +1,44 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="MotorManualTeleop", group="A")
+public class MotorManualTeleop extends DriveMethods{
+    DcMotor motor;
+    double motorPower = 0;
+    @Override
+    public void runOpMode() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        waitForStart();
+
+
+        while(opModeIsActive()){
+            if(gamepad2.dpad_up){
+                motorPower = motorPower + 0.01;
+                sleep(100);
+            }
+            if(gamepad2.dpad_down){
+                motorPower = motorPower - 0.01;
+                sleep(100);
+
+            }
+
+            if(gamepad2.dpad_right){
+                motorPower = motorPower + 0.05;
+                sleep(100);
+
+            }
+            if(gamepad2.dpad_left){
+                motorPower = motorPower - 0.05;
+                sleep(100);
+
+            }
+
+            motor.setPower(motorPower);
+            telemetry.addLine("servoPosition" + motorPower);
+            telemetry.update();
+        }
+    }
+}
