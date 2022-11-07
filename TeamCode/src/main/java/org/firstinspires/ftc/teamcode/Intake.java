48f8aa90f5932ab3b1730b64d2b66f4a6eb1cb19
commandervander <commandervander@gmail.com>
2022-11-07 12:57:45 -0600

    Added Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -5,18 +5,39 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Intake {
-    private Servo arm;
+    private Servo armLeft;
+    private Servo armRight;
     private Servo wrist;
     private Servo claw;
 
+    WristMode wristMode;
+
     Intake(HardwareMap hardwareMap, Telemetry telemetry){
-        arm = hardwareMap.get(Servo.class, "arm");
+        armLeft = hardwareMap.get(Servo.class, "armLeft");
+        armRight = hardwareMap.get(Servo.class, "armRight");
         wrist = hardwareMap.get(Servo.class, "wrist");
         claw = hardwareMap.get(Servo.class, "claw");
+        wristMode = WristMode.MATCHED;
     }
 
+    public enum WristMode{INDEPENDENT, MATCHED}
+
     public void runArm(double pos){
-        arm.setPosition(pos);
+        armLeft.setPosition(pos);
+        armRight.setPosition(1-pos);
+
+        if (wristMode== WristMode.MATCHED){
+            runWrist(pos);
+        }
+    }
+
+    public void runArm(double pos, WristMode newWristMode){
+        armLeft.setPosition(pos);
+        armRight.setPosition(1-pos);
+
+        if (newWristMode== WristMode.MATCHED){
+            runWrist(pos);
+        }
     }
 
     public void runWrist(double pos){
@@ -25,5 +46,9 @@ public class Intake {
 
     public void runClaw(double pos){
         claw.setPosition(pos);
+    }
+
+    public void changeWristMode(WristMode newWristMode){
+        wristMode = newWristMode;
     }
 }
