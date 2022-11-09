f544f34803e447db84e512fcb9444642b7b43d65
tevel-ftc / FtcPowerPlay <tevel.ftc@gmail.com>
2022-11-09 17:58:43 +0200

    elevator op mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorOpModeLinear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ElevatorOpModeLinear.java
@@ -33,7 +33,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.hardware.Elevator;
 
 
 /**
@@ -70,21 +74,15 @@ public class ElevatorOpModeLinear extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    private Elevator elevator = null;
+
 
     @Override
     public void runOpMode() {
 
         // Initialize the hardware variables. Note that the strings used here must correspond
         // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_  front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-
+        elevator = new Elevator(hardwareMap, telemetry);
         // ########################################################################################
         // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
         // ########################################################################################
@@ -95,10 +93,7 @@ public class ElevatorOpModeLinear extends LinearOpMode {
         // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
         // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
         // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        elevator.initMotors();
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
@@ -109,31 +104,19 @@ public class ElevatorOpModeLinear extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max;
+            double power;
 
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
+
+            power  = -gamepad1.left_stick_y ;
+            elevator.setElevatorMotorPower(power);
 
             // Combine the joystick requests for each axis-motion to determine each wheel's power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
+
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
             }
 
             // This is test code:
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Elevator.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+public class Elevator {
+    private Servo mainServo = null;
+    private Servo rightServo = null;
+    private DcMotor elevatorMotor = null;
+    private Telemetry telemetry;
+
+    public Elevator(HardwareMap hardwareMap, Telemetry telemetry){
+        mainServo = hardwareMap.get(Servo.class, "elevator_main_servo");
+        rightServo = hardwareMap.get(Servo.class, "elevator_right_servo");
+        elevatorMotor = hardwareMap.get(DcMotor.class, "elevator_motor");
+        this.telemetry = telemetry;
+    }
+
+    public void initMotors(){
+        elevatorMotor.setDirection(DcMotor.Direction.FORWARD);
+        mainServo.setPosition(Servo.MIN_POSITION);
+        rightServo.setPosition(Servo.MAX_POSITION);
+    }
+    public void setElevatorMotorPower(double power){
+        elevatorMotor.setPower(power);
+        telemetry.addData("position", elevatorMotor.getCurrentPosition());
+        telemetry.addData("terget position", elevatorMotor.getTargetPosition());
+    }
+
+}
