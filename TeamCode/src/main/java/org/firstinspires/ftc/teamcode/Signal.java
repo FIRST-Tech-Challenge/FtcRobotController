6ca6ccfba1deaa3f52720d3a1ccaaa8224b88e5e
Dimitri Epling <depling931@gmail.com>
2022-11-09 16:21:11 -0800

    Change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Signal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Signal.java
@@ -1,28 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.View;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-
-import java.util.Locale;
-
-@Autonomous(name="BlueSpinner", group="Autonomous")
+@Autonomous(name="Grnd_Junc_Sig_Prk101", group="Autonomous")
 //@Disabled
-public class BlueSpinner extends LinearOpMode {
+public class Signal extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor lf = null;  //left front wheel
     private DcMotor rf = null;  //right front wheel
@@ -30,9 +16,7 @@ public class BlueSpinner extends LinearOpMode {
     private DcMotor rb = null;  //right back wheel
     private DcMotor tower1 = null; //arm motor 1
     private DcMotor tower2 = null; //arm motor 2
-    private Servo clawservo = null; //clawservo
-    private DcMotor duckspinnerL = null; //duckspinnerL
-    private DcMotor duckspinnerR = null; //duckspinnerR
+
 
     @Override
     public void runOpMode() {
@@ -45,20 +29,41 @@ public class BlueSpinner extends LinearOpMode {
         rb = hardwareMap.get(DcMotor.class, "rb");
         tower1 = hardwareMap.get(DcMotor.class, "tower1");
         tower2 = hardwareMap.get(DcMotor.class, "tower2");
-        clawservo = hardwareMap.get(Servo.class, "clawservo");
-        duckspinnerL = hardwareMap.get(DcMotor.class, "duckspinnerL");
-        duckspinnerR = hardwareMap.get(DcMotor.class, "duckspinnerR");
 
-        lf.setDirection(DcMotor.Direction.FORWARD);
+        double sidemult = -1.0; //Red side = 1.0 Blue = -1.0
+
+
+        lf.setDirection(DcMotor.Direction.FORWARD);//do we need?
         rf.setDirection(DcMotor.Direction.REVERSE);
         lb.setDirection(DcMotor.Direction.FORWARD);
         rb.setDirection(DcMotor.Direction.REVERSE);
         tower1.setDirection(DcMotor.Direction.FORWARD);
-        tower2.setDirection(DcMotor.Direction.REVERSE);
+        tower2.setDirection(DcMotor.Direction.FORWARD);
+
 
         waitForStart();
         runtime.reset();
-        // while (opModeIsActive() && (runtime.seconds() < 30.0)) {
 
+        while (opModeIsActive() && (runtime.seconds() < 30.0)) {
+
+            tower1.setPower(0);
+            tower2.setPower(0);
+            lf.setPower(0);
+            rf.setPower(0);
+            lb.setPower(0);
+            rb.setPower(0);
+
+            sleep(20000); // Wait for 20 Seconds
+
+
+            tower1.setPower(0);
+            tower2.setPower(0);
+            lf.setPower(-0.5 * sidemult);
+            rf.setPower(-0.5 * sidemult);
+            lb.setPower(-0.5 * sidemult);
+            rb.setPower(-0.5 * sidemult);
+
+            sleep(250); // Forward to low junction
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/forward.java
@@ -2,31 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-<<<<<<< Updated upstream
-import com.qualcomm.robotcore.hardware.CRServo;
 //import com.qualcomm.robotcore.hardware.;
-=======
-c
->>>>>>> Stashed changes
 
 
 @TeleOp(name="forward", group="Teleop")
 //@Disabled
 public class forward extends LinearOpMode {
-
-    private DcMotor lf = null;  //left front wheel
-    private DcMotor rf = null;  //right front wheel
-    private DcMotor lb = null;  //left back wheel
-    private DcMotor rb = null;  //right back wheel
-    private DcMotor tower1 = null; //arm motor 1
-    private DcMotor tower2 = null; //arm motor 2
-    private Servo clawservo = null; //clawservo
-    private DcMotor duckspinnerL = null; //duckspinnerL
-    private DcMotor duckspinnerR = null; //duckspinnerR
 
     @Override
     public void runOpMode() {
@@ -34,96 +16,78 @@ public class forward extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        lf = hardwareMap.get(DcMotor.class, "lf");
-        rf = hardwareMap.get(DcMotor.class, "rf");
-        lb = hardwareMap.get(DcMotor.class, "lb");
-        rb = hardwareMap.get(DcMotor.class, "rb");
-        tower1 = hardwareMap.get(DcMotor.class, "tower1");
-        tower2 = hardwareMap.get(DcMotor.class, "tower2");
-        clawservo = hardwareMap.get(Servo.class, "clawservo");
-        duckspinnerL = hardwareMap.get(DcMotor.class, "duckspinnerL");
-        duckspinnerR = hardwareMap.get(DcMotor.class, "duckspinnerR");
+        //left front wheel
+        DcMotor lf = hardwareMap.get(DcMotor.class, "lf");
+        //right front wheel
+        DcMotor rf = hardwareMap.get(DcMotor.class, "rf");
+        //left back wheel
+        DcMotor lb = hardwareMap.get(DcMotor.class, "lb");
+        //right back wheel
+        DcMotor rb = hardwareMap.get(DcMotor.class, "rb");
+        //arm motor 1
+        DcMotor tower1 = hardwareMap.get(DcMotor.class, "tower1");
+        //arm motor 2
+        DcMotor tower2 = hardwareMap.get(DcMotor.class, "tower2");
 
         lf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rf.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        lf.setDirection(DcMotor.Direction.FORWARD);
-        rf.setDirection(DcMotor.Direction.REVERSE);
-        lb.setDirection(DcMotor.Direction.FORWARD);
-        rb.setDirection(DcMotor.Direction.REVERSE);
+        lf.setDirection(DcMotor.Direction.REVERSE);
+        rf.setDirection(DcMotor.Direction.FORWARD);
+        lb.setDirection(DcMotor.Direction.REVERSE);
+        rb.setDirection(DcMotor.Direction.FORWARD);
         tower1.setDirection(DcMotor.Direction.FORWARD);
         tower2.setDirection(DcMotor.Direction.FORWARD);
-        duckspinnerL.setDirection(DcMotor.Direction.FORWARD);
-        duckspinnerR.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         gamepad1.rumble(1000);
         gamepad2.rumble(1000);
         while (opModeIsActive()) {
 
-            double powerMult;
-            double lPower;
-            double rPower;
-            double towerPower;
-            double deadzone;
-            double towerPower2;
+            if (gamepad1.right_bumper) {
+                // right bumper, "sprint mode"
+                lf.setPower(0.75);
+                rf.setPower(0.75);
+                lb.setPower(0.75);
+                rb.setPower(0.75);
+            } else {
+                // normal power
+                lf.setPower(0.5);
+                rf.setPower(0.5);
+                lb.setPower(0.5);
+                rb.setPower(0.5);
 
-            //powerMult = 0.8;
-            lPower = 0.0f;
-            rPower = 0.0f;
-            towerPower = 0.0f;
-            deadzone = 0.2f;
-            towerPower2 = 0.0f;
+                double lPower;
+                double towerPower;
+                double towerPower2;
 
-            lPower = gamepad1.left_stick_y;
-            rPower = gamepad1.right_stick_y;
-            towerPower = gamepad2.right_trigger;
-            towerPower2 = gamepad2.left_trigger;
+                //powerMult = 0.8;
+                double deadzone;
+                deadzone = 0.2f;
 
-            if (gamepad2.a) {
-                clawservo.setPosition(0.0);
+                lPower = gamepad1.left_stick_y;
+                double rPower = gamepad1.right_stick_y;
+                towerPower = gamepad2.right_trigger;
+                towerPower2 = gamepad2.left_trigger;
+
+                if (towerPower <= deadzone) {
+                    towerPower = 0.0f;
+                }
+                if (towerPower2 <= deadzone) {
+                    towerPower2 = 0.0f;
+                }
+
+                lf.setPower(lPower);
+                rf.setPower(rPower);
+                lb.setPower(lPower);
+                rb.setPower(rPower);
+                tower1.setPower((towerPower - towerPower2) * 0.5);
+                tower2.setPower((towerPower - towerPower2) * 0.5);
+
+
             }
-            if (gamepad2.b) {
-                clawservo.setPosition(.75);
-            }
-
-            if (towerPower <= deadzone) {
-                towerPower = 0.0f;
-            }
-            if (towerPower2 <= deadzone) {
-                towerPower2 = 0.0f;
-            }
-
-            if (gamepad2.x) {
-                duckspinnerL.setPower(1);
-                duckspinnerR.setPower(1);
-            }else {
-                duckspinnerL.setPower(0);
-            }
-
-            if (gamepad2.y) {
-                duckspinnerR.setPower(-1);
-                duckspinnerL.setPower(-1);
-
-            }else {
-                duckspinnerR.setPower(0);
-            }
-
-            lf.setPower(lPower * 1.0);
-            rf.setPower(rPower * 1.0);
-<<<<<<< Updated upstream
-            lb.setPower(lPower * 1.0);
-            rb.setPower(rPower * 1.0);
-=======
-            lb.setPower(lPower * 0.8);
-            rb.setPower(rPower * 0.8);
->>>>>>> Stashed changes
-            tower1.setPower((towerPower - towerPower2) * 0.5);
-            tower2.setPower((towerPower - towerPower2) * 0.5);
-
-
         }
     }
 }
