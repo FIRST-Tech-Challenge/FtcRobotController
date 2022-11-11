f8b4863424e424bf193a92b005af7e8a9bd4ba5b
Sanj8923 <91356812+Sanj8923@users.noreply.github.com>
2022-11-10 19:40:30 -0800

    Update Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseAutonomous.java
    Co-authored-by: Malavika Venkatesan <17712433+malavika1@users.noreply.github.com>

--- Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseAutonomous.java
+++ Team8923_PowerPlay/src/main/java/org/firstinspires/ftc/team8923_PowerPlay/BaseAutonomous.java
@@ -187,7 +187,8 @@ public abstract class BaseAutonomous extends ConceptTensorFlowObjectDetectionWeb
         motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
-    //using imu
+    
+    // using imu
     public void imuPivot(double referenceAngle, double targetAngle, double maxSpeed, double kAngle, double timeout) {
         runtime.reset();
         //counter-clockwise is positive
