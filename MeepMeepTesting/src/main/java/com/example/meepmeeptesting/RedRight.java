54a26d6245b32aee7805eac85167964f68372430
CG-Spectre <aidanta01@gmail.com>
2022-11-11 21:20:11 -0600

    RL&RR

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedRight.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedRight.java
@@ -5,27 +5,48 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+//meep meep example create your own class
 public class RedRight {
     public static void main(String[] args) {
-        int signalZone = 3;
+        //placeholder signal zone
+        int signalZone = 1;
         MeepMeep meepMeep = new MeepMeep(800);
         RoadRunnerBotEntity myBot = null;
 
         switch (signalZone){
             case 1:
-                // done
                 System.out.println("Going to 1");
                 myBot = new DefaultBotBuilder(meepMeep)
                         // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                        .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                        .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(0), 15)
                         .setDimensions(14, 10.5)
                         .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 61, Math.toRadians(-180)))
-                                        .forward(12)
-                                        .turn(Math.toRadians(90))
-                                        .forward(3)
+                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 64.75, Math.toRadians(270)))
+                                        /*
+                                        .strafeRight(25)
+                                        .forward(30)
+                                        .strafeLeft(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .strafeRight(13)
+                                        .back(30)
+                                        .strafeLeft(50)
+                                        .forward(30)
+                                        .build()
+                                         */
+
+                                        .forward(17.5)
                                         .turn(Math.toRadians(-90))
-                                        .forward(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .turn(Math.toRadians(-90))
+                                        .forward(17.5)
+                                        .turn(Math.toRadians(-90))
+                                        .forward(25)
+                                        .turn(Math.toRadians(-90))
+                                        .forward(25)
                                         .build()
                         );
                 break;
@@ -36,14 +57,34 @@ public class RedRight {
                         .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                         .setDimensions(14, 10.5)
                         .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 61, Math.toRadians(-180)))
-                                        .forward(12)
-                                        .turn(Math.toRadians(90))
-                                        .forward(3)
+                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 64.75, Math.toRadians(270)))
+
+                                        /*
+                                        .strafeRight(25)
+                                        .forward(30)
+                                        .strafeLeft(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .strafeRight(13)
+                                        .forward(25)
+                                        .strafeLeft(25)
+                                        .build()
+                                        */
+
+                                        .forward(17.5)
                                         .turn(Math.toRadians(-90))
-                                        .forward(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .turn(Math.toRadians(-90))
+                                        .forward(17.5)
                                         .turn(Math.toRadians(90))
-                                        .forward(27)
+                                        .forward(25)
+                                        .turn(Math.toRadians(90))
+                                        .forward(55)
+                                        .turn(Math.toRadians(90))
+                                        .forward(30)
                                         .build()
                         );
                 break;
@@ -54,14 +95,37 @@ public class RedRight {
                         .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                         .setDimensions(14, 10.5)
                         .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 61, Math.toRadians(-180)))
-                                        .forward(12)
-                                        .turn(Math.toRadians(90))
-                                        .forward(3)
+                                drive.trajectorySequenceBuilder(new Pose2d(35.5, 64.75, Math.toRadians(270)))
+                                        /*
+                                        .strafeRight(25)
+                                        .forward(30)
+                                        .strafeLeft(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .strafeRight(13)
+                                        //.forward(25)
+                                        //.strafeLeft(50)
+                                         */
+
+
+                                        /*.strafeRight(12)
+                                        .forward(10)
+                                        //use claw
+                                        .back(10)*/
+
+                                        .forward(17.5)
                                         .turn(Math.toRadians(-90))
-                                        .forward(13)
+                                        .forward(4)
+                                        //use claw
+                                        .back(4)
+                                        .turn(Math.toRadians(-90))
+                                        .forward(17.5)
                                         .turn(Math.toRadians(90))
-                                        .forward(45)
+                                        .forward(25)
+                                        .turn(Math.toRadians(90))
+                                        .forward(55)
+                                        .turn(Math.toRadians(90))
                                         .build()
                         );
 
@@ -77,5 +141,8 @@ public class RedRight {
                     .addEntity(myBot)
                     .start();
         }
+
     }
 }
+
+
