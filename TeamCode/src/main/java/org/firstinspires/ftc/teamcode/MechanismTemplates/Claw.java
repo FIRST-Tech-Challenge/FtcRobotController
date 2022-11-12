9e78569d19ab596ea2bf707714c9ae4a06d84e17
MushiTea <94145946+MushiTea@users.noreply.github.com>
2022-11-12 00:12:35 -0600

    death

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
@@ -3,57 +3,73 @@ package org.firstinspires.ftc.teamcode.MechanismTemplates;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
-public class Claw{
+import org.firstinspires.ftc.teamcode.SignalEdgeDetector;
+
+import java.util.function.BooleanSupplier;
+
+public class Claw {
     public Servo wristJoint;
     private Servo clawJoint; // public because a button must set the wrist to the intake position regardless of its status
-    private boolean isOpen;
-    private boolean isWristIntakePosition;
     private boolean isAuto;
 
     // claw positions
     private final double OPEN = 0.77;
-    private final double CLOSE = 0.6759;
+    private final double CLOSE = 0.6;
 
     // wrist positions
     public final double WRIST_INTAKE_POSITION = 0.929; // wrist rotates to intake cone, greater values move clockwise, less move counterclockwise
     private final double WRIST_EXTAKE_POSITION = 0.268; // wrist rotates to extake on junction
 
-    public Claw(HardwareMap hardwareMap, boolean isAuton ){
+    SignalEdgeDetector isOpen;
+    SignalEdgeDetector isIntakePosition;
+
+    public Claw(HardwareMap hardwareMap, boolean isAuton, BooleanSupplier rightBumper, BooleanSupplier aButton) {
         isAuto = isAuton;
-        wristJoint = hardwareMap.get(Servo.class,"WRIST"); // Pin 0
+
+        wristJoint = hardwareMap.get(Servo.class, "WRIST"); // Pin 0
         clawJoint = hardwareMap.get(Servo.class, "CLAW"); // Pin 1
-        isOpen = true; // our claw begins in the open position
+        //isOpen = true; // our claw begins in the open position
 
-        if(isAuto)
-        {
+        if (isAuto) {
             clawJoint.setPosition(CLOSE);
             wristJoint.setPosition(WRIST_EXTAKE_POSITION);
-            isWristIntakePosition = false; // our wrist does NOT start at the intake position in auto
-        }
-        else{ // if it is teleOp
+            //isWristIntakePosition = false; // our wrist does NOT start at the intake position in auto
+        } else { // if it is teleOp
             clawJoint.setPosition(OPEN);
             wristJoint.setPosition(WRIST_INTAKE_POSITION);
-            isWristIntakePosition = true; // our wrist starts at the intake position in tele
+            //isWristIntakePosition = true; // our wrist starts at the intake position in tele
+        }
+
+        isOpen = new SignalEdgeDetector(rightBumper);
+        isIntakePosition = new SignalEdgeDetector(aButton);
+    }
+
+    private boolean clawToggled;
+
+    public void toggleOpenClose() {
+        if (isOpen.isRisingEdge()) {
+            if (clawToggled) {
+                clawJoint.setPosition(CLOSE);
+            } else {
+                clawJoint.setPosition(OPEN);
+            }
+            clawToggled = !clawToggled;
         }
     }
 
-    public void toggleOpenClose(){
-        if(isOpen){
-            clawJoint.setPosition(CLOSE);
-        }
-        else{
-            clawJoint.setPosition(OPEN);
-        }
-        isOpen = !isOpen; // we want to switch the condition of `isOpen` every time this method is called
-    }
+    private boolean wristInExtakePosition;
 
-    public void toggleWristRotate(){ // would be used at the beginning of the goToJunction method in the arm class, for example
-        if(isWristIntakePosition) {
+    public void toggleWristRotate() { // would be used at the beginning of the goToJunction method in the arm class, for example
+        if (wristInExtakePosition) {
+            wristJoint.setPosition(WRIST_INTAKE_POSITION);
+        } else { // if the wrist is in the extake position, switch it back to the intake position so it can pick up cones
             wristJoint.setPosition(WRIST_EXTAKE_POSITION);
         }
-        else { // if the wrist is in the extake position, switch it back to the intake position so it can pick up cones
-            wristJoint.setPosition(WRIST_INTAKE_POSITION);
-        }
-        isWristIntakePosition = !isWristIntakePosition;
+        wristInExtakePosition = !wristInExtakePosition;
+    }
+
+    public void update() {
+        isOpen.update();
+        isIntakePosition.update();
     }
 }
