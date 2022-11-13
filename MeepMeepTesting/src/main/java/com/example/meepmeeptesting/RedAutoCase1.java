8107cd4d91ef59c7bbedf3d73124a69e74351fa9
Tanish Tyagi <66917087+23tyagit@users.noreply.github.com>
2022-11-13 15:07:57 -0500

    red auto case 2 sim

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedAutoCase1.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedAutoCase1.java
@@ -1,3 +1,5 @@
+// go to close junction to drop off preloaded; score at 2 high junctions
+
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
@@ -6,18 +8,19 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-
-public class RedAutoSimulationCase1 {
+public class RedAutoCase1 {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(16, 16)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-12, -62, Math.toRadians(90)))
-                                .lineTo(new Vector2d(-12, -24))
-                                .splineTo(new Vector2d(-52,-12), Math.toRadians(180))
+                        drive.trajectorySequenceBuilder(new Pose2d(12, -62, Math.toRadians(90)))
+                                .lineTo(new Vector2d(12, -24))
+                                .splineTo(new Vector2d(52,-12), Math.toRadians(0))
+                                .lineTo(new Vector2d(20, -12))
                                 .build()
                 );
 
