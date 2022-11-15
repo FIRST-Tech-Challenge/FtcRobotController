837852915b87da6276f0a0d88fbd0599c9f76374
MedinaSergio4 <medinasergio@students.lovingtonschools.net>
2022-11-15 16:56:56 -0700

    Added the code for april tags Set up gradle for OpenCV Added tater as hardware start

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/Testing.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.HardwarePushbot;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name="autonomous", group="Test")
+public class Testing extends automethods {
+    HardwarePushbot robot = new HardwarePushbot();// Use a Pushbot's hardware
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        robot.autoinit(hardwareMap);
+
+
+
+        while(!isStarted( ) && !isStopRequested()){
+            telemetry.update();
+
+            sleep(50);
+        }
+
+////////////////////////////////////ROBOT  START////////////////////////////////////////////////////
+
+
+        encoderDrive(.5, 80, 6);
+
+
+
+////////////////////////////////
+
+
+    }
+}
