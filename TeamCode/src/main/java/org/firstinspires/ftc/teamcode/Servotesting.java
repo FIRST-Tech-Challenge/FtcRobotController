77d2ee586e2cccc0f98642fb70cc3c043507bb59
rodoggyro <Fiyrflash@gmail.com>
2022-11-16 13:39:54 -0500

    working on tele op

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesting.java
@@ -8,6 +8,13 @@ import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp
 public class Servotesting extends LinearOpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+
     private CRServo Right;
     private CRServo Left;
     private DcMotor Crain;
@@ -15,24 +22,68 @@ public class Servotesting extends LinearOpMode {
 
 
     public void runOpMode() throws InterruptedException {
+
+
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
         Right = hardwareMap.get(CRServo.class,"Rights");
         Left = hardwareMap.get(CRServo.class, "Lefts");
         Crain = hardwareMap.get(DcMotor.class, "Crain");
         Spin = hardwareMap.get(DcMotor.class, "Spin");
 
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
         waitForStart();
         while (opModeIsActive()) {
+            double turn;
+            double throttle;
+            boolean strafeLeft;
+            boolean strafeRight;
 
             boolean pickup;
             boolean dropoff;
             double crainpower;
             boolean turning;
 
+            throttle = gamepad1.left_stick_y;
+            turn = gamepad1.right_stick_x;
+            strafeLeft = gamepad1.left_bumper;
+            strafeRight = gamepad1.right_bumper;
+
             crainpower = gamepad2.left_stick_y;
             pickup = gamepad2.x;
             dropoff = gamepad2.y;
             turning = gamepad2.b;
 
+            if (strafeLeft) {
+                frontLeft.setPower(-0.75);
+                frontRight.setPower(0.75);
+                backLeft.setPower(0.75);
+                backRight.setPower(-0.75);
+            } else if (strafeRight) {
+                frontLeft.setPower(-0.75);
+                frontRight.setPower(0.75);
+                backLeft.setPower(0.75);
+                backRight.setPower(-0.75);
+            }
+
+
+            frontLeft.setPower(throttle);
+            frontRight.setPower(throttle);
+            backLeft.setPower(throttle);
+            backRight.setPower(throttle);
+
+            frontLeft.setPower(-turn);
+            frontRight.setPower(turn);
+            backLeft.setPower(-turn);
+            backRight.setPower(turn);
 
             Crain.setPower(crainpower);
 
