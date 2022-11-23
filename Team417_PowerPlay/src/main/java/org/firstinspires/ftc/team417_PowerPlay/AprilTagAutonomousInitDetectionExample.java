b3eb82a249461f06651489167c31e048b7abf2c0
Sawar Saini <91710363+sawarsaini3268@users.noreply.github.com>
2022-11-22 19:13:47 -0800

    Update Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/AprilTagAutonomousInitDetectionExample.java
    Co-authored-by: Malavika Venkatesan <17712433+malavika1@users.noreply.github.com>

--- Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/AprilTagAutonomousInitDetectionExample.java
+++ Team417_PowerPlay/src/main/java/org/firstinspires/ftc/team417_PowerPlay/AprilTagAutonomousInitDetectionExample.java
@@ -99,9 +99,7 @@ public class AprilTagAutonomousInitDetectionExample extends LinearOpMode {
                     }
                 }
 
-
-                if(tagFound)
-                {
+                if (tagFound) {
                     telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
                     tagToTelemetry(tagOfInterest);
                 }
