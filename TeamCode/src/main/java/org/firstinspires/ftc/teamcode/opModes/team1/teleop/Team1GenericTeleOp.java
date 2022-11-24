281a1d254b75f8eb7a10f6b9b94c0ba2b1222d31
micnekr <misha.nekrasov@gmail.com>
2022-11-24 17:34:25 +0000

    Got FTCLib to work, added sample teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1GenericTeleOp.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opModes.team1.teleop;
+
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TeamColour;
+
+public class Team1GenericTeleOp {
+    TeamColour teamColour;
+    public void setup(TeamColour teamColor){
+        this.teamColour = teamColor;
+    }
+
+    public void every_tick(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1RedTeleOp.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.opModes.team1.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TeamColour;
+
+@TeleOp(name="Team 1 red", group="Team 1")
+public class Team1RedTeleOp extends TeleOpModeBase {
+    Team1GenericTeleOp teleop;
+
+    @Override
+    public void setup() {
+        teleop = new Team1GenericTeleOp();
+        teleop.setup(TeamColour.RED);
+    }
+
+    @Override
+    public void every_tick() {
+        teleop.every_tick();
+    }
+}
