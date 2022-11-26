832593ea3893bb61db8f245b770b4d699d0c9eac
MostlyOperational18119 <ftc18119@gmail.com>
2022-11-26 12:30:04 -0800

    created untested RRMid auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRMid.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RRMid.java
@@ -9,25 +9,41 @@ public class RRMid extends DriveMethods{
     public void runOpMode() {
         initMotorsBlue();
 
-//        clawClamp();
-//        GoToHeight(1950);
-//        sleep(500);
+        clawClamp();
+        sleep(500);
 
         waitForStart();
 
-        driveForDistance(0.1, Variables.Direction.BACKWARD, 0.35, 0);
-        driveForDistance(1.28, Variables.Direction.LEFT, 0.35, 0);
-        driveForDistance(0.1, Variables.Direction.FORWARD, 0.35, 0);
+        driveForDistance(0.6, Variables.Direction.RIGHT,0.35,0);
+        driveForDistance(0.6, Variables.Direction.FORWARD,0.35,0);
+        driveForDistance(0.33, Variables.Direction.LEFT, 0.35,0);
+        goToMid();
+        sleep(500);
+        driveForDistance(0.15, Variables.Direction.FORWARD,0.35,0);
+        clawRelease();
+        sleep(200);
+        driveForDistance(0.15, Variables.Direction.BACKWARD,0.35,0);
+        goToDown();
+        sleep(500);
+        driveForDistance(0.33, Variables.Direction.RIGHT, 0.35,0);
+        driveForDistance(0.6, Variables.Direction.BACKWARD,0.35,0);
+        driveForDistance(2.5, Variables.Direction.LEFT,0.35,0);
 
-//        clawRelease();
 
-        driveForDistance(0.1, Variables.Direction.BACKWARD, 0.35, 0);
 
-//        sleep(1000);
-//        GoToHeight(0);
-
-        driveForDistance(1.28, Variables.Direction.RIGHT, 0.35, 0);
-        driveForDistance(1, Variables.Direction.BACKWARD, 0.35, 0);
+//        driveForDistance(0.1, Variables.Direction.BACKWARD, 0.35, 0);
+//        driveForDistance(1.28, Variables.Direction.LEFT, 0.35, 0);
+//        driveForDistance(0.1, Variables.Direction.FORWARD, 0.35, 0);
+//
+////        clawRelease();
+//
+//        driveForDistance(0.1, Variables.Direction.BACKWARD, 0.35, 0);
+//
+////        sleep(1000);
+////        GoToHeight(0);
+//
+//        driveForDistance(1.28, Variables.Direction.RIGHT, 0.35, 0);
+//        driveForDistance(1, Variables.Direction.BACKWARD, 0.35, 0);
 
         while (opModeIsActive()) {
 
