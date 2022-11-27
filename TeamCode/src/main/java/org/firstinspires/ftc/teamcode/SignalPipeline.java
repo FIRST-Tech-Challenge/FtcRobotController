321a41766c917a9d9572483466cdff5de42a1bfd
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2022-11-27 12:41:33 -0800

    Countour detection only happens in search zone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SignalPipeline.java
@@ -13,22 +13,28 @@ import java.util.ArrayList;
 public class SignalPipeline extends OpenCvPipeline {
     private int width = 640;
     private int height = 480;
-    private int cornerSearchSize = 100;
+    private int searchWidth = 100;
     
     private Mat hsv = new Mat();
+    private Mat hsvSub = new Mat();
     private Mat gray = new Mat();
+    private Mat graySub = new Mat();
     private Mat canny = new Mat();
     private Mat display = new Mat();
     private Mat contourHierarchy = new Mat();
 
-    // Points to determine what area we are looking in
+    // Points to determine what area we are searching in
     Point center = new Point(width / 2.0, height / 2.0);
-    Point searchTopLeft = new Point(center.x - cornerSearchSize / 2.0, center.y - cornerSearchSize / 2.0);
-    Point searchBottomRight = new Point(center.x + cornerSearchSize / 2.0, center.y + cornerSearchSize / 2.0);
+    Point searchTopLeft = new Point(center.x - searchWidth / 2.0, center.y - searchWidth / 2.0);
+    Point searchBottomRight = new Point(center.x + searchWidth / 2.0, center.y + searchWidth / 2.0);
     
     private void updateConvertedMats(Mat input) {
-        Imgproc.cvtColor(input, hsv, Imgproc.COLOR_RGB2HSV);
-        Imgproc.cvtColor(input, gray, Imgproc.COLOR_RGB2GRAY);
+        // Create sub matrices for our search zone
+        hsvSub = input.submat(new Rect(searchTopLeft, searchBottomRight));
+        graySub = input.submat(new Rect(searchTopLeft, searchBottomRight));
+        
+        Imgproc.cvtColor(hsvSub, hsv, Imgproc.COLOR_RGB2HSV);
+        Imgproc.cvtColor(graySub, gray, Imgproc.COLOR_RGB2GRAY);
     }
     
     private void determineState() {
@@ -49,13 +55,12 @@ public class SignalPipeline extends OpenCvPipeline {
         Imgproc.Canny(gray, canny, 100, 200);
         ArrayList<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(canny, contours, contourHierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_TC89_KCOS);
-        
-        // Create a mask from our main contour
 
         determineState();
 
         // Display what we are looking at, and any debug info
-        Imgproc.cvtColor(gray, display, Imgproc.COLOR_GRAY2RGB);
+        display = input;
+        //Imgproc.cvtColor(input, display, Imgproc.COLOR_GRAY2RGB);
         Imgproc.rectangle(display, new Rect(searchTopLeft, searchBottomRight), new Scalar(0, 255, 0));
             
         Imgproc.drawContours(display, contours, -1, new Scalar(255, 255));
