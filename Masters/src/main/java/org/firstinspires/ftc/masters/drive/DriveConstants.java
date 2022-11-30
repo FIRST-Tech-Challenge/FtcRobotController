aa613335e7bef46ab370671c35daf0dbf16d3579
karine josien <karine.josien@domo.com>
2022-11-30 10:18:50 -0700

    tuning

--- Masters/src/main/java/org/firstinspires/ftc/masters/drive/DriveConstants.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/drive/DriveConstants.java
@@ -46,8 +46,8 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.8; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.5; // in
+    public static double GEAR_RATIO = 1*63/61.09; // output (wheel) speed / input (motor) speed
+    public static double TRACK_WIDTH = 13.8; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-//    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kV = 1.0;
-    public static double kA = 0;
+    //public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kV = 0.02;
+    public static double kA = 0.003;
     public static double kStatic = 0;
 
     /*
@@ -68,9 +68,9 @@ public class DriveConstants {
      * inches.
      */
     // public static double MAX_VEL = 100;
-    public static double MAX_VEL = 47;
+    public static double MAX_VEL = 35;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(180);
+    public static double MAX_ANG_VEL = 4.462;//Math.toRadians(180);
     public static double MAX_ANG_ACCEL = Math.toRadians(180);
 
     public static double encoderTicksToInches(double ticks) {
--- Masters/src/main/java/org/firstinspires/ftc/masters/drive/opmode/TurnTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/drive/opmode/TurnTest.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.masters.drive.SampleMecanumDrive;
 //@Disabled
 @Autonomous(group = "drive")
 public class TurnTest extends LinearOpMode {
-    public static double ANGLE = 180; // deg
+    public static double ANGLE = 90; // deg
 
     @Override
     public void runOpMode() throws InterruptedException {
