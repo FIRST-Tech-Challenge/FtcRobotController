b3bd2f91e15ec97cafb60f5bae9718ac9b656afb
BlockBeasts <kjosien1@gmail.com>
2022-11-30 16:07:32 -0700

    commit for merge

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/TeleopGOODOneController.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/TeleopGOODOneController.java
@@ -24,6 +24,7 @@ public class TeleopGOODOneController extends LinearOpMode {
         motorBackLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         boolean move = false;
+        linearslide.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
