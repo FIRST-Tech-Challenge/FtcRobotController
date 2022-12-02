6a2f2d6a92cb89c07f05e1f5626059c8dc5254b7
Gobln <caasier42@gmail.com>
2022-12-02 15:21:50 -0700

    bye bye errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/liftTest.java
@@ -89,10 +89,6 @@ public class LiftTest extends LinearOpMode {
                 liftMotor.setPower(0);
             }
             // Show the elapsed game time and wheel power.
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.update();
         }
     }
 }
