6b45ae11d595a2d77fe3097f62178250ae9a85fa
Fiyrflash <Fiyrflash@gmail.com>
2022-12-01 21:33:50 -0500

    done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop.java
@@ -3,11 +3,10 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp
-public class Servotesting extends LinearOpMode {
+public class teleop extends LinearOpMode {
 
     private DcMotor frontLeft;
     private DcMotor frontRight;
