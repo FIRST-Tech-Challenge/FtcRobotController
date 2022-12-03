310d827343a917ef7c073c6f942a9c86a1ae9645
aryansinha <WestRidgeRobotics@gmail.com>
2022-12-02 19:04:35 -0600

    Encoders fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autov1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/autov1.java
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import static org.firstinspires.ftc.teamcode.config.DriveUtils.*;
-import static org.firstinspires.ftc.teamcode.config.DriveUtils.logData;
-import static org.firstinspires.ftc.teamcode.config.DriveUtils.encoderDrive;
 
 import org.firstinspires.ftc.teamcode.config.BaseOpMode;
 import org.firstinspires.ftc.teamcode.config.DriveUtils;
@@ -14,13 +12,13 @@ import org.firstinspires.ftc.teamcode.config.Hardware2;
 public class autov1 extends BaseOpMode {
     Hardware2 robot = new Hardware2(true);
     public void runOpMode() throws InterruptedException {
+        robot.initTeleOpIMU(hardwareMap);
         waitForStart();
-        while(opModeIsActive()){
-            DriveUtils.encoderDrive(this, 0.5, 30,30, 7);
-            DriveUtils.encoderDrive(this, -0.5, 10, 10, 7);
-        }
-    }
 
+        DriveUtils.encoderDrive(this, 0.5, 30,30, 7);
+        DriveUtils.encoderDrive(this, -0.5, 10, 10, 7);
+    }
+    @Override
     public Hardware2 getRobot() {
         return robot;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/BaseOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/config/BaseOpMode.java
@@ -3,7 +3,5 @@ package org.firstinspires.ftc.teamcode.config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public abstract class BaseOpMode extends LinearOpMode {
-    public Hardware2 getRobot() {
-        return null;
-    }
+    public abstract Hardware2 getRobot();
 }
