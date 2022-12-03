9ac70346fbbaf015e24aeffb8e6f0433e0830930
Alphalab <equipe@alphalab8067.ndl.qc.ca>
2022-12-03 13:08:15 -0500

    Version ArcadeDrive de base (avec AlphaLab)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveAutoCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DriveAutoCommand.java
@@ -36,6 +36,6 @@ public class DriveAutoCommand extends CommandBase {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return true;
+        return false;
     }
 }
