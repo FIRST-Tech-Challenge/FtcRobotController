a62c42f96f9323bc840144fab9f8e10d725d04a5
A Smith <ajs.mail.home@gmail.com>
2022-12-05 14:20:33 -0500

    OpMode Skeleton added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AndrewOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Andrew Mecanum Drive", group="Linear Opmode")
+public class AndrewOpMode extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+
+        while (opModeIsActive()) {
+
+
+        }
+    }
+}
