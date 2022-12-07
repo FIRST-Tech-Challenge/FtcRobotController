9f24134e9a82702d95512a81116dd4ce3c25b9f5
Avaninder Bhaghayath <charizardavi@gmail.com>
2022-12-07 16:24:31 -0500

    The androidManifest should be fixed for new android targets/ -Avaninder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-public class test {
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "ServoLimits")
+@Config
+
+public class test extends LinearOpMode {
+    public static double limitOne = 0.992;
+    public static double limitTwo = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo one = hardwareMap.get(Servo.class, "claw");
+
+        waitForStart();
+        while (opModeIsActive()) {
+            // one.setPosition(limitOne);
+            telemetry.addData("pos: ", one.getPosition());
+            telemetry.update();
+        }
+    }
 }
