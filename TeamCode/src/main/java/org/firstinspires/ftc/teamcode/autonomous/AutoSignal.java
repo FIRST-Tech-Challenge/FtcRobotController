2f01e03d7f2fdd3fcd8cac587e6bb6e7d81a1b8a
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2022-12-07 15:58:56 -0800

    Hopefully fix autonomous, added drivetrain motor tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoSignal.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoSignal.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode.autonomous;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitUntilCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.teamcode.commands.DetectSignal;
 import org.firstinspires.ftc.teamcode.systems.SignalSystem;
 
 @Autonomous(name = "Move to Signal Zone")
 public class AutoSignal extends CommandOpMode {
-    AutoPhase phase = AutoPhase.Waiting;
-    
     SignalSystem signalSystem;
 
     @Override
@@ -18,15 +18,13 @@ public class AutoSignal extends CommandOpMode {
         signalSystem = new SignalSystem(hardwareMap, "Webcam 1");
         
         schedule(
-                new InstantCommand(() -> signalSystem.startCamera()),
-                new WaitUntilCommand(this::isStarted),
-                new WaitUntilCommand(signalSystem::isResultReady),
+            new SequentialCommandGroup(
+                new DetectSignal(signalSystem),
                 new InstantCommand(() -> {
                     telemetry.addLine("Detected signal " + signalSystem.GetResult().name());
                     telemetry.update();
-                    
-                    signalSystem.stopCamera();
                 })
+            )
         );
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/DetectSignal.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.systems.SignalSystem;
+
+public class DetectSignal extends CommandBase {
+    private SignalSystem system;
+    
+    public DetectSignal(SignalSystem system) {
+        this.system = system;
+        
+        addRequirements(system);
+    }
+
+    @Override
+    public void initialize() {
+        system.startCamera();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        system.stopCamera();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return system.isResultReady();
+    }
+}
