db6b8909fe0bd4a2ea90578395bccf37451d45d1
owenp500 <opringle9@gmail.com>
2022-12-07 16:30:49 -0800

    commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RightCornerAuto.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class RightCornerAuto extends LinearOpMode {
+    DcMotor frontLeftMotor;
+    DcMotor frontRightMotor;
+    DcMotor backLeftMotor;
+    DcMotor backRightMotor;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
