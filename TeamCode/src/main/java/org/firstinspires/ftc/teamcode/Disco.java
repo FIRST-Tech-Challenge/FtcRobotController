6e70a9b4ec6bdc1167e52e8d9eba96647f36a9f7
Mr_Stoffer <49318416+mrstoffer@users.noreply.github.com>
2022-12-08 12:18:07 +0100

    Emergency changes LM1 (3/12/2022)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Disco.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Disco.java
@@ -33,8 +33,8 @@ public class Disco extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double y = gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
             double armPower = gamepad2.left_stick_y;
