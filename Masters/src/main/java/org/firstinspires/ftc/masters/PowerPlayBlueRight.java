7eabd4fe5e1369d4cbe7fe5db9950c9be8b5f12a
karine josien <karine.josien@domo.com>
2022-12-08 20:04:08 -0700

    autonomous

--- Masters/src/main/java/org/firstinspires/ftc/masters/PowerPlayBlueRight.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/PowerPlayBlueRight.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.masters;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.masters.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.masters.trajectorySequence.TrajectorySequence;
 
 import java.util.Date;
 
@@ -18,7 +21,9 @@ public class PowerPlayBlueRight extends LinearOpMode{
         PowerPlayComputerVisionPipelines.SleevePipeline.SleeveColor sleeveColor = null;
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-
+        Pose2d startPose = new Pose2d(new Vector2d(-36, 60), Math.toRadians(-90));
+        drive.setPoseEstimate(startPose);
+        drive.closeClaw();
         waitForStart();
 
         ElapsedTime elapsedTime = new ElapsedTime();
@@ -34,18 +39,49 @@ public class PowerPlayBlueRight extends LinearOpMode{
             telemetry.update();
         }
 
+        drive.setArmServoMiddle();
+
+        drive.liftTop();
+
+        TrajectorySequence startTo270Pole = drive.trajectorySequenceBuilder(startPose)
+                .splineToLinearHeading(new Pose2d( new Vector2d( -9,50), Math.toRadians(-90)), Math.toRadians(90))
+                .lineToLinearHeading(new Pose2d(new Vector2d(-8,27),Math.toRadians(-135)))
+//                .lineToLinearHeading(westPoleDeposit)
+                .build();
+        drive.followTrajectorySequence(startTo270Pole);
+
+        //use vision to align
+        //drop cone
+        drive.openClaw();
+        sleep(300);
+
+        TrajectorySequence back = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                .back(2)
+                .build();
+        drive.followTrajectorySequence(back);
+        drive.setArmServoBottom();
+        drive.turn(Math.toRadians(-45));
 
         //When parking
         switch (sleeveColor) {
             case RED:
+                TrajectorySequence park2 = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                        .strafeTo(new Vector2d(-36, 34))
+                        .build();
+                drive.followTrajectorySequence(park2);
                 //Parking 2
             case GRAY:
-                //Parking 1
+
             case GREEN:
+                TrajectorySequence park1 = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
+                        .strafeTo(new Vector2d(-62, 34))
+                        .build();
+                drive.followTrajectorySequence(park1);
                 //Parking 3
             case INDETERMINATE:
 
         }
+
     }
 
 }
