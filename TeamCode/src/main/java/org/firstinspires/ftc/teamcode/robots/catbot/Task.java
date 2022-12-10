04436470fedd12176840f59f7a3fd6c6955f576c
Digicat509 <106493589+Digicat509@users.noreply.github.com>
2022-12-10 13:54:03 -0600

    Update Task.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/catbot/Task.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/catbot/Task.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.robots.catbot;
 
 interface Task {
+    int TICKSPERTILE = 2500;
+    int TICKSPER90DEGREES = 1000;
+    int STRAFETICKSPERTILE = 2800;
     boolean run();
 }
