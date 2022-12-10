fc1acfe7d6926a7a0882b24105a4e38f91294f0f
TedArmenston <63930022+TedArmenston@users.noreply.github.com>
2022-12-10 11:32:50 -0800

    Tested, moves both motors semi correctly, however proportional control is not stopping it from over extending and carrying over

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiamArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiamArmTest.java
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 public class LiamArmTest extends DriveMethods {
     DcMotor motorScissor30;
     DcMotor motorScissor60;
+    int i = 0;
 
     @Override
     public void runOpMode() {
@@ -20,9 +21,18 @@ public class LiamArmTest extends DriveMethods {
         motorScissor60 = hardwareMap.get(DcMotor.class, "scissor60");
 
 
+        motorScissor60.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        motorScissor30.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motorScissor60.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        motorScissor30.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorScissor60.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
         waitForStart();
 
-        goToScissorHeight(200);
+        goToScissorHeight(700);
 
         while (opModeIsActive()) {
 
@@ -40,20 +50,13 @@ public class LiamArmTest extends DriveMethods {
         //The "60" represents the 60 rpm higher motor (second hinge)
         //Because they have different rpms by a factor of 2, 60rpm has half the clicks/rotation of the 30rpm
 
-        motorScissor30.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorScissor60.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        motorScissor30.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        motorScissor60.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
-        motorScissor60.setDirection(DcMotorSimple.Direction.REVERSE);
 
         int clicksPerRot30 = 5281;
         int clicksPerRot60 = 2786;
         int target30 = Clicks;
         int target60 = Clicks; //target60 despite having half the value, needs to travel twice the distance, so they are in fact the same value :)
-        int current30 = motorScissor30.getCurrentPosition();
-        int current60 = motorScissor60.getCurrentPosition();
+        double current30 = motorScissor30.getCurrentPosition();
+        double current60 = motorScissor60.getCurrentPosition();
         double error30 = target30 - current30; //error of the 30rpm motor
         double error60 = target60 - current60; //error of the 60rpm motor
         double hingeScaleFactor = 0.979123; //This is 469mm/479mm (the length different between each 'arm')
@@ -62,8 +65,17 @@ public class LiamArmTest extends DriveMethods {
         double currentAngle30 = 0;
         double currentAngle60 = 0;
 
+        telemetry.addLine("30Position: " + current30);
+        telemetry.addLine("60Position: " + current60);
+        telemetry.addLine("error30: " + error30);
+        telemetry.addLine("error60: " + error60);
+        telemetry.addLine("power30: " + power30);
+        telemetry.addLine("power60: " + power60);
+        telemetry.update();
 
-        while(Math.abs(error30) <= 10 && Math.abs(error60) <= 10){
+        sleep(500);
+
+        while(Math.abs(error30) >= 10 && Math.abs(error60) >= 10){
             current30 = motorScissor30.getCurrentPosition();
             current60 = motorScissor60.getCurrentPosition(); //This value's resolution is half the resolution of the above value
 
@@ -73,23 +85,39 @@ public class LiamArmTest extends DriveMethods {
             currentAngle30 = (current30/clicksPerRot30)*360;
             currentAngle60 = (current60/clicksPerRot60)*360;
 
-            power30 = 0.45*(1 - (currentAngle30/90));
-            power60 = 0.45*(1- (currentAngle60/180));
+            power30 = 0.52*(1 - (currentAngle30/110) + error30/900);
+            power60 = 0.43*(1- (currentAngle60/180) + error60/450);
 
             motorScissor30.setPower(power30);
             motorScissor60.setPower(power60); //the 60 rpm is traveling twice the distance of the 30 rpm at all times
 
-            telemetry.addLine("30Position: " + current30);
-            telemetry.addLine("60Position: " + current60);
+            telemetry.addLine("30Position: " + motorScissor30.getCurrentPosition());
+            telemetry.addLine("60Position: " + motorScissor60.getCurrentPosition());
             telemetry.addLine("error30: " + error30);
             telemetry.addLine("error60: " + error60);
             telemetry.addLine("power30: " + power30);
             telemetry.addLine("power60: " + power60);
+            telemetry.addLine("currentAngle30: " + currentAngle30);
+            telemetry.addLine("currentAngle60: " + currentAngle60);
+
             telemetry.update();
+
+            if(currentAngle30 > 80 || currentAngle60 > 160) {
+                motorScissor60.setPower(0);
+                motorScissor30.setPower(0);
+                break;
+            }
+            i++;
         }
 
+
+        telemetry.addLine("You're outside the loop!");
+        telemetry.addLine("power30: " + (power30-0.04));
+        telemetry.addLine("power60: " + (power60-0.04));
+        telemetry.update();
         motorScissor30.setPower(power30 - 0.04);
         motorScissor60.setPower(power60 - 0.04);
+
 
     }
 }
