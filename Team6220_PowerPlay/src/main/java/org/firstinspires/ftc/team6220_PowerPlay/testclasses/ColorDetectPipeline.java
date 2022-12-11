6d375ae498a4f088d55cbf88123d544e0a1bc379
Innaias <innaias@harveyosity.org>
2022-12-11 11:57:50 -0800

    Set up framework for juction detection (This class detects any color, so we can extend a class from this to detetc junctions)

---
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/ColorDetectPipeline.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.team6220_PowerPlay.testclasses;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Moments;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.opencv.calib3d.Calib3d;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfDouble;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfPoint3f;
+import org.opencv.core.Point;
+import org.opencv.core.Point3;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ColorDetectPipeline extends OpenCvPipeline {
+    @Override
+    public Mat processFrame(Mat input) {
+        return null;
+    }
+}
