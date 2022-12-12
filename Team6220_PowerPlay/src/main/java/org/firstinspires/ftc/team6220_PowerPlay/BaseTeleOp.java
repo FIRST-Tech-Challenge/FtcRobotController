eef713f32fd2144313806697193030c59c744f57
Satvik Kabbur <satvik.kabbur@gmail.com>
2022-12-11 16:01:27 -0800

    teleop disappeared to I added it back

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/BaseTeleOp.java
@@ -5,6 +5,10 @@ public abstract class BaseTeleOp extends BaseOpMode {
     double yPower;
     double tPower;
 
+    int slideTargetPosition = 0;
+    int junction = 0;
+    int[] junctions = {0, 0};
+
     public void driveChassisWithController() {
         xPower = gamepad1.left_stick_x * Constants.DRIVE_SPEED_MULTIPLIER * (1 - gamepad1.left_trigger * 0.5);
         yPower = gamepad1.left_stick_y * Constants.DRIVE_SPEED_MULTIPLIER * (1 - gamepad1.left_trigger * 0.5);
@@ -14,43 +18,84 @@ public abstract class BaseTeleOp extends BaseOpMode {
         if (Math.abs(Math.toDegrees(Math.atan(gamepad1.left_stick_y / gamepad1.left_stick_x))) < Constants.DRIVE_DEADZONE_DEGREES) {
             driveWithIMU(xPower, 0.0, tPower);
 
-        // case for driving the robot forwards and backwards
+            // case for driving the robot forwards and backwards
         } else if (Math.abs(Math.toDegrees(Math.atan(gamepad1.left_stick_y / gamepad1.left_stick_x))) > Constants.DRIVE_DEADZONE_DEGREES) {
             driveWithIMU(0.0, yPower, tPower);
 
-        // case for if the deadzone limits are passed, the robot drives normally
+            // case for if the deadzone limits are passed, the robot drives normally
         } else {
             driveWithIMU(xPower, yPower, tPower);
         }
     }
 
     public void driveGrabberWithController() {
-        if (gamepad2.a) {
+        if (gamepad2.x) {
             driveGrabber(Constants.GRABBER_CLOSE_POSITION);
-        } else if (gamepad2.x) {
+        } else if (gamepad2.a) {
             driveGrabber(Constants.GRABBER_OPEN_POSITION);
+        } else if (gamepad2.b) {
+            driveGrabber(Constants.GRABBER_INITIALIZE_POSITION);
         }
     }
 
     public void driveSlidesWithController() {
-        if (-gamepad2.left_stick_y < 0 && motorLeftSlides.getCurrentPosition() > Constants.SLIDE_LOW) {
-            motorLeftSlides.setPower(-gamepad2.left_stick_y * 0.05);
-            motorRightSlides.setPower(-gamepad2.left_stick_y * 0.05);
-        } else if (-gamepad2.left_stick_y < 0 && motorLeftSlides.getCurrentPosition() <= Constants.SLIDE_LOW) {
-            motorLeftSlides.setPower(-gamepad2.left_stick_y);
-            motorRightSlides.setPower(-gamepad2.left_stick_y);
-        } else {
-            motorLeftSlides.setPower(-gamepad2.left_stick_y * 0.75);
-            motorRightSlides.setPower(-gamepad2.left_stick_y * 0.75);
+        slideTargetPosition += (int) (-gamepad2.left_stick_y * 25);
+
+        if (gamepad2.right_bumper && junctions[0] == junctions[1]) {
+            junction++;
+
+            if (junction >= 3) {
+                junction = 3;
+            }
+
+            switch (junction) {
+                case 1:
+                    slideTargetPosition = Constants.SLIDE_LOW;
+                    break;
+                case 2:
+                    slideTargetPosition = Constants.SLIDE_MEDIUM;
+                    break;
+                case 3:
+                    slideTargetPosition = Constants.SLIDE_HIGH;
+                    break;
+            }
+        } else if (gamepad2.left_bumper && junctions[0] == junctions[1]) {
+            junction--;
+
+            if (junction <= 0) {
+                junction = 0;
+            }
+
+            switch (junction) {
+                case 0:
+                    slideTargetPosition = Constants.SLIDE_BOTTOM;
+                    break;
+                case 1:
+                    slideTargetPosition = Constants.SLIDE_LOW;
+                    break;
+                case 2:
+                    slideTargetPosition = Constants.SLIDE_MEDIUM;
+                    break;
+            }
         }
 
-        if (motorLeftSlides.getCurrentPosition() < Constants.SLIDE_BOTTOM) {
-            motorLeftSlides.setPower(0.5);
-            motorRightSlides.setPower(0.5);
+        // don't let target position go below slide bottom position
+        if (slideTargetPosition <= Constants.SLIDE_BOTTOM) {
+            slideTargetPosition = Constants.SLIDE_BOTTOM;
+            // don't let target position go above slide top position
+        } else if (slideTargetPosition >= Constants.SLIDE_TOP) {
+            slideTargetPosition = Constants.SLIDE_TOP;
         }
+
+        driveSlides(slideTargetPosition);
+
+        if (!gamepad2.left_bumper && !gamepad2.right_bumper) {
+            junctions[0] = junctions[1];
+        }
+
+        junctions[1] = junction;
     }
 
     public void driveTurntableWithController() {
-        driveTurntable(0);
     }
 }
