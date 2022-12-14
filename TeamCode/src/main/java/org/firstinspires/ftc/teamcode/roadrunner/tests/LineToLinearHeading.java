db2142859c231d9a1b48d884c0aca554a8a3a169
saa938 <83087832+saa938@users.noreply.github.com>
2022-12-13 20:19:46 -0800

    s

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tests/LineToLinearHeading.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tests/LineToLinearHeading.java
@@ -4,7 +4,6 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 import com.acmerobotics.dashboard.FtcDashboard;
 
