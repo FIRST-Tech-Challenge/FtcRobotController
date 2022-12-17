9305acb8f08c04c7bc2985f071b09708aacffaf0
Gobln <caasier42@gmail.com>
2022-12-17 10:00:35 -0700

    Adding turnNinety to auto using complex sensor in control hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -1,3 +1,4 @@
+//Code from https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
