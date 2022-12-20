bb632d26c660a566da4fca169d5018f7812ec728
saa938 <83087832+saa938@users.noreply.github.com>
2022-12-19 17:36:56 -0800

    MeepMeep

---
+++ meepmeep/src/main/java/com/example/meepmeep/meepmeep.java
@@ -0,0 +1,33 @@
+package com.example.meepmeep;
+
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+
+public class meepmeep {
+    public static void main(String args[]) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(35, 35, 5.579565990580749, Math.toRadians(259.2284275862069), 10.6)
+                        .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(0,0,0))
+                                        .forward(30)
+                                        .turn(Math.toRadians(90))
+                                        .forward(30)
+                                        .turn(Math.toRadians(90))
+                                        .forward(30)
+                                        .turn(Math.toRadians(90))
+                                        .forward(30)
+                                        .turn(Math.toRadians(90))
+                                        .build()
+                                );
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
