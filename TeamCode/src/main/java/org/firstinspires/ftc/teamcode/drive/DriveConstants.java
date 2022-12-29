9faa0d56b7d49b1384301ee774fa2539f3ee1de6
MrIndianDude <djayachandra@outlook.com>
2022-12-29 12:30:15 -0600

    roadrunner tuning continued

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,8 +55,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0177;
-    public static double kA = 0.0053;
+    public static double kV = 0.0168;
+    public static double kA = 0.0052;
     public static double kStatic = 0;
 
     /*
@@ -89,8 +89,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 40;
     public static double MAX_ACCEL = 26;
-    public static double MAX_ANG_VEL = Math.toRadians(77.19389954947438);
-    public static double MAX_ANG_ACCEL = Math.toRadians(77.19389954947438);
+    public static double MAX_ANG_VEL = Math.toRadians(388.23830548721475);
+    public static double MAX_ANG_ACCEL = Math.toRadians(88.19389954947438);
 
 
     public static double encoderTicksToInches(double ticks) {
