74549555f8b2826a2fe4b767ba061eaa5c07ea2c
Wizardhatt <118863227+Wizardhatt@users.noreply.github.com>
2022-12-29 20:54:58 -0600

    removed max/min heights and added sleep functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftMechanismEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftMechanismEncoder.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
@@ -26,7 +27,7 @@ public class LiftMechanismEncoder extends LinearOpMode {
 
     // Declare OpMode members.
     DcMotor liftMotor1 = null;
-    DcMotor liftMotor2 = null;
+    //DcMotor liftMotor2 = null;
     private ElapsedTime runtime = new ElapsedTime();
     double liftPower = 0.0;
     double INCREMENT = 0.01;
@@ -46,56 +47,46 @@ public class LiftMechanismEncoder extends LinearOpMode {
         telemetry.update();
 
         digitalTouch = hardwareMap.get(DigitalChannel.class, "Touch");
-         digitalTouch.setMode(DigitalChannel.Mode.INPUT);
+        digitalTouch.setMode(DigitalChannel.Mode.INPUT);
 
-        liftMotor1  = hardwareMap.get(DcMotor.class, "LiftMotor1");
-        liftMotor2 = hardwareMap.get(DcMotor.class, "LiftMotor2");
-
-
-
+        liftMotor1 = hardwareMap.get(DcMotor.class, "liftMotor");
+        //liftMotor2 = hardwareMap.get(DcMotor.class, "LiftMotor2");
 
 
         // setting correct motor directions
         liftMotor1.setDirection(DcMotor.Direction.REVERSE);
-        liftMotor2.setDirection(DcMotor.Direction.REVERSE);
+        //liftMotor2.setDirection(DcMotor.Direction.REVERSE);
 
         //Resets encoders and sets encoder value to 0
         liftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         liftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        liftMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //liftMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry.addData("Starting at", "%7d",
-                liftMotor1.getCurrentPosition(),
-                liftMotor2.getCurrentPosition());
+                liftMotor1.getCurrentPosition());
+        //liftMotor2.getCurrentPosition());
 
 
         //Tells motors to resist movement and hold when power is 0
-         //liftMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //liftMotor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         //liftMotor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
 
 
         //Gets current position of encoders and holds at that position
         int Currentposition = liftMotor1.getCurrentPosition();
 
 
-
-
-
-
-
-
         //if touch sensor = pressed, stop lift motor
-        if (!digitalTouch.getState()) {
+       /* if (!digitalTouch.getState()) {
             telemetry.addData("Digital Touch", "Is Pressed");
             telemetry.update();
             liftPower = 0.0;
             liftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        }
+        }*/
 
         telemetry.addData("Status", "Initialization Finished");
         telemetry.update();
@@ -105,7 +96,9 @@ public class LiftMechanismEncoder extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-           // dualLiftPower(0.0);
+            //Gets current position of encoders and holds at that position
+             Currentposition = liftMotor1.getCurrentPosition();
+            // dualLiftPower(0.0);
 
 
             // Setup a variable for each drive wheel to save power level for telemetry
@@ -132,7 +125,6 @@ public class LiftMechanismEncoder extends LinearOpMode {
             } */
 
 
-
             // Go down
            /* else if (gamepad2.a && liftPower >= MIN_SPEED) {
                 liftPower -= INCREMENT;
@@ -154,39 +146,38 @@ public class LiftMechanismEncoder extends LinearOpMode {
             else{
                 liftPower = 0.0;
             }*/
-          //  liftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-          //  liftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+            //liftMotor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //liftMotor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //liftMotor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            //liftMotor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             // Add 50 counts if current position if less than or equal to max encoder count
-            if(gamepad1.dpad_up && Currentposition <= 2000 ){
-                dualLiftTarget(Currentposition + 50);
+            if (gamepad1.dpad_up){ //&& Currentposition < 1500) {
+                dualLiftTarget(-1500);
                 liftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                liftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                dualLiftPower(0.5);
-
-
-
-
-
+                //liftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                dualLiftPower(-0.5);
+                //dualLiftPower(0.0);
+                //sleep(100);
 
 
             }
             // change max speed to minimum encoder count
-            else if(gamepad1.dpad_down && Currentposition >= 0) {
-                dualLiftTarget(Currentposition - 50);
+            else if (gamepad1.dpad_down){// && Currentposition >= 500) {
+                dualLiftTarget(-500);
                 liftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                liftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                dualLiftPower(-0.5);
-
+                //liftMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+               dualLiftPower(-0.5);
+                //dualLiftPower(0.0);
+                //sleep(100);
             }
 
             // When nothing is pressed stay in place
-           else{
-               dualLiftPower(0.0);
-                //liftMotor1.setTargetPosition(position);
-               // liftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
+            else {
 
+                //liftMotor1.setTargetPosition(position);
+                // liftMotor1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            }
+            dualLiftPower(0.0);
           /* // If lower than max encoder count go up
             if(gamepad1.dpad_up && position < 751.8 ){
             }
@@ -198,27 +189,24 @@ public class LiftMechanismEncoder extends LinearOpMode {
             // if nothing is pressed set power to zero and break
             else{
                 liftMotor1.setPower(0);
-            }*/
+            }
+*/
 
 
-
-
-
-
-
-           // dualLiftPower(liftPower);
+            // dualLiftPower(liftPower);
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motor1", "%5.2f", liftMotor1.getPower());
-            telemetry.addData("Motor2", "%5.2f", liftMotor2.getPower());
+            //telemetry.addData("Motor2", "%5.2f", liftMotor2.getPower());
             telemetry.addData("Starting at", "%7d",
-                                liftMotor1.getCurrentPosition(),
-                                liftMotor2.getCurrentPosition());
 
-
+                    liftMotor1.getCurrentPosition());
+            //liftMotor2.getCurrentPosition());
 
 
             telemetry.addData("DpadUp pressed", String.valueOf(gamepad1.dpad_up));
             telemetry.addData("DpadDown", String.valueOf(gamepad1.dpad_down));
+            telemetry.addData("Motor 1 TargetPos", liftMotor1.getTargetPosition());
+            //telemetry.addData("Motor 2 TargetPos", liftMotor2.getTargetPosition());
             //telemetry.addData("Encoder position", position.toString())
             telemetry.update();
         }
@@ -227,10 +215,14 @@ public class LiftMechanismEncoder extends LinearOpMode {
     // Show the elapsed game time and wheel power
     public void dualLiftPower(double power) {
         liftMotor1.setPower(power);
-        liftMotor2.setPower(power);
+        //liftMotor2.setPower(power);
     }
     public void dualLiftTarget(int position) {
         liftMotor1.setTargetPosition(position);
-        liftMotor2.setTargetPosition(position);
+        //liftMotor2.setTargetPosition(position);
     }
+
+
+
+
     }
