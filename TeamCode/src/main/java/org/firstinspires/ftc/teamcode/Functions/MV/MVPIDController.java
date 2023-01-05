8ba77cfca26f709ba4452c1f412ca47f667d8572
derzse <elisa.erzse@gmail.com>
2023-01-05 09:03:25 +0200

    Update MVPIDController.java
    Updated version with some errors corrected and with new methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Functions/MV/MVPIDController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Functions/MV/MVPIDController.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode.Functions.MV;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
 
 /**
  * This class implements a PID controller. A PID (proportional-integral-derivative) controller is a
@@ -26,10 +29,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * system being controlled.
  */
 public class MVPIDController {
+
+    // instance variables for PID controller constants
     private double kP;                  // proportional gain
     private double kI;                  // integral gain
     private double kD;                  // derivative gain
     private double kF;                  // forward feed gain
+
+    // instance variables for error values and elapsed time
     private double tolerance;           // tolerance
     private double target = 0;          // targeted value (setPoint)
     private double start = 0;           // starting position
@@ -37,73 +44,30 @@ public class MVPIDController {
     private double previousError;       // previous error
     private double integral;            //integral
     private double derivative;          // derivative
-    private double minInput;            // minimum input
-    private double maxInput;            // maximum input
-    private double minOutput;           // minimum output
-    private double maxOutput;           // maximum output
-    private boolean continuous;         // continuous mode
     private ElapsedTime elapsedTime;    // elapsed time object
     private boolean firstRun;           // flag for first run
     private double currentTime = 0.0;   // current time
     private double deltaTime = 0.0;     // time delta
 
-    public void setkP( double kP) {
-        try {
-            if (kP < 0) {
-                throw new IllegalArgumentException("kP must be positive");
-            }
-            this.kP = kP;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException caught in setting kP: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception caught in setting kP: " + e.getMessage());
-        }
-    }
+    // input and output limits
+    private double minInput;            // minimum input
+    private double maxInput;            // maximum input
+    private double minOutput;           // minimum output
+    private double maxOutput;           // maximum output
 
-    public void setkI( double kI){
-        try {
-            if (kI < 0) {
-                throw new IllegalArgumentException("kI must be positive");
-            }
-            this.kI = kI;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException caught in setting kI: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception caught in setting kI: " + e.getMessage());
-        }
-    }
+    // continuous mode flag
+    private boolean continuous;         // continuous mode
 
-    public void setkD( double kD){
-        try {
-            if (kD < 0) {
-                throw new IllegalArgumentException("kD must be positive");
-            }
-            this.kD = kD;
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException caught in setting kD: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Exception caught in setting kD: " + e.getMessage());
-        }
-    }
-
-    public void setkF( double kF){
-        try {
-            if (kF < 0) {
-                throw new IllegalArgumentException("kF must be positive");
-            }
-            this.kF = kF;
-        }
-        catch (NullPointerException e) {
-            System.out.println("NullPointerException caught in setting kF: " + e.getMessage());
-        }
-        catch (Exception e) {
-            System.out.println("Exception caught in setting kF: " + e.getMessage());
-        }
-    }
+    // telemetry object
+    private Telemetry telemetry;
 
     /**
      * Constructor: Create an instance of the object.
-     *
+     * <p></p>
+     * This is the constructor for the MVPIDController class. It sets the proportional, integral,
+     * derivative, and feed forward constants for the PID controller, as well as the tolerance
+     * value. It also initializes an elapsed time object and sets a flag for the first run.
+     *</p></p>
      * @param kP specifies the Proportional constant.
      * @param kI specifies the Integral constant.
      * @param kD specifies the Differential constant.
@@ -126,6 +90,7 @@ public class MVPIDController {
             }
             this.tolerance = tolerance;
 
+            // initialize elapsed time object
             firstRun = true;
             elapsedTime =new ElapsedTime();  // initialize elapsed time
 
@@ -134,6 +99,132 @@ public class MVPIDController {
         } catch (Exception e) {
             System.out.println("Exception caught in PIDController: " + e.getMessage());
         }
+    }
+
+    /**
+     * This method sets the telemetry.
+     * @param telemetry : (Telemetry) value for kP
+     */
+    // setter method for telemetry object
+    public void setTelemetry(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    /**
+     * This method sets the Proportional term of the PID Controller.
+     * @param kP : (boolean) value for kP
+     */
+    public void setkP( double kP) {
+        try {
+            if (kP < 0) {
+                throw new IllegalArgumentException("kP must be positive");
+            }
+            this.kP = kP;
+        } catch (NullPointerException e) {
+            telemetry.addData("Error:", "NullPointerException caught in setkP: " + e.getMessage());
+            telemetry.update();
+        } catch (Exception e) {
+            telemetry.addData("Error:", "Exception caught in setkP: " + e.getMessage());
+            telemetry.update();
+        }
+    }
+
+    /**
+     * This method sets the Integral term of the PID Controller.
+     * @param kI : (boolean) value for kI
+     */
+    public void setkI( double kI){
+        try {
+            if (kI < 0) {
+                throw new IllegalArgumentException("kI must be positive");
+            }
+            this.kI = kI;
+        } catch (NullPointerException e) {
+            sendError("NullPointerException caught in setting kI: " + e.getMessage());
+            //System.out.println("NullPointerException caught in setting kI: " + e.getMessage());
+        } catch (Exception e) {
+            sendError("Exception caught in setting kI: " + e.getMessage());
+            //System.out.println("Exception caught in setting kI: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method sets the Derivative term of the PID Controller.
+     * @param kD : (boolean) value for kD
+     */
+    public void setkD( double kD){
+        try {
+            if (kD < 0) {
+                throw new IllegalArgumentException("kD must be positive");
+            }
+            this.kD = kD;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setting kD: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception caught in setting kD: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method sets the Forward Feed term of the PID Controller.
+     * @param kF : (boolean) value for kF
+     */
+    public void setkF( double kF){
+        try {
+            if (kF < 0) {
+                throw new IllegalArgumentException("kF must be positive");
+            }
+            this.kF = kF;
+        }
+        catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setting kF: " + e.getMessage());
+        }
+        catch (Exception e) {
+            System.out.println("Exception caught in setting kF: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method gets the Proportional term of the PID Controller.
+     */
+    public double getkP(){
+        return kP;
+    }
+
+    /**
+     * This method gets the Integral term of the PID Controller.
+     */
+    public double getkI(){
+        return kI;
+    }
+
+    /**
+     * This method gets the Derivative term of the PID Controller.
+     */
+    public double getkD(){
+        return kD;
+    }
+
+    /**
+     * This method gets the Forward Feed term of the PID Controller.
+     */
+    public double getkF(){
+        return kF;
+    }
+
+    /**
+     * This method gets all the parameters of the PID Controller. It returns an array
+     * of five elements: {kP, kI, kD, kF, tolerance}
+     */
+    public double[] getPIDParameters() {
+        double[] parameters = new double[5];
+        parameters[0] = kP;
+        parameters[1] = kI;
+        parameters[2] = kD;
+        parameters[3] = kF;
+        parameters[4] = tolerance;
+
+        return parameters;
     }
 
     /**
@@ -155,7 +246,7 @@ public class MVPIDController {
     }
 
     /**
-     * This method sets the minInput and maxInput value.
+     * This method sets the minimum and maximum input values for the PID controller.
      * @param minInput : (double) value for minInput
      * @param maxInput : (double) value for maxInput
      */
@@ -207,7 +298,7 @@ public class MVPIDController {
     }
 
     /**
-     * This method sets the minOutput and maxOutput value.
+     * This method sets the minimum and maximum output values for the PID controller.
      * @param minOutput : (double) value for minOutput
      * @param maxOutput : (double) value for maxOutput
      */
@@ -224,20 +315,21 @@ public class MVPIDController {
     }
 
     /**
-     * This method adjust the input value to be between minInput and maxInput.
+     * This method adjust the output value to be between minInput and maxInput.
      * @param output : (double) given value for a output
      */
     public double adjustOutput(double output){
         // Adjust the output value
-        output = Math.max(output, minInput);
-        output = Math.min(output, maxInput);
+        output = Math.max(output, minOutput);
+        output = Math.min(output, maxOutput);
 
         return output;
     }
 
     /**
-     * This method wraps the input value around the minInput and maxInput values if the controller
-     * is in continuous mode.
+     * This method sets the continuous mode flag for the PID controller. If set to true, the input
+     * values will be treated as continuous, meaning that the input value will "wrap around" when
+     * it exceeds the maximum or minimum input value.
      * @param input : (double) value for input
      */
     public double wrapToRange(double input){
@@ -254,41 +346,171 @@ public class MVPIDController {
         return input;
     }
 
-    public void setTarget(double target){
-        this.target = target;
-        this.integral = 0;
-        this.derivative = 0;
+    /**
+     * This method sets the target value (setpoint) for the PID controller.
+     * @param target: (double) the target value
+     */
+    public void setTarget(double target) {
+        try {
+            this.target = target;
+            this.integral = 0;
+            this.derivative = 0;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setTarget: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception caught in setTarget: " + e.getMessage());
+        }
     }
 
-    public double getTarget(){
-        return target;
+    /**
+     * This method returns the target value (setpoint) for the PID controller.
+     * @return the target value
+     */
+    public double getTarget() {
+        try {
+            return target;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in getTarget: " + e.getMessage());
+            return 0.0;
+        } catch (Exception e) {
+            System.out.println("Exception caught in getTarget: " + e.getMessage());
+            return 0.0;
+        }
     }
 
+    /**
+     * This method sets the starting position for the PID controller.
+     * @param startAngle: (double) the starting position
+     */
+    public void setStart(double startAngle) {
+        try {
+            this.start = startAngle;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setStart: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception caught in setStart: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method returns the starting position for the PID controller.
+     * @return the starting position
+     */
+    public double getStart() {
+        try {
+            return start;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in getStart: " + e.getMessage());
+            return 0.0;
+        } catch (Exception e) {
+            System.out.println("Exception caught in getStart: " + e.getMessage());
+            return 0.0;
+        }
+    }
+
+    /**
+     * This method sets the absolute tolerance value for the PID controller.
+     * @param tolerance: (double) the tolerance value
+     */
     public void setTolerance(double tolerance){
-        this.tolerance = tolerance;
+        try {
+            if (tolerance < 0) {
+                throw new IllegalArgumentException("Tolerance must be positive");
+            }
+            this.tolerance = tolerance;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setAbsoluteTolerance: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception caught in setAbsoluteTolerance: " + e.getMessage());
+        }
     }
 
     public double getTolerance(){
         return tolerance;
     }
 
+    /**
+     * This method sets the percentage tolerance value for the PID controller.
+     * @param tolerance: (double) the tolerance value as a percentage of the setpoint
+     */
+    public void setPercentTolerance(double tolerance) {
+        try {
+            if (tolerance < 0 || tolerance > 100) {
+                throw new IllegalArgumentException("Tolerance must be a value between 0 and 100");
+            }
+            this.tolerance = tolerance / 100 * target;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in setPercentTolerance: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Exception caught in setPercentTolerance: " + e.getMessage());
+        }
+    }
+
+    /**
+     * This method returns the percentage tolerance value for the PID controller.
+     * @return the tolerance value as a percentage of the setpoint
+     */
+    public double getPercentageTolerance() {
+        try {
+            return tolerance / target * 100;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in getPercentageTolerance: " + e.getMessage());
+            return 0.0;
+        } catch (Exception e) {
+            System.out.println("Exception caught in getPercentageTolerance: " + e.getMessage());
+            return 0.0;
+        }
+    }
+
+    /**
+     * This method returns a boolean value indicating whether the error between the start and the
+     * target positions is within the specified tolerance.
+     * @return true if the error is within tolerance, false otherwise
+     */
     public boolean onTarget(){
-        double  error = Math.abs(start - target);
-        return error < tolerance;
+        try {
+            double error = Math.abs(start - target);
+            return error < tolerance;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in onTarget: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            System.out.println("Exception caught in onTarget: " + e.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * This method returns the current error value.
+     * @return the current error value
+     */
+    public double getCurrentError() {
+        try {
+            return totalError;
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException caught in getCurrentError: " + e.getMessage());
+            return 0.0;
+        } catch (Exception e) {
+            System.out.println("Exception caught in getCurrentError: " + e.getMessage());
+            return 0.0;
+        }
     }
 
     /**
      * This method calculates the PID output applying the PID equation to the given set point target
-     * and currentinput value.
+     * and current input value.
      *
      * @return PID output value.
      */
-    public double calculate(double current){
+    public double calculate(){
         // Set new start position
-        this.start = current;
+        //this.start = current;
+
+        // Send telemetry message
+        telemetry.addData("PIDController - ", "calculate()");
 
         // Calculating the error
-        double currentError = target - current;
+        double currentError = target - start;
 
         // Check if PIDController is at its first run or not
         if (firstRun) {
@@ -353,11 +575,13 @@ public class MVPIDController {
             output = p + i + d + f;
         }
 
-        // Check if output is in the desired range or throw an exception
+        // Check if output is in the desired range or adjust the output value to be in the desired
+        // range or throw an exception
         try {
             // Check if output is outside of specified range
             if (output < minOutput || output > maxOutput) {
-                throw new IllegalArgumentException("PID controller output is outside of specified range");
+                adjustOutput(output);
+                //throw new IllegalArgumentException("PID controller output is outside of specified range");
             }
         } catch (IllegalArgumentException e) {
             System.out.println("Message of Exception : "  + e.getMessage());
@@ -370,5 +594,27 @@ public class MVPIDController {
         }
 
         return output;
+    }
+
+    /**
+     * This method resets the PID controller.
+     */
+    public void reset() {
+        totalError = 0;
+        integral = 0;
+        derivative = 0;
+        previousError = 0;
+        elapsedTime.reset();
+        firstRun = true;
+    }
+
+    /**
+     * This method sends an error message to the telemetry.
+     *
+     * @param message the error message to send.
+     */
+    private void sendError(String message) {
+        telemetry.addData("Error:", message);
+        telemetry.update();
     }
 }
