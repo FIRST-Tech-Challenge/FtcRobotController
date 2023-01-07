833f934a39c72d91c2b04f49c15159db96eeed9a
Fiyrflash <Fiyrflash@gmail.com>
2023-01-07 15:04:34 -0500

    304

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotesting.java
@@ -3,7 +3,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp
