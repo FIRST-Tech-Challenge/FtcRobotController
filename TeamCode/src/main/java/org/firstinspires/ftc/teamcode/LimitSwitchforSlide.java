586f0d6ee3519ca82dbcad232d4e7946dec7e8f1
Akash <aragam9999@gmail.com>
2023-01-08 15:27:42 -0800

    Working limit swtich code for linear slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimitSwitchforSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LimitSwitchforSlide.java
@@ -1,28 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 
-@TeleOp(name="TouchSensorTelemetryExample", group="Examples")
-public class LimitSwitchTelemetryExample extends LinearOpMode {
+@TeleOp(name="LimitSwitchforSlide", group="Examples")
+public class LimitSwitchforSlide extends LinearOpMode {
 
     // Declare a touch sensor object
-    private TouchSensor touchSensor;
+    // WE NAMED THE LIMIT SWITCH, "limitSwitch" FOR SIMPLICITY
+    private TouchSensor limitSwitch;
+    public DcMotor Slide;
 
     @Override
     public void runOpMode() {
         // Initialize the hardware
-        touchSensor = hardwareMap.get(TouchSensor.class, "touchSensor");
+        limitSwitch = hardwareMap.get(TouchSensor.class, "limitSwitch");
+        Slide = hardwareMap.get(DcMotor.class, "Slide");
+        //public boolean isPressed(){return limitSwitch.isPressed();}
+
 
         // Wait for the start button to be pressed
         waitForStart();
 
         while (opModeIsActive()) {
             // Print the current state of the touch sensor to the telemetry
-            telemetry.addData("Touch Sensor", touchSensor.isPressed());
+            telemetry.addData("Touch Sensor", limitSwitch.isPressed());
             telemetry.update();
+
+            // If the Magnetic Limit Switch is pressed, stop the motor
+            if (limitSwitch.isPressed()) {
+                Slide.setPower(0);
+            } else { // Otherwise, run the motor
+                Slide.setPower(0.3);
+            }
+
         }
+
+
     }
 }
 
