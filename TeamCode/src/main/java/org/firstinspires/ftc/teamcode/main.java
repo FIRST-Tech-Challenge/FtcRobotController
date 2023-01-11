f4b17535f51a79aa9d579ed1e2b288922a6bdb1a
not carl <asd@asd.com>
2023-01-11 19:27:51 +0100

    main and movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -3,8 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class main extends LinearOpMode {
-
     @Override public void runOpMode() {
-
+        while (opModeIsActive()){
+            int a;
+        }
     }
 }
