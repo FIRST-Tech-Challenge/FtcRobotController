fcd70811a434aaa495e33732079444eaca836766
Digicat509 <106493589+Digicat509@users.noreply.github.com>
2023-01-10 21:08:59 -0600

    Scrimmage Iron Giant changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/catbot/Task.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/catbot/Task.java
@@ -5,7 +5,7 @@ import com.acmerobotics.dashboard.config.Config;
 @Config("IronGiantAutonVariables")
 abstract class Task {
     public static int TICKSPERTILE = 700;
-    public static int TICKSPER90DEGREES = 525;
+    public static int TICKSPER90DEGREES = 475;
     public static int STRAFETICKSPERTILE = 1000;
     public static float MAXMOTORSPEED = .7f;
     public static final int NANOTOSECOND = 1000000000;
