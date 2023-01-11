05e5da2793829912a2bed729f4beac2d3d35e9b2
Alicelavander <alicelavander.dev@gmail.com>
2023-01-11 16:47:47 -0500

    activate Autonomous root file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Autonomous_root.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/autonomous/Autonomous_root.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opmodes.autonomous;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -15,6 +16,7 @@ import org.openftc.easyopencv.OpenCvCameraRotation;
 
 import java.util.ArrayList;
 
+@Autonomous
 public class Autonomous_root extends LinearOpMode {
 
     OpenCvCamera camera;
