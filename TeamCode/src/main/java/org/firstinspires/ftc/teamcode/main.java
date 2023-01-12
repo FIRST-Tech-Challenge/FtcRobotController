f46b9a695bf3aac8cc384c56dc91d413ab4171be
carlzrobotics <94403712+carlzrobotics@users.noreply.github.com>
2023-01-11 22:47:19 -0600

    Adding a movement class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -1,10 +1,22 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class main extends LinearOpMode {
-    @Override public void runOpMode() {
+
+
+    movement movement = new movement();
+
+    @Override
+    public void runOpMode() {
+        movement.Motor1 = hardwareMap.get(DcMotor.class, "Motor1");
+        movement.Motor2 = hardwareMap.get(DcMotor.class, "Motor2");
+        movement.Motor3 = hardwareMap.get(DcMotor.class, "Motor3");
+        movement.Motor4 = hardwareMap.get(DcMotor.class, "Motor4");
+
         while (opModeIsActive()){
+
             int a;
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/movement.java
@@ -3,9 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class movement {
-    private DcMotor Motor1;
-    private DcMotor Motor2;
-    private DcMotor Motor3;
-    private DcMotor Motor4;
 
+    public DcMotor Motor1;
+    public DcMotor Motor2;
+    public DcMotor Motor3;
+    public DcMotor Motor4;
+
+
+    public void motorinit(){
+
+    }
+
+    public void verticalMovement(){
+
+    }
+    public void horizontalMovement(){
+
+    }
+    public void rightrotateMovement(){
+
+    }
+    public void leftrotateMovement(){
+
+    }
 }
