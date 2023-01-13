151696c31f4915d340f0949af2fe5295f10c85b7
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-01-13 18:09:05 +0000

    Start fixing drivetrain code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1RedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1RedTeleOp.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode.opModes.team1.teleop;
 
+import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TeamColour;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TelemetryContainer;
 
 @TeleOp(name="Team 1 red", group="Team 1") // Registers TeleOp OpMode Entrypoint
 public class Team1RedTeleOp extends TeleOpModeBase { // Ensure you extend the base
@@ -25,8 +28,12 @@ public class Team1RedTeleOp extends TeleOpModeBase { // Ensure you extend the ba
         // Runs in the main loop
         teleop.every_tick();
         GamepadEx gamepad = Inputs.gamepad1;
-            Motor m_motor_1 = new Motor(hardwareMap, "motorOne");
-            m_motor_1.setRunMode(Motor.RunMode.RawPower);
-            m_motor_1.set(gamepad.getLeftY());
+        Motor left_motor = HardwareMapContainer.motor0;
+        Motor right_motor = HardwareMapContainer.motor1;
+
+        /* Arcade drive */
+        DifferentialDrive drive = new DifferentialDrive(left_motor, right_motor);
+        drive.arcadeDrive(gamepad.getLeftY()*2+1, gamepad.getLeftX());
+        TelemetryContainer.getTelemetry().addData("Joystick Y", gamepad.getLeftY());
     }
 }
