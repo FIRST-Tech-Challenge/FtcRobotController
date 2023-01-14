942b1878df8ea186740e44a5932e831861d3abd6
solinexi <solinexi@gmail.com>
2023-01-14 12:02:10 -0700

    i think i coded

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/drive/opmode/ManualFeedforwardTuner.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/drive/opmode/ManualFeedforwardTuner.java
@@ -43,7 +43,6 @@ import static org.firstinspires.ftc.blackswan.drive.DriveConstants.kV;
  * Pressing B/O (Xbox/PS4) will cede control back to the tuning process.
  */
 @Config
-@Disabled
 @Autonomous(group = "drive")
 public class ManualFeedforwardTuner extends LinearOpMode {
     public static double DISTANCE = 72; // in
