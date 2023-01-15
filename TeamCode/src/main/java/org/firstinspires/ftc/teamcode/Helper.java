2d2052914357ec3bd9efe3e5ef1af9c8a850b825
mishmash <mishmashbgu@gmail.com>
2023-01-15 21:37:56 +0200

    temp commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper.java
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -22,23 +23,31 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
 class elevatorPositions{
-    public static int high   = 20500;
-    public static int middle = 10000;
-    public static int low    = 1000 ;
+    public static int high   = 18800;
+    public static int middle = 12000;
+    public static int low    = 5000 ;
     public static int bottom = 0    ;
 }
 
 class RobotController {
     /** GENERAL CONSTANTS */
     private final double sq2 = Math.sqrt(2);
-    private Gamepad gamepad;
+    private final Telemetry telemetry;
+
+    /** THREADS */
+    public final Thread elevatorController;
+    public final Thread driveController;
+    public final Thread cycleController;
+
+    private final Thread score;
+    public final Thread autoCycle;
 
     /** SERVO CONSTANTS */
-    private final double grabberOut = 0.26;
-    private final double grabberMiddle = 0.79;
-    private final double grabberIn  = 0.92;
+    private final double grabberMiddle = 0.26;
+    public final double grabberIn  = 0.1;
 
-    private final double[] grabberPile = {0.41, 0.38, 0.34, 0.31, 0.28};
+    //                                  low > > > > > > > > > > > high
+    public final double[] grabberPile = {0.77, 0.73, 0.7, 0.66, 0.6};
 
     private final double armOut = 0.89; // hiTech value 0.61;
     private final double armIn  = 0.5;  // hiTech value 0.41;
@@ -48,12 +57,11 @@ class RobotController {
     private final double placerOutAutonomous = 0.76;
     private final double placerOutTeleOp = 0.85;
 
-    private final double pufferInit    = 0.14;
-    private final double pufferGrab    = 0.36;
-    private final double pufferRelease = 0.14;
+    private final double pufferGrab    = 0.18;
+    private final double pufferRelease = 0.018;
 
-    private final double grabberGrab = 0.775;
-    private final double grabberOpen = 0.45 ;
+    private final double grabberGrab = 0.58;
+    private final double grabberOpen = 0.36;
 
     /** SENSOR CONSTANTS */
     private final double grabberCatchTrigger = 8.5;
@@ -62,36 +70,45 @@ class RobotController {
     private final BNO055IMU imu;
     private final DistanceSensor grabberSensor;
     private final TouchSensor armSensor;
+    private final TouchSensor elevatorSensor;
 
     /** SERVOS */
-    final private Servo puffer ;
-    final private Servo grabber;
+    private final Servo puffer ;
+    private final Servo grabber;
 
-    final private Servo placerRight;
-    final private Servo placerLeft ;
+    private final Servo placerRight;
+    private final Servo placerLeft ;
 
-    final private Servo armRight;
-    final private Servo armLeft ;
+    private final Servo armRight;
+    private final Servo armLeft ;
 
-    final private Servo grabberRight;
-    final private Servo grabberLeft ;
+    private final Servo grabberRight;
+    private final Servo grabberLeft ;
 
     /** DRIVING MOTORS */
-    final private DcMotor frontRight;
-    final private DcMotor frontLeft ;
-    final private DcMotor backRight ;
-    final private DcMotor backLeft  ;
+    private final DcMotor frontRight;
+    private final DcMotor frontLeft ;
+    private final DcMotor backRight ;
+    private final DcMotor backLeft  ;
 
     /** DRIVING STATE KEEPERS */
     public double overallDrivingPower;
 
     /** ELEVATOR MOTORS */
-    final private DcMotorEx elevatorLeft ;
-    final private DcMotorEx elevatorRight;
+    private final DcMotorEx elevatorLeft ;
+    private final DcMotorEx elevatorRight;
+
+    /** ELEVATOR STATE KEEPERS */
+    int elevatorPosition;
+    double elevatorPower;
+
+    /** DRIVE CONTROLLER */
+    Vector joystick_left;
+    double A, B;
 
 
-    public RobotController(HardwareMap hm, Gamepad gamepad) {
-        this.gamepad = gamepad;
+    public RobotController(HardwareMap hm, Telemetry t) {
+        this.telemetry = t;
         /** SENSORS */
         // getting the imu from the hardware map
         imu = hm.get(BNO055IMU.class, "imu");
@@ -111,6 +128,7 @@ class RobotController {
         grabberSensor = hm.get(DistanceSensor.class, "grabberSensor");
         // getting the arm sensor
         armSensor = hm.get(TouchSensor.class, "armSensor");
+        elevatorSensor = hm.get(TouchSensor.class, "elevatorSensor");
 
         /** SERVOS */
         // getting the edge units
@@ -119,7 +137,7 @@ class RobotController {
 
         // setting the edge units to their initial positions
         grabber.setPosition(grabberOpen);
-        puffer .setPosition(pufferInit );
+        puffer .setPosition(pufferRelease);
 
 
         // getting the placers, arms and grabbers
@@ -135,7 +153,7 @@ class RobotController {
         // flipping the relevant servos direction
         armLeft     .setDirection(Servo.Direction.REVERSE);
         placerRight .setDirection(Servo.Direction.REVERSE);
-        grabberRight.setDirection(Servo.Direction.REVERSE);
+        grabberLeft .setDirection(Servo.Direction.REVERSE);
 
         // setting the servos position to their initial positions
         setArmPosition(armIn);
@@ -166,57 +184,179 @@ class RobotController {
         // set the left elevator motor to use the encoder
         elevatorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         elevatorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        score = new Thread(() -> {
+            if (elevatorPosition != elevatorPositions.bottom) {
+//                telemetry.addData("elevator height", elevatorPosition);
+                try {
+                    puffer.setPosition(pufferGrab);
+
+                    Thread.sleep(750);
+
+                    setPlacerPosition(placerOutTeleOp);
+
+                    while (gamepad.left_trigger == 0) {
+                    }
+
+                    puffer.setPosition(pufferRelease);
+
+                    while (gamepad.left_trigger > 0) {
+                    }
+
+                    setPlacerPosition(placerIn);
+                    elevatorPosition = elevatorPositions.bottom;
+
+                } catch (InterruptedException e) {
+                }
+            } else {
+//                telemetry.addData("elevator height", "not specified");
+            }
+//            telemetry.update();
+        });
+
+        autoCycle = new Thread(() -> {
+            score(4);
+            for (int i = 4; i > 0; i--){
+                collect(i);
+                score(i - 1);
+            }
+
+            score();
+        });
+
+
+        cycleController = new Thread(() -> {
+            while (gamepad.cycleController) {
+                if (gamepad.right_trigger > 0 || gamepad.right_bumper) {
+
+                    // bring the grabber down
+                    setGrabberPosition(grabberPile[0]);
+
+                    // bring the grabber out the correct amount
+                    setArmPosition(gamepad.right_trigger * (armOut - armIn) + armIn);
+
+                    // catch the cone if its in range
+                    if (grabberSensor.getDistance(DistanceUnit.CM) < grabberCatchTrigger)
+                        grabber.setPosition(grabberGrab);
+                    else grabber.setPosition(grabberOpen);
+
+                } else {
+                    if (armSensor.isPressed()) {
+                        setGrabberPosition(grabberIn);
+                    } else {
+                        setArmPosition(armIn);
+                    }
+                }
+
+                if (!score.isAlive()) {
+                    if (A_pressed()) {
+                        elevatorPosition = elevatorPositions.high;
+                        score.start();
+                    } else if (X_pressed()) {
+                        elevatorPosition = elevatorPositions.middle;
+                        score.start();
+                    } else if (Y_pressed()) {
+                        elevatorPosition = elevatorPositions.low;
+                        score.start();
+                    }
+                }
+            }
+        });
+
+        elevatorPosition = elevatorPositions.bottom;
+        elevatorPower = 0;
+        elevatorController = new Thread(() -> {
+            while (gamepad.elevatorController){
+                elevatorPower = Math.max(Math.min((elevatorPosition - elevatorLeft.getCurrentPosition()) / 2300.0, 1), -1);
+
+                // make the elevator weaker when coming down to compensate for gravity
+                if (elevatorPower < 0) elevatorPower /= 2;
+
+                // set the elevator power into the elevator motors
+                elevatorLeft .setPower(elevatorPower);
+                elevatorRight.setPower(elevatorPower);
+            }
+        });
+
+        joystick_left = new Vector(0, 0);
+        driveController = new Thread(() ->{
+            while(gamepad.driveController) {
+                joystick_left.x = gamepad.left_stick_x;
+                joystick_left.y = -gamepad.left_stick_y;
+
+                // make the bot field oriented while considering the starting angle
+                joystick_left.addAngle(-getRobotAngle() - AutonomousDrive.lastAngle);
+
+
+                // using my equations to calculate the power ratios
+                A = (joystick_left.y - joystick_left.x) / sq2;
+                B = (joystick_left.y + joystick_left.x) / sq2;
+
+                // slow mode if the grabber is out
+                if (grabberLeft.getPosition() == grabberPile[0]) {
+                    overallDrivingPower = 0.4;
+                } else {
+                    overallDrivingPower = 1;
+                }
+                // setting the powers in consideration of the turning speed
+                setDrivingPower((A - gamepad.right_stick_x * 0.7) * overallDrivingPower,
+                        (B + gamepad.right_stick_x * 0.7) * overallDrivingPower,
+                        (B - gamepad.right_stick_x * 0.7) * overallDrivingPower,
+                        (A + gamepad.right_stick_x * 0.7) * overallDrivingPower);
+
+                telemetry.addData("left_trigger", gamepad.left_trigger);
+                telemetry.addData("left_bumper", gamepad.left_bumper);
+                telemetry.addData("right_trigger", gamepad.right_trigger);
+                telemetry.addData("right_bumper", gamepad.right_bumper);
+
+                telemetry.addData("a", gamepad.a);
+                telemetry.addData("b", gamepad.b);
+                telemetry.addData("x", gamepad.x);
+                telemetry.addData("y", gamepad.y);
+
+                telemetry.addData("left_stick_y", gamepad.left_stick_y);
+                telemetry.addData("left_stick_x", gamepad.left_stick_x);
+                telemetry.addData("right_stick_x", gamepad.right_stick_x);
+                telemetry.addData("position", elevatorPosition);
+                telemetry.addData("current position", elevatorLeft.getCurrentPosition());
+
+
+                telemetry.update();
+            }
+        });
     }
 
-    public void scoringController(){
-
+    private boolean a = true;
+    private boolean A_pressed(){
+        if (gamepad.a){
+            if(a){
+                a = false;
+                return true;
+            }
+        } else a = true;
+        return false;
     }
 
-    public void collectingController(){
-
-
+    private boolean x = true;
+    private boolean X_pressed(){
+        if (gamepad.x){
+            if(x){
+                x = false;
+                return true;
+            }
+        } else x = true;
+        return false;
     }
 
-    int elevatorPosition = elevatorPositions.bottom;
-    double elevatorPower = 0;
-    public void elevatorController(){
-        elevatorPower = Math.max(Math.min((elevatorPosition - elevatorLeft.getCurrentPosition()) / 2300.0, 1), -1);
-
-        // make the elevator weaker when coming down to compensate for gravity
-        if (elevatorPower < 0) elevatorPower /= 2;
-
-        // set the elevator power into the elevator motors
-        elevatorLeft .setPower(elevatorPower);
-        elevatorRight.setPower(elevatorPower);
-
-        elevatorController();
-    }
-
-    Vector joystick_left = new Vector(0, 0);
-    double A, B;
-    public void driveController() {
-        joystick_left.x =  gamepad.left_stick_x;
-        joystick_left.y = -gamepad.left_stick_y;
-
-        // make the bot field oriented while considering the starting angle
-        joystick_left.addAngle(-getRobotAngle() - AutonomousDrive.lastAngle);
-
-
-        // using my equations to calculate the power ratios
-        A = (joystick_left.y - joystick_left.x) / sq2;
-        B = (joystick_left.y + joystick_left.x) / sq2;
-
-        // slow mode if the grabber is out
-        if (grabberLeft.getPosition() == grabberOut) {
-            overallDrivingPower = 0.4;
-        } else {
-            overallDrivingPower = 1;
-        }
-            // setting the powers in consideration of the turning speed
-            setDrivingPower((A - gamepad.right_stick_x * 0.7) * overallDrivingPower,
-                            (B + gamepad.right_stick_x * 0.7) * overallDrivingPower,
-                            (B - gamepad.right_stick_x * 0.7) * overallDrivingPower,
-                            (A + gamepad.right_stick_x * 0.7) * overallDrivingPower);
+    private boolean y = true;
+    private boolean Y_pressed(){
+        if (gamepad.y){
+            if(y){
+                y = false;
+                return true;
+            }
+        } else y = true;
+        return false;
     }
 
     public double getRobotAngle() {
@@ -252,54 +392,15 @@ class RobotController {
         backLeft  .setPower(backLeftPower   * overallDrivingPower);
     }
 
+    public void score(int nextConeNum) { score(true, nextConeNum); }
+    public void score() { score(false, 0); }
 
-    public void cycle() {
+    public void score(boolean prepareForNext, int nextConeNum){
         try {
-            for (int i = 0; i < 5; i++){
-                score();
-
-                setGrabberPosition(grabberPile[i]);
-
-                Thread.sleep(1500);
-                elevatorLeft .setPower(elevatorPower);
-                elevatorRight.setPower(elevatorPower);
-
-                setArmPosition(armOut);
-
-
-                while (grabberSensor.getDistance(DistanceUnit.CM) < 6){}
-
-                Thread.sleep(750);
-
-                grabber.setPosition(grabberGrab);
-
-                Thread.sleep(1500);
-
-                setGrabberPosition(grabberMiddle);
-
-                Thread.sleep(750);
-
-                setArmPosition(armIn);
-
-                while (!armSensor.isPressed()){}
-
-                Thread.sleep(750);
-
-                setGrabberPosition(grabberIn);
-
-                Thread.sleep(750); // replacing the sensor for now
-
+            if (grabber.getPosition() != grabberOpen){
                 grabber.setPosition(grabberOpen);
-
                 Thread.sleep(750);
             }
-
-            score();
-        } catch (InterruptedException e) {}
-        elevatorController();
-    }
-    public void score(){
-        try {
             puffer.setPosition(pufferGrab);
             elevatorPosition = elevatorPositions.high;
 
@@ -307,16 +408,85 @@ class RobotController {
 
             setPlacerPosition(placerOutAutonomous);
 
+            if (prepareForNext) {
+                setGrabberPosition(grabberPile[nextConeNum]);
+
+                setArmPosition(armOut * 0.8);
+            }
+
             Thread.sleep(750);
 
             puffer.setPosition(pufferRelease);
 
             Thread.sleep(750);
 
-            puffer.setPosition(pufferInit);
+            puffer.setPosition(pufferRelease);
             setPlacerPosition(placerIn);
             elevatorPosition = elevatorPositions.bottom;
         }catch (InterruptedException e){}
+
+    }
+
+    public void collect(int coneNum){
+        try {
+            setGrabberPosition(grabberPile[coneNum]);
+            Thread.sleep((int)(500 / (grabberIn - grabberPile[0]) * Math.abs(grabberPile[coneNum] - grabberLeft.getPosition())));
+
+            setArmPosition(armOut);
+
+            while (grabberSensor.getDistance(DistanceUnit.CM) < 6) {}
+
+            Thread.sleep(750);
+
+            grabber.setPosition(grabberGrab);
+
+            Thread.sleep(1500);
+
+            setGrabberPosition(grabberMiddle);
+
+            Thread.sleep(750);
+
+            setArmPosition(armIn);
+
+            while (!armSensor.isPressed()) {
+            }
+
+            Thread.sleep(750);
+
+            setGrabberPosition(grabberIn);
+
+            Thread.sleep(750); // replacing the sensor for now
+        }catch (InterruptedException e){}
+    }
+
+    public void terminate(){
+        elevatorController.interrupt();
+        driveController.interrupt();
+        cycleController.interrupt();
+        score.interrupt();
+        autoCycle.interrupt();
+
+        elevatorPosition = elevatorPositions.bottom;
+
+        gamepad.a = false;
+        gamepad.b = false;
+        gamepad.y = false;
+        gamepad.x = false;
+
+        gamepad.left_trigger = 0;
+        gamepad.right_trigger = 0;
+
+        gamepad.left_bumper = false;
+        gamepad.right_bumper = false;
+
+        gamepad.left_stick_y = 0;
+        gamepad.left_stick_x = 0;
+        gamepad.right_stick_x = 0;
+
+        gamepad.driveController = false;
+        gamepad.elevatorController = false;
+        gamepad.cycleController = false;
+
 
     }
 
@@ -327,40 +497,42 @@ class Vector {
     public double x;
     public double y;
 
-    public Vector(double x, double y){
+    public Vector(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double getAngle(){
+    public double getAngle() {
         double a = Math.PI * 2;
         if (y < 0) a += Math.PI + Math.atan(-x / y);
-        else if (y == 0){
+        else if (y == 0) {
             if (x < 0) a += Math.PI * 0.5;
-            else       a += Math.PI * 1.5;
-        }else{
+            else a += Math.PI * 1.5;
+        } else {
             a += Math.atan(-x / y);
         }
         a = Math.PI * 2 - a % (Math.PI * 2);
 
         return a;
     }
-    public double getLength(){
+
+    public double getLength() {
         return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
     }
 
-    public void setAngle(double a){
+    public void setAngle(double a) {
         double l = this.getLength();
         this.x = Math.sin(a) * l;
         this.y = Math.cos(a) * l;
     }
-    public void setLength(double r){
+
+    public void setLength(double r) {
         double a = this.getAngle();
         this.x = Math.sin(a) * r;
         this.y = Math.cos(a) * r;
     }
 
-    public void addAngle(double angle){
+    public void addAngle(double angle) {
         this.setAngle(this.getAngle() + angle);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manualDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/manualDrive.java
@@ -7,98 +7,59 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class manualDrive extends LinearOpMode{
     @Override
     public void runOpMode() {
-        // make a drive controller instance
-        DriveController driveController = new DriveController(hardwareMap);
-        Vector joystick_left = new Vector(0, 0);
+
+        RobotController robotController = new RobotController(hardwareMap, telemetry);
 
         waitForStart();
         if (isStopRequested()) return;
         resetRuntime();
 
-        while (opModeIsActive()) {
-            // get the movement direction and speed
-            joystick_left.x =  gamepad1.left_stick_x;
-            joystick_left.y = -gamepad1.left_stick_y;
+        robotController.cycleController.start();
+        robotController.elevatorController.start();
+        robotController.driveController.start();
 
-            // make the bot field oriented while considering the starting angle
-            joystick_left.addAngle(-driveController.getRobotAngle() - AutonomousDrive.lastAngle);
+        while (opModeIsActive()){
+            gamepad.a = gamepad1.a;
+            gamepad.b = gamepad1.b;
+            gamepad.y = gamepad1.y;
+            gamepad.x = gamepad1.x;
 
-            // slow mode if the right bumper is pressed
-            if (gamepad1.right_bumper) driveController.overallDrivingPower = 0.4;
+            gamepad.left_trigger = gamepad1.left_trigger;
+            gamepad.right_trigger = gamepad1.right_trigger;
 
-            // move the bot with 70% turning speed
-            driveController.mecanumDrive(joystick_left, gamepad1.right_stick_x * 0.7);
+            gamepad.left_bumper = gamepad1.left_bumper;
+            gamepad.right_bumper = gamepad1.right_bumper;
 
-            // move elevator based on button presses
-            if      (A_pressed()) driveController.setElevatorPosition(elevatorPositions.high  );
-            else if (X_pressed()) driveController.setElevatorPosition(elevatorPositions.middle);
-            else if (Y_pressed()) driveController.setElevatorPosition(elevatorPositions.low   );
+            gamepad.left_stick_y = gamepad1.left_stick_y;
+            gamepad.left_stick_x = gamepad1.left_stick_x;
+            gamepad.right_stick_x = gamepad1.right_stick_x;
 
-            // release the cone if the right trigger is pressed
-            else if (right_trigger_pressed() ) driveController.scoreCone();
-            // move the elevator back if the right trigger is released
-            else if (right_trigger_released()) driveController.setElevatorPosition(elevatorPositions.bottom);
-
-            // temp
-            driveController.moveArm(gamepad1.left_trigger);
-
-            // make the commands take effect
-            driveController.update();
         }
-    }
-    private boolean a = true;
-    public boolean A_pressed(){
-        if (gamepad1.a){
-            if(a){
-                a = false;
-                return true;
-            }
-        } else a = true;
-        return false;
-    }
 
-    private boolean x = true;
-    public boolean X_pressed(){
-        if (gamepad1.x){
-            if(x){
-                x = false;
-                return true;
-            }
-        } else x = true;
-        return false;
-    }
+        robotController.terminate();
+        telemetry.clearAll();
+        telemetry.addLine("stop");
+        telemetry.update();
 
-    private boolean y = true;
-    public boolean Y_pressed(){
-        if (gamepad1.y){
-            if(y){
-                y = false;
-                return true;
-            }
-        } else y = true;
-        return false;
     }
+}
 
+class gamepad{
+    public static double right_trigger = 0;
+    public static double left_trigger = 0;
+    public static double right_stick_x = 0;
+    public static double left_stick_x = 0;
+    public static double left_stick_y = 0;
 
-    private boolean right_trigger1 = false;
-    public boolean right_trigger_pressed(){
-        if (gamepad1.right_trigger > 0){
-            if(!right_trigger1){
-                right_trigger1 = true;
-                return true;
-            }
-        } else right_trigger1 = false;
-        return false;
-    }
+    public static boolean right_bumper = false;
+    public static boolean left_bumper = false;
 
-    private boolean right_trigger_was_pressed = false;
-    public boolean right_trigger_released(){
-        if (gamepad1.right_trigger == 0){
-            if(right_trigger_was_pressed){
-                right_trigger_was_pressed = false;
-                return true;
-            }
-        } else right_trigger_was_pressed = true;
-        return false;
-    }
+    public static boolean a = false;
+    public static boolean b = false;
+    public static boolean x = false;
+    public static boolean y = false;
+
+    public static boolean elevatorController = true;
+    public static boolean driveController = true;
+    public static boolean cycleController = true;
 }
