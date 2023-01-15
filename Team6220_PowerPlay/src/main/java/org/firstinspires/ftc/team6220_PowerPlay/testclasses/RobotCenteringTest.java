9c6d8e5f6eed5fcf158e5b93ebd4dcc49bc6e1ac
Jesse <SJJcoding@gmail.com>
2023-01-15 15:55:15 -0800

    lol

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/RobotCenteringTest.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/RobotCenteringTest.java
@@ -16,15 +16,20 @@ public class RobotCenteringTest extends ConeDetection
         detectGrab(lowerRed,upperRed);
         initialize();
         waitForStart();
-        driveSlides(400);
+        driveSlidesAutonomous(400);
         driveGrabber(Constants.GRABBER_OPEN_POSITION);
         while(opModeIsActive()){
-            if(Math.abs(coneDetectionPipeline.distance) > 30){
+            if(Math.abs(coneDetectionPipeline.distance) > 100){
                 telemetry.addData("distance", coneDetectionPipeline.distance);
                 telemetry.update();
                 driveWithIMU(0.4*Math.signum(coneDetectionPipeline.distance),0,0);
             }
             driveWithIMU(0, 0, 0);
+            if(coneDetectionPipeline.coneSize < 54000){
+                driveWithIMU(0,0.4,0);
+            }
+            driveSlidesAutonomous(Constants.SLIDE_LOW);
+            driveGrabber(Constants.GRABBER_CLOSE_POSITION);
         }
     }
 }
