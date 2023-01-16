57d9914a20ed04c76d49170cc5e4a2d23264378d
BlockBeasts <kjosien1@gmail.com>
2023-01-16 14:18:32 -0700

    tuning

--- BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/drive/opmode/DriveVelocityPIDTuner.java
+++ BlackSwan/src/main/java/org/firstinspires/ftc/blackswan/drive/opmode/DriveVelocityPIDTuner.java
@@ -49,7 +49,7 @@ import static org.firstinspires.ftc.blackswan.drive.DriveConstants.kV;
  * Pressing B/O (Xbox/PS4) will cede control back to the tuning process.
  */
 @Config
-@Disabled
+//@Disabled
 @Autonomous(name = "DriveVelocityPIDTuner", group = "drive")
 public class DriveVelocityPIDTuner extends LinearOpMode {
     public static double DISTANCE = 72; // in
