ccc5ba2e679baf444aaf3dbcf982e647e860d262
carlzrobotics <94403712+carlzrobotics@users.noreply.github.com>
2023-01-17 20:28:53 -0600

    telemetry class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/composetelemetry.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+public class composetelemetry {
+    public void composetelem(){
+        //add telemetry
+        //update telemtry
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/telemetry.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode;
-
-public class composetelemetry {
-
-    public void composetelemetry(){
-//add telemetry
-// telemetry.update();
-    }
-}
