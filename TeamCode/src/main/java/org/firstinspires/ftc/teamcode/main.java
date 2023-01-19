ad315734d5c5e5f965291836d4466059c6258abc
not carl <asd@asd.com>
2023-01-19 14:30:21 -0600

    main and movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name = "Bonga")
+@TeleOp(name = "donga")
 
 public class main extends LinearOpMode {
 
