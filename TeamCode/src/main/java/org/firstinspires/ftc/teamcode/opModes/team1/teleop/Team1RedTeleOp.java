05e7987501fb7567b7ad38d42faf321627706ec4
la-reine-c <lawrence.tn.carter@icloud.com>
2023-01-20 17:32:36 +0000

    Add drivetrain [WORKS]

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1RedTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1RedTeleOp.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode.opModes.team1.teleop;
 
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.button.Button;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.PSButtons;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TeamColour;
@@ -15,31 +19,32 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TelemetryContain
 public class Team1RedTeleOp extends TeleOpModeBase { // Ensure you extend the base
     Team1GenericTeleOp teleop;
 
+    DifferentialDrive drive;
+
     @Override
     public void setup() {
+        // TODO: Move to own OpMode
+        
         // Runs once at INIT
         teleop = new Team1GenericTeleOp();
         // Run generic TeleOp
         teleop.setup(TeamColour.RED);
+
+        drive = new DifferentialDrive(HardwareMapContainer.motor0, HardwareMapContainer.motor1);
     }
 
     @Override
     public void every_tick() {
         // Runs in the main loop
         teleop.every_tick();
-        GamepadEx gamepad = Inputs.gamepad1;
-        Motor left_motor = HardwareMapContainer.motor0;
-        Motor right_motor = HardwareMapContainer.motor1;
-        Joystick leftJoystick = new Joystick(gamepad.getLeftX(), gamepad.getLeftY());
-        Joystick rightJoystick = new Joystick(gamepad.getRightX(), gamepad.getRightY());
-        /* Arcade drive */
-        leftJoystick.Transpose(0, 1, -1, 1);
-        DifferentialDrive drive = new DifferentialDrive(left_motor, right_motor);
+        TelemetryContainer.getTelemetry().addLine("No Transpose");
+        Joystick leftJoystick = new Joystick(Inputs.gamepad1.getLeftX(), Inputs.gamepad1.getLeftY());
+//            Joystick rightJoystick = new Joystick(gamepad.getRightX(), gamepad.getRightY());
+
         drive.arcadeDrive(leftJoystick.y, leftJoystick.x);
-        TelemetryContainer.getTelemetry().addData("Joystick Y", gamepad.getLeftY());
+        TelemetryContainer.getTelemetry().addData("Joystick Y", Inputs.gamepad1.getLeftY());
         TelemetryContainer.getTelemetry().addData("Left Joystick - uses Coordinates class - x", leftJoystick.x);
         TelemetryContainer.getTelemetry().addData("Left Joystick - uses coordinates class - y", leftJoystick.y);
-
     }
 }
 
