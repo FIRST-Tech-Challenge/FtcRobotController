e735a88d7a6f9ce5776bbf8e53cc7fa7557f89f6
spamtactics <108055980+spamtactics@users.noreply.github.com>
2023-01-20 17:05:56 +0000

    Attempted to create basic motor inputs

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/Test.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.opModes;
+
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.hardware.GyroEx;
+import com.arcrobotics.ftclib.hardware.RevIMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cGyro;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
+
+/**
+ * Description: [Fill in]
+ * Hardware:
+ *  [motor0] Unused
+ *  [motor1] Unused
+ *  [motor2] Unused
+ *  [motor3] Unused
+ *  [servo0] Unused
+ *  [servo1] Unused
+ *  [servo2] Unused
+ *  [servo3] Unused
+ * Controls:
+ *  [Button] Function
+ */
+@TeleOp(name="<OpMode>", group="<OpMode group name>")
+public class Test extends TeleOpModeBase {
+    // Declare class members here
+
+    RevIMU internal_measurement_unit;
+
+    @Override
+    public void setup() {
+        // Code to run once after `INIT` is pressed
+        internal_measurement_unit = new RevIMU(HardwareMapContainer.getMap());
+        internal_measurement_unit.init();
+    }
+
+    @Override
+    public void every_tick() {
+        // Code to run in a loop after `PLAY` is pressed
+        double startheading = internal_measurement_unit.getAbsoluteHeading();
+        //The required heading
+        double leftX= Inputs.gamepad1.getLeftX();
+        double leftY = Inputs.gamepad1.getLeftY();
+        double nextheading = 
+        //The necessary speed
+        double rightX= Inputs.gamepad1.getRightX();
+        if (rightX!=0){
+            //find the scale of maximum motor speed relative to maximum input for rightX
+        }
+
+
+
+    }
+}
