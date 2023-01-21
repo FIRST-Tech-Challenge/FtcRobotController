fb5e2bd318486bc08ac1e13eacd099b9d536e395
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-01-21 11:41:38 +0000

    Disable+rename test drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/LAREINECExampleDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/LAREINECExampleDriveTrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.opModes.test;
 
 import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
@@ -10,7 +11,8 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TeamColour;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TelemetryContainer;
 import org.firstinspires.ftc.teamcode.opModes.team1.teleop.Team1GenericTeleOp;
 
-@TeleOp(name="Drivetrain Demo", group="Demo") // Registers TeleOp OpMode Entrypoint
+@Disabled
+@TeleOp(name="Drivetrain Demo [la-reine-c]", group="Demo") // Registers TeleOp OpMode Entrypoint
 public class LAREINECExampleDriveTrain extends TeleOpModeBase { // Ensure you extend the base
     Team1GenericTeleOp teleop;
 
