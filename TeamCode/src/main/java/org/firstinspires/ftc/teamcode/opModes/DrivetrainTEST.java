3c8f1e6dce9b2b61b7646a071313e4ad3b3f5932
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-01-21 08:49:38 +0000

    Some fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/DrivetrainTEST.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/DrivetrainTEST.java
@@ -8,6 +8,7 @@ package org.firstinspires.ftc.teamcode.opModes;
         import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
         import com.qualcomm.robotcore.hardware.DcMotor;
         import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+        import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 
 
 @TeleOp(name="SomeName", group="Test")
@@ -15,22 +16,19 @@ package org.firstinspires.ftc.teamcode.opModes;
 public class DrivetrainTEST extends TeleOpModeBase {
     //new code, shoved this here, hope it works?
     GamepadEx gamepadEx = new GamepadEx(gamepad1);
-    Motor m_motor_1;
-    Motor m_motor_2;
-    Motor m_motor_3;
-    Motor m_motor_4;
 
-    //groups motors together for drive
-    MotorGroup myMotors1 = new MotorGroup(m_motor_1, m_motor_2);
-    MotorGroup myMotors2 = new MotorGroup(m_motor_3, m_motor_4);
-    DifferentialDrive m_drive = new DifferentialDrive(myMotors1, myMotors2);
+    DifferentialDrive m_drive;
+    MotorGroup myMotors1;
+    MotorGroup myMotors2;
 
     @Override
     public void setup() {
-        new Motor(hardwareMap, "motorFour");
-        new Motor(hardwareMap, "motorThree");
-        new Motor(hardwareMap, "motorTwo");
-        new Motor(hardwareMap, "motorOne");
+
+        //groups motors together for drive
+        this.myMotors1 = new MotorGroup(HardwareMapContainer.motor0, HardwareMapContainer.motor1);
+        this.myMotors2 = new MotorGroup(HardwareMapContainer.motor2, HardwareMapContainer.motor3);
+        this.m_drive = new DifferentialDrive(myMotors1, myMotors2);
+
         //some of this is unneeded, can remove at a later time
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -41,6 +39,8 @@ public class DrivetrainTEST extends TeleOpModeBase {
     public void every_tick() {
         // Code to run in a loop after `PLAY` is pressed
         telemetry.addData("Status", "Running");
+        telemetry.addData("X", gamepadEx.getLeftX());
+        telemetry.addData("Y", gamepadEx.getLeftY());
         telemetry.update();
         //p sure the test motor thing is useless so I deleted it
         m_drive.arcadeDrive(gamepadEx.getLeftY(),gamepadEx.getLeftX());
