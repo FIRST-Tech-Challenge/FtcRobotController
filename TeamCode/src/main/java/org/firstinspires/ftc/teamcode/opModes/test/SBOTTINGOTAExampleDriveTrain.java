e122486df0d3a69fe47387acd8d469d077f0c902
sbottingota <saku.bottingota@gmail.com>
2023-01-22 11:14:39 +0000

    Added way to change input type into drive mode and added javadoc.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/SBOTTINGOTAExampleDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/SBOTTINGOTAExampleDriveTrain.java
@@ -11,6 +11,18 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeB
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TelemetryContainer;
 
+/**
+ * A simple program to control a drive train.
+ *
+ * Controls:
+ * Left joystick for arcade drive ({@link DifferentialDrive#arcadeDrive(double, double)})
+ * Both joysticks for tank drive ({@link DifferentialDrive#tankDrive(double, double)})
+ *
+ * X to toggle between arcade and tank drive.
+ * Y to toggle between inputting a 0 to 1 value or -1 to 1 value into the current drive mode (test).
+ *
+ * R1 and L1 to make the robot go faster and slower respectively.
+ */
 @Disabled
 @TeleOp(name="Drivetrain Demo [sbottingota]", group="Demo")
 public class SBOTTINGOTAExampleDriveTrain extends TeleOpModeBase {
@@ -23,7 +35,13 @@ public class SBOTTINGOTAExampleDriveTrain extends TeleOpModeBase {
         TANK_DRIVE
     }
 
+    private enum InputTypes {
+        MINUS_ONE_TO_ONE,
+        ZERO_TO_ONE
+    }
+
     private DriveModes driveMode = DriveModes.ARCADE_DRIVE;
+    private InputTypes inputType = InputTypes.ZERO_TO_ONE;
 
     private int speedMultiplier = 1;
 
@@ -34,10 +52,20 @@ public class SBOTTINGOTAExampleDriveTrain extends TeleOpModeBase {
 
     @Override
     public void every_tick() {
-        if (Inputs.gamepad1.wasJustPressed(GamepadKeys.Button.LEFT_STICK_BUTTON)) {
-            driveMode = DriveModes.ARCADE_DRIVE;
-        } else if (Inputs.gamepad1.wasJustPressed(GamepadKeys.Button.RIGHT_STICK_BUTTON)) {
-            driveMode = DriveModes.TANK_DRIVE;
+        if (Inputs.gamepad1.wasJustPressed(GamepadKeys.Button.X)) {
+            if (driveMode.equals(DriveModes.TANK_DRIVE)) {
+                driveMode = DriveModes.ARCADE_DRIVE;
+            } else {
+                driveMode = DriveModes.TANK_DRIVE;
+            }
+        }
+
+        if (Inputs.gamepad1.wasJustPressed(GamepadKeys.Button.Y)) {
+            if (inputType.equals(InputTypes.MINUS_ONE_TO_ONE)) {
+                inputType = InputTypes.ZERO_TO_ONE;
+            } else {
+                inputType = InputTypes.MINUS_ONE_TO_ONE;
+            }
         }
 
         if (Inputs.gamepad1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER)) {
@@ -47,18 +75,29 @@ public class SBOTTINGOTAExampleDriveTrain extends TeleOpModeBase {
         }
 
 
-        if (driveMode == DriveModes.ARCADE_DRIVE) {
-            driveTrain.arcadeDrive(Inputs.gamepad1.getLeftY() * speedMultiplier,
-                    Inputs.gamepad1.getLeftX() * speedMultiplier);
+        if (driveMode.equals(DriveModes.ARCADE_DRIVE)) {
+            double joystickX = Inputs.gamepad1.getLeftX();
+            double joystickY = Inputs.gamepad1.getLeftY();
 
-            telemetry.addData("Let's print the joystick x", Inputs.gamepad1.getLeftX());
-            telemetry.addData("Let's print the joystick y", Inputs.gamepad1.getLeftY());
+
+            driveTrain.arcadeDrive(joystickX * speedMultiplier, joystickY * speedMultiplier);
+
+            telemetry.addData("Joystick x", joystickX);
+            telemetry.addData("Joystick y", joystickY);
         } else {
-            driveTrain.tankDrive(Inputs.gamepad1.getLeftY() * speedMultiplier,
-                    Inputs.gamepad1.getRightY() * speedMultiplier);
+            double leftJoystickY = Inputs.gamepad1.getLeftY();
+            double rightJoystickY = Inputs.gamepad1.getRightY();
 
-            telemetry.addData("Let's print the left joystick y", Inputs.gamepad1.getLeftY());
-            telemetry.addData("Let's print the right joystick y", Inputs.gamepad1.getRightY());
+            if (inputType.equals(InputTypes.ZERO_TO_ONE)) {
+                leftJoystickY = (leftJoystickY + 1) / 2;
+                rightJoystickY = (rightJoystickY + 1) / 2;
+            }
+
+            driveTrain.tankDrive(leftJoystickY * speedMultiplier,
+                    rightJoystickY * speedMultiplier);
+
+            telemetry.addData("Let's print the left joystick y", leftJoystickY);
+            telemetry.addData("Let's print the right joystick y", rightJoystickY);
         }
 
         telemetry.addData("Let's print the speed multiplier", speedMultiplier);
