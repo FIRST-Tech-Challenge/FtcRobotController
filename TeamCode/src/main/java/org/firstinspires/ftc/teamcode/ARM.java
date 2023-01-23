93ce47f2c1a663f962ed30c855da0bdbb219985c
burrobots <103452576+burrobots@users.noreply.github.com>
2023-01-23 14:30:48 -0600

    sdff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ARM.java
@@ -5,4 +5,15 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class ARM {
     public DcMotor Motor5;
     public DcMotor Motor6;
+    public void stop(){
+        Motor5.setPower(0);
+        Motor6.setPower(0);
+    }
+    public void armUp(float power){
+        Motor5.setPower(power*0.7);
+    }
+    public void armExtend(float power){
+        Motor6.setPower(power*0.7);
+    }
+
 }
