90960e6d9a9793a0d971fe916a0adbb5726d22f8
Gobln <caasier42@gmail.com>
2023-01-22 18:20:43 -0700

    Cleaning up the code a little

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleControllerDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleControllerDrive.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
