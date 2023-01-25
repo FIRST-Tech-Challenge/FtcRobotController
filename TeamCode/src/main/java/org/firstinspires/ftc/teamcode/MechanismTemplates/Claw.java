47e5b8e91079d851331e23b4fbe7153d23863c46
yeeT787 <patriotsfan4576@gmail.com>
2023-01-25 16:31:06 -0600

    Pushing just in case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
@@ -17,6 +17,7 @@ public class Claw {
     // claw positions
     public static double OPEN = 0.75;
     public static double CLOSE = 0.58;
+    public static double AUTOCLOSE = 0.44;
 
     // wrist positions
     public static double WRIST_INTAKE_POSITION = 0.255; // wrist rotates to intake cone, greater values move clockwise, less move counterclockwise
@@ -75,7 +76,7 @@ public class Claw {
 
     public void toggleAutoOpenClose(){
         if(clawToggled){
-            clawJoint.setPosition(CLOSE);
+            clawJoint.setPosition(AUTOCLOSE);
         }else {
             clawJoint.setPosition((OPEN));
         }
