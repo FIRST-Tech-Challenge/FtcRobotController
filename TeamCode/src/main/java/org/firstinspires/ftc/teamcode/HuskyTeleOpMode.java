44d8514420cec9dc60ee70cc3ae42ac4856a822f
Suleyman <suleyman@Suleymans-MBP-2.lan>
2023-01-24 19:29:12 -0800

    Cancel preset button added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HuskyTeleOpMode.java
@@ -232,6 +232,16 @@ public class HuskyTeleOpMode extends LinearOpMode {
 
 
         // region ARM PRESETS FINITE STATE MACHINE
+            if(gamepad1.left_stick_button && armState != ArmState.ARM_WAIT){
+                huskyBot.armExtendMotor.setPower(0);
+                huskyBot.armExtendMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                huskyBot.armLiftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                huskyBot.armLiftMotor.setPower(ARM_LIFT_POWER_AT_REST);
+
+                armState = ArmState.ARM_WAIT;
+            }
+
             switch (armState){
                 case ARM_WAIT:
                     finiteTimer.reset();
