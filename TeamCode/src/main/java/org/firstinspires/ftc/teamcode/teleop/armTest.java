82e7c09d5ca711eec9f853d4a035b40a9fb0a6d0
GongInvaders <gonginvaders@gmail.com>
2023-01-27 19:12:12 +1100

    still working on finalising that swerve test code, working on that with jack, now just finished adding an FRC-esque expanded gamepadEX.java class which is included in the robotConfig.java file for consistent access. Mainly includes features related to button.java. with cool lambda options n stuff :))

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/armTest.java
@@ -1,18 +1,17 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.teamUtil.GamepadEX;
+import org.firstinspires.ftc.teamcode.teamUtil.gamepadEX.gamepadEX;
 
-
-@TeleOp(name="Servo Arm Test", group="Worlds")
+@Disabled
+@TeleOp(name="Servo Arm Test (OUTDATED)", group="OUTDATED TEST")
 public class armTest extends OpMode{
     private Servo wrist;
     private Servo arm;
-    private GamepadEX gamepadEX1;
-
     private boolean armState = true;
     private boolean aState = false;
 
@@ -22,7 +21,6 @@ public class armTest extends OpMode{
         telemetry.update();
         wrist = hardwareMap.get(Servo.class, "wrist");
         arm = hardwareMap.get(Servo.class, "arm");
-        gamepadEX1 = new GamepadEX(gamepad1);
         telemetry.addData("Status: ", "Initialised");
         telemetry.update();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/swerveTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/swerveTest.java
@@ -4,7 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.teamUtil.log;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.teamUtil.gamepadEX.button;
 import org.firstinspires.ftc.teamcode.teamUtil.robotConfig;
 import org.firstinspires.ftc.teamcode.teamUtil.robotConstants;
 
@@ -15,6 +16,7 @@ public class swerveTest extends OpMode{
     private final ElapsedTime runTime = new ElapsedTime();
     boolean flipInputPressed = false;
     boolean robotFlipped = false;
+    Telemetry.Item item1;
 
     @Override
     public void init(){
@@ -31,22 +33,21 @@ public class swerveTest extends OpMode{
 
     @Override
     public void start() {
+        r.gamepadEX1.a.debounce(button.debouncingType.BOTH, 0.02);
         runTime.reset();
+        item1 = telemetry.addData("mbruh", false);
     }
-
     @Override
     public void loop(){
-        if(!flipInputPressed && gamepad1.a){
-            robotFlipped = !robotFlipped;
-            flipInputPressed = true;
-        }
-        else if (!gamepad1.a){
-            flipInputPressed = false;
-        }
+        r.gamepadEX1.a.actionOnPress(() -> {
+            item1.setValue(r.gamepadEX1.b.raw());
+        });
         r.encoderRead.encoderBulkRead();
         r.swerve.manualDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_stick_y, (1-gamepad1.right_trigger), false);
 
-        telemetry.update();
+
+
+        r.systemsUpdate();
     }
 
     @Override
