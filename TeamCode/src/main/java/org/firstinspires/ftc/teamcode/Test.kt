0e5503957f379826cb574cf9a5274f4fbf09339b
ninefox <unvariant.winter@gmail.com>
2023-01-28 14:34:32 -0800

    test kotlin code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.kt
@@ -0,0 +1,15 @@
+@file:JvmName("Test")
+
+package org.firstinspires.ftc.teamcode
+
+class Test {
+    companion object {
+        @JvmStatic
+        fun truthy(): Boolean {
+            return true
+        }
+
+        @JvmField
+        var inner: Int = 0;
+    }
+}
