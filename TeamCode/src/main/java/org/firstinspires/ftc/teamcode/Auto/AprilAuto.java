4aeccd9d6ea4d12d6eee9db5323add86c2707ab1
Isaac Mattson <mattsonisaac06@gmail.com>
2023-02-01 16:19:51 -0700

    idk man

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AprilAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AprilAuto.java
@@ -1,26 +1,7 @@
-/*
- * Copyright (c) 2021 OpenFTC Team
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
 
 package org.firstinspires.ftc.teamcode.Auto;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -33,93 +14,117 @@ import org.openftc.easyopencv.OpenCvInternalCamera;
 
 import java.util.ArrayList;
 
-@TeleOp
-public class AprilTest extends LinearOpMode
+
+public class AprilAuto extends LinearOpMode
 {
+    //INTRODUCE VARIABLES HERE
+
     OpenCvCamera camera;
     AprilTagDetectionPipeline aprilTagDetectionPipeline;
 
     static final double FEET_PER_METER = 3.28084;
 
-    // Lens intrinsics
-    // UNITS ARE PIXELS
-    // NOTE: this calibration is for the C920 webcam at 800x448.
-    // You will need to do your own calibration for other configurations!
+    //This is fancy stuff, idk man I don't think this needs to be changed.
+    //If it keeps being finicky than maybe.
+
     double fx = 578.272;
     double fy = 578.272;
     double cx = 402.145;
     double cy = 221.506;
 
     // UNITS ARE METERS
-    double tagsize = 0.166; //THIS MIGHT NEED TO BE CHANGED FOR US
+    double tagsize = 0.166;
 
-    int ID_TAG_OF_INTEREST = 18; // Tag ID 18 from the 36h11 family
+    // Tag ID 1,2,3 from the 36h11 family
 
-    int left = 17;
-    int middle = 18;
-    int right = 19;
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
 
     AprilTagDetection tagOfInterest = null;
 
     @Override
-    public void runOpMode() {
+    public void runOpMode()
+    {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
         aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
 
         camera.setPipeline(aprilTagDetectionPipeline);
-        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
             @Override
-            public void onOpened() {
-                camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+            public void onOpened()
+            {
+                camera.startStreaming(1280,720, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
-            public void onError(int errorCode) {
+            public void onError(int errorCode)
+            {
 
             }
         });
 
         telemetry.setMsTransmissionInterval(50);
 
+
+        //HARDWARE MAPPING HERE etc.
+
+
         /*
          * The INIT-loop:
          * This REPLACES waitForStart!
          */
-        while (!isStarted() && !isStopRequested()) {
+        while (!isStarted() && !isStopRequested())
+        {
             ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
 
-            if (currentDetections.size() != 0) {
+            if(currentDetections.size() != 0)
+            {
                 boolean tagFound = false;
 
-                for (AprilTagDetection tag : currentDetections) {
-                    if (tag.id == ID_TAG_OF_INTEREST) {
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == LEFT || tag.id == MIDDLE || tag.id == RIGHT)
+                    {
                         tagOfInterest = tag;
                         tagFound = true;
                         break;
                     }
                 }
 
-                if (tagFound) {
+                if(tagFound)
+                {
                     telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
                     tagToTelemetry(tagOfInterest);
-                } else {
-                    telemetry.addLine("Don't see tag of interest :(");
+                }
+                else
+                {
+                    telemetry.addLine("Don't see the right tags :(");
 
-                    if (tagOfInterest == null) {
+                    if(tagOfInterest == null)
+                    {
                         telemetry.addLine("(The tag has never been seen)");
-                    } else {
+                    }
+                    else
+                    {
                         telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                         tagToTelemetry(tagOfInterest);
                     }
                 }
 
-            } else {
-                telemetry.addLine("Don't see tag of interest :(");
+            }
+            else
+            {
+                telemetry.addLine("Don't see any tags :(");
 
-                if (tagOfInterest == null) {
+                if(tagOfInterest == null)
+                {
                     telemetry.addLine("(The tag has never been seen)");
-                } else {
+                }
+                else
+                {
                     telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
                     tagToTelemetry(tagOfInterest);
                 }
@@ -130,36 +135,28 @@ public class AprilTest extends LinearOpMode
             sleep(20);
         }
 
-        /*
-         * The START command just came in: now work off the latest snapshot acquired
-         * during the init loop.
-         */
 
-        /* Update the telemetry */
-        if (tagOfInterest != null) {
+
+
+
+        if(tagOfInterest != null)
+        {
             telemetry.addLine("Tag snapshot:\n");
             tagToTelemetry(tagOfInterest);
             telemetry.update();
-        } else {
+        }
+        else
+        {
             telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
             telemetry.update();
         }
 
-        /* Cool Shit */
-        if (tagOfInterest == null || tagOfInterest.id == left) {
-            // This would be left code, i would presume call a func to move to a spot we need.
+        //PUT AUTON CODE HERE (DRIVER PRESSED THE PLAY BUTTON!)
 
-        } else if (tagOfInterest.id == middle) {
-
-            // Same as the left code, just if we see the middle tag.
-
-        } else {
-
-            // If we see the right tag, or something goes very wrong.
-
-        }
     }
-    void tagToTelemetry(AprilTagDetection detection){
+
+    void tagToTelemetry(AprilTagDetection detection)
+    {
         telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
         telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
         telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
