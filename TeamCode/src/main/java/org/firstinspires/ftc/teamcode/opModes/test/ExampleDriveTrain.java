571b9a165bc0b94393ccdda2b14f8c098b482fd9
spamtactics <108055980+spamtactics@users.noreply.github.com>
2023-02-03 16:57:55 +0000

    Update ExampleDriveTrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/ExampleDriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/ExampleDriveTrain.java
@@ -44,7 +44,43 @@ public class ExampleDriveTrain extends TeleOpModeBase {
         internal_measurement_unit = new RevIMU(HardwareMapContainer.getMap());
         internal_measurement_unit.init();
     }
+    @Override
+    public void Autonomous(){
+        //Code for autonomous running
+        //Coordinate in meters from start point in terms of x and y
+        double[][] pole_Coordinates={};
+        double[][] dock_Coordinates={};
+        double[] current={0,0};
+        int i=0
+        while(time<20){
+            //Distance from target in terms of x distance and y distance
+            double[] next = pole_Coordinates[i];
+            double[] distance={} ;
+            distance[0]= current[0]-next[0];
+            distance[1]= current[1]-next[1];
+            double idealheading= Math.atan2(distance[1],distance[0]);
+            double startheading = internal_measurement_unit.getAbsoluteHeading();
+            if(startheading==idealheading){
+                //Drive at full speed towards target
+                motor1.set(1)
+            } else if ((startheading-idealheading>0)|| (startheading-idealheading<-180)) {
+                //Cause the motor to turn left
+                motor1.set(-1);
+                motor2.set(1);
 
+            } else{
+                //Cause the motor to turn right
+                motor1.set(1);
+                motor2.set(-1);
+            }
+            //find a way to tell if you have arrived
+        }
+
+
+
+
+
+    }
     @Override
     public void every_tick() {
         // Code to run in a loop after `PLAY` is pressed
@@ -59,12 +95,14 @@ public class ExampleDriveTrain extends TeleOpModeBase {
             double rightX= Inputs.gamepad1.getRightX();
             motor1.set(rightX)
         } else if ((startheading-nextheading>0)|| (startheading-nextheading<-180)) {
-            motor1.set(1);
-            motor2.set(-1);
-
-        } else{
+            //Cause the motor to turn left
             motor1.set(-1);
             motor2.set(1);
+
+        } else{
+            //Cause the motor to turn right
+            motor1.set(1);
+            motor2.set(-1);
         }
 
 
