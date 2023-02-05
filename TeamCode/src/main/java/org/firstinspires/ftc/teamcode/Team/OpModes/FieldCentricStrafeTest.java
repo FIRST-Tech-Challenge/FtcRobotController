1aa1a93fde60373b460c28cfbcd88fc66333ba95
fishstew <42249546+krill11@users.noreply.github.com>
2023-02-04 19:36:13 -0800

    Added comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team/OpModes/FieldCentricStrafeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Team/OpModes/FieldCentricStrafeTest.java
@@ -10,47 +10,45 @@ import org.firstinspires.ftc.teamcode.Team.ComplexRobots.Robot;
 @TeleOp
 public class FieldCentricStrafeTest extends LinearOpMode {
 
-    public int RFMotor = 0;
-    public int RBMotor = 1;
-    public int LFMotor = 2;
-    public int LBMotor = 3;
-
     public Robot robot = new Robot();
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         robot.init(hardwareMap);
-
+        //Makes a new IMU object
         BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        //Sets IMU Units to Radian
         parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        //Inits IMU
         imu.initialize(parameters);
-
+        //Waits for play button
         waitForStart();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            //gets stick coordinates
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
-
+            //Gets the bots heading (direction)
             double botHeading = -imu.getAngularOrientation().firstAngle;
-
+            //Does trig on it to determine vectors of movement in x and y directions
             double rotX = x * Math.cos(botHeading) - y * Math.sin(botHeading);
             double rotY = x * Math.sin(botHeading) + y * Math.cos(botHeading);
-
+            //Adds the X, Y, and rotation vectors in different combination to determine power to each wheel
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
-
-            robot.motors[LFMotor].setPower(frontLeftPower);
-            robot.motors[RBMotor].setPower(backRightPower);
-            robot.motors[RFMotor].setPower(frontRightPower);
-            robot.motors[LBMotor].setPower(backLeftPower);
+            //Sets the motor power
+            robot.motors[robot.LFMotor].setPower(frontLeftPower);
+            robot.motors[robot.RBMotor].setPower(backRightPower);
+            robot.motors[robot.RFMotor].setPower(frontRightPower);
+            robot.motors[robot.LBMotor].setPower(backLeftPower);
         }
     }
 }
