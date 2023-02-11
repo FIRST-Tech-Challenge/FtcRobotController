1f882411b67b7aa6f61fa344ef4bd1e420063784
Gobln <caasier42@gmail.com>
2023-02-10 21:07:40 -0700

    Update constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.01368;
+    public static double kA = 0.00032;
+    public static double kStatic = 0.06135;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
