5cd3eba7732311abb7b34cb8369232225bf60eee
Fiyrflash <Fiyrflash@gmail.com>
2023-02-11 20:59:33 -0500

    new encoder test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CraneEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CraneEncoderTest.java
@@ -9,19 +9,36 @@ public class CraneEncoderTest extends LinearOpMode {
 
 
     private DcMotor crane;
+    private DcMotor spin;
 
     public void runOpMode() {
         crane = hardwareMap.get(DcMotor.class, "Crane");
+        spin =hardwareMap.get(DcMotor.class,"Spin");
         crane.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        crane.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //crane.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         waitForStart();
 
         while (opModeIsActive()) {
 
             double throttle;
-            throttle = gamepad1.left_stick_y;
+            boolean spinpowerup;
+            boolean spinpowerdown;
+            spinpowerup = gamepad2.dpad_right;
+            spinpowerdown =gamepad2.dpad_left;
+            throttle = gamepad2.left_stick_y;
+
+            if (spinpowerdown){
+                spin.setPower(-1);
+            }
+            if (spinpowerup){
+                spin.setPower(1);
+            }
+            if (!spinpowerdown&&!spinpowerup){
+                spin.setPower(0);
+            }
 
             crane.setPower(throttle);
+            telemetry.addData("spin encoder",spin.getCurrentPosition());
             telemetry.addData("encoder value", crane.getCurrentPosition());
             telemetry.update();
         }
