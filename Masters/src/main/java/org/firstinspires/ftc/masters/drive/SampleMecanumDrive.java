70b71ca558be8c10d4c7822ba3adde3030639098
karine josien <karine.josien@domo.com>
2023-02-13 17:07:23 -0700

    set back camera with cycling

--- Masters/src/main/java/org/firstinspires/ftc/masters/drive/SampleMecanumDrive.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/drive/SampleMecanumDrive.java
@@ -445,7 +445,7 @@ public class SampleMecanumDrive extends MecanumDrive {
 
     }
 
-    public boolean alignPole(PowerPlayComputerVisionPipelines.SleevePipeline.PipePosition pos) {
+    public boolean alignPole(PowerPlayComputerVisionPipelines.PipePosition pos) {
         switch (pos) {
             case LEFT1:
             case LEFT2:
