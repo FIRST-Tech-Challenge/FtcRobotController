63d0a755c69aee93c61f563f831873cb52d7d12e
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-02-16 16:13:13 -0600

    2/16/23 Skeleton auto made but still needs to be tuned. Odds are software has a week to get an auto running.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Auto_TrajectorySequences/MainAutos/SPLINEAutoLeftSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/Auto_TrajectorySequences/MainAutos/SPLINEAutoLeftSide.java
@@ -24,14 +24,14 @@ public class SPLINEAutoLeftSide extends PowerPlay_AprilTagDetectionDeposit {
 
 	public static double angle = 150;
 	public static double angleConeStack = 95;
-	public static double xConeStack = 51.5;
+	public static double xConeStack = 51.5; // TODO: Change this value before forward() command
 	public static double yConeStack = 29.5;
 	public static int armPosition = 935;
 	public static double firstStrafe = 17;
 
 	////////////// FIRST \\\\\\\\\\\\\\\\\\\\\\
-	public static double xFirstLinear = 65;
-	public static double yFirstLinear = 2.5;
+	public static double xFirstLinear = 65; // needs to change for medium junction
+	public static double yFirstLinear = 2.5; // needs to change for medium junction
 	public static double angleFirstLinear = 85;
 
 	////////////// SECOND \\\\\\\\\\\\\\\\\\\\\\
@@ -59,12 +59,10 @@ public class SPLINEAutoLeftSide extends PowerPlay_AprilTagDetectionDeposit {
 	public static double xParkZoneThree=-46;
 	public static double yParkZoneThree=-25;
 
-
-
-
 	public static int strafeThird = -28;
 
 	public void initialize(){
+		//If 13.53 V, y = -3.5
 		armControl = new Arm(hardwareMap);
 		slideControl = new Slide(hardwareMap);
 	}
@@ -79,8 +77,6 @@ public class SPLINEAutoLeftSide extends PowerPlay_AprilTagDetectionDeposit {
 		Pose2d startPose = new Pose2d(0,0,Math.toRadians(180));
 		SampleMecanumDrive bot = new SampleMecanumDrive(hardwareMap);
 		bot.setPoseEstimate(startPose);
-
-		//Trajectories
 		initialize();
 
 		TrajectorySequence junction = bot.trajectorySequenceBuilder(startPose)
@@ -89,45 +85,49 @@ public class SPLINEAutoLeftSide extends PowerPlay_AprilTagDetectionDeposit {
 					OdoPod odoControl = new OdoPod(hardwareMap);
 				})
 				.waitSeconds(0.5)
-				//If 13.53 V, y = -3.5
-				// Mechanisms deposit preload at high junction with a 0.65 delay
+				// Mechanisms deposit preload at medium junction
 				.UNSTABLE_addTemporalMarkerOffset(0.75,()->{
-					slideControl.setCustom(2200);
+					slideControl.setMidJunction();
 					armControl.setExtake();
 					clawControl.toggleWristRotate();
-
 				})
-				// MOVES TO HIGH JUNCTION FOR FIRST TIME
+				// MOVES TO MEDIUM JUNCTION FOR FIRST TIME
 				.lineToLinearHeading(new Pose2d(xFirstLinear, yFirstLinear, Math.toRadians(angleFirstLinear)))
 				.waitSeconds(0.5)
 				.UNSTABLE_addTemporalMarkerOffset(0,()->{
-					slideControl.setCustom(1370); // Smacks down into the high junction
+					slideControl.setCustom(750); // Smacks down into the Medium junction
 				})
 				.waitSeconds(.35)
-					// DEPOSIT AT HIGH JUNCTION
+					// DEPOSIT AT MEDIUM JUNCTION
 					.UNSTABLE_addTemporalMarkerOffset(0,()->{
 						clawControl.toggleAutoOpenClose();
 					})
 					.waitSeconds(0.5)
-
-				.UNSTABLE_addTemporalMarkerOffset(0, ()->{
-					armControl.setIntake();
-					clawControl.toggleWristRotate();
-				})
+					.UNSTABLE_addTemporalMarkerOffset(0, ()->{
+						armControl.setIntake();
+						clawControl.toggleWristRotate();
+						slideControl.setCustom(1310); // TODO: change this value
+					})
 				.waitSeconds(0.15)
 
-				// CONE STACK TO HIGH JUNCTION
+				// MEDIUM JUNCTION TO CONE STACK
 				.splineToLinearHeading(new Pose2d(xConeStack, yConeStack, Math.toRadians(90)), Math.toRadians(0))
-				//                               (x position, y position, angle of the bot), shape of the spline)
+				                               // (x position, y position, angle of the bot), shape of the spline)
+				.forward(2) // TODO: change this value
 				.waitSeconds(0.5)
-				.UNSTABLE_addTemporalMarkerOffset(0, () ->{
-					clawControl.toggleAutoOpenClose();
-				})
-				.waitSeconds(2)
-				.UNSTABLE_addTemporalMarkerOffset(0, () ->{
-					// idk if this is the right value to raise the slides slightly after moving away from the cone stack slideControl.setCustom(1310);
+					.UNSTABLE_addTemporalMarkerOffset(0, () ->{
+						clawControl.toggleAutoOpenClose();
+					})
+				.waitSeconds(0.5)
+					.UNSTABLE_addTemporalMarkerOffset(0, () ->{
+						slideControl.setCustom(1400); // TODO: change this value
+					})
+				.waitSeconds(0.5)
 
-				})
+				// CONE STACK TO MEDIUM JUNCTION
+				.splineToLinearHeading(new Pose2d(xFirstLinear, yFirstLinear, Math.toRadians(angleFirstLinear)), Math.toRadians(90)) // TODO: change this value
+				.waitSeconds(2)
+
 
 
 
