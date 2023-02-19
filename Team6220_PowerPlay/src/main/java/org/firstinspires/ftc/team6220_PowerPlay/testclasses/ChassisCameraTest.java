6d23c0494621510619908ed67b472dbeb8d16352
Jesse <SJJcoding@gmail.com>
2023-02-19 15:51:58 -0800

    Made teleop framework for vision testing

--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/ChassisCameraTest.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/ChassisCameraTest.java
@@ -2,16 +2,29 @@ package org.firstinspires.ftc.team6220_PowerPlay.testclasses;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.team6220_PowerPlay.BaseAutonomous;
+import org.firstinspires.ftc.team6220_PowerPlay.Constants;
 import org.firstinspires.ftc.team6220_PowerPlay.GrabberCameraPipeline;
+import org.firstinspires.ftc.team6220_PowerPlay.RobotCameraPipeline;
 import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "Chassis camera test", group = "Camera testing")
 public class ChassisCameraTest extends BaseAutonomous {
     @Override
     public void runOpMode() throws InterruptedException {
-        initialize();
+
+        //Initialize new cameras due to funny init method
+        int robotCameraStream = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        robotCamera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "RobotCamera"),robotCameraStream);
+        robotCameraPipeline = new RobotCameraPipeline();
+
+        //Set camera ranges (Blue atm)
+        robotCameraPipeline.setRanges(Constants.LOWER_BLUE, Constants.UPPER_BLUE);
+
+        //Start camera streaming
         robotCamera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
@@ -21,7 +34,8 @@ public class ChassisCameraTest extends BaseAutonomous {
             @Override
             public void onError(int errorCode) {}
         });
-        robotCamera.setPipeline(grabberCameraPipeline);
+        //Set pipeline
+        robotCamera.setPipeline(robotCameraPipeline);
         telemetry.addLine("6220 rap at worlds");
         waitForStart();
         while(opModeIsActive()){
--- Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/TeleOpTest.java
+++ Team6220_PowerPlay/src/main/java/org/firstinspires/ftc/team6220_PowerPlay/testclasses/TeleOpTest.java
@@ -3,15 +3,63 @@ package org.firstinspires.ftc.team6220_PowerPlay.testclasses;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.team6220_PowerPlay.BaseTeleOp;
+import org.firstinspires.ftc.team6220_PowerPlay.Constants;
+import org.firstinspires.ftc.team6220_PowerPlay.RobotCameraPipeline;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
 
-@Disabled
-@TeleOp(name = "TeleOpTest")
+@TeleOp(name = "TeleOpCameraTest")
 public class TeleOpTest extends BaseTeleOp {
 
     @Override
     public void runOpMode() {
-        initialize();
+        String color = "blue";
+        //Initialize new cameras due to funny init method
+        int robotCameraStream = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        robotCamera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "RobotCamera"),robotCameraStream);
+        robotCameraPipeline = new RobotCameraPipeline();
+
+        //Set camera ranges (Blue atm)
+        robotCameraPipeline.setRanges(Constants.LOWER_BLUE, Constants.UPPER_BLUE);
+
+        //Start camera streaming
+        robotCamera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                robotCamera.startStreaming(800, 600, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+        //Set pipeline
+        robotCamera.setPipeline(robotCameraPipeline);
+        telemetry.addLine("6220 rap at worlds");
         waitForStart();
+        while(opModeIsActive()){
+            if(gamepad1.b){
+                robotCameraPipeline.setRanges(Constants.LOWER_RED, Constants.UPPER_RED);
+                color = "red";
+            }else if (gamepad1.x){
+                robotCameraPipeline.setRanges(Constants.LOWER_BLUE, Constants.UPPER_BLUE);
+                color = "blue";
+            }else if(gamepad1.a){
+                robotCameraPipeline.setRanges(Constants.LOWER_BLACK, Constants.UPPER_BLACK);
+                color = "black";
+            }else if(gamepad1.y){
+                robotCameraPipeline.setRanges(Constants.LOWER_YELLOW, Constants.UPPER_YELLOW);
+                color = "yellow";
+            }
+            telemetry.addLine(color);
+            telemetry.addData("width", robotCameraPipeline.width);
+            telemetry.addData("positionX", robotCameraPipeline.xPosition);
+            telemetry.addData("positionY", robotCameraPipeline.yPosition);
+            telemetry.addData("distance from center", (400-robotCameraPipeline.xPosition));
+            telemetry.addData("L-H ratio", (robotCameraPipeline.width / robotCameraPipeline.height));
+            telemetry.update();
+        }
     }
 }
