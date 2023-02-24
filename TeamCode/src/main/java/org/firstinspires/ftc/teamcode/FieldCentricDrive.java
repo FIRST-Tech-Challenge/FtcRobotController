32be4cb9325e1e6fa0fe76314f4c4c08e0013688
Gobln <caasier42@gmail.com>
2023-02-24 15:18:01 -0700

    Update with example code to hopefully get rid of bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricDrive.java
@@ -1,72 +1,41 @@
-//Code from https://gm0.org/en/latest/docs/software/tutorials/mecanum-drive.html
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.IMU;
 
-@TeleOp(name="FieldCentricDrive", group="Linear Opmode")
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
 public class FieldCentricDrive extends LinearOpMode {
-
-    private ElapsedTime runtime = new ElapsedTime();
-    private DcMotorEx leftFrontDrive = null;
-    private DcMotorEx leftBackDrive = null;
-    private DcMotorEx rightFrontDrive = null;
-    private DcMotorEx rightBackDrive = null;
-
-    private DcMotor liftMotor = null;
-    private Servo servoGrabber1 = null;
-    private Servo servoGrabber2 = null;
-
-    static final double MAX_POS     =    .5;
-    static final double MAX_POS2    =    .50;
-    static final double MIN_POS     =     1;
-    static final double MIN_POS2    =     0;
-
-    double direction = 0;
-    double position = 1;
-    double position2 = 0;
-
     @Override
-    public void runOpMode() {
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor leftFrontDrive = hardwareMap.dcMotor.get("left_front_drive");
-        DcMotor leftBackDrive = hardwareMap.dcMotor.get("left_back_drive");
-        DcMotor rightFrontDrive = hardwareMap.dcMotor.get("right_front_drive");
-        DcMotor rightBackDrive = hardwareMap.dcMotor.get("right_back_drive");
-
-        liftMotor  = hardwareMap.get(DcMotor.class, "lift_motor");
-        servoGrabber1 = hardwareMap.get(Servo.class, "servo_grabber_one");
-        servoGrabber2 = hardwareMap.get(Servo.class, "servo_grabber_two");
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
 
         // Reverse the right side motors
         // Reverse left motors if you are using NeveRests
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         // Retrieve the IMU from the hardware map
-        BNO055IMU imu = hardwareMap.get(BNO055IMU.class, "imu");
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        // Technically this is the default, however specifying it is clearer
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        // Without this, data retrieving from the IMU throws an exception
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        // Adjust the orientation parameters to match your robot
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
         imu.initialize(parameters);
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        servoGrabber1.setPosition(position);
-        servoGrabber2.setPosition(position2);
-
         waitForStart();
-        runtime.reset();
 
         if (isStopRequested()) return;
 
@@ -75,25 +44,32 @@ public class FieldCentricDrive extends LinearOpMode {
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
 
-            // Read inverse IMU heading, as the IMU heading is CW positive
-            double botHeading = -imu.getAngularOrientation().firstAngle;
+            // This button choice was made so that it is hard to hit on accident,
+            // it can be freely changed based on preference.
+            // The equivalent button is start on Xbox-style controllers.
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
 
-            double rotX = (x*x) * Math.cos(botHeading) - (y*y) * Math.sin(botHeading);
-            double rotY = (x*x) * Math.sin(botHeading) + (y*y) * Math.cos(botHeading);
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bots rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio, but only when
             // at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
             double frontLeftPower = (rotY + rotX + rx) / denominator;
             double backLeftPower = (rotY - rotX + rx) / denominator;
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
-            leftFrontDrive.setPower(frontLeftPower);
-            leftBackDrive.setPower(backLeftPower);
-            rightFrontDrive.setPower(frontRightPower);
-            rightBackDrive.setPower(backRightPower);
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
         }
     }
 }
