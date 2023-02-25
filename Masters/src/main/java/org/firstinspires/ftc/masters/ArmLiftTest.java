8cf075be230c6dd4a4fa65bb010f2d22d49edb4b
BlockBeasts <kjosien1@gmail.com>
2023-02-24 17:50:11 -0700

    arm

--- Masters/src/main/java/org/firstinspires/ftc/masters/ArmLiftTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/ArmLiftTest.java
@@ -5,10 +5,17 @@ import static org.firstinspires.ftc.masters.BadgerConstants.ARM_BACK;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.roadrunner.control.PIDCoefficients;
+import com.acmerobotics.roadrunner.control.PIDFController;
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.masters.drive.SampleMecanumDrive;
+
+import java.util.Date;
 
 @Config
 @Autonomous(name = "Arm Lift Test", group="pid")
@@ -21,6 +28,14 @@ public class ArmLiftTest extends LinearOpMode {
     public static int armTarget = 0;
     public static int liftTarget = 0;
     public static double maxSpeed = 0.3;
+
+    public static int maxVel = 25;
+    public static int maxAccel=40;
+    public static int maxJerk = 100;
+
+    public static double kP=0,kI=0,kD =0;
+
+    public int previousTarget=0;
 
 
     @Override
@@ -35,37 +50,82 @@ public class ArmLiftTest extends LinearOpMode {
         drive.closeClaw();
 
 
+        MotionProfile motionProfile=null;
+        PIDFController controller =null;
+
         waitForStart();
 
         drive.closeClaw();
         drive.tipBack();
 
+        long startTime = new Date().getTime();
+
 
         while (opModeIsActive() && !isStopRequested()) {
 
+            if (armTarget != previousTarget) {
+                previousTarget = armTarget;
 
-            armPIDController.setTarget(armTarget);
-            double armPower = armPIDController.calculateVelocity();
-            if (armTarget<0){
-                if (armPower<0){
-                    armPower= Math.max(armPower, -maxSpeed);
-                }
-                else armPower = Math.min(armPower, maxSpeed);
+
+                motionProfile = MotionProfileGenerator.generateSimpleMotionProfile(new MotionState(drive.armMotor.getCurrentPosition(), 0, 0),
+                        new MotionState(armTarget, 0, 0),
+                        25,
+                        40,
+                        100);
+                startTime = new Date().getTime();
             }
-            drive.armMotor.setPower(armPower);
+            PIDCoefficients coeffs = new PIDCoefficients(kP, kI, kD);
+// create the controller
+            controller = new PIDFController(coeffs);
 
-            liftPIDController.setTarget(liftTarget);
+// specify the setpoint
+            controller.setTargetPosition(armTarget);
 
-            double power = liftPIDController.calculatePower();
 
-            drive.linearSlide.setPower(power);
-            drive.frontSlide.setPower(power);
-            drive.slideOtherer.setPower(power);
+// in each iteration of the control loop
+// measure the position or output variable
+// apply the correction to the input variable
 
-            //  telemetry.addData("power ", power);
+            if (motionProfile!=null && controller!=null) {
+                MotionState state = motionProfile.get((new Date().getTime() - startTime) / 1000);
+
+                controller.setTargetPosition(state.getX());
+                controller.setTargetVelocity(state.getV());
+                controller.setTargetAcceleration(state.getA());
+
+
+
+                telemetry.addData("x", state.getX());
+                telemetry.addData("v", state.getV());
+                telemetry.addData("a", state.getA());
+
+
+                double correction = controller.update(drive.armMotor.getCurrentPosition());
+
+//            armPIDController.setTarget(armTarget);
+//            double armPower = armPIDController.calculateVelocity();
+//            if (armTarget<0){
+//                if (armPower<0){
+//                    armPower= Math.max(armPower, -maxSpeed);
+//                }
+//                else armPower = Math.min(armPower, maxSpeed);
+//            }
+            drive.armMotor.setPower(correction);
+//
+//            liftPIDController.setTarget(liftTarget);
+//
+//            double power = liftPIDController.calculatePower();
+//
+//            drive.linearSlide.setPower(power);
+//            drive.frontSlide.setPower(power);
+//            drive.slideOtherer.setPower(power);
+
+                //  telemetry.addData("power ", power);
+                telemetry.addData("power", correction);
+            }
             telemetry.addData("arm target", armTarget);
             telemetry.addData("arm position", drive.armMotor.getCurrentPosition());
-            telemetry.addData("lift target", liftTarget);
+
             telemetry.addData(" lift position", drive.linearSlide.getCurrentPosition());
 
             telemetry.update();
--- Masters/src/main/java/org/firstinspires/ftc/masters/ArmLiftTestMotionProfile.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/ArmLiftTestMotionProfile.java
@@ -7,11 +7,12 @@ import com.arcrobotics.ftclib.controller.wpilibcontroller.ProfiledPIDController;
 import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.masters.drive.SampleMecanumDrive;
 
 @Config
-@Autonomous(name = "Arm Lift Test Motion Profile", group="pid")
+@TeleOp(name = "Arm Lift Test Motion Profile", group="pid")
 public class ArmLiftTestMotionProfile extends LinearOpMode {
 
 
@@ -21,7 +22,7 @@ public class ArmLiftTestMotionProfile extends LinearOpMode {
     public static int armTarget = 0;
     public static int liftTarget = 0;
     public static double maxSpeed = 0.3;
-    public static double kp=0, ki=0, kd=0;
+    public static double kp=6, ki=0, kd=0.09;
 
 
     @Override
@@ -32,10 +33,10 @@ public class ArmLiftTestMotionProfile extends LinearOpMode {
 
         liftPIDController = new LiftPIDController(drive.linearSlide, drive.frontSlide, drive.slideOtherer);
 
-        TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(2500, 1500);
-        Object kDt;
-        ProfiledPIDController m_controller = new ProfiledPIDController(kp, ki, kd, m_constraints);
+        TrapezoidProfile.Constraints m_constraints = new TrapezoidProfile.Constraints(2800, 3000);
 
+        ProfiledPIDController m_controller = new ProfiledPIDController(kp, ki, kd, m_constraints);
+m_controller.setTolerance(3);
 
         drive.tipCenter();
         drive.closeClaw();
@@ -48,20 +49,24 @@ public class ArmLiftTestMotionProfile extends LinearOpMode {
 
 
         while (opModeIsActive() && !isStopRequested()) {
+            m_controller.setD(kd);
+            m_controller.setI(ki);
+            m_controller.setP(kp);
 
             m_controller.setGoal(armTarget);
 
-            if(!m_controller.atGoal()) {
+           // if(!m_controller.atGoal()) {
                 double output = m_controller.calculate(
                         drive.armMotor.getCurrentPosition()  // the measured value
                 );
+                telemetry.addData("output", output);
                 drive.armMotor.setVelocity(output);
                 telemetry.addData("arm target", armTarget);
                 telemetry.addData("arm position", drive.armMotor.getCurrentPosition());
 
 
                 telemetry.update();
-            }
+           // }
 
 
 
