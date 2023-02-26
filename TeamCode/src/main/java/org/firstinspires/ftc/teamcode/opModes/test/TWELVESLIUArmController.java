2498438b0563fa85a6d20e0247fc2c0dd026243c
WebCoder49 <69071853+WebCoder49@users.noreply.github.com>
2023-02-26 17:32:49 +0000

    Update CRServo to motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TWELVESLIUArmController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TWELVESLIUArmController.java
@@ -2,22 +2,19 @@ package org.firstinspires.ftc.teamcode.opModes.test;
 
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
-import com.arcrobotics.ftclib.command.button.Trigger;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
-import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.PSButtons;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeBase;
+import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 
 /**
  * Description: [Fill in]
  * Hardware:
- *  [motor0] Left Drive Motor
+ *  [motor0] Arm Motor
  *  [motor1] Unused
  *  [motor2] Unused
  *  [motor3] Unused
@@ -31,9 +28,9 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeB
  */
 
 @TeleOp(name="Arm Controller [12sliu]", group="test")
-@Disabled
+//@Disabled
 public class TWELVESLIUArmController extends TeleOpModeBase {
-    CRServo crservo;
+    Motor motor;
     GamepadButton grabButton;
     boolean buttonPressed;
     boolean servoPosition;
@@ -41,17 +38,17 @@ public class TWELVESLIUArmController extends TeleOpModeBase {
     // true if going to 120 or on 120, false if going to 240 or in 240
     @Override
     public void setup() {
-        this.crservo = new CRServo(hardwareMap, "motor0");
-        crservo.setRunMode(Motor.RunMode.PositionControl);
-        crservo.setDistancePerPulse(0.015);
+        this.motor = HardwareMapContainer.motor0;
+        motor.setRunMode(Motor.RunMode.PositionControl);
+        motor.setDistancePerPulse(0.015);
         new GamepadButton(
                 Inputs.gamepad1, PSButtons.CIRCLE
         ).whenPressed(new InstantCommand(() -> {
             if (servoPosition){
-                crservo.setTargetDistance(18.0);
+                motor.setTargetDistance(18.0);
             }
             else{
-                crservo.setTargetDistance(0);
+                motor.setTargetDistance(0);
             }
             servoPosition = !servoPosition;
         }));
@@ -66,10 +63,10 @@ public class TWELVESLIUArmController extends TeleOpModeBase {
         /**if (buttonPressed){
             if (!lastPressed){
                 if (servoPosition){
-                    crservo.setTargetDistance(18.0);
+                    motor.setTargetDistance(18.0);
                 }
                 else{
-                    crservo.setTargetDistance(0);
+                    motor.setTargetDistance(0);
                 }
                 servoPosition = !servoPosition;
             }
@@ -80,7 +77,7 @@ public class TWELVESLIUArmController extends TeleOpModeBase {
         }
          */
 
-        crservo.set(0.5);
+        motor.set(0.5);
 
         telemetry.addData("Status", "Running");
         telemetry.addData("Pressed", Inputs.gamepad1.getButton(GamepadKeys.Button.A));
