d2056ecc0c69696bc7e78f0eabb76c5baa0be19f
BlockBeasts <kjosien1@gmail.com>
2023-03-03 07:47:28 -0700

    fix teleop

--- Masters/src/main/java/org/firstinspires/ftc/masters/BadgerConstants.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/BadgerConstants.java
@@ -10,7 +10,7 @@ public class BadgerConstants {
     public  static int SLIDE_HIGH_AUTO= 970 *537/ 384;
     public static int SLIDE_MIDDLE = 480 *537/ 384;
     public static int SLIDE_BOTTOM = 0;
-    public static  int SLIDE_HIGH_BACK = 1250;
+    public static  int SLIDE_HIGH_BACK = 1320;
     public static int SLIDE_MIDDLE_BACK= 500;
 
     public static  int ARM_BOTTOM = 0;
@@ -25,13 +25,11 @@ public class BadgerConstants {
     public static double CLAW_OPEN = 0.35;
     public static double CLAW_CLOSED = 0.8;
 
-    public static final double TIP_CENTER = 0.77;
-    public static final double TIP_BACK = 0.6;
-    public static final double TIP_FRONT =0.99;
+    public static double TIP_CENTER = 0.77;
+    public static double TIP_BACK = 0.58;
+    public static double TIP_FRONT =0.99;
 
     public static int SLIDE_CONE_INCREMENT = 14;
-
-
 
 }
 
