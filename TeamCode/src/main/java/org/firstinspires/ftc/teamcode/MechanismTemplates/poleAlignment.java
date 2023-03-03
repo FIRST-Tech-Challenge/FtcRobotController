8a8c6a6f2b06a4f42d0e4a3c127e4d716bc95589
sam_h <samjharsch@gmail.com>
2023-03-03 17:05:04 -0600

    Pole alignment code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/poleAlignment.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.MechanismTemplates;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class poleAlignment
+{
+    public static Servo retractServo;
+    public static double raise=.55;
+    public static double lower=.13;
+    public poleAlignment(HardwareMap hardwareMap) {
+        retractServo=hardwareMap.get(Servo.class, "poleAlignment");//port 5
+    }
+    public static void lower()
+    {
+        retractServo.setPosition(lower);
+    }
+
+    public static void raiseServo()
+    {
+        retractServo.setPosition(raise);
+    }
+    public static void toggleAlignmentDevice() {
+        if (retractServo.getPosition() == raise) {
+            retractServo.setPosition(lower);
+        }
+        else {
+            retractServo.setPosition(raise);
+        }
+
+    }
+}
+
