3afe6dfff1e11f74ec1c37fdf0894af9f0804055
Digicat509 <106493589+Digicat509@users.noreply.github.com>
2023-03-03 22:38:52 -0600

    84 of 29411
    Added amperage testing for motor speed test class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/MotorSpeedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/samples/MotorSpeedTest.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "MotorTesting", group = "Challenge")
 public class MotorSpeedTest extends OpMode {
     private DcMotorEx motor1, motor2;
@@ -18,21 +20,26 @@ public class MotorSpeedTest extends OpMode {
         motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor1.setPower(1);
         motor2.setPower(1);
+        motor1InitTicks = motor1.getCurrentPosition();
+        motor2InitTicks = motor2.getCurrentPosition();
         currTime = System.nanoTime();
         telemetry.update();
     }
 
     @Override
     public void loop() {
-        if(currTime % 1e9 == 0)
+        if(currTime-oldTime >= 1e9)
         {
             motor1TicksPerSecond = motor1.getCurrentPosition()-motor1InitTicks;
             motor2TicksPerSecond = motor2.getCurrentPosition()-motor2InitTicks;
             motor1InitTicks = motor1.getCurrentPosition();
             motor2InitTicks = motor2.getCurrentPosition();
+            oldTime = currTime;
         }
-        currTime = System.nanoTime();
         telemetry.addData("motor1 ticks per second:\t", motor1TicksPerSecond);
         telemetry.addData("motor1 ticks per second:\t", motor2TicksPerSecond);
+        telemetry.addData("motor1 amps:\t", motor1.getCurrent(CurrentUnit.AMPS));
+        telemetry.addData("motor2 amps:\t", motor2.getCurrent(CurrentUnit.AMPS));
+        currTime = System.nanoTime();
     }
 }
