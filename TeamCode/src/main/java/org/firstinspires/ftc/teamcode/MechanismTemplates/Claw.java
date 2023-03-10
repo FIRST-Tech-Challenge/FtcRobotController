63f521bd5155c32a625da23ec6a42805524a4418
MushiTea <94145946+MushiTea@users.noreply.github.com>
2023-03-09 22:32:45 -0600

    Test branch (#62)
    * please work for the love of god
    
    * please work for the love of god.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanismTemplates/Claw.java
@@ -12,16 +12,22 @@ import java.util.function.BooleanSupplier;
 public class Claw {
     public Servo wristJoint;
     public Servo clawJoint; // public because a button must set the wrist to the intake position regardless of its status
+    private boolean isAuto;
 
     // claw positions
     public static double OPEN = 0.7;
     public static double CLOSE = 0.5;
+    public static double AUTOCLOSE = 0.5;
 
     // wrist positions
     public static double WRIST_INTAKE_POSITION = 0.255; // wrist rotates to intake cone, greater values move clockwise, less move counterclockwise
     public static double WRIST_EXTAKE_POSITION = 0.915; // wrist rotates to extake on junction
 
-    public Claw(HardwareMap hardwareMap) {
+    SignalEdgeDetector isOpen;
+    SignalEdgeDetector isIntakePosition;
+
+    public Claw(HardwareMap hardwareMap, BooleanSupplier rightBumper, BooleanSupplier aButton) {
+
         // Control Hub Pins
         wristJoint = hardwareMap.get(Servo.class, "WRIST"); // Pin 0
         clawJoint = hardwareMap.get(Servo.class, "CLAW"); // Pin 1
@@ -29,28 +35,47 @@ public class Claw {
         // Presets for teleOp
         clawJoint.setPosition(CLOSE);
         wristJoint.setPosition(WRIST_INTAKE_POSITION);
+
+        isOpen = new SignalEdgeDetector(rightBumper);
+        isIntakePosition = new SignalEdgeDetector(aButton);
     }
 
-    public void toggleOpenClose(){
-        if(clawJoint.getPosition() == OPEN){
-            clawJoint.setPosition(CLOSE);
-        }else {
-            clawJoint.setPosition((OPEN));
+    // Overloaded method for autonomous
+    public Claw(HardwareMap hardwareMap){
+        // Control Hub Pins
+        wristJoint = hardwareMap.get(Servo.class, "WRIST"); // Pin 0
+        clawJoint = hardwareMap.get(Servo.class, "CLAW"); // Pin 1
+
+        clawJoint.setPosition(AUTOCLOSE);
+        wristJoint.setPosition(WRIST_INTAKE_POSITION);
+    }
+
+    private boolean clawToggled;
+
+    public void toggleOpenClose() {
+        if (isOpen.isRisingEdge()) {
+            if (clawToggled) {
+                clawJoint.setPosition(AUTOCLOSE);
+            } else {
+                clawJoint.setPosition(OPEN);
+            }
+            clawToggled = !clawToggled;
         }
     }
 
-    public void toggleWristRotate(){
-        if (wristJoint.getPosition() == WRIST_EXTAKE_POSITION) {
+    public boolean wristInExtakePosition;
+
+    public void toggleWristRotate() { // would be used at the beginning of the goToJunction method in the arm class, for example
+        if (wristInExtakePosition) {
             wristJoint.setPosition(WRIST_INTAKE_POSITION);
-        } else {
+        } else { // if the wrist is in the extake position, switch it back to the intake position so it can pick up cones
             wristJoint.setPosition(WRIST_EXTAKE_POSITION);
         }
-
         wristInExtakePosition = !wristInExtakePosition;
     }
 
     public void toggleAutoOpenClose(){
-        if(clawToggled){ // hello
+        if(clawToggled){
             clawJoint.setPosition(AUTOCLOSE);
         }else {
             clawJoint.setPosition((OPEN));
