81b24385b75d0bb90b01392956cab4c4a550a6b2
spamtactics <108055980+spamtactics@users.noreply.github.com>
2023-03-15 17:21:57 +0000

    Update TestAutonomousLinearSearch.java
    Fixed a logic error where I would use absolute heading rather than heading relative to start point. Do ignore my comments regarding the robot turning as the Team 2 robot does no need to turn

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TestAutonomousLinearSearch.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/test/TestAutonomousLinearSearch.java
@@ -9,7 +9,10 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.AutonomousL
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.AutonomousModeBase;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
 
-
+// Test log 15/3/23:
+//Figure out why robot can only drive straight when it the velocity reads that both motors are going
+//in the opposite direction based on their velocities, implying that the robot cannot turn.
+//Perhaphs shift to a turning mechanism where only 1 of the robot's wheels are turning
 @Autonomous(name="<Autonomous>", group="<OpMode group name>")
 public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
 
@@ -30,8 +33,10 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
         //remember to account for size of robot when inputting coordinates.
         //Can we standardise placing these coordinates on the bottom left corner of the station
         //test data:
-        // {{1,0},{1,1},{1,2}}
-        double[][] junction_Coordinates = {{1,0},{1,1},{1,2}};
+        // {{0.5,0},{0.5,0.5}}
+
+
+        double[][] junction_Coordinates = {{0.5,0},{0.5,0.5}};
         int num_junctions = 3;
 
         //Is it possible to use the terminals as a way to check coordinates?
@@ -39,13 +44,13 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
         //remember to account for size of robot when inputting coordinates.
         //Can we standardise placing these coordinates at the center of the terminal
         //test data:
-        // {{0,0},{3,3}}
-        double[][] terminal = {{0,0},{3,3}};
+        // {{0,0},{2,2}}
+        double[][] terminal = {{0,0},{1,1}};
         //remember to account for size of robot when inputting coordinates.
         //Can we standardise placing these coordinates at the center of the terminal
         //test data:
         //{0,2}
-        double[][] cone_coordinates={{0,2}};
+        double[][] cone_coordinates={{0,1}};
 
         //used later for finding closest cone stack
         double[] closest_coordinate= {cone_coordinates[0][0], cone_coordinates[0][1]};
@@ -76,10 +81,9 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
         while (opModeIsActive()) {
             // Code to run in a loop
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.update();
-            //getting time the previous tick started
             //Telling the robot to automatically break from driving loop when 10s left to return to base
             while (time < 20) {
+                telemetry.addData("entered first loop", runtime.toString());
                 speed1= motor1.getCorrectedVelocity();
                 speed2= motor2.getCorrectedVelocity();
                 if (speed1-speed2>-0.2 && speed1-speed2<-0.2){
@@ -94,16 +98,16 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                 elapsed = time - start_elapsed;
                 //resetting the new tick time from this moment
                 start_elapsed=time;
-                telemetry.addData("time", elapsed);
+                telemetry.addData("time per tick", elapsed);
                 //finding distance travelled in last tick
                 travelled = speed * elapsed;
                 //updating current x coordinate
                 current[0] = current[0] + travelled * Math.cos(idealheading);
                 //updating current y coordinate
                 current[1] = current[1] + travelled * Math.sin(idealheading);
-                telemetry.addData("position", current);
                 //Distance from target in terms of x distance and y distance
                 if (has_cone == false) {
+                    telemetry.addData("Entered cone pick up",current);
                     //Run code to return to nearest loading station
                     //finding distance from each robot to cones using pythagoras
                     //assuming that the first coordinate is the closest
@@ -142,14 +146,15 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                         current[1] = current[1] + travelled * Math.sin(idealheading);
                         //finding heading required to drive towards coordinate
                         idealheading = Math.atan2(current[1] - closest_coordinate[1], current[0] - closest_coordinate[0]);
+                        startheading = internal_measurement_unit.getHeading();
                         //allowing an error of 1 degree as long as it is constantly updated.
-                        startheading = internal_measurement_unit.getAbsoluteHeading();
                         if (startheading < idealheading + 1 && startheading > idealheading - 1) {
                             //100 is just a stopgap but need to find ticks per tick of the code so that
                             //the motors stops running the moment another cycle starts
                             motor1.setTargetPosition(100);
                             motor2.setTargetPosition(100);
                         } else if ((startheading - idealheading > 0) || (startheading - idealheading < -180)) {
+
                             //Cause the motor to turn left
                             //10 degree error allowance for reducing speed is stopgap, need to input degree
                             //change generated by 1 tick of maximum turning
@@ -185,6 +190,7 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                     //should we enter code for the robot to drive to the nearest picture in order
                     //to reconfirm coordinates
                 } else {
+                    telemetry.addData("Entered cone deliver", current);
                     //allowance of 5cm from pole, though subject to change as necessary
                     if (Math.abs(current[0]-next[0])>0.05 && Math.abs(current[1]-next[1])>0.05){
                         //Calculating heading necessary to go in correct direction
@@ -194,14 +200,23 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
 
                         // Insert code for proximity sensor
 
-                        startheading = internal_measurement_unit.getAbsoluteHeading();
+                        startheading = internal_measurement_unit.getHeading();
                         //allowing an error of 1 degree as long as it is constantly updated.
                         if (startheading < idealheading + 1 && startheading > idealheading - 1) {
+                            telemetry.addData("driving left wheel", motor1.getCorrectedVelocity());
+                            telemetry.addData("driving right wheel",motor2.getCorrectedVelocity());
                             //100 is just a stopgap but need to find ticks per tick of the code so that
                             //the code stops running the moment another cycle starts
                             motor1.setTargetPosition(100);
                             motor2.setTargetPosition(100);
+
+                            //remove this when done
+                            //motor1.set(1.0);
+                            //motor2.set(-1.0);
+                            //apparently the robot does not know how to turn
+
                         } else if ((startheading - idealheading > 10) || (startheading - idealheading < -180)) {
+                            telemetry.addData("turning left", motor1.getCorrectedVelocity());
                             //Cause the motor to turn left
                             //10 degree error allowance for reducing speed is stopgap, need to input degree
                             //change generated by 1 tick of maximum turning
@@ -215,6 +230,8 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                             }
 
                         } else {
+                            telemetry.addData("turning right, right motor", motor2.getCorrectedVelocity());
+                            telemetry.addData("turning right, left motor", motor1.getCorrectedVelocity());
                             //Cause the motor to turn right
                             //10 degree error allowance for reducing speed is stopgap, need to input degree
                             //change generated by 1 tick of maximum turning
@@ -237,11 +254,13 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                             next[0] = junction_Coordinates[junction_number][0];
                             next[1] = junction_Coordinates[junction_number][1];
                         }
+                        telemetry.addData("next junction", next);
                     }
                 }
                 telemetry.update();
             }
             while (time < 30) {
+                telemetry.addData("returning to terminal", current);
                 //write code for returning to terminals
                 speed1= motor1.getCorrectedVelocity();
                 speed2= motor2.getCorrectedVelocity();
@@ -256,6 +275,7 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                 time = runtime.seconds();
                 //finding time elapsed for previous tick
                 elapsed = time - start_elapsed;
+                telemetry.addData("time taken", elapsed);
                 //resetting the new tick time from this moment
                 start_elapsed=time;
                 travelled = speed * elapsed;
@@ -286,7 +306,7 @@ public class TestAutonomousLinearSearch extends AutonomousLinearModeBase {
                     //insert code for proximity sensor
 
                     idealheading = Math.atan2(closest_coordinate[1], closest_coordinate[0]);
-                    startheading = internal_measurement_unit.getAbsoluteHeading();
+                    startheading = internal_measurement_unit.getHeading();
                     //allowing an error of 1 degree as long as it is constantly updated.
                     if (startheading < idealheading + 1 && startheading > idealheading - 1) {
                         //100 is just a stopgap but need to find ticks per tick of the code so that
