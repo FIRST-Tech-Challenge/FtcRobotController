f9200a2f24c93762da9a58386f022af98e19480f
Rafa-Lei <114037765+Rafa-Lei@users.noreply.github.com>
2023-03-18 17:24:40 -0400

    test

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/newcode/TestClass2.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.robotcontroller.newcode;
+
+public class TestClass2 {
+    public static void main(String args[]){
+        System.out.println("hello Rapha");
+    }
+}
