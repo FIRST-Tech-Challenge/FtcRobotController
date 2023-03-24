fd7fa8222b78ff714e9a443c8abc268eb531e476
GongInvaders <gonginvaders@gmail.com>
2023-03-24 14:57:33 +1100

    Attempted to enable per subsystem initialisation messages

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamUtil/CommandScheduler/ConfiguredOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamUtil/CommandScheduler/ConfiguredOpMode.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode.teamUtil.CommandScheduler;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.teamUtil.CommandScheduler.Scheduler;
 import org.firstinspires.ftc.teamcode.teamUtil.CommandScheduler.Subsystem;
 import org.firstinspires.ftc.teamcode.teamUtil.CommandScheduler.gamepadEX.GamepadEX;
 import org.firstinspires.ftc.teamcode.teamUtil.RobotConfig;
+
+import java.util.ArrayList;
 
 /**
  * Assists with update loops and the like for the integration of robotConfig.
@@ -24,12 +27,28 @@ public abstract class ConfiguredOpMode extends OpMode {
     public abstract void registerTriggers();
     @Override
     public final void init() {
+        Telemetry.Item initialising = telemetry.addData("", "");
+        initialising.setCaption("Initialising");
+        initialising.setValue("Robot");
+        
         r = RobotConfig.freshInstance(this);
         gamepadEX1 = new GamepadEX(gamepad1);
         gamepadEX2 = new GamepadEX(gamepad2);
         superInit();
-        for (Subsystem subsystem : r.subsystems) {
+        
+        Telemetry.Item initialisedSubsystems = telemetry.addData("","");
+        StringBuilder initialisationSequencer = new StringBuilder("Robot");
+    
+        initialisedSubsystems.setCaption("Initialised");
+        initialisedSubsystems.setValue(initialisationSequencer);
+        
+        for (int i = 0; i < r.subsystems.size(); i++) {
+            Subsystem subsystem = r.subsystems.get(i);
+            initialising.setValue(subsystem.toString());
             subsystem.init();
+            initialisationSequencer.append("\n");
+            initialisationSequencer.append(subsystem.toString());
+            initialisedSubsystems.setValue(initialisationSequencer);
         }
         registerTriggers();
     }
