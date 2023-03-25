41a94839a5ae9adab90857d72bf157a01673d74f
sbottingota <saku.bottingota@gmail.com>
2023-03-25 08:41:48 +0000

    Added Grabber code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team2/Team2TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team2/Team2TeleOp.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.opModes.team2;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
 import com.arcrobotics.ftclib.drivebase.DifferentialDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.ServoEx;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -21,59 +23,90 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.modeBases.TeleOpModeB
 
 
 /**
- * A program that controls a drive train has special wheels to go sideways.
+ * Team 2 TeleOp
+ *
  * Controls:
+ *  Drive Train:
+ *      Left Joystick: Control the drive train in a normal way.
+ *      Square Toggle between normal speed and slow speed.
+ *      Circle: Toggle between normal drive and forward only (no turning).
  *
- *  Left Joystick: Control the drive train in a normal way.
- *  Right Joystick X: Make drive train go sideways.
+ *  Lift:
+ *      Right Joystick Y: Move the Lift.
  *
- *  Square Toggle between normal speed and slow speed.
- *  Circle: Toggle between both directions and forward only.
+ * Hardware:
+ *  Drive Train:
+ *      Motor 0 and 1 for driving (left and right respectively)
+ *
+ *  Lift:
+ *      Motor 3 for moving lift
  */
 
 
-@TeleOp(name="Team2TeleOp", group="Team 2")
+@TeleOp(name="Team 2 TeleOp", group="Team 2")
 public class Team2TeleOp extends TeleOpModeBase {
 
     private Telemetry telemetry;
 
-    Team2LiftComponent lift;
-
-    double targetPosition;
-
+    // DRIVE TRAIN
     private Motor leftMotor;
     private Motor rightMotor;
-    private Motor sidewaysMotor;
 
     private boolean isSlowMode = false;
     private boolean isForwardOnlyMode = false;
 
+    // LIFT
+    private Team2LiftComponent lift;
+    private double targetPosition;
+
+    // GRABBER
+    public final double SERVO_OPEN_ANGLE = 0D;
+    public final double SERVO_CLOSE_ANGLE = 90D;
+
+    private ServoEx grabberServo;
+
     @Override
     public void setup() {
+        // DRIVE TRAIN
         telemetry = TelemetryContainer.getTelemetry();
 
-        // Motors used to travel
         leftMotor = HardwareMapContainer.motor0;
         rightMotor = HardwareMapContainer.motor1;
-        sidewaysMotor = HardwareMapContainer.motor2;
 
-        // Lift Motor
+
+        // LIFT
         Motor lift_motor = HardwareMapContainer.motor3;
         // Core Hex Motor has 288 counts/revolution; counts/radian = counts/revn / (radians/revn); 3:1 gear
         lift = new Team2LiftComponent(lift_motor, 0.42, (int)((288 / 3) / (Math.PI*2)), 0);
-    }
 
+        // GRABBER
+        telemetry = TelemetryContainer.getTelemetry();
 
-    private void arcadeDrive(Motor leftMotor, Motor rightMotor, double forwardSpeed, double turnSpeed) {
-        leftMotor.set((forwardSpeed - turnSpeed) / 2D);
-        rightMotor.set((forwardSpeed + turnSpeed) / 2D);
+        telemetry.addData("[GRABBER] Open Angle: ", SERVO_OPEN_ANGLE);
+        telemetry.addData("[GRABBER] Close Angle: ", SERVO_CLOSE_ANGLE);
+
+        grabberServo = new SimpleServo(HardwareMapContainer.getMap(), "grabberServo",
+                SERVO_OPEN_ANGLE, SERVO_CLOSE_ANGLE);
+
+        new GamepadButton(Inputs.gamepad1, PSButtons.CIRCLE).whenActive(() ->
+                grabberServo.setPosition(SERVO_CLOSE_ANGLE)
+        );
+
+        new GamepadButton(Inputs.gamepad1, PSButtons.SQUARE).whenActive(() ->
+                grabberServo.setPosition(SERVO_OPEN_ANGLE)
+        );
     }
 
     @Override
     public void every_tick() {
+        runDriveTrain(Inputs.gamepad1.getLeftY(), Inputs.gamepad1.getRightY());
+        runLift(Inputs.gamepad1.getRightY());
+    }
+
+    private void runDriveTrain(double yInput, double xInput) {
         final GamepadEx gamepad = Inputs.gamepad1;
 
-        final double[] inputs = {gamepad.getLeftY(), gamepad.getLeftX(), gamepad.getRightX()}; //forward speed, turn speed, sideways speed
+        final double[] inputs = {yInput, xInput}; //forward speed, turn speed, sideways speed
 
         //if joystick pos is less than this amount from in the middle, the robot doesn't move.
         final double DEAD_ZONE_SIZE = 0.2D;
@@ -100,14 +133,37 @@ public class Team2TeleOp extends TeleOpModeBase {
 
         arcadeDrive(leftMotor, rightMotor, inputs[0], inputs[1]);
 
-        sidewaysMotor.set(inputs[2]);
+        telemetry.addData("[Drive Train] Y", inputs[0]);
+        telemetry.addData("[Drive Train] X", inputs[1]);
 
-        telemetry.addData("Normal Drive Train Y", inputs[0]);
-        telemetry.addData("Normal Drive Train X", inputs[1]);
-        telemetry.addData("Sideways Motor Value", inputs[2]);
-        telemetry.addData("Arm motor value", inputs[3]);
-        telemetry.addData("Slow Mode", isSlowMode);
-        telemetry.addData("Forward Only Mode", isForwardOnlyMode);
-        telemetry.addData("Dead Zone Size", DEAD_ZONE_SIZE);
+        telemetry.addData("[Drive Train] Dead zone size", DEAD_ZONE_SIZE);
+
+        if (isSlowMode) {
+            telemetry.addLine("[Drive Train] Slow mode active");
+        }
+
+        if (isForwardOnlyMode) {
+            telemetry.addLine("[Drive Train] Forward only mode active");
+        }
+    }
+
+    private void runLift(double inputPos) {
+        if(Math.abs(inputPos) != this.targetPosition) {
+            this.targetPosition = Math.abs(inputPos);
+            lift.setHeight(this.targetPosition);
+        } else {
+            telemetry.addLine("[Lift] BUSY");
+        }
+        telemetry.addData("[Lift] Last set position to", this.targetPosition);
+        telemetry.addData("[Lift] Position of gamepad", Math.abs(inputPos));
+    }
+
+    private void runGrabber() {
+        telemetry.addData("[GRABBER] Current Servo Angle: ", grabberServo.getAngle());
+    }
+
+    private void arcadeDrive(Motor leftMotor, Motor rightMotor, double forwardSpeed, double turnSpeed) {
+        leftMotor.set((forwardSpeed - turnSpeed) / 2D);
+        rightMotor.set((forwardSpeed + turnSpeed) / -2D); // TODO: not sure if negative needed here or not, please test
     }
 }
