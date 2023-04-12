3e2cded3855d12acf8c62bf9a1f08333cb63e5b0
karine josien <karine.josien@domo.com>
2023-04-12 10:04:32 -0600

    aligner test

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/AlignerTest.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.masters;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class AlignerTest extends LinearOpMode {
+
+
+    CRServo alignerServo;
+    Servo claw;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        alignerServo = hardwareMap.get(CRServo.class, "alignerServo");
+        claw = hardwareMap.servo.get("clawServo");
+
+        claw.setPosition(BadgerConstants.CLAW_CLOSED);
+        waitForStart();
+        while(opModeIsActive() && !isStopRequested()){
+            if (gamepad1.a){
+                alignerServo.setPower(1);
+            } else if (gamepad1.b){
+                alignerServo.setPower(-1);
+            } else {
+                alignerServo.setPower(0);
+            }
+        }
+    }
+}
