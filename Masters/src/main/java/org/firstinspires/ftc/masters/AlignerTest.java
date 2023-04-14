ca0c62fa9ea91581140d0808dfef13f5e3d9b19e
BlockBeasts <kjosien1@gmail.com>
2023-04-14 09:16:30 -0600

    world auto

--- Masters/src/main/java/org/firstinspires/ftc/masters/AlignerTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/AlignerTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.masters;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp(name="aligner test")
 public class AlignerTest extends LinearOpMode {
 
 
