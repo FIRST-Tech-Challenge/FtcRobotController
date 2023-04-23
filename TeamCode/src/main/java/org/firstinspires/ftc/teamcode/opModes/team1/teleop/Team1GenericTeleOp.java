5649e807b94e5954555216be13bc6daf47e799dc
micnekr <misha.nekrasov@gmail.com>
2023-04-23 17:51:33 +0100

    Added snapping to relative turn angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1GenericTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/team1/teleop/Team1GenericTeleOp.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.opModes.team1.teleop;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.button.GamepadButton;
@@ -7,6 +8,7 @@ import com.arcrobotics.ftclib.drivebase.HDrive;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.arcrobotics.ftclib.hardware.RevIMU;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.components.HDriveWrapper;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.inputs.Inputs;
 import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.HardwareMapContainer;
@@ -16,14 +18,16 @@ import org.firstinspires.ftc.teamcode.libs.brightonCollege.util.TelemetryContain
 /**
  * This isn't an OpMode - create an instance of this in each team-specific OpMode
  */
+@Config
 public class Team1GenericTeleOp {
     TeamColour teamColour;
     RevIMU imu;
     HDriveWrapper drive;
 
-    // If pushed down, the robot can freely turn
-    // Otherwise, snap to the nearest cardinal direction
+    // If pushed down, the robot can freely turn; the turn is relative to the current direction
+    // Otherwise, snap to the nearest cardinal direction; the turn is absolute
     GamepadButton turnModeSwitch;
+    public static double RELATIVE_TURN_SPEED_MULTIPLIER = 0.3;
 
     public void setup(TeamColour teamColor){
         this.teamColour = teamColor;
@@ -47,26 +51,33 @@ public class Team1GenericTeleOp {
         }));
 
         turnModeSwitch = Inputs.gamepad1.getGamepadButton(GamepadKeys.Button.LEFT_STICK_BUTTON);
+        turnModeSwitch.whenReleased(new InstantCommand(() -> {
+            // Snap the desired angle to nearest cardinal direction and rotate 180 deg
+            // this is because we usually use relative turning to aim at a pole, and will need to turn around to
+            // get another cone
+            drive.desiredDirection = HDriveWrapper.snapAngle(imu.getHeading());
+        }));
     }
 
     public void every_tick(){
         // Drivetrain
         // Note: directions of x and y on joystick different to directions on the field
-        double strafe =  Inputs.gamepad1.getRightX();
-        double forward =  -Inputs.gamepad1.getRightY();
+        double strafe =  -Inputs.gamepad1.getRightY();
+        double forward =  -Inputs.gamepad1.getRightX();
         // The direction of the left joystick is the desired direction of heading
         double x_joystick_turn = Inputs.gamepad1.getLeftX();
         double y_joystick_turn = Inputs.gamepad1.getLeftY();
 
         // If not pressed, snap to the nearest cardinal direction
-        drive.setTurnSnap(!turnModeSwitch.get());
+        if (turnModeSwitch.get()) {
+            drive.fieldOrientedDriveRelativeRotation(strafe, forward, -RELATIVE_TURN_SPEED_MULTIPLIER * x_joystick_turn);
+        } else {
+            drive.setTurnDirectionSnap(x_joystick_turn, y_joystick_turn);
+            drive.fieldOrientedDriveAbsoluteRotation(strafe, forward);
+        }
 
         MultipleTelemetry t = TelemetryContainer.getTelemetry();
         t.addData("x", strafe);
         t.addData("y", forward);
-        t.addData("Version", 1);
-
-        drive.updateTurn(x_joystick_turn, y_joystick_turn);
-        drive.fieldOrientedDrive(strafe, forward);
     }
 }
