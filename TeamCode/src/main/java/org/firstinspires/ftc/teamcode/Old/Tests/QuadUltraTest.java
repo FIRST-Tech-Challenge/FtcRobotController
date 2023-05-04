2473388858359d5c63a6745e6a9696e5bd497c76
WarZ69 <minecraftplayer6542@gmail.com>
2023-05-03 19:43:07 -0700

    linear Regression !!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Old/Tests/QuadUltraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Old/Tests/QuadUltraTest.java
@@ -5,6 +5,7 @@ import static org.firstinspires.ftc.teamcode.Robots.BasicRobot.op;
 import static java.lang.Math.abs;
 import static java.lang.Math.floor;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -14,17 +15,22 @@ import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Robots.BasicRobot;
+import org.firstinspires.ftc.teamcode.roadrunner.util.LineRegressionFilter;
 
-
+@Config
 @Autonomous(name= "RAAAAAAAAAAAAAAAAAAAAAAAAHHH")
 public class QuadUltraTest extends LinearOpMode {
     private AnalogInput ultrasonicFront, ultrasonicBack, ultrasonicRight, ultrasonicLeft;
     private LED ultraFront, ultraBack, ultraRight, ultraLeft;
+    public static int length = 5;
+    public static double trust =0;
     @Override
     public void runOpMode(){
 //        logger.createFile("/RobotLogs/GeneralRobot", "Runtime    Component               " +
 //                "Function                        Action");
         BasicRobot robot = new BasicRobot(this,false);
+        LineRegressionFilter rfilter = new LineRegressionFilter(trust,length);
+        LineRegressionFilter lfilter = new LineRegressionFilter(trust,length);
         ultrasonicLeft = hardwareMap.get(AnalogInput.class, "ultrasonicLeft");
         ultrasonicRight = hardwareMap.get(AnalogInput.class, "ultrasonicRight");
 
@@ -44,9 +50,15 @@ public class QuadUltraTest extends LinearOpMode {
 
                 ultraLeft.enable(true);
             }
-
-            op.telemetry.addData("rightS", 90.48337 * ultrasonicRight.getVoltage() - 12.62465);
-            op.telemetry.addData("leftS", 90.48337 * ultrasonicLeft.getVoltage() - 12.62465);
+            double rightRaw = 90.48337 * ultrasonicRight.getVoltage() - 12.62465;
+            double leftRaw = 90.48337 * ultrasonicLeft.getVoltage() - 12.62465;
+            double rightFilter = rfilter.regressedDist(rightRaw);
+            double leftFilter = lfilter.regressedDist(leftRaw);
+            op.telemetry.addData("rightRaw", rightRaw);
+            op.telemetry.addData("leftraw","%.2f", leftRaw);
+            logger.logNoTime("/RobotLogs/GeneralRobot", ""+leftRaw+","+leftFilter);
+            op.telemetry.addData("rightFil", rightFilter);
+            op.telemetry.addData("leftFil","%.2f", leftFilter);
 
 //            op.telemetry.addData("right", ultraRight.isLightOn());
             op.telemetry.addData("left", ultraLeft.isLightOn());
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/util/LineRegressionFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/util/LineRegressionFilter.java
@@ -12,8 +12,18 @@ public class LineRegressionFilter {
         listSize = listLength;
     }
     public double predictedValue(){
-        double regVal = 0;
-        return regVal;
+        double avg =0, avgI = (dataHist.size()+1)/2.0, num=0,denom=0;
+        for(int i=0;i<dataHist.size();i++){
+            avg += dataHist.get(i);
+        }
+        avg/= dataHist.size();
+        for(int i=1;i<dataHist.size()+1;i++){
+            num += (i-avgI)*(dataHist.get(i-1)-avg);
+            denom += (i-avgI)*(i-avgI);
+        }
+        double coeff1 = num/denom, coeff0 = avg - coeff1*avgI;
+
+        return coeff0+(dataHist.size())*coeff1;
     }
     public double regressedDist(double raw){
         if(dataHist.size()>=listSize){
