3debe2eea9273cdce9a108a4e64c3a531befefc2
HelbetDavid <helbetdavid@gmail.com>
2023-05-21 13:30:49 +0300

    better tunning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -55,7 +55,7 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(6, 0, 0.5);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(7, 0, 0.6);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(7, 0, 0.5);
 
     public static double LATERAL_MULTIPLIER = 1.22;
 
