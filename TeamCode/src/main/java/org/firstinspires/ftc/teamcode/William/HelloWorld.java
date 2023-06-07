cb7ed6ef65944b0f341057dc8b5da4bf194d04cf
Weilin Luo <weilin.william.luo@gmail.com>
2023-06-07 16:05:15 -0700

    Add HelloWorld.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/William/HelloWorld.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.William;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class HelloWorld extends OpMode
+{
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
