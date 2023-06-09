ac0554df7ae88108fc45f61473c08ae32895b78e
William <william_luo2004@yahoo.com>
2023-06-08 22:46:02 -0700

    Change hello string.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/William/HelloWorld.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/William/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld extends OpMode
 {
     @Override
     public void init() {
-        telemetry.addData("Hello", "World");
+        telemetry.addData("Hello", "William");
     }
 
     @Override
