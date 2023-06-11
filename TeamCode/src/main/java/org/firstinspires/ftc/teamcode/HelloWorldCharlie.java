e6bb4c94b81edda9534afad8d905dd63de781c92
cfredberg <charliefredberg@outlook.com>
2023-06-11 14:46:42 -0700

    Add HelloWorldCharlie

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldCharlie.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class HelloWorldCharlie extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData(">", "Hello World Charlie");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
