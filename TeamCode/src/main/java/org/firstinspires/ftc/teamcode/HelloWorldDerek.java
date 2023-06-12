88c3d11856da9659ff44da57c555ad90bfab279d
derek <derek.j.kang@gmail.com>
2023-06-11 20:19:35 -0700

    Added HelloWorldDerek.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorldDerek.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp
+public class HelloWorldDerek extends OpMode{
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "World");
+    }
+
+    @Override
+    public void loop() {
+    }
+}
