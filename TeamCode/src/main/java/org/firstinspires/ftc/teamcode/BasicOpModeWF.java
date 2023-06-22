9fb06f4ef4c369ee7629ba9137a98316bbeb89bd
PrestonHegouaburu <hegou10803@loswego.k12.or.us>
2023-06-21 21:12:28 -0700

    This is our Op mode code in java
    This is our movement code for the robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeWF.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpModeWF.java
@@ -109,8 +109,8 @@ public class BasicOpModeWF extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double max;
 
+            double max;
             // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
             double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
             double lateral =  gamepad1.left_stick_x;
