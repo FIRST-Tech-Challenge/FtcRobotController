2f0e463be30e46a44d16ac2d0948b6f3f691b75f
yarilightcav <71334766+badgamer2007@users.noreply.github.com>
2023-07-29 17:17:38 -0400

    the new constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -33,7 +33,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0.1, 0, 0.05,
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(20, 0, 8.5,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
     /*
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.47638; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 15.75; // in
+    public static double TRACK_WIDTH = 13.77; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -65,10 +65,10 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 50.531662145523356;
+    public static double MAX_VEL = 50.35867232024176;
     public static double MAX_ACCEL = 50.531662145523356;
-    public static double MAX_ANG_VEL = Math.toRadians(124.90000837062806);
-    public static double MAX_ANG_ACCEL = Math.toRadians(124.90000837062806);
+    public static double MAX_ANG_VEL = Math.toRadians(169.624988640842);
+    public static double MAX_ANG_ACCEL = Math.toRadians(169.624988640842);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -56,9 +56,9 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 1;
+    public static double LATERAL_MULTIPLIER = 55.66178346208703/45;
 
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
