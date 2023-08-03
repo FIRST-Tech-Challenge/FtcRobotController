0c3ae4d826e0573cec56ba7674a9447183c19fbe
Dolphindasher324 <dolphindasher324@gmail.com>
2023-08-03 10:34:10 -0600

    Actual programming
    We made the motor move!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpModee.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpModee.java
@@ -13,17 +13,17 @@ public class testOpModee extends LinearOpMode {
 
     private Gyroscope imu;
     private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
+    //private DigitalChannel digitalTouch;
+    //private DistanceSensor sensorColorRange;
     private Servo servoTest;
 
 
     @Override
     public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
+        imu = hardwareMap.get(Gyroscope.class,"imu");
         motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        //digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        //sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
         servoTest = hardwareMap.get(Servo.class, "servoTest");
 
         telemetry.addData("Status", "Initialized");
@@ -31,11 +31,16 @@ public class testOpModee extends LinearOpMode {
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
+        double tgtPower=0;
+
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            tgtPower=-this.gamepad1.left_stick_y;
+            motorTest.setPower(tgtPower);
+            telemetry.addData("target Power", tgtPower);
+            telemetry.addData("Gamepad1.yPosition", gamepad1.left_stick_y);
             telemetry.addData("Status", "Running");
             telemetry.update();
-
         }
     }
 }
