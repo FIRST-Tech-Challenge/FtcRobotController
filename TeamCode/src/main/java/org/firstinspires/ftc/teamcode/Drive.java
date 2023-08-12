c50af3805babe065e88652eb8004f13c2396ecce
privatelifeoflei <dohanhle@gmail.com>
2023-08-11 19:56:58 -0700

    le test 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -3,7 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorSimple; //this is le's
+
+//this is le's line
 
 @TeleOp
 public class Drive extends LinearOpMode{
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlynTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlynTest.java
@@ -3,4 +3,5 @@ package org.firstinspires.ftc.teamcode;
 public class kaitlynTest {
     int blah = 1;
     //heehee ethan wuz here
+    //ethan is michael jackson
 }
