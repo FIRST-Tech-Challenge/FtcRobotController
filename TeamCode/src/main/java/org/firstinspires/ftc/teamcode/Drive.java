5ba113825d8e372f827ab636544b5599e4baa337
privatelifeoflei <dohanhle@gmail.com>
2023-08-11 20:03:24 -0700

    small change LOLOL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple; //this is le's
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 //this is le's line
 
