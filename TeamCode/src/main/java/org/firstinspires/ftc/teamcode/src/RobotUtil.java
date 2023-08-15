584841f0e124c69af38ad27691b8dfc0fe738d63
ejmar <ejmarvel6@gmail.com>
2023-08-15 14:45:38 -0500

    S W E R V E

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/RobotUtil.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.src;
+
+public class RobotUtil {
+
+    public static double scaleVal (double input, double minInputVal, double maxInputVal, double minOutputVal, double maxOutputVal) {
+        if (input > maxInputVal) input = maxInputVal;
+        double inputRange = Math.abs(maxInputVal - minInputVal);
+        double outputRange = Math.abs(maxOutputVal - minOutputVal);
+        double scaleFactor = input/inputRange;
+        return outputRange * scaleFactor + minOutputVal;
+    }
+
+}
