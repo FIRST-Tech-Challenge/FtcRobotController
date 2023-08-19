28d3649d028c448b5c8da643aa4b4b817b07bffa
k8liang <k8lcool@gmail.com>
2023-08-19 16:25:35 -0700

    Vivian's Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,6 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -10,37 +8,38 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class Auto extends LinearOpMode {
     @Override
+
     public void runOpMode() throws InterruptedException {
-        DcMotor fLeft = hardwareMap.dcMotor.get("fLeft");
-        DcMotor bLeft = hardwareMap.dcMotor.get("bLeft");
-        DcMotor fRight = hardwareMap.dcMotor.get("fRight");
-        DcMotor bRight = hardwareMap.dcMotor.get("bRight");
-
-        fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        fRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        bRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
-        fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
-        while (opModeIsActive()) {
-            int ticks = fLeft.getCurrentPosition();
-            telemetry.addLine(String.valueOf(ticks));
-            telemetry.update();
+        while (opModeIsActive()){
+            DcMotor fLeft = hardwareMap.dcMotor.get("fLeft");
+            DcMotor bLeft = hardwareMap.dcMotor.get("bLeft");
+            DcMotor fRight = hardwareMap.dcMotor.get("fRight");
+            DcMotor bRight = hardwareMap.dcMotor.get("bRight");
 
-            fLeft.setPower(0.1);
-            bLeft.setPower(0.1);
-            fRight.setPower(0.1);
-            bRight.setPower(0.1);
+            fLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            fRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            bRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            if (ticks >= 383){
+            fLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            fLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            fLeft.getCurrentPosition();
+
+            double ticksPerInch = (537.7 / 1.4) / 11.87373601322835;
+            if (fLeft.getCurrentPosition() >= ticksPerInch * 12){
                 fLeft.setPower(0);
                 bLeft.setPower(0);
                 fRight.setPower(0);
                 bRight.setPower(0);
+            }
+            else if (ticksPerInch * 12 > fLeft.getCurrentPosition()){
+                fLeft.setPower(0.25);
+                bLeft.setPower(0.25);
+                fRight.setPower(0.25);
+                bRight.setPower(0.25);
             }
         }
     }
