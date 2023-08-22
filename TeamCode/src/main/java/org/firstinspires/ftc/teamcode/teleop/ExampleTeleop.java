8f56f6d65b07fed27d5bd7a6f8ba06907ca31f17
NickJerden <jerdenn25556@student.cghsnc.org>
2023-08-21 20:21:03 -0400

    Example teleop made

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ExampleTeleop.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.hardware.ImprovedGamepad;
+import org.firstinspires.ftc.teamcode.hardware.UpdatedClawbotHardware;
+
+public class ExampleTeleop extends OpMode {
+    UpdatedClawbotHardware robot = new UpdatedClawbotHardware();
+    ImprovedGamepad gamepad;
+
+    @Override
+    public void init() {
+        gamepad = new ImprovedGamepad(new Gamepad(), new ElapsedTime(), "Gamepad");
+        robot.init(this.hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        gamepad.update();
+        //write all teleop code below here
+
+    }
+}
