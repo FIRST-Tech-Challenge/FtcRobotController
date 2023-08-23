ff8c0aa5791d3baaa7b19ef7b977c6456bdc3668
LandenMcD42 <142231176+LandenMcD42@users.noreply.github.com>
2023-08-23 08:25:37 -0500

    Update HelloLanden.java
    code update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McDonald/HelloLanden.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McDonald/HelloLanden.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.McDonald;
 
-import...
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp()
+@TeleOp
 public class HelloLanden extends OpMode {
     @Override
     public void init() {
