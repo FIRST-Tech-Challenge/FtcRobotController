02664f1ae79adaa1ed2818615f738a05482d30c3
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-08-23 07:56:20 -0500

    Create HelloDawston.java
    Example program from Ch 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/HelloDawston.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.swift;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+
+public class HelloDawston extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Dawston");
+    }
+    public void loop(){
+    }
+}
