8bd8e503cb926f373a3acc594104c4d77d963943
Echo <142230725+asherhuffman@users.noreply.github.com>
2023-08-23 08:19:04 -0500

    Update HelloAsher.java
    code update

--- TeamCode/src/main/java/huffman/HelloAsher.java
+++ TeamCode/src/main/java/huffman/HelloAsher.java
@@ -3,10 +3,15 @@ package huffman;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp
+@TeleOp()
 public class HelloAsher extends OpMode {
     @Override
     public void init() {
         telemetry.addData("Hello","Asher");
+    }
+
+    @Override
+    public void loop() {
+
     }
 }
