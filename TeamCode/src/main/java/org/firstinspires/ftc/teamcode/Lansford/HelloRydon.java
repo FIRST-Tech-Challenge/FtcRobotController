e9ebbe022baa87701598f52ee6a62af081385315
Rex <142230883+RydonLansford@users.noreply.github.com>
2023-08-24 07:36:13 -0500

    HelloRydon1
    This is the chapter one example code!!!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lansford/HelloRydon.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Lansford;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp()
+public class HelloRydon extends OpMode {
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Rydon"); }
+    public void loop() {
+    }
+}
