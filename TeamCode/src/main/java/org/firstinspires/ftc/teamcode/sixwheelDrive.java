095069be83edc753905d2f411555d9e3b2477012
Dolphindasher324 <dolphindasher324@gmail.com>
2023-08-25 13:51:29 -0600

    Finished first 6 wheel drive program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sixwheelDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/sixwheelDrive.java
@@ -27,8 +27,18 @@ public class sixwheelDrive extends LinearOpMode {
 
         waitForStart();
 
+        double rightStickInput;
+        double leftStickInput;
+
         while opModeIsActive(){
-            
+
+            rightStickInput=gamepad1.right_stick_y;
+            leftStickInput=gamepad1.left_stick_y;
+            frontleft.setPower(gamepad1.left_stick_y);
+            backLeft.setPower(gamepad1.left_stick_y);
+            frontRight.setPower(this-gamepad1.right_stick_y);
+            backRight.setPower(this-gamepad1.right_stick_y);
+
         }
 
     }
