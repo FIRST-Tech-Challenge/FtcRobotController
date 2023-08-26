6d63132da37bda48fa892b8569f1fff707a9ece1
Chloe Wu <chloewu@chloes-air-2.lan>
2023-08-25 22:07:04 -0700

    hell no

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class Drive extends LinearOpMode{
@@ -13,21 +14,20 @@ public class Drive extends LinearOpMode{
         DcMotor bLeft = hardwareMap.dcMotor.get("bLeft"); //yaya kaitlyn was here :)
         DcMotor fRight = hardwareMap.dcMotor.get("fRight");
         DcMotor bRight  = hardwareMap.dcMotor.get("bRight");
-       //  DcMotor arm = hardwareMap.dcMotor.get("arm");
+        DcMotor arm = hardwareMap.dcMotor.get("arm");
 
-       // Servo servo1 = hardwareMap.servo.get("servo1");
-        // chloe's territory
-        // Servo servo2 = hardwareMap.servo.get("servo2");
+        Servo servo1 = hardwareMap.servo.get("servo1");
+        Servo servo2 = hardwareMap.servo.get("servo2");
 
 
         fLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         fRight.setDirection(DcMotorSimple.Direction.REVERSE);
         bRight.setDirection(DcMotorSimple.Direction.FORWARD);
-       // arm.setDirection(DcMotorSimple.Direction.FORWARD);
+        arm.setDirection(DcMotorSimple.Direction.FORWARD);
 
-       //  double servoOnePos = 0.5;
-       //  double servoTwoPos = 0.5;
+         double servoOnePos = 0.5;
+         double servoTwoPos = 0.5;
 
 
         waitForStart();
@@ -36,7 +36,7 @@ public class Drive extends LinearOpMode{
             double forwardMovement = -gamepad1.left_stick_y / 4; //vivian was here hehehe
             double turnMovement = gamepad1.right_stick_x /4;
             double mecanumMovement = gamepad1.left_stick_x /4;
-           //  double armPower = gamepad1.right_stick_y;
+            double armPower = gamepad1.right_stick_y;
 
             double fLeftPower = forwardMovement + turnMovement + mecanumMovement;
             double bLeftPower = forwardMovement + turnMovement - mecanumMovement;
@@ -61,9 +61,9 @@ public class Drive extends LinearOpMode{
             bLeft.setPower(bLeftPower);
             fRight.setPower(fRightPower);
             bRight.setPower(bRightPower);
-            // arm.setPower(armPower);
+            arm.setPower(armPower);
 
-            /* if (gamepad1.left_bumper == true) {
+            if (gamepad1.left_bumper == true) {
                 servoOnePos += 0.025;
                 servoTwoPos -= 0.025;
             }
@@ -77,7 +77,7 @@ public class Drive extends LinearOpMode{
             servo1.setPosition(servoOnePos);
             servo2.setPosition(servoTwoPos);
 
-             */
+
         }
 
 
