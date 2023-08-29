8a1226738debc6a4d0fe771e478354e2827051c0
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-08-29 08:14:36 -0500

    Update MathOpModeDS.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/MathOpModeDS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/MathOpModeDS.java
@@ -17,6 +17,9 @@ public class MathOpModeDS extends OpMode {
         double speedForward = -gamepad1.left_stick_y / 2.0;
         telemetry.addData("Left stick y", gamepad1.left_stick_y);
         telemetry.addData("speed Forward", speedForward);
+        double speedSideways = -gamepad1.right_stick_y / 2.0;
+        telemetry.addData("Right stick x", gamepad1.right_stick_x);
+        telemetry.addData("speed Sideways", speedSideways);
         /*this is whats being called to show on the drive hub, whats showing are the values that
         control/display the different aspects of you controllers movement*/
     }
