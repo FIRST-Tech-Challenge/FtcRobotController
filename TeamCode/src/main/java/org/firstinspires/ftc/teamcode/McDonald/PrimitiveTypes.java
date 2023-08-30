26734509263f7912ff67c9d3949ac27e6457c7bf
LandenMcD42 <142231176+LandenMcD42@users.noreply.github.com>
2023-08-30 07:56:33 -0500

    HelloLanden
    Hello caption was added. Nothing was added to the public void loop.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McDonald/PrimitiveTypes.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.McDonald;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp()
+public class PrimitiveTypes extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 16072;
+        double motorSpeed = 0.5;
+        boolean touchSensorPressed = true;
+        //created three new variables and incorporated them via telemetry.addData
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Touch Sensor", touchSensorPressed);
+    }
+
+    @Override
+    public void loop() {
+// Nothing was added to the public void loop
+    }
+}
