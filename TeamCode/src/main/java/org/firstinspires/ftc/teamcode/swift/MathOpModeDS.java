f15ac1b2ed52501c5238b476519595927eb93865
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-08-31 15:35:26 -0500

    Update MathOpModeDS.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/MathOpModeDS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/MathOpModeDS.java
@@ -20,18 +20,19 @@ public class MathOpModeDS extends OpMode {
         double speedSideways = -gamepad1.right_stick_y / 2.0;
         telemetry.addData("Right stick y", gamepad1.right_stick_y);
         telemetry.addData("speed Sideways", speedSideways);
+// the individual values is what will pop up here when you move the joysticks
 
-        if(gamepad1.a)
-            telemetry.addData("A Button", "pressed");
-        if(gamepad1.b)
-            telemetry.addData("B Button", "pressed");
 
-        double YDiffrence = gamepad1.left_stick_y - gamepad1.right_stick_y;
-        telemetry.addData("Y stick difference", YDiffrence);
-
-        double TriggerDiff = gamepad1.left_trigger - gamepad1.right_trigger;
-        telemetry.addData("Trigger difference",TriggerDiff );
-
+        boolean Bbutton = (gamepad1.b);
+// make a boolean variable that responds as true when b button is pressed
+        telemetry.addData("B button", Bbutton);
+// send that information to display on the driver hub
+        double YDifference = gamepad1.left_stick_y - gamepad1.right_stick_y;
+        telemetry.addData("Y stick difference", YDifference);
+// difference of the stick values when moved
+        double TriggerSum = gamepad1.left_trigger + gamepad1.right_trigger;
+        telemetry.addData("Trigger Sum",TriggerSum );
+// sum of both the trigger values
         /*this is whats being called to show on the drive hub, whats showing are the values that
         control/display the different aspects of you controllers movement*/
     }
