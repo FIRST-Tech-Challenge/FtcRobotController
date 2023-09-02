cb0a3dc1115df05e0b19780205928430bf749d40
confwhat <tanvinator24@gmail.com>
2023-09-02 16:01:11 -0700

    i added my stuffgit add --all

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TANVIII/GamepadPoop.java
@@ -0,0 +1,104 @@
+package org.firstinspires.ftc.teamcode.TANVIII;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class GamepadPoop extends LinearOpMode {
+    Robot robot;
+    DcMotor armMotor;
+    Servo leftyServo;
+    Servo rightyServo;
+
+    //TODO: gamepadDrive is only drivetrain
+    public void gamepadDrive() {
+        final double powerMult = 0.4;
+        double straightSpeed = gamepad1.left_stick_y*powerMult;
+        double centerTurnSpeed = gamepad1.right_stick_x*powerMult;
+        double mecanumSpeed = gamepad1.left_stick_x*powerMult;
+
+        telemetry.addLine(String.valueOf(gamepad1.left_stick_y));
+        telemetry.addLine(String.valueOf(straightSpeed));
+        telemetry.update();
+
+        //set power
+        double flpr = (straightSpeed) - (centerTurnSpeed) - (mecanumSpeed);
+        double frpr = - (straightSpeed) - (centerTurnSpeed) - (mecanumSpeed);
+        double blpr = (straightSpeed) - (centerTurnSpeed) + (mecanumSpeed);
+        double brpr = -(straightSpeed) - (centerTurnSpeed) + (mecanumSpeed);
+
+        //scaling
+        double bigPr = bigAbsVal(flpr, frpr, blpr, brpr);
+
+        if (Math.abs(bigPr) > (powerMult)) {
+            double scaleFactor = Math.abs(bigPr);
+            flpr /= scaleFactor/powerMult;
+            frpr /= scaleFactor/powerMult;
+            blpr /= scaleFactor/powerMult;
+            brpr /= scaleFactor/powerMult;
+
+        }
+        robot.setDrivetrainPower(flpr, frpr, blpr, brpr);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new Robot(hardwareMap);
+
+        leftyServo = hardwareMap.servo.get("1");
+        rightyServo = hardwareMap.servo.get("2");
+
+        //wheel measurements
+        final double ppr = 537.7;
+        final double motorToWheelRatio = 1.4;
+        final double wheelDiaMm = 96;
+        final double pi = 3.14159;
+
+        final double wheelCircMm = wheelDiaMm*pi;
+        final double wheelDiaIn = wheelDiaMm/25.4; //25.4mm = 1 in
+        final double wheelCircIn = wheelDiaIn*pi;
+        double tickToIn = motorToWheelRatio * wheelCircIn / ppr;
+
+        //wait
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            gamepadDrive();
+
+            //moving arm
+            if (gamepad1.left_bumper && gamepad1.right_bumper) {
+                robot.setArmPower(0);
+            } else if (gamepad1.right_bumper) {
+                robot.setArmPower(-1);
+            } else if (gamepad1.left_bumper) {
+                robot.setArmPower(1);
+            } else {
+                robot.setArmPower(0);
+            }
+
+            //move servos
+            if (gamepad1.a) {
+                leftyServo.setPosition(0);
+                rightyServo.setPosition(1);
+            } else if (gamepad1.b) {
+                leftyServo.setPosition(1);
+                rightyServo.setPosition(0);
+            }
+        }
+    }
+    private double bigAbsVal(double a, double... others) {
+
+        double max = a;
+
+        for (double next : others) {
+            if (Math.abs(next) > Math.abs(max)) {
+                max = next;
+            }
+        }
+        return max;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TANVIII/Robot.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.TANVIII;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    HardwareMap hwMap;
+
+    //declare local variables for all motors
+    DcMotor fl;
+    DcMotor fr;
+    DcMotor bl;
+    DcMotor br;
+
+    DcMotor armMotor;
+
+    public Robot (HardwareMap hardwareMap) {
+        this.hwMap = hardwareMap;
+
+        //initialize all motors
+        this.fl = hardwareMap.dcMotor.get("fl");
+        this.fr = hardwareMap.dcMotor.get("fr");
+        this.bl = hardwareMap.dcMotor.get("bl");
+        this.br = hardwareMap.dcMotor.get("br");
+
+        this.armMotor = hardwareMap.dcMotor.get("a");
+
+        //reset encoder
+        fl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        fr.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fr.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        bl.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bl.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        //arm
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        //zero pwr behavior (auto)
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+    public void setDrivetrainPower(double flPower, double frPower, double blPower, double brPower) {
+        fl.setPower(flPower);
+        fr.setPower(frPower);
+        bl.setPower(blPower);
+        br.setPower(brPower);
+    }
+
+    public void setArmPower (double armPower) {
+        armMotor.setPower(armPower);
+    }
+
+}
+
