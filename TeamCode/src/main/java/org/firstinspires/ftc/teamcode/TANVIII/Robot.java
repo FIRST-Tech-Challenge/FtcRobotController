3aee134efdf4c01189302a7217261604b17a94c1
confwhat <tanvinator24@gmail.com>
2023-09-04 21:23:02 -0700

    team project 5.3 tanvi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TANVIII/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TANVIII/Robot.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.TANVIII;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import java.util.Objects;
+
 public class Robot {
     HardwareMap hwMap;
 
@@ -35,9 +37,11 @@ public class Robot {
         br.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         br.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        /*
         //arm
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         */
 
         //zero pwr behavior (auto)
         fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -51,12 +55,24 @@ public class Robot {
         bl.setPower(blPower);
         br.setPower(brPower);
     }
-
     public void setArmPower (double armPower) {
         armMotor.setPower(armPower);
     }
 
+    //getflcurrentpos(name string)
+
+    public int getEncoderPosition (String motorName) {
+        if (Objects.equals(motorName, "fl")) {
+            return fl.getCurrentPosition();
+        } else if (Objects.equals(motorName, "fr")) {
+            return fr.getCurrentPosition();
+        } else if (Objects.equals(motorName, "bl")) {
+            return bl.getCurrentPosition();
+        } else if (Objects.equals(motorName, "br")) {
+            return br.getCurrentPosition();
+        } else {
+            assert false;
+            return 0;
+        }
+    }
 }
-
-
-
