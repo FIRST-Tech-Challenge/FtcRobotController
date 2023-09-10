b12b1214470df1c836552321f6941de1656e24ac
Ryan Brott <rcbrott@gmail.com>
2023-09-10 09:12:35 -0700

    Add more unit comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -56,17 +56,17 @@ public final class TankDrive {
         public double inPerTick = 0;
         public double trackWidthTicks = 0;
 
-        // feedforward parameters in tick units
+        // feedforward parameters (in tick units)
         public double kS = 0;
         public double kV = 0;
         public double kA = 0;
 
-        // path profile parameters
+        // path profile parameters (in inches)
         public double maxWheelVel = 50;
         public double minProfileAccel = -30;
         public double maxProfileAccel = 50;
 
-        // turn profile parameters
+        // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
 
