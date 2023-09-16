9c7e29d8830aa54e6cab89f049d7db8cecb8767b
DamienB325 <damien.brown15@outlook.com>
2023-09-16 11:54:12 -0700

    Fix typo again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagTest1.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AprilTagTest1.kt
@@ -7,7 +7,7 @@ import org.firstinspires.ftc.teamcode.Variables
 @Autonomous(name = "AprilTagTest1", group = "A")
 class AprilTagTest1: DriveMethods() {
     override fun runOpMode() {
-        initVison(Variables.VisionProcessors.APRILTAG)
+        initVision(Variables.VisionProcessors.APRILTAG)
 
         telemetry.addLine("Initialized AprilTag")
         telemetry.update()
