13736269f8646c32c77f84fbddc9b255d51e3a66
DamienB325 <damien.brown15@outlook.com>
2023-09-16 11:56:46 -0700

    Update TestAuto01.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto01.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto01.kt
@@ -26,20 +26,14 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package org.firstinspires.ftc.teamcode.Autonomous
 
-package org.firstinspires.ftc.teamcode.Autonomous;
-
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.trajectory.Trajectory;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-
-import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.RoadRunner.util.trajectorysequence.TrajectorySequence;
-
-import java.util.Locale;
-
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive
+import java.util.Locale
 
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
@@ -54,56 +48,49 @@ import java.util.Locale;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 // Autonomous
-
-@TeleOp(name="TestAuto01", group="Linear Opmode")
-public class TestAuto01 extends LinearOpMode {
-    @Override
-    public void runOpMode() {
+@TeleOp(name = "TestAuto01", group = "Linear Opmode")
+class TestAuto01 : LinearOpMode() {
+    override fun runOpMode() {
         // Setup Odometry :)
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        val drive = SampleMecanumDrive(hardwareMap)
 
         // Setup up the trajectory sequence (drive path)
-        TrajectorySequence traj1 = drive.trajectorySequenceBuilder(new Pose2d())
-                .strafeRight(10)
-                .forward(5)
-                .splineTo(new Vector2d(12,20),Math.toRadians(90))
-                .forward(5)
-                .splineTo(new Vector2d(0,38),Math.toRadians(180))
-                .splineTo(new Vector2d(-12,20),Math.toRadians(-90))
-                .forward(10)
-                .splineTo(new Vector2d(0,-10),Math.toRadians(0))
-                .strafeLeft(10)
-                .build();
+        val traj1 = drive.trajectorySequenceBuilder(Pose2d())
+            .strafeRight(10.0)
+            .forward(5.0)
+            .splineTo(Vector2d(12.0, 20.0), Math.toRadians(90.0))
+            .forward(5.0)
+            .splineTo(Vector2d(0.0, 38.0), Math.toRadians(180.0))
+            .splineTo(Vector2d(-12.0, 20.0), Math.toRadians(-90.0))
+            .forward(10.0)
+            .splineTo(Vector2d(0.0, -10.0), Math.toRadians(0.0))
+            .strafeLeft(10.0)
+            .build()
 
         // Tell the User the Robot has been initialized
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        telemetry.addData("Status", "Initialized")
+        telemetry.update()
 
 
         // Wait for the game to start (driver presses PLAY)
-        waitForStart();
-        telemetry.addLine("started");
-        telemetry.update();
-
-        if(isStopRequested()) return;
-
-        telemetry.addLine("not stopped");
-        telemetry.update();
-
-
-
-        drive.followTrajectorySequenceAsync(traj1);
-        while (opModeIsActive() && !isStopRequested()) {
-            drive.update();
+        waitForStart()
+        telemetry.addLine("started")
+        telemetry.update()
+        if (isStopRequested) return
+        telemetry.addLine("not stopped")
+        telemetry.update()
+        drive.followTrajectorySequenceAsync(traj1)
+        while (opModeIsActive() && !isStopRequested) {
+            drive.update()
         }
-
-        telemetry.addLine("tried to run code");
-        telemetry.update();
-        sleep(1000);
-
-        Pose2d pose = drive.getPoseEstimate();
-
-        telemetry.addData("Current Position", String.format(Locale.ENGLISH, "X: %f, Y: %f, and Rotation: %f", pose.getX(), pose.getY(),pose.getHeading()));
-        telemetry.update();
+        telemetry.addLine("tried to run code")
+        telemetry.update()
+        sleep(1000)
+        val (x, y, heading) = drive.poseEstimate
+        telemetry.addData(
+            "Current Position",
+            String.format(Locale.ENGLISH, "X: %f, Y: %f, and Rotation: %f", x, y, heading)
+        )
+        telemetry.update()
     }
 }
