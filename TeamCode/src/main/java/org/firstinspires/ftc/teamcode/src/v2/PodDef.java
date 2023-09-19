37bc4598d75f4b05f329ad447f25fe1285c1c8a3
ejmar <ejmarvel6@gmail.com>
2023-09-19 18:19:50 -0500

    refactored locations so I could recode the entire project

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/v2/PodDef.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.src.v2;
+
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.SwerveDriveKinematics;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class PodDef {
+
+    Translation2d left_pod_location = new Translation2d();
+    Translation2d right_pod_location = new Translation2d();
+
+    SwerveDriveKinematics differential_swerve_kinematics = new SwerveDriveKinematics(left_pod_location, right_pod_location);
+
+}
