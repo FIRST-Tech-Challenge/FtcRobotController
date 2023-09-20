88688e526fc5134b728c1c8ab2eca3d7f0794600
CodeSplash347 <manchestercedrek@gmail.com>
2023-09-20 19:06:41 -0400

    My code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Cedrek.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name="Cedrek's OpMode")
+public class Cedrek extends LinearOpMode{
+
+    // Definitions
+
+    @Override
+    public void runOpMode() {
+        //Initialization code
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+        }
+    }
+}
