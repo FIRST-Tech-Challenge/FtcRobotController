e684c777693c1e61c2faf08d49a1f0c92ae69665
MiN3CR4FT428 <hodysouthwell@gmail.com>
2023-09-20 18:54:30 -0400

    starting code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hody.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+public class Hody extends LinearOpMode {
+    // Definitions
+
+    @Override
+    public void runOpMode() {
+        //Initialization code
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+        }
+    }
+}
+
