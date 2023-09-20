37ac07637cb4aac50f9d83da22ec4501e07efced
Kevin Furlong <kevinmfurlong@gmail.com>
2023-09-20 18:38:45 -0400

    Added Basic Opmodes
    Added mecanum, tank, and blank opmode template.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTankDrive.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="Basic Tank Drive OpMode", group="Basic")
+public class BasicTankDrive extends LinearOpMode{
+    DcMotor m_frontLeft;
+    DcMotor m_frontRight;
+    DcMotor m_rearLeft;
+    DcMotor m_rearRight;
+
+    @Override
+    public void runOpMode() {
+        m_frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        m_frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        m_rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        m_rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+
+        m_frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        m_rearLeft.setDirection(DcMotor.Direction.REVERSE);
+        m_frontRight.setDirection(DcMotor.Direction.FORWARD);
+        m_rearRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            m_frontLeft.setPower(-gamepad1.left_stick_y); // Note: pushing stick forward gives negative value
+            m_rearLeft.setPower(-gamepad1.left_stick_y);
+            m_frontRight.setPower(-gamepad1.right_stick_y);
+            m_rearRight.setPower(-gamepad1.right_stick_y);
+        }
+
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blank.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@TeleOp(name="Empty Opmode")
+public class Blank extends LinearOpMode{
+    // Definitions
+
+    @Override
+    public void runOpMode() {
+        //Initialization code
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+        }
+    }
+}
