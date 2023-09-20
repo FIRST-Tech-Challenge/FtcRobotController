ed0af04e2be2a58257b9c1e44c0a73e05b4f0b0a
Mountie Megabots <144966050+MountieRoboticsStudent@users.noreply.github.com>
2023-09-20 18:55:24 -0400

    Everett

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Everett.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+
+@TeleOp(name="Empty Everett")
+public class Everett extends LinearOpMode {
+    // Definitions
+
+    @Override
+    public void runOpMode() {
+        //Initialization code
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+        }
+    }
+}
