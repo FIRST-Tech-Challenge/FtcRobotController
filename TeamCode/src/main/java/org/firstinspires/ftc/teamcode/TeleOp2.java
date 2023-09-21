03acf5633b9dc35b5923a2c85fc53eb2c1f4da33
hudshul <144957343+hudshul@users.noreply.github.com>
2023-09-21 13:11:03 -0700

    Update TeleOp2.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -87,12 +87,12 @@ public class TeleOp2 extends LinearOpMode {
             //      the setDirection() calls above.
             // Once the correct motors move in the correct direction re-comment this code.
 
-            /*
+
             leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
             leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
             rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
             rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
+
 
             // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
