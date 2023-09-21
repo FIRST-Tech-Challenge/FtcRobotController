164aae377e71db9aea39f91de50e939b7689338f
Warren Zhou <minecraftplayer6542@gmail.com>
2023-09-20 17:42:49 -0700

    Accomadating for change of plans

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/BradBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/BradBot.java
@@ -4,45 +4,50 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Components.Arm;
 import org.firstinspires.ftc.teamcode.Components.CVMaster;
-import org.firstinspires.ftc.teamcode.Components.Extendo;
-import org.firstinspires.ftc.teamcode.Components.FlippingIntake;
+import org.firstinspires.ftc.teamcode.Components.FutureComponents.Extendo;
+import org.firstinspires.ftc.teamcode.Components.FutureComponents.FlippingIntake;
 import org.firstinspires.ftc.teamcode.Components.Hopper;
+import org.firstinspires.ftc.teamcode.Components.Intake;
 import org.firstinspires.ftc.teamcode.Components.Lift;
 import org.firstinspires.ftc.teamcode.Components.Ultrasonics;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
 
 /**
  * Warren
+ * Robot class to contain all the season's functions
  */
 public class BradBot extends BasicRobot{
     Arm arm;
     CVMaster cv;
-    Extendo extendo;
-    FlippingIntake intake;
+    Intake intake;
     Hopper hopper;
     Lift lift;
     SampleMecanumDrive roadrun;
     Ultrasonics ultras;
+
+    /**
+     * Instatiates all the hardware and sets up initial states of some software
+     * @param p_op opMode
+     * @param p_is_Teleop is the program a teleop program
+     */
     public BradBot(LinearOpMode p_op, boolean p_is_Teleop){
         super(p_op,p_is_Teleop);
         arm = new Arm();
         cv = new CVMaster();
-        extendo = new Extendo();
-        intake = new FlippingIntake();
+        intake = new Intake();
         hopper = new Hopper();
         lift = new Lift();
         roadrun = new SampleMecanumDrive(p_op.hardwareMap);
-        ultras = new Ultrasonics();
+//        ultras = new Ultrasonics();
     }
     public void update(){
         super.update();
         arm.update();
         cv.update();
-        extendo.update();
         intake.update();
         hopper.update();
         lift.update();
         roadrun.update();
-        ultras.update();
+//        ultras.update();
     }
 }
