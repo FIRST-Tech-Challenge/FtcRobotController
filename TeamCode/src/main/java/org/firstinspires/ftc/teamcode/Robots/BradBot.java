91b292cfcfe6cd36bda8cfab266d3a355bac51da
Warren Zhou <minecraftplayer6542@gmail.com>
2023-09-21 10:07:46 -0700

    revamp the skeleton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/BradBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robots/BradBot.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.Robots;
 
+import static org.apache.commons.math3.util.FastMath.abs;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Components.Arm;
@@ -8,6 +11,7 @@ import org.firstinspires.ftc.teamcode.Components.FutureComponents.Extendo;
 import org.firstinspires.ftc.teamcode.Components.FutureComponents.FlippingIntake;
 import org.firstinspires.ftc.teamcode.Components.Hopper;
 import org.firstinspires.ftc.teamcode.Components.Intake;
+import org.firstinspires.ftc.teamcode.Components.Launcher;
 import org.firstinspires.ftc.teamcode.Components.Lift;
 import org.firstinspires.ftc.teamcode.Components.Ultrasonics;
 import org.firstinspires.ftc.teamcode.roadrunner.drive.SampleMecanumDrive;
@@ -21,6 +25,7 @@ public class BradBot extends BasicRobot{
     CVMaster cv;
     Intake intake;
     Hopper hopper;
+    Launcher launcher;
     Lift lift;
     SampleMecanumDrive roadrun;
     Ultrasonics ultras;
@@ -36,9 +41,45 @@ public class BradBot extends BasicRobot{
         cv = new CVMaster();
         intake = new Intake();
         hopper = new Hopper();
+        launcher = new Launcher();
         lift = new Lift();
         roadrun = new SampleMecanumDrive(p_op.hardwareMap);
 //        ultras = new Ultrasonics();
+    }
+    public void teleOp(){
+        boolean isA = gampad.readGamepad(op.gamepad1.a, "gamepad1_a", "toggleArm");
+        boolean rightBumper = gampad.readGamepad(op.gamepad1.right_bumper, "gamepad1_right_bumper", "startIntake");
+        boolean leftBumper = gampad.readGamepad(op.gamepad1.left_bumper, "gamepad1_left_bumper", "reverseIntake");
+        boolean isB = gampad.readGamepad(op.gamepad1.b,"gamepad1_b", "shoot");
+        boolean up = gampad.readGamepad(op.gamepad1.dpad_up, "gamepad1_dpad_up", "lift Up");
+        boolean down = gampad.readGamepad(op.gamepad1.dpad_down, "gamepad1_dpad_down", "lift down");
+        float manualUp = op.gamepad1.right_trigger;
+        float manualDown = op.gamepad1.left_trigger;
+        if(isA){
+//            arm.flip();
+        }
+        if(rightBumper){
+            intake.intake();
+        }
+        if(leftBumper){
+            intake.reverseIntake();
+        }
+        if(isB){
+            launcher.shoot();
+        }
+        if(up){
+//            lift.iterateUp();
+        }
+        if(down){
+//            lift.iterateDown();
+        }
+        if(abs(manualUp-manualDown)>0.05){
+//            lift.setManual(manualUp-manualDown);
+        }
+        roadrun.setWeightedDrivePower(new Pose2d(op.gamepad1.left_stick_y
+                , op.gamepad1.left_stick_x
+                , op.gamepad1.right_stick_x));
+        update();
     }
     public void update(){
         super.update();
