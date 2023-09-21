1bffe1267017454fb7d7468b8a5129a884e601dc
Cindy Yam <cindyyam8@gmail.com>
2023-09-20 17:13:01 -0700

    april tag

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -60,10 +60,10 @@ public class TeleOp extends LinearOpMode {
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = ((rotY + rotX + rx) / denominator)/2;
-            double backLeftPower = ((rotY - rotX + rx) / denominator)/2;
-            double frontRightPower = ((rotY - rotX - rx) / denominator)/2;
-            double backRightPower = ((rotY + rotX - rx) / denominator)/2;
+            double frontLeftPower = ((rotY + rotX + rx) / denominator);
+            double backLeftPower = ((rotY - rotX + rx) / denominator);
+            double frontRightPower = ((rotY - rotX - rx) / denominator);
+            double backRightPower = ((rotY + rotX - rx) / denominator);
 
             frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
