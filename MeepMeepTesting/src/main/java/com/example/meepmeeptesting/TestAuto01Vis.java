4f65690359c1e601cc1629897e35b87fa7cbbd93
MostlyOperational18119 <ftc18119@gmail.com>
2023-09-23 12:39:00 -0700

    weird meepmeep thing (NOT FINISHED)

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TestAuto01Vis.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TestAuto01Vis.java
@@ -2,9 +2,16 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.path.Path;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.SampleMecanumDrive;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import com.noahbres.meepmeep.roadrunner.trajectorysequence.sequencesegment.TrajectorySegment;
+
+import java.util.List;
 
 public class TestAuto01Vis {
     enum detections {
@@ -15,25 +22,29 @@ public class TestAuto01Vis {
     public static void main(String[] args) {
         System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(800);
-
-        detections detection = detections.CENTER;
+        detections detection = detections.LEFT;
         RoadRunnerBotEntity myBot;
-                myBot = new DefaultBotBuilder(meepMeep)
-                        .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                        .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(61.5, -36, Math.toRadians(180)))
-                                        .forward(25.0)
-                                        .build()
-                        );
 
-                switch (detection) {
-                    case LEFT:
-                        break;
-                    case CENTER:
-                        break;
-                    case RIGHT:
-                        break;
-                }
+        myBot = new DefaultBotBuilder(meepMeep)
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+//                .followTrajectorySequence(drive ->
+//                        drive.trajectorySequenceBuilder(new Pose2d(61.5, -36, Math.toRadians(180)))
+//                                .forward(25.0)
+//                                .build()
+//                );
+                .build();
+
+        switch (detection) {
+            case LEFT:
+                myBot.getDrive().
+                myBot.followTrajectorySequence(new TrajectorySequence()
+                );
+                break;
+            case CENTER:
+                break;
+            case RIGHT:
+                break;
+        }
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
