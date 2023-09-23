d8ebbdc4c066c6e188ce1b3a088f75d4553889ee
Budiul Cristian-Carol <cristib_2002@yahoo.com>
2023-09-23 12:23:19 +0300

    Added blabla.java teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/blabla.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class blabla extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
