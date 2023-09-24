9b8d29115eb0b156e7aa7b19f71d8c02b4baddee
Hyunwook Choi <105407466+Sipdip@users.noreply.github.com>
2023-09-23 18:30:54 -0700

    almost finished skelly for hopper and rfcolorsensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/RFColorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/RFModules/Devices/RFColorSensor.java
@@ -1,7 +1,35 @@
 package org.firstinspires.ftc.teamcode.Components.RFModules.Devices;
 
+import static org.firstinspires.ftc.teamcode.Robots.BasicRobot.op;
+
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+
 /**
  * Harry
  */
 public class RFColorSensor {
+    private RevColorSensorV3 colorSensor;
+    public RFColorSensor(String p_deviceName){
+        colorSensor = op.hardwareMap.get(RevColorSensorV3.class, p_deviceName);
+    }
+
+    public boolean isWhite(){
+        boolean white = false;
+        return white;
+    }
+
+    public boolean isPurple(){
+        boolean purple = false;
+        return purple;
+    }
+
+    public boolean isYellow(){
+        boolean yellow = false;
+        return yellow;
+    }
+
+    public boolean isGreen(){
+        boolean green = false;
+        return green;
+    }
 }
