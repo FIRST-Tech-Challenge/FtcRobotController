a8a01c741c30b502dfd4310d28610879441e36c1
A. Teo Welton <76081718+DragonDev07@users.noreply.github.com>
2023-09-26 13:19:30 -0600

    Update subsystems w java doc and new classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
@@ -1,15 +1,18 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.subsystems;
 
 import androidx.annotation.NonNull;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-
+/**
+ * Stores and Declares all hardware devices &amp; related methods
+ */
 public class HWC {
     // Declare empty variables for robot hardware
     public DcMotorEx leftFront, rightFront, leftRear, rightRear;
@@ -18,6 +21,12 @@ public class HWC {
     Telemetry telemetry;
     ElapsedTime time = new ElapsedTime();
 
+    /**
+     * Constructor for HWC, declares all hardware components
+     *
+     * @param hardwareMap HardwareMap - Used to retrieve hardware devices
+     * @param telemetry   Telemetry - Used to add telemetry to driver hub
+     */
     public HWC(@NonNull HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
 
@@ -29,9 +38,9 @@ public class HWC {
 
         // Set the direction of motors
         // TODO: UPDATE VALUES WITH NEW BOT
-        leftFront.setDirection(DcMotorEx.Direction.FORWARD);
+        leftFront.setDirection(DcMotorEx.Direction.REVERSE);
         rightFront.setDirection(DcMotorEx.Direction.FORWARD);
-        leftRear.setDirection(DcMotorEx.Direction.REVERSE);
+        leftRear.setDirection(DcMotorEx.Direction.FORWARD);
         rightRear.setDirection(DcMotorEx.Direction.FORWARD);
 
         // Set motors to break when power = 0
