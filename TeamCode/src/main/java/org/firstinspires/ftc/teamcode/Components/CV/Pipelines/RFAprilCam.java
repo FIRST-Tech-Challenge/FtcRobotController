e9fcb7e845395f3050bb8afa33a2803e44dd5445
Warren Zhou <minecraftplayer6542@gmail.com>
2023-09-26 09:39:03 -0700

    april tags done and IMU reduced test case

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/CV/Pipelines/RFAprilCam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/CV/Pipelines/RFAprilCam.java
@@ -14,7 +14,9 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 
+import org.apache.commons.math3.analysis.function.Exp;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
@@ -23,6 +25,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Warren
@@ -30,17 +33,19 @@ import java.util.ArrayList;
  */
 @Config
 public class RFAprilCam {
-    public static double X_OFFSET = 0, Y_OFFSET = 0, UPSAMPLE_THRESHOLD=50;
-    public static float DOWNSAMPLE = 2, UPSAMPLE = 3;
+    public static double X_OFFSET = 0, Y_OFFSET = 0, UPSAMPLE_THRESHOLD=20;
+    public static int EXPOSURE_MS=20;
+    public static float DOWNSAMPLE = 2, UPSAMPLE = 6;
     private AprilTagProcessor aprilTag;
     public RFVisionPortal visionPortal;
     boolean upsample=false;
+    ExposureControl exposureControl;
     private Vector2d[] values = {new Vector2d(0, 0), new Vector2d(0, 0),new Vector2d(0, 0),new Vector2d(0, 0),
             new Vector2d(0, 0),new Vector2d(0, 0)
-            ,new Vector2d(0, 0),new Vector2d(3*23.5-1,1.5*23.5),new Vector2d(3*23.5-1,1.5*23.5-4.5)
-            ,new Vector2d(3*23.5-1,-1.5*23.5),new Vector2d( 3*23.5-1,-1.5*23.5+4.5)};
+            ,new Vector2d(0, 0),new Vector2d(3*23.5-1,-1.5*23.5),new Vector2d(3*23.5-1,-1.5*23.5+4.5)
+            ,new Vector2d(3*23.5-1,1.5*23.5),new Vector2d( 3*23.5-1,1.5*23.5-4.5)};
     private ArrayList<Pose2d> camPose = new ArrayList<>();
-    private double[][] directions = {{-1, 1}, {-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, -1},{-1, -1},{-1, -1},{-1, -1}};
+    private double[][] directions = {{-1, 1}, {-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, 1},{-1, -1},{-1, -1},{-1, 1},{-1, 1}};
 
     /**
      * Initialize apriltag camera
@@ -57,7 +62,7 @@ public class RFAprilCam {
                 .setLensIntrinsics(822.317f, 822.317f, 319.495f, 242.502f)
                 .build();
         aprilTag.setPoseSolver(AprilTagProcessor.PoseSolver.OPENCV_IPPE);
-        aprilTag.setDecimation(DOWNSAMPLE);
+        aprilTag.setDecimation(UPSAMPLE);
 
         // Create the WEBCAM vision portal by using a builder.
         visionPortal = new RFVisionPortal.Builder()
@@ -68,6 +73,13 @@ public class RFAprilCam {
                 .setCameraResolution(new Size(640, 480))
                 .build();
         visionPortal.stopLiveView();
+        while(visionPortal.getCameraState()!= RFVisionPortal.CameraState.STREAMING){
+            op.sleep(50);
+        }
+        exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+        exposureControl.setMode(ExposureControl.Mode.Manual);
+        exposureControl.setExposure(EXPOSURE_MS, TimeUnit.MILLISECONDS);
+        exposureControl.setAePriority(false);
     }
 
     /**
@@ -81,6 +93,7 @@ public class RFAprilCam {
         double poseCount =0;
         camPose.clear();
         upsample=false;
+        exposureControl.setExposure(EXPOSURE_MS, TimeUnit.MILLISECONDS);
         for (AprilTagDetection detection : detections) {
              AprilTagPoseFtc poseFtc= detection.ftcPose;
              double p_x = poseFtc.y, p_y = poseFtc.x;
@@ -90,18 +103,18 @@ public class RFAprilCam {
              packet.put("Y,P,R", new Pose2d(poseFtc.yaw,poseFtc.pitch,poseFtc.roll));
              packet.put("bearing", poseFtc.bearing);
              double dist = sqrt(poseFtc.x*poseFtc.x + poseFtc.y*poseFtc.y);
-             if(dist<UPSAMPLE_THRESHOLD){
+             if(dist<UPSAMPLE_THRESHOLD&&p_ind==10){
                  upsample=true;
                  poseCount++;
                  newPose = newPose.plus(camPose.get(camPose.size()-1));
              }
-             logger.log("/RobotLogs/GeneralRobot", "aprilPos = "+camPose.get(camPose.size()-1)+", dist:"+dist);
+             logger.log("/RobotLogs/GeneralRobot", "aprilPos = "+camPose.get(camPose.size()-1)+", dist:"+dist+" p_x, p_y: " + p_x +',' +p_y);
         }
-        if(upsample){
-            aprilTag.setDecimation(UPSAMPLE);
-        }else{
-            aprilTag.setDecimation(DOWNSAMPLE);
-        }
+//        if(upsample){
+//            aprilTag.setDecimation(UPSAMPLE);
+//        }else{
+//            aprilTag.setDecimation(DOWNSAMPLE);
+//        }
         if(camPose.size() > 0 && upsample && poseCount!=0){
             logger.log("/RobotLogs/GeneralRobot", "avgAprilPose"+newPose.div(poseCount));
             currentPose = new Pose2d(newPose.div(poseCount).vec(), currentPose.getHeading());
