0daa7d9bdfd988d2d88def2eeacdd6f750bfeeb9
Mountie Megabots <144966050+MountieRoboticsStudent@users.noreply.github.com>
2023-09-26 19:12:43 -0400

    we did work!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blank.java
@@ -2,18 +2,61 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Empty Opmode")
-public class Blank extends LinearOpMode{
-    // Definitions
+@TeleOp(name="Basic Mecanum OpMode", group="Basic")
+public class BasicMecanumDrive extends LinearOpMode{
+    DcMotor m_frontLeft;
+    DcMotor m_frontRight;
+    DcMotor m_rearLeft;
+    DcMotor m_rearRight;
 
     @Override
     public void runOpMode() {
-        //Initialization code
+        m_frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        m_frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        m_rearLeft = hardwareMap.get(DcMotor.class, "rearLeft");
+        m_rearRight = hardwareMap.get(DcMotor.class, "rearRight");
+
+        m_frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        m_rearLeft.setDirection(DcMotor.Direction.REVERSE);
+        m_frontRight.setDirection(DcMotor.Direction.FORWARD);
+        m_rearRight.setDirection(DcMotor.Direction.FORWARD);
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;// Note: pushing stick forward gives negative value
+            double x = gamepad1.left_stick_x;
+            double rotation = gamepad1.right_stick_x;
+
+            if(gamepad.y){
+                m_frontLeft.setPower(1);
+                m_frontRight.setPower(1);
+                m_rearRight.setPower(1);
+                m_rearLeft.setPower(1);
+            }
+            if else(gamepad1.b){
+
+            }
+            else {
+                m_frontLeft.setPower(0);
+                m_rearLeft.setPower(0);
+                m_frontRight.setPower(0);
+                m_rearRight.setPower(0);
+            }
+
         }
+
     }
+
 }
+
