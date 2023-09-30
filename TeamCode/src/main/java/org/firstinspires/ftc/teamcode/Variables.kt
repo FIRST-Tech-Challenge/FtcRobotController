bf8ad9cf458db971621692dcf89e5a1613e22098
Markytt <theinteruptingchicken@gmail.com>
2023-09-29 17:06:17 -0700

    added linear slide variables and disabled road runner tuners

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.kt
@@ -9,6 +9,15 @@ object Variables {
     var servoRelease = 0.0
     var servoClamp = 0.0
 
+    var t = 0.0;
+    var slideToBoard = 0.0;
+    var clawToBoard = 0.0;
+    var x =  slideToBoard - clawToBoard + .5*t;
+    var y = Math.sqrt(3.0)/2 * t;
+    var slideLength = Math.sqrt(Math.pow(x, 2.0) + Math.pow(y, 2.0));
+    var slideAngle = Math.atan(y/x);
+    var clawAngle = 60 - slideAngle;
+
     enum class VisionProcessors {
         TFOD, APRILTAG, BOTH
     }
