da0d168fadda7a2ffb11b7e61a829a0844fd82c4
anikanagle <nikunagle@gmail.com>
2023-09-29 23:11:09 -0400

    Coded an Auto Path - for blue alliance far from the backdrop - includes scoring 2 pixels on backdrop and parking - no sensing
    ...I think this works but idk, thread is a bit weird and may need to change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
@@ -1,6 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -10,15 +13,23 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 @Autonomous(name = "MainAutonomous")
 public class MainAuto extends LinearOpMode{
 
-    //SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+    //need to clarify values later
+    double x,y;
+    double randomDistance;
 
     Bot bot;
 
     enum Side {
         RIGHT, LEFT, NULL;
     }
+    enum DistanceToBackdrop{
+        CLOSE, FAR, NULL;
+    }
 
     Side side = Side.NULL;
+    DistanceToBackdrop dtb= DistanceToBackdrop.NULL;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -26,33 +37,73 @@ public class MainAuto extends LinearOpMode{
         GamepadEx gp1 = new GamepadEx(gamepad1);
         GamepadEx gp2 = new GamepadEx(gamepad2);
 
+        Pose2d startPose = new Pose2d(0, 0, 0);
+        drive.setPoseEstimate(startPose);
+
 
         while (!isStarted()) {
             gp1.readButtons();
             if (gp1.wasJustPressed(GamepadKeys.Button.B)) {
                 side = Side.RIGHT;
             }
-            if (gp1.wasJustPressed(GamepadKeys.Button.X)) {
+            if (gp1.wasJustPressed(GamepadKeys.Button.A)) {
                 side = Side.LEFT;
+            }
+            if(gp1.wasJustPressed(GamepadKeys.Button.X)){
+                dtb= DistanceToBackdrop.CLOSE;
+            }
+            if(gp1.wasJustPressed(GamepadKeys.Button.Y)){
+                dtb= DistanceToBackdrop.FAR;
             }
 
 
-        /*    Trajectory forward = drive.trajectoryBuilder(startPose)
-                    .lineTo(new Vector2d(52, moveDiff))
+            //moves to specified coordinates while maintaining current heading
+            Trajectory forward = drive.trajectoryBuilder(startPose)
+                    .lineTo(new Vector2d(x, y))
                     .build();
 
-            Trajectory parkLeft = drive.trajectoryBuilder(forward.end())
-                    .strafeLeft(22)
-                    .build();
-            Trajectory parkRight = drive.trajectoryBuilder(forward.end())
-                    .strafeRight(24)
+            Trajectory strafeLeft = drive.trajectoryBuilder(forward.end())
+                    .strafeLeft(randomDistance)
                     .build();
 
-         */
+            Trajectory strafeRight = drive.trajectoryBuilder(strafeLeft.end())
+                    .strafeRight(randomDistance)
+                    .build();
+
+
+            Trajectory blueAllianceFarNoSense = drive.trajectoryBuilder(strafeLeft.end())
+                    .splineTo(new Vector2d(-36,-36), Math.toRadians(0))
+
+                    //spline will occur slower
+                    .splineTo(new Vector2d(-36,48), Math.toRadians(90), SampleMecanumDrive.getVelocityConstraint(0.2, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                            SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
+
+                    .splineTo(new Vector2d(-48,48), Math.toRadians(90))
+                    .build();
+
+            Thread blueAllianceFarNoSenseThread = new Thread(() -> drive.followTrajectory(blueAllianceFarNoSense));
+            //this thread will enable the robot to move forward while performing other activities
+
+            waitForStart();
+            if (!isStopRequested()) {
+
+                if(dtb== DistanceToBackdrop.FAR && side==Side.LEFT){
+                    blueAllianceFarNoSenseThread.start();
+                    //place purple pixel
+                    sleep(1000);
+                    Bot.fourbar.outtake();
+                    Bot.slides.runTo(1);
+                    Bot.box.depositFirstPixel();
+                    Bot.slides.runTo(2);
+                    Bot.box.depositSecondPixel();
+                    Bot.resetOuttake();
+                    blueAllianceFarNoSenseThread.interrupt();
+
+                }
 
 
 
-
+            }
 
 
         }
