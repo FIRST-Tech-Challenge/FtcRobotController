6ed9580a6cc7b11f3e3e69429398d3192c8ac505
Dashhyphenminus <robertblendea@outlook.com>
2023-10-01 11:33:09 -0700

    Replace ConceptTensorFlowObjectDetection into Example Op-modes

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import android.os.Environment;
 
