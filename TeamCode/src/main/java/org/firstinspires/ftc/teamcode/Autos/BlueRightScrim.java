99899e0a5aa6886bb80420605d8c9bc44c07c74e
eleanor <eleanorgklein@gmail.com>
2023-10-02 19:31:40 -0400

    setup auto class skeletons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueRightScrim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueRightScrim.java
@@ -1,6 +1,10 @@
 package org.firstinspires.ftc.teamcode.Autos;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.HelperClasses.TSEFinder;
+import org.firstinspires.ftc.teamcode.HelperClasses.Odometry;
+import org.firstinspires.ftc.teamcode.Hardware;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -16,8 +20,33 @@ public class BlueRightScrim extends LinearOpMode
     @Override
     public void runOpMode() throws InterruptedException
     {
+        //initializes robot
+        Hardware robot = new Hardware(hardwareMap);
+
+        //camera setup
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        OpenCvWebcam webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+        webcam.setPipeline(new TSEFinder());
 
         waitForStart();
+
+        //this loop runs after play pressed
         while(opModeIsActive())
         {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedLeftScrim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/RedLeftScrim.java
@@ -1,4 +1,55 @@
 package org.firstinspires.ftc.teamcode.Autos;
 
-public class RedLeftScrim {
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.HelperClasses.TSEFinder;
+import org.firstinspires.ftc.teamcode.HelperClasses.Odometry;
+import org.firstinspires.ftc.teamcode.Hardware;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous(name = "Red Left Auto", group = "Autonomous")
+public class RedLeftScrim extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        //initializes robot
+        Hardware robot = new Hardware(hardwareMap);
+
+        //camera setup
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        OpenCvWebcam webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+
+        webcam.setMillisecondsPermissionTimeout(2500); // Timeout for obtaining permission is configurable. Set before opening.
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+        webcam.setPipeline(new TSEFinder());
+
+        waitForStart();
+
+        //this loop runs after play pressed
+        while(opModeIsActive())
+        {
+
+        }
+    }
 }
