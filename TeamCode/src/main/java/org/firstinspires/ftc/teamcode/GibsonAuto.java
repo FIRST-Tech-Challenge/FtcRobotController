c200bdd57eb70c96863ba333fddcff9aba2deed6
Gibson Goodrich <114115712+XboxerB47@users.noreply.github.com>
2023-10-03 19:01:14 -0400

    made sure there was a continual check.
    made sure that it would check to make sure opmode is running to continuously run.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GibsonAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GibsonAuto.java
@@ -25,7 +25,7 @@ public class GibsonAuto extends LinearOpMode{
         m_rearLeft.setDirection(DcMotor.Direction.REVERSE);
         m_frontRight.setDirection(DcMotor.Direction.FORWARD);
         m_rearRight.setDirection(DcMotor.Direction.FORWARD);
-        
+
         double LY = -gamepad1.left_stick_y; //y axis on left stick
         double LX = -gamepad1.left_stick_x; //X axis on left stick
         double RY = -gamepad1.right_stick_y; //y axis on right stick
@@ -33,35 +33,34 @@ public class GibsonAuto extends LinearOpMode{
 
         waitForStart();
 
-        if(getRuntime() < 1){
-            m_rearLeft.setPower(1);
-            m_rearRight.setPower(1);
-            m_frontLeft.setPower(1);
-            m_frontRight.setPower(1);
-        }
-        else if(getRuntime() < 2){
-            m_rearLeft.setPower(-1);
-            m_rearRight.setPower(-1);
-            m_frontLeft.setPower(-1);
-            m_frontRight.setPower(-1);
-        }
-        if(getRuntime() < 3){
-            m_rearLeft.setPower(1);
-            m_rearRight.setPower(1);
-            m_frontLeft.setPower(1);
-            m_frontRight.setPower(1);
-        }
-        else if(getRuntime() < 5){
-            m_rearLeft.setPower(-1);
-            m_rearRight.setPower(-1);
-            m_frontLeft.setPower(-1);
-            m_frontRight.setPower(-1);
-        }
-        else{
-            m_rearLeft.setPower(0);
-            m_rearRight.setPower(0);
-            m_frontLeft.setPower(0);
-            m_frontRight.setPower(0);
+        while(opModeIsActive()) {
+            if (getRuntime() < 1) {
+                m_rearLeft.setPower(1);
+                m_rearRight.setPower(1);
+                m_frontLeft.setPower(1);
+                m_frontRight.setPower(1);
+            } else if (getRuntime() < 2) {
+                m_rearLeft.setPower(-1);
+                m_rearRight.setPower(-1);
+                m_frontLeft.setPower(-1);
+                m_frontRight.setPower(-1);
+            }
+            if (getRuntime() < 3) {
+                m_rearLeft.setPower(1);
+                m_rearRight.setPower(1);
+                m_frontLeft.setPower(1);
+                m_frontRight.setPower(1);
+            } else if (getRuntime() < 5) {
+                m_rearLeft.setPower(-1);
+                m_rearRight.setPower(-1);
+                m_frontLeft.setPower(-1);
+                m_frontRight.setPower(-1);
+            } else {
+                m_rearLeft.setPower(0);
+                m_rearRight.setPower(0);
+                m_frontLeft.setPower(0);
+                m_frontRight.setPower(0);
+            }
         }
     }
 }
