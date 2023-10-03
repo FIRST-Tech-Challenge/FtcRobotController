ec20eeac2f0294aeb26bd7ab263e7cb0d5f09fae
Shawn Xu <xu107288696@gmail.com>
2023-10-02 23:58:29 -0700

    Format Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestClass.java
@@ -11,31 +11,30 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class TestClass extends LinearOpMode {
 
-    private Gyroscope imu;
-    private DcMotor motorTest;
-    private DigitalChannel digitalTouch;
-    private DistanceSensor sensorColorRange;
-    private Servo servoTest;
+	private Gyroscope imu;
+	private DcMotor motorTest;
+	private DigitalChannel digitalTouch;
+	private DistanceSensor sensorColorRange;
+	private Servo servoTest;
 
+	@Override
+	public void runOpMode() {
+		imu = hardwareMap.get(Gyroscope.class, "imu");
+		motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+		digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+		sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+		servoTest = hardwareMap.get(Servo.class, "servoTest");
 
-    @Override
-    public void runOpMode() {
-        imu = hardwareMap.get(Gyroscope.class, "imu");
-        motorTest = hardwareMap.get(DcMotor.class, "motorTest");
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
-        servoTest = hardwareMap.get(Servo.class, "servoTest");
+		telemetry.addData("Status", "Initialized");
+		telemetry.update();
+		// Wait for the game to start (driver presses PLAY)
+		waitForStart();
 
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
+		// run until the end of the match (driver presses STOP)
+		while (opModeIsActive()) {
+			telemetry.addData("Status", "Running");
+			telemetry.update();
 
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-
-        }
-    }
+		}
+	}
 }
