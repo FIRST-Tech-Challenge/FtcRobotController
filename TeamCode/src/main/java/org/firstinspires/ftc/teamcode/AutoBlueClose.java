be19f306227c889eb49a40878c48a592c8659d24
hudshul <144957343+hudshul@users.noreply.github.com>
2023-10-03 12:36:33 -0700

    Auto set up

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueClose.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Autonomous(name = "AutoBlueClose", group = "linear autoMode")
+@Disabled
+public class AutoBlueClose {
+
+}
