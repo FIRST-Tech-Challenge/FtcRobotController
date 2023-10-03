1257d364367cd23eef199b3c8381e17c8c76ff53
Torterra890 <mr.lucian.smith@gmail.com>
2023-10-03 12:56:13 -0700

    Update AutoBlueClose.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueClose.java
@@ -7,6 +7,43 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 @Autonomous(name = "AutoBlueClose", group = "linear autoMode")
 @Disabled
-public class AutoBlueClose {
+public class AutoBlueClose extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+
+    @Override
+    public void runOpMode()
+    {
+        //Setting motor names
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "frontleft");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "backleft");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "frontright");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "backright");
+        //Setting motor direction
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive() && runtime.seconds() <= 2.0)
+        {
+            leftFrontDrive.setPower(-1.0);
+            leftBackDrive.setPower(1.0);
+            rightFrontDrive.setPower(1.0);
+            rightBackDrive.setPower(-1.0);
+        }
+        leftFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
 
 }
