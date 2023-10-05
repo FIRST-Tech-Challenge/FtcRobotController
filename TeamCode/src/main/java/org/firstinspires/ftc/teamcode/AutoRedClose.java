9cff7712db7b6a2e4e178ed5455078380ed88ab1
otolba <omar16tolba@gmail.com>
2023-10-05 13:45:24 -0700

    test encoder auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedClose.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedClose.java
@@ -13,11 +13,13 @@ public class AutoRedClose extends LinearOpMode {
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
+    private boolean isRunning = true;
+    private final int oneRotaion = 1440;
 
     @Override
     public void runOpMode() {
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "frontleft");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "backleft");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "frontleft");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "backleft");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "frontright");
         rightBackDrive = hardwareMap.get(DcMotor.class, "backright");
 
@@ -26,21 +28,54 @@ public class AutoRedClose extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        leftFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart();
-        runtime.reset();
 
-        while (opModeIsActive() && (runtime.seconds() <= 2.0)) {
+        while (opModeIsActive() && isRunning) {
+            leftFrontDrive.setTargetPosition(leftFrontDrive.getCurrentPosition() + 5*oneRotaion);
+            leftBackDrive.setTargetPosition(leftBackDrive.getCurrentPosition() - 5*oneRotaion);
+            rightFrontDrive.setTargetPosition(leftFrontDrive.getCurrentPosition() - 5*oneRotaion);
+            rightBackDrive.setTargetPosition(leftBackDrive.getCurrentPosition() + 5*oneRotaion);
+
             leftFrontDrive.setPower(0.5);
-            leftBackDrive.setPower(-0.5);
-            rightFrontDrive.setPower(-0.5);
+            leftBackDrive.setPower(0.5);
+            rightFrontDrive.setPower(0.5);
             rightBackDrive.setPower(0.5);
+
+            leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            isRunning = false;
         }
         leftFrontDrive.setPower(0);
         leftBackDrive.setPower(0);
         rightFrontDrive.setPower(0);
         rightBackDrive.setPower(0);
+
+//        while (opModeIsActive() && (runtime.seconds() <= 2.0)) {
+//            leftFrontDrive.setPower(0.5);
+//            leftBackDrive.setPower(-0.5);
+//            rightFrontDrive.setPower(-0.5);
+//            rightBackDrive.setPower(0.5);
+//        }
+//        leftFrontDrive.setPower(0);
+//        leftBackDrive.setPower(0);
+//        rightFrontDrive.setPower(0);
+//        rightBackDrive.setPower(0);
     }
 }
+
