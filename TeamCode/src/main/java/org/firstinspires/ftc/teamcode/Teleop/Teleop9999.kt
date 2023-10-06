d4f262d211484bd1238ee8f2da1f76ab2fe0ed3c
Markytt <theinteruptingchicken@gmail.com>
2023-10-06 15:59:26 -0700

    teleop folder created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop9999.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Teleop9999.kt
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonomous
+package org.firstinspires.ftc.teamcode.Teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
