57ce91ebb1b309eaf51e7c54e901b5d55d9cd730
SynnovationFTC <synnovationftc@gmail.com>
2023-10-06 14:25:06 -0500

    MotorTest Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -3,10 +3,11 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class MotorTest extends LinearOpMode {
     private DcMotor TestMotor = null;
     @Override
+
     public void runOpMode() {
         TestMotor = hardwareMap.get(DcMotor.class, "motor1");
         TestMotor.setPower(0.75);
