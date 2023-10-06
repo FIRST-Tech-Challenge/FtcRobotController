01c7ae155716904c9c4b93ef9c33643622d51b43
MostlyOperational18119 <ftc18119@gmail.com>
2023-10-06 16:12:17 -0700

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/LinearSlideText.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.Teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "LinearSlideTest", group = "LinearSlide")
+public class LinearSlideText {
+
+}
