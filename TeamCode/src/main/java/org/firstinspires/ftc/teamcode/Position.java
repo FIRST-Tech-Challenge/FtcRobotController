a58a5760a8cf29af16c77c0e64fd2be7161bab84
SuperTylerMan <ty.r.montgomery@gmail.com>
2023-10-06 20:41:54 -0400

    Position.java -The whole code is now complete. Code is now documented, and commented. Code was updated slightly from the 2022-2023 season. -Will not create a pull request, as PositionManager.java needs to be done first before pulling...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Position.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Position.java
@@ -13,34 +13,107 @@ package org.firstinspires.ftc.teamcode;
 public class Position {
     //Using protected branches to access both branches PUBLIC or PRIVATE
     //USE DOUBLES TO STORE FRACTIONAL NUMBERS IF BEING WRITTEN, OTHERWISE, IT CAN ONLY STORE AT WHOLE NUMBERS
-     private double rotation; //Stores fractional numbers into a rotation class
+    private double rotation; //Stores fractional numbers into a rotation class
 
-     protected String name;
-     protected double x; //Stores the Y value in the robot's position on the field
-     protected double y; //Stores the Y value in the robot's position on the field
-     protected Navigation.Action action = Navigation.Action.NONE;
-     //Add constructors if needed
-     protected double strafePower = 1.0; //Strafe is set to FULL POWER. Number can range ONLY from -1 to 1
-     protected double rotatePower = 1.0; //Rotation is set to FULL POWER.
+    protected String name;
+    protected double x; //Stores the Y value in the robot's position on the field
+    protected double y; //Stores the Y value in the robot's position on the field
+    protected Navigation.Action action = Navigation.Action.NONE;
+    //Add constructors if needed
+    protected double strafePower = 1.0; //Strafe is set to FULL POWER. Number can range ONLY from -1 to 1
+    protected double rotatePower = 1.0; //Rotation is set to FULL POWER.
 
-     Position() { //This sets all the presets on the field as a blank or a 0.
-        x = 0.0; //Sets X as a value of 0 on the field (where it starts)
-        y = 0.0; //Sets y as a value of 0 on the field (where it starts)
-        name = ""; //Name of the position (set as blank, used for debugging)
-        setRotation(0.0); //Rotation is set as a value of 0 on the field
+     /** creates a point with the coordinats of (0,0), no name, a rotation of 0. and powersettings of 1
+      *
+      */
+    public Position() { //This sets all the presets on the field as a blank or a 0.
+       x = 0.0; //Sets X as a value of 0 on the field (where it starts)
+       y = 0.0; //Sets y as a value of 0 on the field (where it starts)
+       name = ""; //Name of the position (set as blank, used for debugging)
+       setRotation(0.0); //Rotation is set as a value of 0 on the field
+    }
 
-     }
-
-    Position(double x, double y, double theta, String name) {
+     /**Creates a point at a defined position and rotation with a name
+      * @param x the x coordinate of the position
+      * @param y the y coordinate of the position
+      * @param theta the direction the robot should face at theis posotion
+      * @param name the name of this positon  (for debugging)
+      */
+    public Position(double x, double y, double theta, String name) {
         this.x = x; //Calls X from the Protected X (in this code/class)
         this.y = y; //Calls Y from the Protected Y (in this code/class)
         this.name = name; //Calls the String Name (in this code/class)
-        setRotation(theta); //
+        setRotation(theta); //Set's the rotation as the unknown angle
     }
 
-    Position(double x, String name, Navigation.Action action, double strafePower, double strafePower, double roatePower, double theta) {
+     /**Creates a point that has a definited position, rotation, name, strafePower, and rotatePower
+      * @param x the x coordinate of the position
+      * @param y the y coordinate of the position
+      * @param theta the direction the robot should face at theis posotion
+      * @param name the name of this positon  (for debugging)
+      * @param strafePower the amount of power on the wheels when the robots strafe (speeds ranges from 0 to 1)
+      * @param rotatePower the amount of power on the wheels when the robots spins (speeds ranges from 0 to 1)
+      * @param action the action that should be perfomed when the robot reaches this position
+      */
+    public Position(double x, double y, String name, Navigation.Action action, double strafePower, double rotatePower, double theta) {
+        this.x = x;
+        this.y = y;
+        this.name = name;
+        setRotation(theta);
+        this.strafePower = strafePower; //Sets strafePower from Line 23
+        this.rotatePower = rotatePower; //Sets rotatePower form Line 24
 
     }
 
+    //Sets all of the variables as a public function.
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
+    public double getX() {
+      return x;
+    }
+    public double getY(){
+      return y;
+    }
+    public double getStrafePower(){
+        return strafePower;
+    }
+    public double getRotatePower(){
+        return rotatePower;
+    }
+    public double getRotation(){
+        return rotation;
+    }
+    public Navigation.Action getAction(){
+        return action;
+    }
+    public String getName(){
+        return name;
+    }
 
+    /**
+    why are you resetting a Point? that is not the Point of this class. THIS LITERALLY MAKES NO SENSE (lines 98 - 102
+    */
+    public void reset(){
+        x=0; //setX(0.0); + setY(0.0);
+        y=0;
+        setRotation(0);
+    }
+
+    public static Position add(Position a, Position b) {
+       return new Position(a.getX() + b.getX(), a.getY() + b.getY(), (a.getRotation() + b.getRotation()) % (2 * Math.PI),"");
+    }
+    /**sets the rotarion of the position
+    *p@param r theangle to set to
+    *@return the instace of this class. (useful for chaining)
+    */
+    public Position setRotation(double r) {
+      this.rotation = r;
+      return this;
+    }
 }
+//Programmers for position.java: Tyler M, Aria B, Manan C.
+//Alumni Help: Stephen D.
