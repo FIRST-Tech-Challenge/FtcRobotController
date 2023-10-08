0bcf10d61d14a06d93cf71b556895ad7db9a10cc
k8liang <k8lcool@gmail.com>
2023-10-07 19:27:09 -0700

    DRIVE IN TEST TELEOP CLASS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testTeleOp.java
@@ -18,6 +18,16 @@ public class testTeleOp extends LinearOpMode {
         flippyThingy.setDirection(DcMotorSimple.Direction.FORWARD);
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        DcMotor fLeft = hardwareMap.dcMotor.get("fLeft");
+        DcMotor bLeft = hardwareMap.dcMotor.get("bLeft");
+        DcMotor fRight = hardwareMap.dcMotor.get("fRight");
+        DcMotor bRight = hardwareMap.dcMotor.get("bRight");
+
+        fLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        bLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        fRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        bRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
         waitForStart();
 
         while (opModeIsActive()) {
@@ -38,6 +48,43 @@ public class testTeleOp extends LinearOpMode {
             } else {
                 flippyThingy.setPower(0);
             }
+
+            double forwardMovement = -gamepad1.left_stick_y;
+            double turnMovement = gamepad1.right_stick_x;
+            double mecanumMovement = -gamepad1.left_stick_x;
+
+            double fLeftPower = forwardMovement + turnMovement + mecanumMovement;
+            double bLeftPower = forwardMovement + turnMovement - mecanumMovement;
+            double fRightPower = forwardMovement - turnMovement - mecanumMovement;
+            double bRightPower = forwardMovement - turnMovement + mecanumMovement;
+
+            double maxPower = maxAbsValueDouble(fLeftPower, bLeftPower, fRightPower, bRightPower);
+
+            if (Math.abs(maxPower) > 1) {
+                double scale = Math.abs(maxPower);
+
+                fLeftPower /= scale;
+                bLeftPower /= scale;
+                fRightPower /= scale;
+                bRightPower /= scale;
+            }
+
+            fLeft.setPower(fLeftPower);
+            bLeft.setPower(bLeftPower);
+            fRight.setPower(fRightPower);
+            bLeft.setPower(bRightPower);
         }
+    }
+
+    private double maxAbsValueDouble(double a, double... others) {
+        double max = a;
+
+        for (double next : others) {
+            if (Math.abs(next) > Math.abs(max)) {
+                max = next;
+            }
+        }
+
+        return max;
     }
 }
