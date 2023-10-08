ff45a5fd43db935456f0ac5903572234cdf0c143
dnel2 <dnelson@portledge.org>
2023-10-08 19:24:42 -0400

    init commit... added two packages and the new .java file.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/botSetup/RoboSetup.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.botSetup;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * This is the code for setting up my Centerstage robot which consists of
+ *  a small mecanum chassis. This will be version of mecanum drive. The class
+ *  will review other version using mathematical equations to achieve
+ *  more elegant coding.
+ *
+ *  Hardware Setup
+ *
+ *  ---- Control Hub ---
+ *  MOTORS
+ *  Port 0 - backLeft
+ *  Port 1 - frontLeft
+ *  Port 2 - frontRight
+ *  Port 3 - backRight
+ *
+ *  SERVOS
+ *  Port 0 - TBD
+ *
+ *  SENSORS
+ *  I2C Expansion
+ *  Port 0 - rightDist (Distance Sensor)
+ *  Port 1 - backDist
+ *
+ * --- Expansion Hub ----
+ * Port 0 - Motor TBD
+ * Port 3 - Motor TBD
+ *
+ * @author DNel2
+ * @version 10/06/2023
+ *
+ */
+public class RoboSetup {
+
+
+}
