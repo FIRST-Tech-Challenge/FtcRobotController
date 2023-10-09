4cc82e8b6734a3f1171716547a8a52301c1342d0
Oscar Yeung <111371967+Oscar3334@users.noreply.github.com>
2023-10-09 14:54:53 -0400

    Static class config didn't really need to exist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -11,92 +11,84 @@ import java.util.Objects;
  *  Also has a "desired state" for mechanism driving.
  */
 public class Robot {
-    
     /**
-     * Stores config info on robot 
+     * Stores motor configs
      */
-    static class Config {
+    public enum MotorConfigs {
+        //drive motors
+        FRONT_LEFT  ("front_left", DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
+        FRONT_RIGHT ("front_right",DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
+        REAR_LEFT   ("rear_left",  DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
+        REAR_RIGHT  ("rear_right", DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
+        
+        //linear slides motors
+        ONE         ("slides_motor_1",         DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE),
+        TWO         ("slides_motor_2",         DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.BRAKE),
+        SECONDARY   ("secondary_slides_motor", DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE);
+        
+        private final String name;
+        private final DcMotor.Direction direction;
+        private final DcMotor.ZeroPowerBehavior behavior
         
         /**
-         * Stores motor configs
+         * Creates motor configuration
+         * @param name the name of component
+         * @param direction the direction of the motor
+         * @param behavior the zeroPowerBehavior of the motor
          */
-        public enum MotorConfigs {
-            //drive motors
-            FRONT_LEFT  ("front_left", DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
-            FRONT_RIGHT ("front_right",DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
-            REAR_LEFT   ("rear_left",  DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
-            REAR_RIGHT  ("rear_right", DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
-            
-            //linear slides motors
-            ONE         ("slides_motor_1",         DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE),
-            TWO         ("slides_motor_2",         DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.BRAKE),
-            SECONDARY   ("secondary_slides_motor", DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE);
-            
-            private final String name;
-            private final DcMotor.Direction direction;
-            private final DcMotor.ZeroPowerBehavior behavior
-            
-            /**
-             * Creates motor configuration
-             * @param name the name of component
-             * @param direction the direction of the motor
-             * @param behavior the zeroPowerBehavior of the motor
-             */
-            private MotorConfigs(final String name, final DcMotor.Direction direction, final DcMotor.ZeroPowerBehavior behavior) {
-                this.name      = name;
-                this.direction = direction;
-                this.behavior  = behavior;
-            }
-            
-            public String                    getName()              { return name; }
-            public DcMotor.Direction         getDirection()         { return direction; }
-            public DcMotor.ZeroPowerBehavior getZeroPowerBehavior() { return behavior; }
-        }
-
-        /**
-         * Stores motor configs
-         */
-        public enum ServoConfigs { // (name, direction) // not sure if you need direction
-            //not sure what to put here yet
-            
-            private final String name;
-            private final Servo.Direction direction;
-
-            /**
-             * Creates servo configuration
-             * @param name the name of the component
-             * @param direction the direction of the servo
-             */
-            private ServoConfigs(final String name, final Servo.Direction direction) {
-                this.name      = name;
-                this.direction = direction;
-            }
-            
-            public String          getName()      { return name; }
-            public Servo.Direction getDirection() {return direction; }
+        private MotorConfigs(final String name, final DcMotor.Direction direction, final DcMotor.ZeroPowerBehavior behavior) {
+            this.name      = name;
+            this.direction = direction;
+            this.behavior  = behavior;
         }
         
-        /**
-         * Stores switch configs
-         */
-        public enum SwitchConfigs { // (name)
-            SLIDES_LIMIT("slides_limit");
-
-            /**
-             * Creates switch configuration
-             * @param name the name of the component
-             */
-            private final String name;
-            private SwitchConfigs(final String name) {
-                this.name = name;
-            }
-            
-            public String getName() { return name; }
-        }
-        /*more enums to come*/
+        public String                    getName()              { return name; }
+        public DcMotor.Direction         getDirection()         { return direction; }
+        public DcMotor.ZeroPowerBehavior getZeroPowerBehavior() { return behavior; }
     }
 
-    //enum spam
+    /**
+     * Stores servo configs
+     */
+    public enum ServoConfigs {
+        //not sure what to put here yet
+        
+        private final String name;
+        private final Servo.Direction direction;
+
+        /**
+         * Creates servo configuration
+         * @param name the name of the component
+         * @param direction the direction of the servo
+         */
+        private ServoConfigs(final String name, final Servo.Direction direction) {
+            this.name      = name;
+            this.direction = direction;
+        }
+        
+        public String          getName()      { return name; }
+        public Servo.Direction getDirection() { return direction; }
+    }
+    
+    /**
+     * Stores switch configs
+     */
+    public enum SwitchConfigs { // (name)
+        SLIDES_LIMIT("slides_limit");
+
+        /**
+         * Creates switch configuration
+         * @param name the name of the component
+         */
+        private final String name;
+        private SwitchConfigs(final String name) {
+            this.name = name;
+        }
+        
+        public String getName() { return name; }
+    }
+
+    //Enums for state
     public enum SlidesState            {RETRACTED, LOW, MEDIUM, HIGH, UNREADY, MOVE_UP, MOVE_DOWN, STOPPED, FIRST_STACK_CONE, SECOND_STACK_CONE}
     public enum ParkingPosition        {INSIDE, MIDDLE, OUTSIDE}
     
@@ -144,7 +136,7 @@ public class Robot {
         positionManager  = new PositionManager(hardwareMap, telemetry);
         driveMotors      = new DriveMotors(hardwareMap);
         slidesMotors     = new SlideMotors(hardwareMap);
-        slidesLimitSwitch= hardwareMap.get(TouchSensor.class, Robot.Config.SwitchConfigs.SLIDES_LIMIT.getName());
+        slidesLimitSwitch= hardwareMap.get(TouchSensor.class, Robot.SwitchConfigs.SLIDES_LIMIT.getName());
         
         //probably some servos 
         
@@ -163,7 +155,7 @@ public class Robot {
 }
 
 /**
- * container for motors
+ * container template for motors
  */
 abstract class Motors { // this might be a bad abstraction
     /**
@@ -179,11 +171,12 @@ abstract class Motors { // this might be a bad abstraction
      * @param motor the motor acted upon
      * @param config the desired config
      */
-    static void setMotorToConfig(DcMotor motor, Robot.Config.MotorConfigs config) {
+    static void setMotorToConfig(DcMotor motor, Robot.MotorConfigs config) {
         Objects.requireNonNull(motor).setDirection(config.getDirection());
         Objects.requireNonNull(motor).setZeroPowerBehavior(config.getZeroPowerBehavior());
     }
 }
+
 /**
  * stores drive motors
  */
@@ -194,22 +187,22 @@ class DriveMotors extends Motors {
      * @param hardwareMap the hardwareMap used
      */
     public DriveMotors(HardwareMap hardwareMap) {
-        frontLeft = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.FRONT_LEFT.getName());
-        frontRight= hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.FRONT_RIGHT.getName());
-        rearLeft  = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.REAR_LEFT.getName());
-        rearRight = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.REAR_RIGHT.getName());
+        frontLeft = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.FRONT_LEFT.getName());
+        frontRight= hardwareMap.get(DcMotor.class, Robot.MotorConfigs.FRONT_RIGHT.getName());
+        rearLeft  = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.REAR_LEFT.getName());
+        rearRight = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.REAR_RIGHT.getName());
         //pass dcmotor reference and set to config
-        setMotorToConfig(frontLeft, Robot.Config.MotorConfigs.FRONT_LEFT);
-        setMotorToConfig(frontRight,Robot.Config.MotorConfigs.FRONT_RIGHT);
-        setMotorToConfig(rearLeft,  Robot.Config.MotorConfigs.REAR_LEFT);
-        setMotorToConfig(rearRight, Robot.Config.MotorConfigs.REAR_RIGHT);
+        setMotorToConfig(frontLeft, Robot.MotorConfigs.FRONT_LEFT);
+        setMotorToConfig(frontRight,Robot.MotorConfigs.FRONT_RIGHT);
+        setMotorToConfig(rearLeft,  Robot.MotorConfigs.REAR_LEFT);
+        setMotorToConfig(rearRight, Robot.MotorConfigs.REAR_RIGHT);
     }
     /**
      * Sets a motor's config to config and resets motor encoder
      * @param motor the motor acted upon
      * @param config the desired config
      */
-    static void setMotorToConfig(DcMotor motor, Robot.Config.MotorConfigs config) {
+    static void setMotorToConfig(DcMotor motor, Robot.MotorConfigs config) {
         super.setMotorToConfig(motor, config);
         resetMotor(motor);
     }
@@ -224,12 +217,12 @@ class SlidesMotors extends Motors {
      * @param hardwareMap the hardwareMap used
      */
     public DriveMotors(HardwareMap hardwareMap) {
-        one       = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.ONE.getName());
-        two       = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.TWO.getName());
-        secondary = hardwareMap.get(DcMotor.class, Robot.Config.MotorConfigs.SECONDARY.getName());
+        one       = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.ONE.getName());
+        two       = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.TWO.getName());
+        secondary = hardwareMap.get(DcMotor.class, Robot.MotorConfigs.SECONDARY.getName());
         
-        setMotorToConfig(one, Robot.Config.MotorConfigs.ONE);
-        setMotorToConfig(two, Robot.Config.MotorConfigs.TWO);
-        setMotorToConfig(secondary, Robot.Config.MotorConfigs.SECONDARY);
+        setMotorToConfig(one, Robot.MotorConfigs.ONE);
+        setMotorToConfig(two, Robot.MotorConfigs.TWO);
+        setMotorToConfig(secondary, Robot.MotorConfigs.SECONDARY);
     }
 }
