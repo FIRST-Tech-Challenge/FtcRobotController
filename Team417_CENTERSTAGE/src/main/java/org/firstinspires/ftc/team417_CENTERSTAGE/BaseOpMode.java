ca8c754bebea48cd07d7e82d41641c30db895ada
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-10 21:10:55 -0700

    Started class BaseOpMode and void hardwareInitialize()

---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseOpMode.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+abstract class BaseOpMode extends LinearOpMode {
+    //Declares drive-motors
+    public DcMotor FR;
+    public DcMotor FL;
+    public DcMotor BR;
+    public DcMotor BL;
+
+    //Declares IMU
+    public BNO055IMU imu;
+
+    //Initializes motors, servos, and sensors
+    public void initializeHardware() {
+        FL = hardwareMap.get(DcMotor.class, "FLMotor");
+        FR = hardwareMap.get(DcMotor.class, "FRMotor");
+        BL = hardwareMap.get(DcMotor.class, "BLMotor");
+        BR = hardwareMap.get(DcMotor.class, "BRMotor");
+
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        FL.setDirection(DcMotor.Direction.FORWARD);
+        FR.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.FORWARD);
+        BR.setDirection(DcMotor.Direction.REVERSE);
+
+        // Sets up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample op-mode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+
+        // Retrieves and initializes the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // Waits so the imu can process
+        sleep(2000);
+    }
+}
