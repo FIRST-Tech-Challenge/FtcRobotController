1c2d06f9866f857b0b49aad0ded9d9a39d316310
notjaisammpath <114313475+notjaisammpath@users.noreply.github.com>
2023-10-12 17:12:39 -0500

    Update Intake.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/csbot/subsystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/csbot/subsystem/Intake.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.teamcode.robots.csbot.util.Joint;
+
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -15,33 +17,47 @@ public class Intake implements Subsystem {
     //CONSTANTS
     HardwareMap hardwareMap;
     Robot_fromScratch robot;
-    Servo diverter, beaterBarAngleController;
+    Servo diverter;
+    Joint beaterBarAngleController;
     DcMotorEx beaterBar;
     public static double BEATER_BAR_INTAKE_VELOCITY = 1.0;
     public static double BEATER_BAR_EJECT_VELOCITY = -1.0;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_HOME;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_TICKS_PER_DEGREE;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_MIN_DEGREES;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_MAX_DEGREES;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_START_ANGLE;
+    public static double BEATER_BAR_ANGLE_CONTROLLER_SPEED;
+    public static double BEATER_BAR_FOLD_ANGLE;
+    public static double BEATER_BAR_GROUND_ANGLE;
 
 
     public enum Articulation {
-        INTAKE,
+        GROUND_INTAKE,
+        STACK_INTAKE,
         EJECT,
         OFF,
         FOLD,
+        MANUAL,
     }
 
 
     //LIVE STATES
     public Articulation articulation;
+    public static int numPixelsInStack = 6;
+    public static double angleToStack;
+    public static double beaterBarTargetAngle;
 
 
     public Intake(HardwareMap hardwareMap, Robot_fromScratch robot) {
             this.hardwareMap = hardwareMap;
             this.robot = robot;
-            articulation = Articulation.OFF;
+            articulation = Articulation.FOLD;
 
             diverter = hardwareMap.get(Servo.class, "diverter");
-            beaterBarAngleController = hardwareMap.get(Servo.class, "beaterBarAngleController");
             beaterBar = hardwareMap.get(DcMotorEx.class, "beaterBar");
 
+            beaterBarAngleController = new Joint(hardwareMap, "beaterBarAngleController", false, BEATER_BAR_ANGLE_CONTROLLER_HOME, BEATER_BAR_ANGLE_CONTROLLER_TICKS_PER_DEGREE, BEATER_BAR_ANGLE_CONTROLLER_MIN_DEGREES, BEATER_BAR_ANGLE_CONTROLLER_MAX_DEGREES, BEATER_BAR_ANGLE_CONTROLLER_START_ANGLE, BEATER_BAR_ANGLE_CONTROLLER_SPEED);
             beaterBar.setMotorEnable();
             beaterBar.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
@@ -50,16 +66,29 @@ public class Intake implements Subsystem {
     @Override
     public void update(Canvas fieldOverlay) {
         switch (articulation) {
+            case MANUAL:
+                break;
             case OFF:
                 beaterBar.setPower(0);
                 break;
-            case INTAKE:
+            case GROUND_INTAKE:
                 beaterBar.setVelocity(BEATER_BAR_INTAKE_VELOCITY);
+                beaterBarTargetAngle = BEATER_BAR_GROUND_ANGLE;
                 break;
             case EJECT:
                 beaterBar.setVelocity(BEATER_BAR_EJECT_VELOCITY);
                 break;
+            case FOLD:
+                beaterBar.setPower(0);
+                beaterBarAngleController.setTargetAngle(BEATER_BAR_FOLD_ANGLE);
+                break;
+            case STACK_INTAKE:
+                beaterBar.setPower(BEATER_BAR_INTAKE_VELOCITY);
+                beaterBarTargetAngle = angleToStack;
+                break;
         }
+        beaterBarAngleController.setTargetAngle(beaterBarTargetAngle);
+        beaterBarAngleController.update();
     }
 
     public Articulation articulate(Articulation target) {
