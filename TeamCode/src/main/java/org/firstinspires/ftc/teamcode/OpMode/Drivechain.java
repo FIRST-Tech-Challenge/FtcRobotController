1a27f6dc17cdc09612651f5c77ceddfaf00ebce0
jace-wong <k_banh@icloud.com>
2023-10-11 21:49:16 -0600

    tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Drivechain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/Drivechain.java
@@ -15,4 +15,22 @@ public class Drivechain {
         backLeft = backLeftWheel;
         backRight = backRightWheel;
     }
+
+    public static void drive(double leftStick, double rightStick) {
+        double y = -leftStick; // Remember, this is reversed!
+        double x = rightStick * 1.1; // Counteract imperfect strafing
+        double rx = rightStick;
+        // Denominator is the largest motor power (absolute value) or 1
+        // This ensures all the powers maintain the same ratio, but only when
+        // at least one is out of the range [-1, 1]
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (y + x + rx) * 0.65/ denominator;
+        double backLeftPower = (y - x + rx) * 0.65/ denominator;
+        double frontRightPower = (y - x - rx) * 0.65/ denominator;
+        double backRightPower = (y + x - rx)* 0.65/ denominator;
+        frontLeft.setPower(-frontLeftPower*0.6);
+        backLeft.setPower(-backLeftPower*0.6);
+        frontRight.setPower(-frontRightPower*0.6);
+        backRight.setPower(-backRightPower*0.6);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/MainOp.java
@@ -15,6 +15,7 @@ public class MainOp extends LinearOpMode {
 
         //Initialization
 
+        /*
         //Drivechain Motors
         DcMotor wheelFrontLeft = hardwareMap.dcMotor.get("wheelFrontLeft");
         DcMotor wheelFrontRight = hardwareMap.dcMotor.get("wheelFrontRight");
@@ -65,11 +66,55 @@ public class MainOp extends LinearOpMode {
         Drivechain.driveChainInit(wheelFrontLeft, wheelFrontRight, wheelBackLeft, wheelBackRight);
         IntakeGekkoWheelServos.gekkoWheelInit(leftIntakeServo, rightIntakeServo);
 
+
+
+         */
+
+        DcMotor leftLinSlide = hardwareMap.dcMotor.get("leftLinSlide");
+        DcMotor rightLinSlide = hardwareMap.dcMotor.get("rightLinSlide");
+        leftLinSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightLinSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftLinSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        leftLinSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightLinSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightLinSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        //DcMotor wheelFrontLeft = hardwareMap.dcMotor.get("wheelFrontLeft");
+        //DcMotor wheelFrontRight = hardwareMap.dcMotor.get("wheelFrontRight");
+        //DcMotor wheelBackLeft = hardwareMap.dcMotor.get("wheelBackLeft");
+        //DcMotor wheelBackRight = hardwareMap.dcMotor.get("wheelBackRight");
+
+        //Drivechain.driveChainInit(wheelFrontLeft, wheelFrontRight, wheelBackLeft, wheelBackRight);
+
         //OpMode
         waitForStart();
         if(isStopRequested()) return;
         while(opModeIsActive()) {
 
+            //Drivechain.drive(gamepad1.left_stick_y, gamepad2.right_stick_x);
+
+
+
+            if(gamepad2.dpad_up) {
+                leftLinSlide.setPower(0.5);
+                rightLinSlide.setPower(-0.5);
+                telemetry.addData("leftLin", leftLinSlide.getCurrentPosition());
+                telemetry.addData("rightLin", rightLinSlide.getCurrentPosition());
+                telemetry.update();
+            } else if(gamepad2.dpad_down){
+                leftLinSlide.setPower(-0.5);
+                rightLinSlide.setPower(0.5);
+                telemetry.addData("leftLin", leftLinSlide.getCurrentPosition());
+                telemetry.addData("rightLin", rightLinSlide.getCurrentPosition());
+                telemetry.update();
+            } else {
+                leftLinSlide.setPower(0);
+                rightLinSlide.setPower(0);
+                telemetry.addData("leftLin", leftLinSlide.getCurrentPosition());
+                telemetry.addData("rightLin", rightLinSlide.getCurrentPosition());
+                telemetry.update();
+            }
         }
 
 
