099a5f771ad51db2132ce9558ffa33e83cac1853
RudraP-11285 <rudypatel16@gmail.com>
2023-10-11 17:33:29 -0700

    Update TensorFlowObjectDetection.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/TensorFlowObjectDetection.java
@@ -29,6 +29,7 @@ package org.firstinspires.ftc.teamcode.autonomous;
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -47,7 +48,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "TensorFlow Object Detection", group = "Autonomous")
+@Autonomous(name = "TensorFlow Object Detection", group = "Autonomous")
 
 public class TensorFlowObjectDetection extends LinearOpMode {
 
