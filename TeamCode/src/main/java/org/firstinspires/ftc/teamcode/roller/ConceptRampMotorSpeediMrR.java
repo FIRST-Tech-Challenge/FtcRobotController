567f529180cca2a99074de34451650ae0648c213
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-10-13 12:26:29 -0500

    WORKING DRIVE CODE!!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roller/ConceptRampMotorSpeediMrR.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roller/ConceptRampMotorSpeediMrR.java
@@ -46,7 +46,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
-@TeleOp(name = "Concept: Ramp Motor Speed", group = "Concept")
+@TeleOp(name = "Concept: Ramp Motor Speed MrR", group = "Concept")
 @Disabled
 public class ConceptRampMotorSpeediMrR extends LinearOpMode {
 
