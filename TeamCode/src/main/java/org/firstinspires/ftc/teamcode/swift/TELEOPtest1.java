553652c0da006a1da3749911d347950c5f67c517
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-10-13 11:10:41 -0500

    working drone launch code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest1.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.swift;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class TELEOPtest1 extends LinearOpMode{
+    private Servo launcherServo = null;
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        launcherServo = hardwareMap.get(Servo.class, "launcherServo");
+
+        while (opModeIsActive()) {
+            // check to see if we need to move the servo.
+            if(gamepad1.left_bumper) {
+                // move to 0 degrees.
+                launcherServo.setPosition(0.5);
+            } else if (gamepad1.right_bumper || gamepad1.left_bumper) {
+                // move to 90 degrees.
+                launcherServo.setPosition(1);
+            }
+            telemetry.addData("Servo Position", launcherServo.getPosition());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+
+
+
+        telemetry.update();
+    }
+}
