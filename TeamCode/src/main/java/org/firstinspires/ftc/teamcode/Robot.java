71020fc5eefc7cc3d7b0aaf2df8c0e1f96a2d948
6460 team meber <6460@gmail.com>
2023-10-13 17:19:57 -0400

    Moved Motors initalization function into main Robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -19,9 +19,7 @@ public class Robot {
         REAR_RIGHT  ("rear_right", DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.FLOAT),
         
         //linear slides motors
-        ONE         ("slides_motor_1",         DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE),
-        TWO         ("slides_motor_2",         DcMotor.Direction.REVERSE, DcMotor.ZeroPowerBehavior.BRAKE),
-        SECONDARY   ("secondary_slides_motor", DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE);
+        ONE         ("slides_motor_1",         DcMotor.Direction.FORWARD, DcMotor.ZeroPowerBehavior.BRAKE);
         
         private final String name;
         private final DcMotor.Direction direction;
@@ -49,7 +47,7 @@ public class Robot {
          * @param hardwareMap hardwareMap
          * @return Motor from config
          */
-        public DcMotor initalize(Robot.MotorConfigs config, HardwareMap hardwareMap) {
+        public static DcMotor initialize(Robot.MotorConfigs config, HardwareMap hardwareMap) {
             DcMotor motor = hardwareMap.get(DcMotor.class, config.getName());
             motor.setDirection(config.getDirection());
             motor.setZeroPowerBehavior(config.getZeroPowerBehavior());
@@ -62,9 +60,12 @@ public class Robot {
      */
     public enum ServoConfigs {
         //not sure what to put here yet
-        
-        private final String name;
-        private final Servo.Direction direction;
+
+        PLANE_SPRING("plane_spring", Servo.Direction.FORWARD),
+        COMPARTMENT_LEFT("compartment_left", Servo.Direction.FORWARD),
+        COMPARTMENT_RIGHT("compartment_right", Servo.Direction.FORWARD);
+        final String name;
+        final Servo.Direction direction;
 
         /**
          * Creates servo configuration
@@ -85,9 +86,9 @@ public class Robot {
          * @param hardwareMap hardwareMap
          * @return the servo
          */
-        public Servo initalize(Robot.ServoConfigs config, HardwareMap hardwareMap) {
+        public static Servo initialize(Robot.ServoConfigs config, HardwareMap hardwareMap) {
             Servo servo = hardwareMap.get(Servo.class, config.getName());
-            motor.setDirection(config.getDirection());
+            servo.setDirection(config.getDirection());
             return servo;
         }
     }
@@ -95,61 +96,50 @@ public class Robot {
     /**
      * Stores switch configs
      */
-    public enum SwitchConfigs {
-        SLIDES_LIMIT("slides_limit");
-
-        /**
-         * Creates switch configuration
-         * @param name the name of the component
-         */
-        private final String name;
-        private SwitchConfigs(final String name) {
-            this.name = name;
-        }
-        
-        public String getName() { return name; }
-
-        /**
-         * Retrieves switch
-         * @param config the desired switch
-         * @param hardwareMap hardwareMap
-         * @return the switch
-         */
-        public TouchSensor initalize(Robot.SwitchConfigs config, HardwareMap hardwareMap) {
-            TouchSensor sensor = hardwareMap.get(TouchSensor.class, config.getName());
-            return sensor;
-        }
-    }
+//    public enum SwitchConfigs {
+//        SLIDES_LIMIT("slides_limit");
+//
+//        /**
+//         * Creates switch configuration
+//         * @param name the name of the component
+//         */
+//        private final String name;
+//        private SwitchConfigs(final String name) {
+//            this.name = name;
+//        }
+//
+//        public String getName() { return name; }
+//
+//        /**
+//         * Retrieves switch
+//         * @param config the desired switch
+//         * @param hardwareMap hardwareMap
+//         * @return the switch
+//         */
+//        public static TouchSensor initialize(Robot.SwitchConfigs config, HardwareMap hardwareMap) {
+//            TouchSensor sensor = hardwareMap.get(TouchSensor.class, config.getName());
+//            return sensor;
+//        }
+//    }
 
     //Enums for state
     public enum SlidesState            {RETRACTED, LOW, MEDIUM, HIGH, UNREADY, MOVE_UP, MOVE_DOWN, STOPPED, FIRST_STACK_CONE, SECOND_STACK_CONE}
     public enum ParkingPosition        {INSIDE, MIDDLE, OUTSIDE}
-    
-    //no clue what secondary is
-    public enum SecondarySlidesState   {RETRACTED, EXTENDED, PLACE_CONE, SWEEP_EXTENDED, INITIAL_EXTENDED, FINAL_RETRACTED}
-    public enum SecondarySystemStatus  {ON, OFF}
-    public enum SecondaryConeRetrieved {DONE, NOT_DONE}
-    
+
     // State
     public static SlidesState     desiredSlidesState = SlidesState.UNREADY;
-    public SecondarySlidesState   desiredSecondarySlidePosition;
 
-    public SecondarySystemStatus  secondarySystemStatus;
-    public SecondaryConeRetrieved secondaryConeRetrieved;
-
-    public boolean                previousSlidesLimitSwitchState = false;
-    public SecondarySlidesState   previousDesiredSecondarySlidePosition;
-    public SecondarySystemStatus  previousSecondarySystemStatus;
 
     enum MovementMode {NORMAL, FINE, ULTRA_FINE}
     MovementMode movementMode = MovementMode.NORMAL;
     boolean wheelSpeedAdjustment = false;
     
     // Hardware
-    public DriveMotors driveMotors;
-    public SlidesMotors slidesMotors;
+    public DcMotor slide;
+    public DcMotor frontLeft, frontRight, rearLeft, rearRight;
+    public Servo plane_spring, compartment_left, compartment_right;
 
-    public TouchSensor slidesLimitSwitch;
+//    public TouchSensor slidesLimitSwitch;
     //servos
     //idk what else
 
@@ -167,73 +157,36 @@ public class Robot {
         this.telemetry   = telemetry;
         this.elapsedTime = elapsedTime;
         positionManager  = new PositionManager(hardwareMap, telemetry);
-        driveMotors      = new DriveMotors(hardwareMap);
-        slidesMotors     = new SlidesMotors(hardwareMap);
-        slidesLimitSwitch= Robot.SwitchConfigs.initalize(Robot.SwitchConfigs.SLIDES_LIMIT, hardwareMap);
-        
-        //probably some servos 
-        
-        if (desiredSlidesState == SlidesState.UNREADY) { //if the slides have yet to be initialised then reset the encoders for the slides and set the slide state to retracted
-            this.telemetry.addData("desired string state", desiredSlidesState.toString());
-            Motors.resetEncoder(one);
-            Motors.resetEncoder(two);
-            Motors.resetEncoder(secondary);
-            desiredSlidesState = SlidesState.RETRACTED;
-        }
-    }
-    
-    public Position getPosition() {
-        return positionManager.position;
-    }
-}
-
-/**
- * Container template for motors
- */
-abstract class Motors { // this might be a bad abstraction
-    /**
-     * Resets a motor's encoder
-     * @param motor the motor acted upon
-     */
-    public static void resetEncoder(DcMotor motor) {
-        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-}
-
-/**
- * stores drive motors
- */
-class DriveMotors extends Motors {
-    public DcMotor frontLeft, frontRight, rearLeft, rearRight;
-    /**
-     * Takes hardwareMap and gets drive motors from it
-     * @param hardwareMap the hardwareMap used
-     */
-    public DriveMotors(HardwareMap hardwareMap) {
-        frontLeft = Robot.MotorConfigs.initalize(Robot.MotorConfigs.FRONT_LEFT, hardwareMap);
-        frontRight= Robot.MotorConfigs.initalize(Robot.MotorConfigs.FRONT_RIGHT, hardwareMap);
-        rearLeft  = Robot.MotorConfigs.initalize(Robot.MotorConfigs.REAR_LEFT, hardwareMap);
-        rearRight = Robot.MotorConfigs.initalize(Robot.MotorConfigs.REAR_RIGHT, hardwareMap);
+        frontLeft = Robot.MotorConfigs.initialize(Robot.MotorConfigs.FRONT_LEFT, hardwareMap);
+        frontRight= Robot.MotorConfigs.initialize(Robot.MotorConfigs.FRONT_RIGHT, hardwareMap);
+        rearLeft  = Robot.MotorConfigs.initialize(Robot.MotorConfigs.REAR_LEFT, hardwareMap);
+        rearRight = Robot.MotorConfigs.initialize(Robot.MotorConfigs.REAR_RIGHT, hardwareMap);
 
         resetEncoder(frontLeft);
         resetEncoder(frontRight);
         resetEncoder(rearLeft);
         resetEncoder(rearRight);
+
+        slide            = Robot.MotorConfigs.initialize(Robot.MotorConfigs.ONE, hardwareMap);
+        plane_spring     = Robot.ServoConfigs.initialize(ServoConfigs.PLANE_SPRING, hardwareMap);
+        compartment_left     = Robot.ServoConfigs.initialize(ServoConfigs.COMPARTMENT_LEFT, hardwareMap);
+        compartment_right     = Robot.ServoConfigs.initialize(ServoConfigs.COMPARTMENT_RIGHT, hardwareMap);
+//        slidesLimitSwitch= Robot.SwitchConfigs.initialize(Robot.SwitchConfigs.SLIDES_LIMIT, hardwareMap);
+
+        // Set slides state to Retracted
+        if (desiredSlidesState == SlidesState.UNREADY) { //if the slides have yet to be initialised then reset the encoders for the slides and set the slide state to retracted
+            this.telemetry.addData("desired string state", desiredSlidesState.toString());
+            resetEncoder(slide);
+            desiredSlidesState = SlidesState.RETRACTED;
+        }
     }
-}
-/**
- * stores slides motors
- */
-class SlidesMotors extends Motors {
-    public DcMotor one, two, secondary;
-    /**
-     * Gets slides motors from hardware
-     * @param hardwareMap the hardwareMap used
-     */
-    public SlidesMotors(HardwareMap hardwareMap) {
-        one       = Robot.MotorConfigs.initalize(Robot.MotorConfigs.ONE, hardwareMap);
-        two       = Robot.MotorConfigs.initalize(Robot.MotorConfigs.TWO, hardwareMap);
-        secondary = Robot.MotorConfigs.initalize(Robot.MotorConfigs.SECONDARY, hardwareMap);
+
+    public static void resetEncoder(DcMotor motor) {
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+    
+    public Position getPosition() {
+        return positionManager.position;
     }
 }
