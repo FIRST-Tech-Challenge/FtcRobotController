bbfa93316329defc784a4d9f4769351a1c81b043
williamzhou25 <willface2727@gmail.com>
2023-10-13 19:05:49 -0700

    in progress ultras

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/RFUltrasonicTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/RFUltrasonicTest.java
@@ -1,8 +1,25 @@
 package org.firstinspires.ftc.teamcode.Tests;
 
+import static org.firstinspires.ftc.teamcode.Robots.BasicRobot.op;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
+import com.qualcomm.robotcore.hardware.LED;
+
+import org.firstinspires.ftc.teamcode.Components.RFModules.Devices.RFLEDStrip;
+import org.firstinspires.ftc.teamcode.Components.RFModules.Devices.RFUltrasonic;
+
 /**
  * William
  * Program to tune the constants of an RFUltrasonic(x,y,a offset), must have inputable expected obstacle in the form of a linear equation
  */
-public class RFUltrasonicTest {
+public class RFUltrasonicTest extends LinearOpMode {
+
+//    RFLEDStrip ultra = new RFLEDStrip();
+//    LED ultras = new LED();
+//    DigitalChannelController controller =
+//
+    public void runOpMode() {
+//        ultra.
+    }
 }
