07e137031a319eec90353d6bedd32d8037e76d2f
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-10-16 07:54:52 -0500

    Update TELEOPtest1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest1.java
@@ -52,7 +52,6 @@ public class TELEOPtest1 extends LinearOpMode {
             frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
             backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            waitForStart();
 
             if (isStopRequested()) return;
 
@@ -74,89 +73,42 @@ public class TELEOPtest1 extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
-        }
 
 
-        //start Asher
+            //start Asher
 
-        // Define class members
-        DcMotor motor;
-        double power = 0;
-        boolean rampUp = true;
-
-
-        // Connect to motor (Assume standard left wheel)
-        // Change the text in quotes to match any motor name on your robot.
-        motor = hardwareMap.get(DcMotor.class, "left_drive");
-
-        // Wait for the start button
-        telemetry.addData(">", "Press Start to run Motors.");
-        telemetry.update();
-        waitForStart();
-
-        // Ramp motor speeds till stop pressed.
-        while (opModeIsActive()) {
-
-            // Ramp the motors, according to the rampUp variable.
-            if (rampUp) {
-                // Keep stepping up until we hit the max value.
-                power += INCREMENT;
-                if (power >= MAX_FWD) {
-                    power = MAX_FWD;
-                    rampUp = !rampUp;   // Switch ramp direction
-                }
-            } else {
-                // Keep stepping down until we hit the min value.
-                power -= INCREMENT;
-                if (power <= MAX_REV) {
-                    power = MAX_REV;
-                    rampUp = !rampUp;  // Switch ramp direction
-                }
-            }
-
-            // Display the current value
-            telemetry.addData("Motor Power", "%5.2f", power);
-            telemetry.addData(">", "Press Stop to end test.");
-            telemetry.update();
-
-            // Set the motor to the new power and pause;
-            motor.setPower(power);
-            sleep(CYCLE_MS);
-            idle();
-        }
-
-        // Turn off motor and signal done;
-        motor.setPower(0);
-        telemetry.addData(">", "Done");
-        telemetry.update();
+            // Define class members
+            DcMotor motor;
+            double power = 0;
+            boolean rampUp = true;
 
 
-        //Define variables for arm, wrist, and gripper
-        Servo wristServo, left_Gripper, right_Gripper;
-        DcMotor armMotor;
-        //CRServo contServo;
-        float leftY, rightY;
-        double armPosition, gripPosition, contPower;
-        double MIN_POSITION = 0, MAX_POSITION = 1;
 
-        // called when init button is  pressed.
+            //Define variables for arm, wrist, and gripper
+            Servo wristServo, left_Gripper, right_Gripper;
+            DcMotor armMotor;
+            //CRServo contServo;
+            float leftY, rightY;
+            double armPosition, gripPosition, contPower;
+            double MIN_POSITION = 0, MAX_POSITION = 1;
 
-        {
-            armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-            wristServo = hardwareMap.servo.get("Wrist Servo");
-            left_Gripper = hardwareMap.servo.get("Left Gripper");
-            right_Gripper = hardwareMap.servo.get("Right Gripper");
-            //contServo = hardwareMap.crservo.get("cont_servo");
+            // called when init button is  pressed.
 
-            telemetry.addData("Mode", "waiting");
-            telemetry.update();
 
-            // wait for start button.
+                armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+                wristServo = hardwareMap.servo.get("Wrist Servo");
+                left_Gripper = hardwareMap.servo.get("Left Gripper");
+                right_Gripper = hardwareMap.servo.get("Right Gripper");
+                //contServo = hardwareMap.crservo.get("cont_servo");
 
-            waitForStart();
+                telemetry.addData("Mode", "waiting");
+                telemetry.update();
 
-            armPosition = .5;                   // set arm to half way up.
-            gripPosition = MAX_POSITION;        // set grip to full open.
+                // wait for start button.
+
+
+                armPosition = .5;                   // set arm to half way up.
+                gripPosition = MAX_POSITION;        // set grip to full open.
 
 
                 leftY = gamepad2.left_stick_y * -1;
@@ -178,18 +130,18 @@ public class TELEOPtest1 extends LinearOpMode {
                 if (gamepad2.b && armPosition < MAX_POSITION) armPosition += .01;
 
                 // open the gripper on X button if not already at most open position.
-                if (gamepad2.x && gripPosition < MAX_POSITION) gripPosition = gripPosition + .01;
+                if (gamepad2.x && gripPosition < MAX_POSITION) gripPosition = gripPosition + 1;
 
                 // close the gripper on Y button if not already at the closed position.
-                if (gamepad2.y && gripPosition > MIN_POSITION) gripPosition = gripPosition - .01;
+                if (gamepad2.y && gripPosition > MIN_POSITION) gripPosition = gripPosition - .5;
 
                 // Set continuous servo power level and direction.
                 //if (gamepad1.dpad_left)
-                    //contPower = .20;
+                //contPower = .20;
                 //else if (gamepad1.dpad_right)
-                    //contPower = -.20;
+                //contPower = -.20;
                 //else
-                    //contPower = 0.0;
+                //contPower = 0.0;
 
                 // set the servo position/power values as we have computed them.
                 wristServo.setPosition(Range.clip(armPosition, MIN_POSITION, MAX_POSITION));
