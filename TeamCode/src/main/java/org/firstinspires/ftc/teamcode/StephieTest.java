99a8925d2cfdff3c395af481928334644fb6fab0
Stephie <bleepbeepbop33@gmail.com>
2023-10-16 19:46:02 -0700

    auto path INCOMPLETE

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StephieTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StephieTest.java
@@ -127,10 +127,10 @@ public class StephieTest extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-      //encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-       //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, 100, 100, 20.0);
+
+        encoderDrive(DRIVE_SPEED, 42, 42, 20.0);
+        // insert turning things later. screetch
+        encoderDrive(DRIVE_SPEED, 55,55, 20.0);
         telemetry.addData("Path", "Complete");
         telemetry.update();
         sleep(1000);  // pause to display final telemetry message.
