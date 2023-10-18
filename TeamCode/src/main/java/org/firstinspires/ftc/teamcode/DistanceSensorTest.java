03aa7a7467f55466536c2695e8a805adba429227
Kevin Furlong <kevinmfurlong@gmail.com>
2023-10-18 19:18:25 -0400

    Added distance Sensor demo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DistanceSensorTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp(name="Distance Sensor")
+public class DistanceSensorTest extends LinearOpMode{
+    // Definitions
+    DistanceSensor s_distance;
+
+    @Override
+    public void runOpMode() {
+        //Initialization code
+        s_distance = hardwareMap.get(DistanceSensor.class, "distance_sensor");
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Distance",s_distance.getDistance(DistanceUnit.INCH));
+            telemetry.update();
+        }
+    }
+}
