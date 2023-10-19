1c47d81378455d45ac3026c9fddf76013324cf75
Victor Lee <vlee4096@gmail.com>
2023-10-18 22:18:32 -0400

    Updated drive constants
    10/17/23

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -35,7 +35,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(20, 0, 8,
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(9, 0, 4,
             getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
 
     /*
@@ -47,7 +47,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.4763; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 0.66 * 1.25; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 14.20; // in
 
     /*
