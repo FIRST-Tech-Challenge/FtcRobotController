745b6c2d3ff1a8f123bbca0e8aec586d0cb09946
6460 team meber <6460@gmail.com>
2023-10-20 18:47:03 -0400

    Navagation.java -Got the variables in. 50+ lines down, 800+ to go.
    -TOOD: finish up the code, and update it to CENTERSTAGE robot for NiskyRobowarriors 6460

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Navigation.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
@@ -13,5 +12,47 @@ import java.util.Objects;
 /** Keeps track of the robot's desired path and makes it follow it accurately.
  */
 public class Navigation {
+    public enum rotationDirection {CLOCKWISE, COUNTERCLOCKWISE};
+    public static enum Action {NONE, SLIDES_LOW, SLIDES_HIGH,}; //Makes actions of the Robot that can be used anywhere within the folder.
+
+    //**AUTONOMOUS CONSTANTS**
+    public enum MovementMode {FORWARD_ONLY, BACKWARD_ONLY, STRAFE_LEFT, STRAFE_RIGHT}; //Movements within the robot Autonomous Mode
+    public final double STRAFE_ACCELERATION = 0.1; //Number indicated Inches per second squared
+    public final double ROTATE_ACCELERATION = 0.1; //Number indicated Radians per second squared
+    public final double SPEED_FACTOR = 0.7; //Speed of the robot when all motors are set to full power
+    static final double STRAFE_RAMP_DISTANCE = 4; //Number indicated Inches
+    //static final double ROTATION_RAMP_DISTANCE = Math.PI / 2; //NOT BEING USED
+    static final double MIN_STRAFE_POWER = 0.3; //Sets the strafe power to 3/10th power
+    static final double MAX_STRAFE_POWER = 0.5; //Sets the strafe power to 5/10th power
+    static final double STRAFE_CORRECITON_POWER = 0.3; //idk what this means
+    static final double STRAFE_SLOW = 0.1; //idk what this means
+    static final double MAX_ROTATION_POWER = 0.3; //sets the rotation power to 3/10th, power
+    static final double MIN_ROTATION_POWER = 0.03; //sets the rotation power to 3/100th power (why?)
+    static final double ROTATION_CORRECTION_POWER = 0.04; //idk what this means
+
+    // Accepted amounts of deviation between the robot's desired position and actual position.
+    static final double EPSILON_ANGLE = 0.35; //?????
+    static final int NUM_CHECK_FRAMES = 5; // The number of frames to wait after a rotate or travelLinear call in order to check for movement from momentum.
+
+    //Distances between where the robot extends/retracts the linear slides and where it opens the claw.
+    static final double ROTATION_TIME = 1050; //???
+    static final double FLOAT_EPSILON = 0.001; //????
+
+
+    //**TELEOP CONSTANTS**
+    static final double MOVEMENT_MAX_POWER = 1; //Sets the maximum power to full power. (Full power is between 0 - 1)
+    static final double ROTATION_POWER = 0.5; //Sets the maximum rotation power 1/2 full power
+    static final double REDUCED_ROTATION_POWER = 0.2; //Lets the minimum rotation power to 1/5th full power
+    static final double SLOW_MOVEMENT_SCALE_FACTOR = 0.3; //idk what this means
+    static final double MEDIUM_MOVEMENT_SCALE_FACTOR = 0.6; //idk what this means
+
+
+    //**INSTANCE ATTRIBUTES**//
+    public double[] wheel_speeds = {0.95, 1, -1, -0.97}; //Back left, Back right, Front left, Front right. Temporary Note: currently FR from -0.90 to -0.92
+    public double strafePower; //This is for Tele-Op ONLY.
+
+
+
+
 
 }
