030fbd5e918ba4a93c72bd81b8b89e7d13172df1
Isaac Young <isaacyoung272@gmail.com>
2023-10-19 21:22:23 -0400

    Basic Servo Code.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class ServoTest extends OpMode {
+    private Servo intakeServo;
+
+    @Override
+    public void init(){
+        telemetry.addData("Initiated", "True");
+        intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+    }
+
+    @Override
+    public void loop(){
+        if(gamepad1.y) {
+            // move to 0 degrees.
+            intakeServo.setPosition(0);
+        } else if (gamepad1.x || gamepad1.b) {
+            // move to 90 degrees.
+            intakeServo.setPosition(22/14);
+        } else if (gamepad1.a) {
+            // move to 180 degrees.
+            intakeServo.setPosition(22/7);
+        }
+    }
+}
