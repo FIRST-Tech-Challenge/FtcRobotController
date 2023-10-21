7fd809fda1c2e77ed75d52296619c7301868ecd0
Victor Lee <vlee4096@gmail.com>
2023-10-21 14:03:28 -0400

    Updated localization test
    Added ramping for x and y motion, added field centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/LocalizationTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.MovingStatistics;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -16,6 +18,19 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
  */
 @TeleOp(group = "drive")
 public class LocalizationTest extends LinearOpMode {
+    // Big math thingy that creates the curve for our ramp
+    class Ewma {
+        double mAlpha = 0;
+        double mLastValue = 0;
+        public Ewma(double alpha) {
+            mAlpha = alpha;
+        }
+
+        public double update(double x) {
+            mLastValue = mAlpha * x + (1 - mAlpha) * mLastValue;
+            return mLastValue;
+        }
+    }
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
@@ -24,18 +39,31 @@ public class LocalizationTest extends LinearOpMode {
 
         waitForStart();
 
+        // Ramping for x and y motion
+        Ewma statsX = new Ewma(0.3); // Raising alpha will make the ramp more drastic but more potentially create slip
+        Ewma statsY = new Ewma(0.3); // Decreasing alpha will reduce slip
+
         while (!isStopRequested()) {
+            double powX = statsX.update(-gamepad1.left_stick_y);
+            double powY = statsY.update(-gamepad1.left_stick_x);
+
+            Pose2d poseEstimate = drive.getPoseEstimate();
+
+            Vector2d input = new Vector2d(
+                    powX,
+                    powY
+            ).rotated(-poseEstimate.getHeading());
+            
             drive.setWeightedDrivePower(
                     new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
+                            input.getX(),
+                            input.getY(),
                             -gamepad1.right_stick_x
                     )
             );
 
             drive.update();
 
-            Pose2d poseEstimate = drive.getPoseEstimate();
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
