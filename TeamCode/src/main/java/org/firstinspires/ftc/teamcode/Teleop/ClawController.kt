33fc2d5faedb5c887e0be056c1fa57e83568354e
Jesse He <heqiqi2014@hotmail.com>
2023-10-21 11:44:07 -0700

    ClawControl

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/ClawController.kt
@@ -0,0 +1,53 @@
+
+package org.firstinspires.ftc.teamcode.Teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.Servo
+import com.qualcomm.robotcore.hardware.TouchSensor
+import org.firstinspires.ftc.teamcode.DriveMethods
+
+@TeleOp(name = "ClawControl", group = "AprilTag")
+class ClawControl: DriveMethods() {
+    var servo1: Servo? = null;
+    var servo2: Servo? = null;
+
+
+    override fun runOpMode() {
+        //turn
+        servo1 = hardwareMap.get<Servo>(Servo::class.java, "servo1");
+        //claw thing
+        servo2 = hardwareMap.get<Servo>(Servo::class.java, "servo2");
+        telemetry.addData("Initiating", "Servo Motors Init");
+        telemetry.update()
+        waitForStart()
+        var changed1 = false;
+        var changed2= false;
+        while (opModeIsActive()) {
+          telemetry.addLine(" Thang: "+servo1!!.position+" Thangy: "+servo2!!.getPosition())
+            telemetry.update()
+          if(gamepad1.dpad_down&&!changed1){
+              servo1!!.position-=0.01;
+              changed1=true;
+          }else if(gamepad1.dpad_up&&!changed1){
+                servo1!!.position+=0.01;
+              changed1=true;
+          }else if(!gamepad1.dpad_down&&!gamepad1.dpad_up){
+              changed1=false;
+          }
+
+          if(gamepad1.dpad_left&&!changed2){
+            servo2!!.position-=0.01;
+              changed2=true;
+          }else if(gamepad1.dpad_right&&!changed2){
+            servo2!!.position+=0.01;
+              changed2=true;
+          }else if(!gamepad1.dpad_left&&!gamepad1.dpad_right){
+              changed2=false;
+          }
+
+
+        sleep(10);
+        }
+    }
+}
