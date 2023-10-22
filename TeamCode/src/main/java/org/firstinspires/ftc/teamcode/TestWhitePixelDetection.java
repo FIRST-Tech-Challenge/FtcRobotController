1efe54c19d7e7d204dc2944ddd05222d38352d05
Sahiba <kaurs@midlandps.org>
2023-10-22 12:40:24 -0400

    commits
    easyapriltag start - telemetry to rc
    put calibration values in UtilitityCameraFrameCapture
    
    Use 3DF Zephyr for values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestWhitePixelDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestWhitePixelDetection.java
@@ -7,8 +7,10 @@ import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 
@@ -118,7 +120,7 @@ public class TestWhitePixelDetection {
         }
 
 
-        public static double getDistance(double width) {
+        public double getDistance(double width) {
             return (objectWidthInRealWorldUnits * focalLength) / width;
         }
 
