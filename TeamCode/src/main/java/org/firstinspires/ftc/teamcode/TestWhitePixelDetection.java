00c49a01de60282adf5de8d246c95557b0928f02
Ricky Wang <119374782+RickyWang101@users.noreply.github.com>
2023-10-22 15:02:32 -0400

    Revert "April tags sahiba"

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestWhitePixelDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestWhitePixelDetection.java
@@ -7,10 +7,8 @@ import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.imgproc.Moments;
 
@@ -120,7 +118,7 @@ public class TestWhitePixelDetection {
         }
 
 
-        public double getDistance(double width) {
+        public static double getDistance(double width) {
             return (objectWidthInRealWorldUnits * focalLength) / width;
         }
 
