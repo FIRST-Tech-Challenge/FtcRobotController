b7145aaccd51c45fa19d96da7b1b916e3358ec08
Isaac Young <isaacyoung272@gmail.com>
2023-10-22 15:39:11 -0400

    etc.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Test {
+    public int getInteger() {
+        return integer;
+    }
+
+    public void setInteger(int integer) {
+        this.integer = integer;
+    }
+
+    private int integer = 9;
+
+
+}
