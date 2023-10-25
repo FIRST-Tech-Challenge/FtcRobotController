155d06b4e162dea18c80934c9edcd63692656d2a
Oopdawg <94260533+Oopdawg@users.noreply.github.com>
2023-10-25 13:01:59 -0400

    tuned odo on programming chassis

--- TeamCode/src/main/java/roadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/roadRunner/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.88976; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 16.26378; // in
+    public static double TRACK_WIDTH = 20.01; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -67,7 +67,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 68.8675311;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = 5.361480871082184;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
     /*
