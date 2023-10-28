2740b0badb6f4a3456573ca64de68d0cd0466484
k8liang <k8lcool@gmail.com>
2023-10-27 21:43:48 -0700

    teleop minor changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTeleOp.java
@@ -18,7 +18,6 @@ public class TestTeleOp extends LinearOpMode {
         Servo arm = hardwareMap.servo.get("arm");
         DcMotor lsBack = hardwareMap.dcMotor.get("lsBack");
         DcMotor lsFront = hardwareMap.dcMotor.get("lsFront");
-
         DcMotor lFront = hardwareMap.dcMotor.get("fLeft");
         DcMotor rFront = hardwareMap.dcMotor.get("fRight");
         DcMotor lBack = hardwareMap.dcMotor.get("bLeft");
@@ -43,44 +42,61 @@ public class TestTeleOp extends LinearOpMode {
         double holderClampPos = 0.5;
         double armPos = 0.594; //down position
 
+        boolean hangingMode = false;
+
         while (opModeIsActive()) {
+
+            //GAMEPAD 2 CONTROLS
 
             double remainingDistanceHigh = 300 - lsFront.getCurrentPosition();
             double remainingDistanceMid = 200 - lsFront.getCurrentPosition();
             double remainingDistanceLow = 100 - lsFront.getCurrentPosition();
             double remainingDistanceZero = -lsFront.getCurrentPosition();
 
-            if (gamepad1.dpad_up && remainingDistanceHigh > 10) {
-
+            //set linear slide modes
+            if (gamepad2.dpad_up && remainingDistanceHigh > 10) {
                 lsFront.setPower(remainingDistanceHigh*0.002);
                 lsBack.setPower(remainingDistanceHigh*0.002);
-
-            } else if (gamepad1.dpad_right && remainingDistanceMid > 10) {
-
+            } else if (gamepad2.dpad_right && remainingDistanceMid > 10) {
                 lsFront.setPower(remainingDistanceMid*0.002);
                 lsBack.setPower(remainingDistanceMid*0.002);
-
-            } else if (gamepad1.dpad_left && remainingDistanceLow > 10) {
-
+            } else if (gamepad2.dpad_left && remainingDistanceLow > 10) {
                 lsFront.setPower(remainingDistanceLow*0.002);
                 lsBack.setPower(remainingDistanceLow*0.002);
-
-            } else if (gamepad1.dpad_down && remainingDistanceZero > 10) {
-
+            } else if (gamepad2.dpad_down && remainingDistanceZero > 10) {
                 lsFront.setPower(remainingDistanceZero*0.002);
                 lsBack.setPower(remainingDistanceZero*0.002);
-
             }
 
-            if (-gamepad2.left_stick_y > 0) {
-                lsBack.setPower(0.5);
-                lsFront.setPower(0.5);
-            } else if (-gamepad2.left_stick_y < 0) {
-                lsBack.setPower(-0.5);
-                lsFront.setPower(-0.5);
-            } else {
-                lsBack.setPower(0);
-                lsFront.setPower(0);
+            if(gamepad2.b == true) {
+                hangingMode = true;
+                //turn on hanging mode to increase power for linear slides
+            }
+
+            if(hangingMode == false) {
+                //if not hanging, power less
+                if (-gamepad2.left_stick_y > 0) {
+                    lsBack.setPower(0.5);
+                    lsFront.setPower(0.5);
+                } else if (-gamepad2.left_stick_y < 0) {
+                    lsBack.setPower(-0.5);
+                    lsFront.setPower(-0.5);
+                } else {
+                    lsBack.setPower(0);
+                    lsFront.setPower(0);
+                }
+            } else if (hangingMode == true) {
+                //if hanging, power more
+                if (-gamepad2.left_stick_y > 0) {
+                    lsBack.setPower(1);
+                    lsFront.setPower(1);
+                } else if (-gamepad2.left_stick_y < 0) {
+                    lsBack.setPower(-1);
+                    lsFront.setPower(-1);
+                } else {
+                    lsBack.setPower(0);
+                    lsFront.setPower(0);
+                }
             }
 
             telemetry.addLine(String.valueOf(armPos));
@@ -90,22 +106,24 @@ public class TestTeleOp extends LinearOpMode {
             if (gamepad2.left_trigger > 0) {
                 intake.setPower(1);
                 holderClampPos = 0.3;
+                //if intake button held, keep holder open
             } else if (gamepad2.left_bumper == true) {
                 //reversed intake
                 intake.setPower(-1);
                 holderClampPos = 0.3;
             } else {
                 if (Math.abs(gamepad1.left_stick_x) > 0.1 || Math.abs(gamepad1.left_stick_y) >0.1 || Math.abs(gamepad1.right_stick_x) >0.1) {
+                    //if robot moving, keep holder closed
                     holderClampPos = 0.5;
                 }
                 intake.setPower(0);
             }
 
             if(gamepad2.right_bumper == true) {
-                    holderClampPos -= 0.0025;
-                    //open
+                holderClampPos -= 0.025;
+                //open
             } else if (gamepad2.right_trigger > 0) {
-                holderClampPos += 0.0025;
+                holderClampPos += 0.025;
                 //close
             }
 
@@ -115,27 +133,29 @@ public class TestTeleOp extends LinearOpMode {
                 armPos -= -(gamepad2.right_stick_y / (1 / 0.004));
             }
 
+            //set limits for the holder clamp
             if(holderClampPos>1) {
                 holderClampPos = 1;
-            } else if (holderClampPos<0.45) {
-                holderClampPos = 0.45;
+            } else if (holderClampPos<0.3) {
+                holderClampPos = 0.3;
             }
 
+            //set limits for the arm rotation position
             if(armPos>1) {
                 armPos = 1;
             } else if (armPos<0) {
                 armPos=0;
             }
 
+            //automatically move arm to intake and outtake position
             if(gamepad2.a == true) {
-                armPos = 0.594; //down position
+                armPos = 0.594; //down (inttake) position
             }
             if(gamepad2.y == true) {
-                armPos = 0.845; //up position
+                armPos = 0.845; //up (outtake) position
             }
 
-            //0.46 open
-            //0.5
+            //GAMEPAD 1 CONTROLS
 
             double forwardBackward = gamepad1.left_stick_y * -0.5;
             double turning = gamepad1.right_stick_x * 0.5;
