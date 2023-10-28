dc3ae9cadfc4fe6a9728caf5cd79a5c2d450e515
krishvadivel <142186360+krishvadivel@users.noreply.github.com>
2023-10-28 17:30:17 -0500

    Create PullUp.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PullUp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(group = "FINALCODE")
+public class PullUp extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor pullUpMotor = hardwareMap.dcMotor.get("motor1");
+        pullUpMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            if (gamepad1.x) {
+                pullUpMotor.setPower(1);
+            }
+            else if (gamepad1.y) {
+                pullUpMotor.setPower(-1);
+            }
+            else {
+                pullUpMotor.setPower(0);
+            }
+        }
+    }
+}
