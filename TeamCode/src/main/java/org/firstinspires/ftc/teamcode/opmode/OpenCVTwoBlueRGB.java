22d05033c8629cc75acee9f5397783b903ec06bb
MineMaster836 <liamphone0@gmail.com>
2023-10-27 20:57:12 -0400

    renamed the original opmode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpenCVTwoBlueRGB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/OpenCVTwoBlueRGB.java
@@ -5,22 +5,14 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.pipeline.GripPipelineTwoBlueRGB;
-import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
-import java.util.ArrayList;
 
 @TeleOp(name="Open CV Test")
 //@Disabled
-public class OpenCVExampleOpMode extends OpMode {
+public class OpenCVTwoBlueRGB extends OpMode {
     static final int STREAM_WIDTH = 1280; // modify for your camera
     static final int STREAM_HEIGHT = 960; // modify for your camera
     OpenCvWebcam webcam;
