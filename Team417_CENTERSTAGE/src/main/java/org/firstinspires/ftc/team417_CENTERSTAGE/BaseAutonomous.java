5be5dfaf3b7a4606519472457a36d83942a695db
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-29 14:01:16 -0700

    Worked more on base auto

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
@@ -1,5 +1,69 @@
 package org.firstinspires.ftc.team417_CENTERSTAGE;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
 abstract public class BaseAutonomous extends BaseOpMode {
-    
+    OpenCvCamera camera; // calls camera
+
+    static final double FEET_PER_METER = 3.28084;
+    static final int CAMERA_WIDTH_PIXELS = 800;
+    static final int CAMERA_HEIGHT_PIXELS = 448;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagSize = 0.166;
+
+    // Tag ID 1, 2, 3 from the 36h11 family
+    int LEFT = 1;
+    int MIDDLE = 2;
+    int RIGHT = 3;
+    AprilTagDetection tagOfInterest = null;
+
+    static final double HOLD_ARM_AT_MID_OR_LOW_POS_POWER = 0.005;
+    static final double HOLD_ARM_AT_GROUND_POS_POWER = 0.01;
+    static final double ARM_RAISE_POWER = 1.0 / 800.0;
+
+    public void initializeAuto() {
+        initializeHardware();
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                camera.startStreaming(CAMERA_WIDTH_PIXELS, CAMERA_HEIGHT_PIXELS, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) { }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+    }
+
+    /**
+     * Updates telemetry: updates the id if no april tag is sighted
+     */
+    public void updateTelemetryAfterStart() {
+        if (tagOfInterest != null) {
+            telemetry.addLine("Tag snapshot:\n");
+            telemetry.update();
+        } else {
+            telemetry.addLine("No tag snapshot available, it was never sighted during the init loop :(");
+            telemetry.update();
+        }
+    }
 }
