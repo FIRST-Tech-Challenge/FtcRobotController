20f9523b235f010cfc7a5cc18f8aef0d26ca4094
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-29 13:20:02 -0700

    BaseAuto class made

---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "TeleOp League 1")
+public class BaseAutonomous extends BaseOpMode {
+    @Override
+    public void runOpMode() {
+        initializeHardware();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+        }
+    }
+
+    
+}
