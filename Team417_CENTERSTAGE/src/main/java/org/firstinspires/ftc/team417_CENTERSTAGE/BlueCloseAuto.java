2f0be5b856cd690aac0d4557638a15a1561c7252
Sawar Saini <sawar26saini@gmail.com>
2023-10-29 14:16:01 -0700

    worked on blue close data

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAuto.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAuto.java
@@ -1,13 +1,19 @@
-package org.firstinspires.ftc.team417_CENTERSTAGE;
+package org.firstinspires.ftc.team417_CENTERSTAGE.;
 
-iimport com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-public class BlueCloseAuto extends BaseAutonomous {
-    @Override
-    public void runOpMode() throws InterruptedException {
+@Disabled
+@Autonomous (name="RIGHT SIDE")
+public class LowJunctionParkRightSide extends BaseAutonomous {
 
+    @Override
+    public void runOpMode() {
+
+        initializeAuto();
+
+        Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
     }
 }
