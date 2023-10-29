9283a865529b8f311ebdb2fc2b94202a90acd0bd
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-29 13:53:44 -0700

    Worked on base auto

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
@@ -3,19 +3,6 @@ package org.firstinspires.ftc.team417_CENTERSTAGE;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp(name = "TeleOp League 1")
-public class BaseAutonomous extends BaseOpMode {
-    @Override
-    public void runOpMode() {
-        initializeHardware();
+abstract public class BaseAutonomous extends BaseOpMode {
 
-        waitForStart();
-
-        while (opModeIsActive()) {
-
-        }
-    }
-
-    public void data() {
-        
-    }
 }
