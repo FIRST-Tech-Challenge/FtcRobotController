f984b03a892bdd24e193714b6efc5007f1f0942c
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-29 14:06:14 -0700

    Worked on base auto

---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAuto.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+
+iimport com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+public class BlueCloseAuto extends BaseAutonomous {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
