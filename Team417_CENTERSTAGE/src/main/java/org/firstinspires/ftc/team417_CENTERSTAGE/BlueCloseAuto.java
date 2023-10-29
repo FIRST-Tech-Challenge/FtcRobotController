df5fd88d01499846b8bdecb7f23d254a034f6d28
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-10-29 14:22:51 -0700

    worked on auto pathway

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAuto.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAuto.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.team417_CENTERSTAGE.;
+package org.firstinspires.ftc.team417_CENTERSTAGE;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
@@ -6,7 +6,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
 @Disabled
-@Autonomous (name="RIGHT SIDE")
+@Autonomous (name="Blue Close ")
+
 public class LowJunctionParkRightSide extends BaseAutonomous {
 
     @Override
@@ -15,5 +16,12 @@ public class LowJunctionParkRightSide extends BaseAutonomous {
         initializeAuto();
 
         Pose2d startPose = new Pose2d(0, 0, Math.toRadians(0));
+        TrajectorySequence untitled1 = drive.trajectorySequenceBuilder(new Pose2d(-36.00, -60.00, Math.toRadians(90.00)))
+                .splineTo(new Vector2d(-36.00, -37.00), Math.toRadians(90.00))
+                .splineTo(new Vector2d(-36.00, -60.00), Math.toRadians(105.00))
+                .splineTo(new Vector2d(-58.56, -35.09), Math.toRadians(90.00))
+                .splineTo(new Vector2d(-48.00, -12.00), Math.toRadians(360.00))
+                .splineTo(new Vector2d(60.00, -12.00), Math.toRadians(360.00))
+                .build();
     }
 }
