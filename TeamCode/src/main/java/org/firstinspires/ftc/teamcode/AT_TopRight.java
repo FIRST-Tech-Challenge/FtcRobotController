8369b1211709e6016a62839421d819b961d15681
Josh Koppanalil <josh.koppanalil@gmail.com>
2023-10-29 19:25:57 -0500

    Initial checkin for the AT files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AT_TopRight.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.teamcode.HornetSquadObject;
+import org.firstinspires.ftc.teamcode.RobotHardware;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import java.util.List;
+
+/*
+ * This OpMode illustrates the basics of TensorFlow Object Detection,
+ * including Java Builder structures for specifying Vision parameters.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
+ */
+
+@TeleOp(name = "HornetSquad: AT Top Right", group = "Auto")
+public class AT_TopRight extends LinearOpMode {
+    private RobotHardware robot = new RobotHardware(this);
+    static final double FORWARD_SPEED = 0.2;
+    static final double TURNSPEED = 0.2;
+    @Override
+    public void runOpMode() {
+
+        // initialize all the hardware, using the hardware class. See how clean and simple this is?
+        robot.init();
+
+        // Send telemetry message to signify robot waiting;
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        if (opModeIsActive()) {
+            while (opModeIsActive() && !isStopRequested()) {
+
+                robot.driveRobot(FORWARD_SPEED, 0);
+
+                // Pace this loop so hands move at a reasonable speed.
+                sleep(50);
+
+                robot.driveRobot(0, -TURNSPEED);
+                // Pace this loop so hands move at a reasonable speed.
+                sleep(50);
+
+                robot.driveRobot(FORWARD_SPEED*4, 0);
+            }
+        }
+
+
+    }
+
+}
