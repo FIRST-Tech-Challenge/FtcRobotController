9729d54fa96f7e205dd5c0c14b2c1d2113d489d2
Sawar Saini <sawar26saini@gmail.com>
2023-10-31 18:35:34 -0700

    implemented more methods i need for backup auto

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.team417_CENTERSTAGE;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.opencv.core.Core;
@@ -18,13 +19,85 @@ import java.util.ArrayList;
 import java.util.List;
 
 abstract public class BaseAutonomous extends BaseOpMode {
-    
+
     private ElapsedTime runtime = new ElapsedTime();
 
     int lastEncoderFL = 0;
     int lastEncoderFR = 0;
     int lastEncoderBL = 0;
     int lastEncoderBR = 0;
+
+    public void initAuto() {
+        telemetry.addData("Init State", "Init Started");
+        telemetry.update();
+        initializeHardware();
+
+        telemetry.addData("Init State", "Init Finished");
+
+        // Set last know encoder values
+        lastEncoderFR = FR.getCurrentPosition();
+        lastEncoderFL = FL.getCurrentPosition();
+        lastEncoderBL = BL.getCurrentPosition();
+        lastEncoderBR = BR.getCurrentPosition();
+
+        telemetry.clear();
+        telemetry.addLine("Initialized. Ready to start!");
+        telemetry.update();
+    }
+
+    public void driveInches(double x, double y) {
+        double xTicks = x * TICKS_PER_INCH;
+        double yTicks = y * TICKS_PER_INCH;
+
+        double targetFL = xTicks + yTicks;
+        double targetFR = yTicks - xTicks;
+        double targetBL = yTicks - xTicks;
+        double targetBR = yTicks + xTicks;
+
+        // Determine new target position, and pass to motor controller
+        targetFL += FL.getCurrentPosition();
+        targetFR += FR.getCurrentPosition();
+        targetBL += BL.getCurrentPosition();
+        targetBR += BR.getCurrentPosition();
+
+        FL.setTargetPosition((int) targetFL);
+        FR.setTargetPosition((int) targetFR);
+        BL.setTargetPosition((int) targetBL);
+        BR.setTargetPosition((int) targetBR);
+
+        // Turn On RUN_TO_POSITION
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        runtime.reset();
+        FL.setPower(0.5);
+        FR.setPower(0.5);
+        BL.setPower(0.5);
+        BR.setPower(0.5);
+
+        // keep looping while we are still active, and there is time left, and both motors are running.
+        // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+        // its target position, the motion will stop.  This is "safer" in the event that the robot will
+        // always end the motion as soon as possible.
+        // However, if you require that BOTH motors have finished their moves before the robot continues
+        // onto the next step, use (isBusy() || isBusy()) in the loop test.
+        while (opModeIsActive() &&
+                (runtime.seconds() < 30) &&
+                (FL.isBusy() && FR.isBusy() && BL.isBusy() && BR.isBusy())) {
+        }
+
+        // Stop all motion;
+        stopDriving();
+
+        // Turn off RUN_TO_POSITION
+        FL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        FR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        BR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
 
     private int CAMERA_WIDTH_PIXELS = 640;
     private int CAMERA_HEIGHT_PIXELS = 480;
