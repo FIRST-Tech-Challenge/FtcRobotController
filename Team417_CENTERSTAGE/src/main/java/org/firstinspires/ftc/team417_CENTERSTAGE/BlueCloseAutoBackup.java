fc64a9bc7d854fa4f473807110f6d9a2f8f769c0
Sawar Saini <sawar26saini@gmail.com>
2023-10-31 18:21:25 -0700

    worked on creating back up auto

---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BlueCloseAutoBackup.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous (name="Blue Close Auto Backup ")
+
+public class BlueCloseAutoBackup extends BaseAutonomous {
+    public void runOpMode() {
+        initializeAuto();
+        waitForStart();
+        // move forward from the wall
+    }
+}
---
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/RedFarAutoBackup.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.team417_CENTERSTAGE;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous (name="Red Far Auto Backup ")
+
+public class RedFarAutoBackup extends BaseAutonomous {
+    public void runOpMode() {
+        initializeAuto();
+        waitForStart();
+        // move forward from the wall
+    }
+}
+
