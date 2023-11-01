91ea92c0339880fb04f8774bbc2721233473b5d0
Sawar Saini <sawar26saini@gmail.com>
2023-10-31 18:31:39 -0700

    worked some more on auto

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/BaseAutonomous.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 abstract public class BaseAutonomous extends BaseOpMode {
+    
     private ElapsedTime runtime = new ElapsedTime();
 
     int lastEncoderFL = 0;
