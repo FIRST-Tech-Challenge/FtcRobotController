95405caae3a61d30ac33e89df07d8c11cb8c8a9f
Torterra890 <mr.lucian.smith@gmail.com>
2023-11-01 12:25:05 -0700

    Update AprilTag.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTag.java
@@ -12,44 +12,9 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 import java.util.List;
 
-public class AprilTag {
-    /* Copyright (c) 2023 FIRST. All rights reserved.
-     *
-     * Redistribution and use in source and binary forms, with or without modification,
-     * are permitted (subject to the limitations in the disclaimer below) provided that
-     * the following conditions are met:
-     *
-     * Redistributions of source code must retain the above copyright notice, this list
-     * of conditions and the following disclaimer.
-     *
-     * Redistributions in binary form must reproduce the above copyright notice, this
-     * list of conditions and the following disclaimer in the documentation and/or
-     * other materials provided with the distribution.
-     *
-     * Neither the name of FIRST nor the names of its contributors may be used to endorse or
-     * promote products derived from this software without specific prior written permission.
-     *
-     * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-     * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-     * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-     * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-     * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-     * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-     * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-     * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-     * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-     * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-     * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-     */
-    /*
-     * This OpMode illustrates the basics of AprilTag recognition and pose estimation, using
-     * the easy way.
-     *
-     * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
-     * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
-     */
+
     @TeleOp(name = "AprilTagTest", group = "Concept")
-    public class ConceptAprilTagEasy extends LinearOpMode {
+    public class AprilTag extends LinearOpMode {
 
         private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
 
@@ -148,4 +113,3 @@ public class AprilTag {
 
     }   // end class
 
-}
