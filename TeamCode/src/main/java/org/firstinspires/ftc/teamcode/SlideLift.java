dc19873e820806d56f1e3a628ed488cd1fa83766
Inaara Tuan <intuan21@outlook.com>
2023-11-04 12:39:53 -0400

    Code for linear slide with deadzone and exponential acceleration. Min and max lift heights with touch sensor and encoder value. Need to test.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SlideLift.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import org.firstinspires.ftc.teamcode.tools.DeadzoneSquare;
+
+public class SlideLift {
+    private final DcMotor slideMotor;
+    private final double MAX_ENCODER_VAL = 50.0;
+    private final double MAX_LIFT_POWER = 0.5;
+    private final DeadzoneSquare deadzoneObj = new DeadzoneSquare(0.1, 0.25, MAX_LIFT_POWER);
+
+    public SlideLift() {
+        slideMotor = hardwareMap.dcMotor.get("slideMotor");
+    }
+
+    public void resetEncoders()
+    {
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void setLiftPower (double joystick, TouchSensor touchDown) {
+        double powerVal = deadzoneObj.computePower(joystick);
+        if (touchDown.isPressed() && powerVal<0) {
+            slideMotor.setPower(0);
+        }
+        else if (slideMotor.getCurrentPosition() >= MAX_ENCODER_VAL && powerVal>0) {
+            slideMotor.setPower(0);
+        }
+        else {
+            slideMotor.setPower(powerVal);
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopDrive.java
@@ -1,28 +1,30 @@
 package org.firstinspires.ftc.teamcode;
 
-import org.firstinspires.ftc.teamcode.roadRunner.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.tools.SetMotors;
+import org.firstinspires.ftc.teamcode.tools.SetDriveMotors;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp(name = "FC_square_TO_drive", group = "Testing")
 public class TeleopDrive extends LinearOpMode {
-    private SetMotors setMotorsObj;
+    private SetDriveMotors setDriveMotorsObj;
     //private SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    private TouchSensor touchDown;
+
+    private SlideLift slideLiftObj = new SlideLift();
 
     public void Setup(){
-        setMotorsObj = new SetMotors(hardwareMap, gamepad1);
+        setDriveMotorsObj = new SetDriveMotors(hardwareMap, gamepad1);
+        touchDown = hardwareMap.touchSensor.get("touchDown");
     }
 
     public boolean atRest(){
         return(
-                Math.abs(gamepad1.left_stick_y) < setMotorsObj.DEADZONE_MIN_Y &&
-                        Math.abs(gamepad1.right_stick_y) < setMotorsObj.DEADZONE_MIN_Y &&
-                        Math.abs(gamepad1.left_stick_x) < setMotorsObj.DEADZONE_MIN_X &&
-                        Math.abs(gamepad1.right_stick_x) < setMotorsObj.DEADZONE_MIN_X);
+                Math.abs(gamepad1.left_stick_y) < setDriveMotorsObj.DEADZONE_MIN_Y &&
+                        Math.abs(gamepad1.right_stick_y) < setDriveMotorsObj.DEADZONE_MIN_Y &&
+                        Math.abs(gamepad1.left_stick_x) < setDriveMotorsObj.DEADZONE_MIN_X &&
+                        Math.abs(gamepad1.right_stick_x) < setDriveMotorsObj.DEADZONE_MIN_X);
 
     }
 
@@ -41,7 +43,10 @@ public class TeleopDrive extends LinearOpMode {
 //            if (!drive.isBusy() || !atRest()) {
 //                setMotorsObj.driveCommands(hardwareMap, horizontal, vertical, turn, goFast);
 //            }
-            setMotorsObj.driveCommands(horizontal, vertical, turn, goFast);
+            setDriveMotorsObj.driveCommands(horizontal, vertical, turn, goFast);
+
+            slideLiftObj.setLiftPower(gamepad2.left_stick_y, touchDown);
+
         }
     }
 }
