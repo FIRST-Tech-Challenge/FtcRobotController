f1a3a459fba8dd141643008d990c3fdfe42d524d
jackledoux <jledoux@students.hackleyschool.org>
2023-11-04 18:55:07 -0400

    added correctional motor code and a setPosMotor function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/StateMachine.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tools/StateMachine.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.tools;
 import java.util.ArrayList;
 import java.util.function.BooleanSupplier;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -10,7 +11,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 // StateMachine class that handles state management and transitions
 public class StateMachine {
-    Telemetry telemetry;
+
 
     // Inner State class representing an individual state within the StateMachine
     public class State {
@@ -124,7 +125,7 @@ public class StateMachine {
 
 
     // method for a servo to correct itself if it is not within one degree of the desired position
-    public void setPosServo(Servo servo, int pos) {
+    public void setPosServo(Servo servo, int pos, Telemetry telemetry) {
         int tolerance = 1;
         servo.setPosition(pos);
         int counter = 0; // counts the number of times the correctional loop runs
@@ -138,5 +139,43 @@ public class StateMachine {
             telemetry.addLine(servo + " corrected " + counter + " times");
         }
         telemetry.addLine(servo + " correction finished");
+        telemetry.update();
     }
+
+    public void setPosMotor(DcMotor motor, int pos, Telemetry telemetry) {
+        int tolerance = 1;
+        motor.setTargetPosition(pos);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motor.setPower(0.5); // Set some default power
+
+        int counter = 0; // Counts the number of times the correctional loop runs
+
+        while (/*opModeIsActive() &&**/ Math.abs(pos - motor.getCurrentPosition()) > tolerance && counter <= 5) {
+            telemetry.addLine(motor + " correction started");
+            telemetry.update();
+
+            int currentPosition = motor.getCurrentPosition();
+            int error = pos - currentPosition;
+            int halfError = error / 2;
+
+            motor.setTargetPosition(currentPosition + halfError);
+
+            while (/*opModeIsActive() &&**/motor.isBusy()) {
+                // Optionally include idle() if in LinearOpMode
+                telemetry.addData("Encoder Target", pos);
+                telemetry.addData("Current Position", currentPosition);
+                telemetry.update();
+            }
+            motor.setPower(0);
+
+            counter++;
+            telemetry.addData(motor + " corrected ", counter + " times");
+            telemetry.update();
+        }
+
+        motor.setPower(0); // Ensure the motor is stopped at the end
+        telemetry.addLine(motor + " correction finished");
+        telemetry.update();
+    }
+
 }
