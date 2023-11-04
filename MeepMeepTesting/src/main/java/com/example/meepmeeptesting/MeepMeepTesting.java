0c5989229cc6710436f60d6a07620570757e9103
Hyunwook Choi <105407466+Sipdip@users.noreply.github.com>
2023-11-03 18:45:20 -0700

    backdrop sides re-done

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -22,28 +22,28 @@ public class MeepMeepTesting {
                 .setConstraints(50, 40, 4 * PI, 2 * PI, 11)
                 .setConstraints(60, 60, toRadians(180), toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                                drive.trajectorySequenceBuilder(new Pose2d(-38.5, -63.25, Math.toRadians(90)))
+                                drive.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(90)))
                                         //BLUEBACK
-//                                        .setReversed(true) //spike 1
-//                                        .lineToLinearHeading(new Pose2d(31,31,toRadians(0)))
-//                                        .setReversed(false)
-//                                        .lineToLinearHeading(new Pose2d(51, 41.5,toRadians(0)))
-//                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
-//                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
+                                        .setReversed(true) //spike 1
+                                        .lineToLinearHeading(new Pose2d(31,31,toRadians(0)))
+                                        .setReversed(false)
+                                        .lineToLinearHeading(new Pose2d(51, 41.5,toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
 
-//                                        .setReversed(true) //spike 2
-//                                        .lineToLinearHeading(new Pose2d(12.5, 33, toRadians(-90)))
-//                                        .setReversed(false)
-//                                        .lineToLinearHeading(new Pose2d(51, 35.5, toRadians(0)))
-//                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
-//                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
+                                        .setReversed(true) //spike 2
+                                        .lineToLinearHeading(new Pose2d(12.5, 33, toRadians(-90)))
+                                        .setReversed(false)
+                                        .lineToLinearHeading(new Pose2d(51, 35.5, toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
 
-//                                        .setReversed(true) //spike 3
-//                                        .splineTo(new Vector2d(8,35), toRadians(180))
-//                                        .setReversed(false)
-//                                        .splineTo(new Vector2d(51, 30.5), toRadians(0))
-//                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
-//                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
+                                        .setReversed(true) //spike 3
+                                        .splineTo(new Vector2d(8,35), toRadians(180))
+                                        .setReversed(false)
+                                        .splineTo(new Vector2d(51, 30.5), toRadians(0))
+                                        .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
+                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
                                         //BLUEAUD
 //                                        .setReversed(true) //spike 3
 //                                        .lineToLinearHeading(new Pose2d(-38.5, 34, toRadians(0))) //m1
@@ -75,33 +75,33 @@ public class MeepMeepTesting {
 //                                        .lineToLinearHeading(new Pose2d(60, 60, toRadians(0))) //dp
 
                                         //RedAudience
-                                        .setReversed(true) //spike 3
-                                        .lineToLinearHeading(new Pose2d(-38.5, -34, toRadians(90))) //m1
-                                        .lineToLinearHeading(new Pose2d(-34,-33, toRadians(0))) //m1
-                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
-                                        .setReversed(false) //truss
-                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
-                                        .splineTo(new Vector2d(51, -41.5), toRadians(0)) //dp
-                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
-                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
-
-                                        .setReversed(true) //spike 2
-                                        .lineToLinearHeading(new Pose2d(-35.5, -33, toRadians(90))) //m1
-                                        .setReversed(false) //m1
-                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
-                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
-                                        .splineTo(new Vector2d(51,-35.5), toRadians(0)) //dp
-                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
-                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
-
-                                        .setReversed(true) //spike 1
-                                        .lineToLinearHeading(new Pose2d(-46, -37, toRadians(90))) //m1
-                                        .setReversed(false) //m1
-                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
-                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
-                                        .splineTo(new Vector2d(51, -30.5), toRadians(0)) //dp
-                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
-                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
+//                                        .setReversed(true) //spike 3
+//                                        .lineToLinearHeading(new Pose2d(-38.5, -34, toRadians(90))) //m1
+//                                        .lineToLinearHeading(new Pose2d(-34,-33, toRadians(0))) //m1
+//                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
+//                                        .setReversed(false) //truss
+//                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
+//                                        .splineTo(new Vector2d(51, -41.5), toRadians(0)) //dp
+//                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
+//                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
+//
+//                                        .setReversed(true) //spike 2
+//                                        .lineToLinearHeading(new Pose2d(-35.5, -33, toRadians(90))) //m1
+//                                        .setReversed(false) //m1
+//                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
+//                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
+//                                        .splineTo(new Vector2d(51,-35.5), toRadians(0)) //dp
+//                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
+//                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
+//
+//                                        .setReversed(true) //spike 1
+//                                        .lineToLinearHeading(new Pose2d(-46, -37, toRadians(90))) //m1
+//                                        .setReversed(false) //m1
+//                                        .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
+//                                        .splineTo(new Vector2d(10, -58), toRadians(0)) //truss
+//                                        .splineTo(new Vector2d(51, -30.5), toRadians(0)) //dp
+//                                        .lineToLinearHeading(new Pose2d(51, -60, toRadians(0))) //dp
+//                                        .lineToLinearHeading(new Pose2d(60, -60, toRadians(0))) //dp
 
                                         .build()
                 );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueRightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueRightAuto.java
@@ -19,39 +19,38 @@ public class BlueRightAuto extends LinearOpMode {
         int pos = 0;
         TrajectorySequence[] preload = new TrajectorySequence[3];
         preload[0] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
-                .setReversed(true)
-                .splineTo(new Vector2d(8,35), toRadians(-180))
-                .addTemporalMarker(robot::done)
-                .build();
-        preload[1] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
-                .setReversed(true)
-                .lineToLinearHeading(new Pose2d(12.5, 33, toRadians(-90)))
-                .setReversed(false)
-                .lineToLinearHeading(new Pose2d(12.5, 58, toRadians(0)))
-                .addTemporalMarker(robot::done)
-                .build();
-        preload[2] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
-                .setReversed(true)
+                .setReversed(true) //spike 1
                 .lineToLinearHeading(new Pose2d(31,31,toRadians(0)))
                 .addTemporalMarker(robot::done)
                 .build();
+        preload[1] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
+                .setReversed(true) //spike 2
+                .lineToLinearHeading(new Pose2d(12.5, 33, toRadians(-90)))
+                .addTemporalMarker(robot::done)
+                .build();
+        preload[2] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(15.5, 63.25, Math.toRadians(-90)))
+                .setReversed(true) //spike 3
+                .splineTo(new Vector2d(8,35), toRadians(180))
+                .addTemporalMarker(robot::done)
+                .build();
         TrajectorySequence[] dropAndPark = new TrajectorySequence[3];
-        dropAndPark[0]= robot.roadrun.trajectorySequenceBuilder(new Pose2d(12.5, 58, toRadians(0)))
-                .setReversed(false)
-                .splineTo(new Vector2d(51, 30.5), toRadians(0))
-                .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
-                .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
-                .addTemporalMarker(robot::done)
-                .build();
-        dropAndPark[1]= robot.roadrun.trajectorySequenceBuilder(new Pose2d(12.5, 58, toRadians(0)))
-                .splineTo(new Vector2d(51, 35.5), toRadians(0))
-                .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
-                .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
-                .addTemporalMarker(robot::done)
-                .build();
-        dropAndPark[2] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(12.5, 58, toRadians(0)))
+        dropAndPark[0]= robot.roadrun.trajectorySequenceBuilder(new Pose2d(31, 31, toRadians(0)))
                 .setReversed(false)
                 .lineToLinearHeading(new Pose2d(51, 41.5,toRadians(0)))
+                .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
+                .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
+                .addTemporalMarker(robot::done)
+                .build();
+        dropAndPark[1]= robot.roadrun.trajectorySequenceBuilder(new Pose2d(12.5, 33, toRadians(-90)))
+                .setReversed(false)
+                .lineToLinearHeading(new Pose2d(51, 35.5, toRadians(0)))
+                .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
+                .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
+                .addTemporalMarker(robot::done)
+                .build();
+        dropAndPark[2] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(8, 35, toRadians(180)))
+                .setReversed(false)
+                .splineTo(new Vector2d(51, 30.5), toRadians(0))
                 .lineToLinearHeading(new Pose2d(51, 60, toRadians(0)))
                 .lineToLinearHeading(new Pose2d(60, 60, toRadians(0)))
                 .addTemporalMarker(robot::done)
