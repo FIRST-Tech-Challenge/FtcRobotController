dc1e470fad614cf2f2f6b9299b65793af1e121b2
Hyunwook Choi <105407466+Sipdip@users.noreply.github.com>
2023-11-03 18:31:12 -0700

    audience-sides re-done to actually work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedLeftAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedLeftAuto.java
@@ -16,27 +16,32 @@ public class RedLeftAuto extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         BradBot robot = new BradBot(this, false);
-        robot.roadrun.setPoseEstimate(new Pose2d(-38.5,-63.25, Math.toRadians(-90)));
+        robot.roadrun.setPoseEstimate(new Pose2d(-38.5,-63.25, Math.toRadians(90)));
         int pos = 0;
         TrajectorySequence[] spikePosition = new TrajectorySequence[3];
         spikePosition[0] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(-38.5, -63.25, Math.toRadians(-90)))
-                .setReversed(true)
-                .lineToLinearHeading(new Pose2d(-46, -37, toRadians(-90)))
+                .setReversed(true) //spike 1
+                .lineToLinearHeading(new Pose2d(-46, -37, toRadians(90))) //m1
+                .setReversed(false) //m1
+                .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
                 .addTemporalMarker(robot::done)
                 .build();
         spikePosition[1] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(-38.5, -63.5, Math.toRadians(-90)))
-                .setReversed(true)
-                .lineToLinearHeading(new Pose2d(-35.5, -33, toRadians(-90)))
+                .setReversed(true) //spike 2
+                .lineToLinearHeading(new Pose2d(-35.5, -33, toRadians(90))) //m1
+                .setReversed(false) //m1
+                .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
                 .addTemporalMarker(robot::done)
                 .build();
         spikePosition[2] = robot.roadrun.trajectorySequenceBuilder(new Pose2d(-38.5, -63.5, Math.toRadians(-90)))
-                .setReversed(true)
-                .lineToLinearHeading(new Pose2d(-38.5, -34, toRadians(-90)))
+                .setReversed(true) //spike 3
+                .lineToLinearHeading(new Pose2d(-38.5, -34, toRadians(90))) //m1
+                .lineToLinearHeading(new Pose2d(-34,-33, toRadians(0))) //m1
+                .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0))) //m1
+                .setReversed(false) //truss
                 .addTemporalMarker(robot::done)
                 .build();
-        TrajectorySequence throughTruss = robot.roadrun.trajectorySequenceBuilder(new Pose2d(-46,-37, toRadians(90)))
-                .setReversed(false)
-                .lineToLinearHeading(new Pose2d(-46, -58, toRadians(0)))
+        TrajectorySequence throughTruss = robot.roadrun.trajectorySequenceBuilder(new Pose2d(-46,-58, toRadians(0)))
                 .splineTo(new Vector2d(10, -58), toRadians(0))
                 .addTemporalMarker(robot::done)
                 .build();
