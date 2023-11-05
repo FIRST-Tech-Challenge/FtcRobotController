9cbdcf1181ba0c02c59bf073db032d065af76320
RedGaming101 <costa@milopaper.com>
2023-11-05 13:02:05 -0500

    Updated CostaNewTest
    Fixed some more errors in the code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/CostaNewTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/CostaNewTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.Testing.TeachingClass;
@@ -31,45 +32,75 @@ public class CostaNewTest extends LinearOpMode {
         clawServo = hardwareMap.get(Servo.class, "claw_servo");
         droneLauncher = hardwareMap.get(Servo.class, "drone_launcher");
 
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
 
         waitForStart();
+        while(opModeIsActive()){
+            double xSpeed = gamepad1.left_stick_x;
+            double ySpeed = -gamepad1.left_stick_y;
+            double rotation = gamepad1.right_stick_x;
 
-        double xSpeed = gamepad1.left_stick_x;
-        double ySpeed = -gamepad1.left_stick_y;
-        double rotation = gamepad1.right_stick_x;
+            //setting power for forward-backward movement
+            frontLeft.setPower(-ySpeed);
+            //setting the power for frontLeft
+            backLeft.setPower(-ySpeed);
+            //setting the power for backLeft
+            frontRight.setPower(ySpeed);
+            //setting the power for frontRight
+            backRight.setPower(ySpeed);
+            //setting the power for backRight
 
-        frontLeft.setPower(xSpeed + ySpeed + rotation);
-        frontRight.setPower(-xSpeed + ySpeed - rotation);
-        backLeft.setPower(xSpeed + ySpeed - rotation);
-        backRight.setPower(-xSpeed + ySpeed + rotation);
+            //setting up strafing
+            frontLeft.setPower(-xSpeed);
+            backLeft.setPower(xSpeed);
+            frontRight.setPower(-xSpeed);
+            backRight.setPower(xSpeed);
 
-        double intakePower = 0.0;
-        if (gamepad2.right_bumper) {
-            intakePower = 1.0;
+            frontLeft.setPower(rotation);
+            backLeft.setPower(rotation);
+            frontRight.setPower(rotation);
+            backRight.setPower(rotation);
+
+//            double intakePower = 0.0;
+//            if (gamepad2.right_bumper) {
+//                intakePower = 1.0;
+//            }
+//            intakeMotor.setPower(intakePower);
+//
+//            double clawExtensionPower = 0.0;
+//            if (gamepad2.left_bumper) {
+//                clawExtensionPower = 0.8;
+//            }
+//            clawExtender.setPower(clawExtensionPower);
+//
+//            if (gamepad2.a) {
+//                clawServo.setPosition(0.6);
+//                clawOpen = true;
+//            }
+//
+//            if (gamepad2.b) {
+//                clawServo.setPosition(0.0);
+//                clawOpen = false;
+//            }
+//
+//            if (gamepad2.x) {
+//                droneLauncher.setPosition(0.8);
+//            } else {
+//                droneLauncher.setPosition(0.0);
+//            }
+
         }
-        intakeMotor.setPower(intakePower);
 
-        double clawExtensionPower = 0.0;
-        if (gamepad2.left_bumper) {
-            clawExtensionPower = 1.0;
-        }
-        clawExtender.setPower(clawExtensionPower);
+//        frontLeft.setPower(xSpeed + ySpeed + rotation);
+//        frontRight.setPower(-xSpeed + ySpeed - rotation);
+//        backLeft.setPower(xSpeed + ySpeed + rotation);
+//        backRight.setPower(-xSpeed + ySpeed - rotation);
 
-        if (gamepad2.a) {
-            clawServo.setPosition(0.9);
-            clawOpen = true;
-        }
 
-        if (gamepad2.b) {
-            clawServo.setPosition(0.0);
-            clawOpen = false;
-        }
-
-        if (gamepad2.x) {
-            droneLauncher.setPosition(1.0);
-        } else {
-            droneLauncher.setPosition(0.0);
-        }
     }
 
 }
