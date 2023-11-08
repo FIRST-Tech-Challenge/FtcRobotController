6980e41d772160ced82f235569b22c8bd1d263da
Carter Rommelfanger <58859646+RAMENtheNOODLES@users.noreply.github.com>
2023-11-08 19:05:19 +0000

    feat(FTC Dashboard Packets): redid quite a bit of code
    Added some logging features and added some common packets, to make logging easier.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDashboardPackets.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTCDashboardPackets.java
@@ -5,9 +5,20 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 
 public class FTCDashboardPackets {
     public static final FtcDashboard DASHBOARD = FtcDashboard.getInstance();
+    /**
+     * The key to be used when putting a value into a packet.
+     * Should be set as the name of the class that is using this.
+     */
+    public static final String CONTEXT;
     private static TelemetryPacket packet;
 
     public FTCDashboardPackets() {
+        CONTEXT = "ROOT";
+        createNewTelePacket();
+    }
+
+    public FTCDashboardPackets(String context) {
+        CONTEXT = context.toUpperCase();
         createNewTelePacket();
     }
 
@@ -27,6 +38,14 @@ public class FTCDashboardPackets {
      */
     public void put(String key, String value) {
         packet.put(key, value);
+    }
+
+    /**
+     * Takes in a value and puts it into the current packet.
+     * @param value The string to be put into the values of a packet
+     */
+    public void put(String value) {
+        packet.put(CONTEXT, value);
     }
 
     /**
@@ -50,6 +69,104 @@ public class FTCDashboardPackets {
     }
 
     /**
+     * Takes in a message and puts it into the current packet.
+     * @param message The message to be put into the packet
+     */
+    public void info(String message) {
+        packet.put(CONTEXT + " : INFO", message);
+    }
+
+    /**
+     * Takes in a message and puts it into the current packet.
+     * @param message The message to be put into the packet
+     * @param sendPacket A boolean of whether or not to send the packet after the message is put in
+     * @param reinitializePacket A boolean of whether or not to reinitialize the packet after it is sent
+     */
+    public void info(String message, boolean sendPacket, boolean reinitializePacket) {
+        info(message);
+        if (sendPacket)
+            send(reinitializePacket);
+    }
+
+    /**
+     * Takes in a packet and puts it into the current packet.
+     * @param packet The packet to be put into the packet
+     */
+    public void info(CommonPackets packet) {
+        info(getCommonPacket(packet));
+    }
+
+    /**
+     * Takes in a packet and puts it into the current packet.
+     * @param packet The common message to be put into the packet
+     * @param sendPacket A boolean of whether or not to send the packet after the packet is put in
+     * @param reinitializePacket A boolean of whether or not to reinitialize the packet after it is sent
+     */
+    public void info(CommonPackets packet, boolean sendPacket, boolean reinitializePacket) {
+        info(packet);
+        if (sendPacket)
+            send(reinitializePacket);
+    }
+
+    /**
+     * Takes in a message and puts it into the current packet.
+     * @param message The message to be put into the packet
+     */
+    public void debug(String message) {
+        packet.put(CONTEXT + " : DEBUG", message);
+    }
+
+    /**
+     * Takes in a message and puts it into the current packet.
+     * @param message The message to be put into the packet
+     * @param sendPacket A boolean of whether or not to send the packet after the message is put in
+     * @param reinitializePacket A boolean of whether or not to reinitialize the packet after it is sent
+     */
+    public void debug(String message, boolean sendPacket, boolean reinitializePacket) {
+        debug(message);
+        if (sendPacket)
+            send(reinitializePacket);
+    }
+
+    /**
+     * Takes in a packet and puts it into the current packet.
+     * @param packet The packet to be put into the packet
+     */
+    public void debug(CommonPackets packet) {
+        debug(getCommonPacket(packet));
+    }
+
+    /**
+     * Takes in a packet and puts it into the current packet.
+     * @param packet The common message to be put into the packet
+     * @param sendPacket A boolean of whether or not to send the packet after the packet is put in
+     * @param reinitializePacket A boolean of whether or not to reinitialize the packet after it is sent
+     */
+    public void debug(CommonPackets packet, boolean sendPacket, boolean reinitializePacket) {
+        debug(packet);
+        if (sendPacket)
+            send(reinitializePacket);
+    }
+
+    /**
+     * Takes in a packet and returns the value of the packet
+     * @param packet The common packet to retrieve the common message from
+     * @return The value of the packet
+     */
+    public String getCommonPacket(CommonPackets packet) {
+        switch (packet) {
+            case START:
+                return "Starting...";
+            case CONTINUE:
+                return "Continuing...";
+            case WAITING:
+                return "Waiting...";
+            default:
+                return "Unknown packet";
+        }
+    }
+
+    /**
      * Sends the current packet to the dashboard
      * @param reinitializePacket A boolean of whether or not to reinitialize the packet after it is sent.
      */
@@ -61,6 +178,7 @@ public class FTCDashboardPackets {
 
     public enum CommonPackets {
         START,
-        CONTINUE
+        CONTINUE,
+        WAITING
     }
 }
