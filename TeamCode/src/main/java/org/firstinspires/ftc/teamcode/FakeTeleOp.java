abcb3b28fb86990a71904d7f165056a2ea3b1a8a
confwhat <tanvinator24@gmail.com>
2023-11-10 19:33:15 -0800

    faketeleop will not show up in the menu, and realteleop has been refactored to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FakeTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FakeTeleOp.java
@@ -2,7 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class FakeTeleOp extends LinearOpMode {
 
 
