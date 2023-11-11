cf7da211dccc487fce77b6f6adace7aa51294e61
Ethan Vu <ethan.mt.vu@gmail.com>
2023-11-11 15:58:57 -0800

    no changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRed.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
 
 public class AutoRed extends LinearOpMode {
     @Override
