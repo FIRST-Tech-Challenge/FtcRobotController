cd545568daba194a4f40fbef3c5ba23f4580740f
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-11-11 14:52:46 -0800

    Used the values from Coach Truong's color detection tuner, in the kitchen in the shop.

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/opencv/Constants.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/opencv/Constants.java
@@ -6,10 +6,10 @@ import org.opencv.core.Size;
 public class Constants {
 
     // Color ranges for OpenCV detection
-    public static final Scalar RED_COLOR_DETECT_MAX_HSV = new Scalar(10, 255, 255);
+    public static final Scalar RED_COLOR_DETECT_MAX_HSV = new Scalar(4, 255, 255);
     public static final Scalar RED_COLOR_DETECT_MIN_HSV = new Scalar(0, 65, 95);
-    public static final Scalar BLUE_COLOR_DETECT_MAX_HSV = new Scalar(120, 255, 255);
-    public static final Scalar BLUE_COLOR_DETECT_MIN_HSV = new Scalar(103, 60, 50);
+    public static final Scalar BLUE_COLOR_DETECT_MAX_HSV = new Scalar(55, 255, 255);
+    public static final Scalar BLUE_COLOR_DETECT_MIN_HSV = new Scalar(60, 60, 50);
     public static final Scalar borderColor = new Scalar(0, 255, 0);  // green
     public static final int CAMERA_IMAGE_WIDTH = 640;
     public static final int CAMERA_IMAGE_HEIGHT = 480;
