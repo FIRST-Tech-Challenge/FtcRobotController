af8b1298b236c8a9e78848ebe9388de2ca8a363c
confwhat <tanvinator24@gmail.com>
2023-11-10 22:44:13 -0800

    added a button to change which end of the robot is front on gamepad 1. also refactored a lot of variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-//add any test stuff you need to do here
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
 
@@ -16,26 +15,32 @@ public class TeleOp extends LinearOpMode {
         Robot robot = new Robot(hardwareMap, this, telemetry, true);
 
         //getting motors and servos from config
+
         DcMotor intake = hardwareMap.dcMotor.get("intake");
-        Servo holderClamp = hardwareMap.servo.get("holderClamp");
-        Servo arm = hardwareMap.servo.get("arm");
-        Servo planeLauncher = hardwareMap.servo.get("planeLauncher");
-        Servo linearLocker = hardwareMap.servo.get("linearLocker");
+
+        //TODO: refactor to left/right
         DcMotor lsBack = hardwareMap.dcMotor.get("lsBack");
         DcMotor lsFront = hardwareMap.dcMotor.get("lsFront");
+
         DcMotor lFront = hardwareMap.dcMotor.get("fLeft");
         DcMotor rFront = hardwareMap.dcMotor.get("fRight");
         DcMotor lBack = hardwareMap.dcMotor.get("bLeft");
         DcMotor rBack = hardwareMap.dcMotor.get("bRight");
 
+        Servo clamp = hardwareMap.servo.get("holderClamp");
+        Servo tray = hardwareMap.servo.get("arm");
+        Servo launcher = hardwareMap.servo.get("planeLauncher");
+        Servo lock = hardwareMap.servo.get("linearLocker");
+
         //setting motor direction
         intake.setDirection(DcMotorSimple.Direction.REVERSE);
-        rBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        lBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        rFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        lFront.setDirection(DcMotorSimple.Direction.REVERSE);
         lsFront.setDirection(DcMotorSimple.Direction.REVERSE);
         lsBack.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        lFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        lBack.setDirection(DcMotorSimple.Direction.REVERSE);
+        rBack.setDirection(DcMotorSimple.Direction.FORWARD);
 
         //set motor zero power behavior
         lsFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -45,19 +50,25 @@ public class TeleOp extends LinearOpMode {
         lsFront.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         lsFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        double clampPos;
+        double trayPos;
+        boolean hangingMode;
+        int polarity;
+
         waitForStart();
 
-        double holderClampPos = 0.5;
-        double armPos = 0.594; //down
+        //default positions
+        clampPos = 0.5;
+        trayPos = 0.594;
 
-        boolean hangingMode = false;
+        hangingMode = false;
+        polarity = 1;
 
         //set launcher and lock positions at start
-        planeLauncher.setPosition(0.25);
-        linearLocker.setPosition(0.5);
+        launcher.setPosition(0.25);
+        lock.setPosition(0.5);
 
         while (opModeIsActive()) {
-
 
             //GAME PAD 2 CONTROLS NOT DRIVER
 
@@ -69,31 +80,31 @@ public class TeleOp extends LinearOpMode {
             //gamepad 1 dpad moves linear slide to a set position
             if (gamepad1.dpad_up && remainingDistanceHigh > 10) {
 
-                lsFront.setPower(remainingDistanceHigh*0.002);
-                lsBack.setPower(remainingDistanceHigh*0.002);
+                lsFront.setPower(remainingDistanceHigh * 0.002);
+                lsBack.setPower(remainingDistanceHigh * 0.002);
 
             } else if (gamepad1.dpad_right && remainingDistanceMid > 10) {
 
-                lsFront.setPower(remainingDistanceMid*0.002);
-                lsBack.setPower(remainingDistanceMid*0.002);
+                lsFront.setPower(remainingDistanceMid * 0.002);
+                lsBack.setPower(remainingDistanceMid * 0.002);
 
             } else if (gamepad1.dpad_left && remainingDistanceLow > 10) {
 
-                lsFront.setPower(remainingDistanceLow*0.002);
-                lsBack.setPower(remainingDistanceLow*0.002);
+                lsFront.setPower(remainingDistanceLow * 0.002);
+                lsBack.setPower(remainingDistanceLow * 0.002);
 
             } else if (gamepad1.dpad_down && remainingDistanceZero > 10) {
 
-                lsFront.setPower(remainingDistanceZero*0.002);
-                lsBack.setPower(remainingDistanceZero*0.002);
+                lsFront.setPower(remainingDistanceZero * 0.002);
+                lsBack.setPower(remainingDistanceZero * 0.002);
 
             }
 
             //gamepad 2 dpad locks and unlocks the lock
             if (gamepad2.dpad_up) {
-                linearLocker.setPosition(0.3);
-            } else if(gamepad2.dpad_down) {
-                linearLocker.setPosition(0.5);
+                lock.setPosition(0.3);
+            } else if (gamepad2.dpad_down) {
+                lock.setPosition(0.5);
             }
 
             //gamepad 2 B changes amount of linear slide motor power (for endgame hanging)
@@ -128,88 +139,101 @@ public class TeleOp extends LinearOpMode {
                 }
             }
 
-            if (lsFront.getCurrentPosition() < 60) {
-                armPos = 0.594;
+            //if linear slide motor's # of ticks is under 4, set it to down
+            if (lsFront.getCurrentPosition() < 4) {
+                trayPos = 0.594;
             }
-
-            telemetry.addLine(String.valueOf(armPos));
-            telemetry.addLine(String.valueOf(lsFront.getCurrentPosition()));
 
             //intake
             if (gamepad2.left_trigger > 0) {
-                intake.setPower(0.55);
-                holderClampPos = 0.43;
+                intake.setPower(0.4);
+                clampPos = 0.4;
                 //if intake button held, keep holder open
             } else if (gamepad2.left_bumper) {
                 //reversed intake
                 intake.setPower(-0.6);
+                clampPos = 0.4;
             } else {
-                if ((Math.abs(gamepad1.left_stick_x) > 0.1 || Math.abs(gamepad1.left_stick_y) >0.1 || Math.abs(gamepad1.right_stick_x) >0.1) && !gamepad2.left_bumper) {
+                if ((Math.abs(gamepad1.left_stick_x) > 0.1 || Math.abs(gamepad1.left_stick_y) > 0.1 || Math.abs(gamepad1.right_stick_x) > 0.1) && !gamepad2.left_bumper) {
                     //if robot moving, keep holder closed
-                    holderClampPos = 0.5;
+                    clampPos = 0.5;
                 }
                 intake.setPower(0);
             }
 
             //right bumper and trigger of gamepad 2 open and close clamp
             if (gamepad2.right_bumper) {
-                holderClampPos -= 0.025;
+                clampPos -= 0.025;
                 //open
             } else if (gamepad2.right_trigger > 0) {
-                holderClampPos += 0.05;
+                clampPos += 0.05;
                 //close
             }
 
             //gamepad 2 right stick manually pivots tray
             if (Math.abs(gamepad2.right_stick_y) > 0.1) {
-                armPos -= -(gamepad2.right_stick_y / (1 / 0.004));
+                trayPos -= -(gamepad2.right_stick_y / (1 / 0.004));
             }
 
             //set limits on the holder clamp
-            if (holderClampPos > 1) {
-                holderClampPos = 1;
-            } else if (holderClampPos<0.43) {
-                holderClampPos = 0.43;
+            if (clampPos > 1) {
+                clampPos = 1;
+            } else if (clampPos < 0.43) {
+                clampPos = 0.43;
             }
 
             //set limits on how much the tray can pivot
-            if (armPos > 1) {
-                armPos = 1;
-            } else if (armPos < 0) {
-                armPos = 0;
+            if (trayPos > 1) {
+                trayPos = 1;
+            } else if (trayPos < 0) {
+                trayPos = 0;
             }
 
-            //set intake/outtake positions for arm
+            //set intake/outtake positions for tray
             if (gamepad2.a) {
-                armPos = 0.594; //down (intake) position
+                trayPos = 0.594; //down (intake) position
             }
-
-            if(gamepad2.y == true) {
-                armPos = 0.855; //up (outtake) position
+            if (gamepad2.y) {
+                trayPos = 0.855; //up (outtake) position
             }
 
             //the earlier conditionals set variables based on what was pressed
             //here the servos are actually set to those variables
-            holderClamp.setPosition(holderClampPos);
-            arm.setPosition(armPos);
+            clamp.setPosition(clampPos);
+            tray.setPosition(trayPos);
 
             //GAMEPAD 1 CONTROLS DRIVER
 
             //gamepad 1 right bumper launches drone
             if (gamepad1.right_bumper) {
-                planeLauncher.setPosition(0.7);
+                launcher.setPosition(0.7);
+            }
+
+            //y is intake at back, a is intake at front
+            //TODO: test this and all polarity logic
+            if (gamepad1.a) {
+                polarity = 1;
+            } else if (gamepad1.y) {
+                polarity = -1;
             }
 
             //doubles for amount of input for straight, turning, and mecanuming variables
-            double forwardBackward = gamepad1.left_stick_y * -0.75;
-            double turning = gamepad1.right_stick_x * 0.75;
-            double mecanuming = gamepad1.left_stick_x * 0.75;
+            double straight;
+            double turning;
+            double mecanuming;
+
+            //setting forward and mecanum based on where the front is
+            straight = gamepad1.left_stick_y * polarity * -0.75;
+            mecanuming = gamepad1.left_stick_x * polarity * 0.75;
+
+            //turning stays the same
+            turning = gamepad1.right_stick_x * 0.75;
 
             //set powers using this input
-            double fLeftPower = forwardBackward + turning + mecanuming;
-            double fRightPower = forwardBackward - turning - mecanuming;
-            double bLeftPower = forwardBackward + turning - mecanuming;
-            double bRightPower = forwardBackward - turning + mecanuming;
+            double fLeftPower = straight + turning + mecanuming;
+            double fRightPower = straight - turning - mecanuming;
+            double bLeftPower = straight + turning - mecanuming;
+            double bRightPower = straight - turning + mecanuming;
 
             //scale powers
             double maxPower = maxAbsValueDouble(fLeftPower, bLeftPower, fRightPower, bRightPower);
@@ -222,21 +246,7 @@ public class TeleOp extends LinearOpMode {
                 bRightPower /= scale;
             }
 
-            //TODO: this looks duplicate
-            lFront.setPower(fLeftPower);
-            rFront.setPower(fRightPower);
-            lBack.setPower(bLeftPower);
-            rBack.setPower(bRightPower);
-
-            double[] power = {
-                    fLeftPower,
-                    fRightPower,
-                    bLeftPower,
-                    bRightPower
-            };
-
-            power = robot.scalePowers(power);
-            robot.setMotorPower(power);
+            robot.setMotorPower(fLeftPower, fRightPower, bLeftPower, bRightPower);
         }
     }
 
