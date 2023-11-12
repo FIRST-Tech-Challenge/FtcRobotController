e105fea42906cf4fd13125cd14ca1e36881d0a7f
WeDidStartTheFire <ftcwedidthestartthefire@gmail.com>
2023-11-12 13:14:10 -0500

    Auto 1.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageAuto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CenterStageAuto1.java
@@ -29,13 +29,17 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.robotcore.external.navigation.*;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
@@ -73,12 +77,13 @@ public class CenterStageAuto1 extends LinearOpMode {
     private static final boolean USE_WEBCAM = true;
 
     private TfodProcessor tfod;
-    private ElapsedTime runtime = new ElapsedTime();
+    private final ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor rightBackDrive = null;
     private DcMotor carWashMotor = null;
+    private IMU imu = null;
 
     // Calculate the COUNTS_PER_INCH for your specific drive train.
     // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
@@ -86,12 +91,14 @@ public class CenterStageAuto1 extends LinearOpMode {
     // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
     // This is gearing DOWN for less speed and more torque.
     // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
-    static final double     COUNTS_PER_MOTOR_REV    = ((((1+(46/17))) * (1+(46/11))) * 28) ;    // eg: TETRIX Motor Encoder
+    static final double     COUNTS_PER_MOTOR_REV    = (double) ((((1+(46.0/17))) * (1+(46.0/11))) * 28) ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // No External Gearing
     static final double     WHEEL_DIAMETER_INCHES   = 3.77953 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415926535897932384626433832795028841);
     static final double     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.5;
+    double carWashPower = 1.0;
+
     private VisionPortal visionPortal;
 
     public void runOpMode() {
@@ -101,11 +108,15 @@ public class CenterStageAuto1 extends LinearOpMode {
         leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBack");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "rightFront");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
+        carWashMotor = hardwareMap.get(DcMotor.class, "liftMotor");
 
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+
         initTfod();
 
         telemetry.addData("Status", "Initialized");
@@ -115,17 +126,37 @@ public class CenterStageAuto1 extends LinearOpMode {
         runtime.reset();
 
 
-        // Step through each leg of the path,
-        // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        //*
-        encoderDrive(DRIVE_SPEED,  32,  32, 5.0);
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);
-        //*/
+        // Main code
+
+        drive(14);
+        List<Recognition> pixels = telemetryTfod();
+        if (pixels.size() > 0) {
+            drive(6);
+            ejectPixel();
+            drive(-6);
+        } else {
+            turn(-30);
+            pixels = telemetryTfod();
+            if (pixels.size() > 0) {
+                drive(6);
+                ejectPixel();
+                drive(-6);
+                turn(30);
+            } else {
+                turn(60);
+                drive(6);
+                ejectPixel();
+                drive(-6);
+                turn(-30);
+            }
+        }
+        turn(90);
+        drive(24);
+        ejectPixel();
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
-        sleep(1000);  // pause to display final telemetry message.
+        sleep(1000);  // Pause to display final telemetry message.
     }
 
     /*
@@ -136,9 +167,7 @@ public class CenterStageAuto1 extends LinearOpMode {
      *  2) Move runs out of time
      *  3) Driver stops the OpMode running.
      */
-    public void encoderDrive(double speed,
-                             double leftInches, double rightInches,
-                             double timeoutS) {
+    public void encoderDrive(double speed, double leftInches, double rightInches, double timeoutS) {
         int newLeftBackTarget;
         int newRightBackTarget;
         int newLeftFrontTarget;
@@ -150,10 +179,11 @@ public class CenterStageAuto1 extends LinearOpMode {
             // Determine new target position, and pass to motor controller
             newLeftBackTarget = leftBackDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
             newRightBackTarget = rightBackDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
-            leftBackDrive.setTargetPosition(newLeftBackTarget);
-            rightBackDrive.setTargetPosition(newRightBackTarget);
             newLeftFrontTarget = leftFrontDrive.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
             newRightFrontTarget = rightFrontDrive.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+
+            leftBackDrive.setTargetPosition(newLeftBackTarget);
+            rightBackDrive.setTargetPosition(newRightBackTarget);
             leftFrontDrive.setTargetPosition(newLeftFrontTarget);
             rightFrontDrive.setTargetPosition(newRightFrontTarget);
 
@@ -177,28 +207,86 @@ public class CenterStageAuto1 extends LinearOpMode {
             // However, if you require that BOTH motors have finished their moves before the robot continues
             // onto the next step, use (isBusy() || isBusy()) in the loop test.
             while (opModeIsActive() && (runtime.seconds() < timeoutS) && (leftBackDrive.isBusy() && rightBackDrive.isBusy() && leftFrontDrive.isBusy() && rightFrontDrive.isBusy())) {
-
                 // Display it for the driver.
                 telemetry.addData("Running to",  " %7d :%7d", newLeftFrontTarget,  newRightFrontTarget);
                 telemetry.addData("Currently at",  " at %7d :%7d", leftFrontDrive.getCurrentPosition(), rightFrontDrive.getCurrentPosition());
                 telemetry.update();
             }
 
-            // Stop all motion;
-            leftBackDrive.setPower(0);
-            rightBackDrive.setPower(0);
-            leftFrontDrive.setPower(0);
-            rightFrontDrive.setPower(0);
+            //  stopRobot();
 
             // Turn off RUN_TO_POSITION
+            // Note: Following code is technically redundant since called in stopRobot(), but the function
+            // may be changed, so do not delete.
             leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-            sleep(250);   // optional pause after each move.
-        }}
-    private void initTfod() {
+            //sleep(250);   // optional pause after each move.
+        }
+    }
+
+    public void turn(double degrees) {
+        double TURN_ACCURACY = 1.5;
+        if (false) { // Boolean determines the method the robot takes to turn x degrees
+            encoderDrive(TURN_SPEED, degrees / 7.5, -degrees / 7.5, degrees / 36);
+        } else {
+            double startAngle = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+            while (imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - startAngle < degrees - TURN_ACCURACY || imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - startAngle > degrees + TURN_ACCURACY) {
+                leftBackDrive.setPower(-degrees / Math.abs(degrees) * TURN_SPEED);
+                rightBackDrive.setPower(degrees / Math.abs(degrees) * TURN_SPEED);
+                leftFrontDrive.setPower(-degrees / Math.abs(degrees) * TURN_SPEED);
+                rightFrontDrive.setPower(degrees / Math.abs(degrees) * TURN_SPEED);
+            }
+            stopRobot();
+        }
+    }
+
+    public void drive(double inches) {
+        double startAngle = imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        int checks = 5; // Number of times the robot will check its orientation during a single drive movement and correct itself
+        for(int i = 0; i < checks; i++) {
+            encoderDrive(DRIVE_SPEED, inches / checks, inches / checks, inches / checks / 7.5 + 1);
+            turn(startAngle - imu.getRobotOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle);
+            stopRobot();
+        }
+    }
+
+    public void ejectPixel() {
+        ElapsedTime t = runtime;
+        carWashMotor.setPower(carWashPower);
+        while((int) t.milliseconds() + 500 > (int) runtime.milliseconds()) {}
+        carWashMotor.setPower(0);
+    }
+
+    public void stopRobot() {
+        // Turn On RUN_TO_POSITION
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // Stop all motion
+        leftBackDrive.setTargetPosition(leftBackDrive.getCurrentPosition());
+        rightBackDrive.setTargetPosition(rightBackDrive.getCurrentPosition());
+        leftFrontDrive.setTargetPosition(leftFrontDrive.getCurrentPosition());
+        rightFrontDrive.setTargetPosition(rightFrontDrive.getCurrentPosition());
+
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void initTfod() {
 
         // Create the TensorFlow processor the easy way.
         tfod = TfodProcessor.easyCreateWithDefaults();
@@ -214,7 +302,7 @@ public class CenterStageAuto1 extends LinearOpMode {
 
     }
 
-    private List<Recognition> telemetryTfod() {
+    public List<Recognition> telemetryTfod() {
 
         List<Recognition> currentRecognitions = tfod.getRecognitions();
         telemetry.addData("# Objects Detected", currentRecognitions.size());
