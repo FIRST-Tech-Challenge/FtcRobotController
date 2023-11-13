4ee329befb80c5b9d29d1a7687e19bad0edc8dc7
Gavin <gmacgaming40@gmail.com>
2023-11-12 16:19:53 -0800

    Completely redid code structure to make it less jank, added visual bars signifying screen segments, added many comments to code.

--- Team6220_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team6220_CENTERSTAGE/ColorDetectionTests/RedDetectTest.java
+++ Team6220_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team6220_CENTERSTAGE/ColorDetectionTests/RedDetectTest.java
@@ -1,21 +1,22 @@
 package org.firstinspires.ftc.team6220_CENTERSTAGE.ColorDetectionTests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.team6220_CENTERSTAGE.ColorDetectionPipeline;
+import org.firstinspires.ftc.team6220_CENTERSTAGE.ColorDetection;
 import org.firstinspires.ftc.team6220_CENTERSTAGE.Constants;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
 
 @Autonomous(name = "RedDetectTest")
-public class RedDetectTest extends InitializeCameras {
+public class RedDetectTest extends LinearOpMode {
+
+    // passing in makes it use redDetect so the camera can be accessed
+    ColorDetection redDetect = new ColorDetection(this);
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-        initialize(Constants.RED_COLOR_DETECT_MIN_HSV, Constants.RED_COLOR_DETECT_MAX_HSV);
+        // Initialize camera and such, specifying color ranges in the process
+        redDetect.init(Constants.RED_COLOR_DETECT_MIN_HSV, Constants.RED_COLOR_DETECT_MAX_HSV);
 
         telemetry.addLine("Waiting For Start");
         telemetry.addLine("Camera Dimensions: " + Constants.CAMERA_WIDTH + ", " + Constants.CAMERA_HEIGHT);
@@ -24,10 +25,11 @@ public class RedDetectTest extends InitializeCameras {
         waitForStart();
 
         while(opModeIsActive()) {
-            telemetry.addData("propPos: ", pipeline1.returnZone());
+            //Displays readings of detected object
+            telemetry.addData("propPos: ", redDetect.returnZone());
+            telemetry.addData("Contour XPos", redDetect.centerPosX);
+            telemetry.addData("Contour YPos", redDetect.centerPosY);
             telemetry.update();
-
-            sleep(100);
         }
 
     }
