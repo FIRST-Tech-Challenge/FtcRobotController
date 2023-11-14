d408fa894aa7c3ed7ab04eb739f4955bac54f703
Rowan Shenoy <rshenoy309@gmail.com>
2023-11-14 11:22:11 -0500

    Adds Basic code for Blue Audience side aunton and so far just moves forward and turns.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/CostaNewTest.java
@@ -0,0 +1,102 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "CostaNewTest", group = "TeleOp")
+
+public class CostaNewTest extends LinearOpMode {
+
+
+    private DcMotor frontLeft, frontRight, backLeft, backRight;
+//    private DcMotor intakeMotor;
+//    private DcMotor clawExtender;
+//    private Servo clawServo;
+//    private Servo droneLauncher;
+//    private boolean clawOpen = false;
+
+    public void runOpMode() {
+        // Initialize motors and servos
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+//        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+//        clawExtender = hardwareMap.get(DcMotor.class, "claw_extend_motor");
+//        clawServo = hardwareMap.get(Servo.class, "claw_servo");
+//        droneLauncher = hardwareMap.get(Servo.class, "drone_launcher");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        waitForStart();
+        while(opModeIsActive()){
+            double xSpeed = gamepad1.left_stick_x;
+            double ySpeed = -gamepad1.left_stick_y;
+            double rotation = gamepad1.right_stick_x;
+
+            //setting power for forward-backward movement
+            frontLeft.setPower(ySpeed);
+            //setting the power for frontLeft
+            frontRight.setPower(ySpeed);
+            //setting the power for frontRight
+            backLeft.setPower(-ySpeed);
+            //setting the power for backLeft
+            backRight.setPower(-ySpeed);
+            //setting the power for backRight
+
+            //setting up strafing
+            frontLeft.setPower(xSpeed);
+            frontRight.setPower(-xSpeed);
+            backLeft.setPower(-xSpeed);
+            backRight.setPower(xSpeed);
+
+            frontLeft.setPower(rotation);
+            frontRight.setPower(-rotation);
+            backLeft.setPower(rotation);
+            backRight.setPower(-rotation);
+
+//            double intakePower = 0.0;
+//            if (gamepad2.right_bumper) {
+//                intakePower = 1.0;
+//            }
+//            intakeMotor.setPower(intakePower);
+//
+//            double clawExtensionPower = 0.0;
+//            if (gamepad2.left_bumper) {
+//                clawExtensionPower = 0.8;
+//            }
+//            clawExtender.setPower(clawExtensionPower);
+//
+//            if (gamepad2.a) {
+//                clawServo.setPosition(0.6);
+//                clawOpen = true;
+//            }
+//
+//            if (gamepad2.b) {
+//                clawServo.setPosition(0.0);
+//                clawOpen = false;
+//            }
+//
+//            if (gamepad2.x) {
+//                droneLauncher.setPosition(0.8);
+//            } else {
+//                droneLauncher.setPosition(0.0);
+//            }
+
+        }
+
+//        frontLeft.setPower(xSpeed + ySpeed + rotation);
+//        frontRight.setPower(-xSpeed + ySpeed - rotation);
+//        backLeft.setPower(xSpeed + ySpeed + rotation);
+//        backRight.setPower(-xSpeed + ySpeed - rotation);
+
+
+    }
+
+}
