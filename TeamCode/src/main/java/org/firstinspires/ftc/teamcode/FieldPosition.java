a560d1eab85daa445dfaca88d7e4d39112d11c4f
mayurshah530 <mayur.shah@gmail.com>
2023-11-14 23:14:17 -0800

    More auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldPosition.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public enum FieldPosition {
+    UNKNOWN,
+    NEAR, // Near the scoring board
+    FAR, // Far from the scoring board
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevBotAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevBotAuto.java
@@ -32,11 +32,15 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.processors.FirstVisionProcessor;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
 
 /*
  * Demonstrates an empty iterative OpMode
@@ -49,18 +53,31 @@ public class RevBotAuto extends OpMode {
   private ElapsedTime runtime = new ElapsedTime();
   private FirstVisionProcessor visionProcessor;
   private AprilTagProcessor aprilTag;
+
+  // Used to hold the data for a detected AprilTag
+  private AprilTagDetection desiredTag = null;
   private VisionPortal visionPortal;
 
   private ScoringElementLocation selectedSide = ScoringElementLocation.UNKNOWN;
-
-  private boolean isBlue = false;
-  private boolean isFar = false;
 
   private State state = State.START;
 
   private double lastTime;
 
-  private Alliance alliance;
+  private Alliance alliance = Alliance.UNKNOWN;
+
+  private FieldPosition fieldPosition = FieldPosition.UNKNOWN;
+  private int desiredTagId;
+
+  final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
+  //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+  //  applied to the drive motors to correct the error.
+  //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+  final double SPEED_GAIN =   0.02 ;   //  Speed Control "Gain". eg: Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+  final double TURN_GAIN  =   0.01 ;   //  Turn Control "Gain".  eg: Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+  final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+  final double MAX_AUTO_TURN  = 0.25;  //  Clip the turn speed to this max value (adjust for your robot)
+
 
   /// State machine
   enum State {
@@ -72,7 +89,7 @@ public class RevBotAuto extends OpMode {
     SCORE_PIXEL,
     PARK,
     END
-  };
+  }
   /**
    * This method will be called once, when the INIT button is pressed.
    */
@@ -80,6 +97,7 @@ public class RevBotAuto extends OpMode {
   public void init() {
     robot.init();
     state = State.DETERMINE_ALLIANCE;
+    initVisionPortal();
     telemetry.addData("Status", "Initialized");
   }
 
@@ -90,9 +108,13 @@ public class RevBotAuto extends OpMode {
    */
   @Override
   public void init_loop() {
-    // TODO: Determine blue vs. red alliance and near or far position from the scoring board
+    // Determine blue vs. red alliance
+    alliance = getAllianceFromKeyEntry();
+    // Determine near or far position from the scoring board
+    fieldPosition = getFieldPositionFromKeyEntry();
 
-    if (alliance != Alliance.UNKNOWN) {
+    if (alliance != Alliance.UNKNOWN && fieldPosition != FieldPosition.UNKNOWN) {
+      // We know both alliance and field position
       state = State.IDENTIFY_SPIKE;
     }
   }
@@ -105,15 +127,9 @@ public class RevBotAuto extends OpMode {
 
     runtime.reset();
     lastTime = getRuntime();
-    visionProcessor = new FirstVisionProcessor();
-    visionPortal = new VisionPortal.Builder()
-            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
-            .addProcessors(visionProcessor, aprilTag)
-            .build();
-    // At the beginning, vision processor on and aprilTag processor off
-    visionPortal.setProcessorEnabled(aprilTag, false);
-    visionPortal.setProcessorEnabled(visionProcessor, true);
-    visionProcessor.SetAlliance(alliance);
+    if (state == State.IDENTIFY_SPIKE){
+      visionProcessor.SetAlliance(alliance);
+    }
   }
 
   /**
@@ -133,6 +149,7 @@ public class RevBotAuto extends OpMode {
         if (selectedSide != ScoringElementLocation.UNKNOWN){
           visionPortal.stopStreaming();
           visionPortal.setProcessorEnabled(visionProcessor, false);
+          telemetry.addData("selectedSide: " , selectedSide.toString());
           state = State.PLACE_PIXEL_ON_SPIKE;
           lastTime = getRuntime();
         }
@@ -143,7 +160,20 @@ public class RevBotAuto extends OpMode {
 
         break;
       case MOVE_TO_SCOREBOARD:
+        desiredTagId = getDesiredTagId(alliance, selectedSide);
         visionPortal.setProcessorEnabled(aprilTag, true);
+        // TODO: Step 1: Move based on approximation - time/ encoder / gyro.
+        // Keep looking for the desired aprilTag
+        boolean targetFound = findDesiredAprilTag(desiredTagId);
+
+        // Step 2: Move based on AprilTag location once it is found.
+        if (!targetFound){
+          break;
+        }
+        boolean targetReached = driveToAprilTagTarget(desiredTag);
+        if (!targetReached){
+          break;
+        }
         state = State.SCORE_PIXEL;
         lastTime = getRuntime();
 
@@ -177,6 +207,100 @@ public class RevBotAuto extends OpMode {
    */
   @Override
   public void stop() {
-
   }
+
+  /**
+   * Init vision portal. AprilTag and VisionProcessor
+   */
+  private void initVisionPortal() {
+    visionProcessor = new FirstVisionProcessor();
+    // Create the AprilTag processor by using a builder.
+    aprilTag = new AprilTagProcessor.Builder().build();
+    aprilTag.setDecimation(1);
+    visionPortal = new VisionPortal.Builder()
+            .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+            .addProcessors(visionProcessor, aprilTag)
+            .build();
+    // At the beginning, vision processor on and aprilTag processor off
+    visionPortal.setProcessorEnabled(aprilTag, false);
+    visionPortal.setProcessorEnabled(visionProcessor, true);
+  }
+
+  // TODO: Implement this
+  private int getDesiredTagId(Alliance alliance, ScoringElementLocation selectedSide){
+    if (alliance == Alliance.UNKNOWN || selectedSide == ScoringElementLocation.UNKNOWN){
+      return -1;
+    }
+
+    return 5;
+  }
+
+  // TODO: Implement this
+  private Alliance getAllianceFromKeyEntry(){
+    return Alliance.UNKNOWN;
+  }
+
+
+  // TODO: Implement this
+  private FieldPosition getFieldPositionFromKeyEntry(){
+    return FieldPosition.UNKNOWN;
+  }
+
+  boolean findDesiredAprilTag(int desiredTagId){
+    boolean targetFound = false;
+    desiredTag  = null;
+
+    // Step through the list of detected tags and look for a matching tag
+    List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+    for (AprilTagDetection detection : currentDetections) {
+      // Look to see if we have size info on this tag.
+      if (detection.metadata != null) {
+        //  Check to see if we want to track towards this tag.
+        if ((desiredTagId < 0) || (detection.id == desiredTagId)) {
+          // Yes, we want to use this tag.
+          targetFound = true;
+          desiredTag = detection;
+          break;  // don't look any further.
+        } else {
+          // This tag is in the library, but we do not want to track it right now.
+          telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+        }
+      } else {
+        // This tag is NOT in the library, so we don't have enough information to track to it.
+        telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+      }
+    }
+    if (targetFound) {
+      telemetry.addData("Found", "ID %d (%s)", desiredTag.id, desiredTag.metadata.name);
+      telemetry.addData("Range",  "%5.1f inches", desiredTag.ftcPose.range);
+      telemetry.addData("Bearing","%3.0f degrees", desiredTag.ftcPose.bearing);
+    }
+    return targetFound;
+  }
+
+  boolean driveToAprilTagTarget(AprilTagDetection desiredTag){
+    if (desiredTag == null){
+      return false;
+    }
+
+    // Determine heading and range error so we can use them to control the robot automatically.
+    double  rangeError   = (desiredTag.ftcPose.range - DESIRED_DISTANCE);
+    double  headingError = desiredTag.ftcPose.bearing;
+
+    // Use the speed and turn "gains" to calculate how we want the robot to move.  Clip it to the maximum
+    double drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+    double turn  = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN) ;
+
+    telemetry.addData("Auto","Drive %5.2f, Turn %5.2f", drive, turn);
+    telemetry.addData("Auto","Range Err %5.2f, Heading Err %5.2f", rangeError, headingError);
+    robot.driveRobot(drive, turn);
+
+    // TODO: Tune this
+    if (rangeError < 2) {
+      return true;
+    }
+
+    return false;
+  }
+
 }
