930a4c829e118d1f478791fae2dc01f9c4230357
Glenn <gbuckholtz@gmail.com>
2023-11-15 21:17:31 -0500

    created movement class, cleared mentions of gamepad from loop, started an if statement for Auto in loop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Auto1_BlueFieldLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/Auto1_BlueFieldLeft.java
@@ -31,13 +31,13 @@ package org.firstinspires.ftc.teamcode.opmode.auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.pipeline.GripPipelineGreenPixelRGB;
 import org.firstinspires.ftc.teamcode.utility.GamepiecePosition;
+import org.firstinspires.ftc.teamcode.utility.Movement;
 import org.opencv.core.Point;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
@@ -87,13 +87,22 @@ public class Auto1_BlueFieldLeft extends OpMode {
     OpenCvWebcam webcam;
     GripPipelineGreenPixelRGB pipeline;
 
+    Movement moveTo;
+
+    private String gamepieceLocation;
+
+    int state;
+
     @Override
     public void init_loop(){
+        state = 0;
         GamepiecePosition gamepiecePOS = new GamepiecePosition(pipeline.avgContourCoord());
+        gamepieceLocation = gamepiecePOS.getPOS();
         Point avgLoc = pipeline.avgContourCoord();
         telemetry.addData("AvgContour.x",avgLoc.x);
         telemetry.addData("AvgContour.y",avgLoc.y);
         telemetry.addData("location", gamepiecePOS.getPOS());
+        telemetry.addData("state", state);
         telemetry.update();
     }
     @Override
@@ -143,83 +152,46 @@ public class Auto1_BlueFieldLeft extends OpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
+        moveTo = new Movement(leftFrontDrive, rightFrontDrive, leftBackDrive, rightBackDrive);
+
+        state = 0;
         //drive speed limiter
         //double powerFactor = 0.25;
 
         // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
+
         telemetry.update();
 
 
         runtime.reset();
     }
         // run until the end of the match (driver presses STOP)
-        public void loop(){
+
+    @Override
+    public void loop(){
+
+        //moveTo.Forward(200);
 
 
+        if (gamepieceLocation == "left" && state == 0){
+            moveTo.Forward(200);
+            telemetry.addData("run", state);
+            telemetry.update();
 
-            double max;
+                state = 1;
+            } else if (gamepieceLocation == "center" && state == 0) {
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
 
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                state = 2;
             }
 
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
-
-            // Scale the Power down for the 12:1 ultraplanetary setup
-            double powerFactor = 0.25;
-            leftFrontPower = leftFrontPower * powerFactor;
-            leftBackPower = leftBackPower * powerFactor;
-            rightFrontPower = rightFrontPower * powerFactor;
-            rightBackPower = rightBackPower * powerFactor;
-
-            // Send calculated power to wheels
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Drive Power multiplier", powerFactor);
+            telemetry.addData("state", state);
+            telemetry.addData("location", gamepieceLocation);
             telemetry.update();
+
         }
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/Movement.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.utility;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Movement {
+    public static enum Direction {
+        FRONT,
+        BACK,
+        LEFT,
+        RIGHT
+    }
+    private DcMotor lfDrive;
+    private DcMotor rfDrive;
+    private DcMotor lbDrive;
+    private DcMotor rbDrive;
+    public Movement(DcMotor leftFrontDrive,DcMotor rightFrontDrive,
+                    DcMotor leftBackDrive, DcMotor rightBackDrive){
+        lfDrive = leftFrontDrive;
+        rfDrive = rightFrontDrive;
+        lbDrive = leftBackDrive;
+        rbDrive = rightBackDrive;
+    }
+
+    private void initMovement(){
+        lfDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        lfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Turn the motor back on when we are done
+
+        rfDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        rfDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Turn the motor back on when we are done
+
+        lbDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        lbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Turn the motor back on when we are done
+
+        rbDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // Reset the motor encoder
+        rbDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // Turn the motor back on when we are done
+    }
+
+    public void Forward(int ticks){
+        initMovement();
+        lfDrive.setTargetPosition(ticks); // Tells the motor that the position it should go to is desiredPosition
+        rfDrive.setTargetPosition(ticks); // Tells the motor that the position it should go to is desiredPosition
+        lbDrive.setTargetPosition(ticks); // Tells the motor that the position it should go to is desiredPosition
+        rbDrive.setTargetPosition(ticks); // Tells the motor that the position it should go to is desiredPosition
+        lfDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rfDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lbDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        rbDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+}
