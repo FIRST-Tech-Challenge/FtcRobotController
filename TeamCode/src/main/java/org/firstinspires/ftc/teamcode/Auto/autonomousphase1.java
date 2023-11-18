3a7179132c4d59a30798a6e2adcc4ecf78630c57
AndrewXue07 <137796590+AndrewXue07@users.noreply.github.com>
2023-11-18 15:18:56 -0500

    minor package fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/autonomousphase1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/autonomousphase1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
