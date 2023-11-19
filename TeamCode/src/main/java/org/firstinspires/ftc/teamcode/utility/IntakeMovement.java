b986acdaab63253d3cbd9f4c70de7169e4bcdced
Glenn <gbuckholtz@gmail.com>
2023-11-19 16:57:37 -0500

    Added javadoc comments to IntakeMovement class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/IntakeMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/IntakeMovement.java
@@ -6,9 +6,11 @@ import android.annotation.SuppressLint;
 
 import com.qualcomm.robotcore.hardware.Servo;
 
+/**
+ * This class contains methods that control our claw and wrist intake system.
+ */
 public class IntakeMovement {
-    static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
-    static final int    CYCLE_MS    =   50;     // period of each cycle
+
     // Right claw
     static final double RIGHT_MAX_POS     =  0.4;     // Maximum rotational position of the right servo
     static final double RIGHT_MIN_POS     =  0.0;     // Minimum rotational position of the right servo
@@ -16,35 +18,52 @@ public class IntakeMovement {
     static final double LEFT_MAX_POS     =  1.0;     // Maximum rotational position of the left servo
     static final double LEFT_MIN_POS     =  0.6;     // Minimum rotational position of the left servo
     // Intake flip
-    static final double FLIP_MAX_POS     =  1.0;     // Maximum rotational position of the intake flip servo
-    static final double FLIP_MIN_POS     =  0.35;     // Minimum rotational position intake flip servo
+    static final double FLIP_MAX_POS     =  1.0;     // Maximum rotational position of the wrist servo
+    static final double FLIP_MIN_POS     =  0.35;     // Minimum rotational position of the wrist servo
 
     public Servo rightClaw;
     public Servo leftClaw;
     public Servo intakeFlip;
 
-
+    /**
+     * Gets the hardware map information of the servos.
+     * @Param: rightC - the servo that controls the right of the claw
+     * @Param: leftC - the servo that controls the left of the claw
+     * @Param: intFlip - the servo that controls the rotation of the wrist
+     */
    public IntakeMovement(Servo rightC, Servo leftC, Servo intFlip) {
        rightClaw = rightC;
        leftClaw = leftC;
        intakeFlip = intFlip;
    }
+
+    /**
+     *  Sets both the left and right servos to an open position.
+     */
     public void ClawOpen(){
         rightClaw.setPosition(RIGHT_MAX_POS);
         leftClaw.setPosition(LEFT_MIN_POS);
     }
 
+    /**
+     * Sets both the left and right servos to a closed position.
+     */
     public void ClawClosed(){
         rightClaw.setPosition(RIGHT_MIN_POS);
         leftClaw.setPosition(LEFT_MAX_POS);
     }
 
-    // Flips the wrist upwards
+    /**
+     * Sets the wrist position downward past the floor to allow
+     * the claw servos to touch it.
+     */
     public void FlipDown(){
        intakeFlip.setPosition(FLIP_MAX_POS);
     }
 
-    // Flips the wrist downwards
+    /**
+     * Sets the wrist position upward about 180 degrees from the floor.
+     */
     public void FlipUp(){
        intakeFlip.setPosition(FLIP_MIN_POS);
     }
