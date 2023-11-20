2f7e1185f8f37e0aa02a2889b925d232c62e95b4
thunderbolts <emekhefer2630@gmail.com>
2023-11-20 18:42:49 +0200

    we now understand commands based proggramming!! yay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -4,16 +4,19 @@ import com.arcrobotics.ftclib.command.Command.*;
 import com.arcrobotics.ftclib.command.button.Trigger;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.*;
 public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
 
     private final gripper m_gripper;
 
-    public RobotContainer(HardwareMap map){
-        m_gripper = new gripper(map);
+    public RobotContainer(HardwareMap map, Telemetry telemetry){
+        m_gripper = new gripper(map,telemetry);
     }
     //bind commands to trigger
     public void bindCommands(Gamepad gamepad){
+
         new Trigger(()->gamepad.a).whileActiveContinuous(m_gripper.openGripper());
         new Trigger(()->gamepad.b).whileActiveContinuous(m_gripper.closeGripper());
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/gripper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/gripper.java
@@ -1,19 +1,25 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class gripper implements Subsystem {
     private HardwareMap map;
+    Telemetry telemetry;
     Servo serv0;
     Servo serv1;
-    public gripper(HardwareMap map){
+    public gripper(HardwareMap map, Telemetry telemetry){
         this.map=map;
+        this.telemetry=telemetry;
         serv0=map.servo.get("serv0");
         serv1=map.servo.get("serv1");
     }
@@ -25,16 +31,18 @@ public class gripper implements Subsystem {
         Subsystem.super.setDefaultCommand(defaultCommand);
     }
     public Command openGripper(){
-        return new InstantCommand(()->{
+        return new RunCommand(()->{
             serv0.setPosition(0);
-            serv1.setPosition(1);
-        });
+            serv1.setPosition(180);
+            telemetry.addLine("aaa open");
+        },this);
     };
     public Command closeGripper(){
-        return new InstantCommand(()->{
-            serv0.setPosition(1);
+        return new RunCommand(()->{
+            serv0.setPosition(180);
             serv1.setPosition(0);
-        });
+            telemetry.addLine("aaa close");
+        },this);
     };
 //    todo: maybe add toggle option
 
