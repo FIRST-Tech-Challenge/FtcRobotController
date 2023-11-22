2d7b9efc14f65d8fe926d3b27e93ef51e7ff0b75
Sanj Saini <>
2023-11-21 20:04:36 -0800

    changed files

--- Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/RandomizationBlueLeftPark.java
+++ Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/RandomizationBlueLeftPark.java
@@ -5,30 +5,5 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 public class RandomizationBlueLeftPark extends BaseAutonomous {
     public void runOpMode() {
-        // initialize the webcam and openCV pipeline
-        myColorDetection.init();
-
-        telemetry.addLine("Waiting for start");
-        telemetry.update();
-
-        waitForStart();
-
-        while (opModeIsActive()) {
-            // print detection status and coordinates of largest object
-            telemetry.addLine("Detection")
-                    .addData(" ", myColorDetection.targetDetected)
-                    .addData("x", myColorDetection.targetPoint.x)
-                    .addData("y", myColorDetection.targetPoint.y);
-
-            telemetry.addData("Frame Count", myColorDetection.robotCamera.getFrameCount());
-            telemetry.addData("FPS", String.format("%.2f", myColorDetection.robotCamera.getFps()));
-            telemetry.addData("Total frame time ms", myColorDetection.robotCamera.getTotalFrameTimeMs());
-            telemetry.addData("Pipeline time ms", myColorDetection.robotCamera.getPipelineTimeMs());
-            telemetry.addData("Overhead time ms", myColorDetection.robotCamera.getOverheadTimeMs());
-            telemetry.update();
-
-            myColorDetection.robotCamera.stopStreaming();
-            myColorDetection.robotCamera.closeCameraDevice();
-        }
     }
 }
