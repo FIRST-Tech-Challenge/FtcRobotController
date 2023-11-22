f7cfa2756959c2a41b64043b3cdd466e22d52d3d
Warren Zhou <56097394+WarZ42@users.noreply.github.com>
2023-11-22 12:04:02 -0800

    odometry tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.89; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 11.5; // in
+    public static double TRACK_WIDTH = 4.5; // in
 
     public static double kPTrans = 0.00;
     public static double kDTrans = 0.000;
@@ -60,9 +60,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01;
-    public static double kA = 0.002 ;
-    public static double kStatic = 0.09;
+    public static double kV = 0.018;
+    public static double kA = 0.0002 ;
+    public static double kStatic = 0.15;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -71,8 +71,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 60;
-    public static double MAX_ACCEL = 50;
+    public static double MAX_VEL = 120;
+    public static double MAX_ACCEL = 70;
     public static double MAX_ANG_VEL = 6;
     public static double MAX_ANG_ACCEL = 3;
 
