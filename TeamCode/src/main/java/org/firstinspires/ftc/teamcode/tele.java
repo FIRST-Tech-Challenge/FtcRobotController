434ef42e62417f24985014fbad61d07ea277a89c
Raymond Burrage <90794866+Heroberg1@users.noreply.github.com>
2023-11-23 10:53:05 -0600

    Fixing Connectivity issues and adding pixle colors for human player

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
@@ -10,49 +11,75 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @TeleOp(name = "Tele", group = "Pushbot")
 public class tele extends OpMode {
     Hardware robot = new Hardware();
+    double speedLimit;
+    double y;
+    double x;
+    double rx;
+
+    double denominator;
+    double frontLeftPower;
+    double frontRightPower;
+    double backLeftPower;
+    double backRightPower;
+
+    int pixlePickerColorTime = 5000;
 
     Gamepad.LedEffect gamepadLaunchSequenceLed = new Gamepad.LedEffect.Builder()
             .addStep(100, 0, 0, 2000)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(0,100,0,200)
-            .addStep(100,0,0,200)
-            .addStep(100,0,100,5000)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(0, 100, 0, 200)
+            .addStep(100, 0, 0, 200)
+            .addStep(100, 0, 100, 5000)
             .build();
-    Gamepad.RumbleEffect  gamepadLaunchSequenceRumble = new Gamepad.RumbleEffect.Builder()
-            .addStep(.3,.3,2000)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
-            .addStep(.5,0,200)
-            .addStep(0,.5,200)
+    Gamepad.RumbleEffect gamepadLaunchSequenceRumble = new Gamepad.RumbleEffect.Builder()
+            .addStep(.3, .3, 2000)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
+            .addStep(.5, 0, 200)
+            .addStep(0, .5, 200)
             .build();
-
 
 
     public void init() {
 
+        telemetry.addData("Robot:", "Initializing");
+        telemetry.update();
+
         robot.init(hardwareMap);
 
+        robot.leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.leftBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rightBackDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        robot.leftDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.leftBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightBackDrive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        telemetry.addData("Robot:", "Ready");
+        telemetry.update();
 
     }
 
@@ -61,7 +88,6 @@ public class tele extends OpMode {
     }
 
     public void loop() {
-        double speedLimit;
 
         if (gamepad1.right_trigger > .5) {
             speedLimit = 50;
@@ -69,26 +95,30 @@ public class tele extends OpMode {
             speedLimit = 90;
         }
 
-        double speedLimitValue = speedLimit/100;
+        double speedLimitValue = speedLimit / 100;
 
-        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
-        double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-        double rx = gamepad1.right_stick_x;
+        y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        rx = gamepad1.right_stick_x;
 
-        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-        double frontLeftPower = (y + x + rx) / denominator;
-        double frontRightPower = (y - x - rx) / denominator;
-        double backLeftPower = (y - x + rx) / denominator;
-        double backRightPower = (y + x - rx) / denominator;
-
-        robot.leftDrive.setPower(frontLeftPower * speedLimitValue);
-        robot.leftBackDrive.setPower(backLeftPower * speedLimitValue);
-        robot.rightDrive.setPower(frontRightPower * speedLimitValue);
-        robot.rightBackDrive.setPower(backRightPower * speedLimitValue);
+        denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        frontLeftPower = (y + x + rx) / denominator;
+        frontRightPower = (y - x - rx) / denominator;
+        backLeftPower = (y - x + rx) / denominator;
+        backRightPower = (y + x - rx) / denominator;
+        if ((Math.abs(gamepad1.right_stick_x) > 0.1) || (Math.abs(gamepad1.right_stick_y) > 0.1) || (Math.abs(gamepad1.left_stick_x) > 0.1) || (Math.abs(gamepad1.left_stick_y) > 0.1)) {
+            robot.leftDrive.setPower(frontLeftPower * speedLimitValue);
+            robot.leftBackDrive.setPower(backLeftPower * speedLimitValue);
+            robot.rightDrive.setPower(frontRightPower * speedLimitValue);
+            robot.rightBackDrive.setPower(backRightPower * speedLimitValue);
+        } else {
+            robot.leftDrive.setPower(0);
+            robot.leftBackDrive.setPower(0);
+            robot.rightDrive.setPower(0);
+            robot.rightBackDrive.setPower(0);
+        }
 
         robot.winch.setPower(-gamepad2.right_stick_y);
-
-
 
         if (gamepad2.dpad_up) {
             robot.hook.setPosition(1.0);
@@ -120,6 +150,26 @@ public class tele extends OpMode {
             robot.gripper.setPosition(1);
         }
 
+        if (gamepad1.x) {
+            gamepad1.setLedColor(255, 0, 255, pixlePickerColorTime);
+            gamepad2.setLedColor(255, 0, 255, pixlePickerColorTime);
+        }
+
+        if (gamepad1.y) {
+            gamepad1.setLedColor(0, 255, 0, pixlePickerColorTime);
+            gamepad2.setLedColor(0, 255, 0, pixlePickerColorTime);
+        }
+
+        if (gamepad1.b) {
+            gamepad1.setLedColor(249, 100, 0, pixlePickerColorTime);
+            gamepad2.setLedColor(249, 100, 0, pixlePickerColorTime);
+        }
+
+        if (gamepad1.a) {
+            gamepad1.setLedColor(255, 255, 255, pixlePickerColorTime);
+            gamepad2.setLedColor(255, 255, 255, pixlePickerColorTime);
+        }
+
         if (gamepad2.right_bumper && gamepad1.left_bumper) {
             gamepad1.runLedEffect(gamepadLaunchSequenceLed);
             gamepad2.runLedEffect(gamepadLaunchSequenceLed);
@@ -127,8 +177,6 @@ public class tele extends OpMode {
             gamepad2.runRumbleEffect(gamepadLaunchSequenceRumble);
         }
 
-        telemetry.addData("Right Distance", robot.rightDistance.getDistance(DistanceUnit.CM));
-        telemetry.addData("Left Distance", robot.leftDistance.getDistance(DistanceUnit.CM));
         telemetry.update();
 
     }
