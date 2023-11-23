4d9184df9b360efad7b231ac76f0e08aab0b8976
Glenn <gbuckholtz@gmail.com>
2023-11-22 21:48:39 -0500

    LinearSlideMovement variables added and initialization added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/LinearSlideMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/LinearSlideMovement.java
@@ -1,9 +1,32 @@
 package org.firstinspires.ftc.teamcode.utility;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class LinearSlideMovement {
 
+    static final int top_linearslide_ticks = 2000;
 
-    public void linearslideMax(){
+    static final int mid_linearslide_ticks = 1000;
+
+    static final int low_linearslide_ticks = 500;
+
+    static final int bottom_linearslide_ticks = 0;
+
+    private DcMotor leftLinearSlide;
+
+    private DcMotor rightLinearSlide;
+
+    public void LinearSlideMovement(DcMotor leftSlide, DcMotor rightSlide){
+        leftLinearSlide = leftSlide;
+        rightLinearSlide = rightSlide;
+    }
+
+    private void initMovement(){
+        leftLinearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightLinearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void linearslidetop(){
 
     }
 }
