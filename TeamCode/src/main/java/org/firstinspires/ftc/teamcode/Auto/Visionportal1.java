d6a48765a042ec5bd223eb748c195fac23e09804
AndrewXue07 <137796590+AndrewXue07@users.noreply.github.com>
2023-11-25 14:45:47 -0500

    init apriltag init tfod init visionportal init everything

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Visionportal1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/Visionportal1.java
@@ -1,5 +1,3 @@
-// "builder," not "easy"
-
 package org.firstinspires.ftc.teamcode.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -7,30 +5,52 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import android.util.Size;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.tfod.TfodProcessor;
 
-import java.util.List;
+import java.util.*;
 
 @Autonomous(group="Concept")
 public class Visionportal1 extends LinearOpMode {
 
-    private void initTfod() {
-        TfodProcessor myTfodProcessor;
-        myTfodProcessor = new TfodProcessor.Builder()
+    private void initProcessors() {
+        // Tfod = tensorflow object detection
+        TfodProcessor myTfodProcessor = new TfodProcessor.Builder()
                 .setMaxNumRecognitions(10) // max # recognitions
                 .setUseObjectTracker(true) // use object tracker
                 .setTrackerMaxOverlap((float) 0.2) // max % of box overlapped by another box for recognition
                 .setTrackerMinSize(16) // minimum size of a tracked/recognized object (units?)
                 .build();
-    }
 
+        AprilTagProcessor myAprilTagProcessor = new AprilTagProcessor.Builder()
+                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary()) // currentgametaglibrary = centerstage + sample apriltags
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true) // default = false
+                .setDrawCubeProjection(true) // default = false
+                .build();
+
+        VisionPortal myVisionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1")) // change name depending on mech team wants to name it
+                .addProcessor(myAprilTagProcessor) // add apriltag processor
+                .addProcessor(myTfodProcessor) // add tfod processor
+                .setCameraResolution(new Size(640, 480)) // import android.util.Size;
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG) // changed to MJPEG instead of YUY2 since it uses less bandwidth
+                // .enableCameraMonitoring(true) // enableCameraMonitoring not identified? I also didn't find any related methods in VisionPortal.java
+                .enableLiveView(true) // manually added this method because it was the closest thing to enableCameraMonitoring
+                .setAutoStopLiveView(true)
+                .build();
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
+        initProcessors();
         waitForStart();
 
         if (!isStopRequested()) {
