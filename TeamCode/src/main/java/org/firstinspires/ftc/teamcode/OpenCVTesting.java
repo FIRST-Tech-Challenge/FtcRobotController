8f80cd41371e1dcb64245e6c322d3b526f3c3663
soumyamaheshmenon <soumyamahesh@yahoo.com>
2023-11-24 21:44:01 -0600

    OpenCV testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTesting.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "HornetSquad: OpenCV Testing", group = "Hornet Vision")
+public class OpenCVTesting extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+
+}
