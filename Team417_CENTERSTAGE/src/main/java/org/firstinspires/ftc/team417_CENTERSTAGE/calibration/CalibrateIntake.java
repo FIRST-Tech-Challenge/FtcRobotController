c116643ac672816838722f3b1db83b7078dcde4a
Sawar Saini <sawar26saini@gmail.com>
2023-11-26 16:28:44 -0800

    calibrate intake action

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/calibration/CalibrateIntake.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/calibration/CalibrateIntake.java
@@ -1,22 +1,18 @@
 package org.firstinspires.ftc.team417_CENTERSTAGE.calibration;
 
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+import org.firstinspires.ftc.team417_CENTERSTAGE.baseprograms.BaseAutonomous;
 import org.firstinspires.ftc.team417_CENTERSTAGE.baseprograms.League1BaseAutonomous;
 
 @Autonomous(name="Calibrate Intake")
-public class CalibrateIntake extends League1BaseAutonomous {
+public class CalibrateIntake extends BaseAutonomous {
     @Override
     public void runOpMode() throws InterruptedException {
         initializeHardware();
         waitForStart();
 
-        if(intakeMotor != null) {
-            intakeMotor.setPower(INTAKE_SPEED2);
-            sleep((long) INTAKE_TIME2);
-            intakeMotor.setPower(0);
-        } else {
-            sleep(5000);
-        }
+        Actions.runBlocking(dropPixel());
     }
 }
