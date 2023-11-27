0fa69657c2547cea90ea65957af34f2b57ea561c
ymanchanda <ymanchanda@gmail.com>
2023-11-27 17:57:16 -0500

    Concept TFOD (pixel detection) and BlueObjectTFOD (our custom object).

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/BlueGameObjectTFOD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/BlueGameObjectTFOD.java
@@ -47,7 +47,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: TFOD", group = "Concept")
+@TeleOp(name = "Blue Object: TFOD", group = "Main")
 public class BlueGameObjectTFOD extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/ConceptTFOD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/team/ConceptTFOD.java
@@ -48,8 +48,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
+@TeleOp(name = "Concept: TFOD", group = "Concept")
 public class ConceptTFOD extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
