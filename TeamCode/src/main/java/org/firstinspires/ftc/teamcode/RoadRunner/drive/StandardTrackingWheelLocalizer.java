31e5cadbff32dffb000861b6067ab83cf894f47a
BetheBob1 <BetheBob082208@gmail.com>
2023-11-28 18:10:46 -0800

    Update StandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
@@ -28,8 +28,8 @@ import java.util.List;
  */
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
-    public static double TICKS_PER_REV = 8192;
-    public static double WHEEL_RADIUS = .688975; // in
+    public static double TICKS_PER_REV = 2000;
+    public static double WHEEL_RADIUS = 0.945; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
     public static double LATERAL_DISTANCE = 12.17; // in; distance between the left and right wheels
