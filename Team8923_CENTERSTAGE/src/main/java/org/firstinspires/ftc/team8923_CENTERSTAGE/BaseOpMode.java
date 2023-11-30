d495b4e78a084fd2fbc8aac5620c8c9e41cbe9f3
briannanguy <briannangoc@gmail.com>
2023-11-29 20:33:09 -0800

    implemented IMU but only compensates when bumper is pressed

--- Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/BaseOpMode.java
+++ Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/BaseOpMode.java
@@ -1,11 +1,17 @@
 package org.firstinspires.ftc.team8923_CENTERSTAGE;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 
 
 abstract public class BaseOpMode extends LinearOpMode {
@@ -25,13 +31,14 @@ abstract public class BaseOpMode extends LinearOpMode {
     static final double INTAKE_SPEED = 0.8;
 
     //imu constants
-    public BNO055IMU imu;
+    public IMU imu;
     public double startAngle;
     public static final double TURNING_KP = 0.008;
     public static final int ROBOT_HEADING_TOLERANCE_DEGREES = 1;
     public static final double MAXIMUM_TURN_POWER_AUTONOMOUS = 0.7;
     public static final double MINIMUM_TURN_POWER = 0.05;
 
+    public double referenceAngle;
 
     public void initHardware() {
         // drive motors
@@ -57,8 +64,31 @@ abstract public class BaseOpMode extends LinearOpMode {
 
         motorFL.setDirection(DcMotor.Direction.FORWARD);
         motorFR.setDirection(DcMotor.Direction.REVERSE);
-        motorBL.setDirection(DcMotor.Direction.REVERSE);
+        motorBL.setDirection(DcMotor.Direction.FORWARD);
         motorBR.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+       /* BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";*/
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+
+        sleep(2000);// a wait, so the imu can process
 
     }
 
@@ -68,6 +98,55 @@ abstract public class BaseOpMode extends LinearOpMode {
         motorFR.setPower(y - x - turning);
         motorBL.setPower(y - x + turning);
         motorBR.setPower(x + y - turning);
+    }
+
+    public void driveMecanumGyro(double driveAngle, double drivePower, double turnPower) {
+        // proportionality constant to correct angle error
+        double kpConstant = 0.01;
+
+        // Calculate x and y components of drive power, where y is forward (0 degrees) and x is right (-90 degrees)
+        double x = drivePower * Math.cos(Math.toRadians(driveAngle));
+        double y = drivePower * Math.sin(Math.toRadians(driveAngle));
+
+        double robotAngle;
+
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+
+        robotAngle = orientation.getYaw(AngleUnit.DEGREES);
+
+        if (turnPower != 0) {
+            referenceAngle = robotAngle;
+        }
+
+        double angleError;
+        if (gamepad1.left_bumper) {
+            angleError = robotAngle - referenceAngle;
+        } else {
+            angleError = 0;
+            referenceAngle = robotAngle;
+        }
+
+        if (angleError > 180.0) {
+            angleError -= 360.0;
+        }
+
+        double pivotCorrection = angleError * kpConstant;
+
+        double sumPivotTurning = pivotCorrection + turnPower;
+
+        double powerFL = x + y + sumPivotTurning;
+        double powerFR = y - x - sumPivotTurning;
+        double powerBL = y - x + sumPivotTurning;
+        double powerBR = y + x - sumPivotTurning;
+
+        motorFL.setPower(powerFL);
+        motorFR.setPower(powerFR);
+        motorBL.setPower(powerBL);
+        motorBR.setPower(powerBR);
+    }
+
+    double calculateDistance(double deltaX, double deltaY) {
+        return Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
     }
 
 }
--- Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/RandomizationBlueLeftPark.java
+++ Team8923_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team8923_CENTERSTAGE/RandomizationBlueLeftPark.java
@@ -9,31 +9,51 @@ public class RandomizationBlueLeftPark extends BaseAutonomous {
         waitForStart();
         switch (myColorDetection.detectColor()) {
             case ONE:
+                // forward 1 tile
                 driveInches(0, 24);
+                // pivot 90 degrees left
                 pivot(-90);
+                // release pixel
                 runIntake(0.5, 2);
+                // pivot 90 degrees right
                 pivot(90);
+                // backward 1 tile
                 driveInches(0, -24);
+                // pivot 90 degrees left
                 pivot(-90);
+                // forward 2 tiles
                 driveInches(0, 48);
                 break;
             case TWO:
+                // forward 1 tile
                 driveInches(0, 24);
+                // release pixel
                 runIntake(0.5, 2);
+                // backward 1 tile
                 driveInches(0, -24);
+                // pivot 90 degrees left
                 pivot(-90);
+                // forward 2 tiles
                 driveInches(0, 48);
                 break;
             case THREE:
+                // forward 1 tile
                 driveInches(0, 24);
+                // pivot 90 degrees right
                 pivot(90);
+                // release pixel
                 runIntake(0.5, 2);
+                // pivot 90 degrees left
                 pivot(-90);
+                // backward 1 tile
                 driveInches(0, -24);
+                // pivot 90 degrees left
                 pivot(-90);
+                // forward 2 tiles
                 driveInches(0, 48);
                 break;
             case FOUR:
+                // this is just parking tbh ngl (forward 4 inches, pivot left, forward 1 tiles)
                 driveInches(0, 4);
                 pivot(-90);
                 driveInches(0, 24);
