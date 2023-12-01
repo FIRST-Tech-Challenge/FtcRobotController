ee3027464522aa82980225ca27d2ca679ab24e3a
Zechariah Sutjandra <zeluczon2@gmail.com>
2023-11-30 21:00:33 -0600

    Re-introduce the code for Red

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoSpeedRed.java
@@ -0,0 +1,21 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+/*
+This is a copy of the FirstAutonomousIterationMirror just the last portion to make sure we go park
+
+ */
+
+
+@Autonomous(name="Speedy Red: Auto V0.2", group="Robot")
+//@Disabled
+
+public class AutoSpeedRed extends AutoSpeedIncreases {
+
+    public AutoSpeedRed() {
+        super(-1);
+    }
+
+}
