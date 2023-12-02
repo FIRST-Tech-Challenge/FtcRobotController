6fb86720fe43b3659002ea8b381c34627530e3d0
BetheBob1 <BetheBob082208@gmail.com>
2023-12-01 17:46:15 -0800

    Rotation Code. Commented stuff is for the rotating mechanism and current code is for slide extension.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/rotationTest.kt
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.Teleop
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import com.qualcomm.robotcore.hardware.DcMotor
+import org.firstinspires.ftc.teamcode.DriveMethods
+import org.firstinspires.ftc.teamcode.Variables
+import org.firstinspires.ftc.teamcode.Variables.slideRotationMotor
+
+@TeleOp(name = "RotationMotor", group = "Testing")
+class RotationTest: DriveMethods() {
+    override fun runOpMode() {
+        //var motorBeingTested = hardwareMap.get<DcMotor>(DcMotor::class.java, "slideRotationMotor")
+        var motorBeingTested = hardwareMap.get<DcMotor>(DcMotor::class.java, "motorSlideLeft")
+
+        motorBeingTested!!.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        motorBeingTested!!.mode = DcMotor.RunMode.RUN_USING_ENCODER
+        motorBeingTested!!.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        waitForStart()
+
+        while (opModeIsActive()) {
+            motorBeingTested.targetPosition = ((motorBeingTested!!.currentPosition) + gamepad2.left_stick_y.toDouble()*10).toInt()
+            if (gamepad2.left_stick_y >0) {
+                motorBeingTested.power = -.3
+                //motorBeingTested.power = .05
+            }
+            else if (gamepad2.left_stick_y <0){
+                motorBeingTested.power = .3
+                //motorBeingTested.power = -.05
+            }
+            else {
+                motorBeingTested.power = 0.0
+            }
+
+            telemetry.addData("Motor Value: ", motorBeingTested.currentPosition)
+            telemetry.update()
+        }
+    }
+}
