5fbcdc62ff7c65ee66407f43d9f303f5a0b1aaee
Oopdawg <94260533+Oopdawg@users.noreply.github.com>
2023-12-02 08:45:32 -0500

    Saturday morning early fixes second wave: -open claw on backdrop in autonomous -clone LeftRed from leftbue and rightblue coordinates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoLeftRed.java
@@ -22,18 +22,32 @@ public class AutoLeftRed extends AutoBase {
         myLocalizer.setPoseEstimate(c.preStartPoseRedLeft);
         drive.setPoseEstimate(c.preStartPoseRedLeft); // !!!!!
 
+        Pose2d teamPropCoordinate;
+        if (propLoc == TeamPropDetection.propLocation.LEFT) {
+            teamPropCoordinate = c.leftTeamPropRedLeft;
+        }
+        else if (propLoc == TeamPropDetection.propLocation.CENTER) {
+            teamPropCoordinate = c.centerTeamPropRedLeft;
+        }
+        else if (propLoc == TeamPropDetection.propLocation.RIGHT) {
+            teamPropCoordinate = c.rightTeamPropRedLeft;
+        }
+        else {
+            teamPropCoordinate = c.rightTeamPropRedLeft;
+        }
         // hardware map to get motors and sensors
         TrajectorySequence dropPropPixelRight = drive.trajectorySequenceBuilder(c.preStartPoseRedLeft)
                 //.lineTo(c.leftTeamProp)
                 //.lineTo(c.centerTeamProp)
                 .lineToLinearHeading(c.startPoseRedLeft)
-                .lineToLinearHeading(c.rightTeamPropRedLeft)
+                .lineToLinearHeading(teamPropCoordinate)
                 .back(3.5)
+                .forward(3)
                 .build();
 
         TrajectorySequence goToBackdrop = drive.trajectorySequenceBuilder(dropPropPixelRight.end())
                 .lineToLinearHeading(c.rightBackdropIntermediateCenter)
-                .lineToLinearHeading(c.rightBackdropRight, SampleMecanumDrive.getVelocityConstraint(SLOWERVELOCITY, SLOWERANGULARVELOCITY, DriveConstants.TRACK_WIDTH),
+                .lineToLinearHeading(c.rightBackdropLeft, SampleMecanumDrive.getVelocityConstraint(SLOWERVELOCITY, SLOWERANGULARVELOCITY, DriveConstants.TRACK_WIDTH),
                         SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
                 .build();
 
@@ -41,11 +55,11 @@ public class AutoLeftRed extends AutoBase {
         robot.updateSync();
         // Test propLoc here
         drive.followTrajectorySequence(dropPropPixelRight);
-
-        robot.outtakePixels = true;
-        robot.updateSync();
-        drive.followTrajectorySequence(goToBackdrop);
-        robot.closeClaw = false;
-        robot.updateSync();
+//
+//        robot.outtakePixels = true;
+//        robot.updateSync();
+//        drive.followTrajectorySequence(goToBackdrop);
+//        robot.closeClaw = false;
+//        robot.updateSync();
     }
 }
