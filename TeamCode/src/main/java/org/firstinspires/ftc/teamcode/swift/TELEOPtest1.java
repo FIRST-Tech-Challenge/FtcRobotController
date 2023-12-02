6de4340654ce5246cb2cd29363e597690dfd7a1c
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-12-02 12:50:38 -0600

    Update TELEOPtest1.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest1.java
@@ -1,14 +1,17 @@
 package org.firstinspires.ftc.teamcode.swift;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.shared.MotionHardware;
 @TeleOp
 public class TELEOPtest1 extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
     static final double INCREMENT = 0.01;     // amount to ramp motor each CYCLE_MS cycle
     static final int CYCLE_MS = 50;     // period of each cycle
     static final double MAX_FWD = 1.0;     // Maximum FWD power applied to motor
@@ -17,11 +20,13 @@ public class TELEOPtest1 extends LinearOpMode {
     private Servo rightGripper;
     private Servo launcherServo = null;
     private Servo DroneCoverServo = null;
+    private DcMotor armMotor = null;
     MotionHardware robot = new MotionHardware(this);
 
 
     @Override
     public void runOpMode() {
+        robot.init();
 
         waitForStart();
 
@@ -96,7 +101,7 @@ public class TELEOPtest1 extends LinearOpMode {
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 2);
             double frontLeftPower = (y + x + rx) / denominator;
             double backLeftPower = (y - x + rx) / denominator;
             double frontRightPower = (y - x - rx) / denominator;
@@ -117,7 +122,7 @@ public class TELEOPtest1 extends LinearOpMode {
 
             if (gamepad2.left_bumper) {
                 // Move servos in opposite directions when "y" is pressed
-                leftGripper.setPosition(1); // Adjust the position value as needed
+                leftGripper.setPosition(1.5); // Adjust the position value as needed
 
             } else if (gamepad2.left_trigger>0.5) {
                 // Return servos to the center position when "x" is pressed
@@ -125,7 +130,7 @@ public class TELEOPtest1 extends LinearOpMode {
             }
                 if (gamepad2.right_bumper) {
                     // Move servo in opposite directions when "y" is pressed
-                    rightGripper.setPosition(0); // Adjust the position value as needed
+                    rightGripper.setPosition(-0.5); // Adjust the position value as needed
 
                 } else if (gamepad2.right_trigger>0.5) {
                     // Return servos to the center position when "x" is pressed
@@ -137,10 +142,30 @@ public class TELEOPtest1 extends LinearOpMode {
                     wristServo.setPosition(0.3); // Adjust the position value for the center position
                 }
                 if (gamepad2.y) {
-                    robot.moveRobot(.5, 2, 8);
+                   // robot.moveArm(1, 90, 5);
+                    armMotor.setTargetPosition(-4185);
+                    armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    runtime.reset();
+                    armMotor.setPower(Math.abs(1));
+                    while (opModeIsActive() &&
+                            (runtime.seconds() < 5) &&
+                            (armMotor.isBusy()));
+                    armMotor.setPower(0);
+
+                    armMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
                 }
                 else if (gamepad2.x) {
-                    robot.moveRobot(.5, -2, 8);
+                    //robot.moveArm(1, -90, 5);
+                    armMotor.setTargetPosition(1);
+                    armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    runtime.reset();
+                    armMotor.setPower(Math.abs(1));
+                    while (opModeIsActive() &&
+                            (runtime.seconds() < 5) &&
+                            (armMotor.isBusy()));
+                    armMotor.setPower(0);
+
+                    armMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
                 }
             telemetry.update();
             }
