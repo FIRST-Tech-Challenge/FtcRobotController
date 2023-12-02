9d9788a9607f8450189d1b3c6946c59f9a105f2f
aadyaa arora <aadyaaarora29@gmail.com>
2023-12-02 14:27:01 -0800

    stuff

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/roadrunner/ThreeDeadWheelLocalizer.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/roadrunner/ThreeDeadWheelLocalizer.java
@@ -17,9 +17,9 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 @Config
 public final class ThreeDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double par0YTicks = 0.0; // y position of the first parallel encoder (in tick units)
-        public double par1YTicks = 1.0; // y position of the second parallel encoder (in tick units)
-        public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
+        public double par0YTicks = 11926.693419931953; // y position of the first parallel encoder (in tick units)
+        public double par1YTicks = -11520.469981279093; // y position of the second parallel encoder (in tick units)
+        public double perpXTicks = 10858.238797442646; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
@@ -39,9 +39,9 @@ public final class ThreeDeadWheelLocalizer implements Localizer {
         lastPar1Pos = par1.getPositionAndVelocity().position;
         lastPerpPos = perp.getPositionAndVelocity().position;
 
-        par1.setDirection(DcMotorSimple.Direction.REVERSE);
-        par0.setDirection(DcMotorSimple.Direction.REVERSE);
-        //perp.setDirection(DcMotorSimple.Direction.REVERSE);
+        //par1.setDirection(DcMotorSimple.Direction.REVERSE);
+        // par0.setDirection(DcMotorSimple.Direction.REVERSE);
+        perp.setDirection(DcMotorSimple.Direction.REVERSE);
 
         this.inPerTick = inPerTick;
 
