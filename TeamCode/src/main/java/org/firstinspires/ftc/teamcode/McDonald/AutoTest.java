d37c807b1af5720bc14dfe02c80bbcd15de79df8
Rob McDonald <pgp4privacy@gmail.com>
2023-12-02 09:23:02 -0600

    Encoder fwd/back motion, encoder turn right/left, strafe(untested), experimental move to grid(untested)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McDonald/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/McDonald/AutoTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.McDonald;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -20,25 +21,61 @@ public class AutoTest extends LinearOpMode {
         robot.init();
         vision.init();
 
+        robot.moveArm(.5, 5, 5);
+
         waitForStart();
 
+
+        // Center Strike Line
+        // - Forward: 31.75
+        // - Reverse: -44.75
         while(opModeIsActive()) {
-            PropPosition propPosition = vision.detectProp();
+            PropPosition propPosition = PropPosition.RIGHT;
 
-            // Center Strike Line
-            // - Forward: 31.75
-            // - Reverse: -44.75
+            switch(propPosition) {
+                case MIDDLE:
+                    //Drop off pixel
+                    robot.moveRobot(.5, -43.75, 10);
+                    //Pretend to drop pixel
+                    robot.dropPixel();
+                    sleep(1000);
+                    //Backup and clear pixel
+                    robot.moveRobot(.5, -5, 5);
+                    //Turn to parking location
+                    robot.turnRobot(Direction.LEFT, 16, .5, 10);
+                    //Park
+                    robot.moveRobot(.5, 30, 10);
+                    break;
+                case RIGHT:
+                    //Drop off pixel
+                    robot.moveRobot(.5, -43.75, 10);
+                    //Turn left
+                    robot.turnRobot(Direction.LEFT, 10, .5, 10);
+                    //Move to line
+                    robot.moveRobot(.5, 8, 5);
+                    //Drop pixel
+                    robot.dropPixel();
+                    //Park
+                    robot.moveRobot(.5, 40, 10);
+                    robot.turnRobot(Direction.LEFT, 14, 5, 10);
+                    robot.moveRobot(.5, 15, 5);
+                    break;
+                default:
+                    //Drop off pixel
+                    robot.moveRobot(.5, -43.75, 10);
+                    //Pretend to drop pixel
+                    robot.dropPixel();
+                    sleep(1000);
+                    //Backup and clear pixel
+                    robot.moveRobot(.5, -5, 5);
+                    //Turn to parking location
+                    robot.turnRobot(Direction.LEFT, 16, .5, 10);
+                    //Park
+                    robot.moveRobot(.5, 30, 10);
+                    break;
+            }
 
-            //Drop off pixel
-            robot.moveRobot(.5, -44.75, 10);
-            //Pretend to drop pixel
-            sleep(1000);
-            //Backup and clear pixel
-            robot.moveRobot(.5, -5, 5);
-            //Turn to parking location
-            robot.turnRobot(Direction.LEFT, 12, .5, 10);
-            //Park
-            robot.moveRobot(.5, 25, 10);
+
 
             sleep(20);
             break;
