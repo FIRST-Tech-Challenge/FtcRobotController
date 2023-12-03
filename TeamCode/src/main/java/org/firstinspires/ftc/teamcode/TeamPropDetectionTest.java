145a698e4cab610a19bf63d1b9127681b4330122
Payton <paytonbrian1126@gmail.com>
2023-12-03 13:02:54 -0500

    enable program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropDetectionTest.java
@@ -49,7 +49,6 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Team Prop Detection", group = "Tests")
-@Disabled
 public class TeamPropDetectionTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
