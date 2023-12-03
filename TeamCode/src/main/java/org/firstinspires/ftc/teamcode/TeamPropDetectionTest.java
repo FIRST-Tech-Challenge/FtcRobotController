72c171226a1bf984b0773550214a5b1c4f561c70
Owen R <27orinne@granvilleschools.org>
2023-12-03 14:31:07 -0500

    change to auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropDetectionTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamPropDetectionTest.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -48,7 +49,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Team Prop Detection", group = "Tests")
+@Autonomous(name = "Team Prop Detection", group = "Tests")
 public class TeamPropDetectionTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
