705b19109272deb3185c7d3e3240995297ffadd3
Rowan Shenoy <rshenoy309@gmail.com>
2023-12-04 11:20:59 -0500

    Adds comments to Autonomous file on plan for current auton
    In Code.txt all previous code for the autonomous was added.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAudience.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class RedAudience extends LinearOpMode {
+    //Instantiate robot class
+
+    public void runOpMode(){
+        //initialize robot
+
+        waitForStart();
+        
+        //Moving to spike mark grid square
+
+        //Move forwards one more square
+
+        //Turning 90 degrees
+
+        //Move forwards to the wall
+    }
+}
