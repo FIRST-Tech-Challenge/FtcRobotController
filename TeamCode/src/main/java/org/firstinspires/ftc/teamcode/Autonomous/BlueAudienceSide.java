4dff639d4bbfdef873af532b3b37ca8cdbc930ba
RedGaming101 <costa@milopaper.com>
2023-12-04 11:44:32 -0500

    Audience Side Basic Movement to Park
    Made the Red and Blue Audience Side Basic Autonomous to just go and park.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudienceSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueAudienceSide.java
@@ -1,21 +1,32 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RobotClass;
 
 @Autonomous()
 
 public class BlueAudienceSide extends LinearOpMode {
 
-    RobotClass robot = new RobotClass(this);
+    RobotClass teamBot = new RobotClass(this);
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot.init(hardwareMap);
+        teamBot.init(hardwareMap);
 
         waitForStart();
+
+        while (opModeIsActive()){
+            teamBot.moveWithoutEncoders(0.6, 0.6, 2500);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 1500);
+            teamBot.gyroTurning(-90);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 6000);
+            teamBot.strafing(RobotClass.Direction.LEFT, 0.5, 1000);
+        }
 
         //Moving to spike mark grid square
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAudience.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedAudience.java
@@ -1,18 +1,33 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.RobotClass;
 
 @Autonomous
 public class RedAudience extends LinearOpMode {
     //Instantiate robot class
-
-    public void runOpMode(){
+    RobotClass teamBot = new RobotClass(this);
+    public void runOpMode() throws InterruptedException {
         //initialize robot
+            waitForStart();
 
-        waitForStart();
-        
+            while (opModeIsActive()){
+                teamBot.moveWithoutEncoders(0.6, 0.6, 2500);
+                teamBot.moveWithoutEncoders(0.6, 0.6, 1500);
+                teamBot.gyroTurning(90);
+                teamBot.moveWithoutEncoders(0.6, 0.6, 6000);
+                teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 1000);
+            }
+
+
+
         //Moving to spike mark grid square
+
 
         //Move forwards one more square
 
