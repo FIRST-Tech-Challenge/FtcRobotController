30472224db0156494d74f44a2f303e4c993113a0
Warren Zhou <56097394+WarZ42@users.noreply.github.com>
2023-12-04 17:25:22 -0800

    temp commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -90,5 +90,5 @@ public class DriveConstants {
     public static double getMotorVelocityF(double ticksPerSecond) {
         // see https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit#heading=h.61g9ixenznbx
         return 32767 / ticksPerSecond;
-    }
+    } 
 }
