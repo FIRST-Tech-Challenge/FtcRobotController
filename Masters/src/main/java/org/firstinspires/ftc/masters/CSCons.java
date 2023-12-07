191a3c95d6500193b84822b69ecaa073cd4ccade
BlockBeasts <kjosien1@gmail.com>
2023-12-06 20:56:49 -0700

    added angle values

--- Masters/src/main/java/org/firstinspires/ftc/masters/CSCons.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/CSCons.java
@@ -1,10 +1,14 @@
 package org.firstinspires.ftc.masters;
 
 public class CSCons {
-    public static double[] v4b_positions = {0,0,0,0,0}; // 5 layers of pixel heights
-    public static double[] clawAngles = {0,0,0,0,0}; // first four match v4b, last is transfer into bot
+    public static double[] v4b_positions = {0.815,0,0,0,0.205}; // 5 layers of pixel heights
+    public static double[] clawAngles = {1,0,0,0,0.5}; // first four match v4b, last is transfer into bot
     public static double[] backSlidesPos = {0,0,0,0,0,0,0,0,0,0,0}; // 11 layers of scoring
     public static double[] gpSlidePos = {0,0,0,0}; // in, thirds to full extension
-    public static double[] claw = {0,0}; // open, closed
+    public static double[] claw = {0.1,0.69}; // open, closed
+
+    public static double[]outtakeAngle={0.18, 1}; // folder, transfer
+    public static double[] leftSideBack= {0.595, 0.97}; //drop, transfer
+    public static double[] rightSideBack = {0.955, 0.65};
 }
 
