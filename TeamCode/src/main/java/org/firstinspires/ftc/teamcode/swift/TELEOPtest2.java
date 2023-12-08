84ae1758a7e24deb0e48968bc99a8d6bfc3af4e1
Magnum <119624999+DawstonSwift@users.noreply.github.com>
2023-12-08 14:09:04 -0600

    teleop test 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest2.java
@@ -1,168 +1,177 @@
 package org.firstinspires.ftc.teamcode.swift;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.teamcode.shared.MotionHardware;
-
 @TeleOp
 public class TELEOPtest2 extends LinearOpMode {
-    private Servo launcherServo = null;
+
+    
+        private ElapsedTime runtime = new ElapsedTime();
+        static final double INCREMENT = 0.01;     // amount to ramp motor each CYCLE_MS cycle
+        static final int CYCLE_MS = 50;     // period of each cycle
+        static final double MAX_FWD = 1.0;     // Maximum FWD power applied to motor
+        static final double MAX_REV = -1.0;     // Maximum REV power applied to motor
+        private Servo leftGripper;
+        private Servo rightGripper;
+        private Servo launcherServo = null;
+        private Servo DroneCoverServo = null;
+        private DcMotor armMotor = null;
+        MotionHardware robot = new MotionHardware(this);
 
 
-    static final double INCREMENT = 0.01;     // amount to ramp motor each CYCLE_MS cycle
-    static final int CYCLE_MS = 50;     // period of each cycle
-    static final double MAX_FWD = 1.0;     // Maximum FWD power applied to motor
-    static final double MAX_REV = -1.0;     // Maximum REV power applied to motor
+        @Override
+        public void runOpMode() {
+            robot.init();
 
-    private Servo intakeServo;
-    // Set the target position in encoder counts
-    int upPosition = 650;
-    int homePosition = 0;
+            waitForStart();
 
-    @Override
-    public void runOpMode() {
-
-        waitForStart();
-
-        //Define variables for arm, wrist, gripper, and launcher
-        Servo wristServo;
-        DcMotor armMotor;
-        DcMotor armMotor2;
-        armMotor2 = hardwareMap.get(DcMotor.class, "armMotor2");
-        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        wristServo = hardwareMap.servo.get("wristServo");
-
-        launcherServo = hardwareMap.get(Servo.class, "launcherServo");
-        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        while (opModeIsActive()) {
-
+            // Define class members
+            DcMotor motor;
+            double power = 0;
+            boolean rampUp = true;
+            //Define variables for arm, wrist, and gripper
+            Servo wristServo, leftGripper, rightGripper;
+            DcMotor armMotor;
+            //CRServo contServo;
+            float leftY, rightY;
+            double armPosition, gripPosition, contPower;
+            double MIN_POSITION = 0, MAX_POSITION = 1;
             // called when init button is  pressed.
 
-            telemetry.addData("Mode", "waiting");
 
-
-
-            // check to see if we need to move the servo.
-            if (gamepad1.left_bumper) {
-                // move to 0 degrees.
-                launcherServo.setPosition(0.3);
-            } else if (gamepad1.right_bumper || gamepad1.left_bumper) {
-                // move to 90 degrees.
-                launcherServo.setPosition(1);
-            }
-            telemetry.addData("Servo Position", launcherServo.getPosition());
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-
-
-            // Declare our motors
-            // Make sure your ID's match your configuration
+            armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+            armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            wristServo = hardwareMap.servo.get("wristServo");
+            launcherServo = hardwareMap.get(Servo.class, "launcherServo");
+            DroneCoverServo = hardwareMap.get(Servo.class, "DroneCoverServo");
+            leftGripper = hardwareMap.get(Servo.class, "leftGripper");
+            rightGripper = hardwareMap.get(Servo.class, "rightGripper");
             DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
             DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
             DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
             DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+            //frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            //backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-            // Reverse the right side motors. This may be wrong for your setup.
-            // If your robot moves backwards when commanded to go forwards,
-            // reverse the left side instead.
-            // See the note about this earlier on this page.
-            frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+            backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+            backRightMotor.setDirection(DcMotor.Direction.REVERSE);
 
-
-            if (isStopRequested()) return;
-
-
-            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * -1.1; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-
-            //start Asher
-
-
-            // Run the motor to the target position
-            if (gamepad2.right_trigger>.5) {
-                armMotor.setTargetPosition(upPosition);
-                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor.setPower(0.2); // Set the motor power (adjust as needed)
-
-                armMotor2.setTargetPosition(upPosition);
-                armMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor2.setPower(0.2); // Set the motor power (adjust as needed)
-            } else if (gamepad2.left_trigger>.5) {
-                armMotor.setTargetPosition(homePosition);
-                armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor.setPower(0.2); // Set the motor power (adjust as needed)
-
-                armMotor2.setTargetPosition(homePosition);
-                armMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                armMotor2.setPower(0.2); // Set the motor power (adjust as needed)
-            }
-
-            while (armMotor.isBusy()) {
-                // Wait for the motor to reach the target position
-                telemetry.addData("Current Position", armMotor.getCurrentPosition());
-                telemetry.update();
-            }
-
-            // Stop the motor after reaching the target position
-            armMotor.setPower(0);
-            armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
             // check the gamepad buttons and if pressed, increment the appropriate position
             // variable to change the servo location.
 
-            // move arm down on A button if not already at lowest position.
 
-            intakeServo = hardwareMap.get(Servo.class, "intakeServo");
+            telemetry.addData("Mode", "waiting");
 
-            if (gamepad2.y) {
-                // Move servos in opposite directions when "y" is pressed
-               // intakeServo.setPosition(0); // Adjust the position value as needed
-                intakeServo.setDirection(Servo.Direction.FORWARD);
+            while (opModeIsActive()) {
+                telemetry.addData("Mode", "running");
+                // check to see if we need to move the servo.
+                if (gamepad1.left_bumper) {
+                    // move to 0 degrees.
+                    launcherServo.setPosition(0.3);
+                } else if (gamepad1.right_bumper || gamepad1.left_bumper) {
+                    // move to 90 degrees.
+                    launcherServo.setPosition(1);
+                }
+                telemetry.addData("Servo Position", launcherServo.getPosition());
+                telemetry.addData("Status", "Running");
+                telemetry.update();
 
-            } else if (gamepad2.x) {
-                // Return servos to the center position when "x" is pressed
-               // intakeServo.setPosition(1); // Adjust the position value for the center position
-                intakeServo.setDirection(Servo.Direction.REVERSE);
+                telemetry.addData("Mode", "running");
+                // check to see if we need to move the servo.
+                if (gamepad1.dpad_left) {
+                    // move to 0 degrees.
+                    DroneCoverServo.setPosition(0.3);
+                } else if (gamepad1.dpad_right) {
+                    // move to 90 degrees.
+                    DroneCoverServo.setPosition(.7);
+                }
+                telemetry.addData("Servo Position", DroneCoverServo.getPosition());
+                telemetry.addData("Status", "Running");
+                telemetry.update();
 
-            }
-            if (gamepad2.b) {
-                // Move servo in opposite directions when "y" is pressed
-                wristServo.setPosition(.8 ); // Adjust the position value as needed
+                // Declare Motors
 
-            } else if (gamepad2.a) {
-                // Return servos to the center position when "x" is pressed
-                wristServo.setPosition(.2); // Adjust the position value for the center position
+                double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
+                double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+                double rx = -gamepad1.right_stick_x;
 
+                // Denominator is the largest motor power (absolute value) or 1
+                // This ensures all the powers maintain the same ratio,
+                // but only if at least one is out of the range [-1, 1]
+                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+                double frontLeftPower = (y + x + rx) / denominator; //
+                double backLeftPower = (y - x + rx) / denominator;
+                double frontRightPower = (y - x - rx) / denominator;
+                double backRightPower = (y + x - rx) / denominator;
+
+                frontLeftMotor.setPower(frontLeftPower);
+                backLeftMotor.setPower(backLeftPower);
+                frontRightMotor.setPower(frontRightPower);
+                backRightMotor.setPower(backRightPower);
+
+                //start Asher
+                // move arm down on A button if not already at lowest position.
+
+                leftY = gamepad2.left_stick_y * -1;
+                rightY = gamepad2.right_stick_y * -1;
+                armMotor.setPower(Range.clip(leftY, -1.0, 1.0));
+                telemetry.addData("sticks", "  left=" + leftY + "  right=" + rightY);
+
+                if (gamepad2.left_bumper) {
+                    // Move servos in opposite directions when "y" is pressed
+                    leftGripper.setPosition(1.5); // Adjust the position value as needed
+
+                } else if (gamepad2.left_trigger>0.5) {
+                    // Return servos to the center position when "x" is pressed
+                    leftGripper.setPosition(0.95); // Adjust the position value for the center position
+                }
+                if (gamepad2.right_bumper) {
+                    // Move servo in opposite directions when "y" is pressed
+                    rightGripper.setPosition(-0.5); // Adjust the position value as needed
+
+                } else if (gamepad2.right_trigger>0.5) {
+                    // Return servos to the center position when "x" is pressed
+                    rightGripper.setPosition(0.05); // Adjust the position value for the center position
+                }
+                if (gamepad2.a) {
+                    wristServo.setPosition(0.7); // Adjust the position value as needed
+                } else if (gamepad2.b) {
+                    wristServo.setPosition(0.3); // Adjust the position value for the center position
+                }
+                if (gamepad2.y) {
+                    armMotor.setTargetPosition(-4185);
+                    armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    runtime.reset();
+                    armMotor.setPower(Math.abs(1));
+
+                    armMotor.setPower(0);
+
+                    armMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+                }
+                else if (gamepad2.x) {
+                    armMotor.setTargetPosition(1);
+                    armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    runtime.reset();
+                    armMotor.setPower(Math.abs(1));
+
+                    armMotor.setPower(0);
+
+                    armMotor.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
+                }
                 telemetry.update();
             }
         }
     }
-}
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/swift/TELEOPtest3.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.swift;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
-
+import org.firstinspires.ftc.teamcode.shared.MotionHardware;
 @TeleOp
 public class TELEOPtest3 extends LinearOpMode {
+
+    private ElapsedTime runtime = new ElapsedTime();
     static final double INCREMENT = 0.01;     // amount to ramp motor each CYCLE_MS cycle
     static final int CYCLE_MS = 50;     // period of each cycle
     static final double MAX_FWD = 1.0;     // Maximum FWD power applied to motor
@@ -16,105 +20,44 @@ public class TELEOPtest3 extends LinearOpMode {
     private Servo leftGripper;
     private Servo rightGripper;
     private Servo launcherServo = null;
+    private Servo DroneCoverServo = null;
+    private DcMotor armMotor = null;
+    MotionHardware robot = new MotionHardware(this);
 
     @Override
     public void runOpMode() {
+        robot.init();
 
         waitForStart();
+    public static void main(String[] args) {
+        // Creating two instances of the Runnable implementation
+        RunnableTask task1 = new RunnableTask("Driver 1");
+        RunnableTask task2 = new RunnableTask("Driver 2");
 
-        // Define class members
-        DcMotor motor;
-        double power = 0;
-        boolean rampUp = true;
-        //Define variables for arm, wrist, and gripper
-        Servo wristServo, leftGripper, rightGripper;
-        DcMotor armMotor;
-        //CRServo contServo;
-        float leftY, rightY;
-        double armPosition, gripPosition, contPower;
-        double MIN_POSITION = 0, MAX_POSITION = 1;
-        // called when init button is  pressed.
+        // Creating two Thread objects and passing the Runnable instances
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
 
-        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
-        wristServo = hardwareMap.servo.get("wristServo");
-        launcherServo = hardwareMap.get(Servo.class, "launcherServo");
-        leftGripper = hardwareMap.get(Servo.class, "leftGripper");
-        rightGripper = hardwareMap.get(Servo.class, "rightGripper");
-        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
-        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
-        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
-        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftY = gamepad2.left_stick_y * -1;
-        rightY = gamepad2.right_stick_y * -1;
-        armMotor.setPower(Range.clip(leftY, -1.0, 1.0));
-        telemetry.addData("sticks", "  left=" + leftY + "  right=" + rightY);
-
-        // check the gamepad buttons and if pressed, increment the appropriate position
-        // variable to change the servo location.
-
-
-        telemetry.addData("Mode", "waiting");
-
-        while (opModeIsActive()) {
-            telemetry.addData("Mode", "running");
-            // check to see if we need to move the servo.
-            if (gamepad1.left_bumper) {
-                // move to 0 degrees.
-                launcherServo.setPosition(0.3);
-            } else if (gamepad1.right_bumper || gamepad1.left_bumper) {
-                // move to 90 degrees.
-                launcherServo.setPosition(1);
-            }
-            telemetry.addData("Servo Position", launcherServo.getPosition());
-            telemetry.addData("Status", "Running");
-            telemetry.update();
-
-            // Declare Motors
-
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = -gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = -gamepad1.right_stick_x;
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = (y + x + rx) / denominator;
-            double backLeftPower = (y - x + rx) / denominator;
-            double frontRightPower = (y - x - rx) / denominator;
-            double backRightPower = (y + x - rx) / denominator;
-
-            frontLeftMotor.setPower(frontLeftPower);
-            backLeftMotor.setPower(backLeftPower);
-            frontRightMotor.setPower(frontRightPower);
-            backRightMotor.setPower(backRightPower);
-
-            //start Asher
-            // move arm down on A button if not already at lowest position.
-            if (gamepad2.y) {
-                // Move servos in opposite directions when "y" is pressed
-                leftGripper.setPosition(1); // Adjust the position value as needed
-                rightGripper.setPosition(0); // Adjust the position value as needed
-            } else if (gamepad2.x) {
-                // Return servos to the center position when "x" is pressed
-                leftGripper.setPosition(0.9); // Adjust the position value for the center position
-                rightGripper.setPosition(0.1); // Adjust the position value for the center position
-            }
-            if (gamepad2.b) {
-                // Move servo in opposite directions when "y" is pressed
-                wristServo.setPosition(.4 ); // Adjust the position value as needed
-
-            } else if (gamepad2.a) {
-                // Return servos to the center position when "x" is pressed
-                wristServo.setPosition(0); // Adjust the position value for the center position
-
-                telemetry.update();
-            }
-        }
+        // Starting the threads
+        thread1.start();
+        thread2.start();
     }
 }
 
+// Runnable implementation that contains the task to be performed by the threads
+class RunnableTask implements Runnable {
+    private String threadName;
+
+    public RunnableTask(String threadName) {
+        this.threadName = threadName;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(threadName + " - Count: " + i);
 
 
+        }
+    }
+}
