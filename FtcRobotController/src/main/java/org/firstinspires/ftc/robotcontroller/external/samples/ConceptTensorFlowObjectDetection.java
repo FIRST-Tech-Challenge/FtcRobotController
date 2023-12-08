dead7b7e3919ed28ee270d0124a9506acb6d43f5
NRobowarriors <56520176+NRobowarriors@users.noreply.github.com>
2023-12-07 20:02:42 -0600

    redbob

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
@@ -49,7 +49,7 @@ import java.util.List;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
 @TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
-@Disabled
+//@Disabled
 public class ConceptTensorFlowObjectDetection extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
@@ -103,6 +103,7 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
     /**
      * Initialize the TensorFlow Object Detection processor.
      */
+    private static final String TFOD_MODEL_ASSET = "C:\\Users\\norwa\\OneDrive\\Documents\\GitHub\\CenterStage-12783\\FtcRobotController\\src\\main\\assets\\Red_Bob.tflite";
     private void initTfod() {
 
         // Create the TensorFlow processor by using a builder.
@@ -110,8 +111,8 @@ public class ConceptTensorFlowObjectDetection extends LinearOpMode {
 
             // Use setModelAssetName() if the TF Model is built in as an asset.
             // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-            //.setModelAssetName(TFOD_MODEL_ASSET)
-            //.setModelFileName(TFOD_MODEL_FILE)
+           // .setModelAssetName(TFOD_MODEL_ASSET)
+            .setModelFileName(TFOD_MODEL_ASSET)
 
             //.setModelLabels(LABELS)
             //.setIsModelTensorFlow2(true)
