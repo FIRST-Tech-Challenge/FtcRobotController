d7eb88ad1caa48ebd54ab919150e107936a5f260
Cindy Yam <cindyyam8@gmail.com>
2023-12-08 16:33:40 -0800

    add kavi backup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/KaviCode/_2023120201_Kavi_Gupta_TeleOp_V1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/KaviCode/_2023120201_Kavi_Gupta_TeleOp_V1.java
@@ -9,15 +9,13 @@ import org.firstinspires.ftc.teamcode._TeleOp.KaviCode.mechanisms.arm.ClawInstan
 import org.firstinspires.ftc.teamcode._TeleOp.KaviCode.mechanisms.arm.DroneLauncherInstance;
 
 @TeleOp
-public class _2023120202_Cindy_Yam_Smart_TeleOp_V2 extends LinearOpMode {
+public class _2023120201_Kavi_Gupta_TeleOp_V1 extends LinearOpMode {
 
     private int Arm_Adjustment_Value = 50;
 
     private double Driving_Speed = 0.7;
     @Override
     public void runOpMode() throws InterruptedException {
-
-
         ArmInstance Arm = new ArmInstance();
         ClawInstance Claw = new ClawInstance();
         DroneLauncherInstance DroneLauncher = new DroneLauncherInstance();
@@ -36,25 +34,21 @@ public class _2023120202_Cindy_Yam_Smart_TeleOp_V2 extends LinearOpMode {
         frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
         backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
 
-        boolean armDown = true;
-
         waitForStart();
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x;
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
             double rx = gamepad1.right_stick_x;
-
-            x = x * 1.1;  // Counteract imperfect strafing
 
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-            double frontLeftPower = Math.round(((y + x + rx) / denominator));
-            double backLeftPower = Math.round(((y - x + rx) / denominator));
-            double frontRightPower = Math.round(((y - x - rx) / denominator));
-            double backRightPower = Math.round(((y + x - rx) / denominator));
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
 
             if (gamepad1.dpad_up) {
                 Driving_Speed = 0.7;
@@ -71,59 +65,27 @@ public class _2023120202_Cindy_Yam_Smart_TeleOp_V2 extends LinearOpMode {
 
 
 
-            if (gamepad1.y) {
+            if (gamepad2.y) {
                 Claw.Actuate_Claw_Top_Finger("open");
             }
-            if (gamepad1.x) {
+            if (gamepad2.x) {
                 Claw.Actuate_Claw_Top_Finger("close");
             }
 
-            if (gamepad1.a) {
+            if (gamepad2.b) {
                 Claw.Actuate_Claw_Bottom_Finger("open");
             }
-            if (gamepad1.b) {
+            if (gamepad2.a) {
                 Claw.Actuate_Claw_Bottom_Finger("close");
             }
-
-            if (gamepad1.right_trigger > 0) {
-                armDown = false;
-                Arm.moveArmBy((int) (Arm_Adjustment_Value * gamepad1.right_trigger));
-            } else if (gamepad1.left_trigger > 0) {
-                armDown = false;
-                Arm.moveArmBy((int) (-Arm_Adjustment_Value * gamepad1.left_trigger));
+            if (gamepad2.right_trigger > 0) {
+                Arm.moveArmBy((int) (Arm_Adjustment_Value * gamepad2.right_trigger));
+            } else if (gamepad2.left_trigger > 0) {
+                Arm.moveArmBy((int) (-Arm_Adjustment_Value * gamepad2.left_trigger));
             }
-            if (gamepad1.dpad_up) {
+            if (gamepad2.dpad_up) {
                 DroneLauncher.launchDrone();
             }
-
-            //Smart TeleOp
-
-            if (gamepad1.right_bumper) {
-                armDown = false;
-                Arm.setArmPosTo(500);
-                while (Arm.Arm_Motor.isBusy()) {}
-                sleep(50);
-                Claw.Actuate_Claw_Bottom_Finger("open");
-                sleep(250);
-                Claw.Actuate_Claw_Top_Finger("open");
-            }
-
-            if (gamepad1.left_bumper) {
-                if (armDown) {
-                    armDown = false;
-                    Arm.setArmPosTo(100);
-                } else {
-                    armDown = true;
-                    Arm.setArmPosTo(5);
-                }
-            }
-
-            if (!Arm.Arm_Motor.isBusy() && armDown) {
-                sleep(50);
-                Claw.Actuate_Claw_Bottom_Finger("close");
-                Claw.Actuate_Claw_Top_Finger("close");
-            }
-
             telemetry.addLine("Open Claw Top: y");
             telemetry.addLine("Close Claw Top: x");
             telemetry.addLine("Open Claw Bottom: b");
