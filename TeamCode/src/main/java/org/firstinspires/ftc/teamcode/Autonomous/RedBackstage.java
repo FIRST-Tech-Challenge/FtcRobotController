0a0eba28a6577328324e3dfca8cba9718467a66f
RedGaming101 <costa@milopaper.com>
2023-12-09 10:22:23 -0500

    Backstage Auto
    Made the Autonomous in a way to base if off of our robot.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBackstage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/RedBackstage.java
@@ -24,26 +24,50 @@ public class RedBackstage extends LinearOpMode {
 
         if(posOfTag == 1){
             teamBot.moveWithoutEncoders(0.6, 0.6, 1000);
-            teamBot.gyroTurning(90);
-            teamBot.strafing(RobotClass.Direction.LEFT, 0.5, 1000);
-            teamBot.moveWithoutEncoders(0.6, 0.6, 500);
-            //Dropping Pixel
+            teamBot.gyroTurning(-90);
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 1000);
+            teamBot.moveWithoutEncoders(-0.6, -0.6, 500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
+            teamBot.strafing(RobotClass.Direction.LEFT, 0.6, 1000);
+            teamBot.gyroTurning(180);
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 500);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 1500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel in Backstage
+            teamBot.moveWithoutEncoders(0.5, 0.5, 500);
 
         }else if(posOfTag == 2){
-            teamBot.moveWithoutEncoders(0.6, 0.6, 1000);
-            teamBot.gyroTurning(180);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 2500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
             teamBot.moveWithoutEncoders(-0.6, -0.6, 2500);
-            //Dropping Pixel
+            teamBot.gyroTurning(90);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 1500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel into Backstage
+            teamBot.moveWithoutEncoders(0.5, 0.5, 500);
 
         }else if(posOfTag == 3){
             teamBot.moveWithoutEncoders(0.6, 0.6, 1000);
-            teamBot.gyroTurning(-90);
-            teamBot.strafing(RobotClass.Direction.RIGHT, 0.5, 1000);
-            teamBot.moveWithoutEncoders(0.6, 0.6, 500);
-            //Dropping Pixel
+            teamBot.gyroTurning(90);
+            teamBot.strafing(RobotClass.Direction.LEFT, 0.5, 1000);
+            teamBot.moveWithoutEncoders(-0.6, -0.6, 500);
+            //Open Purple Pixel Claw Side
+            //Shooting Purple Pixel onto SpikeMark with Intake
+            teamBot.strafing(RobotClass.Direction.RIGHT, 0.6, 1500);
+            teamBot.moveWithoutEncoders(-0.6, -0.6, 1500);
+            //Open Yellow Pixel Claw Side
+            //Shooting Yellow Pixel into Backstage
+            teamBot.moveWithoutEncoders(0.5, 0.5, 500);
 
         }else{
-            teamBot.strafing(RobotClass.Direction.RIGHT, 0.6, 3000);
+            teamBot.moveWithoutEncoders(0.6,0.6,500);
+            teamBot.gyroTurning(90);
+            teamBot.moveWithoutEncoders(0.6, 0.6, 1500);
+            //Open Both Sides of Claw
+            //Shooting Purple and Yellow Pixels into Backstage
+            teamBot.moveWithoutEncoders(0.5, 0.5, 500);
         }
 
     }
