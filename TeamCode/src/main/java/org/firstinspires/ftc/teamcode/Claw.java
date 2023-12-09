25fc50ce69a00bfe47d3946276e4340b69d4fd07
Raghuram <raghuram.kothakota@gmail.com>
2023-12-09 10:56:27 -0800

    More updates to use Encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,39 +1,37 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Claw {
     private Robot robot;
     private Gamepad gamepad;
+    private int pos_open = 1;
+    private int pos_close = 0;
     public Claw(Robot robot, Gamepad gamepad)
     {
         this.robot = robot;
         this.gamepad = gamepad;
     }
 
-    public void openLeft() {
-        robot.servoCL.setPosition(1);
+    public void open(Servo s) {
+        s.setPosition(pos_open);
     }
-    public void closeLeft() {
-        robot.servoCL.setPosition(0);
-    }
-    public void closeRight() {
-        robot.servoCR.setPosition(0);
-    }
-    public void openRight() {
-        robot.servoCR.setPosition(1);
+    public void close(Servo s) {
+        s.setPosition(pos_close);
     }
 
     public void operate()
     {
-        if (gamepad.a) {
-            closeLeft();
-        } else if (gamepad.b) {
-            openLeft();
-        } else if (gamepad.x) {
-            closeRight();
-        } else if (gamepad.y) {
-            openRight();
+        if (gamepad.left_trigger > 0) {
+            open(robot.servoCL);
+        } else {
+            close(robot.servoCL);
+        }
+        if (gamepad.right_trigger > 0) {
+            open(robot.servoCR);
+        } else {
+            close(robot.servoCR);
         }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -13,7 +13,7 @@ public class Robot {
     public DcMotor motorBL = null; // Back Left
 
     public DcMotorEx motorSlider; // Slider
-    public DcMotor motorArm; // Elbow or Arm
+    public DcMotorEx motorArm; // Elbow or Arm
     public DcMotor motorHanging; // Hanging
 
     public Servo servoCR; // Claw Right
@@ -29,7 +29,7 @@ public class Robot {
         motorBL = hardwareMap.get(DcMotor.class, "back_left");
         motorBR = hardwareMap.get(DcMotor.class, "back_right");
         motorSlider = hardwareMap.get(DcMotorEx.class, "sliders");
-        motorArm = hardwareMap.get(DcMotor.class, "elbow");
+        motorArm = hardwareMap.get(DcMotorEx.class, "elbow");
         motorHanging = hardwareMap.get(DcMotor.class, "hanging");
         servoCL = hardwareMap.get(Servo.class, "claw_left");
         servoCR = hardwareMap.get(Servo.class, "claw_right");
