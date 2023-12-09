9b5b55abdc4d9599181e2600570e9d0e63722423
ZipCPU <dgisselq@ieee.org>
2023-12-08 22:10:22 -0500

    jjohfy8rruhoduo6fgm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecDrive.java
@@ -31,9 +31,18 @@ public class MecDrive extends LinearOpMode {
     private DcMotorEx rightHook;
     private DcMotorEx leftHook;
     private DcMotorEx intake;
+    private DcMotorEx Lift;
+    private DcMotorEx RHang;
+    private DcMotorEx LHang;
     private Servo RHook;
     private Servo LHook;
     private Servo intakeLift;
+    private Servo airplane;
+    private Servo Door;
+    private Servo RLock;
+    private Servo LLock;
+    private Servo Pivot;
+    private Servo SLift;
 
     private float Rservopos;
     private float Lservopos;
@@ -41,7 +50,7 @@ public class MecDrive extends LinearOpMode {
     private double leftStickX;
     private double leftStickY;
     private double rightStickX;
-
+    private int LiftCounts;
     private float speedFactor;
 
     NormalizedColorSensor colorSensor;
@@ -55,6 +64,8 @@ public class MecDrive extends LinearOpMode {
 
     public void runOpMode() {
 
+
+
         trackingWheelIntegrator = new TrackingWheelIntegrator();
 
         FL= (DcMotorEx) hardwareMap.get(DcMotorEx.class, "FL");
@@ -63,7 +74,21 @@ public class MecDrive extends LinearOpMode {
         RR= (DcMotorEx) hardwareMap.get(DcMotorEx.class, "RR");
 
         intake= (DcMotorEx) hardwareMap.get(DcMotorEx.class, "intake");
+        Lift = (DcMotorEx) hardwareMap.get(DcMotorEx.class, "Lift");
+        LHang = (DcMotorEx) hardwareMap.get(DcMotorEx.class, "LHang");
+        RHang = (DcMotorEx) hardwareMap.get(DcMotorEx.class, "RHang");
+
+
         intakeLift=(Servo) hardwareMap.get(Servo.class, "intakeLift");
+        airplane=(Servo)  hardwareMap.get(Servo.class, "airplane");
+        Door=(Servo)  hardwareMap.get(Servo.class, "Door");
+        RLock=(Servo)  hardwareMap.get(Servo.class, "RLock");
+        LLock=(Servo)  hardwareMap.get(Servo.class, "LLock");
+        Pivot=(Servo)  hardwareMap.get(Servo.class, "Pivot");
+        SLift=(Servo)  hardwareMap.get(Servo.class, "SLift");
+        RHook=(Servo)  hardwareMap.get(Servo.class, "RHook");
+        LHook=(Servo)  hardwareMap.get(Servo.class, "LHook");
+
 
         colorSensor= (NormalizedColorSensor) hardwareMap.get(NormalizedColorSensor.class, "colorSensor");
 
@@ -78,6 +103,11 @@ public class MecDrive extends LinearOpMode {
         Rservopos = 1;
         Lservopos = -1;
         speedFactor = 1;
+
+        Lift.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+        Lift.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
+        Lift.setTargetPosition(1);
+        Lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
         telemetry.setMsTransmissionInterval(20);
         telemetry.addData("Status", "Initialized");
@@ -102,18 +132,97 @@ public class MecDrive extends LinearOpMode {
 
 
         if (gamepad1.a) {
-           intakeLift.setPosition(.3);
+            intakeLift.setPosition(.5);
+            intake.setPower(1);
         }
 
-        if (gamepad1.b) {
-            intakeLift.setPosition(.6);
+        if (gamepad1.y) {
+            intakeLift.setPosition(.3);
         }
 
         if (gamepad1.x) {
-            intake.setPower(1);
-        } else {
+
+        }
+        if (gamepad1.b) {
             intake.setPower(0);
         }
+//
+        if (gamepad1.right_bumper) {
+            RHook.setPosition(1);
+            LHook.setPosition(0);
+        }
+        if (gamepad1.left_bumper) {
+            RHook.setPosition(.75);
+            LHook.setPosition(.25);
+        }
+        if (gamepad1.left_trigger > .5) {
+            RHook.setPosition(.4);
+            LHook.setPosition(.4);
+        }
+        if (gamepad1.right_trigger > .5) {
+            airplane.setPosition(0);
+        }
+            else {
+            airplane.setPosition(.3);
+        }
+        if (gamepad1.dpad_up) {
+            LHang.setPower(.2);
+            RHang.setPower(.2);
+        }
+        else {
+            LHang.setPower(0);
+            RHang.setPower(0);
+        }
+        if (gamepad1.dpad_down) {
+            LLock.setPosition(.1);
+            RLock.setPosition(.1);
+        }
+        else {
+            RLock.setPosition(0);
+            LLock.setPosition(0);
+        }
+        if (gamepad2.a) {
+           Lift.setTargetPosition(-1400);
+           Lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+           Lift.setPower(1);
+           Door.setPosition(.3);
+        }
+        if (gamepad2.b) {
+            Lift.setTargetPosition(-10);
+            Lift.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
+            Lift.setPower(1);
+        }
+        if (gamepad2.dpad_down) {
+            SLift.setPosition(.81);
+            Pivot.setPosition(.7);
+            Door.setPosition(.45);
+        }
+        if (gamepad2.dpad_up) {
+            SLift.setPosition(.15);
+            Pivot.setPosition(1);
+        }
+        if (gamepad2.dpad_right) {
+            SLift.setPosition(.5);
+        }
+
+        if (gamepad2.right_bumper) {
+            Door.setPosition(.4);
+        }
+        if (gamepad2.left_bumper) {
+            Door.setPosition(.43);
+        }
+        // Door Open .45
+        // Door Closed .3
+        // Pivot down score .45
+        // Pivot store .65
+        // Pivot up score 1
+        // SLift Placing posistion .15
+        // SLift Store Posistion .75
+
+        LiftCounts = Lift.getCurrentPosition();
+
+
+        telemetry.addData("lift counts:", LiftCounts);
 
         telemetry.addData("ServoRpos", Rservopos);
         telemetry.addData("ServoLpos", Lservopos);
@@ -138,8 +247,8 @@ public class MecDrive extends LinearOpMode {
         telemetry.update();
 
         MecanumDrive.cartesian(Globals.robot,
-                -leftStickY * speedFactor,
-                leftStickX * speedFactor,
-                rightStickX * speedFactor * .75);
+                -leftStickY * .5,
+                leftStickX * .5,
+                rightStickX * .35);
     }
 }
