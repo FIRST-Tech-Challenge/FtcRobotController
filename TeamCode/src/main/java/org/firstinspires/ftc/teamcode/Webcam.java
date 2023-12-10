c086a059aa2c309e5fdaaeec5114a04f7be74abf
NRobowarriors <56520176+NRobowarriors@users.noreply.github.com>
2023-12-10 15:01:50 -0600

    Update Webcam.java
    Completed webcam

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Webcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Webcam.java
@@ -20,6 +20,7 @@
  */
 
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -44,121 +45,81 @@ import java.util.List;
 
 @TeleOp
 @Disabled
-public class Webcam
-{
-    /*
+public class Webcam {
+
     static final boolean USE_WEBCAM = true;
     private TfodProcessor tfod;
     private VisionPortal visionPortal;
 
-    public Webcam(CameraName camera) {
+    public Webcam(CameraName camera, boolean isRed) {
 
 
-        initTfod(camera);
+        initTfod(camera, isRed);
 
     }
 
-        private void initTfod(CameraName camera) {
 
-            // Create the TensorFlow processor by using a builder.
-            tfod = new TfodProcessor.Builder()
+    private static final String TFOD_MODEL_ASSET_RED = "redbob.tflite";
+    private static final String TFOD_MODEL_ASSET_BLUE = "bluebob.tflite";
+    private void initTfod(CameraName camera,boolean isRed) {
+        String FileToUse = isRed ? TFOD_MODEL_ASSET_RED : TFOD_MODEL_ASSET_BLUE;
+        // Create the TensorFlow processor by using a builder.
+        tfod = new TfodProcessor.Builder()
 
-                    // Use setModelAssetName() if the TF Model is built in as an asset.
-                    // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
-                    //.setModelAssetName(TFOD_MODEL_ASSET)
-                    //.setModelFileName(TFOD_MODEL_FILE)
+                .setModelAssetName(FileToUse)
+                .setModelInputSize(300)
+                .setModelAspectRatio(16.0 / 9.0)
 
-                    //.setModelLabels(LABELS)
-                    //.setIsModelTensorFlow2(true)
-                    //.setIsModelQuantized(true)
-                    .setModelInputSize(300)
-                    .setModelAspectRatio(16.0 / 9.0)
+                .build();
 
-                    .build();
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
 
-            // Create the vision portal by using a builder.
-            VisionPortal.Builder builder = new VisionPortal.Builder();
-
-            // Set the camera (webcam vs. built-in RC phone camera).
-            if (USE_WEBCAM) {
-                builder.setCamera(camera);
-            } else {
-                builder.setCamera(BuiltinCameraDirection.BACK);
-            }
-
-            // Choose a camera resolution. Not all cameras support all resolutions.
-            //builder.setCameraResolution(new Size(640, 480));
-
-            // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-            builder.enableLiveView(true);
-
-            // Set the stream format; MJPEG uses less bandwidth than default YUY2.
-            //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
-
-            // Choose whether or not LiveView stops if no processors are enabled.
-            // If set "true", monitor shows solid orange screen if no processors enabled.
-            // If set "false", monitor shows camera view without annotations.
-            //builder.setAutoStopLiveView(false);
-
-            // Set and enable the processor.
-            builder.addProcessor(tfod);
-
-            // Build the Vision Portal, using the above settings.
-            visionPortal = builder.build();
-
-            // Set confidence threshold for TFOD recognitions, at any time.
-            //tfod.setMinResultConfidence(0.75f);
-
-            // Disable or re-enable the TFOD processor at any time.
-            //visionPortal.setProcessorEnabled(tfod, true);
-
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(camera);
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
         }
-        public void RunCamera(){
-            while (opModeIsActive()) {
 
-                telemetryTfod();
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        //builder.setCameraResolution(new Size(640, 480));
 
-                // Push telemetry to the Driver Station.
-                telemetry.update();
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
 
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
 
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        //builder.setAutoStopLiveView(false);
 
-                // Share the CPU.
-                sleep(20);
+        // Set and enable the processor.
+        builder.addProcessor(tfod);
 
-            }
-            visionPortal.close()
-        }
-    private void telemetryTfod() {
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
 
+        // Set confidence threshold for TFOD recognitions, at any time.
+        //tfod.setMinResultConfidence(0.75f);
+
+        // Disable or re-enable the TFOD processor at any time.
+        //visionPortal.setProcessorEnabled(tfod, true);
+
+    }
+
+    public double CheckCamera() {
         List<Recognition> currentRecognitions = tfod.getRecognitions();
-        telemetry.addData("# Objects Detected", currentRecognitions.size());
+        double x = 0;
 
-        // Step through the list of recognitions and display info for each one.
         for (Recognition recognition : currentRecognitions) {
-            double x = (recognition.getLeft() + recognition.getRight()) / 2 ;
-            double y = (recognition.getTop()  + recognition.getBottom()) / 2 ;
-
-            telemetry.addData(""," ");
-            telemetry.addData("Image", "%s (%.0f %% Conf.)", recognition.getLabel(), recognition.getConfidence() * 100);
-            telemetry.addData("- Position", "%.0f / %.0f", x, y);
-            telemetry.addData("- Size", "%.0f x %.0f", recognition.getWidth(), recognition.getHeight());
-        }   // end for() loop
-
-    }
-
- public void CheckCamera()
-
-    {
-
-
-        if {
-        return ((recognition.getLeft() + recognition.getRight()) / 2 ,
-        (recognition.getTop() + recognition.getBottom()) / 2)
+            x = ((recognition.getLeft() + recognition.getRight()) / 2);
         }
-        return tuple 
+        return x;
 
     }
 
-     */
+
 }
