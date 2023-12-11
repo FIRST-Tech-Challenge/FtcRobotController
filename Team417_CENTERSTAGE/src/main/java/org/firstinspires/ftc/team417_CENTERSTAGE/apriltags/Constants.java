1a1c4a2b26552ee27b07b50f7309c1436f392a6e
DESKTOP-RC1PREA\Testing <2001290@apps.nsd.org>
2023-12-10 16:22:53 -0800

    Fixing pull request problems

--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/apriltags/Constants.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/apriltags/Constants.java
@@ -6,11 +6,11 @@ public class Constants {
     public static final double DEVBOT_CAMERA_TO_CENTER_Y = 7.8125;
     public static final double DEVBOT_CAMERA_TO_CENTER_X = 0;
     public static final double DEVBOT_CAMERA_TO_CENTER_ROT = 0;
-    public static final double COMPETITION_BOT_FRONT_CAMERA_TO_CENTER_Y = 7.8125;
-    public static final double COMPETITION_BOT_FRONT_CAMERA_TO_CENTER_X = 0;
+    public static final double COMPETITION_BOT_FRONT_CAMERA_TO_CENTER_Y = 7;
+    public static final double COMPETITION_BOT_FRONT_CAMERA_TO_CENTER_X = 3;
     public static final double COMPETITION_BOT_FRONT_CAMERA_TO_CENTER_ROT = 0;
-    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_Y = 7.8125;
-    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_X = 0;
-    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_ROT = 0;
+    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_Y = -7;
+    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_X = 7;
+    public static final double COMPETITION_BOT_BACK_CAMERA_TO_CENTER_ROT = 180;
     public static final double MAX_DETECTION_DISTANCE = 48;
 }
--- Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/baseprograms/BaseTeleOp.java
+++ Team417_CENTERSTAGE/src/main/java/org/firstinspires/ftc/team417_CENTERSTAGE/baseprograms/BaseTeleOp.java
@@ -13,6 +13,9 @@ import org.firstinspires.ftc.team417_CENTERSTAGE.roadrunner.MecanumDrive;
 
 @Config
 public abstract class BaseTeleOp extends BaseOpMode {
+    // Set to false for competitions to remove lags
+    public static final boolean TESTING = true;
+
     public MecanumDrive drive;
     private ArmMechanism arm;
 
@@ -39,14 +42,16 @@ public abstract class BaseTeleOp extends BaseOpMode {
             telemetry.addData("y", drive.pose.position.y);
             telemetry.addData("heading", drive.pose.heading);
 
-            // Code added to draw the pose, remove before competition, causes lags:
-            TelemetryPacket p = new TelemetryPacket();
-            Canvas c = p.fieldOverlay();
-            c.setStroke("#3F5100");
-            MecanumDrive.drawRobot(c, drive.pose);
+            // Code added to draw the pose, use only when testing
+            if (TESTING) {
+                TelemetryPacket p = new TelemetryPacket();
+                Canvas c = p.fieldOverlay();
+                c.setStroke("#3F5100");
+                MecanumDrive.drawRobot(c, drive.pose);
 
-            FtcDashboard dashboard = FtcDashboard.getInstance();
-            dashboard.sendTelemetryPacket(p);
+                FtcDashboard dashboard = FtcDashboard.getInstance();
+                dashboard.sendTelemetryPacket(p);
+            }
 
             if (armMotor != null && dumperServo != null && gateServo != null) {
                 outputUsingControllers();
