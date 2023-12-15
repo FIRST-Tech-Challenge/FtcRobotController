f6c785adc0fb07bded9c80b98cd81a92bb8f276e
confwhat <tanvinator24@gmail.com>
2023-12-15 01:29:56 -0800

    second path for short auto is being tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShortRedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShortRedAuto.java
@@ -17,7 +17,7 @@ public class ShortRedAuto extends LinearOpMode {
         robot.setUpIntakeOuttake();
         robot.initVisionProcessing();
         double slideStartingPosition;
-        boolean secondPath = false;
+        boolean secondPath = true;
 
         robot.detectPropEarly();
 
@@ -39,7 +39,9 @@ public class ShortRedAuto extends LinearOpMode {
             robot.trayToOuttakePos(true); // pivot tray to outtake position
             robot.alignToBoard();
             robot.autoOuttake(true, slideStartingPosition);
-            robot.parkBot(false);
+            if (!secondPath) {
+                robot.parkBot(false);
+            }
 
             //second trip
 
@@ -47,36 +49,29 @@ public class ShortRedAuto extends LinearOpMode {
                 robot.boardToCenter();
 
                 // move forward and fast
-                robot.setHeading(-90, 0.7);
                 robot.stackAttachmentOut(); //stack attachment out
-                robot.fastStraightFixHeading(107, true, 1); // move forward while stack attachment is moving
-
-                sleep(10000);
+                robot.fastStraightFixHeading(106, true, 1); // move forward while stack attachment is moving
 
                 // remove top 4 pixels
-                robot.mecanumBlocking(11, true, 0.3); // strafe to knock over stack
+                robot.mecanumBlocking(12, true, 0.5); // strafe to knock over stack
                 robot.stackAttachmentIn(); // attachment in
-                robot.mecanumBlocking(11, false, 0.7); // move back to knocked stack
+                robot.mecanumBlocking(12, false, 0.5); // move back to knocked stack
 
                 // gobble more pixels
                 robot.autoIntake();
 
                 // return to board
-                robot.fastStraightFixHeading(100, false, 1); // drive across field
-
-                robot.mecanumBlocking(25, !robot.isRedAlliance, 0.7); // mecanum to board
+                robot.fastStraightFixHeading(92, false, 1); // drive across field
+                robot.mecanumBlocking(23, !robot.isRedAlliance, 0.7); // mecanum to board
 
                 // move linear slide up
                 robot.moveLinearSlideByTicksBlocking(2000 + slideStartingPosition);
                 robot.trayToOuttakePos(true); // pivot tray to outtake position
 
                 // move to board and drop
-                robot.fastStraightFixHeading(12, false, 0.7);
+                robot.goToAnyTag();
                 robot.autoOuttake(false, slideStartingPosition);
-
-                //robot.alignToBoard();
             }
-
 
             break;
         }
