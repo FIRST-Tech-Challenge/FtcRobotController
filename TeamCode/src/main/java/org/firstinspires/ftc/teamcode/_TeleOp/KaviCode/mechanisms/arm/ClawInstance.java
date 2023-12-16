609cccb0a104a5166d280e774dd85b62a0456187
Cindy Yam <cindyyam8@gmail.com>
2023-12-15 18:40:30 -0800

    Update ClawInstance.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/KaviCode/mechanisms/arm/ClawInstance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_TeleOp/KaviCode/mechanisms/arm/ClawInstance.java
@@ -10,38 +10,60 @@ public class ClawInstance {
 
     public Servo Claw_Bottom_Finger;
 
-    private double Claw_Bottom_Open_Position_Value = 0.2;
+    private double Claw_Bottom_Open_Position_Value = 0.3;
 
-    private double Claw_Bottom_Close_Position_Value = 0.4;
+    private double Claw_Bottom_Close_Position_Value = 0;
 
     private double Claw_Top_Open_Position_Value = 0.3;
 
-    private double Claw_Top_Close_Position_Value = 0.5;
+    private double Claw_Top_Close_Position_Value = 0;
+
+    public String Claw_Top_Status = "";
+    public String Claw_Bottom_Status = "";
 
     public void initializeClaw(HardwareMap hardwareMap) {
         Claw_Top_Finger = hardwareMap.get(Servo.class, "Claw_Top_Finger");
         Claw_Bottom_Finger = hardwareMap.get(Servo.class, "Claw_Bottom_Finger");
 
-        Claw_Top_Finger.setDirection(Servo.Direction.FORWARD);
+        Claw_Top_Finger.setDirection(Servo.Direction.REVERSE);
         Claw_Bottom_Finger.setDirection(Servo.Direction.FORWARD);
 
         Claw_Top_Finger.scaleRange(0, 1);
         Claw_Bottom_Finger.scaleRange(0, 1);
+
+        Actuate_Claw_Top_Finger("close");
+        Actuate_Claw_Bottom_Finger("close");
     }
 
     public void Actuate_Claw_Top_Finger(String Action) {
         if (Action == "open") {
             Claw_Top_Finger.setPosition(Claw_Top_Open_Position_Value);
+            Claw_Top_Status = "open";
         } else if (Action == "close") {
             Claw_Top_Finger.setPosition(Claw_Top_Close_Position_Value);
+            Claw_Top_Status = "closed";
+        } else if (Action == "toggle") {
+            if (Claw_Top_Status == "open") {
+                Actuate_Claw_Top_Finger("close");
+            } else if (Claw_Top_Status == "closed") {
+                Actuate_Claw_Top_Finger("open");
+            }
         }
     }
 
     public void Actuate_Claw_Bottom_Finger(String Action) {
-        if (Action == "close") {
+        if (Action == "open") {
             Claw_Bottom_Finger.setPosition(Claw_Bottom_Open_Position_Value);
-        } else if (Action == "open") {
+            Claw_Bottom_Status = "open";
+        } else if (Action == "close") {
             Claw_Bottom_Finger.setPosition(Claw_Bottom_Close_Position_Value);
+            Claw_Bottom_Status = "closed";
+        } else if (Action == "toggle") {
+            if (Claw_Bottom_Status == "open") {
+                Actuate_Claw_Bottom_Finger("close");
+            } else if (Claw_Bottom_Status == "closed") {
+                Actuate_Claw_Bottom_Finger("open");
+            }
         }
     }
 }
