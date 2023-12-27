a38b0fcc4e8ed9d3450ac61d39fcfc72b4ade653
solinexi <solinexi@gmail.om>
2023-12-27 12:26:24 -0700

    slide PID

--- Masters/src/main/java/org/firstinspires/ftc/masters/tests/PIDF_ScoreSlides.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tests/PIDF_ScoreSlides.java
@@ -1,10 +1,15 @@
 package org.firstinspires.ftc.masters.tests;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
+@Config
 public class PIDF_ScoreSlides extends OpMode {
 
     private PIDController controller;
@@ -14,14 +19,32 @@ public class PIDF_ScoreSlides extends OpMode {
 
     public static int target = 0;
 
+    public final double ticks_in_degree = 384.5 / 180;
+
+    private DcMotor gpSlideLeft;
+
     @Override
     public void init() {
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
+        gpSlideLeft = hardwareMap.dcMotor.get("gpSlideLeft");
     }
 
     @Override
     public void loop() {
+        controller.setPID(p, i, d);
+        int slidePos = gpSlideLeft.getCurrentPosition();
+        double pid = controller.calculate(slidePos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
+        double power = pid + ff;
+
+        gpSlideLeft.setPower(power);
+
+        telemetry.addData("pos ", slidePos);
+        telemetry.addData("target ", target);
+        telemetry.update();
     }
 
 }
