cf9b7cfd25cd50b36081314e6984771f2464ff96
solinexi <solinexi@gmail.om>
2023-12-30 09:19:16 -0700

    slide PID fully done

--- Masters/src/main/java/org/firstinspires/ftc/masters/tests/PIDF_ScoreSlides.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tests/PIDF_ScoreSlides.java
@@ -14,33 +14,34 @@ public class PIDF_ScoreSlides extends OpMode {
 
     private PIDController controller;
 
-    public static double p = 0, i = 0, d = 0;
-    public static double f = 0;
+    public static double p = 0.01, i = 0, d = 0.0001;
+    public static double f = 0.05;
 
     public static int target = 0;
 
     public final double ticks_in_degree = 384.5 / 180;
 
-    private DcMotor gpSlideLeft;
+    private DcMotor backSlides;
 
     @Override
     public void init() {
         controller = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        gpSlideLeft = hardwareMap.dcMotor.get("gpSlideLeft");
+        backSlides = hardwareMap.dcMotor.get("backSlides");
+        backSlides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
     @Override
     public void loop() {
         controller.setPID(p, i, d);
-        int slidePos = gpSlideLeft.getCurrentPosition();
+        int slidePos = backSlides.getCurrentPosition();
         double pid = controller.calculate(slidePos, target);
         double ff = Math.cos(Math.toRadians(target / ticks_in_degree)) * f;
 
         double power = pid + ff;
 
-        gpSlideLeft.setPower(power);
+        backSlides.setPower(power);
 
         telemetry.addData("pos ", slidePos);
         telemetry.addData("target ", target);
