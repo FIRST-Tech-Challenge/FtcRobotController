62e001b224600b29406e956dbc753032400a121d
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2023-12-30 10:32:32 -0700

    CV into auto

--- Masters/src/main/java/org/firstinspires/ftc/masters/babysFirstAuto.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/babysFirstAuto.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.masters;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import java.util.Date;
+
 @Autonomous(name = "Baby's First Auto")
 public class babysFirstAuto extends LinearOpMode {
 
@@ -11,9 +13,21 @@ public class babysFirstAuto extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        CenterStageComputerVisionPipelines CV = new CenterStageComputerVisionPipelines(hardwareMap, telemetry);
+        CenterStageComputerVisionPipelines.pos propPos = null;
+
         robot = new Robot(hardwareMap, telemetry, this);
 
         waitForStart();
+
+        long startTime = new Date().getTime();
+        long time = 0;
+
+        while (time < 50 && opModeIsActive()) {
+            time = new Date().getTime() - startTime;
+            propPos = CV.propFind.position;
+            telemetry.addData("Position", propPos);
+        }
 
         robot.init();
         robot.forward(.3, .5);
