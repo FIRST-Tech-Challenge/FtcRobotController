27ef7d420c6265467e19461f6f5de0a96aa31d38
OliverDuncan <60114296+OliverDuncan@users.noreply.github.com>
2023-12-30 11:47:51 -0700

    Teleop Fixs

--- Masters/src/main/java/org/firstinspires/ftc/masters/CenterStageTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/CenterStageTest.java
@@ -67,6 +67,8 @@ public class CenterStageTest extends LinearOpMode {
 
     int backSlidesTargetPos = 0;
     int presetBackSlidesTargetPos = 0;
+    boolean outtakeGoingToTransfer;
+    double outtakeRotationTarget;
 
     int v4bPresetTarget = 0;
 
@@ -234,17 +236,6 @@ RB - Hang up
                 clawArm.setPosition(CSCons.clawArmG);
             }
 
-//            if (gamepad2.dpad_up) {
-//                claw2transfer_time = runtime.time();
-//                clawAngle.setPosition(CSCons.clawAngleTransfer);
-//            }
-//
-//            if (claw2transfer_time != -1) {
-//                if (runtime.time() > claw2transfer_time + claw2transfer_delay) {
-//                    clawArm.setPosition(CSCons.clawArmTransfer);
-//                    claw2transfer_time = -1;
-//                }
-//            }
             if (gamepad2.dpad_up) {
                     clawAngle.setPosition(CSCons.clawAngleTransfer);
                     clawArm.setPosition(CSCons.clawArmTransfer+CSCons.skewampus);
@@ -259,23 +250,11 @@ RB - Hang up
                 clawAngle.setPosition(CSCons.clawAngleTransition);
                 clawArm.setPosition(CSCons.clawArmTransition);
             }
-/*
-            if (gamepad2.x && !hookEngaged && !hookPressed) {
-                hookEngaged = true;
-                hookPressed = true;
-                outtakeHook.setPosition(CSCons.outtakeHook[1]);
-            } else if (gamepad2.x && hookEngaged && !hookPressed) {
-                hookEngaged = false;
-                hookPressed = true;
-                outtakeHook.setPosition(CSCons.outtakeHook[0]);
-            } else {
-                hookPressed = false;
-            }
-*/
-            if (gamepad2.x && outtakeHook.getPosition() <= CSCons.outtakeHook[1] +.1 && outtakeHook.getPosition() >= CSCons.outtakeHook[1]-.1 && runtime.time() > x_last_pressed + .2) {
+
+            if (gamepad2.x && outtakeHook.getPosition() <= CSCons.outtakeHook[1] +.1 && outtakeHook.getPosition() >= CSCons.outtakeHook[1]-.1 && runtime.time() > x_last_pressed + .4) {
                 outtakeHook.setPosition(CSCons.outtakeHook[0]);
                 x_last_pressed = runtime.time();
-            } else if (gamepad2.x && outtakeHook.getPosition() <= CSCons.outtakeHook[0] +.1 && outtakeHook.getPosition() >= CSCons.outtakeHook[0]-.1 && runtime.time() > x_last_pressed + .2) {
+            } else if (gamepad2.x && outtakeHook.getPosition() <= CSCons.outtakeHook[0] +.1 && outtakeHook.getPosition() >= CSCons.outtakeHook[0]-.1 && runtime.time() > x_last_pressed + .4) {
                 outtakeHook.setPosition(CSCons.outtakeHook[1]);
                 x_last_pressed = runtime.time();
             }
@@ -284,27 +263,22 @@ RB - Hang up
                 outtakeHook.setPosition(CSCons.outtakeHook[1]);
                 outtakeMovementLeft.setPosition(CSCons.outtakeMovementBackDrop);
                 outtakeMovementRight.setPosition(CSCons.outtakeMovementBackDrop);
-                outtakeRotation.setPosition(CSCons.outtakeAngleFolder);
+                outtakeRotationTarget = CSCons.outtakeAngleFolder;
             }
             if (gamepad2.left_stick_y < -0.2 && backSlidePos != 0) {
                 outtakeMovementLeft.setPosition(CSCons.outtakeMovementBackTransfer);
                 outtakeMovementRight.setPosition(CSCons.outtakeMovementBackTransfer);
-                outtakeRotation.setPosition(CSCons.outtakeAngleTransfer);
+                outtakeGoingToTransfer = true;
+                outtakeRotationTarget = CSCons.outtakeAngleTransfer;
                 outtakeHook.setPosition(CSCons.outtakeHook[0]);
             }
 
-//            if (gamepad2.left_trigger > 0.5 && backSlidesTargetPos > 0) {
-//                backSlidesTargetPos= backSlidesTargetPos-25;
-//                backSlides.setTargetPosition(backSlidesTargetPos);
-//                backSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                backSlides.setPower(0.7);
-//            }
-//            if (gamepad2.right_trigger > 0.5 && backSlidesTargetPos < 3000) {
-//                backSlidesTargetPos= backSlidesTargetPos+25;
-//                backSlides.setTargetPosition(backSlidesTargetPos);
-//                backSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                backSlides.setPower(0.8);
-//            }
+            if (gamepad2.left_bumper) {
+                outtakeRotationTarget += .005;
+            }
+
+            outtakeRotation.setPosition(outtakeRotationTarget);
+
 
             if (gamepad2.left_trigger > 0.5) {
                 backSlidePos = 1;
@@ -315,28 +289,9 @@ RB - Hang up
 
 
 
-
             if (gamepad2.right_bumper){
-//                backSlidesTargetPos= 0;
-//                presetBackSlidesTargetPos = 0;
-//                backSlides.setTargetPosition(backSlidesTargetPos);
-//                backSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                backSlides.setPower(0.8);
                 backSlidePos = 0;
             }
-//            if (gamepad2.left_bumper &&!presetPushed){
-//                if (presetBackSlidesTargetPos<2) {
-//                    presetBackSlidesTargetPos++;
-//                }
-//                presetPushed = true;
-//                backSlidesTargetPos = CSCons.backSlidesPos[presetBackSlidesTargetPos];
-//                backSlides.setTargetPosition(backSlidesTargetPos);
-//                backSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//                backSlides.setPower(0.8);
-//                //sleep(300);
-//            } else {
-//                presetPushed = false;
-//            }
 
             if (gamepad1.a){
                 clawServo.setPosition(CSCons.claw[2]);
