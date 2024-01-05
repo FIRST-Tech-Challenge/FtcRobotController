12a7811862834943355c3f4735967b16745d960e
Carter Rommelfanger <rommecar000@bdusd.org>
2024-01-05 15:04:31 -0600

    feat: updated autonomous mode stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCommandMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoCommandMode.java
@@ -1,14 +1,8 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.command.CommandOpMode;
-import com.arcrobotics.ftclib.command.FunctionalCommand;
-import com.arcrobotics.ftclib.command.PurePursuitCommand;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-import com.arcrobotics.ftclib.hardware.motors.Motor;
-import com.arcrobotics.ftclib.purepursuit.waypoints.EndWaypoint;
-import com.arcrobotics.ftclib.purepursuit.waypoints.GeneralWaypoint;
-import com.arcrobotics.ftclib.purepursuit.waypoints.StartWaypoint;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
@@ -16,15 +10,13 @@ import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.FingerSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.WristSubsystem;
-import org.firstinspires.ftc.teamcode.util.AutonomousAwareness;
 import org.firstinspires.ftc.teamcode.util.FTCDashboardPackets;
+import org.firstinspires.ftc.teamcode.util.PredefinedAutomation;
 import org.firstinspires.ftc.teamcode.util.RobotHardwareInitializer;
 
 import java.util.HashMap;
-import java.util.function.DoubleSupplier;
 
-import static org.firstinspires.ftc.teamcode.subsystems.FingerSubsystem.FingerPositions;
-import static org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem.ArmPositions;
+import static org.firstinspires.ftc.teamcode.util.PredefinedAutomation.commands.*;
 
 @Autonomous(name = "AutoCommandMode")
 public class AutoCommandMode extends CommandOpMode {
@@ -36,6 +28,7 @@ public class AutoCommandMode extends CommandOpMode {
     public FingerSubsystem fingerSubsystem;
 
     public SequentialCommandGroup autoCommand;
+    private PredefinedAutomation PA;
 
     @Override
     public void initialize() {
@@ -50,24 +43,8 @@ public class AutoCommandMode extends CommandOpMode {
         wristSubsystem = new WristSubsystem(RobotHardwareInitializer.initializeWrist(this));
         fingerSubsystem = new FingerSubsystem(RobotHardwareInitializer.initializeFinger(this));
 
-        autoCommand = new SequentialCommandGroup(
-                new RunCommand(
-                        () -> driveSubsystem.moveRobot(0.5, 0, 0),
-                        driveSubsystem
-                ).withTimeout(1),
-                new RunCommand(
-                        () -> wristSubsystem.moveWrist(0.5, 0),
-                        wristSubsystem
-                ),
-                new RunCommand(
-                        () -> fingerSubsystem.locomoteFinger(FingerPositions.CLOSED),
-                        fingerSubsystem
-                ).whenFinished(() -> fingerSubsystem.locomoteFinger(FingerPositions.OPEN)),
-                new RunCommand(
-                        () -> armSubsystem.positionMoveArm(ArmPositions.BOARD)
-                )
-        );
+        PA = new PredefinedAutomation(driveSubsystem, armSubsystem, wristSubsystem, fingerSubsystem);
 
-        schedule(autoCommand);
+        schedule(PA.automation.get(TESTING));
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PredefinedAutomation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/PredefinedAutomation.java
@@ -1,14 +1,61 @@
 package org.firstinspires.ftc.teamcode.util;
 
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.purepursuit.Waypoint;
 import com.arcrobotics.ftclib.purepursuit.waypoints.EndWaypoint;
 import com.arcrobotics.ftclib.purepursuit.waypoints.GeneralWaypoint;
 import com.arcrobotics.ftclib.purepursuit.waypoints.StartWaypoint;
 
-public final class PredefinedAutomation {
-    public Waypoint[] test = new Waypoint[] {
-            new StartWaypoint(0, 0),
-            new GeneralWaypoint(100, 100),
-            new EndWaypoint()
-    };
+import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.FingerSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.WristSubsystem;
+
+import java.util.HashMap;
+
+public class PredefinedAutomation {
+    private final DriveSubsystem driveSubsystem;
+    private final ArmSubsystem armSubsystem;
+    private final WristSubsystem wristSubsystem;
+    private final FingerSubsystem fingerSubsystem;
+
+    public HashMap<commands, SequentialCommandGroup> automation;
+
+    public enum commands {
+        TESTING,
+    }
+
+    public PredefinedAutomation(final DriveSubsystem _driveSubsystem,
+                                final ArmSubsystem _armSubsystem,
+                                final WristSubsystem _wristSubsystem,
+                                final FingerSubsystem _fingerSubsystem) {
+        driveSubsystem = _driveSubsystem;
+        armSubsystem = _armSubsystem;
+        wristSubsystem = _wristSubsystem;
+        fingerSubsystem = _fingerSubsystem;
+        init();
+    }
+
+    public void init() {
+        automation.put(commands.TESTING,
+                new SequentialCommandGroup(
+                        new RunCommand(
+                                () -> driveSubsystem.moveRobot(0.5, 0, 0),
+                                driveSubsystem
+                        ).withTimeout(1),
+                        new RunCommand(
+                                () -> wristSubsystem.moveWrist(0.5, 0),
+                                wristSubsystem
+                        ),
+                        new RunCommand(
+                                () -> fingerSubsystem.locomoteFinger(FingerSubsystem.FingerPositions.CLOSED),
+                                fingerSubsystem
+                        ).whenFinished(() -> fingerSubsystem.locomoteFinger(FingerSubsystem.FingerPositions.OPEN)),
+                        new RunCommand(
+                                () -> armSubsystem.positionMoveArm(ArmSubsystem.ArmPositions.BOARD)
+                        )
+                )
+        );
+    }
 }
