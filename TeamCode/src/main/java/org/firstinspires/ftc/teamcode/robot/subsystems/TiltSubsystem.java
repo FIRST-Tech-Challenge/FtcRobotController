4c54783cc75f875336be4bdd3ffad2b82fc40ca0
Tymofii Shyshkovtsov <tima.shishkovtsov@gmail.com>
2024-01-10 13:09:06 -0500

    Conversion to angle-based tilt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/TiltSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subsystems/TiltSubsystem.java
@@ -20,13 +20,13 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class TiltSubsystem extends SubsystemBase
 {
     Telemetry telemetry;
-    private static int targetPosition = 0;
+    private double targetAngle = 0;
+    //target angle from vertical with positive angles being towards the front of the robot (deposit) and negative towards the back (intake)
 
-    private static double KP = 0.0012, KI = 0.0, kD = 0.0000001, KF = -0.2;
+    private static double KP = 0.0012, KI = 0.0, kD = 0.0000001;
+    private static double KF = -0.2;
     private static double TICKS_IN_DEGREE = (1.75*1425.1)/360.0;
-
-    private static double TOLERANCE = 10;
-
+    private static double TOLERANCE = 0;
     private static double VERTICAL_ENCODER_VALUE = -300;
     // vertical position of tilt when encoders are reset in the starting position
 
@@ -39,46 +39,65 @@ public class TiltSubsystem extends SubsystemBase
         this.telemetry = telemetry;
 
         tilt_motor =  hMap.get(DcMotorEx.class, "tilt");
+        pid.setTolerance(TOLERANCE);
 
         tilt_motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         tilt_motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         tilt_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
-public void init(){tilt_motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
+
+    public void init(){tilt_motor.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
     tilt_motor.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
     tilt_motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);}
-    public void setTargetPosition(int targetPosition)
+    public void setTargetAngle(double targetAngle)
     {
-        this.targetPosition=targetPosition;
+        this.targetAngle = targetAngle;
     }
 
     public boolean atTargetPosition()
     {
-        if(abs(tilt_motor.getCurrentPosition()-targetPosition)>TOLERANCE) return false;
-        return true;
+        return pid.atSetPoint();
     }
 
+    private int toEncoder(double angle)
+    {
+           return (int) (angle * TICKS_IN_DEGREE + VERTICAL_ENCODER_VALUE);
+    }
+    private double toAngle(int encoderValue)
+    {
+        return (encoderValue - VERTICAL_ENCODER_VALUE)/TICKS_IN_DEGREE;
+    }
     @Override
     //always chases target position
     public void periodic() {
         double output = 0;
         int currentPos = tilt_motor.getCurrentPosition();
+        // current position relative to the arm starting position
+        double currentAngle = toAngle(currentPos);
+        // current angle relative to the vertical
+
+        int targetPosition = toEncoder(targetAngle);
+        // target position relative to the arm starting position
         if(!atTargetPosition())
         {
             double pid = this.pid.calculate(currentPos, targetPosition);
-            double ff = KF * Math.cos(Math.toRadians((currentPos-VERTICAL_ENCODER_VALUE)/TICKS_IN_DEGREE));
+            //double ff = KF * Math.cos(Math.toRadians((currentPos-VERTICAL_ENCODER_VALUE)/TICKS_IN_DEGREE) * ExtensionSubsystem.ARM_LENGTH+ExtensionSubsystem.UNEXTENDED_POSITION+ExtensionSubsystem.getCurrentPosition());
+            double ff = KF * Math.cos(Math.toRadians(currentAngle));
             output = pid + ff;
+
+            telemetry.addData("ff: ", ff);
+            telemetry.addData("pid: ", pid);
+            telemetry.addData("Arm Angle: ", currentAngle);
+            telemetry.addData("- output: ", output);
         }
         tilt_motor.setPower(output);
-        telemetry.addData("ff: ", KF * Math.cos(Math.toRadians((currentPos-VERTICAL_ENCODER_VALUE)/TICKS_IN_DEGREE)));
-        telemetry.addData("pid: ", pid.calculate(currentPos, targetPosition));
         callTelemetry();
     }
 
-    public void callTelemetry()
+    private void callTelemetry()
     {
-        telemetry.addData("Tilt Position: ", tilt_motor.getCurrentPosition());
-        telemetry.addData("power ", tilt_motor.getPower());
-        telemetry.addData("Tilt Target: ", targetPosition);
+        telemetry.addData("- power ", tilt_motor.getPower());
+        telemetry.addData("Encoder Position: ", tilt_motor.getCurrentPosition());
+        telemetry.addData("PID Target: ", pid.getSetPoint());
     }
 }
