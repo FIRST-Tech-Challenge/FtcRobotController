319ce84f56bc18b4fd7075ed8b4a7f544f5085c5
AndrewXue07 <137796590+AndrewXue07@users.noreply.github.com>
2024-01-11 16:58:18 -0500

    test auto for blue1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoTest1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoTest1.java
@@ -10,6 +10,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.TeleOp.Visionportal3;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
 
 /* velocity + acceleration limit command in trajectory movements:
 SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL)
@@ -17,38 +19,52 @@ SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.
 
 @Autonomous(name = "AutoTest1")
 public class AutoTest1 extends LinearOpMode {
+
+
+    // start pose with random estimated values; will change later
+    // we can also make 4 different pose2d values for each of the start positions so changing at start will be easier
+    Pose2d blueStart1 = new Pose2d(-37, 72, Math.toRadians(-90));
+    Pose2d blueStart2 = new Pose2d(37, 72, Math.toRadians(-90));
+    Pose2d redStart1 = new Pose2d(-37, -72, Math.toRadians(90));
+    Pose2d redStart2 = new Pose2d(37, -72, Math.toRadians(90));
+
+    Vector2d blue1LeftMark = new Vector2d(-48, 34);
+    Vector2d blue1CenterMark = new Vector2d(-35, 27);
+    Vector2d blue1RightMark = new Vector2d(-25, 34);
+
     @Override
     public void runOpMode() {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        // start pose with random estimated values; will change later
-        // we can also make 4 different pose2d values for each of the start positions so changing at start will be easier
-        Pose2d startPose = new Pose2d(36, 60, Math.toRadians(90));
-        drive.setPoseEstimate(startPose);
+        drive.setPoseEstimate(blueStart1);
 
 
         // move robot to pixel stash (will change values later)
-        Trajectory moveToStash = drive.trajectoryBuilder(startPose)
-                .splineTo(new Vector2d(60, 0), Math.toRadians(90), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-                .build();
+        TrajectorySequence test = drive.trajectorySequenceBuilder(blueStart1)
+                // detect y-axis custom object from starting location
+                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(30, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .lineToConstantHeading(blue1RightMark)
+                .setVelConstraint(SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH))
+                .lineToLinearHeading(new Pose2d(-35,57, Math.toRadians(0))) // start moving towards backboard for yellow pixel
+                .lineToConstantHeading(new Vector2d(10, 57))
+                .splineToConstantHeading(new Vector2d(52, 36), Math.toRadians(0))
+                .waitSeconds(0.75) // drop yellow pixel
 
-        // move robot to the pixel board
-        Trajectory moveToBoard = drive.trajectoryBuilder(moveToStash.end())
-                .splineTo(new Vector2d(-60, 0), Math.toRadians(-180), SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-                .build();
-
-        // test: move robot backwards back to the stash
-        Trajectory moveBack = drive.trajectoryBuilder(moveToBoard.end(), true)
-                .splineTo(new Vector2d(36, 0), 0, SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH), SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL))
-                .strafeLeft(60)
+                .splineToConstantHeading(new Vector2d(10, 57), Math.toRadians(0)) // go back for two white pixels
+                .lineToConstantHeading(new Vector2d(-35, 57))
+                .splineToConstantHeading(new Vector2d(-65, 36), Math.toRadians(0))
+                .waitSeconds(0.75) // intake two white pixels
+                .splineToConstantHeading(new Vector2d(-35, 57), Math.toRadians(0))
+                .lineToConstantHeading(new Vector2d(10, 57))
+                .splineToConstantHeading(new Vector2d(52, 36), Math.toRadians(0))
+                .waitSeconds(0.75) // drop two white pixels on back board
+                .lineTo(new Vector2d(56, 15)) // park in a spot that the other team probably won't park
                 .build();
 
         waitForStart();
 
         if (isStopRequested()) return;
 
-        drive.followTrajectory(moveToStash);
-        drive.followTrajectory(moveToBoard);
-        drive.followTrajectory(moveBack);
+        drive.followTrajectorySequence(test);
     }
 }
