c7693a5572085925b65efb089e77f1268a3058a1
thunderbolts <shachadavid2013@gmail.com>
2024-01-11 19:36:14 +0200

    i did buttons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import static org.firstinspires.ftc.teamcode.utils.BTController.Buttons.*;
 
-import android.media.Image;
-
 import com.arcrobotics.ftclib.command.Command;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
@@ -11,6 +9,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.*;
 import org.firstinspires.ftc.teamcode.utils.BTController;
+
+import java.util.function.BooleanSupplier;
 
 // todo: check the time from the start to when I have the cords
 
@@ -20,15 +20,17 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
      BTController m_controller;
      Gripper m_gripper;
      plane m_plane;
+     climb m_climb;
 
 
-    public RobotContainer(HardwareMap map,Telemetry telemetry,Gamepad gamepad1,Gamepad gamepad2){
+    public RobotContainer(HardwareMap map,Telemetry telemetry,Gamepad gamepad1,Gamepad gamepad2, BooleanSupplier[] m_buttonsSuppliers){
         m_gamepad1=gamepad1;
         m_gamepad2=gamepad2;
-        m_controller = new BTController(gamepad1);
+        m_controller = new BTController(gamepad1, m_buttonsSuppliers, ()->gamepad1.left_stick_x, ()->gamepad2.left_stick_x, ()->gamepad1.left_stick_y, ()->gamepad2.left_stick_y);
         m_gripper = new Gripper(map,telemetry);
         m_chassis= new Chassis(map, telemetry);
         m_plane = new plane(map, telemetry);
+        m_climb = new climb(map, telemetry);
         m_gamepad1=gamepad1;
         m_gamepad2=gamepad2;
 
@@ -38,13 +40,16 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
     //bind commands to trigger
     public void bindCommands(){
 
-        m_controller.assignCommand(m_chassis.drive(m_controller.left_x, ()-> m_gamepad1.left_trigger+ m_gamepad1.right_trigger, ()-> m_gamepad1.left_stick_y),
-                true, LEFT_X, LEFT_Y, LEFT_TRIGGER, RIGHT_TRIGGER);
+        m_controller.assignCommand(m_chassis.drive(m_controller.left_x2, ()-> m_gamepad1.left_trigger+ m_gamepad1.right_trigger, ()-> m_gamepad1.left_stick_y),
+                true, LEFT_X2, LEFT_Y2, LEFT_TRIGGER2, RIGHT_TRIGGER2);
 
-        m_controller.assignCommand(m_gripper.closeGripper(),false,BUTTON_LEFT).whenInactive();
-        m_controller.assignCommand(m_gripper.openGripper(),false,BUTTON_LEFT);
-        m_controller.assignCommand(m_plane.shootPlane(),false,BUTTON_UP);
+        m_controller.assignCommand(m_gripper.closeGripper(),false,BUTTON_LEFT1);WhileInActive()
+        m_controller.assignCommand(m_gripper.openGripper(),false,BUTTON_LEFT1);
+        m_controller.assignCommand(m_plane.shootPlane(),false,BUTTON_UP1);
+//        m_controller.assignCommand(m_climb.climb( ),false,BUTTON_UP1); //TODO: ask Adi about trapezoid profiles
+        m_controller.assignCommand(m_plane.shootPlane(),false,BUTTON_UP1);
 
+//סוגר ביחד גריפר A
 
     }
     public Command AutonomousCommand(){
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/BTController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/BTController.java
@@ -11,32 +11,62 @@ import java.util.function.DoubleSupplier;
 
 public class BTController{
 
-    public final Gamepad gamepad;
+    public final Gamepad gamepad1;
+    public final Gamepad gamepad2;
+
     static double threshold = 0.1;
     private BooleanSupplier[] m_buttonsSuppliers;
 
-    public final DoubleSupplier left_x;//todo: add more for each of the axis
+    public final DoubleSupplier left_x1;
+    public final DoubleSupplier left_x2;
+    public final DoubleSupplier left_y1;
+    public final DoubleSupplier left_y2;
 
-    public BTController(Gamepad gamepad)
+    public BTController(Gamepad gamepad, BooleanSupplier[] m_buttonsSuppliers, DoubleSupplier left_x1, DoubleSupplier left_x2, DoubleSupplier left_y1, DoubleSupplier left_y2)
     {
-        this.gamepad=gamepad;
-        left_x=()->gamepad.left_stick_x;
-        m_buttonsSuppliers[LEFT_X.ordinal()] = (() -> Math.abs(gamepad.left_stick_x) > threshold);
-        m_buttonsSuppliers[LEFT_Y.ordinal()] = (() -> Math.abs(gamepad.left_stick_y) > threshold);
-        m_buttonsSuppliers[RIGHT_X.ordinal()] = (() -> Math.abs(gamepad.right_stick_x) > threshold);
-        m_buttonsSuppliers[RIGHT_Y.ordinal()] = (() -> Math.abs(gamepad.right_stick_y) > threshold);
-        m_buttonsSuppliers[LEFT_TRIGGER.ordinal()] = (() -> Math.abs(gamepad.left_trigger) > threshold);
-        m_buttonsSuppliers[RIGHT_TRIGGER.ordinal()] = (() -> Math.abs(gamepad.right_trigger) > threshold);
+        this.m_buttonsSuppliers = m_buttonsSuppliers;
+        this.left_x1 = left_x1;
+        this.left_x2 = left_x2;
+        this.left_y1 = left_y1;
+        this.left_y2 = left_y2;
+        this.gamepad1=gamepad1;
+        this.gamepad2=gamepad2;
 
-        m_buttonsSuppliers[BUMPER_LEFT.ordinal()] = ()->gamepad.left_bumper;
-        m_buttonsSuppliers[DPAD_DOWN.ordinal()]=()->gamepad.dpad_down;
-        m_buttonsSuppliers[DPAD_RIGHT.ordinal()]=()->gamepad.dpad_right;
-        m_buttonsSuppliers[DPAD_LEFT.ordinal()]=()->gamepad.dpad_left;
-        m_buttonsSuppliers[DPAD_UP.ordinal()]=()->gamepad.dpad_up;
-        m_buttonsSuppliers[BUTTON_RIGHT.ordinal()]=()->gamepad.b;
-        m_buttonsSuppliers[BUTTON_LEFT.ordinal()]=()->gamepad.x;
-        m_buttonsSuppliers[BUTTON_UP.ordinal()]=()->gamepad.y;
-        m_buttonsSuppliers[BUTTON_DOWN.ordinal()]=()->gamepad.a;
+        left_x=()->gamepad.left_stick_x;
+        this.m_buttonsSuppliers[LEFT_X1.ordinal()] = (() -> Math.abs(gamepad1.left_stick_x) > threshold);
+        this.m_buttonsSuppliers[LEFT_Y1.ordinal()] = (() -> Math.abs(gamepad1.left_stick_y) > threshold);
+        this.m_buttonsSuppliers[RIGHT_X1.ordinal()] = (() -> Math.abs(gamepad1.right_stick_x) > threshold);
+        this.m_buttonsSuppliers[RIGHT_Y1.ordinal()] = (() -> Math.abs(gamepad1.right_stick_y) > threshold);
+        this.m_buttonsSuppliers[LEFT_TRIGGER1.ordinal()] = (() -> Math.abs(gamepad1.left_trigger) > threshold);
+        this.m_buttonsSuppliers[RIGHT_TRIGGER1.ordinal()] = (() -> Math.abs(gamepad1.right_trigger) > threshold);
+
+        this.m_buttonsSuppliers[BUMPER_LEFT1.ordinal()] = ()->gamepad1.left_bumper;
+        this.m_buttonsSuppliers[DPAD_DOWN1.ordinal()]=()->gamepad1.dpad_down;
+        this.m_buttonsSuppliers[DPAD_RIGHT1.ordinal()]=()->gamepad1.dpad_right;
+        this.m_buttonsSuppliers[DPAD_LEFT1.ordinal()]=()->gamepad1.dpad_left;
+        this.m_buttonsSuppliers[DPAD_UP1.ordinal()]=()->gamepad1.dpad_up;
+        this.m_buttonsSuppliers[BUTTON_RIGHT1.ordinal()]=()->gamepad1.b;
+        this.m_buttonsSuppliers[BUTTON_LEFT1.ordinal()]=()->gamepad1.x;
+        this.m_buttonsSuppliers[BUTTON_UP1.ordinal()]=()->gamepad1.y;
+        this.m_buttonsSuppliers[BUTTON_DOWN1.ordinal()]=()->gamepad1.a;
+
+
+        this.m_buttonsSuppliers[LEFT_X2.ordinal()] = (() -> Math.abs(gamepad2.left_stick_x) > threshold);
+        this.m_buttonsSuppliers[LEFT_Y2.ordinal()] = (() -> Math.abs(gamepad2.left_stick_y) > threshold);
+        this.m_buttonsSuppliers[RIGHT_X2.ordinal()] = (() -> Math.abs(gamepad2.right_stick_x) > threshold);
+        this.m_buttonsSuppliers[RIGHT_Y2.ordinal()] = (() -> Math.abs(gamepad2.right_stick_y) > threshold);
+        this.m_buttonsSuppliers[LEFT_TRIGGER2.ordinal()] = (() -> Math.abs(gamepad2.left_trigger) > threshold);
+        this.m_buttonsSuppliers[RIGHT_TRIGGER2.ordinal()] = (() -> Math.abs(gamepad2.right_trigger) > threshold);
+
+        this.m_buttonsSuppliers[BUMPER_LEFT2.ordinal()] = ()->gamepad2.left_bumper;
+        this.m_buttonsSuppliers[DPAD_DOWN2.ordinal()]=()->gamepad2.dpad_down;
+        this.m_buttonsSuppliers[DPAD_RIGHT2.ordinal()]=()->gamepad2.dpad_right;
+        this.m_buttonsSuppliers[DPAD_LEFT2.ordinal()]=()->gamepad2.dpad_left;
+        this.m_buttonsSuppliers[DPAD_UP2.ordinal()]=()->gamepad2.dpad_up;
+        this.m_buttonsSuppliers[BUTTON_RIGHT2.ordinal()]=()->gamepad2.b;
+        this.m_buttonsSuppliers[BUTTON_LEFT2.ordinal()]=()->gamepad2.x;
+        this.m_buttonsSuppliers[BUTTON_UP2.ordinal()]=()->gamepad2.y;
+        this.m_buttonsSuppliers[BUTTON_DOWN2.ordinal()]=()->gamepad2.a;
 
 
 
@@ -62,24 +92,42 @@ public class BTController{
     }
 
     public enum Buttons {
-        BUTTON_LEFT,
-        BUTTON_RIGHT,
-        BUTTON_UP,
-        BUTTON_DOWN,
-        LEFT_X,
-        LEFT_Y,
-        RIGHT_X,
-        RIGHT_Y,
-        LEFT_TRIGGER,
-        RIGHT_TRIGGER,
-        DPAD_UP,
-        DPAD_DOWN,
-        DPAD_LEFT,
-        DPAD_RIGHT,
-        TRIGGER_RIGHT,
-        TRIGGER_LEFT,
-        BUMPER_RIGHT,
-        BUMPER_LEFT;
+        BUTTON_LEFT1,
+        BUTTON_RIGHT1,
+        BUTTON_UP1,
+        BUTTON_DOWN1,
+        LEFT_X1,
+        LEFT_Y1,
+        RIGHT_X1,
+        RIGHT_Y1,
+        LEFT_TRIGGER1,
+        RIGHT_TRIGGER1,
+        DPAD_UP1,
+        DPAD_DOWN1,
+        DPAD_LEFT1,
+        DPAD_RIGHT1,
+        TRIGGER_RIGHT1,
+        TRIGGER_LEFT1,
+        BUMPER_RIGHT1,
+        BUMPER_LEFT1,
+        BUTTON_LEFT2,
+        BUTTON_RIGHT2,
+        BUTTON_UP2,
+        BUTTON_DOWN2,
+        LEFT_X2,
+        LEFT_Y2,
+        RIGHT_X2,
+        RIGHT_Y2,
+        LEFT_TRIGGER2,
+        RIGHT_TRIGGER2,
+        DPAD_UP2,
+        DPAD_DOWN2,
+        DPAD_LEFT2,
+        DPAD_RIGHT2,
+        TRIGGER_RIGHT2,
+        TRIGGER_LEFT2,
+        BUMPER_RIGHT2,
+        BUMPER_LEFT2;
 
     }
 }
