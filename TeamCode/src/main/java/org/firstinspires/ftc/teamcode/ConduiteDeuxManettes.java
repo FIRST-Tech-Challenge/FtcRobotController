ace24ef29355cc44d22c6c2b697dd1255bff710c
unknown <firstrobot2024@gmail.com>
2024-01-12 11:49:35 +0100

    Controlle double manettes + refonte controlle sur ce script

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConduiteDeuxManettes.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConduiteDeuxManettes.java
@@ -7,8 +7,11 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import java.util.Dictionary;
+import java.util.Hashtable;
+
 @TeleOp
-public class ConduiteUneManette extends LinearOpMode {
+public class ConduiteDeuxManettes extends LinearOpMode {
     private DcMotor motorA;
     private DcMotor motorB;
     private DcMotorEx bras1;
@@ -42,23 +45,37 @@ public class ConduiteUneManette extends LinearOpMode {
 
         double coudeZero = 0.5;
         double coudeX = coudeZero;
-        int brasA = 0;
+        double brasA = 0;
         double triggergauche = 0;
         double triggerdroit = 0;
         double varRY = 0;
         double maPosCoude = 0;
         int posbraszero = 0;
+        int goTo = 0;
+        Dictionary<String, Double> mesPosBras = new Hashtable();
+        Dictionary<String, Double> mesPosCoude = new Hashtable();
 
         Gamepad manette1 = this.gamepad1;
-        Gamepad manette2 = this.gamepad1;
+        Gamepad manette2 = this.gamepad2;
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        // Wait for the game to start (driver presses PLAY)
-        waitForStart();
         posbraszero = bras1.getCurrentPosition();
         bras1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         bras2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        mesPosBras.put("x",0.0);
+        mesPosBras.put("y",0.0);
+        mesPosBras.put("a",0.0);
+        mesPosBras.put("b",0.0);
+        mesPosCoude.put("x",0.0);
+        mesPosCoude.put("y",0.0);
+        mesPosCoude.put("a",0.0);
+        mesPosCoude.put("b",0.0);
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
@@ -68,13 +85,13 @@ public class ConduiteUneManette extends LinearOpMode {
             varYpos = Math.abs(varY);
             varXpos = Math.abs(varX);
 
-            varRY = manette1.right_stick_y;
+            varRY = manette2.right_stick_y;
 
             brasA = bras1.getCurrentPosition();
             brasA = bras2.getCurrentPosition();
 
-            triggerdroit = manette1.right_trigger;
-            triggergauche = manette1.left_trigger;
+            triggerdroit = manette2.right_trigger;
+            triggergauche = manette2.left_trigger;
 
             /// Mouvements
             if (varY > 0) //Forward
@@ -120,6 +137,7 @@ public class ConduiteUneManette extends LinearOpMode {
             if (manette1.left_bumper) {
                 motorA.setPower(tgtPowerA);
                 motorB.setPower(-tgtPowerB);
+                manette1.rumble(100);
             } else {
                 motorA.setPower((tgtPowerA / 2));
                 motorB.setPower(-(tgtPowerB / 2));
@@ -134,46 +152,60 @@ public class ConduiteUneManette extends LinearOpMode {
                 tgtBras = varRY/4;
             }
 
-            if (triggergauche > 0) {
-                coudeX += 0.002;
-                if (coudeX > 0.75) {
-                    coudeX = 0.75;
-                }
-            } else if (triggerdroit > 0) {
-
-                coudeX -= 0.002;
-                if (coudeX<0) {
-                    coudeX = 0;
-                }
-            }
+            coudeX += manette2.left_stick_y*0.02;
             coude.setPosition(coudeX);
+            if (coudeX < 0) { coudeX = 0;}
+            if (coudeX > 1) { coudeX = 1;}
 
+
+            if (manette2.dpad_left) {
+                mesPosBras.put("x",brasA);
+                mesPosCoude.put("x",coude.getPosition());
+            }
+            if (manette2.dpad_right) {
+                mesPosBras.put("b",brasA);
+                mesPosCoude.put("b",coude.getPosition());
+            }
+            if (manette2.dpad_up) {
+                mesPosBras.put("y",brasA);
+                mesPosCoude.put("y",coude.getPosition());
+            }
+            if (manette2.dpad_down) {
+                mesPosBras.put("a",brasA);
+                mesPosCoude.put("a",coude.getPosition());
+            }
 
             if (manette2.x) {
-                if (brasA > maPosBras) {
-                    if ((Math.abs(brasA)-Math.abs(maPosBras))<30)
-                    {
-                        tgtBras = -0.2;
-                    } else {
-                        tgtBras = -0.5;
-                    }
+                maPosBras = mesPosBras.get("x");
+                maPosCoude = mesPosCoude.get("x");
+                goTo = 1;
+            }
+            else if (manette2.b) {
+                maPosBras = mesPosBras.get("b");
+                maPosCoude = mesPosCoude.get("b");
+                goTo = 1;
+            }
+            else if (manette2.y) {
+                maPosBras = mesPosBras.get("y");
+                maPosCoude = mesPosCoude.get("y");
+                goTo = 1;
+            }
+            else if (manette2.a) {
+                maPosBras = mesPosBras.get("a");
+                maPosCoude = mesPosCoude.get("a");
+                goTo = 1;
+            }
+            else {goTo = 0;}
 
+            if (goTo == 1) {
+                if (brasA> maPosBras) {
+                    tgtBras = -0.3;
                 } else if (brasA < maPosBras) {
-                    if ((Math.abs(brasA)-Math.abs(maPosBras))<30)
-                    {
-                        tgtBras = 0.05;
-                    } else {
-                        tgtBras = 0.5;
-                    }
+                        tgtBras = 0.3;
                 } else {
-                    tgtBras = 0;
+                        tgtBras = 0;
                 }
                 coudeX = maPosCoude;
-            }
-
-            if (manette2.y) {
-                maPosBras = brasA;
-                maPosCoude = coude.getPosition();
             }
 
             bras1.setPower(tgtBras);
@@ -181,21 +213,22 @@ public class ConduiteUneManette extends LinearOpMode {
             coude.setPosition(coudeX);
 
             if (mainG.getPosition() > 0.50) {
-                while (manette2.a) {
+                while (manette2.left_bumper) {
                     mainG.setPosition(0);
-                }}
+                }
+            }
             if (mainG.getPosition() < 0.20){
-                while (manette2.a) {
+                while (manette2.left_bumper) {
                     mainG.setPosition(0.65);
                 }
             }
 
             if (mainD.getPosition() > 0.50) {
-                while (manette2.b) {
+                while (manette2.right_bumper) {
                     mainD.setPosition(0);
                 }}
             if (mainD.getPosition() < 0.20){
-                while (manette2.b) {
+                while (manette2.right_bumper) {
                     mainD.setPosition(0.65);
                 }
             }
@@ -211,5 +244,4 @@ public class ConduiteUneManette extends LinearOpMode {
 
         }
     }
-
 }
