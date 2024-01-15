3ac1b45212c192290800703dda0749a17a95115c
solinexi <solinexi@gmail.om>
2024-01-15 12:18:24 -0700

    Started Auto

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tests/ClawAngleTest.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.masters.tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.masters.CSCons;
+
+@Config
+@TeleOp(name = "Claw Angle Test: For Karine")
+public class ClawAngleTest extends LinearOpMode {
+
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+    public static double clawAngleUP = 0.4;
+    public static double clawAngleDOWN = 0.82;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        Servo clawAngle = hardwareMap.servo.get("clawAngle");
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            if(gamepad1.dpad_up){
+                clawAngle.setPosition(clawAngleUP);
+            }
+            if(gamepad1.dpad_down){
+                clawAngle.setPosition(clawAngleDOWN);
+            }
+
+        }
+    }
+}
--- Masters/src/main/java/org/firstinspires/ftc/masters/tests/ColorSensorTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/tests/ColorSensorTest.java
@@ -1,21 +1,36 @@
 package org.firstinspires.ftc.masters.tests;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+@Config
 public class ColorSensorTest extends LinearOpMode {
 
     RevColorSensorV3 colorSensor;
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
     @Override
     public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
         colorSensor = hardwareMap.get(RevColorSensorV3.class, "colorSensor");
         waitForStart();
         while(opModeIsActive()) {
             telemetry.addData("Sensed Red", colorSensor.red());
             telemetry.addData("Sensed Blue", colorSensor.blue());
             telemetry.addData("Sensed Green", colorSensor.green());
+            telemetry.addData("Distance CM", colorSensor.getDistance(DistanceUnit.CM));
+            telemetry.addData("Light Detected", colorSensor.getLightDetected());
+            telemetry.addData("Raw Light Detected", colorSensor.getRawLightDetected());
+            telemetry.addData("Max Raw Light Detected", colorSensor.getRawLightDetectedMax());
+            telemetry.addData("Raw Optics", colorSensor.rawOptical());
+            telemetry.addData("Status", colorSensor.status());
             telemetry.update();
         }
     }
