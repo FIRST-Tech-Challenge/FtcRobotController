19cdd7c7b6cd7c10326bc0405dd96114a24e1b80
thunderbolts2630 <emekhefer2630@gmail.com>
2024-01-22 19:51:20 +0200

    drive chassis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -32,12 +32,13 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
     public RobotContainer(HardwareMap map,Telemetry telemetry,Gamepad gamepad1,Gamepad gamepad2){
         m_controller = new BTController(gamepad1);
 
-        m_gripper = new Gripper(map,telemetry);
-//        m_chassis= new Chassis(map, telemetry);
+  //      m_gripper = new Gripper(map,telemetry);
+
+
+        m_chassis= new Chassis(map, telemetry);
 //        m_plane = new plane(map, telemetry);
 //        m_climb = new climb(map, telemetry);
-        m_arm= new arm(map, telemetry);
-
+//        m_arm= new arm(map, telemetry);
 
 
         bindCommands();
@@ -48,20 +49,20 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
     //bind commands to trigger
     public void bindCommands(){
 
-//        m_controller.assignCommand(m_chassis.drive(
-//                ()->-m_controller.left_y.getAsDouble(),
-//                m_controller.left_x,
-//                ()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),
-//                true, LEFT_X, LEFT_Y, LEFT_TRIGGER, RIGHT_TRIGGER).whenInactive(m_chassis.stopMotor());
+        m_controller.assignCommand(m_chassis.drive(
+                ()->-m_controller.left_y.getAsDouble(),
+                m_controller.left_x,
+                ()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),
+                true, LEFT_X, LEFT_Y, LEFT_TRIGGER, RIGHT_TRIGGER).whenInactive(m_chassis.stopMotor());
 //
-       m_controller.assignCommand(m_gripper.toggleGripper(),false,BUTTON_RIGHT);
-//        m_controller.assignCommand(m_plane.shootPlane(),false,DPAD_RIGHT);
-        m_controller.assignCommand(m_gripper.toggleGripper1(),false,BUTTON_RIGHT);
-        m_controller.assignCommand(m_gripper.toggleGripper2(),false,BUTTON_LEFT);
-//        m_controller.assignCommand(m_climb.climb_up( ),false,DPAD_LEFT);
-        m_controller.assignCommand(m_arm.downArm(m_controller.right_y),false,RIGHT_Y);
-        m_controller.assignCommand(m_arm.downArm(m_controller.left_y),false,LEFT_Y);
-        m_controller.assignCommand(m_arm.downArm(()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),false,RIGHT_TRIGGER,LEFT_TRIGGER);
+//       m_controller.assignCommand(m_gripper.toggleGripper(),false,BUTTON_RIGHT);
+////        m_controller.assignCommand(m_plane.shootPlane(),false,DPAD_RIGHT);
+//        m_controller.assignCommand(m_gripper.toggleGripper1(),false,BUTTON_RIGHT);
+//        m_controller.assignCommand(m_gripper.toggleGripper2(),false,BUTTON_LEFT);
+////        m_controller.assignCommand(m_climb.climb_up( ),false,DPAD_LEFT);
+//        m_controller.assignCommand(m_arm.downArm(m_controller.right_y),false,RIGHT_Y);
+//        m_controller.assignCommand(m_arm.downArm(m_controller.left_y),false,LEFT_Y);
+//        m_controller.assignCommand(m_arm.downArm(()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),false,RIGHT_TRIGGER,LEFT_TRIGGER);
 
 
 // TODO:גריפר לזוז
