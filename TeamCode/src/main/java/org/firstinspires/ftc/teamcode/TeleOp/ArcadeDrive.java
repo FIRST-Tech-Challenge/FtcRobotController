5d9b25afcb6bd6738e87511cb7f46f24868f81e0
Rowan Shenoy <rshenoy309@gmail.com>
2024-01-26 15:39:18 -0500

    Hanging mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ArcadeDrive.java
@@ -16,12 +16,9 @@ public class ArcadeDrive extends LinearOpMode {
     DcMotor backLeft;
     DcMotor frontRight;
     DcMotor backRight;
-
-    DcMotor intake;
-    DcMotor lift;
-
-    Servo claw;
-    Servo clawRotator;
+    
+    Servo deployer;
+    DcMotor winch;
 
     public void runOpMode(){
         //Assigning configuration name to variable (for frontLeft, backLeft, frontRight, backRight)
@@ -30,25 +27,21 @@ public class ArcadeDrive extends LinearOpMode {
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-        intake = hardwareMap.get(DcMotor.class, "intake");
-        lift = hardwareMap.get(DcMotor.class, "lift");
+        deployer = hardwareMap.get(Servo.class, "deployer");
+        winch = hardwareMap.get(DcMotor.class, "winch");
 
-        claw = hardwareMap.get(Servo.class, "claw");
-        clawRotator = hardwareMap.get(Servo.class, "clawRotator");
+        //setting direction of motors
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        //resetting encoder values for lift
-        lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        
+        //setting initial position of the deployer servo
+        deployer.setPosition(0.55);
+        
         //Waiting for Start button to be pressed
         waitForStart();
 
         //Looping while the opmode is running
-        boolean liftPowerUp = false;
-        boolean liftPowerDown = false;
         double throttle = 0;
         double turn = 0;
         boolean strafeR = false;
@@ -86,50 +79,21 @@ public class ArcadeDrive extends LinearOpMode {
             frontRight.setPower(turn);
             backRight.setPower(turn);
 
-            //setting power for intake
-            if(gamepad2.a) {
-                intake.setPower(0.6);
+            //raising of the hanging mechanism
+            if (gamepad2.a) {
+                winch.setPower(1);
             } else if (gamepad2.b) {
-                intake.setPower(-0.6);
+                winch.setPower(-1);
             } else {
-                intake.setPower(0);
+                winch.setPower(0);
             }
-
-            intake.setPower(gamepad2.right_trigger * -1);
-
-            //setting power for lift
-            telemetry.addData("lift position", lift.getCurrentPosition());
+            
+            if (gamepad2.dpad_up) {
+                deployer.setPosition(0.17);
+            }
+            
+            telemetry.addData("deployer position", deployer.getPosition());
             telemetry.update();
-
-            telemetry.addData("stick", gamepad2.left_stick_y);
-
-            if (gamepad2.left_stick_y < 0) {
-                if (lift.getCurrentPosition() > -3500) {
-                    lift.setPower(-1 * gamepad2.left_stick_y);
-                }else {
-                    lift.setPower(0);
-                }
-            } else if (gamepad2.left_stick_y > 0) {
-                if (lift.getCurrentPosition() < 0) {
-                    lift.setPower(-1 * gamepad2.left_stick_y);
-                } else {
-                    lift.setPower(0);
-                }
-            } else {
-                lift.setPower(0);
-            }
-
-            // Manipulation of the claw
-
-            claw.setPosition(gamepad2.right_stick_y * 0.4);
-
-//            clawRotator.setPosition(gamepad2.right_stick_y + 0.5);
-            telemetry.addData("clawRotator", claw.getPosition());
-            if (lift.getCurrentPosition() < -1750) {
-                clawRotator.setPosition(0.35);
-            } else {
-                clawRotator.setPosition(0.27);
-            }
         }
     }
 }
