6a921c4090511102fa7eb28b6f0a8c5aca507817
thunderbolts2630 <emekhefer2630@gmail.com>
2024-01-26 16:11:09 +0200

    field relevie drive works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer.java
@@ -37,7 +37,7 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
 
         m_chassis= new Chassis(map, telemetry);
 //        m_plane = new plane(map, telemetry);
-        m_climb = new climb(map, telemetry);
+//        m_climb = new climb(map, telemetry);
 //        m_arm= new arm(map, telemetry);
 
 
@@ -49,7 +49,7 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
     //bind commands to trigger
     public void bindCommands(){
 
-        m_controller.assignCommand(m_chassis.drive(
+        m_controller.assignCommand(m_chassis.fieldRelativeDrive(
                 ()->-m_controller.left_y.getAsDouble(),
                 m_controller.left_x,
                 ()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),
@@ -63,7 +63,7 @@ public class RobotContainer extends com.arcrobotics.ftclib.command.Robot {
 //        m_controller.assignCommand(m_arm.downArm(m_controller.right_y),false,RIGHT_Y);
 //        m_controller.assignCommand(m_arm.downArm(m_controller.left_y),false,LEFT_Y);
 //        m_controller.assignCommand(m_arm.downArm(()-> -m_controller.left_trigger.getAsDouble()+ m_controller.right_trigger.getAsDouble()),false,RIGHT_TRIGGER,LEFT_TRIGGER);
-        m_controller.assignCommand(m_climb.climb_manual(m_controller.right_y), true, RIGHT_Y).whenInactive(m_climb.climb_manual(()->0));
+//        m_controller.assignCommand(m_climb.climb_manual(m_controller.right_y), true, RIGHT_Y).whenInactive(m_climb.climb_manual(()->0));
 
 // TODO:גריפר לזוז
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Chassis.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.Subsystem;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.hardware.RevIMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 
@@ -19,6 +23,7 @@ public class Chassis implements Subsystem {
     private MotorEx motor_FR;
     private MotorEx motor_BL;
     private MotorEx motor_BR;
+    private RevIMU gyro;
     public Chassis(HardwareMap map, Telemetry telemetry){
         this.map=map;
         this.m_telemetry=telemetry;
@@ -27,6 +32,11 @@ public class Chassis implements Subsystem {
         motor_BL = new MotorEx(map, "motor_BL");
         motor_BR = new MotorEx(map, "motor_BR");
 
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        gyro = new RevIMU(map, "imu");
+        gyro.init(parameters);
+        gyro.reset();
     }
     public void setMotors (double FL, double FR, double BL, double BR){
         motor_FR.set(FR);
@@ -40,15 +50,16 @@ public class Chassis implements Subsystem {
         return new RunCommand(()->{
             m_telemetry.addData("front",frontVel);
             m_telemetry.update();
-            double r = Math.hypot(retaliation.getAsDouble(), sidewayVel.getAsDouble());
-            double robotAngle = Math.atan2(retaliation.getAsDouble(), sidewayVel.getAsDouble()) - Math.PI / 4;//shifts by 90 degrees so that 0 is to the right
-            double rightX = frontVel.getAsDouble();
-            final double v1 = r * Math.cos(robotAngle) + rightX;
-            final double v2 = r * Math.sin(robotAngle) - rightX;
-            final double v3 = r * Math.sin(robotAngle) + rightX;
-            final double v4 = r * Math.cos(robotAngle) - rightX;
-
-            setMotors(v1, v2, v3, v4);
+            drive(frontVel.getAsDouble(),sidewayVel.getAsDouble(), retaliation.getAsDouble());
+        },this);
+    }
+    public BTCommand fieldRelativeDrive(DoubleSupplier frontVel, DoubleSupplier sidewayVel, DoubleSupplier retaliation) {
+        return new RunCommand(()->{
+            m_telemetry.addData("front",frontVel);
+            m_telemetry.update();
+            Translation2d vector= new Translation2d(sidewayVel.getAsDouble(),frontVel.getAsDouble());
+            Translation2d rotated=vector.rotateBy(Rotation2d.fromDegrees(-gyro.getHeading()));
+            drive(rotated.getY(),rotated.getX(), retaliation.getAsDouble());
         },this);
     }
 
@@ -63,7 +74,15 @@ public class Chassis implements Subsystem {
     public void setDefaultCommand(Command defaultCommand) {
         Subsystem.super.setDefaultCommand(defaultCommand);
     }
+    private void drive(double frontVel,double sidewayVel,double retaliation) {
+        double r = Math.hypot(retaliation, sidewayVel);
+        double robotAngle = Math.atan2(retaliation, sidewayVel) - Math.PI / 4;//shifts by 90 degrees so that 0 is to the right
+        double rightX = frontVel;
+        final double v1 = r * Math.cos(robotAngle) + rightX;
+        final double v2 = r * Math.sin(robotAngle) - rightX;
+        final double v3 = r * Math.sin(robotAngle) + rightX;
+        final double v4 = r * Math.cos(robotAngle) - rightX;
 
-
-
+        setMotors(v1, v2, v3, v4);
+    }
 }
