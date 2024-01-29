265a8be3d002e7674fa9ebe126398c6be7a86beb
Emily M <emm021810@gmail.com>
2024-01-29 17:23:38 -0600

    Operation_ERROR   Tele-Op to Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Centerstage_AutoBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Centerstage_AutoBlue.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -48,7 +49,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Auto Position Object Detection", group = "Concept")
+@Autonomous(name = "Auto Position Object Detection", group = "Concept")
 //@Disabled
 public class Centerstage_AutoBlue extends LinearOpMode {
 
