d38b0dcf5fd8935b9d6bd0262809fed41031ba46
confwhat <tanvinator24@gmail.com>
2024-02-07 20:31:14 -0800

    tried turning  processors on and off

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LongRedTruss.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous
+public class LongRedTruss extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //robot, dt motors, vision processing setup
+        Robot robot = new Robot(hardwareMap, this, telemetry, true, true, true);
+        robot.setUpDrivetrainMotors();
+        robot.setUpIntakeOuttake();
+        robot.initVisionProcessing();
+        double slideStartingPosition;
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            robot.detectMarkerPosition();
+            robot.visionPortal.setProcessorEnabled(robot.markerProcessor, false);
+            //robot.visionPortal.setProcessorEnabled(robot.aprilTagProcessor, false);
+
+            robot.setMarkerLocation(true, true, robot.markerPos);
+            robot.servoToInitPositions();
+
+            //robot.longMoveToBoard(false);
+            robot.longMoveToBoardTruss();
+
+            robot.alignToBoardFast(robot.wantedAprTagId);
+            //robot.visionPortal.setProcessorEnabled(robot.aprilTagProcessor, false);
+
+            // note slide init position
+            slideStartingPosition = robot.lsFront.getCurrentPosition();
+            robot.autoOuttake(true, slideStartingPosition);
+
+
+            robot.boardToTruss();
+            robot.trussToStackAndIntake();
+            //robot.visionPortal.setProcessorEnabled(robot.aprilTagProcessor, true);
+            robot.stackToBoardTruss();
+
+            robot.alignToBoardFast(6);
+            //robot.visionPortal.setProcessorEnabled(robot.aprilTagProcessor, false);
+
+            //robot.straightBlocking2(-12);
+            robot.autoOuttake(false, slideStartingPosition);
+
+            break;
+
+        }
+    }
+}
