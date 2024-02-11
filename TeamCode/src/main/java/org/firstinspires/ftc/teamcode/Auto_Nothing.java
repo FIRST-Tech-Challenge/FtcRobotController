03609fefb876290bf93e027f1e01db5c1728cc8b
Owen R <27orinne@granvilleschools.org>
2024-02-11 13:38:25 -0500

    true nothing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Nothing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Nothing.java
@@ -1,25 +1,5 @@
-package org.firstinspires.ftc.teamcode;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+package org.firstinspires.ftc.teamcode;import com.qualcomm.robotcore.eventloop.opmode.*;
 @Autonomous(name = "Nothing", group = "CenterStage", preselectTeleOp = "Full")
 public class Auto_Nothing extends LinearOpMode {
-
-    public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
-
-        waitForStart();
-
-        telemetry.addData("Status", "Doing nothing");
-        telemetry.update();
-
-        resetRuntime();
-        while (opModeIsActive()){
-            telemetry.addData("Status", "Doing nothing for " + getRuntime() + " s");
-            telemetry.update();
-        }
-    }
-
+    public void runOpMode() {waitForStart();while (opModeIsActive());}
 }
