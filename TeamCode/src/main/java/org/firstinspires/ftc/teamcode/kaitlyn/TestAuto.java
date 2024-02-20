ea5517d8b84675666ba2c279de3b6a9f1b06fdf3
k8liang <k8lcool@gmail.com>
2024-02-19 19:11:12 -0800

    configurable wait & parking stuff in test auto, need to implement into actual 2+0 autos. minor problem: if you stop the code after init then it crashes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlyn/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlyn/TestAuto.java
@@ -7,6 +7,11 @@ import org.firstinspires.ftc.teamcode.Robot;
 
 @Autonomous
 public class TestAuto extends LinearOpMode {
+
+    public PARKING_POSITION parkingposition = PARKING_POSITION.BOARD;
+    public enum PARKING_POSITION {
+        LEFT, RIGHT, BOARD
+    }
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -15,15 +20,61 @@ public class TestAuto extends LinearOpMode {
         robot.setUpDrivetrainMotors();
         //.setUpIntakeOuttake();
         robot.initVisionProcessing();
+        double waitCounter = 0;
+        boolean wasPressedB = false;
+        boolean wasPressedX = false;
+        boolean wasPressedDPadLeft = false;
+        boolean wasPressedDPadRight = false;
+        boolean wasPressedDPadUp = false;
+
+        while (!opModeIsActive()) {
+            if(wasPressedB == true && !gamepad1.b) {
+                waitCounter++;
+            }
+            wasPressedB = gamepad1.b;
+
+            if(wasPressedX == true && !gamepad1.x) {
+                waitCounter--;
+            }
+            wasPressedX = gamepad1.x;
+
+            if(waitCounter<0){
+                waitCounter = 0;
+            }
+
+
+            if(wasPressedDPadLeft == true && !gamepad1.dpad_left) {
+                parkingposition = PARKING_POSITION.LEFT;
+            }
+            wasPressedDPadLeft = gamepad1.dpad_left;
+
+            if(wasPressedDPadRight == true && !gamepad1.dpad_right) {
+                parkingposition = PARKING_POSITION.RIGHT;
+            }
+            wasPressedDPadRight = gamepad1.dpad_right;
+
+            if(wasPressedDPadUp == true && !gamepad1.dpad_up) {
+                parkingposition = PARKING_POSITION.BOARD;
+            }
+            wasPressedDPadUp = gamepad1.dpad_up;
+
+            telemetry.addLine("+/- WAIT: B/X");
+            telemetry.addData("WAIT TIME: ", waitCounter);
+            telemetry.addLine("");
+
+            telemetry.addLine("PARKING: DPAD LEFT/UP/RIGHT");
+            telemetry.addData("PARKING POS: ", parkingposition);
+            telemetry.update();
+        }
 
         waitForStart();
 
         while (opModeIsActive()) {
-            robot.moveLinearSlideByTicksBlocking(-2000);
-            robot.trayToOuttakePos(true);
+            //robot.moveLinearSlideByTicksBlocking(-2000);
+            //robot.trayToOuttakePos(true);
             this.sleep(1000);
 
-            robot.trayToIntakePos(true);
+            //robot.trayToIntakePos(true);
             this.sleep(2500);
 
             //robot.mecanumAndSlidesDownToZero(-24);
