389363a9f0b2d09158828f7b04255a860729b835
angrybirdsFTC <151655500+angrybirdsFTC@users.noreply.github.com>
2024-02-20 09:18:56 -0800

    Code refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmUtils.java
@@ -0,0 +1,220 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class ArmUtils {
+    // Roller
+    final double ROLLER_FLAT = 0.29;
+    final double ROLLER_UPSIDEDOWN = 0.97;
+    final double ROLLER_ARM_LIMIT = 400;
+
+    // Arm
+    final double ARM_EXTEND_SPEED = 0.5;
+    final double ARM_LIFT_SPEED = 6;
+    final double ARM_LIFT_POWER = 0.6;
+    final int ARM_MAX_POSITION = 3200;
+    final int ARM_MIN_POSITION = -100;
+    final int ARM_EXTEND_LIMIT = 3600;
+
+    // Grip
+    final double GRIP_OPEN = 0.5;
+    final double GRIP_CLOSED = 0;
+    final double GRIP_TRIGGER_THRESHOLD = 0.1;
+
+    // Startup Sequence
+    final int ARM_LIFT_POSITION = 800;
+    //final int ROLLER_WAIT_TIME = 1000;
+
+    // Pixel Pickup Sequence
+    final int PICKUP_EXTEND_TARGET = 1600;
+
+    // Pixel Backdrop Sequence
+    final int BACKDROP_EXTEND_TARGET = 2000;
+    final int BACKDROP_ARM_TARGET = 3000;
+
+    final double SEQUENCE_ARM_POWER = 0.5;
+
+    DcMotor armLift = null;
+    DcMotor armExtend = null;
+    Servo rightGrip = null;
+    Servo leftGrip = null;
+    Servo rollerServo = null;
+
+    int currentArmLiftPos = 0;
+    boolean startupSequenceActive = false;
+    boolean pickupSequenceActive = false;
+    boolean backdropSequenceActive = false;
+
+    public ArmUtils(HardwareMap hardwareMap) {
+        armLift = hardwareMap.dcMotor.get("armLift");
+        armExtend = hardwareMap.dcMotor.get("armExtend");
+        rightGrip = hardwareMap.servo.get("GripR");
+        leftGrip = hardwareMap.servo.get("GripL");
+        rollerServo = hardwareMap.servo.get("Roll");
+
+        rightGrip.setDirection(Servo.Direction.REVERSE);
+        armExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armExtend.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armLift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armLift.setTargetPosition(0);
+        armLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armLift.setPower(ARM_LIFT_POWER);
+    }
+
+    public void startupSequence() {
+        startupSequenceActive = true;
+
+        leftGrip.setPosition(GRIP_CLOSED);
+        rightGrip.setPosition(GRIP_CLOSED);
+        armLift.setPower(SEQUENCE_ARM_POWER);
+        currentArmLiftPos = ARM_LIFT_POSITION;
+        armLift.setTargetPosition(currentArmLiftPos);
+        armExtend.setPower(-ARM_EXTEND_SPEED);
+
+        if (!armLift.isBusy()) {
+            armLift.setPower(ARM_LIFT_POWER);
+            armExtend.setPower(0);
+
+            rollerServo.setPosition(ROLLER_FLAT);
+
+            startupSequenceActive = false;
+        }
+
+        //sleep(ROLLER_WAIT_TIME);
+
+        //currentArmLiftPos = 0;
+        //armLift.setTargetPosition(currentArmLiftPos);
+    }
+
+    void pixelPickupSequence() {
+        pickupSequenceActive = true;
+
+        armLift.setPower(SEQUENCE_ARM_POWER);
+        currentArmLiftPos = ARM_MIN_POSITION;
+        armLift.setTargetPosition(currentArmLiftPos);
+        leftGrip.setPosition(GRIP_OPEN);
+        rightGrip.setPosition(GRIP_OPEN);
+        rollerServo.setPosition(ROLLER_FLAT);
+
+        if (-armExtend.getCurrentPosition() < PICKUP_EXTEND_TARGET) {
+            armExtend.setPower(ARM_EXTEND_SPEED);
+        }
+        else if (-armExtend.getCurrentPosition() > PICKUP_EXTEND_TARGET) {
+            armExtend.setPower(-ARM_EXTEND_SPEED);
+        }
+
+        if (!armLift.isBusy()) {
+            armExtend.setPower(0);
+            armLift.setPower(ARM_LIFT_POWER);
+
+            pickupSequenceActive = false;
+        }
+    }
+
+    void pixelBackdropSequence() {
+        backdropSequenceActive = true;
+
+        armLift.setPower(SEQUENCE_ARM_POWER);
+        currentArmLiftPos = BACKDROP_ARM_TARGET;
+        armLift.setTargetPosition(currentArmLiftPos);
+        rollerServo.setPosition(ROLLER_UPSIDEDOWN);
+
+        if (-armExtend.getCurrentPosition() < BACKDROP_EXTEND_TARGET) {
+            armExtend.setPower(ARM_EXTEND_SPEED);
+        }
+        else if (-armExtend.getCurrentPosition() > BACKDROP_EXTEND_TARGET) {
+            armExtend.setPower(-ARM_EXTEND_SPEED);
+        }
+
+        if (!armLift.isBusy()) {
+            armExtend.setPower(0);
+            armLift.setPower(ARM_LIFT_POWER);
+
+            backdropSequenceActive = false;
+        }
+    }
+
+    public void runSequences(Gamepad gamepad) {
+        if (!startupSequenceActive && !backdropSequenceActive && !pickupSequenceActive) {
+            if (gamepad.a) {
+                pixelPickupSequence();
+            }
+            else if (gamepad.y) {
+                pixelBackdropSequence();
+            }
+        }
+
+        if (startupSequenceActive) {
+            startupSequence();
+        }
+        else if (pickupSequenceActive) {
+            pixelPickupSequence();
+        }
+        else if (backdropSequenceActive) {
+            pixelBackdropSequence();
+        }
+    }
+
+    public void roller(Gamepad gamepad) {
+        if (startupSequenceActive || pickupSequenceActive || backdropSequenceActive) return;
+
+        boolean overrideMode = gamepad.dpad_down;
+
+        if (currentArmLiftPos >= ROLLER_ARM_LIMIT || overrideMode) {
+            if (gamepad.x) {
+                rollerServo.setPosition(ROLLER_UPSIDEDOWN);
+            }
+
+            if (gamepad.b) {
+                rollerServo.setPosition(ROLLER_FLAT);
+            }
+        }
+    }
+
+    public void extend(Gamepad gamepad) {
+        if (startupSequenceActive || pickupSequenceActive || backdropSequenceActive) return;
+
+        boolean overrideMode = gamepad.dpad_down;
+
+        if (-armExtend.getCurrentPosition() < ARM_EXTEND_LIMIT || overrideMode || -gamepad.right_stick_y < 0) {
+            armExtend.setPower(-gamepad.right_stick_y * ARM_EXTEND_SPEED);
+        }
+        else armExtend.setPower(0);
+    }
+
+    public void lift(Gamepad gamepad) {
+        if (startupSequenceActive || pickupSequenceActive || backdropSequenceActive) return;
+
+        boolean overrideMode = gamepad.dpad_down;
+
+        currentArmLiftPos -= (int)(gamepad.left_stick_y * ARM_LIFT_SPEED);
+        if (currentArmLiftPos < ARM_MIN_POSITION && !overrideMode && -gamepad.left_stick_y < 0) currentArmLiftPos = ARM_MIN_POSITION;
+        if (currentArmLiftPos > ARM_MAX_POSITION && !overrideMode && -gamepad.left_stick_y > 0) currentArmLiftPos = ARM_MAX_POSITION;
+
+        armLift.setTargetPosition(currentArmLiftPos);
+    }
+
+    public void grip(Gamepad gamepad) {
+        if (startupSequenceActive || pickupSequenceActive || backdropSequenceActive) return;
+
+        if (gamepad.left_bumper) {
+            leftGrip.setPosition(GRIP_OPEN);
+        }
+
+        if (gamepad.right_bumper) {
+            rightGrip.setPosition(GRIP_OPEN);
+        }
+
+        if (gamepad.left_trigger > GRIP_TRIGGER_THRESHOLD) {
+            leftGrip.setPosition(GRIP_CLOSED);
+        }
+
+        if (gamepad.right_trigger > GRIP_TRIGGER_THRESHOLD) {
+            rightGrip.setPosition(GRIP_CLOSED);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MovementUtils.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MovementUtils {
+    final double AXIAL_SPEED = 0.5;
+    final double LATERAL_SPEED = 0.6;
+    final double YAW_SPEED = 0.5;
+    final double SLOW_MODE_MULTIPLIER = 0.4;
+
+    DcMotor frontLeftMotor = null;
+    DcMotor backLeftMotor = null;
+    DcMotor frontRightMotor = null;
+    DcMotor backRightMotor = null;
+
+    double axialMultiplier;
+    double lateralMultiplier;
+    double yawMultiplier;
+    boolean slowModeActive = false;
+
+    public MovementUtils(HardwareMap hardwareMap) {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "motor0");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "motor1");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "motor2");
+        backRightMotor = hardwareMap.get(DcMotor.class, "motor3");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.FORWARD);
+    }
+
+    void calculateMultipliers(Gamepad gamepad) {
+        boolean slowModeActive = gamepad.right_bumper;
+
+        axialMultiplier = AXIAL_SPEED * (slowModeActive ? SLOW_MODE_MULTIPLIER : 1);
+        lateralMultiplier = LATERAL_SPEED * (slowModeActive ? SLOW_MODE_MULTIPLIER : 1);
+        yawMultiplier = YAW_SPEED * (slowModeActive ? SLOW_MODE_MULTIPLIER : 1);
+    }
+
+    public void movement(Gamepad gamepad) {
+        calculateMultipliers(gamepad);
+
+        double max;
+
+        double axial = gamepad.left_stick_y * axialMultiplier;
+        double lateral = -gamepad.left_stick_x * lateralMultiplier;
+        double yaw = gamepad.right_stick_x * yawMultiplier;
+
+        // Combine the joystick requests for each axis-motion to determine each wheel's power.
+        // Set up a variable for each drive wheel to save the power level for telemetry.
+        double leftFrontPower = axial + lateral + yaw;
+        double rightFrontPower = axial - lateral - yaw;
+        double leftBackPower = axial - lateral + yaw;
+        double rightBackPower = axial + lateral - yaw;
+
+        // Normalize the values so no wheel power exceeds 100%
+        // This ensures that the robot maintains the desired motion.
+        max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
+        max = Math.max(max, Math.abs(leftBackPower));
+        max = Math.max(max, Math.abs(rightBackPower));
+
+        if (max > 1.0) {
+            leftFrontPower /= max;
+            rightFrontPower /= max;
+            leftBackPower /= max;
+            rightBackPower /= max;
+        }
+
+        // Send calculated power to wheels
+        frontLeftMotor.setPower(leftFrontPower);
+        frontRightMotor.setPower(rightFrontPower);
+        backLeftMotor.setPower(leftBackPower);
+        backRightMotor.setPower(rightBackPower);
+    }
+
+    public void vectorsMovement(Gamepad gamepad) {
+        calculateMultipliers(gamepad);
+
+        double axial = gamepad.left_stick_y * axialMultiplier;
+        double lateral = -gamepad.left_stick_x * lateralMultiplier;
+        double yaw = gamepad.right_stick_x * yawMultiplier;
+
+        double theta = Math.atan2(lateral, axial);
+        double power = Math.hypot(axial, lateral);
+
+        double sin = Math.sin(theta - Math.PI / 4);
+        double cos = Math.cos(theta - Math.PI / 4);
+        double max = Math.max(Math.abs(sin), Math.abs(cos));
+
+        double frontLeftMotorPower = (power * cos/max + yaw);
+        double frontRightMotorPower = (power * sin/max - yaw);
+        double backLeftMotorPower = (power * sin/max + yaw);
+        double backRightMotorPower = (power * cos/max - yaw);
+
+        if ((power + Math.abs(yaw)) > 1) {
+            frontLeftMotorPower /= power + yaw;
+            frontRightMotorPower /= power + yaw;
+            backLeftMotorPower /= power + yaw;
+            backRightMotorPower /= power + yaw;
+        }
+
+        frontLeftMotor.setPower(frontLeftMotorPower);
+        frontRightMotor.setPower(frontRightMotorPower);
+        backLeftMotor.setPower(backLeftMotorPower);
+        backRightMotor.setPower(backRightMotorPower);
+    }
+}
