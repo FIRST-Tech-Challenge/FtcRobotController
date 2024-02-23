3cead5716299809be40fb3cdbc4c87bf91b17524
Devin Seth <derpturtlex@gmail.com>
2024-02-23 10:01:55 -0500

    Fixed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyControlRobot2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyControlRobot2.java
@@ -5,10 +5,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
 
 
 @TeleOp
