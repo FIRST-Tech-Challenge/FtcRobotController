686610fb9e5da0b51d5335a74f6a69ce597dfed5
Maxwell <maxwell@pedersenfam.com>
2024-02-23 15:09:11 -0700

    bens_program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/bens_program.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/other/bens_program.java
@@ -3,21 +3,36 @@ package org.firstinspires.ftc.teamcode.other;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp
 public class bens_program extends LinearOpMode {
 
     private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
 
     @Override
     public void runOpMode() {
 
         frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-                frontLeftMotor.setPower(1);
+                if (gamepad1.a) {
+                    frontLeftMotor.setPower(1);
+                    frontRightMotor.setPower(1);
+                    backLeftMotor.setPower(1);
+                    backRightMotor.setPower(1);
+                }
             }
         }
     }
