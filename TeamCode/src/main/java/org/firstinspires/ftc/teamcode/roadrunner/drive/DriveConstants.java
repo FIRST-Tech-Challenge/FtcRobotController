49f42bc5fd9a2d3f345cf46bd1edab2d7bdf4b1b
williamzhou25 <willface2727@gmail.com>
2024-03-04 18:15:46 -0800

    red left 2 + 4 odo tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/drive/DriveConstants.java
@@ -73,8 +73,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 48;
-    public static double MAX_ACCEL = 25;
+    public static double MAX_VEL = 60;
+    public static double MAX_ACCEL = 40;
     public static double MAX_ANG_VEL = 4;
     public static double MAX_ANG_ACCEL = 3;
 
