e25f6a7d5566b9f20e5c655feef96a552ad0b894
Ainacteru <gbenawijr@gmail.com>
2024-03-08 18:14:56 -0700

    New OP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFirstOp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class MyFirstOp extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
