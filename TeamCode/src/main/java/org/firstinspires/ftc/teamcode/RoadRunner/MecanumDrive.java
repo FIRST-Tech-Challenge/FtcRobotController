e4b928857ec5b74f570e03876fef2926241ec741
rawbill <siddharth.goli@gmail.com>
2024-03-09 20:35:49 -0500

    odo up till manualfeedback
    do manual feedback

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/MecanumDrive.java
@@ -62,14 +62,14 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
 
         // drive model parameters
-        public double inPerTick = 0.002981811;
-        public double lateralInPerTick = inPerTick;
-        public double trackWidthTicks = 0;
+        public double inPerTick = 0.0029588863;
+        public double lateralInPerTick = 0.002377605106426438;
+        public double trackWidthTicks = 4270.373278595394;
 
         // feedforward parameters (in tick units)
-        public double kS = 0;
-        public double kV = 0;
-        public double kA = 0;
+        public double kS = 1.0171077823461658;
+        public double kV = 0.0005699751235451943;
+        public double kA = 0.000025;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 50;
