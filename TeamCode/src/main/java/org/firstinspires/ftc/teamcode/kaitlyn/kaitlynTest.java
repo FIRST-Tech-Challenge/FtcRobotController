1316404af7c0143e4854e46b59c3c4312dbb5e64
k8liang <k8lcool@gmail.com>
2024-03-29 20:37:30 -0700

    parallel code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlyn/kaitlynTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kaitlyn/kaitlynTest.java
@@ -1,5 +1,27 @@
 package org.firstinspires.ftc.teamcode.kaitlyn;
 
-public class kaitlynTest {
+import android.util.Log;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@TeleOp
+public class kaitlynTest extends LinearOpMode {
+    Robot robot;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new Robot(hardwareMap, this, telemetry, true, true, false);
+        waitForStart();
+        robot.setUpDrivetrainMotors();
+        while (opModeIsActive()) {
+            telemetry.addData("fleft", robot.fLeft.getCurrentPosition());
+            telemetry.addData("3 inch to tick", robot.bRightMecanumController.convertInchesToTicks(3));
+            telemetry.update();
+            robot.mecanumParallel(3);
+        }
+    }
 }
