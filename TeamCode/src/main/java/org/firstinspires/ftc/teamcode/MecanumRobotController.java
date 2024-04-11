6a3b308b23fe56818215fa556676e0a5a62764cd
brier <bmhehmeyer@gmail.com>
2024-04-11 13:32:51 -0700

    Added sleep method. Added telemetry being automatically sent in move method and paramter to control whether it sends telemetry. Added default send telemetry with overloaded move method. Changed some comments.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumRobotController.java
@@ -2,7 +2,7 @@
 // so you can just instantiate this and use it in other files. Some examples include driving,
 // getting the current heading, and moving any other motors on the robot.
 
-// Remember to comment with BERP:
+// Remember to comment with B.E.R.P.:
 //      B: Behavior, what the method does.
 //      E: Exceptions, what exceptions the method throws.
 //      R: Return, what the method returns.
@@ -12,16 +12,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 
-// TODO: For everything, test!!
 public class MecanumRobotController {
+    public static final boolean DEFAULT_FIELD_CENTRIC = true;
+    public static final boolean DEFAULT_SEND_TELEMETRY = true;
     // Need to find this.
     public static final double COUNTS_PER_INCH = 30.59;
-    public static final boolean DEFAULT_FIELD_CENTRIC = true;
     public static final double HEADING_CORRECTION_POWER = 0.02;
     public static final double MAX_CORRECTION_ERROR = 2.0;
 
@@ -30,18 +31,14 @@ public class MecanumRobotController {
     private final DcMotor frontLeft;
     private final DcMotor frontRight;
     private final IMU gyro;
+    private final LinearOpMode robot;
+    private final ElapsedTime runtime;
 
-    private LinearOpMode robot;
     private double wantedHeading;
     private double currentForward;
     private double currentStrafe;
     private double currentTurn;
 
-    public MecanumRobotController(DcMotor backLeft, DcMotor backRight,
-                                  DcMotor frontLeft, DcMotor frontRight,
-                                  IMU gyro) {
-        this(backLeft, backRight, frontLeft, frontRight, gyro, null);
-    }
     // Create the controller with all the motors needed to control the robot. If another motor,
     // servo, or sensor is added, put that in here so the class can access it.
     public MecanumRobotController(DcMotor backLeft, DcMotor backRight,
@@ -53,8 +50,19 @@ public class MecanumRobotController {
         this.frontRight = frontRight;
 
         this.gyro = gyro;
-        this.wantedHeading = 0;
         this.robot = robot;
+        this.wantedHeading = getAngleImuDegrees();
+
+        this.runtime = new ElapsedTime();
+        this.runtime.reset();
+    }
+
+    // Overloaded constructor to create the robot controller without a LinearOpMode.
+    // You cannot use distanceDrive or turnTo without a LinearOpMode.
+    public MecanumRobotController(DcMotor backLeft, DcMotor backRight,
+                                  DcMotor frontLeft, DcMotor frontRight,
+                                  IMU gyro) {
+        this(backLeft, backRight, frontLeft, frontRight, gyro, null);
     }
 
     // Behavior: Moves the robot using a given forward, strafe, and turn power.
@@ -63,7 +71,9 @@ public class MecanumRobotController {
     //      - double strafe: The strafe power for the robot.
     //      - double turn: The turn power fo the robot.
     //      - double headingCorrectionPower: The speed of heading correction.
-    private void move(double forward, double strafe, double turn, double headingCorrectionPower) {
+    //      - boolean isTelemetry: Whether or not this method will send telemetry.
+    private void move(double forward, double strafe, double turn, double headingCorrectionPower,
+                      boolean isTelemetry) {
         if (turn == 0) {
             double currentHeading = getAngleImuDegrees();
             double headingCorrection = currentHeading - wantedHeading;
@@ -73,10 +83,14 @@ public class MecanumRobotController {
             wantedHeading = getAngleImuDegrees();
         }
 
-        // Set fields so the robot knows what its forward, strafe, and turn is.
+        // Set fields so the robot knows what its current forward, strafe, and turn is in other methods.
         currentForward = forward;
         currentStrafe = strafe;
         currentTurn = turn;
+
+        if (isTelemetry && robot != null) {
+            sendTelemetry();
+        }
 
         double backLeftPower = Range.clip((forward + strafe - turn) / 3, -1.0, 1.0);
         double backRightPower = Range.clip((forward + strafe + turn) / 3, -1.0, 1.0);
@@ -89,20 +103,31 @@ public class MecanumRobotController {
         frontRight.setPower(frontRightPower);
     }
 
+    // Behavior: Overloaded method of move. This sets the default of isTelemetry.
+    // Params:
+    //      - double forward: The forward power for the robot.
+    //      - double strafe: The strafe power for the robot.
+    //      - double turn: The turn power fo the robot.
+    //      - double headingCorrectionPower: The speed of heading correction.
+    public void move(double forward, double strafe, double turn, double headingCorrectionPower) {
+        move(forward, strafe, turn, headingCorrectionPower, DEFAULT_SEND_TELEMETRY);
+    }
+
     // TODO: Find exact values for distance and implement it in COUNTS_PER_INCH to make this method precise.
     // Behavior: Drives the robot a given distance in a given direction without turning it.
     // Exceptions:
-    //      - Throws RuntimeException when a LinearOpMode object is not provided.
+    //      - Throws RuntimeException when a LinearOpMode object was not provided in the constructor.
     // Params:
     //      - double distance: The distance to drive the robot in inches.
     //      - double direction: The direction, in degrees, that the robot will drive in. This is
     //                          based on the direction the robot was initialized in.
     //      - double speed: The speed at which the robot will move.
-    //      - boolean isFieldCentric: determines whether the direction is based from the robot
+    //      - boolean isFieldCentric: Determines whether the direction is based from the robot
     //                                or the field. If its field centric, the robot will always
     //                                move the same direction for the same inputted direction,
     //                                no matter what direction the robot is facing.
-    public void distanceDrive(double distance, double direction, double speed, boolean isFieldCentric) throws RuntimeException {
+    public void distanceDrive(double distance, double direction, double speed, boolean isFieldCentric)
+            throws RuntimeException {
         if (robot == null) {
             throw new RuntimeException("Tried to run distanceDrive but LinearOpMode object not given!");
         }
@@ -139,18 +164,9 @@ public class MecanumRobotController {
         double correctionPower = HEADING_CORRECTION_POWER;
         if (Math.abs(direction - currentHeading) < 90) correctionPower *= -1;
 
-        // Concerned about what happens if this is still going when OpMode deactivates.
-        // Needs troubleshooting probably. Don't know if I can call isOpModeActive() either so idk
-        // how that works.
-        while ((backLeft.isBusy() || backRight.isBusy() || frontLeft.isBusy() || frontRight.isBusy()) && robot.opModeIsActive()) {
+        while ((backLeft.isBusy() || backRight.isBusy() || frontLeft.isBusy() || frontRight.isBusy())
+                && robot.opModeIsActive()) {
             move(speed, 0.0, 0.0, correctionPower);
-            sendTelemetry(robot.telemetry);
-            robot.telemetry.addData("BackLeftTarget", "BackLeftTarget: " + backLeftTarget);
-            robot.telemetry.addData("BackRightTarget", "BackRightTarget: " + backRightTarget);
-            robot.telemetry.addData("FrontLeftTarget", "FrontLeftTarget: " + frontLeftTarget);
-            robot.telemetry.addData("FrontRightTarget", "FrontRightTarget: " + frontRightTarget);
-            robot.telemetry.addData("ForwardCounts", "ForwardCounts: " + forwardCounts);
-            robot.telemetry.addData("StrafeCounts", "StrafeCounts: " + strafeCounts);
         }
 
         // Stop movement and switch modes
@@ -162,6 +178,11 @@ public class MecanumRobotController {
     }
 
     // Behavior: Overloaded method of distanceDrive. This sets the default of isFieldCentric.
+    // Exceptions:
+    //      - Throws RuntimeException when a LinearOpMode object was not provided in the constructor.
+    // Params:
+    //      - double distance: The distance to drive the robot in inches.
+    //      - double direction: The direction, in degrees, that the robot will drive in. This is
     //                          based on the direction the robot was initialized in.
     //      - double speed: The speed at which the robot will move.
     public void distanceDrive(double distance, double direction, double speed) throws RuntimeException {
@@ -203,12 +224,14 @@ public class MecanumRobotController {
     }
 
     // Behavior: Turns the robot to a given angle
+    // Params:
     //      - double degrees: The angle to turn the robot to in degrees.
     //      - double speed: The speed at which the robot should turn.
     public void turnTo(double angle, double speed) {
         wantedHeading = angle;
-        while (Math.abs(getAngleImuDegrees() - wantedHeading) > MAX_CORRECTION_ERROR && robot.opModeIsActive())
+        while (Math.abs(getAngleImuDegrees() - wantedHeading) > MAX_CORRECTION_ERROR && robot.opModeIsActive()) {
             move(0, 0, 0, speed);
+        }
         // Stop the robot
         move(0, 0, 0, 0);
     }
@@ -247,4 +270,32 @@ public class MecanumRobotController {
         telemetry.update();
     }
 
+    // Behavior: Overloaded method of sendTelemetry() that sends the information to the provided
+    //           LinearOpMode's telemetry.
+    // Exceptions:
+    //      - Throws RuntimeException when a LinearOpMode object was not provided in the constructor.
+    public void sendTelemetry() throws RuntimeException {
+        if (robot == null) {
+            throw new RuntimeException(
+                    "Tried to run sendTelemetry (no parameters) but LinearOpMode object not given!");
+        }
+
+        sendTelemetry(robot.telemetry);
+    }
+
+    // Behavior: Stops all movement except heading correction for the given duration of time.
+    // Exceptions:
+    //      - Throws RuntimeException when a LinearOpMode object was not provided in the constructor.
+    // Params:
+    //      - double time: The amount of time the robot should stop for in seconds.
+    public void sleep(double time) throws RuntimeException {
+        if (robot == null) {
+            throw new RuntimeException("Tried to run sleep but LinearOpMode object not given!");
+        }
+
+        double startTime = runtime.seconds();
+        while (robot.opModeIsActive() && (runtime.seconds() - startTime) < time) {
+            move(0, 0, 0, HEADING_CORRECTION_POWER);
+        }
+    }
 }
