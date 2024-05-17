fcede162888dd9a1748ea835f51cf5210ebc3041
Poseidon <156382114+Poseidon22134@users.noreply.github.com>
2024-05-18 09:14:28 +1000

    created frame for remote control opMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Remote_Control.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.util.*;
+
+
+@TeleOp(name = "Basic: Remote Control", group = "Linear Opmode")
+public class Remote_Control extends LinearOpMode {
+  private ElapsedTime runtime = new ElapsedTime();
+      
+  @override
+  public void runOpMode() {
+    NinjaBot robot = new NinjaBot(hardwareMap, this);
+
+    waitForStart();
+    runtime.reset();
+
+    // running loop.
+    while (opModeIsActive()) {
+      // recieve controller inputs
+      // eg: boolean move = gamepad1.a;
+
+      // set motor power
+      // eg: robot.leftDrive.setPower(power);
+
+      // optional
+      // sleep(10);
+    }
+  }
+}
