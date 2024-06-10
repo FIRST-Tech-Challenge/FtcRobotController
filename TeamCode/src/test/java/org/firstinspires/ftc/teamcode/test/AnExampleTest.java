a7c6608e3c3a4e628246a3c1e23ca5607a15ba58
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2024-06-10 15:06:59 -0700

    Project maintenance
    + add JUnit testing support
    + upgrade Gradle
    + upgrade AGP
    + install Kotlin
    + upgrade compileSdk to 34

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/test/AnExampleTest.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class AnExampleTest {
+    @Test
+    public void test() {
+        Assertions.assertEquals(1, Math.abs(-1));
+    }
+}
---
+++ TeamCode/src/test/java/org/firstinspires/ftc/teamcode/test/readme.md
@@ -0,0 +1,4 @@
+# Static Unit Tests
+
+These tests run at compile-time on your computer. Create a test class (java or kt) in this directory.
+Annotate methods/functions with `@Test` to run them as tests.
