38dbb871698a067f3f9608002e9b8e63a53516cd
cnetz <Cameronjnetz@gmail.com>
2024-06-28 17:51:03 -0700

    Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PressedTest.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class PressedTest extends LinearOpMode {
+    //hardwaremap here
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+    }
+}
