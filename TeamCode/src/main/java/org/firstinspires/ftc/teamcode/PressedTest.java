d11a573dba626d304b58d59d5e0596de0294d70f
cnetz <Cameronjnetz@gmail.com>
2024-06-30 14:14:17 -0700

    servos implameted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PressedTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PressedTest.java
@@ -2,12 +2,16 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class PressedTest extends LinearOpMode {
     //hardwaremap here
+    private Servo servo0;
     @Override
     public void runOpMode() {
+        servo0 = hardwareMap.get(Servo.class, "servo0");
 
         waitForStart();
 
@@ -16,6 +20,9 @@ public class PressedTest extends LinearOpMode {
         if (isStopRequested()) return;
 
         boolean pressedLastIteration = false;
+        double billy = 1.1;
+        int bob = 1;
+
 
         while (opModeIsActive()) {
 
@@ -23,16 +30,15 @@ public class PressedTest extends LinearOpMode {
             boolean gamepad1B_pressed = gamepad1.b;
 
             if (buttonHandler.isPressedOnceA(gamepad1A_pressed)) {
-                //DO STUFF
+                servo0.setPosition(0.7);
+                telemetry.addData("A:", gamepad1A_pressed);
+                telemetry.update();
             }
-            if (gamepad1B_pressed & !pressedLastIteration) {
-                //DO STUFF
+            if (buttonHandler.isPressedOnceB(gamepad1B_pressed)) {
+                servo0.setPosition(0.2);
+                telemetry.addData("B", gamepad1B_pressed);
+                telemetry.update();
             }
-
-            pressedLastIteration = gamepad1A_pressed;
-
-
-
         }
 
     }
