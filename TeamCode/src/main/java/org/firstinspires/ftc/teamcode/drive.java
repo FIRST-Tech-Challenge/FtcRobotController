9bd35c49e4d9efb2321e6d20399b8b3d388415ea
cnetz <Cameronjnetz@gmail.com>
2024-06-30 16:58:15 -0700

    starfe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp
 public class drive extends LinearOpMode {
     private DcMotor leftMotor; // location 2
     private DcMotor rightMotor; // location 1
@@ -22,18 +24,25 @@ public class drive extends LinearOpMode {
         if (isStopRequested()) return;
 
         while(opModeIsActive()) {
-            double y = -gamepad1.left_stick_y;
+            double y = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             double leftPower = (y + x + rx) / denominator;
             double rightPower = (y - x - rx) / denominator;
-            double middlePower = (1) / denominator;
+            double middlePower = (x) / denominator;
 
-            leftMotor.setPower(leftPower);
-            rightMotor.setPower(rightPower);
-            middleMotor.setPower(middlePower);
+
+            if (x > 0.2) {
+                middleMotor.setPower(middlePower);
+            } else {
+                leftMotor.setPower(leftPower);
+                rightMotor.setPower(rightPower);
+            }
+
+
+
         }
 
     }
