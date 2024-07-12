852d50fdae1f22f5138b0b481d34cb42d289d641
cookiejar <rommecar000@bdusd.org>
2024-07-12 10:48:46 -0500

    Summer school class stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summerschool/CopyOfRobotAutoDriveByTime_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/summerschool/CopyOfRobotAutoDriveByTime_Linear.java
@@ -30,7 +30,6 @@
 package org.firstinspires.ftc.teamcode.summerschool;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -54,12 +53,14 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Summer School")
-public class RobotAutoDriveByTime_Linear extends LinearOpMode {
+@Autonomous(name="Copy Robot: Auto Drive By Time", group="Summer School")
+public class CopyOfRobotAutoDriveByTime_Linear extends LinearOpMode {
 
     /* Declare OpMode members. */
     private DcMotor         leftDrive   = null;
     private DcMotor         rightDrive  = null;
+    private DcMotor         backLeftDrive   = null;
+    private DcMotor         backRightDrive  = null;
 
     private ElapsedTime     runtime = new ElapsedTime();
 
@@ -71,14 +72,18 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
     public void runOpMode() {
 
         // Initialize the drive system variables.
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive  = hardwareMap.get(DcMotor.class, "fl_drv");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "bl_drv");
+        rightDrive = hardwareMap.get(DcMotor.class, "fr_drv");
+        backRightDrive = hardwareMap.get(DcMotor.class, "br_drv");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Send telemetry message to signify robot waiting;
         telemetry.addData("Status", "Ready to run");    //
@@ -92,6 +97,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Step 1:  Drive forward for 3 seconds
         leftDrive.setPower(FORWARD_SPEED);
         rightDrive.setPower(FORWARD_SPEED);
+        backLeftDrive.setPower(FORWARD_SPEED);
+        backRightDrive.setPower(FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 3.0)) {
             telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
@@ -102,6 +109,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         leftDrive.setPower(TURN_SPEED);
         rightDrive.setPower(-TURN_SPEED);
         runtime.reset();
+        backLeftDrive.setPower(TURN_SPEED);
+        backRightDrive.setPower(-TURN_SPEED);
         while (opModeIsActive() && (runtime.seconds() < 1.3)) {
             telemetry.addData("Path", "Leg 2: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
@@ -110,6 +119,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Step 3:  Drive Backward for 1 Second
         leftDrive.setPower(-FORWARD_SPEED);
         rightDrive.setPower(-FORWARD_SPEED);
+        backLeftDrive.setPower(FORWARD_SPEED);
+        backRightDrive.setPower(-FORWARD_SPEED);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < 1.0)) {
             telemetry.addData("Path", "Leg 3: %4.1f S Elapsed", runtime.seconds());
@@ -119,6 +130,8 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         // Step 4:  Stop
         leftDrive.setPower(0);
         rightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
