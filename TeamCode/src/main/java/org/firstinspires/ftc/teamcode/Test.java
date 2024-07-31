fd9e1ab54c0ac25df3ef55986b8ac9f7b13ab46b
Saarmets <65457878+saarmets@users.noreply.github.com>
2024-07-31 23:55:25 +0300

    skisofreenik vasja oma parimaid päevi täisvõimul küla bussijaamas veetmas

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -51,12 +51,14 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Spinnermees", group="Linear OpMode")
-public class BasicOpMode_Linear extends LinearOpMode {
+@TeleOp(name="testkood spinner", group="Linear OpMode")
+@Disabled
+public class Test extends LinearOpMode {
 
-    // Declare OpMode members..............................
+    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotorEx mootor = null;
+    private DcMotorEx leftDrive = null;
+    private DcMotor rightDrive = null;
 
     @Override
     public void runOpMode() {
@@ -66,12 +68,14 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        mootor  = hardwareMap.get(DcMotorEx.class, "Motor_Port_0_CH");
+        leftDrive  = hardwareMap.get(DcMotorEx.class, "Motor_Port_2_CH");
+        //rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        mootor.setDirection(DcMotor.Direction.FORWARD);
+        leftDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -80,11 +84,32 @@ public class BasicOpMode_Linear extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            double drive = -gamepad1.left_stick_y;
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
 
-            mootor.setVelocity(400);
-            telemetry.addData("actVelocity",    mootor.getVelocity());
-            telemetry.addData("gamepad", drive);
+            // Choose to drive using either Tank Mode, or POV Mode
+            // Comment out the method that's not used.  The default below is POV.
+
+            // POV Mode uses left stick to go forward, and right stick to turn.
+            // - This uses basic math to combine motions and is easier to drive straight.
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            // Tank Mode uses one stick to control each wheel.
+            // - This requires no math, but it is hard to drive forward slowly and keep straight.
+            // leftPower  = -gamepad1.left_stick_y ;
+            // rightPower = -gamepad1.right_stick_y ;
+
+            // Send calculated power to wheels
+            leftDrive.setPower(leftPower);
+            rightDrive.setPower(rightPower);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
             telemetry.update();
         }
     }
