703ad5898773123f17b920f4934909b9cdc8ae41
krishvadivel <125418482+krishv745@users.noreply.github.com>
2024-08-06 15:33:03 -0500

    Dheemath changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -10,15 +10,15 @@ public class Bot extends RobotDrive{
     public DcMotorEx rr;
     public DcMotorEx rl;
    public Servo servoTransfer;
-//    public Servo servoHL;
-//    public Servo servoHR;
-//    public Servo servoSLB;
-//    public Servo servoSLT;
-//    public Servo servoSRT;
-//    public Servo servoSRB;
-//    public Servo servoFlight;
-//    public Servo servoRR;
-//    public Servo servoRL;
+    public Servo servoHL;
+    public Servo servoHR;
+    public Servo servoSLB;
+    public Servo servoSLT;
+    public Servo servoSRT;
+    public Servo servoSRB;
+    public Servo servoFlight;
+    public Servo servoRR;
+    public Servo servoRL;
     public void init(HardwareMap hardwareMap) {
 //        fl = hardwareMap.dcMotor.get("fl"); //port 3 EH - RED (In RobotDrive)
 //        bl = hardwareMap.dcMotor.get("bl"); //port 1 CH - YELLOW (In RobotDrive)
@@ -29,16 +29,16 @@ public class Bot extends RobotDrive{
         rl = (DcMotorEx) hardwareMap.dcMotor.get("rl"); // rigging left, port 2 CH - LIGHT GREEN
 
 
-      servoTransfer = hardwareMap.servo.get("transfer"); // port 3 EH, Transfer
-//        servoHL = hardwareMap.servo.get("hl"); // port 0 CH, Hook Left
-//        servoHR = hardwareMap.servo.get("hr"); // port 5 CH, Right Hook
-//        servoSLB = hardwareMap.servo.get("slb"); // port 2 CH, Slides Left Bottom
-//        servoSLT = hardwareMap.servo.get("slt"); // port 1 CH, Slides Left Top
-//        servoSRT = hardwareMap.servo.get("srt"); // port 4 CH, Slides Right Top
-//        servoSRB = hardwareMap.servo.get("srb"); // port 3 CH, Slides Right Bottom
-//        servoFlight = hardwareMap.servo.get("flight"); //port 1 EH, Flight
-//        servoRR = hardwareMap.servo.get("rigr"); // port 5 EH, Rigging Right
-//        servoRL = hardwareMap.servo.get("rigl"); //port 0 EH, Rigging Left
+        servoTransfer = hardwareMap.servo.get("transfer"); // port 3 EH, Transfer
+        servoHL = hardwareMap.servo.get("hl"); // port 0 CH, Hook Left
+        servoHR = hardwareMap.servo.get("hr"); // port 5 CH, Right Hook
+        servoSLB = hardwareMap.servo.get("slb"); // port 2 CH, Slides Left Bottom
+        servoSLT = hardwareMap.servo.get("slt"); // port 1 CH, Slides Left Top
+        servoSRT = hardwareMap.servo.get("srt"); // port 4 CH, Slides Right Top
+        servoSRB = hardwareMap.servo.get("srb"); // port 3 CH, Slides Right Bottom
+        servoFlight = hardwareMap.servo.get("flight"); //port 1 EH, Flight
+        servoRR = hardwareMap.servo.get("rigr"); // port 5 EH, Rigging Right
+        servoRL = hardwareMap.servo.get("rigl"); //port 0 EH, Rigging Left
 
         //add servos and sensors to be used in all autonmous and teleop classes
         //add all the servo positions and stuff
@@ -51,6 +51,27 @@ public class Bot extends RobotDrive{
         } else {
             servoTransfer.setPosition(0.1);
         }
+
+    }
+
+    //rigging
+    public void rig() {
+        // up: 0.7
+        // down:
+        double position = 0.0;
+        if (position != 0.7) {
+            position = 0.7;
+        } else {
+            position = 0.0;
+        }
+
+        servoHL.setPosition(position);
+        servoHR.setPosition(position);
+    }
+
+    //intakeonoff
+    public void intaketoggle(){
+        double intkpwr = 0.0;
 
     }
 }
