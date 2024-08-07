ebea941f0b67f83d8390ae222c93946d6ef2a019
k8liang <k8lcool@gmail.com>
2024-08-07 13:23:21 -0700

    parallel action code- should be working for very basic commands for intake, outtake, drone launcher. can be used in parallel sequences.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeState.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class IntakeState extends GenericState {
+
+    public static final double ERROR_TOLERANCE = 15;
+    private double currentTargetTicks;
+    private double currentTicks;
+
+    private LinearOpMode opMode;
+
+    private double error;
+
+    public double getCurrentTargetTicks() {
+        return currentTargetTicks;
+    }
+
+    public void setCurrentTargetTicks(double currentTargetTicks) {
+        this.currentTargetTicks = currentTargetTicks;
+        this.error = currentTargetTicks - currentTicks;
+    }
+
+    public double getCurrentTicks() {
+        return currentTicks;
+    }
+
+    public void setCurrentTicks(double currentTicks) {
+        this.currentTicks = currentTicks;
+        this.error = currentTargetTicks - currentTicks;
+    }
+
+    public LinearOpMode getOpMode() {
+        return opMode;
+    }
+
+    public void setOpMode(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public double getError() {
+        error = currentTargetTicks - currentTicks;
+        return error;
+    }
+
+    @Override
+    boolean isDone() {
+        return Math.abs(getError())<ERROR_TOLERANCE;
+    }
+
+}
