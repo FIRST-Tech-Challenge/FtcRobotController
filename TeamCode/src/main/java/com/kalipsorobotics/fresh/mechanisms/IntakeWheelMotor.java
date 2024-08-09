75e84b433f8d65206cd11c820f345a8a85418dbd
Ethan Vu <ethan.mt.vu@gmail.com>
2024-08-08 23:30:50 -0700

    shuttle implemented more

--- TeamCode/src/main/java/com/kalipsorobotics/fresh/mechanisms/IntakeWheelMotor.java
+++ TeamCode/src/main/java/com/kalipsorobotics/fresh/mechanisms/IntakeWheelMotor.java
@@ -7,21 +7,18 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class IntakeWheelMotor{
     private final DcMotor intake;
-
     private IntakeWheelMotor(DcMotor intake) {
         this.intake = intake;
     }
     public IntakeWheelMotor(OpModeUtilities opModeUtilities) {
         this(opModeUtilities.getHardwareMap().get(DcMotorEx.class,"intake"));
     }
-
     public void on() {
         intake.setPower(1);
     }
     public void off() {
         intake.setPower(0);
     }
-
     public void reverse() {
         intake.setPower(-1);
     }
