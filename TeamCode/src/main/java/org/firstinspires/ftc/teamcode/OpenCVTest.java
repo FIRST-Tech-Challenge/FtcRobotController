a78c7fd5b8e3257e32151a795c8b316509ec5eb6
vihaanreddi <vihaanreddi@gmail.com>
2024-08-10 11:13:00 -0700

    OpenCV now works, is streaming video to the Driver Hub.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -1,22 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.*;
+import org.opencv.core.Mat;
 import org.openftc.easyopencv.*;
 
-public class OpenCVTest extends OpMode{
+@Autonomous
+public class OpenCVTest extends OpenCvPipeline {
 
     public void init() {
+        WebcamName webcam = hardwareMap.get(WebcamName.class, "Camera");
 
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
+        // OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
 
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Camera");
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam, cameraMonitorViewId);
+
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                // Usually this is where you'll want to start streaming from the camera (see section 4)
+                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                System.out.println("it no work");
+            }
+        });
     }
 
-    public void loop() {
-
-
+    @Override
+    public Mat processFrame(Mat input) {
+        return input;
     }
 }
