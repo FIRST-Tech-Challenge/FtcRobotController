5457e2ef38a9ae40676e84ad9d12b38bfa2ef948
vihaanreddi <vihaanreddi@gmail.com>
2024-08-11 10:47:10 -0700

    Made OpenCVTest stream in black and white

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -1,12 +1,28 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import org.firstinspires.ftc.robotcore.external.hardware.camera.*;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.*;
 
 @Autonomous
 public class OpenCVTest extends OpMode {
+
+    static class Gray extends OpenCvPipeline {
+        // Notice this is declared as an instance variable (and re-used), not a local variable
+        Mat grey = new Mat();
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGB2GRAY);
+            return grey;
+        }
+    }
 
     public void init() {
         WebcamName webcam = hardwareMap.get(WebcamName.class, "Camera");
@@ -15,6 +31,7 @@ public class OpenCVTest extends OpMode {
         // OpenCvCamera camera = OpenCvCameraFactory.getInstance().createInternalCamera2(OpenCvInternalCamera2.CameraDirection.BACK, cameraMonitorViewId);
 
         OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam, cameraMonitorViewId);
+        camera.setPipeline(new Gray());
 
         camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
@@ -34,4 +51,5 @@ public class OpenCVTest extends OpMode {
     public void loop() {
 
     }
+
 }
