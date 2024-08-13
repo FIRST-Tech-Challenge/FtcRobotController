cc4ccade48577ea18e26ecfd83a6d1e8601f9838
vihaanreddi <vihaanreddi@gmail.com>
2024-08-12 18:11:22 -0700

    Created test program that should detect whether the left or right is red. Not yet tested.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCVTest.java
@@ -11,50 +11,56 @@ import org.opencv.core.*;
 @Autonomous
 public class OpenCVTest extends OpMode {
 
-    static class DetectColor extends OpenCvPipeline {
+    class DetectColor extends OpenCvPipeline {
+        Mat YCrCb = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftAverageFinal;
+        double rightAverageFinal;
+        Mat output = new Mat();
+        final Scalar RED = new Scalar(255, 0, 0);
+        final Scalar GREEN = new Scalar(0, 255, 0);
 
-        // point constants for the top left half and the bottom right half of the screen
-        static final Point topLeft = new Point(320.0/4.0, 240.0/4.0);
-        static final Point bottomRight = new Point(320.0*0.75, 240.0*0.75);
-        // color constants
-        static final Scalar RED = new Scalar(255, 0, 0);
-        static final Scalar GREEN = new Scalar(0, 255, 0);
-        Mat RGB = new Mat();
-        Mat G = new Mat();
-        Mat region1_G;
-        int avgColor;
-
-        @Override
-        public void init(Mat firstFrame) {
-            Core.extractChannel(RGB, G, 0);
-            region1_G = G.submat(new Rect(topLeft, bottomRight));
-        }
-
-        @Override
         public Mat processFrame(Mat input) {
-            Core.extractChannel(RGB, G, 0);
-            avgColor = (int) Core.mean(region1_G).val[0];
 
-            if (avgColor < 100) {
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        topLeft, // First point which defines the rectangle
-                        bottomRight, // Second point which defines the rectangle
-                        GREEN, // The color the rectangle is drawn in
-                        2); // Negative thickness means solid fill
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("DetectColor Pipeline Running");
+
+            Rect leftRect = new Rect(1, 1, 319, 359);
+            Rect rightRect = new Rect(320, 1, 319, 359);
+
+            Imgproc.rectangle(output, leftRect, RED, 2);
+            Imgproc.rectangle(output, rightRect, RED, 2);
+
+            leftCrop = YCrCb.submat(leftRect);
+            rightCrop = YCrCb.submat(rightRect);
+
+            Core.extractChannel(leftCrop, leftCrop, 2);
+            Core.extractChannel(rightCrop, rightCrop, 2);
+
+            Scalar leftAverage = Core.mean(leftCrop);
+            Scalar rightAverage = Core.mean(rightCrop);
+
+            leftAverageFinal = leftAverage.val[0];
+            rightAverageFinal = rightAverage.val[0];
+
+            if (leftAverageFinal > rightAverageFinal) {
+                Imgproc.rectangle(output, leftRect, GREEN, 2);
+                telemetry.addLine("Red object detected in left.");
+            }
+            else if (leftAverageFinal < rightAverageFinal) {
+                Imgproc.rectangle(output, rightRect, GREEN, 2);
+                telemetry.addLine("Red object detected in right.");
             }
             else {
-                Imgproc.rectangle(
-                        input, // Buffer to draw on
-                        topLeft, // First point which defines the rectangle
-                        bottomRight, // Second point which defines the rectangle
-                        RED, // The color the rectangle is drawn in
-                        2); // Thickness of the rectangle lines
+                telemetry.addLine("Red amount in both is equal.");
             }
 
-            return input;
-        }
+            telemetry.addData("Left Average Red Value: ", leftAverageFinal);
+            telemetry.addData("Right Average Red Value: ", rightAverageFinal);
 
+            return output;
+        }
     }
 
     public void init() {
@@ -70,7 +76,7 @@ public class OpenCVTest extends OpMode {
             @Override
             public void onOpened() {
                 // Usually this is where you'll want to start streaming from the camera (see section 4)
-                camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                camera.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT);
             }
 
             @Override
@@ -82,7 +88,7 @@ public class OpenCVTest extends OpMode {
 
     @Override
     public void loop() {
-
+        telemetry.update();
     }
 
 }
