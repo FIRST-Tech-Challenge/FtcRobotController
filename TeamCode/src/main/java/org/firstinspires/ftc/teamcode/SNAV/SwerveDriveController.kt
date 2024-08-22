ea6be99c9edcc8c908aa4f654901b0716d3e72d9
Titan Spellos <titanspellos@gmail.com>
2024-08-22 15:31:59 -0400

    Field Centric Swerve Control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SNAV/SwerveDriveController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SNAV/SwerveDriveController.kt
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.SNAV
 
 import org.firstinspires.ftc.teamcode.SNAV.Util.Angle
+import kotlin.math.cos
+import kotlin.math.sin
 import java.lang.Math.toDegrees
+import java.lang.Math.toRadians
 import kotlin.math.atan2
 import kotlin.math.pow
 import kotlin.math.sqrt
@@ -11,22 +14,41 @@ class SwerveDriveController(val leftModule: SwerveModule, val rightModule: Swerv
     var ySpeedPower: Double = 0.0 // in/s
     var rotationPower: Double = 0.0 // Rad/s
 
-    var leftAngle: Double = 0.0 // in/s
-    var rightAngle: Double = 0.0 // Degrees
-    var leftSpeed: Double = 0.0 // in/s
-    var rightSpeed: Double = 0.0 // Degrees
+    private var leftAngle: Double = 0.0 // in/s
+    private var rightAngle: Double = 0.0 // Degrees
+    private var leftSpeed: Double = 0.0 // in/s
+    private var rightSpeed: Double = 0.0 // Degrees
 
-    fun updateModules() {
-        leftSpeed = sqrt(xSpeedPower.pow(2) + (ySpeedPower - ((rotationPower * leftModule.distanceFromCenter) / 2)).pow(2))
-        rightSpeed = sqrt(xSpeedPower.pow(2) - (ySpeedPower - ((rotationPower * rightModule.distanceFromCenter) / 2)).pow(2))
+    fun updateModules(currentRobotAngle: Double? = null, global: Boolean = true) {
+        if (!global || currentRobotAngle == null) {
+            leftSpeed = sqrt(xSpeedPower.pow(2) + (ySpeedPower - ((rotationPower * leftModule.distanceFromCenter) / 2)).pow(2))
+            rightSpeed = sqrt(xSpeedPower.pow(2) - (ySpeedPower - ((rotationPower * rightModule.distanceFromCenter) / 2)).pow(2))
 
-        leftAngle = toDegrees(atan2(ySpeedPower + (rotationPower * leftModule.distanceFromCenter) / 2, xSpeedPower))
-        rightAngle = toDegrees(atan2(ySpeedPower - (rotationPower * leftModule.distanceFromCenter) / 2, xSpeedPower))
+            leftAngle = toDegrees(atan2(ySpeedPower + (rotationPower * leftModule.distanceFromCenter) / 2, xSpeedPower))
+            rightAngle = toDegrees(atan2(ySpeedPower - (rotationPower * leftModule.distanceFromCenter) / 2, xSpeedPower))
+        } else {
+            val localPowers: Array<Double> = getLocalSpeedPowers(currentRobotAngle)
+            val localXSpeedPower = localPowers[0]
+            val localYSpeedPower = localPowers[1]
+
+            leftSpeed = sqrt((localXSpeedPower + rotationPower * (leftModule.distanceFromCenter / 2)).pow(2) + localYSpeedPower.pow(2))
+            rightSpeed = sqrt((localXSpeedPower - rotationPower * (rightModule.distanceFromCenter / 2)).pow(2) + localYSpeedPower.pow(2))
+
+            leftAngle = atan2(localYSpeedPower + rotationPower * (leftModule.distanceFromCenter / 2), localXSpeedPower)
+            rightAngle = atan2(localYSpeedPower - rotationPower * (leftModule.distanceFromCenter / 2), localXSpeedPower)
+        }
 
         leftModule.setTranslationalSpeed(leftSpeed)
         rightModule.setTranslationalSpeed(rightSpeed)
 
-        leftModule.setTargetAngle(Angle(leftAngle))
-        rightModule.setTargetAngle(Angle(rightAngle))
+        leftModule.targetAngle = Angle(leftAngle)
+        rightModule.targetAngle = Angle(rightAngle)
+    }
+
+    fun getLocalSpeedPowers(currentRobotAngle: Double): Array<Double> {
+        val speed: Array<Double> = arrayOf(0.0)
+        speed[0] = xSpeedPower * cos(currentRobotAngle) + ySpeedPower * sin(currentRobotAngle)
+        speed[1] = -xSpeedPower * cos(currentRobotAngle) + ySpeedPower * cos(currentRobotAngle)
+        return speed
     }
 }
