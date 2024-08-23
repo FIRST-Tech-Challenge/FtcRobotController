c76be67ecc0a0235fabea7003c114f2dd8d09541
samarthmmenon <samarthmmenon@gmail.com>
2024-08-22 23:19:32 -0500

    Created RobotHardware and Manual

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Manual.java
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+
+import java.util.List;
+
+@TeleOp
+public class Manual  extends LinearOpMode{
+
+    private RobotHardware robot = new RobotHardware(this);
+    private boolean isSlow = false;
+
+    @Override
+    public void runOpMode() {
+        // initialize all the hardware, using the hardware class. See how clean and simple this is?
+        robot.init();
+        robot.initCamera();
+
+        // Send telemetry message to signify robot waiting;
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive() && !isStopRequested()) {
+
+            telemetry.addData("Status", "Ready to run...");
+            telemetry.update();
+
+            manageDriveMotors();
+
+
+            telemetryAprilTag();
+
+            if(gamepad2.right_bumper){
+                //robot.goDiagonal(1);
+                robot.goStrafe(-1);
+            }
+            else if (gamepad2.left_bumper){
+                robot.goStrafe(1);
+            }
+
+            // Pace this loop so hands move at a reasonable speed.
+            sleep(50);
+            telemetry.update();
+        }
+    }
+
+    private void manageDriveMotors(){
+        double drive        = 0;
+        double turn         = 0;
+        // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
+        // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+        // This way it's also easy to just drive straight, or just turn.
+        drive = gamepad2.right_stick_y;
+        turn  = gamepad2.right_stick_x;
+
+        //if button on gamepad2 is pressed
+        //then drive and turn should be half
+        if (gamepad2.a){
+            isSlow = true;
+        }
+        if (gamepad2.b){
+            isSlow = false;
+        }
+
+        if (isSlow){
+            drive = drive / 2;
+            turn = turn / 2;
+        }
+
+
+        // Combine drive and turn for blended motion. Use RobotHardware class
+        robot.driveRobot(drive, turn);
+
+        telemetry.addData("Drive", "Left Stick");
+        telemetry.addData("Turn", "Right Stick");
+        telemetry.addData("-", "-------");
+
+        telemetry.addData("Drive Power", "%.2f", drive);
+        telemetry.addData("Turn Power",  "%.2f", turn);
+        telemetry.update();
+    }
+
+    private void telemetryAprilTag(){
+        List<AprilTagDetection> currentDetections = robot.getAprilTag().getDetections();
+        telemetry.addData("# AprilTags Detected", currentDetections.size());
+
+        // Step through the list of detections and display info for each one.
+        for (AprilTagDetection detection : currentDetections) {
+            if (detection.metadata != null) {
+                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+            } else {
+                telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+            }
+
+            if (detection.ftcPose.y <= 5){
+                telemetry.addData("Status", "Running away...");
+                robot.driveRobot(-0.5, 0);
+            }
+        }   // end for() loop
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,276 @@
+/*
+ * Base code to manage the robot. Baseline copied from FtcRobotController sample code
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Blinker;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.firstinspires.ftc.vision.tfod.TfodProcessor;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+public class RobotHardware {
+
+    public static enum MotorDirection {
+        FORWARD,
+        BACKWARD
+    }
+    private Blinker control_Hub;
+    private IMU imu;
+
+    /* Declare OpMode members. */
+    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+
+    // naming motors
+    private DcMotor rfMotor;
+    private DcMotor rbMotor;
+    private DcMotor lfMotor;
+    private DcMotor lbMotor;
+    private DcMotor armMotorL   = null;
+    private DcMotor armMotorR   = null;
+    private DcMotor leftHand    = null;
+    private DcMotor rightHand   = null;
+    private double ticksPerRotationOfLeft;
+    private double ticksPerRotationOfRight;
+
+    private Servo   elbowServo  = null;
+    private Servo   grabberServo= null;
+    private Servo   launcher    = null;
+
+    // Define Drive constants.  Make them public so they CAN be used by the calling OpMode
+    public static final double GRABBER_SPEED   =  0.10 ;
+    public static final double ELBOW_SPEED     =  0.03 ;  // sets rate to move servo
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+
+    public static final double LAUNCHER_MIN     = 0.0;
+    public static final double LAUNCHER_MAX     = 0.4;
+    public static final double ELBOW_MIN        = 0.05;
+    public static final double ELBOW_MAX        = 0.80;
+    public static final double GRABBER_MIN        = 0.25;
+    public static final double GRABBER_MAX        = 0.60;
+
+    private static double elbowDrive           = 0.5;
+    private static double grabberDrive         = 0.0;
+
+    //camera
+    private Camera camera;
+    private static final boolean USE_WEBCAM = true;
+
+    //The variable to store our instance of the AprilTag processor.
+    private AprilTagProcessor aprilTag;
+
+    //The variable to store our instance of the vision portal.
+    private VisionPortal myVisionPortal;
+
+    /** The relativeLayout field is used to aid in providing interesting visual feedback
+     * in this sample application; you probably *don't* need this when you use a color sensor on your
+     * robot. Note that you won't see anything change on the Driver Station, only on the Robot Controller. */
+    private View relativeLayout;
+
+    // Define a constructor that allows the OpMode to pass a reference to itself.
+    public RobotHardware (LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    /**
+     * Initialize all the robot's hardware.
+     * This method must be called ONCE when the OpMode is initialized.
+     * <p>
+     * All of the hardware devices are accessed via the hardware map, and initialized.
+     */
+    public void init()    {
+        //Mapping hardware devices
+        rfMotor = myOpMode.hardwareMap.get(DcMotor.class, "motorRF");
+        rbMotor = myOpMode.hardwareMap.get(DcMotor.class, "motorRB");
+        lfMotor = myOpMode.hardwareMap.get(DcMotor.class, "motorLF");
+        lbMotor = myOpMode.hardwareMap.get(DcMotor.class, "motorLB");
+
+
+        myOpMode.telemetry.addData("Status", "Hardware Initialized");
+        myOpMode.telemetry.update();
+    }
+
+    /**
+     * Initialize all the robot's camera.
+     * This method must be called ONCE when the OpMode is initialized.
+     * <p>
+     * All of the hardware devices are accessed via the hardware map, and initialized.
+     */
+    public void initCamera() {
+        // -----------------------------------------------------------------------------------------
+        // AprilTag Configuration
+        // -----------------------------------------------------------------------------------------
+
+        aprilTag = new AprilTagProcessor.Builder()
+                .build();
+
+        // -----------------------------------------------------------------------------------------
+        // Camera Configuration
+        // -----------------------------------------------------------------------------------------
+
+        myVisionPortal = new VisionPortal.Builder()
+                    .setCamera(myOpMode.hardwareMap.get(WebcamName.class, "webcam1"))
+                    .addProcessors( aprilTag)
+                    .build();
+
+    }   // end initDoubleVision()
+
+    public AprilTagProcessor getAprilTag(){
+        return aprilTag;
+    }
+
+    public void setMotorsMode(DcMotor.RunMode MotorRunMode) {
+        rfMotor.setMode(MotorRunMode);
+        rbMotor.setMode(MotorRunMode);
+        lfMotor.setMode(MotorRunMode);
+        lbMotor.setMode(MotorRunMode);
+
+    }
+
+    public int getLeftMotorCurrentPosition(){
+        return lfMotor.getCurrentPosition() ;
+    }
+    public int getRightMotorCurrentPosition(){
+        return rfMotor.getCurrentPosition();
+    }
+    public double getLeftMotorRotations(){
+        return lfMotor.getCurrentPosition() / ticksPerRotationOfLeft;
+    }
+    public double getRightMotorRotations(){
+        return rfMotor.getCurrentPosition() / ticksPerRotationOfRight;
+    }
+
+    public void setLeftTargetPosition(int leftTarget){
+        lfMotor.setTargetPosition(leftTarget);
+        lbMotor.setTargetPosition(leftTarget);
+    }
+
+    public void setRightTargetPosition(int rightTarget){
+        rfMotor.setTargetPosition(rightTarget);
+        rbMotor.setTargetPosition(rightTarget);
+    }
+
+    public boolean isLeftMotorBusy()
+    {
+        return lfMotor.isBusy();
+    }
+
+    public boolean isRightMotorBusy()
+    {
+        return rfMotor.isBusy();
+    }
+
+    public void setMotorDirection(MotorDirection direction){
+        if (direction == MotorDirection.BACKWARD)
+        {
+            lfMotor.setDirection(DcMotor.Direction.FORWARD);
+            lbMotor.setDirection(DcMotor.Direction.FORWARD);
+            rfMotor.setDirection(DcMotor.Direction.REVERSE);
+            rbMotor.setDirection(DcMotor.Direction.REVERSE);
+        }
+        else
+        {
+            lfMotor.setDirection(DcMotor.Direction.REVERSE);
+            lbMotor.setDirection(DcMotor.Direction.REVERSE);
+            rfMotor.setDirection(DcMotor.Direction.FORWARD);
+            rbMotor.setDirection(DcMotor.Direction.FORWARD);
+        }
+
+    }
+    public void reverseMotors(){
+        setMotorDirection(MotorDirection.BACKWARD);
+    }
+
+
+    /**
+     * Calculates the left/right motor powers required to achieve the requested
+     * robot motions: Drive (Axial motion) and Turn (Yaw motion).
+     * Then sends these power levels to the motors.
+     *
+     * @param Drive     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param Turn      Right/Left turning power (-1.0 to 1.0) +ve is CW
+     */
+    public void driveRobot(double Drive, double Turn) {
+        // Combine drive and turn for blended motion.
+        double left  = Drive + Turn;
+        double right = Drive - Turn;
+
+        // Scale the values so neither exceed +/- 1.0
+        double max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
+
+        // Use existing function to drive both wheels.
+        setDrivePower(left, right);
+    }
+
+    /**
+     * Pass the requested wheel motor powers to the appropriate hardware drive motors.
+     *
+     * @param leftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param rightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     */
+    public void setDrivePower(double leftWheel, double rightWheel) {
+        // Output the values to the motor drives.
+        //leftDrive.setPower(leftWheel);
+        //rightDrive.setPower(rightWheel);
+
+        rfMotor.setPower(rightWheel);
+        rbMotor.setPower(rightWheel);
+        lfMotor.setPower(leftWheel);
+        lbMotor.setPower(leftWheel);
+    }
+
+    public void goDiagonal(double num){
+        //rfMotor.setPower(-num);
+        rbMotor.setPower(num);
+        lfMotor.setPower(num);
+        //lbMotor.setPower(-num);
+    }
+
+    public void goStrafe (double num){
+        rfMotor.setPower(-num);
+        rbMotor.setPower(num);
+        lfMotor.setPower(num);
+        lbMotor.setPower(-num);
+    }
+
+    /**
+     * Pass the requested arm power to the appropriate hardware drive motor
+     *      Power is reduced by 25%
+     *
+     * @param power Should the Arm be moved up or down
+     */
+
+}
