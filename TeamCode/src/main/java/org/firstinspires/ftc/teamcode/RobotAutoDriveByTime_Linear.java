10529be82d02ac04dda9f0cd2a1a5541ac1967f3
Milla <milla.fishler@outlook.com>
2024-08-22 19:16:03 -0700

    Clean up and organizing code.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveByTime_Linear.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -36,43 +7,25 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-/*
- * This OpMode illustrates the concept of driving a path based on time.
- * The code is structured as a LinearOpMode
- *
- * The code assumes that you do NOT have encoders on the wheels,
- *   otherwise you would use: RobotAutoDriveByEncoder;
- *
- *   The desired path in this example is:
- *   - Drive forward for 3 seconds
- *   - Spin right for 1.3 seconds
- *   - Drive Backward for 1 Second
- *
- *  The code is written in a simple form with no optimizations.
- *  However, there are several ways that this type of sequence could be streamlined,
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
-
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
+@Autonomous(name="Vanilla", group="Robot")
 
 public class RobotAutoDriveByTime_Linear extends LinearOpMode {
 
-    private final ElapsedTime     runtime = new ElapsedTime();
-
-    // Initialize the drive system variables.
+    // Initialize all variables for the program
     private IMU imu = null;
     private DcMotor leftFrontDrive = null;
     private DcMotor rightFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightBackDrive = null;
 
+    private final ElapsedTime     runtime = new ElapsedTime();
     static final double     FORWARD_SPEED = 0.6;
     static final double     TURN_SPEED    = 0.5;
 
+
     @Override
     public void runOpMode() {
+        // Define all the hardware
         leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
         leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
@@ -82,8 +35,6 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-
-        // Initialize the IMU configuration
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
         RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
         RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
@@ -97,12 +48,14 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
+
         // Callie and Neha: write autonomous code here! - - - - - - - - - - - - - - - - - - - - - -
         moveForward(0.5);
+        moveForward(0.5, 0.8);
         turnLeft(1);
         moveBackward(1.2);
         turnRight(.4);
-
+        // End of autonomous program
 
         telemetry.addData("Path", "Complete");
         telemetry.update();
@@ -110,49 +63,82 @@ public class RobotAutoDriveByTime_Linear extends LinearOpMode {
     }
 
     private void moveForward(double secondsToDrive) {
-        leftFrontDrive.setPower(FORWARD_SPEED);
-        rightFrontDrive.setPower(FORWARD_SPEED);
+        moveForward(secondsToDrive, FORWARD_SPEED);
+    }
+
+    private void moveForward(double secondsToDrive, double speedToDrive) {
+        leftFrontDrive.setPower(speedToDrive);
+        rightFrontDrive.setPower(speedToDrive);
+        leftBackDrive.setPower(speedToDrive);
+        rightBackDrive.setPower(speedToDrive);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < secondsToDrive)) {
-            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.addData("Path", "Move forward: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
     }
 
     private void moveBackward(double secondsToDrive) {
-        leftFrontDrive.setPower(-FORWARD_SPEED);
-        rightFrontDrive.setPower(-FORWARD_SPEED);
+        moveBackward(secondsToDrive, FORWARD_SPEED);
+    }
+
+    private void moveBackward(double secondsToDrive, double speedToDrive) {
+        leftFrontDrive.setPower(-speedToDrive);
+        rightFrontDrive.setPower(-speedToDrive);
+        leftBackDrive.setPower(-speedToDrive);
+        rightBackDrive.setPower(-speedToDrive);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < secondsToDrive)) {
-            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.addData("Path", "Move backward: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
     }
 
     private void turnRight(double secondsToDrive) {
-        leftFrontDrive.setPower(FORWARD_SPEED);
-        rightFrontDrive.setPower(-FORWARD_SPEED);
-        runtime.reset();
-        while (opModeIsActive() && (runtime.seconds() < secondsToDrive)) {
-            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
-            telemetry.update();
-        }
-        leftFrontDrive.setPower(0);
-        rightFrontDrive.setPower(0);
+        turnRight(secondsToDrive, FORWARD_SPEED);
     }
-    private void turnLeft(double secondsToDrive) {
-        leftFrontDrive.setPower(-FORWARD_SPEED);
-        rightFrontDrive.setPower(FORWARD_SPEED);
+
+    private void turnRight(double secondsToDrive, double speedToDrive) {
+        leftFrontDrive.setPower(speedToDrive);
+        rightFrontDrive.setPower(-speedToDrive);
+        leftBackDrive.setPower(speedToDrive);
+        rightBackDrive.setPower(-speedToDrive);
         runtime.reset();
         while (opModeIsActive() && (runtime.seconds() < secondsToDrive)) {
-            telemetry.addData("Path", "Leg 1: %4.1f S Elapsed", runtime.seconds());
+            telemetry.addData("Path", "Turn right: %4.1f S Elapsed", runtime.seconds());
             telemetry.update();
         }
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
+    }
+
+    private void turnLeft(double secondsToDrive) {
+        turnLeft(secondsToDrive, FORWARD_SPEED);
+    }
+
+    private void turnLeft(double secondsToDrive, double speedToDrive) {
+        leftFrontDrive.setPower(-speedToDrive);
+        rightFrontDrive.setPower(speedToDrive);
+        leftBackDrive.setPower(-speedToDrive);
+        rightBackDrive.setPower(speedToDrive);
+        runtime.reset();
+        while (opModeIsActive() && (runtime.seconds() < secondsToDrive)) {
+            telemetry.addData("Path", "Turn left: %4.1f S Elapsed", runtime.seconds());
+            telemetry.update();
+        }
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftBackDrive.setPower(0);
+        rightBackDrive.setPower(0);
     }
 }
