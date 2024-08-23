903eff1091cd3c0cec1e54b6864ef1a795067183
Masterpro15 <evanckurian@gmail.com>
2024-08-23 17:15:18 -0500

    Adding Code for the Homework

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name = "BasicAutonomous", group = "teamcode")
+
+
+public class BasicAutonomous extends LinearOpMode {
+    Robot robot = new Robot();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException{
+        robot.leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
+        robot.rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        robot.leftBack  = hardwareMap.get(DcMotor.class, "leftRear");
+        robot.rightBack = hardwareMap.get(DcMotor.class, "rightRear");
+
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+        }
+
+
+
+    }
+    public void moveFoward(double power, long time){
+
+        robot.leftFront.setPower(power);
+        robot.leftBack.setPower(power);
+        robot.rightFront.setPower(power);
+        robot.rightBack.setPower(power);
+
+        sleep(time);
+    }
+    public void turnRight(double power, long time){
+
+        robot.leftFront.setPower(-power);
+        robot. leftBack.setPower(-power);
+        robot. rightFront.setPower(power);
+        robot.  rightBack.setPower(power);
+
+        sleep(time);
+    }
+    public void turnLeft(double power, long time){
+
+        robot.leftFront.setPower(power);
+        robot.leftBack.setPower(power);
+        robot.rightFront.setPower(-power);
+        robot. rightBack.setPower(-power);
+
+        sleep(time);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    public HardwareMap hardwareMap;
+    public DcMotor leftFront;
+    public DcMotor rightFront;
+    public DcMotor leftBack;
+    public DcMotor rightBack;
+
+    public Robot(){
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftRear");
+        rightBack = hardwareMap.get(DcMotor.class, "rightRear");
+    }
+
+
+}
