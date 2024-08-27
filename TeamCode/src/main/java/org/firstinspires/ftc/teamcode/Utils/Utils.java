e7528bc6844ee264cd3dd25e42941c8809708188
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-08-27 14:34:37 -0400

    posecontroller + some utils

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Utils.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Utils;
 
 import org.ejml.simple.SimpleMatrix;
 public class Utils {
+    public static double r = 50;
+    public static double l = 320 / 2;
+    public static double w = 350 / 2;
     public static SimpleMatrix R(double th) {
         SimpleMatrix rotation = new SimpleMatrix(
                 new double[][]{
@@ -12,12 +15,35 @@ public class Utils {
         );
         return rotation;
     }
-
+    static SimpleMatrix H_inv = new SimpleMatrix(
+            new double[][]{
+                    new double[]{1d, -1d, -(l + w)},
+                    new double[]{1d, 1d, -(l + w)},
+                    new double[]{1d, -1d, (l + w)},
+                    new double[]{1d, 1d, (l + w)}
+            }
+    ); //Inverse kinematics Matrix
     public static SimpleMatrix rotateBodyToGlobal(SimpleMatrix vectorBody, double th) {
         return R(th).mult(vectorBody);
     }
 
     public static SimpleMatrix rotateGlobalToBody(SimpleMatrix vectorGlobal, double th) {
         return R(th).invert().mult(vectorGlobal);
+    }
+    public static SimpleMatrix inverseKinematics(SimpleMatrix twist) {
+        SimpleMatrix wheelSpeeds = H_inv.scale(1 / r).mult(twist);
+        return wheelSpeeds;
+    }
+    public static double angleWrap(double radians) {
+
+        while (radians > Math.PI) {
+            radians -= 2 * Math.PI;
+        }
+        while (radians < -Math.PI) {
+            radians += 2 * Math.PI;
+        }
+
+        // keep in mind that the result is in radians
+        return radians;
     }
 }
