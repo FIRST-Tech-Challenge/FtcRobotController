fbdd81fcc616564a66818a36e89324e33ba0e8a5
Ainacteru <gbenawijr@gmail.com>
2024-08-28 16:31:15 -0700

    Fixed so you can actually use it

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMain.java
@@ -1,12 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp(name="Main")
 public class TeleMain extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
         //run once
+        waitForStart();
 
         while (opModeIsActive())
         {
