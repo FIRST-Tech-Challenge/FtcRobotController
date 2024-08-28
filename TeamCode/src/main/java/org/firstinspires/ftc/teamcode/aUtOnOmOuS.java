02ca3a05326116e8a2fa1b989ae872a57e6ee170
NTCheese <arirahmani2021@gmail.com>
2024-08-28 16:32:11 -0700

    This is a test autonomous because I don't really know what I'm doing. "The building is only as strong as its foundation or something like that" - Sun Tzu I think

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/aUtOnOmOuS.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="Main")
+
+public class aUtOnOmOuS extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+
+        waitForStart();
+
+        while (opModeIsActive())
+        {
+            //if you have ligma type this
+        }
+    }
+}
