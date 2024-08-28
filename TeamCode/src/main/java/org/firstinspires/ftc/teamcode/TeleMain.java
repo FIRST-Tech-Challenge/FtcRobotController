e07aefe53726d45be60d2f7f96aa032496e7cab0
Ainacteru <gbenawijr@gmail.com>
2024-08-28 16:23:47 -0700

    Main Teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleMain.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class TeleMain extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //run once
+
+        while (opModeIsActive())
+        {
+            //Repeat stuff
+        }
+    }
+}
