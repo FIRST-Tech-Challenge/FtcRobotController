36d5db6905d2cd43a5bbbfcb155834be121d365d
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-08-28 10:51:41 -0400

    a lil bit of documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/FeedForward.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Controllers/FeedForward.java
@@ -4,14 +4,27 @@ public class FeedForward {
     public double kV;
     public double kA;
     public double kS;
+
+    /***
+     *
+     * @param kV (double) Voltage constant
+     * @param kA (double) Acceleration constant
+     * @param kS (double) Velocity constant
+     */
     public FeedForward(double kV, double kA, double kS) {
         this.kV = kV;
         this.kA = kA;
         this.kS = kS;
     }
+
+    /***
+     *
+     * @param desiredVelocity (double) The target velocity of our bot
+     * @param desiredAcceleration (double) The target acceleration of our bot
+     * @return (double) The voltage calculation to send to the motors
+     */
     public double calculate(double desiredVelocity, double desiredAcceleration){
         return kV*desiredVelocity+kA*desiredAcceleration+kS*Math.signum(desiredVelocity);
     }
 }
-
 
