8da59572885a89038ae3a7e4190a4bb190f6c6fb
Ainacteru <gbenawijr@gmail.com>
2024-08-28 20:09:59 -0700

    Made folders for both teleop and autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/aUtOnOmOuS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/aUtOnOmOuS.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.Autonomous;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
