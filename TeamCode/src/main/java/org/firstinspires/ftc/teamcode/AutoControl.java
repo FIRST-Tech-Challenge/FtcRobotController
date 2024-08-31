9f29f4fe5c40b5cf61559c812bae783b595c3dd9
Jimmy Computer <Wyverns12889@gmail.com>
2024-08-31 13:58:33 -0700

    working aprilTags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoControl.java
@@ -12,13 +12,12 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
 import java.util.function.BooleanSupplier;
 
-
+@Autonomous
 public class AutoControl extends OpMode{
     OpticalSensor opticalSensor;
-    public Drive drive;
+    public RobotClass robot;
     AprilTagPipeline aprilTagPipeline;
-    Auto auto;
-    public static boolean isFinished = false;
+
     boolean stop = false;
     BooleanSupplier isStopRequested = new BooleanSupplier() {
         @Override
@@ -28,20 +27,16 @@ public class AutoControl extends OpMode{
     };
     @Override
     public void init(){
+        robot = new RobotClass(hardwareMap);
+        aprilTagPipeline = new AprilTagPipeline(robot.webcamName, telemetry);
 
-        aprilTagPipeline = new AprilTagPipeline(drive.webcamName, telemetry);
-        drive = new Drive(hardwareMap);
-        auto = new Auto();
         //opticalSensor = new OpticalSensor(drive);
+        robot.setDirection();
     }
-    @Override
-    public void start(){
 
-    }
     @Override
     public void loop(){
         aprilTagPipeline.updateAprilTagPipeline();
-
     }
     public static final int WHEEL_RADIUS = 2;
     public static final int CPR_OUTPUT_SHAFT_20TO1 = 560;
@@ -50,10 +45,10 @@ public class AutoControl extends OpMode{
     private final double kD = .01;
     public void AutoDrive(double targetDistance_INCH, double angle){
 
-        telemetry.addData("motor 0 pos", drive.drivetrain.driveMotors[0].getCurrentPosition());
+        telemetry.addData("motor 0 pos", robot.drivetrain.driveMotors[0].getCurrentPosition());
         telemetry.update();
 
-        angle -= drive.getHeading();
+        angle -= robot.getHeading();
 
         double maxErrorAllowed = 1.1 * TICKS_PER_INCH;
         double kP = 0.5;
@@ -63,16 +58,13 @@ public class AutoControl extends OpMode{
         double error, correction;
         double targetDistance = targetDistance_INCH * TICKS_PER_INCH;
 
-        drive.drivetrain.driveMotors[0].setDirection(DcMotorSimple.Direction.FORWARD); //FLM
-        drive.drivetrain.driveMotors[1].setDirection(DcMotorSimple.Direction.FORWARD); //BLM
-        drive.drivetrain.driveMotors[2].setDirection(DcMotorSimple.Direction.REVERSE); //BRM
-        drive.drivetrain.driveMotors[3].setDirection(DcMotorSimple.Direction.FORWARD);
+
 
         while(!isStopRequested.getAsBoolean()){
 
             //SparkFunOTOS.Pose2D Pose2D = opticalSensor.getPos();
 
-            error = targetDistance - drive.drivetrain.driveMotors[0].getCurrentPosition();
+            error = targetDistance - robot.drivetrain.driveMotors[0].getCurrentPosition();
             //(targetDistance - Math.sqrt(Math.pow((targetDistance * Math.cos(angle)
             //- Pose2D.x), 2) + (Math.pow((targetDistance * Math.sin(angle)) - Pose2D.y, 2))));
 
@@ -95,20 +87,20 @@ public class AutoControl extends OpMode{
 
 
 
-            drive.drivetrain.driveMotors[RobotClass.kFrontLeft].setPower((((rotY + rotX) / denominator)) / correction);
-            drive.drivetrain.driveMotors[RobotClass.kBackLeft].setPower((((rotY - rotX) / denominator)) / correction);
-            drive.drivetrain.driveMotors[RobotClass.kFrontRight].setPower((((rotY - rotX) / denominator)) / correction);
-            drive.drivetrain.driveMotors[RobotClass.kBackRight].setPower((((rotY + rotX) / denominator)) / correction);
+            robot.drivetrain.driveMotors[RobotClass.kFrontLeft].setPower((((rotY + rotX) / denominator)) / correction);
+            robot.drivetrain.driveMotors[RobotClass.kBackLeft].setPower((((rotY - rotX) / denominator)) / correction);
+            robot.drivetrain.driveMotors[RobotClass.kFrontRight].setPower((((rotY - rotX) / denominator)) / correction);
+            robot.drivetrain.driveMotors[RobotClass.kBackRight].setPower((((rotY + rotX) / denominator)) / correction);
 
         }
-        drive.drivetrain.setPowerBasic(0.0);
+
     }
     public void AutoTurn(double angle){
         double loopCounter = 0;
         boolean atTarget = false;
         double angularDistance = 0;
 
-        double initialAngle = drive.getHeading();
+        double initialAngle = robot.getHeading();
 
         do{
             double turnVal = 1;
@@ -116,7 +108,7 @@ public class AutoControl extends OpMode{
 
             if(angle - initialAngle < 0) turnVal = -1; // checks which way it should turn
 
-            double currentAngle = drive.getHeading();
+            double currentAngle = robot.getHeading();
 
             angularDistance = Math.abs(currentAngle - angle);
             if(angularDistance > 180){ // dealing with edge case
@@ -128,10 +120,10 @@ public class AutoControl extends OpMode{
             powerReduce = Math.max(powerReduce, 0.2);
             powerReduce = Math.min(powerReduce, 1);
 
-            drive.drivetrain.driveMotors[0].setPower(turnVal * powerReduce);
-            drive.drivetrain.driveMotors[1].setPower(turnVal * powerReduce);
-            drive.drivetrain.driveMotors[2].setPower(-turnVal * powerReduce);
-            drive.drivetrain.driveMotors[3].setPower(-turnVal * powerReduce);
+            robot.drivetrain.driveMotors[0].setPower(turnVal * powerReduce);
+            robot.drivetrain.driveMotors[1].setPower(turnVal * powerReduce);
+            robot.drivetrain.driveMotors[2].setPower(-turnVal * powerReduce);
+            robot.drivetrain.driveMotors[3].setPower(-turnVal * powerReduce);
 
             if(angularDistance < 1.5) atTarget = true;
         }
@@ -140,16 +132,16 @@ public class AutoControl extends OpMode{
 
     private double getDerivative(double error, double targetDistance){
         double previousError = error;
-        error = targetDistance - drive.drivetrain.driveMotors[0].getCurrentPosition();
+        error = targetDistance - robot.drivetrain.driveMotors[0].getCurrentPosition();
         return(kD * error ) / (previousError + 0.01);
     }
     @Override
     public void stop(){
         boolean stop = true;
-        if(drive == null) return; // ensures that stop() is not called before initialization
-        drive.drivetrain.driveMotors[0].setPower(0);
-        drive.drivetrain.driveMotors[1].setPower(0);
-        drive.drivetrain.driveMotors[2].setPower(0);
-        drive.drivetrain.driveMotors[3].setPower(0);
+        if(robot == null) return; // ensures that stop() is not called before initialization
+        robot.drivetrain.driveMotors[0].setPower(0);
+        robot.drivetrain.driveMotors[1].setPower(0);
+        robot.drivetrain.driveMotors[2].setPower(0);
+        robot.drivetrain.driveMotors[3].setPower(0);
     }
 }
