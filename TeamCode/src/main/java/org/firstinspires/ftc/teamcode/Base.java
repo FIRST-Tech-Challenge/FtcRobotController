c8e5cc25e1096448913d7f816487174541f5650f
Owen <27orinne@granvilleschools.org>
2024-09-01 15:41:39 -0400

    Can see control hub name when initializing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Base.java
@@ -40,10 +40,11 @@ public abstract class Base extends LinearOpMode {
      - For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
     //*/
     public String           hubName                 = hardwareMap.get(String.class, "Control Hub");
-    public static final String SMALL_WHEEL_ROBOT_NAME = "Control Hub 1";
-    public static final String LARGE_WHEEL_ROBOT_NAME = "Control Hub 2";
+    public static final String SMALL_WHEEL_ROBOT_NAME = "Expansion Hub 2";
+    public static final String LARGE_WHEEL_ROBOT_NAME = "Control Hub";
     static double           WHEEL_DIAMETER_INCHES;
     {
+        print("Hub Name", hubName);
         if (SMALL_WHEEL_ROBOT_NAME.equals(hubName)) {
             WHEEL_DIAMETER_INCHES = 3.77953;     // For figuring out circumference
         } else if (LARGE_WHEEL_ROBOT_NAME.equals(hubName)) {
@@ -161,6 +162,7 @@ public abstract class Base extends LinearOpMode {
 
 
         print("Status", "Initialized");
+        print("Hub Name", hubName);
         update();
 
         allianceColor = teamColor;
