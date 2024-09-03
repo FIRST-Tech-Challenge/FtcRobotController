360d39615fe150b761f7bf87c038b0222e4e6531
Andrew <andrewgo@hotmail.com>
2024-09-02 17:58:46 -0700

    New menu infrastructure.

--- team417/src/main/java/org/firstinspires/ftc/team417/roadrunner/tuning/LooneyTuner.java
+++ team417/src/main/java/org/firstinspires/ftc/team417/roadrunner/tuning/LooneyTuner.java
@@ -64,7 +64,270 @@ import org.firstinspires.ftc.team417.roadrunner.MecanumDrive;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BooleanSupplier;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
 import java.util.prefs.Preferences;
+
+/** @noinspection StringConcatenationInsideStringBufferAppend*/
+class Settings {
+    static private final String DESCRIPTOR_SEPARATOR = "::";
+    private static Settings settings; // Points to our own  singleton object
+    Telemetry telemetry; // Telemetry object used for output
+    Gamepad gamepad; // Gamepad to use for settings control
+    ArrayList<Option> menuStack = new ArrayList<>(); // Stack of menus, the last is the current
+
+    abstract private static class Option {
+        String description;
+        Option(String descriptor) {
+            // The description comes after the last separator:
+            int lastIndex = descriptor.lastIndexOf(DESCRIPTOR_SEPARATOR);
+            if (lastIndex != -1) {
+                descriptor = descriptor.substring(lastIndex + DESCRIPTOR_SEPARATOR.length());
+            }
+            description = descriptor;
+        }
+        abstract public String string();
+    }
+    private static class MenuOption extends Option {
+        ArrayList<Option> options = new ArrayList<>(); // List of options in this menu
+        int current; // Index in options that has the UI focus
+        public MenuOption(String descriptor) {
+            super(descriptor);
+        }
+        public String string() {
+            return "\uD83D\uDCC1 " + description + "..."; // Folder symbol
+        }
+    }
+    private static class ToggleOption extends Option {
+        boolean value;
+        Consumer<Boolean> callback;
+        public ToggleOption(String descriptor, boolean value, Consumer<Boolean> callback) {
+            super(descriptor); this.value = value; this.callback = callback;
+        }
+        /** @noinspection UnnecessaryUnicodeEscape*/
+        public String string() {
+            return (value ? "\u2612" : "\u2610") + " " + description; // checked-box, empty box
+        }
+    }
+    private static class ListOption extends Option {
+        int index;
+        String[] list;
+        BiConsumer<Integer, String> callback;
+        public ListOption(String descriptor, int index, String[] list, BiConsumer<Integer, String> callback) {
+            super(descriptor); this.index = index; this.list = list; this.callback = callback;
+        }
+        /** @noinspection UnnecessaryUnicodeEscape*/
+        public String string() {
+            return "\u2194\uFE0F <b>" + list[index] + "</b>: " + description; // blue left/right arrow
+        }
+    }
+    private static class ActivationOption extends Option {
+        Function<Boolean, String> callback;
+        public ActivationOption(String descriptor, Function<Boolean, String> callback) {
+            super(descriptor); this.callback = callback;
+        }
+        /** @noinspection UnnecessaryUnicodeEscape*/
+        public String string() {
+            return "\u2757 " + callback.apply(false); // red exclamation mark
+        }
+    }
+    private static class StatsOption extends Option {
+        Supplier<String> callback;
+        public StatsOption(String descriptor, Supplier<String> callback) {
+            super(descriptor); this.callback = callback;
+        }
+        public String string() { return "\uD83D\uDCCA " + description + "..."; }
+    }
+    private static class RunOption extends Option {
+        Runnable runnable;
+        BooleanSupplier enableCheck;
+        public RunOption(String descriptor, Runnable runnable, BooleanSupplier enableCheck) {
+            super(descriptor); this.runnable = runnable; this.enableCheck = enableCheck;
+        }
+        public String string() {
+            if ((enableCheck == null) || (enableCheck.getAsBoolean()))
+                return "o " + description;
+            else
+                return "x <font color='#808080'>" + description + "</font>";
+        }
+    }
+
+    // Button press state:
+    private final boolean[] buttonPressed = new boolean[7];
+    private boolean buttonPress(boolean pressed, int index) {
+        boolean press = pressed && !buttonPressed[index];
+        buttonPressed[index] = pressed;
+        return press;
+    }
+
+    // Button press status:
+    boolean select() { return buttonPress(gamepad.a, 0); }
+    boolean cancel() { return buttonPress(gamepad.b, 1); }
+    boolean left() { return buttonPress(gamepad.dpad_left, 2); }
+    boolean right() { return buttonPress(gamepad.dpad_right, 3); }
+    boolean up() { return buttonPress(gamepad.dpad_up, 4); }
+    boolean down() { return buttonPress(gamepad.dpad_down, 5); }
+    boolean start() { return buttonPress(gamepad.start, 6); }
+
+    // Constructor:
+    public Settings(Telemetry telemetry, Gamepad gamepad) {
+        Settings.settings = this;
+
+        this.telemetry = telemetry;
+        this.gamepad = gamepad;
+
+        // Create the root settings menu:
+        menuStack.add(new MenuOption("Settings"));
+    }
+
+    // Update loop for Settings. If true is returned, the caller should not use gamepad input
+    // because the Settings UI is active:
+    void update() {
+        StringBuilder output = new StringBuilder();
+
+        // Add a header with submenu names:
+        output.append("<h2>");
+        for (int i = 0; i < menuStack.size(); i++) {
+            if (i > 0)
+                output.append("·");
+            output.append(menuStack.get(i).description);
+        }
+        output.append("</h2>");
+
+        MenuOption menu = (MenuOption) menuStack.get(menuStack.size() - 1);
+        if (up()) {
+            menu.current--;
+            if (menu.current < 0)
+                menu.current = 0;
+        }
+        if (down()) {
+            menu.current++;
+            if (menu.current == menu.options.size())
+                menu.current = menu.options.size() - 1;
+        }
+
+        // Now output the options:
+        for (int i = 0; i < menu.options.size(); i++) {
+            if (i == menu.current) // Draw the highlight:
+                output.append("<span style='background: #88285a'>--&gt;" + menu.options.get(i).string() + "</span>\n");
+            else
+                output.append(menu.options.get(i).string() + "\n");
+        }
+        telemetry.addLine(output.toString());
+
+        Option option = menu.options.get(menu.current);
+        if (cancel()) {
+            if (menuStack.size() > 1) {
+                // Pop up the menu stack:
+                menuStack.remove(menuStack.size() - 1);
+            }
+        }
+        else if (option instanceof ToggleOption) {
+            ToggleOption toggleOption = (ToggleOption) option;
+            if (select()) {
+                toggleOption.value = !toggleOption.value;
+                toggleOption.callback.accept(toggleOption.value);
+            }
+        } else if (option instanceof ListOption) {
+            ListOption listOption = (ListOption) option;
+            boolean left = left();
+            boolean right = right();
+            if (left || right) {
+                if (left) {
+                    listOption.index--;
+                    if (listOption.index < 0)
+                        listOption.index = 0;
+                }
+                if (right) {
+                    listOption.index++;
+                    if (listOption.index >= listOption.list.length)
+                        listOption.index = listOption.list.length - 1;
+                }
+                listOption.callback.accept(listOption.index, listOption.list[listOption.index]);
+            }
+        } else if (option instanceof ActivationOption) {
+            if (select()) {
+                ActivationOption activationOption = (ActivationOption) option;
+                activationOption.callback.apply(true);
+            }
+        } else if (option instanceof StatsOption) {
+            if (select()) {
+                menuStack.add(option);
+            }
+        } else if (option instanceof MenuOption) {
+            if (select()) {
+                menuStack.add(option);
+            }
+        } else if (option instanceof RunOption) {
+            RunOption runOption = (RunOption) option;
+            if (select()) {
+                if ((runOption.enableCheck == null) || (runOption.enableCheck.getAsBoolean())) {
+                    runOption.runnable.run();
+                }
+            }
+        }
+
+        telemetry.update();
+    }
+
+    // Add a new option to the appropriate spot in the menu hierarchy:
+    private void register(String descriptor, Option newOption) {
+        MenuOption menu = (MenuOption) menuStack.get(0); // Root menu
+
+        // Peel off the hierarchy which is in the form "Vision::Configuration::Setting":
+        while (true) {
+            int index = descriptor.indexOf(DESCRIPTOR_SEPARATOR);
+            if (index == -1)
+                break; // ====>
+
+            // Peel off the first menu name from the descriptor:
+            String submenuName = descriptor.substring(0, index);
+            descriptor = descriptor.substring(index + DESCRIPTOR_SEPARATOR.length());
+
+            // Find or create the submenu:
+            MenuOption submenu = null;
+            for (Option option: menu.options) {
+                if ((option instanceof MenuOption) && (option.description.equals(submenuName))) {
+                    submenu = (MenuOption) option;
+                    break;
+                }
+            }
+            if (submenu == null) {
+                submenu = new MenuOption(submenuName);
+                menu.options.add(submenu);
+            }
+            // Descend into that submenu:
+            menu = submenu;
+        }
+        menu.options.add(newOption);
+    }
+
+    // Add a toggleable option to the Settings menu:
+    public static void registerToggleOption(String descriptor, boolean initialValue, Consumer<Boolean> callback) {
+        callback.accept(initialValue);
+        settings.register(descriptor, new ToggleOption(descriptor, initialValue, callback));
+    }
+    // Add a list option to the Settings menu:
+    public static void registerListOption(String descriptor, String[] list, int initialIndex, BiConsumer<Integer, String> callback) {
+        callback.accept(initialIndex, list[initialIndex]);
+        settings.register(descriptor, new ListOption(descriptor, initialIndex, list, callback));
+    }
+    // Add an option that can only be activated:
+    public static void registerActivationOption(String descriptor, Function<Boolean, String> callback) {
+        callback.apply(true);
+        settings.register(descriptor, new ActivationOption(descriptor, callback));
+    }
+    public static void registerStats(String descriptor, Supplier<String> callback) {
+        settings.register(descriptor, new StatsOption(descriptor, callback));
+    }
+    // Add an option that can be run:
+    public static void registerRunOption(String descriptor, Runnable callback, BooleanSupplier enableCheck) {
+        settings.register(descriptor, new RunOption(descriptor, callback, enableCheck));
+    }
+}
 
 /**
  * Math helper for points and vectors:
@@ -1620,14 +1883,29 @@ public class LooneyTuner extends LinearOpMode {
         ui.prompt("<big><big><big><big><big><big><big><b>Press Gamepad "+A+" or "+B);
 
         // Execute our main menu loop:
-        int selection = 0;
-        while (opModeIsActive()) {
-            String heading = "<h2>Use Dpad to navigate, "+A+" to select</h2>";
-            selection = ui.menu(heading, selection, true,
-                    tests.size(), i -> tests.get(i).description);
+//        int selection = 0;
+//        while (opModeIsActive()) {
+//            String heading = "<h2>Use Dpad to navigate, "+A+" to select</h2>";
+//            selection = ui.menu(heading, selection, true,
+//                    tests.size(), i -> tests.get(i).description);
+//
+//            tests.get(selection).method.invoke();   // Invoke the chosen test
+//            drive.setPose(zeroPose);                // Reset pose for next test
+//        }
 
-            tests.get(selection).method.invoke();   // Invoke the chosen test
-            drive.setPose(zeroPose);                // Reset pose for next test
-        }
+        Settings settings = new Settings(telemetry, gamepad1);
+        settings.registerRunOption("TestA", this::testA, null);
+        settings.registerRunOption("TestB", this::testB, ()->false);
+        settings.registerRunOption("Minor::TestC", this::testB, ()->true);
+
+        while (opModeIsActive())
+            settings.update();
+    }
+
+    void testA() {
+        ui.prompt("This is test A!");
+    }
+    void testB() {
+        ui.prompt("This is test B!");
     }
 }
