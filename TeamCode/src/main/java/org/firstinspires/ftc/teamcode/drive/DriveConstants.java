a48316badfd95f80396b2b5fa2947b7e57f5ffdc
GoldenElf58 <91762091+GoldenElf58@users.noreply.github.com>
2024-09-03 12:55:01 -0400

    Filled out drive constants
    Filled out drive constants for large chassis, need to change if we put odometry on other chassis. Also, I am not sure if the gear ratio is correct.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -21,8 +21,9 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+    // The following constants are for the larger chassis
+    public static final double TICKS_PER_REV = ((((1.0+(46.0/11.0))) * (1.0+(46.0/11.0))) * 28.0);
+    public static final double MAX_RPM = 223;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -44,9 +45,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 2.755905511811024; // in, for larger wheels
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in
+    public static double TRACK_WIDTH = 17.02755905511811 // in, not filled out
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
