a4b6d85bd1a351c95a3bcccd7b853684b18df0e8
ColinCMcC <118211798+ColinCMcC@users.noreply.github.com>
2024-09-03 20:22:00 -0500

    9-3-24
    Started the file for Jim's SwerveBot
    Includes initRobot() function
    Made functions to convert cartesian to polar and vice versa

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JimBot.java
@@ -0,0 +1,116 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.opencv.core.Mat;
+
+import java.util.Vector;
+
+
+public class JimBot extends LinearOpMode {
+
+    DcMotor FLMotor, BLMotor, BRMotor, FRMotor;
+
+    Servo FLServo, BLServo, BRServo, FRServo;
+
+
+    @Override
+    public void runOpMode() {
+
+        initRobot(); // Does all the robot stuff
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double speed = gamepad1.right_trigger + (-gamepad1.left_trigger); // Makes it so that the triggers cancel each other out if both are pulled at the same time
+
+            FLMotor.setPower(speed);
+            BLMotor.setPower(speed);
+            BRMotor.setPower(speed);
+            FRMotor.setPower(speed);
+
+            FLServo.setPosition((gamepad1.left_stick_x + 1) / 2);
+            BLServo.setPosition((gamepad1.left_stick_x + 1) / 2);
+            BRServo.setPosition((gamepad1.left_stick_x + 1) / 2);
+            FRServo.setPosition((gamepad1.left_stick_x + 1) / 2);
+        }
+
+    }
+
+
+    public void initRobot() {
+
+        // Maps the motor objects to the physical ports
+        FLMotor = hardwareMap.get(DcMotor.class, "FLMotor");
+        BLMotor = hardwareMap.get(DcMotor.class, "BLMotor");
+        BRMotor = hardwareMap.get(DcMotor.class, "BRMotor");
+        FRMotor = hardwareMap.get(DcMotor.class, "FRMotor");
+
+        // Sets the encoder mode
+        FLMotor.setMode(RUN_USING_ENCODER);
+        BLMotor.setMode(RUN_USING_ENCODER);
+        BRMotor.setMode(RUN_USING_ENCODER);
+        FRMotor.setMode(RUN_USING_ENCODER);
+
+        // Sets what happens when no power is applied to the motors.
+        // In this mode, the computer will short the 2 leads of the motor, and because of math, the motor will be a lot harder to turn
+        FLMotor.setZeroPowerBehavior(BRAKE);
+        BLMotor.setZeroPowerBehavior(BRAKE);
+        BRMotor.setZeroPowerBehavior(BRAKE);
+        FRMotor.setZeroPowerBehavior(BRAKE);
+
+        FLMotor.setDirection(FORWARD);
+        BLMotor.setDirection(FORWARD);
+        BRMotor.setDirection(FORWARD);
+        FRMotor.setDirection(FORWARD);
+
+
+        // Maps the servo objects to the physical ports
+        FLServo = hardwareMap.get(Servo.class, "FLServo");
+        BLServo = hardwareMap.get(Servo.class, "BLServo");
+        BRServo = hardwareMap.get(Servo.class, "BRServo");
+        FRServo = hardwareMap.get(Servo.class, "FRServo");
+
+        // Sets the ends of the servos. Hover cursor over function for more info
+        // Will need to be tuned later
+        FLServo.scaleRange(0.0, 1.0);
+        BLServo.scaleRange(0.0, 1.0);
+        BRServo.scaleRange(0.0, 1.0);
+        FRServo.scaleRange(0.0, 1.0);
+
+        double[] test = cartesianToPolar(1, 0);
+
+    }
+
+
+    // Converts cartesian coordinates to polar
+    // 0 = r
+    // 1 = theta
+    public double[] cartesianToPolar(double x, double y)
+    {
+        double[] arrayToReturn = new double[2];
+        arrayToReturn[0] = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)); // Radius
+        arrayToReturn[1] = Math.atan(y / x) * (Math.PI / 180); // Theta
+
+
+        return arrayToReturn;
+    }
+
+
+    // Converts polar coordinates to cartesian
+    // 0 = x
+    // 1 = y
+    public double[] polarToCartesian(double r, double theta){
+        double[] arrayToReturn = new double[2];
+        arrayToReturn[0] = r * Math.cos(theta); // X
+        arrayToReturn[1] = r * Math.sin(theta); // Y
+
+        return arrayToReturn;
+    }
+
+}
