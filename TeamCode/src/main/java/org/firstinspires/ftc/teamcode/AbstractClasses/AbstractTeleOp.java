e09a9c7b569c07230a5c5e86923f3ce2542721f4
JacksonDorschner <jacksondorschner@gmail.com>
2024-09-03 17:23:14 -0700

    glob

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractClasses/AbstractTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AbstractClasses/AbstractTeleOp.java
@@ -2,7 +2,32 @@ package org.firstinspires.ftc.teamcode.AbstractClasses;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import com.roboctopi.cuttlefish.utils.Direction;
+import com.roboctopi.cuttlefishftcbridge.devices.*;
+import com.roboctopi.cuttlefish.controller.*;
+
 public abstract class AbstractTeleOp extends LinearOpMode {
+    public CuttleRevHub ctrlHub;
+    public CuttleRevHub expHub;
+
+    public CuttleMotor leftFrontMotor ;
+    public CuttleMotor rightFrontMotor;
+    public CuttleMotor rightBackMotor ;
+    public CuttleMotor leftBackMotor  ;
+    MecanumController chassis;
+
+    public void onInit(){
+        leftFrontMotor  = ctrlHub.getMotor(3);
+        rightFrontMotor = ctrlHub.getMotor(2);
+        rightBackMotor  = expHub .getMotor(2);
+        leftBackMotor   = expHub .getMotor(3);
+
+        leftBackMotor .setDirection(Direction.REVERSE);
+        leftFrontMotor.setDirection(Direction.REVERSE);
+
+        chassis = new MecanumController(rightFrontMotor,rightBackMotor,leftFrontMotor,leftBackMotor);
+    }
+
     @Override
     public void runOpMode() throws InterruptedException {
 
