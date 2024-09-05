5939c164d721bbb176b1639331bb6cf6783bec2a
SnazzyBeatle115 <86858242+purelyboba@users.noreply.github.com>
2024-09-05 16:24:44 -0400

    add teleop  name annotator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auton.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Auton.kt
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
 import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.Config
 import com.arcrobotics.ftclib.command.CommandOpMode
 import com.arcrobotics.ftclib.gamepad.GamepadEx
 import com.arcrobotics.ftclib.geometry.Pose2d
 import com.millburnx.utils.Vec2d
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.teamcode.common.commands.PurePursuitCommand
 import org.firstinspires.ftc.teamcode.common.subsystems.DriveSubsystem
 import org.firstinspires.ftc.teamcode.common.utils.Telemetry
 
+@Config
+@TeleOp(name = "Auton")
 class Auton : CommandOpMode() {
     private var drive: DriveSubsystem? = null
     var tel: Telemetry? = null
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/VisionPortalOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/VisionPortalOp.kt
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode.opmodes
 
 import com.acmerobotics.dashboard.FtcDashboard
+import com.acmerobotics.dashboard.config.Config
 import com.arcrobotics.ftclib.command.CommandOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import org.firstinspires.ftc.robotcore.external.Telemetry
 import org.firstinspires.ftc.teamcode.common.subsystems.Apriltag
 import org.firstinspires.ftc.teamcode.common.subsystems.VisionPortal
 import org.firstinspires.ftc.vision.VisionProcessor
 
+@Config
+@TeleOp(name = "Vision")
 class VisionPortalOp: CommandOpMode() {
     var visionPortal: VisionPortal? = null
     var aprilTag: Apriltag? = null
