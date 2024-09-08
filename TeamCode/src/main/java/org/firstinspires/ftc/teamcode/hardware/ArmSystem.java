c0a7843207cac2457c8c02725a25e4acec79f43f
Eric Zhang <ericbkzhang@gmail.com>
2024-09-08 00:20:34 -0400

    Create an ArmSystem class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/ArmSystem.java
@@ -0,0 +1,11 @@
+package firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.hardware.*;
+
+public abstract class ArmSystem {
+    protected DcMotor[] armMotors;
+
+    public ArmSystem(DcMotor[] armMotors) {
+        this.armMotors = armMotors;
+    }   
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/DriveSystem.java
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 public abstract class DriveSystem {
-    // How much power the wheels run with (0.0 - 1.0)
+    // A modifier for much power the wheels run with (0.0 - 1.0)
     protected double wheelPower = 1.0;
 
     public DriveSystem() {
