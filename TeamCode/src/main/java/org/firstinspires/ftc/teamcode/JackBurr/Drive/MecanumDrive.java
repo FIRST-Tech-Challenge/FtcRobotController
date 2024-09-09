d7bdafdee9dbc961e7cd308d5bf281aafdd476e4
EPICGAMING191 <112524668+EPICGAMING191@users.noreply.github.com>
2024-09-09 19:27:14 -0400

    Create MecanumDrive.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Drive/MecanumDrive.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.JackBurr.Drive;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDrive extends OpMode {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+
+    @Override
+    public void init() {
+        gamepad1.rumbleBlips(3);
+        telemetry.addData("Gamepad ID:", gamepad1.getGamepadId());
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "fl");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "fr");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "bl");
+        backRightMotor = hardwareMap.get(DcMotor.class, "br");
+
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void loop() {
+        double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+        double x = -gamepad1.left_stick_x; // Counteract imperfect strafing, if the back motors are facing downwards this should be negative
+        double rx = -gamepad1.right_stick_x; //This is reversed for our turning
+        drive(y, x, rx);
+    }
+
+
+    public void drive(double y, double x, double rx) {
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (-y + x + rx) / denominator;
+        double backLeftPower = (-y - x + rx) / denominator;
+        double frontRightPower = (y + x + rx) / denominator;
+        double backRightPower = (y - x + rx) / denominator;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        backLeftMotor.setPower(backLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backRightMotor.setPower(backRightPower);
+    }
+}
