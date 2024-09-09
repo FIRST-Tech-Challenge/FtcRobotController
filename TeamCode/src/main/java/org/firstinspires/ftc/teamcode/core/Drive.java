dbfa1f5213056349c793dfdd2b72cbae35a2bbdb
Luke <delbridgeluke6@gmail.com>
2024-09-09 19:20:36 -0400

    I just added to core package and the drive class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/core/Drive.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.core;
+
+//The internet is TRASH
+
+/*
+This class teaches how to program in java for future robotics development for the future
+
+Example for jobs:
+Robotics Coach or Denso
+ */
+public class Drive {
+
+}
