5625bbe5f9b741bd01f551e19357d7b8e2441f58
Fenwick <BurrJ22@outlook.com>
2024-09-10 16:10:08 -0400

    Removed TFOD and updated AprilTags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Camera/AprilTags/AprilTagIDDetector.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.JackBurr.Camera.AprilTags;
+
+import static org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase.getCurrentGameTagLibrary;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import java.util.List;
+
+@Autonomous
+public class AprilTagIDDetector extends OpMode {
+    private AprilTagProcessor aprilTagProcessor;
+    public int aprilTagsCount = 0;
+    private VisionPortal visionPortal;
+    public WebcamName webcamName;
+    public StringBuilder idsFound;
+    public ElapsedTime main_timer;
+    List<AprilTagDetection> currentDetections;
+    public double poseX;
+    public double poseY;
+    public double poseZ;
+
+    @Override
+    public void init() {
+        webcamName = getWebcamName("Webcam 1");
+        aprilTagProcessor = createAprilTagProcessor();
+        visionPortal = createVisionPortal(webcamName, aprilTagProcessor);
+        main_timer = getTimer();
+        idsFound = createStringBuilder();
+        telemetry.addLine(String.valueOf(getCurrentGameTagLibrary()));
+    }
+
+    @Override
+    public void init_loop(){
+        currentDetections = aprilTagProcessor.getDetections();
+        for (AprilTagDetection detection : currentDetections){
+            telemetry.addLine(String.valueOf(detection.id));
+            poseX = detection.rawPose.x;
+            poseY = detection.rawPose.y;
+            poseZ = detection.rawPose.z;
+        }
+        telemetry.addData("AprilTag Count: ", aprilTagsCount);
+        telemetry.addData("Time: " , main_timer.time());
+        telemetry.addData("Detection X: ",poseX);
+        telemetry.addData("Detection Y: ",poseY);
+        telemetry.addData("Detection Z: ",poseZ);
+        telemetry.update();
+    }
+    @Override
+    public void loop() {
+        visionPortal.stopStreaming();
+    }
+
+    public WebcamName getWebcamName(String webcam_name){
+        return hardwareMap.get(WebcamName.class, webcam_name);
+    }
+    public AprilTagProcessor createAprilTagProcessor(){
+        return AprilTagProcessor.easyCreateWithDefaults();
+    }
+    public VisionPortal createVisionPortal(WebcamName webcamName, AprilTagProcessor processor){
+        return VisionPortal.easyCreateWithDefaults(webcamName, processor);
+    }
+    public StringBuilder createStringBuilder(){
+        return new StringBuilder();
+    }
+    public ElapsedTime getTimer(){
+        return new ElapsedTime();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Camera/AprilTags/CenterStageAprilTags.java
@@ -0,0 +1,136 @@
+package org.firstinspires.ftc.teamcode.JackBurr.Camera.AprilTags;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagPoseFtc;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+@Autonomous
+public class CenterStageAprilTags extends OpMode {
+    private AprilTagProcessor aprilTagProcessor;
+    public int aprilTagsCount = 0;
+    private VisionPortal visionPortal;
+    public WebcamName webcamName;
+    public StringBuilder idsFound;
+    public ElapsedTime main_timer;
+    List <String> idsList = new ArrayList<>();
+    List<AprilTagDetection> currentDetections;
+    public String currentPosition = "UNDEFINED";
+    public String aprilTagID = "NOT_YET_DETECTED";
+    public double xOffset;
+    Mat frame = new Mat();
+    double poseX = -99756478857656756457.0;
+    double poseY = -99756478857656756457.0;
+    double poseZ = -99756478857656756457.0;
+    double poseAX = -99756478857656756457.0;
+    double poseAY = -99756478857656756457.0;
+    double poseAZ = -99756478857656756457.0;
+
+    @Override
+    public void init() {
+        System.out.println("Hello World");
+        webcamName = getWebcamName("Webcam 1");
+        aprilTagProcessor = createAprilTagProcessor();
+        visionPortal = createVisionPortal(webcamName, aprilTagProcessor);
+        main_timer = getTimer();
+        idsFound = createStringBuilder();
+    }
+
+    @Override
+    public void init_loop(){
+        currentDetections = aprilTagProcessor.getDetections();
+        for (AprilTagDetection detection : currentDetections){
+            telemetry.addLine(String.valueOf(detection.id));
+            poseX = detection.rawPose.x;
+            poseY = detection.rawPose.y;
+            poseZ = detection.rawPose.z;
+            telemetry.addLine("FTCPose X: " + String.valueOf(detection.ftcPose.x));
+            if (idsList.contains(String.valueOf(detection.id)) == false) {
+                aprilTagID = String.valueOf(detection.id);
+                aprilTagsCount = aprilTagsCount + 1;
+                idsList.add(String.valueOf(detection.id));
+                telemetry.addLine(String.valueOf(detection.center));
+            }
+        }
+        currentPosition = getPosition(aprilTagID);
+        telemetry.addData("AprilTags found: ", idsList);
+        telemetry.addData("AprilTag Count: ", aprilTagsCount);
+        telemetry.addData("Current Position: ", currentPosition);
+        telemetry.addData("Time: " , main_timer.time());
+        if (poseX != -99756478857656756457.0){
+            telemetry.addData("Detection X: ",poseX);
+        }
+        if (poseY != -99756478857656756457.0){
+            telemetry.addData("Detection Y: ",poseY);
+        }
+        if (poseZ != -99756478857656756457.0){
+            telemetry.addData("Detection Z: ",poseZ);
+        }
+        telemetry.addLine(String.valueOf(xOffset));
+        telemetry.update();
+    }
+    @Override
+    public void loop() {
+        visionPortal.stopStreaming();
+    }
+
+    public WebcamName getWebcamName(String webcam_name){
+        return hardwareMap.get(WebcamName.class, webcam_name);
+    }
+    public AprilTagProcessor createAprilTagProcessor(){
+        return AprilTagProcessor.easyCreateWithDefaults();
+    }
+    public VisionPortal createVisionPortal(WebcamName webcamName, AprilTagProcessor processor){
+        return VisionPortal.easyCreateWithDefaults(webcamName, processor);
+    }
+    public StringBuilder createStringBuilder(){
+        return new StringBuilder();
+    }
+    public ElapsedTime getTimer(){
+        return new ElapsedTime();
+    }
+
+    public String getPosition(String aprilTagID){
+        String position = "";
+        int id = -1000;
+        if (aprilTagID != "NOT_YET_DETECTED"){
+            id = Integer.valueOf(aprilTagID);
+        }
+        switch (id){
+            case 1:
+                position = "BLUE_BACKDROP_LEFT";
+                break;
+            case 2:
+                position = "BLUE_BACKDROP_CENTER";
+                break;
+            case 3:
+                position = "BLUE_BACKDROP_RIGHT";
+                break;
+            case 4:
+                position = "RED_BACKDROP_LEFT";
+                break;
+            case 5:
+                position = "RED_BACKDROP_CENTER";
+                break;
+            case 6:
+                position = "RED_BACKDROP_RIGHT";
+                break;
+            default:
+                position = "UNDEFINED";
+                break;
+        }
+        return position;
+    }
+}
