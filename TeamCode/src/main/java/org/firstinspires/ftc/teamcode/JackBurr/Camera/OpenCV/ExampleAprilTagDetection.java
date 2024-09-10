49b58907ba210bd03bfcbaf0f69a5742b0a4a5ae
Fenwick <BurrJ22@outlook.com>
2024-09-10 16:13:44 -0400

    Removed TFOD and updated AprilTags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Camera/OpenCV/ExampleAprilTagDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Camera/OpenCV/ExampleAprilTagDetection.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.2024_2025_DIVE.Camera.OpenCV;
+package org.firstinspires.ftc.teamcode.JackBurr.Camera.OpenCV;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
