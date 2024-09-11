1473ffc77a4509cf3e6e039f90ab607b9773c5d4
AmiT1458 <97545597+AmiT1458@users.noreply.github.com>
2024-09-11 18:08:13 +0300

    added opencv folder and basic samples detector by color,NOT ready yet, needs to adjust colors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/DetectSamples.java
@@ -0,0 +1,105 @@
+package org.firstinspires.ftc.teamcode.opencv;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetectSamples extends OpenCvPipeline {
+    private final OpenCvCamera webcam;
+    boolean viewportPaused;
+
+    public DetectSamples(OpenCvCamera webcam){
+        this.webcam = webcam;
+    }
+    @Override
+    public Mat processFrame(Mat input) {
+        Mat yellowMask = preprocessFrame(input);
+
+        // Find contours of the detected yellow regions
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // Find the largest yellow contour (blob)
+        MatOfPoint largestContour = findLargestContour(contours);
+
+        if (largestContour != null) {
+            //Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
+
+            for (MatOfPoint contour : contours) {
+                Imgproc.drawContours(input, contours, contours.indexOf(contour), new Scalar(255, 0, 0), 2);
+            }
+        }
+
+        return input;
+    }
+
+
+    private Mat preprocessFrame(Mat frame) {
+        Mat hsvFrame = new Mat();
+        Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_RGB2YCrCb);
+
+        Scalar lowerYellow = new Scalar(0, 138, 0);
+        Scalar upperYellow = new Scalar(255, 255, 104);
+
+
+        Mat yellowMask = new Mat();
+        Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
+
+        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
+        Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
+        Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
+
+        return yellowMask;
+    }
+
+    private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
+        double maxArea = 0;
+        MatOfPoint largestContour = null;
+
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > maxArea) {
+                maxArea = area;
+                largestContour = contour;
+            }
+        }
+
+        return largestContour;
+    }
+    @Override
+    public void onViewportTapped()
+    {
+        /*
+         * The viewport (if one was specified in the constructor) can also be dynamically "paused"
+         * and "resumed". The primary use case of this is to reduce CPU, memory, and power load
+         * when you need your vision pipeline running, but do not require a live preview on the
+         * robot controller screen. For instance, this could be useful if you wish to see the live
+         * camera preview as you are initializing your robot, but you no longer require the live
+         * preview after you have finished your initialization process; pausing the viewport does
+         * not stop running your pipeline.
+         *
+         * Here we demonstrate dynamically pausing/resuming the viewport when the user taps it
+         */
+
+        viewportPaused = !viewportPaused;
+
+        if(viewportPaused)
+        {
+            webcam.pauseViewport();
+        }
+        else
+        {
+            webcam.resumeViewport();
+        }
+    }
+
+}
