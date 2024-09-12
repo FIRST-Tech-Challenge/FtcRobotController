b2d7e1eeafc21d112a914eeeae40178b0c697ccb
AyushThapa026 <ayushthapa026@gmail.com>
2024-09-12 16:55:38 -0700

    vector2 + mecanum auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -15,12 +16,15 @@ public class Mecanum {
 
     // Variables
     private double driveSpeed = 1.0;
+    private ElapsedTime timer;
 
     // Debug
     private boolean debug = true;
     private Telemetry telemetry;
 
     public Mecanum(HardwareMap hardwareMap) {
+        timer = new ElapsedTime();
+
         //Drive base
         leftFrontDrive  = hardwareMap.dcMotor.get("leftFrontDrive");
         rightFrontDrive = hardwareMap.dcMotor.get("rightFrontDrive");
@@ -47,9 +51,22 @@ public class Mecanum {
     }
 
     public void update(Gamepad gamepad) {
-        double r = Math.hypot(gamepad.left_stick_x, gamepad.left_stick_y);
-        double robotAngle = Math.atan2(-gamepad.left_stick_y, gamepad.left_stick_x) - Math.PI / 4;
-        double rightX = gamepad.right_stick_x;
+        this.update(new Vector2(gamepad.left_stick_x, gamepad.left_stick_y));
+    }
+
+    public void updateForTime(Vector2 direction, double seconds) {
+        this.timer.reset();
+        while (this.timer.milliseconds() - this.timer.time() < seconds) {
+            update(direction);
+        }
+    }
+
+    public void update(Vector2 direction) {
+        direction = direction.normalize();
+
+        double r = Math.hypot(direction.x, direction.y);
+        double robotAngle = Math.atan2(-direction.y, direction.x) - Math.PI / 4;
+        double rightX = direction.x;
 
         double leftFrontWheelPower = r * Math.cos(robotAngle) * Math.sqrt(2) + rightX;
         double rightFrontWheelPower = r * Math.sin(robotAngle) * Math.sqrt(2) - rightX;
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vector2.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Vector2 {
+    public double x;
+    public double y;
+
+    public Vector2(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public boolean isUnitVector() {
+        return Math.abs(1 - getMagnitude()) < 1e-5;
+    }
+
+    public double getMagnitude() {
+        return Math.sqrt((this.x * this.x) + (this.y * this.y));
+    }
+    public Vector2 normalize() {
+        double magnitude = getMagnitude();
+        return new Vector2(this.x / magnitude, this.y / magnitude);
+    }
+}
