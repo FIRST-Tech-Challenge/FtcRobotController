f01a5f69a3ba9ef8c2f4e6d8b42e5c3a6c4e70ce
iddob <iddob@DESKTOP-UP92I9T>
2024-09-14 20:02:06 +0300

    unfinished, supposed to find the length in pixels of verticle lines of the sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/DetectSamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/DetectSamples.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.opencv;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -13,11 +14,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DetectSamples extends OpenCvPipeline {
-    private final OpenCvCamera webcam;
+    public OpenCvCamera webcam;
     boolean viewportPaused;
 
-    public DetectSamples(OpenCvCamera webcam){
-        this.webcam = webcam;
+    public DetectSamples(/*OpenCvCamera webcam*/){
+        //this.webcam = webcam;
     }
     @Override
     public Mat processFrame(Mat input) {
@@ -27,53 +28,44 @@ public class DetectSamples extends OpenCvPipeline {
         List<MatOfPoint> contours = new ArrayList<>();
         Mat hierarchy = new Mat();
         Imgproc.findContours(yellowMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        List<Integer> verticalLineLenghs = new ArrayList<Integer>();
+        for (MatOfPoint contour : contours) {
+            Mat canvas = Mat.zeros(input.size(), input.type());
+            Imgproc.drawContours(canvas, contours, -1, new Scalar(255, 255, 255), -1);
+            Mat edges = new Mat();
+            Imgproc.Canny(canvas, edges, 100, 190, 3, true);
+            Mat lines = new Mat();
+            Imgproc.HoughLines(edges, lines, 1, Math.PI / 180, 50, 30, 10);
+            for (int i = 0; i < lines.rows(); i++) {
+                double[] line = lines.get(i, 0);
+                double x1 = line[0], y1 = line[1], x2 = line[2], y2 = line[3];
 
-        // Find the largest yellow contour (blob)
-        MatOfPoint largestContour = findLargestContour(contours);
-
-        if (largestContour != null) {
-            //Imgproc.drawContours(input, contours, contours.indexOf(largestContour), new Scalar(255, 0, 0), 2);
-
-            for (MatOfPoint contour : contours) {
-                Imgproc.drawContours(input, contours, contours.indexOf(contour), new Scalar(255, 0, 0), 2);
+                // Check if the line is vertical (nearly the same x coordinates)
+                if (Math.abs(x1 - x2) < 20) {  // Adjust threshold for vertical lines
+                    double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                    System.out.println("Line from (" + x1 + ", " + y1 + ") to (" + x2 + ", " + y2 + "), Length: " + length + " pixels");
+                }
             }
         }
-
+        Imgproc.drawContours(input, contours, -1, new Scalar(255, 255, 255), -1);
         return input;
     }
-
 
     private Mat preprocessFrame(Mat frame) {
         Mat hsvFrame = new Mat();
         Imgproc.cvtColor(frame, hsvFrame, Imgproc.COLOR_RGB2YCrCb);
 
         Scalar lowerYellow = new Scalar(0, 138, 0);
-        Scalar upperYellow = new Scalar(255, 255, 104);
+        Scalar upperYellow = new Scalar(255, 200, 100);
 
 
         Mat yellowMask = new Mat();
         Core.inRange(hsvFrame, lowerYellow, upperYellow, yellowMask);
-
         Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_OPEN, kernel);
         Imgproc.morphologyEx(yellowMask, yellowMask, Imgproc.MORPH_CLOSE, kernel);
 
         return yellowMask;
-    }
-
-    private MatOfPoint findLargestContour(List<MatOfPoint> contours) {
-        double maxArea = 0;
-        MatOfPoint largestContour = null;
-
-        for (MatOfPoint contour : contours) {
-            double area = Imgproc.contourArea(contour);
-            if (area > maxArea) {
-                maxArea = area;
-                largestContour = contour;
-            }
-        }
-
-        return largestContour;
     }
     @Override
     public void onViewportTapped()
