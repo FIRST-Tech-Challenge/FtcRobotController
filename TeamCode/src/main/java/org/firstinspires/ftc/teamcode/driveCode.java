c57f4c607e1dc17b9cbe477946027cb46a4a2a38
Logannnnnnn <mcneillogan092@gmail.com>
2024-09-14 09:49:55 -0400

    pirates logan

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveCode.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class driveCode extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight"); // input 0
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight"); // input 1
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft"); // input 2
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");  // input 3
+
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        motorFrontLeft.setZeroPowerBehavior(BRAKE);
+        motorFrontRight.setZeroPowerBehavior(BRAKE);
+        motorBackLeft.setZeroPowerBehavior(BRAKE);
+        motorBackRight.setZeroPowerBehavior(BRAKE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+        }
+    }
+}
