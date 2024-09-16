fc5305e6f511ba86c8c7b137b606ea1ffa48cd3f
Eric Zhang <ericbkzhang@gmail.com>
2024-09-15 23:44:47 -0400

    Implement file writing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import java.io.File;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import android.os.Environment;
 
 public class FileManager {
@@ -8,16 +14,38 @@ public class FileManager {
     // The current String is a placeholder,
     // and is intended to be replaced with the name of the current year's repository.
     */
-    private static final String rootDirectory = "chsRobotix";
+    private static final Path seasonDirectory = Paths.get(Environment.getExternalStorage().getPath() + "/chsRobotix");
 
     /**
-     * Writes a given String to a file 
+     * Writes a given String to a given file within seasonDirectory
      * 
      * @param fileName
      * @param inputString
-     * @return
+     * @return Whether the String was successfully written to the file. 
      */
     public static boolean writeToFile(String fileName, String inputString) {
-        String filePath = Environment.getExternalStorage().getPath();
+        // If this season's directory does not exist, create it.
+        if (!Files.exists(seasonDirectory)) {
+            Files.createDirectory(seasonDirectory);
+        }
+
+        Path writeFile = Paths.get(seasonDirectory, fileName);
+        try {
+            // Create the file if it does not exist
+            Files.createFile(writeFile);
+
+        } catch (FileAlreadyExistsException err) { 
+            // The file already existing is not a problem.
+
+        } catch (IOException err) {
+            return false;
+        }
+
+        Files.write(writeFile, inputString);
+        return true;
+    }
+
+    public static String readFromFile(String fileName) {
+        return true;
     }
 }
