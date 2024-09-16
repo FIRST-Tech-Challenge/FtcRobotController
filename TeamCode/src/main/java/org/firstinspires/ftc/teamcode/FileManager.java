75f8df6d1f1c904c563ea36d290318393d953d6e
Eric Zhang <ericbkzhang@gmail.com>
2024-09-16 16:02:45 -0400

    Change comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
@@ -1,32 +1,33 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.os.Environment;
 import java.io.File;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import android.os.Environment;
-
 public class FileManager {
-    /* 
+    /*
      * The directory that save the current season's storage files.
-    // The current String is a placeholder,
-    // and is intended to be replaced with the name of the current year's repository.
-    */
-    private static final Path seasonDirectory = Paths.get(Environment.getExternalStorage().getPath() + "/chsRobotix");
+     * The current String is a placeholder,
+     * and is intended to be replaced with the name of the current year's repository.
+     */
+    private static final Path seasonDirectory = Paths.get(
+        Environment.getExternalStorage().getPath() + "/chsRobotix"
+    );
 
     /**
-     * Writes a given String to a given file within seasonDirectory
-     * 
+     * Writes a String to a text file inside this season's directory.
+     *
      * @param fileName
      * @param inputString
-     * @return Whether the String was successfully written to the file. 
+     * @return Whether the String was successfully written to the file.
      */
     public static boolean writeToFile(String fileName, String inputString) {
         // If this season's directory does not exist, create it.
         if (!Files.exists(seasonDirectory)) {
-            Files.createDirectory(seasonDirectory);
+        Files.createDirectory(seasonDirectory);
         }
 
         Path writeFile = Paths.get(seasonDirectory, fileName);
@@ -34,8 +35,8 @@ public class FileManager {
             // Create the file if it does not exist
             Files.createFile(writeFile);
 
-        } catch (FileAlreadyExistsException err) { 
-            // The file already existing is not a problem.
+        } catch (FileAlreadyExistsException err) {
+        // The file already existing is not a problem.
 
         } catch (IOException err) {
             return false;
@@ -45,7 +46,13 @@ public class FileManager {
         return true;
     }
 
+    /**
+     * Read a text file inside this season's directory.
+     * @param fileName
+     * @return The String contained within the text file.
+     */
     public static String readFromFile(String fileName) {
+        
         return true;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionInput.java
@@ -9,30 +9,16 @@ public class PositionInput extends OpMode {
 
     @Override
     public void init() {
-        String storagePath = android.os.Environment.getExternalStorage().getPath() + "/" + fileDir;
     }
 
     @Override
     public void loop() {
         telemetry.update();
 
-        // If "A" is pressed on either gamepad
         if (gamepad1.a || gamepad2.a) {
-        }
-
-        // If "B" is pressed on either gamepad
-        else if (gamepad1.b || gamepad2.b) {
-
-        }
-
-        // If "X" is pressed on either gamepad
-        else if (gamepad1.x || gamepad2.x) {
-
-        }
-
-        // If "Y" is pressed on either gamepad
-        else if (gamepad1.y || gamepad2.y) {
-
+        } else if (gamepad1.b || gamepad2.b) {
+        } else if (gamepad1.x || gamepad2.x) {
+        } else if (gamepad1.y || gamepad2.y) {
         }
     }
 }
