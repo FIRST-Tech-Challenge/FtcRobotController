8768210632f9141050819b1b2787d7237eba1f88
Eric Zhang <ericbkzhang@gmail.com>
2024-09-17 16:08:58 -0400

    Rename Autonomous to Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -0,0 +1,20 @@
+
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "Auto")
+public class Auto extends LinearOpMode {
+    private CoyotesRobot robot;
+
+    /**
+     * Automatically runs after pressing the "Init" button on the Control Hub
+     */
+    @Override
+    public void runOpMode() {
+        robot = new CoyotesRobot(this);
+
+        // Wait until the player press the start button
+        waitForStart();
+    }
+}
