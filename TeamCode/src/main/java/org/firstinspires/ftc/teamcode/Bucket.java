73b8d0a40e0ee0c78b7fcfff6d91406f53013fa2
AyushThapa026 <ayushthapa026@gmail.com>
2024-09-17 07:55:38 -0700

    bucket + mecanum updates

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bucket.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Bucket {
+    private String SERVO_NAME = "bucketServo";
+
+    private Servo servo;
+
+    public Bucket(HardwareMap hardwareMap) {
+        servo = hardwareMap.servo.get(SERVO_NAME);
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mecanum.java
@@ -8,13 +8,21 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class Mecanum {
+
+    // Motor names
+    private String LEFT_FRONT_DRIVE_NAME = "leftFrontDrive";
+    private String RIGHT_FRONT_DRIVE_NAME = "rightFrontDrive";
+    private String LEFT_REAR_DRIVE_NAME = "leftRearDrive";
+    private String RIGHT_REAR_DRIVE_NAME = "rightRearDrive";
+
     // Motors
-    private DcMotor leftFrontDrive   = null;
-    private DcMotor  rightFrontDrive  = null;
-    private DcMotor  leftRearDrive    = null;
-    private DcMotor  rightRearDrive   = null;
 
     // Variables
+    private DcMotor leftFrontDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor leftRearDrive = null;
+    private DcMotor rightRearDrive = null;
+
     private double driveSpeed = 1.0;
     private ElapsedTime timer;
 
@@ -22,16 +30,25 @@ public class Mecanum {
     private boolean debug = true;
     private Telemetry telemetry;
 
+    public Mecanum(HardwareMap hardwareMap, double driveSpeed) {
+        this.driveSpeed = driveSpeed;
+        initializeMotors(hardwareMap);
+    }
+
     public Mecanum(HardwareMap hardwareMap) {
+
+        // Drive base
+        initializeMotors(hardwareMap);
+    }
+
+    public void initializeMotors(HardwareMap hardwareMap) {
         timer = new ElapsedTime();
 
-        //Drive base
-        leftFrontDrive  = hardwareMap.dcMotor.get("leftFrontDrive");
-        rightFrontDrive = hardwareMap.dcMotor.get("rightFrontDrive");
-        leftRearDrive = hardwareMap.dcMotor.get("leftRearDrive");
-        rightRearDrive = hardwareMap.dcMotor.get("rightRearDrive");
+        leftFrontDrive = hardwareMap.dcMotor.get(LEFT_FRONT_DRIVE_NAME);
+        rightFrontDrive = hardwareMap.dcMotor.get(RIGHT_FRONT_DRIVE_NAME);
+        leftRearDrive = hardwareMap.dcMotor.get(LEFT_REAR_DRIVE_NAME);
+        rightRearDrive = hardwareMap.dcMotor.get(RIGHT_REAR_DRIVE_NAME);
 
-        //this needs to be corrected with testing, this is just and example
         leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -50,15 +67,15 @@ public class Mecanum {
         this.debug = toggle;
     }
 
-    public void update(Gamepad gamepad) {
-        this.update(new Vector2(gamepad.left_stick_x, gamepad.left_stick_y));
-    }
-
     public void updateForTime(Vector2 direction, double seconds) {
         this.timer.reset();
         while (this.timer.milliseconds() - this.timer.time() < seconds) {
             update(direction);
         }
+    }
+
+    public void update(Gamepad gamepad) {
+        this.update(new Vector2(gamepad.left_stick_x, gamepad.left_stick_y));
     }
 
     public void update(Vector2 direction) {
@@ -79,9 +96,9 @@ public class Mecanum {
         rightRearDrive.setPower(rightRearWheelPower * driveSpeed);
 
         if (this.debug) {
-            telemetry.addData("leftFront",  "%.2f", leftFrontWheelPower);
-            telemetry.addData("rightFront",  "%.2f", rightFrontWheelPower);
-            telemetry.addData("leftRear",  "%.2f", leftRearWheelPower);
+            telemetry.addData("leftFront", "%.2f", leftFrontWheelPower);
+            telemetry.addData("rightFront", "%.2f", rightFrontWheelPower);
+            telemetry.addData("leftRear", "%.2f", leftRearWheelPower);
             telemetry.addData("rightRear", "%.2f", rightRearWheelPower);
         }
     }
