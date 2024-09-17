fec23247c61f18ffb2cf7e22b544e181ab1130b8
Bass-Dev2 <goopyvirus@gmail.com>
2024-09-17 16:17:31 -0700

    added set position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bucket.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bucket.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
@@ -14,5 +15,9 @@ public class Bucket {
 
     public Bucket(HardwareMap hardwareMap) {
         servo = hardwareMap.servo.get(SERVO_NAME);
+    }
+
+    public void setPosition(double pos) {
+        servo.setPosition(pos);
     }
 }
