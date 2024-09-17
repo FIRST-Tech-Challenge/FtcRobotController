7182c610fdf136330b050e06e0e31ddc789dbdbc
Eric Zhang <ericbkzhang@gmail.com>
2024-09-17 15:57:39 -0400

    Abandon file writing but keeping it for later

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FileManager.java
@@ -1,21 +1,18 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.os.Environment;
-import java.io.File;
-import java.nio.file.FileAlreadyExistsException;
-import java.nio.file.Files;
+import java.nio.file;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class FileManager {
     /*
      * The directory that save the current season's storage files.
      * The current String is a placeholder,
-     * and is intended to be replaced with the name of the current year's repository.
+     * and is intended to be replaced with the name of the current year's
+     * repository.
      */
     private static final Path seasonDirectory = Paths.get(
-        Environment.getExternalStorage().getPath() + "/chsRobotix"
-    );
+            Environment.getExternalStorage().getPath() + "/chsRobotix");
 
     /**
      * Writes a String to a text file inside this season's directory.
@@ -24,19 +21,19 @@ public class FileManager {
      * @param inputString
      * @return Whether the String was successfully written to the file.
      */
-    public static boolean writeToFile(String fileName, String inputString) {
+    public static boolean writeToFile(Path fileName, String inputString) {
         // If this season's directory does not exist, create it.
         if (!Files.exists(seasonDirectory)) {
-        Files.createDirectory(seasonDirectory);
+            Files.createDirectory(seasonDirectory);
         }
 
-        Path writeFile = Paths.get(seasonDirectory, fileName);
+        Path writeFile = seasonDirectory.resolve(fileName);
         try {
             // Create the file if it does not exist
             Files.createFile(writeFile);
 
         } catch (FileAlreadyExistsException err) {
-        // The file already existing is not a problem.
+            // The file already existing is not a problem.
 
         } catch (IOException err) {
             return false;
@@ -48,11 +45,18 @@ public class FileManager {
 
     /**
      * Read a text file inside this season's directory.
+     * 
      * @param fileName
      * @return The String contained within the text file.
+     *         If an error occurs, null is returned.
      */
-    public static String readFromFile(String fileName) {
-        
-        return true;
+    public static String readFromFile(Path fileName) {
+        Path readFile = seasonDirectory.resolve(fileName);
+
+        if (Files.exists(readFile)) {
+            return Files.readString(readFile);
+        }
+
+        return null;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionInput.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PositionInput.java
@@ -1,24 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 import android.os.Environment;
+import java.nio.file;
+
+import com.apple.eio.FileManager;
 
 @TeleOp(name = "PositionInput")
 public class PositionInput extends OpMode {
-    // The directory that the program stores files in.
-    private final File storageFile;
+    // File name of the storage file,
+    // which is inside the directory specified in FileManager
+    private final Path storageFile = Paths("position.txt");
 
     @Override
     public void init() {
+        telemetry.addData("Current position: ", FileManager.readFile(storageFile));
     }
 
     @Override
     public void loop() {
         telemetry.update();
 
+        String positionString;
+        // Green button
         if (gamepad1.a || gamepad2.a) {
+        
         } else if (gamepad1.b || gamepad2.b) {
+            // Red button
+
         } else if (gamepad1.x || gamepad2.x) {
+            // Blue button
+
         } else if (gamepad1.y || gamepad2.y) {
+            // Orange button
         }
+
+        
     }
 }
