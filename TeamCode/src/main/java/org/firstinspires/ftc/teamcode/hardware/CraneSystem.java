330065062f61ebdb6059638dec4e3d07ada9ec24
Eric Zhang <ericbkzhang@gmail.com>
2024-09-18 19:46:06 -0400

    Reworking hardware classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CraneSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/CraneSystem.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
 public class CraneSystem extends ArmSystem {
-    public CraneSystem() {
+    public CraneSystem(String[] motorNames) {
+        motors = new DcMotor[motorNames.length];
 
+        for (int i=0; i<motorNames.length; i++) {
+            motors[i] = new DcMotor(DcMotor.class, motorNames[i]);
+        }
     }
 }
