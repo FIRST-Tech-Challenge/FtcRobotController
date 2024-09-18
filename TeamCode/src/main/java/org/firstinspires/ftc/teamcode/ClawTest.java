793aa62531b63490206114e89d422ffe86decb64
EagleNotBot <phibersto26@gmail.com>
2024-09-17 19:59:23 -0500

    Class to test claw
    Class to test claw and side on robot(weren't able to actually test)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ClawTest.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.*;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class ClawTest extends LinearOpMode {
+
+    /*
+    to test claw be spinning
+     */
+
+    DcMotor Claw, Slide;
+
+    public void runOpMode() {
+
+        initRobot();
+
+        // Code that needs to be run once is placed here
+
+        waitForStart();
+        while (opModeIsActive()) {
+            double graber = gamepad1.right_trigger - gamepad1.left_trigger;
+            double slidePow = -gamepad1.left_stick_y;
+            if (slidePow > 0 && Slide.getCurrentPosition() > 4250)
+            {
+                slidePow = 0;
+            } else if (slidePow < 0 && Slide.getCurrentPosition() < 0) {
+                slidePow = 0;
+            }
+            jacobsintake(graber);
+            slide(slidePow);
+        }
+    }
+
+    public void initRobot() {
+        Claw = hardwareMap.get(DcMotor.class, "Claw");
+        Claw.setZeroPowerBehavior(BRAKE);
+        Claw.setDirection(FORWARD);
+        Slide = hardwareMap.get(DcMotor.class, "Slide");
+        Slide.setMode(RUN_USING_ENCODER);
+        Slide.setZeroPowerBehavior(BRAKE);
+        Slide.setDirection(FORWARD);
+    }
+
+    public void jacobsintake(double graber) {
+        Claw.setPower(graber);
+    }
+
+    public void slide(double power) {
+        Slide.setPower(power);
+    }
+}
