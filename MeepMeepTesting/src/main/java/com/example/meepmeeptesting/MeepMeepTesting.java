67fe165f524a447f3504b266437280e5a847a13a
atra-ment <makevina000@gmail.com>
2024-09-17 17:17:30 -0700

    added meepmeep and roadrunner yass

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,46 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+
+public class MeepMeepTesting {
+        public static void main(String[] args) {
+            MeepMeep meepMeep = new MeepMeep(800);
+
+            RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                    // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                    .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                    .followTrajectorySequence(drive ->
+                            drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .forward(30)
+                                    .turn(Math.toRadians(90))
+                                    .build()
+                    );
+
+            Image img = null;
+            try { img = ImageIO.read(new File("C:\\Users\\23393 Programming\\Downloads\\field-2024-juice-dark.png")); }
+            catch (IOException e) {}
+            meepMeep.setBackground(img)
+                    .setDarkMode(true)
+                    .setBackgroundAlpha(0.95f)
+                    .addEntity(myBot)
+                    .start();
+        }
+    }
+
+
