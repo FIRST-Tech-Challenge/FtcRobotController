bb8f7e119b690d1789002a7a184959c71b078d9a
BritneyBick <148918388+BritneyBick@users.noreply.github.com>
2024-09-19 18:24:51 -0500

    Cleaned up code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/BlueMiddleChamber.java
@@ -0,0 +1,54 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Hardware.Drivebase;
+
+@Autonomous(name = "BlueMiddleChamber")
+public class BlueMiddleChamber extends LinearOpMode {
+    public void runOpMode() {
+        Drivebase drivebase = new Drivebase(hardwareMap, this::opModeIsActive);
+
+        telemetry.addData("Status", "Initialized");
+
+        waitForStart();
+
+        //Score preloaded specimen.
+        drivebase.autoDriveForward(0.3, 36.0);
+
+        //To put 1st sample into obs zone.
+        drivebase.driveSideways(0.3, 24);
+        drivebase.autoDriveForward(0.3, 12);
+        drivebase.turnToAngle(90, 0.3, telemetry);
+        drivebase.autoDriveForward(0.3, 12);
+        drivebase.turnToAngle(-90, 0.3, telemetry);
+        drivebase.autoDriveForward(0.3,48);
+
+        //Transition from current line to next one down.
+        drivebase.turnToAngle(-180, 0.3, telemetry);
+
+        //Grab second piece and put into obs zone.
+        drivebase.driveSideways(0.3, 12);
+        drivebase.autoDriveForward(36.0, 0.3);
+        drivebase.turnToAngle(-180, 0.3, telemetry);
+        drivebase.autoDriveForward(0.3, 48.0);
+
+        //Transition to score 1st human player made specimen onto the chamber.
+        drivebase.turnToAngle(90, 0.1, telemetry);
+
+        //Score first human made specimen onto chamber.
+        drivebase.autoDriveForward(0.3, 36);
+        drivebase.turnToAngle(90.0, 0.2, telemetry);
+        drivebase.autoDriveForward(0.3, 48);
+
+        //Score second human player made specimen.
+        drivebase.turnToAngle(-35, 0.3, telemetry);
+        drivebase.autoDriveForward(0.3, -60);
+        drivebase.autoDriveForward(0.3, 60);
+
+        //Park in obs zone.
+        drivebase.autoDriveForward(0.3, -60);
+    }
+}
