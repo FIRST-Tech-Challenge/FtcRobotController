e133a868a5c072153a8be51641246ae8d9e506ad
Samuel <sjmesloh@gmail.com>
2024-09-18 20:41:06 -0400

    Added RoadRunner and MeepMeep to FtcRobotController 10.0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rr/Localizer.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.rr;
+
+import com.acmerobotics.roadrunner.Time;
+import com.acmerobotics.roadrunner.Twist2dDual;
+
+public interface Localizer {
+    Twist2dDual<Time> update();
+}
