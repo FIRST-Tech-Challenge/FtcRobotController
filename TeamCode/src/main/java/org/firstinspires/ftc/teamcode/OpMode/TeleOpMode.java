268f11fc8fd9e1d0c9169a51c72b5b07a1687ba9
AJK <103875831+bello89808m@users.noreply.github.com>
2024-09-20 18:39:40 -0400

    push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpMode.java
@@ -1,13 +1,24 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.arcrobotics.ftclib.gamepad.GamepadEx;
-import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class TeleOpMode extends LinearOpMode {
+    DcMotorEx topLeftMotor, topRightMotor, bottomLeftMotor, bottomRightMotor;
 
     @Override
     public void runOpMode() throws InterruptedException {
-            
+        topLeftMotor = hardwareMap.get(DcMotorEx.class, "topLeft");
+        topRightMotor = hardwareMap.get(DcMotorEx.class, "topRight");
+        bottomLeftMotor = hardwareMap.get(DcMotorEx.class, "bottomLeft");
+        bottomRightMotor = hardwareMap.get(DcMotorEx.class, "bottomRight");
+
+        topLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        topRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bottomLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bottomRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        waitForStart();
     }
 }
