70cf8afdea91a80aceafe841790125d16927470f
IsseyBird <IKB3001@iolani.org>
2024-09-19 17:00:26 -1000

    added the code for a lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -18,6 +18,8 @@ public class Bot {
     private DcMotor rightMotorFront;
     private DcMotor leftMotorBack;
     private DcMotor rightMotorBack;
+    private DcMotor rightLift;
+    private DcMotor leftLift;
 
     //Statistics for measurements
     static final double WHEEL_DIAMETER_INCHES = 1; // For circumference / distance measurements
@@ -41,10 +43,12 @@ public class Bot {
     public void init(HardwareMap map){
 
         //Connecting declared motors to classes of DcMotors and respected names
-        leftMotorFront = map.get(DcMotor.class, "left_front");
-        leftMotorBack = map.get(DcMotor.class, "left_back");
-        rightMotorFront = map.get(DcMotor.class, "right_front");
-        rightMotorBack = map.get(DcMotor.class, "right_back");
+//        leftMotorFront = map.get(DcMotor.class, "left_front");
+//        leftMotorBack = map.get(DcMotor.class, "left_back");
+//        rightMotorFront = map.get(DcMotor.class, "right_front");
+//        rightMotorBack = map.get(DcMotor.class, "right_back");
+        leftLift = map.get(DcMotor.class, "left_lift");//giveing the motors a name for codeing
+        rightLift = map.get(DcMotor.class, "right_lift");
 
         //Set RunModes for Encoder Usage
         /*
@@ -57,10 +61,12 @@ public class Bot {
         //Set Direction of each Motors
         // switch REVERSE and FORWARD if controls are opposite
         // This is set for Mechanum drive
-        leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        leftMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightMotorFront.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+//        leftMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
+//        leftMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+//        rightMotorFront.setDirection(DcMotorSimple.Direction.FORWARD);
+//        rightMotorBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftLift.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightLift.setDirection(DcMotorSimple.Direction.REVERSE);//this is because the motors are probalby faceing each other
 
     }
 
@@ -71,15 +77,21 @@ public class Bot {
      * @param frontRightPower
      * @param backRightPower
      */
-    public void setDriveTrain(
-            double frontLeftPower, double backLeftPower,
-            double frontRightPower, double backRightPower
-    ) {
-        leftMotorFront.setPower(frontLeftPower);
-        leftMotorBack.setPower(backLeftPower);
-        rightMotorFront.setPower(frontRightPower);
-        rightMotorBack.setPower(backRightPower);
-    }
+//    public void setDriveTrain(
+//            double frontLeftPower, double backLeftPower,
+//            double frontRightPower, double backRightPower
+//    ) {
+//        leftMotorFront.setPower(frontLeftPower);
+//        leftMotorBack.setPower(backLeftPower);
+//        rightMotorFront.setPower(frontRightPower);
+//        rightMotorBack.setPower(backRightPower);
+//    }
 
+    public void setLift(
+            double liftPower
+    ){
+        leftLift.setPower(liftPower);
+        rightLift.setPower(liftPower);
+    }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -23,12 +23,22 @@ public class Teleop extends LinearOpMode {
             double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = -gamepad1.left_stick_x; // Counteract imperfect strafing
             double pivot = -gamepad1.right_stick_x;
-
+            double rightLiftTrigger = gamepad1.right_trigger; //rightTrigger is raising the lift
+            double leftLiftTrigger = gamepad1.left_trigger; //leftTrigger is lowering the lift
             double frontLeftPower = y + x + pivot;
             double backLeftPower =  y - x + pivot;
             double frontRightPower = y - x - pivot;
             double backRightPower = y + x - pivot;
-
+            if(rightLiftTrigger > 0.1){
+                bot.setLift(1.0);//this makes it go down
+            }
+            else if(leftLiftTrigger > 0.1){
+                bot.setLift(-1.0);//this makes it go up
+            }
+            else
+            {
+                bot.setLift(0.0);
+            }
             //normalize power value
             double max = Math.max(Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)),
                     Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)));
@@ -39,7 +49,7 @@ public class Teleop extends LinearOpMode {
                 backLeftPower /= max;
                 backRightPower /= max;
             }
-            bot.setDriveTrain(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
+           // bot.setDriveTrain(frontLeftPower, backLeftPower, frontRightPower, backRightPower);
         }
     }
 }
