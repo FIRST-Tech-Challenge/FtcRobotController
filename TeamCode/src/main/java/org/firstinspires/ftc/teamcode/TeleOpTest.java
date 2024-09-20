85add95a973325a53299d7b0c11e0b961d8a6ac3
adamzhang06 <reachadamzhang@gmail.com>
2024-09-19 20:54:47 -0500

    Initial commit w/ TeleOpTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -0,0 +1,110 @@
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "TeleOpTest", group = "Test")
+
+public class TeleOpTest extends LinearOpMode {
+    private DcMotor backLeft;
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backRight;
+
+    @Override
+    public void runOpMode() {
+
+        // initialize motors
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // reverse motors
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        double direction = 1;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // gamepad 1 controls
+            double y = -gamepad1.left_stick_y; // Remember, this is reversed!
+            double x = gamepad1.left_stick_x * 1; // Counteract imperfect strafing
+
+            double rx = gamepad1.right_stick_x;
+            if(direction == 1) {
+                rx = gamepad1.right_stick_x * -1;
+            }
+            else {
+                rx = gamepad1.right_stick_x;
+            }
+
+            if(gamepad1.left_stick_x < 40 && gamepad1.left_stick_x > 60) {
+                x = 50;
+            }
+            if(gamepad1.left_stick_y < 45 && gamepad1.left_stick_y > 55) {
+                y = 50;
+            }
+
+            int s = 1;
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            if (gamepad1.left_bumper)
+                s = 4;
+            else if (gamepad1.right_bumper)
+                s = 2;
+            else
+                s = 1;
+
+            backLeft.setPower(backLeftPower / s);
+            frontLeft.setPower(frontLeftPower / s);
+
+            frontRight.setPower(frontRightPower / s);
+            backRight.setPower(backRightPower / s);
+
+            // switching directions
+            if(gamepad1.y) {
+                direction = 1;
+                backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+                frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+                frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+                backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+            }
+            else if(gamepad1.b) {
+                direction = -1;
+                backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+                frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+                backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+            }
+        }
+    }
+}
