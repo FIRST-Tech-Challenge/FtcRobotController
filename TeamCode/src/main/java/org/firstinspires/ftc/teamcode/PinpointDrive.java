7bab5bf085a2873887f7f10c4d8ff2f2cf85fa90
j5155 <54331556+j5155@users.noreply.github.com>
2024-09-19 17:26:35 -0800

    Update library, add initial draft of PinpointDrive and example class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PinpointDrive.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode;
+
+
+
+import static com.acmerobotics.roadrunner.ftc.OTOSKt.OTOSPoseToRRPose;
+import static com.acmerobotics.roadrunner.ftc.OTOSKt.RRPoseToOTOSPose;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.FlightRecorder;
+import com.acmerobotics.roadrunner.ftc.GoBildaPinpointDriver;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.messages.PoseMessage;
+
+/**
+ * Experimental extension of MecanumDrive that uses the Gobilda Pinpoint sensor for localization.
+ * <p>
+ * Released under the BSD 3-Clause Clear License by j5155 from 12087 Capital City Dynamics
+ * Portions of this code made and released under the MIT License by Gobilda (Base 10 Assets, LLC)
+ * Unless otherwise noted, comments are from Gobilda
+ */
+public class PinpointDrive extends MecanumDrive {
+    public static class Params {
+        /*
+        Set the odometry pod positions relative to the point that the odometry computer tracks around.
+        The X pod offset refers to how far sideways from the tracking point the
+        X (forward) odometry pod is. Left of the center is a positive number,
+        right of center is a negative number. the Y pod offset refers to how far forwards from
+        the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
+        backwards is a negative number.
+         */
+        //these are tuned for 3110-0002-0001 Product Insight #1
+        // RR localizer note: These units are inches, presets are converted from mm (which is why they are inexact)
+        public double xOffset = -3.3071;
+        public double yOffset = -6.6142;
+
+        /*
+        Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
+        the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
+        If you're using another kind of odometry pod, input the number of ticks per millimeter of that pod.
+
+        RR LOCALIZER NOTE: this is ticks per MILLIMETER, NOT inches per tick.
+        This value should be more than one; the value for the Gobilda 4 Bar Pod is approximately 20.
+        To get this value from inPerTick, first convert the value to millimeters (multiply by 25.4)
+        and then take its inverse (one over the value)
+         */
+        public double encoderResolution = GoBildaPinpointDriver.goBILDA_4_BAR_POD;
+
+        /*
+        Set the direction that each of the two odometry pods count. The X (forward) pod should
+        increase when you move the robot forward. And the Y (strafe) pod should increase when
+        you move the robot to the left.
+         */
+        public GoBildaPinpointDriver.EncoderDirection xDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+        public GoBildaPinpointDriver.EncoderDirection yDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
+    }
+
+    public static Params PARAMS = new Params();
+    public GoBildaPinpointDriver pinpoint;
+    private Pose2d lastPinpointPose = pose;
+
+    public PinpointDrive(HardwareMap hardwareMap, Pose2d pose) {
+        super(hardwareMap, pose);
+        pinpoint = hardwareMap.get(GoBildaPinpointDriver.class,"pinpoint");
+
+        // RR localizer note: don't love this conversion (change driver?)
+        pinpoint.setOffsets(DistanceUnit.MM.fromInches(PARAMS.xOffset), DistanceUnit.MM.fromInches(PARAMS.yOffset));
+
+
+        pinpoint.setEncoderResolution(PARAMS.encoderResolution);
+
+        /*
+        Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
+        The IMU will automatically calibrate when first powered on, but recalibrating before running
+        the robot is a good idea to ensure that the calibration is "good".
+        resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
+        This is recommended before you run your autonomous, as a bad initial calibration can cause
+        an incorrect starting value for x, y, and heading.
+         */
+        //pinpoint.recalibrateIMU();
+        pinpoint.resetPosAndIMU();
+
+        pinpoint.setPosition(pose);
+    }
+    @Override
+    public PoseVelocity2d updatePoseEstimate() {
+        if (lastPinpointPose != pose) {
+            // RR localizer note:
+            // Something else is modifying our pose (likely for relocalization),
+            // so we override otos pose with the new pose.
+            // This could potentially cause up to 1 loop worth of drift.
+            // I don't like this solution at all, but it preserves compatibility.
+            // The only alternative is to add getter and setters, but that breaks compat.
+            // Potential alternate solution: timestamp the pose set and backtrack it based on speed?
+            pinpoint.setPosition(pose);
+        }
+        pinpoint.bulkUpdate(); // RR LOCALIZER NOTE: this is not ideal for loop times.
+        // Driver needs update to be optimized
+        pose = pinpoint.getPosition();
+        lastPinpointPose = pose;
+
+        // RR standard
+        poseHistory.add(pose);
+        while (poseHistory.size() > 100) {
+            poseHistory.removeFirst();
+        }
+
+        FlightRecorder.write("ESTIMATED_POSE", new PoseMessage(pose));
+
+        Pose2d velocity = pinpoint.getVelocity();
+        // RR localizer note
+        // "velocity.position" is a little weird
+        // it's because velocity is stored as a roadrunner pose2d right now
+        // maybe not ideal for this, probably I should change it to use a PoseVelocity2d directly instead
+        return new PoseVelocity2d(new Vector2d(velocity.position.x, velocity.position.y),velocity.heading.toDouble());
+    }
+
+
+}
