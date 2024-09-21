ad43f4d11d738d841152f2d59d460fa6bb8dd200
JenX1 <jennifer.lane0726@gmail.com>
2024-09-21 14:18:44 -0600

    Created OpMode Structure

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/BaseTeleOp.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+abstract public class BaseTeleOp extends GeneralOpMode {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/GeneralOpMode.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+abstract public class GeneralOpMode extends LinearOpMode {
+    public void initialize() {
+        //TODO: add hardware initialization
+    }
+}
