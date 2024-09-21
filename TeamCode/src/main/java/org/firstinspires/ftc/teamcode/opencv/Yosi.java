e30d083fd867fe35c845938c0576fb5ef058cf54
RavivDurban <raviv.durban@gmail.com>
2024-09-21 15:24:55 +0300

    fixed contains in Yosi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/Yosi.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/Yosi.java
@@ -5,13 +5,6 @@ import org.opencv.core.Point;
 public class Yosi {
     public int x, y, width, height;
 
-    public Yosi(int x, int y, int width, int height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
     public Yosi(Point p1, Point p2) {
         x = (int) p1.x;
         y = (int) p1.y;
@@ -24,6 +17,6 @@ public class Yosi {
     }
 
     public boolean contains(Point p) {
-        return (x <= p.x && p.x < x + width && y <= p.y && p.y < y + height) || (x >= p.x && p.x > x + width && y >= p.y && p.y > y + height);
+        return ((x <= p.x && p.x < x + width) || (x >= p.x && p.x > x + width)) && ((y <= p.y && p.y < y + height) || (y >= p.y && p.y > y + height));
     }
 }
