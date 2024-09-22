c7fa3cb71d2b30afdf0a54b056fd9226a1577194
jules white <jules.white1@protonmail.com>
2024-09-22 23:34:54 +0100

    Add feedback on direction change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MovementTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MovementTestTeleOp.java
@@ -24,12 +24,15 @@ public class MovementTestTeleOp extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        // Get all driving motors from hardwaremap
         DcMotor[] motors = teamHardwareMap.GetDriveMotors();
+        // Create mecanum drive class
         Mecanum m = Mecanum.Init(motors[0], motors[1], motors[2], motors[3]);
 
         int current_motor_selected;
         int dir = 1;
 
+        // Gamepads to use for rising edge detector
         Gamepad current_gp = new Gamepad();
         Gamepad prev_gp = new Gamepad();
 
@@ -39,14 +42,23 @@ public class MovementTestTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
+            // Copy both gamepad
             prev_gp.copy(current_gp);
             current_gp.copy(gamepad1);
 
+            // Give gamepad to mecanum to move wheels
             m.Move(current_gp);
 
+            // If pressing left bump for first time then change dir
             if(current_gp.left_bumper && !prev_gp.left_bumper) {
+                gamepad1.rumble(50);
                 dir = -dir;
-                telemetry.addData("Current motor direction", dir);
+                if(dir == 1) {
+                    gamepad1.setLedColor(255, 0, 0, Gamepad.LED_DURATION_CONTINUOUS);
+                } else {
+                    gamepad1.setLedColor(0, 255, 0, Gamepad.LED_DURATION_CONTINUOUS);
+                }
+                telemetry.addData("Current motor direction", dir == 1 ? "Forward" : "Backward");
             }
 
             int final_dir = dir;
