65f4da991822b80e8200cc24599e23ed5b0e527b
jules white <jules.white1@protonmail.com>
2024-09-22 19:28:30 +0100

    Add individual motor testing and collective motor testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MovementTestTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveropmodes/MovementTestTeleOp.java
@@ -7,6 +7,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.teamcode.hardwaremaps.TestingHardwareMap;
 import org.firstinspires.ftc.teamcode.Mecanum;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * Class for testing of simple Mecanum movement
  */
@@ -23,12 +27,43 @@ public class MovementTestTeleOp extends LinearOpMode {
         DcMotor[] motors = teamHardwareMap.GetDriveMotors();
         Mecanum m = Mecanum.Init(motors[0], motors[1], motors[2], motors[3]);
 
+        int current_motor_selected;
+
         waitForStart();
+        teamHardwareMap.Runtime.reset();
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             m.Move(gamepad1);
+
+            if(gamepad1.right_bumper) {
+                Arrays.stream(motors)
+                        .forEach(x -> x.setPower(1));
+            } else {
+                Arrays.stream(motors)
+                        .forEach(x -> x.setPower(0));
+            }
+
+            if(gamepad1.a) {
+                current_motor_selected = 0;
+            } else if(gamepad1.b) {
+                current_motor_selected = 1;
+            } else if(gamepad1.x) {
+                current_motor_selected = 2;
+            } else if(gamepad1.y) {
+                current_motor_selected = 3;
+            } else {
+                current_motor_selected = -1;
+            }
+
+            if(current_motor_selected != -1) {
+                int sel = current_motor_selected;
+                IntStream.range(0, motors.length)
+                        .forEach(x -> motors[x].setPower(x == sel ? 1 : 0));
+            }
         }
+
+        telemetry.update();
     }
 }
