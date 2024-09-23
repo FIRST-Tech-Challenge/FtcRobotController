00830b980bf24def8930bb3eb9ae31d7aa9ab15f
Moss <mossk9532@gmail.com>
2024-09-23 10:32:26 -0700

    Added field centric drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -1,19 +1,30 @@
 package org.firstinspires.ftc.teamcode; 
 
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public abstract class Constants {
 
     public static abstract class DriveConstants {
-        public static final String FRONT_LEFT_MOTOR_NAME = "";
-        public static final String FRONT_RIGHT_MOTOR_NAME = "";
-        public static final String BACK_LEFT_MOTOR_NAME = "";
-        public static final String BACK_RIGHT_MOTOR_NAME = "";
+        public static final boolean FIELD_CENTRIC = true;
+
+        public static final String FRONT_LEFT_MOTOR_NAME = "frontLeftMotor";
+        public static final String FRONT_RIGHT_MOTOR_NAME = "frontRightMotor";
+        public static final String BACK_LEFT_MOTOR_NAME = "rearLeftMotor";
+        public static final String BACK_RIGHT_MOTOR_NAME = "rearRightMotor";
         
-        public static final DcMotorSimple.Direction FRONT_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD; //Arbitrary
-        public static final DcMotorSimple.Direction FRONT_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD; //Arbitrary
-        public static final DcMotorSimple.Direction BACK_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE; //Arbitrary
-        public static final DcMotorSimple.Direction BACK_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE; //Arbitrary
+        public static final DcMotorSimple.Direction FRONT_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE; //Untested
+        public static final DcMotorSimple.Direction FRONT_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD; //Untested
+        public static final DcMotorSimple.Direction BACK_LEFT_MOTOR_DIRECTION = DcMotorSimple.Direction.REVERSE; //Untested
+        public static final DcMotorSimple.Direction BACK_RIGHT_MOTOR_DIRECTION = DcMotorSimple.Direction.FORWARD; //Untested
+
+        public static final IMU.Parameters IMU_PARAMETERS = new IMU.Parameters(
+            new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+            )
+        ); 
         
         public static final double DEADZONE = 0.1;
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.hardware.bosch.BHI260IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 import org.firstinspires.ftc.teamcode;
 
@@ -19,12 +21,17 @@ public class DriveSubsystem extends SubsystemBase {
     private final DcMotorEx backLeftMotor;
     private final DcMotorEx backRightMotor;
 
+    private final BHI260IMU imu;
+
     private double speedMultiplier = 1.0;
 
     public DriveSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         this.hardwareMap = hardwareMap;
         this.telemetry = telemetry;
         
+        imu = hardwareMap.get(BHI260IMU.class, Constants.DriveConstants.IMU_NAME);
+        imu.initialize(Constants.DriveConstants.IMU_PARAMETERS);
+
         frontLeftMotor = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FRONT_LEFT_MOTOR_NAME);
         frontRightMotor = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.FRONT_RIGHT_MOTOR_NAME);
         backLeftMotor = hardwareMap.get(DcMotorEx.class, Constants.DriveConstants.BACK_LEFT_MOTOR_NAME);
@@ -39,21 +46,59 @@ public class DriveSubsystem extends SubsystemBase {
         frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        resetGyro();
+        resetEncoders();
     }
     
     public void drive(double forward, double strafe, double turn) {
+        Constants.DriveConstants.FIELD_CENTRIC ? driveFieldCentric(forward, strafe, turn) : driveRobotCentric(forward, strafe, turn);
+    }
+
+    private void driveFieldCentric(double forward, double strafe, double turn) {
+        double gyroRadians = Math.toRadians(-getHeading());
+        double fieldCentricStrafe = strafe * Math.cos(gyroRadians) - forward * Math.sin(gyroRadians);
+        double fieldCentricDrive = strafe * Math.sin(gyroRadians) + forward * Math.cos(gyroRadians);
+
+        frontLeftMotor.setPower(Range.clip((fieldCentricDrive - fieldCentricStrafe + turn), -1, 1) * speedMultiplier);
+        frontRightMotor.setPower(Range.clip((fieldCentricDrive - fieldCentricStrafe - turn), -1, 1) * speedMultiplier);
+        backLeftMotor.setPower(Range.clip((fieldCentricDrive + fieldCentricStrafe + turn), -1, 1) * speedMultiplier);
+        backRightMotor.setPower(Range.clip((fieldCentricDrive + fieldCentricStrafe - turn), -1, 1) * speedMultiplier);
+    }
+
+    private void driveRobotCentric(double forward, double strafe, double turn) {
         forward = Math.abs(forward) >= Constants.DriveConstants.DEADZONE ? forward : 0;
         strafe = Math.abs(strafe) >= Constants.DriveConstants.DEADZONE ? strafe : 0;
         turn = Math.abs(turn) >= Constants.DriveConstants.DEADZONE ? turn : 0;
 
-        frontLeftMotor.setPower(Range.clip((forward - strafe + turn), -1, 1));
-        frontRightMotor.setPower(Range.clip((forward - strafe - turn), -1, 1));
-        backLeftMotor.setPower(Range.clip((forward + strafe + turn), -1, 1));
-        backRightMotor.setPower(Range.clip((forward + strafe - turn), -1, 1));
+        frontLeftMotor.setPower(Range.clip((forward - strafe + turn), -1, 1) * speedMultiplier);
+        frontRightMotor.setPower(Range.clip((forward - strafe - turn), -1, 1) * speedMultiplier);
+        backLeftMotor.setPower(Range.clip((forward + strafe + turn), -1, 1) * speedMultiplier);
+        backRightMotor.setPower(Range.clip((forward + strafe - turn), -1, 1) * speedMultiplier);
     }
 
     public void setSpeedMultiplier(double multiplier) {
         speedMultiplier = Range.clip(multiplier, 0, 1);
+    }
+
+    public void resetGyro() {
+        imu.resetYaw();
+    }
+
+    public void resetEncoders() {
+        frontLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private double getHeading() {
+        return imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
     }
    
 }
