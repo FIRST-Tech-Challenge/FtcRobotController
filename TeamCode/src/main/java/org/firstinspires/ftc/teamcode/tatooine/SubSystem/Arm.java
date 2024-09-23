8b2e70a644ca7ac60250468a2af2ce78bd63d1cf
Yanivushka <yanivabramov29@icloud.com>
2024-09-23 19:56:58 +0300

    Add arm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/SubSystem/Arm.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode.tatooine.SubSystem;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.tatooine.utils.PIDFController;
+import org.firstinspires.ftc.teamcode.tatooine.utils.mathUtil.MathUtil;
+
+public class Arm {
+
+    private final double ANGLE_TOLERANCE = 0;//deg
+    private final double EXTEND_TOLERANCE = 0;//mm
+    private final double EXTEND_CPR = 0;
+    private final double ANGLE_CPR = 0;
+    private final double SPOOL_DIM = 0;//mm
+    private final double angle = 0;
+    private final double length = 0;
+    private Telemetry telemetry;
+    private boolean isDebug;
+    private PIDFController anglePID = new PIDFController(0, 0, 0, 0);
+    private PIDFController extendPID = new PIDFController(0, 0, 0, 0);
+    private DcMotorEx angleMotor;
+    private DcMotorEx extendMotor;
+
+    public Arm(OpMode opMode, boolean isDebug) {
+
+        telemetry = opMode.telemetry;
+
+        this.isDebug = isDebug;
+
+        angleMotor = (DcMotorEx) opMode.hardwareMap.get(DcMotor.class, "AngleMotor");
+        extendMotor = (DcMotorEx) opMode.hardwareMap.get(DcMotor.class, "ExtendMotor");
+
+        extendPID.setTolerance(EXTEND_TOLERANCE);
+        anglePID.setTolerance(ANGLE_TOLERANCE);
+    }
+
+    public double getAngle() {
+        return MathUtil.convertTicksToDegries(ANGLE_CPR, angleMotor.getCurrentPosition());
+    }
+
+
+    public double getLength() {
+        return MathUtil.convertTicksToDistance(EXTEND_CPR, SPOOL_DIM, extendMotor.getCurrentPosition());
+    }
+
+
+    public Telemetry getTelemetry() {
+        return telemetry;
+    }
+
+    public void setTelemetry(Telemetry telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    public boolean isDebug() {
+        return isDebug;
+    }
+
+    public void setDebug(boolean debug) {
+        isDebug = debug;
+    }
+
+    public PIDFController getAnglePID() {
+        return anglePID;
+    }
+
+    public void setAnglePID(PIDFController anglePID) {
+        this.anglePID = anglePID;
+    }
+
+    public PIDFController getExtendPID() {
+        return extendPID;
+    }
+
+    public void setExtendPID(PIDFController extendPID) {
+        this.extendPID = extendPID;
+    }
+
+    public DcMotorEx getAngleMotor() {
+        return angleMotor;
+    }
+
+    public void setAngleMotor(DcMotorEx angleMotor) {
+        this.angleMotor = angleMotor;
+    }
+
+    public DcMotorEx getExtendMotor() {
+        return extendMotor;
+    }
+
+    public void setExtendMotor(DcMotorEx extendMotor) {
+        this.extendMotor = extendMotor;
+    }
+
+    public double getANGLE_TOLERANCE() {
+        return ANGLE_TOLERANCE;
+    }
+
+    public double getEXTEND_TOLERANCE() {
+        return EXTEND_TOLERANCE;
+    }
+
+    public Action setAngle(double angle) {
+        moveAngle move = new moveAngle();
+        move.setGoal(angle);
+        if (isDebug) {
+            telemetry.addData("the new ang ", angle);
+        }
+        return move;
+    }
+
+    public Action setExtension(double extension) {
+        moveExtension move = new moveExtension();
+        move.setGoal(extension);
+        telemetry.addData("the new extension ", extension);
+        return move;
+    }
+
+
+    public class moveExtension implements Action {
+        private double goal = 0;
+
+        public double getGoal() {
+            return goal;
+        }
+
+        public void setGoal(double goal) {
+            this.goal = goal;
+        }
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            extendMotor.setPower(extendPID.calculate(MathUtil.convertTicksToDistance(EXTEND_CPR, SPOOL_DIM, extendMotor.getCurrentPosition()), goal));
+            if (isDebug) {
+                telemetryPacket.put("motor (E) Power", extendMotor.getCurrentPosition());
+            }
+            return !extendPID.atSetPoint();
+        }
+    }
+
+    public class moveAngle implements Action {
+        private double goal = 0;
+
+        public double getGoal() {
+            return goal;
+        }
+
+        public void setGoal(double goal) {
+            this.goal = goal;
+        }
+
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            angleMotor.setPower(anglePID.calculate(MathUtil.convertTicksToDegries(ANGLE_CPR, angleMotor.getCurrentPosition()), goal));
+            if (isDebug) {
+                telemetryPacket.put("motor (A) Power", extendMotor.getCurrentPosition());
+            }
+            return !anglePID.atSetPoint();
+        }
+    }
+
+
+}
