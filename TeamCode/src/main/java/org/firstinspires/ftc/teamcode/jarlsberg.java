227c63461def15415e5519adb056b41b461992b4
yeah-caleb <caleb.meeker@mvschool.com>
2024-09-23 17:24:15 -0400

    hoco teleop :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jarlsberg.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/jarlsberg.java
@@ -1,6 +1,42 @@
 package org.firstinspires.ftc.teamcode;
 
-public class jarlsberg {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-    /*blah*/
+@TeleOp(name= "hoco")
+public class jarlsberg extends OpMode{
+
+    DcMotorEx rightFront = null;
+    DcMotorEx leftFront = null;
+    DcMotorEx rightBack = null;
+    DcMotorEx leftBack = null;
+
+    @Override
+    public void init()
+    {
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftRear");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightRear");
+
+        rightFront.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBack.setDirection(DcMotorEx.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        float x = gamepad1.left_stick_x;
+        float y = -gamepad1.left_stick_y;
+        float rx = gamepad1.right_stick_x;
+
+        leftFront.setPower(y+x+rx);
+        rightFront.setPower(y-x-rx);
+        leftBack.setPower(y-x+rx);
+        rightBack.setPower(y+x-rx);
+
+    }
+
+
 }
