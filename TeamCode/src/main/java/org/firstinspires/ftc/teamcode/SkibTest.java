239189694a86f963f7a97f6942c62a30f185d516
itestadmin <a@a.gmail.com>
2024-09-24 15:52:44 +0300

    test git

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkibTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@Autonomous(name = "SkibTest")
+public class SkibTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            // Pre-run
+            while (opModeIsActive()) {
+                // OpMode loop
+            }
+        }
+    }
+}
