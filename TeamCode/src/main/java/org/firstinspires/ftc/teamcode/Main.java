69a79a50b9b978c4842cf1a7ca1f2cf0a354fef1
Itay Yagil <itayyagil@gmail.com>
2024-09-24 16:46:30 +0300

    check

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.os.Debug;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@Autonomous(name = "Main")
+public class Main extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            // Pre-run
+            while (opModeIsActive()) {
+
+            }
+        }
+    }
+}
