87575af869f6670606bf1f4f3f581e78ffacda15
angrybirdsFTC <angrybirdsftc@gmail.com>
2024-09-24 18:57:05 +0300

    added angle factor to cleanLines of OpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/FastDetectSamples.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/FastDetectSamples.java
@@ -20,9 +20,11 @@ import java.util.List;
 public class FastDetectSamples extends OpenCvPipeline {
 
     public /*final*/ OpenCvCamera webcam;
-    boolean viewportPaused;
+    boolean viewportPaused; 
 
     private final Telemetry telemetry;
+
+    private static final double ANGLE = 5;
 
     static int[] linesArray;
 
@@ -36,32 +38,35 @@ public class FastDetectSamples extends OpenCvPipeline {
 
     public static void cleanLines(int noise){
         int[] result = new int[linesArray.length];
-        int[] indices = new int[linesArray.length/4];
+        int[] indices = new int[linesArray.length];
         int values = 0;
 
         for (int i = 0; i < linesArray.length; i += 4){
+            double alpha = Math.atan((double)(linesArray[i+3] - linesArray[i+1]) / (linesArray[i+2] - linesArray[i]));
             if (indices[i/2] != -1){
                 Rectangle outer = new Rectangle(linesArray[i], linesArray[i+1], linesArray[i+2], linesArray[i+3]);
                 outer.width += noise;
                 outer.height += noise;
-                for (int j = i + 2; j < linesArray.length; j += 4){
-                    if (outer.contains(linesArray[j], linesArray[j+1])){
-                        indices[j/2] = -1;
-                        if (!outer.contains(linesArray[j+2], linesArray[j+3])){
-                            outer = outer.fit(outer, i, j + 2, noise);
-                        }
-                    }
-                    else if (outer.contains(linesArray[j+2], linesArray[j+3])){
-                        indices[j/2] = -1;
-                        if (!outer.contains(linesArray[j], linesArray[j+1])){
-                            outer = outer.fit(outer, i, j, noise);
+                for (int j = i + 4; j < linesArray.length; j += 4) {
+                    double theta = Math.atan((double) (linesArray[i + 3] - linesArray[i + 1]) / (linesArray[i + 2] - linesArray[i]));
+                    if (Math.abs(alpha - theta) < ANGLE) {
+                        if (outer.contains(linesArray[j], linesArray[j + 1])) {
+                            indices[j / 2] = -1;
+                            if (!outer.contains(linesArray[j + 2], linesArray[j + 3])) {
+                                outer = outer.fit(outer, i, j + 2, noise);
+                            }
+                        } else if (outer.contains(linesArray[j + 2], linesArray[j + 3])) {
+                            indices[j / 2] = -1;
+                            if (!outer.contains(linesArray[j], linesArray[j + 1])) {
+                                outer = outer.fit(outer, i, j, noise);
+                            }
                         }
                     }
                 }
                 result[values] = outer.x;
-                result[values+1] = outer.y;
-                result[values+2] = outer.x + outer.width - noise;
-                result[values+3] = outer.y + outer.height - noise;
+                result[values + 1] = outer.y;
+                result[values + 2] = outer.x + outer.width - noise;
+                result[values + 3] = outer.y + outer.height - noise;
                 values += 4;
             }
         }
@@ -74,7 +79,7 @@ public class FastDetectSamples extends OpenCvPipeline {
     public Mat processFrame(Mat input) {
         Mat yellowMask = preprocessFrame(input);
         List<MatOfPoint> contours = new ArrayList<>();
-        Imgproc.findContours(yellowMask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(yellowMask, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_TC89_L1);
         for (MatOfPoint contour : contours) {
             MatOfInt4 lines = new MatOfInt4();
             Mat black = Mat.zeros(input.size(), input.type());
@@ -89,27 +94,34 @@ public class FastDetectSamples extends OpenCvPipeline {
             Imgproc.drawContours(black, approxContours, -1, new Scalar(255, 255, 255), 1);
             Imgproc.HoughLinesP(black, lines, 10, Math.PI / 90, 12, 35, 40);
 
-            linesArray = new int[0];
+
+            boolean check = false;
             try {
                 linesArray = lines.toArray();
+                check = true;
             }
             catch (Exception e) {
                 telemetry.addData("No lines found", "");
             }
 
-            cleanLines(5);
-            telemetry.addData("Number of lines", linesArray.length / 4);
+            if (check) {
+                //TODO - fix problem with corners, Raviv thinks we should add to every line a theta (degree) value and if 2 lines aren't close enough we remove them
+                //we can also add to the function something that checks whether or not a line "goes between" (meaning it is a diagonal) 2 other lines, then we should remove it.
+                cleanLines(5);
+                cleanLines(10);
+                telemetry.addData("Number of lines", linesArray.length / 4);
 
-            for (int i = 0; i < linesArray.length; i += 4) {
-                int x1 = linesArray[i], y1 = linesArray[i + 1], x2 = linesArray[i + 2], y2 = linesArray[i + 3];
-                double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-                Imgproc.line(input, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 0, 0), 1);
-                Imgproc.putText(input, String.valueOf(Math.round(length)), new Point((x1 + x2) / 2.0, (y1 + y2) / 2.0), Imgproc.FONT_HERSHEY_SIMPLEX, 0.4, new Scalar(0, 255, 0), 1);
-                telemetry.addData("Vertical Line Length", length);
+                for (int i = 0; i < linesArray.length; i += 4) {
+                    int x1 = linesArray[i], y1 = linesArray[i + 1], x2 = linesArray[i + 2], y2 = linesArray[i + 3];
+                    double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                    Imgproc.line(input, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 0, 0), 1);
+                    Imgproc.putText(input, String.valueOf(Math.round(length)), new Point((x1 + x2) / 2.0, (y1 + y2) / 2.0), Imgproc.FONT_HERSHEY_SIMPLEX, 0.4, new Scalar(0, 255, 0), 1);
+                    telemetry.addData("Vertical Line Length", length);
+                }
             }
         }
         telemetry.update();
-        return input;
+        return yellowMask;
 
     }
 
