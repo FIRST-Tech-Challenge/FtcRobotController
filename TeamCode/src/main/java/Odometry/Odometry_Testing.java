3e980a1c1ad1ec3e1eba79f18d06d1f859beadc1
Gordon <gordon.zhu25@ycdsbk>
2024-09-24 14:48:52 -0400

    Update Odometry

--- TeamCode/src/main/java/Odometry/Odometry_Testing.java
+++ TeamCode/src/main/java/Odometry/Odometry_Testing.java
@@ -88,9 +88,12 @@ public class Odometry_Testing extends LinearOpMode {
                 delta_local_y = delta_encoder_h - (delta_ang * disM_encoderHtoCenter);
 
             // Convert local changes to global coordinates
-           // current_ang = 0;
+            //prev
+            // delta_global_x = delta_local_x * Math.cos(current_ang) - delta_local_y * Math.sin(current_ang);
+            // delta_global_y = delta_local_x * Math.sin(current_ang) + delta_local_y * Math.cos(current_ang);
+
             delta_global_x = delta_local_x * Math.cos(current_ang) - delta_local_y * Math.sin(current_ang);
-            delta_global_y = delta_local_x * Math.sin(current_ang) + delta_local_y * Math.cos(current_ang);
+            delta_global_y = delta_encoder_h * Math.sin(current_ang) + delta_local_y * Math.cos(current_ang);
 
             // Update global positions
             global_xM += delta_global_x;
