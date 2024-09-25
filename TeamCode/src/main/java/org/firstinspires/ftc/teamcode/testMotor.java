8278ffc69263ebaaa61a8cb354847014b105348d
EagleNotBot <phibersto26@gmail.com>
2024-09-24 20:33:16 -0500

    Class to find motor in port

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testMotor.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class testMotor extends LinearOpMode {
+
+    DcMotor motor;
+
+    public void runOpMode() {
+        initR();
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double pow = -gamepad1.right_stick_y;
+            motor.setPower(pow);
+        }
+    }
+
+    public void initR() {
+        motor = hardwareMap.get(DcMotor.class, "motor");
+        motor.setZeroPowerBehavior(BRAKE);
+        motor.setDirection(FORWARD);
+        motor.setMode(RUN_WITHOUT_ENCODER);
+    }
+}
