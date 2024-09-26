6a495ab08bb64379b522a7f64ecc109c1fb3f43d
Eric Zhang <ericbkzhang@gmail.com>
2024-09-26 15:18:20 -0400

    Renamed several classes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/WheelsSystem.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+public abstract class WheelsSystem {
+    // A modifier for much power the wheels run with (0.0 - 1.0)
+    protected double wheelPower = 1.0;
+    
+    public WheelsSystem() {
+
+    }
+}
