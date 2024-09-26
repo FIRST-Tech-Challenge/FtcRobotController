6d0e27024b6cb00177e6190a135ebb01a6021ca1
jules <62078259+juleswhi@users.noreply.github.com>
2024-09-26 11:35:23 +0100

    Update OneMotorHardwareMap.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwaremaps/OneMotorHardwareMap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwaremaps/OneMotorHardwareMap.java
@@ -9,31 +9,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  * Class to provide a helpful abstraction layer for accessing the HardwareMap
  */
 public class OneMotorHardwareMap extends TeamHardwareMap {
-    /*
-        -----------------------------------------------------------------------
-        | FRW               | Front Right Wheel     | Control Hub Motor 0     |
-        -----------------------------------------------------------------------
-        | FLW               | Front Left Wheel      | Control Hub Motor 1     |
-        -----------------------------------------------------------------------
-        | BRW               | Back Right Wheel      | Control Hub Motor 2     |
-        -----------------------------------------------------------------------
-        | BLW               | Back Left Wheel       | Control Hub Motor 3     |
-        -----------------------------------------------------------------------
-        | RIGHT_ODOMETER    | Right Odometer        | None                    |
-        -----------------------------------------------------------------------
-        | LEFT_ODOMETER     | Left Odometer         | None                    |
-        -----------------------------------------------------------------------
-        | CENTRE_ODOMETER   | Centre Odometer       | None                    |
-        -----------------------------------------------------------------------
-     */
-
-    public DcMotorEx FrontRightMotor;
-
+    public DcMotorSimple FrontRightMotor;
 
     public OneMotorHardwareMap(HardwareMap hardwaremap) {
         super(hardwaremap);
     }
-
 
     @Override
     public void initialise() {
