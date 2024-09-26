5f4b332b22b7ee5311edb0efe3732c5b2750624a
ErrorCode23246 <149131927+ErrorCode23246@users.noreply.github.com>
2024-09-25 18:15:42 -0700

    Modified TeleOp.java to work with Hardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -71,14 +71,14 @@ public class TeleOp extends LinearOpMode {
 
     // Create a RobotHardware object to be used to access robot hardware.
     // Prefix any hardware functions with "robot." to access this class.
-    RobotHardware robot       = new RobotHardware(this);
+    Hardware robot       = new Hardware(this);
 
     @Override
     public void runOpMode() {
-        double drive        = 0;
-        double turn         = 0;
-        double arm          = 0;
-        double handOffset   = 0;
+        double drive = 0;
+        double turn = 0;
+        double strafe = 0;
+
 
         // initialize all the hardware, using the hardware class. See how clean and simple this is?
         robot.init();
@@ -94,34 +94,16 @@ public class TeleOp extends LinearOpMode {
             // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
             // This way it's also easy to just drive straight, or just turn.
             drive = -gamepad1.left_stick_y;
+            strafe = -gamepad1.left_stick_x;
             turn  =  gamepad1.right_stick_x;
 
             // Combine drive and turn for blended motion. Use RobotHardware class
-            robot.driveRobot(drive, turn);
+            robot.driveRobot(drive, turn, strafe);
 
             // Use gamepad left & right Bumpers to open and close the claw
             // Use the SERVO constants defined in RobotHardware class.
             // Each time around the loop, the servos will move by a small amount.
             // Limit the total offset to half of the full travel range
-            if (gamepad1.right_bumper)
-                handOffset += robot.HAND_SPEED;
-            else if (gamepad1.left_bumper)
-                handOffset -= robot.HAND_SPEED;
-            handOffset = Range.clip(handOffset, -0.5, 0.5);
-
-            // Move both servos to new position.  Use RobotHardware class
-            robot.setHandPositions(handOffset);
-
-            // Use gamepad buttons to move arm up (Y) and down (A)
-            // Use the MOTOR constants defined in RobotHardware class.
-            if (gamepad1.y)
-                arm = robot.ARM_UP_POWER;
-            else if (gamepad1.a)
-                arm = robot.ARM_DOWN_POWER;
-            else
-                arm = 0;
-
-            robot.setArmPower(arm);
 
             // Send telemetry messages to explain controls and show robot status
             telemetry.addData("Drive", "Left Stick");
@@ -132,8 +114,7 @@ public class TeleOp extends LinearOpMode {
 
             telemetry.addData("Drive Power", "%.2f", drive);
             telemetry.addData("Turn Power",  "%.2f", turn);
-            telemetry.addData("Arm Power",  "%.2f", arm);
-            telemetry.addData("Hand Position",  "Offset = %.2f", handOffset);
+            telemetry.addData("Strafe Power", "%.2f", strafe);
             telemetry.update();
 
             // Pace this loop so hands move at a reasonable speed.
