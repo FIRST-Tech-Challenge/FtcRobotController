7da178e5e632ba3f26d3494217fd261b81b279e0
Eric Zhang <ericbkzhang@gmail.com>
2024-09-26 23:58:53 -0400

    Add getAllMotors() to wheels system, which is intended to return all DcMotors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/WheelsSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/WheelsSystem.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
+import java.util.HashSet;
+
 public abstract class WheelsSystem {
     // A modifier for much power the wheels run with (0.0 - 1.0)
     protected double wheelPower = 1.0;
@@ -7,4 +9,10 @@ public abstract class WheelsSystem {
     public WheelsSystem() {
 
     }
+
+    /**
+     * Get all the DcMotors that are used by this wheels system.
+     * @return A set that contains every DcMotor included by this wheels system.
+     */
+    public abstract HashSet<DcMotor> getAllMotors();
 }
