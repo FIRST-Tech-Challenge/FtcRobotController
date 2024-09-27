171223754249c2b05d8c1be7a9f986787167ce16
jules <62078259+juleswhi@users.noreply.github.com>
2024-09-27 10:34:34 +0100

    Add loads of colours to gamepad colours

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper.java
@@ -16,7 +16,29 @@ public class Helper {
     public enum GamepadColour {
         RED(255, 0, 0),
         GREEN(0, 255, 0),
-        BLUE(0, 0, 255);
+        BLUE(0, 0, 255),
+        BLACK(0, 0, 0),
+        WHITE(255, 255, 255),
+        CYAN(0, 255, 255),
+        PURPLE(127, 0, 255),
+        YELLOW(255, 255, 0),
+        ORANGE(255, 128, 0),
+        PINK(255, 0, 127),
+        
+        DARK_RED(102, 0, 0),
+        DARK_YELLOW(102, 102, 0),
+        DARK_GREEN(0, 102, 0),
+        DARK_BLUE(0, 0, 102),
+        DARK_YELLOW(102, 102, 0),
+        DARK_CYAN(0, 102, 102),
+        
+        LIGHT_RED(255, 153, 153),
+        LIGHT_YELLOW(255, 255, 153),
+        LIGHT_GREEN(153, 255, 153),
+        LIGHT_CYAN(153, 255, 255),
+        LILAC(153, 153, 255),
+        LIGHT_PINK(255, 153, 204);
+        
 
         private final int red;
         private final int green;
