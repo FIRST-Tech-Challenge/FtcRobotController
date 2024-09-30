8f68f924cb4ae439db9aee80b215e198aa9c3d85
10650biohazards <hazmat10650@gmail.com>
2024-09-30 17:08:01 -0400

    Removed old comments
    I removed comments that were from old code that was removed and no longer being used.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/AutonomousWithSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/AutonomousWithSensors.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Robotics_10650_2024_2025_Code;
 
+// Import all of the necessary FTC libraries and code
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -21,49 +22,12 @@ public class AutonomousWithSensors extends LinearOpMode {
 //        telemetry.update();
         // Waits for a person to press start on the control hub
         // then it runs the rest of the program
-        //robot.strafeR(10, 200);
 
-        //forward
-       //robot.goStraight(300, 500);
-//        sleep(2000);
-//        robot.newTurnFunction(90);
         robot.strafeL(20, 500);
         robot.strafeR(20, 500);
         robot.goStraight(20, 500);
         robot.goStraight(-20, 500);
         robot.newTurnFunction(90);
-
-//        sleep(2000);
-//        robot.strafeR(1000, 500);
-        /*robot.goStraight(920, 250);
-        robot.newTurnFunction(-93);
-        robot.goStraight(900, 200);
-        robot.newTurnFunction(5);
-        robot.goStraight(2000, 2000);*/
-
-
-
-
-        // while (opModeIsActive()) {
-
-        //}
-
-        //
-        //turn(45);
-        //turn(0);
-        //Thread.sleep(10000);
-        //turn(-20);
-
-
-
-        // Not necessary to put power value as negative to go backwards
-        //goStraight(-100, 0.3);
-
-        //makeSquare();
-
-//        setMotorVelocity(200);
-//        Thread.sleep(5000);
-        //robot.makeSquare();
 
         // Shutdown motors when the code ends
         robot.stopMotors();
