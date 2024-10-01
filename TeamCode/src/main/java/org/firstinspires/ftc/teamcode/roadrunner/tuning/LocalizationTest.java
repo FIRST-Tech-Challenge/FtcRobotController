d432e85263cfa15b5a8f19c19e6662ab13efcd59
Armaan Sidhu <armaansidhu000@icloud.com>
2024-10-01 13:27:35 -0700

    Worked on imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/LocalizationTest.java
@@ -27,7 +27,6 @@ public class LocalizationTest extends LinearOpMode {
         private Limelight3A limelight;
     public IMU imu;
 
-
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
