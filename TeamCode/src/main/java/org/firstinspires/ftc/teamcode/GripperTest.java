6cd8d99d069d90e5696d7e5514bfc9d833e93a69
Prospectors PC <8533miners@gmail.com>
2024-10-03 18:03:54 -0400

    Cleans old comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GripperTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GripperTest.java
@@ -13,7 +13,6 @@ public class GripperTest extends LinearOpMode {
         Shoulder_Motor.setDirection(DcMotorSimple.Direction.FORWARD);
         Servo Gripper = hardwareMap.servo.get("Gripper_Servo");
 
-//        Servo servoTest = hardwareMap.get(Servo.class, "launch_servo");
 
         Robot2024 robot2024 = new Robot2024(Gripper, Shoulder_Motor);
 
@@ -26,32 +25,19 @@ public class GripperTest extends LinearOpMode {
 
             double modifier = 0.5;
 
-//            Shoulder_Motor.setPower((gamepad2.left_stick_x - gamepad2.left_stick_y - gamepad2.right_stick_x) * modifier);
             robot2024.setShoulderPower((gamepad2.left_stick_x - gamepad2.left_stick_y - gamepad2.right_stick_x) * modifier);
 
 
 
-            // check to see if we need to move the servo.
             if (gamepad2.b) {
-//                closeGripper(Gripper);
                 robot2024.closeGripper();
             } else if (gamepad2.a) {
                robot2024.openGripper();
             }
         telemetry.addData("Servo Position", Gripper.getPosition());
-//        telemetry.addData("Target Power", tgtPower);
         telemetry.addData("Status", "Running");
         telemetry.update();
         }
 
-    }
-    public void closeGripper(Servo gripper){
-        telemetry.addLine("Closing Gripper");
-        gripper.setPosition(0);
-    }
-
-    public void OpenGripper(Servo gripper){
-        telemetry.addLine("Opening Gripper");
-        gripper.setPosition(0.4);
     }
 }
