fa62c3b6dcc59ba738f7e9f690e9c1bacef66867
isabellammjfk <izzysoccer14@icloud.com>
2024-10-02 17:21:59 -0700

    adding meepmeep for rr1.0

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -20,20 +21,13 @@ public class MeepMeepTesting {
                     // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                     .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                     .followTrajectorySequence(drive ->
-                            drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                    .forward(30)
-                                    .turn(Math.toRadians(90))
-                                    .forward(30)
-                                    .turn(Math.toRadians(90))
-                                    .forward(30)
-                                    .turn(Math.toRadians(90))
-                                    .forward(30)
-                                    .turn(Math.toRadians(90))
+                            drive.trajectorySequenceBuilder(new Pose2d(36, 60, Math.toRadians(270)))
+                                    .strafeTo(new Vector2d(40,40))
                                     .build()
                     );
 
             Image img = null;
-            try { img = ImageIO.read(new File("C:\\Users\\23393 Programming\\Downloads\\field-2024-juice-dark.png")); }
+            try { img = ImageIO.read(new File("MeepMeepTesting\\field-2024-juice-dark.png")); }
             catch (IOException e) {}
             meepMeep.setBackground(img)
                     .setDarkMode(true)
