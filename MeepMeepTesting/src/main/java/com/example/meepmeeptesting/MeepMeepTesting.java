bd9fbc12ae5242079e752cb99be8d0cd73d9a93e
slapp <slappedtoast@gmail.com>
2024-10-03 15:16:06 -0700

    working on changing trajectorysequences

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -2,33 +2,59 @@ package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import kotlin.random.Random;
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.DriveShim;
+import org.rowlandhall.meepmeep.roadrunner.SampleMecanumDrive;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequence;
+import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+import java.util.Scanner;
 
 public class MeepMeepTesting {
+
+    private static final Pose2d gameStartPose = new Pose2d(35, 60, Math.toRadians(-90));
+    private static final Pose2d pickUpPose = new Pose2d(27, 0, Math.toRadians(180));
+
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(600);
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f);
+//                .addEntity(myBot)
+//                .start();
+
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(gameStartPose)
+                        .splineToLinearHeading(pickUpPose, Math.toRadians(180 + 90))
                         .build());
 
+        DriveShim botDrive = myBot.getDrive();
+        TrajectorySequence mySequence = botDrive.trajectorySequenceBuilder(gameStartPose)
+                .lineToLinearHeading(pickUpPose)
+                .build();
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
-                .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
-                .start();
+        Scanner scanner = new Scanner(System.in);
+        int poseHeading = 0;
+        int endHeading = 0;
+        meepMeep.addEntity(myBot);
+        while (true) {
+            meepMeep.start();
+
+            System.out.println("enter new pose heading:");
+            poseHeading = scanner.nextInt();
+            System.out.println("enter new end heading:");
+            poseHeading = scanner.nextInt();
+            myBot.followTrajectorySequence(mySequence);
+            System.out.println("next cycle");
+        }
     }
+
 }
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TestRoute.java
@@ -0,0 +1,13 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import org.rowlandhall.meepmeep.roadrunner.trajectorysequence.TrajectorySequenceBuilder;
+
+public class TestRoute {
+
+
+
+}
