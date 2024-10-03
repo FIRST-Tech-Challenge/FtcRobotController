e47d3b27fa9de5cb64b3fbd29fe8e2e06207ac98
Terry Johnson <slaptoast@gmail.com>
2024-10-03 18:55:42 -0500

    Update StandardTrackingWheelLocalizer.java
    reversing the encoder on the left side

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -57,6 +57,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
         //frontEncoder.setDirection(Encoder.Direction.REVERSE);
+        leftEncoder.setDirection(Encoder.Direction.REVERSE );
     }
 
     public static double encoderTicksToInches(double ticks) {
