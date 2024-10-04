ea18934dc1f3369f9a353e182e1184a02728112a
Pickle158 <electricnick490@gmail.com>
2024-10-03 18:41:13 -0600

    change stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -18,7 +18,7 @@ public class MotorTest extends LinearOpMode {
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        Motor  = hardwareMap.get(DcMotor.class, "LeftDR");
+        Motor  = hardwareMap.get(DcMotor.class, "Test");
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
