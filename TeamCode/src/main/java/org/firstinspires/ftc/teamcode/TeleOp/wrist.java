7e22c55d269755f17fa9408951eeb5f3105bf746
ariel <arielg0312@gmail.com>
2024-10-04 15:20:18 -0500

    Wrist; moves claw servo up, mid, down

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/wrist.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+@Config
+public class wrist {
+    public double flexedUp= 1;
+    public double rest = 0.5;
+    public double flexedDown = 0;
+
+    public Servo wrist;
+    public Gamepad gamepad2;
+    public HardwareMap hardwareMap;
+    public Telemetry telemetry;
+
+    public wrist(OpMode opMode){
+        this.hardwareMap = opMode.hardwareMap;
+        this.gamepad2 = opMode.gamepad2;
+        this.telemetry = opMode.telemetry;
+
+        wrist = (Servo) hardwareMap.get("wristSERVO");
+        wrist.setDirection(Servo.Direction.FORWARD);
+    }
+    public void teleOp() {
+        if (gamepad2.left_stick_button) wrist.setPosition(flexedDown);
+        else if (gamepad2.left_stick_button && gamepad2.right_stick_button) wrist.setPosition(rest);
+        else if (gamepad2.right_stick_button) wrist.setPosition(flexedUp);
+    }
+}
