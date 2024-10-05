1788e24b5f541471e38224de62f00b0d665b7406
NT_Bean <samrahmani2010@gmail.com>
2024-10-05 14:13:04 -0700

    created a file for camera code to go into, as well as a file for repository rules to reference

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Camera.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Systems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+// import com.qualcomm.hardware;
+
+public class Camera {
+    // i don't know what im doing
+
+}
