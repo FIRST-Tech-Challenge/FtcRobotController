73ae5dd3dd059462ccc17657276717a9ee1f15e6
tatooine12201 <tatooine12201@gmail.com>
2024-10-07 19:13:57 +0300

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/utils/AllianceSelector.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.tatooine.utils;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AllianceSelector extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
