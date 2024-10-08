4ab1f32b07fd62bf9fdbde44029c4fb2906d57f3
Gordon <gordon.zhu25@ycdsbk>
2024-10-08 14:42:51 -0400

    Updated comments and Flipped globalxM and globalyM around for more accurate field mapping

--- TeamCode/src/main/java/Odometry/Odometry_Testing.java
+++ TeamCode/src/main/java/Odometry/Odometry_Testing.java
@@ -55,7 +55,7 @@ public class Odometry_Testing extends LinearOpMode {
         double prev_encoder_l = 0, prev_encoder_r = 0, prev_encoder_h = 0, prev_ang = 0, current_ang ;
         double delta_encoder_l, delta_encoder_r, delta_encoder_h, delta_local_x, delta_local_y, delta_global_x, delta_global_y, delta_ang;
         double global_xM = 0, global_yM = 0;
-        double disM_encoderHtoCenter = 0.235; // Distance from the horizontal encoder to the center of the robot in meters, allegedly
+        double disM_encoderHtoCenter = 0.22; // Distance from the horizontal encoder to the center of the robot in meters, allegedly
 
         waitForStart();
         runtime.reset();
@@ -73,7 +73,6 @@ public class Odometry_Testing extends LinearOpMode {
             current_ang = Math.toRadians(getAngle()); //IMU angle
            // current_ang = Math.toRadians((encoder_r-encoder_l)/0.023); //(r-l) divided by distance (METRES) between the encoder wheels
 
-
             // Calculate changes in encoder values and angle
             delta_encoder_l = encoder_l - prev_encoder_l;
             delta_encoder_r = encoder_r - prev_encoder_r;
@@ -86,25 +85,27 @@ public class Odometry_Testing extends LinearOpMode {
             delta_local_x = (delta_encoder_l + delta_encoder_r) / 2;
             delta_local_y = delta_encoder_h - (delta_ang * disM_encoderHtoCenter);
 
-             // Convert local changes to global coordinates; Note X and Y are switched
-            delta_global_y = delta_local_x * Math.cos(current_ang) - delta_local_y * Math.sin(current_ang);
-            delta_global_x = delta_local_x * Math.sin(current_ang) + delta_local_y * Math.cos(current_ang);
+             // Convert local changes to global coordinates; Note X and Y are switched (maybe switched idk)
+            delta_global_x = delta_local_x * Math.cos(current_ang) - delta_local_y * Math.sin(current_ang);
+            delta_global_y = delta_local_x * Math.sin(current_ang) + delta_local_y * Math.cos(current_ang);
 
             // Update global positions
             global_xM += delta_global_x;
             global_yM += delta_global_y;
-
-            // Display telemetry data
-            telemetry.addData("x (cm)", global_xM*100);
-            telemetry.addData("y (cm)", global_yM*100);
-            telemetry.addData("Angle (degrees)", Math.toDegrees(current_ang));
-            telemetry.update();
 
             // Update previous values for next loop iteration
             prev_encoder_l = encoder_l;
             prev_encoder_r = encoder_r;
             prev_encoder_h = encoder_h;
             prev_ang = current_ang;
+
+            // Display telemetry data
+            telemetry.addData("x (cm)", global_xM*100);
+            telemetry.addData("y (cm)", global_yM*100);
+            telemetry.addData("Angle (degrees)", Math.toDegrees(current_ang));
+            telemetry.addData("Angle (delta)", Math.toDegrees(delta_ang));
+
+            telemetry.update();
         }
     }
 
@@ -128,16 +129,10 @@ public class Odometry_Testing extends LinearOpMode {
 
         double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
 
-        if (deltaAngle < -180)
-            deltaAngle += 360;
-        else if (deltaAngle > 180)
-            deltaAngle -= 360;
-
         globalAngle += deltaAngle;
 
         lastAngles = angles;
 
-        //angle is negative on purpose
-        return -globalAngle;
+        return globalAngle;
     }
 }
