1581d080b11daa9cf5be1e8b308da8da6d2fc816
eitandas <142007178+eitandas@users.noreply.github.com>
2024-10-08 22:34:00 +0300

    added ColorSensor subSystem
    need to add color none and if color none (in checkColors fanction ) and if none color then isRightColor( for specimen or sample) returns true

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/SubSystem/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/SubSystem/Intake.java
@@ -1,48 +1,34 @@
 package org.firstinspires.ftc.teamcode.tatooine.SubSystem;
 
 
-import android.graphics.ColorSpace;
-
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
-import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
-import org.firstinspires.ftc.teamcode.tatooine.utils.SampleColor;
-import org.opencv.core.Scalar;
+import org.firstinspires.ftc.teamcode.tatooine.utils.Alliance.CheckAlliance;
 
 public class Intake  {
     CRServo intake = null;
-    NormalizedColorSensor normalizedColorSensor = null;
-    SampleColor sampleColor;
-
+    CheckAlliance alliance;
     private double power = 0;
     private boolean buttonPressed = false;
 
-    public Intake(HardwareMap hardwareMap, SampleColor sampleColor) {
-        this.sampleColor = sampleColor;
-        intake = hardwareMap.get(CRServo.class, "intake");
-        normalizedColorSensor = hardwareMap.get(NormalizedColorSensor.class, "Color Sensor");
-    }
-    public void chooseColor(){
-        if (sampleColor == SampleColor.BLUE || sampleColor == SampleColor.BLUEANDYELLOW){
-            
-        }
-        else{
 
-        }
+    public Intake(HardwareMap hardwareMap, CheckAlliance alliance) {
+        this.alliance = alliance;
+        intake = hardwareMap.get(CRServo.class, "intake");
     }
+
     public Action intake(boolean buttonPressed){
         this.buttonPressed = buttonPressed;
         power = 1;
         return new SetPowerAction();
     }
-    public Action outtake(){
+    public Action outtake(boolean buttonPressed){
         power = -1;
         return new SetPowerAction();
     }
