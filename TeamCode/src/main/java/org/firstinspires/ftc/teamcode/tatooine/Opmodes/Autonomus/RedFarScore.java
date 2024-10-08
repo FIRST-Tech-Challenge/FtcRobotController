8fa0b2358a6798b886f88327c749e102f353b413
EliadTheStronk <eliadhadar9@gmail.com>
2024-10-08 22:36:50 +0300

    New RedFarScore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/Opmodes/Autonomus/RedFarScore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/Opmodes/Autonomus/RedFarScore.java
@@ -19,21 +19,12 @@ public class RedFarScore extends LinearOpMode {
         Pose2d beginPose = new Pose2d(12.09+3.5, -59.84, Math.toRadians(90.00));
         MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
         TrajectoryActionBuilder trajectoryAction1 = drive.actionBuilder(beginPose)
-                .splineTo(new Vector2d(7.79, -36.70), Math.toRadians(90))
-                .turnTo(Math.toRadians(90))
-                .waitSeconds(1)
-                .strafeToSplineHeading(new Vector2d(12.09+3.5, -56),Math.toRadians(90))
-                .turnTo(0)
-                .strafeToSplineHeading(new Vector2d(-50, -57.84), Math.toRadians(0))
-                .strafeToSplineHeading(new Vector2d(55, -53.5), Math.toRadians(70))
-                .waitSeconds(1)
-                .turnTo(Math.toRadians(70))
-                .strafeToSplineHeading(new Vector2d(-54, -60), Math.toRadians(0))
-                .strafeToSplineHeading(new Vector2d(55, -52.8), Math.toRadians(105))
-                .waitSeconds(1)
-                .turnTo(Math.toRadians(105))
-                .strafeToSplineHeading(new Vector2d(-54, -60), Math.toRadians(0))
-                .strafeToSplineHeading(new Vector2d(55, -52), Math.toRadians(90));
+                .splineTo(new Vector2d(8,36.7),Math.toRadians(90))
+                .waitSeconds(1.5)
+                .lineToY(58)
+                .strafeTo(new Vector2d())
+
+
 
         Action test = trajectoryAction1.build();
 
