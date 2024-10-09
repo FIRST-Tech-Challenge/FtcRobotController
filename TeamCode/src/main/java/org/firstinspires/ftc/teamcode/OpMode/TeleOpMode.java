5967b01d09d3cda9ccee5f4677791d9566ac6e57
AJK <103875831+bello89808m@users.noreply.github.com>
2024-10-09 16:29:19 -0400

    MeepMeep Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/TeleOpMode.java
@@ -1,39 +1,27 @@
 package org.firstinspires.ftc.teamcode.OpMode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Robot.Robot;
 
 public class TeleOpMode extends LinearOpMode {
     DcMotorEx topLeftMotor, topRightMotor, bottomLeftMotor, bottomRightMotor;
 
+    double maxSpeed;
+
     @Override
     public void runOpMode() throws InterruptedException {
-
-        topLeftMotor = hardwareMap.get(DcMotorEx.class, "topLeft");
-        topRightMotor = hardwareMap.get(DcMotorEx.class, "topRight");
-        bottomLeftMotor = hardwareMap.get(DcMotorEx.class, "bottomLeft");
-        bottomRightMotor = hardwareMap.get(DcMotorEx.class, "bottomRight");
-
-        topLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        topRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bottomLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        bottomRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        topLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
-        bottomLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        Robot robot = new Robot(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x;
-            double rx = gamepad1.right_stick_x;
+            double drive = gamepad1.left_stick_y;
+            double strafe = gamepad1.left_stick_x;
+            double rotate = gamepad1.right_stick_x;
 
-            topLeftMotor.setPower(y+x+rx);
-            bottomLeftMotor.setPower(y-x+rx);
-            topRightMotor.setPower(y+x-rx);
-            bottomRightMotor.setPower(y-x-rx);
+            robot.teleOpDrive(drive,strafe,rotate);
         }
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.Robot;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot {
+    DcMotorEx topLeftMotor, topRightMotor, bottomLeftMotor, bottomRightMotor;
+
+    double maxSpeed;
+
+    public Robot(HardwareMap hardwareMap){
+        topLeftMotor = hardwareMap.get(DcMotorEx.class, "topLeft");
+        topRightMotor = hardwareMap.get(DcMotorEx.class, "topRight");
+        bottomLeftMotor = hardwareMap.get(DcMotorEx.class, "bottomLeft");
+        bottomRightMotor = hardwareMap.get(DcMotorEx.class, "bottomRight");
+
+        topLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        topRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bottomLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bottomRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        topLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        bottomLeftMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
+        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+    }
+
+    public void teleOpDrive(double drive, double strafe, double rotate){
+
+        double[] motorPower = {drive+strafe+rotate,
+                drive-strafe+rotate,
+                drive+strafe-rotate,
+                drive-strafe-rotate};
+
+        maxSpeed = Math.abs(motorPower[0]);
+
+        for (double power : motorPower){
+            if(Math.abs(power) > maxSpeed){
+                maxSpeed = Math.abs(power);
+            }
+        }
+
+        if(maxSpeed > 1){
+            for (int i = 0; i < motorPower.length; i++){
+                motorPower[i] /= maxSpeed;
+            }
+        }
+
+        topLeftMotor.setPower(motorPower[0]);
+        bottomLeftMotor.setPower(motorPower[1]);
+        topRightMotor.setPower(motorPower[2]);
+        bottomRightMotor.setPower(motorPower[3]);
+    }
+}
