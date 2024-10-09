794c233305237c57c2b1b3e6e2944320956d222c
Ram Shreyas Naik Sabavat <rsns2012@outlook.com>
2024-10-08 22:41:21 -0700

    Salmon Day Code

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/SalmonDayDrive.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.robotcontroller.internal;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Salmon Day Drive")
+public class SalmonDayDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        // Reverse right motors
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if(isStopRequested()) return;
+
+        while(opModeIsActive()) {
+            double y = gamepad1.left_stick_y / 2.75; // Y-Axis is default reversed in controller
+            double x = -gamepad1.left_stick_x * 1.1 / 2.75; // Counter imperfect strafing
+            double rx = -gamepad1.right_stick_x / 2.75;
+
+            /* Denominator is the largest motor power (absolute value) or 1
+               This ensures all the powers maintain the same ratio
+               But only if at least one is out of range [-1, 1]
+             */
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backLeftMotor.setPower(backLeftPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
