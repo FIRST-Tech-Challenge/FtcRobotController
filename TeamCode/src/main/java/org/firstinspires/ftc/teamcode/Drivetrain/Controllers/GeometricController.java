05acac9d7c7e02e426a54df5e43e5853c6ee9576
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-10-09 22:29:16 -0400

    slight progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Controllers/GeometricController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Controllers/GeometricController.java
@@ -11,26 +11,26 @@ public class GeometricController {
     public static double radiusXY = 10;
     public static double radiusTheta = 20;
     public double[][] wayPoints;
-    public int lastLookahead = 0;
-    public GeometricController(double[][] wp){
-        wayPoints = wp;
+    public GeometricController(){
     }
-    double solveParameter(double x, double y, int i, double radius){
+    double calcCircleLineIntersection(double xPos, double yPos, int i, double radius){
         double a = Math.pow((wayPoints[i+1][0] - wayPoints[i][0]), 2) + Math.pow((wayPoints[i+1][1] - wayPoints[i][1]), 2);
-        double b = 2*((wayPoints[i][0]-x)*(wayPoints[i+1][0]-wayPoints[i][0]) + (wayPoints[i][1]-y)*(wayPoints[i+1][1]-wayPoints[i][1]));
-        double c = Math.pow(wayPoints[i][0],2)-2*x*wayPoints[i][0]+Math.pow(x, 2)+Math.pow(wayPoints[i][1],2)-2*x*wayPoints[i][1]+Math.pow(y, 2)+Math.pow(radius, 2);
+        double b = 2*((wayPoints[i][0]-xPos)*(wayPoints[i+1][0]-wayPoints[i][0]) + (wayPoints[i][1]-yPos)*(wayPoints[i+1][1]-wayPoints[i][1]));
+        double c = Math.pow(wayPoints[i][0],2)-2*xPos*wayPoints[i][0]+Math.pow(xPos, 2)+Math.pow(wayPoints[i][1],2)-2*xPos*wayPoints[i][1]+Math.pow(yPos, 2)+Math.pow(radius, 2);
         double discriminant = Math.pow(b, 2) - 4 * a * c;
         if (discriminant>=0) {
             double rootOne = (-b + Math.sqrt(discriminant)) / (2 * a);
-            return (rootOne >= 0 && rootOne <= 1) ? rootOne : (-b - Math.sqrt(discriminant)) / (2 * a);
+            double root = (rootOne >= 0 && rootOne <= 1) ? rootOne : (-b - Math.sqrt(discriminant)) / (2 * a);
+            double pX = wayPoints[i][0]+(wayPoints[i+1][0]-wayPoints[i][0])*root;
+            double pY = wayPoints[i][1]+(wayPoints[i+1][1]-wayPoints[i][1])*root;
         }
         return -1;
     }
-    public double calculate(double x, double y){
+    public double calculate(double x, double y, double[][] wayPoints){
+        int lastLookahead = 0;
+        LinkedHashSet<double[]> potentialPoints = new LinkedHashSet<>();
         for (int i=lastLookahead; i< wayPoints.length-1; i++){
-            double tI = solveParameter(x, y, i, radiusXY);
-            double pX = wayPoints[i][0]+(wayPoints[i+1][0]-wayPoints[i][0])*tI;
-            double pY = wayPoints[i][1]+(wayPoints[i+1][1]-wayPoints[i][1])*tI;
+
         }
         //work so far
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Motors/DcMotorExComposition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Motors/DcMotorExComposition.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Drivetrain.Motors;
+package org.firstinspires.ftc.teamcode.Hardware.Motors;
 
 import com.qualcomm.robotcore.hardware.DcMotorController;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
