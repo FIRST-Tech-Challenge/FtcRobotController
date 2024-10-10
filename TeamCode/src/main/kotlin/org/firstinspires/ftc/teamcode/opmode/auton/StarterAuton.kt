12ac02a726270b10259fe367550a602a03a89c54
Bill Broughton <wlbrough@gmail.com>
2024-10-09 20:54:22 -0400

    Starter auton

---
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/opmode/auton/StarterAuton.kt
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.opmode.auton
+
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import org.firstinspires.ftc.teamcode.command.FollowTrajectorySequence
+import org.firstinspires.ftc.teamcode.opmode.OpModeBase
+
+class StarterAuton : OpModeBase() {
+    override fun initialize() {
+        initHardware()
+
+        // Start Pose
+        val startingPose = Pose2d(
+            12.0,
+            63.5,
+            Math.toRadians(270.0)
+        )
+
+        val nextPose = Pose2d(
+            12.0,
+            58.5,
+            Math.toRadians(270.0)
+        )
+
+        val sampleTrajectorySequence = mecanumDrive.trajectorySequenceBuilder(startingPose)
+            .lineToLinearHeading(nextPose)
+            .build()
+
+        schedule(
+            FollowTrajectorySequence(mecanumDrive, sampleTrajectorySequence)
+        )
+    }
+}
