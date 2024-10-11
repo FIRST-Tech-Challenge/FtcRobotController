a7dea6fbeb5b286427aa077cebda8a17710cd8d9
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-10-11 09:25:41 -0400

    more progress

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Controllers/GeometricController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain/Controllers/GeometricController.java
@@ -4,16 +4,18 @@ import com.acmerobotics.dashboard.config.Config;
 
 import org.ejml.simple.SimpleMatrix;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashSet;
 
 @Config
 public class GeometricController {
-    public static double radiusXY = 10;
-    public static double radiusTheta = 20;
+    public static double lookAheadXY = 10;
+    public static double lookAheadTheta = 20;
     public double[][] wayPoints;
     public GeometricController(){
     }
-    double calcCircleLineIntersection(double xPos, double yPos, int i, double radius){
+    double[] calcCircleLineIntersection(double xPos, double yPos, int i, double radius){
         double a = Math.pow((wayPoints[i+1][0] - wayPoints[i][0]), 2) + Math.pow((wayPoints[i+1][1] - wayPoints[i][1]), 2);
         double b = 2*((wayPoints[i][0]-xPos)*(wayPoints[i+1][0]-wayPoints[i][0]) + (wayPoints[i][1]-yPos)*(wayPoints[i+1][1]-wayPoints[i][1]));
         double c = Math.pow(wayPoints[i][0],2)-2*xPos*wayPoints[i][0]+Math.pow(xPos, 2)+Math.pow(wayPoints[i][1],2)-2*xPos*wayPoints[i][1]+Math.pow(yPos, 2)+Math.pow(radius, 2);
@@ -23,15 +25,30 @@ public class GeometricController {
             double root = (rootOne >= 0 && rootOne <= 1) ? rootOne : (-b - Math.sqrt(discriminant)) / (2 * a);
             double pX = wayPoints[i][0]+(wayPoints[i+1][0]-wayPoints[i][0])*root;
             double pY = wayPoints[i][1]+(wayPoints[i+1][1]-wayPoints[i][1])*root;
+            return new double[]{pX, pY};
         }
-        return -1;
+        return new double[]{-99999, -99999};
     }
-    public double calculate(double x, double y, double[][] wayPoints){
+    public SimpleMatrix calculate(double x, double y, double[][] wayPoints){
         int lastLookahead = 0;
         LinkedHashSet<double[]> potentialPoints = new LinkedHashSet<>();
-        for (int i=lastLookahead; i< wayPoints.length-1; i++){
-
+        LinkedHashSet<double[]> thetaPoints = new LinkedHashSet<>();
+        for (int i=lastLookahead; i<wayPoints.length-1; i++){
+            potentialPoints.add(calcCircleLineIntersection(x,y,i,lookAheadXY));
         }
-        //work so far
+        for (int i=lastLookahead; i<wayPoints.length-1; i++){
+            thetaPoints.add(calcCircleLineIntersection(x,y,i,lookAheadXY));
+        }
+        ArrayList<double[]> thetaArray = new ArrayList<>(thetaPoints);
+        ArrayList<double[]> posArray = new ArrayList<>(potentialPoints);
+        double desiredTheta = Math.atan2((thetaArray.get(thetaArray.size()-1)[1]-y), (thetaArray.get(thetaArray.size()-1)[0]-x));
+        SimpleMatrix pose = new SimpleMatrix(
+            new double[]{
+                    posArray.get(thetaArray.size()-1)[0],
+                    posArray.get(thetaArray.size()-1)[1],
+                    desiredTheta
+            }
+        );
+        return pose;
     }
 }
