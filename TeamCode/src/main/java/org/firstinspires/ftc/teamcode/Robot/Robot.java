0439833c039f46c3eed4dd5b8c2447491c97e247
FriendlyGuardian <purplepenguin998@gmail.com>
2024-10-12 14:35:07 -0600

    fixed merge conflict issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot/Robot.java
@@ -1,74 +1,95 @@
 package org.firstinspires.ftc.teamcode.Robot;
 
 import org.firstinspires.ftc.teamcode.Robot.systems.MecanumDriveTrain;
-import android.view.View;
+
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import com.qualcomm.robotcore.hardware.DcMotor;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
+import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.resourses.PIDController;
 import org.firstinspires.ftc.teamcode.resourses.Odometry;
 
 @Config
 public class Robot {
-
-    private double TargetAngle = 0;
-    private double lastTimeAngle;
-
+   
+    
+    //used for how fast the turning input is used.
+    public static double turningInputConstant = 0.005;
+    public static double pCon = 1;
+    public static double dCon = 0;
+    
     private double drive;
     private double slide;
     private double turn;
-
+    //TODO Make more permanent system to detect turning
+    public boolean turningBoolean;
     public MecanumDriveTrain driveTrain;
     public Odometry odometry;
     private double RobotX, RobotY;
     private ElapsedTime runtime = new ElapsedTime();
+    //TODO Tune the pConstant and d Constant numbers, these are place holders.
+    public PIDController anglePID = new PIDController(pCon,dCon, runtime);
+    
     FtcDashboard dashboard;
+    private double robotHeading;
+    private double lastTimeAngle;
     private boolean CurrentAlign = true;
     private boolean DriverOrientationDriveMode = true;
     
     public double derivativeConstantAngle;
     public double proportionalConstantAngle;
+    
     public Telemetry telemetry = null;
+    
+    private boolean resettingImu = false;
+    private double AutoStartAngle = 0;
+    
+    private double previousLoopTime, secondPreviousLoopTime;
+    
+    public void init(HardwareMap hardwareMap, double x, double y, double angle){
+        driveTrain = new MecanumDriveTrain(hardwareMap);
+        odometry = new Odometry(x,y,angle,telemetry);
+    }
+    
+    public double getDeltaTime(){
+        double deltaTime = Math.abs(secondPreviousLoopTime-previousLoopTime);
+        secondPreviousLoopTime = previousLoopTime;
+        previousLoopTime = runtime.nanoseconds();
+        return deltaTime;
+    }
+    
+    public void IMUReset() {
+        odometry.IMUReset();
+        anglePID.setTarget(0);
+    }
     
     private double lastErrorAngle;
     private boolean IsProgramAutonomous;
     
-    public void init(HardwareMap hardwareMap, double robotX, double robotY, double robotAngle){
-        driveTrain = new MecanumDriveTrain(hardwareMap);
-        odometry = new Odometry(robotX,robotY,robotAngle, telemetry);
+    public double radiansToDegrees(double radians){
+        return radians*(180/Math.PI);
     }
-
-    public void ProportionalFeedbackControl() {
-        double currentTime = runtime.time();
-        double derivativeAngle;
-        double error = 0;
-        if (odometry.isResetingIMU())
+    
+    public double degreesToRadians(double degrees){
+        return degrees*(Math.PI/180);
+    }
+    
+    public void turnUpdate() {
+        if (resettingImu){
             return;
-        telemetry.addData("target", TargetAngle);
-        error = Wrap((TargetAngle/180)*Math.PI - odometry.getRobotAngle())*180/Math.PI;
-        derivativeAngle = (error - lastErrorAngle)/(currentTime - lastTimeAngle);
-
-        TargetAngle = (odometry.getRobotAngle() * 180 / Math.PI);
-
-        telemetry.addData("ERROR", error);
-        telemetry.addData("BEFORE", turn);
-        turn -= error * proportionalConstantAngle + (derivativeConstantAngle * derivativeAngle);
-        telemetry.addData("AFTER", turn);
-        lastTimeAngle = currentTime;
-        lastErrorAngle = error;
-    }
-    public void IMUReset() {
-        TargetAngle = 0;
-        odometry.IMUReset();
+        }
+        anglePID.setTarget(odometry.getRobotAngle());
+        anglePID.update(odometry.getRobotAngle());
+        
     }
     
     double Wrap(double angle) {
@@ -80,4 +101,7 @@ public class Robot {
         }
         return angle;
     }
+    
+    
+    
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 public class MainTeleOp extends BaseTeleOp {
     @Override
     public void runOpMode() throws InterruptedException {
-        final float speedMultiplier = .40;
+        final double speedMultiplier = 0.40;
         initialize();
         waitForStart();
         while (opModeIsActive()) {
@@ -18,18 +18,18 @@ public class MainTeleOp extends BaseTeleOp {
             } else{
                 robot.driveTrain.setSpeedMultiplier(1);
 
-            robot.IMU_Update();
+            robot.odometry.updateOdometry();
             double target = robot.anglePID.getTarget() + (-gamepad1.right_stick_x * robot.turningInputConstant * robot.getDeltaTime());
             robot.anglePID.setTarget(target);
-            robot.anglePID.update(robot.getRobotAngle());
-            robot.driveTrain.setDrivePower(-gamepad1.left_stick_y, gamepad1.left_stick_x, robot.anglePID.getPower(), robot.getRobotAngle());
-            telemetry.addData("Robot Angle",robot.getRobotAngle());
+            robot.anglePID.update(robot.odometry.getRobotAngle());
+            robot.driveTrain.setDrivePower(-gamepad1.left_stick_y, gamepad1.left_stick_x, robot.anglePID.getPower(), robot.odometry.getRobotAngle());
+            telemetry.addData("Robot Angle",robot.odometry.getRobotAngle());
             telemetry.addData("Target Angle",target);
             telemetry.addData("Robot Power",robot.anglePID.getPower());
             telemetry.update();
 
             }
-            robot.driveTrain.setDrivePower(-gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x);
+            robot.driveTrain.setDrivePower(-gamepad1.left_stick_y,gamepad1.left_stick_x,gamepad1.right_stick_x,robot.odometry.getRobotAngle());
         }
     }
 }
