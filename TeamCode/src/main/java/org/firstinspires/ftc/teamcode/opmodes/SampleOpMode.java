abdfe417d28544ccdf5ba64961feebb7c738a6d7
andrew-cua <andrew@cuafamily.com>
2024-10-11 20:17:31 -0400

    Adds roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleOpMode.java
@@ -10,7 +10,7 @@ import com.arcrobotics.ftclib.gamepad.GamepadEx;
 public class SampleOpMode extends CommandOpMode {
     // GamepadEx is a better version of the gamepad object you are used to.
     // You can setup command to be ran on button press, release, or hold
-    // you also don't have to invert the value from the triggers and sticks. 
+    // you also don't have to invert the value from the triggers and sticks.
     private GamepadEx driver;
     private GamepadEx operator;
 
