38f688617134c416ca137f5e109883ae7f54df9c
Xendy <xendyexe@gmail.com>
2024-10-13 11:37:01 -0700

    10/13/2024 - Xendy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -55,9 +55,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.068;
-    public static double kA = 0.008;
-    public static double kStatic = 0.004;
+    public static double kV = 0.049;
+    public static double kA = 0.002;
+    public static double kStatic = 0.005;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
