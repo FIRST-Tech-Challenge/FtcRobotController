55a3c7ba8fc910a0d77b044f0b61760d191c0255
bakedyouapie <148584429+bakedyouapie@users.noreply.github.com>
2024-10-13 19:47:44 -0400

    Update RobotAutoDriveToAprilTagMecanum.java
    Substitution

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveToAprilTagMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAutoDriveToAprilTagMecanum.java
@@ -187,10 +187,11 @@ public class RobotAutoDriveToAprilTagMecanum extends LinearOpMode
      */
     public void moveRobot(double x, double y, double yaw) {
         // Calculate wheel powers.
-        double leftFrontPower    =  x -y -yaw;
-        double rightFrontPower   =  x +y +yaw;
-        double leftBackPower     =  x +y -yaw;
-        double rightBackPower    =  x -y +yaw;
+        double frontLeftPower = (y + x + turn) / denominator;
+        double backLeftPower = (y - x + turn) / denominator;
+        double frontRightPower = (y - x - turn) / denominator;
+        double backRightPower = (y + x - turn) / denominator;
+
 
         // Normalize wheel powers to be less than 1.0
         double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
