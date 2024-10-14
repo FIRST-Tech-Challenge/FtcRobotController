052727aad95ffd92db8de7aae25daf8e369c5dc7
welu28 <151058854+welu28@users.noreply.github.com>
2024-10-14 15:23:33 -0400

    2 lift instead of 1 lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Lift.java
@@ -1,25 +1,34 @@
 package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Lift {
-    public DcMotor lift;
+    public DcMotor lift1;
+    public DcMotor lift2;
 
     public void init(HardwareMap map) {
-        lift = map.dcMotor.get("Lift");
-        lift.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift1 = map.dcMotor.get("Lift1");
+        lift2 = map.dcMotor.get("Lift2");
+        lift1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lift2.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void moveCC() {
-        lift.setPower(-0.69);
+        lift1.setPower(-1);
+        lift2.setPower(-1);
     }
 
     public void moveCW() {
-        lift.setPower(0.69);
+        lift2.setPower(1);
+        lift1.setPower(1);
     }
 
     public void stop() {
-        lift.setPower(0.0);
+        lift1.setPower(0);
+        lift2.setPower(0);
+
     }
 }
