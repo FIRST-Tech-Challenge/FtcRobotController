72bd8f569d276d3a3012a3868d25eb73e25f74de
manavch06 <manavch06@gmail.com>
2024-10-14 16:18:49 -0400

    compartmentalize
    moves drive mech outside of main op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveMech.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+public class driveMech {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeftMotor  = hardwareMap.dcMotor.get("frontLeftMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+
+        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        frontRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backLeftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        backRightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void setPower(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+    }
+
+    public void drive(double forward, double strafe, double rotate) {
+        double frontLeft = Range.clip(forward + strafe + rotate, -1.0, 1.0);
+        double frontRight = Range.clip(forward - strafe - rotate, -1.0,1.0);
+        double backLeft = Range.clip(forward - strafe + rotate, -1.0, 1.0);
+        double backRight = Range.clip(forward + strafe - rotate, -1.0, 1.0);
+        setPower(frontLeft, frontRight, backLeft, backRight);
+    }
+}
