25ec97202fe296f7cfb76b41c2eb465dbf5abe57
MochaSm <130510590+MochaSm@users.noreply.github.com>
2024-10-15 21:14:26 -0700

    Functional linearOpMode.java
    Mecanum drive instead of random code snippets

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearOpMode.java
@@ -1,112 +1,89 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import static java.lang.Math.sqrt;
-
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Gamepad;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-//import org.firstinspires.ftc.teamcode.components.device.GamepadKt;
-//import org.firstinspires.ftc.teamcode.components.meta.Hardware;
-//import org.firstinspires.ftc.teamcode.components.meta.MotorGroup;
 
-@TeleOp(name = "DriveTrainTest1 (Blocks to Java)")
+@TeleOp(name = "Mecanum Drive")
 public class linearOpMode extends LinearOpMode {
+    private DcMotor frontLeftMotor = null, backLeftMotor = null;
+    private DcMotor frontRightMotor = null, backRightMotor = null;
 
-  private DcMotor leftBack;
-  private DcMotor leftFront;
-  private DcMotor rightBack;
-  private DcMotor rightFront;
-  private Servo claw;
 
-  /**
-   * This sample contains the bare minimum Blocks for any regular OpMode. The 3 blue
-   * Comment Blocks show where to place Initialization code (runs once, after touching the
-   * DS INIT button, and before touching the DS Start arrow), Run code (runs once, after
-   * touching Start), and Loop code (runs repeatedly while the OpMode is active, namely not
-   * Stopped).
-   */
-  @Override
-  public void runOpMode() {
-    leftBack = hardwareMap.get(DcMotor.class, "leftBack");
-    leftFront = hardwareMap.get(DcMotor.class, "leftFront");
-    rightBack = hardwareMap.get(DcMotor.class, "rightBack");
-    rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-    claw = hardwareMap.get(Servo.class, "claw");
 
-    // Put initialization blocks here.
-    waitForStart();
-    if (opModeIsActive()) {
-      // Put run blocks here.
-      while (opModeIsActive()) {
-//        leftBack.setPower(-gamepad1.left_stick_y);
-//        leftFront.setPower(-gamepad1.left_stick_y);
-//        rightBack.setPower(gamepad1.right_stick_y);
-//        rightFront.setPower(gamepad1.right_stick_y);
-//        telemetry.update();
 
-          drive(gamepad1, 1);
-      }
+    @Override
+    public void runOpMode(){
+
+
+
+
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "leftFront");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "rightFront");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "leftBack");
+        backRightMotor = hardwareMap.get(DcMotor.class, "rightBack");
+
+
+        frontLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
+        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+
+
+
+        waitForStart();
+
+
+        if (isStopRequested()) return;
+
+
+        while (opModeIsActive()) {
+
+
+            double y = -gamepad1.left_stick_y;
+            double x = -gamepad1.left_stick_x ;
+            double turn = gamepad1.right_stick_x;
+
+
+            //input: theta and power
+            //theta is where we want the direction the robot to go
+            //power is (-1) to 1 scale where increasing power will cause the engines to go faster
+            double theta = Math.atan2(y,x);
+            double power = Math.hypot(x,y);
+            double sin = Math.sin(theta - Math.PI/4);
+            double cos = Math.cos(theta - Math.PI/4);
+            //max variable allows to use the motors at it's max power with out disabling it
+            double max = Math.max(Math.abs(sin),Math.abs(cos));
+
+
+            double leftFront = power * cos/max + turn;
+            double rightFront = power * cos/max - turn;
+            double leftRear = power * sin/max + turn;
+            double rightRear = power * sin/max - turn;
+
+
+
+
+            //Prevents the motors exceeding max power thus motors will not seize and act sporadically
+            if ((power + Math.abs(turn))>1){
+                leftFront /= power  + turn;
+                rightFront /= power  - turn;
+                leftRear /= power  + turn;
+                rightRear /= power  - turn;
+            }
+
+
+
+
+            frontLeftMotor.setPower(leftFront);
+            backLeftMotor.setPower(leftRear);
+            frontRightMotor.setPower(rightFront);
+            backRightMotor.setPower(rightRear);
+        }
     }
-  }
-
-  // drive code stolen from Anthony
-  public void drive(Gamepad gamepad, double powerMulti) {
-    double[] driveSticks = GamepadKt.getDriveSticks(gamepad);
-    double x = driveSticks[0];
-    double y = driveSticks[1];
-    double r = driveSticks[2];
-
-    double theta = Math.atan2(y, x);
-    double power = Math.min(Math.hypot(x, y), 1.0);
-
-    double xComponent = power * Math.cos(theta - Math.PI / 4);
-    double yComponent = power * Math.sin(theta - Math.PI / 4);
-    double[] arr = {Math.abs(xComponent), Math.abs(yComponent), Math.abs(1e-16)}; // ? untested
-    double max = largest(arr);
-
-    double[] powers = {
-            power * (xComponent / max) + r,
-            power * (yComponent / max) - r,
-            power * (yComponent / max) + r,
-            power * (xComponent / max) - r
-    };
-
-
-//        if (power + Math.abs(r) > 1) {
-//            for (int i = 0; i < powers.length; i++) {
-//                powers[i] /= (power + Math.abs(r));
-//            }
-//        }
-
-    double _powerMulti = !GamepadKt.isAnyJoystickTriggered(gamepad) ? 0.0 : powerMulti;
-
-    for (int i = 0; i < powers.length; i++) {
-      powers[i] = Math.pow(powers[i], 3.0) * _powerMulti;
-    }
-
-    //powers = reduce(powers);
-
-    double[] finalPowers = powers;
-    motorGroup.applyToMotors(motor -> motor.setPower(finalPowers[motorGroup.motorIndex]));
-//        telemetry.addData("FL", frontLeft.getPower());
-//        telemetry.addData("FR", frontRight.getPower());
-//        telemetry.addData("BL", backLeft.getPower());
-//        telemetry.addData("BR", backRight.getPower());
-//        telemetry.addData("FLF", powers[0]);
-//        telemetry.addData("FRF", powers[1]);
-//        telemetry.addData("BLF", powers[2]);
-//        telemetry.addData("BRF", powers[3]);
-//        telemetry.update();
-
-  }
-
 }
+
