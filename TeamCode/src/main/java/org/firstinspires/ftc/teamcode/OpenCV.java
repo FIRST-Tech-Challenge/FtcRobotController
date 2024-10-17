a34fa09b16fa61dbc0822d54797a98a174f1aca1
Dakspieth <jacob_bancroft@students.nksd.net>
2024-10-17 15:32:24 -0400

    OpenCV stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV.java
@@ -0,0 +1,94 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import  org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvPipeline;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@Autonomous
+public class OpenCV extends OpMode {
+
+    OpenCvWebcam webcam1 = null;
+
+    @Override
+    public void init(){
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam1");
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId","id", hardwareMap.appContext.getPackageName());
+        webcam1 = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+
+        webcam1.setPipeline(new PipeLine()); // might wanna rename this eventually but womp womp
+
+        webcam1.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            public void onOpened() {
+                webcam1.startStreaming(640, 360, OpenCvCameraRotation.UPRIGHT); // setting screen to 640 by 360 pixels and orientation is upright
+            }
+
+            public void onError(int errorCode) {
+
+            }
+        });
+    }
+
+    @Override
+    public void loop(){
+
+    }
+
+    class PipeLine extends OpenCvPipeline {
+
+        // We're setting up bounding boxes for the camera to detect things
+        Mat YCbCr = new Mat();
+        Mat leftCrop;
+        Mat rightCrop;
+        double leftavgfin;
+        double rightavgfin;
+        Mat outPut = new Mat();
+        Scalar rectColor = new Scalar(255.0, 0.0, 0.0);
+
+        public Mat processFrame(Mat input) {
+
+            Imgproc.cvtColor(input, YCbCr, Imgproc.COLOR_RGB2YCrCb);
+            telemetry.addLine("pipeline running");
+
+            Rect leftRect = new Rect(1, 1, 319, 359);
+            Rect rightRect = new Rect(320, 1, 319, 359);
+
+            input.copyTo(outPut);
+            Imgproc.rectangle(outPut,leftRect, rectColor, 3);
+            Imgproc.rectangle(outPut, rightRect, rectColor, 3);
+
+            leftCrop = YCbCr.submat(leftRect);
+            rightCrop = YCbCr.submat(rightRect);
+
+            Core.extractChannel(leftCrop, leftCrop,2);
+            Core.extractChannel(rightCrop, rightCrop, 2);
+
+            Scalar leftavg = Core.mean(leftCrop);
+            Scalar rightavg = Core.mean(rightCrop);
+
+            leftavgfin = leftavg.val[0];
+            rightavgfin = rightavg.val[0];
+
+            if (leftavgfin > rightavgfin) {
+                telemetry.addLine("Left");
+            } else {
+                telemetry.addLine("Right");
+            }
+
+
+            return(outPut);
+
+        }
+    }
+}
