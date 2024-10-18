b101a9bc40eaddd9cbcc0a8c7829f9303127a4b0
Joseph <jingshunceng@gmail.com>
2024-10-18 15:44:14 -0700

    Lift Tester and lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -1,20 +1,35 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.acmerobotics.roadrunner.ftc.Encoder;
+import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
+import com.acmerobotics.roadrunner.ftc.RawEncoder;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class Lift extends LinearOpMode{
-    DcMotor lyft;
-    @Override
-    public void runOpMode() {
-        lyft = hardwareMap.get(DcMotor.class, "lyft");
+public class Lift{
+    public int Lift_SPEED = 20;
+    private DcMotor lift;
+    private Encoder encoder;
 
-        waitForStart();
-//        while(opModeIsActive()){}
-        while(!isStopRequested()){
-            lyft.setPower(gamepad1.right_trigger);
-            telemetry.addData("Lift Position", lyft.getCurrentPosition());
-            telemetry.update();
-        }
+    public Lift(HardwareMap hw){
+        this(hw, "lift", "encoder");
+    }
+    /**
+     * Primary constructor for the Lift Subsystem Class
+     * @param hw [HardwareMap] Hardware map necessary to initialize motors.
+     * @param nameLift [String] Name of the lift motor assigned in the configuration.
+     * @param nameEncoder [String] Name of the encoder assigned in the configuration.
+     */
+    public Lift(HardwareMap hw, String nameLift, String nameEncoder){
+        this.lift = hw.get(DcMotor.class, nameLift);
+        encoder = new OverflowEncoder(new RawEncoder(hw.get(DcMotorEx.class, nameEncoder)));
+    }
+    public void moveLift(double power){
+        lift.setPower(power);
+    }
+    public int getPosition(){
+        return encoder.getPositionAndVelocity().position;
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LiftTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/LiftTester.java
@@ -1,9 +1,31 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
+import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
 
 public class LiftTester {
-    Lift lyft = new Lift();
+    private Lift lift;
+    private GamepadEvents controller;
+    public void runOpMode(){
+        lift = new Lift(hardwareMap);
+        telemetry.addData("Status","Initialized");
+        telemetry.update();
+        while(linearOpMode.opModeIsActive()){
+            double liftPower = controller.right_trigger.getTriggerValue();
+            lift.moveLift(liftPower);
+
+            telemetry.addData("Lift Power", liftPower);
+            telemetry.addData("Lift Position", lift.getPosition());
+            telemetry.update();
+        }
+    }
 
 }
