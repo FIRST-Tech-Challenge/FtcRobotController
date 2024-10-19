5f20a22457a579f82700394b424bf09e9e6948fe
Eric Zhang <ericbkzhang@gmail.com>
2024-10-19 00:26:31 -0400

    Add getters and setters for autoSleepEnabled in Hardware, add an Enum for Motor type, and a calculation for the number of ticks per degree of arm rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareSystems/ExtendableArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardwareSystems/ExtendableArm.java
@@ -10,16 +10,25 @@ public class ExtendableArm extends Arm {
     private final DcMotor ROTATION_MOTOR;
     // The motor power that the arm uses when rotating.
     private final double rotationPower = 1.0;
-    private final static double TICKS_PER_ROTATION_DEGREE = 1440.0 / 360.0;
-    // The maximum rotation of the arm in ticks.
-    private final static int MAX_ROTATION = 1000;
+    // How many ticks it takes to rotate the arm by one degree.
+    private final static double TICKS_PER_ROTATION_DEGREE =
+            // Base number of ticks per degree
+            Motor.TETRIX_TORQUENADO.getTicksPerRotation() / 360.0
+            // Gear ratio between motor and arm
+            * 120.0 / 40.0;
+
+    /*
+     * The maximum rotation of the arm in ticks.
+     * Theoretically, 90 degrees.
+     */
+    private final static int MAX_ROTATION = 1080;
     // The minimum rotation of the arm in ticks.
     private final static int MIN_ROTATION = 0;
 
     // The motor that extends and retracts the arm.
     private final DcMotor EXTENSION_MOTOR;
     // The motor power that the arm uses when rotating.
-    private static double extensionPower = 1.0;
+    private final static double EXTENSION_POWER = 1.0;
     // The maximum extension of the arm in ticks.
     private final static int MAX_EXTENSION = 1000;
     // The minimum extension of the arm in ticks.
@@ -32,18 +41,18 @@ public class ExtendableArm extends Arm {
     // The servo that rotates the claw about the Z-axis
     private final Servo CLAW_Z_SERVO;
     // How much to gradually move the servo.
-    private final static double servoIncrement = 0.1;
+    private final static double SERVO_INCREMENT = 0.1;
 
     // The servo that opens and closes the grip.
     private final CRServo INTAKE_SERVO;
-    private static double intakePower = 0.5;
-    private static double ejectPower = -1.0;
+    private final static double INTAKE_POWER = 0.5;
+    private final static double EJECT_POWER = -1.0;
 
     /**
      * Instantiate the motors using HashMaps.
      * Each key should be the name of the motor as defined by the configuration.
      * Note that the provided keys should be replaced with the current season's.
-     * 
+     *
      * @param motors A HashMap with motor names as keys and `DcMotor`s as values.
      * @param servos A HashMap with servo names as keys and `Servo`s as values.
      */
@@ -61,7 +70,7 @@ public class ExtendableArm extends Arm {
 
     /**
      * Instantiate each motor and servo individually.
-     * 
+     *
      * @param rotationMotor  The motor that rotates the arm up and down.
      * @param extensionMotor The motor that extends and retracts the arm.
      * @param clawXServo     The servo that rotates the claw about the X-axis.
@@ -70,22 +79,22 @@ public class ExtendableArm extends Arm {
      * @param intakeServo    The servo that opens and closes the claw.
      */
     public ExtendableArm(DcMotor rotationMotor, DcMotor extensionMotor, Servo clawXServo, Servo clawYServo,
-            Servo clawZServo, CRServo intakeServo) {
+                         Servo clawZServo, CRServo intakeServo) {
         super();
-        
+
         /* Motors */
         this.ROTATION_MOTOR = rotationMotor;
         this.EXTENSION_MOTOR = extensionMotor;
 
         super.MOTORS.add(rotationMotor);
         super.MOTORS.add(extensionMotor);
-        
+
         /* Servos */
         this.CLAW_X_SERVO = clawXServo;
         this.CLAW_Y_SERVO = clawYServo;
         this.CLAW_Z_SERVO = clawZServo;
         this.INTAKE_SERVO = intakeServo;
-        
+
         super.SERVOS.add(clawXServo);
         super.SERVOS.add(clawYServo);
         super.SERVOS.add(clawZServo);
@@ -140,7 +149,6 @@ public class ExtendableArm extends Arm {
     }
 
     /**
-     *
      * @param direction The direction that the extension motor moves.
      *                  Positive values extend the arm, negative values retract it.
      */
@@ -150,55 +158,58 @@ public class ExtendableArm extends Arm {
             return;
         }
 
-        EXTENSION_MOTOR.setPower(Math.signum(direction) * extensionPower);
+        EXTENSION_MOTOR.setPower(Math.signum(direction) * EXTENSION_POWER);
     }
 
     public void extendArmToPosition(int targetPosition) {
         EXTENSION_MOTOR.setTargetPosition(targetPosition);
         int direction = (int) Math.signum(targetPosition - EXTENSION_MOTOR.getCurrentPosition());
-        EXTENSION_MOTOR.setPower(direction * extensionPower);
+        EXTENSION_MOTOR.setPower(direction * EXTENSION_POWER);
         EXTENSION_MOTOR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     /**
      * Rotate the X rotation servo.
+     *
      * @param direction The direction to rotate the servo in.
      *                  Positive values rotate it clockwise, negative values rotate it counterclockwise.
      */
     public void rotateClawXServo(double direction) {
         double targetPosition = CLAW_X_SERVO.getPosition()
-                                + Math.signum(direction) * servoIncrement;
+                + Math.signum(direction) * SERVO_INCREMENT;
         CLAW_X_SERVO.setPosition(targetPosition);
     }
 
     /**
      * Rotate the Y rotation servo.
+     *
      * @param direction The direction to rotate the servo in.
      *                  Positive values rotate it clockwise, negative values rotate it counterclockwise.
      */
     public void rotateClawYServo(double direction) {
         double targetPosition = CLAW_Z_SERVO.getPosition()
-                                + Math.signum(direction) * servoIncrement;
+                + Math.signum(direction) * SERVO_INCREMENT;
         CLAW_Y_SERVO.setPosition(targetPosition);
     }
 
     /**
      * Rotate the Z rotation servo.
+     *
      * @param direction The direction to rotate the servo in.
      *                  Positive values rotate it clockwise, negative values rotate it counterclockwise.
      */
     public void rotateClawZServo(double direction) {
         double targetPosition = CLAW_Z_SERVO.getPosition()
-                                + Math.signum(direction) * servoIncrement;
+                + Math.signum(direction) * SERVO_INCREMENT;
         CLAW_Z_SERVO.setPosition(targetPosition);
     }
 
-    public boolean intakeActive() {
+    public boolean isIntakeActive() {
         return INTAKE_SERVO.getPower() != 0;
     }
 
     public void startIntake() {
-        INTAKE_SERVO.setPower(intakePower);
+        INTAKE_SERVO.setPower(INTAKE_POWER);
     }
 
     public void stopIntake() {
@@ -209,6 +220,6 @@ public class ExtendableArm extends Arm {
      * Make the intake spin in reverse and eject the object.
      */
     public void ejectIntake() {
-        INTAKE_SERVO.setPower(ejectPower);
+        INTAKE_SERVO.setPower(EJECT_POWER);
     }
 }
