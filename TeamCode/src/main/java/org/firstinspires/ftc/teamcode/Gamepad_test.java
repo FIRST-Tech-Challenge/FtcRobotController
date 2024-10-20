ff3bf25ce9d0bd56741d77cbe083c83b7ffe948e
dylanisyess <dylanlin2009@gmail.com>
2024-10-20 15:11:53 -0700

    Update Gamepad_test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test.java
@@ -99,8 +99,8 @@ public class Gamepad_test extends LinearOpMode {
 
             // Tank Mode uses one stick to control each wheel.
             // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            leftPower  = gamepad1.left_stick_y ;
-            rightPower = gamepad1.right_stick_y ;
+            leftPower  = -gamepad1.left_stick_y ;
+            rightPower = -gamepad1.right_stick_y ;
 
             // Send calculated power to wheels
             leftDrive.setPower(leftPower);
