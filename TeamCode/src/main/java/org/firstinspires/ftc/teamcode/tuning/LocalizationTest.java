b6fb1c8ed8b020a1ec8b3dba61b964bb11a015ff
Slogicc <136170772+Slogicc@users.noreply.github.com>
2024-10-20 14:11:30 -0500

    cleaned up code further

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/LocalizationTest.java
@@ -8,7 +8,6 @@ import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.Drawing;
 import org.firstinspires.ftc.teamcode.autoshellclasses.MecanumDrive;
 
 public class LocalizationTest extends LinearOpMode {
