bb23268e67fe99b69afd4c1f1b9bd10be904be38
Slogicc <136170772+Slogicc@users.noreply.github.com>
2024-10-20 13:42:56 -0500

    cleaned up code further

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/AutonomousShell1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/AutonomousShell1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autoshellclasses;
 import androidx.annotation.NonNull;
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/BlueRightProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/BlueRightProcessor.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.autoshellclasses;
 
 
 import android.graphics.Bitmap;
