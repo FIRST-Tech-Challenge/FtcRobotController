eed7daf0688090c65ce00939fb6ff48c49af1d4c
dylanisyess <dylanlin2009@gmail.com>
2024-10-20 11:42:14 -0700

    added the basic things needed to sense the angle of the joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test2.java
@@ -36,6 +36,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 
+
 /*
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
@@ -85,25 +86,25 @@ public class Gamepad_test2 extends LinearOpMode {
             // Setup a variable for each drive wheel to save power level for telemetry
             double leftPower;
             double rightPower;
+            double leftAngle;
+            double rightAngle;
+            double leftdirection;
+            double rightdirection;
 
-            // Choose to drive using either Tank Mode, or POV Mode
-            // Comment out the method that's not used.  The default below is POV.
+            leftPower  = -gamepad1.left_stick_y ;
+            rightPower = -gamepad1.right_stick_y ;
+            leftAngle  = Math.atan(gamepad1.left_stick_y/gamepad1.left_stick_x);
+            rightAngle = Math.atan(gamepad1.right_stick_y/gamepad1.right_stick_x);
+            leftdirection = (leftAngle * 0.00555555555);
+            rightdirection = (rightAngle * 0.00555555555);
 
-            // POV Mode uses left stick to go forward, and right stick to turn.
-            // - This uses basic math to combine motions and is easier to drive straight.
-            double drive = -gamepad1.left_stick_y;
-            double turn  =  gamepad1.right_stick_x;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-            // Tank Mode uses one stick to control each wheel.
-            // - This requires no math, but it is hard to drive forward slowly and keep straight.
-            // leftPower  = -gamepad1.left_stick_y ;
-            // rightPower = -gamepad1.right_stick_y ;
 
             // Send calculated power to wheels
-            leftDrive.setPower(leftPower);
-            rightDrive.setPower(rightPower);
+            //leftDrive.setPower(leftPower);
+            //rightDrive.setPower(rightPower);
+            //servoLeft.setposition(leftAngle);
+            //servoRight.setposition(rightAngle);
+
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
