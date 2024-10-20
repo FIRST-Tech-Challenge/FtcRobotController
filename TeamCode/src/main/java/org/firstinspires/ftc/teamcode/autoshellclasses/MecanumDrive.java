9ea0b6a0519df7936a8528a560a62dd5d45481c9
Slogicc <136170772+Slogicc@users.noreply.github.com>
2024-10-20 16:45:18 -0500

    cleaned up code further

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autoshellclasses/MecanumDrive.java
@@ -61,7 +61,7 @@ public final class MecanumDrive {
         // TODO: fill in these values based on
         //   see https://ftc-docs.firstinspires.org/en/latest/programming_resources/imu/imu.html?highlight=imu#physical-hub-mounting
         public RevHubOrientationOnRobot.LogoFacingDirection logoFacingDirection =
-                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT;
+                RevHubOrientationOnRobot.LogoFacingDirection.UP;
         public RevHubOrientationOnRobot.UsbFacingDirection usbFacingDirection =
                 RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
 
