6ddbb7f558907b67324d18789a470a31cb9934c6
Alan Huang <alan.wenhao.huang@gmail.com>
2024-10-20 15:50:12 -0700

    trying to add sparkfun otos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alan/OdometryTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alan/OdometryTest.java
@@ -1,7 +1,7 @@
 
 package org.firstinspires.ftc.teamcode.Alan;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+//import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
