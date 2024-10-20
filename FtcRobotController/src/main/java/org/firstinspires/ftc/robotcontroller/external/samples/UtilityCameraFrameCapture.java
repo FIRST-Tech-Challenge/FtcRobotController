a8e5ac8130dc28f80dedd4c1e5822a967cbf397b
Alan Huang <alan.wenhao.huang@gmail.com>
2024-10-19 19:07:55 -0700

    VISION CODE THAT WORKS YES

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/UtilityCameraFrameCapture.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/UtilityCameraFrameCapture.java
@@ -64,7 +64,7 @@ public class UtilityCameraFrameCapture extends LinearOpMode
     /*
      * EDIT THESE PARAMETERS AS NEEDED
      */
-    final boolean USING_WEBCAM = false;
+    final boolean USING_WEBCAM = true;
     final BuiltinCameraDirection INTERNAL_CAM_DIR = BuiltinCameraDirection.BACK;
     final int RESOLUTION_WIDTH = 640;
     final int RESOLUTION_HEIGHT = 480;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.util.Log;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Mat;
@@ -19,23 +22,42 @@ public class TestAuto extends LinearOpMode {
     OpenCvWebcam webcam;
     private TestDetector detector;
     private String position;
+    private double LEFT_CR_AVG;
+
+    ElapsedTime elapsedTime;
 
     @Override
     public void runOpMode() {
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        elapsedTime = new ElapsedTime();
 
+        detector = new TestDetector();
         webcam.setPipeline(detector);
         webcam.openCameraDevice();
         webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
 
-        detector = new TestDetector();
         position = detector.position;
 
-        while (!isStarted()) {
+        while (!isStarted() || opModeIsActive()) {
             position = detector.position;
-            telemetry.addData("position", position);
+            LEFT_CR_AVG = detector.avgLeftCr;
+            telemetry.addData("position: ", position);
+            telemetry.update();
+            telemetry.addLine(String.valueOf(LEFT_CR_AVG));
+            telemetry.addLine(String.valueOf(detector.leftCrTotal));
+
+            if (elapsedTime.milliseconds() >= 2000) {
+                telemetry.addData("position: ", position);
+                telemetry.addLine(String.valueOf(LEFT_CR_AVG));
+                telemetry.addLine(String.valueOf(detector.leftCrTotal));
+                Log.v("Vision", position);
+                telemetry.update();
+                elapsedTime.reset();
+            }
         }
 
     }
+
+
 }
