978ac26f52186ccb81d16da756bdae2c6070f6f4
coooIIIII <satviktherobo@gmail.com>
2024-10-21 16:03:13 -0400

    Update WolfTech_TeleopPOV_Linear.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WolfTech_TeleopPOV_Linear.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WolfTech_TeleopPOV_Linear.java
@@ -108,7 +108,7 @@ public class WolfTech_TeleopPOV_Linear extends LinearOpMode {
 
             // Normalize the values so neither exceed +/- 1.0
             max = Math.max(Math.abs(left), Math.abs(right));
-            if (max > 0.5)
+            if (max > 0.2)
             {
                 left /= max;
                 right /= max;
