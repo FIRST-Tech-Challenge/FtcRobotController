24b2dc942d301205e67c636e8da8a24648cffd0e
aiden <sourapple9@outlook.com>
2024-10-20 17:58:18 -0700

    [untested] PID framework

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/DriveTrain.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.PID;
+
+public class DriveTrain {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PID/Odometry.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.PID;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+
+public class OTOS {
+    private final SparkFunOTOS otos;
+
+    public OTOS(DriveTrain driveTrain) {
+        otos = driveTrain.getOtos();
+
+        // (incomplete) config; offset depends on where we mount, scalars need tuning
+        otos.setAngularUnit(AngleUnit.RADIANS);
+        otos.setOffset(new SparkFunOTOS.Pose2D(0, 0, 0));
+        otos.setLinearScalar(1.0);
+        otos.setAngularScalar(1.0);
+
+        otos.calibrateImu();
+        otos.resetTracking();
+    }
+
+    public SparkFunOTOS.Pose2D getPosition() {
+        return otos.getPosition();
+    }
+}
