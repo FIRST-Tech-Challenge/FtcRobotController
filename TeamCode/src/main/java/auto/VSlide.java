24919a499800b49ab698284169f0fefe0fa40917
LianAustin <austin.lian25@ycdsbk12.ca>
2024-10-22 15:37:58 -0400

    Implemented VSlide class

--- TeamCode/src/main/java/auto/VSlide.java
+++ TeamCode/src/main/java/auto/VSlide.java
@@ -1,6 +1,7 @@
 package auto;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class VSlide extends MainTest {
 
@@ -11,14 +12,18 @@ public class VSlide extends MainTest {
 
     public void goToPosition(int position) {
         // Code to move V-Slide to specified position
+        motor.setTargetPosition(position);
     }
 
     @Override
     public void resetPosition() {
         // Code to reset V-Slide position using touch sensor
+        motor.setDirection(DcMotorSimple.Direction.REVERSE);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     public void goToPosition(double position) {
         // Code to move V-Slide to specified position
+        motor.setTargetPosition((int) position);
     }
 }
