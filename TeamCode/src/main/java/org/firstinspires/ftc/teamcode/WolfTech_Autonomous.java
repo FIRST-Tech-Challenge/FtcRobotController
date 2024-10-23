321b75838b374ce1521199ed051a9bb847279def
coooIIIII <satviktherobo@gmail.com>
2024-10-23 16:38:40 -0400

    Update WolfTech_Autonomous.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WolfTech_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WolfTech_Autonomous.java
@@ -53,7 +53,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@Autonomous(name="Robot: Auto Drive By Time", group="Robot")
+@Autonomous(name="Robot: Autonomous", group="Robot")
 public class WolfTech_Autonomous extends LinearOpMode {
 
     /* Declare OpMode members. */
