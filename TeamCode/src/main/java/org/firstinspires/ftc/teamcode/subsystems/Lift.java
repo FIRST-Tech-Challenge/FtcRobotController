e34e18198ec9053e6ce76b51c3bf19e2c05fe9b9
Armaan Sidhu <armaansidhu000@icloud.com>
2024-10-22 17:19:20 -0700

    copy and pasting Joseph code into my code to fix merge issues

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.acmerobotics.roadrunner.ftc.Encoder;
+import com.acmerobotics.roadrunner.ftc.OverflowEncoder;
+import com.acmerobotics.roadrunner.ftc.RawEncoder;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class Lift{
+    public int Lift_SPEED = 20;
+    private DcMotor lift;
+    private Encoder encoder;
+    //    public Lift(HardwareMap hw){
+//        this(hw, "lift", "encoder");
+//    }
+
+    /**
+     * Primary constructor for the Lift Subsystem Class
+     * @param hw [HardwareMap] Hardware map necessary to initialize motors.
+     * @param nameLift [String] Name of the lift motor assigned in the configuration.
+     */
+    public Lift(HardwareMap hw, String nameLift){
+        lift = hw.get(DcMotor.class, nameLift);
+    }
+    public void moveLift(double power){
+        lift.setPower(power);
+    }
+//    public int getPosition(){
+//        return encoder.getPositionAndVelocity().position;
+//    }
+}
