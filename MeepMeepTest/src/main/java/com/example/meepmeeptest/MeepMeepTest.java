ad89a433ceea5288364326e60be2089d2791ff9a
Jack-Xie-Akioi <xjz22789@gmail.com>
2024-10-23 16:43:31 -0400

    Update MeepMeepTest.java

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptest;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
@@ -13,15 +14,21 @@ public class MeepMeepTest {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(90)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(48,-58, 3.14159268979/2))
+                        .lineTo(new Vector2d(48,-36))
+                        .lineTo(new Vector2d(11,-33))
+                        .lineTo(new Vector2d(58,-36))
+                        .lineTo(new Vector2d(9,-33))
+                        .lineTo(new Vector2d(69,-36))
+                        .lineTo(new Vector2d(7,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(5,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(3,-33))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(1,-33))
+                        .waitSeconds(5)
+                        .lineTo(new Vector2d(58,-58))
                         .build());
 
 
