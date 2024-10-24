b8295200b2c61cc239914b4537e2227adc025a5a
Sushant <sushindupuru@gmail.com>
2024-10-23 18:47:50 -0700

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/ServoTest.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Tests;
-
-public class ServoTest {
-}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/WristServoTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class WristServoTest extends LinearOpMode {
+    public void runOpMode(){
+
+    }
+}
