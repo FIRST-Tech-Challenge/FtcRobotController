c6c157ba55f248b5e2dfcca4243d728ee8770bcb
dylanisyess <dylanlin2009@gmail.com>
2024-10-24 22:48:51 -0700

    change the simple test code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test4.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gamepad_test4.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Simple Test", group="Linear OpMode")
+@Autonomous(name="Simple Test", group="Linear OpMode")
 public class Gamepad_test4 extends LinearOpMode {
 
     public DcMotor leftwheel = null;
@@ -19,22 +21,27 @@ public class Gamepad_test4 extends LinearOpMode {
         rightwheel = hardwareMap.get(DcMotor.class, "rightwheel");
         leftservo  = hardwareMap.get(Servo.class, "leftservo");
         rightservo = hardwareMap.get(Servo.class, "rightservo");
+        leftwheel.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
 
-        leftwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        // Move wheels forward
-//        leftwheel.setPower(0.5);
-//        rightwheel.setPower(0.5);
-//
+//        leftwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//        rightwheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+//         Move wheels forward
+        leftwheel.setPower(1.0);
+        rightwheel.setPower(1.0);
+        leftservo.setPosition(0);
+        rightservo.setPosition(0);
+
 //         Set servos to neutral
-//        leftservo.setPosition(0.5);
-//        rightservo.setPosition(0.5);
+        leftservo.setPosition(0.5);
+        rightservo.setPosition(0.5);
 //
         sleep(10000);  // Run for 2 seconds, then stop
 //
-//        leftwheel.setPower(0);
-//        rightwheel.setPower(0);
+        leftwheel.setPower(0);
+        rightwheel.setPower(0);
+        leftservo.setPosition(1);
+        rightservo.setPosition(1);
     }
 }
