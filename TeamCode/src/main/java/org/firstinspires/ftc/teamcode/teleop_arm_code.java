9593be9da3a7729b3940855127f7890c9d681132
cheesebroccoli <122124161+cheesebroccoli@users.noreply.github.com>
2024-10-25 22:32:45 -0400

    added some prelim claw code, in this case we'll have to program the servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_arm_code.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop_arm_code.java
@@ -5,7 +5,7 @@ import static com.qualcomm.robotcore.util.Range.clip;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.PowerPlay23_24.TemplateJanx;
 
@@ -16,6 +16,7 @@ public class teleop_arm_code extends LinearOpMode {
     // private motorType nameOfMotor
     private DcMotor leftMotor;
     private DcMotor rightMotor;
+    private Servo claw;
     @Override
     public void runOpMode() {
         TemplateJanx janx = new TemplateJanx(hardwareMap);
@@ -25,18 +26,30 @@ public class teleop_arm_code extends LinearOpMode {
         if (opModeIsActive()) {
             while (opModeIsActive()) {
                 //call functions here
+                arm(gamepad2.left_stick_y);
+                servo();
             }
         }
     }
     //write functions here
-    private void arm(){
-        if(gamepad2.left_stick_y > 0) {
+    private void arm(double LSY){
+        if(LSY > 0) {
          leftMotor.setPower(1);
          rightMotor.setPower(1);
         }
-        else if(gamepad2.left_stick_y < 0){
+        else if(LSY< 0){
             leftMotor.setPower(-1);
             rightMotor.setPower(-1);
+        }
+    }
+    private void servo(){
+        if(gamepad2.a)
+        {
+            claw.setPosition(1);
+        }
+        if(gamepad2.b)
+        {
+            claw.setPosition(-1);
         }
     }
 }
