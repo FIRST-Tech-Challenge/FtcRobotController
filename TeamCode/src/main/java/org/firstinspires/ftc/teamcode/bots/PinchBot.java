6623017d63138642e7ccab81cde0337d6bd4f055
pig8pig <supasupaweiwei@gmail.com>
2024-10-27 10:55:57 -0700

    pickup() done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/PinchBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/PinchBot.java
@@ -8,6 +8,13 @@ import com.qualcomm.robotcore.util.SerialNumber;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class PinchBot extends PivotBot{
+    static final double VERTICAL_OFFSET = 0;
+    static final double VERTICAL_PROPORTION = 1;
+    static final double HORIZONTAL_PROPORTION = 1;
+    static final double ROTATIONAL_PROPORTION = 1;
+    static final double CENTER_POSITION = 0;
+
+    public boolean isOpen = false;
 
     public Servo pinch;
     public Servo rotate;
@@ -22,11 +29,12 @@ public class PinchBot extends PivotBot{
         rotate = hardwareMap.get(Servo.class, "rotate");
     }
 
-    public void PinchControl(boolean open, boolean close){
-        if(open){
+    public void pinchControl(){
+        isOpen = !isOpen;
+        if(isOpen){
             pinch.setPosition(0.5);
         }
-        if(close){
+        if(!isOpen){
             pinch.setPosition(0);
         }
     }
@@ -41,8 +49,14 @@ public class PinchBot extends PivotBot{
         double x = position[0];
         double y = position[1];
         double theta = position[2];
-        
+        slideMove((int) ((y + VERTICAL_OFFSET)*VERTICAL_PROPORTION)); //move slide vertically
+        driveStraightByDistance(90, x*HORIZONTAL_PROPORTION, 2);
+        rotate(theta * ROTATIONAL_PROPORTION);
+        isOpen = false;
+        pinchControl();
 
+        slideControl(false, true);
+        rotate(CENTER_POSITION);
 
     }
 }
