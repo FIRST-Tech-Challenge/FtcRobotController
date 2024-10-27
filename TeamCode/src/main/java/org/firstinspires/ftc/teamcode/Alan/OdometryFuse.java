9e8e1111a681e3ce25021ed935757e4ac40c758b
Alan Huang <alan.wenhao.huang@gmail.com>
2024-10-27 15:26:33 -0700

    added Configuration code for OTOS and fixed some issues

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alan/OdometryFuse.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Alan/OdometryFuse.java
@@ -40,6 +40,7 @@ public class OdometryFuse extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
+            SparkFunOTOS.Pose2D pos = myOtos.getPosition();
             double forward = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
@@ -48,44 +49,67 @@ public class OdometryFuse extends LinearOpMode {
             leftBack.setPower(forward + turn - strafe);
             rightBack.setPower(forward - turn + strafe);
 
+            // Reset the tracking when needed
+            if (gamepad1.y) {
+                myOtos.resetTracking();
+            }
+
+            // Re-calibrate the IMU (for fixing issues ig)
+            if (gamepad1.x) {
+                myOtos.calibrateImu();
+            }
+            //Info things
+            telemetry.addLine("Press Y (triangle) on Gamepad to reset tracking");
+            telemetry.addLine("Press X (square) on Gamepad to calibrate the IMU");
+            telemetry.addLine();
+
+            //sprkfun update pos
+            telemetry.addData("X coordinate", pos.x);
+            telemetry.addData("Y coordinate", pos.y);
+            telemetry.addData("Heading angle", pos.h);
+
+            telemetry.addLine();
+
             telemetry.addData("Encoder Pos:", rightBack.getCurrentPosition() * INCHES_PER_TICK);
             telemetry.update();
         }
     }
-}
 
 
-@SuppressLint("DefaultLocale")
-private void configureOtos() {
-    telemetry.addLine("Configuring OTOS...");
-    telemetry.update();
 
-    myOtos.setLinearUnit(DistanceUnit.INCH);
 
-    myOtos.setAngularUnit(AngleUnit.DEGREES);
+    //configure SPARK FUN Otos
+    @SuppressLint("DefaultLocale")
+    private void configureOtos() {
+        telemetry.addLine("Configuring OTOS...");
+        telemetry.update();
 
-    SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
-    myOtos.setOffset(offset);
+        myOtos.setLinearUnit(DistanceUnit.INCH);
 
-    myOtos.setLinearScalar(1.0);
-    myOtos.setAngularScalar(1.0);
+        myOtos.setAngularUnit(AngleUnit.DEGREES);
 
-    myOtos.calibrateImu();
+        SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
+        myOtos.setOffset(offset);
 
-    myOtos.resetTracking();
+        myOtos.setLinearScalar(1.0);
+        myOtos.setAngularScalar(1.0);
 
-    SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
-    myOtos.setPosition(currentPosition);
+        myOtos.calibrateImu();
 
-    // Get the hardware and firmware version
-    SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
-    SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
-    myOtos.getVersionInfo(hwVersion, fwVersion);
+        myOtos.resetTracking();
 
-    telemetry.addLine("OTOS configured! Press start to get position data!");
-    telemetry.addLine();
-    telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
-    telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
-    telemetry.update();
-}
+        SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
+        myOtos.setPosition(currentPosition);
+
+        // Get the hardware and firmware version
+        SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
+        SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
+        myOtos.getVersionInfo(hwVersion, fwVersion);
+
+        telemetry.addLine("OTOS configured! Press start to get position data!");
+        telemetry.addLine();
+        telemetry.addLine(String.format("OTOS Hardware Version: v%d.%d", hwVersion.major, hwVersion.minor));
+        telemetry.addLine(String.format("OTOS Firmware Version: v%d.%d", fwVersion.major, fwVersion.minor));
+        telemetry.update();
+    }
 }
