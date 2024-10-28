be63235fb565a8d68f9d4f18f40504e26086011c
mikebernst17 <mbernst17@gmail.com>
2024-10-28 14:01:16 -0400

    Initial Gobilda files for Pinpoint

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveToPoint.java
@@ -0,0 +1,193 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.RADIANS;
+import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.MM;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+
+public class DriveToPoint {
+
+    private enum Direction {
+        x,
+        y,
+        h;
+    }
+
+    private static double xyTolerance = 12;
+    private static double yawTolerance = 0.0349066;
+
+    private static double pGain = 0.03;
+    private static double dGain = 0.002;
+    private static double accel = 2.0;
+
+    /*todo: make the forward/perpendicular pid values smarter. Instead of just using perpendicular for strafe. Use it for the smaller error axis, maybe a cutoff?
+       consider if this is even better than a well tuned single PID loop*/
+
+    private static double yawPGain = 5.0;
+    private static double yawDGain = 0.0;
+    private static double yawAccel = 2.0;
+
+    private DcMotor leftFrontDrive;
+    private DcMotor rightFrontDrive;
+    private DcMotor leftBackDrive;
+    private DcMotor rightBackDrive;
+
+    private ElapsedTime holdTimer = new ElapsedTime();
+    private ElapsedTime currentTime = new ElapsedTime();
+
+    private PIDLoops xPID = new PIDLoops();
+    private PIDLoops yPID = new PIDLoops();
+    private PIDLoops hPID = new PIDLoops();
+
+    private LinearOpMode myOpMode; //todo: consider if this is required
+
+    public void setXYCoefficients(double p, double d, double acceleration, DistanceUnit unit, double tolerance){
+        pGain = p;
+        dGain = d;
+        accel = acceleration;
+        xyTolerance = unit.toMm(tolerance);
+    }
+
+    public void setYawCoefficients(double p, double d, double acceleration, AngleUnit unit, double tolerance){
+        yawPGain = p;
+        yawDGain = d;
+        yawAccel = acceleration;
+        yawTolerance = unit.toRadians(tolerance);
+    }
+
+    public DriveToPoint(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    public void initializeMotors() {
+        leftFrontDrive = setupDriveMotor("frontLeft", DcMotorSimple.Direction.REVERSE);
+        leftBackDrive = setupDriveMotor("backLeft", DcMotorSimple.Direction.REVERSE);
+        rightFrontDrive = setupDriveMotor("frontRight", DcMotorSimple.Direction.FORWARD);
+        rightBackDrive = setupDriveMotor("backRight", DcMotorSimple.Direction.FORWARD);
+    }
+
+    public boolean driveTo(Pose2D currentPosition, Pose2D targetPosition, double power, double holdTime) {
+        boolean atTarget;
+        double xPWR    = calculatePID(currentPosition,targetPosition,Direction.x);
+        double yPWR    = calculatePID(currentPosition,targetPosition,Direction.y);
+        double hOutput = calculatePID(currentPosition,targetPosition,Direction.h);
+
+        double heading = currentPosition.getHeading(AngleUnit.RADIANS);
+        double cosine  = Math.cos(heading);
+        double sine    = Math.sin(heading);
+
+        double xOutput = (xPWR * cosine) + (yPWR * sine);
+        double yOutput = (xPWR * sine) - (yPWR * cosine);
+
+        driveMecanums(xOutput*power, yOutput*power, hOutput*power);
+
+        if(inBounds(currentPosition,targetPosition)){
+            atTarget = true;
+        }
+        else {
+            holdTimer.reset();
+            atTarget = false;
+        }
+
+        if (atTarget && (holdTimer.time() > holdTime)){
+            return true;
+        }
+        return false;
+    }
+
+    private void driveMecanums(double forward, double strafe, double yaw) {
+        double leftFront = forward - -strafe - yaw;
+        double rightFront = forward + -strafe + yaw;
+        double leftBack = forward + -strafe - yaw;
+        double rightBack = forward - -strafe + yaw;
+
+        double max = Math.max(Math.abs(leftFront), Math.abs(rightFront));
+        max = Math.max(max, Math.abs(leftBack));
+        max = Math.max(max, Math.abs(rightBack));
+
+        if (max > 1.0) {
+            leftFront /= max;
+            rightFront /= max;
+            leftBack /= max;
+            rightBack /= max;
+        }
+
+        leftFrontDrive.setPower(leftFront);
+        rightFrontDrive.setPower(rightFront);
+        leftBackDrive.setPower(leftBack);
+        rightBackDrive.setPower(rightBack);
+    }
+
+    private DcMotor setupDriveMotor(String deviceName, DcMotor.Direction direction) {
+        DcMotor aMotor = myOpMode.hardwareMap.get(DcMotor.class, deviceName);
+        aMotor.setDirection(direction);
+        aMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        return aMotor;
+    }
+
+
+    private double calculatePID(Pose2D currentPosition, Pose2D targetPosition, Direction direction){
+        if(direction == Direction.x){
+            double xError = targetPosition.getX(MM) - currentPosition.getX(MM);
+            return xPID.calculateAxisPID(xError, pGain, dGain, accel, currentTime.time());
+        }
+        if(direction == Direction.y){
+            double yError = targetPosition.getY(MM) - currentPosition.getY(MM);
+            return yPID.calculateAxisPID(yError, pGain, dGain, accel, currentTime.time());
+        }
+        if(direction == Direction.h){
+            double hError = targetPosition.getHeading(AngleUnit.RADIANS) - currentPosition.getHeading(AngleUnit.RADIANS);
+            return hPID.calculateAxisPID(hError, yawPGain, yawDGain, yawAccel, currentTime.time());
+        }
+        return 0;
+    }
+
+    private boolean inBounds (Pose2D currPose, Pose2D trgtPose){
+        boolean xOutOfBounds = currPose.getX(MM) > (trgtPose.getX(MM) - xyTolerance) && currPose.getX(MM) < (trgtPose.getX(MM) + xyTolerance);
+        boolean yOutOfBounds = currPose.getY(MM) > (trgtPose.getY(MM) - xyTolerance) && currPose.getY(MM) < (trgtPose.getY(MM) + xyTolerance);
+        boolean hOutOfBounds = currPose.getHeading(RADIANS) > (trgtPose.getHeading(RADIANS) - yawTolerance) &&
+                currPose.getHeading(RADIANS) < (trgtPose.getHeading(RADIANS) + yawTolerance);
+
+        return xOutOfBounds && yOutOfBounds && hOutOfBounds;
+    }
+}
+
+class PIDLoops{
+    private double previousError;
+    private double previousTime;
+    private double previousOutput;
+
+    double calculateAxisPID(double error, double pGain, double dGain, double accel, double currentTime){
+        double p = error * pGain;
+        double cycleTime = currentTime - previousTime;
+        double d = dGain * (previousError - error) / (cycleTime);
+        double output = p+d;
+        double dV = cycleTime * accel;
+
+        double max = Math.abs(output);
+        if(max > 1.0){
+            output /= max;
+        }
+
+        if((output - previousOutput) > dV){
+            output = previousOutput + dV;
+        } else if ((output - previousOutput) < -dV){
+            output = previousOutput - dV;
+        }
+
+        previousOutput = output;
+        previousError  = error;
+        previousTime   = currentTime;
+
+        return output;
+    }
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorPinpointDriveToPoint.java
@@ -0,0 +1,109 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+import java.util.Locale;
+
+@Autonomous(name="Pinpoint Navigation Example", group="Pinpoint")
+//@Disabled
+
+public class SensorPinpointDriveToPoint extends LinearOpMode {
+
+    GoBildaPinpointDriver odo; // Declare OpMode member for the Odometry Computer
+    private DriveToPoint nav = new DriveToPoint(this); //OpMode member for the point-to-point navigation class
+
+    enum StateMachine{
+        WAITING_FOR_START,
+        AT_TARGET,
+        DRIVE_TO_TARGET_1,
+        DRIVE_TO_TARGET_2,
+        DRIVE_TO_TARGET_3;
+    }
+
+    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM,0,0,AngleUnit.DEGREES,0);
+    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 1000, -406, AngleUnit.DEGREES, 0);
+    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM,500,0, AngleUnit.DEGREES,0);
+
+    final double SPEED = 0.3;
+    @Override
+    public void runOpMode() {
+
+        // Initialize the hardware variables. Note that the strings used here must correspond
+        // to the names assigned during the robot configuration step on the DS or RC devices.
+
+        odo = hardwareMap.get(GoBildaPinpointDriver.class,"odo");
+        odo.setOffsets(-101.6, -120.65); //these are tuned for 3110-0002-0001 Product Insight #1
+        odo.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.REVERSED);
+
+        odo.resetPosAndIMU();
+
+        nav.initializeMotors();
+        nav.setXYCoefficients(.005,0,2.0,DistanceUnit.MM,12);
+        nav.setYawCoefficients(3.1,0,2.0, AngleUnit.DEGREES,2);
+
+        StateMachine stateMachine;
+        stateMachine = StateMachine.WAITING_FOR_START;
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.addData("X offset", odo.getXOffset());
+        telemetry.addData("Y offset", odo.getYOffset());
+        telemetry.addData("Device Version Number:", odo.getDeviceVersion());
+        telemetry.addData("Device Scalar", odo.getYawScalar());
+        Pose2D pos = odo.getPosition();
+        String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+        telemetry.addData("Position", data);
+        telemetry.update();
+
+        // Wait for the game to start (driver presses START)
+        waitForStart();
+        resetRuntime();
+
+        while (opModeIsActive()) {
+            odo.update();
+
+            if(stateMachine == StateMachine.WAITING_FOR_START){
+                stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+            }
+
+            if (stateMachine == StateMachine.DRIVE_TO_TARGET_1) {
+                if (nav.driveTo(odo.getPosition(), TARGET_1, SPEED, 3)) {
+                    telemetry.addLine("at position #1!");
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_2;
+                }
+            }
+
+            if (stateMachine == StateMachine.DRIVE_TO_TARGET_2){
+                if (nav.driveTo(odo.getPosition(), TARGET_2, SPEED, 3)) {
+                    telemetry.addLine("at position #2!");
+                    stateMachine = StateMachine.DRIVE_TO_TARGET_3;
+                    break;
+                }
+            }
+
+            if (stateMachine == StateMachine.DRIVE_TO_TARGET_3){
+                if (nav.driveTo(odo.getPosition(), TARGET_3, SPEED, 3)){
+                    telemetry.addLine("at position #3!");
+                    stateMachine = StateMachine.AT_TARGET;
+                }
+            }
+
+            telemetry.addData("current state:",stateMachine);
+            pos = odo.getPosition();
+            data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+            telemetry.update();
+
+        }
+        while (opModeIsActive()) {
+            pos = odo.getPosition();
+            data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+            telemetry.update();
+        }
+    }}
