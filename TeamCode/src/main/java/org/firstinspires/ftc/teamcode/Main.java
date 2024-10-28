86dbe5cda7d099aab6fb0e6a2b0dcd65d6082b8f
Yashi <yajai25@icstudents.org>
2024-10-28 16:46:53 -0500

    Created Main file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Main {
+//    @Override
+    public void init() {
+//        Do something
+    }
+}
