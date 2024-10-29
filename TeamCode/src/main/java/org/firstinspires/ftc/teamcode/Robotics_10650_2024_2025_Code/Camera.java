3dbd93d9c89f4e26292797600f1a29aaf4c93abe
10650biohazards <hazmat10650@gmail.com>
2024-10-28 20:56:44 -0400

    Minor change to reduce confusion
    Added the disabled label to the camera class so it does not show up as a choosable OpMode in the Driver Station.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/Camera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robotics_10650_2024_2025_Code/Camera.java
@@ -21,6 +21,7 @@
 
 package org.firstinspires.ftc.teamcode.Robotics_10650_2024_2025_Code;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -36,6 +37,7 @@ import org.openftc.easyopencv.OpenCvPipeline;
 import org.openftc.easyopencv.OpenCvWebcam;
 
 @TeleOp
+@Disabled
 public class Camera extends LinearOpMode
 {
     OpenCvWebcam webcam;
