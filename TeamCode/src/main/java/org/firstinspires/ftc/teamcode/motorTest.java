41f5519f8eefd80e6e2bee87ca4f1a295dd495ac
CSY Student <noreply@csyonline.com>
2024-10-30 14:19:46 -0700

    Add motor functionality

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
@@ -9,9 +10,11 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class motorTest extends LinearOpMode {
-    @Override
 
+    @Override
     public void runOpMode(){
+        DcMotor leftMotor = hardwareMap.get(DcMotor.class, "Left");
+        DcMotor rightMotor = hardwareMap.get(DcMotor.class, "Right");
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -20,6 +23,9 @@ public class motorTest extends LinearOpMode {
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
             telemetry.update();
+            
+            leftMotor.setPower(1);
+            rightMotor.setPower(-1);
         }
     }
 
