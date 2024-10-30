b462ebd46e8bd21a8463d3be25c2efdb45c95d97
strangejmaster <strangejmaster@gmail.com>
2024-10-30 21:27:02 +0000

    Added ViperSlide bounds

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperSlide.java
@@ -5,20 +5,40 @@ import com.qualcomm.robotcore.hardware.*;
 public class ViperSlide {
     private DcMotor slide = null;
 
+    private int target = 0;
+
+    // Define the floor and ceiling of ViperSlide movement
+    final int ceil = 300;
+    final int floor = 0;
+
     public ViperSlide(HardwareMap hardwareMap) {
         slide = hardwareMap.get(DcMotor.class, "Slide");
         slide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
+    public int getTarget() {
+        return target;
+    }
+
     public void up() {
-        slide.setTargetPosition(200);
+        target += 10;
+        if (target > ceil) {
+            target = ceil;
+        }
+        slide.setTargetPosition(target);
     }
 
     public void down() {
-        slide.setTargetPosition(0);
+        target -= 10;
+        if (target < floor) {
+            target = floor;
+        }
+        slide.setTargetPosition(target);
     }
 
     public void stop() {
         slide.setPower(0);
     }
+
+
 }
