ac3c4e5b61b7f0b15607878ee513b7ddbd189a78
Sarah-Robot <sarah.g.patch@gmail.com>
2024-10-30 19:56:21 -0500

    Claw servo test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Servotest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import static org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware.MID_SERVO;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Servotest")
+public class Servotest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Servo servo = hardwareMap.tryGet(Servo.class, "wrist");
+        waitForStart();
+        while (opModeIsActive()) { //while loop for when program is active
+            if(gamepad1.y) {
+                servo.setPosition(MID_SERVO);
+
+            }
+            if(gamepad1.x){
+                servo.setPosition(.3725);
+
+            }
+            if(gamepad1.b) {
+                servo.setPosition(.755);
+            }
+        }
+    }
+}
