cbb4480e7dd074da25429400f5a4773a1f9132ec
Seth <swdurda@gmail.com>
2024-10-31 18:03:10 -0500

    Added function for each auto route in MeepMeepMain

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMain.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepMain.java
@@ -14,14 +14,51 @@ public class MeepMeepMain {
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(BlueRight(meepMeep))
+                .addEntity(BlueLeft(meepMeep))
+                .addEntity(RedRight(meepMeep))
+                .addEntity(RedLeft(meepMeep))
                 .start();
     }
 
+    //generates path for BlueRight
     private static RoadRunnerBotEntity BlueRight(MeepMeep meepMeep)
     {
         RoadRunnerBotEntity botEntity = CreateBotEntity(meepMeep);
         botEntity.runAction(botEntity.getDrive().actionBuilder(new Pose2d(-15, 63, Math.toRadians(-90)))
                 .splineTo(new Vector2d(-15,55), Math.toRadians(-90))
+                .build());
+
+        return botEntity;
+    }
+
+    //generates path for BlueLeft
+    private static RoadRunnerBotEntity BlueLeft(MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity botEntity = CreateBotEntity(meepMeep);
+        botEntity.runAction(botEntity.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+
+                .build());
+
+        return botEntity;
+    }
+
+    //generates path for RedRight
+    private static RoadRunnerBotEntity RedRight(MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity botEntity = CreateBotEntity(meepMeep);
+        botEntity.runAction(botEntity.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+
+                .build());
+
+        return botEntity;
+    }
+
+    //generates path for RedLeft
+    private static RoadRunnerBotEntity RedLeft(MeepMeep meepMeep)
+    {
+        RoadRunnerBotEntity botEntity = CreateBotEntity(meepMeep);
+        botEntity.runAction(botEntity.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+
                 .build());
 
         return botEntity;
