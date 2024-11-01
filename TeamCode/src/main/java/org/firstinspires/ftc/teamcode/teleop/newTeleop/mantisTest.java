567272719c270ba971b52750827cf9c8e1371ce6
Ali-K317 <akassmieh4233262@gmail.com>
2024-11-01 14:48:17 -0500

    Mantis arm speeds that work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/newTeleop/mantisTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/newTeleop/mantisTest.java
@@ -9,26 +9,35 @@ import org.firstinspires.ftc.teamcode.hardware;
 @TeleOp(name = "Mantis Test", group = "Teleop")
 public class mantisTest extends LinearOpMode {
     hardware hardware = new hardware();
-    final double driveSpeed = 0.75;
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
         waitForStart();
         while(opModeIsActive()){
             mantis();
+//            while(hardware.mantis.isBusy()){
+//                telemetry.addData("Posiiton", hardware.mantis.getCurrentPosition());
+//                telemetry.update();
+//            }
         }
     }
 
     private void initialize(){
         hardware.mantis = hardwareMap.get(DcMotor.class, "mantis");
-        hardware.mantis.setDirection(DcMotor.Direction.FORWARD);
+        hardware.mantis.setDirection(DcMotor.Direction.REVERSE);
         hardware.mantis.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //hardware.mantis.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
     private void mantis(){
-        if(gamepad1.right_trigger > 0){
+        double driveSpeed = gamepad1.right_stick_y;
+        //boolean ballCrusher = gamepad1.right_b
+        if(gamepad1.right_stick_y > 0) {
             hardware.mantis.setPower(driveSpeed);
-        }else if(gamepad1.left_trigger > 0){
-            hardware.mantis.setPower(driveSpeed * -0.5);
+
+        }else if(gamepad1.right_stick_y < 0){
+            hardware.mantis.setPower(0.2 * driveSpeed);
+        }else{
+            hardware.mantis.setPower(0.1);
         }
     }
 }
