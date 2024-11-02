8852f7e1fe5b4c109a2bc59e277247e5f52c0466
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-11-01 21:07:15 -0400

    Create Lift.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lift/Lift.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.Lift;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Lift {
+    HardwareMap hardwareMap;
+    
+    public Lift(HardwareMap hardwareMap){
+        this.hardwareMap = hardwareMap;
+    }
+}
