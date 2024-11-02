24e1830b4deed791a3e2a45a4368ff30ff76e7bc
sqviid <sameer.kozubowski2010@gmail.com>
2024-11-02 13:15:46 -0400

    Update SimpleDebugTeleOp.java
    We set the test power to 50%.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDebugTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleDebugTeleOp.java
@@ -1,15 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+@TeleOp
 public class SimpleDebugTeleOp extends OpMode {
+
+    DcMotor motorTest;
+
     @Override
     public void init() {
-        
+      motorTest = hardwareMap.get(DcMotor.class, "motorTest");
+
+
+
     }
 
     @Override
     public void loop() {
-
+        motorTest.setPower(0.5);
     }
 }
