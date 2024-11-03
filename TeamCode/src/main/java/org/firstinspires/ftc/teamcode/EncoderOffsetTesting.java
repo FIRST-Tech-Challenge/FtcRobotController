d44ec0dcda8abf5bc91d50c8dc60805ba3f3a375
yarilightcav <71334766+badgamer2007@users.noreply.github.com>
2024-11-02 20:52:10 -0400

    I did some stuff
    I updated gradle from .6.1 tp .7.1 then added constants for a module and then made the constructor for it but I am confused on Class CRServoImplEx
    and where to get the PWM Ranges for the Axon Max+. I also decided to make a tester opMode to find servo offsets because that might be useful idk bruh

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderOffsetTesting.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.apache.commons.math3.analysis.function.Abs;
+
+@TeleOp(name="Encoder Offset Tester")
+
+public class EncoderOffsetTesting extends LinearOpMode {
+
+    public CRServo axon = hardwareMap.get(CRServo.class, "armServo1");
+    public AbsoluteAnalogEncoder encoder =  new AbsoluteAnalogEncoder(hardwareMap.get(AnalogInput.class, "someHWMapName"), 3.3);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //I HAVE NO IDEA HOW TO DO THIS
+    }
+}
