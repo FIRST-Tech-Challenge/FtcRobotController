2b194f1b8b9ed24d694d9fa58e593aa736c95a47
Carl <carlankorhillam@gmail.com>
2024-11-03 23:50:55 -0600

    made Controller.java more readable

--- TeamCode/src/main/java/com/bosons/Hardware/Controller.java
+++ TeamCode/src/main/java/com/bosons/Hardware/Controller.java
@@ -77,72 +77,167 @@ public class Controller{
 
     public boolean toggleButtonState(Button input){
         switch (input) {
-            case a: {if(this.GamePad.a&&!this.was_a){this.toggle_a=!this.toggle_a;}return(this.toggle_a);}
-            case y: {if(this.GamePad.y&&!this.was_y){this.toggle_b=!this.toggle_b;}return(this.toggle_y);}
-            case x: {if(this.GamePad.x&&!this.was_x){this.toggle_x=!this.toggle_x;}return(this.toggle_x);}
-            case b: {if(this.GamePad.b&&!this.was_b){this.toggle_y=!this.toggle_y;}return(this.toggle_b);}
-            case leftStickButton: {if(this.GamePad.left_stick_button&&!this.was_lsb){this.toggle_lsb=!this.toggle_lsb;}return(this.toggle_lsb);}
-            case rightStickButton: {if(this.GamePad.right_stick_button&&!this.was_rsb){this.toggle_rsb=!this.toggle_rsb;}return(this.toggle_rsb);}
-            case rightBumper: {if(this.GamePad.right_bumper&&!this.was_rb){this.toggle_rb=!this.toggle_rb;}return(this.toggle_rb);}
-            case leftBumper: {if(this.GamePad.left_bumper&&!this.was_lb){this.toggle_lb=!this.toggle_lb;}return(this.toggle_lb);}
-            case dPadUp: {if(this.GamePad.dpad_up&&!this.was_dup){this.toggle_dup=!this.toggle_dup;}return(this.toggle_dup);}
-            case dPadDown: {if(this.GamePad.dpad_down&&!this.was_ddown){this.toggle_ddown=!this.toggle_ddown;}return(this.toggle_ddown);}
-            case dPadRight: {if(this.GamePad.dpad_right&&!this.was_dright){this.toggle_dright=!this.toggle_dright;}return(this.toggle_dright);}
-            case dPadLeft: {if(this.GamePad.dpad_left&&!this.was_dleft){this.toggle_dleft=!this.toggle_dleft;}return(this.toggle_dleft);}
-            default: return(false);
+            case a: {
+                if(this.GamePad.a&&!this.was_a){
+                    this.toggle_a=!this.toggle_a;
+                }
+                return(this.toggle_a);
+            }
+            case y: {
+                if(this.GamePad.y&&!this.was_y){
+                    this.toggle_b=!this.toggle_b;
+                }
+                return(this.toggle_y);
+            }
+            case x: {
+                if(this.GamePad.x&&!this.was_x){
+                    this.toggle_x=!this.toggle_x;
+                }
+                return(this.toggle_x);
+            }
+            case b: {
+                if(this.GamePad.b&&!this.was_b){
+                    this.toggle_y=!this.toggle_y;
+                }
+                return(this.toggle_b);
+            }
+            case leftStickButton: {
+                if(this.GamePad.left_stick_button&&!this.was_lsb){
+                    this.toggle_lsb=!this.toggle_lsb;
+                }
+                return(this.toggle_lsb);
+            }
+            case rightStickButton: {
+                if(this.GamePad.right_stick_button&&!this.was_rsb){
+                    this.toggle_rsb=!this.toggle_rsb;
+                }
+                return(this.toggle_rsb);
+            }
+            case rightBumper: {
+                if(this.GamePad.right_bumper&&!this.was_rb){
+                    this.toggle_rb=!this.toggle_rb;
+                }
+                return(this.toggle_rb);
+            }
+            case leftBumper: {
+                if(this.GamePad.left_bumper&&!this.was_lb){
+                    this.toggle_lb=!this.toggle_lb;
+                }
+                return(this.toggle_lb);
+            }
+            case dPadUp: {
+                if(this.GamePad.dpad_up&&!this.was_dup){
+                    this.toggle_dup=!this.toggle_dup;
+                }
+                return(this.toggle_dup);
+            }
+            case dPadDown: {
+                if(this.GamePad.dpad_down&&!this.was_ddown){
+                    this.toggle_ddown=!this.toggle_ddown;
+                }
+                return(this.toggle_ddown);
+            }
+            case dPadRight: {
+                if(this.GamePad.dpad_right&&!this.was_dright){
+                    this.toggle_dright=!this.toggle_dright;
+                }
+                return(this.toggle_dright);
+            }
+            case dPadLeft: {
+                if(this.GamePad.dpad_left&&!this.was_dleft){
+                    this.toggle_dleft=!this.toggle_dleft;
+                }
+                return(this.toggle_dleft);
+            }
+            default:
+                return(false);
         }
     }
 
     public boolean onButtonPress(Button input){
         switch (input){
-            case a: return(GamePad.a&&!this.was_a);
-            case y: return(this.GamePad.y&&!this.was_y);
-            case x: return(this.GamePad.x&&!this.was_x);
-            case b: return(this.GamePad.b&&!this.was_b);
-            case leftStickButton: return(this.GamePad.left_stick_button&&!this.was_lsb);
-            case rightStickButton: return(this.GamePad.right_stick_button&&!this.was_rsb);
-            case rightBumper: return(this.GamePad.right_bumper&&!this.was_rb);
-            case leftBumper: return(this.GamePad.left_bumper&&!this.was_lb);
-            case dPadUp: return(this.GamePad.dpad_up&&!this.was_dup);
-            case dPadDown: return(this.GamePad.dpad_down&&!this.was_ddown);
-            case dPadRight: return(this.GamePad.dpad_right&&!this.was_dright);
-            case dPadLeft: return(this.GamePad.dpad_left&&!this.was_dleft);
-            default: return(false);
+            case a:
+                return(GamePad.a&&!this.was_a);
+            case y:
+                return(this.GamePad.y&&!this.was_y);
+            case x:
+                return(this.GamePad.x&&!this.was_x);
+            case b:
+                return(this.GamePad.b&&!this.was_b);
+            case leftStickButton:
+                return(this.GamePad.left_stick_button&&!this.was_lsb);
+            case rightStickButton:
+                return(this.GamePad.right_stick_button&&!this.was_rsb);
+            case rightBumper:
+                return(this.GamePad.right_bumper&&!this.was_rb);
+            case leftBumper:
+                return(this.GamePad.left_bumper&&!this.was_lb);
+            case dPadUp:
+                return(this.GamePad.dpad_up&&!this.was_dup);
+            case dPadDown:
+                return(this.GamePad.dpad_down&&!this.was_ddown);
+            case dPadRight:
+                return(this.GamePad.dpad_right&&!this.was_dright);
+            case dPadLeft:
+                return(this.GamePad.dpad_left&&!this.was_dleft);
+            default:
+                return(false);
         }
     }
 
     public boolean onButtonHold(Button Input){
         switch (Input){
-            case a: return(this.GamePad.a);
-            case y: return(this.GamePad.y);
-            case x: return(this.GamePad.x);
-            case b: return(this.GamePad.b);
-            case leftStickButton: return(this.GamePad.left_stick_button);
-            case rightStickButton: return(this.GamePad.right_stick_button);
-            case rightBumper: return(this.GamePad.right_bumper);
-            case leftBumper: return(this.GamePad.left_bumper);
-            case dPadUp: return(this.GamePad.dpad_up);
-            case dPadDown: return(this.GamePad.dpad_down);
-            case dPadRight: return(this.GamePad.dpad_right);
-            case dPadLeft: return(this.GamePad.dpad_left);
-            default: return(false);
+            case a:
+                return(this.GamePad.a);
+            case y:
+                return(this.GamePad.y);
+            case x:
+                return(this.GamePad.x);
+            case b:
+                return(this.GamePad.b);
+            case leftStickButton:
+                return(this.GamePad.left_stick_button);
+            case rightStickButton:
+                return(this.GamePad.right_stick_button);
+            case rightBumper:
+                return(this.GamePad.right_bumper);
+            case leftBumper:
+                return(this.GamePad.left_bumper);
+            case dPadUp:
+                return(this.GamePad.dpad_up);
+            case dPadDown:
+                return(this.GamePad.dpad_down);
+            case dPadRight:
+                return(this.GamePad.dpad_right);
+            case dPadLeft:
+                return(this.GamePad.dpad_left);
+            default:
+                return(false);
         }
     }
 
     public float getAnalogValue(Joystick Input){
         switch (Input){
-            case LeftX:return(GamePad.left_stick_x);
-            case LeftY:return(GamePad.left_stick_y);
-            case RightX:return(GamePad.right_stick_x);
-            case RightY:return(GamePad.right_stick_y);
-            default: return((float) 0.0);
+            case LeftX:
+                return(GamePad.left_stick_x);
+            case LeftY:
+                return(GamePad.left_stick_y);
+            case RightX:
+                return(GamePad.right_stick_x);
+            case RightY:
+                return(GamePad.right_stick_y);
+            default:
+                return((float) 0.0);
         }
     }
     public float getTriggerValue(Trigger Input){
         switch (Input){
-            case Right:return(GamePad.right_trigger);
-            case Left:return(GamePad.left_trigger);
-            default: return((float) 0.0);
+            case Right:
+                return(GamePad.right_trigger);
+            case Left:
+                return(GamePad.left_trigger);
+            default:
+                return((float) 0.0);
         }
     }
 
--- TeamCode/src/main/java/com/bosons/Hardware/DriveTrain.java
+++ TeamCode/src/main/java/com/bosons/Hardware/DriveTrain.java
@@ -1,57 +1,44 @@
 package com.bosons.Hardware;
 
-import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class DriveTrain
 {
 
-    private Motor fl;
-    private Motor bl;
-    private Motor fr;
-    private Motor br;
+    private final Motor fl;
+    private final Motor bl;
+    private final Motor fr;
+    private final Motor br;
 
     private double drivePowerCoefficient = 1;//default
     private double turnPowerCoefficient = 1;//default
 
-    //private BNO055IMU IMU;
-
     public DriveTrain(OpMode op)
     {
-
         fl = new Motor("fl", op);
         bl = new Motor("bl", op);
         fr = new Motor("fr", op);
         br = new Motor("br", op);
 
-
-
-        fl.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotorSimple.Direction.FORWARD);
-        bl.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotorSimple.Direction.FORWARD);
-        fr.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotorSimple.Direction.REVERSE);
-        br.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotorSimple.Direction.REVERSE);
+        fl.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.Direction.FORWARD);
+        bl.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.Direction.FORWARD);
+        fr.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.Direction.REVERSE);
+        br.setConstants(DcMotor.RunMode.RUN_WITHOUT_ENCODER, DcMotor.ZeroPowerBehavior.BRAKE, DcMotor.Direction.REVERSE);
     }
 
-    public void drive(double power, double theta, double turn)
-    {//vroom
-
-
+    public void drive(double power, double theta, double turn) {//vroom vroom im in me mums car.
         double x = power*drivePowerCoefficient * Math.cos(theta) * 1.5;
         double y = power*drivePowerCoefficient * Math.sin(theta);
 
-
-
-        double flPower = y + x + turn*turnPowerCoefficient;
-        double blPower = y - x + turn*turnPowerCoefficient;
-        double frPower = y - x - turn*turnPowerCoefficient;
-        double brPower = y + x - turn*turnPowerCoefficient;
+        double flPower = y + x + (turn*turnPowerCoefficient);
+        double blPower = y - x + (turn*turnPowerCoefficient);
+        double frPower = y - x - (turn*turnPowerCoefficient);
+        double brPower = y + x - (turn*turnPowerCoefficient);
 
         double maxPower = 1;
-        if(Math.abs(flPower) > maxPower || Math.abs(blPower) > maxPower
-                || Math.abs(frPower) > maxPower || Math.abs(brPower) > maxPower)
-        {
+
+        if(Math.abs(flPower) > maxPower || Math.abs(blPower) > maxPower || Math.abs(frPower) > maxPower || Math.abs(brPower) > maxPower) {
             double max = Math.max(Math.abs(flPower), Math.abs(blPower));
             max = Math.max(max, Math.abs(frPower));
             max = Math.max(max, Math.abs(brPower));
@@ -68,11 +55,7 @@ public class DriveTrain
         br.setPower(brPower);
     }
 
-    public void setDrivePowerCoefficient(double coefficient){
-        drivePowerCoefficient = coefficient;
-    }
-    public void setTurnPowerCoefficient(double coefficient){
-        turnPowerCoefficient = coefficient;
-    }
+    public void setDrivePowerCoefficient(double coefficient){drivePowerCoefficient = coefficient;}
+    public void setTurnPowerCoefficient(double coefficient){turnPowerCoefficient = coefficient;}
 
 }
