8d60398bae20993aa93b5349709c3d8332b5b4ab
= <25adayasankar@frcsfalcons.org>
2024-11-04 10:19:03 -0500

    Added runOpMode code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopCode.java
@@ -1,2 +1,16 @@
-package org.firstinspires.ftc.teamcode;public class TeleopCode {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * This code is the main central teleop code! The code in this file will run after the code is
+ * initialized, but cannot run during the player-controlled period.
+*/
+
+public class TeleopCode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        
+    }
 }
