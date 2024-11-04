b47b15a059b7ad8ddc012a44a5f8d8f971aedb56
Yashi <yajai25@icstudents.org>
2024-11-04 17:29:47 -0600

    Temporary claw code with open and close methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Claw.java
@@ -1,4 +1,17 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.hardware.*;
 
 public class Claw {
+    private Servo servo1 = null;
+    public Claw (HardwareMap hardwareMap) {
+        servo1 = hardwareMap.get(Servo.class, "servo1");
+    }
+
+    public void open() {
+       servo1.setPosition(0); //temp position
+    }
+
+    public void close() {
+        servo1.setPosition(1); //temp position
+    }
 }
