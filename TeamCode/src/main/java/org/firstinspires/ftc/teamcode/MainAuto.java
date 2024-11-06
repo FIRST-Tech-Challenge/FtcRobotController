3360c85328dae7a3326a45d7e36d169385a7459d
Dakspieth <jrb.games09@gmail.com>
2024-11-06 16:01:34 -0500

    auto stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainAuto.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode;
+import androidx.annotation.VisibleForTesting;
+
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Robot: Auto Drive By Encoder", group="Robot")
 @Disabled
@@ -17,6 +20,10 @@ public class MainAuto extends LinearOpMode {
     //move left a few inches (for other team's auto
 
     ///////////////////////////////code///////////////////////////////
+    private ElapsedTime     runtime = new ElapsedTime();
+    static final double countsPerRev = 1440;
+    static final double wheelDiameter = 3.5;     // For figuring circumference (in inches)
+    static final double countsPerInch  = countsPerRev / (wheelDiameter * 3.141592653589793238462643383279502884197169399375);
     private DcMotor leftBack; //Initializes Back-Left direct current motor for the driving function of our robot, gary.
     private DcMotor rightBack; //Initializes Back-Right direct current motor for the driving function of our robot, gary.
     private DcMotor leftFront; //Initializes Front-Left direct current motor for the driving function of our robot, gary.
@@ -24,11 +31,70 @@ public class MainAuto extends LinearOpMode {
 
     @Override
     public void runOpMode() {
+        telemetry.addData("Starting pos: ", leftBack.getCurrentPosition());
+        telemetry.update();
+        waitForStart();
+
+        driveInches(24, 1, dir.RIGHT, 5);
+        //extend linear slide up to second ladder
+        driveInches(7, 1, dir.FORWARD, 5);
+        //move slide down slightly (clip specimen on second ladder)
+        //release claw
+        //move slide up slightly
+        driveInches(3, 1, dir.BACKWARD, 5);
+        driveInches(30, 1, dir.LEFT, 5);
+        driveInches(24, 1, dir.FORWARD, 5);
 
     }
+    enum dir {
+        LEFT,
+        RIGHT,
+        FORWARD,
+        BACKWARD
 
-    private void driveInches(float inches) {
-        int targetPos = leftBack.getCurrentPosition() * ;
     }
+    private void driveInches(float inches, float speed, dir direction, float timeoutS) {
+        int lbDir = 1;
+        int rbDir = 1;
+        int lfDir = 1;
+        int rfDir = 1;
+        int targetPos;
 
+        //sets some motors to negative power depending on direction
+        switch(direction) {
+            case LEFT:
+                rbDir = -1;
+                lfDir = -1;
+                break;
+            case RIGHT:
+                lbDir = -1;
+                rfDir = -1;
+                break;
+            case FORWARD:
+                break;
+            case BACKWARD:
+                lbDir = -1;
+                rbDir = -1;
+                lfDir = -1;
+                rfDir = -1;
+                break;
+        }
+        if(opModeIsActive()) {
+            runtime.reset();
+            targetPos = lbDir + (int)(inches * countsPerInch);
+            leftBack.setPower(lbDir * speed);
+            rightBack.setPower(rbDir * speed);
+            leftFront.setPower(lfDir * speed);
+            rightFront.setPower(rfDir * speed);
+            while(opModeIsActive() && timeoutS < runtime.seconds()) {
+                telemetry.addData("currently going", String.valueOf(direction), " to ", targetPos);
+                telemetry.update();
+            }
+            leftBack.setPower(0);
+            rightBack.setPower(0);
+            leftFront.setPower(0);
+            rightFront.setPower(0);
+            sleep(100);
+        }
+    }
 }
