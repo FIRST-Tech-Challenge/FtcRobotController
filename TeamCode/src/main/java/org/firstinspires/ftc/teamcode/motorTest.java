a501f7ed2c15568b1ba59a09ac2a2a0ad30427ca
Josiah Blackstone <jblackstone2027@csyonline.com>
2024-11-06 19:02:41 -0500

    redo Trim.java to be better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motorTest.java
@@ -3,11 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp
 public class motorTest extends LinearOpMode {
@@ -19,27 +14,31 @@ public class motorTest extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
         waitForStart();
+        Trim t = new Trim();
 
         while (opModeIsActive()) {
             telemetry.addData("Status", "Running");
+            telemetry.addData("left Trim", t.getLeftTrim());
+            telemetry.addData("right Trim", t.getRightTrim());
+
             float leftThumbstickValue = gamepad1.left_stick_y;
             float rightThumbstickValue = gamepad1.right_stick_y;
-
+            PowerLevels pl=t.getPowerLevel(leftThumbstickValue,rightThumbstickValue);
             telemetry.addData("Left Thumbstick Value", leftThumbstickValue);
             telemetry.addData("Right Thumbstick Value", rightThumbstickValue);
 
-            trim.update();
+
             if (gamepad1.a) {
-                leftMotor.setPower(-1 * trim.trimLeft());
-                //0.95 was the value that the trim was set to before I added my code
-                rightMotor.setPower(1 * trim.trimRight());
+                leftMotor.setPower(-pl.getLeftPower());
+                //0.95 was the value that the Trim was set to before I added my code
+                rightMotor.setPower(pl.getRightPower());
             } else {
                 leftMotor.setPower(-0);
                 rightMotor.setPower(0);
 
                 if (gamepad1.b) {
-                    leftMotor.setPower(1 * trim.trimLeft());
-                    rightMotor.setPower(-1 * trim.trimRight());
+                    leftMotor.setPower(pl.getLeftPower());
+                    rightMotor.setPower(-pl.getRightPower());
                 } else {
                     leftMotor.setPower(-0);
                     rightMotor.setPower(0);
