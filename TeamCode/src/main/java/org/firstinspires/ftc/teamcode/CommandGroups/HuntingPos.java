22b5ee74b9db5f6b216e9bf910678a832ca5a90c
superzokabear <superzokabear@gmail.com>
2024-11-06 20:58:20 -0500

    made a pause command and made a sequence for hunting for samples.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroups/HuntingPos.java
@@ -0,0 +1,117 @@
+package org.firstinspires.ftc.teamcode.CommandGroups;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.geometry.Pose2d;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
+
+import org.firstinspires.ftc.teamcode.Commands.FollowPath;
+import org.firstinspires.ftc.teamcode.RobotContainer;
+import org.firstinspires.ftc.teamcode.Subsystems.ClawState;
+
+import java.util.ArrayList;
+
+// Example Sequential Command Group
+// There are also:
+// ParallelCommandGroup
+// ParallelRaceGroup
+// ParallelDeadlineGroup
+
+public class HuntingPos extends SequentialCommandGroup {
+
+    // constructor
+    public HuntingPos() {
+        addCommands(
+
+
+                new InstantCommand(() -> RobotContainer.shoulderJoint.RotateTo(135)),
+
+//                // folds the elbow in 10
+                new InstantCommand(() ->RobotContainer.elbowJoint.RotateTo(145)),
+//
+                new InstantCommand(() -> RobotContainer.flappyFlappyWrist.RotateTo(45)),
+
+                new InstantCommand(() -> RobotContainer.wristRotateServo.RotateTo(135))
+
+                //new InstantCommand(() -> RobotContainer.claw.ControlClaw(ClawState.CLOSE))
+
+
+        );
+
+
+        // new command1
+        // new command2
+        // new command3
+
+    }
+
+}
+
+// Example #1: Lily's 2023 FRC super cube auto
+/*          // enable arm, and lift to stow position
+            new InstantCommand(() -> RobotContainer.arm.SetEnableArm(true)),
+
+            // move arm back to drop off cube
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(RobotContainer.arm.HIGH_DEG)),
+
+            // delay until arm gets back
+            new DelayCommand(1.0),
+
+            // place cube
+            new InstantCommand(() -> RobotContainer.grabber.setClose()),
+
+            // delay for gripper to close
+            new DelayCommand(0.7),
+
+            // move arm to 'forward position' but inside robot bumper)
+            // move to 135deg
+            new InstantCommand(() -> RobotContainer.arm.SetArmPosition(135.0)),
+
+            // delay for arm to get to stow
+            new DelayCommand(1.5),
+
+            // ensure arm is stowed before it is allow to begin moving over charge station
+            new SafetyCheckStowPosition(),
+
+            // drive right
+            // new DrivetoRelativePose(new Pose2d(0,-2.0, new Rotation2d(0.0)), 1.0, 0.1, 5.0),
+
+            // drive straight
+            new DrivetoRelativePose(new Pose2d(5.0, 0, new Rotation2d(0.0)),1.8,0.1, 7.0),
+
+            // pick up cube from floor :)
+            new AutoFloorCubePickup(),
+
+            // delay
+            new DelayCommand(0.5),
+
+            // drive back
+            //new DrivetoRelativePose(new Pose2d(1.0,0, new Rotation2d(0.0)), 1.0, 0.1, 2.0),
+
+            // drive left to center
+            new DrivetoRelativePose(new Pose2d(-1.0,2.0, new Rotation2d(0.0)), 1.8, 0.1, 5.0),
+
+            // drive straight onto charge station
+            new DrivetoRelativePose(new Pose2d(-1.5, 0, new Rotation2d(0.0)),1.0,0.1, 30.0),
+
+            // balance
+            new AutoBalance()
+
+// Example #2: Matthew's 2024 shoot donut sequence.
+This sequence contains parallel and parallelrace subgroups within an overall series command
+
+      addCommands(
+      new ParallelRaceGroup(
+        new AimToSpeaker(),
+        new SpinupSpeaker()
+      ),
+      new ParallelCommandGroup(
+        new WaitForEffectorAngle(),
+        new WaitForShooterSpinup()
+      ),
+
+      new ShootSpeaker()
+    );
+
+ */
