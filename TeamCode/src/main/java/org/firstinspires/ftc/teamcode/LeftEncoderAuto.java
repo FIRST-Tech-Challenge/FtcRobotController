d121e97d8e2f94d1ccea7cc8ba6586b3c2ef045b
Dakspieth <jrb.games09@gmail.com>
2024-11-07 15:59:43 -0500

    JIGGLEY

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftEncoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LeftEncoderAuto.java
@@ -8,14 +8,14 @@ public class LeftEncoderAuto extends MainEncoderAuto {
     public void runOpMode() {
         // Call the parent class method to use its setup
         super.runOpMode();
-        driveInches(24, 1, dir.LEFT, 5);
+        driveInches(24, 1, dir.RIGHT, 5);
         moveSlide(20, 1, true, 5);
         driveInches(7, 1, dir.FORWARD, 5);
         moveSlide(2, 1, false, 1);
         moveClaw(false);
         moveSlide(2, 1, true, 1);
         driveInches(3, 1, dir.BACKWARD, 5);
-        driveInches(30, 1, dir.RIGHT, 5);
+        driveInches(30, 1, dir.LEFT, 5);
         driveInches(24, 1, dir.FORWARD, 5);
         moveSlide(12, 1, false, 1);
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainEncoderAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainEncoderAuto.java
@@ -42,19 +42,8 @@ public class MainEncoderAuto extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
-        driveInches(24, 1, dir.RIGHT, 5);
-        moveSlide(20, 1, true, 5);
-        driveInches(7, 1, dir.FORWARD, 5);
-        moveSlide(2, 1, false, 1);
-        moveClaw(false);
-        moveSlide(2, 1, true, 1);
-        driveInches(3, 1, dir.BACKWARD, 5);
-        driveInches(30, 1, dir.LEFT, 5);
-        driveInches(24, 1, dir.FORWARD, 5);
-        moveSlide(12, 1, false, 1);
-
     }
-    protected enum dir {
+    protected enum dir { // dir is short for direction btw
         LEFT,
         RIGHT,
         FORWARD,
