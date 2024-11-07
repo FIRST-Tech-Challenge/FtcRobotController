3545fb947570b494bd9340d84f44790418ee17e4
NTCheese <arirahmani2021@gmail.com>
2024-11-07 15:19:52 -0700

    made the motor test work, going to commit just in case battery runs out. Still need to test if Input.java works together with it.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/testMotorMove.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/testMotorMove.java
@@ -10,54 +10,30 @@ import org.firstinspires.ftc.teamcode.Systems.Motors;
 public class testMotorMove extends LinearOpMode {
 
     Motors motors;
+    Input input;
 
     @Override
     public void runOpMode() throws InterruptedException {
         motors = new Motors(hardwareMap);
+        input = new Input(hardwareMap);
 
         waitForStart();
 
         while (opModeIsActive()) {
+
             double leftsticky = gamepad1.left_stick_y * 100;
-            double rightstickx = gamepad1.left_stick_y * 100;
-            double leftstickx = gamepad1.left_stick_y * 100;
+            double rightstickx = gamepad1.right_stick_x * 100;
+            double leftstickx = gamepad1.left_stick_x * 100;
 
 
-            Move(leftsticky);
-            Spin(rightstickx * 100);
-            Strafe(leftstickx * 100);
+            input.Move(leftsticky);
+            input.Spin(rightstickx);
+            input.Strafe(leftstickx);
 
             telemetry.addData("MOVE:", "left_y (%.2f),", leftsticky);
             telemetry.addData("SPIN:", "right_x (%.2f),", rightstickx);
-            telemetry.addData("Strafe:", "left_x (%.2f),", leftstickx);
+            telemetry.addData("STRAFE:", "left_x (%.2f),", leftstickx);
             telemetry.update(); // telemtryy
         }
-    }
-
-    private void Move(double power)
-    {
-
-        for (int i = 0; i < motors.driveTrainMotors.length; i++) {
-
-            motors.MoveMotor(i, power); // loop over every motor and move them by gamepad input
-        }
-    }
-
-    private void Strafe(double power)
-    {
-        motors.MoveMotor(1, power); // left front
-        motors.MoveMotor(3, power); // right front
-
-        motors.MoveMotor(0, -power); // left back
-        motors.MoveMotor(2, -power); // right back
-    }
-
-    private void Spin(double power)
-    {
-        motors.MoveMotor(1, power); // left front
-        motors.MoveMotor(0, power); // left back
-
-        motors.MoveMotor(2, -power); // right front
-        motors.MoveMotor(3, -power); // right back
     }
 }
