035dd5e709652acbe6b52e3bf11b00ef06f41f83
Ben Boonstra <bboonstra26@gmail.com>
2024-11-08 14:35:10 -0600

    Stable main!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseRobot.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
