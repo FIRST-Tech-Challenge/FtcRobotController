fe6ae6146eb2573bd4cb4bec924b0a623d2d31ab
PhuongLam1 <143738348+PhuongLam1@users.noreply.github.com>
2024-11-08 17:02:34 -0500

    Update SensorLimelight3A.java

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorLimelight3A.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorLimelight3A.java
@@ -68,7 +68,8 @@ import java.util.List;
  */
 @TeleOp(name = "Sensor: Limelight3A", group = "Sensor")
 @Disabled
-public class SensorLimelight3A extends LinearOpMode {
+public class
+SensorLimelight3A extends LinearOpMode {
 
     private Limelight3A limelight;
 
