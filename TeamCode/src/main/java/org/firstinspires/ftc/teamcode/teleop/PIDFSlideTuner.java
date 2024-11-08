67e61ed63cb14537b1cfaea0c76b00de9bb34f99
s0hum <sohum.p.gupta@gmail.com>
2024-11-07 17:27:29 -0800

    Added SlideTuner for PID

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/PIDFSlideTuner.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@Config
+@TeleOp
+
+public class PIDFSlideTuner extends OpMode {
+
+    private PIDController controller;
+
+    public static double p = 0, i = 0, d = 0;
+    public static double f = 0;
+
+    public static int target = 0;
+
+    private final double ticks_in_degrees = 537.7 / 360.0;
+
+    private DcMotorEx rightSlideMotor;
+
+    @Override
+    public void init() {
+        controller = new PIDController(p, i, d);
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        rightSlideMotor = hardwareMap.get(DcMotorEx.class, "rightSlideMotor");
+    }
+
+    @Override
+    public void loop() {
+        controller.setPID(p, i, d);
+        int intakeArmPos = rightSlideMotor.getCurrentPosition();
+        double pid = controller.calculate(intakeArmPos, target);
+        double ff = Math.cos(Math.toRadians(target / ticks_in_degrees)) * f;
+
+        double power = pid + ff;
+
+        rightSlideMotor.setPower(power);
+
+        telemetry.addData("pos", intakeArmPos);
+        telemetry.addData("target", target);
+        telemetry.update();
+    }
+
+}
+
