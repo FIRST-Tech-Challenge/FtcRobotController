3edf4c377f0b91edfa1204e9435f3f4a822e7ca0
alinaw <wasupanalina@gmail.com>
2024-11-09 01:55:06 -0500

    i hope this works
    servo fart stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/BadServoFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/BadServoFunctions.java
@@ -11,37 +11,38 @@ public class BadServoFunctions {
         public Servo wristServo;
         public Servo wristServo2;
         public Servo clawServo;
-        // Variables to store positions for wrist and claw
 
-        double wristPosition = 0.5; // Starting position for wrist 1
-        double wristPosition2 = 0.5; // Starting position for wrist 2
-        double clawPosition = 0.5; // Starting position for claw
 
     public BadServoFunctions(HardwareMap hardwareMap){
         wristServo=hardwareMap.get(Servo.class,"wrist_1");
         wristServo2=hardwareMap.get(Servo.class,"wrist_2");
         clawServo=hardwareMap.get(Servo.class,"claw");
+
+        // initialization positions
+        wristServo.setPosition(0.5); // Starting position for wrist 1
+        wristServo2.setPosition(0.5); // Starting position for wrist 2
+        clawServo.setPosition(0.5); // Starting position for claw
     }
         // Method to control the wrist position
         public void controlWrist (Gamepad gamepad1, Telemetry telemetry){
-            // good wrist
-            if (gamepad1.a) {
-                wristServo.setPosition(0.5);
-            }
-
-            // Minimum position used for testing purposes
-            if (gamepad1.b) {
-                wristServo.setPosition(0);
-            }
-            if (gamepad1.x){
-                wristServo2.setPosition(0.5);
-            }
-            if (gamepad1.y){
-                wristServo2.setPosition(0);
-            }
-            // Get position of the wrist and display on driver station for feedback
+            // Get position of the wrist so it knows
             double wristPosition = wristServo.getPosition();
             double wristPosition2 = wristServo2.getPosition();
+
+            // good wrist
+            if (gamepad1.a && wristPosition == 0) {
+                wristServo.setPosition(0.5);
+            } else if (gamepad1.a && wristPosition == 0.5){
+                wristServo.setPosition(0);
+            }
+
+            if (gamepad1.b & wristPosition2 ==0) {
+                wristServo.setPosition(0.5);
+            } else if (gamepad1.b && wristPosition2 == 0) {
+                wristServo.setPosition(0);
+            }
+
+            //sending telemetry data
             telemetry.addData("Wrist Position 1", wristPosition);
             telemetry.addData("Wrist Position 2",wristPosition2);
         }
@@ -51,11 +52,10 @@ public class BadServoFunctions {
         public void controlClaw (Gamepad gamepad1, Telemetry telemetry){
            if(gamepad1.right_bumper){
                clawServo.setPosition(0);
-           }
-
-           if (gamepad1.left_bumper){
+           } else if (gamepad1.left_bumper) {
                clawServo.setPosition(1);
            }
+
             // Send telemetry data to the driver station
             double clawPosition = clawServo.getPosition();
             telemetry.addData("Claw Position", clawPosition);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/SlideFunctions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/functions/SlideFunctions.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.functions;
 
+import android.text.method.Touch;
+
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -11,37 +14,45 @@ public class SlideFunctions {
 
     public DcMotor slideMotor;
     public DcMotor armMotor;
+    public TouchSensor slideSafety;
 
     public SlideFunctions(HardwareMap hardwareMap) {
 
         slideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
         armMotor = hardwareMap.get(DcMotor.class, "arm_motor");
-
+        slideSafety = hardwareMap.get(TouchSensor.class,"slide_safety");
         slideMotor.setDirection(DcMotor.Direction.REVERSE);
         armMotor.setDirection(DcMotor.Direction.FORWARD);
+        //reset encoders
+        slideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //everything brakes at 0
+        slideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
 
     public void SlidePosition(Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry){
 
-        double slidePowerConst = 0.7; //max power of slide
+        double slidePowerConst = 0.9; //max power of slide
         double slidePower = -gamepad2.left_stick_y;
 
-        slideMotor.setPower(slidePower * slidePowerConst);
-
-        //display of power and position of motor
+        //get position of slides
         int slidePosition= slideMotor.getCurrentPosition();
-        telemetry.addData("Slide power","%4.2f", slidePower);
-        telemetry.addData("Slide Position", slidePosition);
 
-        if (slidePosition == -800 && slidePower>0){
+        //slide safety
+        if ((slidePosition <= -800) && (slidePower>0)){
             slideMotor.setPower(0);
-        } else if (slidePosition==0 && slidePower<0) {
+        } else if (slideSafety.isPressed() && slidePower<0 ){
             slideMotor.setPower(0);
         } else {
             slideMotor.setPower(slidePower * slidePowerConst);
         }
-
+        //telemetry poo
+        telemetry.addData("Slide power","%4.2f", slidePower);
+        telemetry.addData("Slide Position", slidePosition);
     }
 
     public void ArmPosition(Gamepad gamepad1, Gamepad gamepad2, Telemetry telemetry) {
