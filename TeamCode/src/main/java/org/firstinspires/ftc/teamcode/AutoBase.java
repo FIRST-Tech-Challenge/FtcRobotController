2ec3bbe8e90157d3920d4aec588930814ef94ed6
Ben Boonstra <bboonstra26@gmail.com>
2024-11-09 14:21:21 -0600

    stable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBase.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.autonomous;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
