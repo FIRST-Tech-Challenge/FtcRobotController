1b7c5dc4d576785eafde0008392f977aa2052b89
GoldStar184 <caichapmansharp@gmail.com>
2024-11-10 14:36:42 -0800

    Added Code Comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -16,6 +16,9 @@ public class Main extends LinearOpMode {
         public void run() {
             if(active){
                 while(active){
+                    /*
+                    Debug Values, shows on driver hub during run
+                     */
                     main.addData("Controller 1 left stick", (Main.GetControllerDataItem("LeftStickX1") + "," + Main.GetControllerDataItem("LeftStickY1")));
                     main.addData("Controller 1 right stick", (Main.GetControllerDataItem("RightStickX1") + "," + Main.GetControllerDataItem("RightStickY1")));
                     main.addData("Controller 1 left trigger", (float)Main.GetControllerDataItem("LeftTrigger1"));
@@ -44,7 +47,9 @@ public class Main extends LinearOpMode {
     //private Thread dta = new Thread(DataUpdate);
     @Override
     public void runOpMode() throws InterruptedException {
-
+        /*
+        Updates Data
+         */
         ControllerData.put("LeftStickX1", 0.0f);
         ControllerData.put("LeftStickX2", 0.0f);
         ControllerData.put("RightStickX1", 0.0f);
