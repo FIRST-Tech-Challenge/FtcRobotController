2cecf55ae34e0a8eaeb9f236ad0da965f5aeca50
geppp41 <egep41414141@gmail.com>
2024-11-10 15:15:14 -0800

    updated some stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -4,6 +4,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.util.HashMap;
 
@@ -11,7 +15,9 @@ import java.util.HashMap;
 public class Main extends LinearOpMode {
 
 
-    private static volatile HashMap<String, Float> ControllerData = new HashMap<String, Float>();
+    //private static volatile HashMap<String, Float> ControllerData = new HashMap<String, Float>();
+    private Servo Hand_Rotator_Servo; // rotates the hand
+    private CRServo Hand_Servo;// open and closes the hand
     //private Controls_Update ControlsUpdate = new Controls_Update();
     //private Data_Update DataUpdate = new Data_Update();
     //private Thread dta = new Thread(DataUpdate);
@@ -20,7 +26,7 @@ public class Main extends LinearOpMode {
         /*
         Updates Data
          */
-        ControllerData.put("LeftStickX1", 0.0f);
+        /*ControllerData.put("LeftStickX1", 0.0f);
         ControllerData.put("LeftStickX2", 0.0f);
         ControllerData.put("RightStickX1", 0.0f);
         ControllerData.put("RightStickX2", 0.0f);
@@ -39,15 +45,19 @@ public class Main extends LinearOpMode {
         ControllerData.put("LeftTrigger1", 0.0f);
         ControllerData.put("LeftTrigger2", 0.0f);
         ControllerData.put("RightTrigger1", 0.0f);
-        ControllerData.put("RightTrigger2", 0.0f);
+        ControllerData.put("RightTrigger2", 0.0f);*/
+        Hand_Rotator_Servo = hardwareMap.get(Servo.class, "hand_rotator");
+        Hand_Servo = hardwareMap.get(CRServo.class, "hand_servo");
         waitForStart();
         //ControlsUpdate.runOpMode();
         //dta.start();
         if(opModeIsActive()){
             while(opModeIsActive()){
                 //telemetry.addData("Test", true);
+                Hand_Rotator_Servo.setPosition(Hand_Rotator_Servo.getPosition() + gamepad1.left_stick_x);
+                Hand_Servo.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
                 telemetry.update();
-                updateData();
+                //updateData();
             }
             //DataUpdate.active = false;
         }
@@ -57,16 +67,16 @@ public class Main extends LinearOpMode {
      * @param key the key for the item you want to get
      * @return the data of the requested item
      * */
-    public static float GetControllerDataItem(String key) throws NullPointerException{
+    /*public static float GetControllerDataItem(String key) throws NullPointerException{
 
         return ControllerData.get(key);
-    }
+    }*/
     /**
      * used for updating the controller data
      * @param key the key for the item that you want to change
      * @param data the data that you want to store
      * */
-    public static void updateControllerDataItem(String key, Float data) {
+    /*public static void updateControllerDataItem(String key, Float data) {
         ControllerData.replace(key, data);
     }
     public void addData(String name, float data){
@@ -77,8 +87,8 @@ public class Main extends LinearOpMode {
     }
     public void addData(String name, String data){
         telemetry.addData(name, data);
-    }
-    public void updateData(){
+    }*/
+    /*public void updateData(){
         addData("test", "Test");
         addData("Controller 1 left stick", (Main.GetControllerDataItem("LeftStickX1") + "," + Main.GetControllerDataItem("LeftStickY1")));
         addData("Controller 1 right stick", (Main.GetControllerDataItem("RightStickX1") + "," + Main.GetControllerDataItem("RightStickY1")));
@@ -90,5 +100,5 @@ public class Main extends LinearOpMode {
         addData("Controller 1 y", (Main.GetControllerDataItem("Y1") == 1f));
         addData("Controller 1 b", (Main.GetControllerDataItem("B1") == 1f));
         addData("Controller 1 a", (Main.GetControllerDataItem("A1") == 1f));
-    }
+    }*/
 }
