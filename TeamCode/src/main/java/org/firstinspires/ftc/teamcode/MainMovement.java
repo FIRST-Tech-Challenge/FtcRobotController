ca076731993c25918c70d4f8fdb004757c0bc7b1
eamon b <eamon_brisson@students.nksd.net>
2024-11-12 13:38:00 -0500

    more sleepiness

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainMovement.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import static android.os.SystemClock.sleep;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
