28ee73378b6a438d324564913a2aaabba9d27d7a
geppp41 <egep41414141@gmail.com>
2024-11-13 11:29:26 -0800

    Updated Main.java
    removed unneccary code and made code look a bit nicer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 
 
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
@@ -19,11 +20,12 @@ public class Main extends LinearOpMode {
 
     //private static volatile HashMap<String, Float> ControllerData = new HashMap<String, Float>();
     private Servo Hand_Rotator_Servo; // rotates the hand
-    private CRServo Hand_Servo;// open and closes the hand
+    private Servo Hand_Servo;// open and closes the hand
     private DcMotor Front_Left_Wheel;
     private DcMotor Front_Right_Wheel;
     private DcMotor Back_Left_Wheel;
     private  DcMotor Back_Right_Wheel;
+    private DcMotor Arm_Motor;
     //private Controls_Update ControlsUpdate = new Controls_Update();
     //private Data_Update DataUpdate = new Data_Update();
     //private Thread dta = new Thread(DataUpdate);
@@ -32,76 +34,84 @@ public class Main extends LinearOpMode {
         /*
         Updates Data
          */
-
-        Hand_Rotator_Servo = hardwareMap.get(Servo.class, "hand_rotator");
-        Hand_Servo = hardwareMap.get(CRServo.class, "hand_servo");
-        Front_Left_Wheel = hardwareMap.get(DcMotor.class, "Front_Left_Wheel");
-        Back_Left_Wheel = hardwareMap.get(DcMotor.class, "Back_Left_Wheel");
-        Front_Right_Wheel = hardwareMap.get(DcMotor.class, "Front_Right_Wheel");
-        Back_Right_Wheel = hardwareMap.get(DcMotor.class, "Back_Right_Wheel");
-
+        //create failsafe so if joysticks are not being used then switch between joystick and dpad for checking movement
+        initialize_motors();//motor setup
+        initialize_servos();//setvo setup
+        //wait for start to be pressed
         waitForStart();
-        //ControlsUpdate.runOpMode();
-        //dta.start();
+        //if op mode is active then we can continue
         if(opModeIsActive()){
             while(opModeIsActive()){
                 float left_stick_y = gamepad1.left_stick_y;
                 float left_stick_x = gamepad1.left_stick_x;
                 float right_stick_x = gamepad1.right_stick_x;
                 //telemetry.addData("Test", true);
-                Back_Left_Wheel.setDirection(DcMotorSimple.Direction.FORWARD);
-                Back_Right_Wheel.setDirection(DcMotorSimple.Direction.FORWARD);
-                Front_Left_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-                Front_Right_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
-                Hand_Rotator_Servo.setPosition(Hand_Rotator_Servo.getPosition() + gamepad2.left_stick_x);
-                Hand_Servo.setPower(gamepad2.right_trigger - gamepad2.left_trigger);
+                telemetry.addData("left_stick_x", gamepad2.left_stick_x);
+                Arm_Motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                initialize_direction();//sets up the direction for motors and servos
+                Arm_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                Hand_Rotator_Servo.setPosition(Hand_Rotator_Servo.getPosition() + gamepad2.left_stick_x*0.001);
+                //Hand_Servo.setPosition(Hand_Servo.getPosition());
+                telemetry.addData("Hand Servo Position", Hand_Servo.getPosition());
+                telemetry.addData("Hand Servo Position incorrect", Hand_Rotator_Servo.getPosition());
+                telemetry.addData("direction", Hand_Servo.getDirection());
+                telemetry.addData("arm position", Arm_Motor.getCurrentPosition());
+
+                //Hand_Servo.setDirection(gamepad2.a ? Servo.Direction.REVERSE: Servo.Direction.FORWARD);
+                //Hand_Servo.setPosition(gamepad2.a ? 0.65: 0);
+
+                if (gamepad2.left_trigger > 0){
+                      if (Hand_Servo.getPosition() > 0){
+                          //Hand_Servo.setPosition(Hand_Servo.getPosition() - (gamepad2.left_trigger*0.001));
+                          //Hand_Servo.setPosition(Hand_Servo.getPosition() - ((Math.round(gamepad2.left_trigger * Math.pow(10, 2))/Math.pow(10, 2))*0.001));
+                          Hand_Servo.setPosition(0);
+                      }
+                } else if (gamepad2.right_trigger > 0){
+                    if (Hand_Servo.getPosition() < 0.65){
+                        //Hand_Servo.setPosition(Hand_Servo.getPosition() + (gamepad2.right_trigger*0.001));
+                        //Hand_Servo.setPosition(Hand_Servo.getPosition() + ((Math.round(gamepad2.right_trigger * Math.pow(10, 2))/Math.pow(10, 2))*0.001));
+                        Hand_Servo.setPosition(0.65);
+                    }
+                } else if (gamepad2.b) {
+                    Hand_Servo.setPosition(0);
+                }
+                telemetry.addData("Hand Servo grip", Hand_Servo.getPosition());
+
+
+                Arm_Motor.setTargetPosition(0);
+                //Hand_Rotator_Servo.setPosition(0.5);
                 Back_Left_Wheel.setPower(((left_stick_y - left_stick_x)*-1) + right_stick_x);
                 Back_Right_Wheel.setPower(((left_stick_y + left_stick_x)*-1) - right_stick_x);
                 Front_Left_Wheel.setPower(((left_stick_y + left_stick_x)*-1) + right_stick_x);
                 Front_Right_Wheel.setPower(((left_stick_y - left_stick_x)*-1) - right_stick_x);
+                //update loop
                 telemetry.update();
-                //updateData();
             }
             //DataUpdate.active = false;
         }
     }
-    /**
-     * used to get controller data
-     * @param key the key for the item you want to get
-     * @return the data of the requested item
-     * */
-    /*public static float GetControllerDataItem(String key) throws NullPointerException{
+    private void initialize_motors() {
+        Front_Left_Wheel = hardwareMap.get(DcMotor.class, "front_left_wheel");
+        Back_Left_Wheel = hardwareMap.get(DcMotor.class, "back_left_wheel");
+        Front_Right_Wheel = hardwareMap.get(DcMotor.class, "front_right_wheel");
+        Back_Right_Wheel = hardwareMap.get(DcMotor.class, "back_right_wheel");
+        Arm_Motor = hardwareMap.get(DcMotor.class, "arm_control");
+    }
+    private void initialize_servos() {
+        Hand_Rotator_Servo = hardwareMap.get(Servo.class, "hand_rotator");
+        Hand_Servo = hardwareMap.get(Servo.class, "hand_servo");
+    }
+    private void initialize_direction() {
+        Back_Left_Wheel.setDirection(DcMotorSimple.Direction.FORWARD);
+        Back_Right_Wheel.setDirection(DcMotorSimple.Direction.FORWARD);
+        Front_Left_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        Front_Right_Wheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        Hand_Rotator_Servo.setDirection(Servo.Direction.REVERSE);
+        Hand_Servo.setDirection(Servo.Direction.FORWARD);
 
-        return ControllerData.get(key);
-    }*/
-    /**
-     * used for updating the controller data
-     * @param key the key for the item that you want to change
-     * @param data the data that you want to store
-     * */
-    /*public static void updateControllerDataItem(String key, Float data) {
-        ControllerData.replace(key, data);
     }
-    public void addData(String name, float data){
-        telemetry.addData(name, data);
+    private void display_data(){
+        //create sections
+        //1. directions 2. servo power 3. servo positions
     }
-    public void addData(String name, boolean data){
-        telemetry.addData(name, data);
-    }
-    public void addData(String name, String data){
-        telemetry.addData(name, data);
-    }*
-    /*public void updateData(){
-        addData("test", "Test");
-        addData("Controller 1 left stick", (Main.GetControllerDataItem("LeftStickX1") + "," + Main.GetControllerDataItem("LeftStickY1")));
-        addData("Controller 1 right stick", (Main.GetControllerDataItem("RightStickX1") + "," + Main.GetControllerDataItem("RightStickY1")));
-        addData("Controller 1 left trigger", (float)Main.GetControllerDataItem("LeftTrigger1"));
-        addData("Controller 1 right trigger", (float)Main.GetControllerDataItem("RightTrigger1"));
-        addData("Controller 1 left bumper", (Main.GetControllerDataItem("LeftBumper1") == 1f));
-        addData("Controller 1 right bumper", (Main.GetControllerDataItem("RightBumper1") == 1f));
-        addData("Controller 1 x", (Main.GetControllerDataItem("X1") == 1f));
-        addData("Controller 1 y", (Main.GetControllerDataItem("Y1") == 1f));
-        addData("Controller 1 b", (Main.GetControllerDataItem("B1") == 1f));
-        addData("Controller 1 a", (Main.GetControllerDataItem("A1") == 1f));
-    }*/
 }
