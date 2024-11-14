0df7224fc6cd5a3800c0a51f3f247bbc1ee9adac
Vbwfe <67kevin26@gmail.com>
2024-11-13 22:02:37 -0500

    Fixed Battery Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Battery/Battery.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Battery/Battery.java
@@ -1,25 +1,26 @@
 package org.firstinspires.ftc.teamcode.Battery;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
-
+import com.qualcomm.robotcore.util.ElapsedTime;
+@Config
 public class Battery {
     HardwareMap hardwareMap;
     VoltageSensor voltageSensor;
+    public ElapsedTime timer = new ElapsedTime();
     public Battery (HardwareMap hardwareMap){
         this.hardwareMap = hardwareMap;
         voltageSensor = hardwareMap.get(VoltageSensor.class, "Voltage Sensor");
     }
 
-    public double getVoltage(){
-        double volts = 0;
-
-            double voltage = voltageSensor.getVoltage();
-            if (voltage > 0) {
-                volts = Math.min(volts, voltage);
-            }
-        return volts;
+    public double getVoltage() {
+        double voltage = voltageSensor.getVoltage();
+        double timeLastUpdate = timer.seconds();
+        if (timeLastUpdate > 0.5) {
+            voltage = voltageSensor.getVoltage();
+            timer.reset();
+        }
+        return voltage;
     }
 }
