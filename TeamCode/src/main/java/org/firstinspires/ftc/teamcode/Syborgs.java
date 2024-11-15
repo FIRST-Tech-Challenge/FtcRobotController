c00d36f35e9c2ee2730863d7276ea4aa56d9ad28
Justin Liu <liujustin604@gmail.com>
2024-11-15 15:49:57 -0500

    day 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Syborgs.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Syborgs extends LinearOpMode {
+    private DcMotor
+            frontLeftDrive,
+            frontRightDrive,
+            backLeftDrive,
+            backRightDrive;
+    private DcMotor armMotor;
+    private DcMotor liftMotor;
+    private DcMotor hangMotor;
+    private CRServo intake;
+    private Servo wrist;
+
+    final double ARM_TICKS_PER_DEGREE = 28.0 * 250047.0 / 4913.0 * 100.0 / 20.0 / 360.0;
+    final double ARM_COLLAPSED_INTO_ROBOT = 0;
+    final double ARM_COLLECT = 0 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER = 15 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN = 90 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW = 90 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK = 110 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT = 10 * ARM_TICKS_PER_DEGREE;
+    final double INTAKE_COLLECT = -1.0;
+    final double INTAKE_OFF = 0.0;
+    final double INTAKE_DEPOSIT = 0.5;
+    final double WRIST_FOLDED = 1.0 / 6.0;
+    final double WRIST_FOLDED_OUT = 0.5;
+    // slightly change the position if it isn't working
+    final double FUDGE_FACTOR = 15.0 * ARM_TICKS_PER_DEGREE;
+    double armPosition = Math.floor(ARM_COLLAPSED_INTO_ROBOT);
+    double getArmPositionFudgeFactor;
+
+    final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
+    final double LIFT_COLLAPSED = 0.0 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_LOW_BASKET = 0.0 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_HIGH_BASKET = 480.0 * LIFT_TICKS_PER_MM;
+    double liftPosition = LIFT_COLLAPSED;
+    double cycleTime = 0, loopTime = 0, oldTime = 0, armLiftComp = 0;
+
+    @Override
+    public void runOpMode() {
+        double left, right, forward, rotate, max;
+        frontLeftDrive = hardwareMap.dcMotor.get("frontLeftDrive");
+        frontRightDrive = hardwareMap.dcMotor.get("frontRightDrive");
+        backLeftDrive = hardwareMap.dcMotor.get("backLeftDrive");
+        backRightDrive = hardwareMap.dcMotor.get("backRightDrive");
+        armMotor = hardwareMap.dcMotor.get("armMotor");
+        liftMotor = hardwareMap.dcMotor.get("liftMotor");
+        hangMotor = hardwareMap.dcMotor.get("hangMotor");
+
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        hangMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        armMotor.setTargetPosition(0);
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        liftMotor.setDirection(DcMotor.Direction.REVERSE);
+        liftMotor.setTargetPosition(0);
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+}
