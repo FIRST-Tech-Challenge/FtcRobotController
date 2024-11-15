40deb19b8f1f966281e90a528459e1cb186f537f
eamon b <eamon_brisson@students.nksd.net>
2024-11-15 10:20:39 -0500

    lots of additions
    * adds comment code to a lot of things
    * flips right side motors so they should work now and be mismo as the diagram we have saved
    * hopefully fixed the linear slide bug of it moving even after opmode has been turned off (silly denim shorts)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainMovement.java
@@ -8,36 +8,36 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="MainMovement", group="Linear OpMode")
 public class MainMovement extends LinearOpMode {
-    //move forward: all = cc
-    //move backwards all = c
-    //strafe left = BL, FR = cc, BR, FL = c
 
     private DcMotor leftBack; //Initializes Back-Left direct current motor for the driving function of our robot, gary.
     private DcMotor rightBack; //Initializes Back-Right direct current motor for the driving function of our robot, gary.
     private DcMotor leftFront; //Initializes Front-Left direct current motor for the driving function of our robot, gary.
     private DcMotor rightFront; //Initializes Front-Right direct current motor for the driving function of our robot, gary.
 
-    private Servo clawServo;
-    private CRServo clawRotate;
-    private DcMotor linearSlide;
-
-    final float joystickDeadzone = 0.1f;
-    final float speedSlow = 0.5f;
-    final float speedFast = 1.5f;
-    float rotationSpeed = 1f;
-
-    boolean usingLStick;
-    // declaring the joysticks here because the values need to be updated in OpMode ??? (maybe i think???)
+    // joysticks' position variables here
     public float LjoystickX;
     public float LjoystickY;
     public float RjoystickX;
     public float RjoystickY;
 
-    boolean open = false;
+    final float joystickDeadzone = 0.1f; // Area where joystick will not detect input
+    final float speedSlow = 0.5f; // Slow mode for robot chassis movement
+    final float speedFast = 1.5f; // Speedy mode for robot chassis movement
+    float rotationSpeed = 1f; // Robot rotation speed multiplier, 1.5 for fast mode, 0.5 for slow mode
 
-    //speeds
+    boolean usingLStick; // Detect whether left stick is being used or not- for prioritizing rotation over directional movement
+
+    
+    private Servo clawServo; // servo to control claw opening and closing
+    private CRServo clawRotate; // servo to control the rotation of the claw
+    private DcMotor linearSlide; // motor to control linear slide
+
+    // Speeds for claw and linear slide
     private float clawSpeed = 1.0f;
     private float linearSlideSpeed = 0.75f;
+
+    boolean open = false; // Is the claw open? False = closed, true = open 
+    
 
     @Override
     public void runOpMode() {
@@ -51,50 +51,55 @@ public class MainMovement extends LinearOpMode {
         clawRotate = hardwareMap.get(CRServo.class, "cr"); // claw rotate
         linearSlide = hardwareMap.get(DcMotor.class, "ls"); // linear slide
 
+        linearSlide.setPower(0); // zero the linear slide's power so it doesn't move while not active
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
         waitForStart(); //waits for play on the driver hub :3
 
         while (opModeIsActive()) {
-            LjoystickX = gamepad1.left_stick_x; //shouldn't this stuff be called before waitForStart?
+            // detecting the x and y position of both joysticks
+            LjoystickX = gamepad1.left_stick_x; 
             LjoystickY = gamepad1.left_stick_y;
             RjoystickX = gamepad1.right_stick_x;
             RjoystickY = gamepad1.right_stick_y;
 
             epicRotationMovement(); // rotation on gary, the robot
             legendaryStrafeMovement(); // movement on gary
-            LimbMovement();
+            LimbMovement(); // controlling linear slide and claw
             telemetry.update(); //update output screen
         }
 
     }
 
     //////////////////////// START OF MOVEMENT CODE ////////////////////////
+
     //////////////////////// START OF MOVEMENT CODE ////////////////////////
+    
     //////////////////////// START OF MOVEMENT CODE ////////////////////////
-    //////////////////////// START OF MOVEMENT CODE ////////////////////////
-    //////////////////////// START OF MOVEMENT CODE ////////////////////////
+    
     private void setMotorPowers(float BL, float BR, float FL, float FR, float speed) {
         // set all the motor powers to the floats defined
-        leftBack.setPower(-BL* speed * 0.5);
-        rightBack.setPower(BR* speed * 0.5);
-        leftFront.setPower(-FL* speed * 0.5);
-        rightFront.setPower(FR* speed * 0.5);
+        leftBack.setPower(-BL * speed * 0.5);
+        
+        rightFront.setPower(BR * speed * 0.5);     // ATTENTION // - rightFront motor is set to the BR b/c motors on robot are flipped
+        
+        leftFront.setPower(-FL * speed * 0.5);
+        
+        rightBack.setPower(FR * speed * 0.5);     // ATTENTION // - rightBack motor is set to the FR b/c motors on robot are flipped
     }
 
     private void epicRotationMovement() {
         // rotates the robot if left stick is not being used (movement takes priorities)
         if ((Math.abs(RjoystickX) >= joystickDeadzone / 2) && !usingLStick) {
             if(RjoystickX < 0) {
-
-                telemetry.addData("left", RjoystickX);
                 setMotorPowers(1, -1, 1, -1, -Math.abs(RjoystickX) * rotationSpeed); // clockwise rotation
+                telemetry.addData("Right Stick rotating LEFT: ", RjoystickX);
 
             } else if (RjoystickX > 0) {
-                
                 setMotorPowers(-1, 1, -1, 1, -Math.abs(RjoystickX) * rotationSpeed); // counter-clockwise rotation
-                telemetry.addData("right", RjoystickX);
+                telemetry.addData("Right Stick rotating RIGHT: ", RjoystickX);
                 
             }
         }
@@ -110,76 +115,78 @@ public class MainMovement extends LinearOpMode {
     // |__/                            |__/   |___/ |___/   |___/
 
     private void legendaryStrafeMovement() {
-        float maxSpeed = 1.0f;
-        double addSpeed = Math.sqrt(LjoystickX*LjoystickX + LjoystickY*LjoystickY);
-        float netS;
+        float maxSpeed = 1.0f; // Cap for speed robot can travel
+        double addSpeed = Math.sqrt(LjoystickX * LjoystickX + LjoystickY * LjoystickY); // Added speed by calculating the distance the joystick is from the center
+        float netS; // speed the motor actually uses
 
-        if(gamepad1.left_bumper) { // holding x puts the robot into slow movement mode
-            netS = speedSlow;
+        // Alternate between SLOW && FAST mode depending on which bumper is held :P
+        if (gamepad1.left_bumper) {    // slow mode !
+            netS = speedSlow;    // Speed is set to a slow constant speed for more precise movements 
             rotationSpeed = speedSlow;
-
-        } else if(gamepad1.right_bumper){
-            netS = (Math.min(maxSpeed, (float) (addSpeed - joystickDeadzone) / (1.0f - joystickDeadzone))) * speedFast; //net speed
+            
+        } else if (gamepad1.right_bumper) {     // fast mode !
+            netS = (Math.min(maxSpeed, (float) (addSpeed - joystickDeadzone) / (1.0f - joystickDeadzone))) * speedFast; // Speed is multiplied by the speedFast variable 
             rotationSpeed = speedFast;
 
-        }else {
-            netS = Math.min(maxSpeed, (float) (addSpeed - joystickDeadzone) / (1.0f - joystickDeadzone)); //net speed
+        } else {    // default- no bumpers are held !
+            netS = Math.min(maxSpeed, (float) (addSpeed - joystickDeadzone) / (1.0f - joystickDeadzone)); // Speed is set to default speed
             rotationSpeed = 1f;
         }
 
-        // calculates the angle of the joystick in radians --> degrees
+        // calculates the angle of the joystick in radians --> degrees..
         double LangleInRadians = Math.atan2(-LjoystickY, LjoystickX);
         double LangleInDegrees = LangleInRadians * (180 / Math.PI);
 
-        // strafe based on joystick angle
+        // strafe based on joystick angle :D
         if (Math.abs(LjoystickX) > joystickDeadzone || Math.abs(LjoystickY) > joystickDeadzone) {
             usingLStick = true;
-            //if not in dead zone
+            
+            //if stick is past the deadzone ->
             if (LangleInDegrees >= -22.5 && LangleInDegrees <= 22.5) {
-                // right quadrant, move right
+                // right quadrant
                 setMotorPowers(-1, 1, 1, -1, netS);
-                telemetry.addData("Left Stick in RIGHT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "RIGHT");
 
             } else if (LangleInDegrees > 22.5 && LangleInDegrees < 67.5) {
                 // top-right quadrant
                 setMotorPowers(0, 1, 1, 0, netS);
-                telemetry.addData("Left Stick in TOP-RIGHT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "TOP RIGHT");
 
             } else if (LangleInDegrees > -67.5 && LangleInDegrees < -22.5) {
                 // bottom-right quadrant
                 setMotorPowers(-1, 0, 0, -1, netS);
-                telemetry.addData("Left Stick in BOTTOM-RIGHT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "BOTTOM RIGHT");
 
             } else if (LangleInDegrees >= 67.5 && LangleInDegrees <= 112.5) {
                 // top quadrant
                 setMotorPowers(1, 1, 1, 1, netS);
-                telemetry.addData("Left Stick in TOP quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "TOP");
 
             } else if (LangleInDegrees > -112.5 && LangleInDegrees < -67.5) {
                 // bottom quadrant
                 setMotorPowers(-1, -1, -1, -1, netS);
-                telemetry.addData("Left Stick in BOTTOM quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "BOTTOM");
 
             } else if (LangleInDegrees > 112.5 && LangleInDegrees < 157.5) {
                 // top-left quadrant
                 setMotorPowers(1, 0, 0, 1, netS);
-                telemetry.addData("Left Stick in TOP-LEFT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "TOP LEFT");
 
             } else if (LangleInDegrees > -157.5 && LangleInDegrees < -112.5) {
                 // bottom-left quadrant
                 setMotorPowers(0, -1, -1, 0, netS);
-                telemetry.addData("Left Stick in BOTTOM-LEFT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "BOTTOM LEFT");
 
             } else if (LangleInDegrees >= 157.5 || LangleInDegrees <= -157.5) {
                 // left quadrant
                 setMotorPowers(1, -1, -1, 1, netS);
-                telemetry.addData("Left Stick in LEFT quadrant", null);
+                telemetry.addData("Left Stick quadrant: ", "LEFT");
 
             }
 
         } else {
             usingLStick = false;
-            setMotorPowers(0, 0, 0, 0, 0);
+            setMotorPowers(0, 0, 0, 0, 0); // zero all motor powers
         }
 
 
@@ -189,11 +196,10 @@ public class MainMovement extends LinearOpMode {
     }
 
     //////////////////////// END OF MOVEMENT CODE ////////////////////////
-    //////////////////////// END OF MOVEMENT CODE ////////////////////////
-    //////////////////////// END OF MOVEMENT CODE ////////////////////////
-    //////////////////////// END OF MOVEMENT CODE ////////////////////////
+
     //////////////////////// END OF MOVEMENT CODE ////////////////////////
 
+    //////////////////////// END OF MOVEMENT CODE ////////////////////////
 
     private void LimbMovement() {
 
