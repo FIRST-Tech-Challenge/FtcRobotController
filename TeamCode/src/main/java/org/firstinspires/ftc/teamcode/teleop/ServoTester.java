a2d5e0589767ae318fd284c420876ee20f7ede16
RachitTamrakar <Rtam0011@student.monash.edu>
2024-11-14 21:02:37 -0800

    Positions for Arm Claw and Spec Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
@@ -10,15 +10,18 @@ import com.qualcomm.robotcore.hardware.Servo;
 public class ServoTester extends LinearOpMode {
     public static double WRIST_SERVO_POSITION = 0;
     public static double SPEC_SERVO_POSITION = 0;
+    public static double INTAKE_CLAW_POSITION = 0;
     public void runOpMode() throws InterruptedException {
-        final Servo rightWristServo, specServo;
+        final Servo rightWristServo, specServo, clawIntake;
         rightWristServo = hardwareMap.get(Servo.class, "rightWristServo");
         specServo = hardwareMap.get(Servo.class, "specServo");
+        clawIntake = hardwareMap.get(Servo.class, "clawIntake");
         waitForStart();
 
         while(opModeIsActive()){
             rightWristServo.setPosition(WRIST_SERVO_POSITION);
             specServo.setPosition(SPEC_SERVO_POSITION);
+            clawIntake.setPosition(INTAKE_CLAW_POSITION);
         }
     }
 
