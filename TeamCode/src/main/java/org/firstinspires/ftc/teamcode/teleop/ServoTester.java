88f28384bb703d17fb5e21baf533019515bb37fe
RachitTamrakar <Rtam0011@student.monash.edu>
2024-11-14 19:59:00 -0800

    Updated Spec Servo Positions And start poses for auto trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
@@ -1,24 +1,24 @@
 package org.firstinspires.ftc.teamcode.teleop;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 @TeleOp
 public class ServoTester extends LinearOpMode {
-    public static double SERVO_POSITION = 0;
+    public static double WRIST_SERVO_POSITION = 0;
+    public static double SPEC_SERVO_POSITION = 0;
     public void runOpMode() throws InterruptedException {
-        final Servo rightWristServo;
+        final Servo rightWristServo, specServo;
         rightWristServo = hardwareMap.get(Servo.class, "rightWristServo");
+        specServo = hardwareMap.get(Servo.class, "specServo");
         waitForStart();
 
         while(opModeIsActive()){
-            rightWristServo.setPosition(SERVO_POSITION);
+            rightWristServo.setPosition(WRIST_SERVO_POSITION);
+            specServo.setPosition(SPEC_SERVO_POSITION);
         }
     }
 
