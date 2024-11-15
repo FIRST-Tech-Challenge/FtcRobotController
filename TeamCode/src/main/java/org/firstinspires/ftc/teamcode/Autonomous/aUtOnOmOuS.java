62330ffee4f889abe1cdda641638e10aa8307833
NTCheese <arirahmani2021@gmail.com>
2024-11-14 21:16:38 -0700

    amonug sussy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/aUtOnOmOuS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/aUtOnOmOuS.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Systems.IMU;
+import org.firstinspires.ftc.teamcode.Systems.Input;
 import org.firstinspires.ftc.teamcode.Systems.Motors;
 
 @Autonomous(name="Main")
@@ -10,18 +12,42 @@ import org.firstinspires.ftc.teamcode.Systems.Motors;
 public class aUtOnOmOuS extends LinearOpMode {
 
     Motors motor;
+    IMU imu;
+    Input input;
 
     public void runOpMode() throws InterruptedException {
 
         motor = new Motors(hardwareMap);
+        imu = new IMU(hardwareMap);
+        input = new Input(hardwareMap);
+
+        imu.SetYaw();
+
+
+
+        int step = 0;
 
         waitForStart();
         //run once
 
         while (opModeIsActive())
         {
-            motor.MoveMotor(0, 100);
-            //Runs here NEVRER (n't)
+            double yaw = imu.getAngle('y');
+
+
+            if(step == 0)
+            {
+                if (yaw < 85 && yaw > 105 ) {
+                    input.Spin(50);
+                }
+                else {
+                    step++;
+                }
+            }
+
+
+            telemetry.addData("IMU YAW:", "yaw (%.2f),", yaw);
+            telemetry.update(); // telemtryy
         }
     }
 }
