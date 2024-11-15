e0fe35519af176321769f5454f5d950017a7ac1d
oobeegamer <ritvikkr@outlook.com>
2024-11-14 19:18:32 -0800

    Changed servo positions for wrist

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServoTester.java
@@ -3,20 +3,23 @@ package org.firstinspires.ftc.teamcode.teleop;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @Config
 @TeleOp
-public class ServoTester {
+public class ServoTester extends LinearOpMode {
+    public static double SERVO_POSITION = 0;
     public void runOpMode() throws InterruptedException {
-
         final Servo rightWristServo;
         rightWristServo = hardwareMap.get(Servo.class, "rightWristServo");
-        int SERVO_POSITION= 0;
-                rightWristServo.setPosition(SERVO_POSITION);
+        waitForStart();
 
+        while(opModeIsActive()){
+            rightWristServo.setPosition(SERVO_POSITION);
+        }
     }
 
 }
