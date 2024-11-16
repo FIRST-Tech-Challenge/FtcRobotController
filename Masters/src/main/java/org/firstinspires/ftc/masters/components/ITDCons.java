479f68a452dad8839b83bc3e4850a1bfae76fd45
solinexi <solinexi@gmail.com>
2024-11-16 10:10:43 -0700

    meow

---
+++ Masters/src/main/java/org/firstinspires/ftc/masters/components/ITDCons.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.masters.components;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
+@Config
+public class ITDCons {
+
+//    public static double skewampus = 0;
+//    public static double skewompus = 0.5;
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+
+    public static double zero = 0;
+    public static double pointFive = 0.5;
+
+}
+
+/*
+
+   ╚ ╔ ╩ ╦ ╠ ═ ╬ ╣ ║ ╗ ╝
+       THE WISDOM!
+   T H E   W I S D O M !
+                    ║
+            ╔═══════╣
+            ║  ╔════╣
+            ║  ║    ║
+            ║  ║    ║
+                    ║
+
+ */
