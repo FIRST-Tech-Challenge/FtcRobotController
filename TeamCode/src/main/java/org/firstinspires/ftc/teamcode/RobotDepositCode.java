9817fa59ea8fb64b36c00142ddea184602691ed2
ThatOneIdiot333 <ken.ma.yyc@gmail.com>
2024-11-17 15:10:28 -0700

    Changed slightly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDepositCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotDepositCode.java
@@ -2,12 +2,13 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.teamcode.RobotHardware;
 
-public class RobotDepositCode extends OpMode {
+public class RobotDepositCode {
 
     private RobotHardware robot = new RobotHardware();
 
@@ -15,7 +16,6 @@ public class RobotDepositCode extends OpMode {
         START_POSITION,
         LIFT_EXTEND,
         ARM_WRIST_EXTEND,
-        DUMP,
         LIFT_RETRACT
     }
 
@@ -30,6 +30,8 @@ public class RobotDepositCode extends OpMode {
     public double depositServoClosePosition = 0;
     public double depositWristServoInitialPosition = 0;
     public double depositWristServoExtendedPosition = 0.54;
+
+    public Gamepad gamepad1;
 
 
 
@@ -47,26 +49,25 @@ public class RobotDepositCode extends OpMode {
 
     public void init() {
 
-        deposit_Arm_Left_Servo.setPosition(0);
-        deposit_Arm_Left_Servo.setDirection(Servo.Direction.FORWARD);
-        deposit_Arm_Left_Servo.setPosition(0);
-        deposit
-        deposit_Wrist_Servo.setDirection(Servo.Direction.FORWARD);
-        deposit_Wrist_Servo.setPosition(0);
-        deposit_Claw_Servo.setDirection(Servo.Direction.FORWARD);
-        deposit_Claw_Servo.setPosition(0.11);
+        robot.depositLeftArmServo.setPosition(0);
+        robot.depositRightArmServo.setDirection(Servo.Direction.FORWARD);
+        robot.depositLeftArmServo.setPosition(0);
+        robot.depositRightArmServo.setDirection(Servo.Direction.REVERSE);
+        robot.depositWristServo.setPosition(0);
+        robot.depositWristServo.setDirection(Servo.Direction.FORWARD);
+        robot.depositClawServo.setPosition(0.11);
+        robot.depositClawServo.setDirection(Servo.Direction.FORWARD);
         robot.liftMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        vs_Left_Motor.setTargetPosition(0);
-        vs_Left_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vs_Left_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vs_Left_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        vs_Left_Motor.setPower(depositMotorPower);
-        vs_Right_Motor.setDirection(DcMotorSimple.Direction.FORWARD);
-        vs_Right_Motor.setTargetPosition(0);
-        vs_Right_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vs_Right_Motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        vs_Right_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        vs_Right_Motor.setPower(depositMotorPower);
+        robot.liftMotorLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.liftMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.liftMotorLeft.setPower(depositMotorPower);
+        robot.liftMotorRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        robot.liftMotorRight.setTargetPosition(0);
+        robot.liftMotorRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.liftMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        robot.liftMotorRight.setPower(depositMotorPower);
 
 
 
@@ -77,76 +78,57 @@ public class RobotDepositCode extends OpMode {
         switch (currentState) {
             case START_POSITION: {
                 robot.liftMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                vs_Right_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                vs_Right_Motor.setTargetPosition(0);
-                vs_Right_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                vs_Right_Motor.setPower(DepositMotorPower);
-                vs_Left_Motor.setTargetPosition(0);
-                vs_Left_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                vs_Left_Motor.setPower(DepositMotorPower);
-                deposit_Arm_Left_Servo.setPosition(LeftDepositArmServoInitialPosition);
-                deposit_Arm_Right_Servo.setPosition(RightDepositArmServoInitialPosition);
-                deposit_Wrist_Servo.setPosition(DepositWristServoInitialPosition);
+                robot.liftMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+                robot.liftMotorRight.setTargetPosition(0);
+                robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.liftMotorRight.setPower(DepositMotorPower);
+                robot.liftMotorLeft.setTargetPosition(0);
+                robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.liftMotorLeft.setPower(DepositMotorPower);
+                robot.depositLeftArmServo.setPosition(LeftDepositArmServoInitialPosition);
+                robot.depositRightArmServo.setPosition(RightDepositArmServoInitialPosition);
+                robot.depositWristServo.setPosition(DepositWristServoInitialPosition);
                 currentState = DepositSlideState.LIFT_EXTEND;
             }
             break;
             case LIFT_EXTEND:
                 if (gamepad1.y && debounceTimer.seconds() > DEBOUNCE_THRESHOLD) {
                     debounceTimer.reset();
-                    deposit_Claw_Servo.setPosition(depositServoClosePosition);
-                    vs_Right_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    vs_Left_Motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-                    vs_Left_Motor.setTargetPosition(DepositSlideMotorExtendPosition);
-                    vs_Right_Motor.setTargetPosition(DepositSlideMotorExtendPosition);
-                    vs_Left_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    vs_Right_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    vs_Right_Motor.setPower(DepositMotorPower);
-                    vs_Left_Motor.setPower(DepositMotorPower);
+                    robot.depositClawServo.setPosition(depositServoClosePosition);
+                    robot.liftMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    robot.liftMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                    robot.liftMotorLeft.setTargetPosition(DepositSlideMotorExtendPosition);
+                    robot.liftMotorRight.setTargetPosition(DepositSlideMotorExtendPosition);
+                    robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.liftMotorRight.setPower(DepositMotorPower);
+                    robot.liftMotorLeft.setPower(DepositMotorPower);
                     currentState = DepositSlideState.ARM_WRIST_EXTEND;
 
                 }
                 break;
             case ARM_WRIST_EXTEND:
                 if (DE_IsAtPosition(DepositSlideMotorExtendPosition)) {
-                    deposit_Arm_Left_Servo.setPosition(LeftDepositArmServoExtendedPosition);
-                    deposit_Arm_Right_Servo.setPosition(RightDepositArmServoExtendedPosition);
-                    deposit_Wrist_Servo.setPosition(DepositWristServoExtendedPosition);
-                    currentState = DepositSlideState.DUMP;
-                }
-                break;
-            case DUMP:
-                if (gamepad1.x && debounceTimer.seconds() > DEBOUNCE_THRESHOLD) {
-                    dumpTime.reset();
-                    debounceTimer.reset();
-                    while (dumpTime.seconds() < DUMPTIME_THRESHOLD) {
-                        telemetry.addData("DumpTime", dumpTime.seconds());
-                        telemetry.update();
-                    }
-                    dumpTime.reset();
-                    if (dumpTime.seconds() > DUMPTIME_THRESHOLD);
-                    deposit_Claw_Servo.setPosition(DepositServoOpenPosition);
+                    robot.depositLeftArmServo.setPosition(LeftDepositArmServoExtendedPosition);
+                    robot.depositRightArmServo.setPosition(RightDepositArmServoExtendedPosition);
+                    robot.depositWristServo.setPosition(DepositWristServoExtendedPosition);
                     currentState = DepositSlideState.LIFT_RETRACT;
-
-
-
                 }
                 break;
             case LIFT_RETRACT:
-                if (retractTime.seconds() > RETRACTTIME_THRESHOLD) {
-                    telemetry.addData("RetractTime", retractTime.seconds());
-                    telemetry.update();
+                if (SERVO_IsAtPosition(depositServoOpenPosition) && retractTime.seconds() > RETRACTTIME_THRESHOLD) {
                     robot.liftMotorRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
                     robot.liftMotorLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-                    vs_Right_Motor.setTargetPosition(DepositSlideMotorRetractPosition);
-                    vs_Left_Motor.setTargetPosition(DepositSlideMotorRetractPosition);
-                    vs_Right_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    vs_Left_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    vs_Right_Motor.setPower(DepositMotorPower);
-                    vs_Left_Motor.setPower(DepositMotorPower);
-                    deposit_Arm_Left_Servo.setPosition(LeftDepositArmServoInitialPosition);
-                    deposit_Arm_Right_Servo.setPosition(RightDepositArmServoInitialPosition);
-                    deposit_Claw_Servo.setPosition(DepositServoOpenPosition);
-                    deposit_Wrist_Servo.setPosition(DepositWristServoInitialPosition);
+                    robot.liftMotorRight.setTargetPosition(DepositSlideMotorRetractPosition);
+                    robot.liftMotorLeft.setTargetPosition(DepositSlideMotorRetractPosition);
+                    robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.liftMotorRight.setPower(DepositMotorPower);
+                    robot.liftMotorLeft.setPower(DepositMotorPower);
+                    robot.depositLeftArmServo.setPosition(LeftDepositArmServoInitialPosition);
+                    robot.depositRightArmServo.setPosition(RightDepositArmServoInitialPosition);
+                    robot.depositClawServo.setPosition(DepositServoOpenPosition);
+                    robot.depositWristServo.setPosition(DepositWristServoInitialPosition);
                 }
                 if (DE_IsAtPosition(DepositSlideMotorRetractPosition)) {
                     currentState = DepositSlideState.START_POSITION;
@@ -163,32 +145,33 @@ public class RobotDepositCode extends OpMode {
         }
         if (gamepad1.right_bumper && debounceTimer.seconds() > DEBOUNCE_THRESHOLD) {
             debounceTimer.reset();
-            .setPosition(depositServoOpenPosition);
+            robot.depositClawServo.setPosition(depositServoOpenPosition);
         }
         if (gamepad1.right_trigger > 0.5) {
-            deposit_Claw_Servo.setPosition(depositServoClosePosition);
+            robot.depositClawServo.setPosition(depositServoClosePosition);
         }
     }
     private boolean DE_IsAtPosition(int targetPosition) {
-        return (Math.abs(vs_Left_Motor.getCurrentPosition() - targetPosition) <= 5) &&
-                (Math.abs(vs_Right_Motor.getCurrentPosition() - targetPosition) <= 5);
+        return (Math.abs(robot.liftMotorLeft.getCurrentPosition() - targetPosition) <= 5) &&
+                (Math.abs(robot.liftMotorRight.getCurrentPosition() - targetPosition) <= 5);
+    }
+    private boolean SERVO_IsAtPosition(double targetPosition) {
+        return (Math.abs(robot.depositClawServo.getPosition() - targetPosition) <= 0.01);
     }
 
 
-    private void DepositSlidesInit(int MotorTargetPosition, double RightdepositArmServoInitialPosition, double LeftDepositArmServoInitialPosition, double DepositWristServoInitialPosition, double DepositServoInitialPosition) {
-        vs_Left_Motor.setTargetPosition(MotorTargetPosition);
-        vs_Right_Motor.setTargetPosition(MotorTargetPosition);
-        vs_Left_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vs_Right_Motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        vs_Left_Motor.setPower(0.2);
-        vs_Right_Motor.setPower(0.2);
-        deposit_Arm_Left_Servo.setPosition(LeftDepositArmServoInitialPosition);
-        deposit_Arm_Right_Servo.setPosition(RightdepositArmServoInitialPosition);
-        deposit_Claw_Servo.setPosition(DepositServoInitialPosition);
-        deposit_Wrist_Servo.setPosition(DepositWristServoInitialPosition);
+        private void DepositSlidesInit ( int MotorTargetPosition, double RightdepositArmServoInitialPosition, double LeftDepositArmServoInitialPosition, double DepositWristServoInitialPosition, double DepositServoInitialPosition) {
+            robot.liftMotorLeft.setTargetPosition(MotorTargetPosition);
+            robot.liftMotorRight.setTargetPosition(MotorTargetPosition);
+            robot.liftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.liftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.liftMotorLeft.setPower(0.4);
+            robot.liftMotorRight.setPower(0.4);
+            robot.depositLeftArmServo.setPosition(LeftDepositArmServoInitialPosition);
+            robot.depositRightArmServo.setPosition(RightdepositArmServoInitialPosition);
+            robot.depositClawServo.setPosition(DepositServoInitialPosition);
+            robot.depositWristServo.setPosition(DepositWristServoInitialPosition);
 
-    }
-
-
+        }
 
 }
