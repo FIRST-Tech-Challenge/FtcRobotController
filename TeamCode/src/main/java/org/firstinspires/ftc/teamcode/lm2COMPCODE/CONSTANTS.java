50aa84d03a9787eecf5436b8d9858303ca892cc8
aarav <aarav2win@gmail.com>
2024-11-17 20:06:02 -0800

    Aarav Pikle code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/CONSTANTS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lm2COMPCODE/CONSTANTS.java
@@ -5,7 +5,7 @@ public class CONSTANTS {
     public static final double SERVOROTATELOW = 0.1;
     public static final double SERVOROTATEMIDDLE = 0.4;
     public static final double SERVOROTATEHIGH = 0.6;
-    public static final double SERVOOPEN = 0;
+    public static final double SERVOOPEN = 0.4;
     public static final double SERVOCLOSE = 0.7;
     public static final double SERVOROTATE2MID = 0.5;
 
