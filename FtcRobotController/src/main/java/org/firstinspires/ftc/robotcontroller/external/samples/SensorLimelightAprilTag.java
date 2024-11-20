9845b259947345ee4faceb2f1f29b2e30f7ec723
PhuongLam1 <143738348+PhuongLam1@users.noreply.github.com>
2024-11-19 20:51:00 -0500

    more limeligth

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorLimelightAprilTag.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/SensorLimelightAprilTag.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.LLStatus;
@@ -7,22 +8,33 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @TeleOp(name = "Sensor: Limelight3A", group = "Sensor")
-@Disabled
 public class
 SensorLimelightAprilTag extends LinearOpMode {
 
     private Limelight3A limelight;
+    IMU imu;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
+        imu = hardwareMap.get(IMU.class, "imu");
+        imu.resetYaw();
+
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
-
         telemetry.setMsTransmissionInterval(11);
-
         limelight.pipelineSwitch(0);
+
+        // First, tell Limelight which way your robot is facing
+        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+        double robotYaw = orientation.getYaw(AngleUnit.DEGREES);
+        limelight.updateRobotOrientation(robotYaw);
 
         /*
          * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
@@ -39,7 +51,7 @@ SensorLimelightAprilTag extends LinearOpMode {
             telemetry.addData("Name", "%s",
                     status.getName());
             telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
-                    status.getTemp(), status.getCpu(),(int)status.getFps());
+                    status.getTemp(), status.getCpu(), (int) status.getFps());
             telemetry.addData("Pipeline", "Index: %d, Type: %s",
                     status.getPipelineIndex(), status.getPipelineType());
 
@@ -54,6 +66,13 @@ SensorLimelightAprilTag extends LinearOpMode {
                 telemetry.addData("Target X", tx);
                 telemetry.addData("Target Y", ty);
                 telemetry.addData("Target Area", ta);
+
+                Pose3D botpose_mt2 = result.getBotpose_MT2();
+                if (botpose_mt2 != null) {
+                    double x = botpose_mt2.getPosition().x;
+                    double y = botpose_mt2.getPosition().y;
+                    telemetry.addData("MT2 Location:", "(" + x + ", " + y + ")");
+                }
             } else {
                 telemetry.addData("Limelight", "No Targets");
             }
