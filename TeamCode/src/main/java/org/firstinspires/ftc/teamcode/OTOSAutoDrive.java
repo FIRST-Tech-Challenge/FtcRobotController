ba3159724aa6cc595cf34bc1e76220e9509ae60b
Milla <milla.fishler@outlook.com>
2024-11-19 19:14:35 -0800

    Untested updates.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSAutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OTOSAutoDrive.java
@@ -11,7 +11,7 @@ import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-// todo: you have 5 warnings and 11 typos. Please fix.
+
 @Autonomous(name="Auto Drive", group="Robot")
 public class OTOSAutoDrive extends LinearOpMode {
     // Initialize all variables for the program
@@ -38,57 +38,24 @@ public class OTOSAutoDrive extends LinearOpMode {
 
     // This chunk controls our viper slide
     DcMotor viperSlide = null;
+    final int VIPER_MIN = 0;
     final int VIPER_MAX = 3100;
-    final int VIPER_MIN = 0; // todo: MIN goes before MAX
 
     // This chunk controls our claw
     Servo claw = null;
-    final double CLAW_MIN = 0.00;    // Claw is closed
-    final double CLAW_MAX = 0.16;    // Claw is open
+    final double CLAW_MIN = 0.00;           // Claw is closed
+    final double CLAW_MAX = 0.16;           // Claw is open
 
     Servo ascentStick = null;
     final double ASCENT_MIN = 0.2;          // Stick is down
     final double ASCENT_MAX = 0.49;         // Stick is up
-    @Override // todo: new line before this
+
+    @Override
     public void runOpMode() {
-        // todo: put all of the hardware definitions in a separate method
-        // Define all the hardware
-        myOtos = hardwareMap.get(SparkFunOTOS.class, "OTOS");
-        SparkFunOTOS.Pose2D pos = myOtos.getPosition();
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
-        vertical = hardwareMap.get(DcMotor.class, "vertical");
-        vertical.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        vertical.setTargetPosition(0);
-        vertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        viperSlide = hardwareMap.get(DcMotor.class, "viper_slide");
-        viperSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        viperSlide.setDirection(DcMotor.Direction.REVERSE);
-        viperSlide.setTargetPosition(0);
-        viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        claw = hardwareMap.get(Servo.class, "claw");
-        claw.setDirection(Servo.Direction.REVERSE);
-        claw.setPosition(CLAW_MIN);
-
-        ascentStick = hardwareMap.get(Servo.class, "ascentStick");
-        ascentStick.setDirection(Servo.Direction.REVERSE);
-        ascentStick.setPosition(ASCENT_MIN);
-
-        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
-        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
-        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+        defineHardware();
 
         // Send telemetry message to signify robot waiting;
-        telemetry.addData("Ready", "You can press start"); // todo: Add "Autonomous" before ready so it is super clear to the driver
+        telemetry.addData("Autonomous Ready", "You can press start");
         telemetry.update();
 
         configureOtos();
@@ -97,25 +64,25 @@ public class OTOSAutoDrive extends LinearOpMode {
         waitForStart();
 
         // First Sample ///////////////////////////////////////////////////////////////
-        setVertical(VERTICAL_MAX);                        // Raising Arm
+        setVertical(VERTICAL_MAX);                                  // Raising Arm
         sleep(300);
-        setViper(VIPER_MAX);                              // Extending Viper
-        driveToLoc(2, 13, 20);     // Go to basket
+        setViper(VIPER_MAX);                                        // Extending Viper
+        driveToLoc(2, 13, 20);               // Go to basket
         sleep(600);
-        setClaw(CLAW_MAX);                                // Drop the block
+        setClaw(CLAW_MAX);                                          // Drop the block
 
         // Second Sample ///////////////////////////////////////////////////////////////
         driveToLoc(34, 2, 0, 1);
         setViper(1900);
         setVertical(100, 1000);
         sleep(1500);
-        setClaw(CLAW_MIN);                              // Grab second block
+        setClaw(CLAW_MIN);                                          // Grab second block
         sleep(100);
         setVertical(VERTICAL_MAX);
         setViper(VIPER_MAX);
         driveToLoc(8, 15, 45, 1.5);  // Go to basket
         sleep(600);
-        setClaw(CLAW_MAX);                              // Drop second block
+        setClaw(CLAW_MAX);                                          // Drop second block
 
         // Third Sample ///////////////////////////////////////////////////////////////
         driveToLoc(36, -2, 0, 1);
@@ -123,13 +90,13 @@ public class OTOSAutoDrive extends LinearOpMode {
         sleep(700);
         setVertical(0, 1500);
         sleep(1700);
-        setClaw(CLAW_MIN);                               // Grab third block
+        setClaw(CLAW_MIN);                                          // Grab third block
         sleep(100);
         setVertical(VERTICAL_MAX);
         setViper(VIPER_MAX);
-        driveToLoc(8, 15, 45, 1.5);   // Go to basket
+        driveToLoc(8, 15, 45, 1.5);  // Go to basket
         sleep(600);
-        setClaw(CLAW_MAX);                               // Drop third block
+        setClaw(CLAW_MAX);                                          // Drop third block
 
         // Park ///////////////////////////////////////////////////////////////
         driveToLoc(25, 5, 0, 3);
@@ -139,27 +106,26 @@ public class OTOSAutoDrive extends LinearOpMode {
         driveToLoc(55, 5, 0);
         RobotLog.vv("Rockin'", "Set to max");
         setAscentStick(ASCENT_MAX);
-        turnRight(0.5, 170);
+        turnRight(0.5, 180);
         moveForward(0.5, 1000);
         RobotLog.vv("Rockin'", "End program");
-        claw.close();                                    // Release tension on the claw
+        claw.close();                                               // Release tension on the claw
         // End of autonomous program
         telemetry.addData("Autonomous", "Complete");
         telemetry.update();
     }
 
     public void setAscentStick(double target) {
-        RobotLog.vv("Rockin' Robots", "Set Ascent Stick to: %4.2f, Current: %4.2f", target, ascentStick.getPosition()); // todo: we don't need this comment anymore
+        RobotLog.vv("Rockin' Robots", "setAscentStick() target: %4.2f, current: %4.2f", target, ascentStick.getPosition());
         ascentStick.setPosition(target);
         sleep(1000);
-        RobotLog.vv("Rockin' Robots", "Target: %4.2f, Current: %4.2f", target, ascentStick.getPosition());
     }
 
     public void setViper(int length){
+        RobotLog.vv("Rockin' Robots", "setViper() length: %4.2f, current: %4.2f", length, viperSlide.getCurrentPosition());
         viperSlide.setTargetPosition(length);
         ((DcMotorEx) viperSlide).setVelocity(2000);
         viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RobotLog.vv("Rockin' Robots", "Viper set to %d", viperSlide.getCurrentPosition());
     }
 
     public void setVertical(int height){
@@ -167,17 +133,20 @@ public class OTOSAutoDrive extends LinearOpMode {
     }
 
     public void setVertical(int height, int speed){
+        RobotLog.vv("Rockin' Robots", "setVertical() height: %4.2f, speed: %4.2f, current: %4.2f", height, speed, vertical.getCurrentPosition());
         vertical.setTargetPosition(height);
         ((DcMotorEx) vertical).setVelocity(speed);
         vertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void setClaw(double position) {
+        RobotLog.vv("Rockin' Robots", "setClaw() position: %4.2f, current: %4.2f", position, claw.getPosition());
         claw.setPosition(position);
         sleep(300);
     }
 
     private void moveForward(double speed, long msDuration) {
+        RobotLog.vv("Rockin' Robots", "moveForward() speed: %4.2f, msDuration:: %4.2f", speed, msDuration);
         leftFrontDrive.setPower(speed);
         rightFrontDrive.setPower(speed);
         leftBackDrive.setPower(speed);
@@ -187,6 +156,7 @@ public class OTOSAutoDrive extends LinearOpMode {
     }
 
     private void turnRight(double speed, double target) {
+        RobotLog.vv("Rockin' Robots", "turnRight() speed: %4.2f, target: %4.2f, current: %4.2f", speed, target, hLoc);
         leftFrontDrive.setPower(speed);
         rightFrontDrive.setPower(-speed);
         leftBackDrive.setPower(speed);
@@ -202,6 +172,7 @@ public class OTOSAutoDrive extends LinearOpMode {
     }
 
     private void stopMoving() {
+        RobotLog.vv("Rockin' Robots", "stopMoving()");
         leftFrontDrive.setPower(0);
         rightFrontDrive.setPower(0);
         leftBackDrive.setPower(0);
@@ -223,15 +194,18 @@ public class OTOSAutoDrive extends LinearOpMode {
         SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
         myOtos.getVersionInfo(hwVersion, fwVersion);
     }
+
     private void getPosition() {
         SparkFunOTOS.Pose2D pos = myOtos.getPosition();
         xLoc = pos.x;
         yLoc = pos.y;
         hLoc = pos.h;
     }
+
     private void driveToLoc(double xTarget, double yTarget, double hTarget) {
         driveToLoc(xTarget, yTarget, hTarget, 2);
     }
+
     private void driveToLoc(double xTarget, double yTarget, double hTarget, double accuracy) {
         getPosition();
         double xDistance = xTarget - xLoc;
@@ -240,18 +214,14 @@ public class OTOSAutoDrive extends LinearOpMode {
         if(hDistance > 180) hDistance -= 360;
         if(hDistance < -180) hDistance += 360;
 
-        RobotLog.vv("Rockin' Robots", "driveToLoc: xTarget: %.2f, yTarget: %.2f, hTarget: %.2f, accuracy: %.2f", xTarget, yTarget, hTarget, accuracy);
-        RobotLog.vv("Rockin' Robots", "xDistance: %.2f, yDistance: %.2f, hDistance: %.2f, " +
-                        "leftFrontPower: %.2f, rightFrontPower: %.2f, leftBackPower: %.2f, rightBackPower: %.2f",
-                xDistance, yDistance, hDistance, leftFrontPower, rightFrontPower, leftBackPower, rightBackPower);
+        RobotLog.vv("Rockin' Robots", "driveToLoc() xTarget: %.2f, yTarget: %.2f, hTarget: %.2f, accuracy: %.2f",
+                        xTarget, yTarget, hTarget, accuracy);
 
         while (opModeIsActive()
                 && (Math.abs(xDistance) > accuracy
                 || Math.abs(yDistance) > accuracy
                 || Math.abs(hDistance) > accuracy)) {
 
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power.
             leftFrontPower = (yDistance + xDistance - hDistance) / 8;
             rightFrontPower = (yDistance - xDistance + hDistance) / 8;
             leftBackPower = (yDistance - xDistance - hDistance) / 8;
@@ -292,7 +262,45 @@ public class OTOSAutoDrive extends LinearOpMode {
             }
         }
         stopMoving();
-        RobotLog.vv("Rockin' Robots", "Done Moving: xDist: %.2f, yDist: %.2f, hDist: %.2f",
+        RobotLog.vv("Rockin' Robots", "driveToLoc() done: xDist: %.2f, yDist: %.2f, hDist: %.2f",
                 xDistance, yDistance, hDistance);
     }
+
+    public void defineHardware() {
+        // Define all the hardware
+        myOtos = hardwareMap.get(SparkFunOTOS.class, "OTOS");
+        SparkFunOTOS.Pose2D pos = myOtos.getPosition();
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        vertical = hardwareMap.get(DcMotor.class, "vertical");
+        vertical.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        vertical.setTargetPosition(0);
+        vertical.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        viperSlide = hardwareMap.get(DcMotor.class, "viper_slide");
+        viperSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        viperSlide.setDirection(DcMotor.Direction.REVERSE);
+        viperSlide.setTargetPosition(0);
+        viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        claw = hardwareMap.get(Servo.class, "claw");
+        claw.setDirection(Servo.Direction.REVERSE);
+        claw.setPosition(CLAW_MIN);
+
+        ascentStick = hardwareMap.get(Servo.class, "ascentStick");
+        ascentStick.setDirection(Servo.Direction.REVERSE);
+        ascentStick.setPosition(ASCENT_MIN);
+
+        RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+        RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+        RevHubOrientationOnRobot orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
+    }
+
 }
