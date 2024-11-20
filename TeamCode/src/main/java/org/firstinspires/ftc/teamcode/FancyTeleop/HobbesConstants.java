09abf6066b2afb1caa082ee67239fc334a099b03
davidyyyyyyyyy <dbattlecardemil@gmail.com>
2024-11-20 16:24:42 -0500

    constants done

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FancyTeleop/HobbesConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FancyTeleop/HobbesConstants.java
@@ -13,16 +13,16 @@ public class HobbesConstants {
     WRIST: pivots end of an arm
 
      */
-    public static int SLIDES_MAX = 1000;
+    public static int SLIDES_MAX = 1600;
     public static int SLIDES_MIN = 0;
     public static double SLIDES_SIGMOID_SCALER = 0.008; // DONEISH
-    public static double SLIDES_KP = 1;
+    public static double SLIDES_KP = 0.04;
 
     public static int SLIDES_IN = 0;
     public static int SLIDES_SPECIMEN_PICKUP = 0;
-    public static int SLIDES_OUT_TOP_SAMPLE = 0;
-    public static int SLIDES_OUT_TOP_SPECIMEN = 0;
-    public static int SLIDES_OUT_TOP_SPECIMEN_DOWN = 0;
+    public static int SLIDES_OUT_TOP_SAMPLE = 1450;
+    public static int SLIDES_OUT_TOP_SPECIMEN = 850;
+    public static int SLIDES_OUT_TOP_SPECIMEN_DOWN = 650;
 
     public static double EXTENDO_IN = 0.1;
     public static double EXTENDO_OUT_FULL = 0.58;
@@ -31,28 +31,25 @@ public class HobbesConstants {
     public static double EXTENDO_SPEED = 0;
 
     public static double EXTENDO_ARM_TRANSFER = 1;
-    public static double EXTENDO_ARM_INTAKE = 0.06;
+    public static double EXTENDO_ARM_INTAKE = 0.03;
 
-    public static double EXTENDO_WRIST_TRANSFER = 0.55;
-    public static double EXTENDO_WRIST_INTAKE = 0;
+    public static double EXTENDO_WRIST_TRANSFER = 0.5;
     public static double EXTENDO_WRIST_INTAKE_FLAT = 0;
 
-    public static double EXTENDO_ARM_SPECIMEN = 0;
-    public static double EXTENDO_WRIST_SPECIMEN = 0;
+    public static double SLIDES_ARM_TRANSFER = 1;
+    public static double SLIDES_ARM_DEPOSIT = 0.4;
+    public static double SLIDES_ARM_SPECIMEN = 0.07;
 
-    public static double SLIDES_ARM_TRANSFER = 0;
-    public static double SLIDES_ARM_DEPOSIT = 0;
-    public static double SLIDES_ARM_SPECIMEN = 0;
+    public static double SLIDES_WRIST_TRANSFER = 1;
+    public static double SLIDES_WRIST_DEPOSIT = 0.2;
+    public static double SLIDES_WRIST_SPECIMEN = 0.07;
 
-    public static double SLIDES_WRIST_TRANSFER = 0;
-    public static double SLIDES_WRIST_DEPOSIT = 0;
-    public static double SLIDES_WRIST_SPECIMEN = 0;
-
-    public static double CLAW_OPEN = 0;
-    public static double CLAW_CLOSED = 0;
+    public static double CLAW_OPEN = 0.4;
+    public static double CLAW_CLOSED = 0.76;
 
     public static double INTAKE_POWER = 1;
     public static double INTAKE_OFF = 0;
+    public static double INTAKE_REVERSE = -1;
 
     public static int INFINITY =  2000000000;
     public static double EXTENDO_OFFSET = 1;
