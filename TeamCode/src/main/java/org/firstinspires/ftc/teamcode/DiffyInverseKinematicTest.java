221fa60c4a043416b542c961bd9029e7a17ea12a
FTC18035 <ftc18035@gmail.com>
2024-11-21 18:55:48 -0500

    Added automatic flexion depending on virtual four bar lift position.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffyInverseKinematicTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffyInverseKinematicTest.java
@@ -12,8 +12,8 @@ public class DiffyInverseKinematicTest extends LinearOpMode {
 
     private double TouchPadInput = .5;
 
-    private double LeftServo = 1;
-    private double RightServo = 1;
+    private double LeftServo = .5;
+    private double RightServo = .5;
     private double V4Bpos = 1;
     private double Flex = .5;
     private double Yaw = .5;
@@ -29,13 +29,14 @@ public class DiffyInverseKinematicTest extends LinearOpMode {
         Servo RightIntakeV4B = hardwareMap.servo.get("Right Intake V4B");     // Chub Port 3 // Preset To Swing Out With X
         Servo LeftIntakeV4B = hardwareMap.servo.get("Left Intake V4B");       // Chub Port 4 // --------------------------
 
-
+        LeftServo = Flex + (1/2)*Yaw;
+        RightServo = Flex - (1/2)*Yaw;
         //IntakeClaw.setPosition(0);    // Closes Intake Claw
         LeftIntakeWrist.setPosition(LeftServo);    // Sets the intake wrist to the starting position // Left is 0 Right is 1
         RightIntakeWrist.setPosition(RightServo);   // Sets the intake wrist to the starting position // Left is 0 Right is 1
 
-        LeftIntakeV4B.setPosition(0);   // Sets the intake virtual four bar to the starting position
-        RightIntakeV4B.setPosition(0);  // Sets the intake virtual four bar to the starting position
+        LeftIntakeV4B.setPosition(1);   // Sets the intake virtual four bar to the starting position
+        RightIntakeV4B.setPosition(1);  // Sets the intake virtual four bar to the starting position
 
         waitForStart();
 
@@ -54,27 +55,32 @@ public class DiffyInverseKinematicTest extends LinearOpMode {
             }*/
             if (gamepad1.a){    // Intake Position
                 V4Bpos = .5;
+                Flex = .5;
             }
             else if (gamepad1.x){  //Transfer position
                 V4Bpos = 1;
+                Flex = .75;
             }
             else if (gamepad1.right_trigger > 0){
                 V4Bpos = 0.5 - 0.5 * gamepad1.right_trigger;
+                Flex = .5;
             }
-            LeftIntakeV4B.setPosition(V4Bpos);
-            RightIntakeV4B.setPosition(V4Bpos);
-            //**************************** INTAKE WRIST ************************************************************
-            // Intake Wrist
+
             if (RightIntakeV4B.getPosition() > .5){
                 Yaw = .5;
             }
             else if (gamepad1.touchpad_finger_1){   // The intake is extended past 200 ticks and a finger is on the touchpad
                 Yaw = (gamepad1.touchpad_finger_1_x + 1) / 2;     // This is taking a range from -1 to 1 and converting it to a range of 0 to 1 and saving it to a variable
-                LeftServo = Flex + (1/2)*Yaw;
-                RightServo = Flex - (1/2)*Yaw;
             }
+
+            LeftServo = Flex + (1/2)*Yaw;
+            RightServo = Flex - (1/2)*Yaw;
+
             LeftIntakeWrist.setPosition(LeftServo);
             RightIntakeWrist.setPosition(RightServo);
+
+            LeftIntakeV4B.setPosition(V4Bpos);
+            RightIntakeV4B.setPosition(V4Bpos);
 
             telemetry.addData("Touch Pad Yaw Input", Yaw)
                     .addData("Flex Input", Flex)
