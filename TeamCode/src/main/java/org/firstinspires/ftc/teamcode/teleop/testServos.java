dd6a5c548b98a798f0286c03856d830b987562cc
amazingjay26 <145387791+amazingjay26@users.noreply.github.com>
2024-11-20 22:04:53 -0500

    Update testServos.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/testServos.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/testServos.java
@@ -6,62 +6,56 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="test servos", group="Linear OpMode")
 //@Disabled
 public class testServos extends LinearOpMode {
+    /* Declare OpMode members. */
+    public Servo    servo1    = null;
+    public Servo    servo2   = null;
 
+    double clawOffset = 0;
     private ElapsedTime runtime = new ElapsedTime();
-
-    private Servo servo1;
-    private Servo servo2;
     public static final double MID_SERVO   =  0.5 ;
+
 
     @Override
     public void runOpMode() {
+        double left;
+        double right;
+        double turn;
 
-
+        // Define and initialize ALL installed servos.
         servo1  = hardwareMap.get(Servo.class, "2");
-        servo1  = hardwareMap.get(Servo.class, "3");
+        servo2 = hardwareMap.get(Servo.class, "3");
         servo1.setPosition(MID_SERVO);
         servo2.setPosition(MID_SERVO);
-
-        // Wait for the game to start (driver presses START)
-        telemetry.addData("Status", "Initialized");
+        //
         telemetry.update();
 
+        // Wait for the game to start (driver presses START)
         waitForStart();
-        runtime.reset();
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            double power1 = gamepad1.left_stick_y;
-            servo1.setPosition(power1);
-        /*
-            if (gamepad1.a){
-                servo1.setPosition(0);
-            }
-            else if (gamepad1.b){
-                servo1.setPosition(MID_SERVO);
-            }
-            else{
-                servo1.setPosition(gamepad1.left_stick_y);
-            }
+            left = -gamepad1.left_stick_y + 0.5;
+            right = gamepad1.right_stick_y + 0.5;
+            servo1.setPosition(left);
+            servo2.setPosition(right);
 
-            if (gamepad1.x){
-                servo2.setPosition(0);
-            }
-            else if (gamepad1.y){
-                servo2.setPosition(MID_SERVO);
-            }
-            else{
-                servo2.setPosition(gamepad1.right_stick_y);
-            }*/
+            // Send telemetry message to signify robot running;
+            telemetry.addData("claw",  "Offset = %.2f", clawOffset);
+            telemetry.addData("left",  "%.2f", servo1.getPosition());
+            telemetry.addData("right", "%.2f", servo2.getPosition());
 
+
+            // Pace this loop so jaw action is reasonable speed.
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("servo1 position", "double", servo1.getPosition());
-            telemetry.addData("servo2 position", "double", servo2.getPosition());
             telemetry.update();
+
         }
-    }}
+    }
+}
+
