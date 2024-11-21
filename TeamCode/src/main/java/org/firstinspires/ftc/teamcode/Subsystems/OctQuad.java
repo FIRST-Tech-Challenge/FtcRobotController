312f278496677380365a6ff92bb9ebf2fdd07ea1
Glenn Buckholtz <buckholt@queensu.ca>
2024-11-20 20:12:43 -0500

    added some comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/OctQuad.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/OctQuad.java
@@ -5,8 +5,10 @@ import com.qualcomm.hardware.digitalchickenlabs.OctoQuad;
 import com.qualcomm.hardware.digitalchickenlabs.OctoQuadBase;
 import org.firstinspires.ftc.teamcode.RobotContainer;
 
-
-/** Subsystem */
+/**
+ * The OctQuad class is a subsystem that manages the odometry pods using the OctoQuad hardware.
+ * It extends the SubsystemBase class from FTCLib.
+ */
 public class OctQuad extends SubsystemBase {
 
     // Odometry pods - constants
@@ -17,65 +19,84 @@ public class OctQuad extends SubsystemBase {
     public static double LATERAL_DISTANCE = 0.315; // m; distance between the left and right wheels
     public static double FORWARD_OFFSET = 0.100; // m; offset of the lateral wheel
 
-    // travel distnce per encoder pulse (m)
-    private double DistancePerPulse = Math.PI * 2.0 * WHEEL_RADIUS/(TICKS_PER_REV*GEAR_RATIO);
+    // Travel distance per encoder pulse (m)
+    private double DistancePerPulse = Math.PI * 2.0 * WHEEL_RADIUS / (TICKS_PER_REV * GEAR_RATIO);
 
     // Create octoquad object
     OctoQuad octoquad;
 
-    // encoder values from each odometry pod
+    // Encoder values from each odometry pod
     double OdometryLeftEncoder;
     double OdometryRightEncoder;
     double OdometryFrontEncoder;
 
-    /** Place code here to initialize subsystem */
+    /**
+     * Constructor for the OctQuad class.
+     * Initializes the OctoQuad hardware and resets encoders.
+     */
     public OctQuad() {
-        // set up octoquad object
+        // Set up octoquad object
         octoquad = RobotContainer.ActiveOpMode.hardwareMap.get(OctoQuad.class, "OctoQuad");
 
-        // reset octoquad
+        // Reset octoquad
         octoquad.resetEverything();
 
-        // reset all encoders
+        // Reset all encoders
         octoquad.resetAllPositions();
 
-        // set encoder directions
+        // Set encoder directions
         octoquad.setSingleEncoderDirection(0, OctoQuadBase.EncoderDirection.FORWARD);
         octoquad.setSingleEncoderDirection(1, OctoQuadBase.EncoderDirection.REVERSE);
         octoquad.setSingleEncoderDirection(2, OctoQuadBase.EncoderDirection.FORWARD);
 
-        // set all encoders to quadrature
+        // Set all encoders to quadrature
         octoquad.setChannelBankConfig(OctoQuadBase.ChannelBankConfig.ALL_QUADRATURE);
 
-        // save update to flash
+        // Save update to flash
         octoquad.saveParametersToFlash();
 
-        // reset all encoders
-        OdometryLeftEncoder=0;
-        OdometryRightEncoder=0;
-        OdometryFrontEncoder=0;
+        // Reset all encoders
+        OdometryLeftEncoder = 0;
+        OdometryRightEncoder = 0;
+        OdometryFrontEncoder = 0;
     }
 
-    /** Method called periodically by the scheduler
-     * Place any code here you wish to have run periodically */
+    /**
+     * The periodic method is called periodically by the scheduler.
+     * Reads the encoder values from the odometry pods.
+     */
     @Override
     public void periodic() {
-
-        // read encoder values
+        // Read encoder values
         OdometryRightEncoder = octoquad.readSinglePosition(0);
         OdometryLeftEncoder = octoquad.readSinglePosition(1);
         OdometryFrontEncoder = octoquad.readSinglePosition(2);
     }
 
-    // methods to return encoder values of odometry pods
+    /**
+     * Returns the distance traveled by the left encoder.
+     *
+     * @return Distance traveled by the left encoder in meters.
+     */
     public double getLeftEncoderDistance() {
-        return DistancePerPulse*OdometryLeftEncoder;
-    }
-    public double getRightEncoderDistance() {
-        return DistancePerPulse*OdometryRightEncoder;
-    }
-    public double getFrontEncoderDistance() {
-        return DistancePerPulse*OdometryFrontEncoder;
+        return DistancePerPulse * OdometryLeftEncoder;
     }
 
+    /**
+     * Returns the distance traveled by the right encoder.
+     *
+     * @return Distance traveled by the right encoder in meters.
+     */
+    public double getRightEncoderDistance() {
+        return DistancePerPulse * OdometryRightEncoder;
+    }
+
+    /**
+     * Returns the distance traveled by the front encoder.
+     *
+     * @return Distance traveled by the front encoder in meters.
+     */
+    public double getFrontEncoderDistance() {
+        return DistancePerPulse * OdometryFrontEncoder;
+    }
 }
