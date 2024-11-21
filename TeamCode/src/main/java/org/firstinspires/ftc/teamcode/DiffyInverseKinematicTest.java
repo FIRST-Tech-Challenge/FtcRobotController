863e6634f42f1462620df892538ee6970521c47a
FTC18035 <ftc18035@gmail.com>
2024-11-21 15:44:10 -0500

    Created new testing program using inverse kinematic equations to solve for differential wrist rotation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DiffyInverseKinematicTest.java
@@ -0,0 +1,87 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp
+public class DiffyInverseKinematicTest extends LinearOpMode {
+
+    private ElapsedTime IntakeClawTime = new ElapsedTime();
+
+    private double TouchPadInput = .5;
+
+    private double LeftServo = 1;
+    private double RightServo = 1;
+    private double V4Bpos = 1;
+    private double Flex = .5;
+    private double Yaw = .5;
+
+    private boolean IntakeClawClosed = false;
+    private boolean OuttakeActive = false;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        //Servo IntakeClaw = hardwareMap.servo.get("Intake Claw");              // Chub Port 0 // O Button
+        Servo RightIntakeWrist = hardwareMap.servo.get("Right Intake Wrist"); // Chub Port 1 // Increments Using Dpad Side Buttons?
+        Servo LeftIntakeWrist = hardwareMap.servo.get("Left Intake Wrist");   // Chub Port 2 // Ideally Stick Controlled
+        Servo RightIntakeV4B = hardwareMap.servo.get("Right Intake V4B");     // Chub Port 3 // Preset To Swing Out With X
+        Servo LeftIntakeV4B = hardwareMap.servo.get("Left Intake V4B");       // Chub Port 4 // --------------------------
+
+
+        //IntakeClaw.setPosition(0);    // Closes Intake Claw
+        LeftIntakeWrist.setPosition(LeftServo);    // Sets the intake wrist to the starting position // Left is 0 Right is 1
+        RightIntakeWrist.setPosition(RightServo);   // Sets the intake wrist to the starting position // Left is 0 Right is 1
+
+        LeftIntakeV4B.setPosition(0);   // Sets the intake virtual four bar to the starting position
+        RightIntakeV4B.setPosition(0);  // Sets the intake virtual four bar to the starting position
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            /* Closes the intake claw
+            if (gamepad2.b && !OuttakeActive && IntakeClawTime.seconds() >= .3 && !IntakeClawClosed){ // If the B button was pressed, Outtake is retracted, It has been more than .3 seconds since the intake claw has been used and the intake claw is open
+                IntakeClawTime.reset();     // Reset the timer since the intake claw was just used
+                IntakeClaw.setPosition(1);  // Closes the intake claw
+                IntakeClawClosed = true;    // Since the intake claw was closed we change this to stay accurate
+            }
+            // Opens the intake claw
+            else if (gamepad2.b && !OuttakeActive && IntakeClawTime.seconds() >= .3 && IntakeClawClosed){  // If the B button was pressed, Outtake is retracted, It has been more than .3 seconds since the intake claw has been used and the intake claw is closed
+                IntakeClawTime.reset();     // Reset the timer since the intake claw was just used
+                IntakeClaw.setPosition(0);  // Opens the intake claw
+                IntakeClawClosed = false;   // Since the intake claw was opened we change this to stay accurate
+            }*/
+            if (gamepad1.a){    // Intake Position
+                V4Bpos = .5;
+            }
+            else if (gamepad1.x){  //Transfer position
+                V4Bpos = 1;
+            }
+            else if (gamepad1.right_trigger > 0){
+                V4Bpos = 0.5 - 0.5 * gamepad1.right_trigger;
+            }
+            LeftIntakeV4B.setPosition(V4Bpos);
+            RightIntakeV4B.setPosition(V4Bpos);
+            //**************************** INTAKE WRIST ************************************************************
+            // Intake Wrist
+            if (RightIntakeV4B.getPosition() > .5){
+                Yaw = .5;
+            }
+            else if (gamepad1.touchpad_finger_1){   // The intake is extended past 200 ticks and a finger is on the touchpad
+                Yaw = (gamepad1.touchpad_finger_1_x + 1) / 2;     // This is taking a range from -1 to 1 and converting it to a range of 0 to 1 and saving it to a variable
+                LeftServo = Flex + (1/2)*Yaw;
+                RightServo = Flex - (1/2)*Yaw;
+            }
+            LeftIntakeWrist.setPosition(LeftServo);
+            RightIntakeWrist.setPosition(RightServo);
+
+            telemetry.addData("Touch Pad Yaw Input", Yaw)
+                    .addData("Flex Input", Flex)
+                    .addData("Left Wrist Position", LeftIntakeWrist.getPosition())
+                    .addData("Right Wrist Position", RightIntakeWrist.getPosition())
+                    .addData("Claw Closed?", IntakeClawClosed);
+            telemetry.update();
+        }
+    }
+}
