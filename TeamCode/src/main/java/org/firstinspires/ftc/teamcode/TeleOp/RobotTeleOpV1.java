f4be99bd991be03bbbc8e077a310fda1eb906c25
Zirui-Ma <ziruima@outlook.com>
2024-11-20 23:40:11 -0700

    Added all the classes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotTeleOpV1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/RobotTeleOpV1.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
-public class RobotTeleOpV1 {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class RobotTeleOpV1 extends OpMode {
+    public void init () {
+
+    }
+
+    public void loop () {
+
+    }
 }
