fe8f98bda6ea16f0ce655a0b56b087bc7ac9f035
Moss <mossk9532@gmail.com>
2024-11-22 11:38:43 -0800

    Fixed some issues with GamepadUtils class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/GamepadUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/GamepadUtils.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.utils;
 
 import java.lang.Math;
 
-public abstract class GamepadUtils {
+public static abstract class GamepadUtils {
     public double deadzone(double input, double deadzone) {
         if(Math.abs(input) >= deadzone) return input;
         return Math.pow(input, 3) / Math.pow(deadzone, 2); //Makes input follow a cubic curve to smooth deadzone
