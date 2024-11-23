27883f9f35dd2933af315686006d62cffbef15e5
Teagan Thomes <teagan@anydesk.net>
2024-11-22 21:14:39 -0600

    update autotest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/AutoTest.java
@@ -1,4 +1,33 @@
 package org.firstinspires.ftc.teamcode.Test;
 
-public class AutoTest {
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Components.ViperSlide;
+
+import java.util.Arrays;
+
+@Autonomous(name="AutoTest", group="Test")
+public class AutoTest extends LinearOpMode {
+    ViperSlide viperSlide = new ViperSlide(this);
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        while (!isStopRequested() && !opModeIsActive()) {
+            telemetry.addData("Initialized", "");
+            telemetry.update();
+        }
+
+        waitForStart();
+
+        Actions.runBlocking(
+                new ParallelAction(
+                        viperSlide.goToPositionAction(1000)
+                )
+        );
+
+        if (isStopRequested() || gamepad1.b) return;
+    }
 }
