36a03613bd07a7100e41c6bb3b670ec1e30e593c
EagleNotBot <phibersto26@gmail.com>
2024-11-22 20:02:24 -0600

    Make autohome reset the gyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoHome.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AutoHome.java
@@ -4,12 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Mekanism.Mekanism;
+import org.firstinspires.ftc.teamcode.Swerve.Swerve;
 
-@Autonomous(name = "Home arm")
+@Autonomous(name = "Home arm", preselectTeleOp = "Blue Bot Teleop")
 public class AutoHome extends LinearOpMode {
   @Override
   public void runOpMode() throws InterruptedException {
     var arm = new Mekanism(this);
+    var drivebase = new Swerve(this);
+
+    drivebase.initGyro();
 
     waitForStart();
     arm.homeArm();
