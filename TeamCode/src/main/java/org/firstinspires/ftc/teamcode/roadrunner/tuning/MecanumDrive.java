6a580c45dd76934dbca9fbc7cdf7eb9e412745dd
Armaan Sidhu <armaansidhu000@icloud.com>
2024-11-23 18:43:44 -0800

    fixed problem with heading

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/tuning/MecanumDrive.java
@@ -85,7 +85,7 @@ public class MecanumDrive {
         // path controller gains
         public double axialGain = 3.0;
         public double lateralGain = 0.5;
-        public double headingGain = 0; // shared with turn
+        public double headingGain = 8; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
