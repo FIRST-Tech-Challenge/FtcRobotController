55c551ab57ddc8c6f10e31a2b031abf97eadfbc9
ShivamTheShrimp <100112804+ShivamTheShrimp@users.noreply.github.com>
2024-11-26 15:25:16 -0800

    sigma boy

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight_test.java
@@ -0,0 +1,113 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+//MINE ( AARUSH )
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.Camera;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraManager;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+
+@Autonomous(name="limelight_test", group="Linear Opmode2")
+public class limelight_test extends CommonUtil {
+
+    Orientation myRobotOrientation;
+
+
+    @Override
+    public void runOpMode() {
+
+        //setup
+        telemetry.setAutoClear(false);
+        // initialize hardware
+        initialize(hardwareMap);
+        // Initialize motors
+        setMotorOrientation();
+        //resetMotorEncoderCounts();
+        setMotorToZeroPower();
+        setZeroPowerBehavior();
+        NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
+        // Set the pipeline to the one that detects the blue rectangle
+        limelight.getEntry("yellow-sample").setNumber(1); // Pipeline index for "blue rectangle"
+
+        telemetry.addLine("Pipeline set to 3 for blue rectangle detection.");
+        telemetry.update();
+
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        while (opModeIsActive()) {
+            double tx = limelight.getEntry("tx").getDouble(0.0);  // Horizontal offset
+            double ta = limelight.getEntry("ta").getDouble(0.0);  // Target area
+            double tv = limelight.getEntry("tv").getDouble(0.0);  // Valid target (1 = valid, 0 = no target)
+
+            // Display values on telemetry
+            telemetry.addData("tx (Horizontal Offset)", tx);
+            telemetry.addData("ta (Target Area)", ta);
+            telemetry.addData("tv (Valid Target Detected)", tv == 1 ? "Yes" : "No");
+            telemetry.update();
+
+
+
+        }
+    }
+
+
+}
+
+
+
+
