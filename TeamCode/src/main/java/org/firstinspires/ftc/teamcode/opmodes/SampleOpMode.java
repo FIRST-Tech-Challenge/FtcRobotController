2ba93c16b6b0c84ade6bb8ee50ff55c004a0157b
Chris Henry <cwhenry101@gmail.com>
2024-11-26 16:28:25 -0500

    Disabled Elevator Up/Down

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/SampleOpMode.java
@@ -118,9 +118,9 @@ public class SampleOpMode extends CommandOpMode {
 
         //ScoreAtBucket.whenPressed(new ScoreAtBucket(drivetrain, arm, elevator));
 
-        elevatorUpButton.whenPressed(new ElevatorGoTo(elevator, 35));
+        //elevatorUpButton.whenPressed(new ElevatorGoTo(elevator, 35));
 
-       elevatorDownButton.whenPressed(new ElevatorGoTo(elevator, 0));
+       //elevatorDownButton.whenPressed(new ElevatorGoTo(elevator, 0));
 
         CommandScheduler.getInstance().setDefaultCommand(elevator, new ManualElevatorCommand(elevator,
                 () -> (operator.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) - operator.getTrigger(GamepadKeys.Trigger.RIGHT_TRIGGER)), telemetry));
