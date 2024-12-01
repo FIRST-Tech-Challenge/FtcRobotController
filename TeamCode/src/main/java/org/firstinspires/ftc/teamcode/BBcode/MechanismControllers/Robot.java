963985dc7852959d91e8088b75c69d5bf033ac94
Matthew <zacharykeepers@gmail.com>
2024-12-01 17:30:29 -0600

    set up the robot class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/MechanismControllers/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/MechanismControllers/Robot.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.BBcode.MechanismControllers;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.BBcode.TelemetryHelper;
+
 public class Robot {
+    OpMode _Opmode;
+    //constructor
+    public Robot(OpMode opmode){
+        _Opmode = opmode;
+    }
+    //-------------------------------------------------------------------------------
+
+    //Creates instance of MechanismControllers
+    Viper _Viper = new Viper(_Opmode);
+    WristClaw _WristClaw = new WristClaw(_Opmode);
+    Arm _Arm = new Arm(_Opmode, new TelemetryHelper(_Opmode));
 }
