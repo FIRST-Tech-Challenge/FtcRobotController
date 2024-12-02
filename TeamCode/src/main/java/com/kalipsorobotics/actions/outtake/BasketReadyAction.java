e90573f4d7055dd7230342917dc2f88d461a048c
Alan Huang <alan.wenhao.huang@gmail.com>
2024-12-01 16:19:37 -0800

    basket ready action done, to be tested

--- TeamCode/src/main/java/com/kalipsorobotics/actions/outtake/BasketReadyAction.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/outtake/BasketReadyAction.java
@@ -1,4 +1,15 @@
 package com.kalipsorobotics.actions.outtake;
 
+import com.kalipsorobotics.actions.outtake.teleopActions.OuttakePivotAction;
+import com.kalipsorobotics.modules.Outtake;
+
 public class BasketReadyAction {
+    Outtake outtake;
+    MoveLSAction moveLSAction;
+    OuttakePivotAutoAction outtakePivotAutoAction;
+    public BasketReadyAction() {
+        moveLSAction = new MoveLSAction(outtake, 2040);
+        outtakePivotAutoAction = new OuttakePivotAutoAction(outtake, 0.5);
+        outtakePivotAutoAction = new OuttakePivotAutoAction(outtake, 0.05);
+    }
 }
