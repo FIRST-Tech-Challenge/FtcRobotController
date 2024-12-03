81d28d4e84501f1fb852a574bd3697546437f2a0
Mr. Bacon <aj.780875@gmail.com>
2024-12-03 15:21:07 -0700

    Copy master

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class Robot extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
