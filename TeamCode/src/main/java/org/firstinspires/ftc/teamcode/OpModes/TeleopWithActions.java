67bc299373229544c287ea0aab4a09c12ae777ca
potatobob <83433649+RohanSen08@users.noreply.github.com>
2024-12-02 21:45:08 -0500

    idk what i just did

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleopWithActions.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/TeleopWithActions.java
@@ -1,25 +1,10 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Claw.Claw.clawState.CLOSE;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Claw.Claw.clawState.OPEN;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Utils.Utils.inverseKinematics;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Utils.Utils.l;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Utils.Utils.r;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Utils.Utils.w;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Extension.Extension.extensionState.EXTEND;
-import static org.firstinspires.ftc.teamcode.Mechanisms.Extension.Extension.extensionState.RETRACT;
-
-import androidx.annotation.NonNull;
-
 import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.ejml.simple.SimpleMatrix;
 import org.firstinspires.ftc.teamcode.Hardware.Sensors.Battery;
 import org.firstinspires.ftc.teamcode.Mechanisms.Arm.Arm;
 import org.firstinspires.ftc.teamcode.Mechanisms.Claw.Claw;
@@ -31,9 +16,8 @@ import org.firstinspires.ftc.teamcode.Mechanisms.Robot.Robot;
 import java.util.ArrayList;
 import java.util.List;
 
-@Config
-@TeleOp(name = "TeleOp", group = "Autonomous")
 public class TeleopWithActions extends OpMode {
+
     private FtcDashboard dash = FtcDashboard.getInstance();
     private List<Action> runningActions = new ArrayList<>();
 
@@ -51,12 +35,16 @@ public class TeleopWithActions extends OpMode {
         dashboard = FtcDashboard.getInstance();
         drivetrain = robot.drivetrain;
         Battery battery = new Battery(hardwareMap);
+        Arm arm = new Arm(hardwareMap);
+        Claw claw = new Claw(hardwareMap);
+        Extension extension = new Extension(hardwareMap);
+        Lift lift = new Lift(hardwareMap, battery);
     }
 
     @Override
     public void loop() {
         TelemetryPacket packet = new TelemetryPacket();
-        if(gamepad1.a){
+        if(gamepad1.right_trigger > 0){
             runningActions.add(extension.servoExtension());
         }
         if(gamepad1.left_trigger > 0){
@@ -70,30 +58,7 @@ public class TeleopWithActions extends OpMode {
         }
         // updated based on gamepads
         runningActions.add(
-        new Action() {
-            @Override
-            public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                double y = -gamepad1.left_stick_y;
-                double x = -gamepad1.left_stick_x;
-                double rx = gamepad1.right_stick_x;
-
-                double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
-                double frontLeftPower = (y - x + rx) / denominator;
-                double backLeftPower = (y + x + rx) / denominator;
-                double frontRightPower = (y + x - rx) / denominator;
-                double backRightPower = (y - x - rx) / denominator;
-                SimpleMatrix drivePowers = new SimpleMatrix(
-                        new double[][]{
-                                new double[]{frontLeftPower},
-                                new double[]{backLeftPower},
-                                new double[]{backRightPower},
-                                new double[]{frontRightPower}
-                        }
-                );
-                drivetrain.setPower(drivePowers);
-                return true;
-            }
-        }
+                drivetrain.drive()
         );
         // update running actions
         List<Action> newActions = new ArrayList<>();
