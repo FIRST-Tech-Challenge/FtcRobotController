1944add542dcd053e166871f17281f5e2b7eaa0b
Allen Durda <60194370+jadurda@users.noreply.github.com>
2024-12-03 16:17:58 -0600

    moved the ServoTuner class to the BBCode.Tuning package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Tuning/ServoTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/Tuning/ServoTuner.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.BBcode.Tuning;
 
 import static org.firstinspires.ftc.robotcontroller.external.samples.RobotHardware.MID_SERVO;
 
