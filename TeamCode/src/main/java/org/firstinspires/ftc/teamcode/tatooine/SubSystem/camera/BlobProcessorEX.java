29e1616ba4011607df98ccbfb252d8531c3f231c
sz77 <zalmanstefan2004@gmail.com>
2024-12-04 18:18:26 +0200

    Added support for neg angels(yaniv)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/SubSystem/camera/BlobProcessorEX.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/SubSystem/camera/BlobProcessorEX.java
@@ -2,24 +2,41 @@ package org.firstinspires.ftc.teamcode.tatooine.SubSystem.camera;
 
 import android.graphics.Canvas;
 
+import com.qualcomm.robotcore.util.SortOrder;
+
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 
-import java.util.Collections;
-import java.util.List;
-
-public class BlobProcessorEX extends ColorBlobLocatorProcessor {
+public abstract class BlobProcessorEX extends ColorBlobLocatorProcessor {
+    double angle = 0;
+    BlobSort sort = new BlobSort(BlobCriteria.BY_CONTOUR_AREA, SortOrder.ASCENDING);
 
 
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
+        setSort(sort);
 
     }
 
     @Override
     public Object processFrame(Mat frame, long captureTimeNanos) {
-        return
+        Blob blob = getBlobs().get(0);
+        Point[] pt = new Point[4];
+        blob.getBoxFit().points(pt);
+        Rect bbox = blob.getBoxFit().boundingRect();
+        Rect rect = new Rect(new Point(bbox.x, bbox.y + bbox.height / 2.0),
+                new Point(bbox.x + bbox.width / 2.0, bbox.y + bbox.height));
+
+        if (rect.contains(pt[0]) && rect.contains(pt[1])){
+            angle = -blob.getBoxFit().angle;
+        }
+        else {
+            angle = blob.getBoxFit().angle;
+        }
+        return frame;
     }
 
     @Override
@@ -27,28 +44,8 @@ public class BlobProcessorEX extends ColorBlobLocatorProcessor {
 
     }
 
-    @Override
-    public void addFilter(BlobFilter filter) {
 
-    }
-
-    @Override
-    public void removeFilter(BlobFilter filter) {
-
-    }
-
-    @Override
-    public void removeAllFilters() {
-
-    }
-
-    @Override
-    public void setSort(BlobSort sort) {
-
-    }
-
-    @Override
-    public List<Blob> getBlobs() {
-        return Collections.emptyList();
+    public double getAngle() {
+        return angle;
     }
 }
