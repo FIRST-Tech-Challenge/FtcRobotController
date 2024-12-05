1eae74cf28a3c354b44e3d266d1a75e4549af06b
buckholt <gbuckholtz@gmail.com>
2024-12-04 20:16:41 -0500

    most of the code, still need to map to the buttons to teleop commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ButtonSwitcher.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/ButtonSwitcher.java
@@ -2,16 +2,30 @@ package org.firstinspires.ftc.teamcode.Subsystems;
 
 import com.arcrobotics.ftclib.command.Subsystem;
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+
+import org.firstinspires.ftc.teamcode.RobotContainer;
 
 public class ButtonSwitcher extends SubsystemBase {
-    private boolean initalState;
-    public ButtonSwitcher(){
-        initalState = true;
+
+   private ButtonSwitcherState state;
+    public ButtonSwitcher()
+    {
+        state = ButtonSwitcherState.AUTO_MODE;
+
     }
 
     @Override
     public void periodic(){
         //check the state of the xbox switch mode button
+    }
+
+
+    public ButtonSwitcherState getButtonState() {
+        if (RobotContainer.driverOp.getTrigger(GamepadKeys.Trigger.LEFT_TRIGGER) > 0.5) {
+            state = (state == ButtonSwitcherState.AUTO_MODE) ? ButtonSwitcherState.MANUAL_MODE : ButtonSwitcherState.AUTO_MODE;
+        }
+        return state;
     }
 
 
