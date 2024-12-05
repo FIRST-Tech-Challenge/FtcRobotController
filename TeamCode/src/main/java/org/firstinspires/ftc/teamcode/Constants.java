ffedb4255538fb04700cd89f5fdaff3fa1f24fdb
Mr. Bacon <144743899+Mauric3Moss@users.noreply.github.com>
2024-12-04 18:03:26 -0700

    Create Constants.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+public interface constants {
+    double openClawValue = 1;
+    double closedClawValue = 0.4;
+    double openWristValue = 1;
+    double closedWristValue = 0;
+
+}
