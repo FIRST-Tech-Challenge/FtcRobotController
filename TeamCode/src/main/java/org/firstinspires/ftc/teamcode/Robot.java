7ffe70d3c964b44f2d2a527e8f0469eb5ea98651
Mr. Bacon <144743899+Mauric3Moss@users.noreply.github.com>
2024-12-04 18:01:47 -0700

    Update Robot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -14,6 +14,23 @@ public class Robot extends OpMode {
 
     @Override
     public void loop() {
+        
+    drivetrain.loop(gamepad1.left_stick_y,gamepad1.right_stick_x, gamepad1.left_stick_x);
+        
+    this.arm.runArmMotor(gamepad1.left_stick_y);
+        
+    this.linerarSlides.slidePower(gamepad1.right_stick_y);
+        
+    if (gamepad1.a) {
+            Claw.setServoClawPos(openClawValue);
+        } else {
+            Claw.setServoClawPos(closedClawValue);
+        }
 
+        if (gamepad1.b) {
+            Wrist.setServoWristPos(openWristValue);
+        } else {
+            Wrist.setServoWristPos(closedWristValue);
+        }
     }
 }
