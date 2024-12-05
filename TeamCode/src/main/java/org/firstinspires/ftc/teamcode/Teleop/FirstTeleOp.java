d237c692f265a5a52190fc460977d7f3da85d3f6
LegedaryKing09 <156239045+LegedaryKing09@users.noreply.github.com>
2024-12-05 17:48:56 -0500

    Update FirstTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FirstTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FirstTeleOp.java
@@ -5,12 +5,18 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
+@Config
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(group = "Teleop")
 public class FirstTeleOp extends LinearOpMode {
-    DcMotorEx lb, lf, rb, rf, intake1, intake2, vertical;
+    DcMotorEx lb, lf, rb, rf, intake, vertical1, vertical2;
     Servo inClaw, depClaw, inWrist, depWrist, transfer;
     ColorSensor transColor, inColor;
     IMU imu;
+
+    public static double CLAW_CLOSE = 0.0;
+    public static double CLAW_OPEN = 1.0;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -18,9 +24,9 @@ public class FirstTeleOp extends LinearOpMode {
         lf       = hardwareMap.get(DcMotorEx.class, "lf");
         rb       = hardwareMap.get(DcMotorEx.class, "rb");
         rf       = hardwareMap.get(DcMotorEx.class, "rf");
-        intake1  = hardwareMap.get(DcMotorEx.class, "intake1");
-        intake2  = hardwareMap.get(DcMotorEx.class, "intake2");
-        vertical = hardwareMap.get(DcMotorEx.class, "vertical");
+        intake  = hardwareMap.get(DcMotorEx.class, "intake");
+        vertical1  = hardwareMap.get(DcMotorEx.class, "vertical1");
+        vertical2 = hardwareMap.get(DcMotorEx.class, "vertical2");
         inClaw   = hardwareMap.get(Servo.class, "inClaw");
         depClaw  = hardwareMap.get(Servo.class, "depClaw");
         inWrist  = hardwareMap.get(Servo.class, "inWrist");
@@ -28,13 +34,43 @@ public class FirstTeleOp extends LinearOpMode {
         transfer = hardwareMap.get(Servo.class, "transfer");
         imu      = hardwareMap.get(IMU.class, "imu");
 
+        boolean isPressingStart = false;
+        boolean isPressingBack = false;
+        boolean isPressingLeftStickButton = false;
+        boolean isPressingRightStickButton = false;
+        boolean isPressingLeftBumper = false;
+        boolean isPressingRightBumper = false;
+        boolean isPressingLeftTrigger = false;
+        boolean isPressingRightTrigger = false;
+        boolean isPressingDpadUp = false;
+        boolean isPressingDpadDown = false;
+        boolean isPressingDpadLeft = false;
+        boolean isPressingDpadRight = false;
+        boolean isPressingA = false;
+        boolean isPressingB = false;
+        boolean isPressingX = false;
+        boolean isPressingY = false;
+        boolean hasSample = false;
+
 
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-        }
+            telemetry.update();
 
+            private  void openClaw() {
+                inClaw.setPosition(CLAW_OPEN);
+                hasSample = false;
+            }
+
+            private void closeClaw() {
+                inClaw.setPosition(CLAW_CLOSE);
+                hasSample = true;
+            }
+
+
+        }
     }
 }
