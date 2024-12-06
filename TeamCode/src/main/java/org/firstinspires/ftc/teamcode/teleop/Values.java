67ae393fd2e7c7bd0d274e0725a3576f05afb297
amazingjay26 <145387791+amazingjay26@users.noreply.github.com>
2024-12-06 16:32:00 -0500

    umm

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Values.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.teleop;
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class Values {
+    public static double intakeClawOpen = .46;
+    public static double intakeclawClose = .9;
+    public static double clawPivotPos = .5;
+
+    public static double wristDown = .5;
+    public static double wristUp = .6;
+
+    public static double intakeElbowDown = .2;
+    public static double intakeElbowUp = .5;
+
+    public static double outtakeElbowDown = .5;
+    public static double outtakeElbowUp = .7;
+    public static double outtakeClawClose = .5;
+    public static double outtakeclawUp = 1;
+
+    public static final double MID_SERVO   =  0.5 ;
+
+}
