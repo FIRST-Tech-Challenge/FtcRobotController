04e15de7ead31b142f59c6b969428204519af4a0
solinexi <solinexi@gmail.com>
2024-12-06 18:14:13 -0700

    my bad gang

--- Masters/src/main/java/org/firstinspires/ftc/masters/test/diffyTest.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/test/diffyTest.java
@@ -1,59 +1,53 @@
-//package org.firstinspires.ftc.masters.test;
-//
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.Servo;
-//
-//import org.firstinspires.ftc.masters.Localizer;
-//import org.firstinspires.ftc.masters.MecanumDrive;
-//import org.firstinspires.ftc.masters.ThreeDeadWheelLocalizer;
-//
-//import java.util.Locale;
-//
-//@TeleOp(name = "Diffy Test ")
-//public class diffyTest extends LinearOpMode {
-//
-//    public void runOpMode() throws InterruptedException {
-//
-//        Servo servo1 = hardwareMap.servo.get("servo1");
-//        Servo servo2 = hardwareMap.servo.get("servo2");
-//
-//
-//
-//        double servo1pos = 0.5;
-//        double servo2pos = 0.5;
-//
-//        servo1.setPosition(servo1pos);
-//        servo2.setPosition(servo2pos);
-//
-//        waitForStart();
-//
-//        while (opModeIsActive()) {
-//
-//            if (gamepad1.dpad_up) {
-//                servo1pos += 0.01;
-//                servo2pos += 0.01;
-//            }
-//
-//            if (gamepad1.dpad_down) {
-//                servo1pos -= 0.01;
-//                servo2pos -= 0.01;
-//            }
-//
-//            if (gamepad1.dpad_left) {
-//                servo1pos += 0.01;
-//                servo2pos -= 0.01;
-//            }
-//
-//            if (gamepad1.dpad_right) {
-//                servo1pos -= 0.01;
-//                servo2pos += 0.01;
-//            }
-//
-//            servo1.setPosition(servo1pos);
-//            servo2.setPosition(servo2pos);
-//
-//        }
-//    }
-//}
-//
+package org.firstinspires.ftc.masters.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Diffy Test ")
+public class diffyTest extends LinearOpMode {
+
+    public void runOpMode() throws InterruptedException {
+
+        Servo servo1 = hardwareMap.servo.get("elbow1");
+        Servo servo2 = hardwareMap.servo.get("elbow2");
+
+
+
+        double servo1pos = 0.5;
+        double servo2pos = 0.5;
+
+        servo1.setPosition(servo1pos);
+        servo2.setPosition(servo2pos);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            if (gamepad1.dpad_up) {
+                servo1pos += 0.01;
+                servo2pos += 0.01;
+            }
+
+            if (gamepad1.dpad_down) {
+                servo1pos -= 0.01;
+                servo2pos -= 0.01;
+            }
+
+            if (gamepad1.dpad_left) {
+                servo1pos += 0.01;
+                servo2pos -= 0.01;
+            }
+
+            if (gamepad1.dpad_right) {
+                servo1pos -= 0.01;
+                servo2pos += 0.01;
+            }
+
+            servo1.setPosition(servo1pos);
+            servo2.setPosition(servo2pos);
+
+        }
+    }
+}
+
