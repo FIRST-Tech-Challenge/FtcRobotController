1c53b05b6128a52672c8d578e80d0e6ed9a4e6fc
Alon Barkan <alon.barkan08@gmail.com>
2024-12-08 16:17:10 +0100

    better movement and more constents

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,61 +11,63 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
+        final double robotCenterToArm = 10;
         MeepMeep meepMeep = new MeepMeep(700);
         Pose2d blueBasket = new Pose2d(64, 64,Math.toRadians(45));
         Pose2d redBasket = new Pose2d(-64, -64,Math.toRadians(225));
         Pose2d blueRobot2StartPosition = new Pose2d(-58, 64,Math.toRadians(270));
         Pose2d blueRobot1StartPosition = new Pose2d(60, 60,Math.toRadians(270));
-        Pose2d redRobot1StartPosition = new Pose2d(0,0,0);
-        Pose2d redRobot2StartPosition = new Pose2d(0,0,0);
-        Pose2d blueGamePiece1 = new Pose2d(-48, 26,Math.toRadians(180));
-        Pose2d blueGamePiece2 = new Pose2d(-58, 26,Math.toRadians(180));
-        Pose2d blueGamePiece3 = new Pose2d(-68, 26,Math.toRadians(180));
-        Pose2d yellow1GamePiece1 = new Pose2d(68, 26,Math.toRadians(270));
-        Pose2d yellow1GamePiece2 = new Pose2d(58, 26,Math.toRadians(270));
-        Pose2d yellow1GamePiece3 = new Pose2d(48, 26,Math.toRadians(270));
-        Pose2d redGamePiece1 = new Pose2d(68, -26, Math.toRadians(0));
-        Pose2d redGamePiece2 = new Pose2d(58, -26, Math.toRadians(0));
-        Pose2d redGamePiece3 = new Pose2d(48, -26, Math.toRadians(0));
-        Pose2d yellow2GamePiece1 = new Pose2d(-68, -26,Math.toRadians(270));
-        Pose2d yellow2GamePiece2 = new Pose2d(-58, -26,Math.toRadians(270));
-        Pose2d yellow2GamePiece3 = new Pose2d(-48, -26,Math.toRadians(270));
+        Pose2d redRobot1StartPosition = new Pose2d(-60,-60,Math.toRadians(90));
+        Pose2d redRobot2StartPosition = new Pose2d(58,-64,Math.toRadians(90));
+        Pose2d blueGamePiece1 = new Pose2d(-48+robotCenterToArm, 26,Math.toRadians(180));
+        Pose2d blueGamePiece2 = new Pose2d(-58+robotCenterToArm, 26,Math.toRadians(180));
+        Pose2d blueGamePiece3 = new Pose2d(-68+robotCenterToArm, 26,Math.toRadians(180));
+        Pose2d yellow1GamePiece1 = new Pose2d(68, 26+robotCenterToArm,Math.toRadians(270));
+        Pose2d yellow1GamePiece2 = new Pose2d(58, 26+robotCenterToArm,Math.toRadians(270));
+        Pose2d yellow1GamePiece3 = new Pose2d(48, 26+robotCenterToArm,Math.toRadians(270));
+        Pose2d redGamePiece1 = new Pose2d(68-robotCenterToArm, -26, Math.toRadians(0));
+        Pose2d redGamePiece2 = new Pose2d(58-robotCenterToArm, -26, Math.toRadians(0));
+        Pose2d redGamePiece3 = new Pose2d(48-robotCenterToArm, -26, Math.toRadians(0));
+        Pose2d yellow2GamePiece1 = new Pose2d(-68, -26-robotCenterToArm,Math.toRadians(90));
+        Pose2d yellow2GamePiece2 = new Pose2d(-58, -26-robotCenterToArm,Math.toRadians(90));
+        Pose2d yellow2GamePiece3 = new Pose2d(-48, -26-robotCenterToArm,Math.toRadians(90));
 
         // Declare our first bot
-        RoadRunnerBotEntity myFirstBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity blue1Bot = new DefaultBotBuilder(meepMeep)
                 // We set this bot to be blue
                 .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myFirstBot.runAction(myFirstBot.getDrive().actionBuilder(blueRobot1StartPosition)
-//                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
-//                .strafeToLinearHeading(yellow1GamePiece1.position,yellow1GamePiece1.heading)
-//                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
-//                .strafeToLinearHeading(yellow1GamePiece2.position,yellow1GamePiece2.heading)
-//                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
-//                .strafeToLinearHeading(yellow1GamePiece3.position,yellow1GamePiece3.heading)
-
-                .splineToLinearHeading(blueBasket,Math.toRadians(0))
-                .strafeToLinearHeading(yellow1GamePiece3.position,yellow1GamePiece3.heading)
-                .splineToLinearHeading(blueBasket,Math.toRadians(0))
-                .strafeToLinearHeading(yellow1GamePiece2.position,yellow1GamePiece2.heading)
-                .splineToLinearHeading(blueBasket,Math.toRadians(0))
+        blue1Bot.runAction(blue1Bot.getDrive().actionBuilder(blueRobot1StartPosition)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
                 .strafeToLinearHeading(yellow1GamePiece1.position,yellow1GamePiece1.heading)
                 .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+                .strafeToLinearHeading(yellow1GamePiece2.position,yellow1GamePiece2.heading)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+                .strafeToLinearHeading(yellow1GamePiece3.position,yellow1GamePiece3.heading)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+
+//                .splineToLinearHeading(blueBasket,Math.toRadians(0))
+//                .strafeToLinearHeading(yellow1GamePiece3.position,yellow1GamePiece3.heading)
+//                .splineToLinearHeading(blueBasket,Math.toRadians(0))
+//                .strafeToLinearHeading(yellow1GamePiece2.position,yellow1GamePiece2.heading)
+//                .splineToLinearHeading(blueBasket,Math.toRadians(0))
+//                .strafeToLinearHeading(yellow1GamePiece1.position,yellow1GamePiece1.heading)
+//                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
 
 
 
                 .build());
 
         // Declare out second bot
-        RoadRunnerBotEntity mySecondBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity blue2Bot = new DefaultBotBuilder(meepMeep)
                 // We set this bot to be red
                 .setColorScheme(new ColorSchemeBlueLight())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        mySecondBot.runAction(mySecondBot.getDrive().actionBuilder(blueRobot2StartPosition)
+        blue2Bot.runAction(blue2Bot.getDrive().actionBuilder(blueRobot2StartPosition)
 //                .strafeToLinearHeading(basket.position,basket.heading)
                 .splineToLinearHeading(blueBasket, Math.toRadians(22.5))
                 .strafeToLinearHeading(blueGamePiece1.position,blueGamePiece1.heading)
@@ -76,40 +78,65 @@ public class MeepMeepTesting {
                 .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
                 .build());
 
-        RoadRunnerBotEntity myThirdBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity red2Bot = new DefaultBotBuilder(meepMeep)
                 // We set this bot to be blue
                 .setColorScheme(new ColorSchemeRedDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myThirdBot.runAction(myFirstBot.getDrive().actionBuilder(new Pose2d(23.7, -69, Math.toRadians(90)))
-                .waitSeconds(1)
+        red2Bot.runAction(red2Bot.getDrive().actionBuilder(redRobot2StartPosition)
+                        //.waitSeconds(1)
 //                .strafeToLinearHeading(redBasket.position,redBasket.heading)
-                .splineToLinearHeading(redBasket, Math.toRadians(22.5))
-                .strafeToLinearHeading(redGamePiece3.position,redGamePiece3.heading)
+                        .splineToLinearHeading(redBasket, Math.toRadians(22.5))
+                        .strafeToLinearHeading(redGamePiece3.position,redGamePiece3.heading)
                 .strafeToLinearHeading(redBasket.position,redBasket.heading)
                 .strafeToLinearHeading(redGamePiece2.position,redGamePiece2.heading)
                 .strafeToLinearHeading(redBasket.position,redBasket.heading)
                 .strafeToLinearHeading(redGamePiece1.position,redGamePiece1.heading)
                 .build());
 
-        RoadRunnerBotEntity myForthBot = new DefaultBotBuilder(meepMeep)
+        RoadRunnerBotEntity red1Bot = new DefaultBotBuilder(meepMeep)
                 .setColorScheme(new ColorSchemeRedLight())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myForthBot.runAction(myForthBot.getDrive().actionBuilder(new Pose2d(-23.7, -69, Math.toRadians(90)))
-
+        red1Bot.runAction(red1Bot.getDrive().actionBuilder(redRobot1StartPosition)
+                .strafeToLinearHeading(redBasket.position,redBasket.heading)
+                .strafeToLinearHeading(yellow2GamePiece1.position,yellow2GamePiece1.heading)
+                .strafeToLinearHeading(redBasket.position,redBasket.heading)
+                .strafeToLinearHeading(yellow2GamePiece2.position,yellow2GamePiece2.heading)
+                .strafeToLinearHeading(redBasket.position,redBasket.heading)
+                .strafeToLinearHeading(yellow2GamePiece3.position,yellow2GamePiece3.heading)
+                .strafeToLinearHeading(redBasket.position,redBasket.heading)
                 .build()
         );
+
+        RoadRunnerBotEntity blue3Bot = new DefaultBotBuilder(meepMeep)
+                // We set this bot to be red
+                .setColorScheme(new ColorSchemeBlueDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        blue3Bot.runAction(blue3Bot.getDrive().actionBuilder(blueRobot2StartPosition)
+//                .strafeToLinearHeading(basket.position,basket.heading)
+                .splineToLinearHeading(blueBasket, Math.toRadians(22.5))
+                .strafeToLinearHeading(yellow1GamePiece1.position,yellow1GamePiece1.heading)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+                .strafeToLinearHeading(yellow1GamePiece2.position,yellow1GamePiece2.heading)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+                .strafeToLinearHeading(yellow1GamePiece3.position,yellow2GamePiece3.heading)
+                .strafeToLinearHeading(blueBasket.position,blueBasket.heading)
+                .build());
+
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 // Add both of our declared bot entities
-                .addEntity(myFirstBot)
-                .addEntity(mySecondBot)
-                .addEntity(myThirdBot)
-                .addEntity(myForthBot)
+                .addEntity(blue1Bot)
+                .addEntity(blue2Bot)
+                .addEntity(red2Bot)
+                .addEntity(red1Bot)
+                .addEntity(blue3Bot)
                 .start();
     }
 }
