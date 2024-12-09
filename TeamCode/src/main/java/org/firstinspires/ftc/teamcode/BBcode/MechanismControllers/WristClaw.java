7acff142808708e3c8bde6d72eacc3a23afc9298
Matthew <zacharykeepers@gmail.com>
2024-12-08 20:19:51 -0600

    Everything is working for state machine, tuned wrist positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/MechanismControllers/WristClaw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BBcode/MechanismControllers/WristClaw.java
@@ -17,7 +17,7 @@ public class WristClaw {
     //Variable Storage:
     double openPosition = 0.57;
     double closePosition = 0.2;
-    double upPosition = 0.855;
+    double upPosition = 0.82;
     double flipPosition = 0.45;
     double downPosition = 0.4;
     double dumpPosition = 0.3;
