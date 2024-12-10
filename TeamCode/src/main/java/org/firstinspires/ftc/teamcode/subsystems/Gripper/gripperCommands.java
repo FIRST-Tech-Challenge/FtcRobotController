ab61e0e7528ca4e372db1181bbac9018565ae54b
Thunder Bolts <you@example.com>
2024-12-10 17:21:17 +0000

    started

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Gripper/gripperCommands.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.subsystems.Gripper;
+
+import org.firstinspires.ftc.teamcode.subsystems.Gripper.gripperSubsystem;
+import org.firstinspires.ftc.teamcode.utils.BT.BTCommand;
+
+public class gripperCommands extends BTCommand {
+    public Command openGripper(){
+
+    }
+    public Command closeGripper(){
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Gripper/gripperSubsystem.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsystems.Gripper;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class gripperSubsystem {
+
+    private HardwareMap map;
+    Servo angularServo1;
+    Servo angularServo2;
+
+}
