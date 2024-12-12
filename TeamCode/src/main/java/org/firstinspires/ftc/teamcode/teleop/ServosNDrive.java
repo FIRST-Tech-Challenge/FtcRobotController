3178ba859647a403a5d6e0097506030348e9169d
amazingjay26 <145387791+amazingjay26@users.noreply.github.com>
2024-12-12 15:23:56 -0500

    okay

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServosNDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/ServosNDrive.java
@@ -29,12 +29,16 @@
 
 package org.firstinspires.ftc.teamcode.teleop;
 
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.teleop.Values;
 /*
  * This OpMode executes a POV Game style Teleop for a direct drive robot
@@ -48,7 +52,7 @@ import org.firstinspires.ftc.teamcode.teleop.Values;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="all da servos and Drive", group="Linear OpMode")
+@TeleOp(name="all da servos and imu", group="Linear OpMode")
 //@Disabled
 public class ServosNDrive extends LinearOpMode {
 
@@ -75,6 +79,8 @@ public class ServosNDrive extends LinearOpMode {
     Gamepad currentGamepad1 = new Gamepad();
     Gamepad previousGamepad1 = new Gamepad();
 
+    private IMU imu;
+
     @Override
     public void runOpMode() {
 
@@ -93,7 +99,7 @@ public class ServosNDrive extends LinearOpMode {
         //slide motors
         outtakeSlide1 = hardwareMap.get(DcMotor.class, "Motor5");
         outtakeSlide2 = hardwareMap.get(DcMotor.class, "Motor6");
-        outtakeSlide1.setDirection(DcMotor.Direction.REVERSE);
+        outtakeSlide2.setDirection(DcMotor.Direction.REVERSE);
         outtakeSlide1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         outtakeSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
@@ -107,18 +113,27 @@ public class ServosNDrive extends LinearOpMode {
 
         outtakeElbow = hardwareMap.get(Servo.class, "6");
         outtakeClaw = hardwareMap.get(Servo.class, "7");
-        rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
 
         clawPivot.setPosition(Values.MID_SERVO);
         wrist.setPosition(Values.MID_SERVO);
-        intakeElbow.setPosition(Values.MID_SERVO);
+        intakeElbow.setPosition(Values.intakeElbowUp);
 
         //make servo go slower
 //        intakeSlide1.scaleRange(2000, 600);
 //        intakeSlide1.scaleRange(2000, 600);
         intakeSlide1.setPosition(Values.slide1in);
         intakeSlide2.setPosition(Values.slide2in);
+
+        imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters myIMUparameters;
+
+        myIMUparameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
+
+        // Initialize IMU using Parameters
+        imu.initialize(myIMUparameters);
+        imu.resetYaw();
 
         telemetry.update();
 
@@ -131,32 +146,36 @@ public class ServosNDrive extends LinearOpMode {
             previousGamepad1.copy(currentGamepad1);
             currentGamepad1.copy(gamepad1);
 
-            double max;
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -currentGamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  currentGamepad1.left_stick_x;
-            double yaw     =  currentGamepad1.right_stick_x;
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
-            // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
-            double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+            if (currentGamepad1.options && !previousGamepad1.options) {
+//                posX = drive.pose.position.x;
+//                posY = drive.pose.position.y;
+//               drive.pose =  new Pose2d(posX,posY,Math.toRadians(90));
+                imu.resetYaw();
             }
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+            //double botHeading = drive.pose.heading.toDouble();
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            // Rotate the movement direction counter to the bot's rotation
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;  // Counteract imperfect strafing
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+            leftFrontDrive.setPower(frontLeftPower);
+            leftBackDrive.setPower(backLeftPower);
+            rightFrontDrive.setPower(frontRightPower);
+            rightBackDrive.setPower(backRightPower);
 
 
             //slide motors
@@ -196,7 +215,7 @@ public class ServosNDrive extends LinearOpMode {
                 if(intakeSlide1.getPosition() == Values.slide1out){
                     slidesIn();
                 }
-                else if(intakeSlide1.getPosition() == Values.slide1out){
+                else if(intakeSlide1.getPosition() != Values.slide1out){
                     slidesOut();
                 }
                 //else{
@@ -249,13 +268,12 @@ public class ServosNDrive extends LinearOpMode {
             //outtake
             if (currentGamepad1.triangle && !previousGamepad1.triangle){
                 if (outtakeElbow.getPosition() != Values.outtakeElbowDown){
-                    outtakeClaw.setPosition(Values.outtakeclawOpen);
+                    outtakeClaw.setPosition(Values.outakeclawOpen);
                 }else if (outtakeElbow.getPosition() == Values.outtakeElbowDown){
                     outtakeClaw.setPosition(Values.outtakeClawClose);
                     sleep(500);
                     outtakeElbow.setPosition(Values.outtakeElbowUp);
                 }
-                outtakeElbow.setPosition(Values.outtakeElbowDown);
             }
             //pivot!
             if (currentGamepad1.dpad_left) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Values.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Values.java
@@ -4,26 +4,25 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class Values {
     public static double intakeClawOpen = .7;
-    public static double intakeclawClose = .9;
-    public static double clawPivotPos = .5;
+    public static double intakeclawClose = .95;
 
-    public static double wristDown = .25;
+    public static double wristDown = .1;
     public static double wristUp = .6;
 
     public static double intakeElbowDown = 1;
-    public static double intakeElbowUp = .2;
+    public static double intakeElbowUp = 0;
 
     public static double outtakeElbowDown = .7;
     public static double outtakeElbowUp = .2;
     public static double outtakeClawClose = .55;
-    public static double outtakeclawOpen = .7;
+    public static double outakeclawOpen = .7;
 
     public static final double MID_SERVO   =  0.5 ;
 
     public static double slide1in = 1;
     public static double slide2in = 0;
-    public static double slide1out = .9;
-    public static double slide2out = .1;
+    public static double slide1out = .6;
+    public static double slide2out = .4;
 
     public static int slideMax = 2300;
     public static double velocity = 1.00;
