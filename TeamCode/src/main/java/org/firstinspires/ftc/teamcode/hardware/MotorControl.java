1f3cb31312d668dfbb59d697255fbc4dba8b56f2
900012768 <seangood522@gmail.com>
2024-12-12 15:01:31 -0500

    publish motor control class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/MotorControl.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+public class MotorControl extends LinearOpMode {
+    DcMotorEx frontLeft, frontRight, backLeft, backRight;
+    @Override
+    public void runOpMode() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        frontLeft.setPower(0.5);
+        frontRight.setPower(0.5);
+        backLeft.setPower(0.5);
+        backRight.setPower(0.5);
+    }
+}
