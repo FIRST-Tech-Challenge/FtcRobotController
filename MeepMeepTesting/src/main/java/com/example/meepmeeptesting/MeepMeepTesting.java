110949a643f6722450702ca1c5b9f66d9dd3bf7b
ConstantineSebs <150309844+ConstantineSebs@users.noreply.github.com>
2024-12-14 09:49:12 -0500

    update todo

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -15,13 +15,14 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-10, 60, 0))
+                        //todo finish this
                         .strafeLeft(5)
                         .turn(Math.toRadians(-90))
                         .forward(30)
                         .splineToLinearHeading(new Pose2d(-58, 36, 0), 0)
                         .build());
 
-
+        
         meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
