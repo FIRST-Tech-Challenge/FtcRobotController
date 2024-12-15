9029ace72540939d45df3f0ad5e813b691eef5a3
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-12-14 19:48:15 -0800

    It might work!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RightAuto.java
@@ -23,12 +23,14 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
 
+import dev.aether.collaborative_multitasking.ITask;
 import dev.aether.collaborative_multitasking.MultitaskScheduler;
 import dev.aether.collaborative_multitasking.OneShot;
 import dev.aether.collaborative_multitasking.Scheduler;
 import dev.aether.collaborative_multitasking.SharedResource;
 import dev.aether.collaborative_multitasking.TaskTemplate;
 import dev.aether.collaborative_multitasking.ext.Pause;
+import kotlin.Pair;
 import kotlin.Unit;
 
 @Autonomous // Appear on the autonomous drop down
@@ -190,6 +192,7 @@ public class RightAuto extends LinearOpMode {
     private Ramps ramps;
     private LoopStopwatch loopTimer;
     private Speed2Power speed2Power;
+    private MultitaskScheduler scheduler;
 
     private MoveToTask moveTo(Scheduler s, Pose target) {
         return new MoveToTask(
@@ -197,28 +200,67 @@ public class RightAuto extends LinearOpMode {
         );
     }
 
-    @Override
-    public void runOpMode() {
-        MultitaskScheduler scheduler = new MultitaskScheduler();
+    public static final double FLIP_UP = 0.98;
+    public static final double FLIP_DOWN = 0.04;
+    public static final double H_SLIDE_OUT = 0.35;
+    public static final double H_SLIDE_IN = 0.1;
+    public static final double CLAW_OPEN = 0.55;
+    public static final double CLAW_CLOSE = 0.02;
+    public static final double WRIST_UP = 0.46;
+    public static final double WRIST_BACK = 0.30;
+    public static final double VERTICAL_SLIDE_SPEED = 0.75;
 
-        hardware = new Hardware(hardwareMap);
-        tracker = new EncoderTracking(hardware);
-        ElapsedTime scoreTimer = new ElapsedTime();
-        ElapsedTime finalizeTimer = new ElapsedTime();
-        AtomicReference<Double> scoredIn = new AtomicReference<>((double) 0);
-        double doneIn = 0;
-        loopTimer = new LoopStopwatch();
-        speed2Power = new Speed2Power(0.20); // Set a speed2Power corresponding to a speed of 0.15 seconds
-        ramps = new Ramps(
-                Ramps.linear(2.0),
-                Ramps.linear(1 / 12.0),
-//                Easing.power(3.0, 12.0),
-                Ramps.LimitMode.SCALE
-        );
+    private ITask run(Runnable it) {
+        return new OneShot(scheduler, it);
+    }
 
-        scheduler.task(new BackgroundTasks(
-                scheduler, tracker, loopTimer
-        ));
+    private ITask wait(double seconds) {
+        return new Pause(scheduler, seconds);
+    }
+
+    private Pair<ITask, ITask> flipOut() {
+        ITask first = scheduler.task(run(() -> hardware.horizontalSlide.setPosition(H_SLIDE_OUT)));
+        ITask last = first
+                .then(wait(0.500))
+                .then(run(() -> hardware.clawFlip.setPosition(FLIP_DOWN)))
+                .then(wait(0.500));
+        return new Pair<>(first, last);
+    }
+
+    private Pair<ITask, ITask> flipIn() {
+        ITask first = scheduler.task(run(() -> hardware.clawFlip.setPosition(FLIP_UP)));
+        ITask last = first
+                .then(wait(0.500))
+                .then(run(() -> hardware.horizontalSlide.setPosition(H_SLIDE_IN)))
+                .then(wait(0.500));
+        return new Pair<>(first, last);
+    }
+
+    private Pair<ITask, ITask> specimenWallPick() {
+        ITask first = scheduler.task(run(() -> hardware.claw.setPosition(CLAW_OPEN)));
+        ITask last = first
+                .then(wait(1.000))
+                .then(run(() -> hardware.wrist.setPosition(WRIST_UP)))
+                .then(wait(1.000))
+                .then(run(() -> hardware.arm.setTargetPosition(45)))
+                .then(wait(0.500))
+                .then(run(() -> hardware.claw.setPosition(CLAW_CLOSE)))
+                .then(wait(1.000))
+                .then(run(() -> {
+                    hardware.verticalSlide.setTargetPosition(300);
+                    hardware.verticalSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    hardware.verticalSlide.setPower(VERTICAL_SLIDE_SPEED);
+                }))
+                .then(wait(1.000))
+                .then(run(() -> hardware.wrist.setPosition(WRIST_BACK)))
+                .then(wait(1.000))
+                .then(run(() -> hardware.arm.setTargetPosition(10)))
+                .then(wait(1.000))
+                .then(run(() -> hardware.verticalSlide.setTargetPosition(0)));
+        return new Pair<>(first, last);
+    }
+
+    public void mainAuto() {
         scheduler.task(new OneShot(scheduler, () -> {
             hardware.claw.setPosition(0.55);
             hardware.wrist.setPosition(0.28);
@@ -228,11 +270,11 @@ public class RightAuto extends LinearOpMode {
         }));
         scheduler
                 .task(moveTo(scheduler, new Pose(6, -40, 0)))
-                .then(new OneShot(scheduler, () -> hardware.wrist.setPosition(0.44)))
-                .then(new Pause(scheduler, 0.500))
-                .then(new OneShot(scheduler, () -> hardware.claw.setPosition(0.02)))
-                .then(new Pause(scheduler, 0.500))
-                .then(new OneShot(scheduler, () -> {
+                .then(run(() -> hardware.wrist.setPosition(0.44)))
+                .then(wait(0.500))
+                .then(run(() -> hardware.claw.setPosition(0.02)))
+                .then(wait(0.500))
+                .then(run(() -> {
                     hardware.wrist.setPosition(0.28);
                     hardware.claw.setPosition(0.55);
                 }))
@@ -240,6 +282,46 @@ public class RightAuto extends LinearOpMode {
                 .then(moveTo(scheduler, new Pose(4, -48, 0)));
 //                .then(moveTo(scheduler,
 //                        new Pose(65, -12, Math.toRadians(0))))
+
+    }
+
+    public void test() {
+        scheduler.task(new OneShot(scheduler, () -> {
+                    hardware.claw.setPosition(0.55);
+                    hardware.wrist.setPosition(0.28);
+                    hardware.twist.setPosition(0.17);
+                    hardware.arm.setTargetPosition(0);
+                    hardware.arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                }))
+                .then(flipOut())
+                .then(wait(2.0))
+                .then(flipIn());
+    }
+
+    @Override
+    public void runOpMode() {
+        scheduler = new MultitaskScheduler();
+
+        hardware = new Hardware(hardwareMap);
+        tracker = new EncoderTracking(hardware);
+        loopTimer = new LoopStopwatch();
+        speed2Power = new Speed2Power(0.20); // Set a speed2Power corresponding to a speed of 0.15 seconds
+        ramps = new Ramps(
+                Ramps.linear(2.0),
+                Ramps.linear(1 / 12.0),
+//                Easing.power(3.0, 12.0),
+                Ramps.LimitMode.SCALE
+        );
+        ElapsedTime finalizeTimer = new ElapsedTime();
+//        AtomicReference<Double> scoredIn = new AtomicReference<>((double) 0);
+        double doneIn = 0;
+
+        // queue everything up
+        scheduler.task(new BackgroundTasks(
+                scheduler, tracker, loopTimer
+        ));
+//        mainAuto();
+        test();
         // park
 
         telemetry.addLine("Initialized.");
@@ -249,7 +331,6 @@ public class RightAuto extends LinearOpMode {
         waitForStart(); // Wait for start button
 
         telemetry.update();
-        scoreTimer.reset();
         finalizeTimer.reset();
 
         while (scheduler.hasJobs() && opModeIsActive()) {
@@ -265,7 +346,7 @@ public class RightAuto extends LinearOpMode {
         while (opModeIsActive()) {
             hardware.driveMotors.setAll(0);
             telemetry.addLine(String.format("done in %.2fs", doneIn));
-            telemetry.addLine(String.format("scored in %.2fs", scoredIn.get()));
+//            telemetry.addLine(String.format("scored in %.2fs", scoredIn.get()));
             telemetry.addData("x", tracker.getPose().x()); // Print x attribute for pose
             telemetry.addData("y", tracker.getPose().y()); // Print y attribute for pose
             telemetry.addData("heading (rad)", tracker.getPose().heading()); // Print the heading in radians
