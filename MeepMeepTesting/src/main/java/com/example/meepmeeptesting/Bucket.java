9ba755834b229d6f1c6bf5b4624868ae97f7bb9e
JohnSebs <153127103+JohnSebs@users.noreply.github.com>
2024-12-14 21:33:39 -0500

    Auto Paths

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Bucket.java
@@ -0,0 +1,50 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class Bucket {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new ActualBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(37, 60, Math.toRadians(270)))
+                        .forward(3)
+                        .splineToConstantHeading(new Vector2d(47,40),Math.toRadians(280))           
+                        .waitSeconds(3)
+                        //pickup
+                        .setTangent(Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(53,58,Math.toRadians(45)),Math.toRadians(70))
+                        .waitSeconds(1)
+                        //Slides up
+                        //open claw
+                        .setTangent(Math.toRadians(225))
+                        .back(3)
+                        //slides down
+                        .splineToLinearHeading(new Pose2d(58,40,Math.toRadians(270)),Math.toRadians(270))
+                        .waitSeconds(3)
+                        //pickup
+                        .setTangent(Math.toRadians(90))
+                        .splineToLinearHeading(new Pose2d(53,58,Math.toRadians(45)),Math.toRadians(70))
+                        .waitSeconds(1)
+                        //Slides up
+                        //open claw
+                        .setTangent(Math.toRadians(225))
+                        .back(3)
+
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Clips.java
@@ -0,0 +1,59 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import org.rowlandhall.meepmeep.MeepMeep;
+import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class Clips {
+    public static void main(String[] args) {
+        System.setProperty("sun.java2d.opengl", "true");
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new ActualBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-37, 60, Math.toRadians(270)))
+                        //todo finish this
+                        //Slides Up
+                        .forward(1)
+                        .splineToConstantHeading(new Vector2d(-7,32),Math.toRadians(270))
+                        .waitSeconds(5)
+                        //slides down
+                        //Open claw
+                        .back(3)
+                        //slides all the way down
+                        .splineToConstantHeading(new Vector2d(-27,38),Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(-46,13,Math.toRadians(90)),Math.toRadians(180))
+                        .forward(45)
+                        .back(45)
+                        .strafeLeft(10)
+                        .forward(45)
+                        .back(45)
+                        .strafeLeft(7)
+                        .forward(45)
+                        .back(5)
+                        .strafeRight(15)
+                        //slides up
+                        //open claw
+                        .waitSeconds(2)
+                        .forward(7)
+                        .waitSeconds(5)
+                        //close claw
+                        .setTangent(Math.toRadians(270))
+                        //Slides Up
+                        .splineToConstantHeading(new Vector2d(-7,32),Math.toRadians(270))
+                        //slides down
+                        //Open claw
+
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
