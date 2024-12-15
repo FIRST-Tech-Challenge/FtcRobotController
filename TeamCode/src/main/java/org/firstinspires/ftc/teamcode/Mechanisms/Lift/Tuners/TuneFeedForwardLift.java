a6dd6fba7b1778e207274464aab8ed7ffc4c02a0
Vbwfe <67kevin26@gmail.com>
2024-12-15 15:20:18 -0500

    Update TuneFeedForwardLift.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Lift/Tuners/TuneFeedForwardLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Lift/Tuners/TuneFeedForwardLift.java
@@ -9,6 +9,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -20,8 +21,6 @@ import org.firstinspires.ftc.teamcode.Hardware.Sensors.Encoder;
 import org.firstinspires.ftc.teamcode.Mechanisms.Drivetrain.Drivetrain;
 import org.firstinspires.ftc.teamcode.Mechanisms.Lift.Lift;
 
-import java.util.HashSet;
-
 @Config
 @Autonomous(name = "Test Lift Feed Forward", group = "Autonomous")
 public class TuneFeedForwardLift  extends LinearOpMode {
@@ -29,38 +28,36 @@ public class TuneFeedForwardLift  extends LinearOpMode {
     FtcDashboard dashboard;
     Battery battery;
     Encoder encoder;
-    private HashSet<Action> runningActions = new HashSet<>();
-
     public static double height = 12;
-
     public void runOpMode() throws InterruptedException {
         Battery battery = new Battery(hardwareMap);
-
         Lift lift = new Lift(hardwareMap, battery);
         this.encoder = new Encoder(hardwareMap.get(DcMotorEx.class, "liftMotorLeft"));
         int currentPosition = encoder.getCurrentPosition();
-        ElapsedTime wait = new ElapsedTime();
-
+        ElapsedTime Timer = new ElapsedTime();
         if (gamepad1.a) {
-            runningActions.add(
-                    lift.moveToHeight(height)
-            );
-            runningActions.add(
-                    lift.moveToHeight(currentPosition)
-            );
+//            runningActions.add(
+//                    lift.moveToHeight(height)
+//            );
+//            runningActions.add(
+//                    lift.moveToHeight(currentPosition)
+//            );
+            Actions.runBlocking((lift.moveToHeight(height)));
+            if(Timer.seconds() > 3) {
+                Actions.runBlocking(lift.moveToHeight(currentPosition));
 
 
-
-                    new Action() {
-                        @Override
-                        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-                            telemetryPacket.addLine("Height: " + lift.currentPosition);
+                new Action() {
+                    @Override
+                    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+                        telemetryPacket.addLine("Height: " + lift.currentPosition);
 //                                    telemetryPacket.addLine("Velocity: " + lift.liftMotorLeft.getVelocity());
 //                                    telemetryPacket.addLine("Acceleration: " + lift.liftMotorLe);
 //                                    telemetryPacket.addLine("maxHeight: " + maxHeight);
-                            return true;
-                        }
+                       return false;
                     }
+                };
+            }
         }
     }
 }
