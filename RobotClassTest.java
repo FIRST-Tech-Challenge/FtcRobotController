103dbe7487f036ef56aadf9fa86a6bd02fbf508b
Caden Bowman <cadenbowman53@gmail.com>
2024-12-19 01:28:25 -0500

    Add files via upload

---
+++ RobotClassTest.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import org.
+
+@Autonomous
+import org.firstinspires.ftc.teamcode.auto.OdometryMotor;
+
+public class RobotClassTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update;
+        Robot robot = new Robot(hardwareMap);
+
+        while (opModeIsActive()) {
+
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+            Robot.driveRelative(270, 0, 1);
+            Robot.driveRelative(180, 1, 0);
+            Robot.driveRelative(180, 0, -1);
+            Robot.driveRelative(180, -1, 0);
+
+        }
+
+
+
+    }
+}
