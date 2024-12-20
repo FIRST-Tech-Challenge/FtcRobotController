a46d2ca77211af20fe766235adc1d7bc0e48c292
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-12-19 17:38:55 -0800

    additional safeguard against dropping items into the robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumTeleOp2.java
@@ -447,6 +447,7 @@ public class MecanumTeleOp2 extends LinearOpMode {
     public void ScoreHighBasket2() {
         // prevent doing this by accident
         if (liftProxy.lift.getCurrentPosition() < 1000) return;
+        if (hardware.arm.getCurrentPosition() < 190) return;
         abandonLock(Locks.ArmAssembly);
         abandonLock(liftProxy.CONTROL);
         scheduler.add(groupOf(inner -> inner.add(run(() -> hardware.wrist.setPosition(0.94)))
