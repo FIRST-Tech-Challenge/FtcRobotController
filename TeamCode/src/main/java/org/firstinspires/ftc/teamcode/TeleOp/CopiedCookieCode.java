48ff807b9cf0cd0e8494438d47d628d627560235
Traffic Cone Pigeon Girls <trafficconepigeongirls@gmail.com>
2024-12-19 19:22:30 -0700

    Renamed for clarity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CopiedCookieCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/CopiedCookieCode.java
@@ -64,9 +64,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-+
 
-@TeleOp(name="Solo - Regular Opmode better", group="Linear Opmode")
+@TeleOp(name="Copied Cookie Code", group="Linear Opmode")
 public class CopiedCookieCode extends LinearOpMode {
     //HardwarePushbot robot = new HardwarePushbot();
 
