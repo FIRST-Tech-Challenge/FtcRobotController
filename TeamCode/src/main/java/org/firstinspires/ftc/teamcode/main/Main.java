cd33c19e51a851c04db310a17f7513990276c4b6
ManosFps1pt <manos.dragasakis@gmail.com>
2024-12-24 10:06:25 +0200

    Update Main.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/main/Main.java
@@ -77,31 +77,6 @@ public class Main extends LinearOpMode {
     public SparkFunOTOS odometry         = null;
 
 
-
-    /* This constant is the number of encoder ticks for each degree of rotation of the arm.
-    To find this, we first need to consider the total gear reduction powering our arm.
-    First, we have an external 20t:100t (5:1) reduction created by two spur gears.
-    But we also have an internal gear reduction in our motor.
-    The motor we use for this arm is a 117RPM Yellow Jacket. Which has an internal gear
-    reduction of ~50.9:1. (more precisely it is 250047/4913:1)
-    We can multiply these two ratios together to get our final reduction of ~254.47:1.
-    The motor's encoder counts 28 times per rotation. So in total you should see about 7125.16
-    counts per rotation of the arm. We divide that by 360 to get the counts per degree. */
-    // final double ARM_TICKS_PER_DEGREE =
-    //         28 // number of encoder ticks per rotation of the bare motor
-    //                 * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
-    //                 * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
-    //                 * 1/360.0; // we want ticks per degree, not per rotation
-
-    //for the 223rpm motor we have the following:
-    final double ARM_TICKS_PER_DEGREE =
-
-            753.2 // This is the exact gear ratio of the (26.9:1) Yellow Jacket gearbox
-                    //* 28 // number of encoder ticks per rotation of the bare motor
-                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
-                    * 1/360.0; // we want ticks per degree, not per rotation
-
-
     /* These constants hold the position that the arm is commanded to run to.
     These are relative to where the arm was located when you start the OpMode. So make sure the
     arm is reset to collapsed inside the robot before you start the program.
@@ -114,12 +89,12 @@ public class Main extends LinearOpMode {
     as far from the starting position, decrease it. */
 
     final double ARM_COLLAPSED_INTO_ROBOT  = 0;
-    final double ARM_COLLECT               = 5 * ARM_TICKS_PER_DEGREE;
-    final double ARM_CLEAR_BARRIER         = 20 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SPECIMEN        = 90 * ARM_TICKS_PER_DEGREE;
-    final double ARM_SCORE_SAMPLE_IN_LOW   = 90 * ARM_TICKS_PER_DEGREE;
-    final double ARM_ATTACH_HANGING_HOOK   = 110 * ARM_TICKS_PER_DEGREE;
-    final double ARM_WINCH_ROBOT           = 10  * ARM_TICKS_PER_DEGREE;
+    final double ARM_COLLECT               = armDegreesToTicks(5);
+    final double ARM_CLEAR_BARRIER         = armDegreesToTicks(20);
+    final double ARM_SCORE_SPECIMEN        = armDegreesToTicks(90);
+    final double ARM_SCORE_SAMPLE_IN_LOW   = armDegreesToTicks(90);
+    final double ARM_ATTACH_HANGING_HOOK   = armDegreesToTicks(110);
+    final double ARM_WINCH_ROBOT           = armDegreesToTicks(10);
 
     /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
     final double INTAKE_COLLECT    = -0.7;
@@ -131,26 +106,15 @@ public class Main extends LinearOpMode {
     final double WRIST_FOLDED_OUT  = 0.8;
 
     /* A number in degrees that the triggers can adjust the arm position by */
-    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
+    final double FUDGE_FACTOR = armDegreesToTicks(15);
 
     /* Variables that are used to set the arm to a specific position */
     double armPosition = (int)ARM_COLLAPSED_INTO_ROBOT;
     double armPositionFudgeFactor;
-
-    /*
-     * 312 rpm motor: 537.7 ticks per revolution
-     * 4 stage viper slide (240mm): 5,8 rotations to fully expand
-     * max travel distance: 696mm
-     * ticks per mm = (537,7 * 5,8) ticks / (696) mm = 4,48 ticks / mm
-     */
-    final double LIFT_TICKS_PER_MM = (537.7 * 5.8) / 696;
-
-    final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
-    final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
-    final double LIFT_SCORING_IN_HIGH_BASKET = 480 * LIFT_TICKS_PER_MM; // 480
-
+    final double LIFT_COLLAPSED = liftMotorMmToTicks(0);
+    final double LIFT_SCORING_IN_LOW_BASKET = liftMotorMmToTicks(0);
+    final double LIFT_SCORING_IN_HIGH_BASKET = liftMotorMmToTicks(480); // 480
     double liftPosition = LIFT_COLLAPSED;
-
     double cycleTime = 0;
     double loopTime = 0;
     double oldTime = 0;
@@ -391,7 +355,7 @@ public class Main extends LinearOpMode {
             to a value.
              */
 
-            if (armPosition < 45 * ARM_TICKS_PER_DEGREE){
+            if (armPosition < armDegreesToTicks(45)) {
                 armLiftComp = (0.25568 * liftPosition);
             }
             else{
@@ -494,6 +458,34 @@ public class Main extends LinearOpMode {
 
 
         }
+    }
+
+    public int armDegreesToTicks(double degrees) {
+        return (int) (
+                753.2 // This is the exact gear ratio of the (26.9:1) Yellow Jacket gearbox
+                * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                * 1/360.0 // we want ticks per degree, not per rotation
+                * degrees // the specified degrees
+        );
+    }
+
+    public int liftMotorMmToTicks(double mm) {
+        /*
+         * 312 rpm motor: 537.7 ticks per revolution
+         * 4 stage viper slide (240mm): 5,8 rotations to fully expand
+         * max travel distance: 696mm
+         * ticks per mm = (537,7 * 5,8) ticks / (696) mm = 4,48 ticks / mm
+         */
+        return (int)
+        (
+            (
+                (
+                    537.7 * 5.8
+                ) // total ticks
+                / 696
+            ) // viper slide unfolded length
+            * mm // specified length
+        );
     }
 /*
     private void configureOtos() {
