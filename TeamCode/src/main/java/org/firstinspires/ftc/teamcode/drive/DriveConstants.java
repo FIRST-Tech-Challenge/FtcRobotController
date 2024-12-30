62afa9c6ea641abcc9337985b2d1bfd0c0415b72
Ray <raymond.krause@gmail.com>
2024-12-29 19:51:04 -0600

    Updates after FeedForward Tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -56,9 +56,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.016; //1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.0045;
+    public static double kStatic = 0.01;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
