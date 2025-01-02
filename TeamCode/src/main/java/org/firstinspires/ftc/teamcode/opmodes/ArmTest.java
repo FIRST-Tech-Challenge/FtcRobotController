f260fcbe8831e7e98529cfe6c86eac8521d4f219
Itay Yagil <itayyagil@gmail.com>
2025-01-02 16:23:46 +0200

    Code for elevator arm movement - I couldn't test it due the robot being a little sorry bitch and refusing to run my code while insisting on running unrelated DELETED code...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTest.java
@@ -8,19 +8,39 @@ public class ArmTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        DcMotor leftFrontDrive  = hardwareMap.get(DcMotor.class, "armLeft");
+        DcMotor leftDrive  = hardwareMap.get(DcMotor.class, "armLeft"); // port 0
+        DcMotor rightDrive = hardwareMap.get(DcMotor.class, "armRight"); // port 3
         waitForStart();
         if (opModeIsActive()) {
 
             // Pre-run
             while (opModeIsActive()) {
-                if (gamepad1.y){
-                        leftFrontDrive.setPower(-1);
+                //////////////////////////////////////////////////////////////////////////////////////
+                /// TEST FOR ELEVATOR OPERATION - NOT TESTED DUE TO THE ROBOT BEING A LITTLE BITCH ///
+                //////////////////////////////////////////////////////////////////////////////////////
+
+                // make the motors brake when [power == 0]
+                // should stop the elevator from retracting because of gravity...
+                rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+                // Extend / Retract
+                if (gamepad1.right_bumper) {
+                    rightDrive.setPower(0.1);
+                    leftDrive.setPower(0.1);
+                } else if (gamepad1.left_bumper) {
+                    rightDrive.setPower(-0.1);
+                    leftDrive.setPower(-0.1);
                 }
-                else if (gamepad1.x) {
-                    leftFrontDrive.setPower(-1);
+                // Brake
+                else {
+                    rightDrive.setPower(0);
+                    leftDrive.setPower(0);
                 }
-                else leftFrontDrive.setPower(0);
+
+                /// Telemetry
+                telemetry.addData("Right: ", rightDrive.getPower());
+                telemetry.addData("Left: ",  leftDrive.getPower());
             }
         }
     }
