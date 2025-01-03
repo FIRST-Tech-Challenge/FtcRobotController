9092622d2b3064f9260bd6ee35c24e6f2505535f
Darren <2darrenchan@gmail.com>
2025-01-02 17:28:54 -0800

    FUNNEL ACTION AND READY

--- TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeFunnelAction.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeFunnelAction.java
@@ -8,26 +8,24 @@ public class IntakeFunnelAction extends KActionSet {
 
     public IntakeFunnelAction(IntakeClaw intake) {
 
-        KServoAutoAction moveBigPivot = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_BIG_PIVOT_FUNNEL_READY_POS);
-        moveBigPivot.setName("moveBigPivot");
-        this.addAction(moveBigPivot);
+        KServoAutoAction funnelMoveBigPivot = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_BIG_PIVOT_TRANSFER_READY_POS);
+        funnelMoveBigPivot.setName("moveBigPivot");
+        this.addAction(funnelMoveBigPivot);
 
         KServoAutoAction funnelMoveSmallPivot = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_SMALL_PIVOT_FUNNEL_GRAB_POS);
         funnelMoveSmallPivot.setName("funnelMoveSmallPivot");
-        this.addAction(funnelMoveSmallPivot);
-
-        KServoAutoAction funnelMoveBigPivot = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_BIG_PIVOT_FUNNEL_GRAB_POS);
-        funnelMoveBigPivot.setName("funnelMoveBigPivot");
         this.addAction(funnelMoveBigPivot);
 
-        KServoAutoAction openClaw = new KServoAutoAction(intake.getIntakeClawServo(), IntakeClaw.INTAKE_CLAW_CLOSE);
-        openClaw.setName("openClaw");
-        this.addAction(openClaw);
 
+        KServoAutoAction closeClaw = new KServoAutoAction(intake.getIntakeClawServo(), IntakeClaw.INTAKE_CLAW_CLOSE);
+        closeClaw.setName("openClaw");
+        closeClaw.setDependentActions(funnelMoveSmallPivot, funnelMoveBigPivot);
+        this.addAction(closeClaw);
 
+        KServoAutoAction moveBigPivot2 = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_BIG_PIVOT_TRANSFER_READY_POS);
+        moveBigPivot2.setName("moveBigPivot");
+        moveBigPivot2.setDependentActions(funnelMoveSmallPivot, funnelMoveBigPivot, closeClaw);
+        this.addAction(moveBigPivot2);
 
     }
-
-
-
 }
--- TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeFunnelReady.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/intake/IntakeFunnelReady.java
@@ -5,19 +5,8 @@ import com.kalipsorobotics.actions.autoActions.KServoAutoAction;
 import com.kalipsorobotics.modules.IntakeClaw;
 
 public class IntakeFunnelReady extends KActionSet {
+
     public IntakeFunnelReady(IntakeClaw intake) {
-
-        KServoAutoAction moveSmallSweep = new KServoAutoAction(intake.getIntakeSmallSweepServo(), IntakeClaw.INTAKE_SMALL_SWEEP_TRANSFER_READY_POS);
-        moveSmallSweep.setName("moveSmallSweep");
-        this.addAction(moveSmallSweep);
-
-        KServoAutoAction linkageRetract = new KServoAutoAction(intake.getIntakeLinkageServo(), IntakeClaw.INTAKE_LINKAGE_IN_POS);
-        linkageRetract.setName("linkageRetract");
-        this.addAction(linkageRetract);
-
-        KServoAutoAction openClaw = new KServoAutoAction(intake.getIntakeClawServo(), IntakeClaw.INTAKE_CLAW_OPEN);
-        openClaw.setName("openClaw");
-        this.addAction(openClaw);
 
         KServoAutoAction moveBigPivot = new KServoAutoAction(intake.getIntakeBigSweepServo(), IntakeClaw.INTAKE_BIG_PIVOT_FUNNEL_READY_POS);
         moveBigPivot.setName("moveBigPivot");
@@ -27,11 +16,17 @@ public class IntakeFunnelReady extends KActionSet {
         moveSmallPivot.setName("moveSmallPivot");
         this.addAction(moveSmallPivot);
 
+        KServoAutoAction moveSmallSweep = new KServoAutoAction(intake.getIntakeSmallSweepServo(), IntakeClaw.INTAKE_SMALL_SWEEP_TRANSFER_READY_POS);
+        moveSmallSweep.setName("moveSmallSweep");
+        this.addAction(moveSmallSweep);
 
+        KServoAutoAction openClaw = new KServoAutoAction(intake.getIntakeClawServo(), IntakeClaw.INTAKE_CLAW_OPEN);
+        openClaw.setName("openClaw");
+        this.addAction(openClaw);
 
+        KServoAutoAction linkageRetract = new KServoAutoAction(intake.getIntakeLinkageServo(), IntakeClaw.INTAKE_LINKAGE_IN_POS);
+        linkageRetract.setName("linkageRetract");
+        this.addAction(linkageRetract);
 
     }
-
-
-
 }
