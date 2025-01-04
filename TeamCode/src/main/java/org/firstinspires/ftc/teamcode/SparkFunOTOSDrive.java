9bcc1ac9d52ffdf488a8e93a8e576fd82c689816
Martin Dutang <martin.dutang@gmail.com>
2025-01-04 13:22:26 -0500

    Test servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SparkFunOTOSDrive.java
@@ -39,7 +39,7 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
 
         // RR localizer note: These units are inches and radians.
-        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0.01);
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0.024, 0.0961, 0.01);
 
         // Here we can set the linear and angular scalars, which can compensate for
         // scaling issues with the sensor measurements. Note that as of firmware`
@@ -57,7 +57,7 @@ public class SparkFunOTOSDrive extends MecanumDrive {
         // multiple speeds to get an average, then set the linear scalar to the
         // inverse of the error. For example, if you move the robot 100 inches and
         // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
-        public double linearScalar = 1;
+        public double linearScalar = 1.0047;
         public double angularScalar = 0.9982;
     }
 
