70e71c8845fb96ba125df8567661171d2a86e131
esteban0304 <120337377+esteban0304@users.noreply.github.com>
2025-01-05 15:57:41 -0500

    Updating SplineTest from latest quickstart release

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HMapConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HMapConfig.java
@@ -2,20 +2,22 @@ package org.firstinspires.ftc.teamcode;
 
 public class HMapConfig {
     //input hardware//
+
     public static final String FRONT_LEFT_MOTOR = "frontLeft";
     public static final String BACK_LEFT_MOTOR = "backLeft";
     public static final String FRONT_RIGHT_MOTOR = "frontRight";
     public static final String BACK_RIGHT_MOTOR = "backRight";
+    public static final String INPUT_SLIDES = "inputSlides";
     public static final String INPUT_ARM_PITCH_LEFT = "inputArmPitchLeft";
     public static final String INPUT_ARM_PITCH_RIGHT = "inputArmPitchRight";
     public static final String INPUT_ELBOW_PITCH = "inputElbowPitch" ;
     public static final String INPUT_WRIST_ROLL = "inputWristRoll";
     public static final String INPUT_CLAW = "inputClaw";
     public static final String OUTPUT_SLIDES_LEFT = "outputSlidesLeft";
-    public static final String  OUTPUT_SLIDES_RIGHT = "outputSlidesRight";
+    public static final String OUTPUT_SLIDES_RIGHT = "outputSlidesRight";
     public static final String OUTPUT_ELBOW_PITCH_RIGHT = "outputElbowPitchRight";
     public static final String OUTPUT_ELBOW_PITCH_LEFT = "outputElbowPitchLeft";
-    public static final String OUTPUT_RIST_ROLL = "outputRistRoll";
+    public static final String OUTPUT_WRIST_ROLL = "outputWristRoll";
     public static final String OUTPUT_CLAW = "outputClaw";
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/claw.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class claw {
@@ -23,6 +22,17 @@ public class claw {
     Servo output_wrist_roll;
     Servo output_claw;
     public void clawHW(HardwareMap hwm, Telemetry tm, Gamepad gp){
-
+        input_slides = hwm.dcMotor.get(HMapConfig.INPUT_SLIDES);
+        input_arm_pitch_left = hwm.servo.get(HMapConfig.INPUT_ARM_PITCH_LEFT);
+        input_arm_pitch_right = hwm.servo.get(HMapConfig.INPUT_ARM_PITCH_RIGHT);
+        input_elbow_pitch = hwm.servo.get(HMapConfig.INPUT_ELBOW_PITCH);
+        input_wrist_roll = hwm.servo.get(HMapConfig.INPUT_WRIST_ROLL);
+        input_claw = hwm.servo.get(HMapConfig.INPUT_CLAW);
+        output_slides_left = hwm.dcMotor.get(HMapConfig.OUTPUT_SLIDES_LEFT);
+        output_slides_right = hwm.dcMotor.get(HMapConfig.OUTPUT_SLIDES_RIGHT);
+        output_elbow_pitch_right = hwm.servo.get(HMapConfig.OUTPUT_ELBOW_PITCH_RIGHT);
+        output_elbow_pitch_left = hwm.servo.get(HMapConfig.OUTPUT_ELBOW_PITCH_LEFT);
+        output_wrist_roll = hwm.servo.get(HMapConfig.OUTPUT_WRIST_ROLL);
+        output_claw = hwm.servo.get(HMapConfig.OUTPUT_CLAW);
     }
 }
