07ec9665cc07d842070ad7a29f920bd182341d2b
esteban0304 <120337377+esteban0304@users.noreply.github.com>
2025-01-05 15:15:27 -0500

    Updating SplineTest from latest quickstart release

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HMapConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HMapConfig.java
@@ -7,16 +7,16 @@ public class HMapConfig {
     public static final String FRONT_RIGHT_MOTOR = "frontRight";
     public static final String BACK_RIGHT_MOTOR = "backRight";
     public static final String INPUT_ARM_PITCH_LEFT = "inputArmPitchLeft";
-    public static final String input_arm_pitch_right = "inputArmPitchRight";
-    public static final String input_elbow_pitch = "inputElbowPitch" ;
-    public static final String input_wrist_roll = "inputWristRoll";
-    public static final String input_claw = "inputClaw";
-    public static final String output_slides_left = "outputSlidesLeft";
-    public static final String output_slides_right = "outputSlidesRight";
-    public static final String output_elbow_pitch_right = "outputElbowPitchRight";
-    public static final String output_elbow_pitch_left = "outputElbowPitchLeft";
-    public static final String output_wrist_roll = "outputRistRoll";
-    public static final String output_claw = "outputClaw";
+    public static final String INPUT_ARM_PITCH_RIGHT = "inputArmPitchRight";
+    public static final String INPUT_ELBOW_PITCH = "inputElbowPitch" ;
+    public static final String INPUT_WRIST_ROLL = "inputWristRoll";
+    public static final String INPUT_CLAW = "inputClaw";
+    public static final String OUTPUT_SLIDES_LEFT = "outputSlidesLeft";
+    public static final String  OUTPUT_SLIDES_RIGHT = "outputSlidesRight";
+    public static final String OUTPUT_ELBOW_PITCH_RIGHT = "outputElbowPitchRight";
+    public static final String OUTPUT_ELBOW_PITCH_LEFT = "outputElbowPitchLeft";
+    public static final String OUTPUT_RIST_ROLL = "outputRistRoll";
+    public static final String OUTPUT_CLAW = "outputClaw";
 
 }
 
