176478f9dbcbe3192b656f54da0e8f9d4e8f206a
Caden Bowman <cadenbowman53@gmail.com>
2025-01-06 11:17:31 -0500

    Added different types of movement curves
    Movement curves are designed to control robot power output more precisely to create more precise and accurate movement.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MovementCurves.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/MovementCurves.java
@@ -4,17 +4,76 @@ public class MovementCurves {
     //each of these curves work by taking in a value (0,1) and returning an
     //adjusted value following a curve function, returns a value (0,1);
 
+    private static boolean inRange(double x) {
+        return x >= 0 || x <= 1;
+    }
+
+    public static double linear(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return 1-Math.abs(2*x-1);
+    }
+
+
     public static double sinCurve(double x) {
-        if(x < 0 || x > 1) {
-            x = 0;
+        if(!inRange(x)) {
+            return -1;
         }
         return Math.sin(Math.PI*(x));
     }
-    public static double circleArc(double x) {
-        if(x < 0 || x > 1) {
-            x = 0;
+
+    public static double circleCurve(double x) {
+        if(!inRange(x)) {
+            return -1;
         }
         return Math.sqrt(1 - Math.pow((1 - x), 2));
     }
 
+    public static double quadraticCurve(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return 1-4*Math.pow(x-.5, 2);
+    }
+
+    public static double roundedSquareCurve(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return Math.pow(1-Math.pow(1-x,4), 0.25);
+    }
+
+    public static double parametricCurve(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return -(2*Math.pow(x,2)-2*x) /
+                (2*Math.pow(2*Math.pow(x,2)-2*x+1, 2));
+    }
+
+    public static double exponentialEaseIn(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return Math.pow(Math.E, 4*x-4);
+    }
+
+    public static double exponentialEaseOut(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return Math.pow(Math.E, -4*x);
+    }
+
+    //follows the normal distribution curve
+    public static double Normal(double x) {
+        if(!inRange(x)) {
+            return -1;
+        }
+        return Math.pow(Math.E, -4*Math.pow(2*x-1, 2));
+    }
 }
+
+
+
