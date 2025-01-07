b3acde8b3d8fc2e770b4e36ddad7e616161c0d6e
Justin Liu <liujustin604@gmail.com>
2025-01-07 13:28:57 -0500

    park auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SyborgsParkAuto.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="SyborgsAuto")
+public class SyborgsAuto extends LinearOpMode {
+
+}
