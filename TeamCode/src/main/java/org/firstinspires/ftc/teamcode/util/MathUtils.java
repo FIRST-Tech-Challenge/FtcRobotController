b444e2b4b194c9d3b92f72faab88a34c925b6d03
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2025-01-07 12:39:14 -0500

    Math Utils

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/MathUtils.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.util;
+
+public class MathUtils {
+    public static final double TWO_PI = Math.PI * 2;
+    public static final double HALF_PI = Math.PI / 2;
+
+    public static double normalizeAngle(double angle) {
+        return angle - TWO_PI * Math.floor((angle + Math.PI) / TWO_PI);
+    }
+
+    public static double round(double num, int digits) {
+        return Math.round(num * Math.pow(10, digits)) / Math.pow(10, digits);
+    }
+
+    public static double normalize(double value, double lowIn, double highIn, double lowOut, double highOut) {
+        return (Math.max(Math.min(value, highIn), lowIn) - lowIn) / (highIn - lowIn) * (highOut - lowOut) + lowOut;
+    }
+
+    public static double lawOfCosines(double a, double b, double c) {
+        // c = sqrt(a^2 + b^2 - 2ab cos C)
+        // c^2 = a^2 + b^2 - 2ab cos C
+        // 2ab cos C = a^2 + b^2 - c^2
+        // cos C = (a^2 + b^2 - c^2)/2ab
+        return Math.acos((a*a + b*b - c*c) / (2*a*b));
+    }
+}
