eb1dadda29169045b780da046af35e093b1f7ad7
Itay Yagil <itayyagil@gmail.com>
2025-01-07 18:44:44 +0200

    Connection problems caused by build team are fixed - New problem is innacurate elevator positions (probably bc of poor connections) as well as stuck motor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ArmTest.java
@@ -11,7 +11,7 @@ public class ArmTest extends LinearOpMode {
 
     public static class Params {
         public double power = 1;
-        public int ticks = 3930;
+        public int ticks = 3850;
 
     }
     public static Params PARAMS = new Params();
@@ -22,26 +22,50 @@ public class ArmTest extends LinearOpMode {
         DcMotor rightDrive = hardwareMap.get(DcMotor.class, "armRight");
         DcMotor frontDrive = hardwareMap.get(DcMotor.class, "frontArm");
         rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
         // make the motors brake when [power == 0]
         // should stop the elevator from retracting because of gravity...
         rightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Getting some weird behaviour here where one motor is at -4500 or something
+        // while the other one is at 0 or 1
+        // - I believe it is because poor sensor connections by the build team
+        // Poor illustration below cause i got bored waiting...
+        /*
+        //
+        //   _ -> rightPos = 0  which is not correct
+        //   |          _ -> leftPos = 0
+        //   |  .____.  |
+        // [[============]] <- robot
+        */
 
         int rightStartingPos = rightDrive.getCurrentPosition();
         int leftStartingPos = leftDrive.getCurrentPosition();
 
+        // TODO: Find the root cause of the problem (inaccurate pos readings)
+        /// TEMP FIX TO COMBAT POOR WIRING ERRORS
+        leftStartingPos = 0;
+        rightStartingPos = 0;
+        ///
+
+        int frontStartingPos = frontDrive.getCurrentPosition();
+
         rightDrive.setTargetPosition(rightStartingPos);
         leftDrive.setTargetPosition(leftStartingPos);
+        //frontDrive.setTargetPosition(frontStartingPos);
 
         rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //frontDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         waitForStart();
         if (opModeIsActive()) {
 
             // Pre-run
             while (opModeIsActive()) {
-                // Extend / Retract
+//                // Extend / Retract
                 if (gamepad1.right_bumper) {
                     rightDrive.setPower(PARAMS.power);
                     leftDrive.setPower(PARAMS.power);
@@ -54,14 +78,22 @@ public class ArmTest extends LinearOpMode {
                     leftDrive.setTargetPosition(leftStartingPos);
                 }
 
-                /// ARM MECHANISM TEST ///
+                // TODO: Change to targetposition movement once motor is actually fixed
+                /// ARM MECHANISM TEST - TEMPORARY///
+                // Arad requested me to program this to use power and NOT targets
+                // Dont blame me
                 if(gamepad1.y) {
-                    frontDrive.setPower(0.1);
+                    frontDrive.setPower(-PARAMS.power);
                 } else if (gamepad1.a) {
-                    frontDrive.setPower(-0.1 );
+                    frontDrive.setPower(PARAMS.power);
+                }
+                else {
+                    frontDrive.setPower(0);
                 }
 
-                /// Telemetry
+                /* ##################################################
+                             TELEMETRY ADDITIONS
+               ################################################## */
                 telemetry.addData("Right: ", rightDrive.getCurrentPosition());
                 telemetry.addData("Left: ",  leftDrive.getCurrentPosition());
                 telemetry.addData("Front: ", frontDrive.getCurrentPosition());
