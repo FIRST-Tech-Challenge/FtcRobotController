d23c5f5707f0ce7eff21c4f08c92e9be496f5ae9
AdvayRoongta <152351411+AdvayRoongta@users.noreply.github.com>
2025-01-08 18:47:04 -0800

    Update ServoTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -3,7 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
-
+//testing push directly from git (broswer)
 @Autonomous
 public class ServoTest extends LinearOpMode {
     private Servo servoTest;
