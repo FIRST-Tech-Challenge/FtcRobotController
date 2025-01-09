2fb3cd1a3c128ca8a8ed0c36601d4087e9f79757
caden <cadenbowman53@gmail.com>
2025-01-09 03:50:33 -0500

    added DPAD control and speed multipliers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotManual.java
@@ -10,7 +10,15 @@ public class RobotManual extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
 
+        //change SLOWSPEED to change how dpad works
+        final double SLOWSPEED = .2;
 
+        //change value to change the speed of joysticks
+        final double TOTALSPEED = 1;
+
+        double speed;
+        double strafe;
+        double turn;
 
         DcMotor backRightDrive = null;
         DcMotor frontRightDrive = null;
@@ -34,10 +42,21 @@ public class RobotManual extends LinearOpMode {
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            frontLeftDrive.setPower(gamepad1.left_stick_y+ gamepad1.left_stick_x + gamepad1.right_stick_x);
-            frontRightDrive.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x - gamepad1.right_stick_x);
-            backLeftDrive.setPower(gamepad1.left_stick_y - gamepad1.left_stick_x + gamepad1.right_stick_x);
-            backRightDrive.setPower(gamepad1.left_stick_y + gamepad1.left_stick_x - gamepad1.right_stick_x);
+
+            speed = -gamepad1.left_stick_y * TOTALSPEED;
+            speed = gamepad1.dpad_up ? speed+SLOWSPEED : speed;
+            speed = gamepad1.dpad_down ? speed-SLOWSPEED : speed;
+
+            strafe = gamepad1.left_stick_x * TOTALSPEED;
+            strafe = gamepad1.dpad_right ? strafe+SLOWSPEED : strafe;
+            strafe = gamepad1.dpad_left ? strafe-SLOWSPEED : strafe;
+
+            turn = gamepad1.right_stick_x * TOTALSPEED;
+
+            frontLeftDrive.setPower(speed + strafe + turn);
+            frontRightDrive.setPower(speed - strafe - turn);
+            backLeftDrive.setPower(speed - strafe + turn);
+            backRightDrive.setPower(speed + strafe - turn);
         }
     }
 }
