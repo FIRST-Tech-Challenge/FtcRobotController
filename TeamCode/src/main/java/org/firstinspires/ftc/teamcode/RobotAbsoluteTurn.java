1b11ea1062a94d2a94552dac38489cfe8e98d87a
caden <cadenbowman53@gmail.com>
2025-01-09 19:29:22 -0500

    RobotAbsoluteTurn mecanum drive finished.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAbsoluteTurn.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotAbsoluteTurn.java
@@ -1,13 +1,25 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.*;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.libraries.Vector2D.Vector2D;
+import org.firstinspires.ftc.teamcode.libraries.MovementCurves.MovementCurves;
+import static java.lang.Math.*;
+
+
 
 @TeleOp
 public class RobotAbsoluteTurn extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
-
+        IMU  imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.FORWARD,
+                RevHubOrientationOnRobot.UsbFacingDirection.UP));
+        imu.initialize(parameters);
+        imu.resetYaw();
         //change SLOWSPEED to change how dpad works
         final double SLOWSPEED = .2;
 
@@ -16,7 +28,13 @@ public class RobotAbsoluteTurn extends LinearOpMode {
 
         double speed;
         double strafe;
-        double turn;
+        double turn = 0;
+
+
+        double currentFacing;
+        double angleDifference;
+
+        Vector2D direction = new Vector2D(0, 0);
 
         DcMotor backRightDrive = null;
         DcMotor frontRightDrive = null;
@@ -40,6 +58,7 @@ public class RobotAbsoluteTurn extends LinearOpMode {
         waitForStart();
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
+            currentFacing = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
 
             speed = -gamepad1.left_stick_y * TOTALSPEED;
             speed = gamepad1.dpad_up ? speed+SLOWSPEED : speed;
@@ -49,12 +68,50 @@ public class RobotAbsoluteTurn extends LinearOpMode {
             strafe = gamepad1.dpad_right ? strafe+SLOWSPEED : strafe;
             strafe = gamepad1.dpad_left ? strafe-SLOWSPEED : strafe;
 
-            turn = gamepad1.right_stick_x * TOTALSPEED;
+            direction.setVector(gamepad1.right_stick_x, -gamepad1.right_stick_y);
+
+            angleDifference = currentFacing-direction.getAngle();
+
+
+            if (angleDifference > 180) {
+                angleDifference -= 360;
+            }
+
+            if (angleDifference < -180) {
+                angleDifference += 360;
+            }
+
+            if (angleDifference > 2 && angleDifference < 180) {
+                turn = MovementCurves.circleCurve(angleDifference/360);
+            } else if (angleDifference < -2 && angleDifference > -180) {
+
+                turn = -MovementCurves.circleCurve(-angleDifference/360);
+
+            } else {
+                turn = 0;
+            }
+
+
+
+
+            turn *= direction.getMagnitude();
+
+
 
             frontLeftDrive.setPower(speed + strafe + turn);
             frontRightDrive.setPower(speed - strafe - turn);
             backLeftDrive.setPower(speed - strafe + turn);
             backRightDrive.setPower(speed + strafe - turn);
+
+
+            telemetry.addData("rightStickx", gamepad1.right_stick_x);
+            telemetry.addData("leftSticky", gamepad1.right_stick_y);
+            telemetry.addData("currentFacing", currentFacing);
+            telemetry.addData("wanted angle", direction.getAngle());
+            telemetry.addData("angleDifference", angleDifference);
+            telemetry.addData("vec Mag", direction.getMagnitude());
+            telemetry.update();
+
         }
     }
 }
