6e3a9411e4f8b1bfd241041e869da1fba5c88e05
Darren <2darrenchan@gmail.com>
2025-01-10 20:32:24 -0800

    outtakepivotaction

---
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/outtake/OuttakePivotAction.java
@@ -0,0 +1,23 @@
+package com.kalipsorobotics.actions.outtake;
+
+import com.kalipsorobotics.actions.KActionSet;
+import com.kalipsorobotics.actions.autoActions.KServoAutoAction;
+import com.kalipsorobotics.modules.Outtake;
+
+public class OuttakePivotAction extends KActionSet {
+    public OuttakePivotAction(Outtake outtake, int targetPos){
+
+        if (outtake.outtakePivotServo.getCurrentPosition() > 0.65 && targetPos < 0.65) {
+            KServoAutoAction pivotOuttakeMiddle = new KServoAutoAction(outtake.getOuttakePivotServo(), 0.65);
+            pivotOuttakeMiddle.setName("pivotOuttakeMiddle");
+            this.addAction(pivotOuttakeMiddle);
+        }
+
+        else {
+            KServoAutoAction pivotOuttake = new KServoAutoAction(outtake.getOuttakePivotServo(), targetPos);
+            pivotOuttake.setName("pivotOuttake");
+            this.addAction(pivotOuttake);
+        }
+    }
+}
+
