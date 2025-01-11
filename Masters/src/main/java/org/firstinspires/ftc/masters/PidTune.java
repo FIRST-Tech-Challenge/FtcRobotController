5d017e5e8aa642ffad72f1171193344f67109b1e
solinexi <solinexi@gmail.com>
2025-01-11 07:59:42 -0700

    FPA: Build Day 2, Electric Boogaloo

--- Masters/src/main/java/org/firstinspires/ftc/masters/PidTune.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/PidTune.java
@@ -16,37 +16,39 @@ public class PidTune extends OpMode {
 
     private PIDController controller;
 
-    public static double p = 0.00057, i = 0, d = 0.000000015;
-    public static double f = 0.002;
+    public static double p = 0.002, i = 0, d = 0;
+    public static double f = 0.09;
 
     public static int target = 0;
 
     //
 
-    DcMotor extension1, extension2;
+    DcMotor rightFrontMotor, outtakeSlideRight, outtakeSlideLeft;
 
     @Override
     public void init() {
         controller = new PIDController(p, i, d);
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
-        extension2 = hardwareMap.dcMotor.get("extension2");
-        extension2.setDirection(DcMotorSimple.Direction.REVERSE);
-        extension2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightFrontMotor = hardwareMap.get(DcMotorEx.class, "frontRight");
+        rightFrontMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        extension1 = hardwareMap.dcMotor.get("extension1");
+        outtakeSlideRight = hardwareMap.dcMotor.get("vertSlideRight");
+        outtakeSlideRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        outtakeSlideLeft= hardwareMap.dcMotor.get("vertSlideLeft");
 
     }
 
     public void loop(){
         controller.setPID(p,i,d);
-        int rotatePos = extension2.getCurrentPosition();
+        int rotatePos = -(rightFrontMotor.getCurrentPosition());
         double pid = controller.calculate(rotatePos, target);
 
         double lift = pid + f;
 
-        extension2.setPower(lift);
-        extension1.setPower(lift);
+        outtakeSlideLeft.setPower(lift);
+        outtakeSlideRight.setPower(lift);
 
         telemetry.addData("ArmPos", rotatePos);
         telemetry.addData("Target", target);
--- Masters/src/main/java/org/firstinspires/ftc/masters/test/whatsExtendo.java
+++ Masters/src/main/java/org/firstinspires/ftc/masters/test/whatsExtendo.java
@@ -3,47 +3,35 @@ package org.firstinspires.ftc.masters.test;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
 @Config // Enables FTC Dashboard
-@TeleOp(group ="Test", name = "LED Test")
-public class ledTest extends LinearOpMode {
+@TeleOp(name = "WhatsExtendo?")
+public class whatsExtendo extends LinearOpMode {
 
     private final FtcDashboard dashboard = FtcDashboard.getInstance();
 
-    public static double Blank = 0;
+    DcMotor intakeExtendo;
 
     public void runOpMode() throws InterruptedException {
+
+        intakeExtendo= hardwareMap.dcMotor.get("intakeExtendo");
+        intakeExtendo.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeExtendo.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeExtendo.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
 
         telemetry.update();
 
-        Servo servo = hardwareMap.servo.get("led");
-
         waitForStart();
 
         while (opModeIsActive()) {
 
-            while (true){
-
-                servo.setPosition(.611);
-                sleep(1000);
-                servo.setPosition(.722);
-                sleep(1000);
-                servo.setPosition(1);
-                sleep(1000);
-                servo.setPosition(.722);
-                sleep(1000);
-                servo.setPosition(.611);
-                sleep(1000);
-
-            }
+            telemetry.addData("Position", intakeExtendo.getCurrentPosition());
+            telemetry.update();
 
         }
     }
