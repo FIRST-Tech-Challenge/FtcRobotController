32b823a199acd57b1c2df47de634006cc266651a
Alan Huang <alan.wenhao.huang@gmail.com>
2025-01-12 11:08:07 -0800

    fixed filter issues

--- TeamCode/src/main/java/com/kalipsorobotics/test/TestKalmanOdometry.java
+++ TeamCode/src/main/java/com/kalipsorobotics/test/TestKalmanOdometry.java
@@ -1,6 +1,7 @@
 package com.kalipsorobotics.test;
 
 import com.kalipsorobotics.localization.OdometrySpark;
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -8,7 +9,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 @Disabled
 public class TestKalmanOdometry extends LinearOpMode {
-    OdometrySpark odometry;
+    SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, "sprk sensor OTOS");
+    OdometrySpark odometry = new OdometrySpark(otos);
     @Override
     public void runOpMode() throws InterruptedException {
         while (opModeIsActive()) {
