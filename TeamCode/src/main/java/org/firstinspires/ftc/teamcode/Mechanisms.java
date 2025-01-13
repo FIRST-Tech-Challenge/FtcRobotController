3e281269ff8cfc7c8bff77dd96d8dfe277cf5aa4
JasonL337 <jampanoff@gmail.com>
2025-01-13 17:12:58 -0600

    gradle and mechanism test changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms.java
@@ -115,12 +115,6 @@ public class Mechanisms {
         outTakePivotRight = opMode.hardwareMap.servo.get("otpr");
         outTakePivotLeft = opMode.hardwareMap.servo.get("otpl");
 
-        inTakeLift = opMode.hardwareMap.dcMotor.get("itl");
-        inTakeLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        outTakeLiftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        outTakeLiftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         /*// color sensor
         intakeColorSensor = opMode.hardwareMap.colorSensor.get("colorSensor");*/
 
@@ -139,44 +133,6 @@ public class Mechanisms {
         intakePivotR = opMode.hardwareMap.servo.get(("itpr"));
 
         brakePosIT = inTakeLift.getCurrentPosition();
-        outTakeLiftRight = opMode.hardwareMap.dcMotor.get("otlr");
-        outTakeLiftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        outTakeLiftLeft = opMode.hardwareMap.dcMotor.get("otll");
-        outTakeLiftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        // outtake servos
-        outTakeClaw = opMode.hardwareMap.servo.get(("otc"));
-        outTakeFlip = opMode.hardwareMap.servo.get(("otf"));
-        outTakePivotRight = opMode.hardwareMap.servo.get("otpr");
-        outTakePivotLeft = opMode.hardwareMap.servo.get("otpl");
-
-        inTakeLift = opMode.hardwareMap.dcMotor.get("itl");
-        inTakeLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        outTakeLiftLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        outTakeLiftRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-
-
-        //Intake lift
-        inTakeLift = opMode.hardwareMap.dcMotor.get("itl");
-        inTakeLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        //Intake spinners
-        inTakeSpinners = opMode.hardwareMap.dcMotor.get("its");
-        inTakeSpinners.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-        //inTakeFlipControl.getController().pwmEnable();
-
-        // intake servos
-        intakePivotL = opMode.hardwareMap.servo.get(("itpl"));
-        intakePivotR = opMode.hardwareMap.servo.get(("itpr"));
-
-        brakePosIT = inTakeLift.getCurrentPosition();
-        brakePosOT = (outTakeLiftLeft.getCurrentPosition() + outTakeLiftLeft.getCurrentPosition()) / 2.0;
-
-        intakePivotL.setPosition(UP_IT_FLIP_POS - .15);
-
         brakePosOT = (outTakeLiftLeft.getCurrentPosition() + outTakeLiftLeft.getCurrentPosition()) / 2.0;
 
         intakePivotL.setPosition(UP_IT_FLIP_POS - .15);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMechanismTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestMechanismTeleOp.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "MechanismTestTeleOp", group = "Teleops")
 @Config
@@ -13,12 +14,35 @@ public class TestMechanismTeleOp extends OpMode{
     public static boolean[] motorPorts = new boolean[4];
     public static boolean[] servoPorts = new boolean[6];
 
+    int curMotor = 0;
+    int curServo = 0;
+
+    ElapsedTime clickTime = new ElapsedTime();
+
     DcMotor[] motors = new DcMotor[4];
     Servo[] servos = new Servo[6];
 
     @Override
     public void init() {
         checkInit();
+        int index = 0;
+        for (DcMotor motor : motors)
+        {
+            if (motor != null)
+            {
+                curMotor = index;
+            }
+            index++;
+        }
+        index = 0;
+        for (Servo servo : servos)
+        {
+            if (servo != null)
+            {
+                curServo = index;
+            }
+            index++;
+        }
     }
 
     void checkInit()
@@ -48,49 +72,52 @@ public class TestMechanismTeleOp extends OpMode{
 
     @Override
     public void loop() {
+        if (gamepad1.dpad_right && clickTime.milliseconds() > 500)
+        {
+            clickTime.reset();
+            for (int i = curMotor + 1; i < motors.length + curMotor; i++)
+            {
+                if (motors[i % motors.length] != null) {
+                    curMotor = i % motors.length;
+                    break;
+                }
+            }
+        }
+        else if (gamepad1.dpad_left && clickTime.milliseconds() > 500)
+        {
+            clickTime.reset();
+            curMotor--;
+            if (curMotor < 0)
+                curMotor = 3;
+        }
+        else if (gamepad1.dpad_up && clickTime.milliseconds() > 500)
+        {
+            clickTime.reset();
+            curServo++;
+            if (curServo > 5)
+                curServo = 0;
+        }
+        else if (gamepad1.dpad_down && clickTime.milliseconds() > 500)
+        {
+            clickTime.reset();
+            curServo--;
+            if (curServo < 0)
+                curServo = 5;
+        }
+
         if (gamepad1.a)
         {
-            for (DcMotor motor : motors)
-            {
-                if (motor != null)
-                {
-                    motor.setPower(1);
-                }
-            }
-            for (Servo servo : servos)
-            {
-                if (servo != null)
-                {
-                    servo.setPosition(1);
-                }
-            }
+            motors[curMotor].setPower(1);
+            servos[curServo].setPosition(1);
         }
         else if (gamepad1.b)
         {
-            for (DcMotor motor : motors)
-            {
-                if (motor != null)
-                {
-                    motor.setPower(-1);
-                }
-            }
-            for (Servo servo : servos)
-            {
-                if (servo != null)
-                {
-                    servo.setPosition(0);
-                }
-            }
+            motors[curMotor].setPower(-1);
+            servos[curServo].setPosition(0);
         }
         else
         {
-            for (DcMotor motor : motors)
-            {
-                if (motor != null)
-                {
-                    motor.setPower(0);
-                }
-            }
+            motors[curMotor].setPower(0);
         }
     }
 }
