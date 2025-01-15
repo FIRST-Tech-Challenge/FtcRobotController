a95f515e65895354f2ebda68d793b3a759c76942
Teagan Thomes <teagan@anydesk.net>
2025-01-15 08:09:04 -0600

    started adding log file to debug class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/Debug.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/Debug.java
@@ -1,8 +1,14 @@
 package org.firstinspires.ftc.teamcode.Debug;
+import android.os.Environment;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class Debug {
@@ -10,17 +16,56 @@ public class Debug {
     private final OpMode opMode;
     private final Telemetry telemetry;
 
+    private File logFile;
+
+
+
+
+
     public Debug(OpMode opMode) {
         this.opMode = opMode;
         this.telemetry = opMode.telemetry;
+
+        try {
+            // Get the directory for public external storage
+            File logDir = new File(Environment.getExternalStorageDirectory(), "FTCLogs");
+            if (!logDir.exists()) {
+                logDir.mkdirs(); // Create the directory if it doesn't exist
+            }
+
+            // Create a new log file inside the "FTCLogs" directory
+            logFile = new File(logDir, "log.txt");
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+            }
+
+            log("Log file created");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void checkDebugButtons(Gamepad gamepad) {
+
         if (gamepad.start && gamepad.back) {
             debugMode = !debugMode;
             telemetry.addData("debug mode: ", debugMode);
             telemetry.update();
         }
+    }
+
+    public void log(String message) {
+        try {
+            FileWriter writer = new FileWriter(logFile, true);
+            writer.write(message + " ");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getLogFilePath() {
+        return logFile.getAbsolutePath();
     }
 
     public boolean getDebugMode() {
