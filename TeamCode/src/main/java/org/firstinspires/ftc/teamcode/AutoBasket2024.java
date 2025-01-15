fb8284738fc4b82d24ac4b5ca708550c5eb8629f
sqviid <sameer.kozubowski2010@gmail.com>
2025-01-15 17:07:10 -0500

    Added Auto
    raise arm, extend slider, open claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBasket2024.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBasket2024.java
@@ -2,11 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous
 public class AutoBasket2024 extends DriveMethods {
 
     double startTime = -1;
+
+    enum State {
+        Sideways,
+        Turning,
+        Finished,
+        Unstarted,
+        RaiseArm,
+        ExtendSlider,
+        OpenClaw
+    }
+
+    State currentState = State.Unstarted;
 
     @Override
     public void init() {
@@ -18,14 +31,49 @@ public class AutoBasket2024 extends DriveMethods {
     public void loop() {
         telemetry.addData("code", "running");
         telemetry.addData("time", "%.1f", getRuntime());
+        telemetry.addData("sliderLength","%.1f", (double) robot.sliderMotor.getCurrentPosition());
         telemetry.addData("imu", "%.1f", robot.imu.getRobotYawPitchRollAngles().getYaw());
 
+        telemetry.addData("state", currentState);
 
-        if (getRuntime() < 2) {
-            omniDrive(0, 1, 0);
+        switch (currentState) {
+            case Unstarted:
+                currentState = State.RaiseArm;
+                break;
+            case RaiseArm:
+                robot.wormGear.setPower(0.5);
+
+                if (robot.wormGearAngle() >= 50) {
+                    robot.wormGear.setPower(0);
+                    currentState = State.ExtendSlider;
+                }
+                break;
+            case ExtendSlider:
+                robot.sliderMotor.setPower(1);
+                robot.sliderMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.sliderMotor.setTargetPosition(2000);
+                if (robot.sliderMotor.getCurrentPosition() >= 2000) {
+                    currentState = State.OpenClaw;
+                }
+                break;
+            case OpenClaw:
+                robot.clawServo.setPosition(robot.CLAW_OPEN);
+                currentState = State.Finished;
+                break;
+            case Finished:
+                omniDrive(0, 0, 0);
+                break;
         }
-        else {
-            omniDrive(0, 0, 0);
-        }
+
     }
-}
+
+//        if (getRuntime() < 2) {
+//            omniDrive(0, 1, 0);
+//        } else if (getRuntime() < 4) {
+//            omniDrive(0, 0, 1);
+//        }
+//        else {
+//                omniDrive(0, 0, 0);
+//            }
+//        }
+    }
