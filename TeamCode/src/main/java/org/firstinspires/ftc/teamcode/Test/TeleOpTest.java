1fa7741a6e265e9b5e80ae1c3a636642f0dd9675
Teagan Thomes <teagan@anydesk.net>
2025-01-15 08:15:07 -0600

    removed extra controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TeleOpTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TeleOpTest.java
@@ -50,17 +50,8 @@ public class TeleOpTest extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive() && !emergencyStop) {
-//            viperSlide.checkInputs(
-//                    gamepad2.right_trigger,  // retract
-//                    gamepad2.left_trigger,   // extend
-//                    gamepad2.guide,          // reset encoders
-//                    gamepad2.a,              // hold viper position
-//                    gamepad2.y,              // bucket rest
-//                    gamepad2.x              // bucket score
-////                    gamepad2.dpad_up,        // open bucket
-////                    gamepad2.dpad_down       // close bucket
-//
-//            );
+            debug.checkDebugButtons(gamepad1);
+            debug.log("test");
         }
 
         telemetry.addData("Status", "Initialized");
