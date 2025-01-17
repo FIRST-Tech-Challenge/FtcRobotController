a1b6b2c68d8747af46a69211ea77ff4f1757c427
adiaviad <adaviad4@gmail.com>
2025-01-17 22:33:39 +0200

    changed expection to be logged and then read default vlaues istead of crashing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/BT/BTRecordedController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/BT/BTRecordedController.java
@@ -17,17 +17,22 @@ public class BTRecordedController extends BTController {
     protected boolean[] m_buttons;
     protected int iterationCnt;
     protected int maxIterations;
-    private final Telemetry dashboardTelemetry = FtcDashboard.getInstance().getTelemetry();
-
+    protected boolean readErrorAccured;
+    FtcDashboard dashboard = FtcDashboard.getInstance();
+    private Telemetry dashboardTelemetry = dashboard.getTelemetry();
     public BTRecordedController(Gamepad gamepad, BufferedReader bufferedReader, int maxIterations) {
         super(gamepad);
         this.bufferedReader = bufferedReader;
         this.maxIterations = maxIterations;
         this.iterationCnt = 0;
-        m_axes = new double[Axes.values().length];
-        m_axesSuppliers = new DoubleSupplier[Axes.values().length];
-        m_buttons = new boolean[Buttons.values().length];
-        m_buttonsSuppliers = new BooleanSupplier[Buttons.values().length];
+        this.readErrorAccured=false;
+
+        this.m_axes = new double[Axes.values().length];
+        super.m_axesSuppliers = new DoubleSupplier[Axes.values().length];
+
+        this.m_buttons = new boolean[Buttons.values().length];
+        super.m_buttonsSuppliers = new BooleanSupplier[Buttons.values().length];
+
         for (int i = 0; i < Axes.values().length; i++) {
             int finalI = i;
             m_axesSuppliers[i] = () -> m_axes[finalI];
@@ -36,20 +41,31 @@ public class BTRecordedController extends BTController {
             int finalI = i;
             m_buttonsSuppliers[i] = () -> m_buttons[finalI];
         }
+
+        //later this could be change to true and i want the user to see the change so i clear the data now.
+        dashboardTelemetry.addData("readErrorAccured",false);
+
     }
 
     public BTRecordedController(Gamepad gamepad) throws Exception {
         super(gamepad);
-        throw new Exception("not implemented");
+        throw new Exception("not implemented, you must provide a file to read");
     }
 
     public void next()  {
-        if (iterationCnt < maxIterations) {
+        if ((iterationCnt < maxIterations) || readErrorAccured) {
             iterationCnt++;
             String[] line = new String[0];
-            try {line = bufferedReader.readLine().split(",");} catch (IOException e) {throw new RuntimeException("could not read next line. potentially configured more iterations then in recording. max configured iterations:"+maxIterations+" current iteration:"+iterationCnt+"\n"+e.toString());}
-            m_axes = stringArrToDouble(line, Axes.values().length, 0, Axes.values().length);
-            m_buttons = stringArrToBool(line, Buttons.values().length, Axes.values().length, Buttons.values().length + Axes.values().length);
+            try {
+                line = bufferedReader.readLine().split(",");
+                m_axes = stringArrToDouble(line, Axes.values().length, 0, Axes.values().length);
+                m_buttons = stringArrToBool(line, Buttons.values().length, Axes.values().length, Buttons.values().length + Axes.values().length);
+            } catch (IOException e) {
+                //we don't want the code to crash so we just log and then read only default values.
+                readErrorAccured=true;
+                dashboardTelemetry.addData("readErrorAccured",true);
+                dashboardTelemetry.addLine("could not read next line. potentially configured more iterations then in recording. max configured iterations:"+maxIterations+" current iteration:"+iterationCnt+"\n"+e.toString());
+            }
         } else {
             Arrays.fill(m_axes, 0);
             Arrays.fill(m_buttons,false);
