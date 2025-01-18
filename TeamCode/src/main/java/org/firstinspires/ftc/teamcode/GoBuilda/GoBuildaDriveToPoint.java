3c5d2ab56814c09b21271fba4ebbdd536c83725c
Techtonics Student <115182387+Techtonics8947ftc@users.noreply.github.com>
2025-01-18 12:32:46 -0500

    Autonomous tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuilda/GoBuildaDriveToPoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoBuilda/GoBuildaDriveToPoint.java
@@ -14,11 +14,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 public class GoBuildaDriveToPoint {
 
     public enum DriveType {
-        MECANUM,
-        TANK
+        MECANUM
     }
 
-    public enum DriveMotor{
+    public enum DriveMotor {
         LEFT_FRONT,
         RIGHT_FRONT,
         LEFT_BACK,
@@ -38,21 +37,21 @@ public class GoBuildaDriveToPoint {
         IN_BOUNDS
     }
 
-    private static double xyTolerance = 12;
+    private static double xyTolerance = 20;
     private static double yawTolerance = 0.0349066;
 
     private static double pGain = 0.008;
     private static double dGain = 0.00001;
-    private static double accel = 10.0;
+    private static double accel = 8.0;
 
     private static double yawPGain = 5.0;
     private static double yawDGain = 0.0;
     private static double yawAccel = 20.0;
 
-    private double leftFrontMotorOutput  = 0;
+    private double leftFrontMotorOutput = 0;
     private double rightFrontMotorOutput = 0;
-    private double leftBackMotorOutput   = 0;
-    private double rightBackMotorOutput  = 0;
+    private double leftBackMotorOutput = 0;
+    private double rightBackMotorOutput = 0;
 
     private final ElapsedTime holdTimer = new ElapsedTime();
     private final ElapsedTime PIDTimer = new ElapsedTime();
@@ -63,39 +62,39 @@ public class GoBuildaDriveToPoint {
     private final PIDLoop yPID = new PIDLoop();
     private final PIDLoop hPID = new PIDLoop();
 
-    private final PIDLoop xTankPID = new PIDLoop();
+    //private final PIDLoop xTankPID = new PIDLoop();
 
-    private DriveType selectedDriveType = DriveType.MECANUM;
+    //private DriveType selectedDriveType = DriveType.MECANUM;
 
-    public GoBuildaDriveToPoint(LinearOpMode opmode){
+    public GoBuildaDriveToPoint(LinearOpMode opmode) {
         myOpMode = opmode;
     }
 
-    public void setDriveType(DriveType driveType){
-        selectedDriveType = driveType;
-    }
+    //public void setDriveType(DriveType driveType) {
+       // selectedDriveType = driveType;
+   //}
 
 
-    public void setXYCoefficients(double p, double d, double acceleration, DistanceUnit unit, double tolerance){
+    public void setXYCoefficients(double p, double d, double acceleration, DistanceUnit unit, double tolerance) {
         pGain = p;
         dGain = d;
         accel = acceleration;
         xyTolerance = unit.toMm(tolerance);
     }
 
-    public void setYawCoefficients(double p, double d, double acceleration, AngleUnit unit, double tolerance){
+    public void setYawCoefficients(double p, double d, double acceleration, AngleUnit unit, double tolerance) {
         yawPGain = p;
         yawDGain = d;
         yawAccel = acceleration;
         yawTolerance = unit.toRadians(tolerance);
     }
 
-    public double getMotorPower(DriveMotor driveMotor){
-        if(driveMotor == DriveMotor.LEFT_FRONT){
+    public double getMotorPower(DriveMotor driveMotor) {
+        if (driveMotor == DriveMotor.LEFT_FRONT) {
             return leftFrontMotorOutput;
-        } else if (driveMotor == DriveMotor.RIGHT_FRONT){
+        } else if (driveMotor == DriveMotor.RIGHT_FRONT) {
             return rightFrontMotorOutput;
-        } else if (driveMotor == DriveMotor.LEFT_BACK){
+        } else if (driveMotor == DriveMotor.LEFT_BACK) {
             return leftBackMotorOutput;
         } else {
             return rightBackMotorOutput;
@@ -105,62 +104,27 @@ public class GoBuildaDriveToPoint {
     public boolean driveTo(Pose2D currentPosition, Pose2D targetPosition, double power, double holdTime) {
         boolean atTarget;
 
-        if (selectedDriveType == DriveType.TANK){
-            double xPWR;
-            double hPWR;
-            double headingTowardsTarget = calculateTargetHeading(currentPosition,targetPosition);
-            double lengthToTarget = Math.hypot((targetPosition.getX(MM) - currentPosition.getX(MM)),(targetPosition.getY(MM) - currentPosition.getY(MM)));
-            Pose2D temp = new Pose2D(MM,targetPosition.getX(MM),targetPosition.getY(MM),RADIANS,headingTowardsTarget);
+        double xPWR = calculatePID(currentPosition, targetPosition, Direction.x);
+        double yPWR = calculatePID(currentPosition, targetPosition, Direction.y);
+        double hOutput = calculatePID(currentPosition, targetPosition, Direction.h);
 
-            if (headingTowardsTarget > (Math.PI/2) || headingTowardsTarget < -(Math.PI/2)){
-                //headingTowardsTarget -= Math.PI;
-                headingTowardsTarget = targetPosition.getHeading(RADIANS);
-                lengthToTarget = -lengthToTarget;
-            }
+        double heading = currentPosition.getHeading(AngleUnit.RADIANS);
+        double cosine = Math.cos(heading);
+        double sine = Math.sin(heading);
 
-            xPWR = xTankPID.calculateAxisPID(lengthToTarget,pGain,dGain,accel, PIDTimer.seconds());
-            hPWR = calculatePID(currentPosition, temp, Direction.h);
+        double xOutput = (xPWR * cosine) + (yPWR * sine);
+        double yOutput = (xPWR * sine) - (yPWR * cosine);
 
-//            if (inBounds(currentPosition,temp) == InBounds.IN_X_Y){
-//                xPWR = 0;
-//                hPWR = calculatePID(currentPosition,targetPosition,Direction.h);
+        calculateMecanumOutput(xOutput * power, yOutput * power, hOutput * power);
 
-//            if(inBounds(currentPosition,temp) == InBounds.IN_HEADING) {
-//                xPWR = xTankPID.calculateAxisPID(lengthToTarget,pGain,dGain,accel,currentTime.time());
-//                hPWR = calculatePID(currentPosition, temp, Direction.h);
-//
-//            } else {
-//                xPWR = 0;
-//                hPWR = calculatePID(currentPosition, temp, Direction.h);
-//            }
-            calculateTankOutput(xPWR * power, hPWR * power);
-
-
-        //Mecanum Drive Code:
-        } else {
-            double xPWR = calculatePID(currentPosition, targetPosition, Direction.x);
-            double yPWR = calculatePID(currentPosition, targetPosition, Direction.y);
-            double hOutput = calculatePID(currentPosition, targetPosition, Direction.h);
-
-            double heading = currentPosition.getHeading(AngleUnit.RADIANS);
-            double cosine = Math.cos(heading);
-            double sine = Math.sin(heading);
-
-            double xOutput = (xPWR * cosine) + (yPWR * sine);
-            double yOutput = (xPWR * sine) - (yPWR * cosine);
-
-            calculateMecanumOutput(xOutput * power, yOutput * power, hOutput * power);
-        }
-
-        if(inBounds(currentPosition,targetPosition) == InBounds.IN_BOUNDS){
+        if (inBounds(currentPosition, targetPosition) == InBounds.IN_BOUNDS) {
             atTarget = true;
-        }
-        else {
+        } else {
             holdTimer.reset();
             atTarget = false;
         }
 
-        if(atTarget && holdTimer.time() > holdTime){
+        if (atTarget && holdTimer.time() > holdTime) {
             return true;
         }
         return false;
@@ -183,67 +147,49 @@ public class GoBuildaDriveToPoint {
             rightBack /= max;
         }
 
-        leftFrontMotorOutput  = leftFront;
+        leftFrontMotorOutput = leftFront;
         rightFrontMotorOutput = rightFront;
-        leftBackMotorOutput   = leftBack;
-        rightBackMotorOutput  = rightBack;
+        leftBackMotorOutput = leftBack;
+        rightBackMotorOutput = rightBack;
     }
 
-    private void calculateTankOutput(double forward, double yaw){
-        double left = forward - yaw;
-        double right = forward + yaw;
-
-        double max = Math.max(Math.abs(left),Math.abs(right));
-
-        if (max > 1.0) {
-            left /= max;
-            right /= max;
-        }
-
-        leftFrontMotorOutput  = left;
-        rightFrontMotorOutput = right;
-        leftBackMotorOutput   = left;
-        rightBackMotorOutput  = right;
-    }
-
-
-    private double calculatePID(Pose2D currentPosition, Pose2D targetPosition, Direction direction){
-        if(direction == Direction.x){
+    private double calculatePID(Pose2D currentPosition, Pose2D targetPosition, Direction direction) {
+        if (direction == Direction.x) {
             double xError = targetPosition.getX(MM) - currentPosition.getX(MM);
-            return xPID.calculateAxisPID(xError, pGain, dGain, accel,PIDTimer.seconds());
+            return xPID.calculateAxisPID(xError, pGain, dGain, accel, PIDTimer.seconds());
         }
-        if(direction == Direction.y){
+        if (direction == Direction.y) {
             double yError = targetPosition.getY(MM) - currentPosition.getY(MM);
             return yPID.calculateAxisPID(yError, pGain, dGain, accel, PIDTimer.seconds());
         }
-        if(direction == Direction.h){
+        if (direction == Direction.h) {
             double hError = targetPosition.getHeading(AngleUnit.RADIANS) - currentPosition.getHeading(AngleUnit.RADIANS);
             return hPID.calculateAxisPID(hError, yawPGain, yawDGain, yawAccel, PIDTimer.seconds());
         }
         return 0;
     }
 
-    private InBounds inBounds (Pose2D currPose, Pose2D trgtPose){
+    private InBounds inBounds(Pose2D currPose, Pose2D trgtPose) {
         boolean xInBounds = currPose.getX(MM) > (trgtPose.getX(MM) - xyTolerance) && currPose.getX(MM) < (trgtPose.getX(MM) + xyTolerance);
         boolean yInBounds = currPose.getY(MM) > (trgtPose.getY(MM) - xyTolerance) && currPose.getY(MM) < (trgtPose.getY(MM) + xyTolerance);
         boolean hInBounds = currPose.getHeading(RADIANS) > (trgtPose.getHeading(RADIANS) - yawTolerance) &&
                 currPose.getHeading(RADIANS) < (trgtPose.getHeading(RADIANS) + yawTolerance);
 
-        if (xInBounds && yInBounds && hInBounds){
+        if (xInBounds && yInBounds && hInBounds) {
             return InBounds.IN_BOUNDS;
-        } else if (xInBounds && yInBounds){
+        } else if (xInBounds && yInBounds) {
             return InBounds.IN_X_Y;
-        } else if (hInBounds){
+        } else if (hInBounds) {
             return InBounds.IN_HEADING;
         } else
             return InBounds.NOT_IN_BOUNDS;
     }
 
-    public double calculateTargetHeading(Pose2D currPose, Pose2D trgtPose){
+    public double calculateTargetHeading(Pose2D currPose, Pose2D trgtPose) {
         double xDelta = trgtPose.getX(MM) - currPose.getX(MM);
         double yDelta = trgtPose.getY(MM) - currPose.getY(MM);
 
-        if(Math.abs(xDelta) > xyTolerance || Math.abs(yDelta) > xyTolerance){
+        if (Math.abs(xDelta) > xyTolerance || Math.abs(yDelta) > xyTolerance) {
             return Math.atan2(yDelta, xDelta);
         } else {
             return currPose.getHeading(RADIANS);
@@ -252,14 +198,14 @@ public class GoBuildaDriveToPoint {
     }
 }
 
-class PIDLoop{
+class PIDLoop {
     private double previousError;
     private double previousTime;
     private double previousOutput;
 
     private double errorR;
 
-    public double calculateAxisPID(double error, double pGain, double dGain, double accel, double currentTime){
+    public double calculateAxisPID(double error, double pGain, double dGain, double accel, double currentTime) {
         double p = error * pGain;
         double cycleTime = currentTime - previousTime;
         double d = dGain * (previousError - error) / (cycleTime);
@@ -267,19 +213,19 @@ class PIDLoop{
         double dV = cycleTime * accel;
 
         double max = Math.abs(output);
-        if(max > 1.0){
+        if (max > 1.0) {
             output /= max;
         }
 
-        if((output - previousOutput) > dV){
+        if ((output - previousOutput) > dV) {
             output = previousOutput + dV;
-        } else if ((output - previousOutput) < -dV){
+        } else if ((output - previousOutput) < -dV) {
             output = previousOutput - dV;
         }
 
         previousOutput = output;
-        previousError  = error;
-        previousTime   = currentTime;
+        previousError = error;
+        previousTime = currentTime;
 
         errorR = error;
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TT_AutonomousRightSide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TT_AutonomousRightSide.java
@@ -8,11 +8,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.Robot.TT_RobotHardware;
 
-@Autonomous(name="Right Side Auto", group="Techtonics")
+import java.util.Locale;
+
+@Autonomous(name = "Right Side Auto", group = "Techtonics")
 //@Disabled
 
 public class TT_AutonomousRightSide extends LinearOpMode {
     TT_RobotHardware robot = new TT_RobotHardware(this);
+    double autonomousPower = 0.5;
+    double startingPoint = 0;   // just right of Center
 
     enum StateMachine {
         WAITING_FOR_START,
@@ -21,22 +25,37 @@ public class TT_AutonomousRightSide extends LinearOpMode {
         DRIVE_TO_TARGET_2,
         DRIVE_TO_TARGET_3,
         DRIVE_TO_TARGET_4,
-        DRIVE_TO_TARGET_5
+        DRIVE_TO_TARGET_5,
+        DRIVE_TO_TARGET_6,
+        DRIVE_TO_TARGET_7,
+        DRIVE_TO_TARGET_8,
+        DRIVE_TO_TARGET_9,
+        DRIVE_TO_TARGET_10,
+        DRIVE_TO_TARGET_11,
+        DRIVE_TO_TARGET_12,
+        DRIVE_TO_TARGET_13,
+        DRIVE_TO_TARGET_14,
+        DRIVE_TO_TARGET_15,
     }
-
-    static final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM,500,20,AngleUnit.DEGREES,0);
-    static final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, 500, -20, AngleUnit.DEGREES, -90);
-    static final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM,500,-500, AngleUnit.DEGREES,-90);
-    static final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, 100, -500, AngleUnit.DEGREES, 90);
-    static final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, 100, 0, AngleUnit.DEGREES, 0);
-
-
+// REMEMBER TO ADD 50 DISTANCE TO Y AXIS MOVING RIGHT
+    final Pose2D TARGET_1 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-745), 0, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_2 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-600), 725, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_3 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 725, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_4 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 900, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_5 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-100), 900, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_6 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 900, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_7 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 1125, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_8 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-100), 1125, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_9 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 1125, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_10 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-1300), 1350, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_11 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-100), 1350, AngleUnit.DEGREES, 0);
+    final Pose2D TARGET_12 = new Pose2D(DistanceUnit.MM, calcXCoordinate(-50), 0, AngleUnit.DEGREES, 0);
     @Override
     public void runOpMode() {
-
+        robot.init();
         StateMachine stateMachine;
         stateMachine = StateMachine.WAITING_FOR_START;
-
+       robot.MaxPowerAdjustment=1.5;
 
         // Wait for the game to start (driver presses START)
         waitForStart();
@@ -45,10 +64,12 @@ public class TT_AutonomousRightSide extends LinearOpMode {
         while (opModeIsActive()) {
             robot.odo.update();
 
-            switch (stateMachine){
+            switch (stateMachine) {
                 case WAITING_FOR_START:
                     //the first step in the autonomous
                     stateMachine = StateMachine.DRIVE_TO_TARGET_1;
+                    robot.setLiftPosition(robot.liftHeightMax);
+                    sleep(250);
                     break;
                 case DRIVE_TO_TARGET_1:
                     /*
@@ -56,45 +77,122 @@ public class TT_AutonomousRightSide extends LinearOpMode {
                     the robot has reached the target, and has been there for (holdTime) seconds.
                     Once driveTo returns true, it prints a telemetry line and moves the state machine forward.
                      */
-                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_1, 0.7, 0)){
+
+                    robot.setLiftPosition(robot.liftHeightMax);
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_1, autonomousPower, 6)) {
                         telemetry.addLine("at position #1!");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_2;
+                        robot.setLiftPosition(1345);
+                        sleep(500);
+                        robot.setLiftPosition(0);
+
                     }
                     break;
                 case DRIVE_TO_TARGET_2:
                     //drive to the second target
-                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_2, 0.7, 1)){
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_2, autonomousPower, 0)) {
                         telemetry.addLine("at position #2!");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_3;
                     }
                     break;
                 case DRIVE_TO_TARGET_3:
-                    if(robot.nav.driveTo(robot.odo.getPosition(), TARGET_3, 0.7, 3)){
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_3, autonomousPower, 0)) {
                         telemetry.addLine("at position #3");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_4;
                     }
                     break;
                 case DRIVE_TO_TARGET_4:
-                    if(robot.nav.driveTo(robot.odo.getPosition(),TARGET_4,0.7,1)){
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_4, autonomousPower, 0)) {
                         telemetry.addLine("at position #4");
                         stateMachine = StateMachine.DRIVE_TO_TARGET_5;
                     }
                     break;
                 case DRIVE_TO_TARGET_5:
-                    if(robot.nav.driveTo(robot.odo.getPosition(),TARGET_5,0.7,1)){
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_5, autonomousPower, 0)) {
                         telemetry.addLine("There!");
-                        stateMachine = StateMachine.AT_TARGET;
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_6;
+                    }
+                    break;
+                //case DRIVE_TO_TARGET_6:
+                //    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_6, autonomousPower, 1)) {
+                //       telemetry.addLine("There!");
+                //       stateMachine = StateMachine.AT_TARGET;
+                //   }
+                //   break;
+                case DRIVE_TO_TARGET_6:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_6, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_7;
+                    }
+                    break;
+
+                case DRIVE_TO_TARGET_7:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_7, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_8;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_8:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_8, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_9;
+                    }
+                    break;
+               /* case DRIVE_TO_TARGET_9:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_9, autonomousPower, 5)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_9;
+                    }
+
+                    break;
+
+                */
+                case DRIVE_TO_TARGET_9:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_9, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_10;
+                    }
+                    break;
+
+                case DRIVE_TO_TARGET_10:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_10, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_11;
+                    }
+                    break;
+
+                case DRIVE_TO_TARGET_11:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_11, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_12;
+                    }
+                    break;
+                case DRIVE_TO_TARGET_12:
+                    if (robot.nav.driveTo(robot.odo.getPosition(), TARGET_12, autonomousPower, 0)) {
+                        telemetry.addLine("There!");
+                        stateMachine = StateMachine.DRIVE_TO_TARGET_12;
                     }
                     break;
             }
+
+
             robot.setDrivePower();
-            telemetry.addData("current state:",stateMachine);
+            telemetry.addData("current state:", stateMachine);
+            Pose2D pos = robot.odo.getPosition();
+            String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
+            telemetry.addData("Position", data);
+            telemetry.update();
+            telemetry.update();
 
             //Pose2D pos = odo.getPosition();
             //String data = String.format(Locale.US, "{X: %.3f, Y: %.3f, H: %.3f}", pos.getX(DistanceUnit.MM), pos.getY(DistanceUnit.MM), pos.getHeading(AngleUnit.DEGREES));
             //telemetry.addData("Position", data);
 
             //telemetry.update();
-
         }
-    }}
+    }
+
+    private double calcXCoordinate(double xChange) {
+        return xChange + startingPoint;
+    }
+}
