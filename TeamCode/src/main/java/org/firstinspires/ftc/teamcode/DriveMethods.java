6908baf047456defc27fe5404810b03e2bc72a5d
Pixelotol7 <mattpsn2011@gmail.com>
2025-01-19 17:32:20 -0500

    added more of the last

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveMethods.java
@@ -62,9 +62,12 @@ public abstract class DriveMethods extends OpMode {
         double position = targetPosition;
 
         position = Math.min(position, robot.upperMaxLegalSliderLength());
-        position = Math.min(position, robot.lowerMaxLegalSliderLength());
         position = Math.min(position, robot.MAX_SAFE_SLIDER_TICKS);
         position = Math.max(position, robot.MIN_SLIDER_TICKS);
+
+        if (robot.wormGearAngle() < 0) {
+            position = Math.min(position, robot.lowerMaxSliderLength());
+        }
 
         robot.sliderMotor.setTargetPosition((int) position);
 
