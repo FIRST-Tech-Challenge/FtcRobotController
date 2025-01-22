b014ddc772bbd1b6f4cae9af0f3fb1d080fd4f4c
Kye1331 <kyeelde@gmail.com>
2025-01-21 16:16:42 -0800

    Update Auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -4,7 +4,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 //import com.qualcomm.robotcore.eventloop.
 import com.qualcomm.robotcore.hardware.DcMotor;
-// start autonoumos period code here!
+// start autonoumos period code here! 83 is not bad, but 511 is. chameleon. just a personal opinion
 public class Auto extends LinearOpMode {
 
     @Override
