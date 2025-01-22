1bee25c424ddcaa20b62d6d643ae267f0d3428d4
Kye1331 <kyeelde@gmail.com>
2025-01-21 16:27:01 -0800

    Update general.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/general.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 //import java.util.Optional;
-
+//
 //import com.qualcomm.robotcore.hardware.Servo;
 
 
