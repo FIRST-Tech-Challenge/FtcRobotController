2e266bbfed741f7cfba293cde6d1ece9ab603009
Julie Kohler <julie.kohler@splice.com>
2025-01-23 19:27:42 -0700

    Make claw servo move

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Servo_Test.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/*
+ * This OpMode scans a single servo back and forward until Stop is pressed.
+ * The code is structured as a LinearOpMode
+ * INCREMENT sets how much to increase/decrease the servo position each cycle
+ * CYCLE_MS sets the update period.
+ *
+ * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
+ *
+ * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
+ * connected servos are able to move freely before running this test.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+@TeleOp(name = "Concept: Servo Test")
+public class Servo_Test extends LinearOpMode {
+
+    static final double MIN_POS     =  0.5;     // Minimum rotational position
+
+    // Define class members
+    Servo   servo;
+    double  position = MIN_POS; // Start at halfway position
+
+
+    @Override
+    public void runOpMode() {
+
+        // Connect to servo (Assume Robot Left Hand)
+        // Change the text in quotes to match any servo name on your robot.
+        servo = hardwareMap.get(Servo.class, "servo_claw");
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to scan Servo." );
+        telemetry.update();
+        waitForStart();
+
+
+        // Scan servo till stop pressed.
+        while(opModeIsActive()){
+            position = gamepad2.right_trigger >MIN_POS?gamepad2.right_trigger:MIN_POS;
+
+            // Display the current value
+            telemetry.addData("Servo Position", "%5.2f", position);
+            telemetry.addData(">", "Press Stop to end test." );
+            telemetry.update();
+
+            // Set the servo to the new position and pause;
+            servo.setPosition(position);
+            idle();
+        }
+
+        // Signal done;
+        telemetry.addData(">", "Done");
+        telemetry.update();
+    }
+}
+
