b555813b998aab29317439a8748802bea1580d1d
SujayCh07 <chavasujay91@gmail.com>
2025-01-27 15:09:54 -0500

    Anya testing push

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_5518.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOp_5518.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Libs.AR.AR_Arm;
 import org.firstinspires.ftc.teamcode.Libs.AR.MecanumDrive_5518;
-
+//Anya testing push ability
 @TeleOp(name = "5518 TeleOp", group = "TeleOp")
 public class TeleOp_5518 extends LinearOpMode
 {
