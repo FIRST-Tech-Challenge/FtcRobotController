0f5e2e4c46d44b5599d6ece83558f67c46155b42
Justin Liu <liujustin604@gmail.com>
2025-01-28 16:23:20 -0500

    actually enforce limiter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/DriveSubsystem.java
@@ -19,34 +19,33 @@ public class DriveSubsystem {
             backLeftDrive,
             backRightDrive;
     private final DcMotor[] motorList;
+
     public DriveSubsystem(HardwareMap hardwareMap, Telemetry telemetry) {
         this.telemetry = telemetry;
         frontLeftDrive = hardwareMap.dcMotor.get("FL");
         frontRightDrive = hardwareMap.dcMotor.get("FR");
         backLeftDrive = hardwareMap.dcMotor.get("BL");
         backRightDrive = hardwareMap.dcMotor.get("BR");
-        motorList = new DcMotor[] { frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive };
+        motorList = new DcMotor[]{frontLeftDrive, frontRightDrive, backLeftDrive, backRightDrive};
+
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotor.Direction.REVERSE);
 
         runForAllMotors(motor -> motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE));
-
         runForAllMotors(motor -> motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER));
-
     }
 
     public boolean isMoving() {
         return (frontRightDrive.isBusy() || frontLeftDrive.isBusy() || backRightDrive.isBusy() || backLeftDrive.isBusy());
-
     }
+
     public void cartesianMove(double cmY, double cmX) {
         Vector target = new Vector(cmX, cmY);
 
         final int TICKS_PER_CM = 54;
 
-        int xTicks = (int)(target.getX() * TICKS_PER_CM);
-        int yTicks = (int)(target.getY() * TICKS_PER_CM);
-
+        int xTicks = (int) (target.getX() * TICKS_PER_CM);
+        int yTicks = (int) (target.getY() * TICKS_PER_CM);
 
         runForAllMotors(motor -> motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER));
 
@@ -69,9 +68,12 @@ public class DriveSubsystem {
         double turn = controls[2];
         runWithControls(x, y, turn, imu);
     }
+
     public void runWithControls(double x, double y, double turn, IMU imu) {
+        // Add any corrections here
         setDrivePower(x, y, turn);
     }
+
     private double[] readControls(Gamepad gamepad1, Gamepad gamepad2) {
         double x = gamepad1.left_stick_x;
         double y = -gamepad1.left_stick_y;
@@ -80,26 +82,27 @@ public class DriveSubsystem {
         if (gamepad1.dpad_down) y -= 0.2;
         if (gamepad1.dpad_right) x += 0.2;
         if (gamepad1.dpad_left) x -= 0.2;
-        return new double[] {x, y, turn};
+        return new double[]{x, y, turn};
     }
+
     private void setDrivePower(double speed, double strafe, double turn) {
-        double normalizedSlidePosition = (Common.slidePosition - 800)/800;
-        double normalizedArmPosition = (Common.armPosition - 1400)/1000;
+        double normalizedSlidePosition = (Common.slidePosition - 800) / 800;
+        double normalizedArmPosition = (Common.armPosition - 1400) / 1000;
         double limiter = normalizedArmPosition * normalizedSlidePosition * 0.4;
-        double frontLeftPower  = speed + strafe + turn;
+        double frontLeftPower = speed + strafe + turn;
         double frontRightPower = speed - strafe + turn;
-        double backLeftPower   = speed - strafe - turn;
-        double backRightPower  = speed + strafe - turn;
+        double backLeftPower = speed - strafe - turn;
+        double backRightPower = speed + strafe - turn;
 
-        telemetry.addData("frontLeftPower", frontLeftPower/limiter);
-        telemetry.addData("frontRightPower", frontRightPower/limiter);
-        telemetry.addData("backLeftPower", backLeftPower/limiter);
-        telemetry.addData("backRightPower", backRightPower/limiter);
+        telemetry.addData("frontLeftPower", frontLeftPower / limiter);
+        telemetry.addData("frontRightPower", frontRightPower / limiter);
+        telemetry.addData("backLeftPower", backLeftPower / limiter);
+        telemetry.addData("backRightPower", backRightPower / limiter);
 
-        frontLeftDrive.setPower(frontLeftPower);
-        frontRightDrive.setPower(frontRightPower);
-        backLeftDrive.setPower(backLeftPower);
-        backRightDrive.setPower(backRightPower);
+        frontLeftDrive.setPower(frontLeftPower / limiter);
+        frontRightDrive.setPower(frontRightPower / limiter);
+        backLeftDrive.setPower(backLeftPower / limiter);
+        backRightDrive.setPower(backRightPower / limiter);
     }
 
     private void runForAllMotors(Consumer<DcMotor> f) {
