6e3c405e0e96a07b96a7883d31267734d91c6f4d
Justin Liu <liujustin604@gmail.com>
2025-01-28 21:05:16 -0500

    update

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SyborgsBetterAuto.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.teamcode.subsystems.ArmSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.DriveSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.IMUFactory;
+import org.firstinspires.ftc.teamcode.subsystems.IntakeAndWristSubsystem;
+import org.firstinspires.ftc.teamcode.subsystems.SlideSubsystem;
+@SuppressWarnings("unused")
+@Autonomous(name="SyborgsBetterAuto")
+public class SyborgsBetterAuto extends LinearOpMode {
+    DriveSubsystem drive;
+    IMU imu;
+    ArmSubsystem arm;
+    SlideSubsystem lift;
+    IntakeAndWristSubsystem intakeAndWrist;
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        initSubsystems();
+        sleep(3000);
+        arm.setPositionAuto(arm.ARM_SCORE_SAMPLE_IN_HIGH);
+
+    }
+    public void initSubsystems() {
+        drive = new DriveSubsystem(hardwareMap, telemetry);
+        imu = IMUFactory.initIMU(hardwareMap);
+        arm = new ArmSubsystem(hardwareMap, telemetry);
+        lift = new SlideSubsystem(hardwareMap, telemetry);
+        intakeAndWrist = new IntakeAndWristSubsystem(hardwareMap, telemetry);
+    }
+}
