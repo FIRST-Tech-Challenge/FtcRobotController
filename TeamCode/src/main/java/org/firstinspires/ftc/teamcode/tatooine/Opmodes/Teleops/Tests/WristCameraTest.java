cc5eae6733e37de2bb14761146b6895212f09feb
eitandas <142007178+eitandas@users.noreply.github.com>
2025-01-31 20:31:28 +0200

    tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/Opmodes/Teleops/Tests/WristCameraTest.java
@@ -0,0 +1,60 @@
+package org.firstinspires.ftc.teamcode.tatooine.Opmodes.Teleops.Tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.tatooine.SubSystem.Camera;
+import org.firstinspires.ftc.teamcode.tatooine.SubSystem.Wrist;
+import org.firstinspires.ftc.teamcode.tatooine.utils.Alliance.CheckAlliance;
+import org.firstinspires.ftc.teamcode.tatooine.utils.gamepads.EasyGamepad;
+import org.firstinspires.ftc.teamcode.tatooine.utils.gamepads.GamepadKeys;
+import org.firstinspires.ftc.teamcode.tatooine.utils.mathUtil.MathUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name = "WristCameraTest", group = "Tests")
+public class WristCameraTest extends LinearOpMode {
+
+    private List<Action> runningActions = new ArrayList<>();
+
+    private boolean isRed = CheckAlliance.isRed();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        EasyGamepad gamepadEx1 = new EasyGamepad(gamepad1);
+        Wrist wrist = new Wrist(this, true);
+        Camera camera = new Camera(this, true, isRed);
+        waitForStart();
+        while (opModeIsActive()){
+            TelemetryPacket packet = new TelemetryPacket();
+            List<Action> newActions = new ArrayList<>();
+            for (Action action : runningActions) {
+
+                action.preview(packet.fieldOverlay());
+                if (action.run(packet)) {
+                    newActions.add(action);
+                }
+            }
+            runningActions = newActions;
+
+            if (gamepadEx1.justPressedButton(GamepadKeys.Button.DPAD_UP)){
+                wrist.setPositionWristAngle(camera.getAngle());
+            } else if (gamepadEx1.justPressedButton(GamepadKeys.Button.START)) {
+                camera.setSpecimen(!camera.isSpecimen());
+            }
+
+
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
+            telemetry.update();
+
+            gamepadEx1.update(gamepad1);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tatooine/Opmodes/Teleops/Tests/WristTuningTest.java
@@ -0,0 +1,78 @@
+package org.firstinspires.ftc.teamcode.tatooine.Opmodes.Teleops.Tests;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.tatooine.SubSystem.Wrist;
+import org.firstinspires.ftc.teamcode.tatooine.utils.gamepads.EasyGamepad;
+import org.firstinspires.ftc.teamcode.tatooine.utils.gamepads.GamepadKeys;
+import org.firstinspires.ftc.teamcode.tatooine.utils.mathUtil.MathUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@TeleOp(name = "WristTuningTest", group = "Tests")
+public class WristTuningTest extends LinearOpMode {
+
+    private List<Action> runningActions = new ArrayList<>();
+
+    private double pos = 0.5;
+
+    private final double WRIST_SENSITIVITY = 0.03;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        EasyGamepad gamepadEx1 = new EasyGamepad(gamepad1);
+        Wrist wrist = new Wrist(this, true);
+        waitForStart();
+        while (opModeIsActive()){
+            TelemetryPacket packet = new TelemetryPacket();
+            List<Action> newActions = new ArrayList<>();
+            for (Action action : runningActions) {
+
+                action.preview(packet.fieldOverlay());
+                if (action.run(packet)) {
+                    newActions.add(action);
+                }
+            }
+            runningActions = newActions;
+
+            if (gamepadEx1.justPressedButton(GamepadKeys.Button.CROSS)){
+                wrist.home();
+            }
+            else if (gamepadEx1.justPressedButton(GamepadKeys.Button.CIRCLE)){
+                wrist.intake();
+            }
+            else if (gamepadEx1.justPressedButton(GamepadKeys.Button.TRIANGLE)){
+                wrist.straight();
+            }
+            else if (gamepadEx1.justPressedButton(GamepadKeys.Button.SQUARE)){
+                wrist.scoreSample();
+            }
+            else if (gamepadEx1.justPressedButton(GamepadKeys.Button.DPAD_UP)){
+                wrist.front();
+            } else if (gamepadEx1.justPressedButton(GamepadKeys.Button.DPAD_DOWN)) {
+                wrist.back();
+            }
+            if (gamepadEx1.justPressedButton(GamepadKeys.Button.RIGHT_BUMPER)){
+                pos = MathUtil.wrap(pos + WRIST_SENSITIVITY, 0, 1);
+                wrist.setPositionWristAngle(pos);
+            }
+            else if (gamepadEx1.justPressedButton(GamepadKeys.Button.LEFT_BUMPER)){
+                pos = MathUtil.wrap(pos - WRIST_SENSITIVITY, 0, 1);
+                wrist.setPositionWristAngle(pos);
+            }
+
+
+            FtcDashboard.getInstance().sendTelemetryPacket(packet);
+
+            telemetry.update();
+
+            gamepadEx1.update(gamepad1);
+        }
+    }
+}
