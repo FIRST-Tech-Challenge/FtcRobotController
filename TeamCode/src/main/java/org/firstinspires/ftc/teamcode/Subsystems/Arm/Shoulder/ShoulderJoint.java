143beafade92600ba9c202c4a684dd93a897b282
knutt5 <44103178+knutt5@users.noreply.github.com>
2025-01-30 19:13:20 -0500

    Shoulder profiling

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm/Shoulder/ShoulderJoint.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Arm/Shoulder/ShoulderJoint.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.Subsystems.Arm.Shoulder;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.RobotContainer;
 
@@ -14,18 +16,30 @@ public class ShoulderJoint extends SubsystemBase {
     /**0° is up*/
     private final Servo ShoulderServo;
 
+    // used for motion profiling of servo
+    TrapezoidProfile profile;
+    ElapsedTime timer;
+
+
     /** Place code here to initialize subsystem */
     public ShoulderJoint() {
 
         // Creates a Servo using the hardware map
         ShoulderServo =  RobotContainer.ActiveOpMode.hardwareMap.get(Servo.class, "shoulderServo");
 
+        timer = new ElapsedTime();
+        timer.reset();
     }
 
     /** Method called periodically by the scheduler
      * Place any code here you wish to have run periodically */
     @Override
     public void periodic() {
+
+        // if we have a profile to control to, then command servo position
+        // based on time elapsed into the profile.  Otherwise do nothing.
+        if (profile!=null)
+            ShoulderServo.setPosition(profile.calculate(timer.seconds()).position);
 
     }
 
@@ -36,8 +50,26 @@ public class ShoulderJoint extends SubsystemBase {
         // Converts degrees into 0-1 float
         double servoPos = degrees/270.0;
 
+        // we are about to be commanded a new profile.
+        // first determine starting state of new profile.
+        // did we previously have a profile? If so, get current state
+        // if no profile, simply get current position and assume zero speed.
+        TrapezoidProfile.State startState;
+        if (profile==null)
+            startState = new TrapezoidProfile.State(ShoulderServo.getPosition(), 0.0);
+        else
+            startState = new TrapezoidProfile.State(ShoulderServo.getPosition(),
+                    profile.calculate(timer.seconds()).velocity);
+
+        // make a new profile
+        profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(0.20, 0.035),
+                new TrapezoidProfile.State(servoPos,0.0),
+                startState);
+
+        timer.reset();
+
         // Set the Servo to ServoPos
-        ShoulderServo.setPosition(servoPos);
+        //ShoulderServo.setPosition(servoPos);
 
     }
 
