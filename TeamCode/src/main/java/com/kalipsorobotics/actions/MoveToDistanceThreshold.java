86d9307ed6dd474f4b8d75399fe3a381ce0aaa8c
Ethan Vu <ethan.mt.vu@gmail.com>
2025-02-02 11:15:14 -0800

    move distance sensor action not done

---
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/MoveToDistanceThreshold.java
@@ -0,0 +1,15 @@
+package com.kalipsorobotics.actions;
+
+import com.kalipsorobotics.actions.outtake.DistanceDetectionAction;
+import com.kalipsorobotics.modules.Outtake;
+import com.kalipsorobotics.utilities.OpModeUtilities;
+
+public class MoveToDistanceThreshold extends KActionSet {
+
+    public MoveToDistanceThreshold(Outtake outtake) {
+
+        DistanceDetectionAction distanceDetectionAction = new DistanceDetectionAction(outtake.)
+
+    }
+
+}
