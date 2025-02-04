dd29c1d0efe751fbf4b6226b79177d2481718ca4
Ethan Vu <ethan.mt.vu@gmail.com>
2025-02-03 22:24:51 -0800

    chimmy changa :):):)

--- TeamCode/src/main/java/com/kalipsorobotics/intoTheDeep/AutoSpecimen.java
+++ TeamCode/src/main/java/com/kalipsorobotics/intoTheDeep/AutoSpecimen.java
@@ -126,7 +126,7 @@ public class AutoSpecimen extends LinearOpMode {
 //        moveFloorSamples.addPoint(-1300, -1050, -180);
         moveFloorSamples.addPoint(-1300, -1065, -180,  PurePursuitAction.P_XY_FAST, PurePursuitAction.P_ANGLE_FAST);
         moveFloorSamples.addPoint(-1300, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY_FAST, PurePursuitAction.P_ANGLE);//before push //-1300, -1175 // -1175 BEFORE DARREN CHANGED
-        moveFloorSamples.addPoint(-600, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY_SLOW, PurePursuitAction.P_ANGLE);
+        moveFloorSamples.addPoint(-600, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY, PurePursuitAction.P_ANGLE);
         moveFloorSamples.addPoint(WallToBarHangRoundTrip.WALL_PICKUP_X, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY_SLOW, PurePursuitAction.P_ANGLE); // -1065 // WAllPICKUP = -150
 //        moveFloorSamples.addPoint(-350, -1065, -180);//move back out to avoid sample carry //y = -1200 y = 375
         redAutoSpecimen.addAction(moveFloorSamples);
