296d8a0654883c1ba23770bd960f3ccc1005e746
Ethan Vu <ethan.mt.vu@gmail.com>
2025-02-05 22:24:55 -0800

    auto tune

--- TeamCode/src/main/java/com/kalipsorobotics/intoTheDeep/AutoSpecimen.java
+++ TeamCode/src/main/java/com/kalipsorobotics/intoTheDeep/AutoSpecimen.java
@@ -128,7 +128,8 @@ public class AutoSpecimen extends LinearOpMode {
         moveFloorSamples.addPoint(-1300, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY_FAST,
                 PurePursuitAction.P_ANGLE_SLOW);//before push //-1300, -1175 // -1175 BEFORE DARREN CHANGED
         moveFloorSamples.addPoint(-600, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY, PurePursuitAction.P_ANGLE_SLOW);
-        moveFloorSamples.addPoint(WallToBarHangRoundTrip.WALL_PICKUP_X, THRID_SAMPLE_PUSHING_Y, -180, PurePursuitAction.P_XY_SLOW, PurePursuitAction.P_ANGLE_SLOW); // -1065 // WAllPICKUP = -150
+        moveFloorSamples.addPoint(WallToBarHangRoundTrip.WALL_PICKUP_X, THRID_SAMPLE_PUSHING_Y, -180,
+                PurePursuitAction.P_XY, PurePursuitAction.P_ANGLE_SLOW); // -1065 // WAllPICKUP = -150
 //        moveFloorSamples.addPoint(-350, -1065, -180);//move back out to avoid sample carry //y = -1200 y = 375
         redAutoSpecimen.addAction(moveFloorSamples);
 //
