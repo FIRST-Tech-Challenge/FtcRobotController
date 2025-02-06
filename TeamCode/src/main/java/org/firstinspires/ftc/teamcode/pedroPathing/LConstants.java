751915f651ec1f523e29301e7e286d11319d4042
Evan-github1 <xiange28@polyprep.org>
2025-02-06 16:47:04 -0500

    starting pedro pathing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/LConstants.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.localization.Encoder;
+import com.pedropathing.localization.constants.ThreeWheelConstants;
+
+public class LConstants {
+    static {
+        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+        ThreeWheelConstants.strafeTicksToInches = .001989436789;
+        ThreeWheelConstants.turnTicksToInches = .001989436789;
+        ThreeWheelConstants.leftY = 1;
+        ThreeWheelConstants.rightY = -1;
+        ThreeWheelConstants.strafeX = -2.5;
+        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+    }
+}
+
+
+
+
