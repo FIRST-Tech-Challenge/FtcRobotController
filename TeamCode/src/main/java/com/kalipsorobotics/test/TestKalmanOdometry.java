fdc6875cd3d3dd8f7a5dd64acb7103312ee5315b
Alano <alan.wenhao.huang@gmail.com>
2025-02-08 11:05:26 -0800

    covarance noise values tuned for sparkfun otos

--- TeamCode/src/main/java/com/kalipsorobotics/test/TestKalmanOdometry.java
+++ TeamCode/src/main/java/com/kalipsorobotics/test/TestKalmanOdometry.java
@@ -4,6 +4,7 @@ package com.kalipsorobotics.test;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
 
+import com.kalipsorobotics.actions.drivetrain.DriveAction;
 import com.kalipsorobotics.localization.KalmanFilter;
 import com.kalipsorobotics.localization.OdometrySpark;
 import com.kalipsorobotics.localization.WheelOdometry;
@@ -20,25 +21,29 @@ import org.opencv.core.Point;
 public class TestKalmanOdometry extends LinearOpMode {
     @Override
     public void runOpMode() {
-        //SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, "sprk sensor OTOS");
-        KalmanFilter kalmanFilter = new KalmanFilter(0.5, 5); //accuate wheel odometry                                   ``); //0.2 and 8 for wheel odometry
-        //OdometrySpark odometrySpark = new OdometrySpark(otos);
+        SparkFunOTOS otos = hardwareMap.get(SparkFunOTOS.class, "sprk sensor OTOS");
+        KalmanFilter kalmanFilter = new KalmanFilter(0.7, 9);
+        //accurate wheel odometry noise: 0.5, 5
+        //accurate sparkfun optical sensor noise: 0.7 9
+        OdometrySpark odometrySpark = new OdometrySpark(otos);
         OpModeUtilities opModeUtilities = new OpModeUtilities(hardwareMap, this, telemetry);
         DriveTrain driveTrain = DriveTrain.getInstance(opModeUtilities);
+        DriveAction driveAction = new DriveAction(driveTrain);
         IMUModule imuModule = IMUModule.getInstance(opModeUtilities);
         WheelOdometry wheelOdometry = WheelOdometry.getInstance(opModeUtilities, driveTrain, imuModule, 0, 0, 0);
         kalmanFilter.reset();
         waitForStart();
 //
         while (opModeIsActive()) {
-            //Point noisyMeasurement = new Point(odometrySpark.sparkUpdateData().getX(),odometrySpark.sparkUpdateData().getY());
-            Point noisyMeasurement =  new Point(wheelOdometry.updatePosition().getX(), wheelOdometry.updatePosition().getY());
-            // Filter the measurement
-            Point filteredPoint = kalmanFilter.update(noisyMeasurement);
+            driveAction.move(gamepad1);
+            Point noisyMeasurement1 = new Point(odometrySpark.sparkUpdateData().getX(),odometrySpark.sparkUpdateData().getY());
+            Point noisyMeasurement2 =  new Point(wheelOdometry.updatePosition().getX(), wheelOdometry.updatePosition().getY());
+            Point filteredPoint1 = kalmanFilter.update(noisyMeasurement1);
+            Point filteredPoint2 = kalmanFilter.update(noisyMeasurement2);
             telemetry.addLine("Noisy data");
-            telemetry.addLine("x: " + noisyMeasurement.x + " y: " + noisyMeasurement.y);
+            telemetry.addLine("x: " + noisyMeasurement1.x + " y: " + noisyMeasurement1.y);
             telemetry.addLine("filtered data");
-            telemetry.addLine("x: " + filteredPoint.x + " y: " + filteredPoint.y);
+            telemetry.addLine("x: " + filteredPoint1.x + " y: " + filteredPoint1.y);
             if (gamepad1.a) {
                 kalmanFilter.reset();
                 telemetry.addLine("odometry values reset");
