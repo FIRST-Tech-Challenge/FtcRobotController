914fc02134ef71cdf53ede0f1c7bf44be0d8e746
Colin M. <ccmcc2005@gmail.com>
2025-02-07 19:32:45 -0600

    2-7-25-WIP1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/ClawTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Debug/ClawTest.java
@@ -3,22 +3,78 @@
 
 package org.firstinspires.ftc.teamcode.Debug;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Mekanism.Clip;
+import org.firstinspires.ftc.teamcode.Mekanism.Grabber;
 import org.firstinspires.ftc.teamcode.Mekanism.Mekanism;
 
-@Disabled
+import java.nio.file.attribute.AclEntryFlag;
+
 @TeleOp
 public class ClawTest extends LinearOpMode {
+
+
   public void runOpMode() throws InterruptedException {
-    Mekanism mek = new Mekanism(this);
+
+    Clip clip = new Clip(this);
+    Grabber grabber = new Grabber(this);
+
+    boolean
+        isA = false,
+        isB = false,
+        game1A = false,
+        game1B = false,
+        isX = false,
+        isY = false,
+        game1X = false,
+        game1Y = false;
+
     waitForStart();
     while (opModeIsActive()) {
-      mek.runIntake(gamepad1.left_trigger > .5, gamepad1.right_trigger > .5);
-      if (gamepad1.b) {
-        mek.toggleWrist();
+
+      if (gamepad1.a && !isA) {
+        game1A = !game1A;
+        isA = true;
+      } else if (!gamepad1.a) {
+        isA = false;
       }
+
+      if (game1A)
+        clip.setFunnel(1.0);
+      else clip.setFunnel(0);
+
+
+      if (gamepad1.b && !isB) {
+        game1B = !game1B;
+        isB = true;
+      } else if (!gamepad1.b) {
+        isB = false;
+      }
+
+      if (game1B)
+        clip.clamp();
+      else clip.unclamp();
+
+
+      grabber.setPower(-gamepad1.left_stick_y);
+
+      if (gamepad1.x && !isX) {
+        game1X = !game1X;
+        isX = true;
+      } else if (!gamepad1.x) {
+        isX = false;
+      }
+
+      if (game1X)
+        grabber.wristUp();
+      else grabber.wristDown();
+
+
+      clip.update();
+      grabber.update();
+
     }
   }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mekanism/Grabber.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.Mekanism;
+
+import static com.qualcomm.robotcore.hardware.Servo.Direction.FORWARD;
+import static com.qualcomm.robotcore.hardware.Servo.Direction.REVERSE;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Grabber {
+
+  LinearOpMode myOp;
+
+  Servo
+      intake1,
+      intake2,
+      wrist;
+
+  private double
+      intakePower = 0,
+      wristPos = 0;
+
+  public Grabber(LinearOpMode opMode) {
+
+    // Servos for intake
+    intake1 = opMode.hardwareMap.get(Servo.class, "intake");
+    intake2 = opMode.hardwareMap.get(Servo.class, "intake2");
+    wrist = opMode.hardwareMap.get(Servo.class, "wrist");
+
+    intake1.setDirection(REVERSE);
+    intake2.setDirection(FORWARD);
+    wrist.setDirection(REVERSE);
+
+    wrist.scaleRange(0.65, 1.0);
+
+    myOp = opMode;
+  }
+
+  public void update() {
+    intake1.setPosition((intakePower + 1) / 2);
+    intake2.setPosition((intakePower + 1) / 2);
+
+    wrist.setPosition(wristPos);
+  }
+
+
+  /**
+   * @param power (-1) to 1
+   */
+  public void setPower(double power) {
+    intakePower = power;
+  }
+
+  public void setWrist(double pos) {
+    wristPos = pos;
+  }
+
+  public void wristUp(){
+    wristPos = 1;
+  }
+  public void wristDown(){
+    wristPos = 0;
+  }
+}
