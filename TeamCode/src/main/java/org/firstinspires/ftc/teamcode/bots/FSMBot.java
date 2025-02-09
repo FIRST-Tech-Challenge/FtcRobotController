adef7b342d2d88862a52f3f178cd3102f7397523
Lolcheese <68646897+geniuspig2986@users.noreply.github.com>
2025-02-08 16:56:12 -0800

    Create FSMBot.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bots/FSMBot.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.bots;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.acmerobotics.dashboard.FtcDashboard;
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+@Disabled
+public class FSMBot extends WristBot{
+
+}
+
