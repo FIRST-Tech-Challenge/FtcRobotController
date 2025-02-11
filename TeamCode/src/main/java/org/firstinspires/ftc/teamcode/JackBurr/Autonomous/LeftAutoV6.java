b08057d77b9ae8808c564b4e11bb31f923a1b9a7
EPICGAMING191 <BurrJ22@outlook.com>
2025-02-10 21:15:06 -0500

    1+1 auto, setup for 1+2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Autonomous/LeftAutoV6.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Autonomous/LeftAutoV6.java
@@ -11,7 +11,6 @@ import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
@@ -25,10 +24,9 @@ import org.firstinspires.ftc.teamcode.JackBurr.Servos.DifferentialV2;
 import org.firstinspires.ftc.teamcode.JackBurr.Servos.GrippersV1;
 import org.firstinspires.ftc.teamcode.JackBurr.Servos.WristAxonV1;
 
-@Disabled
 @Config
 @Autonomous
-public class LeftAutoV4 extends LinearOpMode {
+public class LeftAutoV6 extends LinearOpMode {
     public DeliverySlidesV1 slides = new DeliverySlidesV1();
     public DeliveryAxonV1 deliveryAxonV1 = new DeliveryAxonV1();
     public RobotConstantsV1 robotConstantsV1 = new RobotConstantsV1();
@@ -50,19 +48,14 @@ public class LeftAutoV4 extends LinearOpMode {
     //----------------------------------------------------------------------------------------
     public static Pose2d startPose = new Pose2d(36, 62, Math.toRadians(-90)); // (60,0), 180
 
-    public static Vector2d position1 = new Vector2d(21, 42); // (40,-15) (x-20, y-15)
-    public static double position1HeadingDegrees = -120; // 150 (heading -30)
+    public static Vector2d position1 = new Vector2d(51, 47);
+    public static double position1HeadingDegrees = -135;
 
-    public static Vector2d position2 = new Vector2d(22, 57); // (46, -5) (x+6, y+10)
-    public static double position2HeadingDegrees = -120; //150 (heading +0)
+    public static Vector2d position2 = new Vector2d(47, 43);
+    public static double position2HeadingDegrees = -85;
 
-    public static double position3Degrees = -90; // 180 (heading +30)
+    public static Vector2d position4 = new Vector2d(61, 47);
 
-    public static double position4Degrees = -90; // 180 (heading +0)
-
-    public static Vector2d position5 = new Vector2d(17.75, 54.35); // (41.75, -7.65) (x - 4.25, y - 2.65)
-    public static Vector2d position6 = new Vector2d(30, 52); // (54, -10) (x + 12.25, y + 2.35)
-    public static double position6HeadingDegrees = -140; // 130 (heading - 50)
 
     //+X is left, +Y is backwards
 
@@ -101,24 +94,36 @@ public class LeftAutoV4 extends LinearOpMode {
         telemetry.addData(">", "Press Play to start op mode");
         telemetry.update();
 
-        Pose2d startPose = new Pose2d(60, 0, Math.toRadians(180));
 
         traj1Builder = drive.actionBuilder(startPose)
-                .splineTo(position1, Math.toRadians(position1HeadingDegrees));
-        traj2Builder = traj1Builder.fresh()
-                .lineToX(position2.x);
-        traj3Builder = traj2Builder.fresh()
-                .turnTo(Math.toRadians(position3Degrees));
-        traj4Builder = traj3Builder.fresh()
-                .strafeTo(position5);
-        //traj5Builder = traj4Builder.fresh()
-                //.turnTo(Math.toRadians(position4Degrees));
-        traj6Builder = traj4Builder.fresh()
                 .strafeTo(position1)
-                .turnTo(Math.toRadians(position6HeadingDegrees));
-        traj7Builder = traj6Builder.fresh()
-                .strafeTo(position6)
-                .turnTo(Math.toRadians(position6HeadingDegrees));
+                .turnTo(Math.toRadians(position1HeadingDegrees));
+        traj2Builder = traj1Builder.fresh()
+                .strafeTo(position2)
+                .turnTo(Math.toRadians(position2HeadingDegrees))
+                .turnTo(Math.toRadians(position2HeadingDegrees))
+                .strafeTo(position2);
+        traj3Builder = traj2Builder.fresh()
+                .strafeTo(position1)
+                .turnTo(Math.toRadians(position1HeadingDegrees));
+        traj4Builder = traj3Builder.fresh()
+                .strafeTo(position4)
+                .turnTo(Math.toRadians(position2HeadingDegrees))
+                .turnTo(Math.toRadians(position2HeadingDegrees))
+                .strafeTo(position2);
+
+        //traj3Builder = traj2Builder.fresh()
+        //.turnTo(Math.toRadians(position3Degrees));
+        //traj4Builder = traj3Builder.fresh()
+        //.strafeTo(position5);
+        //traj5Builder = traj4Builder.fresh()
+        //.turnTo(Math.toRadians(position4Degrees));
+        //traj6Builder = traj4Builder.fresh()
+        //.strafeTo(position1)
+        //.turnTo(Math.toRadians(position6HeadingDegrees));
+        //traj7Builder = traj6Builder.fresh()
+        //.strafeTo(position6)
+        //.turnTo(Math.toRadians(position6HeadingDegrees));
 
 
 
@@ -129,8 +134,8 @@ public class LeftAutoV4 extends LinearOpMode {
         traj3 = traj3Builder.build();
         traj4 = traj4Builder.build();
         //traj5 = traj5Builder.build();
-        traj6 = traj6Builder.build();
-        traj7 = traj7Builder.build();
+        //traj6 = traj6Builder.build();
+        //traj7 = traj7Builder.build();
 
 
         // Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
@@ -180,15 +185,14 @@ public class LeftAutoV4 extends LinearOpMode {
                         deliveryAxonV1.setPosition(robotConstantsV1.DELIVERY_UP);
                         servoSet2 = true;
                     }
-                    if(!traj4Followed) {
-                        Actions.runBlocking(traj2);
-                        traj4Followed = true;
-                    }
                 }
                 while (deliveryTimer.seconds() < 3.5){
                     deliveryGrippers.setPosition(robotConstantsV1.DELIVERY_GRIPPERS_OPEN);
                 }
                 while (deliveryTimer.seconds() < 4.5) {
+                    intakeSlides.runToPosition(intakeTarget01, 1);
+                    diffV2.setTopRightServoPosition(robotConstantsV1.FRONT_RIGHT_HOVER);
+                    diffV2.setTopLeftServoPosition(robotConstantsV1.FRONT_LEFT_HOVER);
                     if (!servoSet) {
                         deliveryAxonV1.setPosition(robotConstantsV1.DELIVERY_GRAB);
                         grippers.setPosition(robotConstantsV1.GRIPPERS_OPEN);
@@ -202,8 +206,8 @@ public class LeftAutoV4 extends LinearOpMode {
                         slides.runRightSlideToPosition(0, 0.9);
                     }
                     if(!traj5Followed) {
-                        Actions.runBlocking(traj3);
-                        Actions.runBlocking(traj4);
+                        Actions.runBlocking(traj2);
+                        //Actions.runBlocking(traj4);
                         traj5Followed = true;
                     }
                 }
@@ -215,18 +219,15 @@ public class LeftAutoV4 extends LinearOpMode {
                 }
             }
             if(step == 2) {
-                while (intakeTimer.seconds() < 2){
-                    intakeSlides.runToPosition(intakeTarget01, 1);
+                while (intakeTimer.seconds() < 0.8){
+                    diffV2.setTopRightServoPosition(robotConstantsV1.FRONT_RIGHT_PICKUP);
+                    diffV2.setTopLeftServoPosition(robotConstantsV1.FRONT_LEFT_PICKUP);
                     if(!wristSet){
                         wrist.setPosition(robotConstantsV1.WRIST_CENTER);
                         wristSet = true;
                     }
                 }
-                while(intakeTimer.seconds() < 3){
-                    diffV2.setTopRightServoPosition(robotConstantsV1.FRONT_RIGHT_PICKUP);
-                    diffV2.setTopLeftServoPosition(robotConstantsV1.FRONT_LEFT_PICKUP);
-                }
-                while (intakeTimer.seconds() < 5.5){
+                while (intakeTimer.seconds() < 1.3){
                     grippers.setPosition(robotConstantsV1.GRIPPERS_GRAB);
                 }
                 step = 3;
@@ -248,14 +249,14 @@ public class LeftAutoV4 extends LinearOpMode {
                 while (intakeTimer.seconds() < 3.5){
                     intakeSlides.intakeAllTheWayIn();
                 }
-                while (intakeTimer.seconds() < 4.5){
+                while (intakeTimer.seconds() < 4.2){
                     deliveryGrippers.setPosition(robotConstantsV1.DELIVERY_GRIPPERS_CLOSE);
                 }
-                while (intakeTimer.seconds() < 5.2){
+                while (intakeTimer.seconds() < 5){
                     grippers.setPosition(robotConstantsV1.GRIPPERS_OPEN);
                 }
-                Actions.runBlocking(traj6);
-                Actions.runBlocking(traj7);
+                Actions.runBlocking(traj3);
+                //Actions.runBlocking(traj7);
                 deliveryTimer.reset();
                 servoSet = false;
                 servoSet2 = false;
@@ -263,7 +264,7 @@ public class LeftAutoV4 extends LinearOpMode {
                 step = 5;
             }
             if(step == 5){
-                while (deliveryTimer.seconds() < 2.5){
+                while (deliveryTimer.seconds() < 2){
                     deliveryAxonV1.setPosition(robotConstantsV1.DELIVERY_GRAB);
                     slides.runLeftSlideToPosition(robotConstantsV1.LEFT_SLIDE_HIGH_BASKET, 0.9);
                     slides.runRightSlideToPosition(robotConstantsV1.RIGHT_SLIDE_HIGH_BASKET, 0.9);
@@ -273,7 +274,7 @@ public class LeftAutoV4 extends LinearOpMode {
                     }
                     deliveryGrippers.setPosition(robotConstantsV1.DELIVERY_GRIPPERS_CLOSE);
                 }
-                while (deliveryTimer.seconds() < 4) {
+                while (deliveryTimer.seconds() < 2.75) {
                     servoSet = false;
                     if (!servoSet2) {
                         deliveryAxonV1.setPosition(robotConstantsV1.DELIVERY_UP);
@@ -284,7 +285,7 @@ public class LeftAutoV4 extends LinearOpMode {
                 step = 6;
             }
             if(step == 6){
-                while (deliveryTimer.seconds() < 2) {
+                while (deliveryTimer.seconds() < 1) {
                     deliveryGrippers.setPosition(robotConstantsV1.DELIVERY_GRIPPERS_OPEN);
                 }
                 servoSet2 = false;
@@ -301,6 +302,30 @@ public class LeftAutoV4 extends LinearOpMode {
                     slides.runLeftSlideToPosition(0, 0.9);
                     slides.runRightSlideToPosition(0, 0.9);
                 }
+                intakeSlides.runToPosition(intakeTarget01, 1);
+                diffV2.setTopRightServoPosition(robotConstantsV1.FRONT_RIGHT_HOVER);
+                diffV2.setTopLeftServoPosition(robotConstantsV1.FRONT_LEFT_HOVER);
+                step = 7;
+            }
+            if(step == 8){
+                Actions.runBlocking(traj4);
+                wristSet = false;
+                intakeTimer.reset();
+                step = 8;
+            }
+            if(step == 9){
+                while (intakeTimer.seconds() < 0.8){
+                    diffV2.setTopRightServoPosition(robotConstantsV1.FRONT_RIGHT_PICKUP);
+                    diffV2.setTopLeftServoPosition(robotConstantsV1.FRONT_LEFT_PICKUP);
+                    if(!wristSet){
+                        wrist.setPosition(robotConstantsV1.WRIST_CENTER);
+                        wristSet = true;
+                    }
+                }
+                while (intakeTimer.seconds() < 1.3){
+                    grippers.setPosition(robotConstantsV1.GRIPPERS_GRAB);
+                }
+                step = 10;
             }
         }
     }
