4dca9f2fc11bb5a2184eaed0e718d7b8e58378fc
Colin M. <ccmcc2005@gmail.com>
2025-02-15 14:47:37 -0600

    Utils Update
    Added map function to Utils.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils.java
@@ -11,4 +11,39 @@ public class Utils {
   public static double getTimeMiliSeconds() {
     return System.nanoTime() / 1e6;
   }
+
+
+  /**
+   * Map one range of numbers to another
+   *
+   * @param input - Input variable
+   * @param old_Lower - Lower range of input
+   * @param old_Upper - Upper range of input
+   * @param new_Lower - Lower range of output
+   * @param new_Upper - Upper range of output
+   * @return - New mapped variable
+   *
+   * @see #map(double, double, double, double, double)
+   * @see #map(int, int, int, int, int)
+   */
+  public static double map(double input, double old_Lower, double old_Upper, double new_Lower, double new_Upper) {
+    return (input - old_Lower) / (old_Lower - old_Upper) * (new_Upper - new_Lower) + new_Lower;
+  }
+
+  /**
+   * Map one range of numbers to another
+   *
+   * @param input - Input variable
+   * @param old_Lower - Lower range of input
+   * @param old_Upper - Upper range of input
+   * @param new_Lower - Lower range of output
+   * @param new_Upper - Upper range of output
+   * @return - New mapped variable
+   *
+   * @see #map(double, double, double, double, double)
+   * @see #map(int, int, int, int, int)
+   */
+  public static int map(int input, int old_Lower, int old_Upper, int new_Lower, int new_Upper) {
+    return (input - old_Lower) / (old_Lower - old_Upper) * (new_Upper - new_Lower) + new_Lower;
+  }
 }
