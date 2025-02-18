1454da2e368777d1aa3b8a075bad20f95f9bbf21
EPICGAMING191 <BurrJ22@outlook.com>
2025-02-17 20:42:06 -0500

    Add Pedro Pathing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Odometry/PedroPathing/constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Odometry/PedroPathing/constants/FConstants.java
@@ -8,12 +8,12 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class FConstants {
     static {
-        FollowerConstants.localizers = Localizers.THREE_WHEEL;
+        FollowerConstants.localizers = Localizers.PINPOINT;
 
-        FollowerConstants.leftFrontMotorName = "leftFront";
-        FollowerConstants.leftRearMotorName = "leftRear";
-        FollowerConstants.rightFrontMotorName = "rightFront";
-        FollowerConstants.rightRearMotorName = "rightRear";
+        FollowerConstants.leftFrontMotorName = "fl";
+        FollowerConstants.leftRearMotorName = "bl";
+        FollowerConstants.rightFrontMotorName = "fr";
+        FollowerConstants.rightRearMotorName = "br";
 
         FollowerConstants.leftFrontMotorDirection = DcMotorSimple.Direction.REVERSE;
         FollowerConstants.leftRearMotorDirection = DcMotorSimple.Direction.REVERSE;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Odometry/PedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JackBurr/Odometry/PedroPathing/constants/LConstants.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.JackBurr.Odometry.PedroPathing.constants;
 import com.pedropathing.localization.*;
 import com.pedropathing.localization.constants.*;
 
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
 public class LConstants {
     static {
         ThreeWheelConstants.forwardTicksToInches = .001989436789;
@@ -17,6 +19,17 @@ public class LConstants {
         ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
         ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+        PinpointConstants.forwardY = -1.5;
+        PinpointConstants.strafeX = -6.75;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = "odo";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
     }
 }
 
