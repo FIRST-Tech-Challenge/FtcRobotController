afe458d28a48deb7efa0206fb153d1a940b164a2
Armaan Sidhu <armaansidhu000@icloud.com>
2025-03-12 14:13:05 -0700

    Added Claw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 public class Arm {
+
+    public Arm(String leftArmName, String RightArmName)
+    {
+        
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Claw.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Claw {
+    private Servo claw;
+    HardwareMap hardwareMap;
+    private final double OPEN = 0;
+    private final double CLOSE = 0.7;
+    public Claw(String clawName)
+    {
+        claw = hardwareMap.get(Servo.class, clawName);
+    }
+
+    public void Open()
+    {
+        claw.setPosition(OPEN);
+    }
+
+    public void Close()
+    {
+        claw.setPosition(CLOSE);
+    }
 }
