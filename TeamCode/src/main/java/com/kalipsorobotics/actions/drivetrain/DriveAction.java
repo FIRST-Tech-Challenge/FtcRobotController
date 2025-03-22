b6e68a3b7cbb1cff8065bb267148797f960a1969
Alan <alan.wenhao.huang@gmail.com>
2025-03-22 11:07:52 -0700

    new function in drive action that uses gamepad x y values

--- TeamCode/src/main/java/com/kalipsorobotics/actions/drivetrain/DriveAction.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/drivetrain/DriveAction.java
@@ -30,6 +30,37 @@ public class DriveAction {
         this.backEncoder = driveTrain.getBackEncoder();
     }
 
+    public double[] calculatePower(double gamepadx, double gamepady) {
+        //negative because gamepad y is flip
+        double forward = -gamepady * -gamepady * -gamepady; //cube so slower is slower and faster is faster
+        double turn = gamepadx * gamepadx * gamepadx;
+        double strafe = gamepadx * gamepadx * gamepadx;
+
+        Log.d("drive", "forward " + forward);
+        Log.d("drive", "turn " + turn);
+        Log.d("drive", "strafe " + strafe);
+
+        double fLeftPower = (forward + strafe + turn);
+        double fRightPower = (forward - strafe - turn);
+        double bLeftPower = (forward - strafe + turn);
+        double bRightPower = (forward + strafe - turn);
+
+//        double fLeftPower = powerX + powerY + powerAngle;
+//        double fRightPower = powerX - powerY - powerAngle;
+//        double bLeftPower = powerX - powerY + powerAngle;
+//        double bRightPower = powerX + powerY - powerAngle;
+
+
+        double absMaxPower = MathFunctions.maxAbsValueDouble(fLeftPower, fRightPower, bLeftPower, bRightPower);
+        if (absMaxPower > 1) {
+            fLeftPower = fLeftPower / absMaxPower;
+            fRightPower = fRightPower / absMaxPower;
+            bLeftPower = bLeftPower / absMaxPower;
+            bRightPower = bRightPower / absMaxPower;
+        }
+        double[] driveTrainPowers = {fLeftPower, fRightPower, bLeftPower, bRightPower};
+        return driveTrainPowers;
+    }
     public double[] calculatePower(Gamepad gamepad) {
         //negative because gamepad y is flip
         double forward = -gamepad.left_stick_y * -gamepad.left_stick_y * -gamepad.left_stick_y; //cube so slower is slower and faster is faster
@@ -76,6 +107,19 @@ public class DriveAction {
         Log.d("drive", "bLeft power " + driveTrainPower[2]);
         Log.d("drive", "bRight power " + driveTrainPower[3]);
 
+    }
+    public void moveWithXYValues(double x, double y) {
+        double[] driveTrainPower = calculatePower(x, y);
+
+        fLeft.setPower(driveTrainPower[0]);
+        fRight.setPower(driveTrainPower[1]);
+        bLeft.setPower(driveTrainPower[2]);
+        bRight.setPower(driveTrainPower[3]);
+
+        Log.d("drive", "fLeft power " + driveTrainPower[0]);
+        Log.d("drive", "fRight power " + driveTrainPower[1]);
+        Log.d("drive", "bLeft power " + driveTrainPower[2]);
+        Log.d("drive", "bRight power " + driveTrainPower[3]);
     }
 
     public OpModeUtilities getOpModeUtilities() {
