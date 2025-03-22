1b0a419ddb14d2ce9eea24c6596477086081a0b7
Alan <alan.wenhao.huang@gmail.com>
2025-03-22 11:31:59 -0700

    imu fixed and test code finished

--- TeamCode/src/main/java/com/kalipsorobotics/modules/FieldOrientedDrive.java
+++ TeamCode/src/main/java/com/kalipsorobotics/modules/FieldOrientedDrive.java
@@ -7,27 +7,31 @@ import com.kalipsorobotics.localization.SparkfunOdometry;
 import com.kalipsorobotics.math.Position;
 import com.kalipsorobotics.utilities.OpModeUtilities;
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.IMU;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.opencv.core.Point;
 
 public class FieldOrientedDrive {
-    SparkfunOdometry sparkfunOdometry;
+    OpModeUtilities opModeUtilities;
+    IMU imu;
+    DriveTrain driveTrain;
     DriveAction driveAction;
-    DcMotor bLeft;
-    DcMotor bRight;
-    DcMotor fLeft;
-    DcMotor fRight;
-    public FieldOrientedDrive(SparkfunOdometry sparkfunOdometry, DriveTrain driveTrain, OpModeUtilities opModeUtilities) {
-        sparkfunOdometry = sparkfunOdometry;
-
+    public FieldOrientedDrive(IMU imu, DriveTrain driveTrain, OpModeUtilities opModeUtilities) {
+        imu = imu;
+        driveTrain = driveTrain;
         driveAction = new DriveAction(driveTrain);
+        opModeUtilities = opModeUtilities;
     }
 
-    public SparkfunOdometry getSparkfunOdometry() {
-        return sparkfunOdometry;
+    public IMU getImu() {
+        return imu;
     }
     private double angleCalculator(double x, double y) {
         double r = Math.atan2(y, x);
@@ -35,9 +39,7 @@ public class FieldOrientedDrive {
         return degrees;
     }
     private double calculateRobotDir(Gamepad gamepad){
-        sparkfunOdometry.updatePosition();
-        Position p = sparkfunOdometry.getCurrentPosition();
-        double heading = p.getTheta();
+        double heading = imu.getRobotYawPitchRollAngles().getYaw();
         double gamepadx = gamepad.left_stick_x;
         double gamepady = gamepad.left_stick_y;
         double gamepadHeading = angleCalculator(gamepadx, gamepady);
@@ -49,6 +51,18 @@ public class FieldOrientedDrive {
         double distanceSqr = (p.x*p.x)+(p.y*p.y);
         double distance = Math.sqrt(distanceSqr);
         return distance;
+    }
+    private void initializeIMU(){
+//        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+//        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+//        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+//        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample OpMode
+//        parameters.loggingEnabled      = true;
+//        parameters.loggingTag          = "IMU";
+//        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+//
+//        imu = opModeUtilities.getHardwareMap().get(BNO055IMU.class, "imu");
+//        imu.initialize(parameters);
     }
     public Point reverseCalculate(double angle, double distance) {
 
--- TeamCode/src/main/java/com/kalipsorobotics/test/FieldOrientatedDriveTest.java
+++ TeamCode/src/main/java/com/kalipsorobotics/test/FieldOrientatedDriveTest.java
@@ -1,10 +1,24 @@
 package com.kalipsorobotics.test;
 
+import com.kalipsorobotics.modules.DriveTrain;
+import com.kalipsorobotics.modules.FieldOrientedDrive;
+import com.kalipsorobotics.modules.IMUModule;
+import com.kalipsorobotics.utilities.OpModeUtilities;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class FieldOrientatedDriveTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        OpModeUtilities opModeUtilities = new OpModeUtilities(hardwareMap, this, telemetry);
+        DriveTrain driveTrain = DriveTrain.getInstance(opModeUtilities);
+        IMUModule imuModule = IMUModule.getInstance(opModeUtilities);
 
+        FieldOrientedDrive fieldOrientedDrive = new FieldOrientedDrive(imuModule.imu, driveTrain, opModeUtilities);
+        waitForStart();
+        while (opModeIsActive()) {
+            fieldOrientedDrive.drive(gamepad1);
+        }
     }
 }
