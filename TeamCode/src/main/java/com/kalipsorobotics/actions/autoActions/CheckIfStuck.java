9614973ecacbc60086970c5026f991c3ad9a3e21
Alan <alan.wenhao.huang@gmail.com>
2025-03-22 21:29:46 -0700

    starting function for check stuck

--- TeamCode/src/main/java/com/kalipsorobotics/actions/autoActions/CheckIfStuck.java
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/autoActions/CheckIfStuck.java
@@ -1,11 +1,23 @@
 package com.kalipsorobotics.actions.autoActions;
 
+import com.kalipsorobotics.modules.DriveTrain;
+import com.kalipsorobotics.utilities.OpModeUtilities;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 
 public class CheckIfStuck {
     IMU imu;
-    public CheckIfStuck(IMU imu1) {
+    DriveTrain driveTrain;
+    OpModeUtilities opModeUtilities;
+    public CheckIfStuck(IMU imu1, DriveTrain driveTrain1, OpModeUtilities opModeUtilities1) {
         imu = imu1;
+        driveTrain = driveTrain1;
+        opModeUtilities = opModeUtilities1;
+    }
 
+    public boolean checkStuck() {
+        DcMotor motor = driveTrain.getfRight();
+        boolean isStuck = false;
+        return isStuck;
     }
 }
