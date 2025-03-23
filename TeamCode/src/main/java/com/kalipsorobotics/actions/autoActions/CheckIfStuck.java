7f0596ba2fe41e28fe7b2164d821d3eddcc3418d
Alan <alan.wenhao.huang@gmail.com>
2025-03-22 21:23:21 -0700

    new check stuck class

---
+++ TeamCode/src/main/java/com/kalipsorobotics/actions/autoActions/CheckIfStuck.java
@@ -0,0 +1,11 @@
+package com.kalipsorobotics.actions.autoActions;
+
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class CheckIfStuck {
+    IMU imu;
+    public CheckIfStuck(IMU imu1) {
+        imu = imu1;
+
+    }
+}
