55d4cafe9612e9ab7a7bb0d4ef26b23f1c4dd84e
AidenYav <yavaiden@gmail.com>
2025-03-26 15:46:58 -0700

    Added some fixes/changes to Arm and Wrist.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Arm.java
@@ -1,15 +1,19 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.annotation.SuppressLint;
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Arm {
     private Servo backArm;
     private Servo frontArm;
+    private double position;
     public Arm(HardwareMap hardwareMap, String frontArmName, String backArmName)
     {
         backArm = hardwareMap.get(Servo.class, backArmName);
         frontArm = hardwareMap.get(Servo.class, frontArmName);
+        frontArm.setDirection(Servo.Direction.REVERSE);
 
     }
     public void setPosition(double pos){
@@ -22,5 +26,23 @@ public class Arm {
     }
     public double getBackArm(){
         return backArm.getPosition();
+    }
+
+
+    public void adjustPosition(double increment){
+        position += increment*0.001;
+        backArm.setPosition(position);
+        frontArm.setPosition(position);
+    }
+
+    public void setParallel(){
+        backArm.setPosition(0.5);
+        frontArm.setPosition(0.5);
+        position = 0.5;
+    }
+
+    @SuppressLint("DefaultLocale")
+    public String toString(){
+        return String.format("Arm Position: %.2f",position);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HockeyStick.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HockeyStick.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -7,10 +8,19 @@ public class HockeyStick
 {
     //FIGURE OUT LIMIT
     private final double LIMIT = 0;
+
+    private double parallel;
+    private double hover;
     private DcMotorEx hockeyStick;
     public HockeyStick(HardwareMap hardwareMap, String motorName)
     {
         hockeyStick = hardwareMap.get(DcMotorEx.class, motorName);
+    }
+
+    public void resetEncoders(){
+        hockeyStick.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        hockeyStick.setTargetPosition(0);
+        hockeyStick.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     public void power(double power)
