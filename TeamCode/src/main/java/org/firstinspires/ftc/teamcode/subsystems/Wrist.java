e401c72557a27e98999b4602326dda6c7c48772c
AidenYav <yavaiden@gmail.com>
2025-03-26 15:46:58 -0700

    Added some fixes/changes to Arm and Wrist.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Wrist.java
@@ -1,4 +1,34 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class Wrist {
+
+    Servo wrist;
+    private double position;
+    public Wrist(HardwareMap hw, String wristName){
+        wrist = hw.get(Servo.class,wristName);
+    }
+
+    public void adjustPosition(double increment){
+        position += 0.001 * increment;
+        wrist.setPosition(position);
+    }
+
+    public double getPosition() {
+        return position;
+    }
+
+    public void setParallel(){
+        wrist.setPosition(0.5);
+        position = 0.5;
+    }
+
+    @SuppressLint("DefaultLocale")
+    public String toString(){
+        return String.format("Wrist Position: %.2f",position);
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP/ArmTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOP/ArmTest.java
@@ -7,24 +7,42 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.Arm;
+import org.firstinspires.ftc.teamcode.subsystems.Wrist;
 import org.firstinspires.ftc.teamcode.utils.GamepadEvents;
 
+@TeleOp(name = "Arm Test")
 public class ArmTest extends LinearOpMode {
     GamepadEvents controller1;
     Arm arm;
-    @TeleOp(name = "Arm Test")
+    Wrist wrist;
+
+
+    @Override
     public void runOpMode() throws InterruptedException {
         controller1 = new GamepadEvents(gamepad1);
         arm = new Arm(hardwareMap, "frontArm", "backArm");
+        wrist = new Wrist(hardwareMap, "wrist");
         waitForStart();
-        @Override
+
         while(opModeIsActive()){
+//            if(gamepad1.a){
+//                arm.setPosition(0.5);
+//            }
+//            if(gamepad1.b){
+//                arm.setPosition(0.5);
+//            }
             if(gamepad1.a){
-                arm.setPosition(0.1);
+                arm.setParallel();
             }
             if(gamepad1.b){
-                arm.setPosition(-0.1);
+                wrist.setParallel();
             }
+
+            arm.adjustPosition(controller1.left_stick_y);
+            wrist.adjustPosition(controller1.right_stick_y);
+            telemetry.addLine(arm.toString());
+            telemetry.update();
+            controller1.update();
         }
     }
 }
