ef2e9519f4036642465c548c62e475d58ca748f7
Davi-coder123 <davif.camillo@gmail.com>
2025-05-16 15:12:36 -0300

    teste pro sérgio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/actuators/Garra.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/actuators/Garra.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp
+//@TeleOp
 public class Garra extends OpMode {
     Servo rotate;
     Servo garra;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/Teleop.java
@@ -2,11 +2,6 @@ package org.firstinspires.ftc.teamcode.drive.teleop;
 
 import static android.os.SystemClock.sleep;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLStatus;
-import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -85,10 +80,17 @@ public class Teleop extends OpMode {
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        frontRight.setDirection(DcMotor.Direction.REVERSE);
-        backRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Hardware: ", "Initialized");
+
+        pleft.setPosition(0.8);
+        pright.setPosition(0.2);
+        rotate.setPosition(0.7);
+        pleft.setPosition(0.8);
+        pright.setPosition(0.2);
+        garra.setPosition(0.3);
     }
         public void loop(){
             //INTAKE
@@ -162,7 +164,33 @@ public class Teleop extends OpMode {
                 climb();
             }
             //MOVIMENTACAO
-            anda();
+            Deadline gamepadRateLimit = new Deadline(500, TimeUnit.MILLISECONDS);
+
+            IMU imu = hardwareMap.get(IMU.class, "imu");
+            IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                    RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                    RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD
+            ));
+            imu.initialize(parameters);
+            double lx = -gamepad1.left_stick_x;
+            double ly = gamepad1.left_stick_y;
+            double rx = -gamepad1.right_stick_x;
+
+            double max = Math.max(Math.abs(lx) + Math.abs(ly) + Math.abs(rx), 1);
+
+            double drivePower = 1 - (0.5 * gamepad1.right_trigger);
+
+            if(gamepad1.left_bumper) reset(imu);
+            double heading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+            double adjustedLx = +ly * Math.sin(heading) + lx * Math.cos(heading);
+            double adjustedLy = ly * Math.cos(heading) - lx * Math.sin(heading);
+
+            frontLeft.setPower(((adjustedLy + adjustedLx + rx) / max) * drivePower);
+            backLeft.setPower(((adjustedLy - adjustedLx + rx) / max) * drivePower);
+            frontRight.setPower(((adjustedLy - adjustedLx - rx) / max) * drivePower);
+            backRight.setPower(((adjustedLy + adjustedLx - rx) / max) * drivePower);
+            telemetry.update();
+
         }
         public void viperslide1Up ( int turnage){
         newTarget = ticks / turnage;
@@ -214,16 +242,9 @@ public class Teleop extends OpMode {
         viperslide1Down();
         viperslide2Down();
     }
-    private void anda(){
-        double forward = gamepad2.left_stick_y;
-        double strafe = -gamepad2.left_stick_x;
-        double turn = -gamepad2.right_stick_x;
 
-        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(turn), 0.5);
-
-        frontRight.setPower((forward - strafe - turn) / denominator);
-        frontLeft.setPower((forward + strafe + turn) / denominator);
-        backLeft.setPower((forward - strafe + turn) / denominator);
-        backRight.setPower((forward + strafe - turn) / denominator);
+    public void reset(IMU imu) {
+        imu.resetYaw();
     }
+
 }
