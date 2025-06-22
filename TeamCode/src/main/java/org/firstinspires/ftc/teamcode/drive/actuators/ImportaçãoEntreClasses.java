8abfe988bb8ae8b9e275fa5847b0a49d0995adb2
Davi-coder123 <davif.camillo@gmail.com>
2025-06-22 00:14:15 -0300

    teste de ImportaçãoEntreClasses

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/actuators/ImportaçãoEntreClasses.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.drive.actuators;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+public class ImportaçãoEntreClasses extends OpMode {
+    public void init(){
+    }
+    public void loop(){
+        Garra movements = new Garra();
+        movements.loop();
+    }
+}
