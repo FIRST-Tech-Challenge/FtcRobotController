47aed1d451ade28cc8b2fb979efe2494dfd430e2
isabellad178 <isabelladoni@nestmk12.net>
2025-07-21 19:00:46 -0400

    MotionProfile fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Utils/Planners/MotionProfile.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mechanisms/Utils/Planners/MotionProfile.java
@@ -2,96 +2,199 @@ package org.firstinspires.ftc.teamcode.Mechanisms.Utils.Planners;
 
 import com.acmerobotics.dashboard.config.Config;
 
+
 @Config
 public class MotionProfile {
-    double desiredD;
-    double deltaTAccel;
-    double deltaTDecel;
-    double dAccel;
-    double dDecel;
-    double dCruise;
-    double deltaTCruise;
-    double deltaT;
-    double vMax;
-    double aAccel;
-    double aDecel;
-    int reverse;
-    public MotionProfile(double desiredD, double vMax, double aAccel, double aDecel, boolean reverse){
-        double deltaTAccel = vMax/aAccel;
-        double deltaTDecel = vMax/aDecel;
-        double dAccel = aAccel*Math.pow(deltaTAccel,2)/2;
-        double dDecel = aDecel*Math.pow(deltaTDecel,2)/2;
-        if (desiredD<dAccel+dDecel) {
-            deltaTDecel = Math.sqrt((2*aAccel*desiredD)/(Math.pow(aDecel,2)+aAccel*aDecel));
-            deltaTAccel = (aDecel/aAccel)*deltaTDecel;
-            dAccel = aAccel*Math.pow(deltaTAccel,2)/2;
-            dDecel = aDecel*Math.pow(deltaTDecel,2)/2;
-            vMax = aAccel * deltaTAccel;
+
+    // Motion Profile parameters
+    double targetDistance; // Total distance the robot should move
+    double accelerationTime; // Time spent accelerating
+    double decelerationTime; // Time spent decelerating
+    double accelerationDistance; // Distance covered during acceleration
+    double decelerationDistance; // Distance covered during deceleration
+    double cruiseDistance; // Distance covered at constant max speed
+    double cruiseTime; // Time spent at a constant speed
+    double totalTime; // Total time for the motion
+    double maxVelocity; // Max allowed velocity
+    double maxAcceleration; // Max allowed acceleration
+    double maxDeceleration; // Max allowed deceleration
+    int reverse; //
+
+    /**
+     * Generates a motion profile based on distance and motion constraints.
+     * It creates either a trapezoidal profile (acceleration → cruise → deceleration)
+     * or a triangular profile (acceleration → deceleration only if not enough distance).
+     *
+     * @param targetDistance  Distance to move
+     * @param maxVelocity     Max velocity during motion
+     * @param maxAcceleration Max acceleration
+     * @param maxDeceleration Max deceleration
+     * @param reverse         Whether to reverse the direction of motion
+     */
+
+    public MotionProfile(double targetDistance, double maxVelocity, double maxAcceleration, double maxDeceleration, boolean reverse) {
+
+        // Calculate time to reach max velocity (acceleration and deceleration phases)
+        double accelerationTime = maxVelocity / maxAcceleration;
+        double decelerationTime = maxVelocity / maxDeceleration;
+
+        // Calculate distances covered during acceleration and deceleration
+        double accelerationDistance = maxAcceleration * Math.pow(accelerationTime, 2) / 2;
+        double decelerationDistance = maxDeceleration * Math.pow(decelerationTime, 2) / 2;
+
+        // If the distance is too short to reach full speed, switch to triangular profile
+        if (targetDistance < accelerationDistance + decelerationDistance) {
+            decelerationTime = Math.sqrt((2 * maxAcceleration * targetDistance) / (Math.pow(maxDeceleration, 2) + maxAcceleration * maxDeceleration));
+            accelerationTime = (maxDeceleration / maxAcceleration) * decelerationTime;
+
+            // Recalculate distances for triangular profile
+            accelerationDistance = maxAcceleration * Math.pow(accelerationTime, 2) / 2;
+            decelerationDistance = maxDeceleration * Math.pow(decelerationTime, 2) / 2;
+
+            // Max velocity achieved will be less than input max velocity
+            maxVelocity = maxAcceleration * accelerationTime;
         }
-        double dCruise = desiredD-dAccel-dDecel;
-        double deltaTCruise = dCruise/vMax;
-        double deltaT = deltaTAccel+deltaTCruise+deltaTDecel;
-        this.desiredD = desiredD;
-        this.vMax = vMax;
-        this.dAccel = dAccel;
-        this.dDecel = dDecel;
-        this.aAccel = aAccel;
-        this.aDecel = aDecel;
-        this.dCruise = dCruise;
-        this.deltaTCruise = deltaTCruise;
-        this.deltaT = deltaT;
-        this.deltaTAccel = deltaTAccel;
-        this.deltaTDecel = deltaTDecel;
-        if (reverse){
-            this.reverse=-1;
+
+        // Distance and time for the cruising (constant velocity) phase
+        double cruiseDistance = targetDistance - accelerationDistance - decelerationDistance;
+        double cruiseTime = cruiseDistance / maxVelocity;
+
+        // Total time for the full motion
+        double totalTime = accelerationTime + cruiseTime + decelerationTime;
+
+        // Stores all values in classes
+        this.targetDistance = targetDistance;
+        this.maxVelocity = maxVelocity;
+        this.accelerationDistance = accelerationDistance;
+        this.decelerationDistance = decelerationDistance;
+        this.maxAcceleration = maxAcceleration;
+        this.maxDeceleration = maxDeceleration;
+        this.cruiseDistance = cruiseDistance;
+        this.cruiseTime = cruiseTime;
+        this.totalTime = totalTime;
+        this.accelerationTime = accelerationTime;
+        this.decelerationTime = decelerationTime;
+
+        // Set direction multiplier based on `reverse`
+        if (reverse) {
+            this.reverse = -1;
         } else {
-            this.reverse=1;
+            this.reverse = 1;
         }
 
     }
-    public double getPos(double t){
-        double tCruise = t-deltaTAccel;
-        double tDecel = t-deltaTAccel-deltaTCruise;
-        if (t > deltaT){
-            return desiredD*reverse;
-        } else if (t <= deltaTAccel) {
-            return aAccel*Math.pow(t,2)/2*reverse;
-        } else if (deltaTAccel<t&& t<=deltaTAccel+deltaTCruise){
-            return dAccel+vMax*tCruise*reverse;
-        } else if (deltaTAccel+deltaTCruise<t&&t<=deltaT){
-            return (dAccel+dCruise+vMax*tDecel-aDecel*Math.pow(tDecel,2)/2)*reverse;
+
+    /**
+     * Returns the position (distance traveled) at a given time.
+     *
+     * @param time Time since start of motion
+     * @return Position in distance units (positive or negative based on direction)
+     */
+
+    public double getPosition(double time) {
+        double cruiseTimePassed = time - accelerationTime;
+        double timeDeceleration = time - accelerationTime - cruiseTime;
+
+        // If motion is complete, return to final target distance
+        if (time > totalTime) {
+            return targetDistance * reverse;
         }
+
+        // Acceleration phase
+        else if (time <= accelerationTime) {
+            return maxAcceleration * Math.pow(time, 2) / 2 * reverse;
+        }
+
+        // Constant velocity (cruise) phase
+        else if (accelerationTime < time && time <= accelerationTime + cruiseTime) {
+            return accelerationDistance + maxVelocity * cruiseTimePassed * reverse;
+        }
+
+        // Deceleration phase
+        else if (accelerationTime + cruiseTime < time && time <= totalTime) {
+            return (accelerationDistance + cruiseDistance + maxVelocity * timeDeceleration - maxDeceleration * Math.pow(timeDeceleration, 2) / 2) * reverse;
+        }
+
+        // Should not reach here unless input is invalid
         return -1;
     }
-    public double getVelocity(double t){
-        if (t>deltaT){
+
+    /**
+     * Returns the velocity at a given time.
+     *
+     * @param time Time since start of motion
+     * @return Velocity (positive or negative based on direction)
+     */
+
+    public double getVelocity(double time) {
+        if (time > totalTime) {
             return 0;
-        } else if (t<deltaTAccel){
-            return aAccel*t*reverse;
-        } else if (deltaTAccel<t&&t<=deltaTAccel+deltaTCruise){
-            return vMax*reverse;
-        } else if (deltaTAccel+deltaTCruise<t&&t<=deltaT){
-            double tDecel = t-deltaTAccel-deltaTCruise;
-            return (vMax-aDecel*tDecel)*reverse;
+        }
+
+        // Acceleration phase
+        else if (time < accelerationTime) {
+            return maxAcceleration * time * reverse;
+        }
+
+        // Constant velocity (cruise) phase
+        else if (accelerationTime < time && time <= accelerationTime + cruiseTime) {
+            return maxVelocity * reverse;
+        }
+
+        // Deceleration phase
+        else if (accelerationTime + cruiseTime < time && time <= totalTime) {
+            double timeDeceleration = time - accelerationTime - cruiseTime;
+            return (maxVelocity - maxDeceleration * timeDeceleration) * reverse;
         }
         return 10000;
     }
-    public double getAcceleration(double t){
-        if (t>deltaT){
+
+    /**
+     * Returns the acceleration at a given time.
+     *
+     * @param t Time since start of motion
+     * @return Acceleration (positive, zero, or negative depending on phase)
+     * <p>
+     * I'm pretty sure that t is time, but I'm unsure how to change the variable, because whenever
+     * I attempt to change it, it brings up errors
+     */
+
+    public double getAcceleration(double t) {
+        if (t > totalTime) {
             return 0;
-        } else if (t<=deltaTAccel){
-            return aAccel*reverse;
-        } else if (deltaTAccel<t&&t<=deltaTAccel+deltaTCruise){
-            return 0;
-        } else if (deltaTAccel+deltaTCruise<t&&t<=deltaT){
-            return -aDecel*reverse;
         }
-        return -1;
+
+        // Acceleration phase
+        else if (t <= accelerationTime) {
+            return maxAcceleration * reverse;
+        }
+
+        // Cruise phase
+        else if (accelerationTime < t && t <= accelerationTime + cruiseTime) {
+            return 0;
+        }
+
+        // Deceleration phase
+        else if (accelerationTime + cruiseTime < t && t <= totalTime) {
+            return -maxDeceleration * reverse;
+        }
+
+        return -1; // Fallback for invalid input
     }
-    public double getTime(){
-        return deltaT;
+
+    /**
+     * @return Total time of the motion profile
+     */
+
+    public double getTime() {
+        return totalTime;
     }
-    public int isReverse(){
+
+    /**
+     * @return Direction of the profile: 1 = forward, -1 = reverse
+     */
+
+    public int isReverse() {
         return reverse;
     }
-    }
+}
