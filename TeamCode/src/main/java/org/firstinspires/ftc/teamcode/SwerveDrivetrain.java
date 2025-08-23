e7fc303d625a31634cefaa3409faaf0868edafc9
AsherChilli <115118008+AsherChilli@users.noreply.github.com>
2025-08-23 12:36:33 -0500

    Update SwerveDrivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDrivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDrivetrain.java
@@ -2,11 +2,14 @@ package org.firstinspires.ftc.teamcode;
 
 
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 import java.util.ArrayList;
 import java.util.Vector;
@@ -20,6 +23,7 @@ public class SwerveDrivetrain extends OpMode {
   DcMotorEx fl;
   Servo frs;
   Servo fls;
+  int targetPosition = 0;
 
 
 
@@ -30,6 +34,12 @@ public class SwerveDrivetrain extends OpMode {
       fl = hardwareMap.get(DcMotorEx.class, "fl");
       frs = hardwareMap.get(Servo.class, "frs");
       fls = hardwareMap.get(Servo.class, "fls");
+      frs.setPosition(0.5);
+      fls.setPosition(0.5);
+      fr.setTargetPosition(targetPosition);
+      fl.setTargetPosition(targetPosition);
+      fr.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+      fl.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
     @Override
@@ -45,13 +55,24 @@ public class SwerveDrivetrain extends OpMode {
     @Override
     public void loop() {
       double power = Math.sqrt(gamepad1.left_stick_x * gamepad1.left_stick_x + gamepad1.left_stick_y * gamepad1.left_stick_y);
-      double angle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x)/Math.PI;
+      double angle = 2 - Math.atan2(gamepad1.right_stick_y, gamepad1.right_stick_x)/Math.PI-1;
 
-      frs.setPosition(angle+0.3);
-      fls.setPosition(angle-0.4);
+      frs.setPosition(angle%1);
+      fls.setPosition(angle%1);
+      fr.setTargetPosition(targetPosition);
+      fl.setTargetPosition(targetPosition);
+      fr.setPower(.5);
+      fl.setPower(.5);
 
-      fr.setPower(power);
-      fl.setPower(power);
+      telemetry.addData("Angle", angle);
 
+      if (angle < 1) {
+        targetPosition -= (int) power;
+      }
+      else {
+        targetPosition += (int) power;
+      }
+
+      telemetry.update();
     }
 }
