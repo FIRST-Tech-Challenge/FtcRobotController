72c4f5b0b6d5f49cda022f9bfec2563f0488f2af
andrew-cua <andrew@cuafamily.com>
2025-08-24 13:28:09 -0400

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+
+public class Teleop extends CommandOpMode {
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+
+}
