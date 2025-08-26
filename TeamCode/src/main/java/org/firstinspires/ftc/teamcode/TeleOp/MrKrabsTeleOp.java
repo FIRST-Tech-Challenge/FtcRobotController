623215fd73ea46822e70d56c86fccb93d22d6d78
stenzelkhai <stenzelkhai@gmail.com>
2025-08-25 19:21:44 -0700

    Update MrKrabsTeleOp.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MrKrabsTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MrKrabsTeleOp.java
@@ -6,6 +6,9 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Delivery;
@@ -39,32 +42,43 @@ public class MrKrabsTeleOp extends RoboMom {
     private double extension = 0.25;
     private int PITCH_INCREMENT = 10;
 
+    private DcMotor vertMotor;
+
+    int deliveryPosition;
+    int deliveryError;
+
+    private Servo claw;
+
     private boolean controlsRelinquished = false;
     private final double DRIVE_DEADZONE = 0.05;
     private final double SCORE_SPEED_SCALAR = 0.2;
 
     private double speedScalar = 1;
 
-    //This is the code to add rr actions
-    private FtcDashboard dash = FtcDashboard.getInstance();
-    private List<Action> runningActions = new ArrayList<>();
+
     @Override
     public void runOpMode() {
         super.runOpMode();
 
-        delivery = new Delivery(this, false);
-
         drivetrainFunctions = new DrivetrainFunctions(this);
 
-//        intake = new Intake(this);
+        vertMotor = hardwareMap.get(DcMotor.class, "deliverySlide");
+        vertMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        vertMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        vertMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
+        claw = hardwareMap.get(Servo.class, "claw");
 
-//        intake.unbrakePitch();
+//        delivery = new Delivery(this, true);
+
 
         waitForStart();
 
         while (opModeIsActive()){
-            TelemetryPacket packet = new TelemetryPacket();
+
+            deliveryPosition = vertMotor.getCurrentPosition();
+            deliveryError = targetPosition - deliveryPosition;
+            targetPosition = Math.max(Math.min(targetPosition, 2300), 0);
 
             //driver 1
             //slow down power if bumper is pressed
@@ -77,7 +91,7 @@ public class MrKrabsTeleOp extends RoboMom {
             }
 
             if (!controlsRelinquished) {
-                float leftX = gamepad1.left_stick_x;
+                float leftX = -gamepad1.left_stick_x;
                 float leftY = gamepad1.left_stick_y;
                 float rightX = gamepad1.right_stick_x;
                 float rightY = gamepad1.right_stick_y;
@@ -93,9 +107,6 @@ public class MrKrabsTeleOp extends RoboMom {
             }
 
             //driver 2
-            slidePosition = delivery.getMotorPosition();
-            targetPosition = delivery.getMotorTargetPosition();
-
 
             switch (deliveryState) {
                 case DELIVERY_START:
@@ -104,16 +115,16 @@ public class MrKrabsTeleOp extends RoboMom {
                         targetPosition = 1470;//Intake from observation wall; FIXME: VALUE IS WRONG
                     }
                     if (gamepad2.x) {
-//                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_LIFT;
-//                        targetPosition = 4120;//hook onto high bar
+                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_LIFT;
+                        targetPosition = 2000;//hook onto high bar
                     }
                     if (gamepad2.y) {
-//                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_LIFT;
-//                        targetPosition = 300;//SET_3_HEIGHT;
+                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_LIFT;
+                        targetPosition = 300;//SET_3_HEIGHT;
                     }
                     if (gamepad2.b) {
-//                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_RETRACT;
-//                        deliveryTimer.reset();
+                        deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_RETRACT;
+                        deliveryTimer.reset();
                     }
 
 //                    if (gamepad2.right_bumper && !dumped && deliveryFunctions.getMotorPositionByIndex(0) > deliveryFunctions.CARRIAGE_OUTSIDE_CHASSIS) {
@@ -126,139 +137,78 @@ public class MrKrabsTeleOp extends RoboMom {
                     break;
 
                 case DELIVERY_LIFT:
-                    /*
-                    //if both motors are within stop threshold
-                    if
-                    (targetPosition - leftMotorPosition <= deliveryFunctions.TICK_STOP_THRESHOLD
-                            &&
-                            targetPosition - rightMotorPosition <= deliveryFunctions.TICK_STOP_THRESHOLD) {
-                        deliveryState = CenterStageTeleOp.DeliveryState.DELIVERY_START;
-                        dumped = false;
-                        secondDumped = false;
-                    } else {
-                        //Still going
-                    }
-                    */
+                    if(Math.abs(deliveryError) <= 5) deliveryState = DeliveryState.DELIVERY_START;
                     break;
 
                 case DELIVERY_RETRACT:
-                    /*
-                    retracting = true;
-                    if(deliveryTimer.seconds() <= 0.5){
-                        deliveryFunctions.SetWristPosition(deliveryFunctions.servoDodge);
-                        break;
-                    }else{
-                        targetPosition = LIFT_LOW;
-                        if (deliveryFunctions.getMotorPositionByIndex(0) < deliveryFunctions.CARRIAGE_DODGE) {
-                            deliveryFunctions.SetWristPosition(deliveryFunctions.servoIn);
-                        } else {
-                            deliveryFunctions.SetWristPosition(deliveryFunctions.servoDodge);
-                        }
-
-                        //if both motors are within stop threshold
-                        if
-                        (leftMotorPosition - targetPosition <= deliveryFunctions.TICK_STOP_THRESHOLD
-                                &&
-                                rightMotorPosition - targetPosition <= deliveryFunctions.TICK_STOP_THRESHOLD) {
-
-                            deliveryState = CenterStageTeleOp.DeliveryState.DELIVERY_START;
-                            retracting = false;
-                        }
+                    targetPosition = 0;
+                    if(deliveryPosition <= 10) {
+                        vertMotor.setPower(0);
+                        deliveryState = DeliveryState.DELIVERY_START;
                     }
-
-                     */
 
                     break;
 
             }
 
             telemetry.addData("Delivery State: ", deliveryState);
+            telemetry.addData("Delivery Position: ", deliveryPosition);
+            telemetry.addData("Delivery Error: ", deliveryError);
             telemetry.addData("Delivery Timer: ", deliveryTimer.seconds());
+            telemetry.addData("Delivery Power: ", vertMotor.getPower());
 
 
-            //Adds rr actions
-            List<Action> newActions = new ArrayList<>();
-            for (Action action : runningActions) {
-                action.preview(packet.fieldOverlay());
-                if (action.run(packet)) {
-                    newActions.add(action);
-                }
-            }
-            runningActions = newActions;
-
-            dash.sendTelemetryPacket(packet);
 
             //MANUAL
             if (Math.abs(gamepad2.right_stick_y) >= DRIVE_DEADZONE) {
+                targetPosition = Math.max(Math.min(targetPosition, deliveryPosition+200), deliveryPosition-200);
                 deliveryState = MrKrabsTeleOp.DeliveryState.DELIVERY_START;
 
-                if (slidePosition > delivery.lowPosition) {
+                if (deliveryError < 1000) {
                     targetPosition -= gamepad2.right_stick_y * 35;
-                } else {
-                    targetPosition -= gamepad2.right_stick_y * 25;
                 }
             }
 
-            if (Math.abs(gamepad2.left_stick_y) >= DRIVE_DEADZONE) {
-                if(gamepad2.left_stick_y > 0){
-//                    intake.incrementTargetAngleTicks(-PITCH_INCREMENT);
-                } else if(gamepad2.left_stick_y < 0){
-//                    intake.incrementTargetAngleTicks(+PITCH_INCREMENT);
-                }
-            }
-
-            if(gamepad2.dpad_left){
-//                intake.setTargetAngleTicks(0);
-            }
-
-            if(gamepad2.dpad_right){
-//                intake.setTargetAngleTicks(750);
-            }
-
-            float rightTrigger = gamepad2.right_trigger;
-            float leftTrigger = gamepad2.left_trigger;
-            if(rightTrigger > 0){
-                extension += rightTrigger/100;
-                extension = Math.max(0.01, Math.min(0.25, extension));
-//                intake.setTargetLengthServo(extension);
-            } else if(leftTrigger > 0){
-                extension -= leftTrigger/100;
-                extension = Math.max(0.01, Math.min(0.25, extension));
-//                intake.setTargetLengthServo(extension);
-            }
-
-//            intake.updateAngle();
-//            intake.updateLength();
 
             if(gamepad2.right_bumper){
-                delivery.clawClose();
+                clawClose();
             }else{
-                delivery.clawOpen();
+                clawOpen();
             }
 
-            if(gamepad2.x){
-//                intake.setEndEffectorSpeed(0);//in
-            }else if(gamepad2.b){
-//                intake.setEndEffectorSpeed(1);//out
-            }else{
-//                intake.setEndEffectorSpeed(0.5f);//stop
-            }
-
-            targetPosition = Math.max(delivery.bottomPosition, Math.min(delivery.topDelta, targetPosition));
-            delivery.setSlidesTargetPosition(targetPosition);
-
-            delivery.PControlPower(1);
-
-            //telemetry.addData("Target Position: ", targetPosition);
-            telemetry.addData("Slide Motor Position (in): ", String.format( "%.2f", delivery.getMotorPositionInches()) );
-            telemetry.addData("Slide Motor Target (in): ", String.format( "%.2f", delivery.getMotorTargetPositionInches()) );
-
-            telemetry.addData("Target Position in DeliveryFunctions: ", delivery.getMotorTargetPosition());
-            telemetry.addData("Slide Motor Position: ", slidePosition);
+            squidToPointDelivery();
+            telemetry.addData("Target Position: ", targetPosition);
 
             telemetry.addLine();
-            telemetry.addLine();
-//            telemetry.addData("Intake Extension: ", intake.getExtensionServoPosition());
+            telemetry.addData("Claw Position: ", claw.getPosition());
+            telemetry.update();
         }
+    }
+
+
+
+    void squidToPointDelivery()
+    {
+        double h = 1.5;
+
+        final double MAX_ERROR_FOR_FULL_POWER = 500.0;  //adjust
+        double norm = Math.min(1.0, Math.abs(deliveryError) / MAX_ERROR_FOR_FULL_POWER);
+        double mag = Math.sqrt(h*norm);
+        double power = Math.copySign(mag, deliveryError);
+        final double MIN_POWER = 0.08;  // tweak or set to 0 to disable
+        if (mag > 0 && mag < MIN_POWER) {
+            power = Math.copySign(MIN_POWER, deliveryError);
+        }
+        vertMotor.setPower(power);
+    }
+
+    void clawClose()
+    {
+        claw.setPosition(0.235);
+    }
+
+    void clawOpen()
+    {
+        claw.setPosition(0.0);
     }
 }
