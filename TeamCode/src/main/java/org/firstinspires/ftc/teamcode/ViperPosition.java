428cbec4a8ab1a5ec4ff90d3c06550ed7ac1cb37
Alex <47759254+acurra3398@users.noreply.github.com>
2025-08-26 19:08:08 -0400

    Fixed path for viper slide program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ViperPosition.java
@@ -5,17 +5,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "ViperslideTestAlex")
+@TeleOp(name = "ViperPosition-Patrick")
 public class ViperPosition extends LinearOpMode {
 
-    int position = 0;
     final double speedControl = 0.2;
 
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor Slides = hardwareMap.dcMotor.get("slides");
         Slides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        Slides.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Slides.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         Slides.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         telemetry.addData("Robot is ready!", "Skibidi Toliet Rizz!");
         telemetry.update();
@@ -23,23 +22,37 @@ public class ViperPosition extends LinearOpMode {
 
         double leftstick = gamepad1.left_stick_y;
         while (opModeIsActive()) {
-
-            leftstick = -gamepad1.left_stick_y;
-            leftstick = Math.min(leftstick, 0.2);
+            leftstick =  gamepad1.left_stick_y;
+            leftstick = Math.min(leftstick, 0.7);
+            leftstick = Math.max(leftstick, -0.7);
 
             // set position and limit it
-            position = Slides.getCurrentPosition();
-            if (position < 2910 && leftstick>0){
+            if (Slides.getCurrentPosition() > -3000 && leftstick < 0){
                 //if there is still space to go up
                 Slides.setPower(leftstick);
-            } else if (position > 0 && leftstick <0){
+                telemetry.addData("Still space to go up", Slides.getCurrentPosition());
+                telemetry.update();
+            } else if (Slides.getCurrentPosition() < 0 && leftstick > 0){
                 //if there is still space to go down
-                Slides.setPower(-leftstick);
-            } else {
+                telemetry.addData("Still space to go down", Slides.getCurrentPosition());
+                telemetry.update();
+                Slides.setPower(leftstick);
+            } else if (Slides.getCurrentPosition() < -3000 && leftstick < 0) {
                 Slides.setPower(0);
+                telemetry.addData("no space to go up", Slides.getCurrentPosition());
+                telemetry.update();
+            } else if (Slides.getCurrentPosition() > -2 && leftstick > 0) {
+                Slides.setPower(0);
+                telemetry.addData("no space to go down", Slides.getCurrentPosition());
+                telemetry.update();
+            }
+            else {
+                Slides.setPower(0);
+                telemetry.addData("else", Slides.getCurrentPosition() );
+                telemetry.update();
             }
 
-            telemetry.addData("Current position", (Slides.getCurrentPosition() * -1));
+            telemetry.addData("Current position", Slides.getCurrentPosition());
             telemetry.addData("Stick position", leftstick);
             telemetry.update();
         }
