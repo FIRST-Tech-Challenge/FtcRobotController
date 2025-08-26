b13b4036e82293b207332e5ceecf43f02671c938
Chris Lemoine <chrismlemoine@gmail.com>
2025-08-26 07:19:21 -0500

    feat(drivetrain): establish robot-centric & field-centric drive with external hardware class
    - Implemented working RobotHardware class for centralized hardware mapping
    - Added Robot-Centric TeleOp control
    - Added Field-Centric TeleOp control using IMU orientation
    - Integrated drive methods into RobotHardware for cleaner opmode code
    - Verified drivetrain response in TeleOp with telemetry feedback

--- Team00000/src/main/java/org/firstinspires/ftc/team00000/RobotHardware.java
+++ Team00000/src/main/java/org/firstinspires/ftc/team00000/RobotHardware.java
@@ -1,3 +1,32 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.team00000;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
@@ -6,58 +35,36 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
-/*
- * This file works in conjunction with the External Hardware Class sample called: ConceptExternalHardwareClass.java
- * Please read the explanations in that Sample about how to use this class definition.
+/**
+ * RobotHardware
  *
- * This file defines a Java Class that performs all the setup and configuration for a sample robot's hardware (motors and sensors).
- * It assumes three motors (left_drive, right_drive and arm) and two servos (left_hand and right_hand)
+ * PURPOSE:
+ * - Centralized hardware mapping and drivetrain helpers so OpModes stay clean.
+ * - Provide both robot-Centric and Field-Centric drive utilities
  *
- * This one file/class can be used by ALL of your OpModes without having to cut & paste the code each time.
- *
- * Where possible, the actual hardware objects are "abstracted" (or hidden) so the OpMode code just makes calls into the class,
- * rather than accessing the internal hardware directly. This is why the objects are declared "private".
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with *exactly the same name*.
- *
- * Or... In OnBot Java, add a new file named RobotHardware.java, select this sample, and select Not an OpMode.
- * Also add a new OpMode, select the sample ConceptExternalHardwareClass.java, and select TeleOp.
- *
+ * STUDENT CHECKLIST:
+ * 1) TODO(STUDENTS): Update motor names to match your Robot Configuration
+ * 2) TODO(STUDENTS): Verify IMU orientation (logo/USB direction) match your hub mount.
+ * 3) TODO(STUDENTS); 
  */
 
 public class RobotHardware {
 
-    /* Declare OpMode members. */
-    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+    private LinearOpMode myOpMode = null;
 
-    // Define Motor and Servo objects  (Make them private so they can't be accessed externally)
     private DcMotor frontLeftDrive, backLeftDrive, frontRightDrive, backRightDrive;
     private IMU imu;
 
-    // Define a constructor that allows the OpMode to pass a reference to itself.
     public RobotHardware(LinearOpMode opmode) {
         myOpMode = opmode;
     }
 
-    /**
-     * Initialize all the robot's hardware.
-     * This method must be called ONCE when the OpMode is initialized.
-     * <p>
-     * All of the hardware devices are accessed via the hardware map, and initialized.
-     */
     public void init()    {
-        // Define and Initialize Motors (note: need to use reference to actual OpMode).
         frontLeftDrive = myOpMode.hardwareMap.get(DcMotor.class, "front_left_drive");
         backLeftDrive = myOpMode.hardwareMap.get(DcMotor.class, "back_left_drive");
         frontRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "front_right_drive");
         backRightDrive = myOpMode.hardwareMap.get(DcMotor.class, "back_right_drive");
 
-        /*
-         Define how the hub is mounted on the robot to get the correct Yaw, Pitch, and Roll values. There are two input
-         parameters required to fully specify the orientation. (1) the first parameter specifies the direction of the
-         printed logo on the hub is pointing. (2) the second parameter specifies the direction the USB connector on the
-         hub is pointing. All directions are relative to the robot, and left/right is as-viewed from behind the robot.
-         */
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
                 RevHubOrientationOnRobot.LogoFacingDirection.UP,
                 RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
@@ -65,7 +72,6 @@ public class RobotHardware {
         imu = myOpMode.hardwareMap.get(IMU.class, "imu");
         imu.initialize(parameters);
 
-        // Reset the IMU when initializing the hardware class
         imu.resetYaw();
 
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
@@ -92,25 +98,14 @@ public class RobotHardware {
         myOpMode.telemetry.update();
     }
 
-    /**
-     * Calculates the left/right motor powers required to achieve the requested
-     * robot motions: Axial, Lateral and Yaw. Then sends these power levels to the motors.
-     *
-     * @param axial     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     * @param lateral   Right/Left driving power (-1.0 to 1.0) +ve is right
-     * @param yaw       Right/Left turning power (-1.0 to 1.0) +ve is CW
-     */
     public void driveRobotCentric(double axial, double lateral, double yaw) {
         double max;
 
-        // Combine drive and turn for blended motion.
         double frontLeftPower  = axial + lateral + yaw;
         double frontRightPower = axial - lateral - yaw;
         double backLeftPower   = axial - lateral + yaw;
         double backRightPower  = axial + lateral - yaw;
 
-        // Normalize the values so no wheel power exceeds 100%
-        // This ensures that the robot maintains the desired motion.
         max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
         max = Math.max(max, Math.abs(backLeftPower));
         max = Math.max(max, Math.abs(backRightPower));
@@ -125,14 +120,6 @@ public class RobotHardware {
         setDrivePower(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
     }
 
-    /**
-     * Calculates the left/right motor powers required to achieve the requested
-     * robot motions: Axial, Lateral and Yaw. Then sends these power levels to the motors.
-     *
-     * @param axial     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     * @param lateral   Right/Left driving power (-1.0 to 1.0) +ve is right
-     * @param yaw       Right/Left turning power (-1.0 to 1.0) +ve is CW
-     */
     public void driveFieldCentric(double axial, double lateral, double yaw) {
         double max;
         double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
@@ -159,16 +146,7 @@ public class RobotHardware {
         setDrivePower(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
     }
 
-    /**
-     * Pass the requested wheel motor powers to the appropriate hardware drive motors.
-     *
-     * @param frontLeftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     * @param frontRightWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     * @param backLeftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     * @param backRightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
-     */
     public void setDrivePower(double frontLeftWheel, double frontRightWheel, double backLeftWheel, double backRightWheel) {
-        // Output the values to the motor drives.
         frontLeftDrive.setPower(frontLeftWheel);
         frontRightDrive.setPower(frontRightWheel);
         backLeftDrive.setPower(backLeftWheel);
--- Team00000/src/main/java/org/firstinspires/ftc/team00000/teleop/FieldCentric.java
+++ Team00000/src/main/java/org/firstinspires/ftc/team00000/teleop/FieldCentric.java
@@ -1,3 +1,32 @@
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package org.firstinspires.ftc.team00000.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -8,8 +37,6 @@ import org.firstinspires.ftc.team00000.RobotHardware;
 
 public class FieldCentric extends LinearOpMode {
 
-    // Create a RobotHardware object to be used to access robot hardware.
-    // Prefix any hardware functions with "robot." to access this class.
     RobotHardware robot = new RobotHardware(this);
 
     @Override
@@ -18,27 +45,16 @@ public class FieldCentric extends LinearOpMode {
         double lateral  = 0;
         double yaw      = 0;
 
-        // initialize all the hardware, using the hardware class. See how clean and simple this is?
         robot.init();
-
-        // Send telemetry message to signify robot waiting;
-        // Wait for the game to start (driver presses START)
         waitForStart();
 
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-
-            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
-            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
-            // This way it's also easy to just drive straight, or just turn.
-            axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
+            axial   = -gamepad1.left_stick_y;
             lateral =  gamepad1.left_stick_x;
             yaw     =  gamepad1.right_stick_x;
 
-            // Combine drive and turn for blended motion. Use RobotHardware class
             robot.driveFieldCentric(axial, lateral, yaw);
 
-            // Send telemetry messages to explain controls and show robot status
             telemetry.addData("Drive/Strafe", "Left Stick");
             telemetry.addData("Turn", "Right Stick");
             telemetry.addData("-", "-------");
@@ -48,7 +64,6 @@ public class FieldCentric extends LinearOpMode {
             telemetry.addData("Turn Power",  "%.2f", yaw);
             telemetry.update();
 
-            // Pace this loop so hands move at a reasonable speed.
             sleep(50);
         }
     }
