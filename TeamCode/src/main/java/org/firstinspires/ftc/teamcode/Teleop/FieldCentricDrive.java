968555e48448d06c954bbe95bb3a9cb2e0b3bf54
TreeSquirrles <herkyaaron@gmail.com>
2025-08-29 20:37:04 -0700

    added Robot class! go wild. Check the FieldCentricDrive.java to see how to use it this season.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FieldCentricDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/FieldCentricDrive.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
-public class FieldCentricDrive {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.Modules.Robot;
+
+public class FieldCentricDrive extends LinearOpMode {
+
+    public Robot robot;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        robot = new Robot(hardwareMap);
+
+        telemetry.addData("Robot status", "succesfully initiated");
+        telemetry.update();
+
+        waitForStart();
+        if (isStopRequested()) return;
+
+        telemetry.clear();
+        telemetry.addData("Robot status", "Started!");
+        telemetry.update();
+
+        while (opModeIsActive()){
+            fieldCentricDrive();
+        }
+    }
+
+
+
+    private void fieldCentricDrive() {
+        double slowdown = gamepad1.right_trigger > 0 ? 0.25 : 1;
+        double y = -gamepad1.left_stick_y * slowdown;
+        double x = gamepad1.left_stick_x * 1.1 * slowdown;
+        double rx = gamepad1.right_stick_x * slowdown;
+
+        double heading = robot.getDrivetrain().getRobotHeading(AngleUnit.RADIANS);
+
+
+        double rotX = x * Math.cos(-heading) - y * Math.sin(-heading);
+        double rotY = x * Math.sin(-heading) + y * Math.cos(-heading);
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+        double frWheelPower = (rotY - rotX - rx) / denominator;
+        double flWheelPower = (rotY + rotX + rx) / denominator;
+        double brWheelPower = (rotY + rotX - rx) / denominator;
+        double blWheelPower = (rotY - rotX + rx) / denominator;
+
+        robot.getDrivetrain().setWheelPowers(flWheelPower, frWheelPower, brWheelPower, blWheelPower);
+
+        if (gamepad1.y) {
+            robot.getDrivetrain().resetImu();
+        }
+    }
 }
