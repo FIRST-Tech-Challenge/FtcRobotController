256efea95829b1f8a493f112d5cfa615506c3f1e
pikachu7911 <young.seungyoo.kj@gmail.com>
2025-08-29 19:57:43 -0700

    Arm now has two default functions: getArmAng (gets angle arm is at) and setArmAng (sets arm angle). If setArmAng has some weird conversion shenanigans, then getArmAng probably also needs that too so it makes enough-ish sense

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Arm.java
@@ -1,4 +1,5 @@
-package org.firstinspires.ftc.teamcode.drive.modules;
+package org.firstinspires.ftc.teamcode.modules;
+
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Arm {
@@ -12,5 +13,26 @@ public class Arm {
         this.rArm = rClawArm;
     }
 
-    // getter setters of angle and position idk what else to do
+    public double getArmAng() {
+        return (double) (lArm.getPosition() + rArm.getPosition()) /2;
+        // This returns the *average* of the two arms' positions
+        //     to return the 'true' position of the arm
+    }
+    public void setArmAng(double newAng) {
+        armAng = newAng /* * conversionRatio*/; // Only if there is a conversion ratio
+        // Any weird conversion shenanigans can be added later around here if you need it
+        // Here's a weird ahh thing from 2024-2025 year
+        //
+        // double diff = (slidesDefaultAngle - slidePivotAng/divFactor /*weird conversion shenanigans*/);
+        // double actualPosition = armAngleToServo((armAng + (diff * conversionRatio))); // we want positive numbers to correlate with a upward swing
+        //
+        // slidesDefaultAngle - That's if the slides can change angle stuff
+        //     The default angle for last year was 90
+        // divFactor - I literally don't know man
+        //     I didn't write this, and I don't know why it was there
+        //     Last year divFactor was 10
+        // These values were hardcoded in the 24-25 year, just so y'all know
+        lArm.setPosition(armAng);
+        rArm.setPosition(armAng);
+    }
 }
