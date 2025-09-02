11cf877efc2329552dc38fdd0334fe06de3998e1
juniperrobotics <271018@sycamoreschools.org>
2025-09-02 18:12:30 -0400

    Update to fieldcentric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricCode.java
@@ -10,6 +10,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
 public class FieldCentricCode extends LinearOpMode {
+
     @Override
     public void runOpMode() {
         DcMotor FLM = hardwareMap.dcMotor.get("frontLeftMotor");
@@ -22,10 +23,40 @@ public class FieldCentricCode extends LinearOpMode {
 
         IMU imu = hardwareMap.get(IMU.class, "imu");
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.
-        )
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
 
 
-        )
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double ls = gamepad1.left_stick_x;
+            double rs = gamepad1.right_stick_x;
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = ls * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = ls * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rs), 1);
+            double frontLeftPower = (rotY + rotX + rs) / denominator;
+            double backLeftPower = (rotY - rotX + rs) / denominator;
+            double frontRightPower = (rotY - rotX - rs) / denominator;
+            double backRightPower = (rotY + rotX - rs) / denominator;
+
+            FLM.setPower(frontLeftPower);
+            BLM.setPower(backLeftPower);
+            FRM.setPower(frontRightPower);
+            BRM.setPower(backRightPower);
+        }
     }
 }
