435eed75cfda7e827b04958b3a34c9e75cdb1c59
dzli <dazhi.li@gmail.com>
2025-09-02 20:31:04 -0700

    add Panels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotConfig.java
@@ -2,30 +2,32 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.bylazar.configurables.annotations.Configurable;
 
+@Configurable
 public class RobotConfig {
     // Wheels config
-    public final static String frontLeftWheelName = "frontLeftMotor";
-    public final static DcMotor.Direction frontLeftWheelDirection = DcMotor.Direction.REVERSE;
-    public final static String frontRightWheelName = "frontRightMotor";
-    public final static DcMotor.Direction frontRightWheelDirection = DcMotor.Direction.FORWARD;
-    public final static String backLeftWheelName = "backLeftMotor";
-    public final static DcMotor.Direction backLeftWheelDirection = DcMotor.Direction.REVERSE;
-    public final static String backRightWheelName = "backRightMotor";
-    public final static DcMotor.Direction backRightWheelDirection = DcMotor.Direction.FORWARD;
-    public final static double countPerMotorRev = 537.7;
-    public final static double driveGearReduction = 1.0;
-    public final static double wheelDiameterInches = 4.0;
+    public static String frontLeftWheelName = "frontLeftMotor";
+    public static DcMotor.Direction frontLeftWheelDirection = DcMotor.Direction.REVERSE;
+    public static String frontRightWheelName = "frontRightMotor";
+    public static DcMotor.Direction frontRightWheelDirection = DcMotor.Direction.FORWARD;
+    public static String backLeftWheelName = "backLeftMotor";
+    public static DcMotor.Direction backLeftWheelDirection = DcMotor.Direction.REVERSE;
+    public static String backRightWheelName = "backRightMotor";
+    public static DcMotor.Direction backRightWheelDirection = DcMotor.Direction.FORWARD;
+    public static double countPerMotorRev = 537.7;
+    public static double driveGearReduction = 1.0;
+    public static double wheelDiameterInches = 4.0;
 
     // IMU config
-    public final static String imuName = "imu";
-    public final static RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
-    public final static RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
+    public static String imuName = "imu";
+    public static RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
+    public static RevHubOrientationOnRobot.UsbFacingDirection usbDirection = RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
     // camera
-    public final static boolean cameraEnabled = false;
-    public final static String cameraName = "limelight";
-    public final static double cameraHeight = 420;
-    public final static double cameraAngel = -60;
-    public final static double targetHeight = 35;
+    public static boolean cameraEnabled = false;
+    public static String cameraName = "limelight";
+    public static double cameraHeight = 420;
+    public static double cameraAngel = -60;
+    public static double targetHeight = 35;
 }
