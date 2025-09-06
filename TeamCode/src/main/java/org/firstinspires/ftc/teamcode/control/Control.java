41f62b236123b2b1e063d43b156dbba4fea5f4e4
Vikram <vikram.karamsetty@gmail.com>
2025-09-06 16:41:51 -0400

    added subsystem and control interfaces

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/Control.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.control;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public interface Control {
+    void update();
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Subsystem.java
@@ -0,0 +1,6 @@
+package org.firstinspires.ftc.teamcode.subsystem;
+
+public interface Subsystem {
+    void init();
+    void update();
+}
