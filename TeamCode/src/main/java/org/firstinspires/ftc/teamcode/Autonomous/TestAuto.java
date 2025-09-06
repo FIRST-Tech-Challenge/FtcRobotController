453119a850bb374f655b70f23918688b4912a45d
J A <james.atzberger@gmail.com>
2025-09-05 21:22:34 -0700

    Remove all the comments from TestAuto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.Modules.Drivetrain;
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Config
+@Autonomous(name = "TEST AUTO")
+public class TestAuto extends LinearOpMode {
+    private ElapsedTime timer = new ElapsedTime();
+    HardwareMap hardwareMap;
+    AutonomousRobot autoRobot;
+    Robot robot;
+    Drivetrain drivetrain;
+
+//    Path p = new Path.PathBuilder()
+//
+//    .addNewFullPoint(
+//            new Waypoint(1, 1, 90, 75, 1, DistanceUnit.INCH),
+//            AutonomousRobot.moveStates.ONLYACTION ,
+//            () -> {
+//
+//            },
+//            1)
+//
+//    .build();
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        autoRobot = new AutonomousRobot(hardwareMap);
+
+        drivetrain.startOdometry();
+        //autoRobot.setPath(p);
+        autoRobot.startPath();
+
+        waitForStart();
+
+        timer.reset();
+
+        while(opModeIsActive()){
+            autoRobot.step();
+        }
+    }
+
+}
