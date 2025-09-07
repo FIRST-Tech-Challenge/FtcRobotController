d50e64fb18ebae9dc10f7bd9cb1fa1cbd3e4635b
Sidharth Y <sid.d.yarli@gmail.com>
2025-09-07 14:39:29 -0500

    Made Tic-Tac-Toe Table
    playing tictactoe with kevin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Z_pattern.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class Z_pattern extends LinearOpMode {
+    private DcMotor FL_Drive;
+    private DcMotor FR_Drive;
+    private DcMotor BL_Drive;
+    private DcMotor BR_Drive;
+private
+}
--- TeamCode/src/main/res/raw/readme.md
+++ TeamCode/src/main/res/raw/readme.md
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 Place your sound files in this folder to use them as project resources.
+=======
+Place your sound files in this folder to use them as project resources
+>>>>>>> Stashed changes
