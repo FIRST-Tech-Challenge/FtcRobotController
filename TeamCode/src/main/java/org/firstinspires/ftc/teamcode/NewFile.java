d0bcd50453995c463bae8a1605951d7c5ac9476a
imxxge <imqgel2i2@gmail.com>
2026-07-23 13:20:32 +0900

    니가 뭘 작업했는지 쓰는곳

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewFile.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+@TeleOp(name = "NewFile")
+public class NewFile extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        if (opModeIsActive()) {
+            // Pre-run
+            while (opModeIsActive()) {
+                // OpMode loop
+            }
+        }
+    }
+}
