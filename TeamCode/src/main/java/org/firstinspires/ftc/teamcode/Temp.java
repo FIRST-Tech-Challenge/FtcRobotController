090ce4f677f4c5244a70f20f8689f8db778d5b97
DragonFire32 <maddie32612@gmail.com>
2026-09-11 19:03:12 -0500

    Intake Code Vers1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Temp.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Temp {
+
+    public static int testJava17(Object value) {
+        if (value instanceof String text) {
+            return text.length();
+        }
+
+        return 0;
+    }
+}
