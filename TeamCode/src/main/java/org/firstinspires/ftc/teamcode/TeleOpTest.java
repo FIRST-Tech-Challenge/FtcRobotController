33053160e78b069fb6bcbcdb8c1c05235dd79adc
nanobit <nanob1t@proton.me>
2026-09-13 15:27:26 -0500

    added linear op mode file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Test TeleOp", group = "Linear OpMode")
+public class TeleOpTest extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        return;
+    }
+}
